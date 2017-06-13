7c7d6bb5e75161cb1e24c759a5f4bdb9509ffb81
Jan Dvorak <jdvorak@literaryuniverse.com>
2017-06-13 21:33:56 +0200

    Lang for blog admin

--- cs.js
+++ cs.js
@@ -1159,6 +1159,32 @@ export default {
     few {jsou # údaje}
     many {je # údajů}
   }.`,
+
+  // added 2017-06-10
+  'blog.noneMsg': `You currently don't have {type, select,
+    org {any blog}
+    universe {a blog for your universe}
+    user {a blog}
+  }.`,
+  'blog.create': 'Create a new blog',
+  'blog.settings': `{type, select,
+    org {Blog}
+    universe {Universe page}
+    user {Blog}
+  } settings`,
+  'blog.description': 'Blog introduction',
+  'blog.theme': 'Theme',
+  'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+  'blog.posts.total': `There {total, plural,
+    zero {are no posts}
+    one {is one post}
+    other {are # posts}
+  } in this blog.`,
+  'blog.post.create': 'Create a new post',
+  'blog.post.update': 'Editing {title}',
+  'blog.post.text': 'Post text',
+  'common.slug': 'Slug',
+  'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
 };
 
 /* eslint-enable max-len */
