0d4292115ee7def9eb4b3cd159f2e0de2f357a2e
Jan Dvorak <jdvorak@literaryuniverse.com>
2021-01-06 17:48:43 +0100

    Fix quotation marks on short email texts

--- cs_emails.js
+++ cs_emails.js
@@ -3,7 +3,7 @@
  */
 /* eslint-disable max-len */
 export default {
-  'email.enrollAccount.subject': `Pozvánka na Literary Universe pro {username}`,
+  'email.enrollAccount.subject': 'Pozvánka na Literary Universe pro {username}',
   'email.enrollAccount.message': `Zdravím {username},\n\n
     Chtěli bychom Vás pozvat na Literary Universe! Vytvořili jsme pro Vás speciální osobní účet na kterém můžete otestovat všechny funkce Literary Universe! Doufáme že s námi budete vytvářet další generaci nástrojů pro kreativní psaní a čtení!
     To activate your account, simply click the link below:\n\n{url}`,
@@ -11,6 +11,6 @@ export default {
   'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
         Please follow the link bellow to reset your password:\n\n{url}`,
   'email.verifyEmail.subject': 'Literary Universe potvrzení e-mailu',
-  'email.verifyEmail.message': `Hello {username}!\n\n Please verify your e-mail address by clicking on the link bellow:\n\n{url}`,
+  'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link bellow:\n\n{url}',
 }
 /* eslint-enable max-len */
--- en_emails.js
+++ en_emails.js
@@ -3,7 +3,7 @@
  */
 /* eslint-disable max-len */
 export default {
-  'email.enrollAccount.subject': `Invitation to Literary Universe for {username}`,
+  'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
   'email.enrollAccount.message': `Hello {username}\n\n
     We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
     To activate your account, simply click the link below:\n\n{url}`,
@@ -11,6 +11,6 @@ export default {
   'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
         Please follow the link bellow to reset your password:\n\n{url}`,
   'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-  'email.verifyEmail.message': `Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}`,
+  'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}'
 }
 /* eslint-enable max-len */
