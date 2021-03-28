388a1d1edb954c5cdac0d88380aaf9f07310edbf
Jan Dvorak <storytellercz@protonmail.com>
2021-03-28 16:18:38 +0200

    New translations feed.js (Japanese)

--- ja/feed.js
+++ ja/feed.js
@@ -1,8 +1,8 @@
 export default {
-    'feed.name': 'Stream',
+    'feed.name': 'ストリーム',
     'feed.post.new': 'New post',
     'feed.post.like': 'Like post',
     'feed.post.unlike': 'Unlike post',
     'feed.post.send': 'Post',
-    'feed.empty': 'No entries in the feed.'
+    'feed.empty': 'フィードにエントリがありません。'
 };
