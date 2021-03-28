58108bd9f2616cd3e22ad71e89ea27760c6ad13b
Jan Dvorak <storytellercz@protonmail.com>
2021-03-28 21:25:55 +0200

    New translations pm.js (Japanese)

--- ja/pm.js
+++ ja/pm.js
@@ -1,22 +1,21 @@
 export default {
-    'pm.conversation': 'Conversation',
-    'pm.showolder': 'Show older messages',
-    'pm.errors.saysomething': 'You should really say something...',
-    'pm.errors.addrecipient': 'You need to add at least one other user.',
+    'pm.conversation': '会話',
+    'pm.showolder': '古いメッセージを表示',
+    'pm.errors.saysomething': 'あなたは本当に何かを言うべきです...',
+    'pm.errors.addrecipient': '少なくとも1人の他のユーザーを追加する必要があります。',
     'pm.message': 'メッセージ',
     'pm.send.message': 'メッセージを送信',
     'pm.messages.own': 'あなたのメッセージ',
     'pm.conversations.none': 'あなたは現在誰とも話していません。',
-    'pm.to': 'To:',
+    'pm.to': '受取人',
     'pm.with': 'Conversation with:',
     'pm.send': 'Send',
-    'pm.conversations.unreadCount': `You have {count, plural,
-    zero {no unread conversations}
-    one {one unread conversation}
-    other {# unread conversations}
-  }.`,
+    'pm.conversations.unreadCount': `未読の会話{count, plural,
+    zero {はありません}
+    other {が#件あります}
+  }`,
     'pm.form.minimumParticipants': 'You need to choose a recipient.',
-    'pm.form.maxParticipants': 'You can have maximum of 10 people in a conversation.',
-    'pm.new.aria': 'New conversation',
-    'pm.new.buttonTitle': 'Start a new conversation'
+    'pm.form.maxParticipants': '会話では最大10人まで参加できます。',
+    'pm.new.aria': '新しい会話',
+    'pm.new.buttonTitle': '新しい会話を始める'
 };
