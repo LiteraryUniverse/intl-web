96c25b6b5918b5604e00db3c37db16f23992e669
Jan Dvorak <storytellercz@protonmail.com>
2021-03-28 11:32:32 +0200

    New translations plans.js (Czech)

--- cs/plans.js
+++ cs/plans.js
@@ -32,99 +32,99 @@ export default {
     'plans.feature.scratchpad': 'Zápisník',
     'plans.feature.library': 'Knihovna',
     'plans.feature.blog': 'Osobní a vesmírné blogy',
-    'plans.feature.customBlogTheme': 'Customizable blog themes',
-    'plans.feature.advanceStats': 'Advanced statistics',
-    'plans.feature.challenges': 'Writing challenges',
-    'plans.feature.newFeaturesVote': 'Vote on new features',
-    'plans.feature.newFeaturesSuggest': 'Propose new features',
-    'plans.feature.accessToBeta': 'Access Beta features',
-    'plans.feature.userGroups': 'User groups',
-    'plans.feature.privateUserGroups': 'Create private and hidden user groups',
-    'plans.currency.select': 'Select currency',
-    'plans.subscription.ownAlready': 'You currently have this plan',
-    'plans.subscription.title': 'Subscription',
-    'plans.see': 'See available subscription plans',
-    'plans.current.none': 'You are currently not supporting Literary Universe.',
-    'plans.current.buying': 'It might take a few moments to verify your purchase. We appreciate your patience.',
-    'plans.current.heading': 'Current subscription plan',
-    'payment.methods.title': 'Payment methods',
-    'payment.methods.tabs': 'Payment options',
-    'payment.option.title': 'Payment option',
-    'payment.option.notFount': 'You don\'t have any payment option saved.',
-    'payment.option.create': 'Add a new payment option',
-    'payment.option.description': 'Here is a list of your saved payment methods. You can save new payment methods here for faster checkout. Literary Universe does not store nor process your payment credentials like credit card number.',
-    'payment.history.title': 'Payment history',
-    'payment.history.tab': 'History',
-    'payment.history.none': 'We don\'t have any records of past payments.',
+    'plans.feature.customBlogTheme': 'Vlastní vzhled pro blog',
+    'plans.feature.advanceStats': 'Pokročilé statistiky',
+    'plans.feature.challenges': 'Psací výzvy',
+    'plans.feature.newFeaturesVote': 'Volte pro novou funkcionalitu',
+    'plans.feature.newFeaturesSuggest': 'Navrhování nové funkcionality',
+    'plans.feature.accessToBeta': 'Přístup k modulům v Betě',
+    'plans.feature.userGroups': 'Uživatelské skupiny',
+    'plans.feature.privateUserGroups': 'Tvorba privátních a skrytých skupin',
+    'plans.currency.select': 'Vybrat měnu',
+    'plans.subscription.ownAlready': 'Momentálně máte tento plán',
+    'plans.subscription.title': 'Předplatné',
+    'plans.see': 'Zobrazit dostupné předplatné',
+    'plans.current.none': 'Momentálně nepodporujete Literary Universe.',
+    'plans.current.buying': 'Ověření Vašeho nákupu může chvíli trvat. Děkujeme za pochopení.',
+    'plans.current.heading': 'Aktuální plán předplatného',
+    'payment.methods.title': 'Způsoby plateb',
+    'payment.methods.tabs': 'Možnosti platby',
+    'payment.option.title': 'Možnosti platby',
+    'payment.option.notFount': 'Nemáte uloženy žádné možnosti platby.',
+    'payment.option.create': 'Přidat novou možnost platby',
+    'payment.option.description': 'Zde je seznam vašich uložených platebních metod. Nové platební metody si můžete uložit pro rychlejší platbu. Literární Universe neukládá ani nezpracovává vaše platební údaje, jako je číslo kreditní karty.',
+    'payment.history.title': 'Historie plateb',
+    'payment.history.tab': 'Historie',
+    'payment.history.none': 'Nemáme žádné záznamy o minulých platbách.',
     // Added 2020-04-30
-    'plans.options.aria': 'Payment options',
-    'plans.options.creditCard': 'Credit Card',
-    'plans.purchase.action': 'Subscribe',
-    'plans.purchase.title': 'Buy Literary Universe subscription',
-    'payment.creditCard.details': 'Credit Card details',
-    'payments.zipcode.label': 'ZIP code',
-    'payment.card.name': 'Name on card',
-    'plans.subscribed.thankYou': 'Thank you for supporting Literary Universe',
-    'plans.subscribed.periodEnd': `Your subscription is valid until: {end, date, medium}`,
-    'plans.subscribed.renewal': `At the end your subscription will be {method, select,
-    charge_automatically {renewed automatically}
-    send_invoice {renewed after paying an invoice}
+    'plans.options.aria': 'Možnosti platby',
+    'plans.options.creditCard': 'Platební karta',
+    'plans.purchase.action': 'Přihlaste se k odběru',
+    'plans.purchase.title': 'Koupit Literární Universe předplatné',
+    'payment.creditCard.details': 'Detaily platební karty',
+    'payments.zipcode.label': 'PSČ',
+    'payment.card.name': 'Jméno na kartě',
+    'plans.subscribed.thankYou': 'Děkujeme, že jste podpořili Literary Universe',
+    'plans.subscribed.periodEnd': `Vaše předplatné je platné do: {end, date, medium}`,
+    'plans.subscribed.renewal': `Na konci bude Vaše předplatné {method, select,
+    charge_automatically {automaticky obnoveno}
+    send_invoice {obnoveno po zaplacení faktury}
   }.`,
-    'plans.subscribed.paymentMethod': 'Your default payment method will be used to pay for the next cycle.',
+    'plans.subscribed.paymentMethod': 'Váš výchozí způsob platby bude použit k zaplacení za další cyklu.',
     'plans.subscribed.status': `Subscription status: {status, select,
-    active {Active}
-    incomplete {There is something missing}
-    incomplete_expired {There is something missing}
-    trialing {Trial}
-    past_due {We are missing your payment}
-    canceled {Cancelled}
-    unpaid {Unpaid}
+    active {Aktivní}
+    incomplete {Nekompletní}
+    incomplete_expired {Něco chybí}
+    trialing {Zkušební období}
+    past_due {Neobdrželi jsme Vaší platbu}
+    canceled {Zrušeno}
+    unpaid {Nezaplaceno}
   }`,
-    'payments.changeMethod': 'Change payment method',
-    'plans.subscribed.willNotRenew': ', will not renew at the end of the cycle.',
-    'plans.cancel.button': 'Cancel subscription',
-    'plans.cancel.question': 'Are you sure you want to cancel your subscription? Your subscription will remain active till the end of the billing cycle.',
-    'plans.cancel.confirmationButton': 'Yes, I want to cancel my subscription',
-    'plans.cancel.failed': 'Apologies, we were unable to cancel your plan at this moment. Please try again later.',
-    'plans.cancel.restoreBtn': 'Restore subscription',
-    'plans.cancel.restoreFailed': 'Apologies, we were unable to restore the subscription at this moment. Please try again later.',
-    'plans.change.button': 'Change subscription',
-    'plans.change.explanation': 'Select the plan you want to upgrade or downgrade to. You will be billed the difference in price immediately.',
-    'plans.change.failed': 'Apologies, we were unable to change your plan at this moment. Please try again later.',
-    'plans.change.upgrade': 'Upgrade',
-    'payments.disclaimer': 'Payment information and processing is done via Braintree by PayPal. We do not keep any credit card or bank account information on our servers.',
-    'buy.payment.optionsSelect': 'Select payment option',
-    'payments.card.number': 'Card number',
-    'payments.card.expire': 'Valid Thru',
+    'payments.changeMethod': 'Změnit způsob platby',
+    'plans.subscribed.willNotRenew': ', nebude obnovena na konci cyklu.',
+    'plans.cancel.button': 'Zrušit předplatné',
+    'plans.cancel.question': 'Opravdu chcete zrušit předplatné? Vaše předplatné zůstane aktivní až do konce fakturačního cyklu.',
+    'plans.cancel.confirmationButton': 'Ano, chci zrušit své předplatné',
+    'plans.cancel.failed': 'Omlouváme se, v tuto chvíli jsme nebyli schopni zrušit váš plán. Zkuste to prosím za chvíli.',
+    'plans.cancel.restoreBtn': 'Obnovit předplatné',
+    'plans.cancel.restoreFailed': 'Omlouváme se, momentálně jsme nebyli schopni obnovit předplatné. Zkuste to prosím znovu později.',
+    'plans.change.button': 'Změnit předplatné',
+    'plans.change.explanation': 'Vyberte tarif, na který chcete přejít. Neprodleně vám bude účtován rozdíl v ceně.',
+    'plans.change.failed': 'Omlouváme se, v tuto chvíli jsme nebyli schopni změnit váš plán. Zkuste to prosím za chvíli.',
+    'plans.change.upgrade': 'Upgradovat',
+    'payments.disclaimer': 'Payment information and processing is done via Stripe. We do not keep any credit card or bank account information on our servers.',
+    'buy.payment.optionsSelect': 'Vyberte způsob platby',
+    'payments.card.number': 'Číslo karty',
+    'payments.card.expire': 'Platné do',
     'payments.card.cvv': 'CVV',
-    'payments.card.info': '{cardType} card, ending in {ending}',
-    'payments.none': 'No saved payment options found.',
-    'payments.added': 'New payment option has been successfully added.',
-    'payments.delete.confirm': 'Are you sure you want to delete {cardType}, ending in {ending}?',
-    'payment.invoice.status': 'Invoice status',
+    'payments.card.info': '{cardType} karta končící {ending}',
+    'payments.none': 'Nebyly nalezeny žádné uložené možnosti platby.',
+    'payments.added': 'Nová možnost platby byla úspěšně přidána.',
+    'payments.delete.confirm': 'Jste si jisti, že chcete odstranit {cardType}, končící {ending}?',
+    'payment.invoice.status': 'Stav faktury',
     'payment.invoice.statusMessage': `{status, select,
-    draft {Draft}
-    open {Open}
-    paid {Paid}
+    draft {Návrh}
+    open {Otevřeno}
+    paid {Zaplaceno}
     uncollectible {Uncollectible}
-    void {Void}
+    void {Neplatné}
   }`,
-    'payment.invoice.id': 'Invoice id',
-    'payment.invoice.pdf': 'Invoice pdf',
-    'payment.invoice.total': 'Total',
-    'payment.invoice.what': 'Invoice description',
-    'payment.stripe.powered': 'Powered by ',
-    'payment.listMethods.card': 'Cards',
-    'payment.expiresAt': 'Expires {month}/{year}',
+    'payment.invoice.id': 'ID faktury',
+    'payment.invoice.pdf': 'Faktura ve formátu pdf',
+    'payment.invoice.total': 'Celkem',
+    'payment.invoice.what': 'Popis faktury',
+    'payment.stripe.powered': 'Zprostředkovává ',
+    'payment.listMethods.card': 'Karty',
+    'payment.expiresAt': 'Platí do {month}/{year}',
     'payment.card.details': `{type, select,
-    credit {Credit card}
-    debit {Debit card}
-    other {Card}
-  } ending with {last4}`,
-    'payment.options.default': 'Default',
+    credit {Platební karta}
+    debit {Debetní karta}
+    other {Karta}
+  } končící {last4}`,
+    'payment.options.default': 'Výchozí',
     // Added 2020-05-27
-    'payment.method.add': 'Add a new payment method',
-    'payment.methods.adding': 'You new payment method has been added to your account.',
-    'payment.methods.delete': 'Delete payment method',
+    'payment.method.add': 'Přidat novou možnost platby',
+    'payment.methods.adding': 'Váš nový způsob platby byl přidán k vašemu účtu.',
+    'payment.methods.delete': 'Odstranit způsob platby',
     'payment.upgradePlan.option': `{plan} - {price}`
 };
