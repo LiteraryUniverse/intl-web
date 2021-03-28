8b7e52340eedef22345a3516807c49fe26556a80
Jan Dvorak <storytellercz@protonmail.com>
2021-03-28 11:32:34 +0200

    New translations rating.js (Czech)

--- cs/rating.js
+++ cs/rating.js
@@ -2,18 +2,18 @@
  * Age ratings
  */
 export default {
-    'rating.name': 'Rating',
-    'rating.explained': 'Age rating and restriction.',
-    'rating.all': 'All',
-    'rating.desc.all': 'Accessible to all ages and groups.',
-    'rating.teens': 'Teens',
-    'rating.desc.teens': 'Suitable for an audience between ages 12 and 18.',
-    'rating.mature': 'Mature',
-    'rating.desc.mature': 'Contains advance themes, crude language and implicit scenes.',
-    'rating.explicit': 'Explicit',
-    'rating.desc.explicit': 'Contains explicit scenes.',
-    'rating.selection.all': 'All - Accessible to all ages and groups.',
-    'rating.selection.teens': 'Teens (12-18) - Suitable for audience between ages 12 and 18.',
-    'rating.selection.mature': 'Mature (18+) - Contains advance themes, crude language and implicit scenes.',
-    'rating.selection.explicit': 'Explicit (21+) - Contains explicit scenes.'
+    'rating.name': 'Věkové hodnocení',
+    'rating.explained': 'Věkové hodnocení a omezení.',
+    'rating.all': 'Všichni',
+    'rating.desc.all': 'Dostupné pro všechny věkové skupiny a skupiny.',
+    'rating.teens': 'Teenageři',
+    'rating.desc.teens': 'Vhodné pro publikum ve věku od 12 do 18 let.',
+    'rating.mature': 'Dospělí',
+    'rating.desc.mature': 'Obsahuje pokročilé motivy, hrubý jazyk a implicitní scény.',
+    'rating.explicit': 'Explicitní',
+    'rating.desc.explicit': 'Obsahuje explicitní scény.',
+    'rating.selection.all': 'Všichni - Dostupné pro všechny věkové skupiny a skupiny.',
+    'rating.selection.teens': 'Teenageři (12-18) - Vhodné pro čtenáře ve věku 12 až 18 let.',
+    'rating.selection.mature': 'Dospělí (18+) - Obsahuje pokročilé motivy, hrubý jazyk a implicitní scény.',
+    'rating.selection.explicit': 'Explicitní (21+) - Obsahuje explicitní scény.'
 };
