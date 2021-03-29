0f7f133cbd62a5561ed5553bdb0b507f22a82b37
Jan Dvorak <storytellercz@protonmail.com>
2021-03-29 14:30:06 +0200

    New translations lang_emails.js (Czech)

--- cs/lang_emails.js
+++ cs/lang_emails.js
@@ -3,45 +3,42 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Pozvánka na Literary Universe pro {username}',
-    'email.enrollAccount.message': `Dobrý den {username},\n\n
-    Chtěli bychom Vás pozvat na Literary Universe! Vytvořili jsme pro Vás speciální osobní účet na kterém můžete otestovat všechny funkce Literary Universe! Doufáme že s námi budete vytvářet další generaci nástrojů pro kreativní psaní a čtení!
+    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
+    'email.enrollAccount.message': `Hello {username}\n\n
+    We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
     To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'Literary Universe reset hesla k účtu',
-    'email.resetPassword.message': `Dobrý den {username}!\n\n
- Obdrželi jsme Váš požadavek na resetování Vašeho hesla.
-        Navštivte následující odkaz k dokončení tohoto procesu:\n\n{url}`,
-    'email.verifyEmail.subject': 'Literary Universe potvrzení e-mailu',
-    'email.verifyEmail.message': 'Dobrý den {username}!\n\n Prosím potvrďte tuto e-mailovou adresu návštěvou tohoto odkazu:\n\n{url}',
+    'email.resetPassword.subject': 'Literary Universe reset password',
+    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
+        Please follow the link bellow to reset your password:\n\n{url}`,
+    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
+    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
     // New html email keys
-    'email.salutation': 'Dobrý den {username},',
-    'email.welcome': 'Vítejte na Literary Universe!',
-    'email.signature': 'S přáním hezkého dne,<br />Literary Universe tým',
+    'email.salutation': 'Hello {username},',
+    'email.welcome': 'Welcome to Literary Universe!',
+    'email.signature': 'Sincerely,<br />Literary Universe team',
     // Enrollment
-    'email.enrollAccount.welcome': 'Vítejte na palubě',
-    'email.enrollAccount.messageHtml': 'Rádi by jsme Vás pozvali na Literary Universe! Připravili jsme pro Vás osobní účet s plným přístupem k Literary Universe! Doufáme že se přidáte a pomůžete nám vytvořit další kapitolu v kreativním psaní a čtení!',
-    'email.enrollAccount.button': 'Zpřístupnit účet',
-    'email.enrollAccount.signature': 'Těšíme se na Vás,<br />Literary Universe tým',
+    'email.enrollAccount.welcome': 'Welcome aboard',
+    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
+    'email.enrollAccount.button': 'Access your account',
+    'email.enrollAccount.signature': 'We are looking forward to see you,<br />Literary Universe team',
     // Reset password
-    'email.resetPassword.messageHtml': 'Obdrželi jsme Váš požadavek na resetování Vašeho hesla, prosím klikněte na tlačítko níže k dokončení tohoto procesu.',
-    'email.resetPassword.button': 'Resetovat heslo',
+    'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account, please click on the button bellow to continue the process. If you haven\'t made this request you can ignore this e-mail, but it might be a good time to review your security settings.',
+    'email.resetPassword.button': 'Reset password',
     // E-mail verification
-    'email.verifyEmail.messageHtml': 'Vítejte na palubě! Pro potvrzení této e-mailové adresy prosím klikněte na tlačítko níže.',
-    'email.verifyEmail.button': 'Potvrzení e-mailové adresy',
+    'email.verifyEmail.messageHtml': 'We are happy to have you on board! Please click the button bellow to verify this e-mail address.',
+    'email.verifyEmail.button': 'Verify this e-mail address',
     // Getting premium from admins
-    'email.adminUpgrade.subject': 'Váš účet byl upgradován',
-    'email.adminUpgrade.message': `Upgradovali jsme Váš účet na {plan, select,
+    'email.adminUpgrade.subject': 'Your account has been upgraded',
+    'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     initiate {Initiate}
     adventurer {Adventurer}
     storyteller {Storyteller}
-  } plán na následujících {days} dní.\n\n
-S přáním hezkého dne,\n
-Literary Universe tým`,
-    'email.adminUpgrade.messageHtml': `Upgradovali jsme Váš účet na {plan, select,
+  } tier for the next {days} days.\n\nEnjoy,\nLiterary Universe team`,
+    'email.adminUpgrade.messageHtml': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     initiate {Initiate}
     adventurer {Adventurer}
     storyteller {Storyteller}
-  } plán na následujících {days} dní.`
+  } tier for the next {days} days. Enjoy!`
 }    /* eslint-enable max-len */;
