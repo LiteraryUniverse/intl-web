53b6fb86bab73f8cb240dac614f9c5e0e5aa0b3f
Jan Dvorak <storytellercz@protonmail.com>
2021-04-01 14:28:32 +0200

    New translations sharing.js (Polish)

--- pl/sharing.js
+++ pl/sharing.js
@@ -1,6 +1,6 @@
 export default {
     'sharing.title': 'Share',
-    'sharing.copy': 'Copy',
+    'sharing.copy': 'Skopiuj',
     'sharing.email': 'E-mail',
     'sharing.sms': 'SMS',
     'sharing.messenger': 'Facebook Messenger',
