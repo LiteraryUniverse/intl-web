6d3819b394f4ad40818d08cb5847739b421c15e4
Jan Dvorak <storytellercz@protonmail.com>
2021-04-01 14:28:25 +0200

    New translations sharing.js (Romanian)

--- ro/sharing.js
+++ ro/sharing.js
@@ -1,6 +1,6 @@
 export default {
     'sharing.title': 'Share',
-    'sharing.copy': 'Copy',
+    'sharing.copy': 'Copiere',
     'sharing.email': 'E-mail',
     'sharing.sms': 'SMS',
     'sharing.messenger': 'Facebook Messenger',
