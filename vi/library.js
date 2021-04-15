f46230cb978739ff80ef0a59b7ab973e000b2896
Jan Dvorak <storytellercz@protonmail.com>
2021-04-15 14:17:51 +0200

    New translations library.js (Vietnamese)

---
+++ vi/library.js
@@ -0,0 +1,7 @@
+export default {
+    'library.add': 'Add to library',
+    'library.remove': 'Remove from library',
+    'library.bought': 'Bought stories',
+    'library.stories': 'Followed stories',
+    'library.universes': 'Followed universe'
+};
