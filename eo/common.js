9084fcfc0770a60489fc8fe2bbd83b4827df8a80
Jan Dvorak <storytellercz@protonmail.com>
2021-04-16 15:32:47 +0200

    New translations common.js (Esperanto)

---
+++ eo/common.js
@@ -0,0 +1,223 @@
+/**
+ * @authors Jan Dvorak
+ */
+/* eslint-disable max-len */
+export default {
+    siteName: 'Literary Universe',
+    'site.description': 'Literary Universe is a creative and publishing platform for grand story universes.',
+    'site.keywords': 'creative writing, scifi, sci-fi, fantasy, writing, publishing, translations, story, stories, universe',
+    'site.logoAlt': 'Literary Universe logo',
+    mobileMenu: 'Mobile menu',
+    'common.signin': 'Login',
+    'common.signup': 'Register',
+    'common.signout': 'Logout',
+    'common.dashboard': 'Dashboard',
+    'common.discovery': 'Discovery',
+    'common.library': 'Library',
+    'common.library.own': 'Your Library',
+    'common.workshop': 'Workshop',
+    'common.pm': 'Messages',
+    'common.settings': 'Settings',
+    'common.options': 'Options',
+    'common.profile': 'Profile',
+    'common.blog': 'Blog',
+    'common.blogs': 'Blogs',
+    'common.about': 'About us',
+    'common.accessdenied': 'Access denied',
+    'common.accessdenied.msg': 'You were not given permission to go here. If you should have access here, please contact the owner and tell them to double check the settings.',
+    'common.loading': 'Loading...',
+    'common.add': 'Add',
+    'common.save': 'Save',
+    'common.email': 'E-mail',
+    'common.soon': 'Coming soon...',
+    'common.search': 'Search',
+    'common.search.do': 'Search',
+    'common.searching': 'Searching...',
+    'common.report': 'Report',
+    'common.user': 'User',
+    'common.users': 'Users',
+    'common.showmore': 'Show more',
+    'common.futurefeature.title': 'Future feature',
+    'common.futurefeature.text': 'We are very sorry, but this feature is not available at this time. We are working hard to make it available as soon as possible.',
+    'common.nothingfound': 'Nothing found',
+    'common.explore': 'Explore',
+    'common.by': 'by ',
+    // keep the space at the end as username follows
+    'common.scratchpad': 'Scratchpad',
+    'common.scratchpad.own': 'Your scratchpad',
+    'common.news': 'News',
+    'common.encyclopedia': 'Encyclopedia',
+    'common.story': 'Story',
+    'common.stories': 'Stories',
+    'common.universe': 'Universe',
+    'common.universes': 'Universes',
+    'common.collaborator': 'Collaborator',
+    'common.collaborators': 'Collaborators',
+    'common.translator': 'Translator',
+    'common.translators': 'Translators',
+    'common.betareader': 'Beta reader',
+    'common.betareaders': 'Beta readers',
+    'common.post': 'Post',
+    'common.statistics': 'Statistics',
+    'common.lang.select': 'Select language',
+    'common.submit': 'Submit',
+    'common.close': 'Close',
+    'common.comment': 'Comment',
+    'common.comments': 'Comments',
+    404: 'Page not found',
+    'search.users': 'Search for users',
+    'search.user': 'Search for user',
+    'common.help': 'Help',
+    'common.or': 'or',
+    'common.account.create': 'Create an account',
+    'signup.username': 'Username',
+    'signup.email': 'Your e-mail',
+    'signup.password.label': 'Your password',
+    'signup.password.repeat': 'Repeat your password',
+    'common.password': 'Password',
+    'signin.password.forgot': 'Forgot password?',
+    'dashboard.whatsnew': 'What is new',
+    'dashboard.blog': 'Your blog',
+    'common.footer.tagline': 'Writing and reading complex universes made easy.',
+    'common.footer.plans': 'Plans & Pricing',
+    'common.legal.tos': 'Terms of Service',
+    'common.legal.community': 'Community Guidelines',
+    'common.legal.privacy': 'Privacy Policy',
+    'common.legal.copyright': 'Copyright Agreement',
+    'common.legal.disclaimer': '© {year} All rights reserved, all texts belong to their respective authors.',
+    'premium.only.title': 'Not high enough membership rank',
+    'premium.only.msg': 'We apologize, your membership rank is not high enough to access this feature.',
+    'premium.only.join': 'We offer varieties of plans for everyone to get the most out of Literary Universe.',
+    'premium.only.link': 'Check it out!',
+    'universe.visit': 'Visit universe page',
+    'common.fanfiction': 'Fan Fiction',
+    'story.chapter': 'Chapter',
+    'story.chapters': 'Chapters',
+    'story.genre': 'Genre',
+    'story.genres': 'Genres',
+    'story.read': 'Read',
+    'story.lastUpdate': 'Last updated on {date, date, medium} {date, time, short}.',
+    'story.finishedAt': 'Finished on {date, date, medium}.',
+    'story.comments.for': 'Comments for {story} {chapter}.',
+    'story.cover.alt': 'Story cover image for {story}.',
+    'story.note.fanfiction': 'NOTE: This story is a fan fiction on {universe} universe.',
+    copyrights: 'Copyrights',
+    'common.challenges': 'Challenges',
+    // for beta only
+    'beta.notice': 'Welcome to Literary Universe BETA. This is a public preview and as such the app is still work in progress. This means that you can still expect errors and unfinished features. If you encounter an error or something not working please let us know and please be patient while we get get everything in place.',
+    'beta.thanks': 'Thank you for your support!',
+    'scratchpad.about': 'Your notes and ideas accessible anywhere on the site.',
+    // added 2016-11-02
+    'common.feedback': 'Feedback',
+    'common.createdon': 'Created on ',
+    // added 2016-11-14
+    // account types
+    admin: 'Admin',
+    // added 2016-12-03
+    goodbye: 'Thank you! See you again!',
+    // added 2016-12-31
+    'common.saved': 'Saved!',
+    'common.saving': 'Saving...',
+    // added 2017-01-23
+    'common.friends': 'Friends',
+    // added 2017-01-27
+    'lang.orig': 'Original language',
+    'lang.orig.plus': 'Original language - {language}',
+    // added 2017-03-28
+    buy: 'Buy',
+    // added 2017-03-31
+    confirm: 'Confirm',
+    cancel: 'Cancel',
+    // added 2017-04-14
+    processing: 'Processing...',
+    // added 2017-10-04
+    'common.noData': 'No data',
+    // added 2017-11-25
+    'error.title': 'An error has occurred. We apologize for the inconvenience.',
+    // added 2018-04-20
+    'common.offlineStatus': 'You are currently offline.',
+    // added 2018-04-30
+    male: 'Male',
+    female: 'Female',
+    // added 2018-05-18
+    'common.legal.effectiveAt': 'Effective from',
+    // added 2018-05-24
+    select: 'Please select',
+    // added 2018-06-05
+    'common.statusFailed': 'Failed to connect to the server.',
+    'common.statusConnecting': 'Attempting to connect to the server... ({count, number})',
+    // added 2018-06-13
+    'slug.checking': 'Checking...',
+    'slug.failed': 'This address already exists.',
+    'slug.success': 'This address is available!',
+    // added 2018-07-08
+    'notVerified.msg': 'Your account e-mail is not verified.',
+    'notVerified.verify': 'Please verify your e-mail.',
+    // added 2018-07-12
+    unknown: 'unknown',
+    // added 2018-09-30
+    'common.legal.guidelines': 'Community Guidelines',
+    // added 2019-01-12
+    'errorLoading.title': 'Error during page load',
+    'errorLoading.explained': 'An error has occurred while the page was loading. We apologize for the inconvenience. Please retry accessing the page again. If the problem persists, please let us known.',
+    'errorLoading.retry': 'Retry!',
+    'errorLoading.takingLong': 'This is taking a long time...',
+    // added 2019-02-21
+    'common.title': 'Title',
+    // added 2019-05-05
+    'common.timelines': 'Timelines',
+    // added 2019-10-04
+    'common.legal.gdpr': 'GDPR',
+    'common.legal.gdpr.long': 'General Data Protection Regulation',
+    // Added 2019-10-16
+    'editor.placeholder.link': 'Enter URL...',
+    'editor.placeholder.connection': 'Search for encyclopedia entry...',
+    // Added 2019-10-20
+    'editor.saved.beta': 'New revision for Beta readers created!',
+    'editor.saved.public': 'New version published!',
+    // Added 2019-10-21
+    goodbyeForever: 'We are sorry to see see you go. Was it something we did? Please let us know if that is the case. Either way we wish you the best in your future endeavors and hopefully we\'ll meet again! Live long and prosper!',
+    'goodbyeForever.feedback': 'Let us know what we can do better',
+    // Added 2019-10-27
+    'common.edit': 'Edit',
+    // Added 2019-11-03
+    'common.publish': 'Publish',
+    'common.publishAt': 'Publication date and time',
+    // Added 2019-11-06
+    'a11y.user.menu': 'User menu',
+    'dashboard.profile': 'Your profile',
+    // Added 2019-11-07
+    'profile.website': 'My personal website',
+    // Added 2019-11-23
+    'story.notes.concept': 'Concept',
+    'story.notes.settings': 'Story setting',
+    'story.notes.theme': 'Theme',
+    'story.notes.impact': 'Desired impact',
+    'story.notes.outcome': 'Outcome',
+    'story.notes.audience': 'Target audience',
+    'story.notes.pitch': 'Pitch',
+    'story.notes.notes': 'Notes',
+    'common.members': 'Members',
+    'common.language': 'Language',
+    next: 'Next',
+    'common.legal.tos.short': 'Terms',
+    'common.legal.privacy.short': 'Privacy',
+    'premium.increaseLimit': 'Consider upgrading to increase you limit.',
+    // Added 2020-12-26
+    'common.sharing': 'Sharing',
+    // Added 2021-02-14
+    'story.isbn': 'ISBN',
+    // Added 2021-02-26
+    'common.name': 'Name',
+    // Added 2021-03-04
+    'common.remove': 'Remove',
+    // Added 2021-03-12
+    'common.time.milliseconds': 'Milliseconds',
+    'common.time.seconds': 'Seconds',
+    'common.time.minutes': 'Minutes',
+    'common.time.hours': 'Hours',
+    'common.time.days': 'Days',
+    'common.time.weeks': 'Weeks',
+    'common.time.months': 'Months',
+    'common.time.years': 'Years'
+}    /* eslint-enable max-len */;
