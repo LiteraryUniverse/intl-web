970c0bea5ac9a2e79ea459d83bd37af8a54a4845
Jan Dvorak <storytellercz@protonmail.com>
2021-04-17 11:46:56 +0200

    New translations pm.js (Esperanto)

--- eo/pm.js
+++ eo/pm.js
@@ -1,13 +1,13 @@
 export default {
-    'pm.conversation': 'Conversation',
+    'pm.conversation': 'Konversacio',
     'pm.showolder': 'Show older messages',
     'pm.errors.saysomething': 'You should really say something...',
     'pm.errors.addrecipient': 'You need to add at least one other user.',
-    'pm.message': 'Message',
-    'pm.send.message': 'Send message',
-    'pm.messages.own': 'Your messages',
+    'pm.message': 'Mesaĝo',
+    'pm.send.message': 'Sendi mesaĝon',
+    'pm.messages.own': 'Viaj mesaĝoj',
     'pm.conversations.none': 'You are currently not conversing with anyone.',
-    'pm.to': 'To:',
+    'pm.to': 'Al:',
     'pm.with': 'Conversation with:',
     'pm.send': 'Send',
     'pm.conversations.unreadCount': `You have {count, plural,
@@ -17,6 +17,6 @@ export default {
   }.`,
     'pm.form.minimumParticipants': 'You need to choose a recipient.',
     'pm.form.maxParticipants': 'You can have maximum of 10 people in a conversation.',
-    'pm.new.aria': 'New conversation',
+    'pm.new.aria': 'Nova konversacio',
     'pm.new.buttonTitle': 'Start a new conversation'
 };
