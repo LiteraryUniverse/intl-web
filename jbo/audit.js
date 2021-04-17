7fff8a25a0aa2b0456ef374e028e207ebec7242d
Jan Dvorak <storytellercz@protonmail.com>
2021-04-17 11:46:50 +0200

    New translations audit.js (Lojban)

--- jbo/audit.js
+++ jbo/audit.js
@@ -1,6 +1,6 @@
 export default {
-    'auditLog.header.date': 'Date',
+    'auditLog.header.date': 'detri',
     'auditLog.header.action': 'What',
-    'auditLog.header.group': 'Group',
-    'auditLog.header.user': 'Who'
+    'auditLog.header.group': 'girzu',
+    'auditLog.header.user': 'prenu'
 };
