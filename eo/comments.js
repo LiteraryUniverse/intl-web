6d1f4b599a97701c65198ad44052ba04dbaebd15
Jan Dvorak <storytellercz@protonmail.com>
2021-04-17 11:46:53 +0200

    New translations comments.js (Esperanto)

--- eo/comments.js
+++ eo/comments.js
@@ -1,5 +1,5 @@
 export default {
-    'comment.write': 'Write your comment',
+    'comment.write': 'Verki komenton',
     'comments.none': 'Sorry, no comments to show.',
     'comments.total': `There {count, plural,
     =0 {are no comments}
