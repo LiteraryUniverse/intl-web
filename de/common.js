3a5563d57329c35dce60ff2676baa561e442d891
Jan Dvorak <storytellercz@protonmail.com>
2021-04-20 11:36:46 +0200

    New translations common.js (German)

--- de/common.js
+++ de/common.js
@@ -107,88 +107,56 @@ export default {
     'beta.notice': 'Welcome to Literary Universe BETA. This is a public preview and as such the app is still work in progress. This means that you can still expect errors and unfinished features. If you encounter an error or something not working please let us know and please be patient while we get get everything in place.',
     'beta.thanks': 'Vielen Dank für Ihre Unterstützung!',
     'scratchpad.about': 'Your notes and ideas accessible anywhere on the site.',
-    // added 2016-11-02
     'common.feedback': 'Feedback',
     'common.createdon': 'Created on ',
-    // added 2016-11-14
     // account types
     admin: 'Admin',
-    // added 2016-12-03
     goodbye: 'Vielen Dank und auf Wiedersehen!',
-    // added 2016-12-31
     'common.saved': 'Gespeichert!',
     'common.saving': 'Wird gespeichert...',
-    // added 2017-01-23
     'common.friends': 'Freunde',
-    // added 2017-01-27
     'lang.orig': 'Original language',
     'lang.orig.plus': 'Original language - {language}',
-    // added 2017-03-28
     buy: 'Kaufen',
-    // added 2017-03-31
     confirm: 'Confirm',
     cancel: 'Cancel',
-    // added 2017-04-14
     processing: 'Processing...',
-    // added 2017-10-04
     'common.noData': 'No data',
-    // added 2017-11-25
     'error.title': 'An error has occurred. We apologize for the inconvenience.',
-    // added 2018-04-20
     'common.offlineStatus': 'You are currently offline.',
-    // added 2018-04-30
     male: 'Mann',
     female: 'Frau',
-    // added 2018-05-18
     'common.legal.effectiveAt': 'Effective from',
-    // added 2018-05-24
     select: 'Please select',
-    // added 2018-06-05
     'common.statusFailed': 'Failed to connect to the server.',
     'common.statusConnecting': 'Attempting to connect to the server... ({count, number})',
-    // added 2018-06-13
     'slug.checking': 'Checking...',
     'slug.failed': 'This address already exists.',
     'slug.success': 'This address is available!',
-    // added 2018-07-08
     'notVerified.msg': 'Your account e-mail is not verified.',
     'notVerified.verify': 'Please verify your e-mail.',
-    // added 2018-07-12
     unknown: 'unknown',
-    // added 2018-09-30
     'common.legal.guidelines': 'Community Guidelines',
-    // added 2019-01-12
     'errorLoading.title': 'Error during page load',
     'errorLoading.explained': 'An error has occurred while the page was loading. We apologize for the inconvenience. Please retry accessing the page again. If the problem persists, please let us known.',
     'errorLoading.retry': 'Retry!',
     'errorLoading.takingLong': 'This is taking a long time...',
-    // added 2019-02-21
     'common.title': 'Title',
-    // added 2019-05-05
     'common.timelines': 'Timelines',
-    // added 2019-10-04
     'common.legal.gdpr': 'DSGVO',
     'common.legal.gdpr.long': 'Allgemeine Datenschutzverordnung (DSGVO)',
-    // Added 2019-10-16
     'editor.placeholder.link': 'URL eingeben...',
     'editor.placeholder.connection': 'Suche nach Enzyklopädie Eintrag...',
-    // Added 2019-10-20
     'editor.saved.beta': 'New revision for Beta readers created!',
     'editor.saved.public': 'New version published!',
-    // Added 2019-10-21
     goodbyeForever: 'We are sorry to see see you go. Was it something we did? Please let us know if that is the case. Either way we wish you the best in your future endeavors and hopefully we\'ll meet again! Live long and prosper!',
     'goodbyeForever.feedback': 'Let us know what we can do better',
-    // Added 2019-10-27
     'common.edit': 'Edit',
-    // Added 2019-11-03
     'common.publish': 'Publish',
     'common.publishAt': 'Publication date and time',
-    // Added 2019-11-06
     'a11y.user.menu': 'User menu',
     'dashboard.profile': 'Ihr Profil',
-    // Added 2019-11-07
     'profile.website': 'Persönliche Webseite',
-    // Added 2019-11-23
     'story.notes.concept': 'Konzept',
     'story.notes.settings': 'Story setting',
     'story.notes.theme': 'Theme',
@@ -203,15 +171,10 @@ export default {
     'common.legal.tos.short': 'Terms',
     'common.legal.privacy.short': 'Privacy',
     'premium.increaseLimit': 'Consider upgrading to increase you limit.',
-    // Added 2020-12-26
     'common.sharing': 'Sharing',
-    // Added 2021-02-14
     'story.isbn': 'ISBN',
-    // Added 2021-02-26
     'common.name': 'Name',
-    // Added 2021-03-04
     'common.remove': 'Remove',
-    // Added 2021-03-12
     'common.time.milliseconds': 'Millisekunden',
     'common.time.seconds': 'Sekunden',
     'common.time.minutes': 'Minuten',
