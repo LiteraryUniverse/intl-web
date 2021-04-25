93017ec3504aa337b693ce2db2de4f80ca0f3acb
Jan Dvorak <storytellercz@protonmail.com>
2021-04-25 22:51:53 +0900

    Update source file common.js

--- en/common.js
+++ en/common.js
@@ -166,6 +166,7 @@ export default {
   'story.notes.audience': 'Target audience',
   'story.notes.pitch': 'Pitch',
   'story.notes.notes': 'Notes',
+  'story.authorsNotes': 'Author\'s notes',
   'common.members': 'Members',
   'common.language': 'Language',
   next: 'Next',
