1f4516a2cba2ff923414b3eed4be242587258c13
Jan Dvorak <storytellercz@protonmail.com>
2021-04-28 18:28:56 +0900

    New translations discovery.js (Esperanto)

---
+++ eo-UY/discovery.js
@@ -0,0 +1,20 @@
+export default {
+    'discovery.meta.desc': 'Discover new stories and universes.',
+    'discovery.universes.new': 'Emerging universes',
+    'discovery.stories.new': 'New stories',
+    'discovery.stories.standalone.new': 'New standalone stories',
+    'discovery.stories.updated': 'Recently updated stories',
+    'discovery.continue': 'Legi plu',
+    'discovery.stories.similar.new': 'New stories in the neighborhood',
+    'discovery.stories.similar.regular': 'Stories in the neighborhood',
+    'story.estimatedReadingTime': `This story should take {hours, plural,
+        =0 {}
+        one {1 hour and }
+        other {# hours and }
+    }{minutes, plural,
+        =0 {}
+        one {1 minute}
+        other {# minutes}
+    } to read.`,
+    'story.estimatedReadingTime.explained': 'Based on an average reading speed of {avgReadingNum} words per minute.'
+};
