87b091ad98e78f2523d34fc12bd4017063d5cdf8
Jan Dvorak <storytellercz@protonmail.com>
2021-04-29 06:13:18 +0900

    New translations comments.js (Japanese)

--- ja-JP/comments.js
+++ ja-JP/comments.js
@@ -1,10 +1,9 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
-    'comments.total': `There {count, plural,
-    =0 {are no comments}
-    one {is 1 comment}
-    other {are # comments}
-  }.`,
-    'comments.show.older': 'Show older comments'
+    'comment.write': 'コメントを書く',
+    'comments.none': '申し訳ありませんが、表示するコメントはありません。',
+    'comments.total': `{count, plural,
+    =0 {コメントはありません}
+    other {#件のコメントがあります}
+}。`,
+    'comments.show.older': '古いコメントの表示'
 };
