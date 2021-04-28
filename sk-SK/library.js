37cc9d24b92973939f3faab2e77cb241b24cfac5
Jan Dvorak <storytellercz@protonmail.com>
2021-04-29 06:13:27 +0900

    New translations library.js (Slovak)

---
+++ sk-SK/library.js
@@ -0,0 +1,7 @@
+export default {
+    'library.add': 'Pridať do knižnice',
+    'library.remove': 'Odstrániť z knižnice',
+    'library.bought': 'Kúpené príbehy',
+    'library.stories': 'Sledované príbehy',
+    'library.universes': 'Sledované vesmíry'
+};
