482cd71635063e0d9d765b01cf39dd08a7717e76
Jan Dvorak <storytellercz@protonmail.com>
2021-04-30 06:20:13 +0900

    New translations legal.js (Slovak)

---
+++ sk-SK/legal.js
@@ -0,0 +1,9 @@
+export default {
+    'legal.modal.intro': 'Ospravedlňujeme sa za vyrušenie. Ale skôr, ako vás pustíme ďalej, sme zo zákona povinní uistiť sa, že ste si vedomí našich podmienok používania služby, zásad ochrany osobných údajov a zmluvy o autorských právach a že s nimi súhlasíte.',
+    'legal.modal.agree': 'Súhlasím',
+    'legal.modal.disagree': 'Nesúhlasím',
+    'legal.modal.register': 'Ak sa zaregistrujete, už to neuvidíte.',
+    'legal.modal.goToSettings': 'Prejsť do nastavení',
+    'legal.modal.fewmore': 'Niekoľko ďalších vecí...',
+    'legal.modal.finish': 'Dokončiť'
+};
