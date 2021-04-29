482d73ca8b7e41e994c4c915735470d87aebf992
Jan Dvorak <storytellercz@protonmail.com>
2021-04-30 05:20:03 +0900

    New translations sharing.js (Esperanto)

---
+++ eo-UY/sharing.js
@@ -0,0 +1,16 @@
+export default {
+    'sharing.title': 'Kunhavigi',
+    'sharing.copy': 'Kopii',
+    'sharing.email': 'E-mail',
+    'sharing.sms': 'SMS',
+    'sharing.messenger': 'Facebook Messenger',
+    'sharing.whatsapp': 'WhatsApp',
+    'sharing.twitter': 'Tvitero',
+    'sharing.linkedin': 'LinkedIn',
+    'sharing.telegram': 'Telegram',
+    'sharing.facebook': 'Facebook',
+    'sharing.line': 'LINE',
+    'sharing.mastodonSocial': 'mastodon.social',
+    'sharing.mastodonJapan': 'mastodon.jp',
+    'sharing.gab': 'Gab'
+};
