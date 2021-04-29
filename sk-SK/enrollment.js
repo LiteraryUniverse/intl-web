294bd1f0dc556ed51b44ce22ea4606e71c4939fa
Jan Dvorak <storytellercz@protonmail.com>
2021-04-30 06:20:10 +0900

    New translations enrollment.js (Slovak)

---
+++ sk-SK/enrollment.js
@@ -0,0 +1,6 @@
+export default {
+    'enrollment.title': 'Vyžiadajte si svoj účet!',
+    'enrollment.description': 'Vitajte {username}! Na získanie prístupu k svojmu špeciálnemu účtu si stačí vytvoriť heslo.',
+    'enrollment.submitting': 'Uplatnenie nároku na účet...',
+    'enrollment.submit': 'Vyžiadajte si svoj účet!'
+};
