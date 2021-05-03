49e1b595ffef793fcb28e49d61b01bd996415746
Jan Dvorak <storytellercz@protonmail.com>
2021-05-04 04:24:31 +0900

    New translations rating.js (Slovak)

---
+++ sk-SK/rating.js
@@ -0,0 +1,19 @@
+/**
+ * Age ratings
+ */
+export default {
+    'rating.name': 'Vekové hodnotenie',
+    'rating.explained': 'Veková kategória a obmedzenia.',
+    'rating.all': 'Všetky',
+    'rating.desc.all': 'Prístupné pre všetky vekové kategórie a skupiny.',
+    'rating.teens': 'Tínedžeri',
+    'rating.desc.teens': 'Vhodné pre publikum vo veku od 12 do 18 rokov.',
+    'rating.mature': 'Dospelí',
+    'rating.desc.mature': 'Obsahuje pokročilé témy, hrubý jazyk a implicitné scény.',
+    'rating.explicit': 'Explicitné',
+    'rating.desc.explicit': 'Obsahuje explicitné scény.',
+    'rating.selection.all': 'Všetky - prístupné pre všetky vekové kategórie a skupiny.',
+    'rating.selection.teens': 'Teenageri (12-18) - Vhodné pre divákov vo veku od 12 do 18 rokov.',
+    'rating.selection.mature': 'Dospelí (18+) - Obsahuje pokročilé témy, hrubý jazyk a implicitné scény.',
+    'rating.selection.explicit': 'Explicitné (21+) - obsahuje explicitné scény.'
+};
