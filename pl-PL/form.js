be08c8c14273a50da423536659f30aaf4116f585
Jan Dvorak <storytellercz@protonmail.com>
2021-05-04 04:25:34 +0900

    New translations form.js (Polish)

---
+++ pl-PL/form.js
@@ -0,0 +1,63 @@
+export default {
+    'form.validator.required': 'Required field',
+    'form.validator.mustBeNumber': 'Must be a number',
+    'form.validator.minNumber': 'Should be greater than {min}',
+    'form.validator.maxNumber': 'Should be lower than {max}',
+    'form.validator.maxLength': `Should be less than {max} {what, select,
+    characters {characters}
+    tags {tags}
+    genres {genres}
+    other {}
+  }`,
+    'form.validator.minLength': `Should be at least {min} {what, select,
+    characters {characters}
+    tags {tags}
+    genres {genres}
+    genre {genre}
+    other {}
+  }`,
+    'form.validator.email': 'Must be a valid e-mail',
+    'form.validator.url': 'Must be a valid URL',
+    'form.validator.slug': 'Must be a valid part of URL',
+    'form.submitting.signup': 'Creating account...',
+    'form.submitting.signin': 'Logging in...',
+    'form.submitting.default': 'Submitting...',
+    'form.submitting.sending': 'Sending...',
+    'form.validator.whitespace': 'Must not have white space',
+    'form.validator.zipCode': 'Must be a valid ZIP code',
+    'form.validator.isbn': 'We only accept 10 or 13 version of ISBN',
+    // you can get these messages from: https://github.com/gwendall/meteor-simple-schema-i18n/tree/master/i18n
+    // but don't forget to adjust the format
+    'simpleschema.required': '{label} jest wymagane',
+    'simpleschema.minString': '{label} wymaga co najmniej {min} znaków',
+    'simpleschema.maxString': '{label} przekracza maksymalną liczbę {max} znaków',
+    'simpleschema.minNumber': '{label} musi być co najmniej {min}',
+    'simpleschema.maxNumber': '{label} przekracza maksymalną wartość {max}',
+    'simpleschema.minNumberExclusive': '{label} musi być większe niż {min}',
+    'simpleschema.maxNumberExclusive': '{label} musi być mniejsze niż {max}',
+    'simpleschema.minDate': '{label} nie wcześniej niż {min}',
+    'simpleschema.maxDate': '{label} nie później niż {max}',
+    'simpleschema.badDate': '{label} nie jest prawidłową datą',
+    'simpleschema.minCount': 'Trzeba podać co najmniej {minCount} wartości',
+    'simpleschema.maxCount': 'Nie można podać więcej niż {maxCount} wartości',
+    'simpleschema.noDecimal': '{label} musi być liczbą całkowitą',
+    'simpleschema.notAllowed': '{value} nie ma prawidłowej wartości',
+    'simpleschema.expectedString': '{label} musi być łańcuchem znaków',
+    'simpleschema.expectedNumber': '{label} musi być liczbą',
+    'simpleschema.expectedBoolean': '{label} musi być typu Boolean',
+    'simpleschema.expectedArray': '{label} musi być tablicą',
+    'simpleschema.expectedObject': '{label} musi być obiektem',
+    'simpleschema.expectedConstructor': '{label} musi być typu {type}',
+    'simpleschema.keyNotInSchema': '{key} nie jest dozwolony',
+    'simpleschema.notUnique': '{label} musi być unikalne',
+    'simpleschema.regEx.0': '{label} nie spełnia kryteriów wyrażenia regularnego',
+    'simpleschema.regEx.1': '{label} nie jest prawidłowym adresem e-mail',
+    'simpleschema.regEx.2': '{label} nie jest prawidłowym adresem e-mail',
+    'simpleschema.regEx.3': '{label} musi być prawidłową domeną',
+    'simpleschema.regEx.4': '{label} musi być prawidłową domeną',
+    'simpleschema.regEx.5': '{label} musi być prawidłowym adresem IPv4 lub IPv6',
+    'simpleschema.regEx.6': '{label} musi być prawidłowym adresem IPv4',
+    'simpleschema.regEx.7': '{label} musi być prawidłowym adresem IPv6',
+    'simpleschema.regEx.8': '{label} musi być prawidłowym adresem URL',
+    'simpleschema.regEx.9': '{label} musi być prawidłowym alfanumerycznym ID'
+};
