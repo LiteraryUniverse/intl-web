ae1695040f7e33ed3b67cf14e6c70f7d1c410622
Jan Dvorak <storytellercz@protonmail.com>
2021-05-04 05:25:21 +0900

    New translations comments.js (Portuguese, Brazilian)

--- pt-BR/comments.js
+++ pt-BR/comments.js
@@ -1,10 +1,10 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
+    'comment.write': 'Escreva seu comentário',
+    'comments.none': 'Desculpe, não há comentários para mostrar.',
     'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-    'comments.show.older': 'Show older comments'
+    'comments.show.older': 'Mostrar comentários mais antigos'
 };
