d789743767c400a734e0cd124f80deb37fce75b7
Jan Dvorak <storytellercz@protonmail.com>
2021-05-04 04:24:00 +0900

    New translations pm.js (Slovak)

---
+++ sk-SK/pm.js
@@ -0,0 +1,23 @@
+export default {
+    'pm.conversation': 'Konverzácia',
+    'pm.showolder': 'Zobrazenie starších správ',
+    'pm.errors.saysomething': 'Mali by ste niečo povedať...',
+    'pm.errors.addrecipient': 'Musíte pridať aspoň jedného ďalšieho používateľa.',
+    'pm.message': 'Správa',
+    'pm.send.message': 'Odoslať správu',
+    'pm.messages.own': 'Vaša správy',
+    'pm.conversations.none': 'Momentálne s nikým nekonverzujete.',
+    'pm.to': 'Pre:',
+    'pm.with': 'Konverzácia s:',
+    'pm.send': 'Odoslať',
+    'pm.conversations.unreadCount': `{count, plural,
+    zero {Nemáte žiadne neprečítané konverzácie}
+    one {Máte jednu neprečítanú konverzáciu}
+    few {Máte # neprečítané konverzácie}
+     other {Máte # neprečítaných konverzácií}
+}.`,
+    'pm.form.minimumParticipants': 'Musíte si vybrať príjemcu.',
+    'pm.form.maxParticipants': 'V jednej konverzácii môže byť maximálne 10 ľudí.',
+    'pm.new.aria': 'Nová konverzácia',
+    'pm.new.buttonTitle': 'Začať novú konverzáciu'
+};
