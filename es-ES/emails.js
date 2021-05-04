37ed9057c515db754f6487754d0c853a60be93e9
Jan Dvorak <storytellercz@protonmail.com>
2021-05-04 09:03:51 +0900

    New translations emails.js (Spanish)

--- es-ES/emails.js
+++ es-ES/emails.js
@@ -3,43 +3,43 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
-    'email.enrollAccount.message': `Hello {username}\n\n
-    We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
-    To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'Literary Universe reset password',
-    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
-        Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    'email.enrollAccount.subject': 'Invitación a Literary Universe para {username}',
+    'email.enrollAccount.message': `Hola {username}\n\n
+    ¡Nos gustaría invitarte a Literary Universe! ¡Hemos creado una cuenta personal especial para ti en la que puedes acceder a todas las funciones de Literary Universe! ¡Únete a nosotros para crear la generación futura de herramientas de escritura y lectura creativas!
+    Para activar tu cuenta, haz clic en en siguiente enlace: \n\n{url}`,
+    'email.resetPassword.subject': 'Restablecer contraseña para Literary Universe',
+    'email.resetPassword.message': `Hola  {username}!\n\n Hemos recibido una solicitud para restablecer la contraseña para tu cuenta.
+        Por favor, continúa al siguiente enlace para restablecer la contraseña:\n\n{url}`,
+    'email.verifyEmail.subject': 'Verificación de e-mail para Literary Universe',
+    'email.verifyEmail.message': '¡Hola {username}!\n\n Por favor, verifica tu dirección de correo electrónico haciendo clic en el siguiente enlace:\n\n{url}',
     // New html email keys
-    'email.salutation': 'Hello {username},',
-    'email.welcome': 'Welcome to Literary Universe!',
-    'email.signature': 'Sincerely,<br />Literary Universe team',
+    'email.salutation': 'Hola {username},',
+    'email.welcome': '¡Bienvenido a Literary Universe!',
+    'email.signature': 'Atentamente,<br />el equipo de Literary Universe',
     // Enrollment
-    'email.enrollAccount.welcome': 'Welcome aboard',
-    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-    'email.enrollAccount.button': 'Access your account',
-    'email.enrollAccount.signature': 'We are looking forward to see you,<br />Literary Universe team',
+    'email.enrollAccount.welcome': 'Bienvenido a bordo',
+    'email.enrollAccount.messageHtml': '¡Nos gustaría invitarte a Literary Universe! ¡Te hemos preparado una cuenta personal para ti en la que puedes acceder a todas las funciones de Literary Universe! ¡Únete a nosotros para crear la generación futura de lectura y escritura creativas!',
+    'email.enrollAccount.button': 'Accede a tu cuenta',
+    'email.enrollAccount.signature': 'Estamos deseando verte,<br />equipo de Literary Universe',
     // Reset password
-    'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account, please click on the button bellow to continue the process. If you haven\'t made this request you can ignore this e-mail, but it might be a good time to review your security settings.',
-    'email.resetPassword.button': 'Reset password',
+    'email.resetPassword.messageHtml': 'Hemos recibido una solicitud para restablecer la contraseña de tu cuenta. Por favor, haz clic en el botón de abajo para continuar con el procedimiento. Si no lo has solicitado ignora este mensaje, aunque te convendría revisar tu configuración de seguridad.',
+    'email.resetPassword.button': 'Restablecer contraseña',
     // E-mail verification
-    'email.verifyEmail.messageHtml': 'We are happy to have you on board! Please click the button bellow to verify this e-mail address.',
-    'email.verifyEmail.button': 'Verify this e-mail address',
+    'email.verifyEmail.messageHtml': '¡Nos complace tenerte a bordo! Por favor, haz clic en el botón de abajo para verificar tu dirección de e-mail.',
+    'email.verifyEmail.button': 'Verificar dirección de e-mail',
     // Getting premium from admins
-    'email.adminUpgrade.subject': 'Your account has been upgraded',
-    'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
-    explorer {Explorer}
-    adventurer {Adventurer}
-    storyteller {Storyteller}
-  } tier for the next {days} days.\n\nEnjoy,\nLiterary Universe team`,
-    'email.adminUpgrade.messageHtml': `We have upgraded your account to {plan, select,
-    explorer {Explorer}
-    adventurer {Adventurer}
-    storyteller {Storyteller}
-  } tier for the next {days} days. Enjoy!`,
-    'email.verification': 'E-mail verification',
-    'email.verifying': 'Verifying your e-mail. This will take just a moment...',
-    'email.verified': 'Great news! Your e-mail has been verified!'
+    'email.adminUpgrade.subject': 'Tu cuenta ha subido de categoría',
+    'email.adminUpgrade.message': `Hemos mejorado tu cuenta a {plan, select,
+    explorador {Explorer}
+    aventurero {Adventurer}
+    cuentacuentos {Storyteller}
+  } para los próximos {days} días.\n\nQue lo disfrutes,\nequipo de Literary Universe`,
+    'email.adminUpgrade.messageHtml': `Hemos mejorado tu cuenta a {plan, select,
+    explorador {Explorer}
+    aventurero {Adventurer}
+    cuentacuentos {Storyteller}
+  } para los próximos {days} días. ¡Que lo disfrutes!`,
+    'email.verification': 'Verificación de e-mail',
+    'email.verifying': 'Verificando tu e-mail. Esto sólo llevará un momento...',
+    'email.verified': '¡Buenas noticias! ¡Tu correo electrónico se ha verificado!'
 };    /* eslint-enable max-len */
