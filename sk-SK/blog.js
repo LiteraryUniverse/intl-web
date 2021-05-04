529445d698fe6177f24aaa583bc082deb987460d
Jan Dvorak <storytellercz@protonmail.com>
2021-05-05 07:23:19 +0900

    New translations blog.js (Slovak)

--- sk-SK/blog.js
+++ sk-SK/blog.js
@@ -1,15 +1,17 @@
 export default {
-    'blog.noneMsg': `V súčasnosti nemáte {type, select,
-    org {jakýkoľvek blog}
-    universe {blog pre váš vesmír}
-    user {blog}
+    'blog.noneMsg': `You currently don't have {type, select,
+    org {any blog}
+    universe {a blog for your universe}
+    user {a blog}
+    other {}
   }.`,
     'blog.create': 'Vytvor blog',
-    'blog.settings': `Nastavenia {type, select,
-    org {blogu}
-    universe {stránky vesmíru}
-    user {blogu}
-  } `,
+    'blog.settings': `{type, select,
+    org {Blog}
+    universe {Universe page}
+    user {Blog}
+    other {}
+  } settings`,
     'blog.description': 'Predstavenie blogu',
     'blog.theme': 'Téma',
     'blog.theme.info': 'Úpravou témy budete môcť zmeniť vzhľad stránky. Táto funkcia nie je v súčasnosti pripravená.',
@@ -27,14 +29,16 @@ export default {
     'blog.publicView': 'Verejný pohľad',
     'blog.post.notfound': 'Príspevok nebol nájdený',
     'blog.lists.works': `{type, select,
-    universes {Vesmíry}
-    stories {Samostatné príbehy}
-    fanfiction {Fan Fiction práca}
+    universes {Universes}
+    stories {Standalone stories}
+    fanfiction {Fan Fiction work}
+    other {}
   }`,
     'blog.lists.stories.disclaimer': 'Tu sú uvedené samostatné príbehy, ak bol príbeh napísaný ako súčasť univerza, bude uvedený na stránke univerza.',
-    'blog.social.sameAsMain': `Sociálne prepojenia sú rovnaké, ako sú nastavené v {type, select,
-    organization {nastaveniach organizácie}
-    user {profile používateľa}
+    'blog.social.sameAsMain': `Social links are the same as set in {type, select,
+    organization {organization settings}
+    user {user profile}
+    other {}
   }`,
     'blog.social.website': 'Oficiálna webstránka',
     'blog.settings.universeSettings': 'Blogy vesmíru sú integrované do stránok vesmíru a ich základné nastavenia sú prevzaté z nastavení vesmíru.',
