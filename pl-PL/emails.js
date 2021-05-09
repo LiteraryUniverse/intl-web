f0247c68ecb6e613a63e2a7ca3776047f53f78fa
Jan Dvorak <storytellercz@protonmail.com>
2021-05-09 22:09:52 +0900

    New translations emails.js (Polish)

--- pl-PL/emails.js
+++ pl-PL/emails.js
@@ -3,43 +3,43 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
-    'email.enrollAccount.message': `Hello {username}\n\n
-    We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
-    To activate your account, simply click the link below:\n\n{url}`,
+    'email.enrollAccount.subject': 'Zaproszenie do Literary Universe dla {username}',
+    'email.enrollAccount.message': `Witaj {username}\n\n
+    Chcielibyśmy zaprosić Cię do Literary Universe! Stworzyliśmy dla Ciebie specjalne osobiste konto, z którego możesz korzystać z wszystkich funkcji Literary Universe! Proszę, dołącz do nas w tworzeniu nowej generacji narzędzi do kreatywnego pisania i czytania!
+    Aby aktywować swoje konto, po prostu kliknij na poniższy link:\n\n{url}`,
     'email.resetPassword.subject': 'Literary Universe reset password',
-    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
-        Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    'email.resetPassword.message': `Witaj {username}!\n\nDostaliśmy prośbę o zresetowanie hasła do Twojego konta.
+        Proszę kliknąć na poniższy link, aby zresetować swoje hasło: \n\n{url}`,
+    'email.verifyEmail.subject': 'Weryfikacja poczty elektronicznej Literary Universe',
+    'email.verifyEmail.message': 'Witaj {username}!\n\n Proszę zweryfikować swój adres e-mail klikając na poniższy link:\n\n{url}',
     // New html email keys
-    'email.salutation': 'Hello {username},',
-    'email.welcome': 'Welcome to Literary Universe!',
-    'email.signature': 'Sincerely,<br />Literary Universe team',
+    'email.salutation': 'Witaj {username},',
+    'email.welcome': 'Witamy w Literary Universe!',
+    'email.signature': 'Z poważaniem,<br />Zespół Literary Universe',
     // Enrollment
-    'email.enrollAccount.welcome': 'Welcome aboard',
-    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-    'email.enrollAccount.button': 'Access your account',
-    'email.enrollAccount.signature': 'We are looking forward to see you,<br />Literary Universe team',
+    'email.enrollAccount.welcome': 'Witamy na pokładzie',
+    'email.enrollAccount.messageHtml': 'Zapraszamy Cię do Literary Universe! Przygotowaliśmy dla Ciebie osobiste konto, które umożliwi Ci dostęp do wszystkich funkcji Literary Universe! Dołącz do nas w tworzeniu nowej generacji kreatywnego pisania i czytania!',
+    'email.enrollAccount.button': 'Dostęp do swojego konta',
+    'email.enrollAccount.signature': 'Czekamy na Ciebie,<br />Zespół Literary Universe',
     // Reset password
-    'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account, please click on the button bellow to continue the process. If you haven\'t made this request you can ignore this e-mail, but it might be a good time to review your security settings.',
-    'email.resetPassword.button': 'Reset password',
+    'email.resetPassword.messageHtml': 'Otrzymaliśmy prośbę o zresetowanie hasła do Twojego konta, proszę kliknąć na przycisk poniżej, aby kontynuować proces. Jeśli nie złożyłeś takiej prośby, możesz zignorować ten e-mail, ale może to być dobry czas, aby przejrzeć swoje ustawienia bezpieczeństwa.',
+    'email.resetPassword.button': 'Resetowanie hasła',
     // E-mail verification
-    'email.verifyEmail.messageHtml': 'We are happy to have you on board! Please click the button bellow to verify this e-mail address.',
-    'email.verifyEmail.button': 'Verify this e-mail address',
+    'email.verifyEmail.messageHtml': 'Cieszymy się, że jesteś z nami! Proszę kliknąć na poniższy przycisk, aby zweryfikować ten adres e-mail.',
+    'email.verifyEmail.button': 'Zweryfikuj ten adres e-mail',
     // Getting premium from admins
-    'email.adminUpgrade.subject': 'Your account has been upgraded',
-    'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
-    explorer {Explorer}
-    adventurer {Adventurer}
-    storyteller {Storyteller}
-  } tier for the next {days} days.\n\nEnjoy,\nLiterary Universe team`,
-    'email.adminUpgrade.messageHtml': `We have upgraded your account to {plan, select,
-    explorer {Explorer}
-    adventurer {Adventurer}
-    storyteller {Storyteller}
-  } tier for the next {days} days. Enjoy!`,
-    'email.verification': 'E-mail verification',
-    'email.verifying': 'Verifying your e-mail. This will take just a moment...',
-    'email.verified': 'Great news! Your e-mail has been verified!'
+    'email.adminUpgrade.subject': 'Twoje konto zostało uaktualnione',
+    'email.adminUpgrade.message': `Ulepszyliśmy Twoje konto do {plan, select,
+    explorer {Odkrywca}
+    adventurer {Inwestor przygód}
+    storyteller {Historzy}
+  } Poziom na następne {days} dni.\n\nCiesz się,\nZespół Literary Universe`,
+    'email.adminUpgrade.messageHtml': `Ulepszyliśmy Twoje konto do {plan, select,
+    explorer {Eksplorator}
+    adventurer {Inwestor przygód}
+    storyteller {Historzy}
+  } Poziom na następne {days} dni. Miłej radości!`,
+    'email.verification': 'Weryfikacja poczty elektronicznej',
+    'email.verifying': 'Weryfikacja Twojego e-maila. To zajmie tylko chwilę...',
+    'email.verified': 'Wspaniała wiadomość! Twój e-mail został zweryfikowany!'
 };    /* eslint-enable max-len */
