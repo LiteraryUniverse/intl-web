e1b597644e8fb127a9b20dd74ce50d2eb6eb32d7
Jan Dvorak <storytellercz@protonmail.com>
2021-05-09 22:09:55 +0900

    New translations comments.js (Polish)

--- pl-PL/comments.js
+++ pl-PL/comments.js
@@ -1,10 +1,12 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
-    'comments.total': `There {count, plural,
-    =0 {are no comments}
-    one {is 1 comment}
-    other {are # comments}
+    'comment.write': 'Napisz swój komentarz',
+    'comments.none': 'Niestety, nie ma żadnych komentarzy do pokazania.',
+    'comments.total': `Tutaj {count, plural,
+    =0 {nie ma komentarzy}
+    one {to 1 komentarz}
+    few {są # komentarzy}
+    many {są # komentarzy}
+    other {są # komentarzy}
   }.`,
-    'comments.show.older': 'Show older comments'
+    'comments.show.older': 'Pokaż starsze komentarze'
 };
