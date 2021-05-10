fb02303e3f1b8ea567c101cb14b9fa7d34a87b66
Jan Dvorak <storytellercz@protonmail.com>
2021-05-10 17:29:16 +0900

    New translations universe.js (Japanese)

--- ja-JP/universe.js
+++ ja-JP/universe.js
@@ -3,23 +3,19 @@ export default {
     'universe.fanfiction.see': '{universe}のすべてのファンフィクション',
     'universe.fanfiction.desc': '{universe}のファンフィクション',
     'universe.fanfiction.for': '{universe}ファンフィクション',
-    'universe.stories.count': `There {num, plural,
-    =0 {are no stories}
-    one {is one story}
-    other {are # stories}
-    } in this universe.`,
+    'universe.stories.count': `この宇宙には{num, plural,
+    =0 {物語がない}
+    other {#の物語がある}
+    }。`,
     'universe.authors.title': '著者',
-    'universe.authors.desc': 'Creators and contributors to {universe}',
+    'universe.authors.desc': '{universe}のクリエーターとコントリビューター。',
     'universe.contributors': '貢献者',
     'universe.translators': '翻訳者',
     'universe.authors.notice': '各物語には、その物語に取り組んだ個人が追加されている場合があります。詳細については、もｎの詳細をご覧ください。',
-    'universe.createdBy': `{gender, select,
-    male {Created}
-    female {Created}
-    other {Created}} by `,
+    'universe.createdBy': `作成者`,
     'universe.stories.for': '{universe}の物語',
     'universe.stories.own': '{universe}物語',
-    'universe.stories.desc': 'Cannon stories for {universe}',
+    'universe.stories.desc': '{universe}のキャノニカルストーリー',
     'universe.encyclopedia.title': '{universe}百科事典',
     'universe.encyclopedia.desc': '{universe}のすべての詳細'
 };
