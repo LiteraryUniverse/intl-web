c05fd9e6bbfa6616b0515d6ed96397039cfb0e83
Jan Dvorak <storytellercz@protonmail.com>
2021-05-10 10:25:27 +0900

    New translations discovery.js (Czech)

--- cs-CZ/discovery.js
+++ cs-CZ/discovery.js
@@ -20,5 +20,33 @@ export default {
         many {# minut }
         other {# minut }
     }k přečtení.`,
-    'story.estimatedReadingTime.explained': 'Na základě průměrné rychlosti čtení {avgReadingNum} slov za minutu.'
+    'story.estimatedReadingTime.explained': 'Na základě průměrné rychlosti čtení {avgReadingNum} slov za minutu.',
+    'discover.search.text': 'Search term',
+    'discover.searchStory.title': 'Search for a story',
+    'discover.searchStory.description': 'Search for a stories',
+    'discover.searchUniverse.title': 'Search for a universe',
+    'discover.searchUniverse.description': 'Search for a universes',
+    'discover.search.storyStatus': 'Story progress status',
+    'story.status.complete': 'Complete',
+    'story.status.inProgress': 'In-progress',
+    'discover.search.universeConnection': 'Connection of story to universe',
+    'story.universeRelation.standalone': 'Standalone story',
+    'story.universeRelation.fanFiction': 'Fan Fiction',
+    'story.universeRelation.universe': 'Part of a universe',
+    'discover.search.rating': 'Age rating',
+    'discover.search.license': 'Copyright license',
+    'discover.search.language': 'Language',
+    'discover.search.resultsTotal.universe': `{results, plural,
+        zero {No universes match your parameters}
+        one {1 universe found}
+        other {# universes found}
+    }.`,
+    'discover.search.resultsTotal.story': `{results, plural,
+        zero {No stories match your parameters}
+        one {1 story found}
+        other {# stories found}
+    }.`,
+    'discover.viewSelection.aria': 'View selection',
+    'discover.searchDisplay.covers': 'Covers listing',
+    'discover.searchDisplay.list': 'Compact list'
 };
