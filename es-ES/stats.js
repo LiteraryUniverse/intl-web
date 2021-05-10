c29d6883c86203c48c9bc8bc58f516956ce8f5db
Jan Dvorak <storytellercz@protonmail.com>
2021-05-11 07:35:52 +0900

    New translations stats.js (Spanish)

--- es-ES/stats.js
+++ es-ES/stats.js
@@ -26,15 +26,15 @@ export default {
     'stats.language.title': 'Idiomas que habla',
     'stats.chapter.visitorsAndViews': 'Total de visitantes y de visitas por capítulo',
     'stats.chapter.wordCount': 'Número de palabras por capítulo',
-    'stats.chapter.chartTooltipViews': `Chapter {chapter, number}: {views, number} views`,
-    'stats.chapter.chartTooltipVisitors': `Chapter {chapter, number}: {visitors, number} unique visitors`,
-    'stats.chapter.chartTooltipWordCount': `Chapter {chapter, number}: {wordCount, number} words`,
+    'stats.chapter.chartTooltipViews': `Capítulo {chapter, number}: {views, number} visitas`,
+    'stats.chapter.chartTooltipVisitors': `Capítulo {chapter, number}: {visitors, number} visitantes únicos`,
+    'stats.chapter.chartTooltipWordCount': `Capítulo {chapter, number}: {wordCount, number} palabras`,
     'stats.totalHits': 'Visitas totales',
     'stats.totalVisitors': 'Total de visitantes únicos',
     'stats.totalWords': 'Total de palabras',
     'stats.totalEntries': 'Entradas de la enciclopedia',
     'stats.blogPage': 'Página',
-    'stats.blog.typeSelectionAria': 'Type selection',
-    'stats.blog.pages': 'Pages',
-    'stats.blog.posts': 'Posts'
+    'stats.blog.typeSelectionAria': 'Introducir selección',
+    'stats.blog.pages': 'Páginas',
+    'stats.blog.posts': 'Publicaciones'
 };
