58e6c715237bc9bec6bc60f04627740c536769ac
Jan Dvorak <storytellercz@protonmail.com>
2021-05-10 10:26:02 +0900

    New translations discovery.js (Japanese)

--- ja-JP/discovery.js
+++ ja-JP/discovery.js
@@ -6,7 +6,7 @@ export default {
     'discovery.stories.updated': '最近更新された物語',
     'discovery.continue': '続きを読む',
     'discovery.stories.similar.new': 'ご近所の新しい物語',
-    'discovery.stories.similar.regular': 'Stories in the neighborhood',
+    'discovery.stories.similar.regular': '近所の話',
     'story.estimatedReadingTime': `この物語は{hours, plural,
         =0 {}
         one {1時間と}
@@ -16,5 +16,33 @@ export default {
         one {1分}
         other {#分}
     }で読めるはずです。`,
-    'story.estimatedReadingTime.explained': 'Based on an average reading speed of {avgReadingNum} words per minute.'
+    'story.estimatedReadingTime.explained': '毎分{avgReadingNum}文字の平均読取速度に基づいています。',
+    'discover.search.text': 'Search term',
+    'discover.searchStory.title': 'Search for a story',
+    'discover.searchStory.description': 'Search for a stories',
+    'discover.searchUniverse.title': 'Search for a universe',
+    'discover.searchUniverse.description': 'Search for a universes',
+    'discover.search.storyStatus': 'Story progress status',
+    'story.status.complete': 'Complete',
+    'story.status.inProgress': 'In-progress',
+    'discover.search.universeConnection': 'Connection of story to universe',
+    'story.universeRelation.standalone': 'Standalone story',
+    'story.universeRelation.fanFiction': 'Fan Fiction',
+    'story.universeRelation.universe': 'Part of a universe',
+    'discover.search.rating': 'Age rating',
+    'discover.search.license': 'Copyright license',
+    'discover.search.language': 'Language',
+    'discover.search.resultsTotal.universe': `{results, plural,
+        zero {No universes match your parameters}
+        one {1 universe found}
+        other {# universes found}
+    }.`,
+    'discover.search.resultsTotal.story': `{results, plural,
+        zero {No stories match your parameters}
+        one {1 story found}
+        other {# stories found}
+    }.`,
+    'discover.viewSelection.aria': 'View selection',
+    'discover.searchDisplay.covers': 'Covers listing',
+    'discover.searchDisplay.list': 'Compact list'
 };
