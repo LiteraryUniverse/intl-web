de46659f612f781717047da63b7fb9a9667f72ce
Jan Dvorak <storytellercz@protonmail.com>
2021-07-26 07:37:38 +0200

    New translations user.js (Polish)

--- pl-PL/user.js
+++ pl-PL/user.js
@@ -1,20 +1,20 @@
 export default {
-    'user.friend.request.add': 'Add to friends',
-    'user.friend.requests': `You have {num} new friend {num, plural,
-    zero {requests}
-    one {request}
-    many {requests}
+    'user.friend.request.add': 'Dodaj do znajomych',
+    'user.friend.requests': `Masz {num} nowych {num, plural,
+    zero {zaproszeń}
+    one {zaproszenie}
+    many {zaproszeń}
   }`,
-    'user.friend.unfriend': 'Unfriend',
-    'user.block': 'Block',
-    'user.unblock': 'Unblock',
+    'user.friend.unfriend': 'Usuń ze znajomych',
+    'user.block': 'Zablokuj',
+    'user.unblock': 'Odblokuj',
     'user.joined': `{gender, select,
-    male {Joined}
-    female {Joined}
-    other {Joined}} on {date, date, long}`,
-    'user.friend.request.accept': 'Accept friendship request',
-    'user.friend.request.cancel': 'Cancel friendship request',
-    'user.friend.request.deny': 'Deny friendship',
+    male {Dołączył}
+    female {Dołączyła}
+    other {Dołączyli}} w dniu {date, date, long}`,
+    'user.friend.request.accept': 'Zaakceptuj zaproszenie do znajomych',
+    'user.friend.request.cancel': 'Anuluj zaproszenie do znajomych',
+    'user.friend.request.deny': 'Odrzuć zaproszenie',
     'user.listing': 'User listing',
     'user.profile.visit': 'Visit {user}\'s profile.',
     'user.profile.avatar': '{username}\'s avatar',
