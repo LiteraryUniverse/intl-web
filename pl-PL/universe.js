919feac7c4ba6bb0aaf062ced4328ac30cdf496b
Jan Dvorak <storytellercz@protonmail.com>
2021-08-06 10:24:20 +0200

    New translations universe.js (Polish)

--- pl-PL/universe.js
+++ pl-PL/universe.js
@@ -1,5 +1,5 @@
 export default {
-    'universe.authors.title.meta': '{title} authors',
+    'universe.authors.title.meta': 'Autorzy {title}',
     'universe.fanfiction.see': 'All Fan Fiction for {universe}',
     'universe.fanfiction.desc': 'Fan Fiction for {universe}',
     'universe.fanfiction.for': '{universe} Fan Fiction',
@@ -8,18 +8,18 @@ export default {
     one {is one story}
     other {are # stories}
     } in this universe.`,
-    'universe.authors.title': 'Authors',
-    'universe.authors.desc': 'Creators and contributors to {universe}',
-    'universe.contributors': 'Contributors',
-    'universe.translators': 'Translators',
+    'universe.authors.title': 'Autorzy',
+    'universe.authors.desc': 'Twórcy i współtwórcy {universe}',
+    'universe.contributors': 'Współtwórcy',
+    'universe.translators': 'Tłumacze',
     'universe.authors.notice': 'Each story might have additional personal that has worked on it. For more details see the details of the story.',
     'universe.createdBy': `{gender, select,
-    male {Created}
-    female {Created}
-    other {Created}} by `,
+    male {Stworzył}
+    female {Stworzyła}
+    other {Stworzone przez}}  `,
     'universe.stories.for': 'Stories for {universe}',
     'universe.stories.own': '{universe} stories',
     'universe.stories.desc': 'Cannon stories for {universe}',
     'universe.encyclopedia.title': '{universe} encyclopedia',
-    'universe.encyclopedia.desc': 'All the details for {universe}'
+    'universe.encyclopedia.desc': 'Wszystkie szczegóły dla {universe}'
 };
