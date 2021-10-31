74f5bd4ebbb824f6131ad786eb483d4fdeee97df
Jan Dvorak <storytellercz@protonmail.com>
2021-10-31 13:37:01 +0100

    New translations enrollment.js (Portuguese, Brazilian)

--- pt-BR/enrollment.js
+++ pt-BR/enrollment.js
@@ -1,6 +1,6 @@
 export default {
-    'enrollment.title': 'Reivindique sua conta!',
+    'enrollment.title': 'Solicite sua conta!',
     'enrollment.description': 'Bem-vindo {username}! Para ter acesso à sua conta especial, você só precisa criar uma senha.',
     'enrollment.submitting': 'Reclamando sua conta...',
-    'enrollment.submit': 'Reivindique sua conta!'
+    'enrollment.submit': 'Solicite sua conta!'
 };
