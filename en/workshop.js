1faa5eaabb26ff35681fa942e7d799dcf21c2e8c
Jan Dvorak <jdvorak@literaryuniverse.com>
2021-11-11 17:04:10 +0100

    Add keys for Prolog and Chapter sequence

--- en/workshop.js
+++ en/workshop.js
@@ -23,33 +23,43 @@ export default {
   'workshop.story.chapter.add': 'New chapter',
   'workshop.universe.new': 'Create a new universe',
   'workshop.universe.publish': 'Publish universe',
-  'workshop.universe.publishing.notice': 'If you do not publish the universe, it will be published automatically when the first story connected to the universe is published.',
+  'workshop.universe.publishing.notice':
+    'If you do not publish the universe, it will be published automatically when the first story connected to the universe is published.',
   'workshop.universe.settings': '{universe} universe settings',
-  'workshop.universe.publishedOn': 'This universe has been published on {date, date, long} {date, time, short}',
+  'workshop.universe.publishedOn':
+    'This universe has been published on {date, date, long} {date, time, short}',
   'workshop.universe.description': 'Universe description',
   'workshop.settings.title': 'Title',
   'workshop.description': 'Description',
   'workshop.language.primary': 'Primary language',
   'workshop.publication.options': 'Publication options',
   'workshop.rating': 'General rating',
-  'workshop.universe.rating.desc': 'Stories and fanfiction above this rating will be hidden from the universe page listing from anonymous users and users who are not of the appropriate age.',
+  'workshop.universe.rating.desc':
+    'Stories and fanfiction above this rating will be hidden from the universe page listing from anonymous users and users who are not of the appropriate age.',
   'workshop.universe.license': 'Universe license',
   'workshop.universe.genre.pick': 'Select genre for the universe',
   'workshop.story.genre.pick': 'Select genres for the story',
   'workshop.cover': 'Cover',
   'workshop.nothingtosee': 'Nothing to see here...',
   'workshop.story.cannon': 'Canon story',
-  'workshop.story.cannon.desc': 'This story is canonical in {universe} universe.',
-  'workshop.story.cannon.desc.create': 'This story will be created as a canonical story in {universe} universe.',
-  'workshop.story.fanfiction.search': 'Type in the name of the universe (it needs to be in your library). And then select from suggestions.',
+  'workshop.story.cannon.desc':
+    'This story is canonical in {universe} universe.',
+  'workshop.story.cannon.desc.create':
+    'This story will be created as a canonical story in {universe} universe.',
+  'workshop.story.fanfiction.search':
+    'Type in the name of the universe (it needs to be in your library). And then select from suggestions.',
   'workshop.story.error.nogenres': 'You need to select at least one genre.',
-  'workshop.story.error.toomanygenres': 'Only two genres per story are allowed.',
-  'workshop.story.error.fanfiction.selectuniverse': 'You need to select original universe for your fan fiction!',
+  'workshop.story.error.toomanygenres':
+    'Only two genres per story are allowed.',
+  'workshop.story.error.fanfiction.selectuniverse':
+    'You need to select original universe for your fan fiction!',
   'workshop.story.error.tags.toomany': 'You can only have 10 tags per story.',
   'workshop.story.new': 'Create a new story',
-  'workshop.story.publishing.notice': 'The story will be published automatically once you publish the first chapter.',
+  'workshop.story.publishing.notice':
+    'The story will be published automatically once you publish the first chapter.',
   'workshop.settings.edit': 'Edit settings',
-  'workshop.story.publishedOn': 'This story was published on {date, date, long} {date, time, short}',
+  'workshop.story.publishedOn':
+    'This story was published on {date, date, long} {date, time, short}',
   'workshop.story.settings': 'Story settings',
   'workshop.settings.basic': 'Basic settings',
   'workshop.settings.genres': 'Genres (max 2)',
@@ -60,12 +70,16 @@ export default {
   'workshop.revisions.title': '{story}: {chapter} Revisions',
   'workshop.revisions.current': 'Current draft',
   'workshop.revisions.new.beta': 'Create a new revision out of the draft.',
-  'workshop.revisions.new.public': 'Create a new public revision out of the draft.',
+  'workshop.revisions.new.public':
+    'Create a new public revision out of the draft.',
   'workshop.revisions.toeditor': 'Go to the editor',
   'workshop.revisions.comments': 'Revision comments',
-  'workshop.revisions.comments.title': '{story}: {chapter} - Revision {revision} - Comments',
-  'workshop.revisions.comments.list': '{revision}: {publication, select, beta {for Beta readers} readers {Public} other {}} {datetime, date, medium} {datetime, time, short}',
-  'workshop.revisions.comments.norevision': 'You need to create a revision first.',
+  'workshop.revisions.comments.title':
+    '{story}: {chapter} - Revision {revision} - Comments',
+  'workshop.revisions.comments.list':
+    '{revision}: {publication, select, beta {for Beta readers} readers {Public} other {}} {datetime, date, medium} {datetime, time, short}',
+  'workshop.revisions.comments.norevision':
+    'You need to create a revision first.',
   'workshop.revisions.comments.select': 'Select revision',
   'workshop.collaborators.noresults': 'No results',
   'workshop.collaborators.add.beta': 'Add Beta reader',
@@ -82,10 +96,14 @@ export default {
   'workshop.translation.original.cover': 'Original cover',
   'workshop.translation.original.tags': 'Original tags',
   'workshop.cover.alt': 'cover image',
-  'workshop.translation.updatedOrig': 'Last update of original: {updatedAt, date, long} {updatedAt, time, medium}',
-  'workshop.translation.updatedTrs': 'Last update of translation: {updatedAt, date, long} {updatedAt, time, medium}',
-  'workshop.translation.revisionOrig': 'Last revision {revision, number}, of original: {updatedAt, date, long} {updatedAt, time, medium}',
-  'workshop.translation.revisionTrs': 'Last revision {revision, number}, of translation: {updatedAt, date, long} {updatedAt, time, medium}',
+  'workshop.translation.updatedOrig':
+    'Last update of original: {updatedAt, date, long} {updatedAt, time, medium}',
+  'workshop.translation.updatedTrs':
+    'Last update of translation: {updatedAt, date, long} {updatedAt, time, medium}',
+  'workshop.translation.revisionOrig':
+    'Last revision {revision, number}, of original: {updatedAt, date, long} {updatedAt, time, medium}',
+  'workshop.translation.revisionTrs':
+    'Last revision {revision, number}, of translation: {updatedAt, date, long} {updatedAt, time, medium}',
   'workshop.collaborators.premium': `To add more {type, select,
     collaborator {collaborators}
     beta {beta readers}
@@ -106,7 +124,8 @@ export default {
   }.`,
   'workshop.collaborators.language.added': 'Language added',
   'workshop.story.noChaptersFound': 'No chapters found.',
-  'workshop.fanfic.needUniverseInLibrary': 'There are many universes out there. Please add the ones you want to write in into your library first so that we can find them.',
+  'workshop.fanfic.needUniverseInLibrary':
+    'There are many universes out there. Please add the ones you want to write in into your library first so that we can find them.',
   'workshop.fanfic.findUniverse': 'Go find an amazing universe!',
   'workshop.encSettings': 'Encyclopedia settings',
   'workshop.collaborators.removeUser': 'Remove user',
@@ -115,17 +134,21 @@ export default {
   'workshop.storylines': 'Storylines',
   'workshop.scenesManager': 'Scenes Manager',
   'workshop.collaborators.addRoleForLang': 'Add {role} for {language}',
-  'workshop.beta.revisionInfo': 'This is revision {revision, number}, created on {createdAt, date, long}.',
+  'workshop.beta.revisionInfo':
+    'This is revision {revision, number}, created on {createdAt, date, long}.',
   'workshop.story.notes.title': 'Your notes for {storyTitle}',
   'workshop.story.notes.desc': 'Overview notes for the story.',
   'workshop.story.notes.tile': 'Overview notes',
   'workshop.hideOrgs': 'Hide content from organizations',
-  'workshop.translation.premium': 'In order to add translations, you need to have at least Explorer subscription.',
+  'workshop.translation.premium':
+    'In order to add translations, you need to have at least Explorer subscription.',
   'workshop.story.finished': 'Story is complete',
-  'workshop.story.publishing.externalSellingNotice': 'Since you are selling outside of Literary Universe you can publish the story now. Links to your store listing will appear on the story or at the end of the current story you publish here.',
+  'workshop.story.publishing.externalSellingNotice':
+    'Since you are selling outside of Literary Universe you can publish the story now. Links to your store listing will appear on the story or at the end of the current story you publish here.',
   'workshop.sharing.publicURL': 'Public version',
   'workshop.fictionalTime.title': 'Fictional times for {universeName}',
-  'workshop.fictionalTime.none': 'You currently don\'t have any fictional times set-up.',
+  'workshop.fictionalTime.none':
+    "You currently don't have any fictional times set-up.",
   'workshop.fictionalTime.createNow': 'Create one now!',
   'workshop.fictionalTime.createTitle': 'Create a new fictional time',
   'workshop.fictionalTime.editTitle': 'Edit {ftName}',
@@ -137,16 +160,20 @@ export default {
   'workshop.fictionalTime.timeName': 'Time name',
   'workshop.fictionalTime.basicInfo': 'Basic information',
   'workshop.fictionalTime.timeSetup': 'Time setup',
-  'workshop.fictionalTime.linkedToET': 'Does this time start on an Earth date (Gregorian calendar)?',
+  'workshop.fictionalTime.linkedToET':
+    'Does this time start on an Earth date (Gregorian calendar)?',
   'workshop.fictionalTime.timeDeclaration': 'Time declaration',
-  'workshop.fictionalTime.declarationLocation': 'Where should be the time name be declared?',
+  'workshop.fictionalTime.declarationLocation':
+    'Where should be the time name be declared?',
   'workshop.fictionalTime.declarationBefore': 'Before units',
   'workshop.fictionalTime.declarationAfter': 'After units',
   'workshop.fictionalTime.declarationNone': 'None',
   'workshop.fictionalTime.units': 'Units',
   'workshop.fictionalTime.ETStart': 'Starting date of this time',
-  'workshop.fictionalTime.unitsDescFirst': 'How many units are there in this unit from milliseconds?',
-  'workshop.fictionalTime.unitsDesc': 'How many units is this one from the previous one?',
+  'workshop.fictionalTime.unitsDescFirst':
+    'How many units are there in this unit from milliseconds?',
+  'workshop.fictionalTime.unitsDesc':
+    'How many units is this one from the previous one?',
   'workshop.fictionalTime.unitsName': 'Unit name',
   'workshop.fictionalTime.unitsSeparator': 'Separator from previous unit',
   'workshop.fictionalTime.unitAdd': 'Add unit',
@@ -155,7 +182,8 @@ export default {
   'fictionalTime.pageStartElapsedTime': 'Time elapsed since opening this page',
   'fictionalTime.currentTime': 'Current time',
   'workshop.shoppingExternal.options': 'External purchase options',
-  'workshop.shoppingExternal.explainer': 'If your story is sold elsewhere you can set the links here. The links will then show instead of the content or at the end of the content you choose to preview.',
+  'workshop.shoppingExternal.explainer':
+    'If your story is sold elsewhere you can set the links here. The links will then show instead of the content or at the end of the content you choose to preview.',
   'workshop.shoppingExternal.amazonStore': 'Amazon store',
   'workshop.shoppingExternal.amazon': 'Amazon',
   'workshop.shoppingExternal.storeName': 'Store name',
@@ -207,24 +235,35 @@ export default {
   'fictionalTime.inputSelectETUnit': 'Select Earth time unit',
   'fictionalTime.ETDateToTimeDate': '{date, date, medium} = {timeDate}',
   'workshop.writerSidebar.title': 'Sidebar Toolbox',
-  'workshop.writerSidebar.description': 'Welcome to your story! This sidebar allows you easy access to all resources related to your story. Check the icons above to see latest comments, your notes and much more!',
+  'workshop.writerSidebar.description':
+    'Welcome to your story! This sidebar allows you easy access to all resources related to your story. Check the icons above to see latest comments, your notes and much more!',
   'workshop.story.textopian': 'Link to Textopian',
-  'workshop.story.textopianExplained': 'Textopian is a reading and discussion platform for works in the public domain.',
-  'workshop.story.authorsNotesExplained': 'Your notes for the readers to read at the beginning of the chapter.',
-  'workshop.story.chapterSortSwitch': 'Switch chapter\'s order',
+  'workshop.story.textopianExplained':
+    'Textopian is a reading and discussion platform for works in the public domain.',
+  'workshop.story.authorsNotesExplained':
+    'Your notes for the readers to read at the beginning of the chapter.',
+  'workshop.story.chapterSortSwitch': "Switch chapter's order",
   'workshop.universe.monetizationTitle': '{title} monetization',
-  'workshop.story.monetizationTitle': '{universeTitle}: {storyTitle} monetization',
+  'workshop.story.monetizationTitle':
+    '{universeTitle}: {storyTitle} monetization',
   'workshop.universe.monetizationWebMonetization': 'Web Monetization',
-  'workshop.webMonetization.explain': 'Web Monetization will stream payments to you based on how much time user spends on your content. In your profile monetization settings you can set your own payment pointer. Here you can decide on a split with your collaborators who have Web Payment enabled. Literary Universe automatically takes 10%.',
-  'workshop.webMonetization.revShare': 'Due to the limitation of Web Monetization probabilistic revenue sharing is used. Probabilistic revenue sharing works by randomly choosing from a list of predefined payment pointers each time a web monetized visitor loads your page. The visitor pays to the chosen pointer until the page is reloaded or closed.',
-  'workshop.webMonetization.revShareMore': 'Learn more about Probabilistic Revenue Sharing. We use the same code as on that page.',
+  'workshop.webMonetization.explain':
+    'Web Monetization will stream payments to you based on how much time user spends on your content. In your profile monetization settings you can set your own payment pointer. Here you can decide on a split with your collaborators who have Web Payment enabled. Literary Universe automatically takes 10%.',
+  'workshop.webMonetization.revShare':
+    'Due to the limitation of Web Monetization probabilistic revenue sharing is used. Probabilistic revenue sharing works by randomly choosing from a list of predefined payment pointers each time a web monetized visitor loads your page. The visitor pays to the chosen pointer until the page is reloaded or closed.',
+  'workshop.webMonetization.revShareMore':
+    'Learn more about Probabilistic Revenue Sharing. We use the same code as on that page.',
   'workshop.story.notEligible.licence': `The story's license does not allow for monetization.`,
   'workshop.universe.notEligible.licence': `The universe's license does not allow for monetization.`,
-  'workshop.story.notEligible.fanFiction': 'Fan Fiction stories are not eligible for monetization.',
+  'workshop.story.notEligible.fanFiction':
+    'Fan Fiction stories are not eligible for monetization.',
   'workshop.revShare.mustBe100': 'Share must equal 100%',
   'workshop.revShare.LUCut': 'Literary Universe & payment gate',
-  'workshop.revShare.unallocated': 'Unallocated share {total, number, ::percent}',
+  'workshop.revShare.unallocated':
+    'Unallocated share {total, number, ::percent}',
   'workshop.revShare.allocation': '{username} - {total, number, ::percent}',
   'workshop.revShare.undecided': 'Undecided',
   'workshop.revShare.origLangCreators': 'Original language creators',
+  'workshop.story.prolog': 'Prolog',
+  'workshop.story.chapter': 'Chapter {sequence, number}'
 }
