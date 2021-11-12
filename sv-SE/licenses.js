17c35afe6cd4a4058871383e01dea638e86de44d
Jan Dvorak <storytellercz@protonmail.com>
2021-11-12 17:54:02 +0100

    New translations licenses.js (Swedish)

--- sv-SE/licenses.js
+++ sv-SE/licenses.js
@@ -3,23 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-    'licenses.about': 'About licenses',
-    'licenses.more': 'More information',
-    'licenses.legal': 'Legal document',
-    'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
-    'licenses.cc-by': 'This license lets others distribute, remix, tweak, and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of licenses offered. Recommended for maximum dissemination and use of licensed materials.',
-    'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
-    'licenses.cc-by-sa': 'This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.',
+    'licenses.about': 'Om licenser',
+    'licenses.more': 'Mer information',
+    'licenses.legal': 'Juridiska dokument',
+    'licenses.name.cc-by': 'Creative Commons Erkännande 4.0',
+    'licenses.cc-by': 'Denna licens gör det möjligt för andra att distribuera, remixa, modifiera och bygga vidare på ditt arbete, även kommersiellt, så länge de anger att du är den ursprungliga skaparen. Detta är den mest tillmötesgående av de licenser som erbjuds. Rekommenderas för maximal spridning och användning av licensierat material.',
+    'licenses.name.cc-by-sa': 'Creative Commons Erkännande-DelaLika 4.0',
+    'licenses.cc-by-sa': 'Denna licens låter andra remixa, anpassa och bygga vidare på ditt verk även för kommersiella ändamål, så länge de anger dig som upphovsman och licensierar sina nya verk med exakt samma villkor. Denna licens jämförs ofta med licenser för fri programvara med öppen källkod. Alla nya verk baserade på ditt kommer att ha samma licens, så alla derivat kommer också att tillåta kommersiell användning. Detta är den licens som används av Wikipedia, och rekommenderas för sådant som skulle gynnas av att införliva innehåll från Wikipedia och liknande licensierade projekt.',
     'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
-    'licenses.cc-by-nd': 'This license allows for redistribution, commercial and non-commercial, as long as it is passed along unchanged and in whole, with credit to you.',
+    'licenses.cc-by-nd': 'Denna licens tillåter vidaredistribution, kommersiellt och icke-kommersiellt, så länge som den skickas vidare oförändrad och i sin helhet, med angivande av dig.',
     'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
-    'licenses.cc-by-nc': 'This license lets others remix, tweak, and build upon your work non-commercially, and although their new works must also acknowledge you and be non-commercial, they don’t have to license their derivative works on the same terms.',
+    'licenses.cc-by-nc': 'Denna licens låter andra remixa, ändra och bygga på ditt verk för icke-kommersiella syften. Deras nya verk måste också erkänna dig som upphovsman och vara icke-kommersiella men de måste inte licensiera sin bearbetning på samma villkor.',
     'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
-    'licenses.cc-by-nc-sa': 'This license lets others remix, tweak, and build upon your work non-commercially, as long as they credit you and license their new creations under the identical terms.',
+    'licenses.cc-by-nc-sa': 'Denna licens låter andra remixa, anpassa och bygga vidare på ditt verk i icke-kommersiella syften så länge de erkänner dig och licensierar sina bearbetningar under samma villkor.',
     'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
-    'licenses.cc-by-nc-nd': 'This license is the most restrictive of our six main licenses, only allowing others to download your works and share them with others as long as they credit you, but they can’t change them in any way or use them commercially.',
-    'licenses.name.lu-c': 'Literary Universe Copyrights',
-    'licenses.lu-c': 'Standard full copyrights license that allows you to sell publish your work as you like and have your rights to it protected. This license also gives you the option to sell your stories on Literary Universe.',
-    'licenses.name.cc0': 'No Rights Reserved - CC0 1.0',
-    'licenses.cc0': 'Put your work into public domain. In contrast to CC’s licenses that allow copyright holders to choose from a range of permissions while retaining their copyright, CC0 empowers yet another choice altogether – the choice to opt out of copyright and database protection, and the exclusive rights automatically granted to creators – the “no rights reserved” alternative to our licenses.'
+    'licenses.cc-by-nc-nd': 'Denna licens är den mest restriktiva av våra sex huvudlicenser. Den tillåter bara andra att ladda ner dina verk och dela dem med andra så länge de erkänner dig som upphovsman. De får inte ändra dem på något sätt eller använda dem kommersiellt.',
+    'licenses.name.lu-c': 'Litterära Universum Upphovsrätt',
+    'licenses.lu-c': 'Standardlicens med fullständiga upphovsrättigheter som gör att du kan sälja och publicera ditt verk som du vill och få dina rättigheter skyddade. Denna licens ger dig också möjlighet att sälja dina berättelser på Literary Universe.',
+    'licenses.name.cc0': 'Inga rättigheter reserverade - CC0 1,0',
+    'licenses.cc0': 'Gör ditt arbete offentligt tillgängligt. I motsats till CC-licenser som låter upphovsrättsinnehavare välja mellan en rad olika tillstånd samtidigt som de behåller sin upphovsrätt, ger CC0 ett helt annat val - valet att välja bort upphovsrätts- och databasskydd och de exklusiva rättigheter som automatiskt beviljas upphovsmännen - alternativet "inga rättigheter förbehållna" till våra licenser.'
 };
