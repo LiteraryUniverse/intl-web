5c8ae8214105d5d1cc5a3d9d486172ef1be60526
Jan Dvorak <storytellercz@protonmail.com>
2021-11-12 16:49:26 +0100

    New translations pm.js (Portuguese, Brazilian)

--- pt-BR/pm.js
+++ pt-BR/pm.js
@@ -1,22 +1,21 @@
 export default {
-    'pm.conversation': 'Conversation',
-    'pm.showolder': 'Show older messages',
-    'pm.errors.saysomething': 'You should really say something...',
-    'pm.errors.addrecipient': 'You need to add at least one other user.',
-    'pm.message': 'Message',
-    'pm.send.message': 'Send message',
-    'pm.messages.own': 'Your messages',
-    'pm.conversations.none': 'You are currently not conversing with anyone.',
-    'pm.to': 'To:',
-    'pm.with': 'Conversation with:',
-    'pm.send': 'Send',
-    'pm.conversations.unreadCount': `You have {count, plural,
-    zero {no unread conversations}
-    one {one unread conversation}
-    other {# unread conversations}
-  }.`,
-    'pm.form.minimumParticipants': 'You need to choose a recipient.',
-    'pm.form.maxParticipants': 'You can have maximum of 10 people in a conversation.',
-    'pm.new.aria': 'New conversation',
-    'pm.new.buttonTitle': 'Start a new conversation'
+    'pm.conversation': 'Conversa',
+    'pm.showolder': 'Mostrar mensagens mais antigas',
+    'pm.errors.saysomething': 'Você realmente deveria dizer algo...',
+    'pm.errors.addrecipient': 'Você precisa acrescentar pelo menos um outro usuário.',
+    'pm.message': 'Mensagem',
+    'pm.send.message': 'Enviar mensagem',
+    'pm.messages.own': 'Suas mensagens',
+    'pm.conversations.none': 'Atualmente você não está conversando com ninguém.',
+    'pm.to': 'Para:',
+    'pm.with': 'Conversa com:',
+    'pm.send': 'Enviar',
+    'pm.conversations.unreadCount': `Você tem {count, plural,
+    one {Uma conversa não lida}
+    other {# conversas não lidas}
+  }`,
+    'pm.form.minimumParticipants': 'Você precisa escolher um destinatário.',
+    'pm.form.maxParticipants': 'Você pode ter no máximo 10 pessoas em uma conversa.',
+    'pm.new.aria': 'Nova conversa',
+    'pm.new.buttonTitle': 'Iniciar uma nova conversa'
 };
