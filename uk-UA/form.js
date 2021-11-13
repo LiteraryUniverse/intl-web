a304d99cd8f08a8006486626ff1240aec1967972
Jan Dvorak <storytellercz@protonmail.com>
2021-11-13 13:51:35 +0100

    New translations form.js (Ukrainian)

--- uk-UA/form.js
+++ uk-UA/form.js
@@ -1,15 +1,15 @@
 export default {
     'form.validator.required': 'Required field',
     'form.validator.mustBeNumber': 'Must be a number',
-    'form.validator.minNumber': 'Should be greater than {min}',
-    'form.validator.maxNumber': 'Should be lower than {max}',
-    'form.validator.maxLength': `Should be less than {max} {what, select,
+    'form.validator.minNumber': 'Should be greater than {min, number}',
+    'form.validator.maxNumber': 'Should be lower than {max, number}',
+    'form.validator.maxLength': `Should be less than {max, number} {what, select,
     characters {characters}
     tags {tags}
     genres {genres}
     other {}
   }`,
-    'form.validator.minLength': `Should be at least {min} {what, select,
+    'form.validator.minLength': `Should be at least {min, number} {what, select,
     characters {characters}
     tags {tags}
     genres {genres}
@@ -29,17 +29,17 @@ export default {
     // you can get these messages from: https://github.com/gwendall/meteor-simple-schema-i18n/tree/master/i18n
     // but don't forget to adjust the format
     'simpleschema.required': '{label} є обов\'язковим',
-    'simpleschema.minString': '{label} має містити щонайменше {min} символів',
-    'simpleschema.maxString': '{label} не може перевищувати {max} символів',
-    'simpleschema.minNumber': '{label} має бути не меншим, ніж {min}',
-    'simpleschema.maxNumber': '{label} має бути не більшим, ніж {max}',
-    'simpleschema.minNumberExclusive': '{label} має бути більшим, ніж {min}',
-    'simpleschema.maxNumberExclusive': '{label} має бути меншим, ніж {max}',
-    'simpleschema.minDate': '{label} має бути не раніше, ніж {min}',
-    'simpleschema.maxDate': '{label} не може бути пізніше, ніж {max}',
+    'simpleschema.minString': '{label} must be at least {min, number} characters',
+    'simpleschema.maxString': '{label} cannot exceed {max, number} characters',
+    'simpleschema.minNumber': '{label} must be at least {min, number}',
+    'simpleschema.maxNumber': '{label} cannot exceed {max, number}',
+    'simpleschema.minNumberExclusive': '{label} must be greater than {min, number}',
+    'simpleschema.maxNumberExclusive': '{label} must be less than {max, number}',
+    'simpleschema.minDate': '{label} must be on or after {min, number}',
+    'simpleschema.maxDate': '{label} cannot be after {max, number}',
     'simpleschema.badDate': '{label} не є коректною датою',
-    'simpleschema.minCount': 'Ви повинні задати хоча б {minCount} значень',
-    'simpleschema.maxCount': 'Ви не можете задати більше, ніж {maxCount} значень',
+    'simpleschema.minCount': 'You must specify at least {minCount, number} values',
+    'simpleschema.maxCount': 'You cannot specify more than {maxCount, number} values',
     'simpleschema.noDecimal': '{label} має бути цілим числом',
     'simpleschema.notAllowed': '{value} не є допустимим значенням',
     'simpleschema.expectedString': '{label} має бути стрічкою',
