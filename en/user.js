25d0311dcab86a1fbed3654ac6205f23042677b9
Jan Dvorak <storytellercz@protonmail.com>
2021-11-13 15:44:04 +0100

    Update source file user.js

--- en/user.js
+++ en/user.js
@@ -1,6 +1,6 @@
 export default {
   'user.friend.request.add': 'Add to friends',
-  'user.friend.requests': `You have {num} new friend {num, plural,
+  'user.friend.requests': `You have {num, number} new friend {num, plural,
     zero {requests}
     one {request}
     many {requests}
@@ -16,8 +16,8 @@ export default {
   'user.friend.request.cancel': 'Cancel friendship request',
   'user.friend.request.deny': 'Deny friendship',
   'user.listing': 'User listing',
-  'user.profile.visit': 'Visit {user}\'s profile.',
-  'user.profile.avatar': '{username}\'s avatar',
+  'user.profile.visit': "Visit {user}'s profile.",
+  'user.profile.avatar': "{username}'s avatar",
   'user.friend.list.requests': 'Friendships requests',
   'user.friend.norequests': 'You have no requests for friendship.',
   'user.dashboardWelcome': `Good {dayPart, select,
