140d2928bbde9119f94abb10f17774b18b6d408c
Jan Dvorak <storytellercz@protonmail.com>
2021-11-13 22:25:33 +0100

    New translations emails.js (Italian)

--- it-IT/emails.js
+++ it-IT/emails.js
@@ -3,7 +3,7 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
+    'email.enrollAccount.subject': 'Invito all\'Universo Letterario per {username}',
     'email.enrollAccount.message': `Hello {username}\n\n
     We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
     To activate your account, simply click the link below:\n\n{url}`,
