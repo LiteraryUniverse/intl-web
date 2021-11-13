fbe5d49d1200d15ee1db88169da585ab42887bb0
Jan Dvorak <storytellercz@protonmail.com>
2021-11-13 21:13:59 +0100

    New translations user.js (German)

--- de-DE/user.js
+++ de-DE/user.js
@@ -1,9 +1,9 @@
 export default {
     'user.friend.request.add': 'Zu Freunden hinzufügen',
-    'user.friend.requests': `You have {num, number} new friend {num, plural,
-    zero {requests}
-    one {request}
-    many {requests}
+    'user.friend.requests': `Sie haben {num, number} neuen Freund {num, Plural,
+    zero {Anfrage}
+    one {Anfrage}
+    other {Anfragen}
   }`,
     'user.friend.unfriend': 'Entfreunden',
     'user.block': 'Blocken',
