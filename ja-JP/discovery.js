a05ebb7466a05f999c57072a071d921dbf9afe9c
Jan Dvorak <storytellercz@protonmail.com>
2021-11-14 17:19:29 +0100

    New translations discovery.js (Japanese)

--- ja-JP/discovery.js
+++ ja-JP/discovery.js
@@ -43,5 +43,5 @@ export default {
     'discover.viewSelection.aria': 'セレクションを見る',
     'discover.searchDisplay.covers': 'カバー一覧',
     'discover.searchDisplay.list': 'コンパクトリスト',
-    'discover.includesAds': 'This work has product placement or includes other form of advertisements in it.'
+    'discover.includesAds': 'この作品にはプロダクトプレイスメントなどの広告が含まれています。'
 };
