745ca4f7ece03d9db4a999eaf532787a837044c3
Jan Dvorak <storytellercz@protonmail.com>
2021-11-14 11:33:25 +0100

    New translations universe.js (Polish)

--- pl-PL/universe.js
+++ pl-PL/universe.js
@@ -1,25 +1,27 @@
 export default {
     'universe.authors.title.meta': 'Autorzy {title}',
-    'universe.fanfiction.see': 'All Fan Fiction for {universe}',
-    'universe.fanfiction.desc': 'Fan Fiction for {universe}',
+    'universe.fanfiction.see': 'Wszystkie Fan Fiction dla {universe}',
+    'universe.fanfiction.desc': 'Fan Fiction dla {universe}',
     'universe.fanfiction.for': '{universe} Fan Fiction',
-    'universe.stories.count': `There {num, plural,
-    =0 {are no stories}
-    one {is one story}
-    other {are # stories}
-    } in this universe.`,
+    'universe.stories.count': `Tutaj {num, plural,
+    =0 {nie ma opowieści}
+    one {to jedna opowieść}
+    few {to # opowieści}
+    many {to # opowieści}
+    other {to # opowieści}
+    } w tym wszechświecie.`,
     'universe.authors.title': 'Autorzy',
     'universe.authors.desc': 'Twórcy i współtwórcy {universe}',
     'universe.contributors': 'Współtwórcy',
     'universe.translators': 'Tłumacze',
-    'universe.authors.notice': 'Each story might have additional personal that has worked on it. For more details see the details of the story.',
+    'universe.authors.notice': 'Każda historia może mieć dodatkowe osoby, które nad nią pracowały. Więcej szczegółów można znaleźć w szczegółach historii.',
     'universe.createdBy': `{gender, select,
     male {Stworzył}
     female {Stworzyła}
     other {Stworzone przez}}  `,
-    'universe.stories.for': 'Stories for {universe}',
-    'universe.stories.own': '{universe} stories',
-    'universe.stories.desc': 'Cannon stories for {universe}',
-    'universe.encyclopedia.title': '{universe} encyclopedia',
+    'universe.stories.for': 'Opowiadania dla {universe}',
+    'universe.stories.own': '{universe} historie',
+    'universe.stories.desc': 'Historia z działem dla {universe}',
+    'universe.encyclopedia.title': '{universe} encyklopedia',
     'universe.encyclopedia.desc': 'Wszystkie szczegóły dla {universe}'
 };
