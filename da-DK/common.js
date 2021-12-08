d9b8cd150714ee21d554984b1c44e0b103fa083b
Jan Dvorak <storytellercz@protonmail.com>
2021-12-09 00:54:54 +0100

    New translations common.js (Danish)

--- da-DK/common.js
+++ da-DK/common.js
@@ -43,7 +43,7 @@ export default {
     'common.nothingfound': 'Nothing found',
     'common.explore': 'Explore',
     'common.by': 'by ',
-    // keep the space at the end as username follows
+    // keep the space at the end as username follows TODO: needs fixing
     'common.scratchpad': 'Scratchpad',
     'common.scratchpad.own': 'Your scratchpad',
     'common.news': 'Nyheder',
@@ -157,6 +157,7 @@ export default {
     'common.publish': 'Publish',
     'common.publishAt': 'Publication date and time',
     'a11y.user.menu': 'User menu',
+    'a11y.navigation': 'Navigation',
     'dashboard.profile': 'Your profile',
     'profile.website': 'My personal website',
     'story.notes.concept': 'Concept',
