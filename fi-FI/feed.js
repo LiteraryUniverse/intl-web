766c34f802a6c179cc586e6241cae048b4e0c864
Jan Dvorak <storytellercz@protonmail.com>
2021-12-13 22:32:42 +0100

    New translations feed.js (Finnish)

--- fi-FI/feed.js
+++ fi-FI/feed.js
@@ -1,7 +1,7 @@
 export default {
-    'feed.name': 'Stream',
-    'feed.post.new': 'New post',
-    'feed.post.like': 'Like post',
+    'feed.name': 'Striimi',
+    'feed.post.new': 'Uusi virka',
+    'feed.post.like': 'Tykkää viestistä',
     'feed.post.unlike': 'Unlike post',
     'feed.post.send': 'Post',
     'feed.empty': 'No entries in the feed.'
