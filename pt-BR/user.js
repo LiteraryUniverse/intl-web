9085e1fc26e7ba7b60965d56882ddd78313691c4
Jan Dvorak <storytellercz@protonmail.com>
2021-12-13 15:55:40 +0100

    New translations user.js (Portuguese, Brazilian)

--- pt-BR/user.js
+++ pt-BR/user.js
@@ -26,5 +26,9 @@ export default {
     evening {noite}
     night {dia}
     other {dia}
-  } {displayName}!`
+  } {displayName}!`,
+    'user.verification.publishedAuthor': 'This author has been published the traditional way',
+    'user.verification.luAuthor': 'Literary Universe based author of note',
+    'user.verification.luEmployee': 'Literary Universe employee',
+    'user.verification.publicFigure': 'Verified public figure'
 };
