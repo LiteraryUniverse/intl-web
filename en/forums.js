a7e2fe4a6edf4e251f2bf1ca94dd290f9b27d0d6
Jan Dvorak <storytellercz@protonmail.com>
2022-01-04 12:57:50 +0100

    Update source file forums.js

---
+++ en/forums.js
@@ -0,0 +1,21 @@
+export default {
+  'forums.create': 'Enable forums',
+  'forums.archive': 'Archive forums',
+  'forums.category.create': 'Create forum category',
+  'forums.category.delete': 'Delete category',
+  'forums.category.name': 'Category name',
+  'forums.category.language':
+    'Limit display of this category to the following language. Leave empty if you want to display the category in all languages.',
+  'forums.category.intlNameDescription':
+    'We have localized titles of common category names for your selection, so that the category can be available to all supported languages.',
+  'forums.thread.new': 'New thread',
+  'forums.thread.delete': 'Delete thread',
+  'forums.post.new': 'New post',
+
+  // Pre-defined category names
+  'forums.category.intlName.general': 'General',
+  'forums.category.intlName.announcements': 'Announcements',
+  'forums.category.intlName.stories': 'Stories',
+  'forums.category.intlName.universes': 'Universes',
+  'forums.category.intlName.universe': 'Universe',
+}
