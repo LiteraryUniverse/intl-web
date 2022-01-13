ffaa3a92b48b12c2b755bd0513b68ca6433f38eb
Jan Dvorak <storytellercz@protonmail.com>
2022-01-13 19:37:54 +0900

    New translations common.js (Irish)

--- ga-IE/common.js
+++ ga-IE/common.js
@@ -85,6 +85,7 @@ export default {
     'common.legal.community': 'Community Guidelines',
     'common.legal.privacy': 'Polasaí Príobháideachais',
     'common.legal.copyright': 'Copyright Agreement',
+    'common.legal.newVersionAvailable': 'A new version ({newVersion}) of this document will come into effect on {effectiveAt, date}.',
     'common.legal.disclaimer': '© {year} All rights reserved, all texts belong to their respective authors.',
     'premium.only.title': 'Not high enough membership rank',
     'premium.only.msg': 'We apologize, your membership rank is not high enough to access this feature.',
