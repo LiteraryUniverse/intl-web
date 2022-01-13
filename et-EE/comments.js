5981eaeb1b28aff9ba155805ed79fe979617942a
Jan Dvorak <storytellercz@protonmail.com>
2022-01-14 03:26:44 +0900

    New translations comments.js (Estonian)

--- et-EE/comments.js
+++ et-EE/comments.js
@@ -1,10 +1,10 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
+    'comment.write': 'Kirjutage oma kommentaar',
+    'comments.none': 'Vabandust, ei ole kommentaare näidata.',
     'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-    'comments.show.older': 'Show older comments'
+    'comments.show.older': 'Näita vanemaid kommentaare'
 };
