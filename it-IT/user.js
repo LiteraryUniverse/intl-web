0d2f66e2bfea59eda2583bd393d6e0a0d615a691
Jan Dvorak <storytellercz@protonmail.com>
2022-01-23 19:14:05 +0900

    New translations user.js (Italian)

--- it-IT/user.js
+++ it-IT/user.js
@@ -16,19 +16,19 @@ other {Aggiunti}} il {date, date,long}`,
     'user.friend.request.cancel': 'Cancella richiesta di amicizia',
     'user.friend.request.deny': 'Rifiuta amicizia',
     'user.listing': 'Lista utenti',
-    'user.profile.visit': 'Visit {user}\'s profile.',
-    'user.profile.avatar': '{username}\'s avatar',
-    'user.friend.list.requests': 'Friendships requests',
-    'user.friend.norequests': 'You have no requests for friendship.',
-    'user.dashboardWelcome': `Good {dayPart, select,
-    morning {morning}
-    afternoon {afternoon}
-    evening {evening}
-    night {night}
-    other {day}
+    'user.profile.visit': 'Visita il profilo di {user}.',
+    'user.profile.avatar': 'Avatar di {username}',
+    'user.friend.list.requests': 'Richieste di amicizia',
+    'user.friend.norequests': 'Non hai richieste di amicizia.',
+    'user.dashboardWelcome': `Buona {dayPart, select,
+    morning {mattinata}
+    afternoon {giornata}
+    evening {sera}
+    night {notte}
+    other {giornata}
   } {displayName}!`,
-    'user.verification.publishedAuthor': 'This author has been published the traditional way',
-    'user.verification.luAuthor': 'Literary Universe based author of note',
-    'user.verification.luEmployee': 'Literary Universe employee',
-    'user.verification.publicFigure': 'Verified public figure'
+    'user.verification.publishedAuthor': 'Questo autore è stato pubblicato in modo tradizionale',
+    'user.verification.luAuthor': 'Noto autore di Literary Universe',
+    'user.verification.luEmployee': 'Dipendente di Literary Universe',
+    'user.verification.publicFigure': 'Figura pubblica verificata'
 };
