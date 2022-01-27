a5b6f8d583012e4c5ced045239a02e413848e29b
Jan Dvorak <storytellercz@protonmail.com>
2022-01-28 07:56:25 +0900

    New translations blog.js (Vietnamese)

--- vi-VN/blog.js
+++ vi-VN/blog.js
@@ -46,5 +46,8 @@ export default {
     'blogs.new': 'Blog mới nhất',
     'blogs.title': 'Blogs of Literary Universe',
     'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog'
+    'blogs.visit': 'Visit blog',
+    'blog.settingsAria': 'Settings categories for the blog',
+    'blog.settings.blog': 'Blog',
+    'blog.settings.forums': 'Forums'
 };
