4ae2c86131a60b6c325309f9d91bd806133cca63
Jan Dvorak <storytellercz@protonmail.com>
2022-01-28 07:56:12 +0900

    New translations forums.js (Afrikaans)

--- af-ZA/forums.js
+++ af-ZA/forums.js
@@ -9,6 +9,7 @@ export default {
   'forums.category.create': 'Create forum category',
   'forums.category.delete': 'Delete category',
   'forums.category.name': 'Category name',
+  'forums.category.slug': 'SEO url part',
   'forums.category.settings': 'Category settings',
   'forums.category.language': 'Limit display of this category to the following language. Leave empty if you want to display the category in all languages.',
   'forums.category.intlNameDescription': 'We have localized titles of common category names for your selection, so that the category can be available to all supported languages.',
@@ -16,10 +17,23 @@ export default {
   'forums.thread.delete': 'Delete thread',
   'forums.post.new': 'New post',
   'forums.archived.forum': 'This forums has been archived on {archivedAt, date, long}. You can still view the forums, but you are no longer able to create new content.',
+  'forums.category.archive': 'Archive category',
+  'forums.category.reactivate': 'Re-activate category',
+  'forums.category.isVotable': 'Allow users to up/down vote threads',
   // Pre-defined category names
   'forums.category.intlName.general': 'General',
   'forums.category.intlName.announcements': 'Announcements',
   'forums.category.intlName.stories': 'Stories',
   'forums.category.intlName.universes': 'Universes',
-  'forums.category.intlName.universe': 'Universe'
+  'forums.category.intlName.universe': 'Universe',
+  'forums.category.intlName.community': 'Community',
+  'forums.category.intlName.meetups': 'Meetups',
+  'forums.category.intlName.characters': 'Characters',
+  'forums.category.intlName.locations': 'Locations',
+  'forums.category.intlName.history': 'History',
+  'forums.category.intlName.art': 'Art',
+  'forums.category.intlName.inspiration': 'Inspiration',
+  'forums.category.intlName.news': 'News',
+  // Forum restrictions
+  'forums.forumRestriction.usersOnly': 'Only allow access to registered users'
 };
