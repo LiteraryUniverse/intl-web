56be35c35d7ab17866175b1db0a40cab21ef6458
Jan Dvorak <storytellercz@protonmail.com>
2022-01-31 06:43:44 +0900

    New translations common.js (Vietnamese)

--- vi-VN/common.js
+++ vi-VN/common.js
@@ -190,5 +190,6 @@ export default {
     'common.time.years': 'Năm',
     'common.monetization': 'Monetization',
     'common.tab.monetizationOptions': 'Monetization options',
-    'common.pagination': 'Pagination'
+    'common.pagination': 'Pagination',
+    'common.forum': 'Forum'
 }    /* eslint-enable max-len */;
