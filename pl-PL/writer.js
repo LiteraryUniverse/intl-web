643899b1031c646efa0bfe71d6b9e2c7b8857a17
Jan Dvorak <storytellercz@protonmail.com>
2022-03-13 20:37:13 +0100

    New translations writer.js (Polish)

--- pl-PL/writer.js
+++ pl-PL/writer.js
@@ -15,5 +15,5 @@ export default {
     'finder.details': 'szczegóły',
     'finder.remove': 'usuń połączenie',
     'finder.connect': 'Połącz zaznaczenie z wpisem encyklopedii',
-    'writer.aside.ariaLabel': 'Help options menu'
+    'writer.aside.ariaLabel': 'Menu opcji pomocy'
 };
