54464fdb4f49d3dc7b0380d693a2632eb326c04e
Jan Dvorak <storytellercz@protonmail.com>
2022-04-03 11:58:30 +0200

    New translations challenges.js (Japanese)

--- ja-JP/challenges.js
+++ ja-JP/challenges.js
@@ -1,35 +1,35 @@
 export default {
     'challenges.current': '現在の課題',
     'challenges.wordcount.goal': '目標語数',
-    'challenges.wordcount.dailygoal': 'Target daily word count',
+    'challenges.wordcount.dailygoal': '一日の目標語数',
     'challenges.wordcount.today': '今日の記事',
-    'challenges.wordcount.total': 'Total words written',
-    'challenges.wordcount.remaining': 'Words remaining',
+    'challenges.wordcount.total': '総書記数',
+    'challenges.wordcount.remaining': '残り文字数',
     'challenges.wordcount.daysleft': '残り日数',
     'challenges.wordcount.currentDay': '今日',
-    'challenges.wordcount.average': 'Average words per day',
-    'challenges.wordcount.finishOn': 'At current speed you will finish on',
-    'challenges.wordcount.finishOnTime': 'Words to write daily to finish on time',
+    'challenges.wordcount.average': '一日あたりの平均単語数',
+    'challenges.wordcount.finishOn': '今のスピードだと、この日にゴールしてしまう。',
+    'challenges.wordcount.finishOnTime': '時間内に終わらせるために毎日書くべき言葉',
     'challenges.wordcount.graph.day': '{day}日目',
     'challenges.underway': 'この挑戦はすでに始まっています。',
-    'challenges.targetWords': 'Total targeted word count',
+    'challenges.targetWords': '目標総語数',
     'challenges.startDate': '開始日',
     'challenges.endDate': '終了日',
     'challenges.future': '今後の課題',
-    'challenges.future.wordcount': 'Wordcount challenge staring on {startDate, date, short} with goal of {target, number} by {endDate, date, short}.',
+    'challenges.future.wordcount': '{startDate, date, short}で、{endDate, date, short}までに{target, number}を目標に、ワードカウントチャレンジを開始。',
     'challenges.future.nanowrimo': 'ナショナル・ノベル・ライティング・マンスのチャレンジは準備ができています。',
-    'challenges.wordcount': 'Word count',
-    'challenges.wordcount.desc': `Set yourself a goal of how many words you want to write this month for this {type, select,
-    story {story}
-    universe {universe}
+    'challenges.wordcount': '単語数',
+    'challenges.wordcount.desc': `この{type, select,
+    story {物語}
+    universe {宇宙}
     other {}
-  }.`,
-    'challenges.wordcount.select': 'How many words do you want to challenge yourself to?',
+  }を今月は何文字書きたい、という目標を立てます。`,
+    'challenges.wordcount.select': '何語に挑戦しますか？',
     'challenges.start': 'チャレンジを開始',
     'challenges.report': '概要',
     'challenges.victory': 'おめでとう！ このチャレンジを完了しました。',
     'challenges.defeat': '残念ながら、この課題をクリアできませんでした。',
-    'challenges.baseline': 'The current wordcount for this story is {wordcount, number}, this will be the starting point.',
+    'challenges.baseline': 'この物語の現在の語数は{wordcount, number}で、これが出発点となる。',
     // NaNoWriMo
     'nanowrimo.full': 'ナショナル・ノベル・ライティング・マンス',
     'nanowrimo.site': 'NaNoWriMo公式サイト',
@@ -39,19 +39,19 @@ export default {
     'nanowrimo.key.get': 'NaNoWriMo キーを取得します。',
     'nanowrimo.key.get.notice': 'ご覧になるにはログインが必要です。',
     'nanowrimo.ongoing': 'が本格始動! {countdown}あと1日!',
-    'nanowrimo.startsin': `will start in {countdown, plural,
-      one {1 day}
-      other {# days}
-    }! Get ready!`,
+    'nanowrimo.startsin': `は、{countdown, plural,
+      one {1日}
+      other {#日}
+    }! 準備しろ！`,
     'nanowrimo.isover': 'NaNoWriMoは終わりました。また来年お会いしましょう！',
     'nanowrimo.select': 'この話をNaNoWriMoに指定する。',
     'nanowrimo.baseline': 'NaNoWriMoが始まると、現在の単語数を基準として、そこからさらに単語を追加してNaNoWriMoの一部とします。',
     'nanowrimo.victory': 'おめでとうございます。あなたはNaNoWriMoに勝ちました!',
     'nanowrimo.challenge': 'ナショナル・ノベル・ライティング・マンスチャレンジ',
-    'nanowrimo.description': 'The challenge is to write a novel of {words, number} words during November.',
-    'nanowrimo.error.usernotfound': 'Username has not been found on the NaNoWriMo servers.',
-    'nanowrimo.error.nobook': 'No book has been found. Please make sure you have created your book on NaNoWriMo.',
-    'nanowrimo.error.wordcount': 'Unable to set wordcount on NaNoWrimo, please double check your credentials and try again.',
+    'nanowrimo.description': '11月中に{words, number}の小説を書くという課題です。',
+    'nanowrimo.error.usernotfound': 'ユーザー名がNaNoWriMoのサーバーに見つかりません。',
+    'nanowrimo.error.nobook': '書籍は見つかりませんでした。NaNoWriMoで本を作成したことを確認してください。',
+    'nanowrimo.error.wordcount': 'NaNoWrimoでワードカウントを設定できません。認証情報を再確認して、もう一度やり直してください。',
     'nanowrimo.connected': 'あなたはこの物語をNaNoWriMoに指定しました。',
     'nanowrimo.connected.other': 'すでに別の話をつなげていますね。'
 };
