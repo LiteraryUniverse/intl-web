3ea17e539aee73d0e5368b4362c2abbcd14417ab
Jan Dvorak <storytellercz@protonmail.com>
2022-04-03 11:58:29 +0200

    New translations rating.js (Greek)

--- el-GR/rating.js
+++ el-GR/rating.js
@@ -2,18 +2,18 @@
  * Age ratings
  */
 export default {
-    'rating.name': 'Rating',
-    'rating.explained': 'Age rating and restriction.',
-    'rating.all': 'All',
-    'rating.desc.all': 'Accessible to all ages and groups.',
-    'rating.teens': 'Teens',
-    'rating.desc.teens': 'Suitable for an audience between ages 12 and 18.',
-    'rating.mature': 'Mature',
-    'rating.desc.mature': 'Contains advance themes, crude language and implicit scenes.',
-    'rating.explicit': 'Explicit',
-    'rating.desc.explicit': 'Contains explicit scenes.',
-    'rating.selection.all': 'All - Accessible to all ages and groups.',
-    'rating.selection.teens': 'Teens (12-18) - Suitable for audience between ages 12 and 18.',
-    'rating.selection.mature': 'Mature (18+) - Contains advance themes, crude language and implicit scenes.',
-    'rating.selection.explicit': 'Explicit (21+) - Contains explicit scenes.'
+    'rating.name': 'Αξιολόγηση',
+    'rating.explained': 'Ηλικιακή διαβάθμιση και περιορισμός.',
+    'rating.all': 'Όλα',
+    'rating.desc.all': 'Προσβάσιμο σε όλες τις ηλικίες και ομάδες.',
+    'rating.teens': 'Έφηβοι',
+    'rating.desc.teens': 'Κατάλληλο για κοινό ηλικίας 12 έως 18 ετών.',
+    'rating.mature': 'Ώριμη',
+    'rating.desc.mature': 'Περιέχει προχωρημένα θέματα, χυδαία γλώσσα και έμμεσες σκηνές.',
+    'rating.explicit': 'Ρητή',
+    'rating.desc.explicit': 'Περιέχει σαφείς σκηνές.',
+    'rating.selection.all': 'Όλοι - Προσβάσιμο σε όλες τις ηλικίες και ομάδες.',
+    'rating.selection.teens': 'Έφηβοι (12-18) - Κατάλληλο για κοινό ηλικίας 12-18 ετών.',
+    'rating.selection.mature': 'Ώριμη (18+) - Περιέχει προχωρημένα θέματα, σκληρή γλώσσα και έμμεσες σκηνές.',
+    'rating.selection.explicit': 'Ρητή (21+) - Περιέχει σαφείς σκηνές.'
 };
