a60c43791a82fda6a0dc2c9b6d299932f1a6e393
Jan Dvorak <storytellercz@protonmail.com>
2022-05-02 21:27:29 +0200

    New translations common.js (Portuguese, Brazilian)

--- pt-BR/common.js
+++ pt-BR/common.js
@@ -194,5 +194,6 @@ export default {
     'common.forum': 'Fórum',
     'common.2faCode': 'Código 2FA',
     'common.2faCodeIntro': 'Por favor, digite seu código único gerado por sua aplicação autenticadora.',
-    'common.2faCodeAria': 'Formulário de entrada de código 2FA'
+    'common.2faCodeAria': 'Formulário de entrada de código 2FA',
+    'common.flashnews': 'Flashnews'
 }    /* eslint-enable max-len */;
