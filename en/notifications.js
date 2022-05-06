168f2cfffd5f57bf8f2da92d250186f6f07b8444
Jan Dvorak <storytellercz@protonmail.com>
2022-05-06 10:26:47 +0200

    Update source file notifications.js

--- en/notifications.js
+++ en/notifications.js
@@ -5,5 +5,5 @@ export default {
   'flashnews.endsAt': 'Stop displaying this message at',
   'flashnews.newLanguage': 'Select new language to add',
   'flashnews.onlyDisplayOn':
-    'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown in default language in other languages.'
+    'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown, even in the default language, in other languages.'
 }
