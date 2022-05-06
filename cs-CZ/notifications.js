c46b141fe7ba54134a8c884034b030985df7ce98
Jan Dvorak <storytellercz@protonmail.com>
2022-05-06 09:15:56 +0200

    New translations notifications.js (Czech)

--- cs-CZ/notifications.js
+++ cs-CZ/notifications.js
@@ -1,7 +1,7 @@
 // Notifications & flashnews
 export default {
-  'flashnews.create': 'Create a new news flash',
-  'flashnews.startsAt': 'Start displaying this message at',
+  'flashnews.create': 'Vytvořit novou rychlou novinku',
+  'flashnews.startsAt': 'Začněme zobrazovat tuto zprávu od',
   'flashnews.endsAt': 'Stop displaying this message at',
   'flashnews.newLanguage': 'Select new language to add',
   'flashnews.onlyDisplayOn': 'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown in default language in other languages.'
