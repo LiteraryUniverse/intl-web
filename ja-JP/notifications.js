5b26f376b60bdfbbc5e00577b406ce0cab3ac64c
Jan Dvorak <storytellercz@protonmail.com>
2022-05-06 10:27:33 +0200

    New translations notifications.js (Japanese)

--- ja-JP/notifications.js
+++ ja-JP/notifications.js
@@ -1,8 +1,8 @@
 // Notifications & flashnews
 export default {
-  'flashnews.create': 'Create a new news flash',
-  'flashnews.startsAt': 'Start displaying this message at',
-  'flashnews.endsAt': 'Stop displaying this message at',
-  'flashnews.newLanguage': 'Select new language to add',
-  'flashnews.onlyDisplayOn': 'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown in default language in other languages.'
+  'flashnews.create': '新しいニュースフラッシュを作成',
+  'flashnews.startsAt': 'このメッセージの表示を開始：',
+  'flashnews.endsAt': 'このメッセージの表示を停止：',
+  'flashnews.newLanguage': '追加する新しい言語を選択する',
+  'flashnews.onlyDisplayOn': 'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown, even in the default language, in other languages.'
 };
