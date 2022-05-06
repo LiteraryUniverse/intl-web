5d78e0b8456c8520755115c3c90c5e37f84ed5f0
Jan Dvorak <storytellercz@protonmail.com>
2022-05-06 10:27:38 +0200

    New translations notifications.js (German)

--- de-DE/notifications.js
+++ de-DE/notifications.js
@@ -1,8 +1,8 @@
 // Notifications & flashnews
 export default {
-  'flashnews.create': 'Create a new news flash',
-  'flashnews.startsAt': 'Start displaying this message at',
-  'flashnews.endsAt': 'Stop displaying this message at',
-  'flashnews.newLanguage': 'Select new language to add',
-  'flashnews.onlyDisplayOn': 'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown in default language in other languages.'
+  'flashnews.create': 'Neuen News-Flash erstellen',
+  'flashnews.startsAt': 'Starte die Anzeige dieser Nachricht um',
+  'flashnews.endsAt': 'Diese Nachricht nicht mehr anzeigen bei',
+  'flashnews.newLanguage': 'Neue Sprache zum Hinzufügen auswählen',
+  'flashnews.onlyDisplayOn': 'Die Nachrichten werden nur in den unten ausgewählten Sprachen angezeigt (falls vorhanden), in anderen Sprachen werden keine Nachrichten angezeigt, auch nicht in der Standardsprache.'
 };
