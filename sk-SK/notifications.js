5203cc59aaf107667a9f2590bf13f0ceea637a0c
Jan Dvorak <storytellercz@protonmail.com>
2022-05-06 11:33:35 +0200

    New translations notifications.js (Slovak)

--- sk-SK/notifications.js
+++ sk-SK/notifications.js
@@ -4,5 +4,5 @@ export default {
   'flashnews.startsAt': 'Začnite zobrazovať túto správu od',
   'flashnews.endsAt': 'Prestaňte zobrazovať túto správu od',
   'flashnews.newLanguage': 'Vyberte nový jazyk, ktorý chcete pridať',
-  'flashnews.onlyDisplayOn': 'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown, even in the default language, in other languages.'
+  'flashnews.onlyDisplayOn': 'Správy sa budú zobrazovať len v jazykoch vybraných nižšie (ak sú vybrané), v iných jazykoch sa nebudú zobrazovať žiadne správy, dokonca ani v predvolenom jazyku.'
 };
