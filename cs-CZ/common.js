ba08a1020cdcf7f2bf3ed72679fda1cafbbaf90a
Jan Dvorak <storytellercz@protonmail.com>
2022-05-06 10:27:24 +0200

    New translations common.js (Czech)

--- cs-CZ/common.js
+++ cs-CZ/common.js
@@ -195,6 +195,6 @@ export default {
     'common.2faCode': '2FA kód',
     'common.2faCodeIntro': 'Zadejte Váš jednorázový kód vygenerovaný vaší ověřovací aplikací.',
     'common.2faCodeAria': 'Formulář pro zadávání kódu 2FA',
-    'common.flashnews': 'Rychlé zprávy',
+    'common.flashnews': 'Bleskové zprávy',
     'common.defaultLanguage': 'Výchozí jazyk'
 }    /* eslint-enable max-len */;
