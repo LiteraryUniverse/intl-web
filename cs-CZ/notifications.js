74fe6ff0368cf376df285e6ae9074eae237e7673
Jan Dvorak <storytellercz@protonmail.com>
2022-05-06 10:27:34 +0200

    New translations notifications.js (Czech)

--- cs-CZ/notifications.js
+++ cs-CZ/notifications.js
@@ -2,7 +2,7 @@
 export default {
   'flashnews.create': 'Vytvořit novou rychlou novinku',
   'flashnews.startsAt': 'Začněme zobrazovat tuto zprávu od',
-  'flashnews.endsAt': 'Stop displaying this message at',
-  'flashnews.newLanguage': 'Select new language to add',
-  'flashnews.onlyDisplayOn': 'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown in default language in other languages.'
+  'flashnews.endsAt': 'Přestataneme zobrazovat tuto zprávu od',
+  'flashnews.newLanguage': 'Vyberte nový jazyk pro přidání',
+  'flashnews.onlyDisplayOn': 'Zprávy budou zobrazeny pouze na jazycích vybraných níže (pokud jsou vybrány). Zpráva nebude zobrazena ve výchozím jazyku pro uživatele s jiným jazykem.'
 };
