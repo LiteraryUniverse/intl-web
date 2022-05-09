3d52d3943ffd4db5be5411feab5b1657755dcd31
Jan Dvorak <storytellercz@protonmail.com>
2022-05-09 23:32:42 +0200

    New translations settings.js (Czech)

--- cs-CZ/settings.js
+++ cs-CZ/settings.js
@@ -88,5 +88,13 @@ export default {
     'settings.2fa.instructions3': 'Naskenujte QR kód na obrazovce nebo pokud to není možné, zadejte kód níže.',
     'settings.2fa.instructions4': 'Zadejte 6 čísel, která se objeví na obrazovce do vstupu pro dokončení.',
     'settings.2fa.saved': 'Dvoufaktorové ověření bylo úspěšně vytvořeno!',
-    'settings.2fa.disableOTP': 'Vypnout OTP dvoufaktorové ověřování'
+    'settings.2fa.disableOTP': 'Vypnout OTP dvoufaktorové ověřování',
+    'settings.web3.legend': 'Web3 wallet connection',
+    'settings.web3.explained': 'You can connect your Web3 crypto wallet to you Literary Universe account. Once connected you will be able to login with your wallet and do even more things in the future as we expand our Web3 offering.',
+    'settings.web3.connect': 'Connect your wallet',
+    'settings.web3.address': 'Connected address: {address}',
+    'settings.web3.verify': 'Sign your address',
+    'settings.web3.verify.explained': 'To verify that this address belongs to you we will ask you to sign a message to ensure your ownership of the wallet.',
+    'settings.web3.verified': 'Verified',
+    'settings.web3.verificationMessage': 'Verifying user {username} for Literary Universe.'
 };
