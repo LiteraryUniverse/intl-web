7287d74696bf762f7765b12e9381c587a0f1a125
Jan Dvorak <storytellercz@protonmail.com>
2022-05-09 23:32:51 +0200

    New translations settings.js (Japanese)

--- ja-JP/settings.js
+++ ja-JP/settings.js
@@ -86,5 +86,13 @@ export default {
     'settings.2fa.instructions3': '画面上に表示されたQRコードを読み取るか、それができない場合は下のコードを入力してください。',
     'settings.2fa.instructions4': '画面に表示される6つの数字をここに入力すると終了です。',
     'settings.2fa.saved': '二要素認証が正常に確立されました。',
-    'settings.2fa.disableOTP': 'OTP二要素認証の無効化'
+    'settings.2fa.disableOTP': 'OTP二要素認証の無効化',
+    'settings.web3.legend': 'Web3 wallet connection',
+    'settings.web3.explained': 'You can connect your Web3 crypto wallet to you Literary Universe account. Once connected you will be able to login with your wallet and do even more things in the future as we expand our Web3 offering.',
+    'settings.web3.connect': 'Connect your wallet',
+    'settings.web3.address': 'Connected address: {address}',
+    'settings.web3.verify': 'Sign your address',
+    'settings.web3.verify.explained': 'To verify that this address belongs to you we will ask you to sign a message to ensure your ownership of the wallet.',
+    'settings.web3.verified': 'Verified',
+    'settings.web3.verificationMessage': 'Verifying user {username} for Literary Universe.'
 };
