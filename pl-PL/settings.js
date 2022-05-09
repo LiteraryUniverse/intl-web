e44852c81ef2556cbc89d193aaefcc3ec0202b61
Jan Dvorak <storytellercz@protonmail.com>
2022-05-09 23:32:36 +0200

    New translations settings.js (Polish)

--- pl-PL/settings.js
+++ pl-PL/settings.js
@@ -88,5 +88,13 @@ export default {
     'settings.2fa.instructions3': 'Zeskanuj kod QR widoczny na ekranie lub, jeśli nie jest to możliwe, wpisz kod znajdujący się poniżej.',
     'settings.2fa.instructions4': 'Aby zakończyć, wpisz 6 liczb, które pojawią się na ekranie, w polu wprowadzania danych.',
     'settings.2fa.saved': 'Uwierzytelnianie dwuskładnikowe zostało pomyślnie ustanowione!',
-    'settings.2fa.disableOTP': 'Wyłącz uwierzytelnianie dwuskładnikowe OTP'
+    'settings.2fa.disableOTP': 'Wyłącz uwierzytelnianie dwuskładnikowe OTP',
+    'settings.web3.legend': 'Web3 wallet connection',
+    'settings.web3.explained': 'You can connect your Web3 crypto wallet to you Literary Universe account. Once connected you will be able to login with your wallet and do even more things in the future as we expand our Web3 offering.',
+    'settings.web3.connect': 'Connect your wallet',
+    'settings.web3.address': 'Connected address: {address}',
+    'settings.web3.verify': 'Sign your address',
+    'settings.web3.verify.explained': 'To verify that this address belongs to you we will ask you to sign a message to ensure your ownership of the wallet.',
+    'settings.web3.verified': 'Verified',
+    'settings.web3.verificationMessage': 'Verifying user {username} for Literary Universe.'
 };
