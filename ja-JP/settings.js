ce2535695f43ef0b34435e258f82f13424be37b6
Jan Dvorak <storytellercz@protonmail.com>
2022-05-10 11:35:45 +0200

    New translations settings.js (Japanese)

--- ja-JP/settings.js
+++ ja-JP/settings.js
@@ -94,5 +94,7 @@ export default {
     'settings.web3.verify': 'Sign your address',
     'settings.web3.verify.explained': 'To verify that this address belongs to you we will ask you to sign a message to ensure your ownership of the wallet.',
     'settings.web3.verified': 'Verified',
-    'settings.web3.verificationMessage': 'Verifying user {username} for Literary Universe.'
+    'settings.web3.verificationMessage': 'Verifying user {username} for Literary Universe.',
+    'settings.web3.unlink': 'Remove wallet',
+    'settings.web3.linkingMessage': 'Please confirm that you want to link your wallet to Literary Universe.'
 };
