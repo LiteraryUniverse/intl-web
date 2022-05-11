51d2a9ca254761aa0dd29e24772ed488c563073f
Jan Dvorak <jdvorak@literaryuniverse.com>
2022-05-11 16:57:02 +0200

    Fixes and adjustments for Web3 strings

--- en/settings.js
+++ en/settings.js
@@ -126,8 +126,10 @@ export default {
     'To verify that this address belongs to you we will ask you to sign a message to ensure your ownership of the wallet.',
   'settings.web3.verified': 'Verified',
   'settings.web3.verificationMessage':
-    'Verifying user {username} for Literary Universe.',
+    'Verifying user {username} for Literary Universe. Code: {code}',
   'settings.web3.unlink': 'Remove wallet',
   'settings.web3.linkingMessage':
-    'Please confirm that you want to link your wallet to Literary Universe.'
+    'Please confirm that you want to link your wallet to Literary Universe. Code: {code}',
+  'settings.web3.linkError':
+    "This address is already listed in our database and can't assigned!"
 }
