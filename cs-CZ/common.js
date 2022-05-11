ee207ffd55b09dee290dab206accb99ff5193971
Jan Dvorak <storytellercz@protonmail.com>
2022-05-11 18:29:27 +0200

    New translations common.js (Czech)

--- cs-CZ/common.js
+++ cs-CZ/common.js
@@ -205,6 +205,6 @@ export default {
     'common.2faCodeAria': 'Formulář pro zadávání kódu 2FA',
     'common.flashnews': 'Bleskové zprávy',
     'common.defaultLanguage': 'Výchozí jazyk',
-    'signin.web3.signMessage': 'Please confirm that you want to enter Literary Universe with your wallet as authentication method. Code: {code}',
-    'common.web3.confirmationCode': 'Confirmation code: {code}'
+    'signin.web3.signMessage': 'Potvrďte, že chcete vstoupit do Literary Univese s Vaší peněženkou jako metodu ověření. Kód: {code}',
+    'common.web3.confirmationCode': 'Ověřovací kód: {code}'
 }    /* eslint-enable max-len */;
