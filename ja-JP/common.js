d3f23bb2d678bf66ba209f93e9d2a868a9d34dd6
Jan Dvorak <storytellercz@protonmail.com>
2022-05-11 17:12:25 +0200

    New translations common.js (Japanese)

--- ja-JP/common.js
+++ ja-JP/common.js
@@ -205,5 +205,6 @@ export default {
     'common.2faCodeAria': '2FAコードエントリフォーム',
     'common.flashnews': 'フラッシュニュース',
     'common.defaultLanguage': '既定の言語',
-    'signin.web3.signMessage': 'Please confirm that you want to enter Literary Universe with your wallet as authentication method.'
+    'signin.web3.signMessage': 'Please confirm that you want to enter Literary Universe with your wallet as authentication method. Code: {code}',
+    'common.web3.confirmationCode': 'Confirmation code: {code}'
 }    /* eslint-enable max-len */;
