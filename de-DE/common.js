05509fd47b69c9214c0edcd818755f59fa609297
Jan Dvorak <storytellercz@protonmail.com>
2022-05-11 17:12:19 +0200

    New translations common.js (German)

--- de-DE/common.js
+++ de-DE/common.js
@@ -205,5 +205,6 @@ export default {
     'common.2faCodeAria': '2FA Code Eintragsformular',
     'common.flashnews': 'Flashnews',
     'common.defaultLanguage': 'Standardsprache',
-    'signin.web3.signMessage': 'Please confirm that you want to enter Literary Universe with your wallet as authentication method.'
+    'signin.web3.signMessage': 'Please confirm that you want to enter Literary Universe with your wallet as authentication method. Code: {code}',
+    'common.web3.confirmationCode': 'Confirmation code: {code}'
 }    /* eslint-enable max-len */;
