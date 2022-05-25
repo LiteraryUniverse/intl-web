f91b1e5cdc6e5477409409c9784b57cd0ebb0c20
Jan Dvorak <storytellercz@protonmail.com>
2022-05-25 15:47:48 +0200

    New translations form.js (Hungarian)

--- hu-HU/form.js
+++ hu-HU/form.js
@@ -29,6 +29,9 @@ export default {
     'form.validator.whitespace': 'Must not have white space',
     'form.validator.zipCode': 'Must be a valid ZIP code',
     'form.validator.isbn': 'We only accept 10 or 13 version of ISBN',
+    'form.validator.btc': 'This is not a valid BTC address.',
+    'form.validator.eth': 'This is not a valid ETH address.',
+    'form.weakPassword': 'This password looks weak. Please consider making it stronger by adding numbers and special characters.',
     // you can get these messages from: https://github.com/gwendall/meteor-simple-schema-i18n/tree/master/i18n
     // but don't forget to adjust the format
     'simpleschema.required': '{label} megadása kötelező',
