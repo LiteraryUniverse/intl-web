349f08dec08f18c47cbbb4f419eba1edf6c1e777
Jan Dvorak <storytellercz@protonmail.com>
2022-05-25 15:46:54 +0200

    New translations form.js (German)

--- de-DE/form.js
+++ de-DE/form.js
@@ -28,6 +28,9 @@ export default {
     'form.validator.whitespace': 'Muss keinen Leerzeichen haben',
     'form.validator.zipCode': 'Muss eine gültige Postleitzahl sein',
     'form.validator.isbn': 'Wir akzeptieren nur 10 oder 13 Versionen von ISBN',
+    'form.validator.btc': 'This is not a valid BTC address.',
+    'form.validator.eth': 'This is not a valid ETH address.',
+    'form.weakPassword': 'This password looks weak. Please consider making it stronger by adding numbers and special characters.',
     // you can get these messages from: https://github.com/gwendall/meteor-simple-schema-i18n/tree/master/i18n
     // but don't forget to adjust the format
     'simpleschema.required': '{label} ist erforderlich',
