3de335cf88bde30e52074de982626601d2304736
Jan Dvorak <jdvorak@literaryuniverse.com>
2022-07-03 21:22:46 +0200

    Few initial strings for roadmap

--- en/roadmap.js
+++ en/roadmap.js
@@ -1,1 +1,7 @@
-export default {}
+export default {
+  'roadmap.ariaTimeOptions':
+    'Selections for roadmap items based on their status.',
+  'roadmap.past': 'Past',
+  'roadmap.current': 'In-progress',
+  'roadmap.future': 'Future'
+}
