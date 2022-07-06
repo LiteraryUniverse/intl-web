5b7ae8bc3410c81d746c5a82efafd62f4466ed6f
Jan Dvorak <jdvorak@literaryuniverse.com>
2022-07-06 20:48:32 +0200

    Quarters

--- en/roadmap.js
+++ en/roadmap.js
@@ -8,5 +8,13 @@ export default {
   'roadmap.state.inprogress': 'In-progress',
   'roadmap.state.preview': 'Preview',
   'roadmap.state.beta': 'Beta',
-  'roadmap.state.ga': 'General availability'
+  'roadmap.state.ga': 'General availability',
+  'roadmap.eta.q1.short': 'Q1',
+  'roadmap.eta.q2.short': 'Q2',
+  'roadmap.eta.q3.short': 'Q3',
+  'roadmap.eta.q4.short': 'Q4',
+  'roadmap.eta.q1.long': 'First quarter',
+  'roadmap.eta.q2.long': 'Second quarter',
+  'roadmap.eta.q3.long': 'Third quarter',
+  'roadmap.eta.q4.long': 'Fourth quarter'
 }
