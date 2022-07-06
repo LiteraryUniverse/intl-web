b46add9aadf9e43906186ea18f05fff2da9d7cc0
Jan Dvorak <jdvorak@literaryuniverse.com>
2022-07-06 20:25:52 +0200

    Roadmap states

--- en/roadmap.js
+++ en/roadmap.js
@@ -3,5 +3,10 @@ export default {
     'Selections for roadmap items based on their status.',
   'roadmap.past': 'Past',
   'roadmap.current': 'In-progress',
-  'roadmap.future': 'Future'
+  'roadmap.future': 'Future',
+  'roadmap.state.backlog': 'Backlog',
+  'roadmap.state.inprogress': 'In-progress',
+  'roadmap.state.preview': 'Preview',
+  'roadmap.state.beta': 'Beta',
+  'roadmap.state.ga': 'General availability'
 }
