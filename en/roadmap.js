36d8de3264f0e0565dbe5c13bb60cde6568a916e
Jan Dvorak <storytellercz@protonmail.com>
2022-07-07 21:23:15 +0200

    Update source file roadmap.js

--- en/roadmap.js
+++ en/roadmap.js
@@ -3,5 +3,18 @@ export default {
     'Selections for roadmap items based on their status.',
   'roadmap.past': 'Past',
   'roadmap.current': 'In-progress',
-  'roadmap.future': 'Future'
+  'roadmap.future': 'Future',
+  'roadmap.state.backlog': 'Backlog',
+  'roadmap.state.inprogress': 'In-progress',
+  'roadmap.state.preview': 'Preview',
+  'roadmap.state.beta': 'Beta',
+  'roadmap.state.ga': 'General availability',
+  'roadmap.eta.q1.short': 'Q1',
+  'roadmap.eta.q2.short': 'Q2',
+  'roadmap.eta.q3.short': 'Q3',
+  'roadmap.eta.q4.short': 'Q4',
+  'roadmap.eta.q1.long': 'First quarter',
+  'roadmap.eta.q2.long': 'Second quarter',
+  'roadmap.eta.q3.long': 'Third quarter',
+  'roadmap.eta.q4.long': 'Fourth quarter'
 }
