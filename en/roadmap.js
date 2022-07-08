3603873f240a2f44d09be2a890df0577718e9fcd
Jan Dvorak <storytellercz@protonmail.com>
2022-07-08 19:30:39 +0200

    Update source file roadmap.js

--- en/roadmap.js
+++ en/roadmap.js
@@ -1,9 +1,6 @@
 export default {
   'roadmap.ariaTimeOptions':
     'Selections for roadmap items based on their status.',
-  'roadmap.past': 'Past',
-  'roadmap.current': 'In-progress',
-  'roadmap.future': 'Future',
   'roadmap.state.backlog': 'Backlog',
   'roadmap.state.inprogress': 'In-progress',
   'roadmap.state.preview': 'Preview',
