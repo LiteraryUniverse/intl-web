b7e478e8afb1efd8918ed120f8641c4047a4250a
Jan Dvorak <storytellercz@protonmail.com>
2022-07-08 19:32:20 +0200

    New translations pages.js (Polish)

--- pl-PL/pages.js
+++ pl-PL/pages.js
@@ -59,5 +59,17 @@ export default {
     'about.attributions': 'Przypisy',
     'about.attributionsIntro': 'Literary Universe opiera się na wielu projektach Open Source i wolnych zasobach. Poniżej znajdziesz ich listę.',
     'about.attributionsMatomo': 'W naszych analizach wykorzystujemy samoobsługowe Matomo z danymi IP2Location LITE dostępnymi na stronie https://lite.ip2location.com.',
-    'about.attributionIcons': 'Następujące ikony są używane w witrynie za pośrednictwem styled-icons:'
+    'about.attributionIcons': 'Następujące ikony są używane w witrynie za pośrednictwem styled-icons:',
+    'team.name.sedwards': 'Scott Edwards',
+    'team.name.rhubbell': 'Raena Hubbell',
+    'team.name.pjakubec': 'Pavel Jakubec',
+    'team.name.asebesta': 'Adam Šebesta',
+    'about.developer': 'Developer',
+    'about.seniorDeveloper': 'Senior Developer',
+    'about.juniorDeveloper': 'Junior Developer',
+    'about.apprenticeDeveloper': 'Apprentice Developer',
+    'about.intern': 'Intern',
+    'about.advisor': 'Advisor',
+    'about.headOfResearch': 'Head of Research',
+    'about.cfo': 'Chief Financial Officer'
 };
