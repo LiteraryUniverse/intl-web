ef704fc3712e561f63bfdd8122ac1e68b38b7ee8
Jan Dvorak <storytellercz@protonmail.com>
2022-07-10 21:39:36 +0200

    New translations roadmap.js (German)

--- de-DE/roadmap.js
+++ de-DE/roadmap.js
@@ -1,34 +1,34 @@
 export default {
-  'roadmap.ariaTimeOptions': 'Selections for roadmap items based on their status.',
+  'roadmap.ariaTimeOptions': 'Auswahlen für Roadmap-Elemente auf der Grundlage ihres Status.',
   'roadmap.state.backlog': 'Backlog',
-  'roadmap.state.inprogress': 'In-progress',
-  'roadmap.state.preview': 'Preview',
+  'roadmap.state.inprogress': 'In Bearbeitung',
+  'roadmap.state.preview': 'Vorschau',
   'roadmap.state.beta': 'Beta',
-  'roadmap.state.ga': 'General availability',
-  'roadmap.eta.q1.short': 'Q1',
-  'roadmap.eta.q2.short': 'Q2',
-  'roadmap.eta.q3.short': 'Q3',
-  'roadmap.eta.q4.short': 'Q4',
-  'roadmap.eta.q1.long': 'First quarter',
-  'roadmap.eta.q2.long': 'Second quarter',
-  'roadmap.eta.q3.long': 'Third quarter',
-  'roadmap.eta.q4.long': 'Fourth quarter',
+  'roadmap.state.ga': 'Allgemeine Verfügbarkeit',
+  'roadmap.eta.q1.short': 'V1',
+  'roadmap.eta.q2.short': 'V2',
+  'roadmap.eta.q3.short': 'V3',
+  'roadmap.eta.q4.short': 'V4',
+  'roadmap.eta.q1.long': 'Erstes Viertel',
+  'roadmap.eta.q2.long': 'Zweites Viertel',
+  'roadmap.eta.q3.long': 'Drittes Viertel',
+  'roadmap.eta.q4.long': 'Viertes Viertel',
   'roadmap.backlog': 'Backlog',
-  'roadmap.history': 'History of development',
-  'roadmap.item.inBacklog': 'We are planning to develop this feature in the future.',
-  'roadmap.item.inProgress': 'The development of the feature started {startedAt, date}.',
-  'roadmap.item.inBeta': 'This feature has entered beta on {betaAt, date}.',
-  'roadmap.item.inPreview': 'This feature became available to Storyteller subscribers on {previewAt, date}.',
-  'roadmap.item.inGa': 'This feature has become generally available on {gaAt, date}.',
-  'roadmap.item.originalIssue': 'This feature was originally suggested by our users.',
-  'roadmap.eta.mapTitle': `Planned for {quarter, select,
-    Q1 {Q1}
-    Q2 {Q2}
-    Q3 {Q3}
-    Q4 {Q4}
+  'roadmap.history': 'Entwicklungsgeschichte',
+  'roadmap.item.inBacklog': 'Wir planen diese Funktion in Zukunft zu entwickeln.',
+  'roadmap.item.inProgress': 'Die Entwicklung der Funktion startete {startedAt, date}.',
+  'roadmap.item.inBeta': 'Diese Funktion ist Beta auf {betaAt, date}.',
+  'roadmap.item.inPreview': 'Diese Funktion wurde für Storyteller-Abonnenten auf {previewAt, date}.',
+  'roadmap.item.inGa': 'Diese Funktion ist allgemein verfügbar auf {gaAt, date}.',
+  'roadmap.item.originalIssue': 'Diese Funktion wurde ursprünglich von unseren Benutzern vorgeschlagen.',
+  'roadmap.eta.mapTitle': `Geplant für {quarter, select,
+    Q1 {V1}
+    Q2 {V2}
+    Q3 {V3}
+    Q4 {V4}
     other {}
   } {year, date, ::yyyy}`,
-  'roadmap.back': 'Back to roadmap',
-  'roadmap.seeBacklog': 'Check out what other things we plan to do',
-  'roadmap.seeFinished': 'If you like to dig more into our history, we have a nice overview ready for you'
+  'roadmap.back': 'Zurück zur Roadmap',
+  'roadmap.seeBacklog': 'Sieh dir an, was wir sonst noch tun wollen',
+  'roadmap.seeFinished': 'Wenn du mehr in unsere Geschichte graben möchtest, haben wir einen netten Überblick für dich bereit'
 };
