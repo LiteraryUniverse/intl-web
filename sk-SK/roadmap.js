e4f542611c2318a3249f070a658191a897944fe1
Jan Dvorak <storytellercz@protonmail.com>
2022-07-10 21:39:42 +0200

    New translations roadmap.js (Slovak)

--- sk-SK/roadmap.js
+++ sk-SK/roadmap.js
@@ -1,34 +1,34 @@
 export default {
-  'roadmap.ariaTimeOptions': 'Selections for roadmap items based on their status.',
+  'roadmap.ariaTimeOptions': 'Výbery položiek plánu na základe ich stavu.',
   'roadmap.state.backlog': 'Backlog',
-  'roadmap.state.inprogress': 'In-progress',
-  'roadmap.state.preview': 'Preview',
+  'roadmap.state.inprogress': 'Rozpracovaný',
+  'roadmap.state.preview': 'Náhľad',
   'roadmap.state.beta': 'Beta',
-  'roadmap.state.ga': 'General availability',
-  'roadmap.eta.q1.short': 'Q1',
-  'roadmap.eta.q2.short': 'Q2',
-  'roadmap.eta.q3.short': 'Q3',
-  'roadmap.eta.q4.short': 'Q4',
-  'roadmap.eta.q1.long': 'First quarter',
-  'roadmap.eta.q2.long': 'Second quarter',
-  'roadmap.eta.q3.long': 'Third quarter',
-  'roadmap.eta.q4.long': 'Fourth quarter',
+  'roadmap.state.ga': 'Všeobecná dostupnosť',
+  'roadmap.eta.q1.short': 'Š1',
+  'roadmap.eta.q2.short': 'Š2',
+  'roadmap.eta.q3.short': 'Š3',
+  'roadmap.eta.q4.short': 'Š4',
+  'roadmap.eta.q1.long': 'Prvý štvrťrok',
+  'roadmap.eta.q2.long': 'Druhý štvrťrok',
+  'roadmap.eta.q3.long': 'Tretí štvrťrok',
+  'roadmap.eta.q4.long': 'Štvrtý štvrťrok',
   'roadmap.backlog': 'Backlog',
-  'roadmap.history': 'History of development',
-  'roadmap.item.inBacklog': 'We are planning to develop this feature in the future.',
-  'roadmap.item.inProgress': 'The development of the feature started {startedAt, date}.',
-  'roadmap.item.inBeta': 'This feature has entered beta on {betaAt, date}.',
-  'roadmap.item.inPreview': 'This feature became available to Storyteller subscribers on {previewAt, date}.',
-  'roadmap.item.inGa': 'This feature has become generally available on {gaAt, date}.',
-  'roadmap.item.originalIssue': 'This feature was originally suggested by our users.',
+  'roadmap.history': 'História vývoja',
+  'roadmap.item.inBacklog': 'Túto funkciu plánujeme v budúcnosti rozvíjať.',
+  'roadmap.item.inProgress': 'Vývoj funkcie sa začal {startedAt, date}.',
+  'roadmap.item.inBeta': 'Táto funkcia vstúpila do beta verzie dňa {betaAt, date}.',
+  'roadmap.item.inPreview': 'Táto funkcia bola sprístupnená predplatiteľom služby Storyteller dňa {previewAt, date}.',
+  'roadmap.item.inGa': 'Táto funkcia je všeobecne dostupná od {gaAt, date}.',
+  'roadmap.item.originalIssue': 'Túto funkciu pôvodne navrhli naši používatelia.',
   'roadmap.eta.mapTitle': `Planned for {quarter, select,
-    Q1 {Q1}
-    Q2 {Q2}
-    Q3 {Q3}
-    Q4 {Q4}
+    Q1 {Š1}
+    Q2 {Š2}
+    Q3 {Š3}
+    Q4 {Š4}
     other {}
   } {year, date, ::yyyy}`,
-  'roadmap.back': 'Back to roadmap',
-  'roadmap.seeBacklog': 'Check out what other things we plan to do',
-  'roadmap.seeFinished': 'If you like to dig more into our history, we have a nice overview ready for you'
+  'roadmap.back': 'Späť na plán',
+  'roadmap.seeBacklog': 'Pozrite sa, čo ďalšie plánujeme urobiť',
+  'roadmap.seeFinished': 'Ak by ste chceli preniknúť do našej histórie, máme pre vás pripravený pekný prehľad'
 };
