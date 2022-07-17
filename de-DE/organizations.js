263057d85819bfcb925b0c5ef73f2bd906316d9a
Jan Dvorak <storytellercz@protonmail.com>
2022-07-17 22:10:44 +0200

    New translations organizations.js (German)

--- de-DE/organizations.js
+++ de-DE/organizations.js
@@ -4,6 +4,7 @@ export default {
     'org.settings.name': 'Name der Organisation',
     'org.settings.membersPrivate': 'Mitglieder NICHT öffentlich anzeigen',
     'org.logo': 'Organisationslogo',
+    'org.logo.alt': '{organizationName} logo',
     'org.superAdmin': 'Super Admin',
     'org.settings.language': 'Primäre Sprache',
     'org.meta.desc': 'Organisationen und kreative Gruppen auf Literary Universe.',
@@ -30,5 +31,17 @@ export default {
     internal {Die Organisation}
     allProperties {zeige auf alle geistigen Eigentumsrechte}
     other {Die Organisation}
-  }`
+  }`,
+    'org.members.invited': 'Invited members',
+    'org.members.list': 'Organization members',
+    'org.invitesTileNotice': 'You have been invited to {count, number} {count, plural, one {organization} other {organizations}}',
+    'org.invitePage.title': 'Invitations to organizations',
+    'org.invitePage.invitedAt': 'You have been invited on {invitedAt, date, short}.',
+    'org.invitePage.ignoring': 'You are currently ignoring this invitation, which means that it is still active, but doesn\'t bothers with notifications.',
+    'org.invitePage.expiresAt': 'This invitation will expire at {expiresAt, date, short} {expiresAt, time}',
+    'org.invitePage.accept': 'Join organization',
+    'org.invitePage.reject': 'Reject invitation',
+    'org.invitePage.ignore': 'Ignore for now',
+    'org.leaveOrg.title': 'Leave organization',
+    'org.leaveOrg.confirm': 'This is a destructive action. Once you leave you will loose all access to this organization. Are you sure you want to leave the organization?'
 };
