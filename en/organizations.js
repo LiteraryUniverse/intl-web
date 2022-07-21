2cec52c0934092d91aa916da09aa59351b5fe404
Jan Dvorak <jdvorak@literaryuniverse.com>
2022-07-21 15:52:35 +0200

    Org user removal texts

--- en/organizations.js
+++ en/organizations.js
@@ -21,7 +21,7 @@ export default {
   'org.authorOverride.legend': 'Author override',
   'org.authorOverride.desc':
     'Shows this text instead of the name of the organization.',
-  'org.authorOverride.label': 'Author\'s name',
+  'org.authorOverride.label': "Author's name",
   'org.membersAdd.title': 'Invite members to the organization',
   'org.flashnews.admin': 'Flashnews administration',
   'org.settings.tabListAria': 'Setting sections for organization',
@@ -35,14 +35,23 @@ export default {
   }`,
   'org.members.invited': 'Invited members',
   'org.members.list': 'Organization members',
-  'org.invitesTileNotice': 'You have been invited to {count, number} {count, plural, one {organization} other {organizations}}',
+  'org.invitesTileNotice':
+    'You have been invited to {count, number} {count, plural, one {organization} other {organizations}}',
   'org.invitePage.title': 'Invitations to organizations',
-  'org.invitePage.invitedAt': 'You have been invited on {invitedAt, date, short}.',
-  'org.invitePage.ignoring': 'You are currently ignoring this invitation, which means that it is still active, but doesn\'t bothers with notifications.',
-  'org.invitePage.expiresAt': 'This invitation will expire at {expiresAt, date, short} {expiresAt, time}',
+  'org.invitePage.invitedAt':
+    'You have been invited on {invitedAt, date, short}.',
+  'org.invitePage.ignoring':
+    "You are currently ignoring this invitation, which means that it is still active, but doesn't bothers with notifications.",
+  'org.invitePage.expiresAt':
+    'This invitation will expire at {expiresAt, date, short} {expiresAt, time}',
   'org.invitePage.accept': 'Join organization',
   'org.invitePage.reject': 'Reject invitation',
   'org.invitePage.ignore': 'Ignore for now',
   'org.leaveOrg.title': 'Leave organization',
-  'org.leaveOrg.confirm': 'This is a destructive action. Once you leave you will loose all access to this organization. Are you sure you want to leave the organization?'
+  'org.leaveOrg.confirm':
+    'This is a destructive action. Once you leave you will loose all access to this organization. Are you sure you want to leave the organization?',
+  'org.confirmRemove.aria': 'Confirm user removal',
+  'org.confirmRemove.confirm':
+    'Are you sure that you want to remove {username} from {organization}?',
+  'org.confirmRemove.remove': 'Remove user'
 }
