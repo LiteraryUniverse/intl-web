88a131d8d5ca0a07b9ab02dc79b18b35c016d09d
Jan Dvorak <storytellercz@protonmail.com>
2022-07-21 16:07:58 +0200

    New translations organizations.js (Czech)

--- cs-CZ/organizations.js
+++ cs-CZ/organizations.js
@@ -43,5 +43,8 @@ export default {
     'org.invitePage.reject': 'Reject invitation',
     'org.invitePage.ignore': 'Ignore for now',
     'org.leaveOrg.title': 'Leave organization',
-    'org.leaveOrg.confirm': 'This is a destructive action. Once you leave you will loose all access to this organization. Are you sure you want to leave the organization?'
+    'org.leaveOrg.confirm': 'This is a destructive action. Once you leave you will loose all access to this organization. Are you sure you want to leave the organization?',
+    'org.confirmRemove.aria': 'Confirm user removal',
+    'org.confirmRemove.confirm': 'Are you sure that you want to remove {username} from {organization}?',
+    'org.confirmRemove.remove': 'Remove user'
 };
