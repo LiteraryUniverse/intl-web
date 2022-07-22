6b05c984a191117de6e06390bdd1d819b92f6463
Jan Dvorak <storytellercz@protonmail.com>
2022-07-22 21:17:13 +0200

    New translations groups.js (Czech)

--- cs-CZ/groups.js
+++ cs-CZ/groups.js
@@ -1,1 +1,12 @@
-export default {};
+export default {
+    'groups.listing.title': 'User groups',
+    'groups.listing.organizationTitle': 'Teams',
+    'groups.create.title': 'Create a new team',
+    'groups.type.label': 'Type',
+    'groups.type.open': 'Open',
+    'groups.type.openDesc': 'Open- Anyone can join the group.',
+    'groups.type.closed': 'Closed',
+    'groups.type.closedDesc': 'Closed - People need to approved before they can join your group.',
+    'groups.type.hidden': 'Hidden',
+    'groups.type.hiddenDesc': 'Hidden - Only people you invite can join.'
+};
