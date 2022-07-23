bccf95d5b003ced4108d65399f534dc00b3fc6a5
Jan Dvorak <storytellercz@protonmail.com>
2022-07-23 20:21:52 +0200

    New translations common.js (Portuguese)

--- pt-PT/common.js
+++ pt-PT/common.js
@@ -208,5 +208,8 @@ export default {
     'signin.web3.signMessage': 'Please confirm that you want to enter Literary Universe with your wallet as authentication method. Code: {code}',
     'common.web3.confirmationCode': 'Confirmation code: {code}',
     'common.roadmap': 'Roadmap',
-    'common.luStatsIFrameOptOut': 'Literary Universe statistics opt-out'
+    'common.luStatsIFrameOptOut': 'Literary Universe statistics opt-out',
+    'common.image': 'Image',
+    'common.home': 'Home',
+    'common.administration': 'Administration'
 }    /* eslint-enable max-len */;
