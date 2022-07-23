5c5f538a695f2a4aa5a0802eea474c88497dcc37
Jan Dvorak <storytellercz@protonmail.com>
2022-07-23 20:22:39 +0200

    New translations groups.js (Portuguese, Brazilian)

--- pt-BR/groups.js
+++ pt-BR/groups.js
@@ -4,9 +4,19 @@ export default {
     'groups.create.title': 'Create a new team',
     'groups.type.label': 'Type',
     'groups.type.open': 'Open',
-    'groups.type.openDesc': 'Open- Anyone can join the group.',
+    'groups.type.openDesc': 'Open - Anyone can join the group.',
     'groups.type.closed': 'Closed',
     'groups.type.closedDesc': 'Closed - People need to approved before they can join your group.',
     'groups.type.hidden': 'Hidden',
-    'groups.type.hiddenDesc': 'Hidden - Only people you invite can join.'
+    'groups.type.hiddenDesc': 'Hidden - Only people you invite can join.',
+    'groups.imageAlt': '{groupName} picture',
+    'groups.members': 'Members',
+    'groups.members.total': `There {totalMembers, plural,
+    zero {are no members}
+    one {is one member}
+    other {are # members}
+  } in this group`,
+    'groups.leaveGroup': 'Leave group',
+    'groups.leaveGroup.confirm': 'Just to make sure. Are you certain that you want to leave {groupName}?',
+    'group.settings': 'Group settings'
 };
