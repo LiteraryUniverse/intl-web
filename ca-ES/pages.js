6d56ee880c6ee66ae86e9b554c5c6230bc777e4e
Jan Dvorak <storytellercz@protonmail.com>
2022-07-25 09:57:24 +0200

    New translations pages.js (Catalan)

--- ca-ES/pages.js
+++ ca-ES/pages.js
@@ -60,7 +60,6 @@ export default {
     'about.attributionsIntro': 'Literary Universe builds on top of many Open Source projects and free resources. Bellow you will find their listing.',
     'about.attributionsMatomo': 'Our analytics use self-hosted Matomo with IP2Location LITE data available from https://lite.ip2location.com.',
     'about.attributionIcons': 'The following icons are used through the site via styled-icons:',
-    'team.name.sedwards': 'Scott Edwards',
     'team.name.rhubbell': 'Raena Hubbell',
     'team.name.pjakubec': 'Pavel Jakubec',
     'team.name.asebesta': 'Adam Šebesta',
@@ -71,6 +70,5 @@ export default {
     'about.intern': 'Intern',
     'about.advisor': 'Assessor',
     'about.headOfResearch': 'Head of Research',
-    'about.CFO': 'Chief Financial Officer',
-    'about.scott': 'Scott is the co-founder of Literary Universe. Scott became interested in creative writing in his early years and to this day is an avid reader. As someone who has always wanted to write his own book Scott became interested in Literary Universe when Jan Dvorak approached him with the idea for Literary Universe while they were in college. Scott has been been a part of Jan\'s development team since its inception and created the name for the site, among other notable contributions. Scott manages the business side of Literary Universe to allow Jan to focus on technological development.'
+    'about.CFO': 'Chief Financial Officer'
 };
