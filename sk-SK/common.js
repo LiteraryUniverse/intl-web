ba0dcf5d890945313acaa9b3d90478860c7852f7
Jan Dvorak <storytellercz@protonmail.com>
2022-07-26 21:19:01 +0200

    New translations common.js (Slovak)

--- sk-SK/common.js
+++ sk-SK/common.js
@@ -211,5 +211,8 @@ export default {
     'common.luStatsIFrameOptOut': 'Štatistiky Literary Universe opt-out',
     'common.image': 'Image',
     'common.home': 'Home',
-    'common.administration': 'Administration'
+    'common.administration': 'Administration',
+    'common.truncateAppend': '...',
+    // What should be the symbol used to denote that the text continues when it was truncated
+    'common.groups': 'Groups'
 }    /* eslint-enable max-len */;
