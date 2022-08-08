204d173dd1f3ac1c69f2807dfb4137407d254d76
Jan Dvorak <storytellercz@protonmail.com>
2022-08-08 11:56:56 +0200

    New translations feed.js (Maithili)

---
+++ mai-IN/feed.js
@@ -0,0 +1,8 @@
+export default {
+    'feed.name': 'Stream',
+    'feed.post.new': 'New post',
+    'feed.post.like': 'Like post',
+    'feed.post.unlike': 'Unlike post',
+    'feed.post.send': 'Post',
+    'feed.empty': 'No entries in the feed.'
+};
