b30a63b1e58987c4452550bacee0aec7a9981f96
Jan Dvorak <storytellercz@protonmail.com>
2022-08-08 11:54:34 +0200

    New translations roadmap.js (Malayalam)

---
+++ ml-IN/roadmap.js
@@ -0,0 +1,37 @@
+export default {
+  'roadmap.ariaTimeOptions': 'Selections for roadmap items based on their status.',
+  'roadmap.state.backlog': 'Backlog',
+  'roadmap.state.inprogress': 'In-progress',
+  'roadmap.state.preview': 'Preview',
+  'roadmap.state.beta': 'Beta',
+  'roadmap.state.ga': 'General availability',
+  'roadmap.eta.q1.short': 'Q1',
+  'roadmap.eta.q2.short': 'Q2',
+  'roadmap.eta.q3.short': 'Q3',
+  'roadmap.eta.q4.short': 'Q4',
+  'roadmap.eta.q1.long': 'First quarter',
+  'roadmap.eta.q2.long': 'Second quarter',
+  'roadmap.eta.q3.long': 'Third quarter',
+  'roadmap.eta.q4.long': 'Fourth quarter',
+  'roadmap.backlog': 'Backlog',
+  'roadmap.history': 'History of development',
+  'roadmap.item.inBacklog': 'We are planning to develop this feature in the future.',
+  'roadmap.item.inProgress': 'The development of the feature started {startedAt, date}.',
+  'roadmap.item.inBeta': 'This feature has entered beta on {betaAt, date}.',
+  'roadmap.item.inPreview': 'This feature became available to Storyteller subscribers on {previewAt, date}.',
+  'roadmap.item.inGa': 'This feature has become generally available on {gaAt, date}.',
+  'roadmap.item.originalIssue': 'This feature was originally suggested by our users.',
+  'roadmap.eta.mapTitle': `Planned for {quarter, select,
+    Q1 {Q1}
+    Q2 {Q2}
+    Q3 {Q3}
+    Q4 {Q4}
+    other {}
+  } {year, date, ::yyyy}`,
+  'roadmap.eta.mapTitleFinished': 'Finished in {time, date, ::MMMM ::yyyy}',
+  'roadmap.back': 'Back to roadmap',
+  'roadmap.seeBacklog': 'Check out what other things we plan to do',
+  'roadmap.seeFinished': 'If you like to dig more into our history, we have a nice overview ready for you',
+  'roadmap.planned': 'Planned',
+  'roadmap.finished': 'Finished'
+};
