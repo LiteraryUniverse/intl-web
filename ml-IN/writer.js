bb3acd5fd019629215ffa3a6c71f924946e38e64
Jan Dvorak <storytellercz@protonmail.com>
2022-08-08 11:54:31 +0200

    New translations writer.js (Malayalam)

---
+++ ml-IN/writer.js
@@ -0,0 +1,19 @@
+export default {
+    'writer.menu.optionsAria': 'Editor options menu',
+    'writer.menu.saveOptionsAria': 'Save options',
+    'writer.undo': 'Undo',
+    'writer.redo': 'Redo',
+    'writer.ul': 'Unordered list',
+    'writer.ol': 'Ordered list',
+    'writer.blockquote': 'Quote',
+    'writer.link': 'Insert a link',
+    'writer.image': 'Insert an image',
+    'writer.bold': 'Bold the selection',
+    'writer.underline': 'Underline the selection',
+    'writer.italic': 'Emphasize the selection',
+    'writer.wordcount': '{characters, number} characters, {words, number} words',
+    'finder.details': 'details',
+    'finder.remove': 'remove the connection',
+    'finder.connect': 'Connect selection to encyclopedia entry',
+    'writer.aside.ariaLabel': 'Help options menu'
+};
