fac0c082508bc3d3879c737d50d0c8381f33964a
Jan Dvorak <storytellercz@protonmail.com>
2022-08-12 17:09:48 +0200

    New translations events.js (Czech)

--- cs-CZ/events.js
+++ cs-CZ/events.js
@@ -9,29 +9,29 @@ export default {
   'events.details.description': 'Event description / Agenda',
   'events.details.url': 'Link to more details',
   'events.details.virtualLink': 'Link to virtual location (ie. Zoom, Google Meets, etc.)',
-  'events.details.address': 'Venue address',
-  'events.details.startsAt': 'Event start',
-  'events.details.endsAt': 'Event ends',
-  'events.details.type': 'Event type',
-  'events.type.inPerson': 'In-person',
-  'events.type.virtual': 'Virtual',
-  'events.type.hybrid': 'Hybrid',
-  'events.join.startsIn': 'Event starts {startsAt}',
+  'events.details.address': 'Adresa místa konání',
+  'events.details.startsAt': 'Začátek',
+  'events.details.endsAt': 'Konec',
+  'events.details.type': 'Typ události',
+  'events.type.inPerson': 'Osobní',
+  'events.type.virtual': 'Virtuální',
+  'events.type.hybrid': 'Hybridní',
+  'events.join.startsIn': 'Událost začíná {startsAt}',
   // The startsAt is going to be in a relative time format. Eg. `in 1 minute`, `1 day ago`, etc...
   'events.join.virtual': 'Join event',
   'events.selection.origin': 'Event origin',
-  'events.origin.universe': 'Universe',
-  'events.origin.author': 'Author',
-  'events.origin.groups': 'Groups',
-  'events.origin.organizations': 'Organizations',
-  'events.origin.lu': 'Literary Universe events',
-  'events.visibility.public': 'Public - anyone can see this event',
+  'events.origin.universe': 'Vesmír',
+  'events.origin.author': 'Autor',
+  'events.origin.groups': 'Skupiny',
+  'events.origin.organizations': 'Organizace',
+  'events.origin.lu': 'Oficiální akce Literary Universe',
+  'events.visibility.public': 'Veřejná - Kdokoli může vidět tuto událost',
   'events.visibility.owner': `Owner - only {type, select,
     group {members of your group}
     universe {collaborators and fans of the universe}
     organization {members of your organization}
     other {you and the people you invite}
   } can see this event`,
-  'events.visibility.private': 'Private - only people you invite will see the event',
-  'events.inviting': 'Can people who see this event invite others?'
+  'events.visibility.private': 'Soukromé - pouze osoby, které pozvete uvidí událost',
+  'events.inviting': 'Mohou lidé, kteří vidí tuto událost, pozvat ostatní?'
 };
