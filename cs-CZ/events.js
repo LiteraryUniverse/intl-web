b4ba8f7e51146493eedae54109321b6f07659d57
Jan Dvorak <hello@jandvorak.me>
2022-09-04 20:52:16 +0200

    New translations events.js (Czech)

--- cs-CZ/events.js
+++ cs-CZ/events.js
@@ -38,5 +38,9 @@ export default {
   'events.administration': 'Administrace akcí',
   'events.create.title': 'Vytvořit novou akci',
   'events.hostedBy': 'Hosted by {hostName}',
-  'events.openVirtual': 'Go to virtual location'
+  'events.openVirtual': 'Go to virtual location',
+  'events.moreInfo': 'More information',
+  'event.visibilityShort.private': 'Private',
+  'event.visibilityShort.public': 'Public',
+  'event.visibilityShort.owner': 'Owner'
 };
