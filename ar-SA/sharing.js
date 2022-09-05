b45507f3e2aa4a997ca61c5ff6eae1fc7669bc5d
Jan Dvorak <hello@jandvorak.me>
2022-09-05 15:36:17 +0200

    New translations sharing.js (Arabic)

--- ar-SA/sharing.js
+++ ar-SA/sharing.js
@@ -1,6 +1,6 @@
 export default {
-    'sharing.title': 'Share',
-    'sharing.copy': 'Copy',
+    'sharing.title': 'مشاركة',
+    'sharing.copy': 'نسخ',
     'sharing.email': 'E-mail',
     'sharing.sms': 'SMS',
     'sharing.messenger': 'Facebook Messenger',
