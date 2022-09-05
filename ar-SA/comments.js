be3593eb6fb95acb52e5c3a8b5c2376febd358b0
Jan Dvorak <hello@jandvorak.me>
2022-09-05 11:49:42 +0200

    New translations comments.js (Arabic)

--- ar-SA/comments.js
+++ ar-SA/comments.js
@@ -1,10 +1,10 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
+    'comment.write': 'اكتب تعليقك',
+    'comments.none': 'عذراً، لا توجد تعليقات للعرض.',
     'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-    'comments.show.older': 'Show older comments'
+    'comments.show.older': 'عرض التعليقات الأقدم'
 };
