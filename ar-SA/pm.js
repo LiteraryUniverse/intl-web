7cfaa8d5ba6bad473dd6dbd1a82b2b2c1722856e
Jan Dvorak <hello@jandvorak.me>
2022-09-05 14:36:35 +0200

    New translations pm.js (Arabic)

--- ar-SA/pm.js
+++ ar-SA/pm.js
@@ -4,12 +4,12 @@ export default {
     'pm.errors.saysomething': 'You should really say something...',
     'pm.errors.addrecipient': 'You need to add at least one other user.',
     'pm.message': 'Message',
-    'pm.send.message': 'Send message',
-    'pm.messages.own': 'Your messages',
+    'pm.send.message': 'إرسال رسالة',
+    'pm.messages.own': 'رسائلك',
     'pm.conversations.none': 'You are currently not conversing with anyone.',
-    'pm.to': 'To:',
-    'pm.with': 'Conversation with:',
-    'pm.send': 'Send',
+    'pm.to': 'إلى:',
+    'pm.with': 'محادثة مع:',
+    'pm.send': 'إرسال',
     'pm.conversations.unreadCount': `You have {count, plural,
     zero {no unread conversations}
     one {one unread conversation}
@@ -17,6 +17,6 @@ export default {
   }.`,
     'pm.form.minimumParticipants': 'You need to choose a recipient.',
     'pm.form.maxParticipants': 'You can have maximum of 10 people in a conversation.',
-    'pm.new.aria': 'New conversation',
-    'pm.new.buttonTitle': 'Start a new conversation'
+    'pm.new.aria': 'محادثة جديدة',
+    'pm.new.buttonTitle': 'بدء محادثة جديدة'
 };
