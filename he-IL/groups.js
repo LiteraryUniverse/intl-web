499668a08ceaf43ff7c6f447e5583ff009db778a
Jan Dvorak <hello@jandvorak.me>
2022-09-05 10:41:28 +0200

    New translations groups.js (Hebrew)

---
+++ he-IL/groups.js
@@ -0,0 +1,41 @@
+export default {
+    'groups.listing.title': 'User groups',
+    'groups.listing.organizationTitle': 'Teams',
+    'groups.create.title': 'Create a new group',
+    'groups.type.label': 'Type',
+    'groups.type.open': 'Open',
+    'groups.type.openDesc': 'Open - Anyone can join the group.',
+    'groups.type.closed': 'Closed',
+    'groups.type.closedDesc': 'Closed - People need to approved before they can join your group.',
+    'groups.type.hidden': 'Hidden',
+    'groups.type.hiddenDesc': 'Hidden - Only people you invite can join.',
+    'groups.imageAlt': '{groupName} picture',
+    'groups.members': 'Members',
+    'groups.members.total': `There {totalMembers, plural,
+    zero {are no members}
+    one {is one member}
+    other {are # members}
+  } in this group`,
+    'groups.leaveGroup': 'Leave group',
+    'groups.leaveGroup.confirm': 'Just to make sure. Are you certain that you want to leave {groupName}?',
+    'group.settings': 'Group settings',
+    'group.role.member': 'Member',
+    'group.role.moderator': 'Moderator',
+    'group.role.admin': 'Admin',
+    'groups.join.openGroup': 'Join group',
+    'groups.join.closedGroup': 'Request to join group',
+    'groups.join.questionToJoin': 'Question for new members',
+    'groups.join.closedAria': 'Request form to join closed group',
+    'groups.join.questionDefault': 'Why should you be allowed to join?',
+    'groups.join.pendingRequest': 'Your request to join this group is waiting for approval from the group admins.',
+    'groups.join.requesting': 'Requesting to join',
+    'groups.invite.btn': 'Invite to group',
+    'groups.invite.modalAria': 'Invite users to group dialog',
+    'groups.invite.send': 'Send invite',
+    'groups.invite.label': 'Find user to invite',
+    'groups.invite.tileMsg': `You have {count, plural,
+    other {# group invites}
+  }`,
+    'groups.invites.title': 'Group invitations',
+    'group.invite.accept': 'Accept invite'
+};
