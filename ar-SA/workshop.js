fe70fea34049e8ed38f86e1eb025d64410bfc71d
Jan Dvorak <hello@jandvorak.me>
2022-09-05 15:36:18 +0200

    New translations workshop.js (Arabic)

--- ar-SA/workshop.js
+++ ar-SA/workshop.js
@@ -1,5 +1,5 @@
 export default {
-    'workshop.title': 'Your work',
+    'workshop.title': 'عملك',
     'workshop.noneassigned': 'You have not been assigned any yet.',
     'workshop.notavailable': 'This feature is not yet available.',
     'workshop.none': `You currently have no {type, select,
