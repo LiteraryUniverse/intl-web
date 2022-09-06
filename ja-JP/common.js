9c8b0f939bf35b950dab935a7c45d65a6d50c3c6
Jan Dvorak <hello@jandvorak.me>
2022-09-06 20:52:55 +0200

    New translations common.js (Japanese)

--- ja-JP/common.js
+++ ja-JP/common.js
@@ -5,7 +5,7 @@
 export default {
     siteName: '文学の宇宙',
     twitterHandle: '@lituniapp',
-    'site.description': '文学の宇宙は、壮大な物語の宇宙のための創造的で出版のプラットフォームです。',
+    'site.description': 'Literary Universe is an all-in-one creative and publishing platform for grand story universes.',
     'site.keywords': '文芸, SF, ファンタジー, 書き込み, 出版, 翻訳, 物語, 宇宙, ストーリー, ウェブ小説',
     'site.logoAlt': '文学の宇宙のロゴ',
     mobileMenu: 'モバイル メニュー',
