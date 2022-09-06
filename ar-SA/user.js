15dcd8aaa63c65aaa20fc15309243fdd76f396b4
Jan Dvorak <hello@jandvorak.me>
2022-09-06 18:52:36 +0200

    New translations user.js (Arabic)

--- ar-SA/user.js
+++ ar-SA/user.js
@@ -5,21 +5,21 @@ export default {
     one {request}
     many {requests}
   }`,
-    'user.friend.unfriend': 'Unfriend',
-    'user.block': 'Block',
-    'user.unblock': 'Unblock',
+    'user.friend.unfriend': 'إلغاء الصداقة',
+    'user.block': 'حظر',
+    'user.unblock': 'إلغاء الحظر',
     'user.joined': `{gender, select,
     male {Joined}
     female {Joined}
     other {Joined}} on {date, date, long}`,
-    'user.friend.request.accept': 'Accept friendship request',
-    'user.friend.request.cancel': 'Cancel friendship request',
-    'user.friend.request.deny': 'Deny friendship',
+    'user.friend.request.accept': 'قبول طلب الصداقة',
+    'user.friend.request.cancel': 'إلغاء طلب الصداقة',
+    'user.friend.request.deny': 'رفض الصداقة',
     'user.listing': 'User listing',
-    'user.profile.visit': 'Visit {user}\'s profile.',
+    'user.profile.visit': 'زيارة ملف {user} الشخصي.',
     'user.profile.avatar': '{username}\'s avatar',
-    'user.friend.list.requests': 'Friendships requests',
-    'user.friend.norequests': 'You have no requests for friendship.',
+    'user.friend.list.requests': 'طلبات الصداقة',
+    'user.friend.norequests': 'ليس لديك أي طلبات للصداقة.',
     'user.dashboardWelcome': `Good {dayPart, select,
     morning {morning}
     afternoon {afternoon}
