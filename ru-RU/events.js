e1e048a34b862d853e8ba8318720f4134f6a753b
Jan Dvorak <hello@jandvorak.me>
2022-09-06 20:52:49 +0200

    New translations events.js (Russian)

--- ru-RU/events.js
+++ ru-RU/events.js
@@ -27,12 +27,12 @@ export default {
   'events.origin.lu': 'Literary Universe events',
   'events.visibility.label': 'Event visibility',
   'events.visibility.public': 'Public - anyone can see this event',
-  'events.visibility.owner': `Owner - only {type, select,
-    group {members of your group}
-    universe {collaborators and fans of the universe}
-    organization {members of your organization}
-    other {you and the people you invite}
-  } can see this event`,
+  'events.visibility.owner': `Владелец - только {type, select,
+    group {члены вашей группы}
+    universe {соавторы и поклонники вселенной}
+    organization {члены вашей организации}
+    other {вы и приглашенные вами люди}
+  } могут видеть это событие`,
   'events.visibility.private': 'Private - only people you invite will see the event',
   'events.inviting': 'Can people who see this event invite others?',
   'events.administration': 'Events administration',
