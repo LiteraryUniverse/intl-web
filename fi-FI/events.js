46c132d250c9f79d70d4da3e0bf1f94c440ea39e
Jan Dvorak <hello@jandvorak.me>
2022-09-06 20:52:46 +0200

    New translations events.js (Finnish)

--- fi-FI/events.js
+++ fi-FI/events.js
@@ -27,12 +27,12 @@ export default {
   'events.origin.lu': 'Literary Universe events',
   'events.visibility.label': 'Event visibility',
   'events.visibility.public': 'Public - anyone can see this event',
-  'events.visibility.owner': `Owner - only {type, select,
-    group {members of your group}
-    universe {collaborators and fans of the universe}
-    organization {members of your organization}
-    other {you and the people you invite}
-  } can see this event`,
+  'events.visibility.owner': `Omistaja - vain {type, select,
+    group {Ryhmäsi jäsenet}
+    universe {maailmankaikkeuden yhteistyökumppanit ja fanit}
+    organization {organisaatiosi jäsenet}
+    other {sinä ja kutsumasi ihmiset}
+  } voi nähdä tämän tapahtuman`,
   'events.visibility.private': 'Private - only people you invite will see the event',
   'events.inviting': 'Can people who see this event invite others?',
   'events.administration': 'Events administration',
