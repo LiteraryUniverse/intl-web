20422701ba0e69b26c139dd10859f271c5f96b8b
Jan Dvorak <hello@jandvorak.me>
2022-10-06 21:34:38 +0200

    New translations settings.js (Italian)

--- it-IT/settings.js
+++ it-IT/settings.js
@@ -100,5 +100,16 @@ export default {
     'settings.web3.verificationMessage': 'Verifica dell\'utente {username} per Literary Universe. Codice: {code}',
     'settings.web3.unlink': 'Rimuovere il portafoglio',
     'settings.web3.linkingMessage': 'Confermate di voler collegare il vostro portafoglio a Literary Universe. Codice: {code}',
-    'settings.web3.linkError': 'Questo indirizzo è già presente nel nostro database e non può essere assegnato!'
+    'settings.web3.linkError': 'Questo indirizzo è già presente nel nostro database e non può essere assegnato!',
+    // Reader settings
+    'settings.reader.title': 'Reader settings',
+    'settings.reader.darkMode': 'Always display in dark mode',
+    'settings.reader.asideView': 'Show magical aside panel',
+    'settings.reader.textSize': 'Show text in larger font',
+    // Accessibility
+    'settings.accessibility.title': 'Reader accessibility',
+    'settings.accessibility.openDyslexic': 'Use OpenDyslexic font',
+    'settings.accessibility.paragraphStripes': 'Alternate in paragraph background color',
+    'settings.accessibility.paragraphHighlighting': 'Highlight the current paragraph that you are hovering over',
+    'settings.accessibility.ruler': 'Attach a ruler to the mouse'
 };
