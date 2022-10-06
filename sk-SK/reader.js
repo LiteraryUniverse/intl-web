80cb2248859506c73930cce0f316b797d2b5bcd4
Jan Dvorak <hello@jandvorak.me>
2022-10-07 00:41:52 +0200

    New translations reader.js (Slovak)

--- sk-SK/reader.js
+++ sk-SK/reader.js
@@ -26,7 +26,7 @@ export default {
     'external.textopian': 'Textopian',
     'external.textopian.title': 'Čítanie a diskusia o verejne prístupných prácach na portáli Textopian',
     'reader.accessibility.aria': 'Accessibility options',
-    'reader.accessibility.toggleDyslectic': 'Toggle dyslectic text',
+    'reader.accessibility.toggleDyslexic': 'Toggle dyslexic font',
     'reader.accessibility.toggleParagraphStripes': 'Toggle paragraphs background alteration',
     'reader.accessibility.toggleParagraphHighlight': 'Toggle selected paragraph highlighting',
     'reader.accessibility.toggleRules': 'Toggle assistive ruler'
