da15a08477bc26457314deaa535c063cd492a776
Jan Dvorak <hello@jandvorak.me>
2022-10-07 10:01:10 +0200

    New translations fan_manager.js (Slovak)

--- sk-SK/fan_manager.js
+++ sk-SK/fan_manager.js
@@ -1,12 +1,12 @@
 export default {
-  'fanManager.settings.for': 'Fan manager settings',
-  'fanManager.allowArtSubmissions': 'Allow fan art submissions',
-  'fanManager.displayFanArt': 'Allow displaying fan arts',
-  'fanManager.allowFanStories': 'Allow fan fiction',
-  'fanManager.displayFanStories': 'Allow displaying fan stories',
-  'fanManager.state.aria': 'Select state of submitted artwork',
-  'fanManager.state.submitted': 'Submitted',
-  'fanManager.state.approved': 'Approved',
-  'fanManager.state.rejected': 'Rejected',
-  'fanManager.state.hidden': 'Hidden'
+  'fanManager.settings.for': 'Správce fanoušku',
+  'fanManager.allowArtSubmissions': 'Povoľte predkladanie diel fanúšikov',
+  'fanManager.displayFanArt': 'Povolenie zobrazovania fanúšikovských umení',
+  'fanManager.allowFanStories': 'Povoľte fanfiction',
+  'fanManager.displayFanStories': 'Umožniť zobrazovanie príbehov fanúšikov',
+  'fanManager.state.aria': 'Vyberte stav predloženého diela',
+  'fanManager.state.submitted': 'Odoslané',
+  'fanManager.state.approved': 'Schválené',
+  'fanManager.state.rejected': 'Odmietnuté',
+  'fanManager.state.hidden': 'Skryté'
 };
