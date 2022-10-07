cea288bf8f87adc5abf9dfc13d418d847217b2bb
Jan Dvorak <hello@jandvorak.me>
2022-10-07 10:01:15 +0200

    New translations fan_manager.js (German)

--- de-DE/fan_manager.js
+++ de-DE/fan_manager.js
@@ -1,12 +1,12 @@
 export default {
-  'fanManager.settings.for': 'Fan manager settings',
-  'fanManager.allowArtSubmissions': 'Allow fan art submissions',
-  'fanManager.displayFanArt': 'Allow displaying fan arts',
-  'fanManager.allowFanStories': 'Allow fan fiction',
-  'fanManager.displayFanStories': 'Allow displaying fan stories',
-  'fanManager.state.aria': 'Select state of submitted artwork',
-  'fanManager.state.submitted': 'Submitted',
-  'fanManager.state.approved': 'Approved',
-  'fanManager.state.rejected': 'Rejected',
-  'fanManager.state.hidden': 'Hidden'
+  'fanManager.settings.for': 'Fan-Manager',
+  'fanManager.allowArtSubmissions': 'Einsendungen von Fankunst zulassen',
+  'fanManager.displayFanArt': 'Anzeige von Fankunstwerken zulassen',
+  'fanManager.allowFanStories': 'Fan-Fiction zulassen',
+  'fanManager.displayFanStories': 'Erlauben Sie die Anzeige von Fan-Geschichten',
+  'fanManager.state.aria': 'Status des eingereichten Kunstwerks auswählen',
+  'fanManager.state.submitted': 'Übermittelt',
+  'fanManager.state.approved': 'Bestätigt',
+  'fanManager.state.rejected': 'Abgelehnt',
+  'fanManager.state.hidden': 'Versteckt'
 };
