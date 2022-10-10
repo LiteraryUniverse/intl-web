b5dcdf95f1face5aa1343a231ad59178dcfd0a30
Jan Dvorak <hello@jandvorak.me>
2022-10-10 09:56:10 +0200

    New translations fan_manager.js (Spanish)

--- es-ES/fan_manager.js
+++ es-ES/fan_manager.js
@@ -8,5 +8,9 @@ export default {
   'fanManager.state.submitted': 'Enviado',
   'fanManager.state.approved': 'Aprobado',
   'fanManager.state.rejected': 'Rechazado',
-  'fanManager.state.hidden': 'Oculto'
+  'fanManager.state.hidden': 'Oculto',
+  'fanManager.publicComment': 'Public comment',
+  'fanManager.publicComment.desc': 'This text will be displayed on the public page as your official comment on this art.',
+  'fanManager.authorComment': 'Comment for the author',
+  'fanManager.authorComment.desc': 'This is a private comment from you to the author of this art.'
 };
