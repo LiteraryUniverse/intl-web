48759774dcd5e207c4e0bc9f362eea319f61640a
Jan Dvorak <hello@jandvorak.me>
2022-10-10 09:56:14 +0200

    New translations fan_manager.js (Czech)

--- cs-CZ/fan_manager.js
+++ cs-CZ/fan_manager.js
@@ -8,5 +8,9 @@ export default {
   'fanManager.state.submitted': 'Odeslaný',
   'fanManager.state.approved': 'Schválený',
   'fanManager.state.rejected': 'Odmítnuté',
-  'fanManager.state.hidden': 'Skryté'
+  'fanManager.state.hidden': 'Skryté',
+  'fanManager.publicComment': 'Public comment',
+  'fanManager.publicComment.desc': 'This text will be displayed on the public page as your official comment on this art.',
+  'fanManager.authorComment': 'Comment for the author',
+  'fanManager.authorComment.desc': 'This is a private comment from you to the author of this art.'
 };
