7d6572e55669b5aa490c7d917885a88c9fdb08e5
Jan Dvorak <hello@jandvorak.me>
2022-10-30 00:02:58 +0200

    Update source file feedback.js

--- en/feedback.js
+++ en/feedback.js
@@ -1,9 +1,11 @@
 export default {
   'feedback.home': 'Help center',
   'feedback.bugs': 'Issues',
-  'feedback.bugs.desc': 'Did you encounter an error or something else that is wrong or out of place. Let us know here!',
+  'feedback.bugs.desc':
+    'Did you encounter an error or something else that is wrong or out of place. Let us know here!',
   'feedback.features': 'Ideas & Proposals',
-  'feedback.features.desc': 'Have ideas for new features or how to make the site better? Let us know!',
+  'feedback.features.desc':
+    'Have ideas for new features or how to make the site better? Let us know!',
   'feedback.help': 'Help & Tutorials',
   'feedback.help.desc': 'How do you _____? Go here to find the answers.',
   'feedback.desc': 'Welcome to our help and resolution center!',
@@ -15,8 +17,10 @@ export default {
   'feedback.fixed': 'Fixed',
   'feedback.new.bugs': 'Report new issue',
   'feedback.new.features': 'Suggest a new feature',
-  'feedback.lang.nosupport': 'Your current language is not supported for feedback, please write in English. Thank you!',
-  'feedback.new.thanks': 'Thank you for the notice! We will look into it shorty.',
+  'feedback.lang.nosupport':
+    'Your current language is not supported for feedback, please write in English. Thank you!',
+  'feedback.new.thanks':
+    'Thank you for the notice! We will look into it shorty.',
   'feedback.comment.title': 'Comment',
   'feedback.comment.desc': 'Send us your feedback',
   'feedback.comment.legend': 'Your feedback - let us know about anything',
@@ -24,7 +28,8 @@ export default {
   'feedback.comment.label': 'Your comment',
   'feedback.copyrightReport.title': 'Copyright violation',
   'feedback.copyrightReport.desc': 'Report Copyright violation',
-  'feedback.report.noUser': 'You need to be signed in, in order to file a report.',
+  'feedback.report.noUser':
+    'You need to be signed in, in order to file a report.',
   'feedback.voting.totalVoters': 'Users voted: {voted, number}',
   'feedback.voting.upvote': 'Up vote',
   'feedback.voting.downvote': 'Down vote',
@@ -35,14 +40,19 @@ export default {
   'feedback.reportType.vulgar': 'Vulgar/obscene',
   'feedback.reportType.copyright': 'Copyright violation',
   'feedback.reportType.storyRating': 'Incorrect age rating',
+  'feedback.reportType.scam': 'Scam',
   'feedback.report.submit': 'Report',
   'feedback.report.submitting': 'Reporting...',
   'feedback.report.violationDesc': 'Details',
-  'feedback.report.reportSaved': 'Thank you for the report! We will investigate and take action if needed.',
-  'feedback.report.copyrightReportSaved': 'Thank you for the report! We will investigate and be in touch.',
+  'feedback.report.reportSaved':
+    'Thank you for the report! We will investigate and take action if needed.',
+  'feedback.report.copyrightReportSaved':
+    'Thank you for the report! We will investigate and be in touch.',
   'feedback.report.copyrightOwnershipDesc': 'Ownership statement',
-  'feedback.report.copyrightNotice': 'Please note that you need to be the rightful owner of the work to use this form. If you are not the owner, please inform the owner or use the other category to inform us. We take copyright violations extremely seriously and the same applies to improper and abusive use of this form.',
-  'feedback.report.copyrightInternalProperty': 'I have published this work on Literary Universe',
+  'feedback.report.copyrightNotice':
+    'Please note that you need to be the rightful owner of the work to use this form. If you are not the owner, please inform the owner or use the other category to inform us. We take copyright violations extremely seriously and the same applies to improper and abusive use of this form.',
+  'feedback.report.copyrightInternalProperty':
+    'I have published this work on Literary Universe',
   'feedback.reportType.spam': 'Spam or phishing',
   'feedback.reportType.other': 'Other',
   'feedback.report.copyrightType.propertySelectLabel': `Which {type, select,
@@ -50,4 +60,4 @@ export default {
     story {story}
     other {story}
   } is being infringed?`
-};
+}
