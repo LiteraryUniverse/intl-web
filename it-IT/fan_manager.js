c2363adac9bea04b87414a3c89a594cb6dbff5b7
Jan Dvorak <hello@jandvorak.me>
2022-10-31 12:56:08 +0100

    New translations fan_manager.js (Italian)

--- it-IT/fan_manager.js
+++ it-IT/fan_manager.js
@@ -12,5 +12,9 @@ export default {
   'fanManager.publicComment': 'Public comment',
   'fanManager.publicComment.desc': 'This text will be displayed on the public page as your official comment on this art.',
   'fanManager.authorComment': 'Comment for the author',
-  'fanManager.authorComment.desc': 'This is a private comment from you to the author of this art.'
+  'fanManager.authorComment.desc': 'This is a private comment from you to the author of this art.',
+  'fanManager.stateChange.approve': 'Approve fan art',
+  'fanManager.stateChange.reject': 'Reject fan art',
+  'fanManager.stateChange.hide': 'Hide fan art',
+  'fanManager.stateChange.unhide': 'Make fan art visible'
 };
