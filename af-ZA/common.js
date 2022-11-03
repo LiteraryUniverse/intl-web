b326f18d526816408c30183910d8ac4e3a0f7536
Jan Dvorak <hello@jandvorak.me>
2022-11-03 21:35:57 +0100

    New translations common.js (Afrikaans)

--- af-ZA/common.js
+++ af-ZA/common.js
@@ -185,6 +185,7 @@ export default {
     'common.members': 'Members',
     'common.language': 'Taal',
     next: 'Volgende',
+    previous: 'Previous',
     'common.legal.tos.short': 'Terms',
     'common.legal.privacy.short': 'Privacy',
     'premium.increaseLimit': 'Consider upgrading to increase you limit.',
