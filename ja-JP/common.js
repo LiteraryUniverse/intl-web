dd87e491ae4c10173d6f88e3f4a4e7f9349c460c
Jan Dvorak <hello@jandvorak.me>
2022-11-03 21:36:12 +0100

    New translations common.js (Japanese)

--- ja-JP/common.js
+++ ja-JP/common.js
@@ -185,6 +185,7 @@ export default {
     'common.members': 'メンバー',
     'common.language': '言語',
     next: '次へ',
+    previous: 'Previous',
     'common.legal.tos.short': '条件',
     'common.legal.privacy.short': '通知',
     'premium.increaseLimit': '限度額を増やすためにアップグレードを検討してください。',
