e94e925f577f8ab63a631c0d1a0e956fb8a4a69d
Jan Dvorak <hello@jandvorak.me>
2022-11-07 21:09:42 +0100

    New translations sharing.js (Portuguese)

--- pt-PT/sharing.js
+++ pt-PT/sharing.js
@@ -1,9 +1,9 @@
 export default {
-    'sharing.title': 'Share',
-    'sharing.button': 'Sharing Button',
-    'sharing.share': 'Share with...',
-    'sharing.feedback': 'Are we missing some place you would like to share to? Let us know about it!',
-    'sharing.copy': 'Copy',
+    'sharing.title': 'Partilhar',
+    'sharing.button': 'Botão de Partilha',
+    'sharing.share': 'Compartilhar com...',
+    'sharing.feedback': 'Falta-nos algum lugar para onde gostaria de partilhar? Informe-nos sobre isso!',
+    'sharing.copy': 'Copiar',
     'sharing.email': 'E-mail',
     'sharing.sms': 'SMS',
     'sharing.messenger': 'Facebook Messenger',
@@ -14,7 +14,7 @@ export default {
     'sharing.facebook': 'Facebook',
     'sharing.line': 'LINE',
     'sharing.mastodonSocial': 'Mastodon Social',
-    'sharing.mastodonJapan': 'Mastodon Japan',
+    'sharing.mastodonJapan': 'Mastodon Japão',
     'sharing.gab': 'Gab',
     'sharing.pinterest': 'Pinterest',
     'sharing.gettr': 'Gettr'
