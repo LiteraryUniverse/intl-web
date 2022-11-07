c95d3663e12098a544e5b1a489bf461f95f985b9
Jan Dvorak <hello@jandvorak.me>
2022-11-07 21:29:28 +0100

    New translations library.js (Swedish)

--- sv-SE/library.js
+++ sv-SE/library.js
@@ -3,5 +3,5 @@ export default {
     'library.remove': 'Ta bort från biblioteket',
     'library.bought': 'Köpta berättelser',
     'library.stories': 'Följda berättelser',
-    'library.universes': 'Följt universum'
+    'library.universes': 'Followed universes'
 };
