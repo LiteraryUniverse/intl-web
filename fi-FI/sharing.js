4ff2e4da932873939e5e6e7c7fdb164ff8419be1
Jan Dvorak <hello@jandvorak.me>
2022-11-07 08:34:00 +0100

    New translations sharing.js (Finnish)

--- fi-FI/sharing.js
+++ fi-FI/sharing.js
@@ -1,5 +1,8 @@
 export default {
     'sharing.title': 'Jaa',
+    'sharing.button': 'Sharing Button',
+    'sharing.share': 'Share with...',
+    'sharing.feedback': 'Are we missing some place you would like to share to? Let us know about it!',
     'sharing.copy': 'Kopioi',
     'sharing.email': 'Sähköposti',
     'sharing.sms': 'SMS',
@@ -10,8 +13,8 @@ export default {
     'sharing.telegram': 'Telegram',
     'sharing.facebook': 'Facebook',
     'sharing.line': 'LINE',
-    'sharing.mastodonSocial': 'mastodon.social',
-    'sharing.mastodonJapan': 'mstdn.jp',
+    'sharing.mastodonSocial': 'Mastodon Social',
+    'sharing.mastodonJapan': 'Mastodon Japan',
     'sharing.gab': 'Gab',
     'sharing.pinterest': 'Pinterest',
     'sharing.gettr': 'Gettr'
