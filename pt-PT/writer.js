6cd5533faa31cbbb8258bed95576ac2ca1a2d690
Jan Dvorak <hello@jandvorak.me>
2022-12-14 13:44:22 +0100

    New translations writer.js (Portuguese)

--- pt-PT/writer.js
+++ pt-PT/writer.js
@@ -16,5 +16,10 @@ export default {
     'finder.remove': 'remove the connection',
     'finder.connect': 'Connect selection to encyclopedia entry',
     'writer.aside.ariaLabel': 'Help options menu',
-    'writer.nothingToSave': 'Nada para guardar'
+    'writer.nothingToSave': 'Nada para guardar',
+    'writer.publish.publishAria': 'Publication options',
+    'writer.publish.scheduleExplained': 'You can either publish immediately or schedule the chapter to be available at a later date.',
+    'writer.publish.schedule': 'Set publication date time',
+    'writer.publish.previousChapterPublished': 'The previous chapter was published on {publishDate, date, short} at {publishDate, time}',
+    'writer.publish.previousChapterScheduled': 'The previous chapter is scheduled to be published on {publishDate, date, short} at {publishDate, time}'
 };
