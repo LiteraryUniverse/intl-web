89cb9a5eaea23f93c80c28d65688b2e903d6f87a
Jan Dvorak <hello@jandvorak.me>
2022-12-15 03:42:31 +0100

    New translations workshop.js (Slovak)

--- sk-SK/workshop.js
+++ sk-SK/workshop.js
@@ -237,5 +237,8 @@ export default {
     'workshop.universe.forum': 'Interné fórum {universeName}',
     'workshop.ratingAdjusted': 'Kvôli sťažnostiam sme museli upraviť vaše hodnotenie na také, ktoré sme považovali za primerané. Zmena hodnotenia je preto teraz zakázaná.',
     'workshop.selectCollaborator': 'Vyberte spolupracovníka',
-    'workshop.chapters.scheduledOn': 'Will be published on {publishDate, date, short} at {publishDate, time}'
+    'workshop.chapters.scheduledOn': '{publishDate, date, short} | {publishDate, time}',
+    'workshop.chapterStatus.draft': 'Draft',
+    'workshop.chapterStatus.scheduled': 'Scheduled',
+    'workshop.chapterStatus.published': 'Published'
 };
