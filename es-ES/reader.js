3041c345cfdbca01389690d503ce4c2458c26147
Jan Dvorak <hello@jandvorak.me>
2022-12-21 17:44:45 +0100

    New translations reader.js (Spanish)

--- es-ES/reader.js
+++ es-ES/reader.js
@@ -30,5 +30,5 @@ export default {
     'reader.accessibility.toggleParagraphStripes': 'Alterar el fondo de los párrafos',
     'reader.accessibility.toggleParagraphHighlight': 'Conmutar el resaltado del párrafo seleccionado',
     'reader.accessibility.toggleRules': 'Alternar la regla de asistencia',
-    'reader.nextRelease.scheduled': 'Next chapter will come on {nextReleaseAt, date} at {nextReleaseAt, time}!'
+    'reader.nextRelease.scheduled': '¡El próximo capítulo llegará el {nextReleaseAt, date} a las {nextReleaseAt, time}!'
 };
