3bd0b01e009d639020df2ba7e37e60777b5e24bd
Jan Dvorak <hello@jandvorak.me>
2023-01-03 08:37:51 +0100

    New translations comments.js (Afrikaans)

--- af-ZA/comments.js
+++ af-ZA/comments.js
@@ -1,6 +1,6 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
+    'comment.write': 'Skryf jou kommentaar',
+    'comments.none': 'Jammer, daar is geen kommentaar om te wys nie.',
     'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
