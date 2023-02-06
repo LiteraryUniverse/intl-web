0b65e5d1b070d7e97cf2a9e252e1469ffbb60f6b
Jan Dvorak <hello@jandvorak.me>
2023-02-06 10:35:39 +0100

    New translations enrollment.js (Spanish)

--- es-ES/enrollment.js
+++ es-ES/enrollment.js
@@ -1,6 +1,6 @@
 export default {
     'enrollment.title': '¡Consigue tu cuenta!',
-    'enrollment.description': '¡Bienvenido, {username}! Para poder acceder a tu cuenta especial sólo tienes que crear una contraseña.',
+    'enrollment.description': '¡Bienvenido, {username}! Para poder acceder a tu cuenta especial solo tienes que crear una contraseña.',
     'enrollment.submitting': 'Consiguiendo tu cuenta...',
     'enrollment.submit': '¡Consigue tu cuenta!'
 };
