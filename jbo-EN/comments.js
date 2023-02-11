137ed05413a820b43acf285d58f72baeae900a0d
Jan Dvorak <hello@jandvorak.me>
2023-02-11 10:17:01 +0100

    New translations comments.js (Lojban)

--- jbo-EN/comments.js
+++ jbo-EN/comments.js
@@ -1,10 +1,10 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
+    'comment.write': '.i piku do cusku lo se pilno',
+    'comments.none': '.i je\'u klama le nu do na djuno lo nu do pu cusku vau li\'o',
     'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-    'comments.show.older': 'Show older comments'
+    'comments.show.older': '.i jarco lo purdi cusku'
 };
