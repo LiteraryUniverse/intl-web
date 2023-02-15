de148e9d8ea38e61fd66cbb461f812648269bda9
Jan Dvorak <hello@jandvorak.me>
2023-02-15 12:17:51 +0100

    New translations universe.js (Spanish)

--- es-ES/universe.js
+++ es-ES/universe.js
@@ -3,10 +3,10 @@ export default {
     'universe.fanfiction.see': 'Todos los fánfics para {universe}',
     'universe.fanfiction.desc': 'Fánfic para {universe}',
     'universe.fanfiction.for': 'Fánfic de {universe}',
-    'universe.fanarts.for': '{universe} Fan Artes',
+    'universe.fanarts.for': 'Arte de los fans de {universe}',
     'universe.fansettings.for': 'Ajustes de fans de {universe}',
     'universe.fanfiction.stories': 'Historias',
-    'universe.fanfiction.fanarts': 'Fan Artes',
+    'universe.fanfiction.fanarts': 'Arte de los fans',
     'universe.stories.count': `{num, plural,
     =0 {No hay historias}
     one {Hay una historia}
@@ -26,8 +26,8 @@ export default {
     'universe.stories.desc': 'Historias canónicas para {universe}',
     'universe.encyclopedia.title': 'Enciclopedia de {universe}',
     'universe.encyclopedia.desc': 'Todos los detalles para {universe}',
-    'universe.fanArt.submit': 'Envíe su arte',
-    'universe.fanArtHeaderDesc.item': 'Fan art de {universe} - {name}',
+    'universe.fanArt.submit': 'Envía tu arte',
+    'universe.fanArtHeaderDesc.item': 'Arte de los fans de {universe} - {name}',
     'universe.fanArtHeaderDesc.submit': '{universe} envíos de fan art',
     'universe.fanArtHeaderDesc.overview': 'Fan art de {universe}'
 };
