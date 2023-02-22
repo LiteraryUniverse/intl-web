783bf1cf0710078158ffe4810969fe3882915f5b
Jan Dvorak <hello@jandvorak.me>
2023-02-22 11:40:54 +0100

    New translations emails.js (French)

--- fr-FR/emails.js
+++ fr-FR/emails.js
@@ -4,31 +4,31 @@
 /* eslint-disable max-len */
 export default {
     'email.enrollAccount.subject': 'Invitation à l\'Univers Littéraire pour {username}',
-    'email.enrollAccount.message': `Hello {username}\n\n
-    We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
-    To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'Literary Universe reset password',
-    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
-        Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    'email.enrollAccount.message': `Bonjour {username}\n\n
+    Nous avons le plaisir de vous inviter sur L'Univers Littéraire ! Nous avons créé un compte personnel spécial pour vous où vous pouvez accéder à toutes les fonctionnalités de L'Univers Littéraire ! Rejoignez-nous pour créer la prochaine génération d'outils d'écriture et de lecture créative !
+    Pour activer votre compte, il vous suffit de cliquer sur le lien ci-dessous :\n\n{url}`,
+    'email.resetPassword.subject': 'Réinitialisation du mot de passe de l\'Univers Littéraire',
+    'email.resetPassword.message': `Bonjour {username}!\n\n Nous avons reçu une demande de réinitialisation du mot de passe de votre compte.
+        Veuillez suivre le lien ci-dessous pour réinitialiser votre mot de passe :\n\n{url}`,
+    'email.verifyEmail.subject': 'Vérification de l\'e-mail de L\'Univers Littéraire',
+    'email.verifyEmail.message': 'Bonjour {username}!\n\n Veuillez vérifier votre adresse e-mail en cliquant sur le lien ci-dessous :\n\n{url}',
     // New html email keys
-    'email.salutation': 'Hello {username},',
-    'email.welcome': 'Welcome to Literary Universe!',
-    'email.signature': 'Sincerely,<br />Literary Universe team',
+    'email.salutation': 'Bonjour {username},',
+    'email.welcome': 'Bienvenue dans L\'Univers Littéraire !',
+    'email.signature': 'Sincèrement,<br />L\'équipe de L\'Univers Littéraire',
     // Enrollment
-    'email.enrollAccount.welcome': 'Welcome aboard',
-    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-    'email.enrollAccount.button': 'Access your account',
-    'email.enrollAccount.signature': 'We are looking forward to see you,<br />Literary Universe team',
+    'email.enrollAccount.welcome': 'Bienvenue à bord',
+    'email.enrollAccount.messageHtml': 'Nous aimerions vous inviter sur L\'Univers Littéraire ! Nous avons préparé pour vous un compte personnel où vous pourrez accéder à toutes les fonctionnalités de L\'Univers Littéraire ! Rejoignez-nous pour créer la prochaine génération d\'écriture et de lecture créatives !',
+    'email.enrollAccount.button': 'Accéder à votre compte',
+    'email.enrollAccount.signature': 'Nous nous réjouissons de vous voir,<br />L\'équipe de l\'Univers Littéraire',
     // Reset password
-    'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account, please click on the button bellow to continue the process. If you haven\'t made this request you can ignore this e-mail, but it might be a good time to review your security settings.',
-    'email.resetPassword.button': 'Reset password',
+    'email.resetPassword.messageHtml': 'Nous avons reçu une demande de réinitialisation du mot de passe de votre compte. Veuillez cliquer sur le bouton ci-dessous pour poursuivre la procédure. Si vous n\'avez pas fait cette demande, vous pouvez ignorer cet email, mais c\'est peut-être le moment de revoir vos paramètres de sécurité.',
+    'email.resetPassword.button': 'Réinitialiser le mot de passe',
     // E-mail verification
-    'email.verifyEmail.messageHtml': 'We are happy to have you on board! Please click the button bellow to verify this e-mail address.',
-    'email.verifyEmail.button': 'Verify this e-mail address',
+    'email.verifyEmail.messageHtml': 'Nous sommes heureux de vous compter parmi nous ! Veuillez cliquer sur le bouton ci-dessous pour vérifier cette adresse email.',
+    'email.verifyEmail.button': 'Vérifier cette adresse email',
     // Getting premium from admins
-    'email.adminUpgrade.subject': 'Your account has been upgraded',
+    'email.adminUpgrade.subject': 'Votre compte a été mis à jour',
     'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     adventurer {Adventurer}
