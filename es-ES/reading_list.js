e7f0981cb661b5d1637f5ecced04db0a137b2386
Jan Dvorak <hello@jandvorak.me>
2023-02-23 21:23:31 +0100

    New translations reading_list.js (Spanish)

--- es-ES/reading_list.js
+++ es-ES/reading_list.js
@@ -1,8 +1,14 @@
 export default {
+  'readingList.title': 'Reading list',
   'readingList.type.label': 'Tipo de lista',
   'readingList.type.stories': 'Historias',
   'readingList.type.universes': 'Universo',
-  'readingList.ranked.label': 'Lista por valoraciones',
+  'readingList.ordered.label': 'Ranked list',
   'readingList.button.add': 'Añadir a la lista de lectura',
-  'readingList.selectList': 'Elegir lista de lectura'
+  'readingList.selectList': 'Elegir lista de lectura',
+  'readingList.create': 'Create a new reading list',
+  'readingList.create.aria': 'Modal with settings for a new reading list',
+  'readingList.edit.aria': 'Modal to edit reading list\'s settings',
+  'readingList.library.title': 'Your reading lists',
+  'readingList.public.label': 'Is publicly visible?'
 };
