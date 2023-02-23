f7af950fae8d6c124fc0dbcfd7d8d45741c8bf58
Jan Dvorak <jdvorak@literaryuniverse.com>
2023-02-23 21:15:44 +0100

    More strings for reading list

--- en/reading_list.js
+++ en/reading_list.js
@@ -1,8 +1,14 @@
 export default {
+  'readingList.title': 'Reading list',
   'readingList.type.label': 'List type',
   'readingList.type.stories': 'Stories',
   'readingList.type.universes': 'Universe',
-  'readingList.ranked.label': 'Ranked list',
+  'readingList.ordered.label': 'Ranked list',
   'readingList.button.add': 'Add to reading list',
-  'readingList.selectList': 'Select reading list'
+  'readingList.selectList': 'Select reading list',
+  'readingList.create': 'Create a new reading list',
+  'readingList.create.aria': 'Modal with settings for a new reading list',
+  'readingList.edit.aria': "Modal to edit reading list's settings",
+  'readingList.library.title': 'Your reading lists',
+  'readingList.public.label': 'Is publicly visible?'
 }
