4ff3b14ce03f02a5f2fbc7d76aa15d53743bf12b
Jan Dvorak <hello@jandvorak.me>
2023-02-27 22:44:26 +0100

    New translations reading_list.js (Spanish)

--- es-ES/reading_list.js
+++ es-ES/reading_list.js
@@ -10,5 +10,18 @@ export default {
   'readingList.create.aria': 'Ajustes para la nueva lista de lectura',
   'readingList.edit.aria': 'Editar los ajustes de la lista de lecturas',
   'readingList.library.title': 'Tus listas de lectura',
-  'readingList.public.label': '¿Quieres que el público la vea?'
+  'readingList.public.label': '¿Quieres que el público la vea?',
+  'readingList.addItem': `Add {type, select,
+    universes {universe}
+    stories {story}
+  }`,
+  'readingList.addItem.modalAria': `Modal to add a {type, select,
+    universes {universe}
+    stories {story}
+  } to the reading list.`,
+  'readingList.addItem.fromLibrary': 'From library',
+  'readingList.addItem.added': `{type, select,
+    universes {Universe}
+    stories {Story}
+  } has been added.`
 };
