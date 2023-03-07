43f5198be87cfe312981164fc7adbfa1d02cc081
Jan Dvorak <hello@jandvorak.me>
2023-03-08 00:21:39 +0100

    New translations common.js (Odia)

--- or-IN/common.js
+++ or-IN/common.js
@@ -238,5 +238,6 @@ export default {
     'common.discordCommunity': 'Discord Community',
     'common.account': 'Account',
     'common.notifications': 'ଵିଜ୍ଞପ୍ତି',
-    'common.byDeclaration': 'By:'
+    'common.byDeclaration': 'By:',
+    'common.done': 'Done'
 }    /* eslint-enable max-len */;
