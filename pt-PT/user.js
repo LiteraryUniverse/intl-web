d8d9d3fb8e44129d61c0364839c1678dcb7c5293
Jan Dvorak <hello@jandvorak.me>
2023-03-09 12:51:29 +0100

    New translations user.js (Portuguese)

--- pt-PT/user.js
+++ pt-PT/user.js
@@ -1,35 +1,35 @@
 export default {
-    'user.friend.request.add': 'Add to friends',
-    'user.friend.requests': `You have {num, number} new friend {num, plural,
-    zero {requests}
-    one {request}
-    many {requests}
-    other {requests}
+    'user.friend.request.add': 'Adicionar aos amigos',
+    'user.friend.requests': `Tem {num, number} {num, plural,
+    zero {pedidos de novo amigos}
+    one {pedido de novo amigos}
+    many {pedidos de novo amigos}
+    other {pedidos de novo amigos}
   }`,
-    'user.friend.unfriend': 'Unfriend',
-    'user.block': 'Block',
-    'user.unblock': 'Unblock',
+    'user.friend.unfriend': 'Remover amizade',
+    'user.block': 'Bloquear',
+    'user.unblock': 'Desbloquear',
     'user.joined': `{gender, select,
-    male {Joined}
-    female {Joined}
-    other {Joined}} on {date, date, long}`,
-    'user.friend.request.accept': 'Accept friendship request',
-    'user.friend.request.cancel': 'Cancel friendship request',
-    'user.friend.request.deny': 'Deny friendship',
-    'user.listing': 'User listing',
-    'user.profile.visit': 'Visit {user}\'s profile.',
-    'user.profile.avatar': '{username}\'s avatar',
-    'user.friend.list.requests': 'Friendships requests',
-    'user.friend.norequests': 'You have no requests for friendship.',
-    'user.dashboardWelcome': `Good {dayPart, select,
-    morning {morning}
-    afternoon {afternoon}
-    evening {evening}
-    night {night}
-    other {day}
+    male {Aderiu}
+    female {Aderiu}
+    other {Aderiu}} no {date, date, long}`,
+    'user.friend.request.accept': 'Aceitar pedido de amizade',
+    'user.friend.request.cancel': 'Cancelar pedido de amizade',
+    'user.friend.request.deny': 'Negar pedido de amizade',
+    'user.listing': 'Listagem de utilizadores',
+    'user.profile.visit': 'Visite o perfil do {user}.',
+    'user.profile.avatar': 'Avatar de {username}',
+    'user.friend.list.requests': 'Pedidos de amizade',
+    'user.friend.norequests': 'Não tem pedidos de amizade.',
+    'user.dashboardWelcome': `{dayPart, select,
+    morning {Bom dia}
+    afternoon {Boa tarde}
+    evening {Boa noite}
+    night {Boa noite}
+    other {Bom dia}
   } {displayName}!`,
-    'user.verification.publishedAuthor': 'This author has been published the traditional way',
-    'user.verification.luAuthor': 'Literary Universe based author of note',
-    'user.verification.luEmployee': 'Literary Universe employee',
-    'user.verification.publicFigure': 'Verified public figure'
+    'user.verification.publishedAuthor': 'Este autor foi publicado da maneira tradicional',
+    'user.verification.luAuthor': 'Autor de nota baseado no Universo Literário',
+    'user.verification.luEmployee': 'Empregado do Universo Literário',
+    'user.verification.publicFigure': 'Figura pública verificada'
 };
