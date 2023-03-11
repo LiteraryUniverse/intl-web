83f9f97b97f3c314bc809a552d8704bbd26fd120
Jan Dvorak <hello@jandvorak.me>
2023-03-11 21:12:05 +0100

    New translations plans.js (Spanish)

--- es-ES/plans.js
+++ es-ES/plans.js
@@ -147,5 +147,6 @@ export default {
     'payment.methods.webMonetization': 'Monetización Web',
     'settings.webMonetization.placeholder': 'Tu dirección de cuenta de pagos',
     'payment.webMonetization.explained': 'La monetización web es una propuesta para estandarizar los pagos en la web. Aquí puedes añadir tu dirección de cuenta de pagos que se añadirá a tu página de perfil, blog, universos e historias que se podrá utilizar para la monetización web.',
-    'payment.webMonetization.learnMore': 'Más información sobre la monetización web'
+    'payment.webMonetization.learnMore': 'Más información sobre la monetización web',
+    'plans.subscription.settingsAria': 'Subscription settings options'
 };
