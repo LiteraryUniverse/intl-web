e22de27407cb36d7c8a2a7b83a6cf7d699e13634
Jan Dvorak <hello@jandvorak.me>
2023-03-29 10:54:36 +0200

    New translations comments.js (Korean)

--- ko-KR/comments.js
+++ ko-KR/comments.js
@@ -1,10 +1,10 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
+    'comment.write': '댓글 작성',
+    'comments.none': '표시할 댓글이 없습니다.',
     'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-    'comments.show.older': 'Show older comments'
+    'comments.show.older': '이전 댓글 표시'
 };
