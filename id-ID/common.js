31f72183084dca84ab4148b40ac1d1a18e0202ce
Jan Dvorak <hello@jandvorak.me>
2023-03-30 23:16:50 +0200

    New translations common.js (Indonesian)

--- id-ID/common.js
+++ id-ID/common.js
@@ -239,5 +239,6 @@ export default {
     'common.account': 'Akun',
     'common.notifications': 'Notifikasi',
     'common.byDeclaration': 'By:',
-    'common.done': 'Done'
+    'common.done': 'Done',
+    recommended: 'Recommended'
 }    /* eslint-enable max-len */;
