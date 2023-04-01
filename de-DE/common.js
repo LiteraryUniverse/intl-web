3ccbe1cf2263ccf03719a78b327d327a1145aa56
Jan Dvorak <hello@jandvorak.me>
2023-04-01 19:14:56 +0200

    New translations common.js (German)

--- de-DE/common.js
+++ de-DE/common.js
@@ -240,5 +240,13 @@ export default {
     'common.notifications': 'Benachrichtigungen',
     'common.byDeclaration': 'Von:',
     'common.done': 'Fertig',
-    recommended: 'Recommended'
+    recommended: 'Recommended',
+    'select.typeToCreate': 'Start typing to create an item',
+    'select.noSearchResults': 'No results for “{search}”',
+    'select.filterOptions': 'Filter options',
+    'select.closeDropdown': 'Close Dropdown',
+    'select.removeOption': 'Remove {option}',
+    'select.selectOptions': 'Select options for {name}',
+    'select.selectOption': 'Select an option for {name}',
+    create: 'Create'
 }    /* eslint-enable max-len */;
