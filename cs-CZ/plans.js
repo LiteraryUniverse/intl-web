7e7c084aded26755eadd35851dd26eb018e38333
Jan Dvorak <hello@jandvorak.me>
2023-04-05 17:50:25 +0200

    New translations plans.js (Czech)

--- cs-CZ/plans.js
+++ cs-CZ/plans.js
@@ -161,6 +161,6 @@ export default {
     'plan.button.chooseNow': 'Vyberte si svůj plán pro Literary Universe',
     'plans.subscription.manage': 'Spravovat předplatné',
     'plans.taxMayApply': '* DPH a další daně zde nejsou započteny',
-    'plans.faq.crypto': 'Will you offer crypto payments?',
-    'plans.faq.cryptoAnswer': 'Yes, we are working on it. We are big fans of Web3 we are looking for ways to integrate Web3 into the platform in the near future. We currently allow for login with Web3 and we are looking to expand on this offering especially when it comes to providing compensation to creators and giving creators the ability to choose all the possible payment options that they want to accept.'
+    'plans.faq.crypto': 'Nabídnete krypto platby?',
+    'plans.faq.cryptoAnswer': 'Ano, pracujeme na tom. Jsme velcí fanoušci Web3 hledáme způsoby, jak integrovat Web3 do platformy v blízké budoucnosti. V současné době povolujeme přihlášení pomocí Web3 a hledáme chytré možnosti rozšíření z tohoto základu, zejména pokud jde o poskytování náhrad tvůrcům a tvůrcům možnost vybrat si všechny možné možnosti platby, které chtějí přijmout.'
 };
