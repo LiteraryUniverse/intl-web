b483170a4efd0b19ea3d2d31bcce6cbbbd2e53e5
Jan Dvorak <hello@jandvorak.me>
2023-04-18 06:31:29 +0200

    New translations discovery.js (Japanese)

--- ja-JP/discovery.js
+++ ja-JP/discovery.js
@@ -2,7 +2,7 @@ export default {
     'discovery.meta.desc': '新しい物語や宇宙を発見する。',
     'discovery.universes.new': '新たな宇宙',
     'discovery.stories.new': '新しい物語',
-    'discovery.stories.standalone.new': '新しい独立した物語',
+    'discovery.stories.standalone.new': '個別の新規物語',
     'discovery.stories.updated': '最近更新された物語',
     'discovery.continue': '続きを読む',
     'discovery.stories.similar.new': 'ご近所の新しい物語',
