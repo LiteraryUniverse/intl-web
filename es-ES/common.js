73f3e613c89a8e3027216e193bf0c85458a9f8cc
Jan Dvorak <hello@jandvorak.me>
2023-04-22 20:41:38 +0200

    New translations common.js (Spanish)

--- es-ES/common.js
+++ es-ES/common.js
@@ -248,5 +248,6 @@ export default {
     'banner.support': '¡Los usuarios como tú sois quienes mantenéis Literary Universe a flote! Por favor, considera unirte a uno de nuestros planes de suscripción y hacer de Literary Universe algo incluso mejor!',
     'banner.seePlans': 'Visita nuestra página de suscripciones si quieres más información.',
     'common.noAccountQuestion': 'Don\'t have an account?',
-    'common.haveAccountQuestion': 'Already have an account?'
+    'common.haveAccountQuestion': 'Already have an account?',
+    'common.socialLogin': 'Login with other services'
 }    /* eslint-enable max-len */;
