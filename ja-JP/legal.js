2a09144450dfa1b40f025b8c700633b98563af71
Jan Dvorak <hello@jandvorak.me>
2023-04-25 06:47:21 +0200

    New translations legal.js (Japanese)

--- ja-JP/legal.js
+++ ja-JP/legal.js
@@ -2,7 +2,7 @@ export default {
     'legal.modal.intro': '突然すみません。今後進めるにあたり、お客様が私たちの利用規約、プライバシーポリシー、著作権契約を理解し、同意していることを確認するという法的義務があります',
     'legal.modal.agree': '同意します',
     'legal.modal.disagree': '同意しません',
-    'legal.modal.register': '登録するとこのメッセージは表示されません。',
+    'legal.modal.register': '登録すると今後このメッセージは表示されません。',
     'legal.modal.goToSettings': '設定画面へ進む',
     'legal.modal.fewmore': 'あと少しです',
     'legal.modal.finish': '完了しました'
