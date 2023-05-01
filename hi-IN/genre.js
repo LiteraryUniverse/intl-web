b2e79e2eb1ce77ce39b20c17674a8faf40868f81
Jan Dvorak <hello@jandvorak.me>
2023-05-01 17:17:00 +0200

    New translations genre.js (Hindi)

--- hi-IN/genre.js
+++ hi-IN/genre.js
@@ -23,5 +23,7 @@ export default {
     'genre.fairytale': 'परियों की कहानी',
     'genre.superhero': 'Superhero',
     'genre.children': 'Children',
-    'genre.postapocalyptic': 'Post-Apocalyptic'
+    'genre.postapocalyptic': 'Post-Apocalyptic',
+    'genre.political': 'Political document',
+    'genre.history': 'History'
 };
