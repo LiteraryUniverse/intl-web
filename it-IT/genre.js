aed8e052adc5a7f6959970f10f3e08f79e7662ba
Jan Dvorak <hello@jandvorak.me>
2023-05-01 17:16:37 +0200

    New translations genre.js (Italian)

--- it-IT/genre.js
+++ it-IT/genre.js
@@ -23,5 +23,7 @@ export default {
     'genre.fairytale': 'Fairy tale',
     'genre.superhero': 'Supereroe',
     'genre.children': 'Children',
-    'genre.postapocalyptic': 'Post-Apocalyptic'
+    'genre.postapocalyptic': 'Post-Apocalyptic',
+    'genre.political': 'Political document',
+    'genre.history': 'History'
 };
