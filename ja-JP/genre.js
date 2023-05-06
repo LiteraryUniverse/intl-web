901946698b46b9ee48dd222716fe0c934839e827
Jan Dvorak <hello@jandvorak.me>
2023-05-06 17:44:49 +0200

    New translations genre.js (Japanese)

--- ja-JP/genre.js
+++ ja-JP/genre.js
@@ -25,5 +25,6 @@ export default {
     'genre.children': '子ども向け',
     'genre.postapocalyptic': 'Post-Apocalyptic',
     'genre.political': 'Political document',
-    'genre.history': 'History'
+    'genre.history': 'History',
+    'genre.iseaki': 'Isekai'
 };
