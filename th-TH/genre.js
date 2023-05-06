d3e0887e61abb0884a196b90dc1d05e36c12edef
Jan Dvorak <hello@jandvorak.me>
2023-05-06 17:45:10 +0200

    New translations genre.js (Thai)

--- th-TH/genre.js
+++ th-TH/genre.js
@@ -25,5 +25,6 @@ export default {
     'genre.children': 'Children',
     'genre.postapocalyptic': 'Post-Apocalyptic',
     'genre.political': 'Political document',
-    'genre.history': 'History'
+    'genre.history': 'History',
+    'genre.iseaki': 'Isekai'
 };
