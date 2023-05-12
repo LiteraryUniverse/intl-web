9d479f83da4e4ac6575aaf7d1c39ae480ab189fc
Jan Dvorak <hello@jandvorak.me>
2023-05-12 09:39:35 +0200

    New translations common.js (Japanese)

--- ja-JP/common.js
+++ ja-JP/common.js
@@ -249,5 +249,6 @@ export default {
     'banner.seePlans': '詳しくは、プランのページをご覧ください。',
     'common.noAccountQuestion': 'Don\'t have an account?',
     'common.haveAccountQuestion': 'Already have an account?',
-    'common.socialLogin': 'Login with other services'
+    'common.socialLogin': 'Login with other services',
+    'common.viewMore': 'View more'
 }    /* eslint-enable max-len */;
