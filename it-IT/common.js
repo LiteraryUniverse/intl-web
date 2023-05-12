091b6c2fc8859d581a36169dbc49400a0cf26855
Jan Dvorak <hello@jandvorak.me>
2023-05-12 09:39:34 +0200

    New translations common.js (Italian)

--- it-IT/common.js
+++ it-IT/common.js
@@ -249,5 +249,6 @@ export default {
     'banner.seePlans': 'Visit our plans page for more information.',
     'common.noAccountQuestion': 'Don\'t have an account?',
     'common.haveAccountQuestion': 'Already have an account?',
-    'common.socialLogin': 'Login with other services'
+    'common.socialLogin': 'Login with other services',
+    'common.viewMore': 'View more'
 }    /* eslint-enable max-len */;
