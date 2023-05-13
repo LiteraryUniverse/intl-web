185a0ebb85181930e461004beb83a64e0873930d
Jan Dvorak <hello@jandvorak.me>
2023-05-13 23:37:39 +0200

    New translations common.js (Portuguese, Brazilian)

--- pt-BR/common.js
+++ pt-BR/common.js
@@ -91,6 +91,7 @@ export default {
     community {Community Guidelines}
     privacy {Privacy Policy}
     copyright {Copyright Agreement}
+    publishingAgreement {Publishing Agreement}
     other {Agreement}
   } Version {version} - {date, date}`,
     'common.legal.newVersionAvailable': 'Uma nova versão ({newVersion}) deste documento entrará em vigor em {effectiveAt, date}.',
