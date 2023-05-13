260a26f2d040da1ee565e48ab143d9a31baca713
Jan Dvorak <hello@jandvorak.me>
2023-05-13 23:37:36 +0200

    New translations common.js (Ukrainian)

--- uk-UA/common.js
+++ uk-UA/common.js
@@ -91,6 +91,7 @@ export default {
     community {Community Guidelines}
     privacy {Privacy Policy}
     copyright {Copyright Agreement}
+    publishingAgreement {Publishing Agreement}
     other {Agreement}
   } Version {version} - {date, date}`,
     'common.legal.newVersionAvailable': 'Нова версія ({newVersion}) цього документа набуде чинності {effectiveAt, date}.',
