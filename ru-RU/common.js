891e76f95e4940860039da98513bb4255645e91c
Jan Dvorak <hello@jandvorak.me>
2023-05-14 19:37:09 +0200

    New translations common.js (Russian)

--- ru-RU/common.js
+++ ru-RU/common.js
@@ -5,7 +5,7 @@
 export default {
     siteName: 'Literary Universe',
     twitterHandle: '@lituniapp',
-    'site.description': 'Literary Universe is an all-in-one creative and publishing platform for grand story universes.',
+    'site.description': 'Literary Universe - это универсальная творческая и издательская платформа для создания вселенных больших историй.',
     'site.keywords': 'creative writing, scifi, sci-fi, fantasy, writing, publishing, story, stories, universe, web novel, webnovel',
     'site.logoAlt': 'Literary Universe logo',
     mobileMenu: 'Мобильное меню',
