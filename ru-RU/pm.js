61561fcaf9536b8d6bf8773256653865bd58d0f0
Jan Dvorak <hello@jandvorak.me>
2023-05-15 15:29:59 +0200

    New translations pm.js (Russian)

--- ru-RU/pm.js
+++ ru-RU/pm.js
@@ -1,25 +1,28 @@
 export default {
-    'pm.conversation': 'Conversation',
-    'pm.showolder': 'Show older messages',
-    'pm.errors.saysomething': 'You should really say something...',
-    'pm.errors.addrecipient': 'You need to add at least one other user.',
-    'pm.message': 'Message',
-    'pm.send.message': 'Send message',
-    'pm.messages.own': 'Your messages',
-    'pm.conversations.none': 'You are currently not conversing with anyone.',
-    'pm.to': 'To:',
-    'pm.with': 'Conversation with:',
-    'pm.send': 'Send',
-    'pm.conversations.unreadCount': `You have {count, plural,
-    zero {no unread conversations}
-    one {one unread conversation}
-    other {# unread conversations}
+    'pm.conversation': 'Беседа',
+    'pm.showolder': 'Отображать старые сообщения',
+    'pm.errors.saysomething': 'Вы должны сказать что-нибудь...',
+    'pm.errors.addrecipient': 'Вам необходимо добавить как минимум ещё одного пользователя.',
+    'pm.message': 'Сообщение',
+    'pm.send.message': 'Отправить сообщение',
+    'pm.messages.own': 'Ваши сообщения',
+    'pm.conversations.none': 'В настоящее время вы ни с кем не ведёте беседу.',
+    'pm.to': 'Кому:',
+    'pm.with': 'Беседа с:',
+    'pm.send': 'Отправить',
+    'pm.conversations.unreadCount': `У вас есть {count, plural,
+    one {одна непрочитанная беседа}
+    few {# непрочитанные беседы}
+    many {# непрочитанных бесед}
+    other {# непрочитанных бесед}
   }.`,
-    'pm.form.minimumParticipants': 'You need to choose a recipient.',
-    'pm.form.maxParticipants': `You can have maximum of {maxPeople, plural,
-    one {one person}
-    other {# people}
-  } in a conversation.`,
-    'pm.new.aria': 'New conversation',
-    'pm.new.buttonTitle': 'Start a new conversation'
+    'pm.form.minimumParticipants': 'Вам необходимо выбрать получателя.',
+    'pm.form.maxParticipants': `В беседе могут участвовать не более {maxPeople, plural,
+    one {одного человека}
+    few {# людей}
+    many {# людей}
+    other {# людей}
+  }.`,
+    'pm.new.aria': 'Новая беседа',
+    'pm.new.buttonTitle': 'Начать новую беседу'
 };
