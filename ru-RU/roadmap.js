e27f75f0c567f5699f1edd2c5692f6be8078b0b7
Jan Dvorak <hello@jandvorak.me>
2023-05-15 16:42:04 +0200

    New translations roadmap.js (Russian)

--- ru-RU/roadmap.js
+++ ru-RU/roadmap.js
@@ -1,37 +1,37 @@
 export default {
-  'roadmap.ariaTimeOptions': 'Selections for roadmap items based on their status.',
-  'roadmap.state.backlog': 'Backlog',
-  'roadmap.state.inprogress': 'In-progress',
-  'roadmap.state.preview': 'Preview',
-  'roadmap.state.beta': 'Beta',
-  'roadmap.state.ga': 'General availability',
-  'roadmap.eta.q1.short': 'Q1',
-  'roadmap.eta.q2.short': 'Q2',
-  'roadmap.eta.q3.short': 'Q3',
-  'roadmap.eta.q4.short': 'Q4',
-  'roadmap.eta.q1.long': 'First quarter',
-  'roadmap.eta.q2.long': 'Second quarter',
-  'roadmap.eta.q3.long': 'Third quarter',
-  'roadmap.eta.q4.long': 'Fourth quarter',
-  'roadmap.backlog': 'Backlog',
-  'roadmap.history': 'History of development',
-  'roadmap.item.inBacklog': 'We are planning to develop this feature in the future.',
-  'roadmap.item.inProgress': 'The development of the feature started {startedAt, date}.',
-  'roadmap.item.inBeta': 'This feature has entered beta on {betaAt, date}.',
-  'roadmap.item.inPreview': 'This feature became available to Storyteller subscribers on {previewAt, date}.',
-  'roadmap.item.inGa': 'This feature has become generally available on {gaAt, date}.',
-  'roadmap.item.originalIssue': 'This feature was originally suggested by our users.',
-  'roadmap.eta.mapTitle': `Planned for {quarter, select,
-    Q1 {Q1}
-    Q2 {Q2}
-    Q3 {Q3}
-    Q4 {Q4}
+  'roadmap.ariaTimeOptions': 'Выбор элементов карты разработки в зависимости от их состояния.',
+  'roadmap.state.backlog': 'Накопившиеся',
+  'roadmap.state.inprogress': 'В разработке',
+  'roadmap.state.preview': 'Предпросмотр',
+  'roadmap.state.beta': 'Бета',
+  'roadmap.state.ga': 'Общая работоспособность',
+  'roadmap.eta.q1.short': '1-й кв.',
+  'roadmap.eta.q2.short': '2-й кв.',
+  'roadmap.eta.q3.short': '3-й кв.',
+  'roadmap.eta.q4.short': '4-й кв.',
+  'roadmap.eta.q1.long': 'Первый квартал',
+  'roadmap.eta.q2.long': 'Второй квартал',
+  'roadmap.eta.q3.long': 'Третий квартал',
+  'roadmap.eta.q4.long': 'Четвёртый квартал',
+  'roadmap.backlog': 'Накопившиеся',
+  'roadmap.history': 'История разработок',
+  'roadmap.item.inBacklog': 'Мы планируем развивать эту функцию в будущем.',
+  'roadmap.item.inProgress': 'Разработка функции началась {startedAt, date}',
+  'roadmap.item.inBeta': 'Эта функция вступила в бета-тестирование {betaAt, date}',
+  'roadmap.item.inPreview': 'Эта функция стала доступна подписчикам плана «Рассказчик» {previewAt, date}',
+  'roadmap.item.inGa': 'Эта функция стала доступной {gaAt, date}',
+  'roadmap.item.originalIssue': 'Эта функция была первоначально предложена нашими пользователями.',
+  'roadmap.eta.mapTitle': `Запланировано на {quarter, select,
+    Q1 {1-й кв.}
+    Q2 {2-й кв.}
+    Q3 {3-й кв.}
+    Q4 {4-й кв.}
     other {}
   } {year, date, ::yyyy}`,
-  'roadmap.eta.mapTitleFinished': 'Finished in {time, date, ::MMMM ::yyyy}',
-  'roadmap.back': 'Back to roadmap',
-  'roadmap.seeBacklog': 'Check out what other things we plan to do',
-  'roadmap.seeFinished': 'If you like to dig more into our history, we have a nice overview ready for you',
-  'roadmap.planned': 'Planned',
-  'roadmap.finished': 'Finished'
+  'roadmap.eta.mapTitleFinished': 'Завершено {time, date, ::MMMM ::yyyy}',
+  'roadmap.back': 'Вернуться к карте разработки',
+  'roadmap.seeBacklog': 'Посмотрите, что ещё мы планируем сделать',
+  'roadmap.seeFinished': 'Если вы хотите узнать больше о нашей истории, мы подготовили для вас хороший обзор',
+  'roadmap.planned': 'Запланировано',
+  'roadmap.finished': 'Завершено'
 };
