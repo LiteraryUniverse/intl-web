d56eee9e517efbc90e98638b6c6aeef2f14cb694
Jan Dvorak <hello@jandvorak.me>
2023-05-17 10:18:19 +0200

    New translations common.js (Arabic)

--- ar-SA/common.js
+++ ar-SA/common.js
@@ -126,7 +126,6 @@ export default {
     'common.createdOn.title': 'Created on',
     // account types
     admin: 'Admin',
-    goodbye: 'Thank you! See you again!',
     'common.saved': 'Saved!',
     'common.saving': 'Saving...',
     'common.friends': 'الأصدقاء',
@@ -252,5 +251,8 @@ export default {
     'common.haveAccountQuestion': 'Already have an account?',
     'common.socialLogin': 'Login with other services',
     'common.viewMore': 'View more',
-    'common.overview': 'Overview'
+    'common.overview': 'Overview',
+    'common.loggingOut': 'Logging out',
+    'common.thankYou': 'Thank you!',
+    'common.seeYouAgain': 'See you again!'
 }    /* eslint-enable max-len */;
