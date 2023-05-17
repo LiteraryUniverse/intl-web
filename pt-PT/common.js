0852ef5a499402474afb9519d79c8e23d3ba4510
Jan Dvorak <hello@jandvorak.me>
2023-05-17 10:18:39 +0200

    New translations common.js (Portuguese)

--- pt-PT/common.js
+++ pt-PT/common.js
@@ -126,7 +126,6 @@ export default {
     'common.createdOn.title': 'Criado no',
     // account types
     admin: 'Administrador',
-    goodbye: 'Obrigado! Até à próxima!',
     'common.saved': 'Guardado!',
     'common.saving': 'A guardar...',
     'common.friends': 'Amigos',
@@ -252,5 +251,8 @@ export default {
     'common.haveAccountQuestion': 'Already have an account?',
     'common.socialLogin': 'Login with other services',
     'common.viewMore': 'View more',
-    'common.overview': 'Overview'
+    'common.overview': 'Overview',
+    'common.loggingOut': 'Logging out',
+    'common.thankYou': 'Thank you!',
+    'common.seeYouAgain': 'See you again!'
 }    /* eslint-enable max-len */;
