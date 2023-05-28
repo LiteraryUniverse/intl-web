0715fb7e05cfbb946983f76bb506f0a7ebda9fe9
Jan Dvorak <hello@jandvorak.me>
2023-05-28 12:26:56 +0200

    New translations discovery.js (Portuguese)

--- pt-PT/discovery.js
+++ pt-PT/discovery.js
@@ -16,7 +16,7 @@ export default {
         one {1 minuto}
         other {# minutos}
     } a ler.`,
-    'story.estimatedReadingTime.explained': 'Com base numa velocidade de leitura média de {avgReadingNum} palavras por minuto.',
+    'story.estimatedReadingTime.explained': 'Based on an average reading speed of {avgReadingNum, number} words per minute.',
     'discover.search.text': 'Pesquisar termo',
     'discover.searchStory.title': 'Procurar uma história',
     'discover.searchStory.description': 'Procurar histórias',
@@ -47,5 +47,7 @@ export default {
     'discover.searchDisplay.list': 'Lista compacta',
     'discover.includesAds': 'Este trabalho possui uma colocação de produto ou inclui nele outras formas de publicidade.',
     'discovery.featured.universes': 'Universos em destaque',
-    'discovery.featured.stories': 'Histórias em destaque'
+    'discovery.featured.stories': 'Histórias em destaque',
+    'discovery.story.tableOfContent': 'Table of content',
+    'story.status.hiatus': 'On hiatus'
 };
