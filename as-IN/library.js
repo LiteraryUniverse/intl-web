afd15f0f7a1e6fbc1769c555c1d57f9cbcb05ac9
Jan Dvorak <hello@jandvorak.me>
2023-06-21 20:21:26 +0200

    New translations library.js (Assamese)

--- as-IN/library.js
+++ as-IN/library.js
@@ -3,5 +3,6 @@ export default {
     'library.remove': 'Remove from library',
     'library.bought': 'Bought stories',
     'library.stories': 'Followed stories',
-    'library.universes': 'Followed universes'
+    'library.universes': 'Followed universes',
+    'bookmark.continueReading': 'Continue reading'
 };
