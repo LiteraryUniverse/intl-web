743c8abe01239c3f36bd893b74d2338f7cf1287f
Jan Dvorak <hello@jandvorak.me>
2023-06-21 20:20:45 +0200

    New translations library.js (Arabic)

--- ar-SA/library.js
+++ ar-SA/library.js
@@ -3,5 +3,6 @@ export default {
     'library.remove': 'إزالة من المكتبة',
     'library.bought': 'Bought stories',
     'library.stories': 'Followed stories',
-    'library.universes': 'Followed universes'
+    'library.universes': 'Followed universes',
+    'bookmark.continueReading': 'Continue reading'
 };
