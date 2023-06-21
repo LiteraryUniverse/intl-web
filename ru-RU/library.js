50728ba46f62fc304249c9fffb91c7fbec901fb3
Jan Dvorak <hello@jandvorak.me>
2023-06-21 20:21:05 +0200

    New translations library.js (Russian)

--- ru-RU/library.js
+++ ru-RU/library.js
@@ -3,5 +3,6 @@ export default {
     'library.remove': 'Удалить из библиотеки',
     'library.bought': 'Купленные истории',
     'library.stories': 'Отслеживаемые истории',
-    'library.universes': 'Отслеживаемые вселенные'
+    'library.universes': 'Отслеживаемые вселенные',
+    'bookmark.continueReading': 'Continue reading'
 };
