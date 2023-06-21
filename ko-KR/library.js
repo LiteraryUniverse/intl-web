e905e8769be9dbe5f7e9cd5f08cd860a3b8ba378
Jan Dvorak <hello@jandvorak.me>
2023-06-21 20:20:59 +0200

    New translations library.js (Korean)

--- ko-KR/library.js
+++ ko-KR/library.js
@@ -3,5 +3,6 @@ export default {
     'library.remove': '라이브러리에서 제거',
     'library.bought': '구매한 스토리',
     'library.stories': '팔로우한 스토리',
-    'library.universes': '팔로우한 유니버스'
+    'library.universes': '팔로우한 유니버스',
+    'bookmark.continueReading': 'Continue reading'
 };
