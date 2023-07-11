678387b7d3b2076f7115041d6519e8265c10b7f6
Jan Dvorak <hello@jandvorak.me>
2023-07-11 17:36:57 +0200

    New translations library.js (Korean)

--- ko-KR/library.js
+++ ko-KR/library.js
@@ -4,5 +4,6 @@ export default {
     'library.bought': '구매한 스토리',
     'library.stories': '팔로우한 스토리',
     'library.universes': '팔로우한 유니버스',
-    'bookmark.continueReading': 'Continue reading'
+    'bookmark.continueReading': 'Continue reading',
+    'bookmark.statusSummary': 'You have read {readPercent, number, ::percent} of the story.'
 };
