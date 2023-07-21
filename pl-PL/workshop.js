5a98d7343c050fccbfbe7c7df0d6d6d71d86e900
Jan Dvorak <hello@jandvorak.me>
2023-07-21 19:46:05 +0200

    New translations workshop.js (Polish)

--- pl-PL/workshop.js
+++ pl-PL/workshop.js
@@ -264,5 +264,13 @@ export default {
     'workshop.statusExplain.finished1': 'Congratulations! After all that hard work you have finally reached the end of an incredible journey. While the story might end now, we hope that there will be more stories to tell in the future, but for now, relax and enjoy the well earned rest.',
     'workshop.statusExplain.finished2': 'Marking story as complete will tell the readers that the story is finished. While you shouldn\'t be adding any new chapters we won\'t stop you and adding special chapters isn\'t a bad idea, to for example add more epilogues for all characters.',
     'workshop.statusExplain.finished3': 'While you can always cancel this status we highly advice against it. If the story is to continue it might be better to start a new story.',
-    'workshop.status.goActive': 'Make the story active again'
+    'workshop.status.goActive': 'Make the story active again',
+    'workshop.statusChangeEmail.storyIntro': 'We would like to inform you that your story:',
+    'workshop.statusChangeEmail.hiatusTitle': 'Your story status was automatically set to hiatus',
+    'workshop.statusChangeEmail.hiatusIntro': 'Due to inactivity your story status was automatically set to hiatus.',
+    'workshop.statusChangeEmail.hiatusWhy': 'Has been automatically put on hiatus due to no new chapter being published for over 6 months.',
+    'workshop.statusChangeEmail.hiatusWhatFollows': 'This status will be automatically lifted if you publish a new chapter. If you don\'t publish a new chapter in the next 6 months we will automatically set the story as cancelled.',
+    'workshop.statusChangeEmail.cancellationIntro': 'Your story status was automatically set to cancelled.',
+    'workshop.statusChangeEmail.cancellationWhy': 'This has happened because you haven\'t posted any new chapter to the story since it was set to hiatus 6 months ago. So after a year of inactivity we automatically set stories to cancelled so that readers know which stories are active.',
+    'workshop.statusChangeEmail.cancellationWhatFollows': 'This status will be automatically lifted if you publish a new chapter. If you don\'t plan to work on this story anymore there is nothing else that you need to do.'
 };
