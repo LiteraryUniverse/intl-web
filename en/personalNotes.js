b94fe423db4a7b94f2c3d9448f3d8696ccd98f9f
Sebadam <adam.sebesta3@gmail.com>
2023-07-22 15:19:18 +0200

    Texts for personal notes

---
+++ en/personalNotes.js
@@ -0,0 +1,8 @@
+export default {
+  'personalNotes.personalNotes': 'Personal Notes',
+  'personalNotes.chooseType': 'Choose the type of personal note',
+  'personalNotes.storyNotes': 'Story Notes',
+  'personalNotes.chapterNotes': 'Chapter Notes',
+  'personalNotes.universeNotes': 'Universe Notes',
+  'personalNotes.encyclopediaNotes': 'Encyclopedia Notes',
+}
