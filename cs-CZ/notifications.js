503c172c43f2b6b89b991f1605e488141fe4f07b
Jan Dvorak <hello@jandvorak.me>
2023-07-25 23:44:30 +0200

    New translations notifications.js (Czech)

--- cs-CZ/notifications.js
+++ cs-CZ/notifications.js
@@ -4,5 +4,9 @@ export default {
   'flashnews.startsAt': 'Začněme zobrazovat tuto zprávu od',
   'flashnews.endsAt': 'Přestaneme zobrazovat tuto zprávu od',
   'flashnews.newLanguage': 'Vyberte nový jazyk pro přidání',
-  'flashnews.onlyDisplayOn': 'Zprávy budou zobrazeny pouze na jazycích vybraných níže (pokud jsou vybrány). Zpráva nebude zobrazena ve výchozím jazyku pro uživatele s jiným jazykem.'
+  'flashnews.onlyDisplayOn': 'Zprávy budou zobrazeny pouze na jazycích vybraných níže (pokud jsou vybrány). Zpráva nebude zobrazena ve výchozím jazyku pro uživatele s jiným jazykem.',
+  'notifications.menu.all': 'All notification',
+  'notifications.menu.following': 'Following',
+  'notifications.menu.workshop': 'Workshop',
+  'notifications.menu.news': 'News & updates'
 };
