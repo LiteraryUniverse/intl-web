c4ec73bfc63294d6515a0c79e95d062ad1a86d75
Jan Dvorak <hello@jandvorak.me>
2023-07-25 23:44:46 +0200

    New translations notifications.js (Slovak)

--- sk-SK/notifications.js
+++ sk-SK/notifications.js
@@ -4,5 +4,9 @@ export default {
   'flashnews.startsAt': 'Začnite zobrazovať túto správu od',
   'flashnews.endsAt': 'Prestaňte zobrazovať túto správu od',
   'flashnews.newLanguage': 'Vyberte nový jazyk, ktorý chcete pridať',
-  'flashnews.onlyDisplayOn': 'Správy sa budú zobrazovať len v jazykoch vybraných nižšie (ak sú vybrané), v iných jazykoch sa nebudú zobrazovať žiadne správy, dokonca ani v predvolenom jazyku.'
+  'flashnews.onlyDisplayOn': 'Správy sa budú zobrazovať len v jazykoch vybraných nižšie (ak sú vybrané), v iných jazykoch sa nebudú zobrazovať žiadne správy, dokonca ani v predvolenom jazyku.',
+  'notifications.menu.all': 'All notification',
+  'notifications.menu.following': 'Following',
+  'notifications.menu.workshop': 'Workshop',
+  'notifications.menu.news': 'News & updates'
 };
