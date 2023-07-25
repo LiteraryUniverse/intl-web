9256e0dedcfb69a70fc088e8b5b929f4b2bb98f8
Jan Dvorak <hello@jandvorak.me>
2023-07-25 23:43:07 +0200

    Update source file notifications.js

--- en/notifications.js
+++ en/notifications.js
@@ -5,10 +5,5 @@ export default {
   'flashnews.endsAt': 'Stop displaying this message at',
   'flashnews.newLanguage': 'Select new language to add',
   'flashnews.onlyDisplayOn':
-    'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown, even in the default language, in other languages.',
-
-  'notifications.menu.all': 'All notification',
-  'notifications.menu.following': 'Following',
-  'notifications.menu.workshop': 'Workshop',
-  'notifications.menu.news': 'News & updates'
+    'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown, even in the default language, in other languages.'
 }
