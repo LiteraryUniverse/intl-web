c1c9c5750c8f84cb9b3225525fb2f1db02b5e763
Jan Dvorak <hello@jandvorak.me>
2023-07-25 23:44:26 +0200

    New translations notifications.js (French)

--- fr-FR/notifications.js
+++ fr-FR/notifications.js
@@ -4,5 +4,9 @@ export default {
   'flashnews.startsAt': 'Commencer à afficher ce message à',
   'flashnews.endsAt': 'Arrêter l\'affichage de ce message à',
   'flashnews.newLanguage': 'Sélectionnez la nouvelle langue à ajouter',
-  'flashnews.onlyDisplayOn': 'Les actualités ne seront affichées que dans les langues sélectionnées ci-dessous (s\'il y en a une sélectionnée), il n\'y aura pas d\'actualités affichées, même dans la langue par défaut, dans d\'autres langues.'
+  'flashnews.onlyDisplayOn': 'Les actualités ne seront affichées que dans les langues sélectionnées ci-dessous (s\'il y en a une sélectionnée), il n\'y aura pas d\'actualités affichées, même dans la langue par défaut, dans d\'autres langues.',
+  'notifications.menu.all': 'All notification',
+  'notifications.menu.following': 'Following',
+  'notifications.menu.workshop': 'Workshop',
+  'notifications.menu.news': 'News & updates'
 };
