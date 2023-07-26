2bda3a726d2bfe72daa721427dcccd5ba66f842c
Jan Dvorak <hello@jandvorak.me>
2023-07-26 18:36:06 +0200

    Update source file notifications.js

--- en/notifications.js
+++ en/notifications.js
@@ -10,5 +10,18 @@ export default {
   'notifications.menu.all': 'All notification',
   'notifications.menu.following': 'Following',
   'notifications.menu.workshop': 'Workshop',
-  'notifications.menu.news': 'News & updates'
+  'notifications.menu.news': 'News & updates',
+
+  // Notification messages
+  'notification.workshopStoryStatus.toHiatus':
+    'Your story {storyName} was automatically set to hiatus after 6 months of inactivity.',
+  'notification.workshopStoryStatus.toCancelled':
+    'Your story {storyName} was automatically set to cancelled after one year of inactivity.',
+  'notification.followingStoryStatus.toHiatus': '{storyName} is on hiatus.',
+  'notification.followingStoryStatus.toCancelled':
+    '{storyName} has been cancelled.',
+  'notification.followingNewChapter.new':
+    'New chapter in {storyName} has been published!',
+  'notification.workshopStory.newComment':
+    '{username} has commented on {storyName}'
 }
