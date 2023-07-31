b168393877c7957671315c76a5ba3245ed939baf
Jan Dvorak <hello@jandvorak.me>
2023-07-31 23:50:34 +0200

    New translations notifications.js (Spanish)

--- es-ES/notifications.js
+++ es-ES/notifications.js
@@ -5,15 +5,15 @@ export default {
   'flashnews.endsAt': 'Dejar de mostrar este mensaje a las',
   'flashnews.newLanguage': 'Seleccionar un nuevo idioma que añadir',
   'flashnews.onlyDisplayOn': 'Las noticias se mostrarán solo en los idiomas seleccionados a continuación (si es que se selecciona alguno), no se mostrarán noticias, ni siquiera en el idioma por defecto, en otros idiomas.',
-  'notifications.menu.all': 'All notification',
-  'notifications.menu.following': 'Following',
-  'notifications.menu.workshop': 'Workshop',
-  'notifications.menu.news': 'News & updates',
+  'notifications.menu.all': 'Todas las notificaciones',
+  'notifications.menu.following': 'Siguiendo',
+  'notifications.menu.workshop': 'Taller',
+  'notifications.menu.news': 'Noticias y actualizaciones',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus': 'Your story {storyName} was automatically set to hiatus after 6 months of inactivity.',
-  'notification.workshopStoryStatus.toCancelled': 'Your story {storyName} was automatically set to cancelled after one year of inactivity.',
-  'notification.followingStoryStatus.toHiatus': '{storyName} is on hiatus.',
-  'notification.followingStoryStatus.toCancelled': '{storyName} has been cancelled.',
-  'notification.followingNewChapter.new': 'New chapter in {storyName} has been published!',
-  'notification.workshopStory.newComment': '{username} has commented on {storyName}'
+  'notification.workshopStoryStatus.toHiatus': 'Tu historia {storyName} ha sido marcada como en hiato automáticamente tras 6 meses de inactividad.',
+  'notification.workshopStoryStatus.toCancelled': 'Tu historia {storyName} ha sido marcada como cancelada automáticamente tras un año de inactividad.',
+  'notification.followingStoryStatus.toHiatus': '{storyName} está en hiato.',
+  'notification.followingStoryStatus.toCancelled': '{storyName} ha sido cancelada.',
+  'notification.followingNewChapter.new': '¡Se ha publicado un nuevo capítulo de {storyName}!',
+  'notification.workshopStory.newComment': '{username} ha dejado un comentario en {storyName}'
 };
