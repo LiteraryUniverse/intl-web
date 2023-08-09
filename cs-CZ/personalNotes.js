a1124d8fdfc561b8ac02e21bbd59144be1c37fa3
Jan Dvorak <hello@jandvorak.me>
2023-08-09 09:35:21 +0200

    New translations personalNotes.js (Czech)

--- cs-CZ/personalNotes.js
+++ cs-CZ/personalNotes.js
@@ -8,9 +8,9 @@ export default {
   'personalNotes.storyPersonalNotesHeading': 'Personal Notes on Stories',
   'personalNotes.encyclopediaPersonalNotesHeading': 'Personal Notes on Encyclopedia',
   'personalNotes.searchPersonalNotesHeading': 'Search Personal Notes',
-  'personalNotes.goToStory': 'Go to story',
-  'personalNotes.goToChapter': 'Go to chapter',
+  'personalNotes.goToStory': 'Přejít na příběh',
+  'personalNotes.goToChapter': 'Přejít do kapitoly',
   'personalNotes.goToEncyclopedia': 'Go to Universe’s Encyclopedia',
   'personalNotes.goToEncyclopediaEntry': 'Go To Enscylopedia’s Entry',
-  'personalNotes.closeDropdown': 'Close the dropdown'
+  'personalNotes.closeDropdown': 'Zavřít seznam'
 };
