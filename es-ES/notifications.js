c9298176289c44bc13693f85078c42a1d3c8adba
Jan Dvorak <hello@jandvorak.me>
2023-08-11 15:45:06 +0200

    New translations notifications.js (Spanish)

--- es-ES/notifications.js
+++ es-ES/notifications.js
@@ -9,11 +9,19 @@ export default {
   'notifications.menu.following': 'Siguiendo',
   'notifications.menu.workshop': 'Taller',
   'notifications.menu.news': 'Noticias y actualizaciones',
+  'notifications.menu.markUnread': 'Mark as unread',
+  'notifications.menu.markRead': 'Mark as read',
+  'notifications.menu.delete': 'Delete notification',
   // Notification messages
   'notification.workshopStoryStatus.toHiatus': 'Tu historia {storyName} ha sido marcada como en hiato automáticamente tras 6 meses de inactividad.',
   'notification.workshopStoryStatus.toCancelled': 'Tu historia {storyName} ha sido marcada como cancelada automáticamente tras un año de inactividad.',
   'notification.followingStoryStatus.toHiatus': '{storyName} está en hiato.',
   'notification.followingStoryStatus.toCancelled': '{storyName} ha sido cancelada.',
   'notification.followingNewChapter.new': '¡Se ha publicado un nuevo capítulo de {storyName}!',
-  'notification.workshopStory.newComment': '{username} ha dejado un comentario en {storyName}'
+  'notification.workshopStory.newComment': 'has commented on {storyName}',
+  // Before this text will be the username of the user
+  'notification.friendRequest': 'send you a friend request.',
+  // Before this text will be the username of the user
+  // Strings for extra section
+  'notificationExtra.workshopStoryStatus.goToStory': 'Go to story'
 };
