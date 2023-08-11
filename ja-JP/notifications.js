d63e5b5227376fd072a3cb2e799d8ed1d431a58d
Jan Dvorak <hello@jandvorak.me>
2023-08-11 15:45:20 +0200

    New translations notifications.js (Japanese)

--- ja-JP/notifications.js
+++ ja-JP/notifications.js
@@ -9,11 +9,19 @@ export default {
   'notifications.menu.following': 'Following',
   'notifications.menu.workshop': 'Workshop',
   'notifications.menu.news': 'News & updates',
+  'notifications.menu.markUnread': 'Mark as unread',
+  'notifications.menu.markRead': 'Mark as read',
+  'notifications.menu.delete': 'Delete notification',
   // Notification messages
   'notification.workshopStoryStatus.toHiatus': 'Your story {storyName} was automatically set to hiatus after 6 months of inactivity.',
   'notification.workshopStoryStatus.toCancelled': 'Your story {storyName} was automatically set to cancelled after one year of inactivity.',
   'notification.followingStoryStatus.toHiatus': '{storyName} is on hiatus.',
   'notification.followingStoryStatus.toCancelled': '{storyName} has been cancelled.',
   'notification.followingNewChapter.new': 'New chapter in {storyName} has been published!',
-  'notification.workshopStory.newComment': '{username} has commented on {storyName}'
+  'notification.workshopStory.newComment': 'has commented on {storyName}',
+  // Before this text will be the username of the user
+  'notification.friendRequest': 'send you a friend request.',
+  // Before this text will be the username of the user
+  // Strings for extra section
+  'notificationExtra.workshopStoryStatus.goToStory': 'Go to story'
 };
