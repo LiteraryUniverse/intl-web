4dc737d8cfa6f8ca0f9b657452ac5c127d40d66e
Jan Dvorak <hello@jandvorak.me>
2023-08-11 15:45:07 +0200

    New translations notifications.js (Czech)

--- cs-CZ/notifications.js
+++ cs-CZ/notifications.js
@@ -9,11 +9,19 @@ export default {
   'notifications.menu.following': 'Sledující',
   'notifications.menu.workshop': 'Ateliér',
   'notifications.menu.news': 'Novinky a aktualizace',
+  'notifications.menu.markUnread': 'Mark as unread',
+  'notifications.menu.markRead': 'Mark as read',
+  'notifications.menu.delete': 'Delete notification',
   // Notification messages
   'notification.workshopStoryStatus.toHiatus': 'Váš příběh {storyName} byl automaticky nastaven na hiatus po 6 měsících nečinnosti.',
   'notification.workshopStoryStatus.toCancelled': 'Váš příběh {storyName} byl automaticky nastaven na zrušený po roce bez nových kapitol.',
   'notification.followingStoryStatus.toHiatus': '{storyName} je na hiatusu.',
   'notification.followingStoryStatus.toCancelled': '{storyName} byla zrušena.',
   'notification.followingNewChapter.new': 'Nová kapitola {storyName} byla publikována!',
-  'notification.workshopStory.newComment': '{username} okomentoval/a na {storyName}'
+  'notification.workshopStory.newComment': 'has commented on {storyName}',
+  // Before this text will be the username of the user
+  'notification.friendRequest': 'send you a friend request.',
+  // Before this text will be the username of the user
+  // Strings for extra section
+  'notificationExtra.workshopStoryStatus.goToStory': 'Go to story'
 };
