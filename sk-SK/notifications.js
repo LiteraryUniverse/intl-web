b8b1cb768e221da0f3e4aed0cad60d2a02b35b6e
Jan Dvorak <hello@jandvorak.me>
2023-08-12 08:42:00 +0200

    New translations notifications.js (Slovak)

--- sk-SK/notifications.js
+++ sk-SK/notifications.js
@@ -5,13 +5,13 @@ export default {
   'flashnews.endsAt': 'Prestaňte zobrazovať túto správu od',
   'flashnews.newLanguage': 'Vyberte nový jazyk, ktorý chcete pridať',
   'flashnews.onlyDisplayOn': 'Správy sa budú zobrazovať len v jazykoch vybraných nižšie (ak sú vybrané), v iných jazykoch sa nebudú zobrazovať žiadne správy, dokonca ani v predvolenom jazyku.',
-  'notifications.menu.all': 'All notification',
-  'notifications.menu.following': 'Following',
-  'notifications.menu.workshop': 'Workshop',
-  'notifications.menu.news': 'News & updates',
-  'notifications.menu.markUnread': 'Mark as unread',
-  'notifications.menu.markRead': 'Mark as read',
-  'notifications.menu.delete': 'Delete notification',
+  'notifications.menu.all': 'Všetky upozornenia',
+  'notifications.menu.following': 'Nasledovanie',
+  'notifications.menu.workshop': 'Dielňa',
+  'notifications.menu.news': 'Novinky a aktualizácie',
+  'notifications.menu.markUnread': 'Označiť ako neprečítané',
+  'notifications.menu.markRead': 'Označiť ako prečítané',
+  'notifications.menu.delete': 'Zmazať notifikáciu',
   // Notification messages
   'notification.workshopStoryStatus.toHiatus': 'Your story {storyName} was automatically set to hiatus after 6 months of inactivity.',
   'notification.workshopStoryStatus.toCancelled': 'Your story {storyName} was automatically set to cancelled after one year of inactivity.',
@@ -20,7 +20,7 @@ export default {
   'notification.followingNewChapter.new': 'New chapter in {storyName} has been published!',
   'notification.workshopStory.newComment': 'has commented on {storyName}',
   // Before this text will be the username of the user
-  'notification.friendRequest': 'send you a friend request.',
+  'notification.friendRequest': 'Vám poslal(a) žiadosť o priateľstvo.',
   // Before this text will be the username of the user
   // Strings for extra section
   'notificationExtra.workshopStoryStatus.goToStory': 'Go to story'
