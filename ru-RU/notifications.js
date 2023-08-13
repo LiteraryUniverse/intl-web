a0711c18573a24d87516d4ce4fe43518e45e1fb6
Jan Dvorak <hello@jandvorak.me>
2023-08-13 16:35:11 +0200

    New translations notifications.js (Russian)

--- ru-RU/notifications.js
+++ ru-RU/notifications.js
@@ -5,23 +5,23 @@ export default {
   'flashnews.endsAt': 'Остановить отображение этого сообщения',
   'flashnews.newLanguage': 'Выберите новый язык для добавления',
   'flashnews.onlyDisplayOn': 'Новости будут отображаться только на выбранных ниже языках (если они выбраны), на других языках новости, даже на языке по умолчанию, отображаться не будут.',
-  'notifications.menu.all': 'All notification',
-  'notifications.menu.following': 'Following',
-  'notifications.menu.workshop': 'Workshop',
-  'notifications.menu.news': 'News & updates',
-  'notifications.menu.markUnread': 'Mark as unread',
-  'notifications.menu.markRead': 'Mark as read',
-  'notifications.menu.delete': 'Delete notification',
+  'notifications.menu.all': 'Все уведомления',
+  'notifications.menu.following': 'Отслеживания',
+  'notifications.menu.workshop': 'Мастерская',
+  'notifications.menu.news': 'Новости и обновления',
+  'notifications.menu.markUnread': 'Отметить как непрочитанное',
+  'notifications.menu.markRead': 'Пометить как прочитанное',
+  'notifications.menu.delete': 'Удалить уведомление',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus': 'Your story {storyName} was automatically set to hiatus after 6 months of inactivity.',
-  'notification.workshopStoryStatus.toCancelled': 'Your story {storyName} was automatically set to cancelled after one year of inactivity.',
-  'notification.followingStoryStatus.toHiatus': '{storyName} is on hiatus.',
-  'notification.followingStoryStatus.toCancelled': '{storyName} has been cancelled.',
-  'notification.followingNewChapter.new': 'New chapter in {storyName} has been published!',
-  'notification.workshopStory.newComment': 'has commented on {storyName}',
+  'notification.workshopStoryStatus.toHiatus': 'Ваша история "{storyName}" была автоматически переведена в состояние приостановленой после 6 месяцев неактивности.',
+  'notification.workshopStoryStatus.toCancelled': 'Ваша история "{storyName}" была автоматически переведена в состояние отменённых после одного года неактивности.',
+  'notification.followingStoryStatus.toHiatus': '{storyName} — «Приостановлено».',
+  'notification.followingStoryStatus.toCancelled': '{storyName} — «Отменено».',
+  'notification.followingNewChapter.new': 'Опубликована новая глава в "{storyName}"!',
+  'notification.workshopStory.newComment': 'оставляет комментарий к "{storyName}"',
   // Before this text will be the username of the user
-  'notification.friendRequest': 'send you a friend request.',
+  'notification.friendRequest': 'отправляет вам запрос на добавление в друзья.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Go to story'
+  'notificationExtra.workshopStoryStatus.goToStory': 'Перейти к истории'
 };
