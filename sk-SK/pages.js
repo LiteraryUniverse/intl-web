003dfe8a59a0dec24a26f5804c179f33eb784a74
Jan Dvorak <hello@jandvorak.me>
2023-10-02 19:35:26 +0200

    New translations pages.js (Slovak)

--- sk-SK/pages.js
+++ sk-SK/pages.js
@@ -88,7 +88,6 @@ export default {
     'about.attributions': 'Atribuce',
     'about.attributionsIntro': 'Literary Universe je postavený na mnohých projektoch s otvoreným zdrojovým kódom a bezplatných zdrojoch. Nižšie nájdete ich zoznam.',
     'about.attributionsMatomo': 'Naša analytika využíva vlastný hosting Matomo s údajmi IP2Location LITE dostupnými zo stránky https://lite.ip2location.com.',
-    'about.attributionIcons': 'Na stránke sa používajú nasledujúce ikony prostredníctvom styled-icons:',
     'team.name.pjakubec': 'Pavel Jakubec',
     'team.name.asebesta': 'Adam Šebesta',
     'about.developer': 'Vývojár',
