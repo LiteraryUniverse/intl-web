e95216972d982d34bff8f43aa59276847b9dfaa1
Jan Dvorak <jdvorak@literaryuniverse.com>
2023-11-01 17:22:02 +0100

    Add aria description for enc menu

--- en/encyclopedia.js
+++ en/encyclopedia.js
@@ -28,9 +28,11 @@ export default {
     one {1 related entry}
     other {# related entries}
   } found.`,
-  'encyclopedia.help.text': 'Use the menu on the left to navigate between the items in the encyclopedia. Once you select an item, it will be displayed instead of this text.',
+  'encyclopedia.help.text':
+    'Use the menu on the left to navigate between the items in the encyclopedia. Once you select an item, it will be displayed instead of this text.',
   'encyclopedia.help.sidebar': 'Sidebar Toolbox',
-  'encyclopedia.help.sidebar.text': 'Welcome to your story! This sidebar allows you easy access to all resources related to your story. Check the icons above to see latest comments, your notes, search your encyclopedia, browse and add new entries to your encyclopedia. If you want to add connection to an entry from the story text, select the text where you want to place it and then click the connect button. You will be then prompted to find and select an item.',
+  'encyclopedia.help.sidebar.text':
+    'Welcome to your story! This sidebar allows you easy access to all resources related to your story. Check the icons above to see latest comments, your notes, search your encyclopedia, browse and add new entries to your encyclopedia. If you want to add connection to an entry from the story text, select the text where you want to place it and then click the connect button. You will be then prompted to find and select an item.',
   'encyclopedia.intro': 'Welcome to the {universe} encyclopedia.',
   'encyclopedia.add.title': 'Add a new entry to encyclopedia',
   'encyclopedia.add.heading': 'What do you wish to add?',
@@ -103,10 +105,12 @@ export default {
   'encyclopedia.public': 'Public',
   'encyclopedia.private': 'Private',
   'encyclopedia.container': 'Container',
-  'encyclopedia.container.desc': 'Make this entry a container for a new group of entries. This will hide it from categories listing.',
+  'encyclopedia.container.desc':
+    'Make this entry a container for a new group of entries. This will hide it from categories listing.',
   'encyclopedia.entry.name': 'Name of the entry',
   'encyclopedia.summary': 'Summary',
-  'encyclopedia.summary.desc': 'This is a summary that will be the only initial information displayed on the reader.',
+  'encyclopedia.summary.desc':
+    'This is a summary that will be the only initial information displayed on the reader.',
   'encyclopedia.parent': 'Parent entry',
   'encyclopedia.related': 'Related entries',
   'encyclopedia.errors.tags': 'You can only have 10 tags per entry.',
@@ -118,16 +122,20 @@ export default {
   'encyclopedia.connections.existing.abbr': 'EE',
   // abbreviation for Existing Entry
   'encyclopedia.cover': 'Cover image',
-  'encyclopedia.translation.title': 'Welcome to {universe} universe translation!',
-  'encyclopedia.translation.desc': 'This encyclopedia interface is used for translation into {language}. Choose existing entries in the list on the left to begin their translation.',
+  'encyclopedia.translation.title':
+    'Welcome to {universe} universe translation!',
+  'encyclopedia.translation.desc':
+    'This encyclopedia interface is used for translation into {language}. Choose existing entries in the list on the left to begin their translation.',
   'encyclopedia.welcome.totalEntries': `There {totalEntries, plural, 
     zero {are no entries}
     one {is one entry}
     other {are # entries}
   } in this universe.`,
   'encyclopedia.settings.title': 'Encyclopedia settings',
-  'encyclopedia.settings.categoriesDesc': 'Turn on and off categories which you want to use in your encyclopedia.',
-  'encyclopedia.settings.introText': 'Welcome message to be displayed after universe description.',
+  'encyclopedia.settings.categoriesDesc':
+    'Turn on and off categories which you want to use in your encyclopedia.',
+  'encyclopedia.settings.introText':
+    'Welcome message to be displayed after universe description.',
   'encyclopedia.fields.placeholder': 'Field name',
   'encyclopedia.fields.delete': 'Delete field',
   'encyclopedia.timeOption.fictionalDate': 'Fictional date',
@@ -136,7 +144,8 @@ export default {
   'encyclopedia.timeOption.date': 'Date',
   'encyclopedia.timeOption.time': 'Time',
   'encyclopedia.timeOption.dateTime': 'Date and time',
-  'encyclopedia.settings.groupByCategories': 'Include categories in group listing',
+  'encyclopedia.settings.groupByCategories':
+    'Include categories in group listing',
   'encyclopedia.menuOpen': 'Open Encyclopedia menu',
   'encyclopedia.menuClose': 'Close Encyclopedia menu',
   'encyclopedia.menuAria': 'Encyclopedia menu',
@@ -145,5 +154,7 @@ export default {
     =0 {unlimited}
     other {{max, number}}
   } entries.`,
-  'enc.entries.limitReached': 'You have reached maximum allowed encyclopedia entries for you current subscription.'
+  'enc.entries.limitReached':
+    'You have reached maximum allowed encyclopedia entries for you current subscription.',
+  'enc.menuControls.ariaDesc': 'Encyclopedia controls'
 }
