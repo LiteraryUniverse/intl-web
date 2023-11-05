dc50a21644f01e4bdd544e960bd06e245b441edf
Jan Dvorak <hello@jandvorak.me>
2023-11-05 15:12:53 +0100

    New translations encyclopedia.js (Portuguese)

--- pt-PT/encyclopedia.js
+++ pt-PT/encyclopedia.js
@@ -147,7 +147,7 @@ export default {
   } entradas.`,
     'enc.entries.limitReached': 'Atingiu o máximo de entradas enciclopédicas permitidas para a sua subscrição atual.',
     'enc.menuControls.ariaDesc': 'Encyclopedia controls',
-    'enc.pronunciation.title': 'Pronunciation',
-    'enc.wip.abbreviation': 'WIP',
-    'enc.wip.full': 'Work in Progress'
+    'encyclopedia.pronunciation.title': 'Pronunciation',
+    'encyclopedia.wip.abbreviation': 'WIP',
+    'encyclopedia.wip.full': 'Work in Progress'
 };
