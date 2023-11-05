9d295602d359900ae54d9dd7bf6b02157c6bb888
Jan Dvorak <hello@jandvorak.me>
2023-11-05 12:14:48 +0100

    New translations encyclopedia.js (Portuguese)

--- pt-PT/encyclopedia.js
+++ pt-PT/encyclopedia.js
@@ -146,5 +146,8 @@ export default {
     other {{max, number}}
   } entradas.`,
     'enc.entries.limitReached': 'Atingiu o máximo de entradas enciclopédicas permitidas para a sua subscrição atual.',
-    'enc.menuControls.ariaDesc': 'Encyclopedia controls'
+    'enc.menuControls.ariaDesc': 'Encyclopedia controls',
+    'enc.pronunciation.title': 'Pronunciation',
+    'enc.wip.abbreviation': 'WIP',
+    'enc.wip.full': 'Work in Progress'
 };
