b3a3dddd8b4ea940f1aa6448fa122ea40a32d6d9
Jan Dvorak <hello@jandvorak.me>
2023-11-05 15:12:43 +0100

    New translations encyclopedia.js (Italian)

--- it-IT/encyclopedia.js
+++ it-IT/encyclopedia.js
@@ -147,7 +147,7 @@ export default {
   } disponibili.`,
     'enc.entries.limitReached': 'Hai raggiunto il massimo di voci consentito nell\'enciclopedia per il tuo abbonamento attuale.',
     'enc.menuControls.ariaDesc': 'Encyclopedia controls',
-    'enc.pronunciation.title': 'Pronunciation',
-    'enc.wip.abbreviation': 'WIP',
-    'enc.wip.full': 'Work in Progress'
+    'encyclopedia.pronunciation.title': 'Pronunciation',
+    'encyclopedia.wip.abbreviation': 'WIP',
+    'encyclopedia.wip.full': 'Work in Progress'
 };
