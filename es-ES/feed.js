259f0909816b01946c08a44a4209b8aa841da525
Jan Dvorak <hello@jandvorak.me>
2023-11-09 23:15:16 +0100

    New translations feed.js (Spanish)

--- es-ES/feed.js
+++ es-ES/feed.js
@@ -1,5 +1,5 @@
 export default {
-    'feed.name': 'Flujo',
+    'feed.name': 'Feed',
     'feed.post.new': 'Nueva publicación',
     'feed.post.like': 'Dar me gusta',
     'feed.post.unlike': 'Quitar me gusta',
