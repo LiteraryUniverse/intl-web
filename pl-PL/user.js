4c2254a26104dd632bca794c5b3eed91b41b0333
Jan Dvorak <hello@jandvorak.me>
2023-11-09 20:07:43 +0100

    New translations user.js (Polish)

--- pl-PL/user.js
+++ pl-PL/user.js
@@ -31,5 +31,17 @@ export default {
     'user.verification.publishedAuthor': 'Ten autor został opublikowany jako tradycyjny sposób',
     'user.verification.luAuthor': 'Autorka z Literackiego Wszechświata',
     'user.verification.luEmployee': 'Pracownik Wszechświata Literackiego',
-    'user.verification.publicFigure': 'Zweryfikowana osoba publiczna'
+    'user.verification.publicFigure': 'Zweryfikowana osoba publiczna',
+    'profile.storiesCount': `{count, number} {count, plural,
+    one {story}
+    other {stories}
+  }`,
+    'profile.universesCount': `{count, number} {count, plural,
+    one {universe}
+    other {universes}
+  }`,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
+    one {fan fiction}
+    other {fan fictions} 
+  }`
 };
