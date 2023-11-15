c564d85a0d45cdc4e27bed9941ad789698d33509
Jan Dvorak <hello@jandvorak.me>
2023-11-15 16:51:36 +0100

    New translations legal.js (Arabic)

--- ar-SA/legal.js
+++ ar-SA/legal.js
@@ -4,6 +4,8 @@ export default {
     'legal.modal.disagree': 'غير موافق',
     'legal.modal.register': 'لن ترى هذا بعد الآن إذا قمت بالتسجيل.',
     'legal.modal.goToSettings': 'انتقل إلى إعداداتك',
-    'legal.modal.fewmore': 'Few more things...',
-    'legal.modal.finish': 'إنهاء'
+    'legal.modal.oneLastThing': 'One last thing...',
+    'legal.modal.finish': 'إنهاء',
+    'legal.modal.existingUser': 'Already registered?',
+    'legal.modal.pleaseLogin': 'Please login.'
 };
