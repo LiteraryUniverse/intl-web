d6edf71fcf7e4c3fef25df8b7fa2fc838f66b502
Jan Dvorak <hello@jandvorak.me>
2023-11-18 12:01:11 +0100

    New translations discovery.js (Russian)

--- ru-RU/discovery.js
+++ ru-RU/discovery.js
@@ -62,6 +62,6 @@ export default {
     'story.chapter.wasRead': 'Прочитано - {readAt, date}',
     'discovery.moreDetails': 'Больше подробностей',
     'discovery.read': 'Читать',
-    'discovery.chapters.showAll': 'Show all chapters',
-    'discovery.chapters.showMore': 'Show more chapters'
+    'discovery.chapters.showAll': 'Показать все главы',
+    'discovery.chapters.showMore': 'Ещё главы'
 };
