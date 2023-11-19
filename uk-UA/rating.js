67c9189a56d2705e69c3fca546530e73d05dfec7
Jan Dvorak <hello@jandvorak.me>
2023-11-19 22:54:28 +0100

    New translations rating.js (Ukrainian)

--- uk-UA/rating.js
+++ uk-UA/rating.js
@@ -5,15 +5,15 @@ export default {
     'rating.name': 'Рейтинг',
     'rating.explained': 'Віковий ценз та обмеження.',
     'rating.all': 'Всі',
-    'rating.desc.all': 'Доступний для всіх вікових категорій.',
+    'rating.desc.all': 'Доступний для всіх вікових категорій та груп.',
     'rating.teens': 'Підлітки',
     'rating.desc.teens': 'Підходить для аудиторії віком від 12 до 18 років.',
     'rating.mature': 'Дорослі',
-    'rating.desc.mature': 'Contains advance themes, crude language and implicit scenes.',
-    'rating.explicit': 'Explicit',
-    'rating.desc.explicit': 'Contains explicit scenes.',
-    'rating.selection.all': 'All - Accessible to all ages and groups.',
-    'rating.selection.teens': 'Teens (12-18) - Suitable for audience between ages 12 and 18.',
-    'rating.selection.mature': 'Mature (18+) - Contains advance themes, crude language and implicit scenes.',
-    'rating.selection.explicit': 'Explicit (21+) - Contains explicit scenes.'
+    'rating.desc.mature': 'Містить відверті теми та нецензурну лексику.',
+    'rating.explicit': '18+',
+    'rating.desc.explicit': 'Містить відверті сцени.',
+    'rating.selection.all': 'Всі - Доступно для всіх вікових категорій та груп.',
+    'rating.selection.teens': 'Підлітки (12-18) - Підходить для аудиторії віком від 12 до 18 років.',
+    'rating.selection.mature': 'Дорослі (18+) - Містить відверті теми та нецензурну лексику.',
+    'rating.selection.explicit': '18+ (21+) - Містить відверті сцени.'
 };
