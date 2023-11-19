f30a6f78df9f4d3b1e6844a667f9961ed7922781
Jan Dvorak <hello@jandvorak.me>
2023-11-19 18:08:52 +0100

    New translations comments.js (Ukrainian)

--- uk-UA/comments.js
+++ uk-UA/comments.js
@@ -1,9 +1,9 @@
 export default {
     'comment.write': 'Залиште свій коментар',
     'comments.none': 'Вибачте, жодних коментарів для показу.',
-    'comments.total': `{count, plural,
-    =0 {коментарів немає}
-    one {є 1 коментар}
+    'comments.total': `Є {count, plural,
+    =0 {0 коментарів}
+    one {1 коментар}
     few {# коментарів}
     many {# коментарів}
     other {# коментарі}
