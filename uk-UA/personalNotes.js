ff6a49f713664353933fd44540418b2def34e253
Jan Dvorak <hello@jandvorak.me>
2023-11-21 10:47:41 +0100

    New translations personalnotes.js (Ukrainian)

--- uk-UA/personalNotes.js
+++ uk-UA/personalNotes.js
@@ -16,10 +16,11 @@ export default {
   'personalNotes.chapterName': 'Chapter {sequence} - {title}',
   'personalNotes.objectPersonalNotes': '{object} personal note',
   'personalNotes.searchResults': 'Search Results',
-  'personalNotes.type.story': 'Story',
-  'personalNotes.type.universe': 'Universe',
-  'personalNotes.type.chapter': 'Chapter',
+  'personalNotes.type.story': 'Історія',
+  'personalNotes.type.universe': 'Всесвіт',
+  'personalNotes.type.chapter': 'Розділ',
   'personalNotes.type.encEntry': 'Encyclopedia Entry',
   'personalNotes.type.revTextBlock': 'Revision Text Block',
-  'personalNotes.chapter': 'Chapter'
+  'personalNotes.chapter': 'Розділ',
+  'personalNotes.recentSearches': 'Recent Searches:'
 };
