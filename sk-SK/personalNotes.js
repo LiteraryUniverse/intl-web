0de4d5bb523e2962b2f75bb470471ef6930b81a4
Jan Dvorak <hello@jandvorak.me>
2023-11-21 10:47:37 +0100

    New translations personalnotes.js (Slovak)

--- sk-SK/personalNotes.js
+++ sk-SK/personalNotes.js
@@ -16,10 +16,11 @@ export default {
   'personalNotes.chapterName': 'Chapter {sequence} - {title}',
   'personalNotes.objectPersonalNotes': '{object} personal note',
   'personalNotes.searchResults': 'Search Results',
-  'personalNotes.type.story': 'Story',
-  'personalNotes.type.universe': 'Universe',
-  'personalNotes.type.chapter': 'Chapter',
+  'personalNotes.type.story': 'Príbeh',
+  'personalNotes.type.universe': 'Vesmír',
+  'personalNotes.type.chapter': 'Kapitola',
   'personalNotes.type.encEntry': 'Encyclopedia Entry',
   'personalNotes.type.revTextBlock': 'Revision Text Block',
-  'personalNotes.chapter': 'Chapter'
+  'personalNotes.chapter': 'Kapitola',
+  'personalNotes.recentSearches': 'Recent Searches:'
 };
