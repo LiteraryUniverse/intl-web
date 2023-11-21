ee4469ef4fe0608fb3aac9ae5c9a4b93b373fb00
Jan Dvorak <hello@jandvorak.me>
2023-11-21 13:04:33 +0100

    New translations personalnotes.js (Spanish)

--- es-ES/personalNotes.js
+++ es-ES/personalNotes.js
@@ -22,5 +22,5 @@ export default {
   'personalNotes.type.encEntry': 'Entrada de la Enciclopedia',
   'personalNotes.type.revTextBlock': 'Bloque de texto de revisión',
   'personalNotes.chapter': 'Capítulo',
-  'personalNotes.recentSearches': 'Recent Searches:'
+  'personalNotes.recentSearches': 'Búsquedas recientes:'
 };
