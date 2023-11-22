1ba90b4a93defbfafd06ef5613bf97659fc78315
Jan Dvorak <hello@jandvorak.me>
2023-11-22 19:25:35 +0100

    New translations discovery.js (Hebrew)

--- he-IL/discovery.js
+++ he-IL/discovery.js
@@ -1,12 +1,12 @@
 export default {
-    'discovery.meta.desc': 'Discover new stories and universes.',
-    'discovery.universes.new': 'Emerging universes',
-    'discovery.stories.new': 'New stories',
-    'discovery.stories.standalone.new': 'New standalone stories',
-    'discovery.stories.updated': 'Recently updated stories',
-    'discovery.continue': 'Continue reading',
-    'discovery.stories.similar.new': 'New stories in the neighborhood',
-    'discovery.stories.similar.regular': 'Stories in the neighborhood',
+    'discovery.meta.desc': 'גלה סיפורים ויקבים חדשים.',
+    'discovery.universes.new': 'יקבים חדשים במתמודדות',
+    'discovery.stories.new': 'סיפורים חדשים',
+    'discovery.stories.standalone.new': 'סיפורים בודדים חדשים',
+    'discovery.stories.updated': 'סיפורים שעודכנו לאחרונה',
+    'discovery.continue': 'המשך קריאה',
+    'discovery.stories.similar.new': 'סיפורים חדשים בשכונה',
+    'discovery.stories.similar.regular': 'סיפורים בשכונה',
     'story.estimatedReadingTime': `This story should take {hours, plural,
         =0 {}
         one {1 hour and }
@@ -17,44 +17,44 @@ export default {
         other {# minutes}
     } to read.`,
     'story.estimatedReadingTime.explained': 'Based on an average reading speed of {avgReadingNum, number} words per minute.',
-    'discover.search.text': 'Search term',
-    'discover.searchStory.title': 'Search for a story',
-    'discover.searchStory.description': 'Search for a stories',
-    'discover.searchUniverse.title': 'Search for a universe',
-    'discover.searchUniverse.description': 'Search for a universes',
-    'discover.search.storyStatus': 'Story progress status',
-    'story.status.complete': 'Complete',
-    'story.status.inProgress': 'In-progress',
-    'discover.search.universeConnection': 'Connection of story to universe',
-    'story.universeRelation.standalone': 'Standalone story',
-    'story.universeRelation.fanFiction': 'Fan Fiction',
-    'story.universeRelation.universe': 'Part of a universe',
-    'discover.search.rating': 'Age rating',
-    'discover.search.license': 'Copyright license',
-    'discover.search.language': 'Language',
+    'discover.search.text': 'טקסט חיפוש',
+    'discover.searchStory.title': 'חפש סיפור',
+    'discover.searchStory.description': 'חפש סיפורים',
+    'discover.searchUniverse.title': 'חפש יקב',
+    'discover.searchUniverse.description': 'חפש יקבים',
+    'discover.search.storyStatus': 'סטטוס התקדמות הסיפור',
+    'story.status.complete': 'הסתיים',
+    'story.status.inProgress': 'בתהליך',
+    'discover.search.universeConnection': 'חיבור הסיפור ליקב',
+    'story.universeRelation.standalone': 'סיפור בודד',
+    'story.universeRelation.fanFiction': 'פאן פיקשן',
+    'story.universeRelation.universe': 'חלק מיקב',
+    'discover.search.rating': 'דירוג גיל',
+    'discover.search.license': 'רישיון זכויות יוצרים',
+    'discover.search.language': 'שפה',
     'discover.search.resultsTotal.universe': `{results, plural,
-        =0 {No universes match your parameters}
-        one {1 universe found}
-        other {# universes found}
+        =0 {אין יקבים המתאימים לפרמטרים שלך}
+        one {נמצא יקב אחד}
+        other {נמצאו # יקבים}
     }.`,
     'discover.search.resultsTotal.story': `{results, plural,
-        =0 {No stories match your parameters}
-        one {1 story found}
-        other {# stories found}
+        =0 {אין סיפורים המתאימים לפרמטרים שלך}
+        one {נמצא סיפור אחד}
+        other {נמצאו # סיפורים}
     }.`,
-    'discover.viewSelection.aria': 'View selection',
-    'discover.searchDisplay.covers': 'Covers listing',
-    'discover.searchDisplay.list': 'Compact list',
-    'discover.includesAds': 'This work has product placement or includes other form of advertisements in it.',
-    'discovery.featured.universes': 'Featured universes',
-    'discovery.featured.stories': 'Featured stories',
-    'discovery.story.tableOfContent': 'Table of content',
-    'story.status.hiatus': 'On hiatus',
-    'story.status.cancelled': 'Cancelled',
+    'discover.viewSelection.aria': 'בחירת תצוגה',
+    'discover.searchDisplay.covers': 'עטיפות רשימה',
+    'discover.searchDisplay.list': 'רשימה קומפקטית',
+    'discover.includesAds': '!0! עובדות המקומות הראשיים',
+    'discovery.featured.universes': '!0! סיפורים שפורסמו',
+    'discovery.featured.stories': '!0! שולי השמש',
+    'discovery.story.tableOfContent': 'מחולקים לפרקים',
+    'story.status.hiatus': 'בהשהייה',
+    'story.status.cancelled': 'לא נמשך',
     'story.chapter.wasRead': 'Read - {readAt, date}',
-    'discovery.moreDetails': 'More details',
-    'discovery.read': 'Read',
-    'discovery.chapters.showAll': 'Show all chapters',
-    'discovery.chapters.showMore': 'Show more chapters',
-    'discovery.chaptersTotal': 'Total number of chapters in the story'
+    'discovery.moreDetails': 'עוד פרטים',
+    'discovery.read': 'נקרא',
+    'discovery.chapters.showAll': 'הצג את כל הפרקים',
+    'discovery.chapters.showMore': 'הצג עוד פרקים',
+    'discovery.chaptersTotal': 'סך הכל מספר הפרקים בסיפור'
 };
