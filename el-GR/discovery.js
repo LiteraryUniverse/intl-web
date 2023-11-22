024d435340cdbc839ce56d67a4eb3737da3a32b9
Jan Dvorak <hello@jandvorak.me>
2023-11-22 19:24:21 +0100

    New translations discovery.js (Greek)

--- el-GR/discovery.js
+++ el-GR/discovery.js
@@ -1,12 +1,12 @@
 export default {
-    'discovery.meta.desc': 'Discover new stories and universes.',
-    'discovery.universes.new': 'Emerging universes',
-    'discovery.stories.new': 'New stories',
-    'discovery.stories.standalone.new': 'New standalone stories',
-    'discovery.stories.updated': 'Recently updated stories',
-    'discovery.continue': 'Continue reading',
-    'discovery.stories.similar.new': 'New stories in the neighborhood',
-    'discovery.stories.similar.regular': 'Stories in the neighborhood',
+    'discovery.meta.desc': 'Ανακαλύψτε νέες ιστορίες και σύμπαντα.',
+    'discovery.universes.new': 'Αναδυόμενα σύμπαντα',
+    'discovery.stories.new': 'Νέες ιστορίες',
+    'discovery.stories.standalone.new': 'Νέες αυτόνομες ιστορίες',
+    'discovery.stories.updated': 'Πρόσφατα ανανεωμένες ιστορίες',
+    'discovery.continue': 'Συνεχίστε την ανάγνωση',
+    'discovery.stories.similar.new': 'Νέες ιστορίες στη γειτονιά',
+    'discovery.stories.similar.regular': 'Ιστορίες στη γειτονιά',
     'story.estimatedReadingTime': `This story should take {hours, plural,
         =0 {}
         one {1 hour and }
@@ -17,44 +17,44 @@ export default {
         other {# minutes}
     } to read.`,
     'story.estimatedReadingTime.explained': 'Based on an average reading speed of {avgReadingNum, number} words per minute.',
-    'discover.search.text': 'Search term',
-    'discover.searchStory.title': 'Search for a story',
-    'discover.searchStory.description': 'Search for a stories',
-    'discover.searchUniverse.title': 'Search for a universe',
-    'discover.searchUniverse.description': 'Search for a universes',
-    'discover.search.storyStatus': 'Story progress status',
-    'story.status.complete': 'Complete',
-    'story.status.inProgress': 'In-progress',
-    'discover.search.universeConnection': 'Connection of story to universe',
-    'story.universeRelation.standalone': 'Standalone story',
-    'story.universeRelation.fanFiction': 'Fan Fiction',
-    'story.universeRelation.universe': 'Part of a universe',
-    'discover.search.rating': 'Age rating',
-    'discover.search.license': 'Copyright license',
-    'discover.search.language': 'Language',
+    'discover.search.text': 'Όρος αναζήτησης',
+    'discover.searchStory.title': 'Αναζητήστε μια ιστορία',
+    'discover.searchStory.description': 'Αναζητήστε ιστορίες',
+    'discover.searchUniverse.title': 'Αναζητήστε ένα σύμπαν',
+    'discover.searchUniverse.description': 'Αναζητήστε σύμπαντα',
+    'discover.search.storyStatus': 'Κατάσταση προόδου ιστορίας',
+    'story.status.complete': 'Ολοκληρωμένη',
+    'story.status.inProgress': 'Σε εξέλιξη',
+    'discover.search.universeConnection': 'Σύνδεση ιστορίας με σύμπαν',
+    'story.universeRelation.standalone': 'Αυτόνομη ιστορία',
+    'story.universeRelation.fanFiction': 'Φαντασία οπαδών',
+    'story.universeRelation.universe': 'Μέρος ενός σύμπαντος',
+    'discover.search.rating': 'Βαθμολογία ηλικίας',
+    'discover.search.license': 'Άδεια πνευματικών δικαιωμάτων',
+    'discover.search.language': 'Γλώσσα',
     'discover.search.resultsTotal.universe': `{results, plural,
-        =0 {No universes match your parameters}
-        one {1 universe found}
-        other {# universes found}
+        =0 {Δεν υπάρχουν σύμπαντα που να ταιριάζουν με τα παράμετρα σας}
+        one {1 σύμπαν βρέθηκε}
+        other {# σύμπαντα βρέθηκαν}
     }.`,
     'discover.search.resultsTotal.story': `{results, plural,
-        =0 {No stories match your parameters}
-        one {1 story found}
-        other {# stories found}
+        =0 {Δεν υπάρχουν ιστορίες που να ταιριάζουν με τα παράμετρα σας}
+        one {1 ιστορία βρέθηκε}
+        other {# ιστορίες βρέθηκαν}
     }.`,
-    'discover.viewSelection.aria': 'View selection',
-    'discover.searchDisplay.covers': 'Covers listing',
-    'discover.searchDisplay.list': 'Compact list',
-    'discover.includesAds': 'This work has product placement or includes other form of advertisements in it.',
-    'discovery.featured.universes': 'Featured universes',
-    'discovery.featured.stories': 'Featured stories',
-    'discovery.story.tableOfContent': 'Table of content',
-    'story.status.hiatus': 'On hiatus',
-    'story.status.cancelled': 'Cancelled',
+    'discover.viewSelection.aria': 'Επιλογή προβολής',
+    'discover.searchDisplay.covers': 'Κατάλογος εξωφύλλων',
+    'discover.searchDisplay.list': 'Συμπυκνωμένη λίστα',
+    'discover.includesAds': 'Αυτό το έργο περιλαμβάνει προϊόντα ή περιλαμβάνει άλλου είδους διαφημίσεις.',
+    'discovery.featured.universes': 'Επιλεγμένοι κόσμοι',
+    'discovery.featured.stories': 'Επιλεγμένες ιστορίες',
+    'discovery.story.tableOfContent': 'Πίνακας περιεχομένων',
+    'story.status.hiatus': 'Σε αναστολή',
+    'story.status.cancelled': 'Ακυρωμένο',
     'story.chapter.wasRead': 'Read - {readAt, date}',
-    'discovery.moreDetails': 'More details',
-    'discovery.read': 'Read',
-    'discovery.chapters.showAll': 'Show all chapters',
-    'discovery.chapters.showMore': 'Show more chapters',
-    'discovery.chaptersTotal': 'Total number of chapters in the story'
+    'discovery.moreDetails': 'Περισσότερες λεπτομέρειες',
+    'discovery.read': 'Διάβασμα',
+    'discovery.chapters.showAll': 'Εμφάνιση όλων των κεφαλαίων',
+    'discovery.chapters.showMore': 'Εμφάνιση περισσότερων κεφαλαίων',
+    'discovery.chaptersTotal': 'Συνολικός αριθμός κεφαλαίων στην ιστορία'
 };
