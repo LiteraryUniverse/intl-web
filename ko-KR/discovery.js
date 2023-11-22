6a58f4d0ba001965e349a8995d02acdbb681bac8
Jan Dvorak <hello@jandvorak.me>
2023-11-22 19:26:56 +0100

    New translations discovery.js (Korean)

--- ko-KR/discovery.js
+++ ko-KR/discovery.js
@@ -7,21 +7,21 @@ export default {
     'discovery.continue': '계속 읽기',
     'discovery.stories.similar.new': '이웃의 새로운 이야기',
     'discovery.stories.similar.regular': '이웃의 이야기',
-    'story.estimatedReadingTime': `This story should take {hours, plural,
+    'story.estimatedReadingTime': `이 이야기는 읽는 데 {hours, plural,
         =0 {}
-        one {1 hour and }
-        other {# hours and }
+        one {1시간과 }
+        other {#시간 }
     }{minutes, plural,
-        =0 {0 minutes}
-        one {1 minute}
-        other {# minutes}
-    } to read.`,
+        =0 {0분}
+        one {1분}
+        other {#분}
+    } 읽는 데 걸립니다.`,
     'story.estimatedReadingTime.explained': '분당 평균 읽기 속도 {avgReadingNum, number} 단어 기준.',
     'discover.search.text': '검색어',
     'discover.searchStory.title': '스토리 검색',
     'discover.searchStory.description': '스토리 검색',
     'discover.searchUniverse.title': '유니버스 검색',
-    'discover.searchUniverse.description': 'Search for a universes',
+    'discover.searchUniverse.description': '우주를 탐색하세요',
     'discover.search.storyStatus': '스토리 진행 상태',
     'story.status.complete': '완료',
     'story.status.inProgress': '진행 중',
@@ -33,14 +33,14 @@ export default {
     'discover.search.license': '저작권 라이선스',
     'discover.search.language': '언어',
     'discover.search.resultsTotal.universe': `{results, plural,
-        =0 {No universes match your parameters}
-        one {1 universe found}
-        other {# universes found}
+        =0 {매개변수와 일치하는 우주가 없습니다}
+        one {1개의 우주가 발견되었습니다}
+        other {#개의 우주가 발견되었습니다}
     }.`,
     'discover.search.resultsTotal.story': `{results, plural,
-        =0 {No stories match your parameters}
-        one {1 story found}
-        other {# stories found}
+        =0 {매개변수와 일치하는 이야기가 없습니다}
+        one {1개의 이야기가 발견되었습니다}
+        other {#개의 이야기가 발견되었습니다}
     }.`,
     'discover.viewSelection.aria': '선택 보기',
     'discover.searchDisplay.covers': '커버 목록',
@@ -48,13 +48,13 @@ export default {
     'discover.includesAds': '이 작품에는 제품 배치가 있거나 다른 형태의 광고가 포함되어 있습니다.',
     'discovery.featured.universes': '주요 유니버스',
     'discovery.featured.stories': '추천 스토리',
-    'discovery.story.tableOfContent': 'Table of content',
-    'story.status.hiatus': 'On hiatus',
-    'story.status.cancelled': 'Cancelled',
-    'story.chapter.wasRead': 'Read - {readAt, date}',
-    'discovery.moreDetails': 'More details',
-    'discovery.read': 'Read',
-    'discovery.chapters.showAll': 'Show all chapters',
-    'discovery.chapters.showMore': 'Show more chapters',
-    'discovery.chaptersTotal': 'Total number of chapters in the story'
+    'discovery.story.tableOfContent': '목차',
+    'story.status.hiatus': '휴지 상태',
+    'story.status.cancelled': '취소됨',
+    'story.chapter.wasRead': '읽음 - {readAt, date}',
+    'discovery.moreDetails': '추가 세부 정보',
+    'discovery.read': '읽기',
+    'discovery.chapters.showAll': '모든 챕터 보기',
+    'discovery.chapters.showMore': '더 많은 챕터 보기',
+    'discovery.chaptersTotal': '이 이야기의 총 챕터 수'
 };
