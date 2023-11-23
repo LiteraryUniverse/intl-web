09df11df07104cabc9c39525fb74c30da8d7ab44
Jan Dvorak <hello@jandvorak.me>
2023-11-23 20:25:03 +0100

    New translations discovery.js (Tamil)

--- ta-IN/discovery.js
+++ ta-IN/discovery.js
@@ -1,12 +1,12 @@
 export default {
-    'discovery.meta.desc': 'Discover new stories and universes.',
-    'discovery.universes.new': 'Emerging universes',
-    'discovery.stories.new': 'New stories',
-    'discovery.stories.standalone.new': 'New standalone stories',
-    'discovery.stories.updated': 'Recently updated stories',
-    'discovery.continue': 'Continue reading',
-    'discovery.stories.similar.new': 'New stories in the neighborhood',
-    'discovery.stories.similar.regular': 'Stories in the neighborhood',
+    'discovery.meta.desc': 'புதிய கதைகள் மற்றும் அமைப்புகளைக் கண்டறியவும்.',
+    'discovery.universes.new': 'மூலமெலிந்த அமைப்புகள்',
+    'discovery.stories.new': 'புதிய கதைகள்',
+    'discovery.stories.standalone.new': 'புதிய ஒற்றுமைகள் உள்ள கதைகள்',
+    'discovery.stories.updated': 'சமீபத்தியதானவற்றிலுள்ள கதைகள்',
+    'discovery.continue': 'படிக்க தொடரவும்',
+    'discovery.stories.similar.new': 'அருகிலுள்ள புதிய கதைகள்',
+    'discovery.stories.similar.regular': 'அருகிலுள்ள கதைகள்',
     'story.estimatedReadingTime': `இந்த கதைக்கு {hours, plural,
         =0 {}
         one {1 மணி நேரம் மற்றும் }
@@ -17,34 +17,34 @@ export default {
         other {# நிமிடம்}
     } படிக்க`,
     'story.estimatedReadingTime.explained': 'மிகவும் சரியான படிக்கும் வேகத்தின் மூலம் உள்ள பெரும்பாலும் படித்த அல்லது படிக்க முடியேன்கள் {avgReadingNum, number} வார்த்தைகள் பேர் நிமிடத்திற்க்கு பிறகு.',
-    'discover.search.text': 'Search term',
-    'discover.searchStory.title': 'Search for a story',
-    'discover.searchStory.description': 'Search for a stories',
-    'discover.searchUniverse.title': 'Search for a universe',
-    'discover.searchUniverse.description': 'Search for a universes',
-    'discover.search.storyStatus': 'Story progress status',
-    'story.status.complete': 'Complete',
-    'story.status.inProgress': 'In-progress',
-    'discover.search.universeConnection': 'Connection of story to universe',
-    'story.universeRelation.standalone': 'Standalone story',
-    'story.universeRelation.fanFiction': 'Fan Fiction',
-    'story.universeRelation.universe': 'Part of a universe',
-    'discover.search.rating': 'Age rating',
-    'discover.search.license': 'Copyright license',
-    'discover.search.language': 'Language',
+    'discover.search.text': 'தேடுப் பெயர்',
+    'discover.searchStory.title': 'ஒரு கதையை தேடுங்கள்',
+    'discover.searchStory.description': 'பதிவேடு பதிவீடுகளை மெனக்கல் செய்யுங்கள்',
+    'discover.searchUniverse.title': 'ஒரு உலகத்தை தேடுங்கள்',
+    'discover.searchUniverse.description': 'உலகத்தைக் கண்டறியுங்கள்',
+    'discover.search.storyStatus': 'கதை முடிவு நிலை',
+    'story.status.complete': 'முழுமையான',
+    'story.status.inProgress': 'முழுமையில் இருக்கின்றது',
+    'discover.search.universeConnection': 'கதைக்கு உலகம் இணைக்கப்படுகிறது',
+    'story.universeRelation.standalone': 'ஒற்றுமையான கதை',
+    'story.universeRelation.fanFiction': 'பேனான்றப்பு நூல்',
+    'story.universeRelation.universe': 'ஒரு உலகத்தின் பகுதி',
+    'discover.search.rating': 'வயது மதிப்பு',
+    'discover.search.license': 'பதிப்புரிமை உரிமை',
+    'discover.search.language': 'மொழி',
     'discover.search.resultsTotal.universe': `{results, plural,
-        =0 {No universes match your parameters}
-        one {1 universe found}
-        other {# universes found}
+        =0 {உங்கள் பாரம்பரியங்களுக்குப் பொருந்துவதுமில்லை}
+        one {1 உலகம் கிடைக்கும்}
+        other {# உலகங்கள் கிடைக்கும்}
     }.`,
     'discover.search.resultsTotal.story': `{results, plural,
-        =0 {No stories match your parameters}
-        one {1 story found}
-        other {# stories found}
+        =0 {உங்கள் பாரம்பரியங்களுக்குப் பொருந்துவதுமில்லை}
+        one {1 கதை கிடைக்கும்}
+        other {# கதைகள் கிடைக்கும்}
     }.`,
-    'discover.viewSelection.aria': 'View selection',
-    'discover.searchDisplay.covers': 'Covers listing',
-    'discover.searchDisplay.list': 'Compact list',
+    'discover.viewSelection.aria': 'காண்க தேர்வு',
+    'discover.searchDisplay.covers': 'முன்னாள் பட்டியல்',
+    'discover.searchDisplay.list': 'சுருக்கமான பட்டியல்',
     'discover.includesAds': 'இந்த பணியம் பொறுப்பு அல்லது பிரசாரப் பகுதிகளைக் கொண்டுள்ளது.',
     'discovery.featured.universes': 'புதிய உலகில் உள்ள கதைகள்',
     'discovery.featured.stories': 'புதிய கதைகள்',
@@ -56,5 +56,7 @@ export default {
     'discovery.read': 'படிக்க',
     'discovery.chapters.showAll': 'அனைத்து அத்தியாயங்களையும் காட்டு',
     'discovery.chapters.showMore': 'மேலும் அத்தியாயங்களைக் காட்டு',
-    'discovery.chaptersTotal': 'கதையின் மொத்த அத்தியாயங்கள் எண்ணிக்கை'
+    'discovery.chaptersTotal': 'கதையின் மொத்த அத்தியாயங்கள் எண்ணிக்கை',
+    'discovery.readFromStart': 'தொடர்ந்து முதல் அத்தகாத்திரிக்குச் செல்க',
+    'discovery.readLatest': 'சமீபத்திய அத்தகாத்திரம் வாசிக்க'
 };
