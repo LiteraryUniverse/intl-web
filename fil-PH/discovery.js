7b3ed4b04a8f746c159dfc329ef1620209dc344d
Jan Dvorak <hello@jandvorak.me>
2023-11-23 20:27:45 +0100

    New translations discovery.js (Filipino)

--- fil-PH/discovery.js
+++ fil-PH/discovery.js
@@ -1,12 +1,12 @@
 export default {
-    'discovery.meta.desc': 'Discover new stories and universes.',
-    'discovery.universes.new': 'Emerging universes',
-    'discovery.stories.new': 'New stories',
-    'discovery.stories.standalone.new': 'New standalone stories',
-    'discovery.stories.updated': 'Recently updated stories',
-    'discovery.continue': 'Continue reading',
-    'discovery.stories.similar.new': 'New stories in the neighborhood',
-    'discovery.stories.similar.regular': 'Stories in the neighborhood',
+    'discovery.meta.desc': 'Tuklasin ang mga bagong kwento at mga palapagang mundo.',
+    'discovery.universes.new': 'Paglitaw ng mga palapagang mundo',
+    'discovery.stories.new': 'Mga bagong kwento',
+    'discovery.stories.standalone.new': 'Mga bagong kuwentong mag-isa',
+    'discovery.stories.updated': 'Kamakailang na-update na mga kuwento',
+    'discovery.continue': 'Magpatuloy sa pagbabasa',
+    'discovery.stories.similar.new': 'Mga bagong kuwento sa kapitbahayan',
+    'discovery.stories.similar.regular': 'Mga kwento sa kapitbahayan',
     'story.estimatedReadingTime': `This story should take {hours, plural,
         =0 {}
         one {1 hour and }
@@ -18,33 +18,33 @@ export default {
     } to read.`,
     'story.estimatedReadingTime.explained': 'Based on an average reading speed of {avgReadingNum, number} words per minute.',
     'discover.search.text': 'Search term',
-    'discover.searchStory.title': 'Search for a story',
-    'discover.searchStory.description': 'Search for a stories',
-    'discover.searchUniverse.title': 'Search for a universe',
-    'discover.searchUniverse.description': 'Search for a universes',
-    'discover.search.storyStatus': 'Story progress status',
-    'story.status.complete': 'Complete',
-    'story.status.inProgress': 'In-progress',
-    'discover.search.universeConnection': 'Connection of story to universe',
-    'story.universeRelation.standalone': 'Standalone story',
+    'discover.searchStory.title': 'I-search ang isang kwento',
+    'discover.searchStory.description': 'I-search ang mga kwento',
+    'discover.searchUniverse.title': 'I-search ang isang palapagang mundo',
+    'discover.searchUniverse.description': 'I-search ang mga palapagang mundo',
+    'discover.search.storyStatus': 'Katayuang pag-angat ng kwento',
+    'story.status.complete': 'Kumpleto',
+    'story.status.inProgress': 'Nasa proseso',
+    'discover.search.universeConnection': 'Koneksyon ng kwento sa palapagang mundo',
+    'story.universeRelation.standalone': 'Kwento na mag-isa',
     'story.universeRelation.fanFiction': 'Fan Fiction',
-    'story.universeRelation.universe': 'Part of a universe',
-    'discover.search.rating': 'Age rating',
-    'discover.search.license': 'Copyright license',
-    'discover.search.language': 'Language',
+    'story.universeRelation.universe': 'Bahagi ng isang palapagang mundo',
+    'discover.search.rating': 'Batayang edad',
+    'discover.search.license': 'Lisensya sa karapatan',
+    'discover.search.language': 'Wika',
     'discover.search.resultsTotal.universe': `{results, plural,
-        =0 {No universes match your parameters}
-        one {1 universe found}
-        other {# universes found}
+        =0 {Walang mga palapagang mundo na tugma sa iyong mga parameter}
+        one {Nakita ang 1 palapagang mundo}
+        other {Nakita ang # mga palapagang mundo}
     }.`,
     'discover.search.resultsTotal.story': `{results, plural,
-        =0 {No stories match your parameters}
-        one {1 story found}
-        other {# stories found}
+        =0 {Walang mga kuwentong tugma sa iyong mga parameter}
+        one {Nakita ang 1 kwento}
+        other {Nakita ang # mga kwento}
     }.`,
-    'discover.viewSelection.aria': 'View selection',
-    'discover.searchDisplay.covers': 'Covers listing',
-    'discover.searchDisplay.list': 'Compact list',
+    'discover.viewSelection.aria': 'Tingnan ang pagpipilian',
+    'discover.searchDisplay.covers': 'Listahan ng mga pabalat',
+    'discover.searchDisplay.list': 'Tinipid na listahan',
     'discover.includesAds': 'Mayroong placement ng produkto o iba pang form ng advertisement sa gawaing ito.',
     'discovery.featured.universes': 'Mga uniberso sa tampok',
     'discovery.featured.stories': 'Mga tampok na kuwento',
@@ -56,5 +56,7 @@ export default {
     'discovery.read': 'Basahin',
     'discovery.chapters.showAll': 'Ipakita lahat ng mga kabanata',
     'discovery.chapters.showMore': 'Ipakita ang mas maraming mga kabanata',
-    'discovery.chaptersTotal': 'Kabuuang bilang ng mga kabanata sa kwento'
+    'discovery.chaptersTotal': 'Kabuuang bilang ng mga kabanata sa kwento',
+    'discovery.readFromStart': 'Basahin mula sa unang kabanata',
+    'discovery.readLatest': 'Basahin ang pinakabagong kabanata'
 };
