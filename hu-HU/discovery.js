d5781b4b75ae629f23393d74b3bfc4836e8c3c42
Jan Dvorak <hello@jandvorak.me>
2023-11-23 21:22:24 +0100

    New translations discovery.js (Hungarian)

--- hu-HU/discovery.js
+++ hu-HU/discovery.js
@@ -1,12 +1,12 @@
 export default {
-    'discovery.meta.desc': 'Discover new stories and universes.',
-    'discovery.universes.new': 'Emerging universes',
-    'discovery.stories.new': 'New stories',
-    'discovery.stories.standalone.new': 'New standalone stories',
-    'discovery.stories.updated': 'Recently updated stories',
-    'discovery.continue': 'Continue reading',
-    'discovery.stories.similar.new': 'New stories in the neighborhood',
-    'discovery.stories.similar.regular': 'Stories in the neighborhood',
+    'discovery.meta.desc': 'Fedezz fel új történeteket és univerzumokat.',
+    'discovery.universes.new': 'Feltörekvő univerzumok',
+    'discovery.stories.new': 'Új történetek',
+    'discovery.stories.standalone.new': 'Új önálló történetek',
+    'discovery.stories.updated': 'Frissített történetek',
+    'discovery.continue': 'Olvass tovább',
+    'discovery.stories.similar.new': 'Új történetek a környéken',
+    'discovery.stories.similar.regular': 'Történetek a környéken',
     'story.estimatedReadingTime': `This story should take {hours, plural,
         =0 {}
         one {1 hour and }
@@ -17,46 +17,46 @@ export default {
         other {# minutes}
     } to read.`,
     'story.estimatedReadingTime.explained': 'Based on an average reading speed of {avgReadingNum, number} words per minute.',
-    'discover.search.text': 'Search term',
-    'discover.searchStory.title': 'Search for a story',
-    'discover.searchStory.description': 'Search for a stories',
-    'discover.searchUniverse.title': 'Search for a universe',
-    'discover.searchUniverse.description': 'Search for a universes',
-    'discover.search.storyStatus': 'Story progress status',
-    'story.status.complete': 'Complete',
-    'story.status.inProgress': 'In-progress',
-    'discover.search.universeConnection': 'Connection of story to universe',
-    'story.universeRelation.standalone': 'Standalone story',
+    'discover.search.text': 'Kereső kifejezés',
+    'discover.searchStory.title': 'Keresés egy történetre',
+    'discover.searchStory.description': 'Keresés történetekre',
+    'discover.searchUniverse.title': 'Keresés egy univerzumra',
+    'discover.searchUniverse.description': 'Keresés univerzumokra',
+    'discover.search.storyStatus': 'Történet állapot',
+    'story.status.complete': 'Kész',
+    'story.status.inProgress': 'Folyamatban',
+    'discover.search.universeConnection': 'Történet kapcsolódása univerzumhoz',
+    'story.universeRelation.standalone': 'Önálló történet',
     'story.universeRelation.fanFiction': 'Fan Fiction',
-    'story.universeRelation.universe': 'Part of a universe',
-    'discover.search.rating': 'Age rating',
-    'discover.search.license': 'Copyright license',
-    'discover.search.language': 'Language',
+    'story.universeRelation.universe': 'Univerzum része',
+    'discover.search.rating': 'Korhatár',
+    'discover.search.license': 'Szerzői jogi engedély',
+    'discover.search.language': 'Nyelv',
     'discover.search.resultsTotal.universe': `{results, plural,
-        =0 {No universes match your parameters}
-        one {1 universe found}
-        other {# universes found}
+        =0 {Nincsenek univerzumok a paramétereidnek megfelelően}
+        one {1 univerzum található}
+        other {# univerzum található}
     }.`,
     'discover.search.resultsTotal.story': `{results, plural,
-        =0 {No stories match your parameters}
-        one {1 story found}
-        other {# stories found}
+        =0 {Nincsenek történetek a paramétereidnek megfelelően}
+        one {1 történet található}
+        other {# történet található}
     }.`,
-    'discover.viewSelection.aria': 'View selection',
-    'discover.searchDisplay.covers': 'Covers listing',
-    'discover.searchDisplay.list': 'Compact list',
-    'discover.includesAds': 'This work has product placement or includes other form of advertisements in it.',
-    'discovery.featured.universes': 'Featured universes',
-    'discovery.featured.stories': 'Featured stories',
-    'discovery.story.tableOfContent': 'Table of content',
-    'story.status.hiatus': 'On hiatus',
-    'story.status.cancelled': 'Cancelled',
+    'discover.viewSelection.aria': 'Nézet kiválasztása',
+    'discover.searchDisplay.covers': 'Címlista',
+    'discover.searchDisplay.list': 'Kompakt lista',
+    'discover.includesAds': 'Ez a munka termékhelyet vagy másfajta hirdetést tartalmaz.',
+    'discovery.featured.universes': 'Kiemelt univerzumok',
+    'discovery.featured.stories': 'Kiemelt történetek',
+    'discovery.story.tableOfContent': 'Tartalomjegyzék',
+    'story.status.hiatus': 'Szünetel',
+    'story.status.cancelled': 'Megszüntetve',
     'story.chapter.wasRead': 'Read - {readAt, date}',
-    'discovery.moreDetails': 'More details',
-    'discovery.read': 'Read',
-    'discovery.chapters.showAll': 'Show all chapters',
-    'discovery.chapters.showMore': 'Show more chapters',
-    'discovery.chaptersTotal': 'Total number of chapters in the story',
-    'discovery.readFromStart': 'Read from first chapter',
-    'discovery.readLatest': 'Read the latest chapter'
+    'discovery.moreDetails': 'További részletek',
+    'discovery.read': 'Olvasható',
+    'discovery.chapters.showAll': 'Összes fejezet megjelenítése',
+    'discovery.chapters.showMore': 'További fejezetek megjelenítése',
+    'discovery.chaptersTotal': 'A történet összes fejezete',
+    'discovery.readFromStart': 'Olvasás az első fejezettől',
+    'discovery.readLatest': 'Legfrissebb fejezet olvasása'
 };
