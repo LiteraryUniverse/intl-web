ae8c364e46850e1541726b94b8ec853c986a1854
Jan Dvorak <hello@jandvorak.me>
2023-11-26 23:00:51 +0100

    New translations notifications.js (Tamil)

--- ta-IN/notifications.js
+++ ta-IN/notifications.js
@@ -1,27 +1,27 @@
 // Notifications & flashnews
 export default {
-  'flashnews.create': 'Create a new news flash',
-  'flashnews.startsAt': 'Start displaying this message at',
-  'flashnews.endsAt': 'Stop displaying this message at',
-  'flashnews.newLanguage': 'Select new language to add',
-  'flashnews.onlyDisplayOn': 'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown, even in the default language, in other languages.',
-  'notifications.menu.all': 'All notification',
-  'notifications.menu.following': 'Following',
-  'notifications.menu.workshop': 'Workshop',
-  'notifications.menu.news': 'News & updates',
-  'notifications.menu.markUnread': 'Mark as unread',
-  'notifications.menu.markRead': 'Mark as read',
-  'notifications.menu.delete': 'Delete notification',
+  'flashnews.create': 'புதிய செய்தி துவர்க்கவும்',
+  'flashnews.startsAt': 'இந்த செய்தி முதலில் காட்டியிருக்க தொடங்குவது',
+  'flashnews.endsAt': 'இந்த செய்தி முடிவில் காட்டியிருக்க நிறுத்துவது',
+  'flashnews.newLanguage': 'சேர்க்க புதிய மொழியைத் தேர்வுசெய்க',
+  'flashnews.onlyDisplayOn': 'செய்தி இந்த மொழிகளின் மீது தரப்படும் (ஏனெனவே தரப்படும் எந்த முனையான மொழிகளுக்கும் யாரேனும் தரக்கூடிய மொழிகள் வாக்குக்களாக இருந்தாலும் இந்த இந்த இயல்பில் செய்தி காட்டப்படும் இல்லை)',
+  'notifications.menu.all': 'அனைத்து அறிவிப்புகளும்',
+  'notifications.menu.following': 'பின்னூட்டவும்',
+  'notifications.menu.workshop': 'வலைப் பயிற்சி',
+  'notifications.menu.news': 'செய்திகள் & புதுப்பிப்புகள்',
+  'notifications.menu.markUnread': 'படிக்காதவராக்கு',
+  'notifications.menu.markRead': 'படித்தவராக்கு',
+  'notifications.menu.delete': 'அறிவிப்பை நீக்கு',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus': 'Your story {storyName} was automatically set to hiatus after 6 months of inactivity.',
-  'notification.workshopStoryStatus.toCancelled': 'Your story {storyName} was automatically set to cancelled after one year of inactivity.',
+  'notification.workshopStoryStatus.toHiatus': 'உங்கள் கதை {storyName} நிரப்புப்படுத்தப்பட்டுள்ளது பொறுப்பற்றது என்று தானாக்கம் செய்யப்பட்டது.',
+  'notification.workshopStoryStatus.toCancelled': 'உங்கள் கதை {storyName} நிரப்புப்பட்டுள்ளது ரத்துசெய்யப்பட்டது என்று தானாக்கம் செய்யப்பட்டது.',
   'notification.followingStoryStatus.toHiatus': '{storyName} is on hiatus.',
   'notification.followingStoryStatus.toCancelled': '{storyName} has been cancelled.',
-  'notification.followingNewChapter.new': 'New chapter in {storyName} has been published!',
-  'notification.workshopStory.newComment': 'has commented on {storyName}',
+  'notification.followingNewChapter.new': 'புதிய பதிகை {storyName} வெளியிடப்பட்டுள்ளது!',
+  'notification.workshopStory.newComment': '{storyName}குறிச்சொல்லில்  {storyName} பரப்பப்பட்டார்',
   // Before this text will be the username of the user
-  'notification.friendRequest': 'send you a friend request.',
+  'notification.friendRequest': 'நீங்கள் ஒரு நண்பர் கோரிக்கையை அனுப்பியார்.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Go to story'
+  'notificationExtra.workshopStoryStatus.goToStory': 'கதைக்குச் செல்க'
 };
