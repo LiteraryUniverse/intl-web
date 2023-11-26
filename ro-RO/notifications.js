b5194acf952f15f2c9345a3e8106a5295e50766f
Jan Dvorak <hello@jandvorak.me>
2023-11-26 23:03:26 +0100

    New translations notifications.js (Romanian)

--- ro-RO/notifications.js
+++ ro-RO/notifications.js
@@ -1,27 +1,27 @@
 // Notifications & flashnews
 export default {
-  'flashnews.create': 'Create a new news flash',
-  'flashnews.startsAt': 'Start displaying this message at',
-  'flashnews.endsAt': 'Stop displaying this message at',
-  'flashnews.newLanguage': 'Select new language to add',
-  'flashnews.onlyDisplayOn': 'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown, even in the default language, in other languages.',
-  'notifications.menu.all': 'All notification',
-  'notifications.menu.following': 'Following',
-  'notifications.menu.workshop': 'Workshop',
-  'notifications.menu.news': 'News & updates',
-  'notifications.menu.markUnread': 'Mark as unread',
-  'notifications.menu.markRead': 'Mark as read',
-  'notifications.menu.delete': 'Delete notification',
+  'flashnews.create': 'Creați o notificare nouă',
+  'flashnews.startsAt': 'Începeți afișarea acestui mesaj la',
+  'flashnews.endsAt': 'Opriți afișarea acestui mesaj la',
+  'flashnews.newLanguage': 'Selectați o limbă nouă pentru adăugare',
+  'flashnews.onlyDisplayOn': 'Știrile vor fi afișate doar în limbile selectate mai jos (dacă este selectată o limbă), nu vor fi afișate știri nici măcar în limba implicită, în alte limbi.',
+  'notifications.menu.all': 'Toate notificările',
+  'notifications.menu.following': 'Urmăririle',
+  'notifications.menu.workshop': 'Ateliere',
+  'notifications.menu.news': 'Știri și actualizări',
+  'notifications.menu.markUnread': 'Marchează ca necitită',
+  'notifications.menu.markRead': 'Marchează ca citită',
+  'notifications.menu.delete': 'Șterge notificarea',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus': 'Your story {storyName} was automatically set to hiatus after 6 months of inactivity.',
-  'notification.workshopStoryStatus.toCancelled': 'Your story {storyName} was automatically set to cancelled after one year of inactivity.',
-  'notification.followingStoryStatus.toHiatus': '{storyName} is on hiatus.',
-  'notification.followingStoryStatus.toCancelled': '{storyName} has been cancelled.',
-  'notification.followingNewChapter.new': 'New chapter in {storyName} has been published!',
-  'notification.workshopStory.newComment': 'has commented on {storyName}',
+  'notification.workshopStoryStatus.toHiatus': 'Povestea dvs. {storyName} a fost setată automat pe pauză după 6 luni de inactivitate.',
+  'notification.workshopStoryStatus.toCancelled': 'Povestea dvs. {storyName} a fost setată automat pe anulată după un an de inactivitate.',
+  'notification.followingStoryStatus.toHiatus': '{storyName} este în pauză.',
+  'notification.followingStoryStatus.toCancelled': '{storyName} a fost anulată.',
+  'notification.followingNewChapter.new': 'Capitolul nou în {storyName} a fost publicat!',
+  'notification.workshopStory.newComment': 'a comentat la {storyName}',
   // Before this text will be the username of the user
-  'notification.friendRequest': 'send you a friend request.',
+  'notification.friendRequest': 'ți-a trimis o cerere de prietenie.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Go to story'
+  'notificationExtra.workshopStoryStatus.goToStory': 'Du-te la poveste'
 };
