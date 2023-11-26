10673d59b1a35c1aed3ff1db9ff6b0da485e0fce
Jan Dvorak <hello@jandvorak.me>
2023-11-26 23:01:52 +0100

    New translations notifications.js (Hindi)

--- hi-IN/notifications.js
+++ hi-IN/notifications.js
@@ -1,17 +1,17 @@
 // Notifications & flashnews
 export default {
-  'flashnews.create': 'Create a new news flash',
-  'flashnews.startsAt': 'Start displaying this message at',
-  'flashnews.endsAt': 'Stop displaying this message at',
-  'flashnews.newLanguage': 'Select new language to add',
-  'flashnews.onlyDisplayOn': 'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown, even in the default language, in other languages.',
-  'notifications.menu.all': 'All notification',
-  'notifications.menu.following': 'Following',
-  'notifications.menu.workshop': 'Workshop',
-  'notifications.menu.news': 'News & updates',
-  'notifications.menu.markUnread': 'Mark as unread',
-  'notifications.menu.markRead': 'Mark as read',
-  'notifications.menu.delete': 'Delete notification',
+  'flashnews.create': 'एक नई न्यूज़ फ्लैश बनाएं',
+  'flashnews.startsAt': 'इस संदेश को दिखाना शुरू करें',
+  'flashnews.endsAt': 'इस संदेश को दिखाना बंद करें',
+  'flashnews.newLanguage': 'नयी भाषा का चयन करें',
+  'flashnews.onlyDisplayOn': 'समाचार केवल नीचे चयनित भाषाओं में ही दिखाई जाएगा (यदि कोई भाषा चयनित हो), उन भाषाओं में कोई भी समाचार नहीं दिखाई जाएगा, दूसरी भाषाओं में भी डिफ़ॉल्ट भाषा में नहीं.',
+  'notifications.menu.all': 'सभी सूचनाएँ',
+  'notifications.menu.following': 'फ़ॉलो करना',
+  'notifications.menu.workshop': 'वर्कशॉप',
+  'notifications.menu.news': 'समाचार और अपडेट',
+  'notifications.menu.markUnread': 'अपठित चिह्नित करें',
+  'notifications.menu.markRead': 'पठित चिह्नित करें',
+  'notifications.menu.delete': 'सूचना हटाएं',
   // Notification messages
   'notification.workshopStoryStatus.toHiatus': 'Your story {storyName} was automatically set to hiatus after 6 months of inactivity.',
   'notification.workshopStoryStatus.toCancelled': 'Your story {storyName} was automatically set to cancelled after one year of inactivity.',
@@ -20,8 +20,8 @@ export default {
   'notification.followingNewChapter.new': 'New chapter in {storyName} has been published!',
   'notification.workshopStory.newComment': 'has commented on {storyName}',
   // Before this text will be the username of the user
-  'notification.friendRequest': 'send you a friend request.',
+  'notification.friendRequest': 'आपको एक मित्र अनुरोध भेजा गया है।',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Go to story'
+  'notificationExtra.workshopStoryStatus.goToStory': 'कहानी में जाएँ'
 };
