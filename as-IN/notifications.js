ceaa7fb36acbb83ce28ff067531f9def88e06bb5
Jan Dvorak <hello@jandvorak.me>
2023-11-26 23:06:33 +0100

    New translations notifications.js (Assamese)

--- as-IN/notifications.js
+++ as-IN/notifications.js
@@ -1,27 +1,27 @@
 // Notifications & flashnews
 export default {
-  'flashnews.create': 'Create a new news flash',
-  'flashnews.startsAt': 'Start displaying this message at',
-  'flashnews.endsAt': 'Stop displaying this message at',
-  'flashnews.newLanguage': 'Select new language to add',
-  'flashnews.onlyDisplayOn': 'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown, even in the default language, in other languages.',
-  'notifications.menu.all': 'All notification',
-  'notifications.menu.following': 'Following',
-  'notifications.menu.workshop': 'Workshop',
-  'notifications.menu.news': 'News & updates',
-  'notifications.menu.markUnread': 'Mark as unread',
-  'notifications.menu.markRead': 'Mark as read',
-  'notifications.menu.delete': 'Delete notification',
+  'flashnews.create': 'নতুন নিউজ ফ্লাশ তৈরি কৰক',
+  'flashnews.startsAt': 'এই বার্তা প্ৰদর্শন কৰাত আৰম্ভ কৰক',
+  'flashnews.endsAt': 'বার্তা প্ৰদৰ্শন সৰু কৰক',
+  'flashnews.newLanguage': 'নতুন ভাষা যোগ কৰিবলৈ নিৰ্বাচন কৰক',
+  'flashnews.onlyDisplayOn': 'নির্বাচিত ভাষাত কেৱলমাত্ৰ এই খবৰটো প্ৰদৰ্শিত হয়, বাকী ভাষাতে কোনো খবৰ প্ৰদর্শন কৰা হ\'ব নেই।',
+  'notifications.menu.all': 'সকলো জাননি',
+  'notifications.menu.following': 'অনুসৰণ কৰা হয়.',
+  'notifications.menu.workshop': 'শিল্প শিক্ষাৰ্থী',
+  'notifications.menu.news': 'বার্তা আৰু আপডেটসমূহ',
+  'notifications.menu.markUnread': 'অপঠিত হিচাপে চিহ্নিত কৰক',
+  'notifications.menu.markRead': 'পঠিত হিচাপে চিহ্নিত কৰক',
+  'notifications.menu.delete': 'জাননি মচি পৰিলোঁগিক কৰক',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus': 'Your story {storyName} was automatically set to hiatus after 6 months of inactivity.',
-  'notification.workshopStoryStatus.toCancelled': 'Your story {storyName} was automatically set to cancelled after one year of inactivity.',
-  'notification.followingStoryStatus.toHiatus': '{storyName} is on hiatus.',
-  'notification.followingStoryStatus.toCancelled': '{storyName} has been cancelled.',
-  'notification.followingNewChapter.new': 'New chapter in {storyName} has been published!',
-  'notification.workshopStory.newComment': 'has commented on {storyName}',
+  'notification.workshopStoryStatus.toHiatus': 'আপোনাৰ গল্প {storyName} ব্যৱহাৰ অনকুলতাৰ পিছত শান্তিময় হৈ গছিল।',
+  'notification.workshopStoryStatus.toCancelled': 'আপোনাৰ গল্প {storyName} ব্যৱহাৰ নবাধ্য হৈ গছিল।',
+  'notification.followingStoryStatus.toHiatus': '{storyName} শান্তিময়।',
+  'notification.followingStoryStatus.toCancelled': '{storyName} নিষ্ক্ৰিয় কৰিবলৈ আগতি দিব।',
+  'notification.followingNewChapter.new': 'নতুন চাপ্তা {storyName} প্ৰকাশ হৈছে!',
+  'notification.workshopStory.newComment': '{storyName}-টি উপদেশ কৰিছে।',
   // Before this text will be the username of the user
-  'notification.friendRequest': 'send you a friend request.',
+  'notification.friendRequest': 'তোমাৰ একান্ত অনুৰোধ পাঠিছে।',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Go to story'
+  'notificationExtra.workshopStoryStatus.goToStory': 'গল্পে যান'
 };
