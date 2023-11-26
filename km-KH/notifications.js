01c78ae956ae813d0773aa57c2a93e982e64e5ed
Jan Dvorak <hello@jandvorak.me>
2023-11-26 23:00:40 +0100

    New translations notifications.js (Khmer)

--- km-KH/notifications.js
+++ km-KH/notifications.js
@@ -1,17 +1,17 @@
 // Notifications & flashnews
 export default {
-  'flashnews.create': 'Create a new news flash',
-  'flashnews.startsAt': 'Start displaying this message at',
-  'flashnews.endsAt': 'Stop displaying this message at',
-  'flashnews.newLanguage': 'Select new language to add',
-  'flashnews.onlyDisplayOn': 'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown, even in the default language, in other languages.',
-  'notifications.menu.all': 'All notification',
-  'notifications.menu.following': 'Following',
-  'notifications.menu.workshop': 'Workshop',
-  'notifications.menu.news': 'News & updates',
-  'notifications.menu.markUnread': 'Mark as unread',
-  'notifications.menu.markRead': 'Mark as read',
-  'notifications.menu.delete': 'Delete notification',
+  'flashnews.create': 'បង្កើត​សារ​ពេលពេលថ្ងៃ​ថ្មី',
+  'flashnews.startsAt': 'ចាប់ផ្ដើម​បង្ហាញ​សារ​នេះ​នៅពេល',
+  'flashnews.endsAt': 'ឈប់​បង្ហាញ​សារ​នេះ​នៅពេល',
+  'flashnews.newLanguage': 'ជ្រើស​ភាសា​ថែម​ថ្មី',
+  'flashnews.onlyDisplayOn': 'សារ​នឹង​ត្រូវ​បាន​បង្ហាញ​តែតាម​ភាសា​ដែលបាន​ជ្រើស​នៅខាងក្រោម (ប្រសិនបើ​ជ្រើស​មិនបាន​គ្រាន់​តែ​អ្នក​ជ្រើស) ហើយ​មិនមាន​សារ​បង្ហាញ​បន្ថែម​ទេ ក្នុងភាសា​ដែល​អត់​បាន​ជ្រើស។',
+  'notifications.menu.all': 'សារ​សុពលភាព​ទាំងអស់',
+  'notifications.menu.following': 'ចូលរួម',
+  'notifications.menu.workshop': 'សិទ្ធិ១',
+  'notifications.menu.news': 'ព័ត៌មាន នឹង ធ្វើបច្ចុប្បន្នភាព​ទំនាក់ទំនង',
+  'notifications.menu.markUnread': 'កំណត់ថ្នាក់លើសពីរ។',
+  'notifications.menu.markRead': 'កំណត់ថ្នាក់អានរួច។',
+  'notifications.menu.delete': 'លុប​ការជូនដំណឹង',
   // Notification messages
   'notification.workshopStoryStatus.toHiatus': 'Your story {storyName} was automatically set to hiatus after 6 months of inactivity.',
   'notification.workshopStoryStatus.toCancelled': 'Your story {storyName} was automatically set to cancelled after one year of inactivity.',
@@ -20,8 +20,8 @@ export default {
   'notification.followingNewChapter.new': 'New chapter in {storyName} has been published!',
   'notification.workshopStory.newComment': 'has commented on {storyName}',
   // Before this text will be the username of the user
-  'notification.friendRequest': 'send you a friend request.',
+  'notification.friendRequest': 'អ្នក​បានចែក​រំលែក​អ្នកដឹកនាំ។',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Go to story'
+  'notificationExtra.workshopStoryStatus.goToStory': 'ទៅកាន់រឿង'
 };
