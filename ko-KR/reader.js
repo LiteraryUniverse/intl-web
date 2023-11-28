ebee1d205ecfcb3f0ca03627b7e9554eac1af3d9
Jan Dvorak <hello@jandvorak.me>
2023-11-28 21:42:41 +0100

    New translations reader.js (Korean)

--- ko-KR/reader.js
+++ ko-KR/reader.js
@@ -32,5 +32,8 @@ export default {
     'reader.storyContinues': 'The story continues...',
     'reader.storyOnHiatus': 'The author is currently taking a break from this story and this story is on hiatus for now. Check back in a month or two.',
     'reader.storyCancelled': 'Sadly this is it. The story has been cancelled so it is unlikely there is going to be anything new.',
-    'reader.storyCancelled.heading': '취소됨'
+    'reader.storyCancelled.heading': '취소됨',
+    'reader.nextChapterEstimation.verySoon': 'Based on the last {chaptersCount} chapters publish date the next chapter should be out very soon.',
+    'reader.nextChapterEstimation.days': 'Based on the last {chaptersCount} chapters publish date the next chapter should be out in {daysLeft} days',
+    'reader.nextChapterEstimation.notEnoughChapters': 'We will also provide an estimate of when the next chapter might be published once this story has more than one chapter.'
 };
