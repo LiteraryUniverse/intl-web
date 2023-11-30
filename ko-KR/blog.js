ef3f3d69c30e8bd6b1f87fc6e55bd143bfed0ffe
Jan Dvorak <hello@jandvorak.me>
2023-11-30 16:27:36 +0100

    New translations blog.js (Korean)

--- ko-KR/blog.js
+++ ko-KR/blog.js
@@ -14,26 +14,26 @@ export default {
   } 설정`,
     'blog.description': '블로그 소개',
     'blog.theme': '테마',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+    'blog.theme.info': '테마를 조정하면 페이지의 모양을 변경할 수 있습니다. 이 기능은 현재 사용할 수 없습니다.',
     'blog.posts.total': `There {total, plural,
     zero {are no posts}
     one {is one post}
     other {are # posts}
   } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
+    'blog.post.create': '새 포스트 만들기',
+    'blog.post.update': '편집 {title}',
+    'blog.post.text': '포스트 텍스트',
     'common.slug': 'SEO 친화적인 주소',
     'blog.slug.explained': '멋진 글 URL을 입력합니다. "-"를 제외한 특수 문자를 사용하지 마세요.',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.publicView': '공개 뷰',
+    'blog.post.notfound': '블로그 글을 찾을 수 없습니다',
     'blog.lists.works': `{type, select,
-    universes {Universes}
-    stories {Standalone stories}
-    fanfiction {Fan Fiction work}
+    universes {우주}
+    stories {독립적인 이야기들}
+    fanfiction {팬픽션 작품들}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': '여기에는 독립적인 이야기가 나와 있으며, 이야기가 우주의 일부로 쓰여진 경우에는 우주 페이지에 나열됩니다.',
     'blog.social.sameAsMain': `Social links are the same as set in {type, select,
     organization {organization settings}
     user {user profile}
@@ -41,18 +41,18 @@ export default {
   }`,
     'blog.social.website': '공식 웹사이트',
     'blog.settings.universeSettings': '유니버스 블로그는 유니버스 설정에서 기본 설정을 가져와 유니버스 페이지에 통합됩니다.',
-    'blog.settings.organizationSettings': 'Organization blogs are integrated into organization pages.',
-    'blogs.lu': 'Official Literary Universe blogs',
+    'blog.settings.organizationSettings': '조직 블로그는 조직 페이지에 통합되었습니다.',
+    'blogs.lu': '공식 문학 우주 블로그',
     'blogs.featured': '추천 블로그',
     'blogs.new': '최신 블로그',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
+    'blogs.title': '문학 우주의 블로그',
+    'blogs.description': '문학 우주 플랫폼의 블로그 목록입니다.',
     'blogs.visit': '블로그 방문',
-    'blog.settingsAria': 'Settings categories for the blog',
+    'blog.settingsAria': '블로그 설정 카테고리',
     'blog.settings.blog': '블로그',
     'blog.settings.forum': '포럼',
     'blog.post.introText': '소개 단락',
     'blog.post.writtenBy': '작성자: {usernameDisplay}',
-    'blog.post.writtenByFor': 'Written by {usernameDisplay} for {publicationName}',
-    'blog.posts.recent': 'Recent posts'
+    'blog.post.writtenByFor': '{usernameDisplay} 님이 {publicationName}에 작성하였습니다.',
+    'blog.posts.recent': '최근 글'
 };
