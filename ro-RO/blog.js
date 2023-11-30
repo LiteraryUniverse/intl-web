bd4c0b78bb269a49efc264ce47c3159fb23d329a
Jan Dvorak <hello@jandvorak.me>
2023-11-30 16:30:38 +0100

    New translations blog.js (Romanian)

--- ro-RO/blog.js
+++ ro-RO/blog.js
@@ -5,54 +5,54 @@ export default {
     user {a blog}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
-    'blog.settings': `{type, select,
-    org {Blog}
-    universe {Universe page}
-    user {Blog}
+    'blog.create': 'Creați un blog nou',
+    'blog.settings': `Setări blog {type, select,
+    org {Pagina univers}
+    universe {Setări blog}
+    user {}
     other {}
-  } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+  }`,
+    'blog.description': 'Introducere în blog',
+    'blog.theme': 'Tematică',
+    'blog.theme.info': 'Ajustând tema, veți putea schimba aspectul paginii. Această funcționalitate nu este disponibilă în acest moment.',
     'blog.posts.total': `There {total, plural,
     zero {are no posts}
     one {is one post}
     other {are # posts}
   } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.post.create': 'Creați o postare nouă',
+    'blog.post.update': 'Editarea postării {title}',
+    'blog.post.text': 'Textul postării',
+    'common.slug': 'Adresă amicală pentru SEO',
+    'blog.slug.explained': 'Adresă URL frumoasă pentru postare. Evitați utilizarea de caractere speciale, cu excepția "-".',
+    'blog.publicView': 'Vizualizare publică',
+    'blog.post.notfound': 'Postarea blogului nu a fost găsită',
     'blog.lists.works': `{type, select,
     universes {Universes}
     stories {Standalone stories}
     fanfiction {Fan Fiction work}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': 'Aici sunt prezentate povestiri independente; dacă o poveste a fost scrisă ca parte a unui univers, aceasta va fi prezentată pe pagina universului.',
     'blog.social.sameAsMain': `Social links are the same as set in {type, select,
     organization {organization settings}
     user {user profile}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
-    'blog.settings.organizationSettings': 'Organization blogs are integrated into organization pages.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
-    'blog.settingsAria': 'Settings categories for the blog',
+    'blog.social.website': 'Site web oficial',
+    'blog.settings.universeSettings': 'Blogurile universului sunt integrate în paginile universului cu setările lor de bază preluate din setările universului.',
+    'blog.settings.organizationSettings': 'Blogurile organizației sunt integrate în paginile organizației.',
+    'blogs.lu': 'Bloguri oficiale ale Literary Universe',
+    'blogs.featured': 'Bloguri recomandate',
+    'blogs.new': 'Cele mai noi bloguri',
+    'blogs.title': 'Bloguri ale Literary Universe',
+    'blogs.description': 'Listare a blogurilor pe platforma Literary Universe.',
+    'blogs.visit': 'Vizitați blogul',
+    'blog.settingsAria': 'Categorii de setări pentru blog',
     'blog.settings.blog': 'Blog',
     'blog.settings.forum': 'Forum',
-    'blog.post.introText': 'Introduction paragraph',
+    'blog.post.introText': 'Paragraful de introducere',
     'blog.post.writtenBy': 'Written by {usernameDisplay}',
     'blog.post.writtenByFor': 'Written by {usernameDisplay} for {publicationName}',
-    'blog.posts.recent': 'Recent posts'
+    'blog.posts.recent': 'Postări recente'
 };
