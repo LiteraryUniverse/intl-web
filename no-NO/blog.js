3b5594e6e0e8c09e12e47c493d727ac89acdcc09
Jan Dvorak <hello@jandvorak.me>
2023-11-30 16:20:07 +0100

    New translations blog.js (Norwegian)

--- no-NO/blog.js
+++ no-NO/blog.js
@@ -1,58 +1,58 @@
 export default {
-    'blog.noneMsg': `You currently don't have {type, select,
-    organization {any blog}
-    universe {a blog for your universe}
-    user {a blog}
+    'blog.noneMsg': `Du har for øyeblikket ingen {type, select,
+    organization {noen blogg}
+    universe {en blogg for universet ditt}
+    user {en blogg}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
+    'blog.create': 'Opprett en ny blogg',
     'blog.settings': `{type, select,
-    org {Blog}
-    universe {Universe page}
-    user {Blog}
+    org {Blogg}
+    universe {Univers side}
+    user {Blogg}
     other {}
-  } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
-    'blog.posts.total': `There {total, plural,
-    zero {are no posts}
-    one {is one post}
-    other {are # posts}
-  } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+  } innstillinger`,
+    'blog.description': 'Introduksjon av blogg',
+    'blog.theme': 'Tema',
+    'blog.theme.info': 'Ved å justere temaet vil du kunne endre utseendet på siden. Denne funksjonen er ikke klar i øyeblikket.',
+    'blog.posts.total': `Det er {total, plural,
+    zero {ingen innlegg}
+    one {er ett innlegg}
+    other {er # innlegg}
+  } i denne bloggen.`,
+    'blog.post.create': 'Opprett et nytt innlegg',
+    'blog.post.update': 'Redigerer {title}',
+    'blog.post.text': 'Innlegg tekst',
+    'common.slug': 'SEO vennlig adresse',
+    'blog.slug.explained': 'Pen URL-adresse for innlegget. Unngå å bruke spesialtegn unntatt "-".',
+    'blog.publicView': 'Offentlig visning',
+    'blog.post.notfound': 'Blogginnlegget ble ikke funnet',
     'blog.lists.works': `{type, select,
-    universes {Universes}
-    stories {Standalone stories}
-    fanfiction {Fan Fiction work}
+    universes {Universer}
+    stories {Selvstendige historier}
+    fanfiction {Fanfiction-arbeid}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
-    'blog.social.sameAsMain': `Social links are the same as set in {type, select,
-    organization {organization settings}
-    user {user profile}
+    'blog.lists.stories.disclaimer': 'Her er frittstående fortellinger listet, hvis fortellingen ble skrevet som en del av et univers, vil den bli oppført på universets side.',
+    'blog.social.sameAsMain': `Sosiale lenker er de samme som er satt i {type, select,
+    organization {organisasjonsinnstillinger}
+    user {brukerprofil}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
-    'blog.settings.organizationSettings': 'Organization blogs are integrated into organization pages.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
-    'blog.settingsAria': 'Settings categories for the blog',
+    'blog.social.website': 'Offisiell nettside',
+    'blog.settings.universeSettings': 'Universe blogger er integrert i universe sider med deres grunnleggende innstillinger tatt fra universe innstillinger.',
+    'blog.settings.organizationSettings': 'Organisasjonsblogger er integrert i organisasjonssider.',
+    'blogs.lu': 'Offisielle Litteraturunivers blogger',
+    'blogs.featured': 'Utvalgte blogger',
+    'blogs.new': 'Nyeste blogger',
+    'blogs.title': 'Blogger av Litteraturuniverset',
+    'blogs.description': 'Oversikt over blogger på Litteraturunivers plattformen.',
+    'blogs.visit': 'Besøk bloggen',
+    'blog.settingsAria': 'Innstillingskategorier for bloggen',
     'blog.settings.blog': 'Blogg',
     'blog.settings.forum': 'Forum',
-    'blog.post.introText': 'Introduction paragraph',
-    'blog.post.writtenBy': 'Written by {usernameDisplay}',
-    'blog.post.writtenByFor': 'Written by {usernameDisplay} for {publicationName}',
-    'blog.posts.recent': 'Recent posts'
+    'blog.post.introText': 'Introduksjonsavsnitt',
+    'blog.post.writtenBy': 'Skrevet av {usernameDisplay}',
+    'blog.post.writtenByFor': 'Skrevet av {usernameDisplay} for {publicationName}',
+    'blog.posts.recent': 'Nylige innlegg'
 };
