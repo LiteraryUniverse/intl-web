e853ffd269aa1f5572d35dccf505ca9b3725f2cb
Jan Dvorak <hello@jandvorak.me>
2023-11-30 14:43:32 +0100

    New translations blog.js (Greek)

--- el-GR/blog.js
+++ el-GR/blog.js
@@ -1,57 +1,58 @@
 export default {
-    'blog.noneMsg': `You currently don't have {type, select,
-    organization {any blog}
-    universe {a blog for your universe}
-    user {a blog}
+    'blog.noneMsg': `Δεν έχετε αυτήν τη στιγμή {type, select,
+    organization {κανένα blog}
+    universe {ένα blog για το σύμπαν σας}
+    user {ένα blog}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
+    'blog.create': 'Δημιουργία νέου blog',
     'blog.settings': `{type, select,
     org {Blog}
-    universe {Universe page}
+    universe {Σελίδα Συμπαντος}
     user {Blog}
     other {}
-  } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
-    'blog.posts.total': `There {total, plural,
-    zero {are no posts}
-    one {is one post}
-    other {are # posts}
-  } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+  } ρυθμίσεις`,
+    'blog.description': 'Εισαγωγή blog',
+    'blog.theme': 'Θέμα',
+    'blog.theme.info': 'Ρυθμίζοντας το θέμα θα μπορείτε να αλλάξετε την εμφάνιση της σελίδας. Αυτή η λειτουργία δεν είναι έτοιμη αυτήν τη στιγμή.',
+    'blog.posts.total': `{total, plural,
+    zero {Δεν υπάρχουν αναρτήσεις}
+    one {μια ανάρτηση}
+    other {# αναρτήσεις}
+  } σε αυτό το blog.`,
+    'blog.post.create': 'Δημιουργία νέας ανάρτησης',
+    'blog.post.update': 'Επεξεργασία {title}',
+    'blog.post.text': 'Κείμενο ανάρτησης',
+    'common.slug': 'Φιλική προς το SEO διεύθυνση',
+    'blog.slug.explained': 'Ένα url που δίνει ωραία εμφάνιση στην ανάρτηση. Αποφύγετε τη χρήση ειδικών χαρακτήρων εκτός από "-".',
+    'blog.publicView': 'Δημόσια προβολή',
+    'blog.post.notfound': 'Η ανάρτηση blog δεν βρέθηκε',
     'blog.lists.works': `{type, select,
-    universes {Universes}
-    stories {Standalone stories}
-    fanfiction {Fan Fiction work}
+    universes {Σύμπαντα}
+    stories {Αυτόνομες ιστορίες}
+    fanfiction {Έργα fan fiction}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': 'Εδώ παρατίθενται ανεξάρτητες ιστορίες. Εάν η ιστορία έχει γραφεί ως μέρος ενός σύμπαντος, τότε θα παρατίθεται στη σελίδα του σύμπαντος.',
     'blog.social.sameAsMain': `Social links are the same as set in {type, select,
     organization {organization settings}
     user {user profile}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
-    'blog.settings.organizationSettings': 'Organization blogs are integrated into organization pages.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
-    'blog.settingsAria': 'Settings categories for the blog',
-    'blog.settings.blog': 'Blog',
-    'blog.settings.forum': 'Forum',
-    'blog.post.introText': 'Introduction paragraph',
-    'blog.post.writtenBy': 'Written by {usernameDisplay}',
-    'blog.post.writtenByFor': 'Written by {usernameDisplay} for {publicationName}'
+    'blog.social.website': 'Επίσημη ιστοσελίδα',
+    'blog.settings.universeSettings': 'Τα blogs των συμπαντικών ιστοσελίδων ενσωματώνονται στις σελίδες συμπαντικών ιστοσελίδων με τις βασικές τους ρυθμίσεις να προέρχονται από τις ρυθμίσεις του σύμπαντος.',
+    'blog.settings.organizationSettings': 'Τα blogs του οργανισμού ενσωματώνονται στις σελίδες του οργανισμού.',
+    'blogs.lu': 'Επίσημα blogs Λογοτεχνικού Συμπαντος',
+    'blogs.featured': 'Προτεινόμενα blogs',
+    'blogs.new': 'Νεότερα blogs',
+    'blogs.title': 'Blogs του Λογοτεχνικού Συμπαντος',
+    'blogs.description': 'Κατάλογος blogs στην πλατφόρμα του Λογοτεχνικού Συμπαντος.',
+    'blogs.visit': 'Επίσκεψη στο blog',
+    'blog.settingsAria': 'Κατηγορίες ρυθμίσεων για το blog',
+    'blog.settings.blog': 'Ιστολόγιο',
+    'blog.settings.forum': 'Φόρουμ',
+    'blog.post.introText': 'Εισαγωγικό παράγραφο',
+    'blog.post.writtenBy': 'Γραμμένο από τον {usernameDisplay}',
+    'blog.post.writtenByFor': 'Γραμμένο από τον {usernameDisplay} για τον {publicationName}',
+    'blog.posts.recent': 'Πρόσφατες αναρτήσεις'
 };
