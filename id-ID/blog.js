86bee9ceef92e9bc5aedcb9ee93c98a586c2e661
Jan Dvorak <hello@jandvorak.me>
2023-11-30 16:22:51 +0100

    New translations blog.js (Indonesian)

--- id-ID/blog.js
+++ id-ID/blog.js
@@ -5,54 +5,54 @@ export default {
     user {a blog}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
+    'blog.create': 'Buat blog baru',
     'blog.settings': `{type, select,
     org {Blog}
-    universe {Universe page}
+    universe {Halaman alam semesta}
     user {Blog}
     other {}
-  } settings`,
-    'blog.description': 'Blog introduction',
+  } pengaturan`,
+    'blog.description': 'Pengenalan blog',
     'blog.theme': 'Tema',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
-    'blog.posts.total': `There {total, plural,
-    zero {are no posts}
-    one {is one post}
-    other {are # posts}
-  } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.theme.info': 'Dengan mengubah tema, Anda akan dapat mengubah tampilan halaman. Fitur ini belum siap saat ini.',
+    'blog.posts.total': `Ada {total, plural,
+    zero {postingan}
+    one {ada satu postingan}
+    other {ada # postingan}
+  } dalam blog ini.`,
+    'blog.post.create': 'Buat postingan baru',
+    'blog.post.update': 'Mengedit {title}',
+    'blog.post.text': 'Teks postingan',
+    'common.slug': 'Alamat yang SEO friendly',
+    'blog.slug.explained': 'URL yang terlihat bagus untuk postingan. Hindari menggunakan karakter khusus kecuali "-".',
+    'blog.publicView': 'Tampilan publik',
+    'blog.post.notfound': 'Postingan blog tidak ditemukan',
     'blog.lists.works': `{type, select,
-    universes {Universes}
-    stories {Standalone stories}
-    fanfiction {Fan Fiction work}
+    universes {Alam semesta}
+    stories {Cerita mandiri}
+    fanfiction {Karya Fan Fiction}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': 'Berikut ini adalah cerita mandiri, jika cerita ditulis sebagai bagian dari alam semesta, maka akan terdaftar di halaman alam semesta.',
     'blog.social.sameAsMain': `Social links are the same as set in {type, select,
     organization {organization settings}
     user {user profile}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
-    'blog.settings.organizationSettings': 'Organization blogs are integrated into organization pages.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
-    'blog.settingsAria': 'Settings categories for the blog',
+    'blog.social.website': 'Situs web resmi',
+    'blog.settings.universeSettings': 'Blog alam semesta terintegrasi ke dalam halaman alam semesta dengan pengaturan dasar dari pengaturan alam semesta.',
+    'blog.settings.organizationSettings': 'Blog organisasi terintegrasi ke dalam halaman organisasi.',
+    'blogs.lu': 'Blog resmi Literary Universe',
+    'blogs.featured': 'Blog terpilih',
+    'blogs.new': 'Blog terbaru',
+    'blogs.title': 'Blog-blogger di Literary Universe',
+    'blogs.description': 'Daftar blog di platform Literary Universe.',
+    'blogs.visit': 'Kunjungi blog',
+    'blog.settingsAria': 'Kategori pengaturan untuk blog',
     'blog.settings.blog': 'Blog',
     'blog.settings.forum': 'Forum',
-    'blog.post.introText': 'Introduction paragraph',
+    'blog.post.introText': 'Paragraf pengantar',
     'blog.post.writtenBy': 'Written by {usernameDisplay}',
     'blog.post.writtenByFor': 'Written by {usernameDisplay} for {publicationName}',
-    'blog.posts.recent': 'Recent posts'
+    'blog.posts.recent': 'Posting terbaru'
 };
