96544f08205887c492fb6518d2756a125b75f676
Jan Dvorak <hello@jandvorak.me>
2023-11-30 14:31:58 +0100

    New translations blog.js (Vietnamese)

--- vi-VN/blog.js
+++ vi-VN/blog.js
@@ -53,5 +53,6 @@ export default {
     'blog.settings.forum': 'Forum',
     'blog.post.introText': 'Introduction paragraph',
     'blog.post.writtenBy': 'Written by {usernameDisplay}',
-    'blog.post.writtenByFor': 'Written by {usernameDisplay} for {publicationName}'
+    'blog.post.writtenByFor': 'Written by {usernameDisplay} for {publicationName}',
+    'blog.posts.recent': 'Recent posts'
 };
