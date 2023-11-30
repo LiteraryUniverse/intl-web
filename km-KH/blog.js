d69e7150f0df027484d548ea5f6fffd4555161dc
Jan Dvorak <hello@jandvorak.me>
2023-11-30 16:23:05 +0100

    New translations blog.js (Khmer)

--- km-KH/blog.js
+++ km-KH/blog.js
@@ -1,58 +1,58 @@
 export default {
-    'blog.noneMsg': `You currently don't have {type, select,
-    organization {any blog}
-    universe {a blog for your universe}
-    user {a blog}
+    'blog.noneMsg': `លទ្ធផលនេះមិនអាចមានក្នុង {type, select,
+    organization {ប្រភេទណាមួយ}
+    universe {ប្លុកសម្ងាត់របស់អ្នកអះអាងប៉ុណ្ណោះទេ}
+    user {ប្លុកសម្ងាត់}
     other {}
-  }.`,
-    'blog.create': 'Create a new blog',
+  }។`,
+    'blog.create': 'បង្កើត​ក្រុមទិន្នន័យ​ថ្មីនៅក្នុង​ប្លុក',
     'blog.settings': `{type, select,
     org {Blog}
     universe {Universe page}
     user {Blog}
     other {}
   } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+    'blog.description': 'ការ​ណែនាំ​ពីប្លុក',
+    'blog.theme': 'ស្បែក',
+    'blog.theme.info': 'ដោះស្វែងរក​ស្បែក​និង​អភិក្ខាប័ត្រក្នុង​បញ្ជាក់​បញ្ជីទំព័រ។ ការ​លក់ខ្លី​នេះ​គ្មានទេពីរបៀបទទក់ទងទៅ​នឹង​ភាពរស់នៅពេលនេះ។',
     'blog.posts.total': `There {total, plural,
     zero {are no posts}
     one {is one post}
     other {are # posts}
   } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.post.create': 'បង្កើតប្លុក័ប្រភ័យថ្មី',
+    'blog.post.update': 'កែសម្រួល {title}',
+    'blog.post.text': 'អត្ថបទប្រភ័យ',
+    'common.slug': 'អាសយដ្ឋានរួមជាមួយ SEO',
+    'blog.slug.explained': 'អត្រាផ្នែកមានទំព័រវេចខ្ចប់ទៅអត្ថបទ។ សូមចាប់ប្រព្រឹត្តការប្រើអក្សរពិសេសពីលេខអណ្តើកទំព័រពីរប។',
+    'blog.publicView': 'ទិដ្ឋភាពសាធារណៈ',
+    'blog.post.notfound': 'រកមិនឃើញប្លុកដែលត្រូវបានរកឃើញ',
     'blog.lists.works': `{type, select,
-    universes {Universes}
-    stories {Standalone stories}
-    fanfiction {Fan Fiction work}
+    universes {សិល្បៈ}
+    stories {រឿងជាក់អោយ}
+    fanfiction {ប្អូនចម្រៀង}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
-    'blog.social.sameAsMain': `Social links are the same as set in {type, select,
-    organization {organization settings}
-    user {user profile}
+    'blog.lists.stories.disclaimer': 'នេះគឺជាប្រធានបទអានាគេដែលអានជាផ្នែករបស់ជនរងភាពមួយនេះហានិនាជាប្រជាជនក្រោយពេលនេះ។',
+    'blog.social.sameAsMain': `តោះធម្មតាឡើងវិញជាដើម្បីផ្ដល់ឱ្យអ្នកកាន់សម្រាប់ការតភ្ជាប់សង្គមដូចគ្នា ។ ទាំងនេះដូចគ្នា ចំរុះគ្នា ដោយស្ហោមការកំណត់ {type, select,
+    organization {សង្គមនៅក្នុង}
+    user {ការទិន្នន័យអ្នកប្រើ}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
-    'blog.settings.organizationSettings': 'Organization blogs are integrated into organization pages.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
-    'blog.settingsAria': 'Settings categories for the blog',
-    'blog.settings.blog': 'Blog',
-    'blog.settings.forum': 'Forum',
-    'blog.post.introText': 'Introduction paragraph',
-    'blog.post.writtenBy': 'Written by {usernameDisplay}',
-    'blog.post.writtenByFor': 'Written by {usernameDisplay} for {publicationName}',
-    'blog.posts.recent': 'Recent posts'
+    'blog.social.website': 'គេហទំព័របណ្តាញមួយនេះ',
+    'blog.settings.universeSettings': 'ប្លុកជាកំណត់ត្រា​​នៅក្នុងទំព័រជាប្រភ័យរងបានត្រឹមត្រូវជាមួយការកំណត់របស់ជនរងគេ។',
+    'blog.settings.organizationSettings': 'ប្លុកស្មាតហ្វេដូនពីការរៀបរង្ស័យរបស់សហគ្រោលទៅទំព័ររបស់សហគ្រោល',
+    'blogs.lu': 'ប្រជាជនសភារៈបរិភោគចម្បងនៃប្លុកជនរងព្រា',
+    'blogs.featured': 'ប្លុកពិតប្រើបរិភោគ',
+    'blogs.new': 'ប្លុកថ្មីបំផុត',
+    'blogs.title': 'ប្លុករបស់ជនរងព្រោះបរិភោគសភារៈ',
+    'blogs.description': 'ការរៀបចំប្លុកនៅលើប្រព័ន្ធសភារៈធម្មតា។',
+    'blogs.visit': 'ទសភារៈមើលប្លុក',
+    'blog.settingsAria': 'ប្រភេទការកំណត់សំណួរសម្បត្តិរបស់ការរៀបចំរបៀប',
+    'blog.settings.blog': 'ប្លុក',
+    'blog.settings.forum': 'វេទិកាក្រៅប្លុក',
+    'blog.post.introText': 'ការណែនាំបន្ទាប់ពីចូលរួម',
+    'blog.post.writtenBy': 'ការសរសេរគឺចំណាយព័ត៌មានដែលមានអំពី {usernameDisplay}',
+    'blog.post.writtenByFor': 'ការសរសេរប៉ុណ្ណោះដោយ {usernameDisplay} សម្រាប់ {publicationName}',
+    'blog.posts.recent': 'ការដាក់ស្នើថ្មីបំផុត'
 };
