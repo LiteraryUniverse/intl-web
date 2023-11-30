7674cba748976e7868a561336d4abf231fbcd891
Jan Dvorak <hello@jandvorak.me>
2023-11-30 16:23:19 +0100

    New translations blog.js (Tamil)

--- ta-IN/blog.js
+++ ta-IN/blog.js
@@ -1,58 +1,58 @@
 export default {
-    'blog.noneMsg': `You currently don't have {type, select,
-    organization {any blog}
-    universe {a blog for your universe}
-    user {a blog}
+    'blog.noneMsg': `நீங்கள் தற்போது {type, select,
+    organization {எந்த வலைப்பதிவும் இல்லை}
+    universe {உங்கள் உலகத்திற்கு}
+    user {தற்போதைய முனைவோர் செழுத்த ஒரு வலைப்பதிவை ஒரு வலைப்பு}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
+    'blog.create': 'புதிய வலைப்பதிவை உருவாக்கவும்',
     'blog.settings': `{type, select,
-    org {Blog}
-    universe {Universe page}
-    user {Blog}
+    org {புதினமான வலைப்பதிவு}
+    universe {பரலோக பக்கம்}
+    user {வலைப்பதிவு}
     other {}
-  } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
-    'blog.posts.total': `There {total, plural,
-    zero {are no posts}
-    one {is one post}
-    other {are # posts}
-  } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+  } அமைப்புகள்`,
+    'blog.description': 'வலைப்பதிவின் அறிமுகம்',
+    'blog.theme': 'தீம்',
+    'blog.theme.info': 'தீமை மாற்றினால் பக்கத்தின் பார்வையை மாற்ற முடியும். தற்போது இந்த அம்சம் பயன்படுத்த முடியவில்லை.',
+    'blog.posts.total': `ப்ளாகில் {total, plural,
+    zero {புதினமான பதிவுகள் இல்லை.}
+    one {ஐ ஒரு பதிவு கொடுப்பான்!}
+    other {பதிவுகள் இருக்கின்றன!}
+  } .`,
+    'blog.post.create': 'புதிய பதிவை உருவாக்கவும்',
+    'blog.post.update': 'மாற்றப்பட்ட பதிவு {title}',
+    'blog.post.text': 'பதிவு உரை',
+    'common.slug': 'SEO அமிழ்தியான முகவரி',
+    'blog.slug.explained': 'பதிவுக்கு அழகான URL. குறிப்பிட்டவைகளை அல்லது "-" ஐ தன்ராசிக்கு மட்டுமே பயன்படுத்துங்கள்.',
+    'blog.publicView': 'பொது பார்வை',
+    'blog.post.notfound': 'வலைப்பதிவு பதிவைக் கண்டறியாமல் உள்ளது',
     'blog.lists.works': `{type, select,
-    universes {Universes}
-    stories {Standalone stories}
-    fanfiction {Fan Fiction work}
+    universes {உலகங்கள்}
+    stories {ஒரு மின்னூல் கதைகள்}
+    fanfiction {வண்ணப்பதிவு வேலை}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
-    'blog.social.sameAsMain': `Social links are the same as set in {type, select,
-    organization {organization settings}
-    user {user profile}
+    'blog.lists.stories.disclaimer': 'பிரிமானமான கதைகள் இங்கே தொகுக்கப்பட்டுள்ளன, கதை ஒரு பரலோகத்தின் ஒரு பகுதி ஆகாமல் எழுதப்பட்டால் அது பரலோக பக்கத்தில் பத்திரிகைக்குச் சேர் பட்டது.',
+    'blog.social.sameAsMain': `சமூக இணையங்கள் பிரத்தியேக அமைப்புகளின் பொது இணைப்புகள் ஆகும் {type, select,
+    organization {அமைப்பு அமைப்பின் அமைப்புகள்}
+    user {பயனர் சுட்டிகள்}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
-    'blog.settings.organizationSettings': 'Organization blogs are integrated into organization pages.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
-    'blog.settingsAria': 'Settings categories for the blog',
-    'blog.settings.blog': 'Blog',
-    'blog.settings.forum': 'Forum',
-    'blog.post.introText': 'Introduction paragraph',
-    'blog.post.writtenBy': 'Written by {usernameDisplay}',
-    'blog.post.writtenByFor': 'Written by {usernameDisplay} for {publicationName}',
-    'blog.posts.recent': 'Recent posts'
+    'blog.social.website': 'அதிகாரப்பூர்வ இணையதளம்',
+    'blog.settings.universeSettings': 'பரலோக பக்கங்களில் பாடக்குறிப்புகளுடன் இணைக்கப்பட்டுள்ளன வலைப்பதிவு பகுதிகள் அடிப்படை அமைப்புகளை எடுக்கும்.',
+    'blog.settings.organizationSettings': 'முனைவோர் வலைப்புத்தகங்கள் அமைப்படும்.',
+    'blogs.lu': 'அதிகாரப்பூர்வ இலக்கண பரலோக வலைப்பதிவுகள்',
+    'blogs.featured': 'சிறப்பு வலைப்பதிவுகள்',
+    'blogs.new': 'புதிய வலைப்பதிவுகள்',
+    'blogs.title': 'இலக்கண பரலோகம் வலைப்பதிவுகள்',
+    'blogs.description': 'இலக்கண பரலோகம் தளத்தின் வலைப்பதிவுகளின் பட்டியல்.',
+    'blogs.visit': 'வலைப்பதிவைப் பார்க்க',
+    'blog.settingsAria': 'வலைப்பூட்டுகளுக்கான அமைப்புகள்',
+    'blog.settings.blog': 'புலங்கள்',
+    'blog.settings.forum': 'வலைப்புத்தகம்',
+    'blog.post.introText': 'அறிமுக பதிப்பு',
+    'blog.post.writtenBy': 'எழுதியவர் {usernameDisplay}',
+    'blog.post.writtenByFor': 'எழுதியவர் {usernameDisplay} உங்களது {publicationName}',
+    'blog.posts.recent': 'சமீபத்திய பதிவுகள்'
 };
