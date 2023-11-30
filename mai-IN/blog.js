b6c387014ee1efe6ef8cf720d6bcd5ab5c093715
Jan Dvorak <hello@jandvorak.me>
2023-11-30 16:30:05 +0100

    New translations blog.js (Maithili)

--- mai-IN/blog.js
+++ mai-IN/blog.js
@@ -5,54 +5,54 @@ export default {
     user {a blog}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
+    'blog.create': 'नया ब्लॉग बनाएँ',
     'blog.settings': `{type, select,
     org {Blog}
     universe {Universe page}
     user {Blog}
     other {}
   } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+    'blog.description': 'ब्लॉग परिचय',
+    'blog.theme': 'विषय',
+    'blog.theme.info': 'पृष्ठ के दिखावट को बदलकर आप पृष्ठ की दिखावट को बदल सकते हैं। यह सुविधा इस समय तैयार नहीं है।',
     'blog.posts.total': `There {total, plural,
     zero {are no posts}
     one {is one post}
     other {are # posts}
   } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.post.create': 'नई पोस्ट बनाएँ',
+    'blog.post.update': '{title}को संपादित करना',
+    'blog.post.text': 'पोस्ट का पाठ',
+    'common.slug': 'एसईओ योग्य पता',
+    'blog.slug.explained': 'पोस्ट के लिए सुंदर यूआरएल। "-" को छोड़कर विशेष वर्णों का उपयोग न करें।',
+    'blog.publicView': 'सार्वजनिक दृश्य',
+    'blog.post.notfound': 'ब्लॉग पोस्ट नहीं मिली',
     'blog.lists.works': `{type, select,
     universes {Universes}
     stories {Standalone stories}
     fanfiction {Fan Fiction work}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': 'यहाँ स्वतंत्र कथाएँ सूचीबद्ध हैं, यदि कहानी किसी ब्रह्मांड का हिस्सा के रूप में लिखी गई है तो वह ब्रह्मांड पृष्ठ पर सूचीबद्ध होगी।',
     'blog.social.sameAsMain': `Social links are the same as set in {type, select,
     organization {organization settings}
     user {user profile}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
-    'blog.settings.organizationSettings': 'Organization blogs are integrated into organization pages.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
-    'blog.settingsAria': 'Settings categories for the blog',
-    'blog.settings.blog': 'Blog',
-    'blog.settings.forum': 'Forum',
-    'blog.post.introText': 'Introduction paragraph',
-    'blog.post.writtenBy': 'Written by {usernameDisplay}',
-    'blog.post.writtenByFor': 'Written by {usernameDisplay} for {publicationName}',
-    'blog.posts.recent': 'Recent posts'
+    'blog.social.website': 'आधिकारिक वेबसाइट',
+    'blog.settings.universeSettings': 'ब्रह्मांड ब्लॉग ब्रह्मांड सेटिंग्स से लिए गए सामान्य सेटिंग्स के साथ यूनिवर्स पृष्ठों के भीतर मिले हुए हैं।',
+    'blog.settings.organizationSettings': 'संगठन ब्लगहरू आँतरगत संगठन पृष्ठमा समावेश गरिएका छन् ।',
+    'blogs.lu': 'आधिकारिक साहित्यिक ब्रह्मांड ब्लॉग',
+    'blogs.featured': 'विशेष ब्लॉग',
+    'blogs.new': 'नवीनतम ब्लॉग',
+    'blogs.title': 'साहित्यिक ब्रह्माण्ड के ब्लॉग',
+    'blogs.description': 'साहित्यिक ब्रह्माण्ड प्लेटफॉर्म पर ब्लॉगों की सूची।',
+    'blogs.visit': 'ब्लॉग पर जाएं',
+    'blog.settingsAria': 'ब्लगका नियामकहरूको समूहहरू',
+    'blog.settings.blog': 'ब्लग',
+    'blog.settings.forum': 'मन्च',
+    'blog.post.introText': 'परिचय पैराग्राफ',
+    'blog.post.writtenBy': '{usernameDisplay}द्वारा लेखित',
+    'blog.post.writtenByFor': '{usernameDisplay} द्वारा {publicationName}को लागि लेखित',
+    'blog.posts.recent': 'हालको पोस्टहरू'
 };
