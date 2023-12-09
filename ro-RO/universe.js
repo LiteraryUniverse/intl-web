d0bfe21346335115613275ae2946f95aa0089781
Jan Dvorak <hello@jandvorak.me>
2023-12-09 15:34:34 +0100

    New translations universe.js (Romanian)

--- ro-RO/universe.js
+++ ro-RO/universe.js
@@ -1,36 +1,36 @@
 export default {
-    'universe.authors.title.meta': '{title} authors',
-    'universe.fanfiction.see': 'All Fan Fiction for {universe}',
-    'universe.fanfiction.desc': 'Fan Fiction for {universe}',
+    'universe.authors.title.meta': '{title} autori',
+    'universe.fanfiction.see': 'Toate Fan Fiction pentru {universe}',
+    'universe.fanfiction.desc': 'Fan Fiction pentru {universe}',
     'universe.fanfiction.for': '{universe} Fan Fiction',
-    'universe.fanarts.for': '{universe} Fan Arts',
-    'universe.fansettings.for': '{universe} Fan Settings',
-    'universe.fanfiction.stories': 'Stories',
-    'universe.fanfiction.fanarts': 'Fan Arts',
+    'universe.fanarts.for': '{universe} Artă Fan',
+    'universe.fansettings.for': '{universe} Setări Fan',
+    'universe.fanfiction.stories': 'Povești',
+    'universe.fanfiction.fanarts': 'Arte Fan',
     'universe.stories.count': `There {num, plural,
     =0 {are no stories}
     one {is one story}
     other {are # stories}
     } in this universe.`,
-    'universe.authors.title': 'Authors',
-    'universe.authors.desc': 'Creators and contributors to {universe}',
-    'universe.contributors': 'Contributors',
-    'universe.translators': 'Translators',
-    'universe.authors.notice': 'Each story might have additional personal that has worked on it. For more details see the details of the story.',
+    'universe.authors.title': 'Autori',
+    'universe.authors.desc': 'Creatori și contribuitori la {universe}',
+    'universe.contributors': 'Contribuitori',
+    'universe.translators': 'Traducători',
+    'universe.authors.notice': 'Fiecare poveste ar putea avea personal adițional care a lucrat la ea. Pentru mai multe detalii, consultați detaliile povestirii.',
     'universe.createdBy': `{gender, select,
-    male {Created}
-    female {Created}
-    other {Created}} by `,
-    'universe.stories.for': 'Stories for {universe}',
-    'universe.stories.own': '{universe} stories',
-    'universe.stories.desc': 'Cannon stories for {universe}',
-    'universe.encyclopedia.title': '{universe} encyclopedia',
-    'universe.encyclopedia.desc': 'All the details for {universe}',
-    'universe.fanArt.submit': 'Submit your art',
-    'universe.fanArtHeaderDesc.item': '{universe} fan art - {name}',
-    'universe.fanArtHeaderDesc.submit': '{universe} fan art submission',
-    'universe.fanArtHeaderDesc.overview': '{universe} fan art',
-    'universe.fanFiction.hidden': 'Hidden',
-    'universe.fanFiction.hide': 'Hide fanfiction',
-    'universe.fanFiction.unhide': 'Unhide fanfiction'
+    male {Creat}
+    female {Creat}
+    other {Creat}} de `,
+    'universe.stories.for': 'Povești pentru {universe}',
+    'universe.stories.own': '{universe} povești',
+    'universe.stories.desc': 'Povești canonice pentru {universe}',
+    'universe.encyclopedia.title': '{universe} enciclopedie',
+    'universe.encyclopedia.desc': 'Toate detaliile pentru {universe}',
+    'universe.fanArt.submit': 'Trimiteți-și arta',
+    'universe.fanArtHeaderDesc.item': '{universe} artă fan - {name}',
+    'universe.fanArtHeaderDesc.submit': '{universe} artă fan (submisie)',
+    'universe.fanArtHeaderDesc.overview': '{universe} artă fan',
+    'universe.fanFiction.hidden': 'Ascunsă',
+    'universe.fanFiction.hide': 'Ascunde fanfiction',
+    'universe.fanFiction.unhide': 'Afisează fanfiction'
 };
