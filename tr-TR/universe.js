b3503d311ceabdf2bf2a4f84f6a2ae422b01d82d
Jan Dvorak <hello@jandvorak.me>
2023-12-09 15:25:55 +0100

    New translations universe.js (Turkish)

--- tr-TR/universe.js
+++ tr-TR/universe.js
@@ -1,36 +1,36 @@
 export default {
-    'universe.authors.title.meta': '{title} authors',
-    'universe.fanfiction.see': 'All Fan Fiction for {universe}',
-    'universe.fanfiction.desc': 'Fan Fiction for {universe}',
-    'universe.fanfiction.for': '{universe} Fan Fiction',
-    'universe.fanarts.for': '{universe} Fan Arts',
-    'universe.fansettings.for': '{universe} Fan Settings',
-    'universe.fanfiction.stories': 'Stories',
-    'universe.fanfiction.fanarts': 'Fan Arts',
-    'universe.stories.count': `There {num, plural,
-    =0 {are no stories}
-    one {is one story}
-    other {are # stories}
-    } in this universe.`,
-    'universe.authors.title': 'Authors',
-    'universe.authors.desc': 'Creators and contributors to {universe}',
-    'universe.contributors': 'Contributors',
-    'universe.translators': 'Translators',
-    'universe.authors.notice': 'Each story might have additional personal that has worked on it. For more details see the details of the story.',
+    'universe.authors.title.meta': '{title} yazarlar',
+    'universe.fanfiction.see': '{universe}için tüm Hayran Kurguları görün',
+    'universe.fanfiction.desc': '{universe}için Hayran Kurgu',
+    'universe.fanfiction.for': '{universe} Hayran Kurgu',
+    'universe.fanarts.for': '{universe} Hayran Sanatları',
+    'universe.fansettings.for': '{universe} Hayran Ayarları',
+    'universe.fanfiction.stories': 'Hikayeler',
+    'universe.fanfiction.fanarts': 'Hayran Sanatları',
+    'universe.stories.count': `Bu evrende {num, plural,
+    =0 {hikaye yok}
+    one {bir hikaye var}
+    other {# hikaye var}
+    }.`,
+    'universe.authors.title': 'Yazarlar',
+    'universe.authors.desc': '{universe}için oluşturucular ve katkıda bulunanlar',
+    'universe.contributors': 'Katkıda bulunanlar',
+    'universe.translators': 'Çevirmenler',
+    'universe.authors.notice': 'Her hikaye üzerinde çalışan ek kişi olabilir. Daha fazla bilgi için hikayenin detaylarına bakınız.',
     'universe.createdBy': `{gender, select,
-    male {Created}
-    female {Created}
-    other {Created}} by `,
-    'universe.stories.for': 'Stories for {universe}',
-    'universe.stories.own': '{universe} stories',
-    'universe.stories.desc': 'Cannon stories for {universe}',
-    'universe.encyclopedia.title': '{universe} encyclopedia',
-    'universe.encyclopedia.desc': 'All the details for {universe}',
-    'universe.fanArt.submit': 'Submit your art',
-    'universe.fanArtHeaderDesc.item': '{universe} fan art - {name}',
-    'universe.fanArtHeaderDesc.submit': '{universe} fan art submission',
-    'universe.fanArtHeaderDesc.overview': '{universe} fan art',
-    'universe.fanFiction.hidden': 'Hidden',
-    'universe.fanFiction.hide': 'Hide fanfiction',
-    'universe.fanFiction.unhide': 'Unhide fanfiction'
+    male {tarafından oluşturuldu}
+    female {tarafından oluşturuldu}
+    other {tarafından oluşturuldu}} `,
+    'universe.stories.for': '{universe}için Hikayeler',
+    'universe.stories.own': '{universe} hikaye',
+    'universe.stories.desc': '{universe}kanon hikayeler',
+    'universe.encyclopedia.title': '{universe} ansiklopedi',
+    'universe.encyclopedia.desc': '{universe}hakkındaki tüm detaylar',
+    'universe.fanArt.submit': 'Sanatınızı Gönderin',
+    'universe.fanArtHeaderDesc.item': '{universe} hayran sanatı - {name}',
+    'universe.fanArtHeaderDesc.submit': '{universe} hayran sanatı gönderimi',
+    'universe.fanArtHeaderDesc.overview': '{universe} hayran sanatı',
+    'universe.fanFiction.hidden': 'Gizli',
+    'universe.fanFiction.hide': 'Hayran Kurguyu Gizle',
+    'universe.fanFiction.unhide': 'Hayran Kurguyu Göster'
 };
