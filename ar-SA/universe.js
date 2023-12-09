638b65683b801848e03c0887bc8b039497dbeaac
Jan Dvorak <hello@jandvorak.me>
2023-12-09 14:32:30 +0100

    New translations universe.js (Arabic)

--- ar-SA/universe.js
+++ ar-SA/universe.js
@@ -1,33 +1,36 @@
 export default {
-    'universe.authors.title.meta': '{title} authors',
-    'universe.fanfiction.see': 'All Fan Fiction for {universe}',
-    'universe.fanfiction.desc': 'Fan Fiction for {universe}',
-    'universe.fanfiction.for': '{universe} Fan Fiction',
-    'universe.fanarts.for': '{universe} Fan Arts',
-    'universe.fansettings.for': '{universe} Fan Settings',
-    'universe.fanfiction.stories': 'Stories',
-    'universe.fanfiction.fanarts': 'Fan Arts',
-    'universe.stories.count': `There {num, plural,
-    =0 {are no stories}
-    one {is one story}
-    other {are # stories}
-    } in this universe.`,
-    'universe.authors.title': 'Authors',
-    'universe.authors.desc': 'Creators and contributors to {universe}',
+    'universe.authors.title.meta': '{title} المؤلفين',
+    'universe.fanfiction.see': 'جميع قصص المشجعين لـ {universe}',
+    'universe.fanfiction.desc': 'قصص المشجعين لـ {universe}',
+    'universe.fanfiction.for': '{universe} قصة مشجعين',
+    'universe.fanarts.for': '{universe} فنون المشجعين',
+    'universe.fansettings.for': '{universe} إعدادات المشجعين',
+    'universe.fanfiction.stories': 'قصص',
+    'universe.fanfiction.fanarts': 'فنون المشجعين',
+    'universe.stories.count': `{num, plural,
+    =0 {لا يوجد قصص}
+    one {هناك قصة واحدة}
+    other {هناك قصص #}
+    } في هذا الكون.`,
+    'universe.authors.title': 'المؤلفون',
+    'universe.authors.desc': 'المبدعون والمساهمون في {universe}',
     'universe.contributors': 'المساهمون',
     'universe.translators': 'المترجمون',
-    'universe.authors.notice': 'Each story might have additional personal that has worked on it. For more details see the details of the story.',
+    'universe.authors.notice': 'ربما يكون لكل قصة مساهمون إضافيون. لمزيد من التفاصيل، راجع تفاصيل القصة.',
     'universe.createdBy': `{gender, select,
     male {Created}
     female {Created}
     other {Created}} by `,
-    'universe.stories.for': 'Stories for {universe}',
-    'universe.stories.own': '{universe} stories',
-    'universe.stories.desc': 'Cannon stories for {universe}',
-    'universe.encyclopedia.title': '{universe} encyclopedia',
-    'universe.encyclopedia.desc': 'All the details for {universe}',
-    'universe.fanArt.submit': 'Submit your art',
-    'universe.fanArtHeaderDesc.item': '{universe} fan art - {name}',
-    'universe.fanArtHeaderDesc.submit': '{universe} fan art submission',
-    'universe.fanArtHeaderDesc.overview': '{universe} fan art'
+    'universe.stories.for': 'قصص لـ {universe}',
+    'universe.stories.own': '{universe} قصة',
+    'universe.stories.desc': 'قصص الكتابة الأصلية لـ {universe}',
+    'universe.encyclopedia.title': '{universe} موسوعة',
+    'universe.encyclopedia.desc': 'جميع التفاصيل لـ {universe}',
+    'universe.fanArt.submit': 'قدم فنك',
+    'universe.fanArtHeaderDesc.item': 'تصميمات المشجعين {universe} - {name}',
+    'universe.fanArtHeaderDesc.submit': 'تقديم تصميمات المشجعين {universe}',
+    'universe.fanArtHeaderDesc.overview': 'تصميمات المشجعين {universe}',
+    'universe.fanFiction.hidden': 'مخفي',
+    'universe.fanFiction.hide': 'إخفاء القصص الخيالية',
+    'universe.fanFiction.unhide': 'إظهار القصص الخيالية'
 };
