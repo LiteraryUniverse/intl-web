9f8409ad688a59c78c92d3cf57ac8d9cc9508c0b
Jan Dvorak <hello@jandvorak.me>
2023-12-09 14:28:28 +0100

    New translations universe.js (Filipino)

--- fil-PH/universe.js
+++ fil-PH/universe.js
@@ -1,33 +1,36 @@
 export default {
-    'universe.authors.title.meta': '{title} authors',
-    'universe.fanfiction.see': 'All Fan Fiction for {universe}',
-    'universe.fanfiction.desc': 'Fan Fiction for {universe}',
-    'universe.fanfiction.for': '{universe} Fan Fiction',
-    'universe.fanarts.for': '{universe} Fan Arts',
-    'universe.fansettings.for': '{universe} Fan Settings',
-    'universe.fanfiction.stories': 'Stories',
-    'universe.fanfiction.fanarts': 'Fan Arts',
-    'universe.stories.count': `There {num, plural,
-    =0 {are no stories}
-    one {is one story}
-    other {are # stories}
-    } in this universe.`,
-    'universe.authors.title': 'Authors',
-    'universe.authors.desc': 'Creators and contributors to {universe}',
-    'universe.contributors': 'Contributors',
-    'universe.translators': 'Translators',
-    'universe.authors.notice': 'Each story might have additional personal that has worked on it. For more details see the details of the story.',
+    'universe.authors.title.meta': '{title} mga may-akda',
+    'universe.fanfiction.see': 'Lahat ng Fan Fiction para kay {universe}',
+    'universe.fanfiction.desc': 'Gawa ng mga Fans para sa {universe}',
+    'universe.fanfiction.for': '{universe} mga Fan Fiction',
+    'universe.fanarts.for': '{universe} mga Sining ng Fan',
+    'universe.fansettings.for': '{universe} mga Setting ng Fan',
+    'universe.fanfiction.stories': 'Mga Kuwento',
+    'universe.fanfiction.fanarts': 'Mga Art ng Fans',
+    'universe.stories.count': `Wala {num, plural,
+    =0 {mga kuwento}
+    one {isa na nga kuwento}
+    other {}
+    } mga kuwento  sa kuwento na ito.`,
+    'universe.authors.title': 'Mga May-Akda',
+    'universe.authors.desc': 'Mga gumawa at nagbahagi sa {universe}',
+    'universe.contributors': 'Mga nagbahagi',
+    'universe.translators': 'Mga tagasalin',
+    'universe.authors.notice': 'Bawat kuwento ay maaaring may karagdagang personal na nakatrabaho dito. Para sa karagdagang mga detalye, tingnan ang mga detalye ng kuwento.',
     'universe.createdBy': `{gender, select,
-    male {Created}
-    female {Created}
-    other {Created}} by `,
-    'universe.stories.for': 'Stories for {universe}',
-    'universe.stories.own': '{universe} stories',
-    'universe.stories.desc': 'Cannon stories for {universe}',
-    'universe.encyclopedia.title': '{universe} encyclopedia',
-    'universe.encyclopedia.desc': 'All the details for {universe}',
-    'universe.fanArt.submit': 'Submit your art',
-    'universe.fanArtHeaderDesc.item': '{universe} fan art - {name}',
-    'universe.fanArtHeaderDesc.submit': '{universe} fan art submission',
-    'universe.fanArtHeaderDesc.overview': '{universe} fan art'
+    male {Nilikha ng}
+    female {Nilikha ng}
+    other {Nilikha ng}} ni `,
+    'universe.stories.for': 'Mga kuwento para kay {universe}',
+    'universe.stories.own': '{universe} mga kuwento',
+    'universe.stories.desc': 'Mga tunay na kuwentong para kay {universe}',
+    'universe.encyclopedia.title': '{universe} sagisag',
+    'universe.encyclopedia.desc': 'Lahat ng mga detalye para sa {universe}',
+    'universe.fanArt.submit': 'Isumite ang iyong sining',
+    'universe.fanArtHeaderDesc.item': '{universe} sining ng mga fan - {name}',
+    'universe.fanArtHeaderDesc.submit': '{universe} pagpapasa ng sining ng mga fan',
+    'universe.fanArtHeaderDesc.overview': '{universe} sining ng mga fan',
+    'universe.fanFiction.hidden': 'Nakatago',
+    'universe.fanFiction.hide': 'Itago ang fanfiction',
+    'universe.fanFiction.unhide': 'Huwag itago ang fanfiction'
 };
