6f42636879c5b99760be9c40b9ebcabdac0e3975
Jan Dvorak <hello@jandvorak.me>
2023-12-09 15:28:36 +0100

    New translations universe.js (Telugu)

--- te-IN/universe.js
+++ te-IN/universe.js
@@ -1,36 +1,36 @@
 export default {
-    'universe.authors.title.meta': '{title} authors',
-    'universe.fanfiction.see': 'All Fan Fiction for {universe}',
-    'universe.fanfiction.desc': 'Fan Fiction for {universe}',
-    'universe.fanfiction.for': '{universe} Fan Fiction',
-    'universe.fanarts.for': '{universe} Fan Arts',
-    'universe.fansettings.for': '{universe} Fan Settings',
-    'universe.fanfiction.stories': 'Stories',
+    'universe.authors.title.meta': '{title} రచయితలు',
+    'universe.fanfiction.see': 'నాయక నిర్మాత కోసం ! {universe}! సృష్టించబడింది',
+    'universe.fanfiction.desc': '{universe}కోసం ఫన్ ఫిక్షన్',
+    'universe.fanfiction.for': '{universe} ఫన్ ఫిక్షన్',
+    'universe.fanarts.for': '{universe} ఫన్ ఆర్ట్లు',
+    'universe.fansettings.for': '{universe} ఫన్ ఆకరణాలు',
+    'universe.fanfiction.stories': 'కథలు',
     'universe.fanfiction.fanarts': 'Fan Arts',
     'universe.stories.count': `There {num, plural,
     =0 {are no stories}
     one {is one story}
     other {are # stories}
     } in this universe.`,
-    'universe.authors.title': 'Authors',
-    'universe.authors.desc': 'Creators and contributors to {universe}',
-    'universe.contributors': 'Contributors',
-    'universe.translators': 'Translators',
-    'universe.authors.notice': 'Each story might have additional personal that has worked on it. For more details see the details of the story.',
-    'universe.createdBy': `{gender, select,
-    male {Created}
-    female {Created}
-    other {Created}} by `,
-    'universe.stories.for': 'Stories for {universe}',
-    'universe.stories.own': '{universe} stories',
-    'universe.stories.desc': 'Cannon stories for {universe}',
-    'universe.encyclopedia.title': '{universe} encyclopedia',
+    'universe.authors.title': 'రచయితలు',
+    'universe.authors.desc': '{universe}కు రచయితలు మరియు కాంట్రిబ్యూటర్లు',
+    'universe.contributors': 'కాంట్రిబ్యూటర్లు',
+    'universe.translators': 'అనువాదకులు',
+    'universe.authors.notice': 'ప్రతి కథ అదనప్పటికే ఉండవచ్చు వారు. మరిన్ని వివరాలకు కథ వివరాలను చూడండి',
+    'universe.createdBy': `ద్వారా {gender, select,
+    male {సృష్టించబడింది ద్వారా}
+    female {సృష్టించబడింది}
+    other {}} `,
+    'universe.stories.for': '{universe}కోసం కథలు',
+    'universe.stories.own': '{universe} కథలు',
+    'universe.stories.desc': '{universe}కు కానన్ కథలు',
+    'universe.encyclopedia.title': '{universe} సంక్షిప్తవిజ్ఞానపీఠం',
     'universe.encyclopedia.desc': 'All the details for {universe}',
     'universe.fanArt.submit': 'Submit your art',
     'universe.fanArtHeaderDesc.item': '{universe} fan art - {name}',
     'universe.fanArtHeaderDesc.submit': '{universe} fan art submission',
     'universe.fanArtHeaderDesc.overview': '{universe} fan art',
-    'universe.fanFiction.hidden': 'Hidden',
+    'universe.fanFiction.hidden': 'దాగిన',
     'universe.fanFiction.hide': 'Hide fanfiction',
     'universe.fanFiction.unhide': 'Unhide fanfiction'
 };
