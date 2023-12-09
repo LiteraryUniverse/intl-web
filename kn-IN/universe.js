3dc92cb26ccfdac49d70e7e62674713e94f5f197
Jan Dvorak <hello@jandvorak.me>
2023-12-09 14:31:14 +0100

    New translations universe.js (Kannada)

--- kn-IN/universe.js
+++ kn-IN/universe.js
@@ -1,33 +1,36 @@
 export default {
-    'universe.authors.title.meta': '{title} authors',
+    'universe.authors.title.meta': '{title} ಲೇಖಕರು',
     'universe.fanfiction.see': 'All Fan Fiction for {universe}',
-    'universe.fanfiction.desc': 'Fan Fiction for {universe}',
-    'universe.fanfiction.for': '{universe} Fan Fiction',
-    'universe.fanarts.for': '{universe} Fan Arts',
-    'universe.fansettings.for': '{universe} Fan Settings',
-    'universe.fanfiction.stories': 'Stories',
-    'universe.fanfiction.fanarts': 'Fan Arts',
-    'universe.stories.count': `There {num, plural,
-    =0 {are no stories}
-    one {is one story}
-    other {are # stories}
-    } in this universe.`,
-    'universe.authors.title': 'Authors',
-    'universe.authors.desc': 'Creators and contributors to {universe}',
-    'universe.contributors': 'Contributors',
-    'universe.translators': 'Translators',
-    'universe.authors.notice': 'Each story might have additional personal that has worked on it. For more details see the details of the story.',
+    'universe.fanfiction.desc': '{universe}ಗೆ ಸಂಬಂಧಪಟ್ಟ ಫ್ಯಾನ್ ಫಿಕ್ಷನ್',
+    'universe.fanfiction.for': '{universe} ಫ್ಯಾನ್ ಫಿಕ್ಷನ್',
+    'universe.fanarts.for': '{universe} ಫ್ಯಾನ್ ಆರ್ಟ್ಸ್',
+    'universe.fansettings.for': '{universe} ಫ್ಯಾನ್ ಸೆಟ್ಟಿಂಗ್ಸ್',
+    'universe.fanfiction.stories': 'ಕಥೆಗಳು',
+    'universe.fanfiction.fanarts': 'ಫ್ಯಾನ್ ಕಲೆಗಳು',
+    'universe.stories.count': `ಈ ವಿಶ್ವದಲ್ಲಿ {num, plural,
+    =0 {ಕಥೆಗಳು ಇವೆ}
+    one {ಒಂದು ಕಥೆ ಇದೆ}
+    other {ಕಥೆ ಇದೆ}
+    } ಕಥೆಗಳು ಇವೆ`,
+    'universe.authors.title': 'ಲೇಖಕರು',
+    'universe.authors.desc': '{universe}ಗೆ ಸೃಷ್ಟಿಸಿದ ನಿರ್ಮಾಪಕರು ಮತ್ತು ಕೊಡುಗೆದಾರರು',
+    'universe.contributors': 'ಕೊಡುಗೆದಾರರು',
+    'universe.translators': 'ಭಾಷಾಂತರಕರು',
+    'universe.authors.notice': 'ಪ್ರತಿ ಕಥೆಗೆ ಇನ್ನಷ್ಟು ವ್ಯಕ್ತಿಗಳು ಸಹ ಕೆಲಸ ಮಾಡಿರುವರು. ಹೆಚ್ಚಿನ ವಿವರಗಳನ್ನು ಕಥೆಯ ವಿವರಗಳಲ್ಲಿ ನೋಡಿ.',
     'universe.createdBy': `{gender, select,
-    male {Created}
-    female {Created}
-    other {Created}} by `,
-    'universe.stories.for': 'Stories for {universe}',
-    'universe.stories.own': '{universe} stories',
-    'universe.stories.desc': 'Cannon stories for {universe}',
-    'universe.encyclopedia.title': '{universe} encyclopedia',
-    'universe.encyclopedia.desc': 'All the details for {universe}',
-    'universe.fanArt.submit': 'Submit your art',
-    'universe.fanArtHeaderDesc.item': '{universe} fan art - {name}',
-    'universe.fanArtHeaderDesc.submit': '{universe} fan art submission',
-    'universe.fanArtHeaderDesc.overview': '{universe} fan art'
+    male {ದ್ವಾರಾ ರಚಿತ}
+    female {ದ್ವಾರಾ ರಚಿತ}
+    other {ದ್ವಾರಾ ರಚಿತ}} `,
+    'universe.stories.for': '{universe}ಕಥೆಗಳು',
+    'universe.stories.own': '{universe} ಕಥೆಗಳು',
+    'universe.stories.desc': 'ಈ ವಿಶ್ವದಲ್ಲಿ ಪ್ರಾಮಾಣಿಕ ಕಥೆಗಳು {universe}',
+    'universe.encyclopedia.title': '{universe} ವಿವರಾಗಾರ',
+    'universe.encyclopedia.desc': '{universe}ವಿವರಗಳು',
+    'universe.fanArt.submit': 'ನಿಮ್ಮ ಕಲೆಯನ್ನು ಸಲ್ಲಿಸಿ',
+    'universe.fanArtHeaderDesc.item': '{universe} ಫ್ಯಾನ್ ಕಲೆ - {name}',
+    'universe.fanArtHeaderDesc.submit': '{universe} ಫ್ಯಾನ್ ಕಲೆ ಸಲ್ಲಿಸಲು',
+    'universe.fanArtHeaderDesc.overview': '{universe} ಫ್ಯಾನ್ ಕಲೆ',
+    'universe.fanFiction.hidden': 'ಮರೆಯಲಾಗಿದೆ',
+    'universe.fanFiction.hide': 'ಫ್ಯಾನ್ ಫಿಕ್ಷನ್ ಮರುಹೊಂದಿಸಿ',
+    'universe.fanFiction.unhide': 'ಫ್ಯಾನ್ ಫಿಕ್ಷನ್ ದೃಶ್ಯದಲ್ಲಿ ತಂದುಕೊಳ್ಳಿ'
 };
