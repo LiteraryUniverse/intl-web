a142958a9171440bbb3129b564f5a4fdadb0a9b9
Jan Dvorak <hello@jandvorak.me>
2023-12-09 14:24:17 +0100

    New translations universe.js (Ukrainian)

--- uk-UA/universe.js
+++ uk-UA/universe.js
@@ -5,8 +5,8 @@ export default {
     'universe.fanfiction.for': '{universe} Fan Fiction',
     'universe.fanarts.for': '{universe} Fan Arts',
     'universe.fansettings.for': '{universe} Fan Settings',
-    'universe.fanfiction.stories': 'Stories',
-    'universe.fanfiction.fanarts': 'Fan Arts',
+    'universe.fanfiction.stories': 'Iсторії',
+    'universe.fanfiction.fanarts': 'Фан-Артс',
     'universe.stories.count': `There {num, plural,
     =0 {are no stories}
     one {is one story}
@@ -15,7 +15,7 @@ export default {
     'universe.authors.title': 'Authors',
     'universe.authors.desc': 'Creators and contributors to {universe}',
     'universe.contributors': 'Contributors',
-    'universe.translators': 'Translators',
+    'universe.translators': 'Перекладачі',
     'universe.authors.notice': 'Each story might have additional personal that has worked on it. For more details see the details of the story.',
     'universe.createdBy': `{gender, select,
     male {Created}
@@ -29,5 +29,8 @@ export default {
     'universe.fanArt.submit': 'Submit your art',
     'universe.fanArtHeaderDesc.item': '{universe} fan art - {name}',
     'universe.fanArtHeaderDesc.submit': '{universe} fan art submission',
-    'universe.fanArtHeaderDesc.overview': '{universe} fan art'
+    'universe.fanArtHeaderDesc.overview': '{universe} fan art',
+    'universe.fanFiction.hidden': 'Hidden',
+    'universe.fanFiction.hide': 'Hide fanfiction',
+    'universe.fanFiction.unhide': 'Unhide fanfiction'
 };
