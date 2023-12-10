c728b84232f6289eb51290a8426c853f0102029e
Jan Dvorak <hello@jandvorak.me>
2023-12-11 00:07:31 +0100

    New translations reader.js (Russian)

--- ru-RU/reader.js
+++ ru-RU/reader.js
@@ -33,7 +33,7 @@ export default {
     'reader.storyOnHiatus': 'В настоящее время автор делает перерыв в работе над этой историей, и пока что эта история находится на перерыве. Загляните через месяц или два.',
     'reader.storyCancelled': 'К сожалению, на этом всё. История отменена, так что вряд ли будет что-то новое.',
     'reader.storyCancelled.heading': 'Отменено',
-    'reader.nextChapterEstimation.verySoon': 'Based on the publication dates of the last {chaptersCount, number} chapters, the next chapter is expected to be released very soon.',
-    'reader.nextChapterEstimation.days': 'Based on the publication dates of the last {chaptersCount, number} chapters, the next chapter should be available in approximately {daysLeft, plural, one {1 day} other {# days}}.',
+    'reader.nextChapterEstimation.verySoon': 'Судя по датам публикации последних {chaptersCount, number} глав, выход следующей главы ожидается очень скоро.',
+    'reader.nextChapterEstimation.days': 'Исходя из дат публикации последних {chaptersCount, number} глав, следующая глава должна быть доступна примерно через {daysLeft, plural, one {1 день} few {# дня} many {# дней} other {# дней}}.',
     'reader.nextChapterEstimation.notEnoughChapters': 'Мы также предоставим приблизительную дату публикации следующей главы, когда в этом рассказе будет больше одной главы.'
 };
