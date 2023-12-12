3f0b7f5dfb30de7718792beec5568d5f450b5e5c
Jan Dvorak <hello@jandvorak.me>
2023-12-12 13:32:46 +0100

    New translations plans.js (Norwegian)

--- no-NO/plans.js
+++ no-NO/plans.js
@@ -89,15 +89,15 @@ export default {
     other {}
   }.`,
     'plans.subscribed.paymentMethod': 'Your default payment method will be used to pay for the next cycle.',
-    'plans.subscribed.status': `Subscription status: {status, select,
-    active {Active}
-    incomplete {There is something missing}
-    incomplete_expired {There is something missing}
-    trialing {Trial}
-    past_due {We are missing your payment}
-    canceled {Cancelled}
-    unpaid {Unpaid}
-    expired {Expired}
+    'plans.subscribed.status': `Abonnementsstatus: {status, select,
+    active {Aktiv}
+    incomplete {Det mangler noe}
+    incomplete_expired {Det mangler noe}
+    trialing {Prøveperiode}
+    past_due {Vi mangler betalingen din}
+    canceled {Avbrutt}
+    unpaid {Ubetalt}
+    expired {Utløpt}
     other {}
   }`,
     'payments.changeMethod': 'Change payment method',
