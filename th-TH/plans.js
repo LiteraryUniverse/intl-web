2c136dc09a83395451df8eb731ecf6f01b4a3b4b
Jan Dvorak <hello@jandvorak.me>
2023-12-12 13:37:57 +0100

    New translations plans.js (Thai)

--- th-TH/plans.js
+++ th-TH/plans.js
@@ -89,15 +89,15 @@ export default {
     other {}
   }.`,
     'plans.subscribed.paymentMethod': 'Your default payment method will be used to pay for the next cycle.',
-    'plans.subscribed.status': `Subscription status: {status, select,
-    active {Active}
-    incomplete {There is something missing}
-    incomplete_expired {There is something missing}
-    trialing {Trial}
-    past_due {We are missing your payment}
-    canceled {Cancelled}
-    unpaid {Unpaid}
-    expired {Expired}
+    'plans.subscribed.status': `สถานะการสมัครสมาชิก: {status, select,
+    active {ใช้งานอยู่}
+    incomplete {บางอย่างขาดหายไป}
+    incomplete_expired {บางอย่างขาดหายไป}
+    trialing {ทดลองใช้}
+    past_due {เราขาดการชำระเงินของคุณ}
+    canceled {ยกเลิกแล้ว}
+    unpaid {ยังไม่ชำระเงิน}
+    expired {หมดอายุแล้ว}
     other {}
   }`,
     'payments.changeMethod': 'Change payment method',
