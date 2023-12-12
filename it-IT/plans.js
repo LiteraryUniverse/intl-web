5030ec25895c3368ea91ae7bd895e24ff89c68b9
Jan Dvorak <hello@jandvorak.me>
2023-12-12 11:31:38 +0100

    New translations plans.js (Italian)

--- it-IT/plans.js
+++ it-IT/plans.js
@@ -89,14 +89,15 @@ export default {
     other {}
   }.`,
     'plans.subscribed.paymentMethod': 'Your default payment method will be used to pay for the next cycle.',
-    'plans.subscribed.status': `Subscription status: {status, select,
-    active {Active}
-    incomplete {There is something missing}
-    incomplete_expired {There is something missing}
-    trialing {Trial}
-    past_due {We are missing your payment}
-    canceled {Cancelled}
-    unpaid {Unpaid}
+    'plans.subscribed.status': `Stato sottoscrizione: {status, select,
+    active {Attivo}
+    incomplete {Qualcosa manca}
+    incomplete_expired {Qualcosa manca}
+    trialing {Prova}
+    past_due {Ci manca il tuo pagamento}
+    canceled {Annullato}
+    unpaid {Non pagato}
+    expired {Scaduto}
     other {}
   }`,
     'payments.changeMethod': 'Change payment method',
