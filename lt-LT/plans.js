eb92b4b4ac26ec767082dc2ee3eb403da034cf54
Jan Dvorak <hello@jandvorak.me>
2023-12-12 11:33:08 +0100

    New translations plans.js (Lithuanian)

--- lt-LT/plans.js
+++ lt-LT/plans.js
@@ -89,14 +89,15 @@ export default {
     other {}
   }.`,
     'plans.subscribed.paymentMethod': 'Your default payment method will be used to pay for the next cycle.',
-    'plans.subscribed.status': `Subscription status: {status, select,
-    active {Active}
-    incomplete {There is something missing}
-    incomplete_expired {There is something missing}
-    trialing {Trial}
-    past_due {We are missing your payment}
-    canceled {Cancelled}
-    unpaid {Unpaid}
+    'plans.subscribed.status': `Prenumeracija: {status, select,
+    active {Aktyvi}
+    incomplete {Kažkas trūksta}
+    incomplete_expired {Kažkas trūksta}
+    trialing {Bandomoji}
+    past_due {Missing your payment}
+    canceled {Atšaukta}
+    unpaid {Nepriimtas}
+    expired {Baigėsi!}
     other {}
   }`,
     'payments.changeMethod': 'Change payment method',
