0aa417ce65b7114cacce46003d9ab5f4a9dace52
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:47:23 +0100

    New translations emails.js (Danish)

--- da-DK/emails.js
+++ da-DK/emails.js
@@ -3,32 +3,32 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
-    'email.enrollAccount.message': `Hello {username}\n\n
-    We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
-    To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'Literary Universe reset password',
-    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
-        Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    'email.enrollAccount.subject': 'Invitation til Literary Universe for {username}',
+    'email.enrollAccount.message': `Hej {username}\n\n
+    Vi vil gerne invitere dig til Literary Universe! Vi har oprettet en speciel personlig konto til dig, hvor du kan få adgang til alle funktioner i Literary Universe! Vær med til at skabe næste generation af kreative skrive- og læseværktøjer!
+    For at aktivere din konto, skal du blot klikke på linket nedenfor:\n\n{url}`,
+    'email.resetPassword.subject': 'Literary Universe nulstil adgangskode',
+    'email.resetPassword.message': `Hej {username}!\n\n Vi har modtaget en anmodning om at nulstille din adgangskode til din konto.
+        Følg venligst linket nedenfor for at nulstille din adgangskode:\n\n{url}`,
+    'email.verifyEmail.subject': 'Literary Universe e-mail verifikation',
+    'email.verifyEmail.message': 'Hej {username}!\n\n Verificer venligst din e-mailadresse ved at klikke på linket nedenfor:\n\n{url}',
     // New html email keys
-    'email.salutation': 'Hello {username},',
+    'email.salutation': 'Hej {username},',
     'email.welcome': 'Velkommen til Litterære Universe!',
-    'email.signature': 'Sincerely,<br />Literary Universe team',
+    'email.signature': 'Venlig hilsen,<br />Literary Universe-teamet',
     // Enrollment
-    'email.enrollAccount.welcome': 'Welcome aboard',
-    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-    'email.enrollAccount.button': 'Access your account',
-    'email.enrollAccount.signature': 'We are looking forward to see you,<br />Literary Universe team',
+    'email.enrollAccount.welcome': 'Velkommen om bord',
+    'email.enrollAccount.messageHtml': 'Vi vil gerne invitere dig til Literary Universe! Vi har allerede oprettet en personlig konto til dig, hvor du kan få adgang til alle funktioner i Literary Universe! Vær med til at skabe næste generation af kreativ skrivning og læsning!',
+    'email.enrollAccount.button': 'Få adgang til din konto',
+    'email.enrollAccount.signature': 'Vi glæder os til at se dig,<br />Literary Universe-teamet',
     // Reset password
-    'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account, please click on the button bellow to continue the process. If you haven\'t made this request you can ignore this e-mail, but it might be a good time to review your security settings.',
-    'email.resetPassword.button': 'Reset password',
+    'email.resetPassword.messageHtml': 'Vi har modtaget en anmodning om at nulstille din adgangskode til din konto. Klik venligst på knappen nedenfor for at fortsætte processen. Hvis du ikke har foretaget denne anmodning, kan du ignorere denne e-mail, men det kan være en god idé at gennemgå dine sikkerhedsindstillinger.',
+    'email.resetPassword.button': 'Nulstil adgangskode',
     // E-mail verification
-    'email.verifyEmail.messageHtml': 'We are happy to have you on board! Please click the button bellow to verify this e-mail address.',
-    'email.verifyEmail.button': 'Verify this e-mail address',
+    'email.verifyEmail.messageHtml': 'Vi er glade for at have dig med om bord! Klik venligst på knappen nedenfor for at verificere denne e-mail-adresse.',
+    'email.verifyEmail.button': 'Verificer denne e-mail-adresse',
     // Getting premium from admins
-    'email.adminUpgrade.subject': 'Your account has been upgraded',
+    'email.adminUpgrade.subject': 'Din konto er blevet opgraderet',
     'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     adventurer {Adventurer}
