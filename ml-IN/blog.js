5374fbb3b20472e306c5055a9f794a2922cf6f5b
Jan Dvorak <hello@jandvorak.me>
2024-01-12 00:02:24 +0100

    New translations blog.js (Malayalam)

--- ml-IN/blog.js
+++ ml-IN/blog.js
@@ -5,49 +5,49 @@ export default {
     user {ഒരു blog}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
+    'blog.create': 'ഒരു പുതിയ ബ്ലോഗ് സൃഷ്ടിക്കുക',
     'blog.settings': `{type, select,
     org {Blog}
     universe {Universe page}
     user {Blog}
     other {}
   } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+    'blog.description': 'ബ്ലോഗ് പരിചയം',
+    'blog.theme': 'തീം',
+    'blog.theme.info': 'തീം സജ്ജീകരിച്ചാൽ നിങ്ങൾക്ക് പേജിന്റെ രൂപം മാറ്റാൻ കഴിയും. ഈ സവിശേഷത ഇപ്പോൾ പൂർണമായി പരിശോധിക്കപ്പെടുന്നില്ല.',
     'blog.posts.total': `There {total, plural,
     zero {are no posts}
     one {is one post}
     other {are # posts}
   } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.post.create': 'ഒരു പുതിയ പോസ്റ്റ് സൃഷ്ടിക്കുക',
+    'blog.post.update': '{title}എഡിറ്റുചെയ്യുന്നു',
+    'blog.post.text': 'പോസ്റ്റ് വചനം',
+    'common.slug': 'SEO സൗഹൃദമായ വിലാസം',
+    'blog.slug.explained': 'പോസ്റ്റിന് നല്ലായി കാണാൻ ഉള്ള യുആർഎൽ. "-" എന്നതിനെതിരെ പ്രത്യേക പ്രതീകങ്ങൾ ഉപയോഗിക്കരുത്.',
+    'blog.publicView': 'പൊതുവെ കാഴ്ച്ച',
+    'blog.post.notfound': 'ബ്ലോഗ് പോസ്റ്റ് കണ്ടെത്തിയില്ല',
     'blog.lists.works': `{type, select,
     universes {യൂണിവേഴ്സ്}
     stories {സ്വതന്ത്ര കഥകൾ}
     fanfiction {ഫാൻ ഫിക്ഷൻ കാര്യം}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': 'ഇവിടെ സ്വതന്ത്ര കഥകൾ നിർവ്വചനങ്ങൾ ആണ്, കഥ ഒരു യൂണിവേഴ്സിന്റെ ഭാഗമായാണെങ്കിൽ അത് യൂണിവേഴ്സ് പേജിൽ പട്ടികിൽ കാണാനാകും.',
     'blog.social.sameAsMain': `സോഷ്യൽ ലിങ്കുകൾ എഴുതേണ്ടത് {type, select,
     organization {ഓർഗനൈസേഷൻ ക്രമങ്ങളിൽ അമർത്താൻ}
     user {ഉപയോക്തൃ പ്രൊഫൈൽ ക്രമങ്ങളിൽ}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
+    'blog.social.website': 'ആധികാരിക വെബ്സൈറ്റ്',
+    'blog.settings.universeSettings': 'യൂണിവേഴ്സ് ബ്ലോഗുകൾ യൂണിവേഴ്സ് കാഴ്ചകളിൽ അവയുടെ അടിസ്ഥാന ക്രമീകരണങ്ങൾ ഉപയോഗിച്ച് സജീവമാക്കപ്പെടുന്നു.',
     'blog.settings.organizationSettings': 'കമ്പനി ബ്ലോഗുകൾ കമ്പനി പേജുകൾക്ക് ഐക്യകരിക്കപ്പെടുന്നു.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
+    'blogs.lu': 'ആധികാരിക സാഹിത്യ യൂണിവേഴ്സ് ബ്ലോഗുകൾ',
+    'blogs.featured': 'വിശേഷിപ്പിച്ച ബ്ലോഗുകൾ',
+    'blogs.new': 'പുതിയതുകൾ ബ്ലോഗുകൾ',
+    'blogs.title': 'സാഹിത്യ യൂണിവേഴ്സിന്റെ ബ്ലോഗുകൾ',
+    'blogs.description': 'സാഹിത്യ യൂണിവേഴ്സിന്റെ പ്ലാറ്റ്ഫോം ലിസ്റ്റിംഗ് ആണ് ഇവിടെ ബ്ലോഗുകൾ പട്ടികിൽ കാണാൻ.',
+    'blogs.visit': 'ബ്ലോഗ് സന്ദർശിക്കുക',
     'blog.settingsAria': 'ബ്ലോഗിന് ക്രമങ്ങൾ വേണം',
     'blog.settings.blog': 'ബ്ലോഗ്',
     'blog.settings.forum': 'ഫോറം',
