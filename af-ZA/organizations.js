d9a892d05543b3d37009425e55218793d6531e7f
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:46:01 +0100

    New translations organizations.js (Afrikaans)

--- af-ZA/organizations.js
+++ af-ZA/organizations.js
@@ -46,7 +46,7 @@ export default {
     'org.leaveOrg.confirm': 'This is a destructive action. Once you leave you will loose all access to this organization. Are you sure you want to leave the organization?',
     'org.confirmRemove.aria': 'Confirm user removal',
     'org.confirmRemove.confirm': 'Are you sure that you want to remove {username} from {organization}?',
-    'org.confirmRemove.remove': 'Remove user',
+    'org.confirmRemove.remove': 'Verwyder gebruiker',
     'org.mainBlog': 'Main blog',
     'org.teams': 'Teams',
     'org.teams.total': 'Total teams: {totalTeams, number}',
