74d2fe12cd34d6c419631f7e7dfce0b8e43785bd
Jan Dvorak <hello@jandvorak.me>
2024-01-12 00:01:35 +0100

    New translations blog.js (Filipino)

--- fil-PH/blog.js
+++ fil-PH/blog.js
@@ -5,49 +5,49 @@ export default {
     user {a blog}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
+    'blog.create': 'Gumawa ng bagong blog',
     'blog.settings': `{type, select,
     org {Blog}
     universe {Universe page}
     user {Blog}
     other {}
   } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+    'blog.description': 'Panimula ng blog',
+    'blog.theme': 'Tema',
+    'blog.theme.info': 'Sa pag-aadjust ng tema, maaari mong baguhin ang hitsura ng pahina. Ang tampok na ito ay hindi pa handa sa ngayon.',
     'blog.posts.total': `There {total, plural,
     zero {are no posts}
     one {is one post}
     other {are # posts}
   } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.post.create': 'Gumawa ng bagong post',
+    'blog.post.update': 'Ine-edit ang {title}',
+    'blog.post.text': 'Teksto ng post',
+    'common.slug': 'SEO friendly na address',
+    'blog.slug.explained': 'Magandang URL para sa post. Iwasan ang paggamit ng mga espesyal na karakter maliban sa "-".',
+    'blog.publicView': 'Pampublikong tanawin',
+    'blog.post.notfound': 'Hindi natagpuan ang blog post',
     'blog.lists.works': `{type, select,
     universes {Universes}
     stories {Standalone stories}
     fanfiction {Fan Fiction work}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': 'Narito ang mga hiwalay na kuwento, kung ang kuwento ay isinulat bilang bahagi ng isang universe, ito ay ililista sa pahina ng universe.',
     'blog.social.sameAsMain': `Social links are the same as set in {type, select,
     organization {organization settings}
     user {user profile}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
+    'blog.social.website': 'Opisyal na website',
+    'blog.settings.universeSettings': 'Ang mga blog ng universe ay integrado sa mga pahina ng universe na may mga pangunahing setting na kinuha mula sa mga setting ng universe.',
     'blog.settings.organizationSettings': 'Organization blogs are integrated into organization pages.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
+    'blogs.lu': 'Opisyal na mga blog ng Literary Universe',
+    'blogs.featured': 'Mga itinatampok na blog',
+    'blogs.new': 'Pinakabagong mga blog',
+    'blogs.title': 'Mga Blog ng Literary Universe',
+    'blogs.description': 'Listahan ng mga blog sa platform ng Literary Universe.',
+    'blogs.visit': 'Pumunta sa blog',
     'blog.settingsAria': 'Settings categories for the blog',
     'blog.settings.blog': 'Blog',
     'blog.settings.forum': 'Forum',
