c2ada120c2c8239b52cb77d2297b0c4a98870713
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:49:17 +0100

    New translations enrollment.js (Gujarati)

--- gu-IN/enrollment.js
+++ gu-IN/enrollment.js
@@ -1,6 +1,6 @@
 export default {
-    'enrollment.title': 'Claim your account!',
-    'enrollment.description': 'Welcome {username}! To gain access to your special account you only need to create password.',
-    'enrollment.submitting': 'Claiming your account...',
-    'enrollment.submit': 'Claim your account!'
+    'enrollment.title': 'તમારું એકાઉન્ટ દાવો!',
+    'enrollment.description': 'આપની વિશેષ એકાઉન્ટમાં પ્રવેશ મેળવવા માટે તમે ફક્ત પાસવર્ડ બનાવવો જરૂરી છે, આપનું સ્વાગત છે {username}!',
+    'enrollment.submitting': 'તમારું એકાઉન્ટ દાવી રહ્યું છે...',
+    'enrollment.submit': 'તમારું એકાઉન્ટ દાવો!'
 };
