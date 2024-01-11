3ac9f84a423b9a2010b2dde4771973b65effca1d
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:55:37 +0100

    New translations blog.js (Ukrainian)

--- uk-UA/blog.js
+++ uk-UA/blog.js
@@ -12,42 +12,42 @@ export default {
     user {Блог}
     other {}
   }`,
-    'blog.description': 'Blog introduction',
+    'blog.description': 'Вступ до блогу',
     'blog.theme': 'Тема',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+    'blog.theme.info': 'Змінюючи тему, ви зможете змінити вигляд сторінки. Наразі ця функція не готова.',
     'blog.posts.total': `There {total, plural,
     zero {are no posts}
     one {is one post}
     other {are # posts}
   } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.post.create': 'Створити новий пост',
+    'blog.post.update': 'Редагування {title}',
+    'blog.post.text': 'Текст поста',
+    'common.slug': 'SEO-дружня адреса',
+    'blog.slug.explained': 'Гарний URL-адреса поста. Уникайте використання спеціальних символів, крім "-".',
+    'blog.publicView': 'Публічний перегляд',
+    'blog.post.notfound': 'Пост блогу не знайдено',
     'blog.lists.works': `{type, select,
     universes {Universes}
     stories {Standalone stories}
     fanfiction {Fan Fiction work}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': 'Тут перераховані самостійні історії, якщо історія була написана як частина всесвіту, то вона буде перерахована на сторінці всесвіту.',
     'blog.social.sameAsMain': `Social links are the same as set in {type, select,
     organization {organization settings}
     user {user profile}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
+    'blog.social.website': 'Офіційний веб-сайт',
+    'blog.settings.universeSettings': 'Блоги всесвіту інтегровані на сторінки всесвіту з їх базовими налаштуваннями, взятими з налаштувань всесвіту.',
     'blog.settings.organizationSettings': 'Organization blogs are integrated into organization pages.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
+    'blogs.lu': 'Офіційні блоги Літературного Всесвіту',
+    'blogs.featured': 'Рекомендовані блоги',
+    'blogs.new': 'Найновіші блоги',
+    'blogs.title': 'Блоги Літературного Всесвіту',
+    'blogs.description': 'Перелік блогів на платформі Літературного Всесвіту.',
+    'blogs.visit': 'Відвідати блог',
     'blog.settingsAria': 'Settings categories for the blog',
     'blog.settings.blog': 'Блог',
     'blog.settings.forum': 'Форум',
