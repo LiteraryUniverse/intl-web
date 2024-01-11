365192d59007f7f3e8c14ea8fef8ada6b981cf63
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:53:44 +0100

    New translations writer.js (Polish)

--- pl-PL/writer.js
+++ pl-PL/writer.js
@@ -17,12 +17,12 @@ export default {
     'finder.connect': 'Połącz zaznaczenie z wpisem encyklopedii',
     'writer.aside.ariaLabel': 'Menu opcji pomocy',
     'writer.nothingToSave': 'Nie ma nic do zapisania',
-    'writer.publish.publishAria': 'Publication options',
+    'writer.publish.publishAria': 'Opcje publikacji',
     'writer.publish.scheduleExplained': 'You can either publish immediately or schedule the chapter to be available at a later date.',
     'writer.publish.schedule': 'Set publication date time',
     'writer.publish.previousChapterPublished': 'The previous chapter was published on {publishDate, date, short} at {publishDate, time}',
     'writer.publish.previousChapterScheduled': 'The previous chapter is scheduled to be published on {publishDate, date, short} at {publishDate, time}',
     'writer.publish.alreadyPublished': 'This chapter was already published on {publishedAt, date, short}. New versions will be published immediately.',
     'writer.publish.betaReader': 'Beta',
-    'writer.publish.public': 'Publish'
+    'writer.publish.public': 'Opublikuj'
 };
