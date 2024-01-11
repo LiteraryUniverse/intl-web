07afb77e5e9d8864135c9241419982d2d0cb8b94
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:58:25 +0100

    New translations blog.js (Bengali)

--- bn-BD/blog.js
+++ bn-BD/blog.js
@@ -5,49 +5,49 @@ export default {
     user {a blog}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
+    'blog.create': 'একটি নতুন ব্লগ তৈরি করুন',
     'blog.settings': `{type, select,
     org {Blog}
     universe {Universe page}
     user {Blog}
     other {}
   } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+    'blog.description': 'ব্লগ পরিচিতি',
+    'blog.theme': 'থিম',
+    'blog.theme.info': 'পৃষ্ঠার চেহারা পরিবর্তন করে আপনি পৃষ্ঠার দেখতে পাবেন। এই বৈশিষ্ট্যটি এখনও প্রস্তুত নেই।',
     'blog.posts.total': `There {total, plural,
     zero {are no posts}
     one {is one post}
     other {are # posts}
   } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.post.create': 'একটি নতুন পোস্ট তৈরি করুন',
+    'blog.post.update': '{title}সম্পাদনা করা হচ্ছে',
+    'blog.post.text': 'পোস্ট পাঠ্য',
+    'common.slug': 'এসইও বন্ধুত্বপূর্ণ ঠিকানা',
+    'blog.slug.explained': 'পোস্টের জন্য সুন্দর লিঙ্ক। "-" ছাড়া বিশেষ চিহ্ন ব্যবহার করার চেষ্টা করুন।',
+    'blog.publicView': 'পাবলিক দৃশ্য',
+    'blog.post.notfound': 'ব্লগ পোস্ট পাওয়া যায়নি',
     'blog.lists.works': `{type, select,
     universes {Universes}
     stories {Standalone stories}
     fanfiction {Fan Fiction work}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': 'এখানে স্থানিক গল্পগুলি তালিকাভুক্ত করা হয়, যদি গল্পটি একটি বিশ্বের অংশ হিসাবে লেখা হয় তবে এটি বিশ্বের পৃষ্ঠায় তালিকাভুক্ত হবে।',
     'blog.social.sameAsMain': `Social links are the same as set in {type, select,
     organization {organization settings}
     user {user profile}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
+    'blog.social.website': 'অফিসিয়াল ওয়েবসাইট',
+    'blog.settings.universeSettings': 'বিশ্ব ব্লগগুলি বিশ্ব সেটিংস থেকে গ্রহণ করে বিশ্ব পৃষ্ঠায় সংযুক্ত হয়।',
     'blog.settings.organizationSettings': 'Organization blogs are integrated into organization pages.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
+    'blogs.lu': 'অফিসিয়াল সাহিত্যিক বিশ্ব ব্লগগুলি',
+    'blogs.featured': 'বৈশিষ্ট্যযুক্ত ব্লগগুলি',
+    'blogs.new': 'নতুনতম ব্লগগুলি',
+    'blogs.title': 'সাহিত্যিক বিশ্বের ব্লগগুলি',
+    'blogs.description': 'সাহিত্যিক বিশ্ব প্ল্যাটফর্মের ব্লগগুলির তালিকা।',
+    'blogs.visit': 'ব্লগ দেখুন',
     'blog.settingsAria': 'Settings categories for the blog',
     'blog.settings.blog': 'Blog',
     'blog.settings.forum': 'Forum',
