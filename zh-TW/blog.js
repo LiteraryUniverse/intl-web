3dd0e920c31c448ec79f208274ddc5591e18c5fa
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:56:01 +0100

    New translations blog.js (Chinese Traditional)

--- zh-TW/blog.js
+++ zh-TW/blog.js
@@ -5,49 +5,49 @@ export default {
     user {a blog}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
+    'blog.create': '創建一個新的部落格',
     'blog.settings': `{type, select,
     org {Blog}
     universe {Universe page}
     user {Blog}
     other {}
   } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+    'blog.description': '部落格介紹',
+    'blog.theme': '主題',
+    'blog.theme.info': '通過調整主題，您將能夠改變頁面的外觀。此功能目前尚未準備好。',
     'blog.posts.total': `There {total, plural,
     zero {are no posts}
     one {is one post}
     other {are # posts}
   } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.post.create': '創建一個新的文章',
+    'blog.post.update': '編輯 {title}',
+    'blog.post.text': '文章內容',
+    'common.slug': 'SEO友好網址',
+    'blog.slug.explained': '文章的網址應該看起來美觀，除了「-」之外，盡量避免使用特殊字符。',
+    'blog.publicView': '公開查看',
+    'blog.post.notfound': '找不到部落格文章',
     'blog.lists.works': `{type, select,
     universes {宇宙}
     stories {獨立故事}
     fanfiction {粉絲創作}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': '這裡列出的是獨立故事，如果故事是作為宇宙的一部分而寫的，則會在宇宙頁面上列出。',
     'blog.social.sameAsMain': `社交連結與 {type, select,
     organization {組織設定}
     user {使用者檔案}
     other {相同}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
+    'blog.social.website': '官方網站',
+    'blog.settings.universeSettings': '宇宙部落格將與宇宙頁面集成，使用宇宙設定的基本設置。',
     'blog.settings.organizationSettings': '組織部落格已整合到組織頁面中。',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
+    'blogs.lu': '官方文學宇宙部落格',
+    'blogs.featured': '特色部落格',
+    'blogs.new': '最新部落格',
+    'blogs.title': '文學宇宙部落格',
+    'blogs.description': '在文學宇宙平台上列出的部落格。',
+    'blogs.visit': '訪問部落格',
     'blog.settingsAria': '部落格的設定分類',
     'blog.settings.blog': '部落格',
     'blog.settings.forum': '討論區',
