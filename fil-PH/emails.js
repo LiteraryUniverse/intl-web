d3ac51ab4a48530e671d7e5de2dd1d6a5f2a4b97
Jan Dvorak <hello@jandvorak.me>
2024-01-12 00:01:39 +0100

    New translations emails.js (Filipino)

--- fil-PH/emails.js
+++ fil-PH/emails.js
@@ -3,32 +3,32 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
-    'email.enrollAccount.message': `Hello {username}\n\n
-    We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
-    To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'Literary Universe reset password',
-    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
-        Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    'email.enrollAccount.subject': 'Imbitasyon sa Literary Universe para kay {username}',
+    'email.enrollAccount.message': `Kamusta {username}\n\n
+    ! Nais naming imbitahan ka sa Literary Universe! Nag-create kami ng espesyal na personal na account para sa iyo kung saan maaari mong ma-access ang lahat ng mga tampok ng Literary Universe! Sumali sa amin sa paglikha ng susunod na henerasyon ng mga tool sa malikhain at pagbabasa!
+    Upang i-activate ang iyong account, i-click lamang ang link sa ibaba:\n\n{url}`,
+    'email.resetPassword.subject': 'Reset ng Password sa Literary Universe',
+    'email.resetPassword.message': `Kamusta {username}!\n\n Natanggap namin ang kahilingan na i-reset ang iyong password para sa iyong account.
+        Mangyaring sundan ang link sa ibaba upang i-reset ang iyong password:\n\n{url}`,
+    'email.verifyEmail.subject': 'Pagpapatunay ng E-mail sa Literary Universe',
+    'email.verifyEmail.message': 'Kamusta {username}!\n\n Mangyaring patunayan ang iyong e-mail address sa pamamagitan ng pag-click sa link sa ibaba:\n\n{url}',
     // New html email keys
-    'email.salutation': 'Hello {username},',
-    'email.welcome': 'Welcome to Literary Universe!',
-    'email.signature': 'Sincerely,<br />Literary Universe team',
+    'email.salutation': 'Kamusta {username},',
+    'email.welcome': 'Maligayang pagdating sa Literary Universe!',
+    'email.signature': 'Buong katapatan,<br />Literary Universe team',
     // Enrollment
-    'email.enrollAccount.welcome': 'Welcome aboard',
-    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-    'email.enrollAccount.button': 'Access your account',
+    'email.enrollAccount.welcome': 'Maligayang pagdating sa Literary Universe!',
+    'email.enrollAccount.messageHtml': 'Nais naming imbitahan ka sa Literary Universe! Kami ay naghanda ng personal na account para sa iyo kung saan maaari mong ma-access ang lahat ng mga tampok ng Literary Universe! Sumali sa amin sa paglikha ng susunod na henerasyon ng malikhain at pagbabasa!',
+    'email.enrollAccount.button': 'Buksan ang iyong account',
     'email.enrollAccount.signature': 'We are looking forward to see you,<br />Literary Universe team',
     // Reset password
-    'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account, please click on the button bellow to continue the process. If you haven\'t made this request you can ignore this e-mail, but it might be a good time to review your security settings.',
-    'email.resetPassword.button': 'Reset password',
+    'email.resetPassword.messageHtml': 'Nakatanggap kami ng kahilingan na i-reset ang iyong password para sa iyong account, mangyaring i-click ang button sa ibaba upang magpatuloy sa proseso. Kung hindi mo ginawa ang kahilingang ito, maaari mong ipagwalang-bahala ang e-mail na ito, ngunit maaaring ito ang tamang panahon upang tingnan ang iyong mga setting sa seguridad.',
+    'email.resetPassword.button': 'I-reset ang password',
     // E-mail verification
-    'email.verifyEmail.messageHtml': 'We are happy to have you on board! Please click the button bellow to verify this e-mail address.',
-    'email.verifyEmail.button': 'Verify this e-mail address',
+    'email.verifyEmail.messageHtml': 'Maligaya kami na ikaw ay sumali! Mangyaring i-click ang button sa ibaba upang patunayan ang e-mail address na ito.',
+    'email.verifyEmail.button': 'Patunayan ang e-mail address na ito',
     // Getting premium from admins
-    'email.adminUpgrade.subject': 'Your account has been upgraded',
+    'email.adminUpgrade.subject': 'Ang iyong account ay na-upgrade',
     'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     adventurer {Adventurer}
