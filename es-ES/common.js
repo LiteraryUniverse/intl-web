ced63824409dd7bddd6b9ffd92daf3d578c65b4e
Jan Dvorak <hello@jandvorak.me>
2024-01-11 14:18:55 +0100

    New translations common.js (Spanish)

--- es-ES/common.js
+++ es-ES/common.js
@@ -258,5 +258,5 @@ export default {
     'email.hello': 'Hola {username},',
     'common.back': 'Volver',
     'console.stop': '¡Detente!',
-    'console.explain': 'This is a browser feature intended for developers. If someone told you to copy-paste something here to enable a Literary Universe feature or "hack" someone\'s account, it is a scam and will give them access to your Literary Universe account.'
+    'console.explain': 'Esta es una función del navegador destinada a los desarrolladores. Si alguien te dijo que copies y pegues algo aquí para habilitar una función del Universo Literario o "hackear" la cuenta de alguien, es una estafa y les dará acceso a tu cuenta del Universo Literario.'
 }    /* eslint-enable max-len */;
