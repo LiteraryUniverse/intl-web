c7832cf9323212b8e8232046921232a8bfbcf4a4
Jan Dvorak <hello@jandvorak.me>
2024-01-12 00:02:03 +0100

    New translations emails.js (Lojban)

--- jbo-EN/emails.js
+++ jbo-EN/emails.js
@@ -4,31 +4,31 @@
 /* eslint-disable max-len */
 export default {
     'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
-    'email.enrollAccount.message': `Hello {username}\n\n
-    We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
-    To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'Literary Universe reset password',
-    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
-        Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    'email.enrollAccount.message': `.i coi {username}\n\n
+    .i mi cilre lo nu do pu'o jivna lo za'u reno cevni! .i mi pu crepu lo se pilno be lo za'u reno cevni pe do .i ko catlu lo te pilno be lo za'u reno cevni be lo nu penmi .i ko penmi mi lo nu pilno lo te pilno be lo za'u reno cevni be lo nu penmi .i .i
+    .i lo nu jivna do lo za'u reno cevni cu se akti lo ka klikpa lo slilu .i\n\n{url}`,
+    'email.resetPassword.subject': '.i se facki do lo se jivna be lo za\'u reno cevni',
+    'email.resetPassword.message': `.i coi {username}!\n\n .i mi pu facki lo se preti be lo nu facki lo se jivna be do lo za'u reno cevni .i
+        .i ko se facki lo slilu be lo nu facki lo se preti be lo nu facki lo se jivna be do lo za'u reno cevni .i\n\n{url}`,
+    'email.verifyEmail.subject': '.i se jivna be lo nu jai gau krici lo du\'u do pu se ctino lo se preti be lo ka facki lo nu jivna do',
+    'email.verifyEmail.message': '.i coi {username}!\n\n .i ko krici lo du\'u do pu se ctino lo se preti be lo ka facki lo nu jivna do lo nu penmi .i\n\n{url}',
     // New html email keys
-    'email.salutation': 'Hello {username},',
-    'email.welcome': 'Welcome to Literary Universe!',
-    'email.signature': 'Sincerely,<br />Literary Universe team',
+    'email.salutation': '.i coi {username},',
+    'email.welcome': '.i do co\'a jivna lo za\'u reno cevni!',
+    'email.signature': '.i cinmo fi lo ka<br />lo se preti be lo ka facki lo nu jivna do lo za\'u reno cevni',
     // Enrollment
-    'email.enrollAccount.welcome': 'Welcome aboard',
-    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-    'email.enrollAccount.button': 'Access your account',
-    'email.enrollAccount.signature': 'We are looking forward to see you,<br />Literary Universe team',
+    'email.enrollAccount.welcome': '.i co\'a jivna',
+    'email.enrollAccount.messageHtml': '.i mi pu cilre lo nu do pu\'o jivna lo za\'u reno cevni! .i mi pu zukte lo se pilno be lo za\'u reno cevni pe do .i ko catlu lo te pilno be lo za\'u reno cevni be lo nu penmi .i ko penmi mi lo nu pilno lo te pilno be lo za\'u reno cevni be lo nu penmi',
+    'email.enrollAccount.button': '.i pilno lo do za\'u reno cevni',
+    'email.enrollAccount.signature': 'Mi\'e la .ui. cu zvati lo nu do viska,<br />la .ui. se tcidu',
     // Reset password
-    'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account, please click on the button bellow to continue the process. If you haven\'t made this request you can ignore this e-mail, but it might be a good time to review your security settings.',
-    'email.resetPassword.button': 'Reset password',
+    'email.resetPassword.messageHtml': 'lo nu do cikre lo se jinvi be do ke\'a cu se klama lo nu do jgari lo japyvla be do, .i ko jai gau kliki lo botpi bau la\'o zoi. bellow .zoi. lo nu cnano lo pruce cu zmadu',
+    'email.resetPassword.button': 'cnino japyvla',
     // E-mail verification
-    'email.verifyEmail.messageHtml': 'We are happy to have you on board! Please click the button bellow to verify this e-mail address.',
-    'email.verifyEmail.button': 'Verify this e-mail address',
+    'email.verifyEmail.messageHtml': 'Mi\'e la .ui. cu gleki lo nu do klama le daski! .i ko jai gau kliki lo botpi bau la\'o zoi. bellow .zoi. lo nu sruma lo pruce cu zmadu',
+    'email.verifyEmail.button': 'sruma lo pruce',
     // Getting premium from admins
-    'email.adminUpgrade.subject': 'Your account has been upgraded',
+    'email.adminUpgrade.subject': 'lo se jinvi be do cu se zbasu',
     'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     adventurer {Adventurer}
