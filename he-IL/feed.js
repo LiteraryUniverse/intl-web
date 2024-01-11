83e5f7afacde74c7bd81a20338f682d4bd3ab3fe
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:49:39 +0100

    New translations feed.js (Hebrew)

--- he-IL/feed.js
+++ he-IL/feed.js
@@ -1,8 +1,8 @@
 export default {
     'feed.name': 'Feed',
-    'feed.post.new': 'New post',
-    'feed.post.like': 'Like post',
-    'feed.post.unlike': 'Unlike post',
-    'feed.post.send': 'Post',
-    'feed.empty': 'No entries in the feed.'
+    'feed.post.new': 'פוסט חדש',
+    'feed.post.like': 'אהבת פוסט',
+    'feed.post.unlike': 'ביטול אהבת פוסט',
+    'feed.post.send': 'פוסט',
+    'feed.empty': 'אין רשומות בפיד.'
 };
