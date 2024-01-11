a37bbd1ab5a9c1df2cb328e3bc6f8aa535667d4a
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:58:06 +0100

    New translations enrollment.js (Tamil)

--- ta-IN/enrollment.js
+++ ta-IN/enrollment.js
@@ -1,6 +1,6 @@
 export default {
-    'enrollment.title': 'Claim your account!',
-    'enrollment.description': 'Welcome {username}! To gain access to your special account you only need to create password.',
-    'enrollment.submitting': 'Claiming your account...',
-    'enrollment.submit': 'Claim your account!'
+    'enrollment.title': 'உங்கள் கணக்கை கேளுங்கள்!',
+    'enrollment.description': 'வரவேற்பு செய்யப்பட்டது {username}! உங்கள் சிறப்பு கணக்கிற்கு அணுகல் பெற நீங்கள் மட்டுமே கடவுச்சொல் உருவாக்க வேண்டும்.',
+    'enrollment.submitting': 'உங்கள் கணக்கை கேளுங்கள்...',
+    'enrollment.submit': 'உங்கள் கணக்கை கேளுங்கள்!'
 };
