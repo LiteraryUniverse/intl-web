bf4ba6abf450018d8b431585dc4c018dfdde9ffc
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:58:07 +0100

    New translations feed.js (Tamil)

--- ta-IN/feed.js
+++ ta-IN/feed.js
@@ -1,8 +1,8 @@
 export default {
     'feed.name': 'Feed',
-    'feed.post.new': 'New post',
-    'feed.post.like': 'Like post',
-    'feed.post.unlike': 'Unlike post',
-    'feed.post.send': 'Post',
-    'feed.empty': 'No entries in the feed.'
+    'feed.post.new': 'புதிய பதிவு',
+    'feed.post.like': 'பதிவை விரும்பு',
+    'feed.post.unlike': 'பதிவை விரும்பாதே',
+    'feed.post.send': 'பதிவு',
+    'feed.empty': 'ஃபீட்டில் உள்ள உள்ளீடுகள் இல்லை.'
 };
