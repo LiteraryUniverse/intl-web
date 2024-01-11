7dc221d94cfde45604aa71ce0e57b843647ce3a4
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:56:27 +0100

    New translations emails.js (Vietnamese)

--- vi-VN/emails.js
+++ vi-VN/emails.js
@@ -3,32 +3,32 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
-    'email.enrollAccount.message': `Hello {username}\n\n
-    We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
-    To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'Literary Universe reset password',
-    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
-        Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    'email.enrollAccount.subject': 'Lời mời đến Vũ trụ Văn học cho {username}',
+    'email.enrollAccount.message': `Xin chào {username}\n\n
+    Chúng tôi muốn mời bạn đến Vũ trụ Văn học! Chúng tôi đã tạo một tài khoản cá nhân đặc biệt cho bạn, nơi bạn có thể truy cập vào tất cả các tính năng của Vũ trụ Văn học! Hãy tham gia cùng chúng tôi trong việc tạo ra thế hệ tiếp theo của các công cụ viết và đọc sáng tạo!
+    Để kích hoạt tài khoản của bạn, chỉ cần nhấp vào liên kết bên dưới:\n\n{url}`,
+    'email.resetPassword.subject': 'Đặt lại mật khẩu Vũ trụ Văn học',
+    'email.resetPassword.message': `Xin chào {username}!\n\n Chúng tôi đã nhận được yêu cầu đặt lại mật khẩu cho tài khoản của bạn.
+        Vui lòng làm theo liên kết bên dưới để đặt lại mật khẩu:\n\n{url}`,
+    'email.verifyEmail.subject': 'Xác minh email Vũ trụ Văn học',
+    'email.verifyEmail.message': 'Xin chào {username}!\n\n Vui lòng xác minh địa chỉ email của bạn bằng cách nhấp vào liên kết bên dưới:\n\n{url}',
     // New html email keys
-    'email.salutation': 'Hello {username},',
-    'email.welcome': 'Welcome to Literary Universe!',
-    'email.signature': 'Sincerely,<br />Literary Universe team',
+    'email.salutation': 'Xin chào {username},',
+    'email.welcome': 'Chào mừng bạn đến Vũ trụ Văn học!',
+    'email.signature': 'Trân trọng,<br />Nhóm Vũ trụ Văn học',
     // Enrollment
-    'email.enrollAccount.welcome': 'Welcome aboard',
-    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-    'email.enrollAccount.button': 'Access your account',
-    'email.enrollAccount.signature': 'We are looking forward to see you,<br />Literary Universe team',
+    'email.enrollAccount.welcome': 'Chào mừng bạn đã gia nhập',
+    'email.enrollAccount.messageHtml': 'Chúng tôi muốn mời bạn đến Vũ trụ Văn học! Chúng tôi đã chuẩn bị sẵn một tài khoản cá nhân cho bạn, nơi bạn có thể truy cập vào tất cả các tính năng của Vũ trụ Văn học! Hãy tham gia cùng chúng tôi trong việc tạo ra thế hệ tiếp theo của viết và đọc sáng tạo!',
+    'email.enrollAccount.button': 'Truy cập tài khoản của bạn',
+    'email.enrollAccount.signature': 'Chúng tôi rất mong được gặp bạn, đội ngũ Literary Universe<br />',
     // Reset password
-    'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account, please click on the button bellow to continue the process. If you haven\'t made this request you can ignore this e-mail, but it might be a good time to review your security settings.',
-    'email.resetPassword.button': 'Reset password',
+    'email.resetPassword.messageHtml': 'Chúng tôi đã nhận được yêu cầu đặt lại mật khẩu cho tài khoản của bạn, vui lòng nhấp vào nút bên dưới để tiếp tục quá trình. Nếu bạn không thực hiện yêu cầu này, bạn có thể bỏ qua email này, nhưng đây có thể là thời điểm tốt để xem lại cài đặt bảo mật của bạn.',
+    'email.resetPassword.button': 'Đặt lại mật khẩu',
     // E-mail verification
-    'email.verifyEmail.messageHtml': 'We are happy to have you on board! Please click the button bellow to verify this e-mail address.',
-    'email.verifyEmail.button': 'Verify this e-mail address',
+    'email.verifyEmail.messageHtml': 'Chúng tôi rất vui khi có bạn tham gia! Vui lòng nhấp vào nút bên dưới để xác minh địa chỉ email này.',
+    'email.verifyEmail.button': 'Xác minh địa chỉ email này',
     // Getting premium from admins
-    'email.adminUpgrade.subject': 'Your account has been upgraded',
+    'email.adminUpgrade.subject': 'Tài khoản của bạn đã được nâng cấp',
     'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     adventurer {Adventurer}
