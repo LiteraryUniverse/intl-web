866e71d93127c31c68ac501d5077d811e707a13e
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:56:07 +0100

    New translations feed.js (Chinese Traditional)

--- zh-TW/feed.js
+++ zh-TW/feed.js
@@ -1,8 +1,8 @@
 export default {
     'feed.name': 'Feed',
-    'feed.post.new': 'New post',
-    'feed.post.like': 'Like post',
-    'feed.post.unlike': 'Unlike post',
-    'feed.post.send': 'Post',
-    'feed.empty': 'No entries in the feed.'
+    'feed.post.new': '新的貼文',
+    'feed.post.like': '喜歡貼文',
+    'feed.post.unlike': '取消喜歡貼文',
+    'feed.post.send': '發布',
+    'feed.empty': '動態消息中沒有任何項目。'
 };
