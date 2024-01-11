e9566bdc76f63aa725520250d58cac66201f0239
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:46:20 +0100

    New translations feed.js (Arabic)

--- ar-SA/feed.js
+++ ar-SA/feed.js
@@ -1,8 +1,8 @@
 export default {
     'feed.name': 'Feed',
-    'feed.post.new': 'New post',
-    'feed.post.like': 'Like post',
-    'feed.post.unlike': 'Unlike post',
-    'feed.post.send': 'Post',
-    'feed.empty': 'No entries in the feed.'
+    'feed.post.new': 'منشور جديد',
+    'feed.post.like': 'إعجاب بالمنشور',
+    'feed.post.unlike': 'إلغاء إعجاب بالمنشور',
+    'feed.post.send': 'نشر',
+    'feed.empty': 'لا توجد مدخلات في الخلاصة.'
 };
