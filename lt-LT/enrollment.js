7c4926cbab833065ffe8ec1a2835a5b8e8848b77
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:51:58 +0100

    New translations enrollment.js (Lithuanian)

--- lt-LT/enrollment.js
+++ lt-LT/enrollment.js
@@ -1,6 +1,6 @@
 export default {
-    'enrollment.title': 'Claim your account!',
-    'enrollment.description': 'Welcome {username}! To gain access to your special account you only need to create password.',
-    'enrollment.submitting': 'Claiming your account...',
-    'enrollment.submit': 'Claim your account!'
+    'enrollment.title': 'Pareikalaukite savo paskyros!',
+    'enrollment.description': 'Sveiki, {username}! Norėdami gauti prieigą prie savo ypatingos paskyros, jums tereikia sukurti slaptažodį.',
+    'enrollment.submitting': 'Pareikalaujama jūsų paskyros...',
+    'enrollment.submit': 'Pareikalaukite savo paskyros!'
 };
