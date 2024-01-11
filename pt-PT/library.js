20a0cb8d894a74e5280ea4f16a8c4df94b7f7e43
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:54:04 +0100

    New translations library.js (Portuguese)

--- pt-PT/library.js
+++ pt-PT/library.js
@@ -4,6 +4,6 @@ export default {
     'library.bought': 'Histórias compradas',
     'library.stories': 'Histórias seguidas',
     'library.universes': 'Universos seguidos',
-    'bookmark.continueReading': 'Continue reading',
+    'bookmark.continueReading': 'Continuar a ler',
     'bookmark.statusSummary': 'You have read {readPercent, number, ::percent} of the story.'
 };
