f21e3b9e7326cb60fef23ddcb4823e006b7f37cc
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:57:19 +0100

    New translations enrollment.js (Indonesian)

--- id-ID/enrollment.js
+++ id-ID/enrollment.js
@@ -1,6 +1,6 @@
 export default {
-    'enrollment.title': 'Claim your account!',
-    'enrollment.description': 'Welcome {username}! To gain access to your special account you only need to create password.',
-    'enrollment.submitting': 'Claiming your account...',
-    'enrollment.submit': 'Claim your account!'
+    'enrollment.title': 'Klaim akun Anda!',
+    'enrollment.description': 'Selamat datang {username}! Untuk mendapatkan akses ke akun khusus Anda, Anda hanya perlu membuat kata sandi.',
+    'enrollment.submitting': 'Mengklaim akun Anda...',
+    'enrollment.submit': 'Klaim akun Anda!'
 };
