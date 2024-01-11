b3e8be08bc9f7cadaa0a1a267cafeefa2d3adf23
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:59:37 +0100

    New translations emails.js (Estonian)

--- et-EE/emails.js
+++ et-EE/emails.js
@@ -3,32 +3,32 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
-    'email.enrollAccount.message': `Hello {username}\n\n
-    We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
-    To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'Literary Universe reset password',
-    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
-        Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    'email.enrollAccount.subject': 'Kutse Kirjanduslikku Universumisse {username}',
+    'email.enrollAccount.message': `Tere {username}\n\n
+    Me soovime sind kutsuda Kirjanduslikku Universumisse! Oleme loonud sulle erilise personaalse konto, kus saad ligi kõikidele Kirjandusliku Universumi funktsioonidele! Palun ühine meiega, et luua järgmine põlvkond loovkirjutamise ja lugemisvahendeid!
+    Konto aktiveerimiseks klõpsa lihtsalt allpool oleval lingil:\n\n{url}`,
+    'email.resetPassword.subject': 'Kirjandusliku Universumi parooli lähtestamine',
+    'email.resetPassword.message': `Tere {username}!\n\n Oleme saanud taotluse sinu parooli lähtestamiseks sinu kontol.
+        Palun järgi allpool olevat linki, et lähtestada oma parool:\n\n{url}`,
+    'email.verifyEmail.subject': 'Kirjandusliku Universumi e-posti kinnitamine',
+    'email.verifyEmail.message': 'Tere {username}!\n\n Palun kinnita oma e-posti aadress, klõpsates allpool oleval lingil:\n\n{url}',
     // New html email keys
-    'email.salutation': 'Hello {username},',
-    'email.welcome': 'Welcome to Literary Universe!',
-    'email.signature': 'Sincerely,<br />Literary Universe team',
+    'email.salutation': 'Tere {username},',
+    'email.welcome': 'Tere tulemast Kirjanduslikku Universumisse!',
+    'email.signature': 'Parimate soovidega,<br />Kirjandusliku Universumi meeskond',
     // Enrollment
-    'email.enrollAccount.welcome': 'Welcome aboard',
-    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-    'email.enrollAccount.button': 'Access your account',
-    'email.enrollAccount.signature': 'We are looking forward to see you,<br />Literary Universe team',
+    'email.enrollAccount.welcome': 'Tere tulemast pardale',
+    'email.enrollAccount.messageHtml': 'Soovime sind kutsuda Kirjanduslikku Universumisse! Oleme sinu jaoks juba ette valmistanud personaalse konto, kus saad ligi kõikidele Kirjandusliku Universumi funktsioonidele! Palun ühine meiega, et luua järgmine põlvkond loovkirjutamise ja lugemise vahendeid!',
+    'email.enrollAccount.button': 'Ligipääs oma kontole',
+    'email.enrollAccount.signature': 'Ootame teid põnevusega,<br />Literary Universe\'i meeskond',
     // Reset password
-    'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account, please click on the button bellow to continue the process. If you haven\'t made this request you can ignore this e-mail, but it might be a good time to review your security settings.',
-    'email.resetPassword.button': 'Reset password',
+    'email.resetPassword.messageHtml': 'Oleme saanud taotluse teie konto parooli lähtestamiseks. Palun klõpsake allpool oleval nupul, et jätkata protsessi. Kui te ei ole seda taotlust teinud, võite selle e-kirja ignoreerida, kuid võiks olla hea aeg üle vaadata oma turvasätted.',
+    'email.resetPassword.button': 'Lähtesta parool',
     // E-mail verification
-    'email.verifyEmail.messageHtml': 'We are happy to have you on board! Please click the button bellow to verify this e-mail address.',
-    'email.verifyEmail.button': 'Verify this e-mail address',
+    'email.verifyEmail.messageHtml': 'Oleme rõõmsad, et teid pardal on! Palun klõpsake allpool oleval nupul, et kinnitada selle e-posti aadressi.',
+    'email.verifyEmail.button': 'Kinnita see e-posti aadress',
     // Getting premium from admins
-    'email.adminUpgrade.subject': 'Your account has been upgraded',
+    'email.adminUpgrade.subject': 'Teie konto on täiustatud',
     'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     adventurer {Adventurer}
