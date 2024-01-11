450aef2e31d9691dfbad09f572f19ed9236e77b4
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:51:53 +0100

    New translations blog.js (Lithuanian)

--- lt-LT/blog.js
+++ lt-LT/blog.js
@@ -5,49 +5,49 @@ export default {
     user {tinklaraštis}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
+    'blog.create': 'Kurti naują tinklaraštį',
     'blog.settings': `{type, select,
     org {Blog}
     universe {Universe page}
     user {Blog}
     other {}
   } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+    'blog.description': 'Tinklaraščio įvadas',
+    'blog.theme': 'Tema',
+    'blog.theme.info': 'Keisdami temą, galėsite pakeisti puslapio išvaizdą. Ši funkcija šiuo metu nėra pasirengusi.',
     'blog.posts.total': `There {total, plural,
     zero {are no posts}
     one {is one post}
     other {are # posts}
   } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.post.create': 'Kurti naują įrašą',
+    'blog.post.update': 'Redaguoti {title}',
+    'blog.post.text': 'Įrašo tekstas',
+    'common.slug': 'SEO draugiškas adresas',
+    'blog.slug.explained': 'Gražiai atrodantis URL adresas į įrašą. Venkite naudoti specialiuosius simbolius, išskyrus "-".',
+    'blog.publicView': 'Viešas rodinys',
+    'blog.post.notfound': 'Tinklaraščio įrašas nerastas',
     'blog.lists.works': `{type, select,
     universes {Visatos}
     stories {Atskiri istorijų rinkiniai}
     fanfiction {Fanų kūriniai}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': 'Čia pateikiamos atskirių istorijų versijos. Jei istorija buvo parašyta kaip universo dalis, ji bus pateikta universoje.',
     'blog.social.sameAsMain': `Socialiniai ryšiai yra tie patys, kaip nustatyta {type, select,
     organization {organizacijos nuostatosse}
     user {vartotojo profilyje}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
+    'blog.social.website': 'Oficiali svetainė',
+    'blog.settings.universeSettings': 'Tinklaraščiai yra integruoti į universo puslapius su pagrindiniais nustatymais iš universo nustatymų.',
     'blog.settings.organizationSettings': 'Organizacijos tinklaraščiai yra įtraukiami į organizacijos puslapius.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
+    'blogs.lu': 'Oficialūs Literatūrinio Universo tinklaraščiai',
+    'blogs.featured': 'Pasiūlyti tinklaraščiai',
+    'blogs.new': 'Naujausi tinklaraščiai',
+    'blogs.title': 'Literatūrinio Universo tinklaraščiai',
+    'blogs.description': 'Tinklaraščių sąrašas Literatūrinio Universo platformoje.',
+    'blogs.visit': 'Aplankyti tinklaraštį',
     'blog.settingsAria': 'Nustatymų kategorijos tinklaraščiui',
     'blog.settings.blog': 'Tinklaraštis',
     'blog.settings.forum': 'Forumas',
