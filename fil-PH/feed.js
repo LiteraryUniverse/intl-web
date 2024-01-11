5994c3292fae18b3f02d599ac2aaecdba29dde2b
Jan Dvorak <hello@jandvorak.me>
2024-01-12 00:01:41 +0100

    New translations feed.js (Filipino)

--- fil-PH/feed.js
+++ fil-PH/feed.js
@@ -1,8 +1,8 @@
 export default {
     'feed.name': 'Feed',
-    'feed.post.new': 'New post',
-    'feed.post.like': 'Like post',
-    'feed.post.unlike': 'Unlike post',
-    'feed.post.send': 'Post',
-    'feed.empty': 'No entries in the feed.'
+    'feed.post.new': 'Bagong post',
+    'feed.post.like': 'Gusto ang post',
+    'feed.post.unlike': 'Hindi gusto ang post',
+    'feed.post.send': 'I-post',
+    'feed.empty': 'Walang mga entry sa feed.'
 };
