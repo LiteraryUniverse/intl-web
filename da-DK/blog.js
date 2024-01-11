1068349c3bb743f5dd56b86c7aff5bbd720ac268
Jan Dvorak <hello@jandvorak.me>
2024-01-11 22:13:14 +0100

    New translations blog.js (Danish)

--- da-DK/blog.js
+++ da-DK/blog.js
@@ -5,49 +5,49 @@ export default {
     user {en blog}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
+    'blog.create': 'Opret en ny blog',
     'blog.settings': `{type, select,
     org {Blog}
     universe {Universe page}
     user {Blog}
     other {}
   } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+    'blog.description': 'Blog introduktion',
+    'blog.theme': 'Tema',
+    'blog.theme.info': 'Ved at justere temaet kan du ændre udseendet af siden. Denne funktion er ikke klar på nuværende tidspunkt.',
     'blog.posts.total': `There {total, plural,
     zero {are no posts}
     one {is one post}
     other {are # posts}
   } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.post.create': 'Opret et nyt indlæg',
+    'blog.post.update': 'Rediger {title}',
+    'blog.post.text': 'Indlægtekst',
+    'common.slug': 'SEO-venlig adresse',
+    'blog.slug.explained': 'En pæn URL til indlægget. Undgå at bruge specialtegn undtagen "-".',
+    'blog.publicView': 'Offentlig visning',
+    'blog.post.notfound': 'Blogindlæg blev ikke fundet',
     'blog.lists.works': `{type, select,
     universes {Universer}
     stories {Enkeltstående historier}
     fanfiction {Fan Fiction arbejde}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': 'Her er selvstændige historier listet. Hvis historien er skrevet som en del af et univers, vil den blive vist på universets side.',
     'blog.social.sameAsMain': `Sociale links er de samme som indstillet i {type, select,
     organization {organisationsindstillinger}
     user {brugerprofil}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
+    'blog.social.website': 'Officiel hjemmeside',
+    'blog.settings.universeSettings': 'Universblogs er integreret i universsider med deres grundlæggende indstillinger taget fra universindstillinger.',
     'blog.settings.organizationSettings': 'Organisationsblogs er integreret i organisationsider.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
+    'blogs.lu': 'Officielle Literary Universe blogs',
+    'blogs.featured': 'Udvalgte blogs',
+    'blogs.new': 'Nyeste blogs',
+    'blogs.title': 'Blogs fra Literary Universe',
+    'blogs.description': 'Oversigt over blogs på Literary Universe-platformen.',
+    'blogs.visit': 'Besøg blog',
     'blog.settingsAria': 'Indstillingskategorier for bloggen',
     'blog.settings.blog': 'Blog',
     'blog.settings.forum': 'Forum',
