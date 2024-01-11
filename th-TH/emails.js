83c9df3cf1e95c6cfa88a4af07ceece752a0b4cc
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:59:14 +0100

    New translations emails.js (Thai)

--- th-TH/emails.js
+++ th-TH/emails.js
@@ -3,32 +3,32 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
-    'email.enrollAccount.message': `Hello {username}\n\n
-    We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
-    To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'Literary Universe reset password',
-    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
-        Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    'email.enrollAccount.subject': 'เชิญชวนให้เข้าสู่จักรวาลวรรณกรรมสำหรับ {username}',
+    'email.enrollAccount.message': `สวัสดี {username}\n\n
+    เราต้องการเชิญชวนคุณเข้าร่วมกับจักรวาลวรรณกรรม! เราได้สร้างบัญชีส่วนตัวพิเศษสำหรับคุณเพื่อให้คุณสามารถเข้าถึงคุณสมบัติทั้งหมดของจักรวาลวรรณกรรมได้! โปรดร่วมกับเราในการสร้างเครื่องมือเขียนและอ่านที่เป็นเจนเนอเรชันต่อไป!
+    เพื่อเปิดใช้งานบัญชีของคุณ โปรดคลิกลิงก์ด้านล่าง:\n\n{url}`,
+    'email.resetPassword.subject': 'รีเซ็ตรหัสผ่านจักรวาลวรรณกรรม',
+    'email.resetPassword.message': `สวัสดี {username}!\n\n เราได้รับคำขอรีเซ็ตรหัสผ่านสำหรับบัญชีของคุณแล้ว
+        โปรดตามลิงก์ด้านล่างเพื่อรีเซ็ตรหัสผ่านของคุณ:\n\n{url}`,
+    'email.verifyEmail.subject': 'การยืนยันอีเมลจักรวาลวรรณกรรม',
+    'email.verifyEmail.message': 'สวัสดี {username}!\n\n โปรดยืนยันที่อยู่อีเมลของคุณโดยคลิกที่ลิงก์ด้านล่าง:\n\n{url}',
     // New html email keys
-    'email.salutation': 'Hello {username},',
-    'email.welcome': 'Welcome to Literary Universe!',
-    'email.signature': 'Sincerely,<br />Literary Universe team',
+    'email.salutation': 'สวัสดี {username},',
+    'email.welcome': 'ยินดีต้อนรับสู่จักรวาลวรรณกรรม!',
+    'email.signature': 'ด้วยความเคารพอย่างสุภาพ<br />ทีมงานจักรวาลวรรณกรรม',
     // Enrollment
-    'email.enrollAccount.welcome': 'Welcome aboard',
-    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-    'email.enrollAccount.button': 'Access your account',
+    'email.enrollAccount.welcome': 'ยินดีต้อนรับเข้าสู่ทางเรือ',
+    'email.enrollAccount.messageHtml': 'เราต้องการเชิญชวนคุณเข้าร่วมกับจักรวาลวรรณกรรม! เราได้เตรียมบัญชีส่วนตัวสำหรับคุณเองไว้แล้ว เพื่อให้คุณสามารถเข้าถึงคุณสมบัติทั้งหมดของจักรวาลวรรณกรรมได้! โปรดร่วมกับเราในการสร้างเครื่องมือเขียนและอ่านเจนเนอเรชันต่อไป!',
+    'email.enrollAccount.button': 'เข้าสู่บัญชีของคุณ',
     'email.enrollAccount.signature': 'We are looking forward to see you,<br />Literary Universe team',
     // Reset password
-    'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account, please click on the button bellow to continue the process. If you haven\'t made this request you can ignore this e-mail, but it might be a good time to review your security settings.',
-    'email.resetPassword.button': 'Reset password',
+    'email.resetPassword.messageHtml': 'เราได้รับคำขอการรีเซ็ตรหัสผ่านสำหรับบัญชีของคุณ โปรดคลิกที่ปุ่มด้านล่างเพื่อดำเนินการต่อ หากคุณไม่ได้ทำคำขอนี้ คุณสามารถเพิกเฉยต่ออีเมลนี้ได้ แต่นี่อาจเป็นเวลาที่ดีในการตรวจสอบการตั้งค่าความปลอดภัยของคุณ',
+    'email.resetPassword.button': 'รีเซ็ตรหัสผ่าน',
     // E-mail verification
-    'email.verifyEmail.messageHtml': 'We are happy to have you on board! Please click the button bellow to verify this e-mail address.',
-    'email.verifyEmail.button': 'Verify this e-mail address',
+    'email.verifyEmail.messageHtml': 'เรายินดีที่ได้มีคุณเข้าร่วมกับเรา! โปรดคลิกที่ปุ่มด้านล่างเพื่อยืนยันที่อยู่อีเมลนี้',
+    'email.verifyEmail.button': 'ยืนยันที่อยู่อีเมลนี้',
     // Getting premium from admins
-    'email.adminUpgrade.subject': 'Your account has been upgraded',
+    'email.adminUpgrade.subject': 'บัญชีของคุณได้รับการอัปเกรดแล้ว',
     'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     adventurer {Adventurer}
