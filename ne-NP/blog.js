48b60cb2bd797d37c86ac795b2866b2b85e3be8b
Jan Dvorak <hello@jandvorak.me>
2024-01-12 00:04:52 +0100

    New translations blog.js (Nepali)

--- ne-NP/blog.js
+++ ne-NP/blog.js
@@ -5,49 +5,49 @@ export default {
     user {a blog}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
+    'blog.create': 'नयाँ ब्लग सिर्जना गर्नुहोस्',
     'blog.settings': `{type, select,
     org {Blog}
     universe {Universe page}
     user {Blog}
     other {}
   } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+    'blog.description': 'ब्लग परिचय',
+    'blog.theme': 'थिम',
+    'blog.theme.info': 'पृष्ठको देखावट बदल्न सक्ने गर्ने थिममा समायोजन गर्दा। यो सुविधा यस समय तयार छैन।',
     'blog.posts.total': `There {total, plural,
     zero {are no posts}
     one {is one post}
     other {are # posts}
   } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.post.create': 'नयाँ पोस्ट सिर्जना गर्नुहोस्',
+    'blog.post.update': '{title}सम्पादन गर्दै',
+    'blog.post.text': 'पोस्ट पाठ',
+    'common.slug': 'SEO सौहार्दपूर्ण ठेगाना',
+    'blog.slug.explained': 'पोस्टको लागि राम्रो देखिने URL। "-" बाहेक कुनै विशेष वर्णहरू प्रयोग नगर्नुहोस्।',
+    'blog.publicView': 'सार्वजनिक दृश्य',
+    'blog.post.notfound': 'ब्लग पोस्ट भेटिएन',
     'blog.lists.works': `{type, select,
     universes {Universes}
     stories {Standalone stories}
     fanfiction {Fan Fiction work}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': 'यहाँ स्वतंत्र कथाहरू सूचीबद्ध छन्, यदि कथा कसैको यूनिभर्सको भागको रूपमा लेखिएको छ भने त्यसो यूनिभर्स पृष्ठमा सूचीबद्ध हुनेछ।',
     'blog.social.sameAsMain': `Social links are the same as set in {type, select,
     organization {organization settings}
     user {user profile}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
+    'blog.social.website': 'आधिकारिक वेबसाइट',
+    'blog.settings.universeSettings': 'यूनिभर्स ब्लग यूनिभर्स सेटिङहरूबाट लिएर यूनिभर्स पृष्ठमा एकीकृत गरिएका छन्।',
     'blog.settings.organizationSettings': 'संगठनिक ब्लगहरूले संगठन सामग्रीमा एकीकरण गरिएको छ',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
+    'blogs.lu': 'आधिकारिक साहित्यिक यूनिभर्स ब्लगहरू',
+    'blogs.featured': 'विशेष ब्लगहरू',
+    'blogs.new': 'नयाँतम ब्लगहरू',
+    'blogs.title': 'साहित्यिक यूनिभर्सका ब्लगहरू',
+    'blogs.description': 'साहित्यिक यूनिभर्स प्लेटफर्ममा ब्लगहरूको सूचीबद्धीकरण।',
+    'blogs.visit': 'ब्लगमा जानुहोस्',
     'blog.settingsAria': 'ब्लगको सेटिङ्ग श्रेरशचाहरू',
     'blog.settings.blog': 'ब्लग',
     'blog.settings.forum': 'फोरम',
