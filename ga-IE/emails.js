001662e3d2f4bf4c360022053d7ae12df688fabc
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:48:51 +0100

    New translations emails.js (Irish)

--- ga-IE/emails.js
+++ ga-IE/emails.js
@@ -3,32 +3,32 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
-    'email.enrollAccount.message': `Hello {username}\n\n
-    We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
-    To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'Literary Universe reset password',
-    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
-        Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    'email.enrollAccount.subject': 'Cuireadh chuig Domhan Liteartha do {username}',
+    'email.enrollAccount.message': `Dia duit {username}\n\n
+    Ba mhaith linn cuireadh a thabhairt duit chuig Domhan Liteartha! Tá cuntas pearsanta speisialta cruthaithe againn duit áit a mbeidh rochtain agat ar gach gné de Domhan Liteartha! Bainimid taitneamh as do chuideachta leis an gcéad ghlúin eile de threoracha scríbhneoireachta agus léitheoireachta cruthaitheacha!
+    Chun do chuntas a chumasú, níl a dhéanamh ach an nasc thíos a chliceáil:\n\n{url}`,
+    'email.resetPassword.subject': 'Athshocraigh focal faire Domhan Liteartha',
+    'email.resetPassword.message': `Dia duit {username}!\n\n Tá iarraidh tagartha faighte againn chun do phasfhocal a athshocrú do do chuntas.
+        Tabhair faoi ndeara an nasc thíos chun do phasfhocal a athshocrú:\n\n{url}`,
+    'email.verifyEmail.subject': 'Dearbhú ríomhphoist Domhan Liteartha',
+    'email.verifyEmail.message': 'Dia duit {username}!\n\n Cuir do sheoladh ríomhphoist i gcrích trí an nasc thíos a chliceáil:\n\n{url}',
     // New html email keys
-    'email.salutation': 'Hello {username},',
-    'email.welcome': 'Welcome to Literary Universe!',
-    'email.signature': 'Sincerely,<br />Literary Universe team',
+    'email.salutation': 'Dia duit {username},',
+    'email.welcome': 'Fáilte go Domhan Liteartha!',
+    'email.signature': 'Le meas,<br />foireann Domhan Liteartha',
     // Enrollment
-    'email.enrollAccount.welcome': 'Welcome aboard',
-    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-    'email.enrollAccount.button': 'Access your account',
-    'email.enrollAccount.signature': 'We are looking forward to see you,<br />Literary Universe team',
+    'email.enrollAccount.welcome': 'Fáilte go bord',
+    'email.enrollAccount.messageHtml': 'Ba mhaith linn cuireadh a thabhairt duit chuig Domhan Liteartha! Tá cuntas pearsanta cruthaithe againn duit áit a mbeidh rochtain agat ar gach gné de Domhan Liteartha! Bainimid taitneamh as do chuideachta leis an gcéad ghlúin eile de scríbhneoireacht agus léitheoireacht chruthaitheacha!',
+    'email.enrollAccount.button': 'Rochtain ar do chuntas',
+    'email.enrollAccount.signature': 'Táimid ag súil go mór leat a fheiceáil, foireann Literary Universe<br />',
     // Reset password
-    'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account, please click on the button bellow to continue the process. If you haven\'t made this request you can ignore this e-mail, but it might be a good time to review your security settings.',
-    'email.resetPassword.button': 'Reset password',
+    'email.resetPassword.messageHtml': 'Táimid tar éis iarratas a fháil chun do phasfhocal a athshocrú do do chuntas, clicáil ar an gcnaipe thíos chun leanúint ar aghaidh leis an bpróiseas. Má níor rinne tú an iarratas seo, is féidir leat an ríomhphost seo a neamhaird, ach b\'fhéidir go bhfuil sé in am maith do shocruithe slándála a athbhreithniú.',
+    'email.resetPassword.button': 'Athshocraigh pasfhocal',
     // E-mail verification
-    'email.verifyEmail.messageHtml': 'We are happy to have you on board! Please click the button bellow to verify this e-mail address.',
-    'email.verifyEmail.button': 'Verify this e-mail address',
+    'email.verifyEmail.messageHtml': 'Táimid sásta go bhfuil tú ar bord! Cliceáil ar an gcnaipe thíos chun an seoladh ríomhphoist seo a dhearbhú.',
+    'email.verifyEmail.button': 'Dearbhú an seoladh ríomhphoist seo',
     // Getting premium from admins
-    'email.adminUpgrade.subject': 'Your account has been upgraded',
+    'email.adminUpgrade.subject': 'Tá do chuntas uasghrádaithe',
     'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     adventurer {Adventurer}
