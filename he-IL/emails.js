d70b82aa4adb803286605a1f01a76d937ff4dc71
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:49:37 +0100

    New translations emails.js (Hebrew)

--- he-IL/emails.js
+++ he-IL/emails.js
@@ -3,32 +3,32 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
-    'email.enrollAccount.message': `Hello {username}\n\n
-    We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
-    To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'Literary Universe reset password',
-    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
-        Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    'email.enrollAccount.subject': 'הזמנה ליקום הספרותי עבור {username}',
+    'email.enrollAccount.message': `שלום {username}\n\n
+    מעוניינים להזמינך ליקום הספרותי! יצרנו עבורך חשבון אישי מיוחד בו תוכל לגשת לכל התכונות של יקום הספרותי! בוא איתנו ליצור את הדור הבא של כלי כתיבה וקריאה יצירתיים!
+    כדי להפעיל את החשבון שלך, פשוט לחץ על הקישור למטה:\n\n{url}`,
+    'email.resetPassword.subject': 'איפוס סיסמה ליקום הספרותי',
+    'email.resetPassword.message': `שלום {username}!\n\n קיבלנו בקשה לאיפוס הסיסמה שלך עבור החשבון שלך.
+        אנא עקוב אחר הקישור למטה כדי לאפס את הסיסמה שלך:\n\n{url}`,
+    'email.verifyEmail.subject': 'אימות דואר אלקטרוני ליקום הספרותי',
+    'email.verifyEmail.message': 'שלום {username}!\n\n אנא אמת את כתובת הדואר האלקטרוני שלך על ידי לחיצה על הקישור למטה:\n\n{url}',
     // New html email keys
-    'email.salutation': 'Hello {username},',
-    'email.welcome': 'Welcome to Literary Universe!',
-    'email.signature': 'Sincerely,<br />Literary Universe team',
+    'email.salutation': 'שלום {username},',
+    'email.welcome': 'ברוך הבא ליקום הספרותי!',
+    'email.signature': 'בברכה,<br />צוות יקום הספרותי',
     // Enrollment
-    'email.enrollAccount.welcome': 'Welcome aboard',
-    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-    'email.enrollAccount.button': 'Access your account',
-    'email.enrollAccount.signature': 'We are looking forward to see you,<br />Literary Universe team',
+    'email.enrollAccount.welcome': 'ברוך הבא לסירוב!',
+    'email.enrollAccount.messageHtml': 'מעוניינים להזמינך ליקום הספרותי! הכנו עבורך חשבון אישי בו תוכל לגשת לכל התכונות של יקום הספרותי! בוא איתנו ליצור את הדור הבא של כתיבה וקריאה יצירתיים!',
+    'email.enrollAccount.button': 'גש לחשבון שלך',
+    'email.enrollAccount.signature': 'אנחנו מצפים לראותך, צוות היקום הספרותי<br />',
     // Reset password
-    'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account, please click on the button bellow to continue the process. If you haven\'t made this request you can ignore this e-mail, but it might be a good time to review your security settings.',
-    'email.resetPassword.button': 'Reset password',
+    'email.resetPassword.messageHtml': 'קיבלנו בקשה לאיפוס הסיסמה של חשבונך, אנא לחץ על הכפתור למטה כדי להמשיך בתהליך. אם לא ביקשת לאפס את הסיסמה, ניתן להתעלם מהודעה זו, אך זה יכול להיות זמן טוב לבדוק שוב את הגדרות האבטחה שלך.',
+    'email.resetPassword.button': 'איפוס סיסמה',
     // E-mail verification
-    'email.verifyEmail.messageHtml': 'We are happy to have you on board! Please click the button bellow to verify this e-mail address.',
-    'email.verifyEmail.button': 'Verify this e-mail address',
+    'email.verifyEmail.messageHtml': 'אנחנו שמחים לראותך בקרבנו! אנא לחץ על הכפתור למטה כדי לאמת את כתובת הדוא"ל הזו.',
+    'email.verifyEmail.button': 'אמת כתובת דוא"ל זו',
     // Getting premium from admins
-    'email.adminUpgrade.subject': 'Your account has been upgraded',
+    'email.adminUpgrade.subject': 'חשבונך עודכן',
     'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     adventurer {Adventurer}
