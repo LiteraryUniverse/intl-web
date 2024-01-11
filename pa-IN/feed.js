466ebfd1f5c9a0640e7c58125fbbafd88746e057
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:53:09 +0100

    New translations feed.js (Punjabi)

--- pa-IN/feed.js
+++ pa-IN/feed.js
@@ -1,8 +1,8 @@
 export default {
     'feed.name': 'Feed',
-    'feed.post.new': 'New post',
-    'feed.post.like': 'Like post',
-    'feed.post.unlike': 'Unlike post',
-    'feed.post.send': 'Post',
-    'feed.empty': 'No entries in the feed.'
+    'feed.post.new': 'ਨਵਾਂ ਪੋਸਟ',
+    'feed.post.like': 'ਪੋਸਟ ਪਸੰਦ ਕਰੋ',
+    'feed.post.unlike': 'ਪੋਸਟ ਦੀ ਪਸੰਦ ਵਾਪਸ ਲੈਣਾ',
+    'feed.post.send': 'ਪੋਸਟ',
+    'feed.empty': 'ਫੀਡ ਵਿੱਚ ਕੋਈ ਇੰਟਰੀ ਨਹੀਂ ਹੈ।'
 };
