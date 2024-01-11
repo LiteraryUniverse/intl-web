afa77e2ae47c1c50701c8da7ac8d9632bee7467e
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:56:05 +0100

    New translations emails.js (Chinese Traditional)

--- zh-TW/emails.js
+++ zh-TW/emails.js
@@ -3,32 +3,32 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
-    'email.enrollAccount.message': `Hello {username}\n\n
-    We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
-    To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'Literary Universe reset password',
-    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
-        Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    'email.enrollAccount.subject': '邀請您加入文學宇宙！ {username}',
+    'email.enrollAccount.message': `您好， {username}\n\n
+    我們想邀請您加入文學宇宙！我們為您創建了一個特殊的個人帳戶，您可以在其中訪問文學宇宙的所有功能！請加入我們一起創造下一代的創作和閱讀工具！
+    要啟用您的帳戶，只需點擊下面的鏈接：\n\n{url}`,
+    'email.resetPassword.subject': '文學宇宙重設密碼',
+    'email.resetPassword.message': `您好， {username}！\n\n 我們收到了重設您帳戶密碼的請求。
+        請按下面的鏈接重設您的密碼：\n\n{url}`,
+    'email.verifyEmail.subject': '文學宇宙電子郵件驗證',
+    'email.verifyEmail.message': '您好， {username}！\n\n 請通過點擊下面的鏈接驗證您的電子郵件地址：\n\n{url}',
     // New html email keys
-    'email.salutation': 'Hello {username},',
-    'email.welcome': 'Welcome to Literary Universe!',
-    'email.signature': 'Sincerely,<br />Literary Universe team',
+    'email.salutation': '您好， {username}，',
+    'email.welcome': '歡迎來到文學宇宙！',
+    'email.signature': '此致，<br />文學宇宙團隊',
     // Enrollment
-    'email.enrollAccount.welcome': 'Welcome aboard',
-    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-    'email.enrollAccount.button': 'Access your account',
-    'email.enrollAccount.signature': 'We are looking forward to see you,<br />Literary Universe team',
+    'email.enrollAccount.welcome': '歡迎加入',
+    'email.enrollAccount.messageHtml': '我們想邀請您加入文學宇宙！我們已經為您準備了一個個人帳戶，您可以在其中訪問文學宇宙的所有功能！請加入我們一起創造下一代的創作和閱讀！',
+    'email.enrollAccount.button': '訪問您的帳戶',
+    'email.enrollAccount.signature': '我們期待著見到你，<br />Literary Universe 團隊',
     // Reset password
-    'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account, please click on the button bellow to continue the process. If you haven\'t made this request you can ignore this e-mail, but it might be a good time to review your security settings.',
-    'email.resetPassword.button': 'Reset password',
+    'email.resetPassword.messageHtml': '我們收到了重設您帳戶密碼的請求，請點擊下面的按鈕繼續進行此過程。如果您並未發出此請求，您可以忽略此電子郵件，但現在可能是檢查您的安全設定的好時機。',
+    'email.resetPassword.button': '重設密碼',
     // E-mail verification
-    'email.verifyEmail.messageHtml': 'We are happy to have you on board! Please click the button bellow to verify this e-mail address.',
-    'email.verifyEmail.button': 'Verify this e-mail address',
+    'email.verifyEmail.messageHtml': '我們很高興您加入我們！請點擊下面的按鈕驗證此電子郵件地址。',
+    'email.verifyEmail.button': '驗證此電子郵件地址',
     // Getting premium from admins
-    'email.adminUpgrade.subject': 'Your account has been upgraded',
+    'email.adminUpgrade.subject': '您的帳戶已升級',
     'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     adventurer {Adventurer}
