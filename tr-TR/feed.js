39eec0e09babdbadc325a6eb719402205c1e309c
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:55:20 +0100

    New translations feed.js (Turkish)

--- tr-TR/feed.js
+++ tr-TR/feed.js
@@ -1,8 +1,8 @@
 export default {
     'feed.name': 'Feed',
-    'feed.post.new': 'New post',
-    'feed.post.like': 'Like post',
-    'feed.post.unlike': 'Unlike post',
-    'feed.post.send': 'Post',
-    'feed.empty': 'No entries in the feed.'
+    'feed.post.new': 'Yeni gönderi',
+    'feed.post.like': 'Gönderiyi beğen',
+    'feed.post.unlike': 'Gönderiyi beğenme',
+    'feed.post.send': 'Gönderi',
+    'feed.empty': 'Akışta giriş yok.'
 };
