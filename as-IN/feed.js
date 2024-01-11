e2c51dd1999ee48c1b8cfb31da2b84dc52aea6ee
Jan Dvorak <hello@jandvorak.me>
2024-01-12 00:03:39 +0100

    New translations feed.js (Assamese)

--- as-IN/feed.js
+++ as-IN/feed.js
@@ -1,8 +1,8 @@
 export default {
     'feed.name': 'Feed',
-    'feed.post.new': 'New post',
-    'feed.post.like': 'Like post',
-    'feed.post.unlike': 'Unlike post',
-    'feed.post.send': 'Post',
-    'feed.empty': 'No entries in the feed.'
+    'feed.post.new': 'নতুন পোষ্ট',
+    'feed.post.like': 'পোষ্টটি পছন্দ কৰক',
+    'feed.post.unlike': 'পোষ্টটি পছন্দ নকৰক',
+    'feed.post.send': 'পোষ্ট',
+    'feed.empty': 'ফিডত কোনো প্ৰৱেশ নাই।'
 };
