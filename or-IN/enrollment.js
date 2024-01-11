fca2f4680993212ec9331cba5d84f82fed2fa4bd
Jan Dvorak <hello@jandvorak.me>
2024-01-12 00:04:26 +0100

    New translations enrollment.js (Odia)

--- or-IN/enrollment.js
+++ or-IN/enrollment.js
@@ -1,6 +1,6 @@
 export default {
-    'enrollment.title': 'Claim your account!',
-    'enrollment.description': 'Welcome {username}! To gain access to your special account you only need to create password.',
-    'enrollment.submitting': 'Claiming your account...',
-    'enrollment.submit': 'Claim your account!'
+    'enrollment.title': 'ସ୍ଵାମ୍ୟଙ୍କ ଆବାଦନ୍ତା କରନ୍ତୁ!',
+    'enrollment.description': 'ସ୍ଵାଗତ {username}! ଆପଣଙ୍କର ବିଶେଷ ଅକାଉଣ୍ଟକୁ ପାସ୍‌ୱାର୍ଡ ତିଆରି କରିବାକୁ ଆବଶ୍ୟକ ଅଛି ।',
+    'enrollment.submitting': 'ସ୍ଵାମ୍ୟଙ୍କ ଆବାଦନ୍ତା କରାଯାଇଛି...',
+    'enrollment.submit': 'ସ୍ଵାମ୍ୟଙ୍କ ଆବାଦନ୍ତା କରନ୍ତୁ!'
 };
