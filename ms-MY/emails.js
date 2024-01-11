b961439c620fc1645c2dbc928e1c0733606ab87b
Jan Dvorak <hello@jandvorak.me>
2024-01-12 00:00:25 +0100

    New translations emails.js (Malay)

--- ms-MY/emails.js
+++ ms-MY/emails.js
@@ -3,32 +3,32 @@
  */
 /* eslint-disable max-len */
 export default {
-    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
+    'email.enrollAccount.subject': 'Jemputan ke Alam Sastera untuk {username}',
     'email.enrollAccount.message': `Hello {username}\n\n
-    We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
-    To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'Literary Universe reset password',
-    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
-        Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    Kami ingin menjemput anda ke Alam Sastera! Kami telah mencipta akaun peribadi khas untuk anda di mana anda boleh mengakses semua ciri-ciri Alam Sastera! Sila sertai kami dalam mencipta generasi seterusnya alat penulisan kreatif dan membaca!
+    Untuk mengaktifkan akaun anda, sila klik pautan di bawah:\n\n{url}`,
+    'email.resetPassword.subject': 'Tetapan semula kata laluan Alam Sastera',
+    'email.resetPassword.message': `Hello {username}!\n\n Kami telah menerima permintaan untuk menetapkan semula kata laluan akaun anda.
+        Sila ikuti pautan di bawah untuk menetapkan semula kata laluan anda:\n\n{url}`,
+    'email.verifyEmail.subject': 'Pengesahan e-mel Alam Sastera',
+    'email.verifyEmail.message': 'Hello {username}!\n\n Sila sahkan alamat e-mel anda dengan mengklik pautan di bawah:\n\n{url}',
     // New html email keys
     'email.salutation': 'Hello {username},',
-    'email.welcome': 'Welcome to Literary Universe!',
-    'email.signature': 'Sincerely,<br />Literary Universe team',
+    'email.welcome': 'Selamat datang ke Alam Sastera!',
+    'email.signature': 'Salam hormat,<br />pasukan Alam Sastera',
     // Enrollment
-    'email.enrollAccount.welcome': 'Welcome aboard',
-    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-    'email.enrollAccount.button': 'Access your account',
-    'email.enrollAccount.signature': 'We are looking forward to see you,<br />Literary Universe team',
+    'email.enrollAccount.welcome': 'Selamat datang',
+    'email.enrollAccount.messageHtml': 'Kami ingin menjemput anda ke Alam Sastera! Kami telah menyediakan akaun peribadi khas untuk anda di mana anda boleh mengakses semua ciri-ciri Alam Sastera! Sila sertai kami dalam mencipta generasi seterusnya penulisan kreatif dan membaca!',
+    'email.enrollAccount.button': 'Akses akaun anda',
+    'email.enrollAccount.signature': 'Kami menantikan untuk berjumpa dengan anda, pasukan Literary Universe<br />',
     // Reset password
-    'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account, please click on the button bellow to continue the process. If you haven\'t made this request you can ignore this e-mail, but it might be a good time to review your security settings.',
-    'email.resetPassword.button': 'Reset password',
+    'email.resetPassword.messageHtml': 'Kami telah menerima permintaan untuk menetapkan semula kata laluan akaun anda, sila klik pada butang di bawah untuk meneruskan proses ini. Jika anda tidak membuat permintaan ini, anda boleh mengabaikan e-mel ini, tetapi ini mungkin adalah masa yang baik untuk mengkaji semula tetapan keselamatan anda.',
+    'email.resetPassword.button': 'Tetapkan semula kata laluan',
     // E-mail verification
-    'email.verifyEmail.messageHtml': 'We are happy to have you on board! Please click the button bellow to verify this e-mail address.',
-    'email.verifyEmail.button': 'Verify this e-mail address',
+    'email.verifyEmail.messageHtml': 'Kami gembira mempunyai anda bersama kami! Sila klik butang di bawah untuk mengesahkan alamat e-mel ini.',
+    'email.verifyEmail.button': 'Mengesahkan alamat e-mel ini',
     // Getting premium from admins
-    'email.adminUpgrade.subject': 'Your account has been upgraded',
+    'email.adminUpgrade.subject': 'Akaun anda telah dinaik taraf',
     'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     adventurer {Adventurer}
