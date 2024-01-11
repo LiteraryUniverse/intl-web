e5a8527aa243c7c38139e3bd8967ea58c113d8e9
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:55:15 +0100

    New translations blog.js (Turkish)

--- tr-TR/blog.js
+++ tr-TR/blog.js
@@ -5,49 +5,49 @@ export default {
     user {bir blog}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
+    'blog.create': 'Yeni bir blog oluştur',
     'blog.settings': `{type, select,
     org {Blog}
     universe {Universe page}
     user {Blog}
     other {}
   } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+    'blog.description': 'Blog tanıtımı',
+    'blog.theme': 'Tema',
+    'blog.theme.info': 'Sayfanın görünümünü değiştirmek için tema ayarlarını düzenleyebilirsiniz. Bu özellik şu anda kullanıma hazır değil.',
     'blog.posts.total': `There {total, plural,
     zero {are no posts}
     one {is one post}
     other {are # posts}
   } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.post.create': 'Yeni bir gönderi oluştur',
+    'blog.post.update': '{title}düzenleniyor',
+    'blog.post.text': 'Gönderi metni',
+    'common.slug': 'SEO dostu adres',
+    'blog.slug.explained': 'Gönderi için hoş görünümlü bir URL. "-" harici özel karakterler kullanmaktan kaçının.',
+    'blog.publicView': 'Halka açık görünüm',
+    'blog.post.notfound': 'Blog gönderisi bulunamadı',
     'blog.lists.works': `{type, select,
     universes {Evrenler}
     stories {Bağımsız hikayeler}
     fanfiction {Hayran Kurgu çalışması}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': 'Burada bağımsız hikayeler listelenir, eğer hikaye bir evrenin bir parçası olarak yazıldıysa, evren sayfasında listelenir.',
     'blog.social.sameAsMain': `Sosyal bağlantılar {type, select,
     organization {kuruluş ayarları}
     user {kullanıcı profiliyle aynıdır}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
+    'blog.social.website': 'Resmi web sitesi',
+    'blog.settings.universeSettings': 'Edebiyat Evreni blogları, evren ayarlarından temel ayarları alınarak edebiyat evreni sayfalarına entegre edilir.',
     'blog.settings.organizationSettings': 'Kuruluş blogları kuruluş sayfalarına entegre edilir.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
+    'blogs.lu': 'Resmi Edebiyat Evreni blogları',
+    'blogs.featured': 'Öne çıkan bloglar',
+    'blogs.new': 'En yeni bloglar',
+    'blogs.title': 'Edebiyat Evreni Blogları',
+    'blogs.description': 'Edebiyat Evreni platformundaki blogların listesi.',
+    'blogs.visit': 'Blogu Ziyaret Et',
     'blog.settingsAria': 'Blog için ayar kategorileri',
     'blog.settings.blog': 'Blog',
     'blog.settings.forum': 'Forum',
