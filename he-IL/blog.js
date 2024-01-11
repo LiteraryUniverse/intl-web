70a0d4e1b6a0109720fd48eef5beee4b648a2214
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:49:34 +0100

    New translations blog.js (Hebrew)

--- he-IL/blog.js
+++ he-IL/blog.js
@@ -5,49 +5,49 @@ export default {
     user {בלוג}
     other {}
   }.`,
-    'blog.create': 'Create a new blog',
+    'blog.create': 'צור בלוג חדש',
     'blog.settings': `{type, select,
     org {Blog}
     universe {Universe page}
     user {Blog}
     other {}
   } settings`,
-    'blog.description': 'Blog introduction',
-    'blog.theme': 'Theme',
-    'blog.theme.info': 'Adjusting the theme you will be able to change the look of the page. This feature is not ready at this time.',
+    'blog.description': 'מבוא לבלוג',
+    'blog.theme': 'ערכת עיצוב',
+    'blog.theme.info': 'על ידי התאמת הערכת עיצוב תהיה לך אפשרות לשנות את מראה העמוד. תכונה זו אינה מוכנה כרגע.',
     'blog.posts.total': `There {total, plural,
     zero {are no posts}
     one {is one post}
     other {are # posts}
   } in this blog.`,
-    'blog.post.create': 'Create a new post',
-    'blog.post.update': 'Editing {title}',
-    'blog.post.text': 'Post text',
-    'common.slug': 'SEO friendly address',
-    'blog.slug.explained': 'Nice looking url to the post. Avoid using special characters except for "-".',
-    'blog.publicView': 'Public view',
-    'blog.post.notfound': 'Blog post was not found',
+    'blog.post.create': 'צור פוסט חדש',
+    'blog.post.update': 'עריכת {title}',
+    'blog.post.text': 'תוכן הפוסט',
+    'common.slug': 'כתובת ידידותית למנוע המונחים',
+    'blog.slug.explained': 'כתובת url יפה לפוסט. יש להימנע משימוש בתווים מיוחדים חוץ מ"-".',
+    'blog.publicView': 'תצוגה ציבורית',
+    'blog.post.notfound': 'פוסט הבלוג לא נמצא',
     'blog.lists.works': `{type, select,
     universes {יקום}
     stories {סיפורים עצמאיים}
     fanfiction {עבודת פאן-פיקשן}
     other {}
   }`,
-    'blog.lists.stories.disclaimer': 'Listed here are standalone stories, if story was written as a part of a universe then it will be listed on the universe page.',
+    'blog.lists.stories.disclaimer': 'כאן מוצגות סיפורים עצמאיים, אם הסיפור נכתב כחלק מיקומה אז יופיע בדף היקום.',
     'blog.social.sameAsMain': `Social links are the same as set in {type, select,
     organization {organization settings}
     user {user profile}
     other {}
   }`,
-    'blog.social.website': 'Official website',
-    'blog.settings.universeSettings': 'Universe blogs are integrated into universe pages with their basic settings taken from universe settings.',
+    'blog.social.website': 'אתר רשמי',
+    'blog.settings.universeSettings': 'בלוגים של יקום משולבים בדפי היקום ומשתמשים בהגדרות הבסיסיות מהגדרות היקום.',
     'blog.settings.organizationSettings': 'בלוגים של ארגון מוטמעים בעמודי הארגון.',
-    'blogs.lu': 'Official Literary Universe blogs',
-    'blogs.featured': 'Featured blogs',
-    'blogs.new': 'Newest blogs',
-    'blogs.title': 'Blogs of Literary Universe',
-    'blogs.description': 'Listing of blogs on the Literary Universe platform.',
-    'blogs.visit': 'Visit blog',
+    'blogs.lu': 'בלוגים רשמיים של יקום הספרותי',
+    'blogs.featured': 'בלוגים מומלצים',
+    'blogs.new': 'בלוגים החדשים ביותר',
+    'blogs.title': 'בלוגים של היקום הספרותי',
+    'blogs.description': 'רשימת בלוגים בפלטפורמה של היקום הספרותי.',
+    'blogs.visit': 'ביקור בבלוג',
     'blog.settingsAria': 'קטגוריות הגדרות עבור הבלוג',
     'blog.settings.blog': 'בלוג',
     'blog.settings.forum': 'פורום',
