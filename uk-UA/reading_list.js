39c141fe0fe4047bdfabb1dde4e94bd25c908faa
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:56:00 +0100

    New translations reading_list.js (Ukrainian)

--- uk-UA/reading_list.js
+++ uk-UA/reading_list.js
@@ -1,8 +1,8 @@
 export default {
   'readingList.title': 'Reading list',
   'readingList.type.label': 'List type',
-  'readingList.type.stories': 'Stories',
-  'readingList.type.universes': 'Universes',
+  'readingList.type.stories': 'Iсторії',
+  'readingList.type.universes': 'Всесвіти',
   'readingList.ordered.label': 'Ranked list',
   'readingList.button.add': 'Add to reading list',
   'readingList.selectList': 'Select reading list',
