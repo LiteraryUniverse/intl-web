2d976d77461cc5fe46b4f7d67593222ae53208fb
Jan Dvorak <hello@jandvorak.me>
2024-01-12 00:02:29 +0100

    New translations enrollment.js (Malayalam)

--- ml-IN/enrollment.js
+++ ml-IN/enrollment.js
@@ -1,6 +1,6 @@
 export default {
-    'enrollment.title': 'Claim your account!',
+    'enrollment.title': 'നിങ്ങളുടെ അക്കൗണ്ട് ക്ലെയിമുചെയ്യുക!',
     'enrollment.description': 'Welcome {username}! To gain access to your special account you only need to create password.',
-    'enrollment.submitting': 'Claiming your account...',
-    'enrollment.submit': 'Claim your account!'
+    'enrollment.submitting': 'നിങ്ങളുടെ അക്കൗണ്ട് ക്ലെയിം ചെയ്യുന്നു...',
+    'enrollment.submit': 'നിങ്ങളുടെ അക്കൗണ്ട് ക്ലെയിമുചെയ്യുക!'
 };
