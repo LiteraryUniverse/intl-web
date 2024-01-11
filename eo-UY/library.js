789c82201cd2e518f3aeec07ac3024f3a00b5efc
Jan Dvorak <hello@jandvorak.me>
2024-01-12 00:01:27 +0100

    New translations library.js (Esperanto)

--- eo-UY/library.js
+++ eo-UY/library.js
@@ -4,6 +4,6 @@ export default {
     'library.bought': 'Bought stories',
     'library.stories': 'Followed stories',
     'library.universes': 'Followed universes',
-    'bookmark.continueReading': 'Continue reading',
+    'bookmark.continueReading': 'Legi plu',
     'bookmark.statusSummary': 'You have read {readPercent, number, ::percent} of the story.'
 };
