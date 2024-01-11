5dc0cf6dc955c52b6e667d752b2eabfe9b31539d
Jan Dvorak <hello@jandvorak.me>
2024-01-11 23:46:31 +0100

    New translations library.js (Arabic)

--- ar-SA/library.js
+++ ar-SA/library.js
@@ -4,6 +4,6 @@ export default {
     'library.bought': 'Bought stories',
     'library.stories': 'Followed stories',
     'library.universes': 'Followed universes',
-    'bookmark.continueReading': 'Continue reading',
+    'bookmark.continueReading': 'متابعة القراءة',
     'bookmark.statusSummary': 'You have read {readPercent, number, ::percent} of the story.'
 };
