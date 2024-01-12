f87d94f8e7e32700a56042444b653e58c66be7bc
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:35:37 +0100

    New translations comments.js (Assamese)

--- as-IN/comments.js
+++ as-IN/comments.js
@@ -1,10 +1,10 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
-    'comments.total': `There {count, plural,
-    =0 {are no comments}
-    one {is 1 comment}
-    other {are # comments}
-  }.`,
-    'comments.show.older': 'Show older comments'
+    'comment.write': 'আপোনাৰ মন্তব্য লিখক',
+    'comments.none': 'দুঃখিত, কোনো মন্তব্য দেখুৱাবলৈ নাই।',
+    'comments.total': `এইটোত {count, plural,
+    =0 {কোনো মন্তব্য নাই}
+    one {1 টা মন্তব্য আছে}
+    other {# টা মন্তব্য আছে}
+  }।`,
+    'comments.show.older': 'পুৰণি মন্তব্যসমূহ দেখুৱাওক'
 };
