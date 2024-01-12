a3b347f3831dd53df209450eb71fac965992b0c9
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:11:46 +0100

    New translations audit.js (Afrikaans)

--- af-ZA/audit.js
+++ af-ZA/audit.js
@@ -2,5 +2,5 @@ export default {
     'auditLog.header.date': 'Datum',
     'auditLog.header.action': 'Wat',
     'auditLog.header.group': 'Groep',
-    'auditLog.header.user': 'Who'
+    'auditLog.header.user': 'Wie'
 };
