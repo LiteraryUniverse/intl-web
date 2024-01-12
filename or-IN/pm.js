0761886e8fc800b456d82ff8b46cf19c913a8ce9
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:36:39 +0100

    New translations pm.js (Odia)

--- or-IN/pm.js
+++ or-IN/pm.js
@@ -1,25 +1,25 @@
 export default {
-    'pm.conversation': 'Conversation',
-    'pm.showolder': 'Show older messages',
-    'pm.errors.saysomething': 'You should really say something...',
-    'pm.errors.addrecipient': 'You need to add at least one other user.',
-    'pm.message': 'Message',
-    'pm.send.message': 'Send message',
-    'pm.messages.own': 'Your messages',
-    'pm.conversations.none': 'You are currently not conversing with anyone.',
-    'pm.to': 'To:',
-    'pm.with': 'Conversation with:',
-    'pm.send': 'Send',
-    'pm.conversations.unreadCount': `You have {count, plural,
-    zero {no unread conversations}
-    one {one unread conversation}
-    other {# unread conversations}
+    'pm.conversation': 'ଆଲୋଚନା',
+    'pm.showolder': 'ପୁରୁଣା ସନ୍ଦେଶ ଦେଖାନ୍ତୁ',
+    'pm.errors.saysomething': 'ଆପଣ ନିଜରେ କିଛି କହିବା ପାଇଁ ପରିଶ୍ରମ କରିବା ଆବଶ୍ୟକ...',
+    'pm.errors.addrecipient': 'ଆପଣ କମାନ୍ଡରେ କମାନ୍ଡ ଯୋଗାଇବା ପାଇଁ କମିଉଥିବା କମାନ୍ଡରେ ଅନ୍ୟକୋଣ ବ୍ୟବହାରକାରୀ ଯୋଗାଇବା ଆବଶ୍ୟକ।',
+    'pm.message': 'ସନ୍ଦେଶ',
+    'pm.send.message': 'ସନ୍ଦେଶ ପଠାନ୍ତୁ',
+    'pm.messages.own': 'ଆପଣଙ୍କ ସନ୍ଦେଶ',
+    'pm.conversations.none': 'ଆପଣ ବର୍ତ୍ତମାନରେ କିମ୍ବା କେଉଁଠି ସଙ୍ଗଠନ ନାହିଁ।',
+    'pm.to': 'ପାଠାନ୍ତୁ:',
+    'pm.with': 'ସଙ୍ଗଠନରେ ଆଲୋଚନା:',
+    'pm.send': 'ପଠାନ୍ତୁ',
+    'pm.conversations.unreadCount': `ଆପଣଙ୍କୁ {count, plural,
+    zero {ପଠାନ୍ତୁ ସନ୍ଦେଶ}
+    one {ଏକ ପଠାନ୍ତୁ ସନ୍ଦେଶ}
+    other {# ପଠାନ୍ତୁ ସନ୍ଦେଶ}
   }.`,
-    'pm.form.minimumParticipants': 'You need to choose a recipient.',
-    'pm.form.maxParticipants': `You can have maximum of {maxPeople, plural,
-    one {one person}
-    other {# people}
-  } in a conversation.`,
-    'pm.new.aria': 'New conversation',
-    'pm.new.buttonTitle': 'Start a new conversation'
+    'pm.form.minimumParticipants': 'ଆପଣ ଏକ ପଠାନ୍ତୁ ସନ୍ଦେଶ ଚୟନ କରିବା ଆବଶ୍ୟକ।',
+    'pm.form.maxParticipants': `ଆପଣ {maxPeople, plural,
+    one {ଏକ ବ୍ୟକ୍ତି}
+    other {# ବ୍ୟକ୍ତି}
+  } ମଧ୍ୟରେ ଅଧିକତମ ରଖିପାରିବେ ।`,
+    'pm.new.aria': 'ନୂତନ ଆଲୋଚନା',
+    'pm.new.buttonTitle': 'ନୂତନ ଆଲୋଚନା ଶୁରୁ କରନ୍ତୁ'
 };
