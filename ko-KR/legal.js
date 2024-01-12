acc53f0be7d2fa61a4148c4de3bfdb804ab234e6
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:19:16 +0100

    New translations legal.js (Korean)

--- ko-KR/legal.js
+++ ko-KR/legal.js
@@ -1,11 +1,11 @@
 export default {
-    'legal.modal.intro': 'Sorry to interrupt. But before we let you go any further, we are legally bound, to make sure that you are aware of and agree with our Terms of Service, Privacy Policy and Copyright Agreement.',
-    'legal.modal.agree': 'I Agree',
-    'legal.modal.disagree': 'I Disagree',
-    'legal.modal.register': 'You won\'t see this anymore if you register.',
-    'legal.modal.goToSettings': 'Go to your settings',
-    'legal.modal.oneLastThing': 'One last thing...',
-    'legal.modal.finish': 'Finish',
-    'legal.modal.existingUser': 'Already registered?',
-    'legal.modal.pleaseLogin': 'Please login.'
+    'legal.modal.intro': '계속 진행하기 전에 우리는 법적으로 귀하가 우리의 서비스 약관, 개인정보 보호정책 및 저작권 동의에 대해 인식하고 동의하는지 확인해야 합니다.',
+    'legal.modal.agree': '동의합니다',
+    'legal.modal.disagree': '동의하지 않습니다',
+    'legal.modal.register': '등록하면 더 이상 이 메시지를 보지 않을 수 있습니다.',
+    'legal.modal.goToSettings': '설정으로 이동',
+    'legal.modal.oneLastThing': '마지막으로...',
+    'legal.modal.finish': '완료',
+    'legal.modal.existingUser': '이미 등록되었습니까?',
+    'legal.modal.pleaseLogin': '로그인해주세요.'
 };
