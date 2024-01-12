007bfca731f55bb7c7ac8cf03353ecf21eaef053
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:24:56 +0100

    New translations reading_list.js (Ukrainian)

--- uk-UA/reading_list.js
+++ uk-UA/reading_list.js
@@ -1,26 +1,26 @@
 export default {
-  'readingList.title': 'Reading list',
-  'readingList.type.label': 'List type',
+  'readingList.title': 'Список для читання',
+  'readingList.type.label': 'Тип списку',
   'readingList.type.stories': 'Iсторії',
   'readingList.type.universes': 'Всесвіти',
-  'readingList.ordered.label': 'Ranked list',
-  'readingList.button.add': 'Add to reading list',
-  'readingList.selectList': 'Select reading list',
-  'readingList.create': 'Create a new reading list',
-  'readingList.create.aria': 'Modal with settings for a new reading list',
-  'readingList.edit.aria': 'Modal to edit reading list\'s settings',
-  'readingList.library.title': 'Your reading lists',
-  'readingList.public.label': 'Is publicly visible?',
-  'readingList.addItem': `Add {type, select,
-    universes {universe}
-    stories {story}
-    other {stories and universes}
+  'readingList.ordered.label': 'Впорядкований список',
+  'readingList.button.add': 'Додати до списку для читання',
+  'readingList.selectList': 'Вибрати список для читання',
+  'readingList.create': 'Створити новий список для читання',
+  'readingList.create.aria': 'Модальне вікно з налаштуваннями для нового списку для читання',
+  'readingList.edit.aria': 'Модальне вікно для редагування налаштувань списку для читання',
+  'readingList.library.title': 'Ваші списки для читання',
+  'readingList.public.label': 'Чи є публічно видимим?',
+  'readingList.addItem': `Додати {type, select,
+    universes {всесвіт}
+    stories {історію}
+    other {історії та всесвіти}
   }`,
-  'readingList.addItem.modalAria': `Modal to add {type, select,
-    universes {universes}
-    stories {stories}
-    other {stories and universes}
-  } to the reading list.`,
-  'readingList.addItem.fromLibrary': 'From library',
-  'readingList.button.added': 'Added'
+  'readingList.addItem.modalAria': `Модальне вікно для додавання {type, select,
+    universes {всесвітів}
+    stories {історій}
+    other {історій та всесвітів}
+  } до списку для читання.`,
+  'readingList.addItem.fromLibrary': 'З бібліотеки',
+  'readingList.button.added': 'Додано'
 };
