970a3546727b8350e0dae68d105382d369c91da7
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:34:09 +0100

    New translations reading_list.js (Malayalam)

--- ml-IN/reading_list.js
+++ ml-IN/reading_list.js
@@ -1,26 +1,26 @@
 export default {
-  'readingList.title': 'Reading list',
-  'readingList.type.label': 'List type',
-  'readingList.type.stories': 'Stories',
-  'readingList.type.universes': 'Universes',
-  'readingList.ordered.label': 'Ranked list',
-  'readingList.button.add': 'Add to reading list',
-  'readingList.selectList': 'Select reading list',
-  'readingList.create': 'Create a new reading list',
-  'readingList.create.aria': 'Modal with settings for a new reading list',
-  'readingList.edit.aria': 'Modal to edit reading list\'s settings',
-  'readingList.library.title': 'Your reading lists',
-  'readingList.public.label': 'Is publicly visible?',
-  'readingList.addItem': `Add {type, select,
-    universes {universe}
-    stories {story}
-    other {stories and universes}
+  'readingList.title': 'പട്ടിക വായനാപട്ടി',
+  'readingList.type.label': 'പട്ടിക തരം',
+  'readingList.type.stories': 'കഥകൾ',
+  'readingList.type.universes': 'ബ്രഹ്മാണ്ഡങ്ങൾ',
+  'readingList.ordered.label': 'അടയാളപ്പെടുത്തിയ പട്ടിക',
+  'readingList.button.add': 'പട്ടികയിൽ ചേർക്കുക',
+  'readingList.selectList': 'പട്ടിക തിരഞ്ഞെടുക്കുക',
+  'readingList.create': 'പുതിയ പട്ടിക സൃഷ്ടിക്കുക',
+  'readingList.create.aria': 'പുതിയ പട്ടികയുടെ ക്രമീകരണങ്ങൾക്ക് സജ്ജമാക്കാൻ ഉള്ള മോഡൽ',
+  'readingList.edit.aria': 'പട്ടികയുടെ ക്രമീകരണങ്ങൾ എഡിറ്റുചെയ്യാൻ ഉള്ള മോഡൽ',
+  'readingList.library.title': 'നിങ്ങളുടെ വായനാപട്ടികൾ',
+  'readingList.public.label': 'പൊതുവായി കാണാനുള്ളതോ?',
+  'readingList.addItem': `ചേർക്കുക {type, select,
+    universes {പ്രപഞ്ചം}
+    stories {കഥ}
+    other {കഥകൾക്കും പ്രപഞ്ചങ്ങൾക്കും}
   }`,
   'readingList.addItem.modalAria': `Modal to add {type, select,
     universes {universes}
     stories {stories}
     other {stories and universes}
   } to the reading list.`,
-  'readingList.addItem.fromLibrary': 'From library',
-  'readingList.button.added': 'Added'
+  'readingList.addItem.fromLibrary': 'ലൈബ്രറിയിൽ നിന്ന്',
+  'readingList.button.added': 'ചേർത്തു'
 };
