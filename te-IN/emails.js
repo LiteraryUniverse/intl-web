3649a5258bc344c37f06da86ce9688f0c27de663
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:31:29 +0100

    New translations emails.js (Telugu)

--- te-IN/emails.js
+++ te-IN/emails.js
@@ -41,7 +41,7 @@ export default {
     storyteller {Storyteller}
     other {}
   } tier for the next {days} days. Enjoy!`,
-    'email.verification': 'E-mail verification',
-    'email.verifying': 'Verifying your e-mail. This will take just a moment...',
-    'email.verified': 'Great news! Your e-mail has been verified!'
+    'email.verification': 'ఇమెయిల్ ధృవీకరణ',
+    'email.verifying': 'మీ ఇమెయిల్ ధృవీకరించడానికి కొనసాగుతున్నాం. ఇది కొనసాగుతుంది...',
+    'email.verified': 'మంచి సమాచారం! మీ ఇమెయిల్ ధృవీకరించబడింది!'
 };    /* eslint-enable max-len */
