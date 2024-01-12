4a2c5d3c6b6c93f7b23ff947ebeb401534ead23e
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:32:53 +0100

    New translations comments.js (Filipino)

--- fil-PH/comments.js
+++ fil-PH/comments.js
@@ -1,10 +1,10 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
+    'comment.write': 'Isulat ang iyong komento',
+    'comments.none': 'Pasensya na, walang mga komento na ipinapakita.',
     'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-    'comments.show.older': 'Show older comments'
+    'comments.show.older': 'Ipakita ang mas lumang mga komento'
 };
