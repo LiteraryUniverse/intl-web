04181de2cf84c34e7cc746a429a9c14a332c3d33
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:15:46 +0100

    New translations roadmap.js (Irish)

--- ga-IE/roadmap.js
+++ ga-IE/roadmap.js
@@ -1,37 +1,37 @@
 export default {
-  'roadmap.ariaTimeOptions': 'Selections for roadmap items based on their status.',
-  'roadmap.state.backlog': 'Backlog',
-  'roadmap.state.inprogress': 'In-progress',
-  'roadmap.state.preview': 'Preview',
-  'roadmap.state.beta': 'Beta',
-  'roadmap.state.ga': 'General availability',
+  'roadmap.ariaTimeOptions': 'Roghanna do mhoíreolaíochtaí bóthair bunaithe ar a gcumas.',
+  'roadmap.state.backlog': 'Liosta feithimh',
+  'roadmap.state.inprogress': 'Ar siúl',
+  'roadmap.state.preview': 'Réamhamharc',
+  'roadmap.state.beta': 'Beidh',
+  'roadmap.state.ga': 'Ginearálta atá ar fáil',
   'roadmap.eta.q1.short': 'Q1',
   'roadmap.eta.q2.short': 'Q2',
   'roadmap.eta.q3.short': 'Q3',
   'roadmap.eta.q4.short': 'Q4',
-  'roadmap.eta.q1.long': 'First quarter',
-  'roadmap.eta.q2.long': 'Second quarter',
-  'roadmap.eta.q3.long': 'Third quarter',
-  'roadmap.eta.q4.long': 'Fourth quarter',
-  'roadmap.backlog': 'Backlog',
-  'roadmap.history': 'History of development',
-  'roadmap.item.inBacklog': 'We are planning to develop this feature in the future.',
-  'roadmap.item.inProgress': 'The development of the feature started {startedAt, date}.',
-  'roadmap.item.inBeta': 'This feature has entered beta on {betaAt, date}.',
-  'roadmap.item.inPreview': 'This feature became available to Storyteller subscribers on {previewAt, date}.',
-  'roadmap.item.inGa': 'This feature has become generally available on {gaAt, date}.',
-  'roadmap.item.originalIssue': 'This feature was originally suggested by our users.',
-  'roadmap.eta.mapTitle': `Planned for {quarter, select,
-    Q1 {Q1}
-    Q2 {Q2}
-    Q3 {Q3}
-    Q4 {Q4}
+  'roadmap.eta.q1.long': 'Céad ráithe',
+  'roadmap.eta.q2.long': 'Dara ráithe',
+  'roadmap.eta.q3.long': 'Tríú ráithe',
+  'roadmap.eta.q4.long': 'Ceathrú ráithe',
+  'roadmap.backlog': 'Liosta feithimh',
+  'roadmap.history': 'Stair mheastacháin',
+  'roadmap.item.inBacklog': 'Táimid ag pleanáil an gné seo a fhorbairt sa todhchaí.',
+  'roadmap.item.inProgress': 'Tá forbairt an ghné tosaithe {startedAt, date}.',
+  'roadmap.item.inBeta': 'Tá an ghné seo i mbeiteá ar an {betaAt, date}.',
+  'roadmap.item.inPreview': 'Tá an ghné seo ar fáil do díoltóirí Scéalaíochta anois ar an {previewAt, date}.',
+  'roadmap.item.inGa': 'Tá an ghné seo ar fáil go ginearálta anois ar an {gaAt, date}.',
+  'roadmap.item.originalIssue': 'Mhol ár úsáideoirí an ghné seo sa tús.',
+  'roadmap.eta.mapTitle': `Pleanáilte don {quarter, select,
+    Q1 {R1}
+    Q2 {R2}
+    Q3 {R3}
+    Q4 {R4}
     other {}
   } {year, date, ::yyyy}`,
-  'roadmap.eta.mapTitleFinished': 'Finished in {time, date, ::MMMM ::yyyy}',
-  'roadmap.back': 'Back to roadmap',
-  'roadmap.seeBacklog': 'Check out what other things we plan to do',
-  'roadmap.seeFinished': 'If you like to dig more into our history, we have a nice overview ready for you',
-  'roadmap.planned': 'Planned',
-  'roadmap.finished': 'Finished'
+  'roadmap.eta.mapTitleFinished': 'Críochnaithe ar {time, date, ::MMMM ::yyyy}',
+  'roadmap.back': 'Ar ais chuig an bóthar pleanála',
+  'roadmap.seeBacklog': 'Féach cad eile atáimid ag pleanáil a dhéanamh',
+  'roadmap.seeFinished': 'Má thaitníonn leat tuilleadh a fháil amach faoinár stair, tá léarscáil deas réidh duit',
+  'roadmap.planned': 'Pleanáilte',
+  'roadmap.finished': 'Críochnaithe'
 };
