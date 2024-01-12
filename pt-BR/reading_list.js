b2b4990eff5d18abc70f9130b963c532a49330bd
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:26:31 +0100

    New translations reading_list.js (Portuguese, Brazilian)

--- pt-BR/reading_list.js
+++ pt-BR/reading_list.js
@@ -1,26 +1,26 @@
 export default {
-  'readingList.title': 'Reading list',
-  'readingList.type.label': 'List type',
+  'readingList.title': 'Lista de leitura',
+  'readingList.type.label': 'Tipo de lista',
   'readingList.type.stories': 'Histórias',
   'readingList.type.universes': 'Universos',
-  'readingList.ordered.label': 'Ranked list',
-  'readingList.button.add': 'Add to reading list',
-  'readingList.selectList': 'Select reading list',
-  'readingList.create': 'Create a new reading list',
-  'readingList.create.aria': 'Modal with settings for a new reading list',
-  'readingList.edit.aria': 'Modal to edit reading list\'s settings',
-  'readingList.library.title': 'Your reading lists',
-  'readingList.public.label': 'Is publicly visible?',
-  'readingList.addItem': `Add {type, select,
-    universes {universe}
-    stories {story}
-    other {stories and universes}
+  'readingList.ordered.label': 'Lista classificada',
+  'readingList.button.add': 'Adicionar à lista de leitura',
+  'readingList.selectList': 'Selecionar lista de leitura',
+  'readingList.create': 'Criar uma nova lista de leitura',
+  'readingList.create.aria': 'Modal com configurações para uma nova lista de leitura',
+  'readingList.edit.aria': 'Modal para editar as configurações da lista de leitura',
+  'readingList.library.title': 'Suas listas de leitura',
+  'readingList.public.label': 'É visível publicamente?',
+  'readingList.addItem': `Adicionar {type, select,
+    universes {universo}
+    stories {história}
+    other {histórias e universos}
   }`,
-  'readingList.addItem.modalAria': `Modal to add {type, select,
-    universes {universes}
-    stories {stories}
-    other {stories and universes}
-  } to the reading list.`,
-  'readingList.addItem.fromLibrary': 'From library',
-  'readingList.button.added': 'Added'
+  'readingList.addItem.modalAria': `Modal para adicionar {type, select,
+    universes {universos}
+    stories {histórias}
+    other {histórias e universos}
+  } à lista de leitura.`,
+  'readingList.addItem.fromLibrary': 'Da biblioteca',
+  'readingList.button.added': 'Adicionado'
 };
