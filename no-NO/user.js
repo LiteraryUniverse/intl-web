7d143d1179ef962d9ae388ac68195b4c584926ba
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:21:00 +0100

    New translations user.js (Norwegian)

--- no-NO/user.js
+++ no-NO/user.js
@@ -1,26 +1,26 @@
 export default {
-    'user.friend.request.add': 'Add to friends',
-    'user.friend.requests': `You have {num, number} new friend {num, plural,
-    zero {requests}
-    one {request}
-    many {requests}
-    other {requests}
+    'user.friend.request.add': 'Legg til som venn',
+    'user.friend.requests': `Du har {num, number} nye venn {num, plural,
+    zero {forespørsler}
+    one {forespørsel}
+    many {forespørsler}
+    other {forespørsler}
   }`,
-    'user.friend.unfriend': 'Unfriend',
-    'user.block': 'Block',
-    'user.unblock': 'Unblock',
+    'user.friend.unfriend': 'Fjern som venn',
+    'user.block': 'Blokker',
+    'user.unblock': 'Fjern blokkering',
     'user.joined': `{gender, select,
-    male {Joined}
-    female {Joined}
-    other {Joined}} on {date, date, long}`,
-    'user.friend.request.accept': 'Accept friendship request',
-    'user.friend.request.cancel': 'Cancel friendship request',
-    'user.friend.request.deny': 'Deny friendship',
-    'user.listing': 'User listing',
-    'user.profile.visit': 'Visit {user}\'s profile.',
-    'user.profile.avatar': '{username}\'s avatar',
-    'user.friend.list.requests': 'Friendships requests',
-    'user.friend.norequests': 'You have no requests for friendship.',
+    male {ble medlem}
+    female {ble medlem}
+    other {ble medlem}} den {date, date, long}`,
+    'user.friend.request.accept': 'Godta venneforespørsel',
+    'user.friend.request.cancel': 'Avbryt venneforespørsel',
+    'user.friend.request.deny': 'Avslå vennskap',
+    'user.listing': 'Brukerliste',
+    'user.profile.visit': 'Besøk profilen til {user}.',
+    'user.profile.avatar': '{username}sin avatar',
+    'user.friend.list.requests': 'Venneforespørsler',
+    'user.friend.norequests': 'Du har ingen venneforespørsler.',
     'user.dashboardWelcome': `Good {dayPart, select,
     morning {morning}
     afternoon {afternoon}
@@ -28,26 +28,26 @@ export default {
     night {night}
     other {day}
   } {displayName}!`,
-    'user.verification.publishedAuthor': 'This author has been published the traditional way',
-    'user.verification.luAuthor': 'Literary Universe based author of note',
-    'user.verification.luEmployee': 'Literary Universe employee',
-    'user.verification.publicFigure': 'Verified public figure',
+    'user.verification.publishedAuthor': 'Denne forfatteren har blitt publisert på tradisjonell måte',
+    'user.verification.luAuthor': 'Forfatter av note basert på Literary Universe',
+    'user.verification.luEmployee': 'Ansatte i Literary Universe',
+    'user.verification.publicFigure': 'Verifisert offentlig person',
     'profile.storiesCount': `{count, number} {count, plural,
-    one {story}
-    other {stories}
+    one {historie}
+    other {historier}
   }`,
     'profile.universesCount': `{count, number} {count, plural,
-    one {universe}
-    other {universes}
+    one {univers}
+    other {universer}
   }`,
     'profile.fanFictionsCount': `{count, number} {count, plural,
     one {fan fiction}
     other {fan fictions} 
   }`,
-    'profile.works': 'Works',
+    'profile.works': 'Verk',
     // As in creative/literary works
-    'profile.wishlist': 'Wishlist',
-    'profile.storiesBy': 'Stories created by {username}',
-    'profile.universesBy': 'Universes created by {username}',
-    'profile.fanFictionBy': 'Fan Fiction created by {username}'
+    'profile.wishlist': 'Ønskeliste',
+    'profile.storiesBy': 'Historier opprettet av {username}',
+    'profile.universesBy': 'Universer opprettet av {username}',
+    'profile.fanFictionBy': 'Fan Fiction opprettet av {username}'
 };
