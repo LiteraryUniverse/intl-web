1c4c9e77446be1b8df4cb8c70cbb8d2ed673d1a5
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:18:23 +0100

    New translations discovery.js (Georgian)

--- ka-GE/discovery.js
+++ ka-GE/discovery.js
@@ -17,34 +17,34 @@ export default {
         other {# წუთი}
     } საათის კითხვაზე.`,
     'story.estimatedReadingTime.explained': 'საშუალო კითხვის სიჩქარეს {avgReadingNum, number} სიტყვა წუთზე დაცვის გასაჩერებად.',
-    'discover.search.text': 'Search term',
-    'discover.searchStory.title': 'Search for a story',
-    'discover.searchStory.description': 'Search for a stories',
-    'discover.searchUniverse.title': 'Search for a universe',
-    'discover.searchUniverse.description': 'Search for a universes',
-    'discover.search.storyStatus': 'Story progress status',
-    'story.status.complete': 'Complete',
-    'story.status.inProgress': 'In-progress',
-    'discover.search.universeConnection': 'Connection of story to universe',
-    'story.universeRelation.standalone': 'Standalone story',
-    'story.universeRelation.fanFiction': 'Fan Fiction',
-    'story.universeRelation.universe': 'Part of a universe',
-    'discover.search.rating': 'Age rating',
-    'discover.search.license': 'Copyright license',
-    'discover.search.language': 'Language',
+    'discover.search.text': 'ძიების ტერმინი',
+    'discover.searchStory.title': 'ისტორიის ძიება',
+    'discover.searchStory.description': 'ისტორიის ძიება',
+    'discover.searchUniverse.title': 'უნივერსის ძიება',
+    'discover.searchUniverse.description': 'უნივერსების ძიება',
+    'discover.search.storyStatus': 'ისტორიის პროგრესის სტატუსი',
+    'story.status.complete': 'დასრულებული',
+    'story.status.inProgress': 'პროცესში',
+    'discover.search.universeConnection': 'ისტორიის კავშირი უნივერსულისთვის',
+    'story.universeRelation.standalone': 'ცივი ისტორია',
+    'story.universeRelation.fanFiction': 'ფან ფიქშენი',
+    'story.universeRelation.universe': 'უნივერსულის ნაწილი',
+    'discover.search.rating': 'ასაკის რეიტინგი',
+    'discover.search.license': 'საავტორო უფლება',
+    'discover.search.language': 'ენა',
     'discover.search.resultsTotal.universe': `{results, plural,
-        =0 {No universes match your parameters}
-        one {1 universe found}
-        other {# universes found}
+        =0 {არცერთი უნივერსი არ ემთხვევა თქვენს პარამეტრებს}
+        one {1 უნივერსი მოიძებნა}
+        other {# უნივერსი მოიძებნა}
     }.`,
     'discover.search.resultsTotal.story': `{results, plural,
-        =0 {No stories match your parameters}
-        one {1 story found}
-        other {# stories found}
+        =0 {არცერთი ისტორია არ ემთხვევა თქვენს პარამეტრებს}
+        one {1 ისტორია მოიძებნა}
+        other {# ისტორია მოიძებნა}
     }.`,
-    'discover.viewSelection.aria': 'View selection',
-    'discover.searchDisplay.covers': 'Covers listing',
-    'discover.searchDisplay.list': 'Compact list',
+    'discover.viewSelection.aria': 'ხელსაწყოთა არჩევა',
+    'discover.searchDisplay.covers': 'გარეკანის ჩამონათვალი',
+    'discover.searchDisplay.list': 'კომპაქტური სია',
     'discover.includesAds': 'ამ ცხოვრებაში მოცულო პროდუქტის განცხადება ან სხვა რეკლამის ფორმათედ არაა.',
     'discovery.featured.universes': 'გამოცემული უნივერსები',
     'discovery.featured.stories': 'გამოცემული ამბები',
