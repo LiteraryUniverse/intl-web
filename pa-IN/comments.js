74c1740c2d72f946058e3b00ce5a20307b0dbf99
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:21:27 +0100

    New translations comments.js (Punjabi)

--- pa-IN/comments.js
+++ pa-IN/comments.js
@@ -1,10 +1,10 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
+    'comment.write': 'ਆਪਣਾ ਟਿੱਪਣੀ ਲਿਖੋ',
+    'comments.none': 'ਮਾਫ ਕਰਨਾ, ਕੋਈ ਟਿੱਪਣੀਆਂ ਨਹੀਂ ਦਿਖਾਈ ਜਾਂਦੀਆਂ।',
     'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-    'comments.show.older': 'Show older comments'
+    'comments.show.older': 'ਪੁਰਾਣੀਆਂ ਟਿੱਪਣੀਆਂ ਵੇਖੋ'
 };
