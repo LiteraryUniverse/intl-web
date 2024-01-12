dc09a7e2ff234125747a47e321f0514fe02440b0
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:29:25 +0100

    New translations form.js (Thai)

--- th-TH/form.js
+++ th-TH/form.js
@@ -1,69 +1,69 @@
 export default {
     'form.requiredSymbol': '*',
-    'form.validator.required': 'Required field',
-    'form.validator.mustBeNumber': 'Must be a number',
-    'form.validator.minNumber': 'Should be greater than {min, number}',
-    'form.validator.maxNumber': 'Should be lower than {max, number}',
-    'form.validator.maxLength': `Should be less than {max, number} {what, select,
-    characters {characters}
-    tags {tags}
-    genres {genres}
-    numbers {numbers}
+    'form.validator.required': 'ฟิลด์ที่ต้องการ',
+    'form.validator.mustBeNumber': 'ต้องเป็นตัวเลข',
+    'form.validator.minNumber': 'ต้องมากกว่า {min, number}',
+    'form.validator.maxNumber': 'ต้องน้อยกว่า {max, number}',
+    'form.validator.maxLength': `ควรน้อยกว่า {max, number} {what, select,
+    characters {ตัวอักษร}
+    tags {แท็ก}
+    genres {ประเภท}
+    numbers {ตัวเลข}
     other {}
   }`,
-    'form.validator.minLength': `Should be at least {min, number} {what, select,
-    characters {characters}
-    tags {tags}
-    genres {genres}
-    genre {genre}
-    numbers {numbers}
+    'form.validator.minLength': `ควรมีอย่างน้อย {min, number} {what, select,
+    characters {ตัวอักษร}
+    tags {แท็ก}
+    genres {ประเภท}
+    genre {ประเภท}
+    numbers {ตัวเลข}
     other {}
   }`,
-    'form.validator.email': 'Must be a valid e-mail',
-    'form.validator.url': 'Must be a valid URL',
-    'form.validator.slug': 'Must be a valid part of URL',
-    'form.submitting.signup': 'Creating account...',
-    'form.submitting.signin': 'Logging in...',
-    'form.submitting.default': 'Submitting...',
-    'form.submitting.sending': 'Sending...',
-    'form.validator.whitespace': 'Must not have white space',
-    'form.validator.zipCode': 'Must be a valid ZIP code',
-    'form.validator.isbn': 'We only accept 10 or 13 version of ISBN',
-    'form.validator.btc': 'This is not a valid BTC address.',
-    'form.validator.eth': 'This is not a valid ETH address.',
-    'form.weakPassword': 'This password looks weak. Please consider making it stronger by adding numbers and special characters.',
+    'form.validator.email': 'ต้องเป็นอีเมลที่ถูกต้อง',
+    'form.validator.url': 'ต้องเป็น URL ที่ถูกต้อง',
+    'form.validator.slug': 'ต้องเป็นส่วนของ URL ที่ถูกต้อง',
+    'form.submitting.signup': 'กำลังสร้างบัญชี...',
+    'form.submitting.signin': 'กำลังเข้าสู่ระบบ...',
+    'form.submitting.default': 'กำลังส่ง...',
+    'form.submitting.sending': 'กำลังส่ง...',
+    'form.validator.whitespace': 'ต้องไม่มีช่องว่าง',
+    'form.validator.zipCode': 'ต้องเป็นรหัสไปรษณีย์ที่ถูกต้อง',
+    'form.validator.isbn': 'เรายอมรับเฉพาะเล่ม 10 หรือ 13 ของ ISBN',
+    'form.validator.btc': 'นี่ไม่ใช่ที่อยู่ BTC ที่ถูกต้อง',
+    'form.validator.eth': 'นี่ไม่ใช่ที่อยู่ ETH ที่ถูกต้อง',
+    'form.weakPassword': 'รหัสผ่านดูแย่นี้ โปรดพิจารณาทำให้แข็งแกร่งขึ้นโดยการเพิ่มตัวเลขและอักขระพิเศษ',
     // you can get these messages from: https://github.com/gwendall/meteor-simple-schema-i18n/tree/master/i18n
     // but don't forget to adjust the format
-    'simpleschema.required': '{label} is required',
-    'simpleschema.minString': '{label} must be at least {min, number} characters',
-    'simpleschema.maxString': '{label} cannot exceed {max, number} characters',
-    'simpleschema.minNumber': '{label} must be at least {min, number}',
-    'simpleschema.maxNumber': '{label} cannot exceed {max, number}',
-    'simpleschema.minNumberExclusive': '{label} must be greater than {min, number}',
-    'simpleschema.maxNumberExclusive': '{label} must be less than {max, number}',
-    'simpleschema.minDate': '{label} must be on or after {min, date}',
-    'simpleschema.maxDate': '{label} cannot be after {max, date}',
-    'simpleschema.badDate': '{label} is not a valid date',
-    'simpleschema.minCount': 'You must specify at least {minCount, number} values',
-    'simpleschema.maxCount': 'You cannot specify more than {maxCount, number} values',
-    'simpleschema.noDecimal': '{label} must be an integer',
-    'simpleschema.notAllowed': '{value} is not an allowed value',
-    'simpleschema.expectedString': '{label} must be a string',
-    'simpleschema.expectedNumber': '{label} must be a number',
-    'simpleschema.expectedBoolean': '{label} must be a boolean',
-    'simpleschema.expectedArray': '{label} must be an array',
-    'simpleschema.expectedObject': '{label} must be an object',
-    'simpleschema.expectedConstructor': '{label} must be a {type}',
-    'simpleschema.keyNotInSchema': '{key} is not allowed by the schema',
-    'simpleschema.notUnique': '{label} must be unique',
-    'simpleschema.regEx.0': '{label} failed regular expression validation',
-    'simpleschema.regEx.1': '{label} must be a valid e-mail address',
-    'simpleschema.regEx.2': '{label} must be a valid e-mail address',
-    'simpleschema.regEx.3': '{label} must be a valid domain',
-    'simpleschema.regEx.4': '{label} must be a valid domain',
-    'simpleschema.regEx.5': '{label} must be a valid IPv4 or IPv6 address',
-    'simpleschema.regEx.6': '{label} must be a valid IPv4 address',
-    'simpleschema.regEx.7': '{label} must be a valid IPv6 address',
-    'simpleschema.regEx.8': '{label} must be a valid URL',
-    'simpleschema.regEx.9': '{label} must be a valid alphanumeric ID'
+    'simpleschema.required': '{label} จำเป็น',
+    'simpleschema.minString': '{label} ต้องมีอย่างน้อย {min, number} อักขระ',
+    'simpleschema.maxString': '{label} ไม่สามารถเกิน {max, number} อักขระ',
+    'simpleschema.minNumber': '{label} ต้องมากกว่า {min, number}',
+    'simpleschema.maxNumber': '{label} ไม่สามารถเกิน {max, number}',
+    'simpleschema.minNumberExclusive': '{label} ต้องมากกว่า {min, number}',
+    'simpleschema.maxNumberExclusive': '{label} ต้องน้อยกว่า {max, number}',
+    'simpleschema.minDate': '{label} ต้องเป็นหรือหลังจาก {min, date}',
+    'simpleschema.maxDate': '{label} ไม่สามารถเกินหลังจาก {max, date}',
+    'simpleschema.badDate': '{label} ไม่ใช่วันที่ที่ถูกต้อง',
+    'simpleschema.minCount': 'คุณต้องระบุอย่างน้อย {minCount, number} ค่า',
+    'simpleschema.maxCount': 'คุณไม่สามารถระบุมากกว่า {maxCount, number} ค่า',
+    'simpleschema.noDecimal': '{label} ต้องเป็นจำนวนเต็ม',
+    'simpleschema.notAllowed': '{value} ไม่ใช่ค่าที่อนุญาต',
+    'simpleschema.expectedString': '{label} ต้องเป็นสตริง',
+    'simpleschema.expectedNumber': '{label} ต้องเป็นตัวเลข',
+    'simpleschema.expectedBoolean': '{label} ต้องเป็นค่าความจริง',
+    'simpleschema.expectedArray': '{label} ต้องเป็นอาร์เรย์',
+    'simpleschema.expectedObject': '{label} ต้องเป็นออบเจ็กต์',
+    'simpleschema.expectedConstructor': '{label} ต้องเป็น {type}',
+    'simpleschema.keyNotInSchema': '{key} ไม่ได้รับอนุญาตโดยแบบแผน',
+    'simpleschema.notUnique': '{label} ต้องไม่ซ้ำกัน',
+    'simpleschema.regEx.0': '{label} ไม่ผ่านการตรวจสอบด้วย regular expression',
+    'simpleschema.regEx.1': '{label} ต้องเป็นที่อยู่อีเมลที่ถูกต้อง',
+    'simpleschema.regEx.2': '{label} ต้องเป็นที่อยู่อีเมลที่ถูกต้อง',
+    'simpleschema.regEx.3': '{label} ต้องเป็นโดเมนที่ถูกต้อง',
+    'simpleschema.regEx.4': '{label} ต้องเป็นโดเมนที่ถูกต้อง',
+    'simpleschema.regEx.5': '{label} ต้องเป็นที่อยู่ IP รูปแบบ IPv4 หรือ IPv6 ที่ถูกต้อง',
+    'simpleschema.regEx.6': '{label} ต้องเป็นที่อยู่ IP รูปแบบ IPv4 ที่ถูกต้อง',
+    'simpleschema.regEx.7': '{label} ต้องเป็นที่อยู่ IP รูปแบบ IPv6 ที่ถูกต้อง',
+    'simpleschema.regEx.8': '{label} ต้องเป็น URL ที่ถูกต้อง',
+    'simpleschema.regEx.9': '{label} ต้องเป็น ID ที่มีตัวอักษรและตัวเลขเท่านั้น'
 };
