edc3751e049e2ac845a47634107fb1eb38caa5cb
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:31:53 +0100

    New translations roadmap.js (Telugu)

--- te-IN/roadmap.js
+++ te-IN/roadmap.js
@@ -1,37 +1,37 @@
 export default {
-  'roadmap.ariaTimeOptions': 'Selections for roadmap items based on their status.',
-  'roadmap.state.backlog': 'Backlog',
-  'roadmap.state.inprogress': 'In-progress',
-  'roadmap.state.preview': 'Preview',
-  'roadmap.state.beta': 'Beta',
-  'roadmap.state.ga': 'General availability',
+  'roadmap.ariaTimeOptions': 'రోడ్‌మాప్ అంశాల ఆధారంగా అవస్థను ఆధారపడి ఎంచుకోవడానికి ఎంచుకోవడానికి ఎంచుకోవడానికి ఎంచుకోవడానికి ఎంచుకోవడానికి.',
+  'roadmap.state.backlog': 'బ్యాక్లాగ్',
+  'roadmap.state.inprogress': 'ప్రోగ్రెస్ లో',
+  'roadmap.state.preview': 'ప్రివ్యూ',
+  'roadmap.state.beta': 'బీటా',
+  'roadmap.state.ga': 'సాధారణ అందుబాటులో',
   'roadmap.eta.q1.short': 'Q1',
   'roadmap.eta.q2.short': 'Q2',
   'roadmap.eta.q3.short': 'Q3',
   'roadmap.eta.q4.short': 'Q4',
-  'roadmap.eta.q1.long': 'First quarter',
-  'roadmap.eta.q2.long': 'Second quarter',
-  'roadmap.eta.q3.long': 'Third quarter',
-  'roadmap.eta.q4.long': 'Fourth quarter',
-  'roadmap.backlog': 'Backlog',
-  'roadmap.history': 'History of development',
-  'roadmap.item.inBacklog': 'We are planning to develop this feature in the future.',
-  'roadmap.item.inProgress': 'The development of the feature started {startedAt, date}.',
-  'roadmap.item.inBeta': 'This feature has entered beta on {betaAt, date}.',
-  'roadmap.item.inPreview': 'This feature became available to Storyteller subscribers on {previewAt, date}.',
-  'roadmap.item.inGa': 'This feature has become generally available on {gaAt, date}.',
-  'roadmap.item.originalIssue': 'This feature was originally suggested by our users.',
-  'roadmap.eta.mapTitle': `Planned for {quarter, select,
-    Q1 {Q1}
-    Q2 {Q2}
-    Q3 {Q3}
-    Q4 {Q4}
-    other {}
-  } {year, date, ::yyyy}`,
-  'roadmap.eta.mapTitleFinished': 'Finished in {time, date, ::MMMM ::yyyy}',
-  'roadmap.back': 'Back to roadmap',
-  'roadmap.seeBacklog': 'Check out what other things we plan to do',
-  'roadmap.seeFinished': 'If you like to dig more into our history, we have a nice overview ready for you',
-  'roadmap.planned': 'Planned',
-  'roadmap.finished': 'Finished'
+  'roadmap.eta.q1.long': 'మొదటి త్రైమాసం',
+  'roadmap.eta.q2.long': 'రెండవ త్రైమాసం',
+  'roadmap.eta.q3.long': 'మూడవ త్రైమాసం',
+  'roadmap.eta.q4.long': 'నాలుగువ త్రైమాసం',
+  'roadmap.backlog': 'బ్యాక్లాగ్',
+  'roadmap.history': 'అభివృద్ధి చరిత్ర',
+  'roadmap.item.inBacklog': 'మేము భవిష్యత్తులో ఈ ఫీచర్ ను అభివృద్ధి చేయాలని ప్రకటించాము.',
+  'roadmap.item.inProgress': 'ఈ ఫీచర్ అభివృద్ధి ప్రారంభించబడింది {startedAt, date}.',
+  'roadmap.item.inBeta': 'ఈ ఫీచర్ బీటాలో ప్రవేశించింది {betaAt, date}.',
+  'roadmap.item.inPreview': 'ఈ ఫీచర్ స్టోరీటెలర్ ఛార్చబడిన వాడకరులకు అందుబాటులో ఉంది {previewAt, date}.',
+  'roadmap.item.inGa': 'ఈ ఫీచర్ సాధారణంగా అందుబాటులో ఉంది {gaAt, date}.',
+  'roadmap.item.originalIssue': 'ఈ ఫీచర్ మొదటిగా మా వాడుకరుల సలహాల ద్వారా సూచించబడింది.',
+  'roadmap.eta.mapTitle': `}
+  } {year, date, ::yyyy}}
+    other {Q4}
+    Q4 {Q3}
+    Q3 {Q2}
+    Q2 {Q1 కోసం ప్రారంభించబడింది {quarter, select,
+    Q1 {కోసం ప్రారంభించబడింది.`,
+  'roadmap.eta.mapTitleFinished': '{time, date, ::MMMM ::yyyy}లో పూర్తి చేయబడింది',
+  'roadmap.back': 'రోడ్‌మ్యాప్‌కు తిరిగి వెళ్ళండి',
+  'roadmap.seeBacklog': 'మేము చేయడానికి మరియు ప్రారంభించడానికి మరొక విషయాలను చూడండి',
+  'roadmap.seeFinished': 'మేము చేసిన చరిత్రను మీకు నచ్చినట్లయిన వివరణను మాకు అందుబాటులో ఉన్నాయి',
+  'roadmap.planned': 'ప్రారంభించబడింది',
+  'roadmap.finished': 'పూర్తి చేయబడింది'
 };
