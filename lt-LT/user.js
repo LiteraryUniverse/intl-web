ea3250ff97f04a0788730345acf29356b3ab222c
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:19:54 +0100

    New translations user.js (Lithuanian)

--- lt-LT/user.js
+++ lt-LT/user.js
@@ -1,53 +1,53 @@
 export default {
-    'user.friend.request.add': 'Add to friends',
-    'user.friend.requests': `You have {num, number} new friend {num, plural,
-    zero {requests}
-    one {request}
-    many {requests}
-    other {requests}
+    'user.friend.request.add': 'Pridėti prie draugų',
+    'user.friend.requests': `Jūs turite {num, number} naują draugą {num, plural,
+    zero {užklausas}
+    one {užklausa}
+    many {užklausos}
+    other {užklausos}
   }`,
-    'user.friend.unfriend': 'Unfriend',
-    'user.block': 'Block',
-    'user.unblock': 'Unblock',
+    'user.friend.unfriend': 'Pašalinti iš draugų',
+    'user.block': 'Blokuoti',
+    'user.unblock': 'Atblokuoti',
     'user.joined': `{gender, select,
-    male {Joined}
-    female {Joined}
-    other {Joined}} on {date, date, long}`,
-    'user.friend.request.accept': 'Accept friendship request',
-    'user.friend.request.cancel': 'Cancel friendship request',
-    'user.friend.request.deny': 'Deny friendship',
-    'user.listing': 'User listing',
-    'user.profile.visit': 'Visit {user}\'s profile.',
-    'user.profile.avatar': '{username}\'s avatar',
-    'user.friend.list.requests': 'Friendships requests',
-    'user.friend.norequests': 'You have no requests for friendship.',
-    'user.dashboardWelcome': `Good {dayPart, select,
-    morning {morning}
-    afternoon {afternoon}
-    evening {evening}
-    night {night}
-    other {day}
+    male {prisijungė}
+    female {prisijungė}
+    other {prisijungė}} prisijungė {date, date, long}`,
+    'user.friend.request.accept': 'Priimti draugystės prašymą',
+    'user.friend.request.cancel': 'Atšaukti draugystės prašymą',
+    'user.friend.request.deny': 'Atmesti draugystę',
+    'user.listing': 'Vartotojų sąrašas',
+    'user.profile.visit': 'Aplankyti {user}profilį.',
+    'user.profile.avatar': '{username}avataras',
+    'user.friend.list.requests': 'Draugystės prašymai',
+    'user.friend.norequests': 'Jūs neturite draugystės prašymų.',
+    'user.dashboardWelcome': `Labas {dayPart, select,
+    morning {ryte}
+    afternoon {popietę}
+    evening {vakare}
+    night {naktį}
+    other {dieną}
   } {displayName}!`,
-    'user.verification.publishedAuthor': 'This author has been published the traditional way',
-    'user.verification.luAuthor': 'Literary Universe based author of note',
-    'user.verification.luEmployee': 'Literary Universe employee',
-    'user.verification.publicFigure': 'Verified public figure',
+    'user.verification.publishedAuthor': 'Šis autorius buvo publikuotas tradiciniu būdu',
+    'user.verification.luAuthor': 'Literary Universe pagrindu žymus autorius',
+    'user.verification.luEmployee': 'Literary Universe darbuotojas',
+    'user.verification.publicFigure': 'Patikrintas viešas asmuo',
     'profile.storiesCount': `{count, number} {count, plural,
-    one {story}
-    other {stories}
+    one {istorija}
+    other {istorijos}
   }`,
     'profile.universesCount': `{count, number} {count, plural,
-    one {universe}
-    other {universes}
+    one {visata}
+    other {visatos}
   }`,
     'profile.fanFictionsCount': `{count, number} {count, plural,
-    one {fan fiction}
-    other {fan fictions} 
+    one {fanų kūrinys}
+    other {fanų kūriniai} 
   }`,
-    'profile.works': 'Works',
+    'profile.works': 'Darbai',
     // As in creative/literary works
-    'profile.wishlist': 'Wishlist',
-    'profile.storiesBy': 'Stories created by {username}',
-    'profile.universesBy': 'Universes created by {username}',
-    'profile.fanFictionBy': 'Fan Fiction created by {username}'
+    'profile.wishlist': 'Pageidavimų sąrašas',
+    'profile.storiesBy': 'Istorijos sukurtos {username}',
+    'profile.universesBy': 'Visatos sukurtos {username}',
+    'profile.fanFictionBy': 'Fanų kūriniai sukurti {username}'
 };
