4b8abdc2be21a26b7bf8dbecfa2c7e59ba248497
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:19:18 +0100

    New translations universe.js (Korean)

--- ko-KR/universe.js
+++ ko-KR/universe.js
@@ -18,9 +18,9 @@ export default {
     'universe.translators': '번역자',
     'universe.authors.notice': '각 이야기에는 함께 일한 추가적인 개인이 있을 수 있습니다. 자세한 내용은 이야기의 세부 사항을 참조하십시오.',
     'universe.createdBy': `{gender, select,
-    male {Created}
-    female {Created}
-    other {Created}} by `,
+    male {생성됨}
+    female {생성됨}
+    other {생성됨}} by `,
     'universe.stories.for': '{universe}에 대한 이야기',
     'universe.stories.own': '{universe} 개의 이야기',
     'universe.stories.desc': '{universe}에 대한 기준 이야기',
@@ -30,7 +30,7 @@ export default {
     'universe.fanArtHeaderDesc.item': '{universe} 팬 아트 - {name}',
     'universe.fanArtHeaderDesc.submit': '{universe} 팬 아트 제출',
     'universe.fanArtHeaderDesc.overview': '{universe} 팬 아트',
-    'universe.fanFiction.hidden': 'Hidden',
+    'universe.fanFiction.hidden': '숨김',
     'universe.fanFiction.hide': '팬픽 숨기기',
     'universe.fanFiction.unhide': '팬픽 숨김 해제'
 };
