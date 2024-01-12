c9406c8816dc1c5e2182ebe3aa3e5cc6cbfcab13
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:19:03 +0100

    New translations form.js (Korean)

--- ko-KR/form.js
+++ ko-KR/form.js
@@ -4,19 +4,19 @@ export default {
     'form.validator.mustBeNumber': '숫자여야합니다',
     'form.validator.minNumber': '보다 커야 합니다 {min, number}',
     'form.validator.maxNumber': 'Should be lower than {max, number}',
-    'form.validator.maxLength': `Should be less than {max, number} {what, select,
-    characters {characters}
-    tags {tags}
-    genres {genres}
-    numbers {numbers}
+    'form.validator.maxLength': `{max, number} 보다 작아야 합니다. {what, select,
+    characters {문자입니다.}
+    tags {태그입니다.}
+    genres {장르입니다.}
+    numbers {숫자입니다.}
     other {}
   }`,
-    'form.validator.minLength': `Should be at least {min, number} {what, select,
-    characters {characters}
-    tags {tags}
-    genres {genres}
-    genre {genre}
-    numbers {numbers}
+    'form.validator.minLength': `{min, number} 보다는 적어도 {what, select,
+    characters {문자여야 합니다.}
+    tags {태그입니다.}
+    genres {장르입니다.}
+    genre {장르입니다.}
+    numbers {숫자입니다.}
     other {}
   }`,
     'form.validator.email': '유효한 이메일이어야 합니다',
@@ -29,8 +29,8 @@ export default {
     'form.validator.whitespace': '공백이 없어야합니다',
     'form.validator.zipCode': '유효한 우편번호여야 합니다',
     'form.validator.isbn': 'ISBN은 10 또는 13 버전만 허용됩니다',
-    'form.validator.btc': 'This is not a valid BTC address.',
-    'form.validator.eth': 'This is not a valid ETH address.',
+    'form.validator.btc': '유효한 BTC 주소가 아닙니다.',
+    'form.validator.eth': '유효한 ETH 주소가 아닙니다.',
     'form.weakPassword': '이 비밀번호는 약합니다. 숫자와 특수문자를 추가하여 강화하는 것을 고려해주세요.',
     // you can get these messages from: https://github.com/gwendall/meteor-simple-schema-i18n/tree/master/i18n
     // but don't forget to adjust the format
@@ -43,27 +43,27 @@ export default {
     'simpleschema.maxNumberExclusive': '{label} must be less than {max, number}',
     'simpleschema.minDate': '{label} must be on or after {min, date}',
     'simpleschema.maxDate': '{label} cannot be after {max, date}',
-    'simpleschema.badDate': '{label} is not a valid date',
+    'simpleschema.badDate': '{label} 날짜가 유효하지 않습니다',
     'simpleschema.minCount': 'You must specify at least {minCount, number} values',
     'simpleschema.maxCount': 'You cannot specify more than {maxCount, number} values',
-    'simpleschema.noDecimal': '{label} must be an integer',
-    'simpleschema.notAllowed': '{value} is not an allowed value',
-    'simpleschema.expectedString': '{label} must be a string',
-    'simpleschema.expectedNumber': '{label} must be a number',
-    'simpleschema.expectedBoolean': '{label} must be a boolean',
-    'simpleschema.expectedArray': '{label} must be an array',
-    'simpleschema.expectedObject': '{label} must be an object',
-    'simpleschema.expectedConstructor': '{label} must be a {type}',
-    'simpleschema.keyNotInSchema': '{key} is not allowed by the schema',
-    'simpleschema.notUnique': '{label} must be unique',
-    'simpleschema.regEx.0': '{label} failed regular expression validation',
-    'simpleschema.regEx.1': '{label} must be a valid e-mail address',
-    'simpleschema.regEx.2': '{label} must be a valid e-mail address',
-    'simpleschema.regEx.3': '{label} must be a valid domain',
-    'simpleschema.regEx.4': '{label} must be a valid domain',
-    'simpleschema.regEx.5': '{label} must be a valid IPv4 or IPv6 address',
-    'simpleschema.regEx.6': '{label} must be a valid IPv4 address',
-    'simpleschema.regEx.7': '{label} must be a valid IPv6 address',
-    'simpleschema.regEx.8': '{label} must be a valid URL',
-    'simpleschema.regEx.9': '{label} must be a valid alphanumeric ID'
+    'simpleschema.noDecimal': '{label} 정수여야 합니다',
+    'simpleschema.notAllowed': '{value} 은(는) 허용되지 않는 값입니다',
+    'simpleschema.expectedString': '{label} 문자열이어야 합니다',
+    'simpleschema.expectedNumber': '{label} 숫자여야 합니다',
+    'simpleschema.expectedBoolean': '{label} 불리언이어야 합니다',
+    'simpleschema.expectedArray': '{label} 배열이어야 합니다',
+    'simpleschema.expectedObject': '{label} 객체여야 합니다',
+    'simpleschema.expectedConstructor': '{label} {type}이어야 합니다',
+    'simpleschema.keyNotInSchema': '{key} 스키마에 허용되지 않습니다',
+    'simpleschema.notUnique': '{label} 고유해야 합니다',
+    'simpleschema.regEx.0': '{label} 정규 표현식 검증에 실패했습니다',
+    'simpleschema.regEx.1': '{label} 유효한 이메일 주소여야 합니다',
+    'simpleschema.regEx.2': '{label} 유효한 이메일 주소여야 합니다',
+    'simpleschema.regEx.3': '{label} 유효한 도메인이어야 합니다',
+    'simpleschema.regEx.4': '{label} 유효한 도메인이어야 합니다',
+    'simpleschema.regEx.5': '{label} 유효한 IPv4 또는 IPv6 주소여야 합니다',
+    'simpleschema.regEx.6': '{label} 유효한 IPv4 주소여야 합니다',
+    'simpleschema.regEx.7': '{label} 유효한 IPv6 주소여야 합니다',
+    'simpleschema.regEx.8': '{label} 은(는) 유효한 URL이어야 합니다',
+    'simpleschema.regEx.9': '{label} 은(는) 유효한 영숫자 ID여야 합니다'
 };
