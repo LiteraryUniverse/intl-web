9e80b2e82587aa1c0707525d9319ed370f0eff45
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:26:26 +0100

    New translations writer.js (Portuguese, Brazilian)

--- pt-BR/writer.js
+++ pt-BR/writer.js
@@ -17,12 +17,12 @@ export default {
     'finder.connect': 'Conectar a seleção à entrada da enciclopédia',
     'writer.aside.ariaLabel': 'Menu de opções de ajuda',
     'writer.nothingToSave': 'Nada a salvar',
-    'writer.publish.publishAria': 'Publication options',
-    'writer.publish.scheduleExplained': 'You can either publish immediately or schedule the chapter to be available at a later date.',
-    'writer.publish.schedule': 'Set publication date time',
-    'writer.publish.previousChapterPublished': 'The previous chapter was published on {publishDate, date, short} at {publishDate, time}',
-    'writer.publish.previousChapterScheduled': 'The previous chapter is scheduled to be published on {publishDate, date, short} at {publishDate, time}',
-    'writer.publish.alreadyPublished': 'This chapter was already published on {publishedAt, date, short}. New versions will be published immediately.',
+    'writer.publish.publishAria': 'Opções de publicação',
+    'writer.publish.scheduleExplained': 'Você pode publicar imediatamente ou agendar o capítulo para ficar disponível em uma data posterior.',
+    'writer.publish.schedule': 'Definir data e hora de publicação',
+    'writer.publish.previousChapterPublished': 'O capítulo anterior foi publicado em {publishDate, date, short} às {publishDate, time}',
+    'writer.publish.previousChapterScheduled': 'O capítulo anterior está agendado para ser publicado em {publishDate, date, short} às {publishDate, time}',
+    'writer.publish.alreadyPublished': 'Este capítulo já foi publicado em {publishedAt, date, short}. Novas versões serão publicadas imediatamente.',
     'writer.publish.betaReader': 'Beta',
     'writer.publish.public': 'Publicar'
 };
