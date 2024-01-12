8e3046ad8a39ceb681dbebe49afc0d937c8d35eb
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:12:37 +0100

    New translations form.js (Catalan)

--- ca-ES/form.js
+++ ca-ES/form.js
@@ -1,37 +1,37 @@
 export default {
     'form.requiredSymbol': '*',
-    'form.validator.required': 'Required field',
-    'form.validator.mustBeNumber': 'Must be a number',
-    'form.validator.minNumber': 'Should be greater than {min, number}',
-    'form.validator.maxNumber': 'Should be lower than {max, number}',
-    'form.validator.maxLength': `Should be less than {max, number} {what, select,
-    characters {characters}
-    tags {tags}
-    genres {genres}
-    numbers {numbers}
+    'form.validator.required': 'Camp obligatori',
+    'form.validator.mustBeNumber': 'Ha de ser un número',
+    'form.validator.minNumber': 'Ha de ser més gran que {min, number}',
+    'form.validator.maxNumber': 'Ha de ser més petit que {max, number}',
+    'form.validator.maxLength': `Ha de ser inferior a {max, number} {what, select,
+    characters {caràcters}
+    tags {etiquetes}
+    genres {gèneres}
+    numbers {números}
     other {}
   }`,
-    'form.validator.minLength': `Should be at least {min, number} {what, select,
-    characters {characters}
-    tags {tags}
-    genres {genres}
-    genre {genre}
-    numbers {numbers}
+    'form.validator.minLength': `Ha de ser com a mínim {min, number} {what, select,
+    characters {caràcters}
+    tags {etiquetes}
+    genres {gèneres}
+    genre {gènere}
+    numbers {números}
     other {}
   }`,
-    'form.validator.email': 'Must be a valid e-mail',
-    'form.validator.url': 'Must be a valid URL',
-    'form.validator.slug': 'Must be a valid part of URL',
-    'form.submitting.signup': 'Creating account...',
-    'form.submitting.signin': 'Logging in...',
-    'form.submitting.default': 'Submitting...',
-    'form.submitting.sending': 'Sending...',
-    'form.validator.whitespace': 'Must not have white space',
-    'form.validator.zipCode': 'Must be a valid ZIP code',
-    'form.validator.isbn': 'We only accept 10 or 13 version of ISBN',
-    'form.validator.btc': 'This is not a valid BTC address.',
-    'form.validator.eth': 'This is not a valid ETH address.',
-    'form.weakPassword': 'This password looks weak. Please consider making it stronger by adding numbers and special characters.',
+    'form.validator.email': 'Ha de ser un correu electrònic vàlid',
+    'form.validator.url': 'Ha de ser un URL vàlid',
+    'form.validator.slug': 'Ha de ser una part vàlida de l\'URL',
+    'form.submitting.signup': 'Creant compte...',
+    'form.submitting.signin': 'Iniciant sessió...',
+    'form.submitting.default': 'Enviant...',
+    'form.submitting.sending': 'Enviant...',
+    'form.validator.whitespace': 'No pot tenir espais en blanc',
+    'form.validator.zipCode': 'Ha de ser un codi postal vàlid',
+    'form.validator.isbn': 'Només acceptem la versió 10 o 13 de l\'ISBN',
+    'form.validator.btc': 'Aquesta no és una adreça BTC vàlida.',
+    'form.validator.eth': 'Aquesta no és una adreça ETH vàlida.',
+    'form.weakPassword': 'Aquesta contrasenya sembla feble. Si us plau, considera fer-la més segura afegint números i caràcters especials.',
     // you can get these messages from: https://github.com/gwendall/meteor-simple-schema-i18n/tree/master/i18n
     // but don't forget to adjust the format
     'simpleschema.required': '{label} és obligatori',
@@ -55,7 +55,7 @@ export default {
     'simpleschema.expectedObject': '{label} ha de ser un objecte',
     'simpleschema.expectedConstructor': '{label} ha de ser de tipus {type}',
     'simpleschema.keyNotInSchema': '{key} no està permès per l\'esquema',
-    'simpleschema.notUnique': '{label} must be unique',
+    'simpleschema.notUnique': '{label} ha de ser únic',
     'simpleschema.regEx.0': '{label} va fallar la validació per Expressió Regular (Regex)',
     'simpleschema.regEx.1': '{label} ha de ser una adreça de correu electrònic vàlida',
     'simpleschema.regEx.2': '{label} ha de ser una adreça de correu electrònic vàlida',
