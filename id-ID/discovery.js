35fcd9de4be5d9792997e2893c9a31c2c616b0b8
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:26:35 +0100

    New translations discovery.js (Indonesian)

--- id-ID/discovery.js
+++ id-ID/discovery.js
@@ -17,34 +17,34 @@ export default {
         other {# menit}
     } untuk dibaca.`,
     'story.estimatedReadingTime.explained': 'Berdasarkan kecepatan membaca rata-rata {avgReadingNum, number} kata per menit.',
-    'discover.search.text': 'Search term',
-    'discover.searchStory.title': 'Search for a story',
-    'discover.searchStory.description': 'Search for a stories',
-    'discover.searchUniverse.title': 'Search for a universe',
-    'discover.searchUniverse.description': 'Search for a universes',
-    'discover.search.storyStatus': 'Story progress status',
-    'story.status.complete': 'Complete',
-    'story.status.inProgress': 'In-progress',
-    'discover.search.universeConnection': 'Connection of story to universe',
-    'story.universeRelation.standalone': 'Standalone story',
+    'discover.search.text': 'Kata kunci pencarian',
+    'discover.searchStory.title': 'Cari cerita',
+    'discover.searchStory.description': 'Cari cerita-cerita',
+    'discover.searchUniverse.title': 'Cari alam semesta',
+    'discover.searchUniverse.description': 'Cari alam semesta-alam semesta',
+    'discover.search.storyStatus': 'Status kemajuan cerita',
+    'story.status.complete': 'Selesai',
+    'story.status.inProgress': 'Sedang berlangsung',
+    'discover.search.universeConnection': 'Hubungan cerita dengan alam semesta',
+    'story.universeRelation.standalone': 'Cerita mandiri',
     'story.universeRelation.fanFiction': 'Fan Fiction',
-    'story.universeRelation.universe': 'Part of a universe',
-    'discover.search.rating': 'Age rating',
-    'discover.search.license': 'Copyright license',
+    'story.universeRelation.universe': 'Bagian dari alam semesta',
+    'discover.search.rating': 'Rating usia',
+    'discover.search.license': 'Lisensi hak cipta',
     'discover.search.language': 'Bahasa',
     'discover.search.resultsTotal.universe': `{results, plural,
-        =0 {No universes match your parameters}
-        one {1 universe found}
-        other {# universes found}
+        =0 {Tidak ada alam semesta yang sesuai dengan parameter Anda}
+        one {1 alam semesta ditemukan}
+        other {# alam semesta ditemukan}
     }.`,
     'discover.search.resultsTotal.story': `{results, plural,
-        =0 {No stories match your parameters}
-        one {1 story found}
-        other {# stories found}
+        =0 {Tidak ada cerita yang sesuai dengan parameter Anda}
+        one {1 cerita ditemukan}
+        other {# cerita ditemukan}
     }.`,
-    'discover.viewSelection.aria': 'View selection',
-    'discover.searchDisplay.covers': 'Covers listing',
-    'discover.searchDisplay.list': 'Compact list',
+    'discover.viewSelection.aria': 'Pilihan tampilan',
+    'discover.searchDisplay.covers': 'Daftar sampul',
+    'discover.searchDisplay.list': 'Daftar kompak',
     'discover.includesAds': 'Karya ini memiliki penempatan produk atau mencakup bentuk iklan lainnya didalamnya.',
     'discovery.featured.universes': 'Alam semesta unggulan',
     'discovery.featured.stories': 'Cerita unggulan',
