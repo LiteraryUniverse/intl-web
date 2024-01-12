d0eadae7b3a15ba1e948273c0ca09cb9edd939c5
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:36:11 +0100

    New translations comments.js (Maithili)

--- mai-IN/comments.js
+++ mai-IN/comments.js
@@ -1,10 +1,10 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
-    'comments.total': `There {count, plural,
-    =0 {are no comments}
-    one {is 1 comment}
-    other {are # comments}
-  }.`,
-    'comments.show.older': 'Show older comments'
+    'comment.write': 'तपाईंको टिप्पणी लेख्नुहोस्',
+    'comments.none': 'माफ गर्नुहोला, कुनै पनि टिप्पणीहरू देखाउनको लागि छैनन्।',
+    'comments.total': `यहाँ {count, plural,
+    =0 {कुनै पनि टिप्पणीहरू छैनन्}
+    one {एक टिप्पणी छ}
+    other {# टिप्पणीहरू छन्}
+  }।`,
+    'comments.show.older': 'पुराना टिप्पणीहरू देखाउनुहोस्'
 };
