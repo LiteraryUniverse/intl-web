a1b110580505fcb6889cfd0a9db4917798abf1da
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:37:30 +0100

    New translations comments.js (Nepali)

--- ne-NP/comments.js
+++ ne-NP/comments.js
@@ -1,10 +1,10 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
-    'comments.total': `There {count, plural,
-    =0 {are no comments}
-    one {is 1 comment}
-    other {are # comments}
+    'comment.write': 'तपाईंको टिप्पणी लेख्नुहोस्',
+    'comments.none': 'माफ गर्नुहोला, कुनै पनि टिप्पणीहरू देखाउन नभएको',
+    'comments.total': `यहाँ {count, plural,
+    =0 {कुनै टिप्पणीहरू छैनन्}
+    one {1 टिप्पणी छ}
+    other {# टिप्पणीहरू छन्}
   }.`,
-    'comments.show.older': 'Show older comments'
+    'comments.show.older': 'पुराना टिप्पणीहरू देखाउनुहोस्'
 };
