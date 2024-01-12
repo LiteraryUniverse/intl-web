f945a1d4a5d6c1e4e5b408b6c70db3d7ad7dde36
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:12:05 +0100

    New translations form.js (Arabic)

--- ar-SA/form.js
+++ ar-SA/form.js
@@ -1,69 +1,69 @@
 export default {
     'form.requiredSymbol': '*',
     'form.validator.required': 'الحقل مطلوب',
-    'form.validator.mustBeNumber': 'Must be a number',
-    'form.validator.minNumber': 'Should be greater than {min, number}',
-    'form.validator.maxNumber': 'Should be lower than {max, number}',
-    'form.validator.maxLength': `Should be less than {max, number} {what, select,
-    characters {characters}
-    tags {tags}
-    genres {genres}
-    numbers {numbers}
+    'form.validator.mustBeNumber': 'يجب أن يكون رقماً',
+    'form.validator.minNumber': 'يجب أن يكون أكبر من {min, number}',
+    'form.validator.maxNumber': 'يجب أن يكون أقل من {max, number}',
+    'form.validator.maxLength': `يجب أن يكون أقل من {max, number} {what, select,
+    characters {حرفاً}
+    tags {علامة}
+    genres {أنواع الأنماط}
+    numbers {أرقام}
     other {}
   }`,
-    'form.validator.minLength': `Should be at least {min, number} {what, select,
-    characters {characters}
-    tags {tags}
-    genres {genres}
-    genre {genre}
-    numbers {numbers}
+    'form.validator.minLength': `يجب أن يكون على الأقل {min, number} {what, select,
+    characters {حرفاً}
+    tags {علامة}
+    genres {أنواع الأنماط}
+    genre {نوع الأنماط}
+    numbers {أرقام}
     other {}
   }`,
-    'form.validator.email': 'Must be a valid e-mail',
-    'form.validator.url': 'Must be a valid URL',
-    'form.validator.slug': 'Must be a valid part of URL',
-    'form.submitting.signup': 'Creating account...',
-    'form.submitting.signin': 'Logging in...',
-    'form.submitting.default': 'Submitting...',
-    'form.submitting.sending': 'Sending...',
-    'form.validator.whitespace': 'Must not have white space',
-    'form.validator.zipCode': 'Must be a valid ZIP code',
-    'form.validator.isbn': 'We only accept 10 or 13 version of ISBN',
-    'form.validator.btc': 'This is not a valid BTC address.',
-    'form.validator.eth': 'This is not a valid ETH address.',
-    'form.weakPassword': 'This password looks weak. Please consider making it stronger by adding numbers and special characters.',
+    'form.validator.email': 'يجب أن يكون عنوان بريد إلكتروني صحيح',
+    'form.validator.url': 'يجب أن يكون عنوان URL صحيح',
+    'form.validator.slug': 'يجب أن يكون جزءاً صحيحاً من عنوان URL',
+    'form.submitting.signup': 'جاري إنشاء الحساب...',
+    'form.submitting.signin': 'جاري تسجيل الدخول...',
+    'form.submitting.default': 'جاري الإرسال...',
+    'form.submitting.sending': 'جاري الإرسال...',
+    'form.validator.whitespace': 'يجب ألا يحتوي على مسافات',
+    'form.validator.zipCode': 'يجب أن يكون رمز بريدي صحيح',
+    'form.validator.isbn': 'نحن نقبل فقط الإصدار 10 أو 13 من رمز ISBN',
+    'form.validator.btc': 'هذا ليس عنوان BTC صالح.',
+    'form.validator.eth': 'هذا ليس عنوان ETH صالح.',
+    'form.weakPassword': 'كلمة المرور ضعيفة. يرجى النظر في تعزيزها عن طريق إضافة أرقام وحروف خاصة.',
     // you can get these messages from: https://github.com/gwendall/meteor-simple-schema-i18n/tree/master/i18n
     // but don't forget to adjust the format
-    'simpleschema.required': '{label} is required',
-    'simpleschema.minString': '{label} must be at least {min, number} characters',
-    'simpleschema.maxString': '{label} cannot exceed {max, number} characters',
-    'simpleschema.minNumber': '{label} must be at least {min, number}',
-    'simpleschema.maxNumber': '{label} cannot exceed {max, number}',
-    'simpleschema.minNumberExclusive': '{label} must be greater than {min, number}',
-    'simpleschema.maxNumberExclusive': '{label} must be less than {max, number}',
-    'simpleschema.minDate': '{label} must be on or after {min, date}',
-    'simpleschema.maxDate': '{label} cannot be after {max, date}',
-    'simpleschema.badDate': '{label} is not a valid date',
-    'simpleschema.minCount': 'You must specify at least {minCount, number} values',
-    'simpleschema.maxCount': 'You cannot specify more than {maxCount, number} values',
-    'simpleschema.noDecimal': '{label} must be an integer',
-    'simpleschema.notAllowed': '{value} is not an allowed value',
-    'simpleschema.expectedString': '{label} must be a string',
-    'simpleschema.expectedNumber': '{label} must be a number',
-    'simpleschema.expectedBoolean': '{label} must be a boolean',
-    'simpleschema.expectedArray': '{label} must be an array',
-    'simpleschema.expectedObject': '{label} must be an object',
-    'simpleschema.expectedConstructor': '{label} must be a {type}',
-    'simpleschema.keyNotInSchema': '{key} is not allowed by the schema',
-    'simpleschema.notUnique': '{label} must be unique',
-    'simpleschema.regEx.0': '{label} failed regular expression validation',
-    'simpleschema.regEx.1': '{label} must be a valid e-mail address',
-    'simpleschema.regEx.2': '{label} must be a valid e-mail address',
-    'simpleschema.regEx.3': '{label} must be a valid domain',
-    'simpleschema.regEx.4': '{label} must be a valid domain',
-    'simpleschema.regEx.5': '{label} must be a valid IPv4 or IPv6 address',
-    'simpleschema.regEx.6': '{label} must be a valid IPv4 address',
-    'simpleschema.regEx.7': '{label} must be a valid IPv6 address',
-    'simpleschema.regEx.8': '{label} must be a valid URL',
-    'simpleschema.regEx.9': '{label} must be a valid alphanumeric ID'
+    'simpleschema.required': '{label} مطلوب',
+    'simpleschema.minString': '{label} يجب أن يكون على الأقل {min, number} أحرف',
+    'simpleschema.maxString': '{label} لا يمكن أن يتجاوز {max, number} أحرف',
+    'simpleschema.minNumber': '{label} يجب أن يكون على الأقل {min, number}',
+    'simpleschema.maxNumber': '{label} لا يمكن أن يتجاوز {max, number}',
+    'simpleschema.minNumberExclusive': '{label} يجب أن يكون أكبر من {min, number}',
+    'simpleschema.maxNumberExclusive': '{label} يجب أن يكون أقل من {max, number}',
+    'simpleschema.minDate': '{label} يجب أن يكون في أو بعد {min, date}',
+    'simpleschema.maxDate': '{label} لا يمكن أن يكون بعد {max, date}',
+    'simpleschema.badDate': '{label} ليس تاريخاً صحيحاً',
+    'simpleschema.minCount': 'يجب تحديد {minCount, number} قيمة على الأقل',
+    'simpleschema.maxCount': 'لا يمكن تحديد أكثر من {maxCount, number} قيمة',
+    'simpleschema.noDecimal': '{label} يجب أن يكون عدداً صحيحاً',
+    'simpleschema.notAllowed': '{value} ليس قيمة مسموح بها',
+    'simpleschema.expectedString': '{label} يجب أن يكون سلسلة نصية',
+    'simpleschema.expectedNumber': '{label} يجب أن يكون رقماً',
+    'simpleschema.expectedBoolean': '{label} يجب أن يكون قيمة منطقية',
+    'simpleschema.expectedArray': '{label} يجب أن يكون مصفوفة',
+    'simpleschema.expectedObject': '{label} يجب أن يكون كائن',
+    'simpleschema.expectedConstructor': '{label} يجب أن يكون {type}',
+    'simpleschema.keyNotInSchema': '{key} غير مسموح به حسب المخطط',
+    'simpleschema.notUnique': '{label} يجب أن يكون فريداً',
+    'simpleschema.regEx.0': '{label} فشل في التحقق من التعبير العادي',
+    'simpleschema.regEx.1': '{label} يجب أن يكون عنوان بريد إلكتروني صالح',
+    'simpleschema.regEx.2': '{label} يجب أن يكون عنوان بريد إلكتروني صالح',
+    'simpleschema.regEx.3': '{label} يجب أن يكون نطاق صالح',
+    'simpleschema.regEx.4': '{label} يجب أن يكون نطاق صالح',
+    'simpleschema.regEx.5': '{label} يجب أن يكون عنوان IP صالح (IPv4 أو IPv6)',
+    'simpleschema.regEx.6': '{label} يجب أن يكون عنوان IP صالح (IPv4)',
+    'simpleschema.regEx.7': '{label} يجب أن يكون عنوان IP صالح (IPv6)',
+    'simpleschema.regEx.8': '{label} يجب أن يكون رابط صالح',
+    'simpleschema.regEx.9': '{label} يجب أن يكون معرف أبجدي رقمي صالح'
 };
