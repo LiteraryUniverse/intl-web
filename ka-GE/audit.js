9f316d0ac55fc42348bfe5ad9bc5c0e3256300f9
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:18:43 +0100

    New translations audit.js (Georgian)

--- ka-GE/audit.js
+++ ka-GE/audit.js
@@ -1,6 +1,6 @@
 export default {
-    'auditLog.header.date': 'Date',
-    'auditLog.header.action': 'What',
-    'auditLog.header.group': 'Group',
-    'auditLog.header.user': 'Who'
+    'auditLog.header.date': 'თარიღი',
+    'auditLog.header.action': 'რა',
+    'auditLog.header.group': 'ჯგუფი',
+    'auditLog.header.user': 'ვინ'
 };
