29db8e0cc9c1ff46ed3b0d12608b86384c5c11d3
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:15:38 +0100

    New translations audit.js (Irish)

--- ga-IE/audit.js
+++ ga-IE/audit.js
@@ -1,6 +1,6 @@
 export default {
-    'auditLog.header.date': 'Date',
-    'auditLog.header.action': 'What',
-    'auditLog.header.group': 'Group',
-    'auditLog.header.user': 'Who'
+    'auditLog.header.date': 'Dáta',
+    'auditLog.header.action': 'Cad',
+    'auditLog.header.group': 'Grúpa',
+    'auditLog.header.user': 'Cé'
 };
