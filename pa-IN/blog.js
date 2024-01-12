27ec8bf145a52ceb37bdd782aa15ec62e464ba9f
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:21:07 +0100

    New translations blog.js (Punjabi)

--- pa-IN/blog.js
+++ pa-IN/blog.js
@@ -7,11 +7,11 @@ export default {
   }.`,
     'blog.create': 'ਇੱਕ ਨਵਾਂ ਬਲਾਗ ਬਣਾਓ',
     'blog.settings': `{type, select,
-    org {Blog}
-    universe {Universe page}
-    user {Blog}
+    org {ਬਲਾਗ}
+    universe {ਯੂਨੀਵਰਸ ਪੰਨਾ}
+    user {ਬਲਾਗ}
     other {}
-  } settings`,
+  } ਸੈਟਿੰਗਾਂ`,
     'blog.description': 'ਬਲਾਗ ਦਾ ਪਰਿਚਯ',
     'blog.theme': 'ਥੀਮ',
     'blog.theme.info': 'ਥੀਮ ਨੂੰ ਸੰਰਚਿਤ ਕਰਕੇ ਤੁਸੀਂ ਪੇਜ ਦੀ ਲੋਕ ਬਦਲ ਸਕਦੇ ਹੋ। ਇਹ ਸੁਵਿਧਾ ਇਸ ਸਮੇਂ ਤਿਆਰ ਨਹੀਂ ਹੈ।',
@@ -28,31 +28,31 @@ export default {
     'blog.publicView': 'ਜਨਤਕ ਵੇਖਾਓ',
     'blog.post.notfound': 'ਬਲਾਗ ਪੋਸਟ ਨਹੀਂ ਲੱਭੀ',
     'blog.lists.works': `{type, select,
-    universes {Universes}
-    stories {Standalone stories}
-    fanfiction {Fan Fiction work}
+    universes {ਯੂਨੀਵਰਸ}
+    stories {ਸਟੈਂਡਅਲੋਨ ਕਹਾਣੀਆਂ}
+    fanfiction {ਫੈਨ ਫਿਕਸ਼ਨ ਕੰਮ}
     other {}
   }`,
     'blog.lists.stories.disclaimer': 'ਇੱਥੇ ਵੱਖ-ਵੱਖ ਕਹਾਣੀਆਂ ਦੀ ਸੂਚੀ ਹਨ, ਜੇ ਕਹਾਣੀ ਕਿਸੇ ਯੂਨੀਵਰਸ ਦਾ ਹਿੱਸਾ ਹੈ ਤਾਂ ਉਹ ਯੂਨੀਵਰਸ ਪੇਜ ਤੇ ਸੂਚੀਬੱਧ ਕੀਤੀ ਜਾਵੇਗੀ।',
-    'blog.social.sameAsMain': `Social links are the same as set in {type, select,
-    organization {organization settings}
-    user {user profile}
+    'blog.social.sameAsMain': `ਸੋਸ਼ਲ ਲਿੰਕ ਮੁੱਖ ਸੈਟਿੰਗਾਂ ਦੇ ਤਰੀਕੇ ਨਾਲ ਸਮਾਨ ਹਨ ਜੋ ਉਪਭੋਗਤਾ ਪ੍ਰੋਫਾਈਲ ਵਿੱਚ ਸੈਟ ਕੀਤੇ ਗਏ ਹਨ {type, select,
+    organization {ਸੰਗਠਨ ਸੈਟਿੰਗਾਂ}
+    user {ਯੂਜ਼ਰ ਪ੍ਰੋਫਾਈਲ}
     other {}
   }`,
     'blog.social.website': 'ਆਧਿਕਾਰਿਕ ਵੈੱਬਸਾਈਟ',
     'blog.settings.universeSettings': 'ਯੂਨੀਵਰਸ ਬਲਾਗ ਯੂਨੀਵਰਸ ਸੈਟਿੰਗਾਂ ਤੋਂ ਲਿਆਈਆਂ ਮੁੱਖ ਸੈਟਿੰਗਾਂ ਨਾਲ ਯੂਨੀਵਰਸ ਪੇਜ਼ਾਂ ਵਿੱਚ ਸਮੇਟੇ ਜਾਂਦੇ ਹਨ।',
-    'blog.settings.organizationSettings': 'Organization blogs are integrated into organization pages.',
+    'blog.settings.organizationSettings': 'ਸੰਗਠਨ ਬਲੌਗ ਸੰਗਠਨ ਪੰਨਿਆਂ ਵਿੱਚ ਸਮੇਲ ਹੋਏ ਹਨ।',
     'blogs.lu': 'ਆਧਾਰਿਤ ਸਾਹਿਤਕ ਯੂਨੀਵਰਸ ਬਲਾਗ',
     'blogs.featured': 'ਪ੍ਰਮੁੱਖ ਬਲਾਗ',
     'blogs.new': 'ਨਵੀਨਤਮ ਬਲਾਗ',
     'blogs.title': 'ਸਾਹਿਤਕ ਯੂਨੀਵਰਸ ਦੇ ਬਲਾਗ',
     'blogs.description': 'ਸਾਹਿਤਕ ਯੂਨੀਵਰਸ ਪਲੇਟਫਾਰਮ \'ਤੇ ਬਲਾਗਾਂ ਦੀ ਸੂਚੀ।',
     'blogs.visit': 'ਬਲਾਗ ਵੇਖੋ',
-    'blog.settingsAria': 'Settings categories for the blog',
-    'blog.settings.blog': 'Blog',
-    'blog.settings.forum': 'Forum',
-    'blog.post.introText': 'Introduction paragraph',
-    'blog.post.writtenBy': 'Written by {usernameDisplay}',
-    'blog.post.writtenByFor': 'Written by {usernameDisplay} for {publicationName}',
-    'blog.posts.recent': 'Recent posts'
+    'blog.settingsAria': 'ਬਲਾਗ ਲਈ ਸੈਟਿੰਗਾਂ ਦੇ ਸ਼੍ਰੇਣੀਆਂ',
+    'blog.settings.blog': 'ਬਲਾਗ',
+    'blog.settings.forum': 'ਫੋਰਮ',
+    'blog.post.introText': 'ਪਰਿਚਯ ਪੈਰਾਗਰਾਫ',
+    'blog.post.writtenBy': 'ਲਿਖਿਆ ਗਿਆ ਹੈ {usernameDisplay}',
+    'blog.post.writtenByFor': 'ਲਿਖਿਆ ਗਿਆ ਹੈ {usernameDisplay} ਦੁਆਰਾ {publicationName}ਲਈ',
+    'blog.posts.recent': 'ਤਾਜ਼ਾ ਪੋਸਟ'
 };
