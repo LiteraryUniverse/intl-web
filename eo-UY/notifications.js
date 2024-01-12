4bd5384f64343e0a8d0ab15dcbebe617ee3c50ab
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:32:25 +0100

    New translations notifications.js (Esperanto)

--- eo-UY/notifications.js
+++ eo-UY/notifications.js
@@ -1,27 +1,27 @@
 // Notifications & flashnews
 export default {
-  'flashnews.create': 'Create a new news flash',
-  'flashnews.startsAt': 'Start displaying this message at',
-  'flashnews.endsAt': 'Stop displaying this message at',
-  'flashnews.newLanguage': 'Select new language to add',
-  'flashnews.onlyDisplayOn': 'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown, even in the default language, in other languages.',
-  'notifications.menu.all': 'All notification',
-  'notifications.menu.following': 'Following',
-  'notifications.menu.workshop': 'Workshop',
-  'notifications.menu.news': 'News & updates',
-  'notifications.menu.markUnread': 'Mark as unread',
-  'notifications.menu.markRead': 'Mark as read',
-  'notifications.menu.delete': 'Delete notification',
+  'flashnews.create': 'Krei novan novaĵan fulmon',
+  'flashnews.startsAt': 'Komenci montri ĉi tiun mesaĝon je',
+  'flashnews.endsAt': 'Ĉesi montri ĉi tiun mesaĝon je',
+  'flashnews.newLanguage': 'Elekti novan lingvon por aldoni',
+  'flashnews.onlyDisplayOn': 'La novaĵo estos montrata nur en la elektitaj lingvoj malsupre (se iuj estas elektitaj), ne estos montrataj novaĵoj, eĉ en la defaŭlta lingvo, en aliaj lingvoj.',
+  'notifications.menu.all': 'Ĉiuj sciigoj',
+  'notifications.menu.following': 'Sekvado',
+  'notifications.menu.workshop': 'Verkstato',
+  'notifications.menu.news': 'Novaĵoj kaj ĝisdatigoj',
+  'notifications.menu.markUnread': 'Marki kiel nelegita',
+  'notifications.menu.markRead': 'Marki kiel legita',
+  'notifications.menu.delete': 'Forigi sciigon',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus': 'Your story {storyName} was automatically set to hiatus after 6 months of inactivity.',
-  'notification.workshopStoryStatus.toCancelled': 'Your story {storyName} was automatically set to cancelled after one year of inactivity.',
-  'notification.followingStoryStatus.toHiatus': '{storyName} is on hiatus.',
-  'notification.followingStoryStatus.toCancelled': '{storyName} has been cancelled.',
-  'notification.followingNewChapter.new': 'New chapter in {storyName} has been published!',
-  'notification.workshopStory.newComment': 'has commented on {storyName}',
+  'notification.workshopStoryStatus.toHiatus': 'Via rakonto {storyName} aŭtomate ŝaltiĝis al paŭzo post 6 monatoj de senaktiveco.',
+  'notification.workshopStoryStatus.toCancelled': 'Via rakonto {storyName} aŭtomate ŝaltiĝis al nuligo post unu jaro de senaktiveco.',
+  'notification.followingStoryStatus.toHiatus': '{storyName} estas en paŭzo.',
+  'notification.followingStoryStatus.toCancelled': '{storyName} estas nuligita.',
+  'notification.followingNewChapter.new': 'Nova ĉapitro en {storyName} estis publikigita!',
+  'notification.workshopStory.newComment': 'komentis pri {storyName}',
   // Before this text will be the username of the user
-  'notification.friendRequest': 'sent you a friend request.',
+  'notification.friendRequest': 'sendis al vi peton de amikeco.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Go to story'
+  'notificationExtra.workshopStoryStatus.goToStory': 'Iri al rakonto'
 };
