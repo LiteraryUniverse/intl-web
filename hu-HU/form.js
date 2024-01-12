d01e1b1e6d5b6ab7062bd4a365c1bf2820f56b5d
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:17:02 +0100

    New translations form.js (Hungarian)

--- hu-HU/form.js
+++ hu-HU/form.js
@@ -1,37 +1,37 @@
 export default {
     'form.requiredSymbol': '*',
-    'form.validator.required': 'Required field',
-    'form.validator.mustBeNumber': 'Must be a number',
-    'form.validator.minNumber': 'Should be greater than {min, number}',
-    'form.validator.maxNumber': 'Should be lower than {max, number}',
-    'form.validator.maxLength': `Should be less than {max, number} {what, select,
-    characters {characters}
-    tags {tags}
-    genres {genres}
-    numbers {numbers}
+    'form.validator.required': 'Kötelező mező',
+    'form.validator.mustBeNumber': 'Számnak kell lennie',
+    'form.validator.minNumber': 'Nagyobbnak kell lennie, mint {min, number}',
+    'form.validator.maxNumber': 'Kisebbnek kell lennie, mint {max, number}',
+    'form.validator.maxLength': `Kevesebbnek kell lennie, mint {max, number} {what, select,
+    characters {karakter}
+    tags {címkék}
+    genres {műfajok}
+    numbers {számok}
     other {}
   }`,
-    'form.validator.minLength': `Should be at least {min, number} {what, select,
-    characters {characters}
-    tags {tags}
-    genres {genres}
-    genre {genre}
-    numbers {numbers}
+    'form.validator.minLength': `Legalább {min, number} {what, select,
+    characters {karakternek kell lennie}
+    tags {címkék}
+    genres {műfajok}
+    genre {műfaj}
+    numbers {számok}
     other {}
   }`,
-    'form.validator.email': 'Must be a valid e-mail',
-    'form.validator.url': 'Must be a valid URL',
-    'form.validator.slug': 'Must be a valid part of URL',
-    'form.submitting.signup': 'Creating account...',
-    'form.submitting.signin': 'Logging in...',
-    'form.submitting.default': 'Submitting...',
-    'form.submitting.sending': 'Sending...',
-    'form.validator.whitespace': 'Must not have white space',
-    'form.validator.zipCode': 'Must be a valid ZIP code',
-    'form.validator.isbn': 'We only accept 10 or 13 version of ISBN',
-    'form.validator.btc': 'This is not a valid BTC address.',
-    'form.validator.eth': 'This is not a valid ETH address.',
-    'form.weakPassword': 'This password looks weak. Please consider making it stronger by adding numbers and special characters.',
+    'form.validator.email': 'Érvényes e-mail címnek kell lennie',
+    'form.validator.url': 'Érvényes URL címnek kell lennie',
+    'form.validator.slug': 'Érvényes résznek kell lennie az URL-ben',
+    'form.submitting.signup': 'Fiók létrehozása...',
+    'form.submitting.signin': 'Bejelentkezés...',
+    'form.submitting.default': 'Küldés...',
+    'form.submitting.sending': 'Küldés...',
+    'form.validator.whitespace': 'Nem lehet szóköz',
+    'form.validator.zipCode': 'Érvényes irányítószámnak kell lennie',
+    'form.validator.isbn': 'Csak a 10 vagy 13 verziók elfogadottak az ISBN-nél',
+    'form.validator.btc': 'Ez nem érvényes BTC cím.',
+    'form.validator.eth': 'Ez nem érvényes ETH cím.',
+    'form.weakPassword': 'A jelszó gyenge. Kérlek, fontold meg, hogy erősítsd meg a számok és speciális karakterek hozzáadásával.',
     // you can get these messages from: https://github.com/gwendall/meteor-simple-schema-i18n/tree/master/i18n
     // but don't forget to adjust the format
     'simpleschema.required': '{label} megadása kötelező',
@@ -55,7 +55,7 @@ export default {
     'simpleschema.expectedObject': '{label} objektum tipusúnak kell lennie',
     'simpleschema.expectedConstructor': '{label} {type} típusúnak kell lennie',
     'simpleschema.keyNotInSchema': '{key} érték a séma miatt nem engedélyezett',
-    'simpleschema.notUnique': '{label} must be unique',
+    'simpleschema.notUnique': '{label} egyedinek kell lennie',
     'simpleschema.regEx.0': '{label} sikertelen ellenőrzés a reguláris kifejezésre',
     'simpleschema.regEx.1': '{label} érvényes e-mail címnek kell lennie',
     'simpleschema.regEx.2': '{label} érvényes e-mail címnek kell lennie',
