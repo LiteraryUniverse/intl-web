5f917744c4a07ac7b911767926a0144ce7337499
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:31:33 +0100

    New translations form.js (Telugu)

--- te-IN/form.js
+++ te-IN/form.js
@@ -1,69 +1,69 @@
 export default {
     'form.requiredSymbol': '*',
-    'form.validator.required': 'Required field',
-    'form.validator.mustBeNumber': 'Must be a number',
-    'form.validator.minNumber': 'Should be greater than {min, number}',
-    'form.validator.maxNumber': 'Should be lower than {max, number}',
-    'form.validator.maxLength': `Should be less than {max, number} {what, select,
-    characters {characters}
-    tags {tags}
-    genres {genres}
-    numbers {numbers}
+    'form.validator.required': 'అవసరమైన ఫీల్డ్',
+    'form.validator.mustBeNumber': 'సంఖ్య అయినది అయినా ఉండాలి',
+    'form.validator.minNumber': 'మంది గొప్పగా ఉండాలి {min, number}',
+    'form.validator.maxNumber': 'మంది తక్కువగా ఉండాలి {max, number}',
+    'form.validator.maxLength': `కనీస ఉండాలి {max, number} {what, select,
+    characters {అక్షరాలు}
+    tags {ట్యాగ్‌లు}
+    genres {జాన్రెస్‌లు}
+    numbers {సంఖ్యలు}
     other {}
   }`,
-    'form.validator.minLength': `Should be at least {min, number} {what, select,
-    characters {characters}
-    tags {tags}
-    genres {genres}
-    genre {genre}
-    numbers {numbers}
+    'form.validator.minLength': `కనీసం ఉండాలి {min, number} {what, select,
+    characters {అక్షరాలు}
+    tags {ట్యాగ్‌లు}
+    genres {జాన్రెస్‌లు}
+    genre {జాన్రె}
+    numbers {సంఖ్యలు}
     other {}
   }`,
-    'form.validator.email': 'Must be a valid e-mail',
-    'form.validator.url': 'Must be a valid URL',
-    'form.validator.slug': 'Must be a valid part of URL',
-    'form.submitting.signup': 'Creating account...',
-    'form.submitting.signin': 'Logging in...',
-    'form.submitting.default': 'Submitting...',
-    'form.submitting.sending': 'Sending...',
-    'form.validator.whitespace': 'Must not have white space',
-    'form.validator.zipCode': 'Must be a valid ZIP code',
-    'form.validator.isbn': 'We only accept 10 or 13 version of ISBN',
-    'form.validator.btc': 'This is not a valid BTC address.',
-    'form.validator.eth': 'This is not a valid ETH address.',
-    'form.weakPassword': 'This password looks weak. Please consider making it stronger by adding numbers and special characters.',
+    'form.validator.email': 'ఒక మాన్యమైన ఇమెయిల్ అయినది అయినా ఉండాలి',
+    'form.validator.url': 'ఒక మాన్యమైన URL అయినది అయినా ఉండాలి',
+    'form.validator.slug': 'URL యొక్క ఒక మాన్యమైన భాగం అయినది అయినా ఉండాలి',
+    'form.submitting.signup': 'ఖాతా సృష్టించినప్పుడు...',
+    'form.submitting.signin': 'లాగిన్ అవుతున్నాం...',
+    'form.submitting.default': 'సమర్పిస్తోంది...',
+    'form.submitting.sending': 'పంపించింది...',
+    'form.validator.whitespace': 'శ్వేత స్థలం ఉండకూడదు',
+    'form.validator.zipCode': 'ఒక మాన్యమైన ZIP కోడ్ అయినది అయినా ఉండాలి',
+    'form.validator.isbn': 'మాత్రమే 10 లేదా 13 వర్షను నమ్ముకోవచ్చు',
+    'form.validator.btc': 'ఇది ఒక చెల్లని BTC చిరునామా కాదు.',
+    'form.validator.eth': 'ఇది ఒక చెల్లని ETH చిరునామా కాదు.',
+    'form.weakPassword': 'ఈ పాస్వర్డు బలహీనంగా ఉంది. దయచేసి అదనపు సంఖ్యలు మరియు విశేష అక్షరాలను జోడించి ఇంకా బలహీనంగా చేయండి.',
     // you can get these messages from: https://github.com/gwendall/meteor-simple-schema-i18n/tree/master/i18n
     // but don't forget to adjust the format
-    'simpleschema.required': '{label} is required',
-    'simpleschema.minString': '{label} must be at least {min, number} characters',
-    'simpleschema.maxString': '{label} cannot exceed {max, number} characters',
-    'simpleschema.minNumber': '{label} must be at least {min, number}',
-    'simpleschema.maxNumber': '{label} cannot exceed {max, number}',
-    'simpleschema.minNumberExclusive': '{label} must be greater than {min, number}',
-    'simpleschema.maxNumberExclusive': '{label} must be less than {max, number}',
-    'simpleschema.minDate': '{label} must be on or after {min, date}',
-    'simpleschema.maxDate': '{label} cannot be after {max, date}',
-    'simpleschema.badDate': '{label} is not a valid date',
-    'simpleschema.minCount': 'You must specify at least {minCount, number} values',
-    'simpleschema.maxCount': 'You cannot specify more than {maxCount, number} values',
-    'simpleschema.noDecimal': '{label} must be an integer',
-    'simpleschema.notAllowed': '{value} is not an allowed value',
-    'simpleschema.expectedString': '{label} must be a string',
-    'simpleschema.expectedNumber': '{label} must be a number',
-    'simpleschema.expectedBoolean': '{label} must be a boolean',
-    'simpleschema.expectedArray': '{label} must be an array',
-    'simpleschema.expectedObject': '{label} must be an object',
-    'simpleschema.expectedConstructor': '{label} must be a {type}',
-    'simpleschema.keyNotInSchema': '{key} is not allowed by the schema',
-    'simpleschema.notUnique': '{label} must be unique',
-    'simpleschema.regEx.0': '{label} failed regular expression validation',
-    'simpleschema.regEx.1': '{label} must be a valid e-mail address',
-    'simpleschema.regEx.2': '{label} must be a valid e-mail address',
-    'simpleschema.regEx.3': '{label} must be a valid domain',
-    'simpleschema.regEx.4': '{label} must be a valid domain',
-    'simpleschema.regEx.5': '{label} must be a valid IPv4 or IPv6 address',
-    'simpleschema.regEx.6': '{label} must be a valid IPv4 address',
-    'simpleschema.regEx.7': '{label} must be a valid IPv6 address',
-    'simpleschema.regEx.8': '{label} must be a valid URL',
-    'simpleschema.regEx.9': '{label} must be a valid alphanumeric ID'
+    'simpleschema.required': '{label} అవసరం',
+    'simpleschema.minString': '{label} కనీసం {min, number} అక్షరాలు ఉండాలి',
+    'simpleschema.maxString': '{label} పైకి పోయింది {max, number} అక్షరాలు కనిపించకూడదు',
+    'simpleschema.minNumber': '{label} కనీసం {min, number}ఉండాలి',
+    'simpleschema.maxNumber': '{label} పైకి పోయింది {max, number}కనిపించకూడదు',
+    'simpleschema.minNumberExclusive': '{label} గదిలోకి వెళ్లాలి {min, number}',
+    'simpleschema.maxNumberExclusive': '{label} తక్కువగా ఉండాలి {max, number}',
+    'simpleschema.minDate': '{label} కనీసం లేదా {min, date}తర్వాత ఉండాలి',
+    'simpleschema.maxDate': '{label} తర్వాత ఉండకుండా ఉండాలి {max, date}',
+    'simpleschema.badDate': '{label} చెల్లని తేదీ',
+    'simpleschema.minCount': 'మీరు కనీసం {minCount, number} విలువలను నిర్దిష్టం చేయాలి',
+    'simpleschema.maxCount': 'మీరు {maxCount, number} విలువలను పెరిగించలేరు',
+    'simpleschema.noDecimal': '{label} పూర్ణాంకం అయినది అయినా ఉండాలి',
+    'simpleschema.notAllowed': '{value} అనుమతించబడని విలువ',
+    'simpleschema.expectedString': '{label} వచ్చినది ఒక స్ట్రింగ్ అయినది అయినా ఉండాలి',
+    'simpleschema.expectedNumber': '{label} వచ్చినది ఒక సంఖ్య అయినది అయినా ఉండాలి',
+    'simpleschema.expectedBoolean': '{label} వచ్చినది ఒక బూలియన్ అయినది అయినా ఉండాలి',
+    'simpleschema.expectedArray': '{label} వచ్చినది ఒక అర్రాయ్ అయినది అయినా ఉండాలి',
+    'simpleschema.expectedObject': '{label} వచ్చినది ఒక ఆబ్జెక్ట్ అయినది అయినా ఉండాలి',
+    'simpleschema.expectedConstructor': '{label} వచ్చినది ఒక {type}',
+    'simpleschema.keyNotInSchema': '{key} స్కీమాను అనుమతించబడని కీ కాదు',
+    'simpleschema.notUnique': '{label} అనుకూలంగా ఉండాలి',
+    'simpleschema.regEx.0': '{label} విఫలమైన సామాన్య వ్యాఖ్యాన చెప్పినది',
+    'simpleschema.regEx.1': '{label} ఒక మాన్యత పొందిన ఇమెయిల్ చిరునామా అయింది',
+    'simpleschema.regEx.2': '{label} ఒక మాన్యత పొందిన ఇమెయిల్ చిరునామా అయింది',
+    'simpleschema.regEx.3': '{label} ఒక మాన్యత పొందిన డొమైన్ అయింది',
+    'simpleschema.regEx.4': '{label} ఒక మాన్యత పొందిన డొమైన్ అయింది',
+    'simpleschema.regEx.5': '{label} ఒక మాన్యత పొందిన IPv4 లేదా IPv6 చిరునామా అయింది',
+    'simpleschema.regEx.6': '{label} ఒక మాన్యత పొందిన IPv4 చిరునామా అయింది',
+    'simpleschema.regEx.7': '{label} ఒక మాన్యత పొందిన IPv6 చిరునామా అయింది',
+    'simpleschema.regEx.8': '{label} ఒక మాన్యత పొందిన URL అయింది',
+    'simpleschema.regEx.9': '{label} ఒక మాన్యత పొందిన అక్షరసమంఖ్య ఐడి అయింది'
 };
