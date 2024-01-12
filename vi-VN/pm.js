e2e2554b804148ab6122035cdf607340b0c05693
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:25:44 +0100

    New translations pm.js (Vietnamese)

--- vi-VN/pm.js
+++ vi-VN/pm.js
@@ -1,25 +1,25 @@
 export default {
-    'pm.conversation': 'Conversation',
-    'pm.showolder': 'Show older messages',
-    'pm.errors.saysomething': 'You should really say something...',
-    'pm.errors.addrecipient': 'You need to add at least one other user.',
-    'pm.message': 'Message',
-    'pm.send.message': 'Send message',
-    'pm.messages.own': 'Your messages',
-    'pm.conversations.none': 'You are currently not conversing with anyone.',
-    'pm.to': 'To:',
-    'pm.with': 'Conversation with:',
-    'pm.send': 'Send',
-    'pm.conversations.unreadCount': `You have {count, plural,
-    zero {no unread conversations}
-    one {one unread conversation}
-    other {# unread conversations}
+    'pm.conversation': 'Cuộc trò chuyện',
+    'pm.showolder': 'Hiển thị tin nhắn cũ hơn',
+    'pm.errors.saysomething': 'Bạn nên nói gì đó...',
+    'pm.errors.addrecipient': 'Bạn cần thêm ít nhất một người dùng khác.',
+    'pm.message': 'Tin nhắn',
+    'pm.send.message': 'Gửi tin nhắn',
+    'pm.messages.own': 'Tin nhắn của bạn',
+    'pm.conversations.none': 'Bạn hiện không đang trò chuyện với ai.',
+    'pm.to': 'Đến:',
+    'pm.with': 'Cuộc trò chuyện với:',
+    'pm.send': 'Gửi',
+    'pm.conversations.unreadCount': `Bạn không có {count, plural,
+    zero {cuộc trò chuyện chưa đọc}
+    one {một cuộc trò chuyện chưa đọc}
+    other {# cuộc trò chuyện chưa đọc}
   }.`,
-    'pm.form.minimumParticipants': 'You need to choose a recipient.',
-    'pm.form.maxParticipants': `You can have maximum of {maxPeople, plural,
-    one {one person}
-    other {# people}
-  } in a conversation.`,
-    'pm.new.aria': 'New conversation',
-    'pm.new.buttonTitle': 'Start a new conversation'
+    'pm.form.minimumParticipants': 'Bạn cần chọn một người nhận.',
+    'pm.form.maxParticipants': `Bạn chỉ có thể có tối đa {maxPeople, plural,
+    one {một người}
+    other {# người}
+  } trong một cuộc trò chuyện.`,
+    'pm.new.aria': 'Cuộc trò chuyện mới',
+    'pm.new.buttonTitle': 'Bắt đầu cuộc trò chuyện mới'
 };
