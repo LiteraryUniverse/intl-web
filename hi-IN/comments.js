f1839dd6cab40e0d667b53232da6d97a4fab7d1d
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:30:42 +0100

    New translations comments.js (Hindi)

--- hi-IN/comments.js
+++ hi-IN/comments.js
@@ -1,10 +1,10 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
-    'comments.total': `There {count, plural,
-    =0 {are no comments}
-    one {is 1 comment}
-    other {are # comments}
-  }.`,
-    'comments.show.older': 'Show older comments'
+    'comment.write': 'अपनी टिप्पणी लिखें',
+    'comments.none': 'क्षमा करें, कोई टिप्पणी नहीं है।',
+    'comments.total': `यहाँ {count, plural,
+    =0 {कोई टिप्पणी नहीं है}
+    one {1 टिप्पणी है}
+    other {# टिप्पणियाँ हैं}
+  }।`,
+    'comments.show.older': 'पुरानी टिप्पणियाँ दिखाएँ'
 };
