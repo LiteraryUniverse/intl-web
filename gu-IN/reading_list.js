d851bf03a049355833a58b766a8d09db53c4b8ad
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:16:20 +0100

    New translations reading_list.js (Gujarati)

--- gu-IN/reading_list.js
+++ gu-IN/reading_list.js
@@ -1,26 +1,26 @@
 export default {
-  'readingList.title': 'Reading list',
-  'readingList.type.label': 'List type',
-  'readingList.type.stories': 'Stories',
-  'readingList.type.universes': 'Universes',
-  'readingList.ordered.label': 'Ranked list',
-  'readingList.button.add': 'Add to reading list',
-  'readingList.selectList': 'Select reading list',
-  'readingList.create': 'Create a new reading list',
-  'readingList.create.aria': 'Modal with settings for a new reading list',
-  'readingList.edit.aria': 'Modal to edit reading list\'s settings',
-  'readingList.library.title': 'Your reading lists',
-  'readingList.public.label': 'Is publicly visible?',
-  'readingList.addItem': `Add {type, select,
-    universes {universe}
-    stories {story}
-    other {stories and universes}
+  'readingList.title': 'યાદદાન સૂચી',
+  'readingList.type.label': 'યાદદાન પ્રકાર',
+  'readingList.type.stories': 'વાર્તાઓ',
+  'readingList.type.universes': 'બ્રહ્માંડો',
+  'readingList.ordered.label': 'ક્રમાંકિત યાદદાન',
+  'readingList.button.add': 'યાદદાનમાં ઉમેરો',
+  'readingList.selectList': 'યાદદાન પસંદ કરો',
+  'readingList.create': 'નવી યાદદાન બનાવો',
+  'readingList.create.aria': 'નવી યાદદાન માટેની સેટિંગ્સની સાથેની મોડલ',
+  'readingList.edit.aria': 'યાદદાનની સેટિંગ્સની સાથેની મોડલ એડિટ કરવા માટે',
+  'readingList.library.title': 'તમારા યાદદાનો',
+  'readingList.public.label': 'સાર્વજનિક દૃશ્યમાં છે?',
+  'readingList.addItem': `યાદદાનમાં ઉમેરો {type, select,
+    universes {વિશ્વ}
+    stories {વાર્તા}
+    other {વાર્તાઓ અને વિશ્વો}
   }`,
-  'readingList.addItem.modalAria': `Modal to add {type, select,
-    universes {universes}
-    stories {stories}
-    other {stories and universes}
-  } to the reading list.`,
-  'readingList.addItem.fromLibrary': 'From library',
-  'readingList.button.added': 'Added'
+  'readingList.addItem.modalAria': `યાદદાનમાં ઉમેરવા માટેની મોડલ {type, select,
+    universes {વિશ્વો}
+    stories {વાર્તાઓ}
+    other {વાર્તાઓ અને વિશ્વો}
+  } માટે.`,
+  'readingList.addItem.fromLibrary': 'પુસ્તકાલયમાંથી',
+  'readingList.button.added': 'ઉમેરાયેલ'
 };
