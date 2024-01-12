64b61f12fbdbf646d00654003b54872021af7ba5
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:12:51 +0100

    New translations library.js (Catalan)

--- ca-ES/library.js
+++ ca-ES/library.js
@@ -1,8 +1,8 @@
 export default {
-    'library.add': 'Add to library',
-    'library.remove': 'Remove from library',
-    'library.bought': 'Bought stories',
-    'library.stories': 'Followed stories',
+    'library.add': 'Afegir a la biblioteca',
+    'library.remove': 'Eliminar de la biblioteca',
+    'library.bought': 'Històries comprades',
+    'library.stories': 'Històries seguides',
     'library.universes': 'Followed universes',
     'bookmark.continueReading': 'Continue reading',
     'bookmark.statusSummary': 'You have read {readPercent, number, ::percent} of the story.'
