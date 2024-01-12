7353ef87ddff8866aa1e54abe0295c3e2d536858
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:16:29 +0100

    New translations form.js (Hebrew)

--- he-IL/form.js
+++ he-IL/form.js
@@ -1,69 +1,69 @@
 export default {
     'form.requiredSymbol': '*',
-    'form.validator.required': 'Required field',
-    'form.validator.mustBeNumber': 'Must be a number',
-    'form.validator.minNumber': 'Should be greater than {min, number}',
-    'form.validator.maxNumber': 'Should be lower than {max, number}',
-    'form.validator.maxLength': `Should be less than {max, number} {what, select,
-    characters {characters}
-    tags {tags}
-    genres {genres}
-    numbers {numbers}
+    'form.validator.required': 'שדה חובה',
+    'form.validator.mustBeNumber': 'יש להזין מספר',
+    'form.validator.minNumber': 'יש להיות גדול מ- {min, number}',
+    'form.validator.maxNumber': 'יש להיות קטן מ- {max, number}',
+    'form.validator.maxLength': `יש להיות פחות מ- {max, number} {what, select,
+    characters {תווים}
+    tags {תגים}
+    genres {ז'אנרים}
+    numbers {מספרים}
     other {}
   }`,
-    'form.validator.minLength': `Should be at least {min, number} {what, select,
-    characters {characters}
-    tags {tags}
-    genres {genres}
-    genre {genre}
-    numbers {numbers}
+    'form.validator.minLength': `יש להיות לפחות {min, number} {what, select,
+    characters {תווים}
+    tags {תגים}
+    genres {ז'אנרים}
+    genre {ז'אנר}
+    numbers {מספרים}
     other {}
   }`,
-    'form.validator.email': 'Must be a valid e-mail',
-    'form.validator.url': 'Must be a valid URL',
-    'form.validator.slug': 'Must be a valid part of URL',
-    'form.submitting.signup': 'Creating account...',
-    'form.submitting.signin': 'Logging in...',
-    'form.submitting.default': 'Submitting...',
-    'form.submitting.sending': 'Sending...',
-    'form.validator.whitespace': 'Must not have white space',
-    'form.validator.zipCode': 'Must be a valid ZIP code',
-    'form.validator.isbn': 'We only accept 10 or 13 version of ISBN',
-    'form.validator.btc': 'This is not a valid BTC address.',
-    'form.validator.eth': 'This is not a valid ETH address.',
-    'form.weakPassword': 'This password looks weak. Please consider making it stronger by adding numbers and special characters.',
+    'form.validator.email': 'יש להזין כתובת אימייל תקנית',
+    'form.validator.url': 'יש להזין כתובת URL תקנית',
+    'form.validator.slug': 'יש להזין חלק תקני של כתובת URL',
+    'form.submitting.signup': 'יוצר חשבון...',
+    'form.submitting.signin': 'מתחבר...',
+    'form.submitting.default': 'מכניס...',
+    'form.submitting.sending': 'שולח...',
+    'form.validator.whitespace': 'אסור להכיל רווחים',
+    'form.validator.zipCode': 'יש להזין מיקוד תקני',
+    'form.validator.isbn': 'אנו מקבלים רק גרסאות 10 או 13 של ISBN',
+    'form.validator.btc': 'זהו לא כתובת BTC תקפה.',
+    'form.validator.eth': 'זהו לא כתובת ETH תקפה.',
+    'form.weakPassword': 'הסיסמה נראית חלשה. יש לשקול להפעיל אותה על ידי הוספת מספרים ותווים מיוחדים.',
     // you can get these messages from: https://github.com/gwendall/meteor-simple-schema-i18n/tree/master/i18n
     // but don't forget to adjust the format
-    'simpleschema.required': '{label} is required',
-    'simpleschema.minString': '{label} must be at least {min, number} characters',
-    'simpleschema.maxString': '{label} cannot exceed {max, number} characters',
-    'simpleschema.minNumber': '{label} must be at least {min, number}',
-    'simpleschema.maxNumber': '{label} cannot exceed {max, number}',
-    'simpleschema.minNumberExclusive': '{label} must be greater than {min, number}',
-    'simpleschema.maxNumberExclusive': '{label} must be less than {max, number}',
-    'simpleschema.minDate': '{label} must be on or after {min, date}',
-    'simpleschema.maxDate': '{label} cannot be after {max, date}',
-    'simpleschema.badDate': '{label} is not a valid date',
-    'simpleschema.minCount': 'You must specify at least {minCount, number} values',
-    'simpleschema.maxCount': 'You cannot specify more than {maxCount, number} values',
-    'simpleschema.noDecimal': '{label} must be an integer',
-    'simpleschema.notAllowed': '{value} is not an allowed value',
-    'simpleschema.expectedString': '{label} must be a string',
-    'simpleschema.expectedNumber': '{label} must be a number',
-    'simpleschema.expectedBoolean': '{label} must be a boolean',
-    'simpleschema.expectedArray': '{label} must be an array',
-    'simpleschema.expectedObject': '{label} must be an object',
-    'simpleschema.expectedConstructor': '{label} must be a {type}',
-    'simpleschema.keyNotInSchema': '{key} is not allowed by the schema',
-    'simpleschema.notUnique': '{label} must be unique',
-    'simpleschema.regEx.0': '{label} failed regular expression validation',
-    'simpleschema.regEx.1': '{label} must be a valid e-mail address',
-    'simpleschema.regEx.2': '{label} must be a valid e-mail address',
-    'simpleschema.regEx.3': '{label} must be a valid domain',
-    'simpleschema.regEx.4': '{label} must be a valid domain',
-    'simpleschema.regEx.5': '{label} must be a valid IPv4 or IPv6 address',
-    'simpleschema.regEx.6': '{label} must be a valid IPv4 address',
-    'simpleschema.regEx.7': '{label} must be a valid IPv6 address',
-    'simpleschema.regEx.8': '{label} must be a valid URL',
-    'simpleschema.regEx.9': '{label} must be a valid alphanumeric ID'
+    'simpleschema.required': '{label} נדרש',
+    'simpleschema.minString': '!{label} ! חייב להיות לפחות {min, number} תווים',
+    'simpleschema.maxString': '!{label} ! לא יכול להיות יותר מ- {max, number} תווים',
+    'simpleschema.minNumber': '!{label} ! חייב להיות לפחות {min, number}',
+    'simpleschema.maxNumber': '!{label} ! לא יכול להיות יותר מ- {max, number}',
+    'simpleschema.minNumberExclusive': '!{label} ! חייב להיות גדול מ- {min, number}',
+    'simpleschema.maxNumberExclusive': '!{label} ! חייב להיות קטן מ- {max, number}',
+    'simpleschema.minDate': '!{label} ! חייב להיות באותו יום או אחריו מ- {min, date}',
+    'simpleschema.maxDate': '!{label} ! לא יכול להיות אחרי מ- {max, date}',
+    'simpleschema.badDate': '{label} אינו תאריך תקני',
+    'simpleschema.minCount': 'יש לציין לפחות {minCount, number} ערכים',
+    'simpleschema.maxCount': 'אין לציין יותר מ- {maxCount, number} ערכים',
+    'simpleschema.noDecimal': '{label} חייב להיות מספר שלם',
+    'simpleschema.notAllowed': '{value} אינו ערך מורשה',
+    'simpleschema.expectedString': '{label} חייב להיות מחרוזת',
+    'simpleschema.expectedNumber': '{label} חייב להיות מספר',
+    'simpleschema.expectedBoolean': '{label} חייב להיות בוליאני',
+    'simpleschema.expectedArray': '{label} חייב להיות מערך',
+    'simpleschema.expectedObject': '{label} חייב להיות אובייקט',
+    'simpleschema.expectedConstructor': '{label} חייב להיות {type}',
+    'simpleschema.keyNotInSchema': '{key} אינו מותר על פי הסכימה',
+    'simpleschema.notUnique': '{label} חייב להיות ייחודי',
+    'simpleschema.regEx.0': '{label} נכשל באימות ביטוי רגולרי',
+    'simpleschema.regEx.1': '{label} חייב להיות כתובת דוא"ל תקינה',
+    'simpleschema.regEx.2': '{label} חייב להיות כתובת דוא"ל תקינה',
+    'simpleschema.regEx.3': '{label} חייב להיות דומיין תקין',
+    'simpleschema.regEx.4': '{label} חייב להיות דומיין תקין',
+    'simpleschema.regEx.5': '{label} חייב להיות כתובת IP תקינה (IPv4 או IPv6)',
+    'simpleschema.regEx.6': '{label} חייב להיות כתובת IP תקינה (IPv4)',
+    'simpleschema.regEx.7': '{label} חייב להיות כתובת IP תקינה (IPv6)',
+    'simpleschema.regEx.8': '{label} חייב להיות כתובת URL תקינה',
+    'simpleschema.regEx.9': '{label} חייב להיות מזהה אלפאנומרי תקין'
 };
