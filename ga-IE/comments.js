4d4c60089e1ec0cafa6c7d964906d044c6982c83
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:15:39 +0100

    New translations comments.js (Irish)

--- ga-IE/comments.js
+++ ga-IE/comments.js
@@ -1,10 +1,10 @@
 export default {
-    'comment.write': 'Write your comment',
-    'comments.none': 'Sorry, no comments to show.',
+    'comment.write': 'Scríobh do chomhfhreagairt',
+    'comments.none': 'Tá brón orm, níl aon tuairimí le taispeáint.',
     'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-    'comments.show.older': 'Show older comments'
+    'comments.show.older': 'Taispeáin tuairimí níos sine'
 };
