bf90636444d5813178b429272e10fe585c7fdfc7
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:25:29 +0100

    New translations reading_list.js (Chinese Traditional)

--- zh-TW/reading_list.js
+++ zh-TW/reading_list.js
@@ -1,26 +1,26 @@
 export default {
-  'readingList.title': 'Reading list',
-  'readingList.type.label': 'List type',
-  'readingList.type.stories': 'Stories',
-  'readingList.type.universes': 'Universes',
-  'readingList.ordered.label': 'Ranked list',
-  'readingList.button.add': 'Add to reading list',
-  'readingList.selectList': 'Select reading list',
-  'readingList.create': 'Create a new reading list',
-  'readingList.create.aria': 'Modal with settings for a new reading list',
-  'readingList.edit.aria': 'Modal to edit reading list\'s settings',
-  'readingList.library.title': 'Your reading lists',
-  'readingList.public.label': 'Is publicly visible?',
-  'readingList.addItem': `Add {type, select,
-    universes {universe}
-    stories {story}
-    other {stories and universes}
+  'readingList.title': '閱讀清單',
+  'readingList.type.label': '清單類型',
+  'readingList.type.stories': '故事',
+  'readingList.type.universes': '宇宙',
+  'readingList.ordered.label': '排名清單',
+  'readingList.button.add': '新增至閱讀清單',
+  'readingList.selectList': '選擇閱讀清單',
+  'readingList.create': '建立新的閱讀清單',
+  'readingList.create.aria': '具有設定的新閱讀清單對話框',
+  'readingList.edit.aria': '編輯閱讀清單設定的對話框',
+  'readingList.library.title': '您的閱讀清單',
+  'readingList.public.label': '是否公開可見？',
+  'readingList.addItem': `新增 {type, select,
+    universes {個宇宙}
+    stories {個故事}
+    other {個故事和宇宙}
   }`,
-  'readingList.addItem.modalAria': `Modal to add {type, select,
-    universes {universes}
-    stories {stories}
-    other {stories and universes}
-  } to the reading list.`,
-  'readingList.addItem.fromLibrary': 'From library',
-  'readingList.button.added': 'Added'
+  'readingList.addItem.modalAria': `將 {type, select,
+    universes {個宇宙}
+    stories {個故事}
+    other {個故事和宇宙}
+  } 新增至閱讀清單的對話框。`,
+  'readingList.addItem.fromLibrary': '從圖書館中',
+  'readingList.button.added': '已新增'
 };
