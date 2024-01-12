eb82eb16a3f5c2e5a56e51aff50729ab6e4ec72f
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:19:52 +0100

    New translations library.js (Lithuanian)

--- lt-LT/library.js
+++ lt-LT/library.js
@@ -1,9 +1,9 @@
 export default {
-    'library.add': 'Add to library',
-    'library.remove': 'Remove from library',
-    'library.bought': 'Bought stories',
-    'library.stories': 'Followed stories',
-    'library.universes': 'Followed universes',
-    'bookmark.continueReading': 'Continue reading',
-    'bookmark.statusSummary': 'You have read {readPercent, number, ::percent} of the story.'
+    'library.add': 'Pridėti į biblioteką',
+    'library.remove': 'Pašalinti iš bibliotekos',
+    'library.bought': 'Įsigytos pasakos',
+    'library.stories': 'Sekamos pasakos',
+    'library.universes': 'Sekami visatos',
+    'bookmark.continueReading': 'Tęsti skaitymą',
+    'bookmark.statusSummary': 'Jūs perskaitėte {readPercent, number, ::percent} iš pasakos.'
 };
