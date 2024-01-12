94cc322190a036d4234dfcbdc6f3e2d862b1c575
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:30:18 +0100

    New translations roadmap.js (Estonian)

--- et-EE/roadmap.js
+++ et-EE/roadmap.js
@@ -1,37 +1,37 @@
 export default {
-  'roadmap.ariaTimeOptions': 'Selections for roadmap items based on their status.',
-  'roadmap.state.backlog': 'Backlog',
-  'roadmap.state.inprogress': 'In-progress',
-  'roadmap.state.preview': 'Preview',
-  'roadmap.state.beta': 'Beta',
-  'roadmap.state.ga': 'General availability',
+  'roadmap.ariaTimeOptions': 'Valikud teekonnapunktide jaoks nende oleku põhjal.',
+  'roadmap.state.backlog': 'Tagalug',
+  'roadmap.state.inprogress': 'Töös',
+  'roadmap.state.preview': 'Eelvaade',
+  'roadmap.state.beta': 'Beeta',
+  'roadmap.state.ga': 'Üldkättesaadavus',
   'roadmap.eta.q1.short': 'Q1',
   'roadmap.eta.q2.short': 'Q2',
   'roadmap.eta.q3.short': 'Q3',
   'roadmap.eta.q4.short': 'Q4',
-  'roadmap.eta.q1.long': 'First quarter',
-  'roadmap.eta.q2.long': 'Second quarter',
-  'roadmap.eta.q3.long': 'Third quarter',
-  'roadmap.eta.q4.long': 'Fourth quarter',
-  'roadmap.backlog': 'Backlog',
-  'roadmap.history': 'History of development',
-  'roadmap.item.inBacklog': 'We are planning to develop this feature in the future.',
-  'roadmap.item.inProgress': 'The development of the feature started {startedAt, date}.',
-  'roadmap.item.inBeta': 'This feature has entered beta on {betaAt, date}.',
-  'roadmap.item.inPreview': 'This feature became available to Storyteller subscribers on {previewAt, date}.',
-  'roadmap.item.inGa': 'This feature has become generally available on {gaAt, date}.',
-  'roadmap.item.originalIssue': 'This feature was originally suggested by our users.',
-  'roadmap.eta.mapTitle': `Planned for {quarter, select,
-    Q1 {Q1}
-    Q2 {Q2}
-    Q3 {Q3}
-    Q4 {Q4}
+  'roadmap.eta.q1.long': 'Esimene kvartal',
+  'roadmap.eta.q2.long': 'Teine kvartal',
+  'roadmap.eta.q3.long': 'Kolmas kvartal',
+  'roadmap.eta.q4.long': 'Neljas kvartal',
+  'roadmap.backlog': 'Tagalug',
+  'roadmap.history': 'Arenduse ajalugu',
+  'roadmap.item.inBacklog': 'Plaanime selle funktsiooni tulevikus arendada.',
+  'roadmap.item.inProgress': 'Funktsiooni arendus on alanud {startedAt, date}.',
+  'roadmap.item.inBeta': 'See funktsioon on beetaversioonis alates {betaAt, date}.',
+  'roadmap.item.inPreview': 'See funktsioon on saadaval Storytelleri tellijatele alates {previewAt, date}.',
+  'roadmap.item.inGa': 'See funktsioon on üldiselt saadaval alates {gaAt, date}.',
+  'roadmap.item.originalIssue': 'See funktsioon oli algselt kasutajate poolt ettepanekuna esitatud.',
+  'roadmap.eta.mapTitle': `Plaanitud {quarter, select,
+    Q1 {I kvartal}
+    Q2 {II kvartal}
+    Q3 {III kvartal}
+    Q4 {IV kvartal}
     other {}
   } {year, date, ::yyyy}`,
-  'roadmap.eta.mapTitleFinished': 'Finished in {time, date, ::MMMM ::yyyy}',
-  'roadmap.back': 'Back to roadmap',
-  'roadmap.seeBacklog': 'Check out what other things we plan to do',
-  'roadmap.seeFinished': 'If you like to dig more into our history, we have a nice overview ready for you',
-  'roadmap.planned': 'Planned',
-  'roadmap.finished': 'Finished'
+  'roadmap.eta.mapTitleFinished': 'Lõpetatud {time, date, ::MMMM ::yyyy}',
+  'roadmap.back': 'Tagasi teekonnakaardile',
+  'roadmap.seeBacklog': 'Vaata, mida me veel plaanime teha',
+  'roadmap.seeFinished': 'Kui soovid meie ajalooga rohkem tutvuda, siis meil on sulle valmis ülevaade',
+  'roadmap.planned': 'Plaanitud',
+  'roadmap.finished': 'Lõpetatud'
 };
