e4782585a0bb6062270e765090a0a889fed86f36
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:20:08 +0100

    New translations feed.js (Dutch)

--- nl-NL/feed.js
+++ nl-NL/feed.js
@@ -1,5 +1,5 @@
 export default {
-    'feed.name': 'Feed',
+    'feed.name': 'Nieuwsfeed',
     'feed.post.new': 'Nieuwe post',
     'feed.post.like': 'Post leuk vinden',
     'feed.post.unlike': 'Post niet leuk vinden',
