0fd035ce2de70eb71a39f8fccc252bdf81a16a04
Jan Dvorak <hello@jandvorak.me>
2024-01-12 11:25:55 +0100

    New translations user.js (Vietnamese)

--- vi-VN/user.js
+++ vi-VN/user.js
@@ -1,26 +1,26 @@
 export default {
-    'user.friend.request.add': 'Add to friends',
+    'user.friend.request.add': 'Thêm vào bạn bè',
     'user.friend.requests': `You have {num, number} new friend {num, plural,
     zero {requests}
     one {request}
     many {requests}
     other {requests}
   }`,
-    'user.friend.unfriend': 'Unfriend',
-    'user.block': 'Block',
-    'user.unblock': 'Unblock',
+    'user.friend.unfriend': 'Hủy kết bạn',
+    'user.block': 'Chặn',
+    'user.unblock': 'Bỏ chặn',
     'user.joined': `{gender, select,
-    male {Joined}
-    female {Joined}
-    other {Joined}} on {date, date, long}`,
-    'user.friend.request.accept': 'Accept friendship request',
-    'user.friend.request.cancel': 'Cancel friendship request',
-    'user.friend.request.deny': 'Deny friendship',
-    'user.listing': 'User listing',
-    'user.profile.visit': 'Visit {user}\'s profile.',
-    'user.profile.avatar': '{username}\'s avatar',
-    'user.friend.list.requests': 'Friendships requests',
-    'user.friend.norequests': 'You have no requests for friendship.',
+    male {đã tham gia}
+    female {đã tham gia}
+    other {đã tham gia}} vào ngày {date, date, long}`,
+    'user.friend.request.accept': 'Chấp nhận yêu cầu kết bạn',
+    'user.friend.request.cancel': 'Hủy yêu cầu kết bạn',
+    'user.friend.request.deny': 'Từ chối kết bạn',
+    'user.listing': 'Danh sách người dùng',
+    'user.profile.visit': 'Xem trang cá nhân của {user}.',
+    'user.profile.avatar': 'Ảnh đại diện của  {username}',
+    'user.friend.list.requests': 'Yêu cầu kết bạn',
+    'user.friend.norequests': 'Bạn không có yêu cầu kết bạn nào.',
     'user.dashboardWelcome': `Good {dayPart, select,
     morning {morning}
     afternoon {afternoon}
@@ -28,10 +28,10 @@ export default {
     night {night}
     other {day}
   } {displayName}!`,
-    'user.verification.publishedAuthor': 'This author has been published the traditional way',
-    'user.verification.luAuthor': 'Literary Universe based author of note',
-    'user.verification.luEmployee': 'Literary Universe employee',
-    'user.verification.publicFigure': 'Verified public figure',
+    'user.verification.publishedAuthor': 'Tác giả này đã được xuất bản theo cách truyền thống',
+    'user.verification.luAuthor': 'Tác giả dựa trên Vũ trụ Văn học có tầm quan trọng',
+    'user.verification.luEmployee': 'Nhân viên Vũ trụ Văn học',
+    'user.verification.publicFigure': 'Nhân vật công khai đã được xác minh',
     'profile.storiesCount': `{count, number} {count, plural,
     one {story}
     other {stories}
@@ -44,10 +44,10 @@ export default {
     one {fan fiction}
     other {fan fictions} 
   }`,
-    'profile.works': 'Works',
+    'profile.works': 'Tác phẩm',
     // As in creative/literary works
-    'profile.wishlist': 'Wishlist',
-    'profile.storiesBy': 'Stories created by {username}',
-    'profile.universesBy': 'Universes created by {username}',
-    'profile.fanFictionBy': 'Fan Fiction created by {username}'
+    'profile.wishlist': 'Danh sách mong muốn',
+    'profile.storiesBy': 'Các câu chuyện được tạo bởi {username}',
+    'profile.universesBy': 'Các vũ trụ được tạo bởi {username}',
+    'profile.fanFictionBy': 'Fan Fiction được tạo bởi {username}'
 };
