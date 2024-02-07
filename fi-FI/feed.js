0ab993ce654a7486a8ba0d36de24db8add038bc7
Jan Dvorak <hello@jandvorak.me>
2024-02-07 16:57:45 +0100

    New translations feed.js (Finnish)

--- fi-FI/feed.js
+++ fi-FI/feed.js
@@ -4,5 +4,6 @@ export default {
     'feed.post.like': 'Tykkää viestistä',
     'feed.post.unlike': 'Toisin kuin viesti',
     'feed.post.send': 'Lähetä',
-    'feed.empty': 'Syötteessä ei ole merkintöjä.'
+    'feed.empty': 'Syötteessä ei ole merkintöjä.',
+    'feed.placeholder': 'Mitä mielessäsi liikkuu?'
 };
