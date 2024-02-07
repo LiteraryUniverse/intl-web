0f846a5e75327924cfabfc47514edc7aad20004d
Jan Dvorak <hello@jandvorak.me>
2024-02-07 17:07:13 +0100

    New translations feed.js (Khmer)

--- km-KH/feed.js
+++ km-KH/feed.js
@@ -4,5 +4,6 @@ export default {
     'feed.post.like': 'ចូល​ចិត្ត​ការ​ប្រកាស',
     'feed.post.unlike': 'មិន​ចូល​ចិត្ត​ការ​ប្រកាស',
     'feed.post.send': 'ប្រកាស',
-    'feed.empty': 'គ្មាន​ធាតុ​ក្នុង​ប្រកាស។'
+    'feed.empty': 'គ្មាន​ធាតុ​ក្នុង​ប្រកាស។',
+    'feed.placeholder': 'តើអ្នកមានអ្វីដែលអ្នកចង់បង្ហាញឬទេ?'
 };
