b4fabc31147f51a20c5aa034a5e1523ba3116887
Jan Dvorak <hello@jandvorak.me>
2024-02-07 16:57:00 +0100

    New translations feed.js (German)

--- de-DE/feed.js
+++ de-DE/feed.js
@@ -4,5 +4,6 @@ export default {
     'feed.post.like': 'Beitrag gefällt',
     'feed.post.unlike': 'Im Gegensatz zu Post',
     'feed.post.send': 'Beitrag',
-    'feed.empty': 'Keine Einträge im Feed.'
+    'feed.empty': 'Keine Einträge im Feed.',
+    'feed.placeholder': 'Was denkst du?'
 };
