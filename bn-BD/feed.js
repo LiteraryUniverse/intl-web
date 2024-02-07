be1b2a9ec4d7abf9390c220b25bd21bc19f31c20
Jan Dvorak <hello@jandvorak.me>
2024-02-07 17:07:59 +0100

    New translations feed.js (Bengali)

--- bn-BD/feed.js
+++ bn-BD/feed.js
@@ -4,5 +4,6 @@ export default {
     'feed.post.like': 'পোস্টটি পছন্দ করুন',
     'feed.post.unlike': 'পোস্টটি পছন্দ করা বাতিল করুন',
     'feed.post.send': 'পোস্ট',
-    'feed.empty': 'ফিডে কোন প্রবেশ নেই।'
+    'feed.empty': 'ফিডে কোন প্রবেশ নেই।',
+    'feed.placeholder': 'আপনার মনে কি আছে?'
 };
