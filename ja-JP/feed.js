5f7712c4f4b158abfc5e2952a74a080e7a24a589
Jan Dvorak <hello@jandvorak.me>
2024-02-07 16:59:53 +0100

    New translations feed.js (Japanese)

--- ja-JP/feed.js
+++ ja-JP/feed.js
@@ -4,5 +4,6 @@ export default {
     'feed.post.like': 'いいね投稿',
     'feed.post.unlike': '投稿とは異なり',
     'feed.post.send': '投稿',
-    'feed.empty': 'フィードにエントリがありません。'
+    'feed.empty': 'フィードにエントリがありません。',
+    'feed.placeholder': '何を考えていますか？'
 };
