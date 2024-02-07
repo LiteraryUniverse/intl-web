f68da08a7a131e0d7ad3e317c74ee73ecbc5b78d
Jan Dvorak <hello@jandvorak.me>
2024-02-07 17:09:26 +0100

    New translations feed.js (Hindi)

--- hi-IN/feed.js
+++ hi-IN/feed.js
@@ -4,5 +4,6 @@ export default {
     'feed.post.like': 'पोस्ट पसंद करें',
     'feed.post.unlike': 'पोस्ट को पसंद नहीं करें',
     'feed.post.send': 'पोस्ट',
-    'feed.empty': 'फ़ीड में कोई एंट्री नहीं है।'
+    'feed.empty': 'फ़ीड में कोई एंट्री नहीं है।',
+    'feed.placeholder': 'तुम्हारे मन में क्या है?'
 };
