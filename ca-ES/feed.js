8cdd1b025a14ae4a6c5ee4fecf94fc7793a790c9
Jan Dvorak <hello@jandvorak.me>
2024-02-07 16:55:53 +0100

    New translations feed.js (Catalan)

--- ca-ES/feed.js
+++ ca-ES/feed.js
@@ -4,5 +4,6 @@ export default {
     'feed.post.like': 'M\'agrada la publicació',
     'feed.post.unlike': 'No m\'agrada la publicació',
     'feed.post.send': 'Publicar',
-    'feed.empty': 'No hi ha entrades al feed.'
+    'feed.empty': 'No hi ha entrades al feed.',
+    'feed.placeholder': 'Què tens al cap?'
 };
