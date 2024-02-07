2133f2aa1dbe5270eb50f8766723d26c578a7af1
Jan Dvorak <hello@jandvorak.me>
2024-02-07 17:13:55 +0100

    New translations feed.js (Nepali)

--- ne-NP/feed.js
+++ ne-NP/feed.js
@@ -4,5 +4,6 @@ export default {
     'feed.post.like': 'पोस्टलाई मन पर्यो',
     'feed.post.unlike': 'पोस्टलाई मन परेन',
     'feed.post.send': 'पोस्ट',
-    'feed.empty': 'फिडमा कुनै प्रविष्टिहरू छैनन्।'
+    'feed.empty': 'फिडमा कुनै प्रविष्टिहरू छैनन्।',
+    'feed.placeholder': 'तपाईंको मनमा के छ?'
 };
