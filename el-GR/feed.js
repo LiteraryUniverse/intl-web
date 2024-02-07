2bf8e0c9861fd852260354a438a7efb38b3711a5
Jan Dvorak <hello@jandvorak.me>
2024-02-07 16:57:22 +0100

    New translations feed.js (Greek)

--- el-GR/feed.js
+++ el-GR/feed.js
@@ -4,5 +4,6 @@ export default {
     'feed.post.like': 'Όπως η θέση',
     'feed.post.unlike': 'Σε αντίθεση με τη θέση',
     'feed.post.send': 'Θέση',
-    'feed.empty': 'Δεν υπάρχουν καταχωρήσεις στην τροφοδοσία.'
+    'feed.empty': 'Δεν υπάρχουν καταχωρήσεις στην τροφοδοσία.',
+    'feed.placeholder': 'Τι σκέφτεσαι;'
 };
