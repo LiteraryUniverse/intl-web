7d663d92321f85480bfd29261b0350e53da94722
Jan Dvorak <hello@jandvorak.me>
2024-02-07 17:13:07 +0100

    New translations feed.js (Maithili)

--- mai-IN/feed.js
+++ mai-IN/feed.js
@@ -4,5 +4,6 @@ export default {
     'feed.post.like': 'पोस्टमाथि पसंद गर्नुहोस्',
     'feed.post.unlike': 'पोस्टमाथि पसंद गर्न छोड्नुहोस्',
     'feed.post.send': 'पोस्ट',
-    'feed.empty': 'फिडमा कुनै प्रविष्टिहरू छैनन्।'
+    'feed.empty': 'फिडमा कुनै प्रविष्टिहरू छैनन्।',
+    'feed.placeholder': 'तुम्हारे मन में के छ छः?'
 };
