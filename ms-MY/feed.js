d8be0f735204fed5e0761ff1bcddb8d6e329b839
Jan Dvorak <hello@jandvorak.me>
2024-02-07 17:09:47 +0100

    New translations feed.js (Malay)

--- ms-MY/feed.js
+++ ms-MY/feed.js
@@ -4,5 +4,6 @@ export default {
     'feed.post.like': 'Suka entri',
     'feed.post.unlike': 'Batal suka entri',
     'feed.post.send': 'Hantar',
-    'feed.empty': 'Tiada entri dalam feed.'
+    'feed.empty': 'Tiada entri dalam feed.',
+    'feed.placeholder': 'Apa yang ada dalam fikiran anda?'
 };
