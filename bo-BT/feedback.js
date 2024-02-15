3755d473496127d060050a45528e10cc8e0cee76
Jan Dvorak <hello@jandvorak.me>
2024-02-15 14:54:51 +0100

    New translations feedback.js (Tibetan)

--- bo-BT/feedback.js
+++ bo-BT/feedback.js
@@ -7,6 +7,8 @@ export default {
     'feedback.help': 'Help & Tutorials',
     'feedback.help.desc': 'How do you _____? Go here to find the answers.',
     'feedback.desc': 'Welcome to our help and resolution center!',
+    'feedback.faq': 'FAQ',
+    'feedback.faq.desc': 'Frequently asked questions',
     'feedback.open': 'Open',
     'feedback.closed': 'Closed',
     'feedback.accepted': 'Accepted',
@@ -50,5 +52,15 @@ export default {
     universe {universe}
     story {story}
     other {story}
-  } is being infringed?`
+  } is being infringed?`,
+    'faq.section.about': 'About us',
+    'faq.section.settings': 'སྒྲིག་འཇུག',
+    'faq.section.discovery': 'Discovery',
+    'faq.section.reader': 'Reader',
+    'faq.section.library': 'ཡིག་ཆ་འདི།',
+    'faq.section.workshop': 'སྦྱར་ཚིག་འདི།',
+    'faq.section.writer': 'Writer',
+    'faq.section.encyclopedia': 'Encyclopedia',
+    'faq.section.groups': 'Groups',
+    'faq.section.blogs': 'Blogs'
 };
