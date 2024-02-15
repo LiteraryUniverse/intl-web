9ce25cda7d2d42e960ae044432eb99379f74e80f
Jan Dvorak <hello@jandvorak.me>
2024-02-15 14:53:13 +0100

    New translations feedback.js (Telugu)

--- te-IN/feedback.js
+++ te-IN/feedback.js
@@ -7,6 +7,8 @@ export default {
     'feedback.help': 'Help & Tutorials',
     'feedback.help.desc': 'How do you _____? Go here to find the answers.',
     'feedback.desc': 'Welcome to our help and resolution center!',
+    'feedback.faq': 'FAQ',
+    'feedback.faq.desc': 'Frequently asked questions',
     'feedback.open': 'Open',
     'feedback.closed': 'Closed',
     'feedback.accepted': 'Accepted',
@@ -50,5 +52,15 @@ export default {
     universe {universe}
     story {story}
     other {story}
-  } is being infringed?`
+  } is being infringed?`,
+    'faq.section.about': 'About us',
+    'faq.section.settings': 'అమరికలు',
+    'faq.section.discovery': 'Discovery',
+    'faq.section.reader': 'Reader',
+    'faq.section.library': 'లైబ్రరీ',
+    'faq.section.workshop': 'కార్యశాల',
+    'faq.section.writer': 'Writer',
+    'faq.section.encyclopedia': 'Encyclopedia',
+    'faq.section.groups': 'Groups',
+    'faq.section.blogs': 'Blogs'
 };
