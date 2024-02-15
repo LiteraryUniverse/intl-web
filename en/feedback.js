34c219db4c73e19844a338a8edceb8c3e25a6a6b
Jan Dvorak <jdvorak@literaryuniverse.com>
2024-02-15 14:31:50 +0100

    FAQ section names

--- en/feedback.js
+++ en/feedback.js
@@ -9,6 +9,8 @@ export default {
   'feedback.help': 'Help & Tutorials',
   'feedback.help.desc': 'How do you _____? Go here to find the answers.',
   'feedback.desc': 'Welcome to our help and resolution center!',
+  'feedback.faq': 'FAQ',
+  'feedback.faq.desc': 'Frequently asked questions',
   'feedback.open': 'Open',
   'feedback.closed': 'Closed',
   'feedback.accepted': 'Accepted',
@@ -59,5 +61,15 @@ export default {
     universe {universe}
     story {story}
     other {story}
-  } is being infringed?`
+  } is being infringed?`,
+  'faq.section.about': 'About us',
+  'faq.section.settings': 'Settings',
+  'faq.section.discovery': 'Discovery',
+  'faq.section.reader': 'Reader',
+  'faq.section.library': 'Library',
+  'faq.section.workshop': 'Workshop',
+  'faq.section.writer': 'Writer',
+  'faq.section.encyclopedia': 'Encyclopedia',
+  'faq.section.groups': 'Groups',
+  'faq.section.blogs': 'Blogs'
 }
