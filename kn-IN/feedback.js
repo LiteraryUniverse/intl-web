082ecdd2987964e6a50ee8f1c33a7315b362dd2a
Jan Dvorak <hello@jandvorak.me>
2024-02-15 14:55:12 +0100

    New translations feedback.js (Kannada)

--- kn-IN/feedback.js
+++ kn-IN/feedback.js
@@ -7,6 +7,8 @@ export default {
     'feedback.help': 'Help & Tutorials',
     'feedback.help.desc': 'How do you _____? Go here to find the answers.',
     'feedback.desc': 'Welcome to our help and resolution center!',
+    'feedback.faq': 'FAQ',
+    'feedback.faq.desc': 'Frequently asked questions',
     'feedback.open': 'Open',
     'feedback.closed': 'Closed',
     'feedback.accepted': 'Accepted',
@@ -50,5 +52,15 @@ export default {
     universe {universe}
     story {story}
     other {story}
-  } is being infringed?`
+  } is being infringed?`,
+    'faq.section.about': 'About us',
+    'faq.section.settings': 'ಸೆಟ್ಟಿಂಗ್ಗಳು',
+    'faq.section.discovery': 'Discovery',
+    'faq.section.reader': 'Reader',
+    'faq.section.library': 'ಗ್ರಂಥಾಲಯ',
+    'faq.section.workshop': 'ಕಾರ್ಯಾಗಾರ',
+    'faq.section.writer': 'Writer',
+    'faq.section.encyclopedia': 'Encyclopedia',
+    'faq.section.groups': 'Groups',
+    'faq.section.blogs': 'Blogs'
 };
