62fa08a199f899f1423c9f9f536660a6f7f8de05
Jan Dvorak <hello@jandvorak.me>
2024-02-15 16:26:33 +0100

    New translations feedback.js (Catalan)

--- ca-ES/feedback.js
+++ ca-ES/feedback.js
@@ -7,8 +7,8 @@ export default {
     'feedback.help': 'Ajuda i tutorials',
     'feedback.help.desc': 'Com ho fas per a _____? Vés aquí per trobar les respostes.',
     'feedback.desc': 'Benvingut al nostre centre d\'ajuda i resolució!',
-    'feedback.faq': 'FAQ',
-    'feedback.faq.desc': 'Frequently asked questions',
+    'feedback.faq': 'PMF',
+    'feedback.faq.desc': 'Preguntes freqüents',
     'feedback.open': 'Obert',
     'feedback.closed': 'Tancat',
     'feedback.accepted': 'Acceptat',
@@ -53,14 +53,30 @@ export default {
     story {història}
     other {història}
   } s'està infringint?`,
-    'faq.section.about': 'About us',
+    'faq.section.about': 'Sobre nosaltres',
     'faq.section.settings': 'Configuració',
-    'faq.section.discovery': 'Discovery',
-    'faq.section.reader': 'Reader',
+    'faq.section.discovery': 'Descoberta',
+    'faq.section.reader': 'Lector',
     'faq.section.library': 'Biblioteca',
     'faq.section.workshop': 'Taller',
-    'faq.section.writer': 'Writer',
-    'faq.section.encyclopedia': 'Encyclopedia',
-    'faq.section.groups': 'Groups',
-    'faq.section.blogs': 'Blogs'
+    'faq.section.writer': 'Escriptor',
+    'faq.section.encyclopedia': 'Enciclopèdia',
+    'faq.section.groups': 'Grups',
+    'faq.section.blogs': 'Blogs',
+    // About us questions
+    'faq.question.about.dataLocation': 'On es guarda la informació de LU?',
+    'faq.answer.about.dataLocation': 'La informació de Literary Universe es guarda en servidors als Estats Units d\'Amèrica, concretament a la regió AWS us-east-1.',
+    'faq.question.about.incorporation': 'On esteu constituïts?',
+    'faq.answer.about.incorporation': 'Literary Universe is incorporated in the Czech Republic. You can find more details on the <a href="/about">about us page</a>.',
+    'faq.question.about.roadmap': 'What are the next steps for Literary Universe?',
+    'faq.answer.about.roadmap': 'This is constantly evolving. Check out our <a href="/roadmap">roadmap</a> for more details.',
+    // Settings questions
+    'faq.question.settings.changeTheme': 'How do I change the look of Literary Universe?',
+    'faq.answer.settings.changeTheme': 'Registered users can change their theme in the <a href="/user/settings/account">account settings</a> under "site theming" section. We have a light and a dark theme available. If you are not registered, then we try to determine which theme to serve you based on your browser and operating system settings.',
+    'faq.question.settings.changeAvatar': 'How do I change my avatar?',
+    'faq.answer.settings.changeAvatar': 'You can change your avatar in the <a href="/user/settings/profile">profile settings</a>.',
+    'faq.question.settings.changeLanguage': 'How do I change the language?',
+    'faq.answer.settings.changeLanguage': 'You can change the language of Literary Universe in the <a href="/user/settings/account">account settings</a>.',
+    'faq.question.settings.translationMistake': 'I found a mistake in the text on the website, where can I report it?',
+    'faq.answer.settings.translationMistake': 'All our language files are available on GitHub. You can <a target="_blank" href="https://github.com/LiteraryUniverse/intl-web/issues/new/choose">submit an issue there</a> or in the Bug section of the feedback, or if you are so inclined, you can fix it directly yourself via <a href="https://crowdin.com/project/literary-universe" target="_blank">our Crowdin page</a>.'
 };
