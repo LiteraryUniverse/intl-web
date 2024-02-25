906f83f8bacfaa893bc60d0c18fb91d5645a9530
Jan Dvorak <hello@jandvorak.me>
2024-02-25 23:41:32 +0100

    New translations plans.js (Chinese Traditional)

--- zh-TW/plans.js
+++ zh-TW/plans.js
@@ -91,11 +91,11 @@ export default {
     'plans.subscribed.paymentMethod': '您的預設付款方式將用於支付下一個週期的費用。',
     'plans.subscribed.status': `訂閱狀態： {status, select,
     active {啟用}
-    incomplete {有一些遺失}
-    incomplete_expired {有一些遺失}
+    incomplete {有一些遺漏}
+    incomplete_expired {有一些遺漏}
     trialing {試用}
-    past_due {我們缺少您的付款}
-    canceled {已取消}
+    past_due {我們遺漏了您的付款}
+    cancelled {已取消}
     unpaid {未付款}
     expired {已過期}
     other {}
