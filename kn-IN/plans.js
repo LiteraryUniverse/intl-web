cf4ec1814582e289dabd72406d7a0e3d69009524
Jan Dvorak <hello@jandvorak.me>
2024-02-25 16:01:16 +0100

    New translations plans.js (Kannada)

--- kn-IN/plans.js
+++ kn-IN/plans.js
@@ -214,5 +214,6 @@ export default {
     'payment.universe.directivesEncFull': 'Fill up your encyclopedia, have at least 30 entries! Better if you are attacking the 100 margin.',
     'payment.universe.haveFans': 'ನೀವು ಮಾಡುವ ಕೆಲಸಕ್ಕೆ ಮೊದಲು ಆಸಕ್ತರಾಗಿರುವ ಜನರು ನಿಮಗೆ ಸ್ಪಾನ್ಸರ್ ಆಗುವುದು ಸುಲಭ. 100 ರಸಾಯನಗಳನ್ನು ಅವರ ಲೈಬ್ರರಿಗೆ ಸೇರಿಸುವುದು ಪ್ರಾರಂಭ ರೇಖೆ.',
     'payment.settings.yourFanClub': 'ನಿಮ್ಮ ಅನುಯಾಯ ಕೌಟುಂಬಿಕ',
-    'payment.settings.newWork': 'ಹೊಸ ಕೆಲಸ ರಚಿಸಿ'
+    'payment.settings.newWork': 'ಹೊಸ ಕೆಲಸ ರಚಿಸಿ',
+    'payment.amount.withNet': `{amount} (net: {netAmount})`
 };
