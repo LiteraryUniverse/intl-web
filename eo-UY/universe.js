a7f7975f2616e233385086553124f87c207a67fb
Jan Dvorak <hello@jandvorak.me>
2024-02-27 16:39:53 +0100

    New translations universe.js (Esperanto)

--- eo-UY/universe.js
+++ eo-UY/universe.js
@@ -33,5 +33,6 @@ export default {
     'universe.fanFiction.hidden': 'Kaŝita',
     'universe.fanFiction.hide': 'Kaŝi fanaĵon',
     'universe.fanFiction.unhide': 'Malapereigi fanaĵon',
-    'universe.fanFiction.create': 'Krei novan fanfikcion'
+    'universe.fanFiction.create': 'Krei novan fanfikcion',
+    'universe.fanFiction.notAllowed': 'Ĉi tiu Universo ne permesas montri fanfikcion.'
 };
