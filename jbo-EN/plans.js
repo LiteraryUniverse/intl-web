433701308c2195fa5c0817db4f583f312752b2d3
Jan Dvorak <hello@jandvorak.me>
2024-02-28 19:47:31 +0100

    New translations plans.js (Lojban)

--- jbo-EN/plans.js
+++ jbo-EN/plans.js
@@ -217,5 +217,8 @@ export default {
     'payment.settings.newWork': 'lo nu zbasu lo nu djuno',
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
-    'payment.fanClub.tiers': 'Tiers'
+    'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.createTier': 'Jmina tierni',
+    'payment.fanClub.description': 'Mildi tierni',
+    'payment.fanClub.picture': 'Cfipu tierni'
 };
