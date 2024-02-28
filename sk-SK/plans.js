5feeb22a7fe0182f0918c698551e58c6c2b66b47
Jan Dvorak <hello@jandvorak.me>
2024-02-28 19:40:34 +0100

    New translations plans.js (Slovak)

--- sk-SK/plans.js
+++ sk-SK/plans.js
@@ -217,5 +217,8 @@ export default {
     'payment.settings.newWork': 'Vytvoriť nové dielo',
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
-    'payment.fanClub.tiers': '[object Promise]'
+    'payment.fanClub.tiers': '[object Promise]',
+    'payment.fanClub.createTier': 'Pridať úroveň',
+    'payment.fanClub.description': 'Popis úrovne',
+    'payment.fanClub.picture': 'Obrázok úrovne'
 };
