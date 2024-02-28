fb7b08cb15ef6b2505349211fd43b6eb8b021404
Jan Dvorak <hello@jandvorak.me>
2024-02-28 19:36:27 +0100

    New translations plans.js (Italian)

--- it-IT/plans.js
+++ it-IT/plans.js
@@ -217,5 +217,8 @@ export default {
     'payment.settings.newWork': 'Crea una nuova opera',
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
-    'payment.fanClub.tiers': 'Tiers'
+    'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.createTier': 'Aggiungi livello',
+    'payment.fanClub.description': 'Descrizione livello',
+    'payment.fanClub.picture': 'Immagine livello'
 };
