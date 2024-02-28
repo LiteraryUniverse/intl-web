07ca8320119e77a4192236d846594e22a7eff5f0
Jan Dvorak <hello@jandvorak.me>
2024-02-28 19:43:09 +0100

    New translations plans.js (Indonesian)

--- id-ID/plans.js
+++ id-ID/plans.js
@@ -217,5 +217,8 @@ export default {
     'payment.settings.newWork': 'Buat karya baru',
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
-    'payment.fanClub.tiers': 'Tiers'
+    'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.createTier': 'Tambahkan tier',
+    'payment.fanClub.description': 'Deskripsi tier',
+    'payment.fanClub.picture': 'Gambar tier'
 };
