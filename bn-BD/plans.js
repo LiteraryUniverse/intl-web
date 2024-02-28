c4d845d3556d41a8d6c6ce4cbcf2ead94235cad9
Jan Dvorak <hello@jandvorak.me>
2024-02-28 19:44:13 +0100

    New translations plans.js (Bengali)

--- bn-BD/plans.js
+++ bn-BD/plans.js
@@ -217,5 +217,8 @@ export default {
     'payment.settings.newWork': 'নতুন কাজ তৈরি করো',
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
-    'payment.fanClub.tiers': 'Tiers'
+    'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.createTier': 'টিয়ার যোগ করুন',
+    'payment.fanClub.description': 'টিয়ার বিবরণ',
+    'payment.fanClub.picture': 'টিয়ার ছবি'
 };
