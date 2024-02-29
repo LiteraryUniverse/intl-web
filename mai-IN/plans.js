5d4245c4ad940d5786f8054c3fc6478fec45f48b
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:16:59 +0100

    New translations plans.js (Maithili)

--- mai-IN/plans.js
+++ mai-IN/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.tierName': 'टियर नाम',
     'payment.fanClub.createTier': 'टियर थप्नुहोस्',
     'payment.fanClub.description': 'टियर वर्णन',
     'payment.fanClub.picture': 'टियर तस्वीर'
