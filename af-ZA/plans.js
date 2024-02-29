4d8a658692985b5b8f53ed3a80ea8f7de00c3a34
Jan Dvorak <hello@jandvorak.me>
2024-02-29 14:57:58 +0100

    New translations plans.js (Afrikaans)

--- af-ZA/plans.js
+++ af-ZA/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.tierName': 'Tiersnaam',
     'payment.fanClub.createTier': 'Voeg vlak by',
     'payment.fanClub.description': 'Vlak beskrywing',
     'payment.fanClub.picture': 'Vlak prentjie'
