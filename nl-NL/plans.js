bdda6c645197ea398acfffc409db9fea688a0882
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:04:45 +0100

    New translations plans.js (Dutch)

--- nl-NL/plans.js
+++ nl-NL/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.tierName': 'Naam van het niveau',
     'payment.fanClub.createTier': 'Voeg niveau toe',
     'payment.fanClub.description': 'Niveaubeschrijving',
     'payment.fanClub.picture': 'Niveau afbeelding'
