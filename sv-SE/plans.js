f245dbb77146515eae15945d4c0f3146b8fef280
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:07:42 +0100

    New translations plans.js (Swedish)

--- sv-SE/plans.js
+++ sv-SE/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.tierName': 'Nivån',
     'payment.fanClub.createTier': 'Lägg till nivå',
     'payment.fanClub.description': 'Nivåbeskrivning',
     'payment.fanClub.picture': 'Nivåbild'
