f3f68106f238a183c33daca9d3d46b0044edf1e3
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:08:07 +0100

    New translations plans.js (Turkish)

--- tr-TR/plans.js
+++ tr-TR/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.tierName': 'Seviye adı',
     'payment.fanClub.createTier': 'Katman ekle',
     'payment.fanClub.description': 'Katman açıklaması',
     'payment.fanClub.picture': 'Katman resmi'
