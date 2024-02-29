c103fa40ab190ec2347b192b6491934fb50efb1c
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:16:11 +0100

    New translations plans.js (Kannada)

--- kn-IN/plans.js
+++ kn-IN/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.tierName': 'ಟಿಯರ್ ಹೆಸರು',
     'payment.fanClub.createTier': 'ಟಿಯರ್ ಸೇರಿಸಿ',
     'payment.fanClub.description': 'ಟಿಯರ್ ವಿವರಣೆ',
     'payment.fanClub.picture': 'ಟಿಯರ್ ಚಿತ್ರ'
