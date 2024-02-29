b2330759734860b99a59769b2d7513974d1e90b7
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:12:26 +0100

    New translations plans.js (Estonian)

--- et-EE/plans.js
+++ et-EE/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.tierName': 'Taseme nimi',
     'payment.fanClub.createTier': 'Lisa tase',
     'payment.fanClub.description': 'Taseme kirjeldus',
     'payment.fanClub.picture': 'Taseme pilt'
