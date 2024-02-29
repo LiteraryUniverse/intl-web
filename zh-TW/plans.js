32cdb30455d517cf1ee6420ef914c62ff3b1fa5b
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:08:54 +0100

    New translations plans.js (Chinese Traditional)

--- zh-TW/plans.js
+++ zh-TW/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} （淨額： {netAmount}）`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.tierName': '層級名稱',
     'payment.fanClub.createTier': '新增層級',
     'payment.fanClub.description': '層級描述',
     'payment.fanClub.picture': '層級圖片'
