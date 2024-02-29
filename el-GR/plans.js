3e8261ebde1cbc2eecc780832ac72c92cedffbc3
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:00:14 +0100

    New translations plans.js (Greek)

--- el-GR/plans.js
+++ el-GR/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.tierName': 'Όνομα επιπέδου',
     'payment.fanClub.createTier': 'Προσθήκη επιπέδου',
     'payment.fanClub.description': 'Περιγραφή επιπέδου',
     'payment.fanClub.picture': 'Εικόνα επιπέδου'
