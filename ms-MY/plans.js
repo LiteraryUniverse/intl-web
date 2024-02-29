4a4fa3af93e4b43d4be1788caa7ec1412e4fd22b
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:13:13 +0100

    New translations plans.js (Malay)

--- ms-MY/plans.js
+++ ms-MY/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': '[object Promise]',
+    'payment.fanClub.tierName': 'Nama tier',
     'payment.fanClub.createTier': 'Tambah tier',
     'payment.fanClub.description': 'Penerangan tier',
     'payment.fanClub.picture': 'Gambar tier'
