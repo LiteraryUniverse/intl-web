7ad8c74fed06ab331a26d9967e25214634ad49eb
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:15:45 +0100

    New translations plans.js (Tibetan)

--- bo-BT/plans.js
+++ bo-BT/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.tierName': 'སྡེ་ཚན་གྱི་མཚན།',
     'payment.fanClub.createTier': 'སྒྲོམ་པ་བཟོ་བ།',
     'payment.fanClub.description': 'སྒྲོམ་ཚད་འགྲེལ།',
     'payment.fanClub.picture': 'སྒྲོམ་པར་རིན།'
