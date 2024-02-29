bf0b7e9ebdf8e08cb95ec01c7b80b1dbbfe52113
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:14:00 +0100

    New translations plans.js (Esperanto)

--- eo-UY/plans.js
+++ eo-UY/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.tierName': 'Nomo de nivelo',
     'payment.fanClub.createTier': 'Aldoni nivelon',
     'payment.fanClub.description': 'Priskribo de nivelo',
     'payment.fanClub.picture': 'Bildo de nivelo'
