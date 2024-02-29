3de455d2c66b3fab1e32e542908f475bd47621bd
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:04:20 +0100

    New translations plans.js (Lithuanian)

--- lt-LT/plans.js
+++ lt-LT/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.tierName': 'Lygio pavadinimas',
     'payment.fanClub.createTier': 'Pridėti lygį',
     'payment.fanClub.description': 'Lygio aprašymas',
     'payment.fanClub.picture': 'Lygio nuotrauka'
