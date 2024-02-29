87f3ccab8cfc2a6a15cc3484d357ea280a7cb1c7
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:01:53 +0100

    New translations plans.js (Hebrew)

--- he-IL/plans.js
+++ he-IL/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.tierName': 'שם השכבה',
     'payment.fanClub.createTier': 'הוסף שכבה',
     'payment.fanClub.description': 'תיאור השכבה',
     'payment.fanClub.picture': 'תמונת השכבה'
