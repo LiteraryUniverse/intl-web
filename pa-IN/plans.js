922e81bcf99c6a710ee5a22c6b4a110437e3c140
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:05:36 +0100

    New translations plans.js (Punjabi)

--- pa-IN/plans.js
+++ pa-IN/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (net: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': '[object Promise]',
+    'payment.fanClub.tierName': 'ਟੀਅਰ ਨਾਮ',
     'payment.fanClub.createTier': 'ਟੀਅਰ ਸ਼ਾਮਲ ਕਰੋ',
     'payment.fanClub.description': 'ਟੀਅਰ ਦਾ ਵੇਰਵਾ',
     'payment.fanClub.picture': 'ਟੀਅਰ ਦੀ ਤਸਵੀਰ'
