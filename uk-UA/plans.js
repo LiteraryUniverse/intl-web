e07bf9a1d67316e212088dd73d9fa6e506e3ba11
Jan Dvorak <hello@jandvorak.me>
2024-02-29 15:08:31 +0100

    New translations plans.js (Ukrainian)

--- uk-UA/plans.js
+++ uk-UA/plans.js
@@ -218,6 +218,7 @@ export default {
     'payment.amount.withNet': `{amount} (мережа: {netAmount})`,
     'payment.fanClub.intro': 'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
     'payment.fanClub.tiers': 'Tiers',
+    'payment.fanClub.tierName': 'Назва рівня',
     'payment.fanClub.createTier': 'Додати рівень',
     'payment.fanClub.description': 'Опис рівня',
     'payment.fanClub.picture': 'Зображення рівня'
