391bb91f7d717ba0f88496fb81318bf824b947b6
Jan Dvorak <jdvorak@literaryuniverse.com>
2024-03-01 12:49:33 +0100

    Price breakdown string

--- en/plans.js
+++ en/plans.js
@@ -257,8 +257,10 @@ export default {
   'payment.fanClub.intro':
     'In this section you can modify your fan club settings. From subscription tiers to history of payments.',
   'payment.fanClub.tiers': 'Tiers',
-  'payment.fanClub.tierName': 'Tier name'
+  'payment.fanClub.tierName': 'Tier name',
   'payment.fanClub.createTier': 'Add tier',
   'payment.fanClub.description': 'Tier description',
-  'payment.fanClub.picture': 'Tier picture'
+  'payment.fanClub.picture': 'Tier picture',
+  'payment.fanClub.priceBreakdown':
+    'Subscriber will pay: {amount}. Platform fee is {fee}, you will get: {net}'
 }
