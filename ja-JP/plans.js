314c027f88fde52164078ce051cf8416f8657cb8
Jan Dvorak <hello@jandvorak.me>
2024-03-04 00:54:46 +0100

    New translations plans.js (Japanese)

--- ja-JP/plans.js
+++ ja-JP/plans.js
@@ -221,5 +221,7 @@ export default {
     'payment.fanClub.tierName': 'ティア名',
     'payment.fanClub.createTier': 'レベルを追加する',
     'payment.fanClub.description': 'レベルの説明',
-    'payment.fanClub.picture': 'レベルの画像'
+    'payment.fanClub.picture': 'レベルの画像',
+    'payment.fanClub.priceBreakdown': '購読者は{amount}を支払います。プラットフォーム手数料は{fee}で、あなたは{net}を受け取ります。',
+    'payment.feeNotice': 'なお、これは見積もりであり、実際の手数料は、お客様の支払い方法やペイゲートからの追加料金によって異なる場合がありますので、ご了承ください。'
 };
