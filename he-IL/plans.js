538e59807fc1d7d8e396a36802f70e16aa945964
Jan Dvorak <hello@jandvorak.me>
2024-03-04 00:53:53 +0100

    New translations plans.js (Hebrew)

--- he-IL/plans.js
+++ he-IL/plans.js
@@ -221,5 +221,7 @@ export default {
     'payment.fanClub.tierName': 'שם השכבה',
     'payment.fanClub.createTier': 'הוסף שכבה',
     'payment.fanClub.description': 'תיאור השכבה',
-    'payment.fanClub.picture': 'תמונת השכבה'
+    'payment.fanClub.picture': 'תמונת השכבה',
+    'payment.fanClub.priceBreakdown': 'המנוי ישלם: {amount}. דמי הפלטפורמה הם {fee}, תקבל: {net}',
+    'payment.feeNotice': 'יש לשים לב כי זהו הערך המשוער והדמי המדויק עשוי להיות שונה בהתאם לסוג התשלום של הלקוח ולתשלומים נוספים משער התשלום.'
 };
