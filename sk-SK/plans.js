deb08e7ebe511b4db4123b68d41f0406a4fbe88a
Jan Dvorak <hello@jandvorak.me>
2024-03-07 13:09:33 +0100

    New translations plans.js (Slovak)

--- sk-SK/plans.js
+++ sk-SK/plans.js
@@ -223,5 +223,6 @@ export default {
     'payment.fanClub.description': 'Popis úrovne',
     'payment.fanClub.picture': 'Obrázok úrovne',
     'payment.fanClub.priceBreakdown': 'Odberateľ zaplatí: {amount}. Poplatok platformy je {fee}, dostanete: {net}',
-    'payment.feeNotice': 'Prosím, majte na pamäti, že ide o odhad a skutočný poplatok sa môže líšiť v závislosti od typu platby zákazníka a ďalších poplatkov od platobnej brány.'
+    'payment.feeNotice': 'Prosím, majte na pamäti, že ide o odhad a skutočný poplatok sa môže líšiť v závislosti od typu platby zákazníka a ďalších poplatkov od platobnej brány.',
+    'payment.fanClub.perks': 'Výhody'
 };
