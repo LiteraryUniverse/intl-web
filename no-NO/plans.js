5b46dea39b1dcae66ab8b6d5ad6efaf467df7422
Jan Dvorak <hello@jandvorak.me>
2024-03-07 13:07:42 +0100

    New translations plans.js (Norwegian)

--- no-NO/plans.js
+++ no-NO/plans.js
@@ -223,5 +223,6 @@ export default {
     'payment.fanClub.description': 'Nivåbeskrivelse',
     'payment.fanClub.picture': 'Nivåbilde',
     'payment.fanClub.priceBreakdown': 'Abonnenten vil betale: {amount}. Plattformavgift er {fee}, du vil få: {net}',
-    'payment.feeNotice': 'Vennligst merk at dette er et estimat, og den faktiske avgiften kan være forskjellig basert på kundens betalingstype og eventuelle ekstra gebyrer fra betalingsportalen.'
+    'payment.feeNotice': 'Vennligst merk at dette er et estimat, og den faktiske avgiften kan være forskjellig basert på kundens betalingstype og eventuelle ekstra gebyrer fra betalingsportalen.',
+    'payment.fanClub.perks': 'Fordeler'
 };
