7f7c89dc659078dc1fb34682ee6b108d49781011
Jan Dvorak <hello@jandvorak.me>
2024-03-07 13:07:21 +0100

    New translations plans.js (Dutch)

--- nl-NL/plans.js
+++ nl-NL/plans.js
@@ -223,5 +223,6 @@ export default {
     'payment.fanClub.description': 'Niveaubeschrijving',
     'payment.fanClub.picture': 'Niveau afbeelding',
     'payment.fanClub.priceBreakdown': 'De abonnee betaalt: {amount}. De platformkosten bedragen {fee}, u ontvangt: {net}',
-    'payment.feeNotice': 'Houd er rekening mee dat dit een schatting is en dat de werkelijke kosten kunnen verschillen op basis van de betaalmethode van de klant en eventuele extra kosten van het betaalplatform.'
+    'payment.feeNotice': 'Houd er rekening mee dat dit een schatting is en dat de werkelijke kosten kunnen verschillen op basis van de betaalmethode van de klant en eventuele extra kosten van het betaalplatform.',
+    'payment.fanClub.perks': 'Voordelen'
 };
