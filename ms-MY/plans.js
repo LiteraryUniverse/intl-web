2fed1e79a03dcc8ff414283af8ea423c89d5f589
Jan Dvorak <hello@jandvorak.me>
2024-03-07 13:14:48 +0100

    New translations plans.js (Malay)

--- ms-MY/plans.js
+++ ms-MY/plans.js
@@ -223,5 +223,6 @@ export default {
     'payment.fanClub.description': 'Penerangan tier',
     'payment.fanClub.picture': 'Gambar tier',
     'payment.fanClub.priceBreakdown': 'Pelanggan akan membayar: {amount}. Yuran platform adalah {fee}, anda akan mendapat: {net}',
-    'payment.feeNotice': 'Sila ambil perhatian bahawa ini hanyalah anggaran dan yuran sebenar mungkin berbeza berdasarkan jenis pembayaran pelanggan dan caj tambahan dari gerbang bayaran.'
+    'payment.feeNotice': 'Sila ambil perhatian bahawa ini hanyalah anggaran dan yuran sebenar mungkin berbeza berdasarkan jenis pembayaran pelanggan dan caj tambahan dari gerbang bayaran.',
+    'payment.fanClub.perks': 'Kelebihan'
 };
