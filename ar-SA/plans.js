3f5bf1c2873b00baee5b251758926b939c90d24e
Jan Dvorak <hello@jandvorak.me>
2024-03-07 13:01:28 +0100

    New translations plans.js (Arabic)

--- ar-SA/plans.js
+++ ar-SA/plans.js
@@ -223,5 +223,6 @@ export default {
     'payment.fanClub.description': 'وصف الطبقة',
     'payment.fanClub.picture': 'صورة الطبقة',
     'payment.fanClub.priceBreakdown': 'المشترك سيدفع: {amount}. رسوم المنصة هي {fee}، ستحصل على: {net}',
-    'payment.feeNotice': 'يرجى ملاحظة أن هذا تقدير وقد تكون الرسوم الفعلية مختلفة بناءً على نوع الدفع الخاص بالعميل والرسوم الإضافية من بوابة الدفع.'
+    'payment.feeNotice': 'يرجى ملاحظة أن هذا تقدير وقد تكون الرسوم الفعلية مختلفة بناءً على نوع الدفع الخاص بالعميل والرسوم الإضافية من بوابة الدفع.',
+    'payment.fanClub.perks': 'الامتيازات'
 };
