f202221262f22b8f0efe9677856bd3326798f438
Jan Dvorak <hello@jandvorak.me>
2024-03-09 23:15:43 +0100

    New translations common.js (Czech)

--- cs-CZ/common.js
+++ cs-CZ/common.js
@@ -259,5 +259,5 @@ export default {
     'common.back': 'Zpět',
     'console.stop': 'STOP! Zastavte!',
     'console.explain': 'Toto je funkce prohlížeče určená pro vývojáře. Pokud vám někdo řekl, abyste sem něco zkopírovali, abyste povolili funkci Literary Universe nebo "hackovali" něčí účet, jedná se o podvod a tímto získá přístup k vašemu účtu.',
-    'common.cancel': 'Zrušit'
+    'common.coping': 'Kopírování...'
 }    /* eslint-enable max-len */;
