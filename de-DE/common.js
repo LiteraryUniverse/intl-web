9b0fa45a6f62de253e88c7476a0d3f29d1ef5d6d
Jan Dvorak <hello@jandvorak.me>
2024-03-09 23:16:22 +0100

    New translations common.js (German)

--- de-DE/common.js
+++ de-DE/common.js
@@ -259,5 +259,5 @@ export default {
     'common.back': 'Zurück',
     'console.stop': 'Halt!',
     'console.explain': 'Dies ist eine Browser-Funktion, die für Entwickler gedacht ist. Wenn Ihnen jemand gesagt hat, dass Sie etwas hier kopieren und einfügen sollen, um eine Funktion des Literarischen Universums zu aktivieren oder das Konto einer Person zu "hacken", handelt es sich um einen Betrug und sie werden Zugriff auf Ihr Literarisches Universum-Konto erhalten.',
-    'common.cancel': 'Abbrechen'
+    'common.coping': 'Kopieren...'
 }    /* eslint-enable max-len */;
