b59f381f6faef7a65f0c4e8be4c19c5c19313fe3
Jan Dvorak <hello@jandvorak.me>
2024-03-09 23:19:13 +0100

    New translations common.js (Georgian)

--- ka-GE/common.js
+++ ka-GE/common.js
@@ -259,5 +259,5 @@ export default {
     'common.back': 'უკან',
     'console.stop': 'შეჩერება!',
     'console.explain': 'This is a browser feature intended for developers. If someone told you to copy-paste something here to enable a Literary Universe feature or "hack" someone\'s account, it is a scam and will give them access to your Literary Universe account.',
-    'common.cancel': 'გაუქმება'
+    'common.coping': 'კოპირება...'
 }    /* eslint-enable max-len */;
