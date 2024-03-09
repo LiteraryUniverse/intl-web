714c56227a5b2c4ffc46fe8e66f9d9433dd033fa
Jan Dvorak <hello@jandvorak.me>
2024-03-09 23:15:45 +0100

    New translations encyclopedia.js (Czech)

--- cs-CZ/encyclopedia.js
+++ cs-CZ/encyclopedia.js
@@ -151,5 +151,11 @@ export default {
     'enc.menuControls.ariaDesc': 'Nastavení encyklopédie',
     'encyclopedia.pronunciation.title': 'Výslovnost',
     'encyclopedia.wip.abbreviation': 'WIP',
-    'encyclopedia.wip.full': 'Rozpracované'
+    'encyclopedia.wip.full': 'Rozpracované',
+    'encyclopedia.settings.templateLegend': 'Template universe',
+    'encyclopedia.settings.templateDescription': 'Allow the content of this encyclopedia to be copied into other universes. This is best if you don\'t have any stories and instead are just creating a template for others to use.',
+    'encyclopedia.settings.templateCheckbox': 'This is a template universe',
+    'encyclopedia.templateCopy.modalTitle': 'Copy encyclopedia content',
+    'encyclopedia.templateCopy.description': 'Copy content of this encyclopedia into your universe to enrich it.',
+    'encyclopedia.templateCopy.targetUniverse': 'Universe to copy to'
 };
