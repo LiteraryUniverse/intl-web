a9eaf3ff110eff91349d8f48c1aebd29908bc87e
Jan Dvorak <hello@jandvorak.me>
2024-03-09 23:27:26 +0100

    New translations common.js (Esperanto)

--- eo-UY/common.js
+++ eo-UY/common.js
@@ -259,5 +259,5 @@ export default {
     'common.back': 'Reen',
     'console.stop': 'Haltu!',
     'console.explain': 'Tio estas retumilo-funkcio celata por programistoj. Se iu diris al vi kopii-kolii ion ĉi tien por ebligi funkciojn de Literatura Universo aŭ "haki" iun konton, tio estas trompo kaj donos al ili aliron al via Literatura Universo konto.',
-    'common.cancel': 'Nuligi'
+    'common.coping': 'Kopii...'
 }    /* eslint-enable max-len */;
