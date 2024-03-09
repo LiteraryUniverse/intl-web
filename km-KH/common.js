8f3af79af9a1eb09ee5c626da891ce1371577e45
Jan Dvorak <hello@jandvorak.me>
2024-03-09 23:24:36 +0100

    New translations common.js (Khmer)

--- km-KH/common.js
+++ km-KH/common.js
@@ -259,5 +259,5 @@ export default {
     'common.back': 'ត្រឡប់ក្រោយ',
     'console.stop': 'ឈប់វាយតម្លៃទូរស័ព្ទ។',
     'console.explain': 'This is a browser feature intended for developers. If someone told you to copy-paste something here to enable a Literary Universe feature or "hack" someone\'s account, it is a scam and will give them access to your Literary Universe account.',
-    'common.cancel': 'បោះបង់'
+    'common.coping': 'កិច្ចការចម្លង...'
 }    /* eslint-enable max-len */;
