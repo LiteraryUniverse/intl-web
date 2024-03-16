cb6c8ad0f848ed3f6d22dc232734d64d3da7a534
Jan Dvorak <hello@jandvorak.me>
2024-03-16 17:56:28 +0100

    New translations workshop.js (Tamil)

--- ta-IN/workshop.js
+++ ta-IN/workshop.js
@@ -281,5 +281,6 @@ export default {
     'workshop.ownWork': 'Own work',
     'workshop.list.thumbs': 'Thumbnails list',
     'workshop.list.list': 'Short list',
-    'workshop.fanfiction.new': 'Create a new fan fiction'
+    'workshop.fanfiction.new': 'Create a new fan fiction',
+    'workshop.videoTrailerLink': 'வீடியோ டிரெய்லர் இணைப்பு (Youtube அல்லது Vimeo)'
 };
