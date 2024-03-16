fd310355bb2852eea11f127f4f108378d7950bb8
Jan Dvorak <hello@jandvorak.me>
2024-03-16 18:01:17 +0100

    New translations workshop.js (Odia)

--- or-IN/workshop.js
+++ or-IN/workshop.js
@@ -281,5 +281,6 @@ export default {
     'workshop.ownWork': 'Own work',
     'workshop.list.thumbs': 'Thumbnails list',
     'workshop.list.list': 'Short list',
-    'workshop.fanfiction.new': 'Create a new fan fiction'
+    'workshop.fanfiction.new': 'Create a new fan fiction',
+    'workshop.videoTrailerLink': 'ଭିଡ଼ିଓ ଟ୍ରେଲର ଲିଙ୍କ (Youtube ବା Vimeo)'
 };
