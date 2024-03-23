89e4926a2fce6fe49cc284a67326d33136bd5552
Jan Dvorak <hello@jandvorak.me>
2024-03-23 20:05:10 +0100

    New translations workshop.js (Catalan)

--- ca-ES/workshop.js
+++ ca-ES/workshop.js
@@ -284,5 +284,5 @@ export default {
     'workshop.fanfiction.new': 'Create a new fan fiction',
     'workshop.videoTrailerLink': 'Enllaç del tràiler del vídeo',
     'workshop.videoTrailerPlayerTitle': 'Reproductor del tràiler del vídeo de la història',
-    'workshop.videoTrailer.supportedPlaftorms': 'We support Youtube, Vimeo and Odysee'
+    'workshop.videoTrailer.supportedPlaftorms': 'Donem suport a Youtube, Vimeo i Odysee'
 };
