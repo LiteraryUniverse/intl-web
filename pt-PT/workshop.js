5e0f0ba1638a5bfe1ce82122cd346ec2f9dd5b4c
Jan Dvorak <hello@jandvorak.me>
2024-03-23 19:02:32 +0100

    New translations workshop.js (Portuguese)

--- pt-PT/workshop.js
+++ pt-PT/workshop.js
@@ -282,5 +282,7 @@ export default {
     'workshop.list.thumbs': 'Lista de miniaturas',
     'workshop.list.list': 'Lista curta',
     'workshop.fanfiction.new': 'Criar uma nova fanfiction',
-    'workshop.videoTrailerLink': 'Link do trailer do vídeo (Youtube ou Vimeo)'
+    'workshop.videoTrailerLink': 'Link do trailer do vídeo',
+    'workshop.videoTrailerPlayerTitle': 'Player do trailer do vídeo da história',
+    'workshop.videoTrailer.supportedPlaftorms': 'Nós suportamos Youtube, Vimeo e Odysee'
 };
