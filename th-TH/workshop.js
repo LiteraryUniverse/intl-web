cf076471383c00bbf1df148554bce17277f1410f
Jan Dvorak <hello@jandvorak.me>
2024-04-05 00:23:55 +0200

    New translations workshop.js (Thai)

--- th-TH/workshop.js
+++ th-TH/workshop.js
@@ -286,5 +286,5 @@ export default {
     'workshop.videoTrailerPlayerTitle': 'เครื่องเล่นวิดีโอตัวอย่างเรื่องราว',
     'workshop.videoTrailer.supportedPlaftorms': 'เราสนับสนุน Youtube, Vimeo และ Odysee',
     'workshop.videoTrailer.rumble': 'เรายังสนับสนุน Rumble ด้วย แต่อย่าลืมคัดลอก URL ของ IFRAME',
-    'workshop.videoTrailer.suggestPlatforms': 'Is your video platform missing, let us know via our <a>feedback</a>!'
+    'workshop.videoTrailer.suggestPlatforms': 'หากแพลตฟอร์มวิดีโอของคุณหายไป โปรดแจ้งให้เราทราบผ่านทาง <a>คำติชม</a> ของเรา!'
 };
