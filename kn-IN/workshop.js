6a27a177dd27ff82f8f56419c6fb8f471c3c1351
Jan Dvorak <hello@jandvorak.me>
2024-04-04 23:00:08 +0200

    New translations workshop.js (Kannada)

--- kn-IN/workshop.js
+++ kn-IN/workshop.js
@@ -285,5 +285,6 @@ export default {
     'workshop.videoTrailerLink': 'ವೀಡಿಯೊ ಟ್ರೈಲರ್ ಲಿಂಕ್',
     'workshop.videoTrailerPlayerTitle': 'ಕಥಾ ವೀಡಿಯೊ ಟ್ರೈಲರ್ ಪ್ಲೇಯರ್',
     'workshop.videoTrailer.supportedPlaftorms': 'ನಾವು Youtube, Vimeo ಮತ್ತು Odysee ಅನ್ನು ಬೆಂಬಲಿಸುತ್ತೇವೆ',
-    'workshop.videoTrailer.rumble': 'ನಾವು ರಂಬಲ್ ಅನ್ನು ಹೂಡಿಕೆಗೆ ಬೆಂಬಲಿಸುತ್ತೇವೆ, ಆದರೆ IFRAME URL ಅನ್ನು ನಕಲಿಸುವುದನ್ನು ಮರೆತುಬಿಡಿ.'
+    'workshop.videoTrailer.rumble': 'ನಾವು ರಂಬಲ್ ಅನ್ನು ಹೂಡಿಕೆಗೆ ಬೆಂಬಲಿಸುತ್ತೇವೆ, ಆದರೆ IFRAME URL ಅನ್ನು ನಕಲಿಸುವುದನ್ನು ಮರೆತುಬಿಡಿ.',
+    'workshop.videoTrailer.suggestPlatforms': 'Is your video platform missing, let us know via our <a>feedback</a>!'
 };
