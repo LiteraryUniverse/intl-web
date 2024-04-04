58f6e1bbe90358217bd4cd5bd779d2863c134c29
Jan Dvorak <hello@jandvorak.me>
2024-04-04 21:18:10 +0200

    New translations workshop.js (Tamil)

--- ta-IN/workshop.js
+++ ta-IN/workshop.js
@@ -284,5 +284,6 @@ export default {
     'workshop.fanfiction.new': 'Create a new fan fiction',
     'workshop.videoTrailerLink': 'வீடியோ டிரெய்லர் இணைப்பு',
     'workshop.videoTrailerPlayerTitle': 'கதை வீடியோ டிரெய்லர் பிளேயர்',
-    'workshop.videoTrailer.supportedPlaftorms': 'நாங்கள் Youtube, Vimeo மற்றும் Odysee ஐ ஆதரிக்கிறோம்'
+    'workshop.videoTrailer.supportedPlaftorms': 'நாங்கள் Youtube, Vimeo மற்றும் Odysee ஐ ஆதரிக்கிறோம்',
+    'workshop.videoTrailer.rumble': 'நாங்கள் ரம்பிளையும் ஆதரிக்கிறோம், ஆனால் IFRAME URL ஐ நகலெடுக்க மறக்காதீர்கள்.'
 };
