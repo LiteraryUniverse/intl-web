6c33b326e0b97e6a007a958aaa3621848ca32dee
Jan Dvorak <hello@jandvorak.me>
2024-04-04 22:59:30 +0200

    New translations workshop.js (Indonesian)

--- id-ID/workshop.js
+++ id-ID/workshop.js
@@ -285,5 +285,6 @@ export default {
     'workshop.videoTrailerLink': 'Tautan trailer video',
     'workshop.videoTrailerPlayerTitle': 'Pemutar trailer video cerita',
     'workshop.videoTrailer.supportedPlaftorms': 'Kami mendukung Youtube, Vimeo, dan Odysee',
-    'workshop.videoTrailer.rumble': 'Kami juga mendukung Rumble, tetapi ingat untuk menyalin URL IFRAME.'
+    'workshop.videoTrailer.rumble': 'Kami juga mendukung Rumble, tetapi ingat untuk menyalin URL IFRAME.',
+    'workshop.videoTrailer.suggestPlatforms': 'Is your video platform missing, let us know via our <a>feedback</a>!'
 };
