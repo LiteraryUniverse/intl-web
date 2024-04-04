afb1a6e27bdf081d0c4e602f0546d862814ec3d0
Jan Dvorak <hello@jandvorak.me>
2024-04-04 21:21:45 +0200

    New translations workshop.js (Malayalam)

--- ml-IN/workshop.js
+++ ml-IN/workshop.js
@@ -284,5 +284,6 @@ export default {
     'workshop.fanfiction.new': 'Create a new fan fiction',
     'workshop.videoTrailerLink': 'വീഡിയോ ട്രെയിലർ ലിങ്ക്',
     'workshop.videoTrailerPlayerTitle': 'കഥ വീഡിയോ ട്രെയിലർ പ്ലേയർ',
-    'workshop.videoTrailer.supportedPlaftorms': 'ഞങ്ങൾ Youtube, Vimeo കുടുംബത്തിൽ പിന്തുണക്കുന്നു, അല്ലെങ്കിൽ Odysee'
+    'workshop.videoTrailer.supportedPlaftorms': 'ഞങ്ങൾ Youtube, Vimeo കുടുംബത്തിൽ പിന്തുണക്കുന്നു, അല്ലെങ്കിൽ Odysee',
+    'workshop.videoTrailer.rumble': 'ഞങ്ങൾ രംഗത്തും റംബിൾ പിന്തുണയ്ക്കുന്നു, പകർപ്പവകാശം ഐഫ്രെയിം URL പകർത്തുകയും അവിടെ പേസ്റ്റ് ചെയ്യുകയും ചെയ്യുക.'
 };
