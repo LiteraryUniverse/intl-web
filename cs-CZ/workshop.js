86d728337dfea93f63cd85da39141cbb298dbc2c
Jan Dvorak <hello@jandvorak.me>
2024-04-05 00:13:51 +0200

    New translations workshop.js (Czech)

--- cs-CZ/workshop.js
+++ cs-CZ/workshop.js
@@ -291,5 +291,5 @@ export default {
     'workshop.videoTrailerPlayerTitle': 'Přehrávač video upoutávky na příběh',
     'workshop.videoTrailer.supportedPlaftorms': 'Podporujeme Youtube, Vimeo a Odysee',
     'workshop.videoTrailer.rumble': 'Podporujeme také Rumble, ale nezapomeňte zkopírovat URL adresu IFRAME.',
-    'workshop.videoTrailer.suggestPlatforms': 'Is your video platform missing, let us know via our <a>feedback</a>!'
+    'workshop.videoTrailer.suggestPlatforms': 'Chybí vám vaše video platforma? Dejte nám vědět prostřednictvím naší <a>zpětné vazby</a>!'
 };
