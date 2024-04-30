dd661fa07add792ab508c6d1dc3fc05836bb0acb
Jan Dvorak <hello@jandvorak.me>
2024-04-30 20:18:53 +0200

    New translations common.js (Afrikaans)

--- af-ZA/common.js
+++ af-ZA/common.js
@@ -260,5 +260,5 @@ export default {
     'console.stop': 'Stop!',
     'console.explain': 'Hierdie is \'n blaaier eienskap bedoel vir ontwikkelaars. As iemand jou vertel het om iets hier te kopieer en plak om \'n Literêre Heelal eienskap te aktiveer of iemand se rekening te "hack", is dit \'n bedrog en sal dit hulle toegang gee tot jou Literêre Heelal rekening.',
     'common.coping': 'Kopiëring...',
-    'common.actions': 'Actions'
+    'common.actions': 'Aksies'
 }    /* eslint-enable max-len */;
