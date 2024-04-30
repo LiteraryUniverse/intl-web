4f8c73dbd257655a2cd6d8eeb16c3b7940017792
Jan Dvorak <hello@jandvorak.me>
2024-04-30 20:23:55 +0200

    New translations common.js (Lithuanian)

--- lt-LT/common.js
+++ lt-LT/common.js
@@ -260,5 +260,5 @@ export default {
     'console.stop': 'Sustok!',
     'console.explain': 'Tai yra naršyklės funkcija, skirta programuotojams. Jei kas nors pasakė jums, kad čia nukopijuosite kažką, kad įgalintumėte „Literary Universe“ funkciją arba „įsilaužtumėte“ į kieno nors paskyrą, tai yra apgavystė ir leis jiems patekti į jūsų „Literary Universe“ paskyrą.',
     'common.coping': 'Kopijuojama...',
-    'common.actions': 'Actions'
+    'common.actions': 'Veiksmai'
 }    /* eslint-enable max-len */;
