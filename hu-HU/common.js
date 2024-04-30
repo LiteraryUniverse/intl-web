ef5bb80c2588f00b7109e5275f8c93ac28af8602
Jan Dvorak <hello@jandvorak.me>
2024-04-30 18:57:00 +0200

    New translations common.js (Hungarian)

--- hu-HU/common.js
+++ hu-HU/common.js
@@ -259,5 +259,6 @@ export default {
     'common.back': 'Vissza',
     'console.stop': 'Állj!',
     'console.explain': 'Ez egy böngésző funkció, amely a fejlesztők számára készült. Ha valaki azt mondta neked, hogy másolj ide valamit, hogy engedélyezd a Literary Universe funkciót, vagy hogy valakinek a fiókját "hackeld", az egy átverés, és hozzáférést biztosít nekik a Literary Universe fiókodhoz.',
-    'common.coping': 'Másolás...'
+    'common.coping': 'Másolás...',
+    'common.actions': 'Actions'
 }    /* eslint-enable max-len */;
