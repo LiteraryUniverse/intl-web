291a16162a8d7d4420b6b8d1b0c0bbae9808b463
Jan Dvorak <hello@jandvorak.me>
2024-04-30 19:05:16 +0200

    New translations common.js (Tamil)

--- ta-IN/common.js
+++ ta-IN/common.js
@@ -259,5 +259,6 @@ export default {
     'common.back': 'பின் செல்லுங்கள்',
     'console.stop': 'நிறுத்துக!',
     'console.explain': 'This is a browser feature intended for developers. If someone told you to copy-paste something here to enable a Literary Universe feature or "hack" someone\'s account, it is a scam and will give them access to your Literary Universe account.',
-    'common.coping': 'நகலெடுக்குகிறது...'
+    'common.coping': 'நகலெடுக்குகிறது...',
+    'common.actions': 'செயல்கள்'
 }    /* eslint-enable max-len */;
