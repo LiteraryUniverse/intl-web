4b7a49ec1d8a8e3aa9e7404ce9f881e59811a791
Jan Dvorak <hello@jandvorak.me>
2024-04-30 20:24:24 +0200

    New translations common.js (Norwegian)

--- no-NO/common.js
+++ no-NO/common.js
@@ -260,5 +260,5 @@ export default {
     'console.stop': 'Stopp!',
     'console.explain': 'Dette er en nettleserfunksjon ment for utviklere. Hvis noen har bedt deg om å kopiere og lime inn noe her for å aktivere en Literary Universe-funksjon eller "hacke" noen konto, er det svindel og vil gi dem tilgang til din Literary Universe-konto.',
     'common.coping': 'Kopierer...',
-    'common.actions': 'Actions'
+    'common.actions': 'Handlinger'
 }    /* eslint-enable max-len */;
