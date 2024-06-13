d4b2f54947b11319bdcc16157e1eb600bd0a4b0a
Jan Dvorak <hello@jandvorak.me>
2024-06-13 09:39:00 +0200

    New translations common.js (Odia)

--- or-IN/common.js
+++ or-IN/common.js
@@ -260,5 +260,8 @@ export default {
     'console.stop': 'ଥମ୍ବ!',
     'console.explain': 'This is a browser feature intended for developers. If someone told you to copy-paste something here to enable a Literary Universe feature or "hack" someone\'s account, it is a scam and will give them access to your Literary Universe account.',
     'common.coping': 'ଅନୁଲେଖନ କରୁଛି...',
-    'common.actions': 'କାର୍ଯ୍ୟଗୁଡ଼ିକ'
+    'common.actions': 'କାର୍ଯ୍ୟଗୁଡ଼ିକ',
+    'common.popular': 'Popular',
+    'common.newest': 'Newest',
+    'common.oldest': 'Oldest'
 }    /* eslint-enable max-len */;
