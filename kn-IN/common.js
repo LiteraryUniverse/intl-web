27e4eff8b684999e0383bd2415ed924f651983c1
Jan Dvorak <hello@jandvorak.me>
2024-06-13 09:37:54 +0200

    New translations common.js (Kannada)

--- kn-IN/common.js
+++ kn-IN/common.js
@@ -260,5 +260,8 @@ export default {
     'console.stop': 'ನಿಲ್ಲಿ!',
     'console.explain': 'This is a browser feature intended for developers. If someone told you to copy-paste something here to enable a Literary Universe feature or "hack" someone\'s account, it is a scam and will give them access to your Literary Universe account.',
     'common.coping': 'ನಕಲಿಕೆ ಮಾಡುವುದು...',
-    'common.actions': 'ಕ್ರಿಯೆಗಳು'
+    'common.actions': 'ಕ್ರಿಯೆಗಳು',
+    'common.popular': 'Popular',
+    'common.newest': 'Newest',
+    'common.oldest': 'Oldest'
 }    /* eslint-enable max-len */;
