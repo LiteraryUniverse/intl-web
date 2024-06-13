d9eec9a62ca1a994bb381dfdb7a73feede2c1307
Jan Dvorak <hello@jandvorak.me>
2024-06-13 09:36:49 +0200

    New translations common.js (Lojban)

--- jbo-EN/common.js
+++ jbo-EN/common.js
@@ -260,5 +260,8 @@ export default {
     'console.stop': 'co\'o!',
     'console.explain': 'This is a browser feature intended for developers. If someone told you to copy-paste something here to enable a Literary Universe feature or "hack" someone\'s account, it is a scam and will give them access to your Literary Universe account.',
     'common.coping': 'kojgau...',
-    'common.actions': 'Rojykam'
+    'common.actions': 'Rojykam',
+    'common.popular': 'Popular',
+    'common.newest': 'Newest',
+    'common.oldest': 'Oldest'
 }    /* eslint-enable max-len */;
