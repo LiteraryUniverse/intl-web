e3b01d75e33c7f841e4cf8ece87964c804ed97bf
Jan Dvorak <hello@jandvorak.me>
2024-06-13 09:35:40 +0200

    New translations common.js (Telugu)

--- te-IN/common.js
+++ te-IN/common.js
@@ -260,5 +260,8 @@ export default {
     'console.stop': 'నిలిపేయండి!',
     'console.explain': 'This is a browser feature intended for developers. If someone told you to copy-paste something here to enable a Literary Universe feature or "hack" someone\'s account, it is a scam and will give them access to your Literary Universe account.',
     'common.coping': 'నకలు చేస్తున్నాం...',
-    'common.actions': 'చర్యలు'
+    'common.actions': 'చర్యలు',
+    'common.popular': 'Popular',
+    'common.newest': 'Newest',
+    'common.oldest': 'Oldest'
 }    /* eslint-enable max-len */;
