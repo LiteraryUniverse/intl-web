9b03b8d960e6d1c181cc8936c62370da46ea15d6
Jan Dvorak <hello@jandvorak.me>
2024-06-13 09:38:38 +0200

    New translations common.js (Maithili)

--- mai-IN/common.js
+++ mai-IN/common.js
@@ -260,5 +260,8 @@ export default {
     'console.stop': 'रुक!',
     'console.explain': 'यह एक ब्राउज़र सुविधा है जो डेवलपर्स के लिए है। यदि किसी ने आपको यहां कुछ कॉपी-पेस्ट करने के लिए कहा है ताकि आप एक साहित्यिक यूनिवर्स फ़ीचर या किसी खाते को "हैक" कर सकें, तो यह एक धोखाधड़ी है और उन्हें आपके साहित्यिक यूनिवर्स खाते तक पहुंच देगी।',
     'common.coping': 'कॉपी कर रहे हैं...',
-    'common.actions': 'क्रियाएँ'
+    'common.actions': 'क्रियाएँ',
+    'common.popular': 'Popular',
+    'common.newest': 'Newest',
+    'common.oldest': 'Oldest'
 }    /* eslint-enable max-len */;
