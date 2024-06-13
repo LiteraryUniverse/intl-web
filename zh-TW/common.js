9b70b10a511ad86e44130c9c4bc44afc367e9f25
Jan Dvorak <hello@jandvorak.me>
2024-06-13 11:43:16 +0200

    New translations common.js (Chinese Traditional)

--- zh-TW/common.js
+++ zh-TW/common.js
@@ -261,7 +261,7 @@ export default {
     'console.explain': '這是一個專為開發人員設計的瀏覽器功能。如果有人告訴你在這裡複製粘貼某些內容以啟用文學宇宙功能或"駭入"某人的帳戶，這是一個詐騙行為，將使他們能夠訪問你的文學宇宙帳戶。',
     'common.coping': '複製中...',
     'common.actions': '操作',
-    'common.popular': 'Popular',
-    'common.newest': 'Newest',
-    'common.oldest': 'Oldest'
+    'common.popular': '熱門',
+    'common.newest': '最新',
+    'common.oldest': '最舊'
 }    /* eslint-enable max-len */;
