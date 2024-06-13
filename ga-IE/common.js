a5cffa2821a57825af76063a785edf8e598d44cb
Jan Dvorak <hello@jandvorak.me>
2024-06-13 09:24:13 +0200

    New translations common.js (Irish)

--- ga-IE/common.js
+++ ga-IE/common.js
@@ -260,5 +260,8 @@ export default {
     'console.stop': 'Stop!',
     'console.explain': 'Is gné brabhsála é seo atá dírithe ar fhorbróirí. Má dúirt duine ar bith leat rud éigin a chóipeáil agus a pháistí anseo chun gné Léitheoireachta Cruinne a chumasú nó chun cuntas duine ar bith a "hackáil", is scam é sin agus tabharfaidh sé rochtain dóibh ar do chuntas Léitheoireachta Cruinne.',
     'common.coping': 'Ag cóipeáil...',
-    'common.actions': 'Gníomhaíochtaí'
+    'common.actions': 'Gníomhaíochtaí',
+    'common.popular': 'Popular',
+    'common.newest': 'Newest',
+    'common.oldest': 'Oldest'
 }    /* eslint-enable max-len */;
