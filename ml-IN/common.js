67b044695399c0508cd2326a66bb1e3a725d05e3
Jan Dvorak <hello@jandvorak.me>
2024-06-13 11:49:51 +0200

    New translations common.js (Malayalam)

--- ml-IN/common.js
+++ ml-IN/common.js
@@ -261,7 +261,7 @@ export default {
     'console.explain': 'This is a browser feature intended for developers. If someone told you to copy-paste something here to enable a Literary Universe feature or "hack" someone\'s account, it is a scam and will give them access to your Literary Universe account.',
     'common.coping': 'പകർത്തുക...',
     'common.actions': 'പ്രവർത്തനങ്ങൾ',
-    'common.popular': 'Popular',
-    'common.newest': 'Newest',
-    'common.oldest': 'Oldest'
+    'common.popular': 'ജനപ്രിയമായ',
+    'common.newest': 'പുതിയത്',
+    'common.oldest': 'പഴയതായി'
 }    /* eslint-enable max-len */;
