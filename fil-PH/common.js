806d09a75fd2ef8c86add092eb69570ac0fa7353
Jan Dvorak <hello@jandvorak.me>
2024-06-13 09:36:25 +0200

    New translations common.js (Filipino)

--- fil-PH/common.js
+++ fil-PH/common.js
@@ -260,5 +260,8 @@ export default {
     'console.stop': 'Tigil!',
     'console.explain': 'Ito ay isang tampok ng browser na inilaan para sa mga developer. Kung sinabihan ka ng ibang tao na i-copy-paste ang isang bagay dito upang paganahin ang isang tampok ng Literary Universe o "hack" ang account ng ibang tao, ito ay isang scam at magbibigay sa kanila ng access sa iyong Literary Universe account.',
     'common.coping': 'Kumokopya...',
-    'common.actions': 'Mga Aksyon'
+    'common.actions': 'Mga Aksyon',
+    'common.popular': 'Popular',
+    'common.newest': 'Newest',
+    'common.oldest': 'Oldest'
 }    /* eslint-enable max-len */;
