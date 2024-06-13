6a274ca30e4a728f5d37cbd57752d0c7d75c701d
Jan Dvorak <hello@jandvorak.me>
2024-06-13 11:51:53 +0200

    New translations common.js (Odia)

--- or-IN/common.js
+++ or-IN/common.js
@@ -261,7 +261,7 @@ export default {
     'console.explain': 'This is a browser feature intended for developers. If someone told you to copy-paste something here to enable a Literary Universe feature or "hack" someone\'s account, it is a scam and will give them access to your Literary Universe account.',
     'common.coping': 'ଅନୁଲେଖନ କରୁଛି...',
     'common.actions': 'କାର୍ଯ୍ୟଗୁଡ଼ିକ',
-    'common.popular': 'Popular',
-    'common.newest': 'Newest',
-    'common.oldest': 'Oldest'
+    'common.popular': 'ପ୍ରସିଦ୍ଧ',
+    'common.newest': 'ନବିନତମ',
+    'common.oldest': 'ପୁରୁଣା'
 }    /* eslint-enable max-len */;
