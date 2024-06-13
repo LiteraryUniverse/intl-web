cc4c3f58636442abf2fca0350858fbae37272410
Jan Dvorak <hello@jandvorak.me>
2024-06-13 11:33:00 +0200

    New translations common.js (Romanian)

--- ro-RO/common.js
+++ ro-RO/common.js
@@ -262,6 +262,6 @@ export default {
     'common.coping': 'Copiere...',
     'common.actions': 'Acțiuni',
     'common.popular': 'Popular',
-    'common.newest': 'Newest',
+    'common.newest': 'Cel mai nou',
     'common.oldest': 'Oldest'
 }    /* eslint-enable max-len */;
