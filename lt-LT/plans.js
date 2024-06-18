a2c3fa0098dfe18a102b3a36315fe68ce79905d7
Jan Dvorak <hello@jandvorak.me>
2024-06-18 15:00:31 +0200

    New translations plans.js (Lithuanian)

--- lt-LT/plans.js
+++ lt-LT/plans.js
@@ -224,5 +224,6 @@ export default {
     'payment.fanClub.picture': 'Lygio nuotrauka',
     'payment.fanClub.priceBreakdown': 'Abonentas sumokės: {amount}. Platformos mokestis yra {fee}, jūs gausite: {net}',
     'payment.feeNotice': 'Prašome atkreipti dėmesį, kad tai yra tik apskaičiavimas ir tikras mokestis gali skirtis priklausomai nuo kliento mokėjimo tipo ir papildomų mokesčių iš mokėjimo vartų.',
-    'payment.fanClub.perks': 'Privalumai'
+    'payment.fanClub.perks': 'Privalumai',
+    'stripeConnect.settings.notifications': 'Pranešimai iš „Stripe“'
 };
