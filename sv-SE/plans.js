a5239c16675039330956099b5ea038e52b8a96ca
Jan Dvorak <hello@jandvorak.me>
2024-06-18 15:03:41 +0200

    New translations plans.js (Swedish)

--- sv-SE/plans.js
+++ sv-SE/plans.js
@@ -224,5 +224,6 @@ export default {
     'payment.fanClub.picture': 'Nivåbild',
     'payment.fanClub.priceBreakdown': 'Abonnenten kommer att betala: {amount}. Plattformavgiften är {fee}, du kommer att få: {net}',
     'payment.feeNotice': 'Observera att detta endast är en uppskattning och den faktiska avgiften kan variera beroende på kundens betalningssätt och eventuella extra avgifter från betalningsportalen.',
-    'payment.fanClub.perks': 'Förmåner'
+    'payment.fanClub.perks': 'Förmåner',
+    'stripeConnect.settings.notifications': 'Meddelanden från Stripe'
 };
