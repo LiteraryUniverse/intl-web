2b6b9af426cdea114bf4f1d2def05c7afbcc0713
Jan Dvorak <hello@jandvorak.me>
2024-06-25 21:43:00 +0200

    New translations plans.js (Assamese)

--- as-IN/plans.js
+++ as-IN/plans.js
@@ -222,8 +222,13 @@ export default {
     'payment.fanClub.createTier': 'টিয়াৰ যোগ কৰক',
     'payment.fanClub.description': 'টিয়াৰৰ বিৱৰণ',
     'payment.fanClub.picture': 'টিয়াৰৰ ছবি',
-    'payment.fanClub.priceBreakdown': 'গ্রাহক প্রদান কৰিব: {amount}। প্লেটফৰ্ম ফি হৈছে {fee}, আপুনি পাব: {net}',
-    'payment.feeNotice': 'অনুগ্ৰহ কৰি লক্ষ্য কৰক যে এইটো এটা আনুমানিক মাত্ৰা হৈছে আৰু গ্ৰাহকৰ পেমেন্ট প্ৰকাৰ আৰু অতিৰিক্ত চাৰ্জৰ উপৰ ভিত্তিত পৰিবৰ্তন কৰিব পাৰে।',
+    'payment.fanClub.priceBreakdown': 'সাবস্ক্রাইবারটো দিব হুবহু: {amount}। প্ল্যাটফর্ম ফী হয় {fee} ({platformPercentage}), যোগাযোগ ধরনের ভিত্তি করে এস্টিমেটেড স্ট্রাইপ ফীঃ {stripeFee} ({stripeBaseFee} বেস লেনদেন ফী + পেমেন্ট ধরনের ভিত্তিতে ফি), আপুনি পাবেন (এস্টিমেটেড)ঃ {net}',
+    'payment.fanClub.learnStripeFees': 'স্ট্রাইপ ফীস সম্পর্কে আরো জানুন।',
+    'payment.feeNotice': 'দয়া করে মনতো হোক যে এটা একটি চিকিৎসা, আর খাইজনা একটি অনুমান এবং মার্জিন। কাস্টমারের পেমেন্ট ধরন এবং পেমেন্ট গেট থেকে অতিরিক্ত চার্জ অনুযায়ী, স্ট্রাইপ ফী আসলেও সল্প আছে হতেও পারে।',
     'payment.fanClub.perks': 'সুবিধা',
-    'stripeConnect.settings.notifications': 'ষ্ট্রাইপৰ বিজ্ঞপ্তি'
+    'stripeConnect.settings.notifications': 'ষ্ট্রাইপৰ বিজ্ঞপ্তি',
+    'plans.pricePerMonth': 'মাসিক পরিমাণ',
+    'payment.fanClub.perkType': 'পার্ক প্রকার',
+    'payment.fanClub.addPerk': 'পার্ক যোগ কৰক',
+    'payment.fanClub.perksExplained': 'এইগুলো হলে পাইউনী যেটা শাস্তি লোক তৰ লগত অৰায় গাল পাবে লিটেৰেৰি ইউনিভার্সে উৎসাহিত স্বয়ংক্রিয়ভাৱে।'
 };
