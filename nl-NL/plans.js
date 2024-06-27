ae86cf4994b441d08efe0daa1cee7ee089ddd7c6
Jan Dvorak <hello@jandvorak.me>
2024-06-27 22:58:02 +0200

    New translations plans.js (Dutch)

--- nl-NL/plans.js
+++ nl-NL/plans.js
@@ -232,5 +232,6 @@ export default {
     'payment.fanClub.addPerk': 'Voeg voordeel toe',
     'payment.fanClub.perksExplained': 'Dit zijn voordelen die automatisch aan de fan worden toegekend via Literary Universe.',
     'plans.deleteTier.header': 'Weet je zeker dat je niveau {tierName} wilt verwijderen?',
-    'plans.deleteTier.explainer': 'Als dit niveau geen abonnees heeft, zal het worden verwijderd. Anders wordt het uitgeschakeld voor nieuwe abonnementen, maar bestaande abonnementen zullen doorgaan totdat de fan zijn abonnement wijzigt.'
+    'plans.deleteTier.explainer': 'Als dit niveau geen abonnees heeft, zal het worden verwijderd. Anders wordt het uitgeschakeld voor nieuwe abonnementen, maar bestaande abonnementen zullen doorgaan totdat de fan zijn abonnement wijzigt.',
+    'plans.fanClub.userSettings': 'Beheer uw abonnementen voor uw fanclub'
 };
