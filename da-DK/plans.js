2bb7a5372fedced9e2fd2f7d373e7160a2220bc9
Jan Dvorak <hello@jandvorak.me>
2024-07-07 21:48:09 +0200

    New translations plans.js (Danish)

--- da-DK/plans.js
+++ da-DK/plans.js
@@ -233,5 +233,6 @@ export default {
     'payment.fanClub.perksExplained': 'Dette er fordele, der automatisk tildeles til fanen gennem Literary Universe.',
     'plans.deleteTier.header': 'Er du sikker på, at du vil slette niveauet {tierName}?',
     'plans.deleteTier.explainer': 'Hvis dette niveau ikke har nogen abonnenter, vil det blive slettet. Ellers vil det blive deaktiveret for nye abonnementer, men eksisterende abonnementer vil fortsætte, indtil brugeren ændrer deres abonnement.',
-    'plans.fanClub.userSettings': 'Administrer dine fan klub abonnementer'
+    'plans.fanClub.userSettings': 'Administrer dine fan klub abonnementer',
+    'plans.fanClub.subscribers': 'Abonnenter'
 };
