1f421dfa79d43cd9775190d2e380f14e3ffe539a
Jan Dvorak <hello@jandvorak.me>
2024-07-17 01:09:02 +0200

    New translations organizations.js (Arabic)

--- ar-SA/organizations.js
+++ ar-SA/organizations.js
@@ -53,5 +53,7 @@ export default {
     'org.teams.create': 'إنشاء فريق جديد',
     'org.teams.settings': 'إعدادات الفريق',
     'org.workshop.ownerUser': 'Not a guild work',
-    'org.workshop.caption': 'Guild ownership'
+    'org.workshop.caption': 'Guild ownership',
+    'org.workshop.title': 'ورشة عمل المنظمة',
+    'org.title': 'المنظمة'
 };
