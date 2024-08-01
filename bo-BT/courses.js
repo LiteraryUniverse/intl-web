755f10464f2ca659742ea8c59b66301b64f70d8d
Jan Dvorak <hello@jandvorak.me>
2024-08-01 13:03:12 +0200

    New translations courses.js (Tibetan)

--- bo-BT/courses.js
+++ bo-BT/courses.js
@@ -1,3 +1,5 @@
 export default {
-  'courses.courses': 'Courses',
+  'courses.courses': 'སློབ་ཚན།',
+  'courses.overview': 'སློབ་ཚན་གྱི་སྐོར་འགྱུར།',
+  'course.create': 'སློབ་ཚན་གསར་འགྱུར་གསར་བསྐྱར།',
 };
