0b0c06afef201069043651dcf97e9ecfe4f0711f
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:15:53 +0200

    New translations library.js (Danish)

--- da-DK/library.js
+++ da-DK/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'Tilføj til bibliotek',
-  'library.remove': 'Fjern fra bibliotek',
-  'library.bought': 'Købte historier',
-  'library.stories': 'Følgte historier',
-  'library.universes': 'Followed universes',
-  'bookmark.continueReading': 'Continue reading',
-  'bookmark.statusSummary': 'You have read {readPercent, number, ::percent} of the story.',
-}
+    'library.add': 'Tilføj til bibliotek',
+    'library.remove': 'Fjern fra bibliotek',
+    'library.bought': 'Købte historier',
+    'library.stories': 'Følgte historier',
+    'library.universes': 'Followed universes',
+    'bookmark.continueReading': 'Continue reading',
+    'bookmark.statusSummary': 'You have read {readPercent, number, ::percent} of the story.'
+};
