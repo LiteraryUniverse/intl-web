bcb990d07fe7a246c95ac8e7bb6ee47738f58ea8
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:27:25 +0200

    New translations enrollment.js (Turkish)

--- tr-TR/enrollment.js
+++ tr-TR/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'Hesabınızı talep edin!',
-  'enrollment.description':
-    'Hoş geldiniz {username}! Özel hesabınıza erişim kazanmak için sadece bir şifre oluşturmanız gerekmektedir.',
-  'enrollment.submitting': 'Hesabınızı talep ediyor...',
-  'enrollment.submit': 'Hesabınızı talep edin!',
-}
+    'enrollment.title': 'Hesabınızı talep edin!',
+    'enrollment.description': 'Hoş geldiniz {username}! Özel hesabınıza erişim kazanmak için sadece bir şifre oluşturmanız gerekmektedir.',
+    'enrollment.submitting': 'Hesabınızı talep ediyor...',
+    'enrollment.submit': 'Hesabınızı talep edin!'
+};
