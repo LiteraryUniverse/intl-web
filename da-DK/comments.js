5850f52b05dd616e404061124f641a3ce91162cc
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:15:51 +0200

    New translations comments.js (Danish)

--- da-DK/comments.js
+++ da-DK/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': 'Skriv din kommentar',
-  'comments.none': 'Beklager, ingen kommentarer at vise.',
-  'comments.total': `There {count, plural,
+    'comment.write': 'Skriv din kommentar',
+    'comments.none': 'Beklager, ingen kommentarer at vise.',
+    'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-  'comments.show.older': 'Vis ældre kommentarer',
-}
+    'comments.show.older': 'Vis ældre kommentarer'
+};
