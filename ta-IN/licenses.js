4ca4c24f8e2fdd3adcad9f7cfe284bc2f533f833
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:31:27 +0200

    New translations licenses.js (Tamil)

--- ta-IN/licenses.js
+++ ta-IN/licenses.js
@@ -3,31 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'About licenses',
-  'licenses.more': 'More information',
-  'licenses.legal': 'Legal document',
-  'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
-  'licenses.cc-by':
-    'This license lets others distribute, remix, tweak, and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of licenses offered. Recommended for maximum dissemination and use of licensed materials.',
-  'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
-  'licenses.cc-by-sa':
-    'This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.',
-  'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
-  'licenses.cc-by-nd':
-    'This license allows for redistribution, commercial and non-commercial, as long as it is passed along unchanged and in whole, with credit to you.',
-  'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
-  'licenses.cc-by-nc':
-    'This license lets others remix, tweak, and build upon your work non-commercially, and although their new works must also acknowledge you and be non-commercial, they don’t have to license their derivative works on the same terms.',
-  'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
-  'licenses.cc-by-nc-sa':
-    'This license lets others remix, tweak, and build upon your work non-commercially, as long as they credit you and license their new creations under the identical terms.',
-  'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
-  'licenses.cc-by-nc-nd':
-    'This license is the most restrictive of our six main licenses, only allowing others to download your works and share them with others as long as they credit you, but they can’t change them in any way or use them commercially.',
-  'licenses.name.lu-c': 'Literary Universe Copyrights',
-  'licenses.lu-c':
-    'நீங்கள் உங்கள் வேலையை விரும்பும்போது விரும்புவதன் மூலம் உங்கள் உரிமைகளை பாதுகாக்குகின்ற ஒரு நியாய உரிமை அனுமதி. இந்த உரிமை உங்களைக் கதைகளை விற்றுக்கொள்ள மற்றும் இலக்கு உரிமையை பெற வழிவகுக்கின்றது.',
-  'licenses.name.cc0': 'எந்த உரிமையுமில்லை - CC0 1.0',
-  'licenses.cc0':
-    'உங்கள் வேலையை பொது படைக்க வைக்கவும். உரிமையை வைப்பதுபோல் எண்ணிக்கையான அனுமதிகளை தேர்வு செய்யும்போது உரிமை உரிமையாளர்களுக்கு விருப்பமான மிகுந்த உரிமையை வைத்துக் கொடுக்குகின்ற CC உரிமைகளுடன் எதிர்பார்க்கப்படும் CC0 என்பது உரிமையை முழுமையாக விடுவிக்கும் மற்றும் கடவுச்சொல்லை வழங்குகின்றது.',
-}
+    'licenses.about': 'About licenses',
+    'licenses.more': 'More information',
+    'licenses.legal': 'Legal document',
+    'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
+    'licenses.cc-by': 'This license lets others distribute, remix, tweak, and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of licenses offered. Recommended for maximum dissemination and use of licensed materials.',
+    'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
+    'licenses.cc-by-sa': 'This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.',
+    'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
+    'licenses.cc-by-nd': 'This license allows for redistribution, commercial and non-commercial, as long as it is passed along unchanged and in whole, with credit to you.',
+    'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
+    'licenses.cc-by-nc': 'This license lets others remix, tweak, and build upon your work non-commercially, and although their new works must also acknowledge you and be non-commercial, they don’t have to license their derivative works on the same terms.',
+    'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
+    'licenses.cc-by-nc-sa': 'This license lets others remix, tweak, and build upon your work non-commercially, as long as they credit you and license their new creations under the identical terms.',
+    'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
+    'licenses.cc-by-nc-nd': 'This license is the most restrictive of our six main licenses, only allowing others to download your works and share them with others as long as they credit you, but they can’t change them in any way or use them commercially.',
+    'licenses.name.lu-c': 'Literary Universe Copyrights',
+    'licenses.lu-c': 'நீங்கள் உங்கள் வேலையை விரும்பும்போது விரும்புவதன் மூலம் உங்கள் உரிமைகளை பாதுகாக்குகின்ற ஒரு நியாய உரிமை அனுமதி. இந்த உரிமை உங்களைக் கதைகளை விற்றுக்கொள்ள மற்றும் இலக்கு உரிமையை பெற வழிவகுக்கின்றது.',
+    'licenses.name.cc0': 'எந்த உரிமையுமில்லை - CC0 1.0',
+    'licenses.cc0': 'உங்கள் வேலையை பொது படைக்க வைக்கவும். உரிமையை வைப்பதுபோல் எண்ணிக்கையான அனுமதிகளை தேர்வு செய்யும்போது உரிமை உரிமையாளர்களுக்கு விருப்பமான மிகுந்த உரிமையை வைத்துக் கொடுக்குகின்ற CC உரிமைகளுடன் எதிர்பார்க்கப்படும் CC0 என்பது உரிமையை முழுமையாக விடுவிக்கும் மற்றும் கடவுச்சொல்லை வழங்குகின்றது.'
+};
