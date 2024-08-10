db6b705875e5d736e23b90ac27dcaa4e9bcb59da
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:34:37 +0200

    New translations blog.js (Malay)

--- ms-MY/blog.js
+++ ms-MY/blog.js
@@ -1,41 +1,39 @@
 export default {
-  'blog.noneMsg': `Anda kini tidak mempunyai {type, select,
+    'blog.noneMsg': `Anda kini tidak mempunyai {type, select,
     organization {sebarang blog}
     universe {satu blog untuk alam semesta anda}
     user {sebuah blog}
     other {}
   }.`,
-  'blog.create': 'Cipta blog baru',
-  'blog.settings': `{type, select,
+    'blog.create': 'Cipta blog baru',
+    'blog.settings': `{type, select,
     org {Blog}
     universe {Halaman alam}
     user {Blog}
     other {}
   } tetapan`,
-  'blog.description': 'Pengenalan blog',
-  'blog.theme': 'Tema',
-  'blog.theme.info':
-    'Dengan menyesuaikan tema, anda akan dapat mengubah rupa halaman. Fungsi ini tidak disediakan pada masa ini.',
-  'blog.posts.total': `Tiada {total, plural,
+    'blog.description': 'Pengenalan blog',
+    'blog.theme': 'Tema',
+    'blog.theme.info': 'Dengan menyesuaikan tema, anda akan dapat mengubah rupa halaman. Fungsi ini tidak disediakan pada masa ini.',
+    'blog.posts.total': `Tiada {total, plural,
     zero {perkara}
     one {satu perkara}
     other {# perkara}
   } di blog ini.`,
-  'blog.post.create': 'Cipta pos baru',
-  'blog.post.update': 'Mengedit {title}',
-  'blog.post.text': 'Teks pos',
-  'common.slug': 'Alamat yang mesra SEO',
-  'blog.slug.explained':
-    'URL yang menarik untuk pos tersebut. Elakkan menggunakan huruf khas kecuali "-".',
-  'blog.publicView': 'Paparan awam',
-  'blog.post.notfound': 'Pos blog tidak dijumpai',
-  'blog.lists.works': `{type, select,
+    'blog.post.create': 'Cipta pos baru',
+    'blog.post.update': 'Mengedit {title}',
+    'blog.post.text': 'Teks pos',
+    'common.slug': 'Alamat yang mesra SEO',
+    'blog.slug.explained': 'URL yang menarik untuk pos tersebut. Elakkan menggunakan huruf khas kecuali "-".',
+    'blog.publicView': 'Paparan awam',
+    'blog.post.notfound': 'Pos blog tidak dijumpai',
+    'blog.lists.works': `{type, select,
     universes {Universiti}
     stories {Cerita}
     fanfiction {Karya Fiksyen Penggemar}
     other {}
   }`,
-  'blog.social.sameAsMain': `Pautan sosial adalah sama seperti yang ditetapkan dalam {type, select,
+    'blog.social.sameAsMain': `Pautan sosial adalah sama seperti yang ditetapkan dalam {type, select,
     organization {tetapan organisasi}
     user {profil pengguna}
     other {}
