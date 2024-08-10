83d9d51e580416aa7940b6e8845ff57ccbd86c3b
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:20:59 +0200

    New translations library.js (Japanese)

--- ja-JP/library.js
+++ ja-JP/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'ライブラリーに追加',
-  'library.remove': 'ライブラリーから削除',
-  'library.bought': '物語を購入',
-  'library.stories': 'ストーリーをフォローする',
-  'library.universes': '宇宙小説をフォローする',
-  'bookmark.continueReading': '続きを読む',
-  'bookmark.statusSummary': '物語の {readPercent, number, ::percent} を読みました。',
-}
+    'library.add': 'ライブラリーに追加',
+    'library.remove': 'ライブラリーから削除',
+    'library.bought': '物語を購入',
+    'library.stories': 'ストーリーをフォローする',
+    'library.universes': '宇宙小説をフォローする',
+    'bookmark.continueReading': '続きを読む',
+    'bookmark.statusSummary': '物語の {readPercent, number, ::percent} を読みました。'
+};
