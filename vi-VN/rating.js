ae4da81ef1ca4537f6b1bb0201ab6e4405594bb6
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:29:16 +0200

    New translations rating.js (Vietnamese)

--- vi-VN/rating.js
+++ vi-VN/rating.js
@@ -2,19 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Đánh giá',
-  'rating.explained': 'Đánh giá tuổi và hạn chế.',
-  'rating.all': 'Tất cả',
-  'rating.desc.all': 'Phù hợp với mọi độ tuổi và nhóm.',
-  'rating.teens': 'Thiếu niên',
-  'rating.desc.teens': 'Phù hợp với khán giả từ 12 đến 18 tuổi.',
-  'rating.mature': 'Người lớn',
-  'rating.desc.mature': 'Chứa những chủ đề tiên tiến, ngôn ngữ thô tục và cảnh nóng ngụ ý.',
-  'rating.explicit': 'Rõ ràng',
-  'rating.desc.explicit': 'Chứa cảnh nóng rõ ràng.',
-  'rating.selection.all': 'Tất cả - Phù hợp với mọi độ tuổi và nhóm.',
-  'rating.selection.teens': 'Thiếu niên (12-18) - Phù hợp với khán giả từ 12 đến 18 tuổi.',
-  'rating.selection.mature':
-    'Người lớn (18+) - Chứa những chủ đề tiên tiến, ngôn ngữ thô tục và cảnh nóng ngụ ý.',
-  'rating.selection.explicit': 'Rõ ràng (21+) - Chứa cảnh nóng rõ ràng.',
-}
+    'rating.name': 'Đánh giá',
+    'rating.explained': 'Đánh giá tuổi và hạn chế.',
+    'rating.all': 'Tất cả',
+    'rating.desc.all': 'Phù hợp với mọi độ tuổi và nhóm.',
+    'rating.teens': 'Thiếu niên',
+    'rating.desc.teens': 'Phù hợp với khán giả từ 12 đến 18 tuổi.',
+    'rating.mature': 'Người lớn',
+    'rating.desc.mature': 'Chứa những chủ đề tiên tiến, ngôn ngữ thô tục và cảnh nóng ngụ ý.',
+    'rating.explicit': 'Rõ ràng',
+    'rating.desc.explicit': 'Chứa cảnh nóng rõ ràng.',
+    'rating.selection.all': 'Tất cả - Phù hợp với mọi độ tuổi và nhóm.',
+    'rating.selection.teens': 'Thiếu niên (12-18) - Phù hợp với khán giả từ 12 đến 18 tuổi.',
+    'rating.selection.mature': 'Người lớn (18+) - Chứa những chủ đề tiên tiến, ngôn ngữ thô tục và cảnh nóng ngụ ý.',
+    'rating.selection.explicit': 'Rõ ràng (21+) - Chứa cảnh nóng rõ ràng.'
+};
