34ea0b696dabdbe4fd3ea114a1de98c53b3a2e96
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:29:11 +0200

    New translations licenses.js (Vietnamese)

--- vi-VN/licenses.js
+++ vi-VN/licenses.js
@@ -3,31 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'Về giấy phép',
-  'licenses.more': 'Thông tin thêm',
-  'licenses.legal': 'Tài liệu pháp lý',
-  'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
-  'licenses.cc-by':
-    'Giấy phép này cho phép người khác phân phối, remix, chỉnh sửa và xây dựng trên công việc của bạn, kể cả với mục đích thương mại, miễn là họ ghi công bạn về sáng tạo gốc. Đây là giấy phép linh hoạt nhất được cung cấp. Được khuyến nghị để tối đa hóa việc phổ biến và sử dụng tài liệu được cấp phép.',
-  'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
-  'licenses.cc-by-sa':
-    'Giấy phép này cho phép người khác remix, chỉnh sửa và xây dựng trên công việc của bạn, kể cả với mục đích thương mại, miễn là họ ghi công bạn và cấp phép công việc mới của họ theo các điều khoản giống nhau. Giấy phép này thường được so sánh với các giấy phép phần mềm mã nguồn mở và tự do "copyleft". Tất cả các công việc mới dựa trên công việc của bạn sẽ mang cùng một giấy phép, vì vậy bất kỳ phái sinh nào cũng sẽ cho phép việc sử dụng thương mại. Đây là giấy phép được sử dụng bởi Wikipedia và được khuyến nghị cho các tài liệu sẽ được hưởng lợi từ việc kết hợp nội dung từ Wikipedia và các dự án được cấp phép tương tự.',
-  'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
-  'licenses.cc-by-nd':
-    'Giấy phép này cho phép tái phân phối, cả về mục đích thương mại và phi thương mại, miễn là nó được chuyển tiếp mà không thay đổi và toàn bộ, với sự ghi công cho bạn.',
-  'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
-  'licenses.cc-by-nc':
-    'Giấy phép này cho phép người khác remix, chỉnh sửa và xây dựng trên công việc của bạn mà không với mục đích thương mại, và mặc dù công việc mới của họ cũng phải công nhận bạn và không với mục đích thương mại, nhưng họ không cần phải cấp phép công việc phái sinh của họ theo các điều khoản giống nhau.',
-  'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
-  'licenses.cc-by-nc-sa':
-    'Giấy phép này cho phép người khác remix, chỉnh sửa và xây dựng trên công việc của bạn mà không với mục đích thương mại, miễn là họ ghi công bạn và cấp phép công việc mới của họ theo các điều khoản giống nhau.',
-  'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
-  'licenses.cc-by-nc-nd':
-    'Giấy phép này là giấy phép hạn chế nhất trong sáu giấy phép chính của chúng tôi, chỉ cho phép người khác tải xuống công việc của bạn và chia sẻ chúng với người khác miễn là họ ghi công bạn, nhưng họ không thể thay đổi chúng theo bất kỳ cách nào hoặc sử dụng với mục đích thương mại.',
-  'licenses.name.lu-c': 'Bản quyền Vũ trụ Văn học',
-  'licenses.lu-c':
-    'Standard full copyrights license that allows you to sell publish your work as you like and have your rights to it protected. This license also gives you the option to sell your stories on Literary Universe.',
-  'licenses.name.cc0': 'No Rights Reserved - CC0 1.0',
-  'licenses.cc0':
-    'Put your work into public domain. In contrast to CC’s licenses that allow copyright holders to choose from a range of permissions while retaining their copyright, CC0 empowers yet another choice altogether – the choice to opt out of copyright and database protection, and the exclusive rights automatically granted to creators – the “no rights reserved” alternative to our licenses.',
-}
+    'licenses.about': 'Về giấy phép',
+    'licenses.more': 'Thông tin thêm',
+    'licenses.legal': 'Tài liệu pháp lý',
+    'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
+    'licenses.cc-by': 'Giấy phép này cho phép người khác phân phối, remix, chỉnh sửa và xây dựng trên công việc của bạn, kể cả với mục đích thương mại, miễn là họ ghi công bạn về sáng tạo gốc. Đây là giấy phép linh hoạt nhất được cung cấp. Được khuyến nghị để tối đa hóa việc phổ biến và sử dụng tài liệu được cấp phép.',
+    'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
+    'licenses.cc-by-sa': 'Giấy phép này cho phép người khác remix, chỉnh sửa và xây dựng trên công việc của bạn, kể cả với mục đích thương mại, miễn là họ ghi công bạn và cấp phép công việc mới của họ theo các điều khoản giống nhau. Giấy phép này thường được so sánh với các giấy phép phần mềm mã nguồn mở và tự do "copyleft". Tất cả các công việc mới dựa trên công việc của bạn sẽ mang cùng một giấy phép, vì vậy bất kỳ phái sinh nào cũng sẽ cho phép việc sử dụng thương mại. Đây là giấy phép được sử dụng bởi Wikipedia và được khuyến nghị cho các tài liệu sẽ được hưởng lợi từ việc kết hợp nội dung từ Wikipedia và các dự án được cấp phép tương tự.',
+    'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
+    'licenses.cc-by-nd': 'Giấy phép này cho phép tái phân phối, cả về mục đích thương mại và phi thương mại, miễn là nó được chuyển tiếp mà không thay đổi và toàn bộ, với sự ghi công cho bạn.',
+    'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
+    'licenses.cc-by-nc': 'Giấy phép này cho phép người khác remix, chỉnh sửa và xây dựng trên công việc của bạn mà không với mục đích thương mại, và mặc dù công việc mới của họ cũng phải công nhận bạn và không với mục đích thương mại, nhưng họ không cần phải cấp phép công việc phái sinh của họ theo các điều khoản giống nhau.',
+    'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
+    'licenses.cc-by-nc-sa': 'Giấy phép này cho phép người khác remix, chỉnh sửa và xây dựng trên công việc của bạn mà không với mục đích thương mại, miễn là họ ghi công bạn và cấp phép công việc mới của họ theo các điều khoản giống nhau.',
+    'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
+    'licenses.cc-by-nc-nd': 'Giấy phép này là giấy phép hạn chế nhất trong sáu giấy phép chính của chúng tôi, chỉ cho phép người khác tải xuống công việc của bạn và chia sẻ chúng với người khác miễn là họ ghi công bạn, nhưng họ không thể thay đổi chúng theo bất kỳ cách nào hoặc sử dụng với mục đích thương mại.',
+    'licenses.name.lu-c': 'Bản quyền Vũ trụ Văn học',
+    'licenses.lu-c': 'Standard full copyrights license that allows you to sell publish your work as you like and have your rights to it protected. This license also gives you the option to sell your stories on Literary Universe.',
+    'licenses.name.cc0': 'No Rights Reserved - CC0 1.0',
+    'licenses.cc0': 'Put your work into public domain. In contrast to CC’s licenses that allow copyright holders to choose from a range of permissions while retaining their copyright, CC0 empowers yet another choice altogether – the choice to opt out of copyright and database protection, and the exclusive rights automatically granted to creators – the “no rights reserved” alternative to our licenses.'
+};
