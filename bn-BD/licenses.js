cab35d4f174b2023803cbf37980ea1f2e1da12e2
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:32:01 +0200

    New translations licenses.js (Bengali)

--- bn-BD/licenses.js
+++ bn-BD/licenses.js
@@ -3,31 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'About licenses',
-  'licenses.more': 'More information',
-  'licenses.legal': 'Legal document',
-  'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
-  'licenses.cc-by':
-    'This license lets others distribute, remix, tweak, and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of licenses offered. Recommended for maximum dissemination and use of licensed materials.',
-  'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
-  'licenses.cc-by-sa':
-    'This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.',
-  'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
-  'licenses.cc-by-nd':
-    'This license allows for redistribution, commercial and non-commercial, as long as it is passed along unchanged and in whole, with credit to you.',
-  'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
-  'licenses.cc-by-nc':
-    'This license lets others remix, tweak, and build upon your work non-commercially, and although their new works must also acknowledge you and be non-commercial, they don’t have to license their derivative works on the same terms.',
-  'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
-  'licenses.cc-by-nc-sa':
-    'This license lets others remix, tweak, and build upon your work non-commercially, as long as they credit you and license their new creations under the identical terms.',
-  'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
-  'licenses.cc-by-nc-nd':
-    'This license is the most restrictive of our six main licenses, only allowing others to download your works and share them with others as long as they credit you, but they can’t change them in any way or use them commercially.',
-  'licenses.name.lu-c': 'Literary Universe Copyrights',
-  'licenses.lu-c':
-    'এটি একটি স্ট্যান্ডার্ড পূর্ণ কপিরাইট লাইসেন্স যা আপনাকে আপনার কাজগুলি বিক্রি করতে এবং আপনার অধিকারগুলি সুরক্ষিত রাখতে দেয়। এই লাইসেন্স আপনাকে লেখকীয় বিশ্বের উপর আপনার গল্পগুলি বিক্রি করার অপশনও দেয়।',
-  'licenses.name.cc0': 'কোন অধিকার সংরক্ষিত নেই - সিসিজি ১.০',
-  'licenses.cc0':
-    'আপনার কাজটি পাবলিক ডোমেইনে রাখুন। সিসিতে কপিরাইট ধারকদের একটি পরিসংখ্যান বেছে নিতে দেয় যা তাদের কপিরাইট সংরক্ষণ রাখার সময় একটি বিভিন্ন অনুমতির ব্যবস্থা থেকে চয়ন করতে দেয়, যখন স্রষ্টাদের অধিকারগুলি সংরক্ষণ রাখার সাথে সাথে তাদের স্বায়ত্তশাসিত অধিকারগুলি স্বয়ংক্রিয়ভাবে দেওয়া হয়ে থাকে - আমাদের লাইসেন্সগুলির সাথে তুলনায় একটি আরও একটি অপশনের প্রতিস্থাপন করতে দেয় - কপিরাইট এবং ডাটাবেস সুরক্ষা থেকে বাইরে পছন্দের বিকল্প হয়ে থাকা - সৃষ্টিদের স্বয়ংক্রিয়ভাবে দেওয়া অধিকারগুলির সাথে সম্পর্কিত সম্পূর্ণরূপে কোন অধিকার সংরক্ষিত নেই।',
-}
+    'licenses.about': 'About licenses',
+    'licenses.more': 'More information',
+    'licenses.legal': 'Legal document',
+    'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
+    'licenses.cc-by': 'This license lets others distribute, remix, tweak, and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of licenses offered. Recommended for maximum dissemination and use of licensed materials.',
+    'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
+    'licenses.cc-by-sa': 'This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.',
+    'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
+    'licenses.cc-by-nd': 'This license allows for redistribution, commercial and non-commercial, as long as it is passed along unchanged and in whole, with credit to you.',
+    'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
+    'licenses.cc-by-nc': 'This license lets others remix, tweak, and build upon your work non-commercially, and although their new works must also acknowledge you and be non-commercial, they don’t have to license their derivative works on the same terms.',
+    'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
+    'licenses.cc-by-nc-sa': 'This license lets others remix, tweak, and build upon your work non-commercially, as long as they credit you and license their new creations under the identical terms.',
+    'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
+    'licenses.cc-by-nc-nd': 'This license is the most restrictive of our six main licenses, only allowing others to download your works and share them with others as long as they credit you, but they can’t change them in any way or use them commercially.',
+    'licenses.name.lu-c': 'Literary Universe Copyrights',
+    'licenses.lu-c': 'এটি একটি স্ট্যান্ডার্ড পূর্ণ কপিরাইট লাইসেন্স যা আপনাকে আপনার কাজগুলি বিক্রি করতে এবং আপনার অধিকারগুলি সুরক্ষিত রাখতে দেয়। এই লাইসেন্স আপনাকে লেখকীয় বিশ্বের উপর আপনার গল্পগুলি বিক্রি করার অপশনও দেয়।',
+    'licenses.name.cc0': 'কোন অধিকার সংরক্ষিত নেই - সিসিজি ১.০',
+    'licenses.cc0': 'আপনার কাজটি পাবলিক ডোমেইনে রাখুন। সিসিতে কপিরাইট ধারকদের একটি পরিসংখ্যান বেছে নিতে দেয় যা তাদের কপিরাইট সংরক্ষণ রাখার সময় একটি বিভিন্ন অনুমতির ব্যবস্থা থেকে চয়ন করতে দেয়, যখন স্রষ্টাদের অধিকারগুলি সংরক্ষণ রাখার সাথে সাথে তাদের স্বায়ত্তশাসিত অধিকারগুলি স্বয়ংক্রিয়ভাবে দেওয়া হয়ে থাকে - আমাদের লাইসেন্সগুলির সাথে তুলনায় একটি আরও একটি অপশনের প্রতিস্থাপন করতে দেয় - কপিরাইট এবং ডাটাবেস সুরক্ষা থেকে বাইরে পছন্দের বিকল্প হয়ে থাকা - সৃষ্টিদের স্বয়ংক্রিয়ভাবে দেওয়া অধিকারগুলির সাথে সম্পর্কিত সম্পূর্ণরূপে কোন অধিকার সংরক্ষিত নেই।'
+};
