8fb8bc325d202568de0ddd11cd46ff9f9bb018f6
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:27:42 +0200

    New translations legal.js (Turkish)

--- tr-TR/legal.js
+++ tr-TR/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Özür dilerim. Ancak daha ileri gitmeden önce, yasal olarak, Hizmet Şartları, Gizlilik Politikası ve Telif Hakkı Sözleşmemiz hakkında bilgilendirildiğinizden ve kabul ettiğinizden emin olmamız gerekiyor.',
-  'legal.modal.agree': 'Kabul Ediyorum',
-  'legal.modal.disagree': 'Kabul Etmiyorum',
-  'legal.modal.register': 'Kayıt olursanız bunu bir daha görmeyeceksiniz.',
-  'legal.modal.goToSettings': 'Ayarlarınıza git',
-  'legal.modal.oneLastThing': 'Son bir şey...',
-  'legal.modal.finish': 'Tamamla',
-  'legal.modal.existingUser': 'Zaten kayıtlı mısınız?',
-  'legal.modal.pleaseLogin': 'Lütfen giriş yapın.',
-  'legal.publicationAgreements': 'Yayın sözleşmeleri',
-  'legal.generalPublishingAgreement': 'Genel Yayın Sözleşmesi',
-  'legal.documentsFor': '{workTitle}için hukuki belgeler',
-}
+    'legal.modal.intro': 'Özür dilerim. Ancak daha ileri gitmeden önce, yasal olarak, Hizmet Şartları, Gizlilik Politikası ve Telif Hakkı Sözleşmemiz hakkında bilgilendirildiğinizden ve kabul ettiğinizden emin olmamız gerekiyor.',
+    'legal.modal.agree': 'Kabul Ediyorum',
+    'legal.modal.disagree': 'Kabul Etmiyorum',
+    'legal.modal.register': 'Kayıt olursanız bunu bir daha görmeyeceksiniz.',
+    'legal.modal.goToSettings': 'Ayarlarınıza git',
+    'legal.modal.oneLastThing': 'Son bir şey...',
+    'legal.modal.finish': 'Tamamla',
+    'legal.modal.existingUser': 'Zaten kayıtlı mısınız?',
+    'legal.modal.pleaseLogin': 'Lütfen giriş yapın.',
+    'legal.publicationAgreements': 'Yayın sözleşmeleri',
+    'legal.generalPublishingAgreement': 'Genel Yayın Sözleşmesi',
+    'legal.documentsFor': '{workTitle}için hukuki belgeler'
+};
