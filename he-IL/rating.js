82571381d39d17f9cf207780f085fb365709827d
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:19:13 +0200

    New translations rating.js (Hebrew)

--- he-IL/rating.js
+++ he-IL/rating.js
@@ -2,18 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'דירוג',
-  'rating.explained': 'דירוג גיל והגבלה.',
-  'rating.all': 'הכל',
-  'rating.desc.all': 'נגיש לכל הגילאים והקבוצות.',
-  'rating.teens': 'נוער',
-  'rating.desc.teens': 'מתאים לקהל בין גילאי 12 ל-18.',
-  'rating.mature': 'מבוגרים',
-  'rating.desc.mature': 'מכיל נושאים מתקדמים, שפה גסה וסצינות מרומזות.',
-  'rating.explicit': 'מפורט',
-  'rating.desc.explicit': 'מכיל סצינות מפורטות.',
-  'rating.selection.all': 'הכל - נגיש לכל הגילאים והקבוצות.',
-  'rating.selection.teens': 'נוער (12-18) - מתאים לקהל בין גילאי 12 ל-18.',
-  'rating.selection.mature': 'מבוגרים (18+) - מכיל נושאים מתקדמים, שפה גסה וסצינות מרומזות.',
-  'rating.selection.explicit': 'מפורט (21+) - מכיל סצינות מפורטות.',
-}
+    'rating.name': 'דירוג',
+    'rating.explained': 'דירוג גיל והגבלה.',
+    'rating.all': 'הכל',
+    'rating.desc.all': 'נגיש לכל הגילאים והקבוצות.',
+    'rating.teens': 'נוער',
+    'rating.desc.teens': 'מתאים לקהל בין גילאי 12 ל-18.',
+    'rating.mature': 'מבוגרים',
+    'rating.desc.mature': 'מכיל נושאים מתקדמים, שפה גסה וסצינות מרומזות.',
+    'rating.explicit': 'מפורט',
+    'rating.desc.explicit': 'מכיל סצינות מפורטות.',
+    'rating.selection.all': 'הכל - נגיש לכל הגילאים והקבוצות.',
+    'rating.selection.teens': 'נוער (12-18) - מתאים לקהל בין גילאי 12 ל-18.',
+    'rating.selection.mature': 'מבוגרים (18+) - מכיל נושאים מתקדמים, שפה גסה וסצינות מרומזות.',
+    'rating.selection.explicit': 'מפורט (21+) - מכיל סצינות מפורטות.'
+};
