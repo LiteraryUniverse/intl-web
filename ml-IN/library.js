7af97be7c9c5eb4ac6da5fdf7f86b0fb8f616933
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:37:47 +0200

    New translations library.js (Malayalam)

--- ml-IN/library.js
+++ ml-IN/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'ലൈബ്രറിയിലേക്ക് ചേരുക',
-  'library.remove': 'ലൈബ്രറിയിൽ നിന്ന് നീക്കം ചെയ്യുക',
-  'library.bought': 'വാങ്ങിയ കഥകൾ',
-  'library.stories': 'പിന്തുടരുന്ന കഥകൾ',
-  'library.universes': 'പിന്തുടരുന്ന മാനദണ്ഡങ്ങൾ',
-  'bookmark.continueReading': 'വായിക്കുകയും തുടരുകയും',
-  'bookmark.statusSummary': 'നിങ്ങൾ {readPercent, number, ::percent} കഥയെ വായിച്ചുകൊണ്ടിരിക്കുന്നു.',
-}
+    'library.add': 'ലൈബ്രറിയിലേക്ക് ചേരുക',
+    'library.remove': 'ലൈബ്രറിയിൽ നിന്ന് നീക്കം ചെയ്യുക',
+    'library.bought': 'വാങ്ങിയ കഥകൾ',
+    'library.stories': 'പിന്തുടരുന്ന കഥകൾ',
+    'library.universes': 'പിന്തുടരുന്ന മാനദണ്ഡങ്ങൾ',
+    'bookmark.continueReading': 'വായിക്കുകയും തുടരുകയും',
+    'bookmark.statusSummary': 'നിങ്ങൾ {readPercent, number, ::percent} കഥയെ വായിച്ചുകൊണ്ടിരിക്കുന്നു.'
+};
