93fcf9b279bea67badfd100d9028f9b52795d76b
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:14:37 +0200

    New translations rating.js (Catalan)

--- ca-ES/rating.js
+++ ca-ES/rating.js
@@ -2,19 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Classificació',
-  'rating.explained': "Classificació d'edat i restricció.",
-  'rating.all': 'Tots',
-  'rating.desc.all': 'Accessible per a totes les edats i grups.',
-  'rating.teens': 'Adolescents',
-  'rating.desc.teens': 'Adecuat per a una audiència entre 12 i 18 anys.',
-  'rating.mature': 'Adults',
-  'rating.desc.mature': 'Conté temes avançats, llenguatge cru i escenes implícites.',
-  'rating.explicit': 'Explícit',
-  'rating.desc.explicit': 'Conté escenes explícites.',
-  'rating.selection.all': 'Tots - Accessible per a totes les edats i grups.',
-  'rating.selection.teens': 'Adolescents (12-18) - Adecuat per a una audiència entre 12 i 18 anys.',
-  'rating.selection.mature':
-    'Adults (18+) - Conté temes avançats, llenguatge cru i escenes implícites.',
-  'rating.selection.explicit': 'Explícit (21+) - Conté escenes explícites.',
-}
+    'rating.name': 'Classificació',
+    'rating.explained': 'Classificació d\'edat i restricció.',
+    'rating.all': 'Tots',
+    'rating.desc.all': 'Accessible per a totes les edats i grups.',
+    'rating.teens': 'Adolescents',
+    'rating.desc.teens': 'Adecuat per a una audiència entre 12 i 18 anys.',
+    'rating.mature': 'Adults',
+    'rating.desc.mature': 'Conté temes avançats, llenguatge cru i escenes implícites.',
+    'rating.explicit': 'Explícit',
+    'rating.desc.explicit': 'Conté escenes explícites.',
+    'rating.selection.all': 'Tots - Accessible per a totes les edats i grups.',
+    'rating.selection.teens': 'Adolescents (12-18) - Adecuat per a una audiència entre 12 i 18 anys.',
+    'rating.selection.mature': 'Adults (18+) - Conté temes avançats, llenguatge cru i escenes implícites.',
+    'rating.selection.explicit': 'Explícit (21+) - Conté escenes explícites.'
+};
