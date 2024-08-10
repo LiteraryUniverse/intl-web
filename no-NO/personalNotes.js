c7e88ac1344a15c8f4f767cd6e13c302cdc3d148
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:23:57 +0200

    New translations personalnotes.js (Norwegian)

--- no-NO/personalNotes.js
+++ no-NO/personalNotes.js
@@ -22,5 +22,5 @@ export default {
   'personalNotes.type.encEntry': 'Leksikonoppføring',
   'personalNotes.type.revTextBlock': 'Revisjonstekstblokk',
   'personalNotes.chapter': 'Kapittel',
-  'personalNotes.recentSearches': 'Nylige søk:',
-}
+  'personalNotes.recentSearches': 'Nylige søk:'
+};
