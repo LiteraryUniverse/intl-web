3ec49e664daa6b322f0e99ebdb934191f2d19f00
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:31:04 +0200

    New translations legal.js (Khmer)

--- km-KH/legal.js
+++ km-KH/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'សូមទោសប្រយ័ត្ន។ មុនពេលយើងអនុញ្ញាតឱ្យអ្នកបន្តទៅមកបន្តផ្សាយបន្តិចបន្តឹមការប្រើប្រាស់របស់យើង យើងត្រូវការឲ្យអ្នកដឹងពី និងយល់ពីគោលនយោបាយរបស់យើង ដូច្នេះអ្នកគួរតែយល់ពីនិងយល់ពីការប្រើប្រាស់របស់យើងដើម្បីបន្តផ្សាយបន្តិចបន្តឹមការប្រើប្រាស់របស់យើង។',
-  'legal.modal.agree': 'យល់ពីនេះ',
-  'legal.modal.disagree': 'មិនយល់ពីនេះ',
-  'legal.modal.register': 'អ្នកនឹងមិនឃើញវា​ទេទើបអ្នកចុះឈ្មោះរួចហើយ។',
-  'legal.modal.goToSettings': 'ទៅកាន់ការកំណត់របស់អ្នក',
-  'legal.modal.oneLastThing': 'មួយផ្សេងទៀតទេ...',
-  'legal.modal.finish': 'បញ្ចប់',
-  'legal.modal.existingUser': 'បានចុះឈ្មោះរួចហើយមែនទេ?',
-  'legal.modal.pleaseLogin': 'សូមចូលប្រើប្រាស់។',
-  'legal.publicationAgreements': 'កិច្ចសន្យាប្រកាសបោះពុម្ព',
-  'legal.generalPublishingAgreement': 'កិច្ចសន្យាប្រកាសផ្សេងៗរបស់ការបោះពុម្ពទូទៅ',
-  'legal.documentsFor': 'ឯកសារព្រឹត្តិការណ៍ស្ថានភាពសិទ្ធិសង្គមសម្រាប់ {workTitle}',
-}
+    'legal.modal.intro': 'សូមទោសប្រយ័ត្ន។ មុនពេលយើងអនុញ្ញាតឱ្យអ្នកបន្តទៅមកបន្តផ្សាយបន្តិចបន្តឹមការប្រើប្រាស់របស់យើង យើងត្រូវការឲ្យអ្នកដឹងពី និងយល់ពីគោលនយោបាយរបស់យើង ដូច្នេះអ្នកគួរតែយល់ពីនិងយល់ពីការប្រើប្រាស់របស់យើងដើម្បីបន្តផ្សាយបន្តិចបន្តឹមការប្រើប្រាស់របស់យើង។',
+    'legal.modal.agree': 'យល់ពីនេះ',
+    'legal.modal.disagree': 'មិនយល់ពីនេះ',
+    'legal.modal.register': 'អ្នកនឹងមិនឃើញវា​ទេទើបអ្នកចុះឈ្មោះរួចហើយ។',
+    'legal.modal.goToSettings': 'ទៅកាន់ការកំណត់របស់អ្នក',
+    'legal.modal.oneLastThing': 'មួយផ្សេងទៀតទេ...',
+    'legal.modal.finish': 'បញ្ចប់',
+    'legal.modal.existingUser': 'បានចុះឈ្មោះរួចហើយមែនទេ?',
+    'legal.modal.pleaseLogin': 'សូមចូលប្រើប្រាស់។',
+    'legal.publicationAgreements': 'កិច្ចសន្យាប្រកាសបោះពុម្ព',
+    'legal.generalPublishingAgreement': 'កិច្ចសន្យាប្រកាសផ្សេងៗរបស់ការបោះពុម្ពទូទៅ',
+    'legal.documentsFor': 'ឯកសារព្រឹត្តិការណ៍ស្ថានភាពសិទ្ធិសង្គមសម្រាប់ {workTitle}'
+};
