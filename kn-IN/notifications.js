0338b78301bc2e741492c98e83adb932e7dac439
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:39:00 +0200

    New translations notifications.js (Kannada)

--- kn-IN/notifications.js
+++ kn-IN/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'ಈ ಸುದ್ದಿಯನ್ನು ಪ್ರದರ್ಶಿಸಲು ಆರಂಭಿಸುವುದು',
   'flashnews.endsAt': 'ಈ ಸುದ್ದಿಯನ್ನು ಪ್ರದರ್ಶಿಸುವುದು ನಿಲ್ಲುವುದು',
   'flashnews.newLanguage': 'ಹೊಸ ಭಾಷೆಯನ್ನು ಆಯ್ಕೆಮಾಡಲು',
-  'flashnews.onlyDisplayOn':
-    'ಸುದ್ದಿಗೆ ಮಾತ್ರ ಕೆಳಗೆ ಆಯ್ಕೆಮಾಡಿದ ಭಾಷೆಯ ಮೇಲೆ (ಯಾವುದೇ ಆಯ್ಕೆಯಾಗಲಿ), ಉಳಿದ ಭಾಷೆಗಳಲ್ಲಿ ಯಾವುದೇ ಸುದ್ದಿಯೂ ತೋರಿಯಾಗುವುದಿಲ್ಲ.',
+  'flashnews.onlyDisplayOn': 'ಸುದ್ದಿಗೆ ಮಾತ್ರ ಕೆಳಗೆ ಆಯ್ಕೆಮಾಡಿದ ಭಾಷೆಯ ಮೇಲೆ (ಯಾವುದೇ ಆಯ್ಕೆಯಾಗಲಿ), ಉಳಿದ ಭಾಷೆಗಳಲ್ಲಿ ಯಾವುದೇ ಸುದ್ದಿಯೂ ತೋರಿಯಾಗುವುದಿಲ್ಲ.',
   'notifications.menu.all': 'ಎಲ್ಲಾ ಅಧಿಸೂಚನೆ',
   'notifications.menu.following': 'ಅನುಸರಣೆ',
   'notifications.menu.workshop': 'ಕಾರ್ಯಾಗಾರ',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'ಓದಿದೆ ಎಂದು ಗುರುತಿಸಿ',
   'notifications.menu.delete': 'ಅಧಿಸೂಚನೆಯನ್ನು ಅಳಿಸಿ',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'ನಿಷ್ಕ್ರಮಣ ಸ್ಥಿತಿಗೆ ಆಯ್ಕೆಮಾಡಲಾಗದ ನಿಮ್ಮ ಕಥೆ {storyName} ಆಫ್ಟರ್ 6 ತಿಂಗಳಾದ ನಿಷ್ಕ್ರಮಣ.',
-  'notification.workshopStoryStatus.toCancelled':
-    'ನಿಷ್ಕ್ರಮಣ ಸ್ಥಿತಿಗೆ ಆಯ್ಕೆಮಾಡಲಾಗದ ನಿಮ್ಮ ಕಥೆ {storyName} ಆಫ್ಟರ್ 1 ವರ್ಷವಾದ ನಿಷ್ಕ್ರಮಣ.',
+  'notification.workshopStoryStatus.toHiatus': 'ನಿಷ್ಕ್ರಮಣ ಸ್ಥಿತಿಗೆ ಆಯ್ಕೆಮಾಡಲಾಗದ ನಿಮ್ಮ ಕಥೆ {storyName} ಆಫ್ಟರ್ 6 ತಿಂಗಳಾದ ನಿಷ್ಕ್ರಮಣ.',
+  'notification.workshopStoryStatus.toCancelled': 'ನಿಷ್ಕ್ರಮಣ ಸ್ಥಿತಿಗೆ ಆಯ್ಕೆಮಾಡಲಾಗದ ನಿಮ್ಮ ಕಥೆ {storyName} ಆಫ್ಟರ್ 1 ವರ್ಷವಾದ ನಿಷ್ಕ್ರಮಣ.',
   'notification.followingStoryStatus.toHiatus': '{storyName} ನಿಷ್ಕ್ರಮಣಗೊಂಡಿದೆ.',
   'notification.followingStoryStatus.toCancelled': '{storyName} ರದ್ದಾಗಿದೆ.',
   'notification.followingNewChapter.new': '{storyName} ನ ಹೊಸ ಅಧ್ಯಾಯವು ಪ್ರಕಟವಾಯಿತು!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'ನೀವು ಸ್ನೇಹ ಕೋರಿದಿದ್ದೀರಿ.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'ಕಥೆಗೆ ಹೋಗಿ',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'ಕಥೆಗೆ ಹೋಗಿ'
+};
