fd5cde5c1ba819c602ad0abf76319bf8f9c350ab
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:23:46 +0200

    New translations legal.js (Norwegian)

--- no-NO/legal.js
+++ no-NO/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Beklager for avbruddet. Men før vi lar deg fortsette, er vi juridisk forpliktet til å forsikre oss om at du er klar over og samtykker i våre Vilkår for bruk, Personvernerklæring og Opphavsrettavtale.',
-  'legal.modal.agree': 'Jeg godtar',
-  'legal.modal.disagree': 'Jeg godtar ikke',
-  'legal.modal.register': 'Du vil ikke se dette igjen hvis du registrerer deg.',
-  'legal.modal.goToSettings': 'Gå til innstillingene dine',
-  'legal.modal.oneLastThing': 'Én siste ting...',
-  'legal.modal.finish': 'Fullfør',
-  'legal.modal.existingUser': 'Allerede registrert?',
-  'legal.modal.pleaseLogin': 'Vennligst logg inn.',
-  'legal.publicationAgreements': 'Publiseringsavtaler',
-  'legal.generalPublishingAgreement': 'Generell utgivelsesavtale',
-  'legal.documentsFor': 'Juridiske dokumenter for {workTitle}',
-}
+    'legal.modal.intro': 'Beklager for avbruddet. Men før vi lar deg fortsette, er vi juridisk forpliktet til å forsikre oss om at du er klar over og samtykker i våre Vilkår for bruk, Personvernerklæring og Opphavsrettavtale.',
+    'legal.modal.agree': 'Jeg godtar',
+    'legal.modal.disagree': 'Jeg godtar ikke',
+    'legal.modal.register': 'Du vil ikke se dette igjen hvis du registrerer deg.',
+    'legal.modal.goToSettings': 'Gå til innstillingene dine',
+    'legal.modal.oneLastThing': 'Én siste ting...',
+    'legal.modal.finish': 'Fullfør',
+    'legal.modal.existingUser': 'Allerede registrert?',
+    'legal.modal.pleaseLogin': 'Vennligst logg inn.',
+    'legal.publicationAgreements': 'Publiseringsavtaler',
+    'legal.generalPublishingAgreement': 'Generell utgivelsesavtale',
+    'legal.documentsFor': 'Juridiske dokumenter for {workTitle}'
+};
