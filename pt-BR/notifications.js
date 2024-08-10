8cf02b1ee24fd73981acdcdfdaba04a0b0d11b0b
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:30:01 +0200

    New translations notifications.js (Portuguese, Brazilian)

--- pt-BR/notifications.js
+++ pt-BR/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'Começar a exibir esta mensagem em',
   'flashnews.endsAt': 'Parar de exibir esta mensagem em',
   'flashnews.newLanguage': 'Selecionar novo idioma para adicionar',
-  'flashnews.onlyDisplayOn':
-    'As notícias serão exibidas apenas nos idiomas selecionados abaixo (se houver algum selecionado), não haverá notícias exibidas, mesmo no idioma padrão, em outros idiomas.',
+  'flashnews.onlyDisplayOn': 'As notícias serão exibidas apenas nos idiomas selecionados abaixo (se houver algum selecionado), não haverá notícias exibidas, mesmo no idioma padrão, em outros idiomas.',
   'notifications.menu.all': 'Todas as notificações',
   'notifications.menu.following': 'Seguindo',
   'notifications.menu.workshop': 'Workshop',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'Marcar como lido',
   'notifications.menu.delete': 'Excluir notificação',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'Sua história {storyName} foi automaticamente pausada após 6 meses de inatividade.',
-  'notification.workshopStoryStatus.toCancelled':
-    'Sua história {storyName} foi automaticamente cancelada após um ano de inatividade.',
+  'notification.workshopStoryStatus.toHiatus': 'Sua história {storyName} foi automaticamente pausada após 6 meses de inatividade.',
+  'notification.workshopStoryStatus.toCancelled': 'Sua história {storyName} foi automaticamente cancelada após um ano de inatividade.',
   'notification.followingStoryStatus.toHiatus': '{storyName} está em pausa.',
   'notification.followingStoryStatus.toCancelled': '{storyName} foi cancelada.',
   'notification.followingNewChapter.new': 'Novo capítulo em {storyName} foi publicado!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'enviou um pedido de amizade.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Ir para a história',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'Ir para a história'
+};
