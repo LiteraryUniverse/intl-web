ec89da70e173300d4fa68aa957626f00d6bd9df9
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:31:05 +0200

    New translations library.js (Khmer)

--- km-KH/library.js
+++ km-KH/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'បន្ថែម​ទៅ​ក្រុមបណ្ដុំ',
-  'library.remove': 'យកចេញ​ពី​ក្រុមបណ្ដុំ',
-  'library.bought': 'របៀប​បញ្ជាទិញ​របស់​របរ',
-  'library.stories': 'របៀប​តាមដាន​របរ',
-  'library.universes': 'ការតាមដានជីវិតរបស់ជនជាតិ',
-  'bookmark.continueReading': 'បន្តទៅអានបន្តទៅអាន',
-  'bookmark.statusSummary': 'អ្នកបានអានរួចហើយ {readPercent, number, ::percent} នៃរឿងនេះ។',
-}
+    'library.add': 'បន្ថែម​ទៅ​ក្រុមបណ្ដុំ',
+    'library.remove': 'យកចេញ​ពី​ក្រុមបណ្ដុំ',
+    'library.bought': 'របៀប​បញ្ជាទិញ​របស់​របរ',
+    'library.stories': 'របៀប​តាមដាន​របរ',
+    'library.universes': 'ការតាមដានជីវិតរបស់ជនជាតិ',
+    'bookmark.continueReading': 'បន្តទៅអានបន្តទៅអាន',
+    'bookmark.statusSummary': 'អ្នកបានអានរួចហើយ {readPercent, number, ::percent} នៃរឿងនេះ។'
+};
