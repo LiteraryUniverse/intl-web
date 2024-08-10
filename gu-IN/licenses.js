9a33c83c3077f912dfa4771cd9fa98162fde21f7
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:18:35 +0200

    New translations licenses.js (Gujarati)

--- gu-IN/licenses.js
+++ gu-IN/licenses.js
@@ -3,31 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'About licenses',
-  'licenses.more': 'More information',
-  'licenses.legal': 'Legal document',
-  'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
-  'licenses.cc-by':
-    'This license lets others distribute, remix, tweak, and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of licenses offered. Recommended for maximum dissemination and use of licensed materials.',
-  'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
-  'licenses.cc-by-sa':
-    'This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.',
-  'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
-  'licenses.cc-by-nd':
-    'This license allows for redistribution, commercial and non-commercial, as long as it is passed along unchanged and in whole, with credit to you.',
-  'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
-  'licenses.cc-by-nc':
-    'This license lets others remix, tweak, and build upon your work non-commercially, and although their new works must also acknowledge you and be non-commercial, they don’t have to license their derivative works on the same terms.',
-  'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
-  'licenses.cc-by-nc-sa':
-    'This license lets others remix, tweak, and build upon your work non-commercially, as long as they credit you and license their new creations under the identical terms.',
-  'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
-  'licenses.cc-by-nc-nd':
-    'This license is the most restrictive of our six main licenses, only allowing others to download your works and share them with others as long as they credit you, but they can’t change them in any way or use them commercially.',
-  'licenses.name.lu-c': 'Literary Universe Copyrights',
-  'licenses.lu-c':
-    'સ્ટાન્ડર્ડ પૂર્ણ કૉપિરાઇટ લાયસન્સ જે તમને તમારું કામ વેચવા અને પ્રકાશિત કરવાની મંજૂરી આપે છે અને તમારા અધિકારોને સંરક્ષિત રાખે છે. આ લાયસન્સ તમને લિટરેરી યુનિવર્સમાં તમારી કથાઓને વેચવાની વિકલ્પની પણ આપે છે.',
-  'licenses.name.cc0': 'કોઈ હક સંરક્ષિત નથી - CC0 1.0',
-  'licenses.cc0':
-    'તમારું કામ સાર્વજનિક ડોમેનમાં મૂકો. CC ના લાયસન્સોથી વાપરતા કૉપિરાઇટ ધારકોને તેમજ તેમની કૉપિરાઇટ સાથે જુદાજુદા પરવાનગીઓને પસંદ કરવાની મંજૂરી આપે છે, CC0 કૉપિરાઇટ અને ડેટાબેઝ સંરક્ષણની પસંદગીને અવશ્યકતાને બાદ છોડી આપે છે અને સર્જકોને આપેલા અનન્ય હકો - "કોઈ હક સંરક્ષિત નથી" આ આપતું વિકલ્પ આપે છે.',
-}
+    'licenses.about': 'About licenses',
+    'licenses.more': 'More information',
+    'licenses.legal': 'Legal document',
+    'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
+    'licenses.cc-by': 'This license lets others distribute, remix, tweak, and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of licenses offered. Recommended for maximum dissemination and use of licensed materials.',
+    'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
+    'licenses.cc-by-sa': 'This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.',
+    'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
+    'licenses.cc-by-nd': 'This license allows for redistribution, commercial and non-commercial, as long as it is passed along unchanged and in whole, with credit to you.',
+    'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
+    'licenses.cc-by-nc': 'This license lets others remix, tweak, and build upon your work non-commercially, and although their new works must also acknowledge you and be non-commercial, they don’t have to license their derivative works on the same terms.',
+    'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
+    'licenses.cc-by-nc-sa': 'This license lets others remix, tweak, and build upon your work non-commercially, as long as they credit you and license their new creations under the identical terms.',
+    'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
+    'licenses.cc-by-nc-nd': 'This license is the most restrictive of our six main licenses, only allowing others to download your works and share them with others as long as they credit you, but they can’t change them in any way or use them commercially.',
+    'licenses.name.lu-c': 'Literary Universe Copyrights',
+    'licenses.lu-c': 'સ્ટાન્ડર્ડ પૂર્ણ કૉપિરાઇટ લાયસન્સ જે તમને તમારું કામ વેચવા અને પ્રકાશિત કરવાની મંજૂરી આપે છે અને તમારા અધિકારોને સંરક્ષિત રાખે છે. આ લાયસન્સ તમને લિટરેરી યુનિવર્સમાં તમારી કથાઓને વેચવાની વિકલ્પની પણ આપે છે.',
+    'licenses.name.cc0': 'કોઈ હક સંરક્ષિત નથી - CC0 1.0',
+    'licenses.cc0': 'તમારું કામ સાર્વજનિક ડોમેનમાં મૂકો. CC ના લાયસન્સોથી વાપરતા કૉપિરાઇટ ધારકોને તેમજ તેમની કૉપિરાઇટ સાથે જુદાજુદા પરવાનગીઓને પસંદ કરવાની મંજૂરી આપે છે, CC0 કૉપિરાઇટ અને ડેટાબેઝ સંરક્ષણની પસંદગીને અવશ્યકતાને બાદ છોડી આપે છે અને સર્જકોને આપેલા અનન્ય હકો - "કોઈ હક સંરક્ષિત નથી" આ આપતું વિકલ્પ આપે છે.'
+};
