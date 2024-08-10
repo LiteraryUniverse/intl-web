a954376e8dd4aa8c0bb4741a8747085582ec2dab
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:31:37 +0200

    New translations comments.js (Tamil)

--- ta-IN/comments.js
+++ ta-IN/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': 'உங்கள் கருத்தை எழுதுங்கள்',
-  'comments.none': 'மன்னிக்கவும், கருத்துக்கள் காணப்படவில்லை.',
-  'comments.total': `There {count, plural,
+    'comment.write': 'உங்கள் கருத்தை எழுதுங்கள்',
+    'comments.none': 'மன்னிக்கவும், கருத்துக்கள் காணப்படவில்லை.',
+    'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-  'comments.show.older': 'பழைய கருத்துக்களைக் காட்டு',
-}
+    'comments.show.older': 'பழைய கருத்துக்களைக் காட்டு'
+};
