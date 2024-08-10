168560a4e0ababc23981701255d13a60c21c47ea
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:29:49 +0200

    New translations rating.js (Portuguese, Brazilian)

--- pt-BR/rating.js
+++ pt-BR/rating.js
@@ -2,20 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Classificação',
-  'rating.explained': 'Classificação por idade e restrição.',
-  'rating.all': 'Todas',
-  'rating.desc.all': 'Acessível a todas as idades e grupos.',
-  'rating.teens': 'Adolescentes',
-  'rating.desc.teens': 'Adequado para um público entre os 12 e os 18 anos.',
-  'rating.mature': 'Adulto',
-  'rating.desc.mature': 'Contém temas avançados, linguagem grosseira e cenas implícitas.',
-  'rating.explicit': 'Explícito',
-  'rating.desc.explicit': 'Contém cenas explícitas.',
-  'rating.selection.all': 'Todos - Acessível a todas as idades e grupos.',
-  'rating.selection.teens':
-    'Adolescentes (12-18) - Adequado para o público entre os 12 e os 18 anos.',
-  'rating.selection.mature':
-    'Adulto (18+) - Contém temas avançados, linguagem grosseira e cenas implícitas.',
-  'rating.selection.explicit': 'Explícito (21+) - Contém cenas explícitas.',
-}
+    'rating.name': 'Classificação',
+    'rating.explained': 'Classificação por idade e restrição.',
+    'rating.all': 'Todas',
+    'rating.desc.all': 'Acessível a todas as idades e grupos.',
+    'rating.teens': 'Adolescentes',
+    'rating.desc.teens': 'Adequado para um público entre os 12 e os 18 anos.',
+    'rating.mature': 'Adulto',
+    'rating.desc.mature': 'Contém temas avançados, linguagem grosseira e cenas implícitas.',
+    'rating.explicit': 'Explícito',
+    'rating.desc.explicit': 'Contém cenas explícitas.',
+    'rating.selection.all': 'Todos - Acessível a todas as idades e grupos.',
+    'rating.selection.teens': 'Adolescentes (12-18) - Adequado para o público entre os 12 e os 18 anos.',
+    'rating.selection.mature': 'Adulto (18+) - Contém temas avançados, linguagem grosseira e cenas implícitas.',
+    'rating.selection.explicit': 'Explícito (21+) - Contém cenas explícitas.'
+};
