e7ab9494a084885970e3b43be6ff5768e907b482
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:19:02 +0200

    New translations enrollment.js (Hebrew)

--- he-IL/enrollment.js
+++ he-IL/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'טען את החשבון שלך!',
-  'enrollment.description':
-    'ברוך הבא {username}! כדי לקבל גישה לחשבון המיוחד שלך, יש רק ליצור סיסמה.',
-  'enrollment.submitting': 'מבצע טעינת החשבון שלך...',
-  'enrollment.submit': 'טען את החשבון שלך!',
-}
+    'enrollment.title': 'טען את החשבון שלך!',
+    'enrollment.description': 'ברוך הבא {username}! כדי לקבל גישה לחשבון המיוחד שלך, יש רק ליצור סיסמה.',
+    'enrollment.submitting': 'מבצע טעינת החשבון שלך...',
+    'enrollment.submit': 'טען את החשבון שלך!'
+};
