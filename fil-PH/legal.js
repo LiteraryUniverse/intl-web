32d48fae91aff59dd85fd1bd8cc029a43a39b470
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:36:40 +0200

    New translations legal.js (Filipino)

--- fil-PH/legal.js
+++ fil-PH/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Paumanhin sa abala. Ngunit bago namin kayo pahintulutan na magpatuloy, kami ay legal na obligado na tiyakin na alam ninyo at sumasang-ayon kayo sa aming Mga Tuntunin ng Serbisyo, Patakaran sa Pagkapribado, at Kasunduan sa Karapatang-Sipi.',
-  'legal.modal.agree': 'Sumasang-ayon ako',
-  'legal.modal.disagree': 'Hindi ako sumasang-ayon',
-  'legal.modal.register': 'Hindi na ninyo makikita ito kung kayo ay magrehistro.',
-  'legal.modal.goToSettings': 'Pumunta sa inyong mga setting',
-  'legal.modal.oneLastThing': 'Isa pang bagay...',
-  'legal.modal.finish': 'Tapos na',
-  'legal.modal.existingUser': 'Nakarehistro na ba kayo?',
-  'legal.modal.pleaseLogin': 'Mag-login po kayo.',
-  'legal.publicationAgreements': 'Mga Kasunduan sa Paglilimbag',
-  'legal.generalPublishingAgreement': 'Pangkalahatang Kasunduan sa Paglilimbag',
-  'legal.documentsFor': 'Legal documents para sa {workTitle}',
-}
+    'legal.modal.intro': 'Paumanhin sa abala. Ngunit bago namin kayo pahintulutan na magpatuloy, kami ay legal na obligado na tiyakin na alam ninyo at sumasang-ayon kayo sa aming Mga Tuntunin ng Serbisyo, Patakaran sa Pagkapribado, at Kasunduan sa Karapatang-Sipi.',
+    'legal.modal.agree': 'Sumasang-ayon ako',
+    'legal.modal.disagree': 'Hindi ako sumasang-ayon',
+    'legal.modal.register': 'Hindi na ninyo makikita ito kung kayo ay magrehistro.',
+    'legal.modal.goToSettings': 'Pumunta sa inyong mga setting',
+    'legal.modal.oneLastThing': 'Isa pang bagay...',
+    'legal.modal.finish': 'Tapos na',
+    'legal.modal.existingUser': 'Nakarehistro na ba kayo?',
+    'legal.modal.pleaseLogin': 'Mag-login po kayo.',
+    'legal.publicationAgreements': 'Mga Kasunduan sa Paglilimbag',
+    'legal.generalPublishingAgreement': 'Pangkalahatang Kasunduan sa Paglilimbag',
+    'legal.documentsFor': 'Legal documents para sa {workTitle}'
+};
