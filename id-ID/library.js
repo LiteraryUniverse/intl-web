829be71ad11f9d98827860ee009410ba9c75dbc5
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:30:30 +0200

    New translations library.js (Indonesian)

--- id-ID/library.js
+++ id-ID/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'Tambahkan ke perpustakaan',
-  'library.remove': 'Hapus dari perpustakaan',
-  'library.bought': 'Cerita yang dibeli',
-  'library.stories': 'Cerita yang diikuti',
-  'library.universes': 'Diikuti oleh alam semesta',
-  'bookmark.continueReading': 'Lanjutkan membaca',
-  'bookmark.statusSummary': 'Anda telah membaca {readPercent, number, ::percent} dari cerita.',
-}
+    'library.add': 'Tambahkan ke perpustakaan',
+    'library.remove': 'Hapus dari perpustakaan',
+    'library.bought': 'Cerita yang dibeli',
+    'library.stories': 'Cerita yang diikuti',
+    'library.universes': 'Diikuti oleh alam semesta',
+    'bookmark.continueReading': 'Lanjutkan membaca',
+    'bookmark.statusSummary': 'Anda telah membaca {readPercent, number, ::percent} dari cerita.'
+};
