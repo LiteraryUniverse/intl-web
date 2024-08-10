c688f6ccbb9d67e0ebeb74f0a9d0baeacb76b343
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:41:25 +0200

    New translations licenses.js (Romanian)

--- ro-RO/licenses.js
+++ ro-RO/licenses.js
@@ -3,32 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'Despre licențe',
-  'licenses.more': 'Mai multe informații',
-  'licenses.legal': 'Document legal',
-  'licenses.name.cc-by': 'Creative Commons Atribuire 4.0',
-  'licenses.cc-by':
-    'Această licență permite altora să distribuie, să remodeleze, să ajusteze și să construiască pe baza lucrării dvs., chiar și în scopuri comerciale, atât timp cât vă creditează pentru creația originală. Aceasta este cea mai permisivă dintre licențele oferite. Recomandată pentru o diseminare maximă și utilizarea materialelor licențiate.',
-  'licenses.name.cc-by-sa': 'Creative Commons Atribuire-Distribuire în condiții identice 4.0',
-  'licenses.cc-by-sa':
-    'Această licență permite altora să remodeleze, să ajusteze și să construiască pe baza lucrării dvs., chiar și în scopuri comerciale, atât timp cât vă creditează și licențiază noile creații sub aceleași termeni. Această licență este adesea comparată cu licențele de software „copyleft” gratuite și open source. Toate lucrările noi bazate pe ale dvs. vor purta aceeași licență, astfel încât orice derivate vor permite și utilizarea comercială. Aceasta este licența utilizată de Wikipedia și este recomandată pentru materialele care ar beneficia de includerea de conținut din Wikipedia și proiecte similare cu licențe.',
-  'licenses.name.cc-by-nd': 'Creative Commons Atribuire-FărăDerivate 4.0',
-  'licenses.cc-by-nd':
-    'Această licență permite redistribuirea, atât în scopuri comerciale, cât și non-comerciale, atâta timp cât este transmisă nemodificată și în întregime, cu menționarea dvs. ca autor.',
-  'licenses.name.cc-by-nc': 'Creative Commons Atribuire-NonComercial 4.0',
-  'licenses.cc-by-nc':
-    'Această licență permite altora să remodeleze, să ajusteze și să construiască pe baza lucrării dvs. în scopuri non-comerciale și, deși noile lor lucrări trebuie să vă recunoască și să fie non-comerciale, ele nu trebuie să licențieze lucrările derivate în aceleași termeni.',
-  'licenses.name.cc-by-nc-sa':
-    'Creative Commons Atribuire-NonComercial-Distribuire în condiții identice 4.0',
-  'licenses.cc-by-nc-sa':
-    'Această licență permite altora să remodeleze, să ajusteze și să construiască pe baza lucrării dvs. în scopuri non-comerciale, atâta timp cât vă creditează și licențiază noile creații sub aceleași termeni.',
-  'licenses.name.cc-by-nc-nd': 'Creative Commons Atribuire-NonComercial-FărăDerivate 4.0',
-  'licenses.cc-by-nc-nd':
-    'Această licență este cea mai restrictivă dintre cele șase licențe principale, permițând doar altora să descarce lucrările dvs. și să le partajeze cu alții, cu menționarea dvs. ca autor, dar fără a le modifica în vreun fel sau a le utiliza în scopuri comerciale.',
-  'licenses.name.lu-c': 'Drepturi de autor în Universul Literar',
-  'licenses.lu-c':
-    'Licență standard de drepturi de autor care vă permite să vindeți și să publicați lucrarea dvs. așa cum doriți și să vă protejați drepturile asupra acesteia. Această licență vă oferă, de asemenea, opțiunea de a vinde poveștile dvs. pe Literary Universe.',
-  'licenses.name.cc0': 'Niciun drept rezervat - CC0 1.0',
-  'licenses.cc0':
-    'Puneți lucrarea dvs. în domeniul public. În contrast cu licențele CC care permit titularilor de drepturi de autor să aleagă dintr-o gamă de permisiuni în timp ce își păstrează drepturile de autor, CC0 oferă încă o altă opțiune - opțiunea de a renunța la drepturile de autor și protecția bazelor de date, și la drepturile exclusive acordate automat creatorilor - alternativa "niciun drept rezervat" la licențele noastre.',
-}
+    'licenses.about': 'Despre licențe',
+    'licenses.more': 'Mai multe informații',
+    'licenses.legal': 'Document legal',
+    'licenses.name.cc-by': 'Creative Commons Atribuire 4.0',
+    'licenses.cc-by': 'Această licență permite altora să distribuie, să remodeleze, să ajusteze și să construiască pe baza lucrării dvs., chiar și în scopuri comerciale, atât timp cât vă creditează pentru creația originală. Aceasta este cea mai permisivă dintre licențele oferite. Recomandată pentru o diseminare maximă și utilizarea materialelor licențiate.',
+    'licenses.name.cc-by-sa': 'Creative Commons Atribuire-Distribuire în condiții identice 4.0',
+    'licenses.cc-by-sa': 'Această licență permite altora să remodeleze, să ajusteze și să construiască pe baza lucrării dvs., chiar și în scopuri comerciale, atât timp cât vă creditează și licențiază noile creații sub aceleași termeni. Această licență este adesea comparată cu licențele de software „copyleft” gratuite și open source. Toate lucrările noi bazate pe ale dvs. vor purta aceeași licență, astfel încât orice derivate vor permite și utilizarea comercială. Aceasta este licența utilizată de Wikipedia și este recomandată pentru materialele care ar beneficia de includerea de conținut din Wikipedia și proiecte similare cu licențe.',
+    'licenses.name.cc-by-nd': 'Creative Commons Atribuire-FărăDerivate 4.0',
+    'licenses.cc-by-nd': 'Această licență permite redistribuirea, atât în scopuri comerciale, cât și non-comerciale, atâta timp cât este transmisă nemodificată și în întregime, cu menționarea dvs. ca autor.',
+    'licenses.name.cc-by-nc': 'Creative Commons Atribuire-NonComercial 4.0',
+    'licenses.cc-by-nc': 'Această licență permite altora să remodeleze, să ajusteze și să construiască pe baza lucrării dvs. în scopuri non-comerciale și, deși noile lor lucrări trebuie să vă recunoască și să fie non-comerciale, ele nu trebuie să licențieze lucrările derivate în aceleași termeni.',
+    'licenses.name.cc-by-nc-sa': 'Creative Commons Atribuire-NonComercial-Distribuire în condiții identice 4.0',
+    'licenses.cc-by-nc-sa': 'Această licență permite altora să remodeleze, să ajusteze și să construiască pe baza lucrării dvs. în scopuri non-comerciale, atâta timp cât vă creditează și licențiază noile creații sub aceleași termeni.',
+    'licenses.name.cc-by-nc-nd': 'Creative Commons Atribuire-NonComercial-FărăDerivate 4.0',
+    'licenses.cc-by-nc-nd': 'Această licență este cea mai restrictivă dintre cele șase licențe principale, permițând doar altora să descarce lucrările dvs. și să le partajeze cu alții, cu menționarea dvs. ca autor, dar fără a le modifica în vreun fel sau a le utiliza în scopuri comerciale.',
+    'licenses.name.lu-c': 'Drepturi de autor în Universul Literar',
+    'licenses.lu-c': 'Licență standard de drepturi de autor care vă permite să vindeți și să publicați lucrarea dvs. așa cum doriți și să vă protejați drepturile asupra acesteia. Această licență vă oferă, de asemenea, opțiunea de a vinde poveștile dvs. pe Literary Universe.',
+    'licenses.name.cc0': 'Niciun drept rezervat - CC0 1.0',
+    'licenses.cc0': 'Puneți lucrarea dvs. în domeniul public. În contrast cu licențele CC care permit titularilor de drepturi de autor să aleagă dintr-o gamă de permisiuni în timp ce își păstrează drepturile de autor, CC0 oferă încă o altă opțiune - opțiunea de a renunța la drepturile de autor și protecția bazelor de date, și la drepturile exclusive acordate automat creatorilor - alternativa "niciun drept rezervat" la licențele noastre.'
+};
