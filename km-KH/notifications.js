3194005e44dcf38adb81894dc33f6f979e1cd083
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:31:10 +0200

    New translations notifications.js (Khmer)

--- km-KH/notifications.js
+++ km-KH/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'ចាប់ផ្ដើម​បង្ហាញ​សារ​នេះ​នៅពេល',
   'flashnews.endsAt': 'ឈប់​បង្ហាញ​សារ​នេះ​នៅពេល',
   'flashnews.newLanguage': 'ជ្រើស​ភាសា​ថែម​ថ្មី',
-  'flashnews.onlyDisplayOn':
-    'សារ​នឹង​ត្រូវ​បាន​បង្ហាញ​តែតាម​ភាសា​ដែលបាន​ជ្រើស​នៅខាងក្រោម (ប្រសិនបើ​ជ្រើស​មិនបាន​គ្រាន់​តែ​អ្នក​ជ្រើស) ហើយ​មិនមាន​សារ​បង្ហាញ​បន្ថែម​ទេ ក្នុងភាសា​ដែល​អត់​បាន​ជ្រើស។',
+  'flashnews.onlyDisplayOn': 'សារ​នឹង​ត្រូវ​បាន​បង្ហាញ​តែតាម​ភាសា​ដែលបាន​ជ្រើស​នៅខាងក្រោម (ប្រសិនបើ​ជ្រើស​មិនបាន​គ្រាន់​តែ​អ្នក​ជ្រើស) ហើយ​មិនមាន​សារ​បង្ហាញ​បន្ថែម​ទេ ក្នុងភាសា​ដែល​អត់​បាន​ជ្រើស។',
   'notifications.menu.all': 'សារ​សុពលភាព​ទាំងអស់',
   'notifications.menu.following': 'ចូលរួម',
   'notifications.menu.workshop': 'សិទ្ធិ១',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'កំណត់ថ្នាក់អានរួច។',
   'notifications.menu.delete': 'លុប​ការជូនដំណឹង',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'របាយការណ៍របស់អ្នក {storyName} ត្រូវបានកំណត់ឱ្យបញ្ឈប់បន្ទាន់ពីកាលបរិច្ឆេទអស់រយៈពេល ៦ ខែក្រោយនៃការអស់ទុន។',
-  'notification.workshopStoryStatus.toCancelled':
-    'របាយការណ៍របស់អ្នក {storyName} ត្រូវបានកំណត់ឱ្យសំរាប់បន្ទាន់ពីកាលបរិច្ឆេទអស់រយៈពេលមួយឆ្នាំក្រោយនៃការអស់ទុន។',
+  'notification.workshopStoryStatus.toHiatus': 'របាយការណ៍របស់អ្នក {storyName} ត្រូវបានកំណត់ឱ្យបញ្ឈប់បន្ទាន់ពីកាលបរិច្ឆេទអស់រយៈពេល ៦ ខែក្រោយនៃការអស់ទុន។',
+  'notification.workshopStoryStatus.toCancelled': 'របាយការណ៍របស់អ្នក {storyName} ត្រូវបានកំណត់ឱ្យសំរាប់បន្ទាន់ពីកាលបរិច្ឆេទអស់រយៈពេលមួយឆ្នាំក្រោយនៃការអស់ទុន។',
   'notification.followingStoryStatus.toHiatus': '{storyName} កំពុងសម្លាប់ទុន។',
   'notification.followingStoryStatus.toCancelled': '{storyName} ត្រូវបានបោះបង់។',
   'notification.followingNewChapter.new': 'បានបោះពុម្ពថ្មីនៃជិតដំបូងក្នុង {storyName}!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'អ្នក​បានចែក​រំលែក​អ្នកដឹកនាំ។',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'ទៅកាន់រឿង',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'ទៅកាន់រឿង'
+};
