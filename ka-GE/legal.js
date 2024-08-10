699b4040a90ef416648c22975000663810e8f7ae
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:21:32 +0200

    New translations legal.js (Georgian)

--- ka-GE/legal.js
+++ ka-GE/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'ბოდიშვით შეგვეძლებათ შეწყვიტოთ. მაგრამ მიიღებთ წესების, კონფიდენციალურობის პოლიტიკისა და საავტორო უფლებების შესახებ გამოცხადებას და თქვენ უნდა იცოდეთ და დაეთანხმეთ მათ.',
-  'legal.modal.agree': 'მე ვეთანხმები',
-  'legal.modal.disagree': 'მე არ ვეთანხმები',
-  'legal.modal.register': 'თუ დარეგისტრირებულხარ, ეს ვერ ხელმეორედ გინდა დაგიმატებები.',
-  'legal.modal.goToSettings': 'გადადით თქვენი პარამეტრებისათვის',
-  'legal.modal.oneLastThing': 'ბოლო რამდენიმეს დამატება...',
-  'legal.modal.finish': 'დასრულება',
-  'legal.modal.existingUser': 'უკვე რეგისტრირებული ხარ?',
-  'legal.modal.pleaseLogin': 'გთხოვთ შესვლა.',
-  'legal.publicationAgreements': 'გამოქვეყნების ხელშეკრულებები',
-  'legal.generalPublishingAgreement': 'ზოგადი გამოქვეყნების ხელშეკრულება',
-  'legal.documentsFor': 'იურიდიული დოკუმენტები {workTitle}-ისთვის',
-}
+    'legal.modal.intro': 'ბოდიშვით შეგვეძლებათ შეწყვიტოთ. მაგრამ მიიღებთ წესების, კონფიდენციალურობის პოლიტიკისა და საავტორო უფლებების შესახებ გამოცხადებას და თქვენ უნდა იცოდეთ და დაეთანხმეთ მათ.',
+    'legal.modal.agree': 'მე ვეთანხმები',
+    'legal.modal.disagree': 'მე არ ვეთანხმები',
+    'legal.modal.register': 'თუ დარეგისტრირებულხარ, ეს ვერ ხელმეორედ გინდა დაგიმატებები.',
+    'legal.modal.goToSettings': 'გადადით თქვენი პარამეტრებისათვის',
+    'legal.modal.oneLastThing': 'ბოლო რამდენიმეს დამატება...',
+    'legal.modal.finish': 'დასრულება',
+    'legal.modal.existingUser': 'უკვე რეგისტრირებული ხარ?',
+    'legal.modal.pleaseLogin': 'გთხოვთ შესვლა.',
+    'legal.publicationAgreements': 'გამოქვეყნების ხელშეკრულებები',
+    'legal.generalPublishingAgreement': 'ზოგადი გამოქვეყნების ხელშეკრულება',
+    'legal.documentsFor': 'იურიდიული დოკუმენტები {workTitle}-ისთვის'
+};
