b1638b36451f17c6f538c68b751463c1d51ba2ca
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:23:47 +0200

    New translations library.js (Norwegian)

--- no-NO/library.js
+++ no-NO/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'Legg til bibliotek',
-  'library.remove': 'Fjern fra bibliotek',
-  'library.bought': 'Kjøpte historier',
-  'library.stories': 'Følgte historier',
-  'library.universes': 'Fulgte universer',
-  'bookmark.continueReading': 'Fortsett å lese',
-  'bookmark.statusSummary': 'Du har lest {readPercent, number, ::percent} av historien.',
-}
+    'library.add': 'Legg til bibliotek',
+    'library.remove': 'Fjern fra bibliotek',
+    'library.bought': 'Kjøpte historier',
+    'library.stories': 'Følgte historier',
+    'library.universes': 'Fulgte universer',
+    'bookmark.continueReading': 'Fortsett å lese',
+    'bookmark.statusSummary': 'Du har lest {readPercent, number, ::percent} av historien.'
+};
