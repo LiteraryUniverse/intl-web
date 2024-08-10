7f2cd98a64705f2b5f1eebc2b82298f665bf86c8
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:27:42 +0200

    New translations library.js (Turkish)

--- tr-TR/library.js
+++ tr-TR/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'Kütüphaneye ekle',
-  'library.remove': 'Kütüphaneden kaldır',
-  'library.bought': 'Satın alınan hikayeler',
-  'library.stories': 'Takip edilen hikayeler',
-  'library.universes': 'Takip edilen evrenler',
-  'bookmark.continueReading': 'Okumaya devam et',
-  'bookmark.statusSummary': 'Hikayenin {readPercent, number, ::percent} bölümünü okudunuz.',
-}
+    'library.add': 'Kütüphaneye ekle',
+    'library.remove': 'Kütüphaneden kaldır',
+    'library.bought': 'Satın alınan hikayeler',
+    'library.stories': 'Takip edilen hikayeler',
+    'library.universes': 'Takip edilen evrenler',
+    'bookmark.continueReading': 'Okumaya devam et',
+    'bookmark.statusSummary': 'Hikayenin {readPercent, number, ::percent} bölümünü okudunuz.'
+};
