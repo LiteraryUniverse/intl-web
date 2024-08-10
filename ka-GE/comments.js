299113733b62173bc136d205b5d040dbead5caa5
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:21:31 +0200

    New translations comments.js (Georgian)

--- ka-GE/comments.js
+++ ka-GE/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': 'დაწერეთ თქვენი კომენტარი',
-  'comments.none': 'უკაცრავად, კომენტარები არ არის არსებული',
-  'comments.total': `არაფერიანდებს {count, plural,
+    'comment.write': 'დაწერეთ თქვენი კომენტარი',
+    'comments.none': 'უკაცრავად, კომენტარები არ არის არსებული',
+    'comments.total': `არაფერიანდებს {count, plural,
     =0 {კომენტარს}
     one {1 კომენტარი}
     other {# კომენტარი}
   }.`,
-  'comments.show.older': 'ძველი კომენტარების ნახვა',
-}
+    'comments.show.older': 'ძველი კომენტარების ნახვა'
+};
