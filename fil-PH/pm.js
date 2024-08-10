4806d429e307aea26448f0f1304120226c2e0eb3
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:36:32 +0200

    New translations pm.js (Filipino)

--- fil-PH/pm.js
+++ fil-PH/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'Usapan',
-  'pm.showolder': 'Ipakita ang mas lumang mga mensahe',
-  'pm.errors.saysomething': 'Dapat kang magsabi ng kahit ano...',
-  'pm.errors.addrecipient': 'Kailangan mong magdagdag ng hindi bababa sa isang ibang user.',
-  'pm.message': 'Mensahe',
-  'pm.send.message': 'Ipadala ang mensahe',
-  'pm.messages.own': 'Iyong mga mensahe',
-  'pm.conversations.none': 'Hindi ka kasalukuyang nakikipag-usap sa sinuman.',
-  'pm.to': 'Sa:',
-  'pm.with': 'Usapan kay:',
-  'pm.send': 'Ipadala',
-  'pm.conversations.unreadCount': `Mayroon kang {count, plural,
+    'pm.conversation': 'Usapan',
+    'pm.showolder': 'Ipakita ang mas lumang mga mensahe',
+    'pm.errors.saysomething': 'Dapat kang magsabi ng kahit ano...',
+    'pm.errors.addrecipient': 'Kailangan mong magdagdag ng hindi bababa sa isang ibang user.',
+    'pm.message': 'Mensahe',
+    'pm.send.message': 'Ipadala ang mensahe',
+    'pm.messages.own': 'Iyong mga mensahe',
+    'pm.conversations.none': 'Hindi ka kasalukuyang nakikipag-usap sa sinuman.',
+    'pm.to': 'Sa:',
+    'pm.with': 'Usapan kay:',
+    'pm.send': 'Ipadala',
+    'pm.conversations.unreadCount': `Mayroon kang {count, plural,
     zero {walang binasang usapan}
     one {isang binasang usapan}
     other {mga binasang usapan}
   }.`,
-  'pm.form.minimumParticipants': 'Kailangan mong pumili ng tatanggap.',
-  'pm.form.maxParticipants': `Maaari kang magkaroon ng maximum ng {maxPeople, plural,
+    'pm.form.minimumParticipants': 'Kailangan mong pumili ng tatanggap.',
+    'pm.form.maxParticipants': `Maaari kang magkaroon ng maximum ng {maxPeople, plural,
     one {isang tao}
     other {# mga tao}
   } sa isang pag-uusap.`,
-  'pm.new.aria': 'Bagong usapan',
-  'pm.new.buttonTitle': 'Simulan ang bagong usapan',
-}
+    'pm.new.aria': 'Bagong usapan',
+    'pm.new.buttonTitle': 'Simulan ang bagong usapan'
+};
