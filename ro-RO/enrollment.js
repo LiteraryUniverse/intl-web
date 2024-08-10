4877c9535e0fe3105a95aff14a277ec121fe5682
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:41:19 +0200

    New translations enrollment.js (Romanian)

--- ro-RO/enrollment.js
+++ ro-RO/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'Revendică-ți contul!',
-  'enrollment.description':
-    'Bun venit {username}! Pentru a avea acces la contul tău special, trebuie doar să creezi o parolă.',
-  'enrollment.submitting': 'Se revendică contul tău...',
-  'enrollment.submit': 'Revendică-ți contul!',
-}
+    'enrollment.title': 'Revendică-ți contul!',
+    'enrollment.description': 'Bun venit {username}! Pentru a avea acces la contul tău special, trebuie doar să creezi o parolă.',
+    'enrollment.submitting': 'Se revendică contul tău...',
+    'enrollment.submit': 'Revendică-ți contul!'
+};
