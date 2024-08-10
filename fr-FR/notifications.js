0a3b56b096c56f5824100e83fc9e93860c8b32ff
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:42:16 +0200

    New translations notifications.js (French)

--- fr-FR/notifications.js
+++ fr-FR/notifications.js
@@ -1,11 +1,10 @@
 // Notifications & flashnews
 export default {
-  'flashnews.create': "Créer un nouveau flash d'actualité",
+  'flashnews.create': 'Créer un nouveau flash d\'actualité',
   'flashnews.startsAt': 'Commencer à afficher ce message à',
-  'flashnews.endsAt': "Arrêter l'affichage de ce message à",
+  'flashnews.endsAt': 'Arrêter l\'affichage de ce message à',
   'flashnews.newLanguage': 'Sélectionnez la nouvelle langue à ajouter',
-  'flashnews.onlyDisplayOn':
-    "Les actualités ne seront affichées que dans les langues sélectionnées ci-dessous (s'il y en a une sélectionnée), il n'y aura pas d'actualités affichées, même dans la langue par défaut, dans d'autres langues.",
+  'flashnews.onlyDisplayOn': 'Les actualités ne seront affichées que dans les langues sélectionnées ci-dessous (s\'il y en a une sélectionnée), il n\'y aura pas d\'actualités affichées, même dans la langue par défaut, dans d\'autres langues.',
   'notifications.menu.all': 'Toutes les notifications',
   'notifications.menu.following': 'Suivis',
   'notifications.menu.workshop': 'Workshop',
@@ -14,17 +13,15 @@ export default {
   'notifications.menu.markRead': 'Marquer comme lu',
   'notifications.menu.delete': 'Supprimer la notification',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    "Votre histoire {storyName} a été automatiquement mise en pause après 6 mois d'inactivité.",
-  'notification.workshopStoryStatus.toCancelled':
-    "Votre histoire {storyName} a été automatiquement annulée après un an d'inactivité.",
+  'notification.workshopStoryStatus.toHiatus': 'Votre histoire {storyName} a été automatiquement mise en pause après 6 mois d\'inactivité.',
+  'notification.workshopStoryStatus.toCancelled': 'Votre histoire {storyName} a été automatiquement annulée après un an d\'inactivité.',
   'notification.followingStoryStatus.toHiatus': '{storyName} est en pause.',
   'notification.followingStoryStatus.toCancelled': '{storyName} a été annulé.',
   'notification.followingNewChapter.new': 'Nouveau chapitre dans {storyName} publié !',
   'notification.workshopStory.newComment': 'a commenté sur {storyName}',
   // Before this text will be the username of the user
-  'notification.friendRequest': "vous a envoyé une demande d'ami.",
+  'notification.friendRequest': 'vous a envoyé une demande d\'ami.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': "Aller à l'histoire",
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'Aller à l\'histoire'
+};
