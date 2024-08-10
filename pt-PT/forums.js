93aaf597697b4cc957ec957c2816f2e6ad19356b
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:25:31 +0200

    New translations forums.js (Portuguese)

--- pt-PT/forums.js
+++ pt-PT/forums.js
@@ -1,8 +1,7 @@
 export default {
   'forums.create': 'Ativar fórum',
   'forums.archive': 'Arquivar fórum',
-  'forums.archive.explained':
-    'Arquivar o fórum irá impedir que os usuários publiquem qualquer conteúdo adicional, mas ele permanecerá visível.',
+  'forums.archive.explained': 'Arquivar o fórum irá impedir que os usuários publiquem qualquer conteúdo adicional, mas ele permanecerá visível.',
   'forums.archive.success': 'O fórum foi arquivado',
   'forums.unArchive': 'Reativar os fóruns',
   'forums.unArchive.success': 'O fórum foi reativado!',
@@ -12,25 +11,18 @@ export default {
   'forums.category.name': 'Nome da categoria',
   'forums.category.slug': 'SEO url parte',
   'forums.category.settings': 'Definições de categoria',
-  'forums.category.language':
-    'Limitar a exibição desta categoria à seguinte língua. Deixe em branco se quiser exibir a categoria em todas as línguas.',
-  'forums.category.intlNameDescription':
-    'Temos títulos localizados de nomes de categorias comuns para a sua seleção, de modo a que a categoria possa estar disponível para todas as línguas suportadas.',
+  'forums.category.language': 'Limitar a exibição desta categoria à seguinte língua. Deixe em branco se quiser exibir a categoria em todas as línguas.',
+  'forums.category.intlNameDescription': 'Temos títulos localizados de nomes de categorias comuns para a sua seleção, de modo a que a categoria possa estar disponível para todas as línguas suportadas.',
   'forums.thread.new': 'Novo tópico',
   'forums.thread.delete': 'Eliminar tópico',
   'forums.post.new': 'Nova publicação',
-  'forums.archived.forum':
-    'Este fórum foi arquivado em {archivedAt, date, long}. Ainda poder ver os fóruns, mas já não é possível criar novos conteúdos.',
+  'forums.archived.forum': 'Este fórum foi arquivado em {archivedAt, date, long}. Ainda poder ver os fóruns, mas já não é possível criar novos conteúdos.',
   'forums.category.archive': 'Arquivar categoria',
-  'forums.category.archiveExplained':
-    'A categoria que não tem tópicos será eliminada, enquanto que as categorias com conteúdo impedirão a criação de novos tópicos.',
+  'forums.category.archiveExplained': 'A categoria que não tem tópicos será eliminada, enquanto que as categorias com conteúdo impedirão a criação de novos tópicos.',
   'forums.category.reactivate': 'Reativar categoria',
-  'forums.category.isVotable':
-    'Permitir que os utilizadores possam fazer votos ascendentes/descendentes em tópicos',
-  'forums.category.titleIsGlobal':
-    'Este título de categoria será exibido na língua do utilizador, se o suportarmos.',
-  'forums.category.languageLimited':
-    'Esta categoria será exibida apenas aos utilizadores nesta língua.',
+  'forums.category.isVotable': 'Permitir que os utilizadores possam fazer votos ascendentes/descendentes em tópicos',
+  'forums.category.titleIsGlobal': 'Este título de categoria será exibido na língua do utilizador, se o suportarmos.',
+  'forums.category.languageLimited': 'Esta categoria será exibida apenas aos utilizadores nesta língua.',
   'forums.threadsCounter': 'Número total de discussões de tópicos',
   // Pre-defined category names
   'forums.category.intlName.general': 'Geral',
@@ -52,8 +44,7 @@ export default {
   // Forum restrictions
   'forums.forumRestriction.usersOnly': 'Só permitir o acesso a utilizadores registados',
   'forums.settings.accessControls': 'Controlos de acesso',
-  'forums.forumRestrictionsMessages.usersOnlyForum':
-    'Este fórum é acessível apenas para usuários registrados.',
+  'forums.forumRestrictionsMessages.usersOnlyForum': 'Este fórum é acessível apenas para usuários registrados.',
   'forums.noThreads': 'Nada para ver aqui. Cabe a si iniciar a conversa.',
   'forums.noPosts': 'Ainda ninguém respondeu. Seja o primeiro!',
   // Threads
@@ -73,5 +64,5 @@ export default {
     one {resposta}
     other {respostas}
   }`,
-  'forums.thread.createdAt': 'Criado em {createdAt, date, medium}',
-}
+  'forums.thread.createdAt': 'Criado em {createdAt, date, medium}'
+};
