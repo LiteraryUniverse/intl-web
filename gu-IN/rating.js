04f99b64c5fc4ccff949e0bfae357f3d342dd2ba
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:18:39 +0200

    New translations rating.js (Gujarati)

--- gu-IN/rating.js
+++ gu-IN/rating.js
@@ -2,18 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'રેટિંગ',
-  'rating.explained': 'ઉંમર રેટિંગ અને પ્રતિબંધ.',
-  'rating.all': 'બધા',
-  'rating.desc.all': 'બધી ઉંમરો અને જૂથો માટે ઉપયોગી.',
-  'rating.teens': 'ટીનેજર્સ',
-  'rating.desc.teens': '12 થી 18 વર્ષ ની વયગત માટે યોગ્ય.',
-  'rating.mature': 'પક્કી',
-  'rating.desc.mature': 'આગળની વિષયો, કચો ભાષા અને નિહિત દૃશ્યો ધરાવે છે.',
-  'rating.explicit': 'પ્રત્યક્ષ',
-  'rating.desc.explicit': 'પ્રત્યક્ષ દૃશ્યો ધરાવે છે.',
-  'rating.selection.all': 'બધા - બધી ઉંમરો અને જૂથો માટે ઉપયોગી.',
-  'rating.selection.teens': 'ટીનેજર્સ (12-18) - 12 થી 18 વર્ષ ની વયગત માટે યોગ્ય.',
-  'rating.selection.mature': 'પક્કી (18+) - આગળની વિષયો, કચો ભાષા અને નિહિત દૃશ્યો ધરાવે છે.',
-  'rating.selection.explicit': 'પ્રત્યક્ષ (21+) - પ્રત્યક્ષ દૃશ્યો ધરાવે છે.',
-}
+    'rating.name': 'રેટિંગ',
+    'rating.explained': 'ઉંમર રેટિંગ અને પ્રતિબંધ.',
+    'rating.all': 'બધા',
+    'rating.desc.all': 'બધી ઉંમરો અને જૂથો માટે ઉપયોગી.',
+    'rating.teens': 'ટીનેજર્સ',
+    'rating.desc.teens': '12 થી 18 વર્ષ ની વયગત માટે યોગ્ય.',
+    'rating.mature': 'પક્કી',
+    'rating.desc.mature': 'આગળની વિષયો, કચો ભાષા અને નિહિત દૃશ્યો ધરાવે છે.',
+    'rating.explicit': 'પ્રત્યક્ષ',
+    'rating.desc.explicit': 'પ્રત્યક્ષ દૃશ્યો ધરાવે છે.',
+    'rating.selection.all': 'બધા - બધી ઉંમરો અને જૂથો માટે ઉપયોગી.',
+    'rating.selection.teens': 'ટીનેજર્સ (12-18) - 12 થી 18 વર્ષ ની વયગત માટે યોગ્ય.',
+    'rating.selection.mature': 'પક્કી (18+) - આગળની વિષયો, કચો ભાષા અને નિહિત દૃશ્યો ધરાવે છે.',
+    'rating.selection.explicit': 'પ્રત્યક્ષ (21+) - પ્રત્યક્ષ દૃશ્યો ધરાવે છે.'
+};
