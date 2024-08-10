c389fa382055dee008376c353249e9add733b17e
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:30:18 +0200

    New translations licenses.js (Indonesian)

--- id-ID/licenses.js
+++ id-ID/licenses.js
@@ -3,31 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'Tentang Lisensi',
-  'licenses.more': 'Informasi lebih lanjut',
-  'licenses.legal': 'Dokumen hukum',
-  'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
-  'licenses.cc-by':
-    'Lisensi ini mengizinkan orang lain untuk mengubah, memperbaiki, membuat ciptaan turunan, dan mendistribusikan secara komersial, selama mereka mencantumkan kredit kepada Anda atas ciptaan asli. Lisensi ini merupakan lisensi yang paling bebas. Lisensi ini direkomendasikan untuk pendistribusian luas secara maksimal dan penggunaan materi berlisensi.',
-  'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
-  'licenses.cc-by-sa':
-    'Lisensi ini memungkinkan orang lain untuk membuat ulang, memodifikasi, dan membangun berdasarkan karya Anda bahkan untuk tujuan komersial, selama mereka memberikan kredit kepada Anda dan melisensikan karya baru mereka dengan persyaratan yang sama. Lisensi ini sering dibandingkan dengan lisensi perangkat lunak sumber terbuka yang menggunakan "copyleft". Semua karya baru yang didasarkan pada karya Anda akan menggunakan lisensi yang sama, sehingga setiap turunan juga akan memperbolehkan penggunaan komersial. Ini adalah lisensi yang digunakan oleh Wikipedia, dan direkomendasikan untuk materi yang akan mendapatkan manfaat dari penambahan konten dari Wikipedia dan proyek dengan lisensi serupa.',
-  'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
-  'licenses.cc-by-nd':
-    'Lisensi ini memungkinkan redistribusi, baik komersial maupun non-komersial, selama tetap tidak berubah dan utuh, dengan memberikan kredit kepada Anda.',
-  'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
-  'licenses.cc-by-nc':
-    'Lisensi ini memungkinkan orang lain untuk membuat ulang, memodifikasi, dan membangun berdasarkan karya Anda secara non-komersial, dan meskipun karya baru mereka juga harus mengakui Anda dan tidak boleh digunakan untuk tujuan komersial, mereka tidak harus melisensikan karya turunannya dengan persyaratan yang sama.',
-  'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
-  'licenses.cc-by-nc-sa':
-    'Lisensi ini memungkinkan orang lain untuk membuat ulang, memodifikasi, dan membangun berdasarkan karya Anda secara non-komersial, selama mereka memberikan kredit kepada Anda dan melisensikan karya baru mereka dengan persyaratan yang sama.',
-  'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
-  'licenses.cc-by-nc-nd':
-    'Lisensi ini adalah yang paling membatasi dari enam lisensi utama kami, hanya memungkinkan orang lain untuk mengunduh karya Anda dan membagikannya kepada orang lain selama memberikan kredit kepada Anda, tetapi mereka tidak boleh mengubahnya dengan cara apa pun atau menggunakan secara komersial.',
-  'licenses.name.lu-c': 'Hak Cipta Literary Universe',
-  'licenses.lu-c':
-    'Lisensi hak cipta penuh standar yang memungkinkan Anda untuk menjual dan menerbitkan karya Anda sesuai keinginan dan melindungi hak-hak Anda terhadapnya. Lisensi ini juga memberi Anda opsi untuk menjual cerita Anda di Literary Universe.',
-  'licenses.name.cc0': 'No Rights Reserved - CC0 1.0',
-  'licenses.cc0':
-    'Letakkan karya Anda di domain publik. Berbeda dengan lisensi CC yang memungkinkan pemegang hak cipta untuk memilih dari berbagai izin sambil tetap mempertahankan hak cipta mereka, CC0 memberikan pilihan yang berbeda sama sekali - pilihan untuk keluar dari hak cipta dan perlindungan basis data, serta hak eksklusif yang secara otomatis diberikan kepada pencipta - sebagai alternatif "tanpa hak yang dipesan" terhadap lisensi kami.',
-}
+    'licenses.about': 'Tentang Lisensi',
+    'licenses.more': 'Informasi lebih lanjut',
+    'licenses.legal': 'Dokumen hukum',
+    'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
+    'licenses.cc-by': 'Lisensi ini mengizinkan orang lain untuk mengubah, memperbaiki, membuat ciptaan turunan, dan mendistribusikan secara komersial, selama mereka mencantumkan kredit kepada Anda atas ciptaan asli. Lisensi ini merupakan lisensi yang paling bebas. Lisensi ini direkomendasikan untuk pendistribusian luas secara maksimal dan penggunaan materi berlisensi.',
+    'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
+    'licenses.cc-by-sa': 'Lisensi ini memungkinkan orang lain untuk membuat ulang, memodifikasi, dan membangun berdasarkan karya Anda bahkan untuk tujuan komersial, selama mereka memberikan kredit kepada Anda dan melisensikan karya baru mereka dengan persyaratan yang sama. Lisensi ini sering dibandingkan dengan lisensi perangkat lunak sumber terbuka yang menggunakan "copyleft". Semua karya baru yang didasarkan pada karya Anda akan menggunakan lisensi yang sama, sehingga setiap turunan juga akan memperbolehkan penggunaan komersial. Ini adalah lisensi yang digunakan oleh Wikipedia, dan direkomendasikan untuk materi yang akan mendapatkan manfaat dari penambahan konten dari Wikipedia dan proyek dengan lisensi serupa.',
+    'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
+    'licenses.cc-by-nd': 'Lisensi ini memungkinkan redistribusi, baik komersial maupun non-komersial, selama tetap tidak berubah dan utuh, dengan memberikan kredit kepada Anda.',
+    'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
+    'licenses.cc-by-nc': 'Lisensi ini memungkinkan orang lain untuk membuat ulang, memodifikasi, dan membangun berdasarkan karya Anda secara non-komersial, dan meskipun karya baru mereka juga harus mengakui Anda dan tidak boleh digunakan untuk tujuan komersial, mereka tidak harus melisensikan karya turunannya dengan persyaratan yang sama.',
+    'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
+    'licenses.cc-by-nc-sa': 'Lisensi ini memungkinkan orang lain untuk membuat ulang, memodifikasi, dan membangun berdasarkan karya Anda secara non-komersial, selama mereka memberikan kredit kepada Anda dan melisensikan karya baru mereka dengan persyaratan yang sama.',
+    'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
+    'licenses.cc-by-nc-nd': 'Lisensi ini adalah yang paling membatasi dari enam lisensi utama kami, hanya memungkinkan orang lain untuk mengunduh karya Anda dan membagikannya kepada orang lain selama memberikan kredit kepada Anda, tetapi mereka tidak boleh mengubahnya dengan cara apa pun atau menggunakan secara komersial.',
+    'licenses.name.lu-c': 'Hak Cipta Literary Universe',
+    'licenses.lu-c': 'Lisensi hak cipta penuh standar yang memungkinkan Anda untuk menjual dan menerbitkan karya Anda sesuai keinginan dan melindungi hak-hak Anda terhadapnya. Lisensi ini juga memberi Anda opsi untuk menjual cerita Anda di Literary Universe.',
+    'licenses.name.cc0': 'No Rights Reserved - CC0 1.0',
+    'licenses.cc0': 'Letakkan karya Anda di domain publik. Berbeda dengan lisensi CC yang memungkinkan pemegang hak cipta untuk memilih dari berbagai izin sambil tetap mempertahankan hak cipta mereka, CC0 memberikan pilihan yang berbeda sama sekali - pilihan untuk keluar dari hak cipta dan perlindungan basis data, serta hak eksklusif yang secara otomatis diberikan kepada pencipta - sebagai alternatif "tanpa hak yang dipesan" terhadap lisensi kami.'
+};
