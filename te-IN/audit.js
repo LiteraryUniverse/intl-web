593373069e76ab6a656aa62fbbebb7bf395ef6a7
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:35:31 +0200

    New translations audit.js (Telugu)

--- te-IN/audit.js
+++ te-IN/audit.js
@@ -1,6 +1,6 @@
 export default {
-  'auditLog.header.date': 'తేదీ',
-  'auditLog.header.action': 'ఏమిటి',
-  'auditLog.header.group': 'గ్రూప్',
-  'auditLog.header.user': 'ఎవరు',
-}
+    'auditLog.header.date': 'తేదీ',
+    'auditLog.header.action': 'ఏమిటి',
+    'auditLog.header.group': 'గ్రూప్',
+    'auditLog.header.user': 'ఎవరు'
+};
