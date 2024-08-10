398f478a136383827de45535767f413dff58a962
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:24:52 +0200

    New translations legal.js (Polish)

--- pl-PL/legal.js
+++ pl-PL/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Przepraszamy za przeszkadzanie, ale przed kontynuowaniem jesteśmy prawnie zobligowani, aby uświadomić Cię o naszych Warunkach użytkowania, Polityce prywatności i Umowie o prawach autorskich oraz wymagamy wyrażenia przez Ciebie zgody.',
-  'legal.modal.agree': 'Wyrażam zgodę',
-  'legal.modal.disagree': 'Nie wyrażam zgody',
-  'legal.modal.register': 'Nie zobaczysz tego więcej, jeśli się zarejestrujesz.',
-  'legal.modal.goToSettings': 'Przejdź do ustawień',
-  'legal.modal.oneLastThing': 'Jeszcze jedno...',
-  'legal.modal.finish': 'Gotowe',
-  'legal.modal.existingUser': 'Już zarejestrowany?',
-  'legal.modal.pleaseLogin': 'Proszę się zalogować.',
-  'legal.publicationAgreements': 'Umowy publikacyjne',
-  'legal.generalPublishingAgreement': 'Ogólna umowa wydawnicza',
-  'legal.documentsFor': 'Dokumenty prawne dla {workTitle}',
-}
+    'legal.modal.intro': 'Przepraszamy za przeszkadzanie, ale przed kontynuowaniem jesteśmy prawnie zobligowani, aby uświadomić Cię o naszych Warunkach użytkowania, Polityce prywatności i Umowie o prawach autorskich oraz wymagamy wyrażenia przez Ciebie zgody.',
+    'legal.modal.agree': 'Wyrażam zgodę',
+    'legal.modal.disagree': 'Nie wyrażam zgody',
+    'legal.modal.register': 'Nie zobaczysz tego więcej, jeśli się zarejestrujesz.',
+    'legal.modal.goToSettings': 'Przejdź do ustawień',
+    'legal.modal.oneLastThing': 'Jeszcze jedno...',
+    'legal.modal.finish': 'Gotowe',
+    'legal.modal.existingUser': 'Już zarejestrowany?',
+    'legal.modal.pleaseLogin': 'Proszę się zalogować.',
+    'legal.publicationAgreements': 'Umowy publikacyjne',
+    'legal.generalPublishingAgreement': 'Ogólna umowa wydawnicza',
+    'legal.documentsFor': 'Dokumenty prawne dla {workTitle}'
+};
