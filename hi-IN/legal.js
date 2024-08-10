7777000a88f8d474bcb23b8218f3641eb1592de8
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:34:26 +0200

    New translations legal.js (Hindi)

--- hi-IN/legal.js
+++ hi-IN/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'क्षमा करें, बाधित करने के पहले हमारे आपको आगे बढ़ने से पहले हमारे सेवा की शर्तें, गोपनीयता नीति और कॉपीराइट समझे और स्वीकार करने के बारे में जानकार होने की सुनिश्चित करने के लिए कानूनी रूप से बाध्य हैं।',
-  'legal.modal.agree': 'मैं सहमत हूँ',
-  'legal.modal.disagree': 'मैं सहमत नहीं हूँ',
-  'legal.modal.register': 'आप इसे देखना बंद कर देंगे अगर आप पंजीकरण करते हैं।',
-  'legal.modal.goToSettings': 'अपनी सेटिंग्स पर जाएं',
-  'legal.modal.oneLastThing': 'एक आखिरी बात...',
-  'legal.modal.finish': 'समाप्त',
-  'legal.modal.existingUser': 'पहले से पंजीकृत हैं?',
-  'legal.modal.pleaseLogin': 'कृपया लॉगिन करें।',
-  'legal.publicationAgreements': 'प्रकाशन समझौते',
-  'legal.generalPublishingAgreement': 'सामान्य प्रकाशन समझौता',
-  'legal.documentsFor': '{workTitle}के लिए कानूनी दस्तावेज़',
-}
+    'legal.modal.intro': 'क्षमा करें, बाधित करने के पहले हमारे आपको आगे बढ़ने से पहले हमारे सेवा की शर्तें, गोपनीयता नीति और कॉपीराइट समझे और स्वीकार करने के बारे में जानकार होने की सुनिश्चित करने के लिए कानूनी रूप से बाध्य हैं।',
+    'legal.modal.agree': 'मैं सहमत हूँ',
+    'legal.modal.disagree': 'मैं सहमत नहीं हूँ',
+    'legal.modal.register': 'आप इसे देखना बंद कर देंगे अगर आप पंजीकरण करते हैं।',
+    'legal.modal.goToSettings': 'अपनी सेटिंग्स पर जाएं',
+    'legal.modal.oneLastThing': 'एक आखिरी बात...',
+    'legal.modal.finish': 'समाप्त',
+    'legal.modal.existingUser': 'पहले से पंजीकृत हैं?',
+    'legal.modal.pleaseLogin': 'कृपया लॉगिन करें।',
+    'legal.publicationAgreements': 'प्रकाशन समझौते',
+    'legal.generalPublishingAgreement': 'सामान्य प्रकाशन समझौता',
+    'legal.documentsFor': '{workTitle}के लिए कानूनी दस्तावेज़'
+};
