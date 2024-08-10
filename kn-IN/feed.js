e1eeceb0638bb2b5e7ccc7602e7dbba92c44bb07
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:38:38 +0200

    New translations feed.js (Kannada)

--- kn-IN/feed.js
+++ kn-IN/feed.js
@@ -1,9 +1,9 @@
 export default {
-  'feed.name': 'ಫೀಡ್',
-  'feed.post.new': 'ಹೊಸ ಪೋಸ್ಟ್',
-  'feed.post.like': 'ಪೋಸ್ಟ್ ಗೆ ಇಷ್ಟವನ್ನು ಹೊಂದಿಸಿ',
-  'feed.post.unlike': 'ಪೋಸ್ಟ್ ಗೆ ಇಷ್ಟ ಇಲ್ಲದಿರಿ',
-  'feed.post.send': 'ಪೋಸ್ಟ್',
-  'feed.empty': 'ಫೀಡ್ ನಲ್ಲಿ ಯಾವುದೇ ಪ್ರವೇಶಗಳಿಲ್ಲ.',
-  'feed.placeholder': "What's on your mind?",
-}
+    'feed.name': 'ಫೀಡ್',
+    'feed.post.new': 'ಹೊಸ ಪೋಸ್ಟ್',
+    'feed.post.like': 'ಪೋಸ್ಟ್ ಗೆ ಇಷ್ಟವನ್ನು ಹೊಂದಿಸಿ',
+    'feed.post.unlike': 'ಪೋಸ್ಟ್ ಗೆ ಇಷ್ಟ ಇಲ್ಲದಿರಿ',
+    'feed.post.send': 'ಪೋಸ್ಟ್',
+    'feed.empty': 'ಫೀಡ್ ನಲ್ಲಿ ಯಾವುದೇ ಪ್ರವೇಶಗಳಿಲ್ಲ.',
+    'feed.placeholder': 'What\'s on your mind?'
+};
