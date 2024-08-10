2cf5e2284a7585746d53372647382a4b5e113a72
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:41:35 +0200

    New translations legal.js (Romanian)

--- ro-RO/legal.js
+++ ro-RO/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Ne pare rău că întrerupem. Înainte de a merge mai departe, suntem legal obligați să ne asigurăm că sunteți conștient și de acord cu Termenii de utilizare, Politica de confidențialitate și Acordul de drepturi de autor.',
-  'legal.modal.agree': 'Sunt de acord',
-  'legal.modal.disagree': 'Nu sunt de acord',
-  'legal.modal.register': 'Nu veți mai vedea asta dacă vă înregistrați.',
-  'legal.modal.goToSettings': 'Mergeți la setările dumneavoastră',
-  'legal.modal.oneLastThing': 'Un ultim lucru...',
-  'legal.modal.finish': 'Finalizați',
-  'legal.modal.existingUser': 'Deja înregistrat?',
-  'legal.modal.pleaseLogin': 'Vă rugăm să vă autentificați.',
-  'legal.publicationAgreements': 'Acorduri de publicare',
-  'legal.generalPublishingAgreement': 'Acord general de publicare',
-  'legal.documentsFor': 'Documente legale pentru {workTitle}',
-}
+    'legal.modal.intro': 'Ne pare rău că întrerupem. Înainte de a merge mai departe, suntem legal obligați să ne asigurăm că sunteți conștient și de acord cu Termenii de utilizare, Politica de confidențialitate și Acordul de drepturi de autor.',
+    'legal.modal.agree': 'Sunt de acord',
+    'legal.modal.disagree': 'Nu sunt de acord',
+    'legal.modal.register': 'Nu veți mai vedea asta dacă vă înregistrați.',
+    'legal.modal.goToSettings': 'Mergeți la setările dumneavoastră',
+    'legal.modal.oneLastThing': 'Un ultim lucru...',
+    'legal.modal.finish': 'Finalizați',
+    'legal.modal.existingUser': 'Deja înregistrat?',
+    'legal.modal.pleaseLogin': 'Vă rugăm să vă autentificați.',
+    'legal.publicationAgreements': 'Acorduri de publicare',
+    'legal.generalPublishingAgreement': 'Acord general de publicare',
+    'legal.documentsFor': 'Documente legale pentru {workTitle}'
+};
