7291fc972c4b6a8b7a016671983f7ce95bcf4736
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:33:04 +0200

    New translations feed.js (Thai)

--- th-TH/feed.js
+++ th-TH/feed.js
@@ -1,9 +1,9 @@
 export default {
-  'feed.name': 'ฟีด',
-  'feed.post.new': 'โพสต์ใหม่',
-  'feed.post.like': 'ถูกใจโพสต์',
-  'feed.post.unlike': 'ไม่ถูกใจโพสต์',
-  'feed.post.send': 'โพสต์',
-  'feed.empty': 'ไม่มีรายการในฟีด',
-  'feed.placeholder': 'คุณคิดอะไรอยู่?',
-}
+    'feed.name': 'ฟีด',
+    'feed.post.new': 'โพสต์ใหม่',
+    'feed.post.like': 'ถูกใจโพสต์',
+    'feed.post.unlike': 'ไม่ถูกใจโพสต์',
+    'feed.post.send': 'โพสต์',
+    'feed.empty': 'ไม่มีรายการในฟีด',
+    'feed.placeholder': 'คุณคิดอะไรอยู่?'
+};
