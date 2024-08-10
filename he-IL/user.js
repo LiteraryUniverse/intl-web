0e602e9ca62e66a438790650b1a21bcdafff2c1a
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:19:22 +0200

    New translations user.js (Hebrew)

--- he-IL/user.js
+++ he-IL/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'הוסף לחברים',
-  'user.friend.requests': `יש לך {num, number} בקשת חברות חדשה {num, plural,
+    'user.friend.request.add': 'הוסף לחברים',
+    'user.friend.requests': `יש לך {num, number} בקשת חברות חדשה {num, plural,
     zero {}
     one {בקשה}
     many {בקשות}
     other {בקשות}
   }`,
-  'user.friend.unfriend': 'הסר מחברים',
-  'user.block': 'חסום',
-  'user.unblock': 'בטל חסימה',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'הסר מחברים',
+    'user.block': 'חסום',
+    'user.unblock': 'בטל חסימה',
+    'user.joined': `{gender, select,
     male {הצטרף ל}
     female {הצטרף ל}
     other {הצטרף ל}} ב {date, date, long}`,
-  'user.friend.request.accept': 'קבל בקשת חברות',
-  'user.friend.request.cancel': 'בטל בקשת חברות',
-  'user.friend.request.deny': 'דחה בקשת חברות',
-  'user.listing': 'רשימת משתמשים',
-  'user.profile.visit': 'בקר בפרופיל של {user}.',
-  'user.profile.avatar': 'אווטר של {username}',
-  'user.friend.list.requests': 'בקשות חברות',
-  'user.friend.norequests': 'אין לך בקשות לחברות.',
-  'user.dashboardWelcome': `Good {dayPart, select,
+    'user.friend.request.accept': 'קבל בקשת חברות',
+    'user.friend.request.cancel': 'בטל בקשת חברות',
+    'user.friend.request.deny': 'דחה בקשת חברות',
+    'user.listing': 'רשימת משתמשים',
+    'user.profile.visit': 'בקר בפרופיל של {user}.',
+    'user.profile.avatar': 'אווטר של {username}',
+    'user.friend.list.requests': 'בקשות חברות',
+    'user.friend.norequests': 'אין לך בקשות לחברות.',
+    'user.dashboardWelcome': `Good {dayPart, select,
     morning {morning}
     afternoon {afternoon}
     evening {evening}
     night {night}
     other {day}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'מחבר זה פורסם בדרך המסורתית',
-  'user.verification.luAuthor': 'מחבר מבוסס ביקום הספרותי',
-  'user.verification.luEmployee': 'עובד ביקום הספרותי',
-  'user.verification.publicFigure': 'דמות ציבור מאומתת',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'מחבר זה פורסם בדרך המסורתית',
+    'user.verification.luAuthor': 'מחבר מבוסס ביקום הספרותי',
+    'user.verification.luEmployee': 'עובד ביקום הספרותי',
+    'user.verification.publicFigure': 'דמות ציבור מאומתת',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {סיפור}
     other {סיפורים}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {ייצוג}
     other {ייצוגים}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {פאן פיקשן}
     other {פאן פיקשנים} 
   }`,
-  'profile.works': 'עבודות',
-  // As in creative/literary works
-  'profile.wishlist': 'רשימת השאלות',
-  'profile.storiesBy': 'סיפורים שנוצרו על ידי {username}',
-  'profile.universesBy': 'יצירת ייצוגים על ידי {username}',
-  'profile.fanFictionBy': 'יצירת פאן פיקשן על ידי {username}',
-}
+    'profile.works': 'עבודות',
+    // As in creative/literary works
+    'profile.wishlist': 'רשימת השאלות',
+    'profile.storiesBy': 'סיפורים שנוצרו על ידי {username}',
+    'profile.universesBy': 'יצירת ייצוגים על ידי {username}',
+    'profile.fanFictionBy': 'יצירת פאן פיקשן על ידי {username}'
+};
