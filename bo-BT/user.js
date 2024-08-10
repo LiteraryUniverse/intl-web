d138e34c319934eac5deb63c9cee7379040f0b97
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:38:24 +0200

    New translations user.js (Tibetan)

--- bo-BT/user.js
+++ bo-BT/user.js
@@ -1,54 +1,53 @@
 export default {
-  'user.friend.request.add': 'ཞིག་བསྟན་བཀོད་པ་བཟོ་བ།',
-  'user.friend.requests': `ཁྱེད་ཀྱིས་ལྷག་པ་ {num, number} གསར་འགྱུར་ {num, plural,
+    'user.friend.request.add': 'ཞིག་བསྟན་བཀོད་པ་བཟོ་བ།',
+    'user.friend.requests': `ཁྱེད་ཀྱིས་ལྷག་པ་ {num, number} གསར་འགྱུར་ {num, plural,
     zero {གསར་འགྱུར་འདེམས་}
     one {གསར་འགྱུར་}
     many {གསར་འགྱུར་}
     other {གསར་འགྱུར་}
   }`,
-  'user.friend.unfriend': 'ཞིག་བསྟན་མེད་པ་བཟོ་བ།',
-  'user.block': 'སྐུགས་མེད་པ་བཟོ་བ།',
-  'user.unblock': 'སྐུགས་མེད་མེད་པ་བཟོ་བ།',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'ཞིག་བསྟན་མེད་པ་བཟོ་བ།',
+    'user.block': 'སྐུགས་མེད་པ་བཟོ་བ།',
+    'user.unblock': 'སྐུགས་མེད་མེད་པ་བཟོ་བ།',
+    'user.joined': `{gender, select,
     male {བསམ་ཚུལ་}
     female {བསམ་ཚུལ་}
     other {བསམ་ཚུལ་}} ལས་ {date, date, long}`,
-  'user.friend.request.accept': 'ཞིག་བསྟན་ལག་ལེན་འབྱེད་པ་བཟོ་བ།',
-  'user.friend.request.cancel': 'ཞིག་བསྟན་ལག་ལེན་འབྱེད་པ་བཟོ་བ།',
-  'user.friend.request.deny': 'ཞིག་བསྟན་ལག་ལེན་འབྱེད་པ་བཟོ་བ།',
-  'user.listing': 'ལག་ལེན་པ་གི་རེ་རེ་བཞིན་པའི་དཔེ་ཚོགས།',
-  'user.profile.visit': '{user}གི་དཔེ་ཚོགས་ལེགས་པ་འབྱེད་པ་བཟོ་བ།',
-  'user.profile.avatar': '{username}གི་སྐུགས་པ།',
-  'user.friend.list.requests': 'ཞིག་བསྟན་ལག་ལེན་འབྱེད་པ་ཚོགས་པ།',
-  'user.friend.norequests': 'ཁྱེད་ཀྱིས་ལག་ལེན་འབྱེད་པ་མི་འདུག',
-  'user.dashboardWelcome': `བདག་སྐྱེད་ {dayPart, select,
+    'user.friend.request.accept': 'ཞིག་བསྟན་ལག་ལེན་འབྱེད་པ་བཟོ་བ།',
+    'user.friend.request.cancel': 'ཞིག་བསྟན་ལག་ལེན་འབྱེད་པ་བཟོ་བ།',
+    'user.friend.request.deny': 'ཞིག་བསྟན་ལག་ལེན་འབྱེད་པ་བཟོ་བ།',
+    'user.listing': 'ལག་ལེན་པ་གི་རེ་རེ་བཞིན་པའི་དཔེ་ཚོགས།',
+    'user.profile.visit': '{user}གི་དཔེ་ཚོགས་ལེགས་པ་འབྱེད་པ་བཟོ་བ།',
+    'user.profile.avatar': '{username}གི་སྐུགས་པ།',
+    'user.friend.list.requests': 'ཞིག་བསྟན་ལག་ལེན་འབྱེད་པ་ཚོགས་པ།',
+    'user.friend.norequests': 'ཁྱེད་ཀྱིས་ལག་ལེན་འབྱེད་པ་མི་འདུག',
+    'user.dashboardWelcome': `བདག་སྐྱེད་ {dayPart, select,
     morning {སྔ་དྲོ་}
     afternoon {ཉིན་གུ་}
     evening {དགོངས་}
     night {མཚོ་}
     other {ཉིན་གུ་}
   } {displayName}!`,
-  'user.verification.publishedAuthor':
-    'གསལ་བཤད་དཔེ་སྐྲུན་བཟོ་བ་ཡོད་པའི་དཔེར་ན་བཞུགས་པ་ཡོད་པའི་གྲོགས་ཀྱི་དཔེའི་ཚིག་ཡོད།',
-  'user.verification.luAuthor': 'རྩོམ་པ་དཔེ་སྐྲུན་བཟོ་བ་ཡོད་པའི་གྲོགས་ཀྱི་དཔེའི་ཚིག་ཡོད།',
-  'user.verification.luEmployee': 'རྩོམ་པ་དཔེ་སྐྲུན་བཟོ་བ་ཡོད་པའི་བརྡ་དོན་ཚིག་ཡོད།',
-  'user.verification.publicFigure': 'དགེ་འདུན་གྱི་མིང་གསལ་བཤད་པ།',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'གསལ་བཤད་དཔེ་སྐྲུན་བཟོ་བ་ཡོད་པའི་དཔེར་ན་བཞུགས་པ་ཡོད་པའི་གྲོགས་ཀྱི་དཔེའི་ཚིག་ཡོད།',
+    'user.verification.luAuthor': 'རྩོམ་པ་དཔེ་སྐྲུན་བཟོ་བ་ཡོད་པའི་གྲོགས་ཀྱི་དཔེའི་ཚིག་ཡོད།',
+    'user.verification.luEmployee': 'རྩོམ་པ་དཔེ་སྐྲུན་བཟོ་བ་ཡོད་པའི་བརྡ་དོན་ཚིག་ཡོད།',
+    'user.verification.publicFigure': 'དགེ་འདུན་གྱི་མིང་གསལ་བཤད་པ།',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {སྤྱི་ཚོགས་}
     other {སྤྱི་ཚོགས་}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {གསར་འགྱུར་}
     other {གསར་འགྱུར་}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {རྩོམ་པ་གསར་འགྱུར་}
     other {རྩོམ་པ་གསར་འགྱུར་} 
   }`,
-  'profile.works': 'ལས་འགན་བྱེད་པ་',
-  // As in creative/literary works
-  'profile.wishlist': 'དགའ་གདམས་',
-  'profile.storiesBy': '! {username}! ནང་དོན་ཚུ་གི་སྤྱི་ཚོགས་',
-  'profile.universesBy': '! {username}! ནང་དོན་ཚུ་གི་གསར་འགྱུར་',
-  'profile.fanFictionBy': '! {username}! ནང་དོན་ཚུ་གི་རྩོམ་པ་གསར་འགྱུར་',
-}
+    'profile.works': 'ལས་འགན་བྱེད་པ་',
+    // As in creative/literary works
+    'profile.wishlist': 'དགའ་གདམས་',
+    'profile.storiesBy': '! {username}! ནང་དོན་ཚུ་གི་སྤྱི་ཚོགས་',
+    'profile.universesBy': '! {username}! ནང་དོན་ཚུ་གི་གསར་འགྱུར་',
+    'profile.fanFictionBy': '! {username}! ནང་དོན་ཚུ་གི་རྩོམ་པ་གསར་འགྱུར་'
+};
