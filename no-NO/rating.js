ed2d18aa40c6783016bb36d95de7c6d160691653
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:23:40 +0200

    New translations rating.js (Norwegian)

--- no-NO/rating.js
+++ no-NO/rating.js
@@ -2,19 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Vurdering',
-  'rating.explained': 'Aldersvurdering og begrensning.',
-  'rating.all': 'Alle',
-  'rating.desc.all': 'Tilgjengelig for alle aldre og grupper.',
-  'rating.teens': 'Tenåringer',
-  'rating.desc.teens': 'Egnet for et publikum mellom 12 og 18 år.',
-  'rating.mature': 'Moden',
-  'rating.desc.mature': 'Inneholder avanserte temaer, grovt språk og implisitte scener.',
-  'rating.explicit': 'Eksplicit',
-  'rating.desc.explicit': 'Inneholder eksplisitte scener.',
-  'rating.selection.all': 'Alle - Tilgjengelig for alle aldre og grupper.',
-  'rating.selection.teens': 'Tenåringer (12-18) - Egnet for publikum mellom 12 og 18 år.',
-  'rating.selection.mature':
-    'Moden (18+) - Inneholder avanserte temaer, grovt språk og implisitte scener.',
-  'rating.selection.explicit': 'Eksplicit (21+) - Inneholder eksplisitte scener.',
-}
+    'rating.name': 'Vurdering',
+    'rating.explained': 'Aldersvurdering og begrensning.',
+    'rating.all': 'Alle',
+    'rating.desc.all': 'Tilgjengelig for alle aldre og grupper.',
+    'rating.teens': 'Tenåringer',
+    'rating.desc.teens': 'Egnet for et publikum mellom 12 og 18 år.',
+    'rating.mature': 'Moden',
+    'rating.desc.mature': 'Inneholder avanserte temaer, grovt språk og implisitte scener.',
+    'rating.explicit': 'Eksplicit',
+    'rating.desc.explicit': 'Inneholder eksplisitte scener.',
+    'rating.selection.all': 'Alle - Tilgjengelig for alle aldre og grupper.',
+    'rating.selection.teens': 'Tenåringer (12-18) - Egnet for publikum mellom 12 og 18 år.',
+    'rating.selection.mature': 'Moden (18+) - Inneholder avanserte temaer, grovt språk og implisitte scener.',
+    'rating.selection.explicit': 'Eksplicit (21+) - Inneholder eksplisitte scener.'
+};
