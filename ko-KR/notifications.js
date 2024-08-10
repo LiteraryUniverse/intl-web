f3fd019cfe6d8724bb208a4d2fdf91813b3ce1e4
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:22:12 +0200

    New translations notifications.js (Korean)

--- ko-KR/notifications.js
+++ ko-KR/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': '이 메시지를 ~에서 표시 시작',
   'flashnews.endsAt': '이 메시지를 ~에서 표시 중단',
   'flashnews.newLanguage': '추가할 새로운 언어 선택',
-  'flashnews.onlyDisplayOn':
-    '뉴스는 아래에서 선택한 언어에서만 표시됩니다. 선택한 언어가 없으면 기본 언어로 표시되는 다른 언어에서는 뉴스가 표시되지 않습니다.',
+  'flashnews.onlyDisplayOn': '뉴스는 아래에서 선택한 언어에서만 표시됩니다. 선택한 언어가 없으면 기본 언어로 표시되는 다른 언어에서는 뉴스가 표시되지 않습니다.',
   'notifications.menu.all': '모든 알림',
   'notifications.menu.following': '팔로우 중인 항목',
   'notifications.menu.workshop': '작업장',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': '읽음으로 표시',
   'notifications.menu.delete': '알림 삭제',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    '6개월간 활동이 없으면 이야기 {storyName} 가 자동으로 휴지 상태로 설정됩니다.',
-  'notification.workshopStoryStatus.toCancelled':
-    '1년간 활동이 없으면 이야기 {storyName} 가 자동으로 취소됩니다.',
+  'notification.workshopStoryStatus.toHiatus': '6개월간 활동이 없으면 이야기 {storyName} 가 자동으로 휴지 상태로 설정됩니다.',
+  'notification.workshopStoryStatus.toCancelled': '1년간 활동이 없으면 이야기 {storyName} 가 자동으로 취소됩니다.',
   'notification.followingStoryStatus.toHiatus': '{storyName} 가 휴지 상태입니다.',
   'notification.followingStoryStatus.toCancelled': '{storyName} 가 취소되었습니다.',
   'notification.followingNewChapter.new': '{storyName} 에 새로운 챕터가 발행되었습니다!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': '친구 요청을 보냈습니다.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': '이야기로 이동',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': '이야기로 이동'
+};
