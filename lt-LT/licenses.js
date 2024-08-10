3d95de09b78a426ff1006e1b483f4175833d6219
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:22:29 +0200

    New translations licenses.js (Lithuanian)

--- lt-LT/licenses.js
+++ lt-LT/licenses.js
@@ -3,31 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'Apie licencijas',
-  'licenses.more': 'Daugiau informacijos',
-  'licenses.legal': 'Teisinis dokumentas',
-  'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
-  'licenses.cc-by':
-    'Ši licencija leidžia kitiems platinti, keisti, pritaikyti ir remiksuoti jūsų darbą net komerciniais tikslais, jei tik pripažįsta jus kaip originalaus kūrinio autorių. Tai yra labiausiai priimtinų licencijų. Rekomenduojama siekiant maksimalaus licencijuotų medžiagų platinimo ir naudojimo.',
-  'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
-  'licenses.cc-by-sa':
-    'Ši licencija leidžia kitiems keisti, pritaikyti ir remiksuoti jūsų darbą net komerciniais tikslais, jei tik pripažįsta jus kaip originalaus kūrinio autorių ir licencijuoja savo naujus kūrinius pagal identiškus terminus. Ši licencija dažnai palyginama su „copyleft“ laisvos ir atviro kodo programinės įrangos licencijomis. Visi nauji jūsų darbais paremti kūriniai turės tą pačią licenciją, todėl bet kokie išvestiniai darbai taip pat leis komercinį naudojimą. Šią licenciją naudoja Vikipedija ir rekomenduojama medžiagoms, kurios gali pasinaudoti Vikipedijos ir panašiai licencijuotų projektų turiniu.',
-  'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
-  'licenses.cc-by-nd':
-    'Ši licencija leidžia platinti jūsų darbus, tiek komercinius, tiek nekomercinius, jei tik jie yra perduodami nepakeisti ir visiškai, su jūsų pripažinimu.',
-  'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
-  'licenses.cc-by-nc':
-    'Ši licencija leidžia kitiems keisti, pritaikyti ir remiksuoti jūsų darbą nekomerciniais tikslais, ir nors jų nauji darbai taip pat turi pripažinti jus ir būti nekomerciniai, jie neprivalo licencijuoti savo išvestinių darbų pagal tuos pačius terminus.',
-  'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
-  'licenses.cc-by-nc-sa':
-    'Ši licencija leidžia kitiems keisti, pritaikyti ir remiksuoti jūsų darbą nekomerciniais tikslais, jei tik pripažįsta jus ir licencijuoja savo naujus kūrinius pagal identiškus terminus.',
-  'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
-  'licenses.cc-by-nc-nd':
-    'Ši licencija yra griežčiausia iš šešių pagrindinių mūsų licencijų, leidžianti tik atsisiųsti jūsų darbus ir dalintis jais su kitais, jei tik pripažįsta jus, tačiau jie negali jų jokiu būdu keisti arba naudoti komerciniais tikslais.',
-  'licenses.name.lu-c': 'Literary Universe autorių teisės',
-  'licenses.lu-c':
-    'Standartinė pilnos autorių teisių licencija, kuri leidžia jums parduoti ir publikuoti savo darbą taip, kaip norite, ir apsaugoti savo teises prie jo. Ši licencija taip pat suteikia galimybę parduoti savo istorijas Literatūros Visatoje.',
-  'licenses.name.cc0': 'Nėra teisių saugomos - CC0 1.0',
-  'licenses.cc0':
-    'Dėkite savo darbą į viešąją nuosavybę. Skirtingai nuo CC licencijų, kurios leidžia autorių teisių turėtojams pasirinkti iš įvairių leidimų ir tuo pačiu išlaikyti savo autorių teises, CC0 suteikia dar vieną visiškai kitą pasirinkimą - pasirinkimą atsisakyti autorių teisių ir duomenų bazės apsaugos bei automatiškai suteiktų teisių kūrėjams - „nėra teisių saugomos“ alternatyvą mūsų licencijoms.',
-}
+    'licenses.about': 'Apie licencijas',
+    'licenses.more': 'Daugiau informacijos',
+    'licenses.legal': 'Teisinis dokumentas',
+    'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
+    'licenses.cc-by': 'Ši licencija leidžia kitiems platinti, keisti, pritaikyti ir remiksuoti jūsų darbą net komerciniais tikslais, jei tik pripažįsta jus kaip originalaus kūrinio autorių. Tai yra labiausiai priimtinų licencijų. Rekomenduojama siekiant maksimalaus licencijuotų medžiagų platinimo ir naudojimo.',
+    'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
+    'licenses.cc-by-sa': 'Ši licencija leidžia kitiems keisti, pritaikyti ir remiksuoti jūsų darbą net komerciniais tikslais, jei tik pripažįsta jus kaip originalaus kūrinio autorių ir licencijuoja savo naujus kūrinius pagal identiškus terminus. Ši licencija dažnai palyginama su „copyleft“ laisvos ir atviro kodo programinės įrangos licencijomis. Visi nauji jūsų darbais paremti kūriniai turės tą pačią licenciją, todėl bet kokie išvestiniai darbai taip pat leis komercinį naudojimą. Šią licenciją naudoja Vikipedija ir rekomenduojama medžiagoms, kurios gali pasinaudoti Vikipedijos ir panašiai licencijuotų projektų turiniu.',
+    'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
+    'licenses.cc-by-nd': 'Ši licencija leidžia platinti jūsų darbus, tiek komercinius, tiek nekomercinius, jei tik jie yra perduodami nepakeisti ir visiškai, su jūsų pripažinimu.',
+    'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
+    'licenses.cc-by-nc': 'Ši licencija leidžia kitiems keisti, pritaikyti ir remiksuoti jūsų darbą nekomerciniais tikslais, ir nors jų nauji darbai taip pat turi pripažinti jus ir būti nekomerciniai, jie neprivalo licencijuoti savo išvestinių darbų pagal tuos pačius terminus.',
+    'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
+    'licenses.cc-by-nc-sa': 'Ši licencija leidžia kitiems keisti, pritaikyti ir remiksuoti jūsų darbą nekomerciniais tikslais, jei tik pripažįsta jus ir licencijuoja savo naujus kūrinius pagal identiškus terminus.',
+    'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
+    'licenses.cc-by-nc-nd': 'Ši licencija yra griežčiausia iš šešių pagrindinių mūsų licencijų, leidžianti tik atsisiųsti jūsų darbus ir dalintis jais su kitais, jei tik pripažįsta jus, tačiau jie negali jų jokiu būdu keisti arba naudoti komerciniais tikslais.',
+    'licenses.name.lu-c': 'Literary Universe autorių teisės',
+    'licenses.lu-c': 'Standartinė pilnos autorių teisių licencija, kuri leidžia jums parduoti ir publikuoti savo darbą taip, kaip norite, ir apsaugoti savo teises prie jo. Ši licencija taip pat suteikia galimybę parduoti savo istorijas Literatūros Visatoje.',
+    'licenses.name.cc0': 'Nėra teisių saugomos - CC0 1.0',
+    'licenses.cc0': 'Dėkite savo darbą į viešąją nuosavybę. Skirtingai nuo CC licencijų, kurios leidžia autorių teisių turėtojams pasirinkti iš įvairių leidimų ir tuo pačiu išlaikyti savo autorių teises, CC0 suteikia dar vieną visiškai kitą pasirinkimą - pasirinkimą atsisakyti autorių teisių ir duomenų bazės apsaugos bei automatiškai suteiktų teisių kūrėjams - „nėra teisių saugomos“ alternatyvą mūsų licencijoms.'
+};
