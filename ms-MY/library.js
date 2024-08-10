105c25080580cf72e249192118182c4fd3eea0e1
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:35:00 +0200

    New translations library.js (Malay)

--- ms-MY/library.js
+++ ms-MY/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'Tambah ke perpustakaan',
-  'library.remove': 'Buang dari perpustakaan',
-  'library.bought': 'Cerita yang dibeli',
-  'library.stories': 'Cerita yang diikuti',
-  'library.universes': 'Diikuti alam semesta',
-  'bookmark.continueReading': 'Teruskan membaca',
-  'bookmark.statusSummary': 'Anda telah membaca {readPercent, number, ::percent} daripada cerita.',
-}
+    'library.add': 'Tambah ke perpustakaan',
+    'library.remove': 'Buang dari perpustakaan',
+    'library.bought': 'Cerita yang dibeli',
+    'library.stories': 'Cerita yang diikuti',
+    'library.universes': 'Diikuti alam semesta',
+    'bookmark.continueReading': 'Teruskan membaca',
+    'bookmark.statusSummary': 'Anda telah membaca {readPercent, number, ::percent} daripada cerita.'
+};
