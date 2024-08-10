01ee4453b461b0bbf9f80476645d75938da3b037
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:33:36 +0200

    New translations enrollment.js (Estonian)

--- et-EE/enrollment.js
+++ et-EE/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'Nõua oma kontot!',
-  'enrollment.description':
-    'Tere tulemast {username}! Selleks, et saada juurdepääs oma erikontole, pead ainult looma parooli.',
-  'enrollment.submitting': 'Konto nõudmine...',
-  'enrollment.submit': 'Nõua oma kontot!',
-}
+    'enrollment.title': 'Nõua oma kontot!',
+    'enrollment.description': 'Tere tulemast {username}! Selleks, et saada juurdepääs oma erikontole, pead ainult looma parooli.',
+    'enrollment.submitting': 'Konto nõudmine...',
+    'enrollment.submit': 'Nõua oma kontot!'
+};
