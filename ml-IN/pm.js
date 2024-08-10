973c6181e97d418a020f389469a19cfe0cf87db2
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:37:40 +0200

    New translations pm.js (Malayalam)

--- ml-IN/pm.js
+++ ml-IN/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'സംഭാഷണം',
-  'pm.showolder': 'പഴയ സന്ദേശങ്ങൾ കാണിക്കുക',
-  'pm.errors.saysomething': 'നിങ്ങൾ നിലനിൽക്കേണ്ടത് ഒന്ന് പറയണം...',
-  'pm.errors.addrecipient': 'കൂടുതൽ ഒരു ഉപയോക്താവിനെ ചേർക്കേണ്ടതുണ്ട്.',
-  'pm.message': 'സന്ദേശം',
-  'pm.send.message': 'സന്ദേശം അയയ്‌ക്കുക',
-  'pm.messages.own': 'നിങ്ങളുടെ സന്ദേശങ്ങൾ',
-  'pm.conversations.none': 'നിങ്ങൾ പറയുന്നവർക്ക് ഇതുവരെ സംസാരിച്ചിട്ടില്ല.',
-  'pm.to': 'വിരസമായി:',
-  'pm.with': 'സംഭാഷണം:',
-  'pm.send': 'അയയ്‌ക്കുക',
-  'pm.conversations.unreadCount': `നിങ്ങൾക്ക് {count, plural,
+    'pm.conversation': 'സംഭാഷണം',
+    'pm.showolder': 'പഴയ സന്ദേശങ്ങൾ കാണിക്കുക',
+    'pm.errors.saysomething': 'നിങ്ങൾ നിലനിൽക്കേണ്ടത് ഒന്ന് പറയണം...',
+    'pm.errors.addrecipient': 'കൂടുതൽ ഒരു ഉപയോക്താവിനെ ചേർക്കേണ്ടതുണ്ട്.',
+    'pm.message': 'സന്ദേശം',
+    'pm.send.message': 'സന്ദേശം അയയ്‌ക്കുക',
+    'pm.messages.own': 'നിങ്ങളുടെ സന്ദേശങ്ങൾ',
+    'pm.conversations.none': 'നിങ്ങൾ പറയുന്നവർക്ക് ഇതുവരെ സംസാരിച്ചിട്ടില്ല.',
+    'pm.to': 'വിരസമായി:',
+    'pm.with': 'സംഭാഷണം:',
+    'pm.send': 'അയയ്‌ക്കുക',
+    'pm.conversations.unreadCount': `നിങ്ങൾക്ക് {count, plural,
     zero {വായിക്കാൻ സംസാരങ്ങൾ ഇല്ലെന്ന്}
     one {ഒരു വായിക്കാൻ സംസാരം ഉണ്ട്}
     other {# വായിക്കാൻ സംസാരങ്ങൾ ഉണ്ട്}
   }.`,
-  'pm.form.minimumParticipants': 'നിങ്ങൾക്ക് ഒരു സ്വീകരിച്ചവരെ തിരഞ്ഞെടുക്കേണ്ടതുണ്ട്.',
-  'pm.form.maxParticipants': `നിങ്ങൾക്ക് ഒരു സംഭാഷണത്തിൽ പരമാവധി {maxPeople, plural,
+    'pm.form.minimumParticipants': 'നിങ്ങൾക്ക് ഒരു സ്വീകരിച്ചവരെ തിരഞ്ഞെടുക്കേണ്ടതുണ്ട്.',
+    'pm.form.maxParticipants': `നിങ്ങൾക്ക് ഒരു സംഭാഷണത്തിൽ പരമാവധി {maxPeople, plural,
     one {ഒരു ആളുണ്ടാക്കാൻ കഴിയും}
     other {# ആളുകൾ}
   }`,
-  'pm.new.aria': 'പുതിയ സംഭാഷണം',
-  'pm.new.buttonTitle': 'പുതിയ സംഭാഷണം ആരംഭിക്കുക',
-}
+    'pm.new.aria': 'പുതിയ സംഭാഷണം',
+    'pm.new.buttonTitle': 'പുതിയ സംഭാഷണം ആരംഭിക്കുക'
+};
