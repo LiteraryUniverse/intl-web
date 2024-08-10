223e45900511e65e3fdde4e01c011f755ed91a0b
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:22:39 +0200

    New translations comments.js (Lithuanian)

--- lt-LT/comments.js
+++ lt-LT/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': 'Įrašykite savo komentarą',
-  'comments.none': 'Atsiprašome, nėra komentarų rodyti.',
-  'comments.total': `There {count, plural,
+    'comment.write': 'Įrašykite savo komentarą',
+    'comments.none': 'Atsiprašome, nėra komentarų rodyti.',
+    'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-  'comments.show.older': 'Rodyti senesnius komentarus',
-}
+    'comments.show.older': 'Rodyti senesnius komentarus'
+};
