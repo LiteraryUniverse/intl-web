cbfe499394fb1978f327b42a133b38b7d9317b7c
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:31:42 +0200

    New translations user.js (Tamil)

--- ta-IN/user.js
+++ ta-IN/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'நண்பர்களில் சேர்க்கவும்',
-  'user.friend.requests': `உங்களுக்கு {num, number} புதிய நண்பர் {num, plural,
+    'user.friend.request.add': 'நண்பர்களில் சேர்க்கவும்',
+    'user.friend.requests': `உங்களுக்கு {num, number} புதிய நண்பர் {num, plural,
     zero {கோரிக்கைகள்}
     one {கோரிக்கை}
     many {கோரிக்கைகள்}
     other {கோரிக்கைகள்}
   }`,
-  'user.friend.unfriend': 'நண்பர்களிலிருந்து நீக்கு',
-  'user.block': 'தடுக்க',
-  'user.unblock': 'தடுப்பை நீக்கு',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'நண்பர்களிலிருந்து நீக்கு',
+    'user.block': 'தடுக்க',
+    'user.unblock': 'தடுப்பை நீக்கு',
+    'user.joined': `{gender, select,
     male {இன் சேர்ந்துள்ளீர்கள்}
     female {இன் சேர்ந்துள்ளீர்கள்}
     other {இன் சேர்ந்துள்ளீர்கள்}} இன் சேர்ந்துள்ளீர்கள் {date, date, long}இல்`,
-  'user.friend.request.accept': 'நண்பர்களாக கேட்கையை ஏற்றுக்கொள்',
-  'user.friend.request.cancel': 'நண்பர்களாக கேட்கையை ரத்து செய்',
-  'user.friend.request.deny': 'நண்பர் விலக்கு',
-  'user.listing': 'பயனர் பட்டியல்',
-  'user.profile.visit': 'பார்வை செய்ய {user}இன் சுயவிவரத்தை.',
-  'user.profile.avatar': '{username}இன் அவதாரம்',
-  'user.friend.list.requests': 'நண்பர்களாக கேட்கைகள்',
-  'user.friend.norequests': 'நீங்கள் நண்பர்களாக்க கேட்கைகள் இல்லை.',
-  'user.dashboardWelcome': `காலை வணக்கம் {dayPart, select,
+    'user.friend.request.accept': 'நண்பர்களாக கேட்கையை ஏற்றுக்கொள்',
+    'user.friend.request.cancel': 'நண்பர்களாக கேட்கையை ரத்து செய்',
+    'user.friend.request.deny': 'நண்பர் விலக்கு',
+    'user.listing': 'பயனர் பட்டியல்',
+    'user.profile.visit': 'பார்வை செய்ய {user}இன் சுயவிவரத்தை.',
+    'user.profile.avatar': '{username}இன் அவதாரம்',
+    'user.friend.list.requests': 'நண்பர்களாக கேட்கைகள்',
+    'user.friend.norequests': 'நீங்கள் நண்பர்களாக்க கேட்கைகள் இல்லை.',
+    'user.dashboardWelcome': `காலை வணக்கம் {dayPart, select,
     morning {மதிய வணக்கம்}
     afternoon {மாலை வணக்கம்}
     evening {இரவு வணக்கம்}
     night {இரவு}
     other {நாள்}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'இந்த ஆசிரியர் பாரம்பரிய வழியில் வெளியிடப்பட்டுள்ளார்',
-  'user.verification.luAuthor': 'இலக்கிய உலகம் அடிப்படையில் ஆசிரியர்',
-  'user.verification.luEmployee': 'இலக்கிய உலகம் பணியாளர்',
-  'user.verification.publicFigure': 'சரிபார்ப்பப்பட்ட பொது பட்டியல்',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'இந்த ஆசிரியர் பாரம்பரிய வழியில் வெளியிடப்பட்டுள்ளார்',
+    'user.verification.luAuthor': 'இலக்கிய உலகம் அடிப்படையில் ஆசிரியர்',
+    'user.verification.luEmployee': 'இலக்கிய உலகம் பணியாளர்',
+    'user.verification.publicFigure': 'சரிபார்ப்பப்பட்ட பொது பட்டியல்',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {கதை}
     other {கதைகள்}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {உலகம்}
     other {உலகங்கள்}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {புத்தகம்}
     other {புத்தகங்கள்} 
   }`,
-  'profile.works': 'பணி',
-  // As in creative/literary works
-  'profile.wishlist': 'விருப்பப் பட்டியல்',
-  'profile.storiesBy': '{username}ஆல் உருப்படிகள் உருவாக்கிய கதைகள்',
-  'profile.universesBy': '{username}ஆல் உருவாக்கிய உலகங்கள்',
-  'profile.fanFictionBy': '{username}ஆல் உருவாக்கிய புத்தகங்கள்',
-}
+    'profile.works': 'பணி',
+    // As in creative/literary works
+    'profile.wishlist': 'விருப்பப் பட்டியல்',
+    'profile.storiesBy': '{username}ஆல் உருப்படிகள் உருவாக்கிய கதைகள்',
+    'profile.universesBy': '{username}ஆல் உருவாக்கிய உலகங்கள்',
+    'profile.fanFictionBy': '{username}ஆல் உருவாக்கிய புத்தகங்கள்'
+};
