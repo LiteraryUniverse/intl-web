e1e3733ec3f08cbc15bfff1b9c7249c748754db0
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:26:57 +0200

    New translations licenses.js (Swedish)

--- sv-SE/licenses.js
+++ sv-SE/licenses.js
@@ -3,31 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'Om licenser',
-  'licenses.more': 'Mer information',
-  'licenses.legal': 'Juridiska dokument',
-  'licenses.name.cc-by': 'Creative Commons Erkännande 4.0',
-  'licenses.cc-by':
-    'Denna licens gör det möjligt för andra att distribuera, remixa, modifiera och bygga vidare på ditt arbete, även kommersiellt, så länge de anger att du är den ursprungliga skaparen. Detta är den mest tillmötesgående av de licenser som erbjuds. Rekommenderas för maximal spridning och användning av licensierat material.',
-  'licenses.name.cc-by-sa': 'Creative Commons Erkännande-DelaLika 4.0',
-  'licenses.cc-by-sa':
-    'Denna licens låter andra remixa, anpassa och bygga vidare på ditt verk även för kommersiella ändamål, så länge de anger dig som upphovsman och licensierar sina nya verk med exakt samma villkor. Denna licens jämförs ofta med licenser för fri programvara med öppen källkod. Alla nya verk baserade på ditt kommer att ha samma licens, så alla derivat kommer också att tillåta kommersiell användning. Detta är den licens som används av Wikipedia, och rekommenderas för sådant som skulle gynnas av att införliva innehåll från Wikipedia och liknande licensierade projekt.',
-  'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
-  'licenses.cc-by-nd':
-    'Denna licens tillåter vidaredistribution, kommersiellt och icke-kommersiellt, så länge som den skickas vidare oförändrad och i sin helhet, med angivande av dig.',
-  'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
-  'licenses.cc-by-nc':
-    'Denna licens låter andra remixa, ändra och bygga på ditt verk för icke-kommersiella syften. Deras nya verk måste också erkänna dig som upphovsman och vara icke-kommersiella men de måste inte licensiera sin bearbetning på samma villkor.',
-  'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
-  'licenses.cc-by-nc-sa':
-    'Denna licens låter andra remixa, anpassa och bygga vidare på ditt verk i icke-kommersiella syften så länge de erkänner dig och licensierar sina bearbetningar under samma villkor.',
-  'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
-  'licenses.cc-by-nc-nd':
-    'Denna licens är den mest restriktiva av våra sex huvudlicenser. Den tillåter bara andra att ladda ner dina verk och dela dem med andra så länge de erkänner dig som upphovsman. De får inte ändra dem på något sätt eller använda dem kommersiellt.',
-  'licenses.name.lu-c': 'Litterära Universum Upphovsrätt',
-  'licenses.lu-c':
-    'Standardlicens med fullständiga upphovsrättigheter som gör att du kan sälja och publicera ditt verk som du vill och få dina rättigheter skyddade. Denna licens ger dig också möjlighet att sälja dina berättelser på Literary Universe.',
-  'licenses.name.cc0': 'Inga rättigheter reserverade - CC0 1,0',
-  'licenses.cc0':
-    'Gör ditt arbete offentligt tillgängligt. I motsats till CC-licenser som låter upphovsrättsinnehavare välja mellan en rad olika tillstånd samtidigt som de behåller sin upphovsrätt, ger CC0 ett helt annat val - valet att välja bort upphovsrätts- och databasskydd och de exklusiva rättigheter som automatiskt beviljas upphovsmännen - alternativet "inga rättigheter förbehållna" till våra licenser.',
-}
+    'licenses.about': 'Om licenser',
+    'licenses.more': 'Mer information',
+    'licenses.legal': 'Juridiska dokument',
+    'licenses.name.cc-by': 'Creative Commons Erkännande 4.0',
+    'licenses.cc-by': 'Denna licens gör det möjligt för andra att distribuera, remixa, modifiera och bygga vidare på ditt arbete, även kommersiellt, så länge de anger att du är den ursprungliga skaparen. Detta är den mest tillmötesgående av de licenser som erbjuds. Rekommenderas för maximal spridning och användning av licensierat material.',
+    'licenses.name.cc-by-sa': 'Creative Commons Erkännande-DelaLika 4.0',
+    'licenses.cc-by-sa': 'Denna licens låter andra remixa, anpassa och bygga vidare på ditt verk även för kommersiella ändamål, så länge de anger dig som upphovsman och licensierar sina nya verk med exakt samma villkor. Denna licens jämförs ofta med licenser för fri programvara med öppen källkod. Alla nya verk baserade på ditt kommer att ha samma licens, så alla derivat kommer också att tillåta kommersiell användning. Detta är den licens som används av Wikipedia, och rekommenderas för sådant som skulle gynnas av att införliva innehåll från Wikipedia och liknande licensierade projekt.',
+    'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
+    'licenses.cc-by-nd': 'Denna licens tillåter vidaredistribution, kommersiellt och icke-kommersiellt, så länge som den skickas vidare oförändrad och i sin helhet, med angivande av dig.',
+    'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
+    'licenses.cc-by-nc': 'Denna licens låter andra remixa, ändra och bygga på ditt verk för icke-kommersiella syften. Deras nya verk måste också erkänna dig som upphovsman och vara icke-kommersiella men de måste inte licensiera sin bearbetning på samma villkor.',
+    'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
+    'licenses.cc-by-nc-sa': 'Denna licens låter andra remixa, anpassa och bygga vidare på ditt verk i icke-kommersiella syften så länge de erkänner dig och licensierar sina bearbetningar under samma villkor.',
+    'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
+    'licenses.cc-by-nc-nd': 'Denna licens är den mest restriktiva av våra sex huvudlicenser. Den tillåter bara andra att ladda ner dina verk och dela dem med andra så länge de erkänner dig som upphovsman. De får inte ändra dem på något sätt eller använda dem kommersiellt.',
+    'licenses.name.lu-c': 'Litterära Universum Upphovsrätt',
+    'licenses.lu-c': 'Standardlicens med fullständiga upphovsrättigheter som gör att du kan sälja och publicera ditt verk som du vill och få dina rättigheter skyddade. Denna licens ger dig också möjlighet att sälja dina berättelser på Literary Universe.',
+    'licenses.name.cc0': 'Inga rättigheter reserverade - CC0 1,0',
+    'licenses.cc0': 'Gör ditt arbete offentligt tillgängligt. I motsats till CC-licenser som låter upphovsrättsinnehavare välja mellan en rad olika tillstånd samtidigt som de behåller sin upphovsrätt, ger CC0 ett helt annat val - valet att välja bort upphovsrätts- och databasskydd och de exklusiva rättigheter som automatiskt beviljas upphovsmännen - alternativet "inga rättigheter förbehållna" till våra licenser.'
+};
