b4b52826249839c45e8acaa00095625ef7f64bb6
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:26:52 +0200

    New translations enrollment.js (Swedish)

--- sv-SE/enrollment.js
+++ sv-SE/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'Gör anspråk på ditt konto!',
-  'enrollment.description':
-    'Välkommen {username}! För att få tillgång till ditt särskilda konto behöver du bara skapa ett lösenord.',
-  'enrollment.submitting': 'Att hävda ditt konto...',
-  'enrollment.submit': 'Gör anspråk på ditt konto!',
-}
+    'enrollment.title': 'Gör anspråk på ditt konto!',
+    'enrollment.description': 'Välkommen {username}! För att få tillgång till ditt särskilda konto behöver du bara skapa ett lösenord.',
+    'enrollment.submitting': 'Att hävda ditt konto...',
+    'enrollment.submit': 'Gör anspråk på ditt konto!'
+};
