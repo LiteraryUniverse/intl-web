38ab646fda31655aa356ef5bd36da2f202ce5a15
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:35:59 +0200

    New translations pm.js (Esperanto)

--- eo-UY/pm.js
+++ eo-UY/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'Konversacio',
-  'pm.showolder': 'Montri pli malnovajn mesaĝojn',
-  'pm.errors.saysomething': 'Vi vere devus diri ion...',
-  'pm.errors.addrecipient': 'Vi devas aldoni almenaŭ unu alian uzanton.',
-  'pm.message': 'Mesaĝo',
-  'pm.send.message': 'Sendi mesaĝon',
-  'pm.messages.own': 'Viaj mesaĝoj',
-  'pm.conversations.none': 'Vi nun ne konversacias kun iu ajn.',
-  'pm.to': 'Al:',
-  'pm.with': 'Konversacio kun:',
-  'pm.send': 'Sendi',
-  'pm.conversations.unreadCount': `Vi havas {count, plural,
+    'pm.conversation': 'Konversacio',
+    'pm.showolder': 'Montri pli malnovajn mesaĝojn',
+    'pm.errors.saysomething': 'Vi vere devus diri ion...',
+    'pm.errors.addrecipient': 'Vi devas aldoni almenaŭ unu alian uzanton.',
+    'pm.message': 'Mesaĝo',
+    'pm.send.message': 'Sendi mesaĝon',
+    'pm.messages.own': 'Viaj mesaĝoj',
+    'pm.conversations.none': 'Vi nun ne konversacias kun iu ajn.',
+    'pm.to': 'Al:',
+    'pm.with': 'Konversacio kun:',
+    'pm.send': 'Sendi',
+    'pm.conversations.unreadCount': `Vi havas {count, plural,
     zero {neniu nelegita konversacio}
     one {unu nelegita konversacio}
     other {# nelegitaj konversacioj}
   }.`,
-  'pm.form.minimumParticipants': 'Vi devas elekti ricevanton.',
-  'pm.form.maxParticipants': `Vi povas havi maksimume {maxPeople, plural,
+    'pm.form.minimumParticipants': 'Vi devas elekti ricevanton.',
+    'pm.form.maxParticipants': `Vi povas havi maksimume {maxPeople, plural,
     one {unu persono}
     other {# personoj}
   } en konversacio.`,
-  'pm.new.aria': 'Nova konversacio',
-  'pm.new.buttonTitle': 'Komenci novan konversacion',
-}
+    'pm.new.aria': 'Nova konversacio',
+    'pm.new.buttonTitle': 'Komenci novan konversacion'
+};
