9aec4cf281154ecb3aea7aabe87c8ece23eccc9e
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:27:02 +0200

    New translations rating.js (Swedish)

--- sv-SE/rating.js
+++ sv-SE/rating.js
@@ -2,19 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Betyg',
-  'rating.explained': 'Åldersbetyg och begränsning.',
-  'rating.all': 'Alla',
-  'rating.desc.all': 'Tillgängligt för alla åldrar och grupper.',
-  'rating.teens': 'Tonåringar',
-  'rating.desc.teens': 'Lämpligt för en publik mellan 12 och 18 år.',
-  'rating.mature': 'Mogen',
-  'rating.desc.mature': 'Innehåller avancerade teman, grovt språk och implicita scener.',
-  'rating.explicit': 'Explicit',
-  'rating.desc.explicit': 'Innehåller explicita scener.',
-  'rating.selection.all': 'Alla - Tillgängligt för alla åldrar och grupper.',
-  'rating.selection.teens': 'Tonåringar (12-18) - Lämpligt för publik mellan 12 och 18 år.',
-  'rating.selection.mature':
-    'Mogen (18+) - Innehåller avancerade teman, grovt språk och implicita scener.',
-  'rating.selection.explicit': 'Explicit (21+) - Innehåller explicita scener.',
-}
+    'rating.name': 'Betyg',
+    'rating.explained': 'Åldersbetyg och begränsning.',
+    'rating.all': 'Alla',
+    'rating.desc.all': 'Tillgängligt för alla åldrar och grupper.',
+    'rating.teens': 'Tonåringar',
+    'rating.desc.teens': 'Lämpligt för en publik mellan 12 och 18 år.',
+    'rating.mature': 'Mogen',
+    'rating.desc.mature': 'Innehåller avancerade teman, grovt språk och implicita scener.',
+    'rating.explicit': 'Explicit',
+    'rating.desc.explicit': 'Innehåller explicita scener.',
+    'rating.selection.all': 'Alla - Tillgängligt för alla åldrar och grupper.',
+    'rating.selection.teens': 'Tonåringar (12-18) - Lämpligt för publik mellan 12 och 18 år.',
+    'rating.selection.mature': 'Mogen (18+) - Innehåller avancerade teman, grovt språk och implicita scener.',
+    'rating.selection.explicit': 'Explicit (21+) - Innehåller explicita scener.'
+};
