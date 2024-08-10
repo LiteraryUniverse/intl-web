71aafc6d22a7a2e653beb5d747ca2cfaa46f7155
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:40:37 +0200

    New translations user.js (Odia)

--- or-IN/user.js
+++ or-IN/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'ସହପାଠୀକୁ ଯୋଡ଼ନ୍ତୁ',
-  'user.friend.requests': `ଆପଣଙ୍କୁ {num, number} ନୂଆ ସହପାଠୀ {num, plural,
+    'user.friend.request.add': 'ସହପାଠୀକୁ ଯୋଡ଼ନ୍ତୁ',
+    'user.friend.requests': `ଆପଣଙ୍କୁ {num, number} ନୂଆ ସହପାଠୀ {num, plural,
     zero {ଅନୁରୋଧ ଅଛନ୍ତି}
     one {ଅନୁରୋଧ}
     many {ଅନୁରୋଧଗୁଡ଼ିକ}
     other {ଅନୁରୋଧଗୁଡ଼ିକ}
   }`,
-  'user.friend.unfriend': 'ସହପାଠୀତ୍ୟ ଛେଡ଼ିବା',
-  'user.block': 'ବନ୍ଦି କରନ୍ତୁ',
-  'user.unblock': 'ଅନବନ୍ଦିତ କରନ୍ତୁ',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'ସହପାଠୀତ୍ୟ ଛେଡ଼ିବା',
+    'user.block': 'ବନ୍ଦି କରନ୍ତୁ',
+    'user.unblock': 'ଅନବନ୍ଦିତ କରନ୍ତୁ',
+    'user.joined': `{gender, select,
     male {ଯୋଡ଼ିବାରେ}
     female {ଯୋଡ଼ିବାରେ}
     other {ଯୋଡ଼ିବାରେ}} {date, date, long}ରେ ସଂଯୁକ୍ତ ହୋଇଛନ୍ତି`,
-  'user.friend.request.accept': 'ସହପାଠୀତ୍ୟ ଅନୁଗ୍ରହ କରନ୍ତୁ',
-  'user.friend.request.cancel': 'ସହପାଠୀତ୍ୟ ଅନୁଗ୍ରହ ରଦ୍ଦ କରନ୍ତୁ',
-  'user.friend.request.deny': 'ସହପାଠୀତ୍ୟ ନିରାକରଣ କରନ୍ତୁ',
-  'user.listing': 'ବ୍ୟବହାରକାରୀ ତାଲିକା',
-  'user.profile.visit': 'ଦର୍ଶନ କରନ୍ତୁ {user}ର ପ୍ରୋଫାଇଲ',
-  'user.profile.avatar': '{username}ର ଆଭାଟାର',
-  'user.friend.list.requests': 'ସହପାଠୀତ୍ୟ ଅନୁଗ୍ରହ ଅଭିରେକ',
-  'user.friend.norequests': 'ଆପଣଙ୍କର କିମ୍ବା ପାଠକର ପାଠକରେ କୌଣସି ଅନୁଗ୍ରହ ଅଭିରେକ ନାହିଁ',
-  'user.dashboardWelcome': `ସୁବେଳା {dayPart, select,
+    'user.friend.request.accept': 'ସହପାଠୀତ୍ୟ ଅନୁଗ୍ରହ କରନ୍ତୁ',
+    'user.friend.request.cancel': 'ସହପାଠୀତ୍ୟ ଅନୁଗ୍ରହ ରଦ୍ଦ କରନ୍ତୁ',
+    'user.friend.request.deny': 'ସହପାଠୀତ୍ୟ ନିରାକରଣ କରନ୍ତୁ',
+    'user.listing': 'ବ୍ୟବହାରକାରୀ ତାଲିକା',
+    'user.profile.visit': 'ଦର୍ଶନ କରନ୍ତୁ {user}ର ପ୍ରୋଫାଇଲ',
+    'user.profile.avatar': '{username}ର ଆଭାଟାର',
+    'user.friend.list.requests': 'ସହପାଠୀତ୍ୟ ଅନୁଗ୍ରହ ଅଭିରେକ',
+    'user.friend.norequests': 'ଆପଣଙ୍କର କିମ୍ବା ପାଠକର ପାଠକରେ କୌଣସି ଅନୁଗ୍ରହ ଅଭିରେକ ନାହିଁ',
+    'user.dashboardWelcome': `ସୁବେଳା {dayPart, select,
     morning {ମଧ୍ୟାହ୍ନ}
     afternoon {ସନ୍ଧ୍ୟା}
     evening {ରାତି}
     night {ରେ}
     other {ଦିନ}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'ଏହି ଲେଖକଟି ପାଠ୍ୟକୃତ ପ୍ରକାଶନ ପ୍ରଣାଳୀରେ ପ୍ରକାଶିତ ହୋଇଛି',
-  'user.verification.luAuthor': 'ଲିଟେରରୀ ଇୟୁନିଭର୍ସ ଆଧାରିତ ଲେଖକ',
-  'user.verification.luEmployee': 'ଲିଟେରରୀ ଇୟୁନିଭର୍ସ କର୍ମଚାରୀ',
-  'user.verification.publicFigure': 'ସ୍ଥାନିକ ନାମରେ ପ୍ରମାଣିତ ବ୍ୟକ୍ତି',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'ଏହି ଲେଖକଟି ପାଠ୍ୟକୃତ ପ୍ରକାଶନ ପ୍ରଣାଳୀରେ ପ୍ରକାଶିତ ହୋଇଛି',
+    'user.verification.luAuthor': 'ଲିଟେରରୀ ଇୟୁନିଭର୍ସ ଆଧାରିତ ଲେଖକ',
+    'user.verification.luEmployee': 'ଲିଟେରରୀ ଇୟୁନିଭର୍ସ କର୍ମଚାରୀ',
+    'user.verification.publicFigure': 'ସ୍ଥାନିକ ନାମରେ ପ୍ରମାଣିତ ବ୍ୟକ୍ତି',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {କାହାଣୀ}
     other {କାହାଣୀଗୁଡ଼ିକ}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {ବିଶ୍ବ}
     other {ବିଶ୍ବଗୁଡ଼ିକ}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {ଫାନ୍ ଫିକ୍ସନ୍}
     other {ଫାନ୍ ଫିକ୍ସନ୍ଗୁଡ଼ିକ} 
   }`,
-  'profile.works': 'କାର୍ଯ୍ୟଗୁଡ଼ିକ',
-  // As in creative/literary works
-  'profile.wishlist': 'ଇଚ୍ଛାପତ୍ର',
-  'profile.storiesBy': '{username}ଦ୍ବାରା ସୃଷ୍ଟିକୃତ କାହାଣୀଗୁଡ଼ିକ',
-  'profile.universesBy': '{username}ଦ୍ବାରା ସୃଷ୍ଟିକୃତ ବିଶ୍ବଗୁଡ଼ିକ',
-  'profile.fanFictionBy': '{username}ଦ୍ବାରା ସୃଷ୍ଟିକୃତ ଫାନ୍ ଫିକ୍ସନ୍',
-}
+    'profile.works': 'କାର୍ଯ୍ୟଗୁଡ଼ିକ',
+    // As in creative/literary works
+    'profile.wishlist': 'ଇଚ୍ଛାପତ୍ର',
+    'profile.storiesBy': '{username}ଦ୍ବାରା ସୃଷ୍ଟିକୃତ କାହାଣୀଗୁଡ଼ିକ',
+    'profile.universesBy': '{username}ଦ୍ବାରା ସୃଷ୍ଟିକୃତ ବିଶ୍ବଗୁଡ଼ିକ',
+    'profile.fanFictionBy': '{username}ଦ୍ବାରା ସୃଷ୍ଟିକୃତ ଫାନ୍ ଫିକ୍ସନ୍'
+};
