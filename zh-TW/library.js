0a8b31f58de4afc7e4b23364f8c95f5601ed3638
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:28:49 +0200

    New translations library.js (Chinese Traditional)

--- zh-TW/library.js
+++ zh-TW/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': '加入圖書館',
-  'library.remove': '從圖書館移除',
-  'library.bought': '已購買的故事',
-  'library.stories': '已追蹤的故事',
-  'library.universes': '已追蹤的宇宙',
-  'bookmark.continueReading': '繼續閱讀',
-  'bookmark.statusSummary': '你已閱讀 {readPercent, number, ::percent} 篇故事。',
-}
+    'library.add': '加入圖書館',
+    'library.remove': '從圖書館移除',
+    'library.bought': '已購買的故事',
+    'library.stories': '已追蹤的故事',
+    'library.universes': '已追蹤的宇宙',
+    'bookmark.continueReading': '繼續閱讀',
+    'bookmark.statusSummary': '你已閱讀 {readPercent, number, ::percent} 篇故事。'
+};
