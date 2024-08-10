6369c00b9ca171c46e75339bd1b76c5854904d71
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:34:09 +0200

    New translations enrollment.js (Hindi)

--- hi-IN/enrollment.js
+++ hi-IN/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'अपने खाते को दावा करें!',
-  'enrollment.description':
-    'आपका स्वागत है {username}! अपने विशेष खाते तक पहुंच पाने के लिए आपको केवल पासवर्ड बनाना होगा।',
-  'enrollment.submitting': 'अपने खाते को दावा कर रहे हैं...',
-  'enrollment.submit': 'अपने खाते को दावा करें!',
-}
+    'enrollment.title': 'अपने खाते को दावा करें!',
+    'enrollment.description': 'आपका स्वागत है {username}! अपने विशेष खाते तक पहुंच पाने के लिए आपको केवल पासवर्ड बनाना होगा।',
+    'enrollment.submitting': 'अपने खाते को दावा कर रहे हैं...',
+    'enrollment.submit': 'अपने खाते को दावा करें!'
+};
