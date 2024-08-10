3a051557a1fa4704b7a3077adb8803f9da70620c
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:41:06 +0200

    New translations legal.js (Nepali)

--- ne-NP/legal.js
+++ ne-NP/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'क्षमा गर्नुहोस्। तर अघि बढ्न अघि, हामीले आफ्नो सेवा सङ्कलन, गोपनीयता नीति र कानुनी अधिकार सम्बन्धमा तपाईंलाई जागरूक गराउनुपर्छ र तपाईंले सहमत छन् भन्ने नियमहरूलाई।',
-  'legal.modal.agree': 'म सहमत छु',
-  'legal.modal.disagree': 'म सहमत छैन',
-  'legal.modal.register': 'तपाईंले यसलाई थप देख्नुहुन्छैन यदि तपाईं दर्ता गर्नुहुन्छ।',
-  'legal.modal.goToSettings': 'तपाईंको सेटिङ्गमा जानुहोस्',
-  'legal.modal.oneLastThing': 'अन्तिम कुरा...',
-  'legal.modal.finish': 'समाप्त गर्नुहोस्',
-  'legal.modal.existingUser': 'पहिले नै दर्ता गरिएको छ?',
-  'legal.modal.pleaseLogin': 'कृपया लगइन गर्नुहोस्।',
-  'legal.publicationAgreements': 'प्रकाशन सम्झौताहरू',
-  'legal.generalPublishingAgreement': 'सामान्य प्रकाशन सम्झौता',
-  'legal.documentsFor': '{workTitle}को लागि कानूनी कागजातहरू',
-}
+    'legal.modal.intro': 'क्षमा गर्नुहोस्। तर अघि बढ्न अघि, हामीले आफ्नो सेवा सङ्कलन, गोपनीयता नीति र कानुनी अधिकार सम्बन्धमा तपाईंलाई जागरूक गराउनुपर्छ र तपाईंले सहमत छन् भन्ने नियमहरूलाई।',
+    'legal.modal.agree': 'म सहमत छु',
+    'legal.modal.disagree': 'म सहमत छैन',
+    'legal.modal.register': 'तपाईंले यसलाई थप देख्नुहुन्छैन यदि तपाईं दर्ता गर्नुहुन्छ।',
+    'legal.modal.goToSettings': 'तपाईंको सेटिङ्गमा जानुहोस्',
+    'legal.modal.oneLastThing': 'अन्तिम कुरा...',
+    'legal.modal.finish': 'समाप्त गर्नुहोस्',
+    'legal.modal.existingUser': 'पहिले नै दर्ता गरिएको छ?',
+    'legal.modal.pleaseLogin': 'कृपया लगइन गर्नुहोस्।',
+    'legal.publicationAgreements': 'प्रकाशन सम्झौताहरू',
+    'legal.generalPublishingAgreement': 'सामान्य प्रकाशन सम्झौता',
+    'legal.documentsFor': '{workTitle}को लागि कानूनी कागजातहरू'
+};
