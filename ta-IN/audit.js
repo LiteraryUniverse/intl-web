f6c8bf64173949defc090c8deed90614a4b27342
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:31:36 +0200

    New translations audit.js (Tamil)

--- ta-IN/audit.js
+++ ta-IN/audit.js
@@ -1,6 +1,6 @@
 export default {
-  'auditLog.header.date': 'Date',
-  'auditLog.header.action': 'என்ன',
-  'auditLog.header.group': 'குழு',
-  'auditLog.header.user': 'யார்',
-}
+    'auditLog.header.date': 'Date',
+    'auditLog.header.action': 'என்ன',
+    'auditLog.header.group': 'குழு',
+    'auditLog.header.user': 'யார்'
+};
