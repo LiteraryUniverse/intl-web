78faf643c54a2f2320a5859a3a79564f82b427b8
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:24:45 +0200

    New translations pm.js (Polish)

--- pl-PL/pm.js
+++ pl-PL/pm.js
@@ -1,26 +1,26 @@
 export default {
-  'pm.conversation': 'Rozmowa',
-  'pm.showolder': 'Pokaż wcześniejsze wiadomości',
-  'pm.errors.saysomething': 'Pasowałoby coś powiedzieć...',
-  'pm.errors.addrecipient': 'Należy dodać co najmniej jednego innego użytkownika.',
-  'pm.message': 'Wiadomość',
-  'pm.send.message': 'Wyślij wiadomość',
-  'pm.messages.own': 'Twoje wiadomości',
-  'pm.conversations.none': 'Obecnie nie prowadzisz z nikim rozmów.',
-  'pm.to': 'Do:',
-  'pm.with': 'Rozmowa z:',
-  'pm.send': 'Wyślij',
-  'pm.conversations.unreadCount': `Masz {count, plural,
+    'pm.conversation': 'Rozmowa',
+    'pm.showolder': 'Pokaż wcześniejsze wiadomości',
+    'pm.errors.saysomething': 'Pasowałoby coś powiedzieć...',
+    'pm.errors.addrecipient': 'Należy dodać co najmniej jednego innego użytkownika.',
+    'pm.message': 'Wiadomość',
+    'pm.send.message': 'Wyślij wiadomość',
+    'pm.messages.own': 'Twoje wiadomości',
+    'pm.conversations.none': 'Obecnie nie prowadzisz z nikim rozmów.',
+    'pm.to': 'Do:',
+    'pm.with': 'Rozmowa z:',
+    'pm.send': 'Wyślij',
+    'pm.conversations.unreadCount': `Masz {count, plural,
     one {jedną nieprzeczytaną wiadomość}
     few {# nieprzeczytane wiadomości}
     many {# nieprzeczytanych wiadomości}
     other {# nieprzeczytanych wiadomości}
   }.`,
-  'pm.form.minimumParticipants': 'Należy wybrać odbiorcę.',
-  'pm.form.maxParticipants': `Możesz mieć maksymalnie {maxPeople, plural,
+    'pm.form.minimumParticipants': 'Należy wybrać odbiorcę.',
+    'pm.form.maxParticipants': `Możesz mieć maksymalnie {maxPeople, plural,
     one {jedną osobę}
     other {# osób}
   } w rozmowie.`,
-  'pm.new.aria': 'Nowa rozmowa',
-  'pm.new.buttonTitle': 'Rozpocznij nową rozmowę',
-}
+    'pm.new.aria': 'Nowa rozmowa',
+    'pm.new.buttonTitle': 'Rozpocznij nową rozmowę'
+};
