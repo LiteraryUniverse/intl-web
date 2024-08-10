a322ce7a1d7a8d0d4ec7e0b2f1fa95fdb23d4a15
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:27:50 +0200

    New translations fan_manager.js (Turkish)

--- tr-TR/fan_manager.js
+++ tr-TR/fan_manager.js
@@ -10,15 +10,17 @@ export default {
   'fanManager.state.rejected': 'Reddedildi',
   'fanManager.state.hidden': 'Gizli',
   'fanManager.publicComment': 'Halka açık yorum',
-  'fanManager.publicComment.desc':
-    'Bu metin, sanatın resmi yorumu olarak halka açılan sayfada görüntülenecektir.',
+  'fanManager.publicComment.desc': 'Bu metin, sanatın resmi yorumu olarak halka açılan sayfada görüntülenecektir.',
   'fanManager.authorComment': 'Yazar için yorum',
   'fanManager.authorComment.desc': 'Bu, sanatın yazarına sizden bir özel yorumdur.',
   'fanManager.stateChange.approve': 'Hayran sanatını onayla',
   'fanManager.stateChange.reject': 'Hayran sanatını reddet',
   'fanManager.stateChange.hide': 'Hayran sanatını gizle',
   'fanManager.stateChange.unhide': 'Hayran sanatını görünür yap',
-  'fanManager.fanFictionNotAllowed':
-    'Lisansınızın doğası gereği hayran hikayelerine izin verilmiyor.',
+  'fanManager.fanFictionNotAllowed': 'Lisansınızın doğası gereği hayran hikayelerine izin verilmiyor.',
   'fanManager.fanFiction.maxRating': 'Görüntülenen hayran hikayelerinin maksimum derecesi.',
-}
+  'fanManager.hidden.nothingFound': 'Gizli fan sanatı bulunamadı.',
+  'fanManager.rejected.nothingFound': 'Reddedilen fan sanatı bulunamadı.',
+  'fanManager.approved.nothingFound': 'Onaylanmış fan sanatı bulunamadı.',
+  'fanManager.submitted.nothingFound': 'Gönderilmiş hayran sanatı bulunamadı.'
+};
