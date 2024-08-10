d632ecab67913e5e36492da3b825c889deaaf7ff
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:16:56 +0200

    New translations pm.js (Greek)

--- el-GR/pm.js
+++ el-GR/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'Συνομιλία',
-  'pm.showolder': 'Εμφάνιση παλαιότερων μηνυμάτων',
-  'pm.errors.saysomething': 'Θα έπρεπε να πεις κάτι...',
-  'pm.errors.addrecipient': 'Πρέπει να προσθέσετε τουλάχιστον έναν άλλο χρήστη.',
-  'pm.message': 'Μήνυμα',
-  'pm.send.message': 'Αποστολή μηνύματος',
-  'pm.messages.own': 'Τα μηνύματά σας',
-  'pm.conversations.none': 'Δεν συνομιλείτε αυτήν τη στιγμή με κανέναν.',
-  'pm.to': 'Προς:',
-  'pm.with': 'Συνομιλία με:',
-  'pm.send': 'Αποστολή',
-  'pm.conversations.unreadCount': `Έχετε {count, plural,
+    'pm.conversation': 'Συνομιλία',
+    'pm.showolder': 'Εμφάνιση παλαιότερων μηνυμάτων',
+    'pm.errors.saysomething': 'Θα έπρεπε να πεις κάτι...',
+    'pm.errors.addrecipient': 'Πρέπει να προσθέσετε τουλάχιστον έναν άλλο χρήστη.',
+    'pm.message': 'Μήνυμα',
+    'pm.send.message': 'Αποστολή μηνύματος',
+    'pm.messages.own': 'Τα μηνύματά σας',
+    'pm.conversations.none': 'Δεν συνομιλείτε αυτήν τη στιγμή με κανέναν.',
+    'pm.to': 'Προς:',
+    'pm.with': 'Συνομιλία με:',
+    'pm.send': 'Αποστολή',
+    'pm.conversations.unreadCount': `Έχετε {count, plural,
     zero {καμία αδιάβαστη συνομιλία}
     one {μία αδιάβαστη συνομιλία}
     other {# αδιάβαστες συνομιλίες}
   }.`,
-  'pm.form.minimumParticipants': 'Πρέπει να επιλέξετε έναν παραλήπτη.',
-  'pm.form.maxParticipants': `Μπορείτε να έχετε μέγιστο {maxPeople, plural,
+    'pm.form.minimumParticipants': 'Πρέπει να επιλέξετε έναν παραλήπτη.',
+    'pm.form.maxParticipants': `Μπορείτε να έχετε μέγιστο {maxPeople, plural,
     one {ένα άτομο}
     other {# άτομα}
   } σε μια συνομιλία.`,
-  'pm.new.aria': 'Νέα συνομιλία',
-  'pm.new.buttonTitle': 'Ξεκινήστε μια νέα συνομιλία',
-}
+    'pm.new.aria': 'Νέα συνομιλία',
+    'pm.new.buttonTitle': 'Ξεκινήστε μια νέα συνομιλία'
+};
