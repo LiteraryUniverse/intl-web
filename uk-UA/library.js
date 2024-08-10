374a22f5f4980813153c38bcf0bf0573045926d7
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:28:16 +0200

    New translations library.js (Ukrainian)

--- uk-UA/library.js
+++ uk-UA/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'Додати до книгозбірні',
-  'library.remove': 'Вилучити з книгозбірні',
-  'library.bought': 'Придбані історії',
-  'library.stories': 'Відстежувані історії',
-  'library.universes': 'Відстежувані всесвіти',
-  'bookmark.continueReading': 'Продовжити читання',
-  'bookmark.statusSummary': 'Ви прочитали {readPercent, number, ::percent} історії.',
-}
+    'library.add': 'Додати до книгозбірні',
+    'library.remove': 'Вилучити з книгозбірні',
+    'library.bought': 'Придбані історії',
+    'library.stories': 'Відстежувані історії',
+    'library.universes': 'Відстежувані всесвіти',
+    'bookmark.continueReading': 'Продовжити читання',
+    'bookmark.statusSummary': 'Ви прочитали {readPercent, number, ::percent} історії.'
+};
