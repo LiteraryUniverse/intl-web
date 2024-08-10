fef8dc5b0dab2fee054a7d2d4d241b4ebaaf7144
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:28:47 +0200

    New translations audit.js (Chinese Traditional)

--- zh-TW/audit.js
+++ zh-TW/audit.js
@@ -1,6 +1,6 @@
 export default {
-  'auditLog.header.date': '日期',
-  'auditLog.header.action': '什麼',
-  'auditLog.header.group': '群組',
-  'auditLog.header.user': '誰',
-}
+    'auditLog.header.date': '日期',
+    'auditLog.header.action': '什麼',
+    'auditLog.header.group': '群組',
+    'auditLog.header.user': '誰'
+};
