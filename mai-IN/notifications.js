c801d838829f9c3c953ffa0bc09cf4c36c194d19
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:40:05 +0200

    New translations notifications.js (Maithili)

--- mai-IN/notifications.js
+++ mai-IN/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'इस संदेश को दिखाना शुरू करें',
   'flashnews.endsAt': 'इस संदेश को दिखाना बंद करें',
   'flashnews.newLanguage': 'नया भाषा जोड़ने के लिए भाषा चुनें',
-  'flashnews.onlyDisplayOn':
-    'समाचार केवल नीचे चयनित भाषाओं पर ही प्रदर्शित होगा (यदि कोई चयनित हो), अन्य भाषाओं में भी डिफ़ॉल्ट भाषा में समाचार दिखाए नहीं जाएंगे।',
+  'flashnews.onlyDisplayOn': 'समाचार केवल नीचे चयनित भाषाओं पर ही प्रदर्शित होगा (यदि कोई चयनित हो), अन्य भाषाओं में भी डिफ़ॉल्ट भाषा में समाचार दिखाए नहीं जाएंगे।',
   'notifications.menu.all': 'सबै सूचना',
   'notifications.menu.following': 'अनुसरण गर्ने',
   'notifications.menu.workshop': 'कार्यशाला',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'पठाउनुहोस्',
   'notifications.menu.delete': 'सूचना हटाउनुहोस्',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'तपाईंको कथा {storyName} 6 महिनाको निष्क्रियतापछि स्वचालित रूपमा हायाटसमा सेट गरियो।',
-  'notification.workshopStoryStatus.toCancelled':
-    'तपाईंको कथा {storyName} 1 वर्षको निष्क्रियतापछि स्वचालित रूपमा रद्द गरियो।',
+  'notification.workshopStoryStatus.toHiatus': 'तपाईंको कथा {storyName} 6 महिनाको निष्क्रियतापछि स्वचालित रूपमा हायाटसमा सेट गरियो।',
+  'notification.workshopStoryStatus.toCancelled': 'तपाईंको कथा {storyName} 1 वर्षको निष्क्रियतापछि स्वचालित रूपमा रद्द गरियो।',
   'notification.followingStoryStatus.toHiatus': '{storyName} हायाटसमा छ।',
   'notification.followingStoryStatus.toCancelled': '{storyName} रद्द गरियो।',
   'notification.followingNewChapter.new': '{storyName} मा नयाँ अध्याय प्रकाशित भयो!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'तपाईंलाई मित्र अनुरोध पठाएको छ।',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'कथामा जानुहोस्',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'कथामा जानुहोस्'
+};
