d8f0f7037e4dc4666f11df2aabe00c801a5bddf7
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:13:40 +0200

    New translations user.js (Afrikaans)

--- af-ZA/user.js
+++ af-ZA/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'Voeg by vriende',
-  'user.friend.requests': `Jy het {num, number} nuwe vriend {num, plural,
+    'user.friend.request.add': 'Voeg by vriende',
+    'user.friend.requests': `Jy het {num, number} nuwe vriend {num, plural,
     zero {versoeke}
     one {versoek}
     many {versoeke}
     other {versoeke}
   }`,
-  'user.friend.unfriend': 'Verwyder vriend',
-  'user.block': 'Blokkeer',
-  'user.unblock': 'Deblokkeer',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'Verwyder vriend',
+    'user.block': 'Blokkeer',
+    'user.unblock': 'Deblokkeer',
+    'user.joined': `{gender, select,
     male {Het aangesluit by}
     female {Het aangesluit by}
     other {Het aangesluit by}} op {date, date, long}`,
-  'user.friend.request.accept': 'Aanvaar vriendskapsversoek',
-  'user.friend.request.cancel': 'Kanselleer vriendskapsversoek',
-  'user.friend.request.deny': 'Weier vriendskap',
-  'user.listing': 'Gebruikerslys',
-  'user.profile.visit': 'Besoek {user}se profiel.',
-  'user.profile.avatar': '{username}se avatar',
-  'user.friend.list.requests': 'Versoeke vir vriendskap',
-  'user.friend.norequests': 'Jy het geen versoeke vir vriendskap nie.',
-  'user.dashboardWelcome': `Goeie {dayPart, select,
+    'user.friend.request.accept': 'Aanvaar vriendskapsversoek',
+    'user.friend.request.cancel': 'Kanselleer vriendskapsversoek',
+    'user.friend.request.deny': 'Weier vriendskap',
+    'user.listing': 'Gebruikerslys',
+    'user.profile.visit': 'Besoek {user}se profiel.',
+    'user.profile.avatar': '{username}se avatar',
+    'user.friend.list.requests': 'Versoeke vir vriendskap',
+    'user.friend.norequests': 'Jy het geen versoeke vir vriendskap nie.',
+    'user.dashboardWelcome': `Goeie {dayPart, select,
     morning {oggend}
     afternoon {middag}
     evening {aand}
     night {nag}
     other {dag}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'Hierdie skrywer is op die tradisionele manier gepubliseer',
-  'user.verification.luAuthor': 'Literary Universe-gebaseerde bekende skrywer',
-  'user.verification.luEmployee': 'Literary Universe-werknemer',
-  'user.verification.publicFigure': 'Geverifieerde publieke figuur',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'Hierdie skrywer is op die tradisionele manier gepubliseer',
+    'user.verification.luAuthor': 'Literary Universe-gebaseerde bekende skrywer',
+    'user.verification.luEmployee': 'Literary Universe-werknemer',
+    'user.verification.publicFigure': 'Geverifieerde publieke figuur',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {storie}
     other {stories}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {heelal}
     other {heelalle}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {fanfiksie}
     other {fanfiksies} 
   }`,
-  'profile.works': 'Werke',
-  // As in creative/literary works
-  'profile.wishlist': 'Wenslys',
-  'profile.storiesBy': 'Stories geskep deur {username}',
-  'profile.universesBy': 'Heelalle geskep deur {username}',
-  'profile.fanFictionBy': 'Fanfiksie geskep deur {username}',
-}
+    'profile.works': 'Werke',
+    // As in creative/literary works
+    'profile.wishlist': 'Wenslys',
+    'profile.storiesBy': 'Stories geskep deur {username}',
+    'profile.universesBy': 'Heelalle geskep deur {username}',
+    'profile.fanFictionBy': 'Fanfiksie geskep deur {username}'
+};
