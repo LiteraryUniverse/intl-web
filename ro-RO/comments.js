b77980200b4d2547ea051eba935c0d858fc43e48
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:41:34 +0200

    New translations comments.js (Romanian)

--- ro-RO/comments.js
+++ ro-RO/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': 'Scrieți comentariul dumneavoastră',
-  'comments.none': 'Ne pare rău, nu există comentarii de afișat.',
-  'comments.total': `There {count, plural,
+    'comment.write': 'Scrieți comentariul dumneavoastră',
+    'comments.none': 'Ne pare rău, nu există comentarii de afișat.',
+    'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-  'comments.show.older': 'Afișați comentariile mai vechi',
-}
+    'comments.show.older': 'Afișați comentariile mai vechi'
+};
