71d928ecb3181e55dea4eb36d416f412a760b679
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:38:47 +0200

    New translations pm.js (Kannada)

--- kn-IN/pm.js
+++ kn-IN/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'ಸಂಭಾಷಣೆ',
-  'pm.showolder': 'ಹಳೆಯ ಸಂದೇಶಗಳನ್ನು ತೋರಿಸು',
-  'pm.errors.saysomething': 'ನೀವು ನಿಜವಾಗಿಯೂ ಏನೋ ಹೇಳಬೇಕು...',
-  'pm.errors.addrecipient': 'ನೀವು ಕನಿಷ್ಠ ಒಬ್ಬ ಬೇರೆ ಬಳಕೆದಾರನನ್ನು ಸೇರಿಸಬೇಕು.',
-  'pm.message': 'ಸಂದೇಶ',
-  'pm.send.message': 'ಸಂದೇಶ ಕಳುಹಿಸು',
-  'pm.messages.own': 'ನಿಮ್ಮ ಸಂದೇಶಗಳು',
-  'pm.conversations.none': 'ನೀವು ಪ್ರಸ್ತುತ ಯಾರೊಂದಿಗೂ ಸಂಭಾಷಿಸುತ್ತಿಲ್ಲ.',
-  'pm.to': 'ಗೆ:',
-  'pm.with': 'ಸಂಭಾಷಣೆ:',
-  'pm.send': 'ಕಳುಹಿಸು',
-  'pm.conversations.unreadCount': `ನಿಮ್ಮಲ್ಲಿ {count, plural,
+    'pm.conversation': 'ಸಂಭಾಷಣೆ',
+    'pm.showolder': 'ಹಳೆಯ ಸಂದೇಶಗಳನ್ನು ತೋರಿಸು',
+    'pm.errors.saysomething': 'ನೀವು ನಿಜವಾಗಿಯೂ ಏನೋ ಹೇಳಬೇಕು...',
+    'pm.errors.addrecipient': 'ನೀವು ಕನಿಷ್ಠ ಒಬ್ಬ ಬೇರೆ ಬಳಕೆದಾರನನ್ನು ಸೇರಿಸಬೇಕು.',
+    'pm.message': 'ಸಂದೇಶ',
+    'pm.send.message': 'ಸಂದೇಶ ಕಳುಹಿಸು',
+    'pm.messages.own': 'ನಿಮ್ಮ ಸಂದೇಶಗಳು',
+    'pm.conversations.none': 'ನೀವು ಪ್ರಸ್ತುತ ಯಾರೊಂದಿಗೂ ಸಂಭಾಷಿಸುತ್ತಿಲ್ಲ.',
+    'pm.to': 'ಗೆ:',
+    'pm.with': 'ಸಂಭಾಷಣೆ:',
+    'pm.send': 'ಕಳುಹಿಸು',
+    'pm.conversations.unreadCount': `ನಿಮ್ಮಲ್ಲಿ {count, plural,
     zero {ಓದದ ಸಂಭಾಷಣೆಗಳಿವೆ}
     one {ಒಂದು ಓದದ ಸಂಭಾಷಣೆಯಿದೆ}
     other {# ಓದದ ಸಂಭಾಷಣೆಗಳಿವೆ}
   }.`,
-  'pm.form.minimumParticipants': 'ನೀವು ಒಂದು ಪ್ರಾಪ್ತಿಯನ್ನು ಆಯ್ಕೆ ಮಾಡಬೇಕು.',
-  'pm.form.maxParticipants': `ನೀವು ಸಂಭಾಷಣೆಯಲ್ಲಿ ಗಣನೀಯವಾಗಿ ಅತ್ಯಂತ ಹೆಚ್ಚುವರಿ ಮಧ್ಯವರ್ತಿಗಳನ್ನು ಹೊಂದಬಹುದು: {maxPeople, plural,
+    'pm.form.minimumParticipants': 'ನೀವು ಒಂದು ಪ್ರಾಪ್ತಿಯನ್ನು ಆಯ್ಕೆ ಮಾಡಬೇಕು.',
+    'pm.form.maxParticipants': `ನೀವು ಸಂಭಾಷಣೆಯಲ್ಲಿ ಗಣನೀಯವಾಗಿ ಅತ್ಯಂತ ಹೆಚ್ಚುವರಿ ಮಧ್ಯವರ್ತಿಗಳನ್ನು ಹೊಂದಬಹುದು: {maxPeople, plural,
     one {ಒಬ್ಬ ವ್ಯಕ್ತಿ}
     other {# ಜನರು}
   }.`,
-  'pm.new.aria': 'ಹೊಸ ಸಂಭಾಷಣೆ',
-  'pm.new.buttonTitle': 'ಹೊಸ ಸಂಭಾಷಣೆ ಪ್ರಾರಂಭಿಸಿ',
-}
+    'pm.new.aria': 'ಹೊಸ ಸಂಭಾಷಣೆ',
+    'pm.new.buttonTitle': 'ಹೊಸ ಸಂಭಾಷಣೆ ಪ್ರಾರಂಭಿಸಿ'
+};
