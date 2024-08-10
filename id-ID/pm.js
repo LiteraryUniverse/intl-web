dde663edae9a5ca0520566fc256e9436fc2905c5
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:30:22 +0200

    New translations pm.js (Indonesian)

--- id-ID/pm.js
+++ id-ID/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'Percakapan',
-  'pm.showolder': 'Tampilkan pesan-pesan lama',
-  'pm.errors.saysomething': 'Anda sebaiknya mengatakan sesuatu...',
-  'pm.errors.addrecipient': 'Anda perlu menambahkan setidaknya satu pengguna lain.',
-  'pm.message': 'Pesan',
-  'pm.send.message': 'Kirim pesan',
-  'pm.messages.own': 'Pesan Anda',
-  'pm.conversations.none': 'Anda saat ini tidak sedang berbicara dengan siapa pun.',
-  'pm.to': 'Kepada:',
-  'pm.with': 'Percakapan dengan:',
-  'pm.send': 'Kirim',
-  'pm.conversations.unreadCount': `Anda memiliki {count, plural,
+    'pm.conversation': 'Percakapan',
+    'pm.showolder': 'Tampilkan pesan-pesan lama',
+    'pm.errors.saysomething': 'Anda sebaiknya mengatakan sesuatu...',
+    'pm.errors.addrecipient': 'Anda perlu menambahkan setidaknya satu pengguna lain.',
+    'pm.message': 'Pesan',
+    'pm.send.message': 'Kirim pesan',
+    'pm.messages.own': 'Pesan Anda',
+    'pm.conversations.none': 'Anda saat ini tidak sedang berbicara dengan siapa pun.',
+    'pm.to': 'Kepada:',
+    'pm.with': 'Percakapan dengan:',
+    'pm.send': 'Kirim',
+    'pm.conversations.unreadCount': `Anda memiliki {count, plural,
     zero {tidak ada percakapan yang belum dibaca}
     one {satu percakapan yang belum dibaca}
     other {percakapan yang belum dibaca}
   }.`,
-  'pm.form.minimumParticipants': 'Anda perlu memilih penerima.',
-  'pm.form.maxParticipants': `You can have maximum of {maxPeople, plural,
+    'pm.form.minimumParticipants': 'Anda perlu memilih penerima.',
+    'pm.form.maxParticipants': `You can have maximum of {maxPeople, plural,
     one {one person}
     other {# people}
   } in a conversation.`,
-  'pm.new.aria': 'Percakapan baru',
-  'pm.new.buttonTitle': 'Mulai percakapan baru',
-}
+    'pm.new.aria': 'Percakapan baru',
+    'pm.new.buttonTitle': 'Mulai percakapan baru'
+};
