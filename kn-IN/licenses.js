57f46efb762095fc9a59cdd8d5c9966f4f1eda96
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:38:43 +0200

    New translations licenses.js (Kannada)

--- kn-IN/licenses.js
+++ kn-IN/licenses.js
@@ -3,31 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'About licenses',
-  'licenses.more': 'More information',
-  'licenses.legal': 'Legal document',
-  'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
-  'licenses.cc-by':
-    'This license lets others distribute, remix, tweak, and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of licenses offered. Recommended for maximum dissemination and use of licensed materials.',
-  'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
-  'licenses.cc-by-sa':
-    'This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.',
-  'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
-  'licenses.cc-by-nd':
-    'This license allows for redistribution, commercial and non-commercial, as long as it is passed along unchanged and in whole, with credit to you.',
-  'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
-  'licenses.cc-by-nc':
-    'This license lets others remix, tweak, and build upon your work non-commercially, and although their new works must also acknowledge you and be non-commercial, they don’t have to license their derivative works on the same terms.',
-  'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
-  'licenses.cc-by-nc-sa':
-    'This license lets others remix, tweak, and build upon your work non-commercially, as long as they credit you and license their new creations under the identical terms.',
-  'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
-  'licenses.cc-by-nc-nd':
-    'This license is the most restrictive of our six main licenses, only allowing others to download your works and share them with others as long as they credit you, but they can’t change them in any way or use them commercially.',
-  'licenses.name.lu-c': 'Literary Universe Copyrights',
-  'licenses.lu-c':
-    'ನಿಮ್ಮ ಕೆಲಸವನ್ನು ಮನವರಿಕೆ ಪಡಿಸಿಕೊಳ್ಳುವುದರ ಮೂಲಕ ನೀವು ಬೇಕಾದಷ್ಟು ಮಾರುಕಟ್ಟಿರಿ ಮತ್ತು ನಿಮ್ಮ ಹಕ್ಕುಗಳನ್ನು ರಕ್ಷಿಸುವ ಪೂರ್ಣ ಕಾರ್ಯಾಚರಣೆ ಅನುಮತಿಸುವ ಸಾಮಾನ್ಯ ಪೂರ್ಣ ಕಾಪಿರೈಟ್ ಅನುಮತಿ. ಈ ಅನುಮತಿ ನಿಮಗೆ ಸಾಹಿತ್ಯ ವಿಶ್ವದಲ್ಲಿ ನಿಮ್ಮ ಕಥೆಗಳನ್ನು ಮಾರುಕಟ್ಟಿದ್ದರೆ ಮತ್ತು ಅದರ ಮೇಲೆ ನಿಮ್ಮ ಕಥೆಗಳನ್ನು ಮಾರುಕಟ್ಟಬಹುದು ಎಂದು ನೀವು ಆಯ್ಕೆ ಮಾಡಬಹುದು.',
-  'licenses.name.cc0': 'ಯಾವುದೇ ಹಕ್ಕುಗಳನ್ನು ಮೀರಿದ ರೂಪದಲ್ಲಿ - CC0 1.0',
-  'licenses.cc0':
-    'ನಿಮ್ಮ ಕೆಲಸವನ್ನು ಸಾರ್ವಜನಿಕ ವಿಭಾಗಕ್ಕೆ ಹಾಕಿಕೊಳ್ಳಿ. CC ಅನುಮತಿಗಳು ಕಾಪಿರೈಟ್ ಹಿಡಿದುಕೊಂಡು ಕೆಲಸದ ಹಾಕುಗಳ ಸಾಮರಾಜ್ಯವನ್ನು ಆಯ್ಕೆಮಾಡಲು ಅನುಮತಿ ಹೊಂದುವಂತೆ ಅವರಿಗೆ ಅವಕಾಶ ಕೊಡುತ್ತದೆ, CC0 ಕಾಪಿರೈಟ್ ಮತ್ತು ಡೇಟಾಬೇಸ್ ಸಂರಕ್ಷಣೆಯಿಂದ ಹೊರಗೆ ಬರಲು ಮತ್ತು ಸೃಷ್ಟಿಸುವವರಿಗೆ ಸ್ವತಃಸ್ವತಃ ನೀಡಲ್ಪಟ್ಟ ಪ್ರತಿಸ್ಪಂದನಗಳನ್ನು ಆಯ್ಕೆಮಾಡುವ ಆಯ್ಕೆಗೆ ಬೇರೆಯೇ ಆಯ್ಕೆಯೆಂದು ಹೇಳಬಹುದು.',
-}
+    'licenses.about': 'About licenses',
+    'licenses.more': 'More information',
+    'licenses.legal': 'Legal document',
+    'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
+    'licenses.cc-by': 'This license lets others distribute, remix, tweak, and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of licenses offered. Recommended for maximum dissemination and use of licensed materials.',
+    'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
+    'licenses.cc-by-sa': 'This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.',
+    'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
+    'licenses.cc-by-nd': 'This license allows for redistribution, commercial and non-commercial, as long as it is passed along unchanged and in whole, with credit to you.',
+    'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
+    'licenses.cc-by-nc': 'This license lets others remix, tweak, and build upon your work non-commercially, and although their new works must also acknowledge you and be non-commercial, they don’t have to license their derivative works on the same terms.',
+    'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
+    'licenses.cc-by-nc-sa': 'This license lets others remix, tweak, and build upon your work non-commercially, as long as they credit you and license their new creations under the identical terms.',
+    'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
+    'licenses.cc-by-nc-nd': 'This license is the most restrictive of our six main licenses, only allowing others to download your works and share them with others as long as they credit you, but they can’t change them in any way or use them commercially.',
+    'licenses.name.lu-c': 'Literary Universe Copyrights',
+    'licenses.lu-c': 'ನಿಮ್ಮ ಕೆಲಸವನ್ನು ಮನವರಿಕೆ ಪಡಿಸಿಕೊಳ್ಳುವುದರ ಮೂಲಕ ನೀವು ಬೇಕಾದಷ್ಟು ಮಾರುಕಟ್ಟಿರಿ ಮತ್ತು ನಿಮ್ಮ ಹಕ್ಕುಗಳನ್ನು ರಕ್ಷಿಸುವ ಪೂರ್ಣ ಕಾರ್ಯಾಚರಣೆ ಅನುಮತಿಸುವ ಸಾಮಾನ್ಯ ಪೂರ್ಣ ಕಾಪಿರೈಟ್ ಅನುಮತಿ. ಈ ಅನುಮತಿ ನಿಮಗೆ ಸಾಹಿತ್ಯ ವಿಶ್ವದಲ್ಲಿ ನಿಮ್ಮ ಕಥೆಗಳನ್ನು ಮಾರುಕಟ್ಟಿದ್ದರೆ ಮತ್ತು ಅದರ ಮೇಲೆ ನಿಮ್ಮ ಕಥೆಗಳನ್ನು ಮಾರುಕಟ್ಟಬಹುದು ಎಂದು ನೀವು ಆಯ್ಕೆ ಮಾಡಬಹುದು.',
+    'licenses.name.cc0': 'ಯಾವುದೇ ಹಕ್ಕುಗಳನ್ನು ಮೀರಿದ ರೂಪದಲ್ಲಿ - CC0 1.0',
+    'licenses.cc0': 'ನಿಮ್ಮ ಕೆಲಸವನ್ನು ಸಾರ್ವಜನಿಕ ವಿಭಾಗಕ್ಕೆ ಹಾಕಿಕೊಳ್ಳಿ. CC ಅನುಮತಿಗಳು ಕಾಪಿರೈಟ್ ಹಿಡಿದುಕೊಂಡು ಕೆಲಸದ ಹಾಕುಗಳ ಸಾಮರಾಜ್ಯವನ್ನು ಆಯ್ಕೆಮಾಡಲು ಅನುಮತಿ ಹೊಂದುವಂತೆ ಅವರಿಗೆ ಅವಕಾಶ ಕೊಡುತ್ತದೆ, CC0 ಕಾಪಿರೈಟ್ ಮತ್ತು ಡೇಟಾಬೇಸ್ ಸಂರಕ್ಷಣೆಯಿಂದ ಹೊರಗೆ ಬರಲು ಮತ್ತು ಸೃಷ್ಟಿಸುವವರಿಗೆ ಸ್ವತಃಸ್ವತಃ ನೀಡಲ್ಪಟ್ಟ ಪ್ರತಿಸ್ಪಂದನಗಳನ್ನು ಆಯ್ಕೆಮಾಡುವ ಆಯ್ಕೆಗೆ ಬೇರೆಯೇ ಆಯ್ಕೆಯೆಂದು ಹೇಳಬಹುದು.'
+};
