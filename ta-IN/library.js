299a789e6b2f8c0c0f155a3331df062e8c933e1d
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:31:39 +0200

    New translations library.js (Tamil)

--- ta-IN/library.js
+++ ta-IN/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'நூலயக்கத்தில் சேர்க்கவும்',
-  'library.remove': 'நூலயக்கத்திலிருந்து அகற்றுக',
-  'library.bought': 'வாங்கிய கதைகள்',
-  'library.stories': 'பின்னூட்டப்பட்ட கதைகள்',
-  'library.universes': 'பின்புகளை பின்பற்றப்பட்ட புலம்பல்கள்',
-  'bookmark.continueReading': 'மேலே வாசிக்க தொடருங்கள்',
-  'bookmark.statusSummary': 'நீங்கள் கதையின் {readPercent, number, ::percent} படித்துவிட்டீர்கள்.',
-}
+    'library.add': 'நூலயக்கத்தில் சேர்க்கவும்',
+    'library.remove': 'நூலயக்கத்திலிருந்து அகற்றுக',
+    'library.bought': 'வாங்கிய கதைகள்',
+    'library.stories': 'பின்னூட்டப்பட்ட கதைகள்',
+    'library.universes': 'பின்புகளை பின்பற்றப்பட்ட புலம்பல்கள்',
+    'bookmark.continueReading': 'மேலே வாசிக்க தொடருங்கள்',
+    'bookmark.statusSummary': 'நீங்கள் கதையின் {readPercent, number, ::percent} படித்துவிட்டீர்கள்.'
+};
