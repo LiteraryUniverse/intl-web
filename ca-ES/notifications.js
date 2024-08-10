a68e5398e7ca396c116aa7eeba0d5fec4ba2969f
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:14:50 +0200

    New translations notifications.js (Catalan)

--- ca-ES/notifications.js
+++ ca-ES/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'Comença a mostrar aquest missatge a',
   'flashnews.endsAt': 'Deixa de mostrar aquest missatge a',
   'flashnews.newLanguage': 'Selecciona un nou idioma per afegir',
-  'flashnews.onlyDisplayOn':
-    "Les notícies només es mostraran en els idiomes seleccionats a continuació (si n'hi ha algun seleccionat), no es mostrarà cap notícia, ni tan sols en l'idioma per defecte, en altres idiomes.",
+  'flashnews.onlyDisplayOn': 'Les notícies només es mostraran en els idiomes seleccionats a continuació (si n\'hi ha algun seleccionat), no es mostrarà cap notícia, ni tan sols en l\'idioma per defecte, en altres idiomes.',
   'notifications.menu.all': 'Totes les notificacions',
   'notifications.menu.following': 'Seguint',
   'notifications.menu.workshop': 'Taller',
@@ -14,17 +13,15 @@ export default {
   'notifications.menu.markRead': 'Marcar com llegit',
   'notifications.menu.delete': 'Eliminar notificació',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    "La teva història {storyName} s'ha posat automàticament en pausa després de 6 mesos d'inactivitat.",
-  'notification.workshopStoryStatus.toCancelled':
-    "La teva història {storyName} s'ha posat automàticament en cancel·lació després d'un any d'inactivitat.",
+  'notification.workshopStoryStatus.toHiatus': 'La teva història {storyName} s\'ha posat automàticament en pausa després de 6 mesos d\'inactivitat.',
+  'notification.workshopStoryStatus.toCancelled': 'La teva història {storyName} s\'ha posat automàticament en cancel·lació després d\'un any d\'inactivitat.',
   'notification.followingStoryStatus.toHiatus': '{storyName} està en pausa.',
   'notification.followingStoryStatus.toCancelled': '{storyName} ha estat cancel·lada.',
   'notification.followingNewChapter.new': 'Nou capítol a {storyName} publicat!',
   'notification.workshopStory.newComment': 'ha comentat a {storyName}',
   // Before this text will be the username of the user
-  'notification.friendRequest': "t'ha enviat una sol·licitud d'amistat.",
+  'notification.friendRequest': 't\'ha enviat una sol·licitud d\'amistat.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Vés a la història',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'Vés a la història'
+};
