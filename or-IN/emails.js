d484df93d172a7668b4aadcc780b261f417953bb
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:40:15 +0200

    New translations emails.js (Odia)

--- or-IN/emails.js
+++ or-IN/emails.js
@@ -1,50 +1,46 @@
 /**
  * @authors Jan Dvorak
  */
-/* eslint-disable max-len */
 export default {
-  'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
-  'email.enrollAccount.message': `Hello {username}\n\n
+    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
+    'email.enrollAccount.message': `Hello {username}\n\n
     We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
     To activate your account, simply click the link below:\n\n{url}`,
-  'email.resetPassword.subject': 'Literary Universe reset password',
-  'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
+    'email.resetPassword.subject': 'Literary Universe reset password',
+    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
         Please follow the link bellow to reset your password:\n\n{url}`,
-  'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-  'email.verifyEmail.message':
-    'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
-  // New html email keys
-  'email.salutation': 'Hello {username},',
-  'email.welcome': 'Welcome to Literary Universe!',
-  'email.signature': 'Sincerely,<br />Literary Universe team',
-  // Enrollment
-  'email.enrollAccount.welcome': 'Welcome aboard',
-  'email.enrollAccount.messageHtml':
-    'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-  'email.enrollAccount.button': 'Access your account',
-  'email.enrollAccount.signature': 'ଆପଣଙ୍କୁ ଦେଖିବାକୁ ଆଗାମୀ ଅପେକ୍ଷା କରୁଛି,<br />ଲିଟେରରୀ ବ୍ୟାପାରରେ ଟିମ୍‌',
-  // Reset password
-  'email.resetPassword.messageHtml':
-    'ଆପଣଙ୍କର ଏକାଉଣ୍ଟର ପାସ୍‌ଵାର୍ଡ ରିସେଟ କରିବା ପାଇଁ ଆମେ ଏକ ଅନୁରୋଧ ପାଇଛି, ଦୟାକରି ପ୍ରକ୍ରିୟାଟି ଜାରି ରଖିବାରେ ବଟନ୍‌କୁ କ୍ଲିକ୍‌ କରନ୍ତୁ । ଯଦି ଆପଣ ଏହି ଅନୁରୋଧ କରିନଥିବା ହେବାରେ ତାହାକୁ ଆପଣ ଅଗ୍ରହରେ ନିହାତିପାରିବେ, କିନ୍ତୁ ଏହା ଆପଣଙ୍କ ସୁରକ୍ଷା ସେଟିଂସମେଣେ ଏକ ଭଲ ସମୟରେ ପରିଗଣନା କରିବାରେ ଉପଯୁକ୍ତ ହେବା ପାଇଁ ।',
-  'email.resetPassword.button': 'ପାସ୍‌ଵାର୍ଡ ରିସେଟ କରନ୍ତୁ',
-  // E-mail verification
-  'email.verifyEmail.messageHtml': 'ଆମେ ଆପଣଙ୍କୁ ସ୍ୱାଗତ କରୁଛୁ! ଦୟାକରି ଏହି ଇମେଲ୍ ଠାରୁ ଯାଆହେବା ବଟନ୍‌କୁ କ୍ଲିକ୍‌ କରନ୍ତୁ ।',
-  'email.verifyEmail.button': 'ଏହି ଇମେଲ୍ ଠାରୁ ସ୍ୱାଗତ କରନ୍ତୁ',
-  // Getting premium from admins
-  'email.adminUpgrade.subject': 'ଆପଣଙ୍କ ଏକାଉଣ୍ଟଟି ଉନ୍ନତ କରାଯାଇଛି',
-  'email.adminUpgrade.message': `ଆମେ ଆପଣଙ୍କର ଅକାଉଣ୍ଟକୁ {plan, select,
+    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
+    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    // New html email keys
+    'email.salutation': 'Hello {username},',
+    'email.welcome': 'Welcome to Literary Universe!',
+    'email.signature': 'Sincerely,<br />Literary Universe team',
+    // Enrollment
+    'email.enrollAccount.welcome': 'Welcome aboard',
+    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
+    'email.enrollAccount.button': 'Access your account',
+    'email.enrollAccount.signature': 'ଆପଣଙ୍କୁ ଦେଖିବାକୁ ଆଗାମୀ ଅପେକ୍ଷା କରୁଛି,<br />ଲିଟେରରୀ ବ୍ୟାପାରରେ ଟିମ୍‌',
+    // Reset password
+    'email.resetPassword.messageHtml': 'ଆପଣଙ୍କର ଏକାଉଣ୍ଟର ପାସ୍‌ଵାର୍ଡ ରିସେଟ କରିବା ପାଇଁ ଆମେ ଏକ ଅନୁରୋଧ ପାଇଛି, ଦୟାକରି ପ୍ରକ୍ରିୟାଟି ଜାରି ରଖିବାରେ ବଟନ୍‌କୁ କ୍ଲିକ୍‌ କରନ୍ତୁ । ଯଦି ଆପଣ ଏହି ଅନୁରୋଧ କରିନଥିବା ହେବାରେ ତାହାକୁ ଆପଣ ଅଗ୍ରହରେ ନିହାତିପାରିବେ, କିନ୍ତୁ ଏହା ଆପଣଙ୍କ ସୁରକ୍ଷା ସେଟିଂସମେଣେ ଏକ ଭଲ ସମୟରେ ପରିଗଣନା କରିବାରେ ଉପଯୁକ୍ତ ହେବା ପାଇଁ ।',
+    'email.resetPassword.button': 'ପାସ୍‌ଵାର୍ଡ ରିସେଟ କରନ୍ତୁ',
+    // E-mail verification
+    'email.verifyEmail.messageHtml': 'ଆମେ ଆପଣଙ୍କୁ ସ୍ୱାଗତ କରୁଛୁ! ଦୟାକରି ଏହି ଇମେଲ୍ ଠାରୁ ଯାଆହେବା ବଟନ୍‌କୁ କ୍ଲିକ୍‌ କରନ୍ତୁ ।',
+    'email.verifyEmail.button': 'ଏହି ଇମେଲ୍ ଠାରୁ ସ୍ୱାଗତ କରନ୍ତୁ',
+    // Getting premium from admins
+    'email.adminUpgrade.subject': 'ଆପଣଙ୍କ ଏକାଉଣ୍ଟଟି ଉନ୍ନତ କରାଯାଇଛି',
+    'email.adminUpgrade.message': `ଆମେ ଆପଣଙ୍କର ଅକାଉଣ୍ଟକୁ {plan, select,
     explorer {ଏକ୍ସପ୍ଲୋରର୍}
     adventurer {ଅଭିଜ୍ଞାନୀ}
     storyteller {କଥାକାର}
     other {}
   } ଟିୟର୍ ପାଇଁ {days} ଦିନରେ ।\n\nଆନନ୍ଦ କରନ୍ତୁ,\nଲିଟେରରୀ ଇୟୁନିଭର୍ସ ଟିମ`,
-  'email.adminUpgrade.messageHtml': `ଆମେ ଆପଣଙ୍କର ଅକାଉଣ୍ଟକୁ {plan, select,
+    'email.adminUpgrade.messageHtml': `ଆମେ ଆପଣଙ୍କର ଅକାଉଣ୍ଟକୁ {plan, select,
     explorer {ଏକ୍ସପ୍ଲୋରର୍}
     adventurer {ଅଭିଜ୍ଞାନୀ}
     storyteller {କଥାକାର}
     other {}
   } ଟିୟର୍ ପାଇଁ {days} ଦିନରେ । ଆନନ୍ଦ କରନ୍ତୁ!`,
-  'email.verification': 'ଇ-ମେଲ ଯାଚନା',
-  'email.verifying': 'ଆପଣଙ୍କର ଇ-ମେଲ ଯାଚନା ହେଉଛି। ଏହି କ୍ଷଣରେ ହୋଇଯାଇବେ...',
-  'email.verified': 'ଖୁବ ଭଲ ଖବର! ଆପଣଙ୍କର ଇ-ମେଲ ଯାଚନା ହୋଇଗଲା!',
-} /* eslint-enable max-len */
+    'email.verification': 'ଇ-ମେଲ ଯାଚନା',
+    'email.verifying': 'ଆପଣଙ୍କର ଇ-ମେଲ ଯାଚନା ହେଉଛି। ଏହି କ୍ଷଣରେ ହୋଇଯାଇବେ...',
+    'email.verified': 'ଖୁବ ଭଲ ଖବର! ଆପଣଙ୍କର ଇ-ମେଲ ଯାଚନା ହୋଇଗଲା!'
+};
