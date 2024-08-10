8d2563e80121ecd644d3a8d78630695c0cbaced9
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:29:28 +0200

    New translations notifications.js (Vietnamese)

--- vi-VN/notifications.js
+++ vi-VN/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'Bắt đầu hiển thị tin nhắn này từ',
   'flashnews.endsAt': 'Ngừng hiển thị tin nhắn này từ',
   'flashnews.newLanguage': 'Chọn ngôn ngữ mới để thêm',
-  'flashnews.onlyDisplayOn':
-    'Tin tức sẽ chỉ hiển thị trên các ngôn ngữ đã chọn bên dưới (nếu có), không có tin tức nào được hiển thị, thậm chí trong ngôn ngữ mặc định, trong các ngôn ngữ khác.',
+  'flashnews.onlyDisplayOn': 'Tin tức sẽ chỉ hiển thị trên các ngôn ngữ đã chọn bên dưới (nếu có), không có tin tức nào được hiển thị, thậm chí trong ngôn ngữ mặc định, trong các ngôn ngữ khác.',
   'notifications.menu.all': 'Tất cả thông báo',
   'notifications.menu.following': 'Theo dõi',
   'notifications.menu.workshop': 'Hội thảo',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'Đánh dấu là đã đọc',
   'notifications.menu.delete': 'Xóa thông báo',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'Câu chuyện của bạn {storyName} đã tự động chuyển sang tạm dừng sau 6 tháng không hoạt động.',
-  'notification.workshopStoryStatus.toCancelled':
-    'Câu chuyện của bạn {storyName} đã tự động chuyển sang hủy sau một năm không hoạt động.',
+  'notification.workshopStoryStatus.toHiatus': 'Câu chuyện của bạn {storyName} đã tự động chuyển sang tạm dừng sau 6 tháng không hoạt động.',
+  'notification.workshopStoryStatus.toCancelled': 'Câu chuyện của bạn {storyName} đã tự động chuyển sang hủy sau một năm không hoạt động.',
   'notification.followingStoryStatus.toHiatus': '{storyName} đang tạm dừng.',
   'notification.followingStoryStatus.toCancelled': '{storyName} đã bị hủy.',
   'notification.followingNewChapter.new': 'Chương mới trong {storyName} đã được xuất bản!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'đã gửi cho bạn một lời mời kết bạn.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Đi đến câu chuyện',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'Đi đến câu chuyện'
+};
