24affdb745d12f75f5c5b85c3da4fe9f37833526
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:31:38 +0200

    New translations legal.js (Tamil)

--- ta-IN/legal.js
+++ ta-IN/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'மன்னிக்கவும். ஆனால் நீங்கள் மேலும் முன்பதிவு செய்ய முன் நீங்கள் எங்கள் சேவை விதிமுறைகள், தனியுரிமை கொள்கை மற்றும் பதிப்புரிமை ஒப்படைக்கும் என்பதை அறிவிக்க மாற்றப்பட்டிருக்கின்றோம்.',
-  'legal.modal.agree': 'நான் ஒப்புக்கொள்கிறேன்',
-  'legal.modal.disagree': 'நான் ஒப்புக்கொள்கிறவன் அல்லை',
-  'legal.modal.register': 'நீங்கள் பதிவு செய்யும் பின்னர் இதைப் பார்க்க மேலும் நீங்கள் இதைப் பார்க்காதீர்கள்.',
-  'legal.modal.goToSettings': 'உங்கள் அமைப்புகளுக்கு செல்லுங்கள்',
-  'legal.modal.oneLastThing': 'ஒரு கடைசி விஷயம்...',
-  'legal.modal.finish': 'முடிந்தது',
-  'legal.modal.existingUser': 'ஏற்கனவே பதிவு செய்யப்பட்டதா?',
-  'legal.modal.pleaseLogin': 'தயவுசெய்து உள்நுழையவும்.',
-  'legal.publicationAgreements': 'பதிப்பு ஒப்பநிலைகள்',
-  'legal.generalPublishingAgreement': 'பொது பதிப்பு ஒப்பநிலை',
-  'legal.documentsFor': '{workTitle}க்கான சட்ட ஆவணங்கள்',
-}
+    'legal.modal.intro': 'மன்னிக்கவும். ஆனால் நீங்கள் மேலும் முன்பதிவு செய்ய முன் நீங்கள் எங்கள் சேவை விதிமுறைகள், தனியுரிமை கொள்கை மற்றும் பதிப்புரிமை ஒப்படைக்கும் என்பதை அறிவிக்க மாற்றப்பட்டிருக்கின்றோம்.',
+    'legal.modal.agree': 'நான் ஒப்புக்கொள்கிறேன்',
+    'legal.modal.disagree': 'நான் ஒப்புக்கொள்கிறவன் அல்லை',
+    'legal.modal.register': 'நீங்கள் பதிவு செய்யும் பின்னர் இதைப் பார்க்க மேலும் நீங்கள் இதைப் பார்க்காதீர்கள்.',
+    'legal.modal.goToSettings': 'உங்கள் அமைப்புகளுக்கு செல்லுங்கள்',
+    'legal.modal.oneLastThing': 'ஒரு கடைசி விஷயம்...',
+    'legal.modal.finish': 'முடிந்தது',
+    'legal.modal.existingUser': 'ஏற்கனவே பதிவு செய்யப்பட்டதா?',
+    'legal.modal.pleaseLogin': 'தயவுசெய்து உள்நுழையவும்.',
+    'legal.publicationAgreements': 'பதிப்பு ஒப்பநிலைகள்',
+    'legal.generalPublishingAgreement': 'பொது பதிப்பு ஒப்பநிலை',
+    'legal.documentsFor': '{workTitle}க்கான சட்ட ஆவணங்கள்'
+};
