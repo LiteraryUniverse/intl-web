f1c0b39f2403e61dde372bdc4a00c14645b9cdc8
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:38:02 +0200

    New translations emails.js (Tibetan)

--- bo-BT/emails.js
+++ bo-BT/emails.js
@@ -1,57 +1,46 @@
 /**
  * @authors Jan Dvorak
  */
-/* eslint-disable max-len */
 export default {
-  'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
-  'email.enrollAccount.message': `Hello {username}\n\n
+    'email.enrollAccount.subject': 'Invitation to Literary Universe for {username}',
+    'email.enrollAccount.message': `Hello {username}\n\n
     We would like to invite you to Literary Universe! We have created a special personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading tools!
     To activate your account, simply click the link below:\n\n{url}`,
-  'email.resetPassword.subject': 'Literary Universe reset password',
-  'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
+    'email.resetPassword.subject': 'Literary Universe reset password',
+    'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
         Please follow the link bellow to reset your password:\n\n{url}`,
-  'email.verifyEmail.subject': 'Literary Universe e-mail verification',
-  'email.verifyEmail.message':
-    'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
-  // New html email keys
-  'email.salutation': 'Hello {username},',
-  'email.welcome': 'Welcome to Literary Universe!',
-  'email.signature': 'Sincerely,<br />Literary Universe team',
-  // Enrollment
-  'email.enrollAccount.welcome': 'Welcome aboard',
-  'email.enrollAccount.messageHtml':
-    'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
-  'email.enrollAccount.button': 'Access your account',
-  'email.enrollAccount.signature':
-    'ང་བཅས་དང་འདི་ལས་གནས་པའི་འགྲེལ་བཤད་སྟེ།<br />བཅུ་གཅིག་ལེན་མི་འདུག་པའི་སྒྲོམ་སྡེ་ཚན་རིགས་བྱ་བ་ཡོད།',
-  // Reset password
-  'email.resetPassword.messageHtml':
-    'ཁྱེད་ཀྱིས་ཁྱེད་ལས་ལུ་ཁྱོད་འདི་ཁྱོད་ཀྱིས་གནང་བ་བསྐྱར་བཅོས་འདི་ལུ་འགོ་བཙུགས་སྤྱོད་འབད་དེ་བཞིན་དུ་གནང་བའི་དྲི་བ་འབྱེད་པའི་དོན་ལུ་མིན་འདུག',
-  'email.resetPassword.button':
-    'ཁྱེད་ཀྱིས་གནང་བ་དང་འདི་ཁྱོད་ཀྱིས་འགོ་བཙུགས་སྤྱོད་འབད་དེ་བཞིན་དུ་གནང་བའི་དྲི་བ་འབྱེད་པའི་དོན་ལུ་མིན་འདུག',
-  // E-mail verification
-  'email.verifyEmail.messageHtml':
-    'ང་བཅས་དང་འདི་ལས་གནས་པའི་འགྲེལ་བཤད་སྟེ། ཁྱེད་ཀྱིས་ཁྱེད་ལས་ལུ་ཁྱོད་འདི་ཁྱོད་ཀྱིས་གནང་བ་བསྐྱར་བཅོས་འདི་ལུ་འགོ་བཙུགས་སྤྱོད་འབད་དེ་བཞིན་དུ་གནང་བའི་དྲི་བ་འབྱེད་པའི་དོན་ལུ་མིན་འདུག',
-  'email.verifyEmail.button':
-    'ཁྱེད་ཀྱིས་གནང་བ་དང་འདི་ཁྱོད་ཀྱིས་འགོ་བཙུགས་སྤྱོད་འབད་དེ་བཞིན་དུ་གནང་བའི་དྲི་བ་འབྱེད་པའི་དོན་ལུ་མིན་འདུག',
-  // Getting premium from admins
-  'email.adminUpgrade.subject':
-    'ཁྱེད་ཀྱིས་ཁྱེད་ལས་ལུ་ཁྱོད་འདི་གནང་བ་བསྐྱར་བཅོས་འདི་ལུ་འགོ་བཙུགས་སྤྱོད་འབད་དེ་བཞིན་དུ་གནང་བའི་དྲི་བ་འབྱེད་པའི་དོན་ལུ་མིན་འདུག',
-  'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
+    'email.verifyEmail.subject': 'Literary Universe e-mail verification',
+    'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
+    // New html email keys
+    'email.salutation': 'Hello {username},',
+    'email.welcome': 'Welcome to Literary Universe!',
+    'email.signature': 'Sincerely,<br />Literary Universe team',
+    // Enrollment
+    'email.enrollAccount.welcome': 'Welcome aboard',
+    'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe! Please join us in creating the next generation of creative writing and reading!',
+    'email.enrollAccount.button': 'Access your account',
+    'email.enrollAccount.signature': 'ང་བཅས་དང་འདི་ལས་གནས་པའི་འགྲེལ་བཤད་སྟེ།<br />བཅུ་གཅིག་ལེན་མི་འདུག་པའི་སྒྲོམ་སྡེ་ཚན་རིགས་བྱ་བ་ཡོད།',
+    // Reset password
+    'email.resetPassword.messageHtml': 'ཁྱེད་ཀྱིས་ཁྱེད་ལས་ལུ་ཁྱོད་འདི་ཁྱོད་ཀྱིས་གནང་བ་བསྐྱར་བཅོས་འདི་ལུ་འགོ་བཙུགས་སྤྱོད་འབད་དེ་བཞིན་དུ་གནང་བའི་དྲི་བ་འབྱེད་པའི་དོན་ལུ་མིན་འདུག',
+    'email.resetPassword.button': 'ཁྱེད་ཀྱིས་གནང་བ་དང་འདི་ཁྱོད་ཀྱིས་འགོ་བཙུགས་སྤྱོད་འབད་དེ་བཞིན་དུ་གནང་བའི་དྲི་བ་འབྱེད་པའི་དོན་ལུ་མིན་འདུག',
+    // E-mail verification
+    'email.verifyEmail.messageHtml': 'ང་བཅས་དང་འདི་ལས་གནས་པའི་འགྲེལ་བཤད་སྟེ། ཁྱེད་ཀྱིས་ཁྱེད་ལས་ལུ་ཁྱོད་འདི་ཁྱོད་ཀྱིས་གནང་བ་བསྐྱར་བཅོས་འདི་ལུ་འགོ་བཙུགས་སྤྱོད་འབད་དེ་བཞིན་དུ་གནང་བའི་དྲི་བ་འབྱེད་པའི་དོན་ལུ་མིན་འདུག',
+    'email.verifyEmail.button': 'ཁྱེད་ཀྱིས་གནང་བ་དང་འདི་ཁྱོད་ཀྱིས་འགོ་བཙུགས་སྤྱོད་འབད་དེ་བཞིན་དུ་གནང་བའི་དྲི་བ་འབྱེད་པའི་དོན་ལུ་མིན་འདུག',
+    // Getting premium from admins
+    'email.adminUpgrade.subject': 'ཁྱེད་ཀྱིས་ཁྱེད་ལས་ལུ་ཁྱོད་འདི་གནང་བ་བསྐྱར་བཅོས་འདི་ལུ་འགོ་བཙུགས་སྤྱོད་འབད་དེ་བཞིན་དུ་གནང་བའི་དྲི་བ་འབྱེད་པའི་དོན་ལུ་མིན་འདུག',
+    'email.adminUpgrade.message': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     adventurer {Adventurer}
     storyteller {Storyteller}
     other {}
   } tier for the next {days} days.\n\nEnjoy,\nLiterary Universe team`,
-  'email.adminUpgrade.messageHtml': `We have upgraded your account to {plan, select,
+    'email.adminUpgrade.messageHtml': `We have upgraded your account to {plan, select,
     explorer {Explorer}
     adventurer {Adventurer}
     storyteller {Storyteller}
     other {}
   } tier for the next {days} days. Enjoy!`,
-  'email.verification': 'ཨེ་མེན་སེམས་ཅན།',
-  'email.verifying':
-    'ཁྱེད་ཀྱིས་ཨེ་མེན་སེམས་བཟོ་བ་འདི་གིས་བྱུང་བའི་དུས་ཡུན་ནང་འཛིན་པའི་དུས་ཡུན་རྗེས་སུ་མཉེན་ཆས་འབད་དེ་བཞིན་པར་བཟོ་བ།',
-  'email.verified':
-    'རྗེས་སུ་ཨེ་མེན་སེམས་བཟོ་བ། ཁྱེད་ཀྱིས་ཨེ་མེན་སེམས་བཟོ་བའི་དུས་ཡུན་ནང་འཛིན་པའི་དུས་ཡུན་འདི་གིས་བྱུང་བའི་དུས་ཡུན་རྗེས་སུ་མཉེན་ཆས་འབད་དེ་བཞིན་པར་བཟོ་བ།',
-} /* eslint-enable max-len */
+    'email.verification': 'ཨེ་མེན་སེམས་ཅན།',
+    'email.verifying': 'ཁྱེད་ཀྱིས་ཨེ་མེན་སེམས་བཟོ་བ་འདི་གིས་བྱུང་བའི་དུས་ཡུན་ནང་འཛིན་པའི་དུས་ཡུན་རྗེས་སུ་མཉེན་ཆས་འབད་དེ་བཞིན་པར་བཟོ་བ།',
+    'email.verified': 'རྗེས་སུ་ཨེ་མེན་སེམས་བཟོ་བ། ཁྱེད་ཀྱིས་ཨེ་མེན་སེམས་བཟོ་བའི་དུས་ཡུན་ནང་འཛིན་པའི་དུས་ཡུན་འདི་གིས་བྱུང་བའི་དུས་ཡུན་རྗེས་སུ་མཉེན་ཆས་འབད་དེ་བཞིན་པར་བཟོ་བ།'
+};
