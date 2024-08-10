2af88d504aa5b2d80e8a5904ca8df92c199ea1f9
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:25:26 +0200

    New translations legal.js (Portuguese)

--- pt-PT/legal.js
+++ pt-PT/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Desculpe interromper. Mas antes de o deixarmos ir mais longe, estamos legalmente obrigados, a assegurar que está ciente e de acordo com os nossos Termos de Serviço, Política de Privacidade e Acordo de Direitos de Autor.',
-  'legal.modal.agree': 'Concordo',
-  'legal.modal.disagree': 'Discordo',
-  'legal.modal.register': 'Não verá mais isto se se registar.',
-  'legal.modal.goToSettings': 'Vá para as suas definições',
-  'legal.modal.oneLastThing': 'Apenas mais uma coisa...',
-  'legal.modal.finish': 'Finalizar',
-  'legal.modal.existingUser': 'Já está registrado?',
-  'legal.modal.pleaseLogin': 'Por favor, faça login.',
-  'legal.publicationAgreements': 'Acordos de publicação',
-  'legal.generalPublishingAgreement': 'Contrato Geral de Publicação',
-  'legal.documentsFor': 'Documentos legais para {workTitle}',
-}
+    'legal.modal.intro': 'Desculpe interromper. Mas antes de o deixarmos ir mais longe, estamos legalmente obrigados, a assegurar que está ciente e de acordo com os nossos Termos de Serviço, Política de Privacidade e Acordo de Direitos de Autor.',
+    'legal.modal.agree': 'Concordo',
+    'legal.modal.disagree': 'Discordo',
+    'legal.modal.register': 'Não verá mais isto se se registar.',
+    'legal.modal.goToSettings': 'Vá para as suas definições',
+    'legal.modal.oneLastThing': 'Apenas mais uma coisa...',
+    'legal.modal.finish': 'Finalizar',
+    'legal.modal.existingUser': 'Já está registrado?',
+    'legal.modal.pleaseLogin': 'Por favor, faça login.',
+    'legal.publicationAgreements': 'Acordos de publicação',
+    'legal.generalPublishingAgreement': 'Contrato Geral de Publicação',
+    'legal.documentsFor': 'Documentos legais para {workTitle}'
+};
