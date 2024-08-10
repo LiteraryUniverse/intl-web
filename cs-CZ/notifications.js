0fdc990a8c2665508a006b94744c4854058f02e5
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:15:24 +0200

    New translations notifications.js (Czech)

--- cs-CZ/notifications.js
+++ cs-CZ/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'Začněme zobrazovat tuto zprávu od',
   'flashnews.endsAt': 'Přestaneme zobrazovat tuto zprávu od',
   'flashnews.newLanguage': 'Vyberte nový jazyk pro přidání',
-  'flashnews.onlyDisplayOn':
-    'Zprávy budou zobrazeny pouze na jazycích vybraných níže (pokud jsou vybrány). Zpráva nebude zobrazena ve výchozím jazyku pro uživatele s jiným jazykem.',
+  'flashnews.onlyDisplayOn': 'Zprávy budou zobrazeny pouze na jazycích vybraných níže (pokud jsou vybrány). Zpráva nebude zobrazena ve výchozím jazyku pro uživatele s jiným jazykem.',
   'notifications.menu.all': 'Všechna oznámení',
   'notifications.menu.following': 'Sledující',
   'notifications.menu.workshop': 'Ateliér',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'Označit jako přečtené',
   'notifications.menu.delete': 'Smazat oznámení',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'Váš příběh {storyName} byl automaticky nastaven na hiatus po 6 měsících nečinnosti.',
-  'notification.workshopStoryStatus.toCancelled':
-    'Váš příběh {storyName} byl automaticky nastaven na zrušený po roce bez nových kapitol.',
+  'notification.workshopStoryStatus.toHiatus': 'Váš příběh {storyName} byl automaticky nastaven na hiatus po 6 měsících nečinnosti.',
+  'notification.workshopStoryStatus.toCancelled': 'Váš příběh {storyName} byl automaticky nastaven na zrušený po roce bez nových kapitol.',
   'notification.followingStoryStatus.toHiatus': '{storyName} je na hiatusu.',
   'notification.followingStoryStatus.toCancelled': '{storyName} byla zrušena.',
   'notification.followingNewChapter.new': 'Nová kapitola {storyName} byla publikována!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'Vám poslal/a žádost o přátelství.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Přejít na příběh',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'Přejít na příběh'
+};
