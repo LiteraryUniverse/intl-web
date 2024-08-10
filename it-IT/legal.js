1cb4788389c0366c7344f1c517d6a811fa8ac176
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:20:25 +0200

    New translations legal.js (Italian)

--- it-IT/legal.js
+++ it-IT/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    "Mi dispiace interrompere. Ma prima di lasciarti andare oltre, siamo legalmente obbligati a garantire che tu sia consapevole e d'accordo con i nostri Termini di servizio, Informativa sulla privacy e Accordo sul copyright.",
-  'legal.modal.agree': 'Accetto',
-  'legal.modal.disagree': 'Non accetto',
-  'legal.modal.register': 'Non vedrai più questo messaggio se ti registri.',
-  'legal.modal.goToSettings': 'Vai alle tue impostazioni',
-  'legal.modal.oneLastThing': 'Una cosa ancora...',
-  'legal.modal.finish': 'Finito',
-  'legal.modal.existingUser': 'Già registrato?',
-  'legal.modal.pleaseLogin': 'Effettua il login.',
-  'legal.publicationAgreements': 'Accordi di pubblicazione',
-  'legal.generalPublishingAgreement': 'Accordo di pubblicazione generale',
-  'legal.documentsFor': 'Documenti legali per {workTitle}',
-}
+    'legal.modal.intro': 'Mi dispiace interrompere. Ma prima di lasciarti andare oltre, siamo legalmente obbligati a garantire che tu sia consapevole e d\'accordo con i nostri Termini di servizio, Informativa sulla privacy e Accordo sul copyright.',
+    'legal.modal.agree': 'Accetto',
+    'legal.modal.disagree': 'Non accetto',
+    'legal.modal.register': 'Non vedrai più questo messaggio se ti registri.',
+    'legal.modal.goToSettings': 'Vai alle tue impostazioni',
+    'legal.modal.oneLastThing': 'Una cosa ancora...',
+    'legal.modal.finish': 'Finito',
+    'legal.modal.existingUser': 'Già registrato?',
+    'legal.modal.pleaseLogin': 'Effettua il login.',
+    'legal.publicationAgreements': 'Accordi di pubblicazione',
+    'legal.generalPublishingAgreement': 'Accordo di pubblicazione generale',
+    'legal.documentsFor': 'Documenti legali per {workTitle}'
+};
