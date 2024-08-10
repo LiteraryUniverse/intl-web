4db30b220c1e35151e3dbe2b7c1158b767eae78c
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:16:16 +0200

    New translations licenses.js (German)

--- de-DE/licenses.js
+++ de-DE/licenses.js
@@ -3,34 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'Über Lizenzen',
-  'licenses.more': 'Mehr Informationen',
-  'licenses.legal': 'Rechtliche Dokumente',
-  'licenses.name.cc-by': 'Creative Commons Namensnennung 4.0',
-  'licenses.cc-by':
-    'Diese Lizenz erlaubt es anderen, Ihre Arbeit zu verbreiten, zu remixen, zu optimieren und darauf aufzubauen, sogar kommerziell, solange sie Sie für die ursprüngliche Kreation nennen. Dies ist die entgegenkommendste der angebotenen Lizenzen. Empfohlen für die maximale Verbreitung und Nutzung von lizenziertem Material.',
-  'licenses.name.cc-by-sa':
-    'Creative Commons Namensnennung - Weitergabe unter gleichen Bedingungen 4.0',
-  'licenses.cc-by-sa':
-    'This license allows others to distribute, remix, tweak and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of the licenses offered. Recommended for maximum distribution and use of licensed material.',
-  'licenses.name.cc-by-nd': 'Creative Commons Namensnennung - Keine Bearbeitungen 4.0',
-  'licenses.cc-by-nd':
-    'Diese Lizenz erlaubt die Weitergabe, kommerziell und nicht-kommerziell, solange sie unverändert und im Ganzen weitergegeben wird, mit Nennung des Urhebers.',
-  'licenses.name.cc-by-nc': 'Creative Commons Namensnennung-Nicht kommerziell 4.0',
-  'licenses.cc-by-nc':
-    'Diese Lizenz erlaubt es anderen, Ihr Werk zu remixen, zu verändern und darauf aufzubauen, ohne dass dies kommerziell ist. Obwohl ihre neuen Werke Sie ebenfalls anerkennen müssen und nicht kommerziell sein dürfen, müssen sie ihre abgeleiteten Werke nicht zu den gleichen Bedingungen lizenzieren.',
-  'licenses.name.cc-by-nc-sa':
-    'Creative Commons Namensnennung - Nicht-kommerziell - Weitergabe unter gleichen Bedingungen 4.0',
-  'licenses.cc-by-nc-sa':
-    'Diese Lizenz erlaubt es anderen, Ihre Arbeit zu remixen, zu verändern und darauf aufzubauen, solange sie Sie als Urheber nennen und ihre neuen Kreationen unter den gleichen Bedingungen lizenzieren.',
-  'licenses.name.cc-by-nc-nd':
-    'Creative Commons Namensnennung - Nicht kommerziell - Keine Bearbeitungen 4.0',
-  'licenses.cc-by-nc-nd':
-    'Diese Lizenz ist die restriktivste unserer sechs Hauptlizenzen. Sie erlaubt es anderen nur, Ihre Werke herunterzuladen und sie mit anderen zu teilen, solange sie Sie als Urheber nennen, aber sie dürfen sie in keiner Weise verändern oder kommerziell nutzen.',
-  'licenses.name.lu-c': 'Literary Universe Copyrights',
-  'licenses.lu-c':
-    'Standardlizenz mit vollem Urheberrecht, die es Ihnen erlaubt, Ihr Werk beliebig zu verkaufen und Ihre Rechte daran schützen zu lassen. Diese Lizenz gibt Ihnen auch die Möglichkeit, Ihre Geschichten auf Literary Universe zu verkaufen.',
-  'licenses.name.cc0': 'Keine Rechte vorbehalten - CC0 1.0',
-  'licenses.cc0':
-    'Stellen Sie Ihr Werk in die Public Domain. Im Gegensatz zu den CC-Lizenzen, die es den Urhebern erlauben, aus einer Reihe von Berechtigungen zu wählen, während sie ihr Urheberrecht behalten, ermöglicht CC0 noch eine ganz andere Wahl - die Wahl, aus dem Urheberrecht und dem Datenbankschutz auszusteigen, und die exklusiven Rechte, die den Urhebern automatisch gewährt werden - die "no rights reserved"-Alternative zu unseren Lizenzen.',
-}
+    'licenses.about': 'Über Lizenzen',
+    'licenses.more': 'Mehr Informationen',
+    'licenses.legal': 'Rechtliche Dokumente',
+    'licenses.name.cc-by': 'Creative Commons Namensnennung 4.0',
+    'licenses.cc-by': 'Diese Lizenz erlaubt es anderen, Ihre Arbeit zu verbreiten, zu remixen, zu optimieren und darauf aufzubauen, sogar kommerziell, solange sie Sie für die ursprüngliche Kreation nennen. Dies ist die entgegenkommendste der angebotenen Lizenzen. Empfohlen für die maximale Verbreitung und Nutzung von lizenziertem Material.',
+    'licenses.name.cc-by-sa': 'Creative Commons Namensnennung - Weitergabe unter gleichen Bedingungen 4.0',
+    'licenses.cc-by-sa': 'This license allows others to distribute, remix, tweak and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of the licenses offered. Recommended for maximum distribution and use of licensed material.',
+    'licenses.name.cc-by-nd': 'Creative Commons Namensnennung - Keine Bearbeitungen 4.0',
+    'licenses.cc-by-nd': 'Diese Lizenz erlaubt die Weitergabe, kommerziell und nicht-kommerziell, solange sie unverändert und im Ganzen weitergegeben wird, mit Nennung des Urhebers.',
+    'licenses.name.cc-by-nc': 'Creative Commons Namensnennung-Nicht kommerziell 4.0',
+    'licenses.cc-by-nc': 'Diese Lizenz erlaubt es anderen, Ihr Werk zu remixen, zu verändern und darauf aufzubauen, ohne dass dies kommerziell ist. Obwohl ihre neuen Werke Sie ebenfalls anerkennen müssen und nicht kommerziell sein dürfen, müssen sie ihre abgeleiteten Werke nicht zu den gleichen Bedingungen lizenzieren.',
+    'licenses.name.cc-by-nc-sa': 'Creative Commons Namensnennung - Nicht-kommerziell - Weitergabe unter gleichen Bedingungen 4.0',
+    'licenses.cc-by-nc-sa': 'Diese Lizenz erlaubt es anderen, Ihre Arbeit zu remixen, zu verändern und darauf aufzubauen, solange sie Sie als Urheber nennen und ihre neuen Kreationen unter den gleichen Bedingungen lizenzieren.',
+    'licenses.name.cc-by-nc-nd': 'Creative Commons Namensnennung - Nicht kommerziell - Keine Bearbeitungen 4.0',
+    'licenses.cc-by-nc-nd': 'Diese Lizenz ist die restriktivste unserer sechs Hauptlizenzen. Sie erlaubt es anderen nur, Ihre Werke herunterzuladen und sie mit anderen zu teilen, solange sie Sie als Urheber nennen, aber sie dürfen sie in keiner Weise verändern oder kommerziell nutzen.',
+    'licenses.name.lu-c': 'Literary Universe Copyrights',
+    'licenses.lu-c': 'Standardlizenz mit vollem Urheberrecht, die es Ihnen erlaubt, Ihr Werk beliebig zu verkaufen und Ihre Rechte daran schützen zu lassen. Diese Lizenz gibt Ihnen auch die Möglichkeit, Ihre Geschichten auf Literary Universe zu verkaufen.',
+    'licenses.name.cc0': 'Keine Rechte vorbehalten - CC0 1.0',
+    'licenses.cc0': 'Stellen Sie Ihr Werk in die Public Domain. Im Gegensatz zu den CC-Lizenzen, die es den Urhebern erlauben, aus einer Reihe von Berechtigungen zu wählen, während sie ihr Urheberrecht behalten, ermöglicht CC0 noch eine ganz andere Wahl - die Wahl, aus dem Urheberrecht und dem Datenbankschutz auszusteigen, und die exklusiven Rechte, die den Urhebern automatisch gewährt werden - die "no rights reserved"-Alternative zu unseren Lizenzen.'
+};
