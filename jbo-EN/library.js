bf85e45df67d2ca69a5d22ce7ef901213f5f6c8a
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:37:14 +0200

    New translations library.js (Lojban)

--- jbo-EN/library.js
+++ jbo-EN/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'Jmina lo rirni',
-  'library.remove': 'Cfari lo rirni',
-  'library.bought': 'Xruti lo pelji',
-  'library.stories': 'Cuxna lo pelji',
-  'library.universes': 'Jorne universiteto',
-  'bookmark.continueReading': 'Jorne lo nu tcidu',
-  'bookmark.statusSummary': "I do'u cu tcidu {readPercent, number, ::percent} be lo lisri.",
-}
+    'library.add': 'Jmina lo rirni',
+    'library.remove': 'Cfari lo rirni',
+    'library.bought': 'Xruti lo pelji',
+    'library.stories': 'Cuxna lo pelji',
+    'library.universes': 'Jorne universiteto',
+    'bookmark.continueReading': 'Jorne lo nu tcidu',
+    'bookmark.statusSummary': 'I do\'u cu tcidu {readPercent, number, ::percent} be lo lisri.'
+};
