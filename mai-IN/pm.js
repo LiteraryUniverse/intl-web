4be42a1f870619429f50c6cce5bc4c29e4c9d8d3
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:39:53 +0200

    New translations pm.js (Maithili)

--- mai-IN/pm.js
+++ mai-IN/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'वार्तालाप',
-  'pm.showolder': 'पुराने संदेश देखाएँ',
-  'pm.errors.saysomething': 'तपाईंले निश्चित रूपमा केही भन्नु पर्छ...',
-  'pm.errors.addrecipient': 'तपाईंले कम्तिमा एक अर्को प्रयोगकर्ता थप्नुपर्छ।',
-  'pm.message': 'संदेश',
-  'pm.send.message': 'संदेश पठाउनुहोस्',
-  'pm.messages.own': 'तपाईंको संदेशहरू',
-  'pm.conversations.none': 'तपाईं कसीसँग वार्ता गरिरहेका छैनन्।',
-  'pm.to': 'सम्म:',
-  'pm.with': 'संवाद संग:',
-  'pm.send': 'पठाउनुहोस्',
-  'pm.conversations.unreadCount': `तपाईंलाई {count, plural,
+    'pm.conversation': 'वार्तालाप',
+    'pm.showolder': 'पुराने संदेश देखाएँ',
+    'pm.errors.saysomething': 'तपाईंले निश्चित रूपमा केही भन्नु पर्छ...',
+    'pm.errors.addrecipient': 'तपाईंले कम्तिमा एक अर्को प्रयोगकर्ता थप्नुपर्छ।',
+    'pm.message': 'संदेश',
+    'pm.send.message': 'संदेश पठाउनुहोस्',
+    'pm.messages.own': 'तपाईंको संदेशहरू',
+    'pm.conversations.none': 'तपाईं कसीसँग वार्ता गरिरहेका छैनन्।',
+    'pm.to': 'सम्म:',
+    'pm.with': 'संवाद संग:',
+    'pm.send': 'पठाउनुहोस्',
+    'pm.conversations.unreadCount': `तपाईंलाई {count, plural,
     zero {कुनै अपठित वार्तालाप छैन}
     one {एक अपठित वार्तालाप छ}
     other {# अपठित वार्तालापहरू छन्}
   }.`,
-  'pm.form.minimumParticipants': 'तपाईंले प्राप्तकर्ता छनौट गर्नुपर्छ।',
-  'pm.form.maxParticipants': `तुम्हारे पास एक वार्तालाप में अधिकतम {maxPeople, plural,
+    'pm.form.minimumParticipants': 'तपाईंले प्राप्तकर्ता छनौट गर्नुपर्छ।',
+    'pm.form.maxParticipants': `तुम्हारे पास एक वार्तालाप में अधिकतम {maxPeople, plural,
     one {एक व्यक्ति}
     other {# लोग}
   } हो सकते हैं।`,
-  'pm.new.aria': 'नयाँ वार्तालाप',
-  'pm.new.buttonTitle': 'नयाँ वार्तालाप सुरु गर्नुहोस्',
-}
+    'pm.new.aria': 'नयाँ वार्तालाप',
+    'pm.new.buttonTitle': 'नयाँ वार्तालाप सुरु गर्नुहोस्'
+};
