8832802a3a976eb354be4c7650a5b5a1d7462884
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:24:23 +0200

    New translations user.js (Punjabi)

--- pa-IN/user.js
+++ pa-IN/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'ਮਿੱਤਰਾਂ ਵਿੱਚ ਸ਼ਾਮਲ ਕਰੋ',
-  'user.friend.requests': `ਤੁਹਾਡੇ ਕੋਲ {num, number} ਨਵੇਂ ਦੋਸਤ {num, plural,
+    'user.friend.request.add': 'ਮਿੱਤਰਾਂ ਵਿੱਚ ਸ਼ਾਮਲ ਕਰੋ',
+    'user.friend.requests': `ਤੁਹਾਡੇ ਕੋਲ {num, number} ਨਵੇਂ ਦੋਸਤ {num, plural,
     zero {ਬੇਨਤੀਆਂ ਹਨ}
     one {ਬੇਨਤੀ}
     many {ਬੇਨਤੀਆਂ ਹਨ}
     other {ਬੇਨਤੀਆਂ ਹਨ}
   }`,
-  'user.friend.unfriend': 'ਮਿੱਤਰੀ ਤੋਂ ਹਟਾਓ',
-  'user.block': 'ਬਲਾਕ ਕਰੋ',
-  'user.unblock': 'ਅਨਬਲਾਕ ਕਰੋ',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'ਮਿੱਤਰੀ ਤੋਂ ਹਟਾਓ',
+    'user.block': 'ਬਲਾਕ ਕਰੋ',
+    'user.unblock': 'ਅਨਬਲਾਕ ਕਰੋ',
+    'user.joined': `{gender, select,
     male {ਨੇ}
     female {ਵਿੱਚ ਸ਼ਾਮਲ ਹੋਇਆ!}
     other {ਨੇ}} ਵਿੱਚ ਸ਼ਾਮਲ ਹੋਇਆ!  ਤੇ {date, date, long}ਨੇ ਸ਼ਾਮਲ ਹੋਇਆ!`,
-  'user.friend.request.accept': 'ਮਿੱਤਰੀ ਦੀ ਬੇਨਤੀ ਮਨਜ਼ੂਰ ਕਰੋ',
-  'user.friend.request.cancel': 'ਮਿੱਤਰੀ ਦੀ ਬੇਨਤੀ ਰੱਦ ਕਰੋ',
-  'user.friend.request.deny': 'ਮਿੱਤਰੀ ਨੂੰ ਇਨਕਾਰ ਕਰੋ',
-  'user.listing': 'ਯੂਜ਼ਰ ਦੀ ਲਿਸਟਿੰਗ',
-  'user.profile.visit': 'ਵੇਖੋ {user}ਦਾ ਪ੍ਰੋਫਾਈਲ।',
-  'user.profile.avatar': '{username}ਦਾ ਅਵਤਾਰ',
-  'user.friend.list.requests': 'ਮਿੱਤਰੀ ਦੀਆਂ ਬੇਨਤੀਆਂ',
-  'user.friend.norequests': 'ਤੁਹਾਡੇ ਕੋਲ ਮਿੱਤਰੀ ਦੀਆਂ ਬੇਨਤੀਆਂ ਨਹੀਂ ਹਨ।',
-  'user.dashboardWelcome': `ਸ਼ੁਭ ਸਵੇਰ! {dayPart, select,
+    'user.friend.request.accept': 'ਮਿੱਤਰੀ ਦੀ ਬੇਨਤੀ ਮਨਜ਼ੂਰ ਕਰੋ',
+    'user.friend.request.cancel': 'ਮਿੱਤਰੀ ਦੀ ਬੇਨਤੀ ਰੱਦ ਕਰੋ',
+    'user.friend.request.deny': 'ਮਿੱਤਰੀ ਨੂੰ ਇਨਕਾਰ ਕਰੋ',
+    'user.listing': 'ਯੂਜ਼ਰ ਦੀ ਲਿਸਟਿੰਗ',
+    'user.profile.visit': 'ਵੇਖੋ {user}ਦਾ ਪ੍ਰੋਫਾਈਲ।',
+    'user.profile.avatar': '{username}ਦਾ ਅਵਤਾਰ',
+    'user.friend.list.requests': 'ਮਿੱਤਰੀ ਦੀਆਂ ਬੇਨਤੀਆਂ',
+    'user.friend.norequests': 'ਤੁਹਾਡੇ ਕੋਲ ਮਿੱਤਰੀ ਦੀਆਂ ਬੇਨਤੀਆਂ ਨਹੀਂ ਹਨ।',
+    'user.dashboardWelcome': `ਸ਼ੁਭ ਸਵੇਰ! {dayPart, select,
     morning {ਸ਼ਾਮ!}
     afternoon {ਸ਼ਾਮ!}
     evening {ਰਾਤ!}
     night {ਰਾਤ!}
     other {ਦਿਨ!}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'ਇਸ ਲੇਖਕ ਨੇ ਰਵਾਇਤੀ ਤਰੀਕੇ ਨਾਲ ਪ੍ਰਕਾਸ਼ਿਤ ਹੋਇਆ ਹੈ',
-  'user.verification.luAuthor': 'ਸਾਹਿਤਿਕ ਯੂਨੀਵਰਸ ਦੇ ਮੌਜੂਦਾ ਲੇਖਕ',
-  'user.verification.luEmployee': 'ਸਾਹਿਤਿਕ ਯੂਨੀਵਰਸ ਦੇ ਕਰਮਚਾਰੀ',
-  'user.verification.publicFigure': 'ਪ੍ਰਮਾਣਿਤ ਜਨਤਕ ਵਖਰਾ',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'ਇਸ ਲੇਖਕ ਨੇ ਰਵਾਇਤੀ ਤਰੀਕੇ ਨਾਲ ਪ੍ਰਕਾਸ਼ਿਤ ਹੋਇਆ ਹੈ',
+    'user.verification.luAuthor': 'ਸਾਹਿਤਿਕ ਯੂਨੀਵਰਸ ਦੇ ਮੌਜੂਦਾ ਲੇਖਕ',
+    'user.verification.luEmployee': 'ਸਾਹਿਤਿਕ ਯੂਨੀਵਰਸ ਦੇ ਕਰਮਚਾਰੀ',
+    'user.verification.publicFigure': 'ਪ੍ਰਮਾਣਿਤ ਜਨਤਕ ਵਖਰਾ',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {ਕਹਾਣੀ}
     other {ਕਹਾਣੀਆਂ}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {ਬ੍ਰਹਮਾਂਡ}
     other {ਬ੍ਰਹਮਾਂਡਾਂ}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {ਫੈਨ ਫਿਕਸ਼ਨ}
     other {ਫੈਨ ਫਿਕਸ਼ਨਾਂ} 
   }`,
-  'profile.works': 'ਕੰਮ',
-  // As in creative/literary works
-  'profile.wishlist': 'ਇੱਛ-ਸੂਚੀ',
-  'profile.storiesBy': '! {username}! ਵੱਡੀਆਂ ਕਹਾਣੀਆਂ',
-  'profile.universesBy': '! {username}! ਵੱਡੇ ਬ੍ਰਹਮਾਂਡ',
-  'profile.fanFictionBy': '! {username}! ਵੱਡੀਆਂ ਫੈਨ ਫਿਕਸ਼ਨਾਂ',
-}
+    'profile.works': 'ਕੰਮ',
+    // As in creative/literary works
+    'profile.wishlist': 'ਇੱਛ-ਸੂਚੀ',
+    'profile.storiesBy': '! {username}! ਵੱਡੀਆਂ ਕਹਾਣੀਆਂ',
+    'profile.universesBy': '! {username}! ਵੱਡੇ ਬ੍ਰਹਮਾਂਡ',
+    'profile.fanFictionBy': '! {username}! ਵੱਡੀਆਂ ਫੈਨ ਫਿਕਸ਼ਨਾਂ'
+};
