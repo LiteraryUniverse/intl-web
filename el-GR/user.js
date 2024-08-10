f498421236287fd4b7162a3d2ca9bd10379fefb6
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:17:08 +0200

    New translations user.js (Greek)

--- el-GR/user.js
+++ el-GR/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'Προσθήκη στους φίλους',
-  'user.friend.requests': `Έχετε {num, number} νέο φίλο {num, plural,
+    'user.friend.request.add': 'Προσθήκη στους φίλους',
+    'user.friend.requests': `Έχετε {num, number} νέο φίλο {num, plural,
     zero {αιτήματα}
     one {αίτημα}
     many {αιτήματα}
     other {αιτήματα}
   }`,
-  'user.friend.unfriend': 'Αποσύνδεση από τους φίλους',
-  'user.block': 'Αποκλεισμός',
-  'user.unblock': 'Ξεμπλοκάρισμα',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'Αποσύνδεση από τους φίλους',
+    'user.block': 'Αποκλεισμός',
+    'user.unblock': 'Ξεμπλοκάρισμα',
+    'user.joined': `{gender, select,
     male {Εγγράφηκε στις}
     female {Εγγράφηκε στις}
     other {Εγγράφηκε στις}} στις {date, date, long}`,
-  'user.friend.request.accept': 'Αποδοχή αίτημα φιλίας',
-  'user.friend.request.cancel': 'Ακύρωση αίτημα φιλίας',
-  'user.friend.request.deny': 'Απόρριψη φιλίας',
-  'user.listing': 'Λίστα χρηστών',
-  'user.profile.visit': 'Επίσκεψη στο προφίλ του χρήστη {user}',
-  'user.profile.avatar': 'Άβαταρ του χρήστη {username}',
-  'user.friend.list.requests': 'Αιτήματα φιλίας',
-  'user.friend.norequests': 'Δεν έχετε αιτήματα φιλίας.',
-  'user.dashboardWelcome': `Καλημέρα {dayPart, select,
+    'user.friend.request.accept': 'Αποδοχή αίτημα φιλίας',
+    'user.friend.request.cancel': 'Ακύρωση αίτημα φιλίας',
+    'user.friend.request.deny': 'Απόρριψη φιλίας',
+    'user.listing': 'Λίστα χρηστών',
+    'user.profile.visit': 'Επίσκεψη στο προφίλ του χρήστη {user}',
+    'user.profile.avatar': 'Άβαταρ του χρήστη {username}',
+    'user.friend.list.requests': 'Αιτήματα φιλίας',
+    'user.friend.norequests': 'Δεν έχετε αιτήματα φιλίας.',
+    'user.dashboardWelcome': `Καλημέρα {dayPart, select,
     morning {καλησπέρα}
     afternoon {καλησπέρα}
     evening {καλησπέρα}
     night {καληνύχτα}
     other {ημέρα}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'Αυτός ο συγγραφέας έχει δημοσιεύσει τον παραδοσιακό τρόπο',
-  'user.verification.luAuthor': 'Συγγραφέας βασισμένος στο Λογοτεχνικό Σύμπαν',
-  'user.verification.luEmployee': 'Υπάλληλος του Λογοτεχνικού Σύμπαντος',
-  'user.verification.publicFigure': 'Επαληθευμένο δημόσιο πρόσωπο',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'Αυτός ο συγγραφέας έχει δημοσιεύσει τον παραδοσιακό τρόπο',
+    'user.verification.luAuthor': 'Συγγραφέας βασισμένος στο Λογοτεχνικό Σύμπαν',
+    'user.verification.luEmployee': 'Υπάλληλος του Λογοτεχνικού Σύμπαντος',
+    'user.verification.publicFigure': 'Επαληθευμένο δημόσιο πρόσωπο',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {ιστορία}
     other {ιστορίες}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {σύμπαν}
     other {σύμπαντα}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {fan fiction}
     other {fan fictions} 
   }`,
-  'profile.works': 'Έργα',
-  // As in creative/literary works
-  'profile.wishlist': 'Λίστα Επιθυμιών',
-  'profile.storiesBy': 'Ιστορίες που δημιουργήθηκαν από {username}',
-  'profile.universesBy': 'Σύμπαντα που δημιουργήθηκαν από {username}',
-  'profile.fanFictionBy': 'Fan Fiction που δημιουργήθηκαν από {username}',
-}
+    'profile.works': 'Έργα',
+    // As in creative/literary works
+    'profile.wishlist': 'Λίστα Επιθυμιών',
+    'profile.storiesBy': 'Ιστορίες που δημιουργήθηκαν από {username}',
+    'profile.universesBy': 'Σύμπαντα που δημιουργήθηκαν από {username}',
+    'profile.fanFictionBy': 'Fan Fiction που δημιουργήθηκαν από {username}'
+};
