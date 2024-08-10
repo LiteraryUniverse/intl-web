2b5ec214a479d4d52eb7cfadbed4f57f9e82c71e
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:20:19 +0200

    New translations rating.js (Italian)

--- it-IT/rating.js
+++ it-IT/rating.js
@@ -2,19 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Classificazione',
-  'rating.explained': 'Classificazione e restrizioni di età.',
-  'rating.all': 'Tutti',
-  'rating.desc.all': 'Accessibile a tutte le età e tutti i gruppi.',
-  'rating.teens': 'Ragazzi',
-  'rating.desc.teens': 'Adatto ad un pubblico di età compresa tra 12 e 18 anni.',
-  'rating.mature': 'Adulti',
-  'rating.desc.mature': 'Contiene temi avanzati, linguaggio grezzo e scene implicite.',
-  'rating.explicit': 'Esplicito',
-  'rating.desc.explicit': 'Contiene scene esplicite.',
-  'rating.selection.all': 'Tutti - Accessibile a tutte le età e tutti i gruppi.',
-  'rating.selection.teens': 'Ragazzi (12-18) - Adatto ad un pubblico tra i 12 e i 18 anni.',
-  'rating.selection.mature':
-    'Adulti (18+) - Contiene i temi avanzati, il linguaggio rozzo e scene implicite.',
-  'rating.selection.explicit': 'Esplicito (21+) - Contiene scene esplicite.',
-}
+    'rating.name': 'Classificazione',
+    'rating.explained': 'Classificazione e restrizioni di età.',
+    'rating.all': 'Tutti',
+    'rating.desc.all': 'Accessibile a tutte le età e tutti i gruppi.',
+    'rating.teens': 'Ragazzi',
+    'rating.desc.teens': 'Adatto ad un pubblico di età compresa tra 12 e 18 anni.',
+    'rating.mature': 'Adulti',
+    'rating.desc.mature': 'Contiene temi avanzati, linguaggio grezzo e scene implicite.',
+    'rating.explicit': 'Esplicito',
+    'rating.desc.explicit': 'Contiene scene esplicite.',
+    'rating.selection.all': 'Tutti - Accessibile a tutte le età e tutti i gruppi.',
+    'rating.selection.teens': 'Ragazzi (12-18) - Adatto ad un pubblico tra i 12 e i 18 anni.',
+    'rating.selection.mature': 'Adulti (18+) - Contiene i temi avanzati, il linguaggio rozzo e scene implicite.',
+    'rating.selection.explicit': 'Esplicito (21+) - Contiene scene esplicite.'
+};
