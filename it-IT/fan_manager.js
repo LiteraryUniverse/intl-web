d2434b8a7d209a51fd48b6950b23c8455684dea2
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:20:33 +0200

    New translations fan_manager.js (Italian)

--- it-IT/fan_manager.js
+++ it-IT/fan_manager.js
@@ -1,25 +1,26 @@
 export default {
   'fanManager.settings.for': 'Impostazioni del gestore dei fan',
-  'fanManager.allowArtSubmissions': "Consenti l'invio di fan art",
+  'fanManager.allowArtSubmissions': 'Consenti l\'invio di fan art',
   'fanManager.displayFanArt': 'Consenti la visualizzazione di fan art',
   'fanManager.allowFanStories': 'Consenti fan fiction',
   'fanManager.displayFanStories': 'Consenti la visualizzazione di storie dei fan',
-  'fanManager.state.aria': "Seleziona lo stato dell'opera d'arte inviata",
+  'fanManager.state.aria': 'Seleziona lo stato dell\'opera d\'arte inviata',
   'fanManager.state.submitted': 'Inviata',
   'fanManager.state.approved': 'Approvata',
   'fanManager.state.rejected': 'Rifiutata',
   'fanManager.state.hidden': 'Nascosta',
   'fanManager.publicComment': 'Commento pubblico',
-  'fanManager.publicComment.desc':
-    'Questo testo verrà visualizzato nella pagina pubblica come tuo commento ufficiale su questa opera.',
-  'fanManager.authorComment': "Commento per l'autore",
-  'fanManager.authorComment.desc':
-    "Questo è un commento privato da parte tua all'autore di questa opera.",
+  'fanManager.publicComment.desc': 'Questo testo verrà visualizzato nella pagina pubblica come tuo commento ufficiale su questa opera.',
+  'fanManager.authorComment': 'Commento per l\'autore',
+  'fanManager.authorComment.desc': 'Questo è un commento privato da parte tua all\'autore di questa opera.',
   'fanManager.stateChange.approve': 'Approva la fan art',
   'fanManager.stateChange.reject': 'Rifiuta la fan art',
   'fanManager.stateChange.hide': 'Nascondi la fan art',
   'fanManager.stateChange.unhide': 'Rendi visibile la fan art',
-  'fanManager.fanFictionNotAllowed':
-    'La fan fiction non è consentita dalla natura della tua licenza.',
+  'fanManager.fanFictionNotAllowed': 'La fan fiction non è consentita dalla natura della tua licenza.',
   'fanManager.fanFiction.maxRating': 'Valutazione massima delle fan fiction visualizzate.',
-}
+  'fanManager.hidden.nothingFound': 'Nessuna fan art nascosta trovata.',
+  'fanManager.rejected.nothingFound': 'Nessuna fan art respinta trovata.',
+  'fanManager.approved.nothingFound': 'Nessuna fan art approvata trovata.',
+  'fanManager.submitted.nothingFound': 'Nessuna fan art inviata trovata.'
+};
