dfa19478936fa9c303716228cac77c9c7faec2d8
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:22:46 +0200

    New translations notifications.js (Lithuanian)

--- lt-LT/notifications.js
+++ lt-LT/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'Pradėti šią žinutę rodyti nuo',
   'flashnews.endsAt': 'Nustoti šią žinutę rodyti nuo',
   'flashnews.newLanguage': 'Pasirinkite naują kalbą pridėti',
-  'flashnews.onlyDisplayOn':
-    'Naujienos bus rodomos tik pasirinktose žemiau nurodytose kalbose (jei pasirinkta), kitose kalbose nebus rodomos jokios naujienos, net ir numatytoje kalboje.',
+  'flashnews.onlyDisplayOn': 'Naujienos bus rodomos tik pasirinktose žemiau nurodytose kalbose (jei pasirinkta), kitose kalbose nebus rodomos jokios naujienos, net ir numatytoje kalboje.',
   'notifications.menu.all': 'Visi pranešimai',
   'notifications.menu.following': 'Sekimas',
   'notifications.menu.workshop': 'Dirbtuvės',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'Pažymėti kaip perskaitytą',
   'notifications.menu.delete': 'Ištrinti pranešimą',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'Jūsų istorija {storyName} automatiškai nustatyta kaip laikinas po 6 mėnesių neaktyvumo.',
-  'notification.workshopStoryStatus.toCancelled':
-    'Jūsų istorija {storyName} automatiškai nustatyta kaip atšaukta po vienerių metų neaktyvumo.',
+  'notification.workshopStoryStatus.toHiatus': 'Jūsų istorija {storyName} automatiškai nustatyta kaip laikinas po 6 mėnesių neaktyvumo.',
+  'notification.workshopStoryStatus.toCancelled': 'Jūsų istorija {storyName} automatiškai nustatyta kaip atšaukta po vienerių metų neaktyvumo.',
   'notification.followingStoryStatus.toHiatus': '{storyName} sustojo.',
   'notification.followingStoryStatus.toCancelled': '{storyName} buvo atšauktas.',
   'notification.followingNewChapter.new': 'Paskelbtas naujas skyrius {storyName}!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'siuntė jums draugystės prašymą.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Eiti į istoriją',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'Eiti į istoriją'
+};
