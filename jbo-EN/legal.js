a28165773469b15b07ee517cb7e4b6e7419aa4f2
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:37:13 +0200

    New translations legal.js (Lojban)

--- jbo-EN/legal.js
+++ jbo-EN/legal.js
@@ -1,16 +1,14 @@
 export default {
-  'legal.modal.intro':
-    "coi la'e mi na'e jai se djica lo nu mi zukte lo se du'u mi jimpe lo te pilno be do gi'e sidju lo nu do tugni lo te pilno be mi lo se du'u mi se finti lo te pilno be do",
-  'legal.modal.agree': 'mi tugni',
-  'legal.modal.disagree': 'mi na tugni',
-  'legal.modal.register': "do na viska ti ze'u lo nu do se sinma",
-  'legal.modal.goToSettings': 'klama lo se setca be do',
-  'legal.modal.oneLastThing': "Lo nu cu bunpe'i...",
-  'legal.modal.finish': 'mulgau',
-  'legal.modal.existingUser': "co'e se tcidu?",
-  'legal.modal.pleaseLogin': "co'a logji",
-  'legal.publicationAgreements': 'lo se tcita be le pamoi se pagbu',
-  'legal.generalPublishingAgreement':
-    'lo se tcita be le pamoi se pagbu be lo se tcita be lo se jibri',
-  'legal.documentsFor': 'Legal documents for {workTitle}',
-}
+    'legal.modal.intro': 'coi la\'e mi na\'e jai se djica lo nu mi zukte lo se du\'u mi jimpe lo te pilno be do gi\'e sidju lo nu do tugni lo te pilno be mi lo se du\'u mi se finti lo te pilno be do',
+    'legal.modal.agree': 'mi tugni',
+    'legal.modal.disagree': 'mi na tugni',
+    'legal.modal.register': 'do na viska ti ze\'u lo nu do se sinma',
+    'legal.modal.goToSettings': 'klama lo se setca be do',
+    'legal.modal.oneLastThing': 'Lo nu cu bunpe\'i...',
+    'legal.modal.finish': 'mulgau',
+    'legal.modal.existingUser': 'co\'e se tcidu?',
+    'legal.modal.pleaseLogin': 'co\'a logji',
+    'legal.publicationAgreements': 'lo se tcita be le pamoi se pagbu',
+    'legal.generalPublishingAgreement': 'lo se tcita be le pamoi se pagbu be lo se tcita be lo se jibri',
+    'legal.documentsFor': 'Legal documents for {workTitle}'
+};
