dc255774fbc2a080c8ad38edb2d76579e89c0932
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:40:18 +0200

    New translations feed.js (Odia)

--- or-IN/feed.js
+++ or-IN/feed.js
@@ -1,9 +1,9 @@
 export default {
-  'feed.name': 'ଫିଡ',
-  'feed.post.new': 'ନୂତନ ପୋଷ୍ଟ',
-  'feed.post.like': 'ପୋଷ୍ଟକୁ ପସନ୍ଦ କରନ୍ତୁ',
-  'feed.post.unlike': 'ପୋଷ୍ଟକୁ ଅପସନ୍ଦ କରନ୍ତୁ',
-  'feed.post.send': 'ପୋଷ୍ଟ',
-  'feed.empty': 'ଫିଡ୍‌ରେ କୌଣସି ଏଣ୍ଟ୍ରି ନାହିଁ।',
-  'feed.placeholder': 'ତୁମର ମନରେ କିଛି ଅଛି?',
-}
+    'feed.name': 'ଫିଡ',
+    'feed.post.new': 'ନୂତନ ପୋଷ୍ଟ',
+    'feed.post.like': 'ପୋଷ୍ଟକୁ ପସନ୍ଦ କରନ୍ତୁ',
+    'feed.post.unlike': 'ପୋଷ୍ଟକୁ ଅପସନ୍ଦ କରନ୍ତୁ',
+    'feed.post.send': 'ପୋଷ୍ଟ',
+    'feed.empty': 'ଫିଡ୍‌ରେ କୌଣସି ଏଣ୍ଟ୍ରି ନାହିଁ।',
+    'feed.placeholder': 'ତୁମର ମନରେ କିଛି ଅଛି?'
+};
