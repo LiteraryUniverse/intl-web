4c5390fc573d63f93a6cf5bf4f24c8d45516f52a
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:32:18 +0200

    New translations notifications.js (Bengali)

--- bn-BD/notifications.js
+++ bn-BD/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'এই মেসেজ টি প্রদর্শন শুরু করুন',
   'flashnews.endsAt': 'এই মেসেজ টি প্রদর্শন বন্ধ করুন',
   'flashnews.newLanguage': 'নতুন ভাষা যোগ করতে নির্বাচন করুন',
-  'flashnews.onlyDisplayOn':
-    'এ সংবাদ কেবলমাত্র নীচে নির্বাচিত ভাষাতে (যদি কোন ভাষা নির্বাচন করা হয়) প্রদর্শন করা হবে, অন্য কোন ভাষায় কোন সংবাদ প্রদর্শন করা হবে না, যদি কোন ভাষা নির্বাচন করা না হয়ে থাকে।',
+  'flashnews.onlyDisplayOn': 'এ সংবাদ কেবলমাত্র নীচে নির্বাচিত ভাষাতে (যদি কোন ভাষা নির্বাচন করা হয়) প্রদর্শন করা হবে, অন্য কোন ভাষায় কোন সংবাদ প্রদর্শন করা হবে না, যদি কোন ভাষা নির্বাচন করা না হয়ে থাকে।',
   'notifications.menu.all': 'সমস্ত বিজ্ঞপ্তি',
   'notifications.menu.following': 'অনুসরণ করা হয়েছে',
   'notifications.menu.workshop': 'কর্মশালা',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'পঠিত হিসাবে চিহ্নিত করুন',
   'notifications.menu.delete': 'বিজ্ঞপ্তি মুছে ফেলুন',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'আপনার গল্প {storyName} নিষ্ক্রিয় হওয়ার ৬ মাস পরে স্বয়ংক্রিয়ভাবে হাইয়াটাসে রাখা হয়েছে।',
-  'notification.workshopStoryStatus.toCancelled':
-    'আপনার গল্প {storyName} নিষ্ক্রিয় হওয়ার পর ১ বছর পরে স্বয়ংক্রিয়ভাবে বাতিল করা হয়েছে।',
+  'notification.workshopStoryStatus.toHiatus': 'আপনার গল্প {storyName} নিষ্ক্রিয় হওয়ার ৬ মাস পরে স্বয়ংক্রিয়ভাবে হাইয়াটাসে রাখা হয়েছে।',
+  'notification.workshopStoryStatus.toCancelled': 'আপনার গল্প {storyName} নিষ্ক্রিয় হওয়ার পর ১ বছর পরে স্বয়ংক্রিয়ভাবে বাতিল করা হয়েছে।',
   'notification.followingStoryStatus.toHiatus': '{storyName} হাইয়াটাসের অবস্থায় রয়েছে।',
   'notification.followingStoryStatus.toCancelled': '{storyName} বাতিল হয়েছে।',
   'notification.followingNewChapter.new': '{storyName} গল্পের নতুন অধ্যায় প্রকাশিত হয়েছে!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'আপনাকে একটি বন্ধুর অনুরোধ পাঠানো হয়েছে।',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'গল্পে যেতে যান',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'গল্পে যেতে যান'
+};
