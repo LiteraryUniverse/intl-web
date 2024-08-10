fb94209d80b23e6b7372ef833bf0c161f40a0e02
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:40:27 +0200

    New translations rating.js (Odia)

--- or-IN/rating.js
+++ or-IN/rating.js
@@ -2,18 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'ରେଟିଂ',
-  'rating.explained': 'ବୟସ ରେଟିଂ ଏବଂ ପ୍ରତିବନ୍ଧନ।',
-  'rating.all': 'ସବୁ',
-  'rating.desc.all': 'ସବୁ ବୟସ ଏବଂ ଗୋଷ୍ଠୀର ପାଇଁ ଉପଯୁକ୍ତ।',
-  'rating.teens': 'ଟିନ୍ସ',
-  'rating.desc.teens': '12 ଥିବା 18 ବର୍ଷର ମଧ୍ୟରେ ପିଣ୍ଡିତ ପ୍ରଶାନ୍ତି ପ୍ରାପ୍ତ ହେବ।',
-  'rating.mature': 'ପ୍ରଗଟିଷ୍ଟ',
-  'rating.desc.mature': 'ଅଗ୍ରିମ ବିଷୟରେ, କ୍ରୁଡ୍ ଭାଷାରେ ଏବଂ ଅସ୍ପଷ୍ଟ ସ୍ଥାନମାନଙ୍କରେ ଅଛି।',
-  'rating.explicit': 'ସ୍ପଷ୍ଟ',
-  'rating.desc.explicit': 'ସ୍ପଷ୍ଟ ସ୍ଥାନମାନଙ୍କରେ ଅଛି।',
-  'rating.selection.all': 'ସବୁ - ସବୁ ବୟସ ଏବଂ ଗୋଷ୍ଠୀର ପାଇଁ ଉପଯୁକ୍ତ।',
-  'rating.selection.teens': 'ଟିନ୍ସ (12-18) - 12 ଥିବା 18 ବର୍ଷର ମଧ୍ୟରେ ପିଣ୍ଡିତ ପ୍ରଶାନ୍ତି ପ୍ରାପ୍ତ ହେବ।',
-  'rating.selection.mature': 'ପ୍ରଗଟିଷ୍ଟ (18+) - ଅଗ୍ରିମ ବିଷୟରେ, କ୍ରୁଡ୍ ଭାଷାରେ ଏବଂ ଅସ୍ପଷ୍ଟ ସ୍ଥାନମାନଙ୍କରେ ଅଛି।',
-  'rating.selection.explicit': 'ସ୍ପଷ୍ଟ (21+) - ସ୍ପଷ୍ଟ ସ୍ଥାନମାନଙ୍କରେ ଅଛି।',
-}
+    'rating.name': 'ରେଟିଂ',
+    'rating.explained': 'ବୟସ ରେଟିଂ ଏବଂ ପ୍ରତିବନ୍ଧନ।',
+    'rating.all': 'ସବୁ',
+    'rating.desc.all': 'ସବୁ ବୟସ ଏବଂ ଗୋଷ୍ଠୀର ପାଇଁ ଉପଯୁକ୍ତ।',
+    'rating.teens': 'ଟିନ୍ସ',
+    'rating.desc.teens': '12 ଥିବା 18 ବର୍ଷର ମଧ୍ୟରେ ପିଣ୍ଡିତ ପ୍ରଶାନ୍ତି ପ୍ରାପ୍ତ ହେବ।',
+    'rating.mature': 'ପ୍ରଗଟିଷ୍ଟ',
+    'rating.desc.mature': 'ଅଗ୍ରିମ ବିଷୟରେ, କ୍ରୁଡ୍ ଭାଷାରେ ଏବଂ ଅସ୍ପଷ୍ଟ ସ୍ଥାନମାନଙ୍କରେ ଅଛି।',
+    'rating.explicit': 'ସ୍ପଷ୍ଟ',
+    'rating.desc.explicit': 'ସ୍ପଷ୍ଟ ସ୍ଥାନମାନଙ୍କରେ ଅଛି।',
+    'rating.selection.all': 'ସବୁ - ସବୁ ବୟସ ଏବଂ ଗୋଷ୍ଠୀର ପାଇଁ ଉପଯୁକ୍ତ।',
+    'rating.selection.teens': 'ଟିନ୍ସ (12-18) - 12 ଥିବା 18 ବର୍ଷର ମଧ୍ୟରେ ପିଣ୍ଡିତ ପ୍ରଶାନ୍ତି ପ୍ରାପ୍ତ ହେବ।',
+    'rating.selection.mature': 'ପ୍ରଗଟିଷ୍ଟ (18+) - ଅଗ୍ରିମ ବିଷୟରେ, କ୍ରୁଡ୍ ଭାଷାରେ ଏବଂ ଅସ୍ପଷ୍ଟ ସ୍ଥାନମାନଙ୍କରେ ଅଛି।',
+    'rating.selection.explicit': 'ସ୍ପଷ୍ଟ (21+) - ସ୍ପଷ୍ଟ ସ୍ଥାନମାନଙ୍କରେ ଅଛି।'
+};
