7f9ddf866d6a95facbd254477f7cb1be9d792def
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:36:00 +0200

    New translations rating.js (Esperanto)

--- eo-UY/rating.js
+++ eo-UY/rating.js
@@ -2,20 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Klasifiko',
-  'rating.explained': 'Aĝoklasifiko kaj limigoj.',
-  'rating.all': 'Ĉiuj',
-  'rating.desc.all': 'Alirebla por ĉiuj aĝoj kaj grupoj.',
-  'rating.teens': 'Adoleskantoj',
-  'rating.desc.teens': 'Taŭga por spektantaro inter la aĝoj de 12 kaj 18 jaroj.',
-  'rating.mature': 'Maljunaj',
-  'rating.desc.mature': 'Enhavas profundajn temojn, malĝentilan lingvon kaj subkomprenajn scenojn.',
-  'rating.explicit': 'Eksplika',
-  'rating.desc.explicit': 'Enhavas eksplikajn scenojn.',
-  'rating.selection.all': 'Ĉiuj - Alirebla por ĉiuj aĝoj kaj grupoj.',
-  'rating.selection.teens':
-    'Adoleskantoj (12-18) - Taŭga por spektantaro inter la aĝoj de 12 kaj 18 jaroj.',
-  'rating.selection.mature':
-    'Plenkreska (18+) - Enhavas profundajn temojn, malĝentilan lingvon kaj subkomprenajn scenojn.',
-  'rating.selection.explicit': 'Eksplika (21+) - Enhavas eksplikajn scenojn.',
-}
+    'rating.name': 'Klasifiko',
+    'rating.explained': 'Aĝoklasifiko kaj limigoj.',
+    'rating.all': 'Ĉiuj',
+    'rating.desc.all': 'Alirebla por ĉiuj aĝoj kaj grupoj.',
+    'rating.teens': 'Adoleskantoj',
+    'rating.desc.teens': 'Taŭga por spektantaro inter la aĝoj de 12 kaj 18 jaroj.',
+    'rating.mature': 'Maljunaj',
+    'rating.desc.mature': 'Enhavas profundajn temojn, malĝentilan lingvon kaj subkomprenajn scenojn.',
+    'rating.explicit': 'Eksplika',
+    'rating.desc.explicit': 'Enhavas eksplikajn scenojn.',
+    'rating.selection.all': 'Ĉiuj - Alirebla por ĉiuj aĝoj kaj grupoj.',
+    'rating.selection.teens': 'Adoleskantoj (12-18) - Taŭga por spektantaro inter la aĝoj de 12 kaj 18 jaroj.',
+    'rating.selection.mature': 'Plenkreska (18+) - Enhavas profundajn temojn, malĝentilan lingvon kaj subkomprenajn scenojn.',
+    'rating.selection.explicit': 'Eksplika (21+) - Enhavas eksplikajn scenojn.'
+};
