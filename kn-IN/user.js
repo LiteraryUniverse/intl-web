ad4755968e565ff9e5d4d12c548e6def98f7fbd3
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:38:57 +0200

    New translations user.js (Kannada)

--- kn-IN/user.js
+++ kn-IN/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'ಸ್ನೇಹಿತರನ್ನು ಸೇರಿಸಿ',
-  'user.friend.requests': `ನೀವು {num, number} ಹೊಸ ಸ್ನೇಹಿತ {num, plural,
+    'user.friend.request.add': 'ಸ್ನೇಹಿತರನ್ನು ಸೇರಿಸಿ',
+    'user.friend.requests': `ನೀವು {num, number} ಹೊಸ ಸ್ನೇಹಿತ {num, plural,
     zero {ಕೋರಿಕೆಗಳು}
     one {ಕೋರಿಕೆ}
     many {ಕೋರಿಕೆಗಳು}
     other {ಕೋರಿಕೆಗಳು}
   }`,
-  'user.friend.unfriend': 'ಸ್ನೇಹಿತರನ್ನು ರದ್ದುಮಾಡಿ',
-  'user.block': 'ಮುಚ್ಚಿಡಿ',
-  'user.unblock': 'ಮುಚ್ಚಿಗೆ ತೆರವು ಮಾಡಿ',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'ಸ್ನೇಹಿತರನ್ನು ರದ್ದುಮಾಡಿ',
+    'user.block': 'ಮುಚ್ಚಿಡಿ',
+    'user.unblock': 'ಮುಚ್ಚಿಗೆ ತೆರವು ಮಾಡಿ',
+    'user.joined': `{gender, select,
     male {ಸೇರಿದ್ದೀರಿ}
     female {ಸೇರಿದ್ದೀರಿ}
     other {ಸೇರಿದ್ದೀರಿ}} ನಲ್ಲಿ {date, date, long}`,
-  'user.friend.request.accept': 'ಸ್ನೇಹಿತರ ಕೋರಿಕೆಯನ್ನು ಸ್ವೀಕರಿಸಿ',
-  'user.friend.request.cancel': 'ಸ್ನೇಹಿತರ ಕೋರಿಕೆಯನ್ನು ರದ್ದುಮಾಡಿ',
-  'user.friend.request.deny': 'ಸ್ನೇಹಿತರಾಗಿ ನಿರಾಕರಿಸಿ',
-  'user.listing': 'ಬಳಕೆದಾರರ ಪಟ್ಟಿ',
-  'user.profile.visit': '{user}ರ ಪ್ರೊಫೈಲ್ ಗೆ ಭೇಟಿ ನೀಡಿ.',
-  'user.profile.avatar': '{username}ರ ಅವತಾರ',
-  'user.friend.list.requests': 'ಸ್ನೇಹಿತರ ಕೋರಿಕೆಗಳು',
-  'user.friend.norequests': 'ನೀವು ಯಾವುದೇ ಸ್ನೇಹಿತರ ಕೋರಿಕೆಗಳನ್ನು ಹೊಂದಿಲ್ಲ.',
-  'user.dashboardWelcome': `Good {dayPart, select,
+    'user.friend.request.accept': 'ಸ್ನೇಹಿತರ ಕೋರಿಕೆಯನ್ನು ಸ್ವೀಕರಿಸಿ',
+    'user.friend.request.cancel': 'ಸ್ನೇಹಿತರ ಕೋರಿಕೆಯನ್ನು ರದ್ದುಮಾಡಿ',
+    'user.friend.request.deny': 'ಸ್ನೇಹಿತರಾಗಿ ನಿರಾಕರಿಸಿ',
+    'user.listing': 'ಬಳಕೆದಾರರ ಪಟ್ಟಿ',
+    'user.profile.visit': '{user}ರ ಪ್ರೊಫೈಲ್ ಗೆ ಭೇಟಿ ನೀಡಿ.',
+    'user.profile.avatar': '{username}ರ ಅವತಾರ',
+    'user.friend.list.requests': 'ಸ್ನೇಹಿತರ ಕೋರಿಕೆಗಳು',
+    'user.friend.norequests': 'ನೀವು ಯಾವುದೇ ಸ್ನೇಹಿತರ ಕೋರಿಕೆಗಳನ್ನು ಹೊಂದಿಲ್ಲ.',
+    'user.dashboardWelcome': `Good {dayPart, select,
     morning {morning}
     afternoon {afternoon}
     evening {evening}
     night {night}
     other {day}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'ಈ ಲೇಖಕ ಸಾಮಾನ್ಯ ವಿಧಾನದಲ್ಲಿ ಪ್ರಕಟಿಸಲಾಗಿದೆ',
-  'user.verification.luAuthor': 'ಸಾಹಿತ್ಯ ವಿಶ್ವದ ಆಧಾರದ ಲೇಖಕ',
-  'user.verification.luEmployee': 'ಸಾಹಿತ್ಯ ವಿಶ್ವ ನೌಕರ',
-  'user.verification.publicFigure': 'ಪರಿಶೀಲಿಸಲಾಗಿದ್ದ ಸಾರ್ವಜನಿಕ ವ್ಯಕ್ತಿ',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'ಈ ಲೇಖಕ ಸಾಮಾನ್ಯ ವಿಧಾನದಲ್ಲಿ ಪ್ರಕಟಿಸಲಾಗಿದೆ',
+    'user.verification.luAuthor': 'ಸಾಹಿತ್ಯ ವಿಶ್ವದ ಆಧಾರದ ಲೇಖಕ',
+    'user.verification.luEmployee': 'ಸಾಹಿತ್ಯ ವಿಶ್ವ ನೌಕರ',
+    'user.verification.publicFigure': 'ಪರಿಶೀಲಿಸಲಾಗಿದ್ದ ಸಾರ್ವಜನಿಕ ವ್ಯಕ್ತಿ',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {ಕಥೆ}
     other {ಕಥೆಗಳು}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {ಬ್ರಹ್ಮಾಂಡ}
     other {ಬ್ರಹ್ಮಾಂಡಗಳು}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {ಫ್ಯಾನ್ ಫಿಕ್ಷನ್}
     other {ಫ್ಯಾನ್ ಫಿಕ್ಷನ್ಗಳು} 
   }`,
-  'profile.works': 'ಕೃತಿಗಳು',
-  // As in creative/literary works
-  'profile.wishlist': 'ಇಚ್ಛಾಪಟುಗಳು',
-  'profile.storiesBy': '{username}ದ್ವಾರಾ ರಚಿತವಾದ ಕಥೆಗಳು',
-  'profile.universesBy': '{username}ದ್ವಾರಾ ರಚಿತವಾದ ಬ್ರಹ್ಮಾಂಡಗಳು',
-  'profile.fanFictionBy': '{username}ದ್ವಾರಾ ರಚಿತವಾದ ಫ್ಯಾನ್ ಫಿಕ್ಷನ್',
-}
+    'profile.works': 'ಕೃತಿಗಳು',
+    // As in creative/literary works
+    'profile.wishlist': 'ಇಚ್ಛಾಪಟುಗಳು',
+    'profile.storiesBy': '{username}ದ್ವಾರಾ ರಚಿತವಾದ ಕಥೆಗಳು',
+    'profile.universesBy': '{username}ದ್ವಾರಾ ರಚಿತವಾದ ಬ್ರಹ್ಮಾಂಡಗಳು',
+    'profile.fanFictionBy': '{username}ದ್ವಾರಾ ರಚಿತವಾದ ಫ್ಯಾನ್ ಫಿಕ್ಷನ್'
+};
