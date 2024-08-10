dc51f3e71fb2f57ba62b9d0a869d811df0c99a05
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:36:57 +0200

    New translations feed.js (Lojban)

--- jbo-EN/feed.js
+++ jbo-EN/feed.js
@@ -1,9 +1,9 @@
 export default {
-  'feed.name': 'Pagbu',
-  'feed.post.new': 'Selskapi be lo se jorne',
-  'feed.post.like': 'Rojyjorne lo se jorne',
-  'feed.post.unlike': 'Nalrojyjorne lo se jorne',
-  'feed.post.send': 'stuzi',
-  'feed.empty': 'Naljorne be lo se jorne',
-  'feed.placeholder': 'ma cmene do?',
-}
+    'feed.name': 'Pagbu',
+    'feed.post.new': 'Selskapi be lo se jorne',
+    'feed.post.like': 'Rojyjorne lo se jorne',
+    'feed.post.unlike': 'Nalrojyjorne lo se jorne',
+    'feed.post.send': 'stuzi',
+    'feed.empty': 'Naljorne be lo se jorne',
+    'feed.placeholder': 'ma cmene do?'
+};
