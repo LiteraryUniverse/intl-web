cb6fcff611b3fb15720f71473cde941f9a187768
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:35:05 +0200

    New translations notifications.js (Malay)

--- ms-MY/notifications.js
+++ ms-MY/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'Mula paparkan mesej ini pada',
   'flashnews.endsAt': 'Hentikan paparan mesej ini pada',
   'flashnews.newLanguage': 'Pilih bahasa baru untuk ditambah',
-  'flashnews.onlyDisplayOn':
-    'Berita akan dipaparkan hanya dalam bahasa yang dipilih di bawah (jika ada yang dipilih), tidak akan ada berita yang ditunjukkan, bahkan dalam bahasa lalai, dalam bahasa lain.',
+  'flashnews.onlyDisplayOn': 'Berita akan dipaparkan hanya dalam bahasa yang dipilih di bawah (jika ada yang dipilih), tidak akan ada berita yang ditunjukkan, bahkan dalam bahasa lalai, dalam bahasa lain.',
   'notifications.menu.all': 'Semua pemberitahuan',
   'notifications.menu.following': 'Mengikuti',
   'notifications.menu.workshop': 'Bengkel',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'Tandai sebagai sudah dibaca',
   'notifications.menu.delete': 'Padam pemberitahuan',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'Kisah anda {storyName} telah ditetapkan kepada hiatus selepas 6 bulan tidak aktif.',
-  'notification.workshopStoryStatus.toCancelled':
-    'Kisah anda {storyName} telah ditetapkan kepada terbatal selepas satu tahun tidak aktif.',
+  'notification.workshopStoryStatus.toHiatus': 'Kisah anda {storyName} telah ditetapkan kepada hiatus selepas 6 bulan tidak aktif.',
+  'notification.workshopStoryStatus.toCancelled': 'Kisah anda {storyName} telah ditetapkan kepada terbatal selepas satu tahun tidak aktif.',
   'notification.followingStoryStatus.toHiatus': '{storyName} sedang dalam hiatus.',
   'notification.followingStoryStatus.toCancelled': '{storyName} telah dibatalkan.',
   'notification.followingNewChapter.new': 'Bab baharu dalam {storyName} telah diterbitkan!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'menghantar permintaan pertemanan kepada anda.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Pergi ke cerita',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'Pergi ke cerita'
+};
