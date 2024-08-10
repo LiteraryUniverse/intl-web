3a5a39d6e4ebe331c611bdc7f66f800de83fbbfe
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:26:40 +0200

    New translations notifications.js (Slovak)

--- sk-SK/notifications.js
+++ sk-SK/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'Začnite zobrazovať túto správu od',
   'flashnews.endsAt': 'Prestaňte zobrazovať túto správu od',
   'flashnews.newLanguage': 'Vyberte nový jazyk, ktorý chcete pridať',
-  'flashnews.onlyDisplayOn':
-    'Správy sa budú zobrazovať len v jazykoch vybraných nižšie (ak sú vybrané), v iných jazykoch sa nebudú zobrazovať žiadne správy, dokonca ani v predvolenom jazyku.',
+  'flashnews.onlyDisplayOn': 'Správy sa budú zobrazovať len v jazykoch vybraných nižšie (ak sú vybrané), v iných jazykoch sa nebudú zobrazovať žiadne správy, dokonca ani v predvolenom jazyku.',
   'notifications.menu.all': 'Všetky upozornenia',
   'notifications.menu.following': 'Nasledovanie',
   'notifications.menu.workshop': 'Dielňa',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'Označiť ako prečítané',
   'notifications.menu.delete': 'Zmazať notifikáciu',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'Vaša poviedka {storyName} bola automaticky nastavená na hiatus po 6 mesiacoch nečinnosti.',
-  'notification.workshopStoryStatus.toCancelled':
-    'Vaša príbeh {storyName} bol automaticky zrušený po jednom roku nečinnosti.',
+  'notification.workshopStoryStatus.toHiatus': 'Vaša poviedka {storyName} bola automaticky nastavená na hiatus po 6 mesiacoch nečinnosti.',
+  'notification.workshopStoryStatus.toCancelled': 'Vaša príbeh {storyName} bol automaticky zrušený po jednom roku nečinnosti.',
   'notification.followingStoryStatus.toHiatus': '{storyName} je na hiatusu.',
   'notification.followingStoryStatus.toCancelled': '{storyName} bol zrušen.',
   'notification.followingNewChapter.new': 'Bol zverejnený nový kapitola v {storyName}!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'Vám poslal(a) žiadosť o priateľstvo.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Prejsť na príbeh',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'Prejsť na príbeh'
+};
