0766ab7fa6fc11b398fa61870e203d2414b1ac1d
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:20:14 +0200

    New translations licenses.js (Italian)

--- it-IT/licenses.js
+++ it-IT/licenses.js
@@ -3,31 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'Informazioni sulle licenze',
-  'licenses.more': 'Ulteriori informazioni',
-  'licenses.legal': 'Documenti legali',
-  'licenses.name.cc-by': 'Attribuzione Creative Commons 4.0',
-  'licenses.cc-by':
-    'Questa licenza permette agli altri di distribuire, remixare, modificare leggermente e sviluppare ulteriormente il tuo lavoro, anche a scopo commerciale, purché essi ti attribuiscano il merito della creazione originale. Questa licenza è la più accomodante. Raccomandata per una massima diffusione e uso di materiali concessi in licenza.',
-  'licenses.name.cc-by-sa': 'Creative Commons Attribuzione-ShareAlike 4.0',
-  'licenses.cc-by-sa':
-    "Questa licenza consente ad altri di remixare, modificare e sviluppare il tuo lavoro anche per scopi commerciali, fintanto che ti accreditano e concedono la licenza alle loro nuove creazioni secondo i medesimi termini. Questa licenza viene spesso confrontata con le licenze software free e open source “copyleft”. Tutti i nuovi lavori basati sul tuo avranno la stessa licenza, quindi anche qualsiasi derivato permetterà l'uso commerciale. Questa è la licenza utilizzata da Wikipedia, ed è consigliata per i materiali che trarrebbero vantaggio dall'incorporazione di contenuti da Wikipedia e progetti con licenza simile.",
-  'licenses.name.cc-by-nd': 'Attribuzione Creative Commons-NoDerivs 4.0',
-  'licenses.cc-by-nd':
-    'Questa licenza consente la ridistribuzione, commerciale e non commerciale, del tuo lavoro, purché rimanga invariato e completo, e tu sia accreditato.',
-  'licenses.name.cc-by-nc': 'Attribuzione Creative Commons-NonCommercial 4.0',
-  'licenses.cc-by-nc':
-    "Questa licenza permette ad altri di remixare, modificare leggermente e sviluppare ulteriormente il tuo lavoro in modo non commerciale, e, sebbene nei nuovi lavori devi essere menzionato ed essi non possono essere commerciali, non c'è l'obbligo di usare la stessa licenza per i lavori derivati.",
-  'licenses.name.cc-by-nc-sa': 'Attribuzione Creative Commons-NonCommercial-ShareAlike 4.0',
-  'licenses.cc-by-nc-sa':
-    'Questa licenza permette gli altri di modificare ed integrare il tuo lavoro, senza sfruttarlo economicamente, purché tu sia accreditato e le nuove creazioni abbiano gli stessi termini di licenza.',
-  'licenses.name.cc-by-nc-nd': 'Attribuzione Creative Commons-NonCommercial-NoDerivs 4.0',
-  'licenses.cc-by-nc-nd':
-    'Questa licenza è la più restrittiva delle nostre sei principali licenze: è consentito esclusivamente scaricare i tuoi lavori e condividerli con altri utenti purché essi te ne riconoscano il merito, ma non possono modificarli in alcun modo o utilizzarli a livello commerciale.',
-  'licenses.name.lu-c': 'Literary Universe Copyright',
-  'licenses.lu-c':
-    'Licenza standard completa di copyright che consente di vendere il tuo lavoro come preferisci e di proteggere i diritti su di esso. Questa licenza ti dà anche la possibilità di vendere le tue storie su Literary Universe.',
-  'licenses.name.cc0': "Nessun Diritto d'autore - CC0 1.0",
-  'licenses.cc0':
-    "Metti il tuo lavoro in pubblico dominio. Contrariamente alle licenze di CC che consentono ai titolari dei diritti d’autore di scegliere tra una serie di autorizzazioni pur mantenendo il loro diritto d’autore, CC0 consente un'ulteriore scelta: non applicare il diritto d'autore e la protezione del database, e i diritti esclusivi concessi automaticamente ai creatori – i “diritti non riservati” alternativi alle nostre licenze.",
-}
+    'licenses.about': 'Informazioni sulle licenze',
+    'licenses.more': 'Ulteriori informazioni',
+    'licenses.legal': 'Documenti legali',
+    'licenses.name.cc-by': 'Attribuzione Creative Commons 4.0',
+    'licenses.cc-by': 'Questa licenza permette agli altri di distribuire, remixare, modificare leggermente e sviluppare ulteriormente il tuo lavoro, anche a scopo commerciale, purché essi ti attribuiscano il merito della creazione originale. Questa licenza è la più accomodante. Raccomandata per una massima diffusione e uso di materiali concessi in licenza.',
+    'licenses.name.cc-by-sa': 'Creative Commons Attribuzione-ShareAlike 4.0',
+    'licenses.cc-by-sa': 'Questa licenza consente ad altri di remixare, modificare e sviluppare il tuo lavoro anche per scopi commerciali, fintanto che ti accreditano e concedono la licenza alle loro nuove creazioni secondo i medesimi termini. Questa licenza viene spesso confrontata con le licenze software free e open source “copyleft”. Tutti i nuovi lavori basati sul tuo avranno la stessa licenza, quindi anche qualsiasi derivato permetterà l\'uso commerciale. Questa è la licenza utilizzata da Wikipedia, ed è consigliata per i materiali che trarrebbero vantaggio dall\'incorporazione di contenuti da Wikipedia e progetti con licenza simile.',
+    'licenses.name.cc-by-nd': 'Attribuzione Creative Commons-NoDerivs 4.0',
+    'licenses.cc-by-nd': 'Questa licenza consente la ridistribuzione, commerciale e non commerciale, del tuo lavoro, purché rimanga invariato e completo, e tu sia accreditato.',
+    'licenses.name.cc-by-nc': 'Attribuzione Creative Commons-NonCommercial 4.0',
+    'licenses.cc-by-nc': 'Questa licenza permette ad altri di remixare, modificare leggermente e sviluppare ulteriormente il tuo lavoro in modo non commerciale, e, sebbene nei nuovi lavori devi essere menzionato ed essi non possono essere commerciali, non c\'è l\'obbligo di usare la stessa licenza per i lavori derivati.',
+    'licenses.name.cc-by-nc-sa': 'Attribuzione Creative Commons-NonCommercial-ShareAlike 4.0',
+    'licenses.cc-by-nc-sa': 'Questa licenza permette gli altri di modificare ed integrare il tuo lavoro, senza sfruttarlo economicamente, purché tu sia accreditato e le nuove creazioni abbiano gli stessi termini di licenza.',
+    'licenses.name.cc-by-nc-nd': 'Attribuzione Creative Commons-NonCommercial-NoDerivs 4.0',
+    'licenses.cc-by-nc-nd': 'Questa licenza è la più restrittiva delle nostre sei principali licenze: è consentito esclusivamente scaricare i tuoi lavori e condividerli con altri utenti purché essi te ne riconoscano il merito, ma non possono modificarli in alcun modo o utilizzarli a livello commerciale.',
+    'licenses.name.lu-c': 'Literary Universe Copyright',
+    'licenses.lu-c': 'Licenza standard completa di copyright che consente di vendere il tuo lavoro come preferisci e di proteggere i diritti su di esso. Questa licenza ti dà anche la possibilità di vendere le tue storie su Literary Universe.',
+    'licenses.name.cc0': 'Nessun Diritto d\'autore - CC0 1.0',
+    'licenses.cc0': 'Metti il tuo lavoro in pubblico dominio. Contrariamente alle licenze di CC che consentono ai titolari dei diritti d’autore di scegliere tra una serie di autorizzazioni pur mantenendo il loro diritto d’autore, CC0 consente un\'ulteriore scelta: non applicare il diritto d\'autore e la protezione del database, e i diritti esclusivi concessi automaticamente ai creatori – i “diritti non riservati” alternativi alle nostre licenze.'
+};
