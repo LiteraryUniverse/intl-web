8ffc5c4300f8894e85d4c4e430ee6d819d4db1c1
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:25:09 +0200

    New translations enrollment.js (Portuguese)

--- pt-PT/enrollment.js
+++ pt-PT/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'Reivindique a sua conta!',
-  'enrollment.description':
-    'Bem-vindo {username}! Para ter acesso à sua conta especial só precisa de criar uma palavra passe.',
-  'enrollment.submitting': 'Reivindicando a sua conta...',
-  'enrollment.submit': 'Reivindique a sua conta!',
-}
+    'enrollment.title': 'Reivindique a sua conta!',
+    'enrollment.description': 'Bem-vindo {username}! Para ter acesso à sua conta especial só precisa de criar uma palavra passe.',
+    'enrollment.submitting': 'Reivindicando a sua conta...',
+    'enrollment.submit': 'Reivindique a sua conta!'
+};
