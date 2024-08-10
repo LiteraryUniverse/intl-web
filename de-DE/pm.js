b56ef46a2cca9a057ff0e3100a9775bb8bd0cfdc
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:16:21 +0200

    New translations pm.js (German)

--- de-DE/pm.js
+++ de-DE/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'Konversation',
-  'pm.showolder': 'Ältere Meldungen anzeigen',
-  'pm.errors.saysomething': 'Sie sollten wirklich etwas sagen...',
-  'pm.errors.addrecipient': 'Sie müssen mindestens einen weiteren Benutzer hinzufügen.',
-  'pm.message': 'Nachricht',
-  'pm.send.message': 'Nachricht senden',
-  'pm.messages.own': 'Ihre Nachrichten',
-  'pm.conversations.none': 'Sie führen derzeit kein Gespräch mit jemandem.',
-  'pm.to': 'Zu:',
-  'pm.with': 'Konversation mit:',
-  'pm.send': 'Senden',
-  'pm.conversations.unreadCount': `Sie haben{count, plural,
+    'pm.conversation': 'Konversation',
+    'pm.showolder': 'Ältere Meldungen anzeigen',
+    'pm.errors.saysomething': 'Sie sollten wirklich etwas sagen...',
+    'pm.errors.addrecipient': 'Sie müssen mindestens einen weiteren Benutzer hinzufügen.',
+    'pm.message': 'Nachricht',
+    'pm.send.message': 'Nachricht senden',
+    'pm.messages.own': 'Ihre Nachrichten',
+    'pm.conversations.none': 'Sie führen derzeit kein Gespräch mit jemandem.',
+    'pm.to': 'Zu:',
+    'pm.with': 'Konversation mit:',
+    'pm.send': 'Senden',
+    'pm.conversations.unreadCount': `Sie haben{count, plural,
     zero {keine ungelesenen Konversationen}
     one {eine ungelesene Unterhaltung}
     other {# ungelesene Unterhaltungen}
   }.`,
-  'pm.form.minimumParticipants': 'Sie müssen einen Empfänger auswählen.',
-  'pm.form.maxParticipants': `Sie können maximal {maxPeople, plural,
+    'pm.form.minimumParticipants': 'Sie müssen einen Empfänger auswählen.',
+    'pm.form.maxParticipants': `Sie können maximal {maxPeople, plural,
     one {eine Person}
     other {# Personen}
   } an einer Unterhaltung teilnehmen.`,
-  'pm.new.aria': 'Neues Gespräch',
-  'pm.new.buttonTitle': 'Neues Gespräch beginnen',
-}
+    'pm.new.aria': 'Neues Gespräch',
+    'pm.new.buttonTitle': 'Neues Gespräch beginnen'
+};
