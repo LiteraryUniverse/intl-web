105bb6e1b6d65afefac5c03cf439da86a07f3d1b
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:41:30 +0200

    New translations rating.js (Romanian)

--- ro-RO/rating.js
+++ ro-RO/rating.js
@@ -2,19 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Clasificare',
-  'rating.explained': 'Clasificare și restricții de vârstă.',
-  'rating.all': 'Toate',
-  'rating.desc.all': 'Accesibil tuturor vârstelor și grupurilor.',
-  'rating.teens': 'Adolescenți',
-  'rating.desc.teens': 'Potrivit pentru un public între 12 și 18 ani.',
-  'rating.mature': 'Matur',
-  'rating.desc.mature': 'Conține teme avansate, limbaj licențios și scene implicite.',
-  'rating.explicit': 'Explicit',
-  'rating.desc.explicit': 'Conține scene explicite.',
-  'rating.selection.all': 'Toate - Accesibil tuturor vârstelor și grupurilor.',
-  'rating.selection.teens': 'Adolescenți (12-18) - Potrivit pentru publicul între 12 și 18 ani.',
-  'rating.selection.mature':
-    'Matur (18+) - Conține teme avansate, limbaj licențios și scene implicite.',
-  'rating.selection.explicit': 'Explicit (21+) - Conține scene explicite.',
-}
+    'rating.name': 'Clasificare',
+    'rating.explained': 'Clasificare și restricții de vârstă.',
+    'rating.all': 'Toate',
+    'rating.desc.all': 'Accesibil tuturor vârstelor și grupurilor.',
+    'rating.teens': 'Adolescenți',
+    'rating.desc.teens': 'Potrivit pentru un public între 12 și 18 ani.',
+    'rating.mature': 'Matur',
+    'rating.desc.mature': 'Conține teme avansate, limbaj licențios și scene implicite.',
+    'rating.explicit': 'Explicit',
+    'rating.desc.explicit': 'Conține scene explicite.',
+    'rating.selection.all': 'Toate - Accesibil tuturor vârstelor și grupurilor.',
+    'rating.selection.teens': 'Adolescenți (12-18) - Potrivit pentru publicul între 12 și 18 ani.',
+    'rating.selection.mature': 'Matur (18+) - Conține teme avansate, limbaj licențios și scene implicite.',
+    'rating.selection.explicit': 'Explicit (21+) - Conține scene explicite.'
+};
