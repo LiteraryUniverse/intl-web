15fbf8df0fe129ce7597ee7012cc21b9709c2a67
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:37:17 +0200

    New translations user.js (Lojban)

--- jbo-EN/user.js
+++ jbo-EN/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': "Citka la'o zoi.friends.zoi",
-  'user.friend.requests': `You have {num, number} new friend {num, plural,
+    'user.friend.request.add': 'Citka la\'o zoi.friends.zoi',
+    'user.friend.requests': `You have {num, number} new friend {num, plural,
     zero {requests}
     one {request}
     many {requests}
     other {requests}
   }`,
-  'user.friend.unfriend': 'Klaku',
-  'user.block': 'Blokadu',
-  'user.unblock': 'Deblokadu',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'Klaku',
+    'user.block': 'Blokadu',
+    'user.unblock': 'Deblokadu',
+    'user.joined': `{gender, select,
     male {Joined}
     female {Joined}
     other {Joined}} on {date, date, long}`,
-  'user.friend.request.accept': "Jinvi la'o zoi.friends.zoi",
-  'user.friend.request.cancel': "Bakfu la'o zoi.friends.zoi",
-  'user.friend.request.deny': "Nalvai la'o zoi.friends.zoi",
-  'user.listing': "Joi la'o zoi.users.zoi",
-  'user.profile.visit': "Visit {user}'s profile.",
-  'user.profile.avatar': "{username}'s avatar",
-  'user.friend.list.requests': "Citka la'o zoi.friends.zoi",
-  'user.friend.norequests': "do na'e cilre la'o zoi.friends.zoi",
-  'user.dashboardWelcome': `coi {dayPart, select,
+    'user.friend.request.accept': 'Jinvi la\'o zoi.friends.zoi',
+    'user.friend.request.cancel': 'Bakfu la\'o zoi.friends.zoi',
+    'user.friend.request.deny': 'Nalvai la\'o zoi.friends.zoi',
+    'user.listing': 'Joi la\'o zoi.users.zoi',
+    'user.profile.visit': 'Visit {user}\'s profile.',
+    'user.profile.avatar': '{username}\'s avatar',
+    'user.friend.list.requests': 'Citka la\'o zoi.friends.zoi',
+    'user.friend.norequests': 'do na\'e cilre la\'o zoi.friends.zoi',
+    'user.dashboardWelcome': `coi {dayPart, select,
     morning {nunti}
     afternoon {nicte}
     evening {djedi}
     night {vanci}
     other {djedi}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'ti penmi cu se jibri lo se traji',
-  'user.verification.luAuthor': 'ti penmi cu se jibri lo se traji',
-  'user.verification.luEmployee': 'ti penmi cu se jibri lo se traji',
-  'user.verification.publicFigure': 'ti penmi cu se jibri lo se traji',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'ti penmi cu se jibri lo se traji',
+    'user.verification.luAuthor': 'ti penmi cu se jibri lo se traji',
+    'user.verification.luEmployee': 'ti penmi cu se jibri lo se traji',
+    'user.verification.publicFigure': 'ti penmi cu se jibri lo se traji',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {story}
     other {stories}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {universe}
     other {universes}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {fan fiction}
     other {fan fictions} 
   }`,
-  'profile.works': 'Works',
-  // As in creative/literary works
-  'profile.wishlist': 'Wishlist',
-  'profile.storiesBy': 'Stories created by {username}',
-  'profile.universesBy': 'Universes created by {username}',
-  'profile.fanFictionBy': 'Fan Fiction created by {username}',
-}
+    'profile.works': 'Works',
+    // As in creative/literary works
+    'profile.wishlist': 'Wishlist',
+    'profile.storiesBy': 'Stories created by {username}',
+    'profile.universesBy': 'Universes created by {username}',
+    'profile.fanFictionBy': 'Fan Fiction created by {username}'
+};
