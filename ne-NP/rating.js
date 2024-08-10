463e7a8c7b2763756e827dbd71463be784a8046c
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:41:00 +0200

    New translations rating.js (Nepali)

--- ne-NP/rating.js
+++ ne-NP/rating.js
@@ -2,18 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'रेटिङ्ग',
-  'rating.explained': 'उमेर रेटिङ्ग र प्रतिबन्ध।',
-  'rating.all': 'सबै',
-  'rating.desc.all': 'सबै उमेर र समूहहरूको लागि पहुँचन सकिने।',
-  'rating.teens': 'किशोरहरू',
-  'rating.desc.teens': '१२ देखि १८ वर्ष बीचका दर्शकहरूको लागि उपयुक्त।',
-  'rating.mature': 'प्रौढ',
-  'rating.desc.mature': 'अग्रिम विषय, कच्चा भाषा र निहित दृश्यहरू समावेश गर्दछ।',
-  'rating.explicit': 'स्पष्ट',
-  'rating.desc.explicit': 'स्पष्ट दृश्यहरू समावेश गर्दछ।',
-  'rating.selection.all': 'सबै - सबै उमेर र समूहहरूको लागि पहुँचन सकिने।',
-  'rating.selection.teens': 'किशोरहरू (१२-१८) - १२ देखि १८ वर्ष बीचका दर्शकहरूको लागि उपयुक्त।',
-  'rating.selection.mature': 'प्रौढ (१८+) - अग्रिम विषय, कच्चा भाषा र निहित दृश्यहरू समावेश गर्दछ।',
-  'rating.selection.explicit': 'स्पष्ट (२१+) - स्पष्ट दृश्यहरू समावेश गर्दछ।',
-}
+    'rating.name': 'रेटिङ्ग',
+    'rating.explained': 'उमेर रेटिङ्ग र प्रतिबन्ध।',
+    'rating.all': 'सबै',
+    'rating.desc.all': 'सबै उमेर र समूहहरूको लागि पहुँचन सकिने।',
+    'rating.teens': 'किशोरहरू',
+    'rating.desc.teens': '१२ देखि १८ वर्ष बीचका दर्शकहरूको लागि उपयुक्त।',
+    'rating.mature': 'प्रौढ',
+    'rating.desc.mature': 'अग्रिम विषय, कच्चा भाषा र निहित दृश्यहरू समावेश गर्दछ।',
+    'rating.explicit': 'स्पष्ट',
+    'rating.desc.explicit': 'स्पष्ट दृश्यहरू समावेश गर्दछ।',
+    'rating.selection.all': 'सबै - सबै उमेर र समूहहरूको लागि पहुँचन सकिने।',
+    'rating.selection.teens': 'किशोरहरू (१२-१८) - १२ देखि १८ वर्ष बीचका दर्शकहरूको लागि उपयुक्त।',
+    'rating.selection.mature': 'प्रौढ (१८+) - अग्रिम विषय, कच्चा भाषा र निहित दृश्यहरू समावेश गर्दछ।',
+    'rating.selection.explicit': 'स्पष्ट (२१+) - स्पष्ट दृश्यहरू समावेश गर्दछ।'
+};
