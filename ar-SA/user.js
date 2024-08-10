e4e4950b77d95c6626af342ddcae82707624728a
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:14:14 +0200

    New translations user.js (Arabic)

--- ar-SA/user.js
+++ ar-SA/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'إضافة إلى الأصدقاء',
-  'user.friend.requests': `You have {num, number} new friend {num, plural,
+    'user.friend.request.add': 'إضافة إلى الأصدقاء',
+    'user.friend.requests': `You have {num, number} new friend {num, plural,
     zero {requests}
     one {request}
     many {requests}
     other {requests}
   }`,
-  'user.friend.unfriend': 'إلغاء الصداقة',
-  'user.block': 'حظر',
-  'user.unblock': 'إلغاء الحظر',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'إلغاء الصداقة',
+    'user.block': 'حظر',
+    'user.unblock': 'إلغاء الحظر',
+    'user.joined': `{gender, select,
     male {انضم إلى}
     female {انضم إلى}
     other {انضم إلى}} في {date, date, long}`,
-  'user.friend.request.accept': 'قبول طلب الصداقة',
-  'user.friend.request.cancel': 'إلغاء طلب الصداقة',
-  'user.friend.request.deny': 'رفض الصداقة',
-  'user.listing': 'قائمة المستخدمين',
-  'user.profile.visit': 'زيارة ملف {user} الشخصي.',
-  'user.profile.avatar': 'صورة الملف الشخصي لـ {username}',
-  'user.friend.list.requests': 'طلبات الصداقة',
-  'user.friend.norequests': 'ليس لديك أي طلبات للصداقة.',
-  'user.dashboardWelcome': `صباح الخير {dayPart, select,
+    'user.friend.request.accept': 'قبول طلب الصداقة',
+    'user.friend.request.cancel': 'إلغاء طلب الصداقة',
+    'user.friend.request.deny': 'رفض الصداقة',
+    'user.listing': 'قائمة المستخدمين',
+    'user.profile.visit': 'زيارة ملف {user} الشخصي.',
+    'user.profile.avatar': 'صورة الملف الشخصي لـ {username}',
+    'user.friend.list.requests': 'طلبات الصداقة',
+    'user.friend.norequests': 'ليس لديك أي طلبات للصداقة.',
+    'user.dashboardWelcome': `صباح الخير {dayPart, select,
     morning {مساء الخير}
     afternoon {صباح الخير}
     evening {مساء الخير}
     night {ليلة سعيدة}
     other {يوم جيد}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'لقد تم نشر هذا الكاتب بالطريقة التقليدية',
-  'user.verification.luAuthor': 'كاتب معتمد في عالم الأدب',
-  'user.verification.luEmployee': 'موظف في عالم الأدب',
-  'user.verification.publicFigure': 'شخصية عامة موثوق بها',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'لقد تم نشر هذا الكاتب بالطريقة التقليدية',
+    'user.verification.luAuthor': 'كاتب معتمد في عالم الأدب',
+    'user.verification.luEmployee': 'موظف في عالم الأدب',
+    'user.verification.publicFigure': 'شخصية عامة موثوق بها',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {story}
     other {stories}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {universe}
     other {universes}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {fan fiction}
     other {fan fictions} 
   }`,
-  'profile.works': 'الأعمال',
-  // As in creative/literary works
-  'profile.wishlist': 'قائمة الرغبات',
-  'profile.storiesBy': 'القصص التي تم إنشاؤها بواسطة {username}',
-  'profile.universesBy': 'الكونات التي تم إنشاؤها بواسطة {username}',
-  'profile.fanFictionBy': 'قصص المشجعين التي تم إنشاؤها بواسطة {username}',
-}
+    'profile.works': 'الأعمال',
+    // As in creative/literary works
+    'profile.wishlist': 'قائمة الرغبات',
+    'profile.storiesBy': 'القصص التي تم إنشاؤها بواسطة {username}',
+    'profile.universesBy': 'الكونات التي تم إنشاؤها بواسطة {username}',
+    'profile.fanFictionBy': 'قصص المشجعين التي تم إنشاؤها بواسطة {username}'
+};
