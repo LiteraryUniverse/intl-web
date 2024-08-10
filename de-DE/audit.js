19bef267c48fcad5466e631ef705fb08278c0901
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:16:27 +0200

    New translations audit.js (German)

--- de-DE/audit.js
+++ de-DE/audit.js
@@ -1,6 +1,6 @@
 export default {
-  'auditLog.header.date': 'Datum',
-  'auditLog.header.action': 'Was',
-  'auditLog.header.group': 'Gruppe',
-  'auditLog.header.user': 'Wer',
-}
+    'auditLog.header.date': 'Datum',
+    'auditLog.header.action': 'Was',
+    'auditLog.header.group': 'Gruppe',
+    'auditLog.header.user': 'Wer'
+};
