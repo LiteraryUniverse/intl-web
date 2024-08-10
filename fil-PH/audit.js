139d1f709dea896447d3ce5475b84849d3812845
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:36:38 +0200

    New translations audit.js (Filipino)

--- fil-PH/audit.js
+++ fil-PH/audit.js
@@ -1,6 +1,6 @@
 export default {
-  'auditLog.header.date': 'Petsa',
-  'auditLog.header.action': 'Ano',
-  'auditLog.header.group': 'Grupo',
-  'auditLog.header.user': 'Sino',
-}
+    'auditLog.header.date': 'Petsa',
+    'auditLog.header.action': 'Ano',
+    'auditLog.header.group': 'Grupo',
+    'auditLog.header.user': 'Sino'
+};
