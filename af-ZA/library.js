439347ac6ad28b45fe7d6f9a4a4a17a24d36f5d1
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:13:37 +0200

    New translations library.js (Afrikaans)

--- af-ZA/library.js
+++ af-ZA/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'Voeg by biblioteek',
-  'library.remove': 'Verwyder uit biblioteek',
-  'library.bought': 'Gekoopte stories',
-  'library.stories': 'Gevolgde stories',
-  'library.universes': 'Followed universes',
-  'bookmark.continueReading': 'Continue reading',
-  'bookmark.statusSummary': 'You have read {readPercent, number, ::percent} of the story.',
-}
+    'library.add': 'Voeg by biblioteek',
+    'library.remove': 'Verwyder uit biblioteek',
+    'library.bought': 'Gekoopte stories',
+    'library.stories': 'Gevolgde stories',
+    'library.universes': 'Followed universes',
+    'bookmark.continueReading': 'Continue reading',
+    'bookmark.statusSummary': 'You have read {readPercent, number, ::percent} of the story.'
+};
