ceb49eda6f0e292b9b5a64886d3ccfd2611b1558
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:42:04 +0200

    New translations rating.js (French)

--- fr-FR/rating.js
+++ fr-FR/rating.js
@@ -2,19 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Évaluation',
-  'rating.explained': 'Classification par âge et restriction.',
-  'rating.all': 'Tous',
-  'rating.desc.all': 'Accessible à tous les âges et à tous les groupes.',
-  'rating.teens': 'Adolescents',
-  'rating.desc.teens': 'Convient à un public âgé de 12 à 18 ans.',
-  'rating.mature': 'Adulte',
-  'rating.desc.mature': 'Contient des thèmes avancés, un langage cru et des scènes implicites.',
-  'rating.explicit': 'Explicite',
-  'rating.desc.explicit': 'Contient des scènes explicites.',
-  'rating.selection.all': 'Tous - Accessible à tous les âges et à tous les groupes.',
-  'rating.selection.teens': 'Adolescents (12-18) - Convient à un public âgé de 12 à 18 ans.',
-  'rating.selection.mature':
-    'Adulte (18+) - Contient des thèmes avancés, un langage cru et des scènes implicites.',
-  'rating.selection.explicit': 'Explicite (21+) - Contient des scènes explicites.',
-}
+    'rating.name': 'Évaluation',
+    'rating.explained': 'Classification par âge et restriction.',
+    'rating.all': 'Tous',
+    'rating.desc.all': 'Accessible à tous les âges et à tous les groupes.',
+    'rating.teens': 'Adolescents',
+    'rating.desc.teens': 'Convient à un public âgé de 12 à 18 ans.',
+    'rating.mature': 'Adulte',
+    'rating.desc.mature': 'Contient des thèmes avancés, un langage cru et des scènes implicites.',
+    'rating.explicit': 'Explicite',
+    'rating.desc.explicit': 'Contient des scènes explicites.',
+    'rating.selection.all': 'Tous - Accessible à tous les âges et à tous les groupes.',
+    'rating.selection.teens': 'Adolescents (12-18) - Convient à un public âgé de 12 à 18 ans.',
+    'rating.selection.mature': 'Adulte (18+) - Contient des thèmes avancés, un langage cru et des scènes implicites.',
+    'rating.selection.explicit': 'Explicite (21+) - Contient des scènes explicites.'
+};
