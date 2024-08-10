6587b402f9281a06d94a94ca73d1c9d630df6b11
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:37:20 +0200

    New translations roadmap.js (Lojban)

--- jbo-EN/roadmap.js
+++ jbo-EN/roadmap.js
@@ -15,14 +15,12 @@ export default {
   'roadmap.eta.q4.long': 'Lo nu se cunso be lo nanca vo',
   'roadmap.backlog': 'Cunso',
   'roadmap.history': 'sance be la mulgau',
-  'roadmap.item.inBacklog':
-    "mi planu lo nu mulgau lo se zukte be ti ze'a lo nu la mulgau cu mulgau",
+  'roadmap.item.inBacklog': 'mi planu lo nu mulgau lo se zukte be ti ze\'a lo nu la mulgau cu mulgau',
   'roadmap.item.inProgress': 'The development of the feature started {startedAt, date}.',
   'roadmap.item.inBeta': 'This feature has entered beta on {betaAt, date}.',
-  'roadmap.item.inPreview':
-    'This feature became available to Storyteller subscribers on {previewAt, date}.',
+  'roadmap.item.inPreview': 'This feature became available to Storyteller subscribers on {previewAt, date}.',
   'roadmap.item.inGa': 'This feature has become generally available on {gaAt, date}.',
-  'roadmap.item.originalIssue': "lo se zukte be ti cu cuxna ze'a lo nu la mulgau cu mulgau",
+  'roadmap.item.originalIssue': 'lo se zukte be ti cu cuxna ze\'a lo nu la mulgau cu mulgau',
   'roadmap.eta.mapTitle': `Planned for {quarter, select,
     Q1 {Q1}
     Q2 {Q2}
@@ -33,8 +31,7 @@ export default {
   'roadmap.eta.mapTitleFinished': 'Finished in {time, date, ::MMMM ::yyyy}',
   'roadmap.back': 'ciksi lo nu la mulgau cu mulgau',
   'roadmap.seeBacklog': 'viska lo se zukte be lo drata cu cuxna',
-  'roadmap.seeFinished':
-    'lo nu xamgu lo nu klama lo se zukte be mi cu se slabu lo nu la mulgau cu mulgau',
+  'roadmap.seeFinished': 'lo nu xamgu lo nu klama lo se zukte be mi cu se slabu lo nu la mulgau cu mulgau',
   'roadmap.planned': 'cuxna',
-  'roadmap.finished': 'mulgau',
-}
+  'roadmap.finished': 'mulgau'
+};
