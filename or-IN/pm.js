a68d772b4ccfb48e6b49765edae73fb9860db3c4
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:40:26 +0200

    New translations pm.js (Odia)

--- or-IN/pm.js
+++ or-IN/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'ଆଲୋଚନା',
-  'pm.showolder': 'ପୁରୁଣା ସନ୍ଦେଶ ଦେଖାନ୍ତୁ',
-  'pm.errors.saysomething': 'ଆପଣ ନିଜରେ କିଛି କହିବା ପାଇଁ ପରିଶ୍ରମ କରିବା ଆବଶ୍ୟକ...',
-  'pm.errors.addrecipient': 'ଆପଣ କମାନ୍ଡରେ କମାନ୍ଡ ଯୋଗାଇବା ପାଇଁ କମିଉଥିବା କମାନ୍ଡରେ ଅନ୍ୟକୋଣ ବ୍ୟବହାରକାରୀ ଯୋଗାଇବା ଆବଶ୍ୟକ।',
-  'pm.message': 'ସନ୍ଦେଶ',
-  'pm.send.message': 'ସନ୍ଦେଶ ପଠାନ୍ତୁ',
-  'pm.messages.own': 'ଆପଣଙ୍କ ସନ୍ଦେଶ',
-  'pm.conversations.none': 'ଆପଣ ବର୍ତ୍ତମାନରେ କିମ୍ବା କେଉଁଠି ସଙ୍ଗଠନ ନାହିଁ।',
-  'pm.to': 'ପାଠାନ୍ତୁ:',
-  'pm.with': 'ସଙ୍ଗଠନରେ ଆଲୋଚନା:',
-  'pm.send': 'ପଠାନ୍ତୁ',
-  'pm.conversations.unreadCount': `ଆପଣଙ୍କୁ {count, plural,
+    'pm.conversation': 'ଆଲୋଚନା',
+    'pm.showolder': 'ପୁରୁଣା ସନ୍ଦେଶ ଦେଖାନ୍ତୁ',
+    'pm.errors.saysomething': 'ଆପଣ ନିଜରେ କିଛି କହିବା ପାଇଁ ପରିଶ୍ରମ କରିବା ଆବଶ୍ୟକ...',
+    'pm.errors.addrecipient': 'ଆପଣ କମାନ୍ଡରେ କମାନ୍ଡ ଯୋଗାଇବା ପାଇଁ କମିଉଥିବା କମାନ୍ଡରେ ଅନ୍ୟକୋଣ ବ୍ୟବହାରକାରୀ ଯୋଗାଇବା ଆବଶ୍ୟକ।',
+    'pm.message': 'ସନ୍ଦେଶ',
+    'pm.send.message': 'ସନ୍ଦେଶ ପଠାନ୍ତୁ',
+    'pm.messages.own': 'ଆପଣଙ୍କ ସନ୍ଦେଶ',
+    'pm.conversations.none': 'ଆପଣ ବର୍ତ୍ତମାନରେ କିମ୍ବା କେଉଁଠି ସଙ୍ଗଠନ ନାହିଁ।',
+    'pm.to': 'ପାଠାନ୍ତୁ:',
+    'pm.with': 'ସଙ୍ଗଠନରେ ଆଲୋଚନା:',
+    'pm.send': 'ପଠାନ୍ତୁ',
+    'pm.conversations.unreadCount': `ଆପଣଙ୍କୁ {count, plural,
     zero {ପଠାନ୍ତୁ ସନ୍ଦେଶ}
     one {ଏକ ପଠାନ୍ତୁ ସନ୍ଦେଶ}
     other {# ପଠାନ୍ତୁ ସନ୍ଦେଶ}
   }.`,
-  'pm.form.minimumParticipants': 'ଆପଣ ଏକ ପଠାନ୍ତୁ ସନ୍ଦେଶ ଚୟନ କରିବା ଆବଶ୍ୟକ।',
-  'pm.form.maxParticipants': `ଆପଣ {maxPeople, plural,
+    'pm.form.minimumParticipants': 'ଆପଣ ଏକ ପଠାନ୍ତୁ ସନ୍ଦେଶ ଚୟନ କରିବା ଆବଶ୍ୟକ।',
+    'pm.form.maxParticipants': `ଆପଣ {maxPeople, plural,
     one {ଏକ ବ୍ୟକ୍ତି}
     other {# ବ୍ୟକ୍ତି}
   } ମଧ୍ୟରେ ଅଧିକତମ ରଖିପାରିବେ ।`,
-  'pm.new.aria': 'ନୂତନ ଆଲୋଚନା',
-  'pm.new.buttonTitle': 'ନୂତନ ଆଲୋଚନା ଶୁରୁ କରନ୍ତୁ',
-}
+    'pm.new.aria': 'ନୂତନ ଆଲୋଚନା',
+    'pm.new.buttonTitle': 'ନୂତନ ଆଲୋଚନା ଶୁରୁ କରନ୍ତୁ'
+};
