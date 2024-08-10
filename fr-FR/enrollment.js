394567f382b485bf80abb486d4d2bf2ef5dcc3b0
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:41:51 +0200

    New translations enrollment.js (French)

--- fr-FR/enrollment.js
+++ fr-FR/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'Réclamez votre compte !',
-  'enrollment.description':
-    'Bienvenue {username}! Pour accéder à votre compte spécial, il vous suffit de créer un mot de passe.',
-  'enrollment.submitting': 'Réclamant votre compte...',
-  'enrollment.submit': 'Réclamez votre compte !',
-}
+    'enrollment.title': 'Réclamez votre compte !',
+    'enrollment.description': 'Bienvenue {username}! Pour accéder à votre compte spécial, il vous suffit de créer un mot de passe.',
+    'enrollment.submitting': 'Réclamant votre compte...',
+    'enrollment.submit': 'Réclamez votre compte !'
+};
