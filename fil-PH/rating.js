70b409178baba5efa2a22f4f109f65ef2ee5513a
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:36:33 +0200

    New translations rating.js (Filipino)

--- fil-PH/rating.js
+++ fil-PH/rating.js
@@ -2,21 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Pangkat',
-  'rating.explained': 'Pangkat ng edad at pagbabawal.',
-  'rating.all': 'Lahat',
-  'rating.desc.all': 'Abot-kaya para sa lahat ng edad at grupo.',
-  'rating.teens': 'Mga Kabataan',
-  'rating.desc.teens': 'Angkop para sa manonood na nasa edad 12 hanggang 18.',
-  'rating.mature': 'Matanda',
-  'rating.desc.mature':
-    'Naglalaman ng mga masalimuot na tema, bastos na salita, at hindi diretsahang mga eksena.',
-  'rating.explicit': 'Malinaw',
-  'rating.desc.explicit': 'Naglalaman ng mga malinaw na eksena.',
-  'rating.selection.all': 'Lahat - Abot-kaya para sa lahat ng edad at grupo.',
-  'rating.selection.teens':
-    'Mga Kabataan (12-18) - Angkop para sa manonood na nasa edad 12 hanggang 18.',
-  'rating.selection.mature':
-    'Matanda (18+) - Naglalaman ng mga masalimuot na tema, bastos na salita, at hindi diretsahang mga eksena.',
-  'rating.selection.explicit': 'Malinaw (21+) - Naglalaman ng mga malinaw na eksena.',
-}
+    'rating.name': 'Pangkat',
+    'rating.explained': 'Pangkat ng edad at pagbabawal.',
+    'rating.all': 'Lahat',
+    'rating.desc.all': 'Abot-kaya para sa lahat ng edad at grupo.',
+    'rating.teens': 'Mga Kabataan',
+    'rating.desc.teens': 'Angkop para sa manonood na nasa edad 12 hanggang 18.',
+    'rating.mature': 'Matanda',
+    'rating.desc.mature': 'Naglalaman ng mga masalimuot na tema, bastos na salita, at hindi diretsahang mga eksena.',
+    'rating.explicit': 'Malinaw',
+    'rating.desc.explicit': 'Naglalaman ng mga malinaw na eksena.',
+    'rating.selection.all': 'Lahat - Abot-kaya para sa lahat ng edad at grupo.',
+    'rating.selection.teens': 'Mga Kabataan (12-18) - Angkop para sa manonood na nasa edad 12 hanggang 18.',
+    'rating.selection.mature': 'Matanda (18+) - Naglalaman ng mga masalimuot na tema, bastos na salita, at hindi diretsahang mga eksena.',
+    'rating.selection.explicit': 'Malinaw (21+) - Naglalaman ng mga malinaw na eksena.'
+};
