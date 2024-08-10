adea160de1247ed61fa1e8ef30519f2ba82d7c54
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:39:32 +0200

    New translations notifications.js (Assamese)

--- as-IN/notifications.js
+++ as-IN/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'এই বার্তা প্ৰদর্শন কৰাত আৰম্ভ কৰক',
   'flashnews.endsAt': 'বার্তা প্ৰদৰ্শন সৰু কৰক',
   'flashnews.newLanguage': 'নতুন ভাষা যোগ কৰিবলৈ নিৰ্বাচন কৰক',
-  'flashnews.onlyDisplayOn':
-    "নির্বাচিত ভাষাত কেৱলমাত্ৰ এই খবৰটো প্ৰদৰ্শিত হয়, বাকী ভাষাতে কোনো খবৰ প্ৰদর্শন কৰা হ'ব নেই।",
+  'flashnews.onlyDisplayOn': 'নির্বাচিত ভাষাত কেৱলমাত্ৰ এই খবৰটো প্ৰদৰ্শিত হয়, বাকী ভাষাতে কোনো খবৰ প্ৰদর্শন কৰা হ\'ব নেই।',
   'notifications.menu.all': 'সকলো জাননি',
   'notifications.menu.following': 'অনুসৰণ কৰা হয়.',
   'notifications.menu.workshop': 'শিল্প শিক্ষাৰ্থী',
@@ -14,8 +13,7 @@ export default {
   'notifications.menu.markRead': 'পঠিত হিচাপে চিহ্নিত কৰক',
   'notifications.menu.delete': 'জাননি মচি পৰিলোঁগিক কৰক',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'আপোনাৰ গল্প {storyName} ব্যৱহাৰ অনকুলতাৰ পিছত শান্তিময় হৈ গছিল।',
+  'notification.workshopStoryStatus.toHiatus': 'আপোনাৰ গল্প {storyName} ব্যৱহাৰ অনকুলতাৰ পিছত শান্তিময় হৈ গছিল।',
   'notification.workshopStoryStatus.toCancelled': 'আপোনাৰ গল্প {storyName} ব্যৱহাৰ নবাধ্য হৈ গছিল।',
   'notification.followingStoryStatus.toHiatus': '{storyName} শান্তিময়।',
   'notification.followingStoryStatus.toCancelled': '{storyName} নিষ্ক্ৰিয় কৰিবলৈ আগতি দিব।',
@@ -25,5 +23,5 @@ export default {
   'notification.friendRequest': 'তোমাৰ একান্ত অনুৰোধ পাঠিছে।',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'গল্পে যান',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'গল্পে যান'
+};
