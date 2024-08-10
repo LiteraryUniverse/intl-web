7786299b49c9771393e8ce822276bae5203d9b98
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:15:46 +0200

    New translations pm.js (Danish)

--- da-DK/pm.js
+++ da-DK/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'Samtale',
-  'pm.showolder': 'Vis ældre beskeder',
-  'pm.errors.saysomething': 'Du bør virkelig sige noget...',
-  'pm.errors.addrecipient': 'Du skal tilføje mindst én anden bruger.',
-  'pm.message': 'Besked',
-  'pm.send.message': 'Send besked',
-  'pm.messages.own': 'Dine beskeder',
-  'pm.conversations.none': 'Du har i øjeblikket ingen samtaler.',
-  'pm.to': 'Til:',
-  'pm.with': 'Samtale med:',
-  'pm.send': 'Send',
-  'pm.conversations.unreadCount': `Du har {count, plural,
+    'pm.conversation': 'Samtale',
+    'pm.showolder': 'Vis ældre beskeder',
+    'pm.errors.saysomething': 'Du bør virkelig sige noget...',
+    'pm.errors.addrecipient': 'Du skal tilføje mindst én anden bruger.',
+    'pm.message': 'Besked',
+    'pm.send.message': 'Send besked',
+    'pm.messages.own': 'Dine beskeder',
+    'pm.conversations.none': 'Du har i øjeblikket ingen samtaler.',
+    'pm.to': 'Til:',
+    'pm.with': 'Samtale med:',
+    'pm.send': 'Send',
+    'pm.conversations.unreadCount': `Du har {count, plural,
     zero {ingen ulæste samtaler}
     one {én ulæst samtale}
     other {# ulæste samtaler}
   }.`,
-  'pm.form.minimumParticipants': 'Du skal vælge en modtager.',
-  'pm.form.maxParticipants': `Du kan maksimalt have {maxPeople, plural,
+    'pm.form.minimumParticipants': 'Du skal vælge en modtager.',
+    'pm.form.maxParticipants': `Du kan maksimalt have {maxPeople, plural,
     one {én person}
     other {# personer}
   } i en samtale.`,
-  'pm.new.aria': 'Ny samtale',
-  'pm.new.buttonTitle': 'Start en ny samtale',
-}
+    'pm.new.aria': 'Ny samtale',
+    'pm.new.buttonTitle': 'Start en ny samtale'
+};
