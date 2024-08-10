6266aa77298191d96778328047e75e91ff0d0fa9
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:33:45 +0200

    New translations pm.js (Estonian)

--- et-EE/pm.js
+++ et-EE/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'Vestlus',
-  'pm.showolder': 'Näita vanemaid sõnumeid',
-  'pm.errors.saysomething': 'Sa peaksid tõesti midagi ütlema...',
-  'pm.errors.addrecipient': 'Peate lisama vähemalt ühe teise kasutaja.',
-  'pm.message': 'Sõnum',
-  'pm.send.message': 'Saada sõnum',
-  'pm.messages.own': 'Sinu sõnumid',
-  'pm.conversations.none': 'Sa ei vestle hetkel kellegagi.',
-  'pm.to': 'Saajale:',
-  'pm.with': 'Vestlus kasutajaga:',
-  'pm.send': 'Saada',
-  'pm.conversations.unreadCount': `Sul on {count, plural,
+    'pm.conversation': 'Vestlus',
+    'pm.showolder': 'Näita vanemaid sõnumeid',
+    'pm.errors.saysomething': 'Sa peaksid tõesti midagi ütlema...',
+    'pm.errors.addrecipient': 'Peate lisama vähemalt ühe teise kasutaja.',
+    'pm.message': 'Sõnum',
+    'pm.send.message': 'Saada sõnum',
+    'pm.messages.own': 'Sinu sõnumid',
+    'pm.conversations.none': 'Sa ei vestle hetkel kellegagi.',
+    'pm.to': 'Saajale:',
+    'pm.with': 'Vestlus kasutajaga:',
+    'pm.send': 'Saada',
+    'pm.conversations.unreadCount': `Sul on {count, plural,
     zero {lugemata vestlust}
     one {üks lugemata vestlus}
     other {# lugemata vestlust}
   }.`,
-  'pm.form.minimumParticipants': 'Peate valima saaja.',
-  'pm.form.maxParticipants': `You can have maximum of {maxPeople, plural,
+    'pm.form.minimumParticipants': 'Peate valima saaja.',
+    'pm.form.maxParticipants': `You can have maximum of {maxPeople, plural,
     one {one person}
     other {# people}
   } in a conversation.`,
-  'pm.new.aria': 'Uus vestlus',
-  'pm.new.buttonTitle': 'Alusta uut vestlust',
-}
+    'pm.new.aria': 'Uus vestlus',
+    'pm.new.buttonTitle': 'Alusta uut vestlust'
+};
