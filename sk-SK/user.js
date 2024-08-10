0b465af40fee517348a648c120031e35b5f4b7d4
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:26:38 +0200

    New translations user.js (Slovak)

--- sk-SK/user.js
+++ sk-SK/user.js
@@ -1,57 +1,57 @@
 export default {
-  'user.friend.request.add': 'Pridať medzi priateľov',
-  'user.friend.requests': `Máte {num, number} {num, plural,
+    'user.friend.request.add': 'Pridať medzi priateľov',
+    'user.friend.requests': `Máte {num, number} {num, plural,
     zero {nových žiadostí}
     one {novú žiadosť}
     few {nové žiadosti}
     other {nových žiadostí}
   } o priateľstvo`,
-  'user.friend.unfriend': 'Zrušenie priateľstva',
-  'user.block': 'Blokovať',
-  'user.unblock': 'Odblokovať',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'Zrušenie priateľstva',
+    'user.block': 'Blokovať',
+    'user.unblock': 'Odblokovať',
+    'user.joined': `{gender, select,
     male {Pripojil sa}
     female {Pripojila}
     other {Pripojil}} sa {date, date, long}`,
-  'user.friend.request.accept': 'Prijať žiadosť o priateľstvo',
-  'user.friend.request.cancel': 'Zrušiť žiadosť o priateľstvo',
-  'user.friend.request.deny': 'Odmietnuť priateľstvo',
-  'user.listing': 'Zoznam užívateľov',
-  'user.profile.visit': 'Navštívte profil {user}.',
-  'user.profile.avatar': 'Avatar {username}',
-  'user.friend.list.requests': 'Žiadosti o priateľstvá',
-  'user.friend.norequests': 'Nemáte žiadne žiadosti o priateľstvo.',
-  'user.dashboardWelcome': `{dayPart, select,
+    'user.friend.request.accept': 'Prijať žiadosť o priateľstvo',
+    'user.friend.request.cancel': 'Zrušiť žiadosť o priateľstvo',
+    'user.friend.request.deny': 'Odmietnuť priateľstvo',
+    'user.listing': 'Zoznam užívateľov',
+    'user.profile.visit': 'Navštívte profil {user}.',
+    'user.profile.avatar': 'Avatar {username}',
+    'user.friend.list.requests': 'Žiadosti o priateľstvá',
+    'user.friend.norequests': 'Nemáte žiadne žiadosti o priateľstvo.',
+    'user.dashboardWelcome': `{dayPart, select,
     morning {Dobré ráno}
     afternoon {Dobrý deň}
     evening {Dobrý večer}
     night {Dobrú noc}
     other {Dobrú noc}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'Tento autor bol publikovaný tradičným spôsobom',
-  'user.verification.luAuthor': 'Pozoruhodný autor z Literary Universe',
-  'user.verification.luEmployee': 'Zamestnanec Literary Universe',
-  'user.verification.publicFigure': 'Overená verejná osoba',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'Tento autor bol publikovaný tradičným spôsobom',
+    'user.verification.luAuthor': 'Pozoruhodný autor z Literary Universe',
+    'user.verification.luEmployee': 'Zamestnanec Literary Universe',
+    'user.verification.publicFigure': 'Overená verejná osoba',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {príbeh}
     few {príbehy}
     many {príbehy}
     other {príbehy}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {vesmír}
     few {vesmíry}
     many {vesmíry}
     other {vesmíry}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {fan fiction}
     other {fan fictions} 
   }`,
-  'profile.works': 'Práce',
-  // As in creative/literary works
-  'profile.wishlist': 'Zoznam želaní',
-  'profile.storiesBy': 'Príbehy vytvorené {username}',
-  'profile.universesBy': 'Vesmíry vytvorené {username}',
-  'profile.fanFictionBy': 'Fan Fiction vytvorené {username}',
-}
+    'profile.works': 'Práce',
+    // As in creative/literary works
+    'profile.wishlist': 'Zoznam želaní',
+    'profile.storiesBy': 'Príbehy vytvorené {username}',
+    'profile.universesBy': 'Vesmíry vytvorené {username}',
+    'profile.fanFictionBy': 'Fan Fiction vytvorené {username}'
+};
