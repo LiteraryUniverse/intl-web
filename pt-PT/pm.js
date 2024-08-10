becc68ebd3a2f9ff6e8abc263a9920cea85c1249
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:25:19 +0200

    New translations pm.js (Portuguese)

--- pt-PT/pm.js
+++ pt-PT/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'Conversa',
-  'pm.showolder': 'Mostrar mensagens mais antigas',
-  'pm.errors.saysomething': 'Deveria realmente dizer alguma coisa...',
-  'pm.errors.addrecipient': 'É necessário acrescentar pelo menos um outro utilizador.',
-  'pm.message': 'Mensagem',
-  'pm.send.message': 'Enviar mensagem',
-  'pm.messages.own': 'As suas mensagens',
-  'pm.conversations.none': 'Atualmente, não está a conversar com ninguém.',
-  'pm.to': 'Para:',
-  'pm.with': 'Conversa com:',
-  'pm.send': 'Enviar',
-  'pm.conversations.unreadCount': `Tem {count, plural,
+    'pm.conversation': 'Conversa',
+    'pm.showolder': 'Mostrar mensagens mais antigas',
+    'pm.errors.saysomething': 'Deveria realmente dizer alguma coisa...',
+    'pm.errors.addrecipient': 'É necessário acrescentar pelo menos um outro utilizador.',
+    'pm.message': 'Mensagem',
+    'pm.send.message': 'Enviar mensagem',
+    'pm.messages.own': 'As suas mensagens',
+    'pm.conversations.none': 'Atualmente, não está a conversar com ninguém.',
+    'pm.to': 'Para:',
+    'pm.with': 'Conversa com:',
+    'pm.send': 'Enviar',
+    'pm.conversations.unreadCount': `Tem {count, plural,
     zero {zero conversas não lidas}
     one {uma conversa não lida}
     other {# conversas não lidas}
   }.`,
-  'pm.form.minimumParticipants': 'Precisa escolher um destinatário.',
-  'pm.form.maxParticipants': `Você pode ter no máximo {maxPeople, plural,
+    'pm.form.minimumParticipants': 'Precisa escolher um destinatário.',
+    'pm.form.maxParticipants': `Você pode ter no máximo {maxPeople, plural,
     one {uma pessoa}
     other {# pessoas}
   } em uma conversa.`,
-  'pm.new.aria': 'Nova conversa',
-  'pm.new.buttonTitle': 'Iniciar uma nova conversa',
-}
+    'pm.new.aria': 'Nova conversa',
+    'pm.new.buttonTitle': 'Iniciar uma nova conversa'
+};
