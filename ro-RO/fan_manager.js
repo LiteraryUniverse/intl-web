93d615b4d2ec6865916ac7089363b55f73828a41
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:41:44 +0200

    New translations fan_manager.js (Romanian)

--- ro-RO/fan_manager.js
+++ ro-RO/fan_manager.js
@@ -10,16 +10,17 @@ export default {
   'fanManager.state.rejected': 'Respinsă',
   'fanManager.state.hidden': 'Ascunsă',
   'fanManager.publicComment': 'Comentariu public',
-  'fanManager.publicComment.desc':
-    'Acest text va fi afișat pe pagina publică ca și comentariul oficial al dvs. la această artă.',
+  'fanManager.publicComment.desc': 'Acest text va fi afișat pe pagina publică ca și comentariul oficial al dvs. la această artă.',
   'fanManager.authorComment': 'Comentariu pentru autor',
-  'fanManager.authorComment.desc':
-    'Acesta este un comentariu privat de la dvs. către autorul acestei arte.',
+  'fanManager.authorComment.desc': 'Acesta este un comentariu privat de la dvs. către autorul acestei arte.',
   'fanManager.stateChange.approve': 'Aprobare artă fan',
   'fanManager.stateChange.reject': 'Respins artă fan',
   'fanManager.stateChange.hide': 'Ascundere artă fan',
   'fanManager.stateChange.unhide': 'Faceți vizibilă arta fan',
-  'fanManager.fanFictionNotAllowed':
-    'Ficțiunea fanilor nu este permisă din natura licenței dumneavoastră.',
+  'fanManager.fanFictionNotAllowed': 'Ficțiunea fanilor nu este permisă din natura licenței dumneavoastră.',
   'fanManager.fanFiction.maxRating': 'Rating maxim pentru afișarea ficțiunii fanilor.',
-}
+  'fanManager.hidden.nothingFound': 'Nu au fost găsite artă ascunsă.',
+  'fanManager.rejected.nothingFound': 'Nu au fost găsite artă respinsă.',
+  'fanManager.approved.nothingFound': 'Nu au fost găsite artă aprobată.',
+  'fanManager.submitted.nothingFound': 'Nu au fost găsite artă fan submisă.'
+};
