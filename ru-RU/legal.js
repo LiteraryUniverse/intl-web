3f5c5c290f1c05a52d121f2cda0455bef1d370c9
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:26:00 +0200

    New translations legal.js (Russian)

--- ru-RU/legal.js
+++ ru-RU/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Извините, что прерываем вас. Но прежде чем мы позволим вам продолжить, мы обязаны по закону убедиться, что вы ознакомлены и согласны с нашими Условиями использования, Политикой конфиденциальности и Соглашением об авторских правах.',
-  'legal.modal.agree': 'Принимаю',
-  'legal.modal.disagree': 'Не принимаю',
-  'legal.modal.register': 'Вы больше не увидите это в случае регистрации.',
-  'legal.modal.goToSettings': 'Перейдите в настройки',
-  'legal.modal.oneLastThing': 'И ещё кое-что...',
-  'legal.modal.finish': 'Завершить',
-  'legal.modal.existingUser': 'Уже зарегистрированы?',
-  'legal.modal.pleaseLogin': 'Пожалуйста, авторизуйтесь.',
-  'legal.publicationAgreements': 'Соглашения о публикации',
-  'legal.generalPublishingAgreement': 'Общее соглашение о публикации',
-  'legal.documentsFor': 'Правовые документы для {workTitle}',
-}
+    'legal.modal.intro': 'Извините, что прерываем вас. Но прежде чем мы позволим вам продолжить, мы обязаны по закону убедиться, что вы ознакомлены и согласны с нашими Условиями использования, Политикой конфиденциальности и Соглашением об авторских правах.',
+    'legal.modal.agree': 'Принимаю',
+    'legal.modal.disagree': 'Не принимаю',
+    'legal.modal.register': 'Вы больше не увидите это в случае регистрации.',
+    'legal.modal.goToSettings': 'Перейдите в настройки',
+    'legal.modal.oneLastThing': 'И ещё кое-что...',
+    'legal.modal.finish': 'Завершить',
+    'legal.modal.existingUser': 'Уже зарегистрированы?',
+    'legal.modal.pleaseLogin': 'Пожалуйста, авторизуйтесь.',
+    'legal.publicationAgreements': 'Соглашения о публикации',
+    'legal.generalPublishingAgreement': 'Общее соглашение о публикации',
+    'legal.documentsFor': 'Правовые документы для {workTitle}'
+};
