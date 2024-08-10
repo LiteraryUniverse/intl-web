973ded8e38ecb792cc0d0442ad4e8cf8b56c1dc6
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:34:53 +0200

    New translations rating.js (Malay)

--- ms-MY/rating.js
+++ ms-MY/rating.js
@@ -2,20 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Penilaian',
-  'rating.explained': 'Penilaian umur dan sekatan.',
-  'rating.all': 'Semua',
-  'rating.desc.all': 'Boleh diakses oleh semua peringkat umur dan kumpulan.',
-  'rating.teens': 'Remaja',
-  'rating.desc.teens': 'Sesuai untuk penonton antara umur 12 hingga 18 tahun.',
-  'rating.mature': 'Dewasa',
-  'rating.desc.mature': 'Mengandungi tema-tema lanjutan, bahasa kasar dan adegan tersirat.',
-  'rating.explicit': 'Eksplisit',
-  'rating.desc.explicit': 'Mengandungi adegan-adegan eksplisit.',
-  'rating.selection.all': 'Semua - Boleh diakses oleh semua peringkat umur dan kumpulan.',
-  'rating.selection.teens':
-    'Remaja (12-18) - Sesuai untuk penonton antara umur 12 hingga 18 tahun.',
-  'rating.selection.mature':
-    'Dewasa (18+) - Mengandungi tema-tema lanjutan, bahasa kasar dan adegan tersirat.',
-  'rating.selection.explicit': 'Eksplisit (21+) - Mengandungi adegan-adegan eksplisit.',
-}
+    'rating.name': 'Penilaian',
+    'rating.explained': 'Penilaian umur dan sekatan.',
+    'rating.all': 'Semua',
+    'rating.desc.all': 'Boleh diakses oleh semua peringkat umur dan kumpulan.',
+    'rating.teens': 'Remaja',
+    'rating.desc.teens': 'Sesuai untuk penonton antara umur 12 hingga 18 tahun.',
+    'rating.mature': 'Dewasa',
+    'rating.desc.mature': 'Mengandungi tema-tema lanjutan, bahasa kasar dan adegan tersirat.',
+    'rating.explicit': 'Eksplisit',
+    'rating.desc.explicit': 'Mengandungi adegan-adegan eksplisit.',
+    'rating.selection.all': 'Semua - Boleh diakses oleh semua peringkat umur dan kumpulan.',
+    'rating.selection.teens': 'Remaja (12-18) - Sesuai untuk penonton antara umur 12 hingga 18 tahun.',
+    'rating.selection.mature': 'Dewasa (18+) - Mengandungi tema-tema lanjutan, bahasa kasar dan adegan tersirat.',
+    'rating.selection.explicit': 'Eksplisit (21+) - Mengandungi adegan-adegan eksplisit.'
+};
