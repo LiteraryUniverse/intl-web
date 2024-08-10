64f21e36642eec456495b268b3cc5088de375f23
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:24:51 +0200

    New translations comments.js (Polish)

--- pl-PL/comments.js
+++ pl-PL/comments.js
@@ -1,12 +1,12 @@
 export default {
-  'comment.write': 'Napisz swój komentarz',
-  'comments.none': 'Niestety, nie ma żadnych komentarzy do pokazania.',
-  'comments.total': `Tutaj {count, plural,
+    'comment.write': 'Napisz swój komentarz',
+    'comments.none': 'Niestety, nie ma żadnych komentarzy do pokazania.',
+    'comments.total': `Tutaj {count, plural,
     =0 {nie ma komentarzy}
     one {to 1 komentarz}
     few {są # komentarzy}
     many {są # komentarzy}
     other {są # komentarzy}
   }.`,
-  'comments.show.older': 'Pokaż starsze komentarze',
-}
+    'comments.show.older': 'Pokaż starsze komentarze'
+};
