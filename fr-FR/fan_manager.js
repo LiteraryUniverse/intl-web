c44ae0985977fc96fb0a73d0e0ab0b092d840b86
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:42:19 +0200

    New translations fan_manager.js (French)

--- fr-FR/fan_manager.js
+++ fr-FR/fan_manager.js
@@ -1,26 +1,26 @@
 export default {
   'fanManager.settings.for': 'Paramètres du gestionnaire de fan',
   'fanManager.allowArtSubmissions': 'Autoriser les soumissions de fan art',
-  'fanManager.displayFanArt': "Autoriser l'affichage des fan arts",
+  'fanManager.displayFanArt': 'Autoriser l\'affichage des fan arts',
   'fanManager.allowFanStories': 'Autoriser les fan fictions',
-  'fanManager.displayFanStories': "Autoriser l'affichage des histoires de fans",
-  'fanManager.state.aria': "Sélectionner l'état des œuvres soumises",
+  'fanManager.displayFanStories': 'Autoriser l\'affichage des histoires de fans',
+  'fanManager.state.aria': 'Sélectionner l\'état des œuvres soumises',
   'fanManager.state.submitted': 'Soumis',
   'fanManager.state.approved': 'Approuvé',
   'fanManager.state.rejected': 'Rejeté',
   'fanManager.state.hidden': 'Caché',
   'fanManager.publicComment': 'Commentaire public',
-  'fanManager.publicComment.desc':
-    'Ce texte sera affiché sur la page publique comme votre commentaire officiel sur cet art.',
-  'fanManager.authorComment': "Commentaire pour l'auteur",
-  'fanManager.authorComment.desc':
-    "Ceci est un commentaire privé de votre part à l'auteur de cet art.",
-  'fanManager.stateChange.approve': "Approuver l'art du fan",
-  'fanManager.stateChange.reject': "Rejeter l'art du fan",
-  'fanManager.stateChange.hide': "Cacher l'art du fan",
-  'fanManager.stateChange.unhide': "Rendre visible l'art du fan",
-  'fanManager.fanFictionNotAllowed':
-    "La fan fiction n'est pas autorisée par la nature de votre licence.",
-  'fanManager.fanFiction.maxRating':
-    "Note that the french translation of the term 'fan fiction' should be 'fanfiction'. Please translate accordingly.",
-}
+  'fanManager.publicComment.desc': 'Ce texte sera affiché sur la page publique comme votre commentaire officiel sur cet art.',
+  'fanManager.authorComment': 'Commentaire pour l\'auteur',
+  'fanManager.authorComment.desc': 'Ceci est un commentaire privé de votre part à l\'auteur de cet art.',
+  'fanManager.stateChange.approve': 'Approuver l\'art du fan',
+  'fanManager.stateChange.reject': 'Rejeter l\'art du fan',
+  'fanManager.stateChange.hide': 'Cacher l\'art du fan',
+  'fanManager.stateChange.unhide': 'Rendre visible l\'art du fan',
+  'fanManager.fanFictionNotAllowed': 'La fan fiction n\'est pas autorisée par la nature de votre licence.',
+  'fanManager.fanFiction.maxRating': 'Note that the french translation of the term \'fan fiction\' should be \'fanfiction\'. Please translate accordingly.',
+  'fanManager.hidden.nothingFound': 'Aucune fanart cachée trouvée.',
+  'fanManager.rejected.nothingFound': 'Aucune fanart rejetée trouvée.',
+  'fanManager.approved.nothingFound': 'Aucune fanart approuvée trouvée.',
+  'fanManager.submitted.nothingFound': 'Aucune illustration de fan soumise trouvée.'
+};
