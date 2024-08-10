134f1054f8782251aff00ac62616999ed5cfaa27
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:27:41 +0200

    New translations comments.js (Turkish)

--- tr-TR/comments.js
+++ tr-TR/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': 'Yorumunuzu yazın',
-  'comments.none': 'Gösterilecek yorum yok.',
-  'comments.total': `There {count, plural,
+    'comment.write': 'Yorumunuzu yazın',
+    'comments.none': 'Gösterilecek yorum yok.',
+    'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-  'comments.show.older': 'Daha eski yorumları göster',
-}
+    'comments.show.older': 'Daha eski yorumları göster'
+};
