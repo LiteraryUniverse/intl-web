1537b1ea71ef0077f66637f18f49545ece8574ce
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:41:52 +0200

    New translations feed.js (French)

--- fr-FR/feed.js
+++ fr-FR/feed.js
@@ -1,9 +1,9 @@
 export default {
-  'feed.name': 'Flux',
-  'feed.post.new': 'Nouveau post',
-  'feed.post.like': 'Aimer le post',
-  'feed.post.unlike': "Enlever j'aime du poste",
-  'feed.post.send': 'Poster',
-  'feed.empty': 'Aucune entrée dans le feed.',
-  'feed.placeholder': "Qu'est-ce qui te préoccupe?",
-}
+    'feed.name': 'Flux',
+    'feed.post.new': 'Nouveau post',
+    'feed.post.like': 'Aimer le post',
+    'feed.post.unlike': 'Enlever j\'aime du poste',
+    'feed.post.send': 'Poster',
+    'feed.empty': 'Aucune entrée dans le feed.',
+    'feed.placeholder': 'Qu\'est-ce qui te préoccupe?'
+};
