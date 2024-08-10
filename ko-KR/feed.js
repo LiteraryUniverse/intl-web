6386f9968da2132f7f2be5d864b1b0a4f95f8e37
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:21:51 +0200

    New translations feed.js (Korean)

--- ko-KR/feed.js
+++ ko-KR/feed.js
@@ -1,9 +1,9 @@
 export default {
-  'feed.name': '피드',
-  'feed.post.new': '새 게시물',
-  'feed.post.like': '게시물 좋아요',
-  'feed.post.unlike': '포스트와 달리',
-  'feed.post.send': '포스트',
-  'feed.empty': '피드에 항목이 없습니다.',
-  'feed.placeholder': '무슨 생각을 하고 있나요?',
-}
+    'feed.name': '피드',
+    'feed.post.new': '새 게시물',
+    'feed.post.like': '게시물 좋아요',
+    'feed.post.unlike': '포스트와 달리',
+    'feed.post.send': '포스트',
+    'feed.empty': '피드에 항목이 없습니다.',
+    'feed.placeholder': '무슨 생각을 하고 있나요?'
+};
