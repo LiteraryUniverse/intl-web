8991c7e0279faf22e6f42406c9117a57c357db88
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:35:32 +0200

    New translations comments.js (Telugu)

--- te-IN/comments.js
+++ te-IN/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': 'మీ వ్యాఖ్యనను రాయండి',
-  'comments.none': 'క్షమించండి, చూపించాల్సిన వ్యాఖ్యలు లేవు.',
-  'comments.total': `ఇక్కడ {count, plural,
+    'comment.write': 'మీ వ్యాఖ్యనను రాయండి',
+    'comments.none': 'క్షమించండి, చూపించాల్సిన వ్యాఖ్యలు లేవు.',
+    'comments.total': `ఇక్కడ {count, plural,
     =0 {వ్యాఖ్యలు లేవు}
     one {ఒక వ్యాఖ్య}
     other {ఉన్నాయి వ్యాఖ్యలు}
   }.`,
-  'comments.show.older': 'పాత వ్యాఖ్యలను చూపించు',
-}
+    'comments.show.older': 'పాత వ్యాఖ్యలను చూపించు'
+};
