f1331ae5bd90fb058f73747c292ad3a9fe36e3ce
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:39:27 +0200

    New translations legal.js (Assamese)

--- as-IN/legal.js
+++ as-IN/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'বিষয়বস্তুতে আগ্রহী হওয়ার আগে আমরা আইনগতভাবে নিশ্চিত করতে চাই, আপনি আমাদের সেবা শর্তাদি, গোপনীয়তা নীতি এবং কপিরাইট চুক্তি সম্পর্কে জানেন এবং সম্মত হন।',
-  'legal.modal.agree': 'আমি সম্মত',
-  'legal.modal.disagree': 'আমি অসম্মত',
-  'legal.modal.register': 'আপনি এটা আর দেখবেন না যদি আপনি নিবন্ধন করুন।',
-  'legal.modal.goToSettings': 'আপনার সেটিংসে যান',
-  'legal.modal.oneLastThing': 'এটা শেষ কিছু...',
-  'legal.modal.finish': 'সমাপ্ত',
-  'legal.modal.existingUser': 'ইতিমধ্যে নিবন্ধিত?',
-  'legal.modal.pleaseLogin': 'অনুগ্রহ করে লগ ইন কৰক।',
-  'legal.publicationAgreements': 'প্রকাশনা চুক্তিসমূহ',
-  'legal.generalPublishingAgreement': 'সাধারণ প্রকাশনা চুক্তি',
-  'legal.documentsFor': '{workTitle}সম্পৰ্কীয় আইনী নথি',
-}
+    'legal.modal.intro': 'বিষয়বস্তুতে আগ্রহী হওয়ার আগে আমরা আইনগতভাবে নিশ্চিত করতে চাই, আপনি আমাদের সেবা শর্তাদি, গোপনীয়তা নীতি এবং কপিরাইট চুক্তি সম্পর্কে জানেন এবং সম্মত হন।',
+    'legal.modal.agree': 'আমি সম্মত',
+    'legal.modal.disagree': 'আমি অসম্মত',
+    'legal.modal.register': 'আপনি এটা আর দেখবেন না যদি আপনি নিবন্ধন করুন।',
+    'legal.modal.goToSettings': 'আপনার সেটিংসে যান',
+    'legal.modal.oneLastThing': 'এটা শেষ কিছু...',
+    'legal.modal.finish': 'সমাপ্ত',
+    'legal.modal.existingUser': 'ইতিমধ্যে নিবন্ধিত?',
+    'legal.modal.pleaseLogin': 'অনুগ্রহ করে লগ ইন কৰক।',
+    'legal.publicationAgreements': 'প্রকাশনা চুক্তিসমূহ',
+    'legal.generalPublishingAgreement': 'সাধারণ প্রকাশনা চুক্তি',
+    'legal.documentsFor': '{workTitle}সম্পৰ্কীয় আইনী নথি'
+};
