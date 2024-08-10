4b15ea8395242602ab87d90e1fb2e4606e5edb9b
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:14:49 +0200

    New translations forums.js (Catalan)

--- ca-ES/forums.js
+++ ca-ES/forums.js
@@ -1,35 +1,28 @@
 export default {
   'forums.create': 'Activar fòrum',
   'forums.archive': 'Arxivar fòrum',
-  'forums.archive.explained':
-    'Arxivar el fòrum impedirà als usuaris publicar més contingut, però romandrà visible.',
-  'forums.archive.success': "S'ha arxivat el fòrum",
+  'forums.archive.explained': 'Arxivar el fòrum impedirà als usuaris publicar més contingut, però romandrà visible.',
+  'forums.archive.success': 'S\'ha arxivat el fòrum',
   'forums.unArchive': 'Reactivar els fòrums',
-  'forums.unArchive.success': "S'ha reactivat el fòrum!",
+  'forums.unArchive.success': 'S\'ha reactivat el fòrum!',
   'forums.settings': 'Configuració del fòrum',
   'forums.category.create': 'Crear categoria del fòrum',
   'forums.category.delete': 'Eliminar categoria',
   'forums.category.name': 'Nom de la categoria',
-  'forums.category.slug': "Part de l'URL SEO",
+  'forums.category.slug': 'Part de l\'URL SEO',
   'forums.category.settings': 'Configuració de la categoria',
-  'forums.category.language':
-    "Limita la visualització d'aquesta categoria al següent idioma. Deixa-ho en blanc si vols mostrar la categoria en tots els idiomes.",
-  'forums.category.intlNameDescription':
-    'Tenim títols localitzats de noms de categories comuns per a la teva selecció, de manera que la categoria pugui estar disponible en tots els idiomes compatibles.',
+  'forums.category.language': 'Limita la visualització d\'aquesta categoria al següent idioma. Deixa-ho en blanc si vols mostrar la categoria en tots els idiomes.',
+  'forums.category.intlNameDescription': 'Tenim títols localitzats de noms de categories comuns per a la teva selecció, de manera que la categoria pugui estar disponible en tots els idiomes compatibles.',
   'forums.thread.new': 'Nou fil',
   'forums.thread.delete': 'Eliminar fil',
   'forums.post.new': 'Nova publicació',
-  'forums.archived.forum':
-    "Aquest fòrum s'ha arxivat el {archivedAt, date, long}. Encara pots veure el fòrum, però ja no pots crear contingut nou.",
+  'forums.archived.forum': 'Aquest fòrum s\'ha arxivat el {archivedAt, date, long}. Encara pots veure el fòrum, però ja no pots crear contingut nou.',
   'forums.category.archive': 'Arxivar categoria',
-  'forums.category.archiveExplained':
-    'Les categories que no tenen fils seran eliminades, mentre que les categories amb contingut evitaran la creació de nous fils.',
+  'forums.category.archiveExplained': 'Les categories que no tenen fils seran eliminades, mentre que les categories amb contingut evitaran la creació de nous fils.',
   'forums.category.reactivate': 'Reactivar categoria',
   'forums.category.isVotable': 'Permetre als usuaris votar pels fils',
-  'forums.category.titleIsGlobal':
-    "Aquest títol de categoria es mostrarà en el idioma de l'usuari si el suportem.",
-  'forums.category.languageLimited':
-    'Aquesta categoria només es mostrarà als usuaris en aquest idioma.',
+  'forums.category.titleIsGlobal': 'Aquest títol de categoria es mostrarà en el idioma de l\'usuari si el suportem.',
+  'forums.category.languageLimited': 'Aquesta categoria només es mostrarà als usuaris en aquest idioma.',
   'forums.threadsCounter': 'Nombre total de temes de discussió',
   // Pre-defined category names
   'forums.category.intlName.general': 'General',
@@ -49,16 +42,15 @@ export default {
   'forums.category.intlName.questions': 'Preguntes',
   'forums.category.back': 'Tornar a la vista general del fòrum',
   // Forum restrictions
-  'forums.forumRestriction.usersOnly': "Permetre l'accés només als usuaris registrats",
-  'forums.settings.accessControls': "Controls d'accés",
-  'forums.forumRestrictionsMessages.usersOnlyForum':
-    'Aquest fòrum només és accessible per a usuaris registrats.',
+  'forums.forumRestriction.usersOnly': 'Permetre l\'accés només als usuaris registrats',
+  'forums.settings.accessControls': 'Controls d\'accés',
+  'forums.forumRestrictionsMessages.usersOnlyForum': 'Aquest fòrum només és accessible per a usuaris registrats.',
   'forums.noThreads': 'Res a veure aquí. Depèn de tu començar la conversa.',
   'forums.noPosts': 'Encara ningú ha contestat. Sigues el primer!',
   // Threads
   'forums.createThread': 'Crear una nova discussió',
   'forums.createThreadIn': 'Crear un nou fil a {categoryName}',
-  'forums.openingPost': "Publicació d'obertura",
+  'forums.openingPost': 'Publicació d\'obertura',
   'forums.createQuestionLabel': 'Això és una pregunta?',
   'forums.thread.noReplies': 'Actualment no hi ha respostes aquí.',
   'forums.reply': 'Resposta',
@@ -67,10 +59,10 @@ export default {
   'forums.reset': 'Restablir',
   'forum.thread.reply': 'Afegir una resposta a aquest fil',
   'forums.yourMessage': 'El teu missatge',
-  'forums.thread.replyPosted': "El teu missatge s'ha publicat",
+  'forums.thread.replyPosted': 'El teu missatge s\'ha publicat',
   'forums.thread.totalPosts': `{totalReplies, number} {totalReplies, plural,
     one {resposta}
     other {respostes}
   }`,
-  'forums.thread.createdAt': 'Creat el {createdAt, date, medium}',
-}
+  'forums.thread.createdAt': 'Creat el {createdAt, date, medium}'
+};
