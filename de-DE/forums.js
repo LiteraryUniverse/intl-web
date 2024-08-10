16c02829ca2bf467bf1473da62571fbc9dd79cf2
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:16:34 +0200

    New translations forums.js (German)

--- de-DE/forums.js
+++ de-DE/forums.js
@@ -1,8 +1,7 @@
 export default {
   'forums.create': 'Forum aktivieren',
   'forums.archive': 'Forum archivieren',
-  'forums.archive.explained':
-    'Wenn Sie das Forum archivieren, können die Benutzer keine weiteren Inhalte mehr einstellen, aber sie bleiben sichtbar.',
+  'forums.archive.explained': 'Wenn Sie das Forum archivieren, können die Benutzer keine weiteren Inhalte mehr einstellen, aber sie bleiben sichtbar.',
   'forums.archive.success': 'Das Forum wurde archiviert',
   'forums.unArchive': 'Foren reaktivieren',
   'forums.unArchive.success': 'Das Forum wurde reaktiviert!',
@@ -12,24 +11,18 @@ export default {
   'forums.category.name': 'Name der Kategorie',
   'forums.category.slug': 'SEO-Url-Teil',
   'forums.category.settings': 'Kategorie-Einstellungen',
-  'forums.category.language':
-    'Beschränken Sie die Anzeige dieser Kategorie auf die folgende Sprache. Leer lassen, wenn Sie die Kategorie in allen Sprachen anzeigen lassen möchten.',
-  'forums.category.intlNameDescription':
-    'Wir haben die Titel der gängigen Kategorienamen für Ihre Auswahl lokalisiert, so dass die Kategorie in allen unterstützten Sprachen verfügbar ist.',
+  'forums.category.language': 'Beschränken Sie die Anzeige dieser Kategorie auf die folgende Sprache. Leer lassen, wenn Sie die Kategorie in allen Sprachen anzeigen lassen möchten.',
+  'forums.category.intlNameDescription': 'Wir haben die Titel der gängigen Kategorienamen für Ihre Auswahl lokalisiert, so dass die Kategorie in allen unterstützten Sprachen verfügbar ist.',
   'forums.thread.new': 'Neues Thema',
   'forums.thread.delete': 'Thread löschen',
   'forums.post.new': 'Neuer Post',
-  'forums.archived.forum':
-    'Dieses Forum wurde am {archivedAt, date, long} archiviert. Sie können die Foren immer noch ansehen, aber Sie können keine neuen Inhalte mehr erstellen.',
+  'forums.archived.forum': 'Dieses Forum wurde am {archivedAt, date, long} archiviert. Sie können die Foren immer noch ansehen, aber Sie können keine neuen Inhalte mehr erstellen.',
   'forums.category.archive': 'Kategorie archivieren',
-  'forums.category.archiveExplained':
-    'Kategorien, die keine Themen enthalten, werden gelöscht, während Kategorien mit Inhalt die Erstellung neuer Themen verhindern.',
+  'forums.category.archiveExplained': 'Kategorien, die keine Themen enthalten, werden gelöscht, während Kategorien mit Inhalt die Erstellung neuer Themen verhindern.',
   'forums.category.reactivate': 'Kategorie reaktivieren',
   'forums.category.isVotable': 'Erlauben Sie Nutzern, Themen hoch/runter zu bewerten',
-  'forums.category.titleIsGlobal':
-    'Dieser Kategorietitel wird in der Sprache des Benutzers angezeigt, sofern wir diese unterstützen.',
-  'forums.category.languageLimited':
-    'Diese Kategorie wird nur für Benutzer in dieser Sprache angezeigt.',
+  'forums.category.titleIsGlobal': 'Dieser Kategorietitel wird in der Sprache des Benutzers angezeigt, sofern wir diese unterstützen.',
+  'forums.category.languageLimited': 'Diese Kategorie wird nur für Benutzer in dieser Sprache angezeigt.',
   'forums.threadsCounter': 'Gesamtzahl der diskutierten Themen',
   // Pre-defined category names
   'forums.category.intlName.general': 'Allgemein',
@@ -51,8 +44,7 @@ export default {
   // Forum restrictions
   'forums.forumRestriction.usersOnly': 'Zugang nur für registrierte Benutzer erlauben',
   'forums.settings.accessControls': 'Zugangskontrollen',
-  'forums.forumRestrictionsMessages.usersOnlyForum':
-    'Dieses Forum ist nur für registrierte Benutzer zugänglich.',
+  'forums.forumRestrictionsMessages.usersOnlyForum': 'Dieses Forum ist nur für registrierte Benutzer zugänglich.',
   'forums.noThreads': 'Hier gibt es nichts zu sehen. Es liegt an Ihnen, ein Gespräch zu beginnen.',
   'forums.noPosts': 'Es hat noch niemand geantwortet. Seien Sie der Erste!',
   // Threads
@@ -72,5 +64,5 @@ export default {
     one {Antwort}
     other {Antworten}
   }`,
-  'forums.thread.createdAt': 'Erstellt am {createdAt, date, medium}',
-}
+  'forums.thread.createdAt': 'Erstellt am {createdAt, date, medium}'
+};
