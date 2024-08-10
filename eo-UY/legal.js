1f4bc2e34f66e0d672127708fd523582c24ef6da
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:36:06 +0200

    New translations legal.js (Esperanto)

--- eo-UY/legal.js
+++ eo-UY/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Pardonu la interrompon. Sed antaŭ ol ni lasas vin pluiri, ni estas lege devigitaj certigi, ke vi estas konscia pri kaj konsentas kun niaj Uzokondiĉoj, Politiko pri Privateco kaj Kopirajto-Akordo.',
-  'legal.modal.agree': 'Mi Konsentas',
-  'legal.modal.disagree': 'Mi Ne Konsentas',
-  'legal.modal.register': 'Vi ne plu vidos tion se vi registras.',
-  'legal.modal.goToSettings': 'Iru al viaj agordoj',
-  'legal.modal.oneLastThing': 'Unu lasta afero...',
-  'legal.modal.finish': 'Fini',
-  'legal.modal.existingUser': 'Ĉu vi jam registriĝis?',
-  'legal.modal.pleaseLogin': 'Bonvolu ensaluti.',
-  'legal.publicationAgreements': 'Aldonoj pri publikigo',
-  'legal.generalPublishingAgreement': 'Ĝenerala kunteksto pri publikigo',
-  'legal.documentsFor': 'Jura dokumentoj por {workTitle}',
-}
+    'legal.modal.intro': 'Pardonu la interrompon. Sed antaŭ ol ni lasas vin pluiri, ni estas lege devigitaj certigi, ke vi estas konscia pri kaj konsentas kun niaj Uzokondiĉoj, Politiko pri Privateco kaj Kopirajto-Akordo.',
+    'legal.modal.agree': 'Mi Konsentas',
+    'legal.modal.disagree': 'Mi Ne Konsentas',
+    'legal.modal.register': 'Vi ne plu vidos tion se vi registras.',
+    'legal.modal.goToSettings': 'Iru al viaj agordoj',
+    'legal.modal.oneLastThing': 'Unu lasta afero...',
+    'legal.modal.finish': 'Fini',
+    'legal.modal.existingUser': 'Ĉu vi jam registriĝis?',
+    'legal.modal.pleaseLogin': 'Bonvolu ensaluti.',
+    'legal.publicationAgreements': 'Aldonoj pri publikigo',
+    'legal.generalPublishingAgreement': 'Ĝenerala kunteksto pri publikigo',
+    'legal.documentsFor': 'Jura dokumentoj por {workTitle}'
+};
