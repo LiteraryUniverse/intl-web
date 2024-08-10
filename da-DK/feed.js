4ddb7592567013fd758ea62ded0d445e1e1671cb
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:15:37 +0200

    New translations feed.js (Danish)

--- da-DK/feed.js
+++ da-DK/feed.js
@@ -1,9 +1,9 @@
 export default {
-  'feed.name': 'Forsyning',
-  'feed.post.new': 'Ny post',
-  'feed.post.like': 'Synes godt om post',
-  'feed.post.unlike': 'Synes ikke godt om post',
-  'feed.post.send': 'Indlæg',
-  'feed.empty': 'Ingen indlæg i feeden.',
-  'feed.placeholder': 'Hvad tænker du på?',
-}
+    'feed.name': 'Forsyning',
+    'feed.post.new': 'Ny post',
+    'feed.post.like': 'Synes godt om post',
+    'feed.post.unlike': 'Synes ikke godt om post',
+    'feed.post.send': 'Indlæg',
+    'feed.empty': 'Ingen indlæg i feeden.',
+    'feed.placeholder': 'Hvad tænker du på?'
+};
