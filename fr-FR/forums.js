bfda3f165fab975d928311303d8c5b2e49572aa4
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:42:15 +0200

    New translations forums.js (French)

--- fr-FR/forums.js
+++ fr-FR/forums.js
@@ -1,8 +1,7 @@
 export default {
   'forums.create': 'Activer le forum',
   'forums.archive': 'Archiver le forum',
-  'forums.archive.explained':
-    "L'archivage du forum empêchera les utilisateurs de publier d'autres contenus, mais il restera visible.",
+  'forums.archive.explained': 'L\'archivage du forum empêchera les utilisateurs de publier d\'autres contenus, mais il restera visible.',
   'forums.archive.success': 'Le forum a été archivé',
   'forums.unArchive': 'Réactiver les forums',
   'forums.unArchive.success': 'Le forum a été réactivé !',
@@ -10,27 +9,20 @@ export default {
   'forums.category.create': 'Créer une catégorie de forum',
   'forums.category.delete': 'Supprimer cette catégorie',
   'forums.category.name': 'Nom de catégorie',
-  'forums.category.slug': "Partie d'url SEO",
+  'forums.category.slug': 'Partie d\'url SEO',
   'forums.category.settings': 'Paramètres de catégorie',
-  'forums.category.language':
-    "Limiter l'affichage de cette catégorie à la langue suivante. Laisser vide si vous voulez afficher la catégorie dans toutes les langues.",
-  'forums.category.intlNameDescription':
-    'Nous avons localisé les titres des noms de catégorie courants pour votre sélection, afin que la catégorie soit disponible dans toutes les langues prises en charge.',
+  'forums.category.language': 'Limiter l\'affichage de cette catégorie à la langue suivante. Laisser vide si vous voulez afficher la catégorie dans toutes les langues.',
+  'forums.category.intlNameDescription': 'Nous avons localisé les titres des noms de catégorie courants pour votre sélection, afin que la catégorie soit disponible dans toutes les langues prises en charge.',
   'forums.thread.new': 'Nouveau fil de discussion',
   'forums.thread.delete': 'Supprimer le fil de discussion',
   'forums.post.new': 'Nouveau post',
-  'forums.archived.forum':
-    "Ce forum a été archivé le {archivedAt, date, long}. Vous pouvez toujours consulter les forums, mais vous n'êtes plus en mesure de créer un nouveau contenu.",
+  'forums.archived.forum': 'Ce forum a été archivé le {archivedAt, date, long}. Vous pouvez toujours consulter les forums, mais vous n\'êtes plus en mesure de créer un nouveau contenu.',
   'forums.category.archive': 'Archiver la catégorie',
-  'forums.category.archiveExplained':
-    "Les catégories qui n'ont pas de fils de discussion seront supprimées, tandis que les catégories avec du contenu empêcheront la création de nouveaux fils de discussion.",
+  'forums.category.archiveExplained': 'Les catégories qui n\'ont pas de fils de discussion seront supprimées, tandis que les catégories avec du contenu empêcheront la création de nouveaux fils de discussion.',
   'forums.category.reactivate': 'Réactiver la catégorie',
-  'forums.category.isVotable':
-    'Autoriser les utilisateurs à voter vers le haut/vers le bas les fils de discussion',
-  'forums.category.titleIsGlobal':
-    "Le titre de cette catégorie sera affiché dans la langue de l'utilisateur si nous le prenons en charge.",
-  'forums.category.languageLimited':
-    "Cette catégorie ne s'affichera que pour les utilisateurs dans cette langue.",
+  'forums.category.isVotable': 'Autoriser les utilisateurs à voter vers le haut/vers le bas les fils de discussion',
+  'forums.category.titleIsGlobal': 'Le titre de cette catégorie sera affiché dans la langue de l\'utilisateur si nous le prenons en charge.',
+  'forums.category.languageLimited': 'Cette catégorie ne s\'affichera que pour les utilisateurs dans cette langue.',
   'forums.threadsCounter': 'Nombre total de sujets de discussion',
   // Pre-defined category names
   'forums.category.intlName.general': 'Général',
@@ -48,20 +40,19 @@ export default {
   'forums.category.intlName.news': 'Actualités',
   'forums.category.intlName.help': 'Aide',
   'forums.category.intlName.questions': 'Questions',
-  'forums.category.back': "Retour à l'aperçu du forum",
+  'forums.category.back': 'Retour à l\'aperçu du forum',
   // Forum restrictions
-  'forums.forumRestriction.usersOnly': "N'autoriser l'accès qu'aux utilisateurs enregistrés",
-  'forums.settings.accessControls': "Contrôles d'accès",
-  'forums.forumRestrictionsMessages.usersOnlyForum':
-    "Ce forum n'est accessible qu'aux utilisateurs enregistrés.",
-  'forums.noThreads': "Rien à voir ici. C'est à vous de commencer la conversation.",
-  'forums.noPosts': "Personne n'a encore répondu. Soyez le premier !",
+  'forums.forumRestriction.usersOnly': 'N\'autoriser l\'accès qu\'aux utilisateurs enregistrés',
+  'forums.settings.accessControls': 'Contrôles d\'accès',
+  'forums.forumRestrictionsMessages.usersOnlyForum': 'Ce forum n\'est accessible qu\'aux utilisateurs enregistrés.',
+  'forums.noThreads': 'Rien à voir ici. C\'est à vous de commencer la conversation.',
+  'forums.noPosts': 'Personne n\'a encore répondu. Soyez le premier !',
   // Threads
   'forums.createThread': 'Créer une nouvelle discussion',
   'forums.createThreadIn': 'Créer un nouveau fil de discussion dans {categoryName}',
-  'forums.openingPost': "Ouverture d'un post",
+  'forums.openingPost': 'Ouverture d\'un post',
   'forums.createQuestionLabel': 'Est-ce une question ?',
-  'forums.thread.noReplies': "Il n'y a actuellement aucune réponse ici.",
+  'forums.thread.noReplies': 'Il n\'y a actuellement aucune réponse ici.',
   'forums.reply': 'Répondre',
   'forums.post': 'Poster',
   'forums.posting': 'Publication...',
@@ -73,5 +64,5 @@ export default {
     one {réponse}
     other {réponses}
   }`,
-  'forums.thread.createdAt': 'Créé le {createdAt, date, medium}',
-}
+  'forums.thread.createdAt': 'Créé le {createdAt, date, medium}'
+};
