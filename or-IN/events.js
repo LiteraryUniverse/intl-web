0422316aa52e1b9d3c3ce50d21945c79e880ba33
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:40:41 +0200

    New translations events.js (Odia)

--- or-IN/events.js
+++ or-IN/events.js
@@ -42,5 +42,5 @@ export default {
   'events.moreInfo': 'More information',
   'event.visibilityShort.private': 'Private',
   'event.visibilityShort.public': 'Public',
-  'event.visibilityShort.owner': 'ମାଲିକ',
-}
+  'event.visibilityShort.owner': 'ମାଲିକ'
+};
