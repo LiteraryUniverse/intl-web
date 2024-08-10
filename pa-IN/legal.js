ac4cfff660fda9f352dfd42ef1e47ae56395bd9a
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:24:19 +0200

    New translations legal.js (Punjabi)

--- pa-IN/legal.js
+++ pa-IN/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    "ਮਾਫ ਕਰਨਾ ਕਿ ਮੁੜ ਇੱਕ ਵਾਰ ਤੁਹਾਨੂੰ ਅੱਗੇ ਜਾਣ ਦੀ ਇਜਾਜ਼ਤ ਦੇਣ ਤੋਂ ਪਹਿਲਾਂ, ਅਸੀਂ ਕਾਨੂੰਨੀ ਤੌਰ 'ਤੇ ਬੰਧਿਤ ਹਾਂ ਕਿ ਤੁਸੀਂ ਸਾਡੇ ਸੇਵਾ ਦੀਆਂ ਸ਼ਰਤਾਂ, ਪਰਦੇਦਾਰੀ ਨੀਤੀ ਅਤੇ ਕਾਪੀਰਾਈਟ ਇਕਰਾਰ ਦੇ ਬਾਰੇ ਜਾਣਕਾਰੀ ਰੱਖਣ ਅਤੇ ਉਨ੍ਹਾਂ ਨਾਲ ਸਹਿਮਤ ਹੋਣ ਦੀ ਯਕੀਨੀ ਬਣਾਉਣ ਲਈ ਹੈ।",
-  'legal.modal.agree': 'ਮੈਂ ਸਹਿਮਤ ਹਾਂ',
-  'legal.modal.disagree': 'ਮੈਂ ਗੈਰ-ਸਹਿਮਤ ਹਾਂ',
-  'legal.modal.register': 'ਜੇ ਤੁਸੀਂ ਰਜਿਸਟਰ ਕਰਦੇ ਹੋ ਤਾਂ ਤੁਸੀਂ ਇਹ ਹੋਰ ਨਹੀਂ ਵੇਖੋਗੇ।',
-  'legal.modal.goToSettings': "ਆਪਣੇ ਸੈਟਿੰਗਾਂ 'ਤੇ ਜਾਓ",
-  'legal.modal.oneLastThing': 'ਇੱਕ ਆਖਰੀ ਗੱਲ...',
-  'legal.modal.finish': 'ਮੁਕੰਮਲ ਕਰੋ',
-  'legal.modal.existingUser': 'ਪਹਿਲਾਂ ਹੀ ਰਜਿਸਟਰ ਕੀਤਾ ਹੈ?',
-  'legal.modal.pleaseLogin': 'ਕਿਰਪਾ ਕਰਕੇ ਲਾਗਇਨ ਕਰੋ।',
-  'legal.publicationAgreements': 'ਪ੍ਰਕਾਸ਼ਨ ਸੁਝਾਅ',
-  'legal.generalPublishingAgreement': 'ਆਮ ਪ੍ਰਕਾਸ਼ਨ ਸਮਝੌਤਾ',
-  'legal.documentsFor': 'ਕਾਨੂੰਨੀ ਦਸਤਾਵੇਜ਼ {workTitle}ਲਈ',
-}
+    'legal.modal.intro': 'ਮਾਫ ਕਰਨਾ ਕਿ ਮੁੜ ਇੱਕ ਵਾਰ ਤੁਹਾਨੂੰ ਅੱਗੇ ਜਾਣ ਦੀ ਇਜਾਜ਼ਤ ਦੇਣ ਤੋਂ ਪਹਿਲਾਂ, ਅਸੀਂ ਕਾਨੂੰਨੀ ਤੌਰ \'ਤੇ ਬੰਧਿਤ ਹਾਂ ਕਿ ਤੁਸੀਂ ਸਾਡੇ ਸੇਵਾ ਦੀਆਂ ਸ਼ਰਤਾਂ, ਪਰਦੇਦਾਰੀ ਨੀਤੀ ਅਤੇ ਕਾਪੀਰਾਈਟ ਇਕਰਾਰ ਦੇ ਬਾਰੇ ਜਾਣਕਾਰੀ ਰੱਖਣ ਅਤੇ ਉਨ੍ਹਾਂ ਨਾਲ ਸਹਿਮਤ ਹੋਣ ਦੀ ਯਕੀਨੀ ਬਣਾਉਣ ਲਈ ਹੈ।',
+    'legal.modal.agree': 'ਮੈਂ ਸਹਿਮਤ ਹਾਂ',
+    'legal.modal.disagree': 'ਮੈਂ ਗੈਰ-ਸਹਿਮਤ ਹਾਂ',
+    'legal.modal.register': 'ਜੇ ਤੁਸੀਂ ਰਜਿਸਟਰ ਕਰਦੇ ਹੋ ਤਾਂ ਤੁਸੀਂ ਇਹ ਹੋਰ ਨਹੀਂ ਵੇਖੋਗੇ।',
+    'legal.modal.goToSettings': 'ਆਪਣੇ ਸੈਟਿੰਗਾਂ \'ਤੇ ਜਾਓ',
+    'legal.modal.oneLastThing': 'ਇੱਕ ਆਖਰੀ ਗੱਲ...',
+    'legal.modal.finish': 'ਮੁਕੰਮਲ ਕਰੋ',
+    'legal.modal.existingUser': 'ਪਹਿਲਾਂ ਹੀ ਰਜਿਸਟਰ ਕੀਤਾ ਹੈ?',
+    'legal.modal.pleaseLogin': 'ਕਿਰਪਾ ਕਰਕੇ ਲਾਗਇਨ ਕਰੋ।',
+    'legal.publicationAgreements': 'ਪ੍ਰਕਾਸ਼ਨ ਸੁਝਾਅ',
+    'legal.generalPublishingAgreement': 'ਆਮ ਪ੍ਰਕਾਸ਼ਨ ਸਮਝੌਤਾ',
+    'legal.documentsFor': 'ਕਾਨੂੰਨੀ ਦਸਤਾਵੇਜ਼ {workTitle}ਲਈ'
+};
