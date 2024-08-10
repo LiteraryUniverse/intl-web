fee5a21df1a8a739b1568e75bfb010c83fc9cfe7
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:25:53 +0200

    New translations pm.js (Russian)

--- ru-RU/pm.js
+++ ru-RU/pm.js
@@ -1,28 +1,28 @@
 export default {
-  'pm.conversation': 'Беседа',
-  'pm.showolder': 'Отображать старые сообщения',
-  'pm.errors.saysomething': 'Вы должны сказать что-нибудь...',
-  'pm.errors.addrecipient': 'Вам необходимо добавить как минимум ещё одного пользователя.',
-  'pm.message': 'Сообщение',
-  'pm.send.message': 'Отправить сообщение',
-  'pm.messages.own': 'Ваши сообщения',
-  'pm.conversations.none': 'В настоящее время вы ни с кем не ведёте беседу.',
-  'pm.to': 'Кому:',
-  'pm.with': 'Беседа с:',
-  'pm.send': 'Отправить',
-  'pm.conversations.unreadCount': `У вас есть {count, plural,
+    'pm.conversation': 'Беседа',
+    'pm.showolder': 'Отображать старые сообщения',
+    'pm.errors.saysomething': 'Вы должны сказать что-нибудь...',
+    'pm.errors.addrecipient': 'Вам необходимо добавить как минимум ещё одного пользователя.',
+    'pm.message': 'Сообщение',
+    'pm.send.message': 'Отправить сообщение',
+    'pm.messages.own': 'Ваши сообщения',
+    'pm.conversations.none': 'В настоящее время вы ни с кем не ведёте беседу.',
+    'pm.to': 'Кому:',
+    'pm.with': 'Беседа с:',
+    'pm.send': 'Отправить',
+    'pm.conversations.unreadCount': `У вас есть {count, plural,
     one {одна непрочитанная беседа}
     few {# непрочитанные беседы}
     many {# непрочитанных бесед}
     other {# непрочитанных бесед}
   }.`,
-  'pm.form.minimumParticipants': 'Вам необходимо выбрать получателя.',
-  'pm.form.maxParticipants': `В беседе могут участвовать не более {maxPeople, plural,
+    'pm.form.minimumParticipants': 'Вам необходимо выбрать получателя.',
+    'pm.form.maxParticipants': `В беседе могут участвовать не более {maxPeople, plural,
     one {одного человека}
     few {# людей}
     many {# людей}
     other {# людей}
   }.`,
-  'pm.new.aria': 'Новая беседа',
-  'pm.new.buttonTitle': 'Начать новую беседу',
-}
+    'pm.new.aria': 'Новая беседа',
+    'pm.new.buttonTitle': 'Начать новую беседу'
+};
