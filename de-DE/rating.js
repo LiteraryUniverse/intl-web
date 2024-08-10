ca648fae72d29fa5bfbf388233114d487b857588
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:16:22 +0200

    New translations rating.js (German)

--- de-DE/rating.js
+++ de-DE/rating.js
@@ -2,20 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Altersfreigabe',
-  'rating.explained': 'Altersfreigabe und -beschränkung.',
-  'rating.all': 'Alle',
-  'rating.desc.all': 'Zugänglich für alle Altersgruppen und Gruppen.',
-  'rating.teens': 'Jugendliche',
-  'rating.desc.teens': 'Geeignet für ein Publikum zwischen 12 und 18 Jahren.',
-  'rating.mature': 'Erwachsen',
-  'rating.desc.mature': 'Enthält fortgeschrittene Themen, grobe Sprache und implizite Szenen.',
-  'rating.explicit': 'Explizit',
-  'rating.desc.explicit': 'Enthält explizite Szenen.',
-  'rating.selection.all': 'Alle - Zugänglich für alle Altersgruppen und Gruppen.',
-  'rating.selection.teens':
-    'Jugendliche (12-18) - Geeignet für Zuschauer zwischen 12 und 18 Jahren.',
-  'rating.selection.mature':
-    'Erwachsen (18+) - Enthält fortgeschrittene Themen, grobe Sprache und implizite Szenen.',
-  'rating.selection.explicit': 'Explicit (21+) - Enthält explizite Szenen.',
-}
+    'rating.name': 'Altersfreigabe',
+    'rating.explained': 'Altersfreigabe und -beschränkung.',
+    'rating.all': 'Alle',
+    'rating.desc.all': 'Zugänglich für alle Altersgruppen und Gruppen.',
+    'rating.teens': 'Jugendliche',
+    'rating.desc.teens': 'Geeignet für ein Publikum zwischen 12 und 18 Jahren.',
+    'rating.mature': 'Erwachsen',
+    'rating.desc.mature': 'Enthält fortgeschrittene Themen, grobe Sprache und implizite Szenen.',
+    'rating.explicit': 'Explizit',
+    'rating.desc.explicit': 'Enthält explizite Szenen.',
+    'rating.selection.all': 'Alle - Zugänglich für alle Altersgruppen und Gruppen.',
+    'rating.selection.teens': 'Jugendliche (12-18) - Geeignet für Zuschauer zwischen 12 und 18 Jahren.',
+    'rating.selection.mature': 'Erwachsen (18+) - Enthält fortgeschrittene Themen, grobe Sprache und implizite Szenen.',
+    'rating.selection.explicit': 'Explicit (21+) - Enthält explizite Szenen.'
+};
