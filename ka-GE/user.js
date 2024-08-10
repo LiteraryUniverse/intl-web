7058ebeaf350b700157b59d6a412ef2934618ae4
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:21:36 +0200

    New translations user.js (Georgian)

--- ka-GE/user.js
+++ ka-GE/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'მეგობრებში დამატება',
-  'user.friend.requests': `გაქვთ {num, number} ახალი მეგობრის {num, plural,
+    'user.friend.request.add': 'მეგობრებში დამატება',
+    'user.friend.requests': `გაქვთ {num, number} ახალი მეგობრის {num, plural,
     zero {მოთხოვნა}
     one {მოთხოვნები}
     many {მოთხოვნა}
     other {მოთხოვნები}
   }`,
-  'user.friend.unfriend': 'მეგობრობის წაშლა',
-  'user.block': 'ბლოკირება',
-  'user.unblock': 'ბლოკის მოხსნა',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'მეგობრობის წაშლა',
+    'user.block': 'ბლოკირება',
+    'user.unblock': 'ბლოკის მოხსნა',
+    'user.joined': `{gender, select,
     male {შემდეგია}
     female {შემდეგია}
     other {შემდეგია}} შემდეგია {date, date, long}-ზე`,
-  'user.friend.request.accept': 'მეგობრობის მოთხოვნის მიღება',
-  'user.friend.request.cancel': 'მეგობრობის მოთხოვნის გაუქმება',
-  'user.friend.request.deny': 'მეგობრობის უარყოფა',
-  'user.listing': 'მომხმარებლების სია',
-  'user.profile.visit': 'მომხმარებლის პროფილის ნახვა {user}',
-  'user.profile.avatar': '{username}-ის ავატარი',
-  'user.friend.list.requests': 'მეგობრობის მოთხოვნები',
-  'user.friend.norequests': 'არცერთი მოთხოვნა მეგობრობისთვის არ გაქვთ.',
-  'user.dashboardWelcome': `გამარჯობა {dayPart, select,
+    'user.friend.request.accept': 'მეგობრობის მოთხოვნის მიღება',
+    'user.friend.request.cancel': 'მეგობრობის მოთხოვნის გაუქმება',
+    'user.friend.request.deny': 'მეგობრობის უარყოფა',
+    'user.listing': 'მომხმარებლების სია',
+    'user.profile.visit': 'მომხმარებლის პროფილის ნახვა {user}',
+    'user.profile.avatar': '{username}-ის ავატარი',
+    'user.friend.list.requests': 'მეგობრობის მოთხოვნები',
+    'user.friend.norequests': 'არცერთი მოთხოვნა მეგობრობისთვის არ გაქვთ.',
+    'user.dashboardWelcome': `გამარჯობა {dayPart, select,
     morning {დილის}
     afternoon {ნაშუადღევის}
     evening {საღამოს}
     night {ღამე}
     other {დღე}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'ამ ავტორის გამოქვეყნება მოხდა ტრადიციულ გზაზე',
-  'user.verification.luAuthor': 'ლიტერატურული უნივერსუმის მიხედვით ავტორი',
-  'user.verification.luEmployee': 'ლიტერატურული უნივერსუმის თამაშების თანამგზავრი',
-  'user.verification.publicFigure': 'გამომხსენებელი საჯარო პირადი',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'ამ ავტორის გამოქვეყნება მოხდა ტრადიციულ გზაზე',
+    'user.verification.luAuthor': 'ლიტერატურული უნივერსუმის მიხედვით ავტორი',
+    'user.verification.luEmployee': 'ლიტერატურული უნივერსუმის თამაშების თანამგზავრი',
+    'user.verification.publicFigure': 'გამომხსენებელი საჯარო პირადი',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {ისტორია}
     other {ისტორიები}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {უნივერსიტეტი}
     other {უნივერსიტეტები}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {ფან ფიქშენი}
     other {ფან ფიქშენები} 
   }`,
-  'profile.works': 'ნამუშევარები',
-  // As in creative/literary works
-  'profile.wishlist': 'სასურველი სია',
-  'profile.storiesBy': 'ისტორიები, შექმნილია {username}',
-  'profile.universesBy': 'უნივერსიტეტები, შექმნილია {username}',
-  'profile.fanFictionBy': 'ფან ფიქშენები, შექმნილია {username}',
-}
+    'profile.works': 'ნამუშევარები',
+    // As in creative/literary works
+    'profile.wishlist': 'სასურველი სია',
+    'profile.storiesBy': 'ისტორიები, შექმნილია {username}',
+    'profile.universesBy': 'უნივერსიტეტები, შექმნილია {username}',
+    'profile.fanFictionBy': 'ფან ფიქშენები, შექმნილია {username}'
+};
