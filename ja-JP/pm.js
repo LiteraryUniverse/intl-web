a4fa4b9ef0455a06798a096b8883749ca0e1b94c
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:20:52 +0200

    New translations pm.js (Japanese)

--- ja-JP/pm.js
+++ ja-JP/pm.js
@@ -1,23 +1,23 @@
 export default {
-  'pm.conversation': '会話',
-  'pm.showolder': '古いメッセージを表示',
-  'pm.errors.saysomething': '何かご意見をお願いします。',
-  'pm.errors.addrecipient': 'ユーザーを少なくともお一人追加してください。',
-  'pm.message': 'メッセージ',
-  'pm.send.message': 'メッセージを送信',
-  'pm.messages.own': 'あなたのメッセージ',
-  'pm.conversations.none': '現在、誰とも会話をしていない状態です。',
-  'pm.to': '受取人',
-  'pm.with': '～と会話',
-  'pm.send': '送信',
-  'pm.conversations.unreadCount': `未読の会話{count, plural,
+    'pm.conversation': '会話',
+    'pm.showolder': '古いメッセージを表示',
+    'pm.errors.saysomething': '何かご意見をお願いします。',
+    'pm.errors.addrecipient': 'ユーザーを少なくともお一人追加してください。',
+    'pm.message': 'メッセージ',
+    'pm.send.message': 'メッセージを送信',
+    'pm.messages.own': 'あなたのメッセージ',
+    'pm.conversations.none': '現在、誰とも会話をしていない状態です。',
+    'pm.to': '受取人',
+    'pm.with': '～と会話',
+    'pm.send': '送信',
+    'pm.conversations.unreadCount': `未読の会話{count, plural,
     zero {はありません}
     other {が#件あります}
   }`,
-  'pm.form.minimumParticipants': '受信者を選択してください。',
-  'pm.form.maxParticipants': `1つの会話に最大{maxPeople, plural,
+    'pm.form.minimumParticipants': '受信者を選択してください。',
+    'pm.form.maxParticipants': `1つの会話に最大{maxPeople, plural,
     other {#}
   }人まで参加できます。`,
-  'pm.new.aria': '新しい会話',
-  'pm.new.buttonTitle': '新しい会話を始める',
-}
+    'pm.new.aria': '新しい会話',
+    'pm.new.buttonTitle': '新しい会話を始める'
+};
