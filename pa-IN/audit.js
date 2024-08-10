97f4ad8f015a933f9cc7cc55923151d8ca50be63
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:24:17 +0200

    New translations audit.js (Punjabi)

--- pa-IN/audit.js
+++ pa-IN/audit.js
@@ -1,6 +1,6 @@
 export default {
-  'auditLog.header.date': 'ਮਿਤੀ',
-  'auditLog.header.action': 'ਕੀ',
-  'auditLog.header.group': 'ਗਰੁੱਪ',
-  'auditLog.header.user': 'ਕੌਣ',
-}
+    'auditLog.header.date': 'ਮਿਤੀ',
+    'auditLog.header.action': 'ਕੀ',
+    'auditLog.header.group': 'ਗਰੁੱਪ',
+    'auditLog.header.user': 'ਕੌਣ'
+};
