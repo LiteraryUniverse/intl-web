160921043ba5535ffb52d9690f0be0d3a111511c
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:39:25 +0200

    New translations audit.js (Assamese)

--- as-IN/audit.js
+++ as-IN/audit.js
@@ -1,6 +1,6 @@
 export default {
-  'auditLog.header.date': 'তারিখ',
-  'auditLog.header.action': 'কি',
-  'auditLog.header.group': 'গোষ্ঠী',
-  'auditLog.header.user': 'কে',
-}
+    'auditLog.header.date': 'তারিখ',
+    'auditLog.header.action': 'কি',
+    'auditLog.header.group': 'গোষ্ঠী',
+    'auditLog.header.user': 'কে'
+};
