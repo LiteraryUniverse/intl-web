43e35966f31cd2386e0c2875a174c3f4d3e0338a
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:33:53 +0200

    New translations library.js (Estonian)

--- et-EE/library.js
+++ et-EE/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'Lisa raamatukokku',
-  'library.remove': 'Eemalda raamatukogust',
-  'library.bought': 'Ostetud lood',
-  'library.stories': 'Jälgitud lood',
-  'library.universes': 'Jälgitud universumid',
-  'bookmark.continueReading': 'Jätka lugemist',
-  'bookmark.statusSummary': 'Oled lugenud {readPercent, number, ::percent} lugu.',
-}
+    'library.add': 'Lisa raamatukokku',
+    'library.remove': 'Eemalda raamatukogust',
+    'library.bought': 'Ostetud lood',
+    'library.stories': 'Jälgitud lood',
+    'library.universes': 'Jälgitud universumid',
+    'bookmark.continueReading': 'Jätka lugemist',
+    'bookmark.statusSummary': 'Oled lugenud {readPercent, number, ::percent} lugu.'
+};
