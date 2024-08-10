06892ba49217c0fc52c8bd32f380d3bc273a448d
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:39:28 +0200

    New translations library.js (Assamese)

--- as-IN/library.js
+++ as-IN/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'লাইব্ৰেৰীত যোগ কৰক',
-  'library.remove': 'লাইব্ৰেৰীৰ পৰা আঁতৰাওক',
-  'library.bought': 'কিনা গল্পসমূহ',
-  'library.stories': 'অনুসরণ কৰা গল্পসমূহ',
-  'library.universes': 'অনুসরণ কৰা বৃহৎমন্ডলীসমূহ',
-  'bookmark.continueReading': 'পঢ়ি থাকক',
-  'bookmark.statusSummary': 'আপুনি গল্পৰ পৰা {readPercent, number, ::percent} পঢ়িছে।',
-}
+    'library.add': 'লাইব্ৰেৰীত যোগ কৰক',
+    'library.remove': 'লাইব্ৰেৰীৰ পৰা আঁতৰাওক',
+    'library.bought': 'কিনা গল্পসমূহ',
+    'library.stories': 'অনুসরণ কৰা গল্পসমূহ',
+    'library.universes': 'অনুসরণ কৰা বৃহৎমন্ডলীসমূহ',
+    'bookmark.continueReading': 'পঢ়ি থাকক',
+    'bookmark.statusSummary': 'আপুনি গল্পৰ পৰা {readPercent, number, ::percent} পঢ়িছে।'
+};
