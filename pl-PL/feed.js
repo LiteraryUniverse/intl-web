98238235982761acc85788d192fa9e04ef946594
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:24:37 +0200

    New translations feed.js (Polish)

--- pl-PL/feed.js
+++ pl-PL/feed.js
@@ -1,9 +1,9 @@
 export default {
-  'feed.name': 'Strumień',
-  'feed.post.new': 'Nowy post',
-  'feed.post.like': 'Nowy post',
-  'feed.post.unlike': 'W odróżnieniu od poczty',
-  'feed.post.send': 'Post',
-  'feed.empty': 'Brak wpisów w streame.',
-  'feed.placeholder': 'Co Ci chodzi po głowie?',
-}
+    'feed.name': 'Strumień',
+    'feed.post.new': 'Nowy post',
+    'feed.post.like': 'Nowy post',
+    'feed.post.unlike': 'W odróżnieniu od poczty',
+    'feed.post.send': 'Post',
+    'feed.empty': 'Brak wpisów w streame.',
+    'feed.placeholder': 'Co Ci chodzi po głowie?'
+};
