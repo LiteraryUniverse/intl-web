62ec631a86692b26b3608b6429ba3a39dacc0ee9
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:25:25 +0200

    New translations comments.js (Portuguese)

--- pt-PT/comments.js
+++ pt-PT/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': 'Escreva o seu comentário',
-  'comments.none': 'Desculpe, não existem comentários a mostrar.',
-  'comments.total': `{count, plural,
+    'comment.write': 'Escreva o seu comentário',
+    'comments.none': 'Desculpe, não existem comentários a mostrar.',
+    'comments.total': `{count, plural,
     =0 {Não existem comentários}
     one {Existe um comentário}
     other {Existem # comentários}
   }.`,
-  'comments.show.older': 'Mostrar comentários mais antigos',
-}
+    'comments.show.older': 'Mostrar comentários mais antigos'
+};
