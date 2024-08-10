7230e29b32b4f370fdedb21b28abac0c06e0415b
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:31:08 +0200

    New translations user.js (Khmer)

--- km-KH/user.js
+++ km-KH/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'បន្ថែម​ទៅ​ក្រុម​មិត្តភក្តិ',
-  'user.friend.requests': `អ្នកមាន {num, number} ការស្នាក់ស្រុកថ្មី {num, plural,
+    'user.friend.request.add': 'បន្ថែម​ទៅ​ក្រុម​មិត្តភក្តិ',
+    'user.friend.requests': `អ្នកមាន {num, number} ការស្នាក់ស្រុកថ្មី {num, plural,
     zero {សំណើរសុំ}
     one {ការស្នាក់ស្រុក}
     many {សំណើរសុំ}
     other {ការស្នាក់ស្រុក}
   }`,
-  'user.friend.unfriend': 'ផ្អាកចុះឈ្មោះពី​ក្រុម​មិត្តភក្តិ',
-  'user.block': 'រារាំង',
-  'user.unblock': 'ដោះ​រារាំង',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'ផ្អាកចុះឈ្មោះពី​ក្រុម​មិត្តភក្តិ',
+    'user.block': 'រារាំង',
+    'user.unblock': 'ដោះ​រារាំង',
+    'user.joined': `{gender, select,
     male {ចូលរួមជាមួយ}
     female {ចូលរួមជាមួយ}
     other {ចូលរួមជាមួយ}} នៅលើ {date, date, long}`,
-  'user.friend.request.accept': 'ទទួល​ការ​ស្នើ​រស់​មិត្តភក្តិ',
-  'user.friend.request.cancel': 'បោះបង់​ការ​ស្នើ​រស់​មិត្តភក្តិ',
-  'user.friend.request.deny': 'បដិសេធការ​ស្នើ​រស់​មិត្តភក្តិ',
-  'user.listing': 'បញ្ជី​អ្នក​ប្រើប្រាស់',
-  'user.profile.visit': 'ទស្សនាកិច្ច​របស់ {user}',
-  'user.profile.avatar': 'រូបភាព​របស់ {username}',
-  'user.friend.list.requests': 'ការ​ស្នើ​រស់​មិត្តភក្តិ',
-  'user.friend.norequests': 'អ្នក​មិន​មាន​ការ​ស្នើ​រស់​មិត្តភក្តិ​ទេ',
-  'user.dashboardWelcome': `អរគុណអត្ថបទសុខភាព {dayPart, select,
+    'user.friend.request.accept': 'ទទួល​ការ​ស្នើ​រស់​មិត្តភក្តិ',
+    'user.friend.request.cancel': 'បោះបង់​ការ​ស្នើ​រស់​មិត្តភក្តិ',
+    'user.friend.request.deny': 'បដិសេធការ​ស្នើ​រស់​មិត្តភក្តិ',
+    'user.listing': 'បញ្ជី​អ្នក​ប្រើប្រាស់',
+    'user.profile.visit': 'ទស្សនាកិច្ច​របស់ {user}',
+    'user.profile.avatar': 'រូបភាព​របស់ {username}',
+    'user.friend.list.requests': 'ការ​ស្នើ​រស់​មិត្តភក្តិ',
+    'user.friend.norequests': 'អ្នក​មិន​មាន​ការ​ស្នើ​រស់​មិត្តភក្តិ​ទេ',
+    'user.dashboardWelcome': `អរគុណអត្ថបទសុខភាព {dayPart, select,
     morning {អរគុណលោកអ្នក}
     afternoon {អរគុណពាក្យក្រុង}
     evening {អរគុណលោកអ្នក}
     night {អរគុណពាក្យក្រុង}
     other {អរគុណលោកអ្នក}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'អ្នកនិពន្ធនេះបានបោះពុម្ពផ្សាយជារួចហើយ',
-  'user.verification.luAuthor': 'អ្នកនិពន្ធនេះជាអ្នកនិពន្ធដែលមានគំនិតពិសេសនៅលើជីវិតអប្សរា',
-  'user.verification.luEmployee': 'និយោជិករបស់ជីវិតអប្សរា',
-  'user.verification.publicFigure': 'ទោលនឹងការបង្ហាញជាអ្នកដែលមានការបោះពុម្ពសារសង្ខេប',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'អ្នកនិពន្ធនេះបានបោះពុម្ពផ្សាយជារួចហើយ',
+    'user.verification.luAuthor': 'អ្នកនិពន្ធនេះជាអ្នកនិពន្ធដែលមានគំនិតពិសេសនៅលើជីវិតអប្សរា',
+    'user.verification.luEmployee': 'និយោជិករបស់ជីវិតអប្សរា',
+    'user.verification.publicFigure': 'ទោលនឹងការបង្ហាញជាអ្នកដែលមានការបោះពុម្ពសារសង្ខេប',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {រឿង}
     other {រឿង}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {សង្គម}
     other {សង្គម}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {រឿងចម្រៀងអ្នកចម្រៀង}
     other {រឿងចម្រៀងអ្នកចម្រៀង} 
   }`,
-  'profile.works': 'បទបង្ហាញ',
-  // As in creative/literary works
-  'profile.wishlist': 'បញ្ជីចង់ទិញ',
-  'profile.storiesBy': 'រឿងដែលបានបង្កើតដោយ {username}',
-  'profile.universesBy': 'សង្គមដែលបានបង្កើតដោយ {username}',
-  'profile.fanFictionBy': 'រឿងចម្រៀងអ្នកចម្រៀងដែលបានបង្កើតដោយ {username}',
-}
+    'profile.works': 'បទបង្ហាញ',
+    // As in creative/literary works
+    'profile.wishlist': 'បញ្ជីចង់ទិញ',
+    'profile.storiesBy': 'រឿងដែលបានបង្កើតដោយ {username}',
+    'profile.universesBy': 'សង្គមដែលបានបង្កើតដោយ {username}',
+    'profile.fanFictionBy': 'រឿងចម្រៀងអ្នកចម្រៀងដែលបានបង្កើតដោយ {username}'
+};
