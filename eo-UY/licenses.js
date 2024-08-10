680878ce48ae811720ec71235d2e63158dbb6180
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:35:55 +0200

    New translations licenses.js (Esperanto)

--- eo-UY/licenses.js
+++ eo-UY/licenses.js
@@ -3,31 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'Pri permesiloj',
-  'licenses.more': 'Plia informo',
-  'licenses.legal': 'Juridika dokumento',
-  'licenses.name.cc-by': 'Krea Komuna Atribuite 4.0',
-  'licenses.cc-by':
-    'Ĉi tiu permesilo permesas al aliaj distribui, remiksi, ŝanĝi kaj konstrui sur via verko, eĉ komerce, dum ili kreditas vin pri la originala kreo. Tio estas la plej akomodema el la ofertataj permesiloj. Rekomendata por maksimuma disvastigo kaj uzo de permesitaj materialoj.',
-  'licenses.name.cc-by-sa': 'Krea Komuna Atribuite-Kompartebla 4.0',
-  'licenses.cc-by-sa':
-    'Ĉi tiu permesilo permesas al aliaj remiksi, ŝanĝi kaj konstrui sur via verko eĉ por komercaj celoj, dum ili kreditas vin kaj permesas siajn novajn kreadojn laŭ la samaj kondiĉoj. Tiu permesilo ofte estas komparata kun “koplefta” libera kaj malferma fonta programaro. Ĉiuj novaj verkoj bazitaj sur via verko portos la saman permesilon, do ĉiuj derivitaj verkoj ankaŭ permesos komercan uzon. Tio estas la permesilo uzata de Vikipedio, kaj estas rekomendata por materialoj, kiuj havus avantaĝon el enkorpigado de enhavo de Vikipedio kaj simile permesitaj projektoj.',
-  'licenses.name.cc-by-nd': 'Krea Komuna Atribuite-Sen-Derivaĵoj 4.0',
-  'licenses.cc-by-nd':
-    'Ĉi tiu permesilo ebligas redistribuadon, komercan kaj nekomercan, dum ĝi estas transdonata sen ŝanĝiĝi kaj en tutaĵo, kun kredito al vi.',
-  'licenses.name.cc-by-nc': 'Krea Komuna Atribuite-Ne-Komerca 4.0',
-  'licenses.cc-by-nc':
-    'Ĉi tiu permesilo permesas al aliaj remiksi, ŝanĝi kaj konstrui sur via verko nekomerce, kaj kvankam iliaj novaj verkoj devas ankaŭ rekonigi vin kaj esti nekomercaj, ili ne devas permesi siajn derivitajn verkojn laŭ la samaj kondiĉoj.',
-  'licenses.name.cc-by-nc-sa': 'Krea Komuna Atribuite-Ne-Komerca-Kompartebla 4.0',
-  'licenses.cc-by-nc-sa':
-    'Ĉi tiu permesilo permesas al aliaj remiksi, ŝanĝi kaj konstrui sur via verko nekomerce, dum ili kreditas vin kaj permesas siajn novajn kreadojn laŭ la samaj kondiĉoj.',
-  'licenses.name.cc-by-nc-nd': 'Krea Komuna Atribuite-Ne-Komerca-Sen-Derivaĵoj 4.0',
-  'licenses.cc-by-nc-nd':
-    'Ĉi tiu permesilo estas la plej limiga el niaj ses ĉefaj permesiloj, permesante nur al aliaj elŝuti viajn verkojn kaj dividi ilin kun aliaj, dum ili kreditas vin, sed ili ne povas ŝanĝi ilin aŭ uzi ilin komerce.',
-  'licenses.name.lu-c': 'Aŭtorrajtoj de Literatura Universo',
-  'licenses.lu-c':
-    'Norma permesilo kun plenaj kopirajtaj rajtoj, kiu permesas al vi vendi kaj publikigi vian laboron laŭ via plaĉo kaj protekti viajn rajtojn al ĝi. Ĉi tiu permesilo ankaŭ donas al vi la eblon vendi viajn rakontojn en Literatura Universo.',
-  'licenses.name.cc0': 'Neniu Rajto Rezervita - CC0 1.0',
-  'licenses.cc0':
-    'Metu vian laboron en publika domajno. Kontraŭe al la permesiloj de CC, kiuj permesas al kopirajtaj posedantoj elekti el gamo de permesoj dum ili konservas sian kopirajton, CC0 donas al vi ankoraŭ alian eblecon – la eblecon malŝalti kopirajton kaj datumbazan protekton, kaj la ekskluzivajn rajtojn aŭtomate donitajn al kreintoj – la “neniu rajto rezervita” alternativo al niaj permesiloj.',
-}
+    'licenses.about': 'Pri permesiloj',
+    'licenses.more': 'Plia informo',
+    'licenses.legal': 'Juridika dokumento',
+    'licenses.name.cc-by': 'Krea Komuna Atribuite 4.0',
+    'licenses.cc-by': 'Ĉi tiu permesilo permesas al aliaj distribui, remiksi, ŝanĝi kaj konstrui sur via verko, eĉ komerce, dum ili kreditas vin pri la originala kreo. Tio estas la plej akomodema el la ofertataj permesiloj. Rekomendata por maksimuma disvastigo kaj uzo de permesitaj materialoj.',
+    'licenses.name.cc-by-sa': 'Krea Komuna Atribuite-Kompartebla 4.0',
+    'licenses.cc-by-sa': 'Ĉi tiu permesilo permesas al aliaj remiksi, ŝanĝi kaj konstrui sur via verko eĉ por komercaj celoj, dum ili kreditas vin kaj permesas siajn novajn kreadojn laŭ la samaj kondiĉoj. Tiu permesilo ofte estas komparata kun “koplefta” libera kaj malferma fonta programaro. Ĉiuj novaj verkoj bazitaj sur via verko portos la saman permesilon, do ĉiuj derivitaj verkoj ankaŭ permesos komercan uzon. Tio estas la permesilo uzata de Vikipedio, kaj estas rekomendata por materialoj, kiuj havus avantaĝon el enkorpigado de enhavo de Vikipedio kaj simile permesitaj projektoj.',
+    'licenses.name.cc-by-nd': 'Krea Komuna Atribuite-Sen-Derivaĵoj 4.0',
+    'licenses.cc-by-nd': 'Ĉi tiu permesilo ebligas redistribuadon, komercan kaj nekomercan, dum ĝi estas transdonata sen ŝanĝiĝi kaj en tutaĵo, kun kredito al vi.',
+    'licenses.name.cc-by-nc': 'Krea Komuna Atribuite-Ne-Komerca 4.0',
+    'licenses.cc-by-nc': 'Ĉi tiu permesilo permesas al aliaj remiksi, ŝanĝi kaj konstrui sur via verko nekomerce, kaj kvankam iliaj novaj verkoj devas ankaŭ rekonigi vin kaj esti nekomercaj, ili ne devas permesi siajn derivitajn verkojn laŭ la samaj kondiĉoj.',
+    'licenses.name.cc-by-nc-sa': 'Krea Komuna Atribuite-Ne-Komerca-Kompartebla 4.0',
+    'licenses.cc-by-nc-sa': 'Ĉi tiu permesilo permesas al aliaj remiksi, ŝanĝi kaj konstrui sur via verko nekomerce, dum ili kreditas vin kaj permesas siajn novajn kreadojn laŭ la samaj kondiĉoj.',
+    'licenses.name.cc-by-nc-nd': 'Krea Komuna Atribuite-Ne-Komerca-Sen-Derivaĵoj 4.0',
+    'licenses.cc-by-nc-nd': 'Ĉi tiu permesilo estas la plej limiga el niaj ses ĉefaj permesiloj, permesante nur al aliaj elŝuti viajn verkojn kaj dividi ilin kun aliaj, dum ili kreditas vin, sed ili ne povas ŝanĝi ilin aŭ uzi ilin komerce.',
+    'licenses.name.lu-c': 'Aŭtorrajtoj de Literatura Universo',
+    'licenses.lu-c': 'Norma permesilo kun plenaj kopirajtaj rajtoj, kiu permesas al vi vendi kaj publikigi vian laboron laŭ via plaĉo kaj protekti viajn rajtojn al ĝi. Ĉi tiu permesilo ankaŭ donas al vi la eblon vendi viajn rakontojn en Literatura Universo.',
+    'licenses.name.cc0': 'Neniu Rajto Rezervita - CC0 1.0',
+    'licenses.cc0': 'Metu vian laboron en publika domajno. Kontraŭe al la permesiloj de CC, kiuj permesas al kopirajtaj posedantoj elekti el gamo de permesoj dum ili konservas sian kopirajton, CC0 donas al vi ankoraŭ alian eblecon – la eblecon malŝalti kopirajton kaj datumbazan protekton, kaj la ekskluzivajn rajtojn aŭtomate donitajn al kreintoj – la “neniu rajto rezervita” alternativo al niaj permesiloj.'
+};
