2c4f502461030163a0c9c9646fbdb0035549eea0
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:15:01 +0200

    New translations enrollment.js (Czech)

--- cs-CZ/enrollment.js
+++ cs-CZ/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'Získejte svůj účet!',
-  'enrollment.description':
-    'Vítej {username}! Pro získání přístupu k Vašemu speciálnímu účtu potřebujete jenom vytvořit heslo.',
-  'enrollment.submitting': 'Připravujeme Váš účet...',
-  'enrollment.submit': 'Získat účet!',
-}
+    'enrollment.title': 'Získejte svůj účet!',
+    'enrollment.description': 'Vítej {username}! Pro získání přístupu k Vašemu speciálnímu účtu potřebujete jenom vytvořit heslo.',
+    'enrollment.submitting': 'Připravujeme Váš účet...',
+    'enrollment.submit': 'Získat účet!'
+};
