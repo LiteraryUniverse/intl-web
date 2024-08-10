e6b687ee562ec410063331c69443e9e9c7b96b29
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:28:47 +0200

    New translations comments.js (Chinese Traditional)

--- zh-TW/comments.js
+++ zh-TW/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': '撰寫你的評論',
-  'comments.none': '抱歉，沒有評論可以顯示。',
-  'comments.total': `There {count, plural,
+    'comment.write': '撰寫你的評論',
+    'comments.none': '抱歉，沒有評論可以顯示。',
+    'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-  'comments.show.older': '顯示較早的評論',
-}
+    'comments.show.older': '顯示較早的評論'
+};
