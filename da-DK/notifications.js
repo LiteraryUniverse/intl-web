066570610be186f6d66f95791cf790b7687dcadc
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:15:58 +0200

    New translations notifications.js (Danish)

--- da-DK/notifications.js
+++ da-DK/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'Start med at vise denne besked kl.',
   'flashnews.endsAt': 'Stop med at vise denne besked kl.',
   'flashnews.newLanguage': 'Vælg nyt sprog at tilføje',
-  'flashnews.onlyDisplayOn':
-    'Nyheden vises kun på de valgte sprog nedenfor (hvis nogen er valgt), der vil ikke vises nogen nyheder, selv på standardsproget, på andre sprog.',
+  'flashnews.onlyDisplayOn': 'Nyheden vises kun på de valgte sprog nedenfor (hvis nogen er valgt), der vil ikke vises nogen nyheder, selv på standardsproget, på andre sprog.',
   'notifications.menu.all': 'Alle notifikationer',
   'notifications.menu.following': 'Følgende',
   'notifications.menu.workshop': 'Værksted',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'Marker som læst',
   'notifications.menu.delete': 'Slet notifikation',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'Din historie {storyName} er automatisk blevet sat på pause efter 6 måneders inaktivitet.',
-  'notification.workshopStoryStatus.toCancelled':
-    'Din historie {storyName} er automatisk blevet afbrudt efter et års inaktivitet.',
+  'notification.workshopStoryStatus.toHiatus': 'Din historie {storyName} er automatisk blevet sat på pause efter 6 måneders inaktivitet.',
+  'notification.workshopStoryStatus.toCancelled': 'Din historie {storyName} er automatisk blevet afbrudt efter et års inaktivitet.',
   'notification.followingStoryStatus.toHiatus': '{storyName} er på pause.',
   'notification.followingStoryStatus.toCancelled': '{storyName} er blevet afbrudt.',
   'notification.followingNewChapter.new': 'Nyt kapitel i {storyName} er blevet udgivet!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'har sendt dig en venneanmodning.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Gå til historie',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'Gå til historie'
+};
