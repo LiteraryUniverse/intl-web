40b6eebe4967f8653828d7ca85e879251e1e4c72
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:28:48 +0200

    New translations legal.js (Chinese Traditional)

--- zh-TW/legal.js
+++ zh-TW/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    '很抱歉打擾您。但在讓您繼續之前，我們有法律義務確保您知道並同意我們的服務條款、隱私政策和版權協議。',
-  'legal.modal.agree': '我同意',
-  'legal.modal.disagree': '我不同意',
-  'legal.modal.register': '如果您註冊，您將不再看到這個。',
-  'legal.modal.goToSettings': '前往您的設定',
-  'legal.modal.oneLastThing': '最後一件事...',
-  'legal.modal.finish': '完成',
-  'legal.modal.existingUser': '已經註冊？',
-  'legal.modal.pleaseLogin': '請登入。',
-  'legal.publicationAgreements': '出版協議',
-  'legal.generalPublishingAgreement': '一般出版協議',
-  'legal.documentsFor': '法律文件給 {workTitle}',
-}
+    'legal.modal.intro': '很抱歉打擾您。但在讓您繼續之前，我們有法律義務確保您知道並同意我們的服務條款、隱私政策和版權協議。',
+    'legal.modal.agree': '我同意',
+    'legal.modal.disagree': '我不同意',
+    'legal.modal.register': '如果您註冊，您將不再看到這個。',
+    'legal.modal.goToSettings': '前往您的設定',
+    'legal.modal.oneLastThing': '最後一件事...',
+    'legal.modal.finish': '完成',
+    'legal.modal.existingUser': '已經註冊？',
+    'legal.modal.pleaseLogin': '請登入。',
+    'legal.publicationAgreements': '出版協議',
+    'legal.generalPublishingAgreement': '一般出版協議',
+    'legal.documentsFor': '法律文件給 {workTitle}'
+};
