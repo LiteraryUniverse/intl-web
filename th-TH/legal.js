eeae628f4b3973470347231a46f2308ccde58b06
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:33:19 +0200

    New translations legal.js (Thai)

--- th-TH/legal.js
+++ th-TH/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'ขออภัยที่ขัดจังหวะครับ ก่อนที่เราจะให้คุณไปต่อไปอีก คุณต้องยอมรับว่าคุณทราบและเห็นด้วยกับข้อกำหนดการให้บริการ นโยบายความเป็นส่วนตัว และข้อตกลงลิขสิทธิ์ของเรา',
-  'legal.modal.agree': 'ฉันยอมรับ',
-  'legal.modal.disagree': 'ฉันไม่ยอมรับ',
-  'legal.modal.register': 'คุณจะไม่เห็นข้อความนี้อีกหากคุณลงทะเบียนแล้ว',
-  'legal.modal.goToSettings': 'ไปที่การตั้งค่าของคุณ',
-  'legal.modal.oneLastThing': 'อีกอย่างสุดท้าย...',
-  'legal.modal.finish': 'เสร็จสิ้น',
-  'legal.modal.existingUser': 'เคยลงทะเบียนแล้วหรือยัง?',
-  'legal.modal.pleaseLogin': 'กรุณาเข้าสู่ระบบ',
-  'legal.publicationAgreements': 'ข้อตกลงการเผยแพร่',
-  'legal.generalPublishingAgreement': 'ข้อตกลงการเผยแพร่ทั่วไป',
-  'legal.documentsFor': 'เอกสารทางกฎหมายสำหรับ {workTitle}',
-}
+    'legal.modal.intro': 'ขออภัยที่ขัดจังหวะครับ ก่อนที่เราจะให้คุณไปต่อไปอีก คุณต้องยอมรับว่าคุณทราบและเห็นด้วยกับข้อกำหนดการให้บริการ นโยบายความเป็นส่วนตัว และข้อตกลงลิขสิทธิ์ของเรา',
+    'legal.modal.agree': 'ฉันยอมรับ',
+    'legal.modal.disagree': 'ฉันไม่ยอมรับ',
+    'legal.modal.register': 'คุณจะไม่เห็นข้อความนี้อีกหากคุณลงทะเบียนแล้ว',
+    'legal.modal.goToSettings': 'ไปที่การตั้งค่าของคุณ',
+    'legal.modal.oneLastThing': 'อีกอย่างสุดท้าย...',
+    'legal.modal.finish': 'เสร็จสิ้น',
+    'legal.modal.existingUser': 'เคยลงทะเบียนแล้วหรือยัง?',
+    'legal.modal.pleaseLogin': 'กรุณาเข้าสู่ระบบ',
+    'legal.publicationAgreements': 'ข้อตกลงการเผยแพร่',
+    'legal.generalPublishingAgreement': 'ข้อตกลงการเผยแพร่ทั่วไป',
+    'legal.documentsFor': 'เอกสารทางกฎหมายสำหรับ {workTitle}'
+};
