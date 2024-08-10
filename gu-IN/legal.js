a5e4148693635658fb9ade7c1618b5ccf4b99689
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:18:45 +0200

    New translations legal.js (Gujarati)

--- gu-IN/legal.js
+++ gu-IN/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'ખેડામાં વિરામ આપવામાં આવ્યા છીએ. પરંતુ અમને આપને આગળ વધવા આપતા પહેલાં, અમારા સેવાની શરતો, ગોપનીયતા નીતિ અને કૉપિરાઇટ ચુકવણી સાથે આપ અવગણી કરેલ છે કે નહીં તે ખાસ કરીને જાણ કરવાની મજબૂરી છે.',
-  'legal.modal.agree': 'હાં માન્ય છું',
-  'legal.modal.disagree': 'હું અસંમત છું',
-  'legal.modal.register': 'જો તમે નોંધણી કરો છો તો આગળ આપને આ નહીં જોઈશ.',
-  'legal.modal.goToSettings': 'તમારા સેટિંગ્સ પર જાઓ',
-  'legal.modal.oneLastThing': 'એક છેલ્લી વાત...',
-  'legal.modal.finish': 'પૂર્ણ કરો',
-  'legal.modal.existingUser': 'પહેલેથી નોંધાયેલ છે?',
-  'legal.modal.pleaseLogin': 'કૃપા કરીને લોગઈન કરો.',
-  'legal.publicationAgreements': 'પ્રકાશન ચુકવણીઓ',
-  'legal.generalPublishingAgreement': 'સામાન્ય પ્રકાશન ચુકવણી',
-  'legal.documentsFor': 'કાયદાસંબંધીત દસ્તાવેજો માટે {workTitle}',
-}
+    'legal.modal.intro': 'ખેડામાં વિરામ આપવામાં આવ્યા છીએ. પરંતુ અમને આપને આગળ વધવા આપતા પહેલાં, અમારા સેવાની શરતો, ગોપનીયતા નીતિ અને કૉપિરાઇટ ચુકવણી સાથે આપ અવગણી કરેલ છે કે નહીં તે ખાસ કરીને જાણ કરવાની મજબૂરી છે.',
+    'legal.modal.agree': 'હાં માન્ય છું',
+    'legal.modal.disagree': 'હું અસંમત છું',
+    'legal.modal.register': 'જો તમે નોંધણી કરો છો તો આગળ આપને આ નહીં જોઈશ.',
+    'legal.modal.goToSettings': 'તમારા સેટિંગ્સ પર જાઓ',
+    'legal.modal.oneLastThing': 'એક છેલ્લી વાત...',
+    'legal.modal.finish': 'પૂર્ણ કરો',
+    'legal.modal.existingUser': 'પહેલેથી નોંધાયેલ છે?',
+    'legal.modal.pleaseLogin': 'કૃપા કરીને લોગઈન કરો.',
+    'legal.publicationAgreements': 'પ્રકાશન ચુકવણીઓ',
+    'legal.generalPublishingAgreement': 'સામાન્ય પ્રકાશન ચુકવણી',
+    'legal.documentsFor': 'કાયદાસંબંધીત દસ્તાવેજો માટે {workTitle}'
+};
