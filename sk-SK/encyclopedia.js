cf24ef5aee643d16415cb7b0e0b972a93f3bb15e
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:26:17 +0200

    New translations encyclopedia.js (Slovak)

--- sk-SK/encyclopedia.js
+++ sk-SK/encyclopedia.js
@@ -1,44 +1,42 @@
 export default {
-  'encyclopedia.add': 'Pridať nový záznam',
-  'encyclopedia.browse': 'Prehľadávanie encyklopédie',
-  'encyclopedia.search': 'Vyhľadávanie v encyklopédii',
-  'encyclopedia.back.universe': 'Späť na stránku vesmíru',
-  'encyclopedia.back.workshop': 'Späť na dielňu',
-  'encyclopedia.categories': 'Kategórie',
-  'encyclopedia.groups': 'Skupiny',
-  'encyclopedia.category.character': 'Postavy',
-  'encyclopedia.category.event': 'Udalosti',
-  'encyclopedia.category.group': 'Skupiny',
-  'encyclopedia.category.item': 'Položky',
-  'encyclopedia.category.location': 'Lokality',
-  'encyclopedia.category.other': 'Ostatné',
-  'encyclopedia.category.state': 'Štáty',
-  'encyclopedia.category.governments': 'Vlády',
-  'encyclopedia.category.languages': 'Jazyky',
-  'encyclopedia.category.creatures': 'Stvorenia',
-  'encyclopedia.category.deity': 'Božstvá',
-  'encyclopedia.category.religions': 'Náboženstvá',
-  'encyclopedia.category.flora': 'Flora',
-  'encyclopedia.category.magic': 'Magia',
-  'encyclopedia.category.planets': 'Planéty',
-  'encyclopedia.category.species': 'Druhy',
-  'encyclopedia.category.technologies': 'Technológie',
-  'encyclopedia.search.found': `{results, plural,
+    'encyclopedia.add': 'Pridať nový záznam',
+    'encyclopedia.browse': 'Prehľadávanie encyklopédie',
+    'encyclopedia.search': 'Vyhľadávanie v encyklopédii',
+    'encyclopedia.back.universe': 'Späť na stránku vesmíru',
+    'encyclopedia.back.workshop': 'Späť na dielňu',
+    'encyclopedia.categories': 'Kategórie',
+    'encyclopedia.groups': 'Skupiny',
+    'encyclopedia.category.character': 'Postavy',
+    'encyclopedia.category.event': 'Udalosti',
+    'encyclopedia.category.group': 'Skupiny',
+    'encyclopedia.category.item': 'Položky',
+    'encyclopedia.category.location': 'Lokality',
+    'encyclopedia.category.other': 'Ostatné',
+    'encyclopedia.category.state': 'Štáty',
+    'encyclopedia.category.governments': 'Vlády',
+    'encyclopedia.category.languages': 'Jazyky',
+    'encyclopedia.category.creatures': 'Stvorenia',
+    'encyclopedia.category.deity': 'Božstvá',
+    'encyclopedia.category.religions': 'Náboženstvá',
+    'encyclopedia.category.flora': 'Flora',
+    'encyclopedia.category.magic': 'Magia',
+    'encyclopedia.category.planets': 'Planéty',
+    'encyclopedia.category.species': 'Druhy',
+    'encyclopedia.category.technologies': 'Technológie',
+    'encyclopedia.search.found': `{results, plural,
     =0 {Žiadne súvisiace položky}
     one {Nájdené 1 súvisiace položky}
     few {Nájdené # súvisiace položky}
     other {Nájdených # súvisiacich položiek}
   }.`,
-  'encyclopedia.help.text':
-    'Na navigáciu medzi položkami encyklopédie použite menu na ľavej strane. Po výbere položky sa zobrazí namiesto tohto textu.',
-  'encyclopedia.help.sidebar': 'Súbor nástrojov bočného panela',
-  'encyclopedia.help.sidebar.text':
-    'Vitajte vo svojom príbehu! Tento bočný panel vám umožňuje jednoduchý prístup ku všetkým zdrojom súvisiacim s vaším príbehom. Zaškrtnutím ikon vyššie si môžete pozrieť najnovšie komentáre, svoje poznámky, vyhľadávať vo svojej encyklopédii, prechádzať a pridávať nové záznamy do svojej encyklopédie. Ak chcete pridať pripojenie k záznamu z textu príbehu, vyberte text, kam ho chcete umiestniť, a potom kliknite na tlačidlo pripojiť. Následne budete vyzvaní na vyhľadanie a výber položky.',
-  'encyclopedia.intro': 'Vitajte v encyklopédii {universe}.',
-  'encyclopedia.add.title': 'Pridanie nového záznamu do encyklopédie',
-  'encyclopedia.add.heading': 'Čo chcete pridať?',
-  'encyclopedia.add.moresoon': 'Ďalšie kategórie už čoskoro!',
-  'encyclopedia.create': `Vytvorte nov{category, select,
+    'encyclopedia.help.text': 'Na navigáciu medzi položkami encyklopédie použite menu na ľavej strane. Po výbere položky sa zobrazí namiesto tohto textu.',
+    'encyclopedia.help.sidebar': 'Súbor nástrojov bočného panela',
+    'encyclopedia.help.sidebar.text': 'Vitajte vo svojom príbehu! Tento bočný panel vám umožňuje jednoduchý prístup ku všetkým zdrojom súvisiacim s vaším príbehom. Zaškrtnutím ikon vyššie si môžete pozrieť najnovšie komentáre, svoje poznámky, vyhľadávať vo svojej encyklopédii, prechádzať a pridávať nové záznamy do svojej encyklopédie. Ak chcete pridať pripojenie k záznamu z textu príbehu, vyberte text, kam ho chcete umiestniť, a potom kliknite na tlačidlo pripojiť. Následne budete vyzvaní na vyhľadanie a výber položky.',
+    'encyclopedia.intro': 'Vitajte v encyklopédii {universe}.',
+    'encyclopedia.add.title': 'Pridanie nového záznamu do encyklopédie',
+    'encyclopedia.add.heading': 'Čo chcete pridať?',
+    'encyclopedia.add.moresoon': 'Ďalšie kategórie už čoskoro!',
+    'encyclopedia.create': `Vytvorte nov{category, select,
     character {ú postavu}
     event {ú udalosť}
     group {ú skupinu}
@@ -57,7 +55,7 @@ export default {
     species {ý druh}
     technologies {ú technológiu}
   }`,
-  'encyclopedia.create.header': `Vytvorenie nov{category, select,
+    'encyclopedia.create.header': `Vytvorenie nov{category, select,
     character {ú postavu}
     event {ú udalosť}
     group {ú skupinu}
@@ -76,7 +74,7 @@ export default {
     species {ý druh}
     technologies {ú technológiu}
   } v {universe} vesmíre`,
-  'encyclopedia.entry.category': `Kategória záznamu: {category, select,
+    'encyclopedia.entry.category': `Kategória záznamu: {category, select,
     character {Postava}
     event {Udalosť}
     group {Skupina}
@@ -95,80 +93,73 @@ export default {
     species {Druh}
     technologies {Technológia}
   }`,
-  'encyclopedia.entry.category.legend': 'Kategória',
-  'encyclopedia.fields.add': 'Pridať nové pole',
-  'encyclopedia.fields.text': 'Definícia',
-  'encyclopedia.fields.textarea': 'Text',
-  'encyclopedia.fields.connection': 'Pripojenie',
-  'encyclopedia.fields.time': 'Čas',
-  'encyclopedia.fields.image': 'Obrázok',
-  'encyclopedia.visibility': 'Viditeľnosť',
-  'encyclopedia.public': 'Verejné',
-  'encyclopedia.private': 'Súkromné',
-  'encyclopedia.container': 'Kontajner',
-  'encyclopedia.container.desc':
-    'Vytvorte z tejto položky kontajner pre novú skupinu položiek. Tým sa skryje zo zoznamu kategórií.',
-  'encyclopedia.entry.name': 'Názov položky',
-  'encyclopedia.summary': 'Zhrnutie',
-  'encyclopedia.summary.desc':
-    'Toto je zhrnutie, ktoré bude jedinou úvodnou informáciou zobrazenou na čítačke.',
-  'encyclopedia.parent': 'Rodičovský záznam',
-  'encyclopedia.related': 'Súvisiace záznamy',
-  'encyclopedia.errors.tags': 'Na jednu položku môžete mať len 10 značiek.',
-  'encyclopedia.field.private': 'Súkromné pole',
-  'encyclopedia.ft.indev': 'Táto možnosť je vo vývoji.',
-  'encyclopedia.connections.remove': 'Odstránenie pripojenia',
-  'encyclopedia.connections.extra': 'Ďalšie informácie',
-  'encyclopedia.connections.existing': 'Existujúca položka',
-  'encyclopedia.connections.existing.abbr': 'EP',
-  // abbreviation for Existing Entry
-  'encyclopedia.cover': 'Obrázok na obálke',
-  'encyclopedia.translation.title': 'Vitajte v preklade vesmíru {universe}!',
-  'encyclopedia.translation.desc':
-    'Toto encyklopedické rozhranie sa používa na preklad do {language}. Vyberte existujúce položky v zozname na ľavej strane a začnite ich preklad.',
-  'encyclopedia.welcome.totalEntries': `V tomto vesmíre {totalEntries, plural,
+    'encyclopedia.entry.category.legend': 'Kategória',
+    'encyclopedia.fields.add': 'Pridať nové pole',
+    'encyclopedia.fields.text': 'Definícia',
+    'encyclopedia.fields.textarea': 'Text',
+    'encyclopedia.fields.connection': 'Pripojenie',
+    'encyclopedia.fields.time': 'Čas',
+    'encyclopedia.fields.image': 'Obrázok',
+    'encyclopedia.visibility': 'Viditeľnosť',
+    'encyclopedia.public': 'Verejné',
+    'encyclopedia.private': 'Súkromné',
+    'encyclopedia.container': 'Kontajner',
+    'encyclopedia.container.desc': 'Vytvorte z tejto položky kontajner pre novú skupinu položiek. Tým sa skryje zo zoznamu kategórií.',
+    'encyclopedia.entry.name': 'Názov položky',
+    'encyclopedia.summary': 'Zhrnutie',
+    'encyclopedia.summary.desc': 'Toto je zhrnutie, ktoré bude jedinou úvodnou informáciou zobrazenou na čítačke.',
+    'encyclopedia.parent': 'Rodičovský záznam',
+    'encyclopedia.related': 'Súvisiace záznamy',
+    'encyclopedia.errors.tags': 'Na jednu položku môžete mať len 10 značiek.',
+    'encyclopedia.field.private': 'Súkromné pole',
+    'encyclopedia.ft.indev': 'Táto možnosť je vo vývoji.',
+    'encyclopedia.connections.remove': 'Odstránenie pripojenia',
+    'encyclopedia.connections.extra': 'Ďalšie informácie',
+    'encyclopedia.connections.existing': 'Existujúca položka',
+    'encyclopedia.connections.existing.abbr': 'EP',
+    // abbreviation for Existing Entry
+    'encyclopedia.cover': 'Obrázok na obálke',
+    'encyclopedia.translation.title': 'Vitajte v preklade vesmíru {universe}!',
+    'encyclopedia.translation.desc': 'Toto encyklopedické rozhranie sa používa na preklad do {language}. Vyberte existujúce položky v zozname na ľavej strane a začnite ich preklad.',
+    'encyclopedia.welcome.totalEntries': `V tomto vesmíre {totalEntries, plural,
     zero {nie sú žiadne záznamy}
     one {je jedna položka}
     few {sú # položky}
 other {je # položiek}
   }.`,
-  'encyclopedia.settings.title': 'Nastavenia encyklopédie',
-  'encyclopedia.settings.categoriesDesc':
-    'Zapnite a vypnite kategórie, ktoré chcete používať v encyklopédii.',
-  'encyclopedia.settings.introText': 'Uvítacia správa, ktorá sa zobrazí po popise vesmíru.',
-  'encyclopedia.fields.placeholder': 'Názov poľa',
-  'encyclopedia.fields.delete': 'Vymazať pole',
-  'encyclopedia.timeOption.fictionalDate': 'Fiktívny dátum',
-  'encyclopedia.timeOption.fictionalTime': 'Fiktívny čas',
-  'encyclopedia.timeOption.fictionalDateTime': 'Fiktívny čas a dátum',
-  'encyclopedia.timeOption.date': 'Dátum',
-  'encyclopedia.timeOption.time': 'Čas',
-  'encyclopedia.timeOption.dateTime': 'Dátum a čas',
-  'encyclopedia.settings.groupByCategories': 'Zahrnúť kategórie do zoznamu skupín',
-  'encyclopedia.menuOpen': 'Otvorenie ponuky Encyklopédia',
-  'encyclopedia.menuClose': 'Zatvoriť ponuku Encyklopédia',
-  'encyclopedia.menuAria': 'Menu encyklopédie',
-  'enc.newField': 'Nové pole',
-  'enc.entries.limit': `V súčasnosti máte{value, number} z {max, plural,
+    'encyclopedia.settings.title': 'Nastavenia encyklopédie',
+    'encyclopedia.settings.categoriesDesc': 'Zapnite a vypnite kategórie, ktoré chcete používať v encyklopédii.',
+    'encyclopedia.settings.introText': 'Uvítacia správa, ktorá sa zobrazí po popise vesmíru.',
+    'encyclopedia.fields.placeholder': 'Názov poľa',
+    'encyclopedia.fields.delete': 'Vymazať pole',
+    'encyclopedia.timeOption.fictionalDate': 'Fiktívny dátum',
+    'encyclopedia.timeOption.fictionalTime': 'Fiktívny čas',
+    'encyclopedia.timeOption.fictionalDateTime': 'Fiktívny čas a dátum',
+    'encyclopedia.timeOption.date': 'Dátum',
+    'encyclopedia.timeOption.time': 'Čas',
+    'encyclopedia.timeOption.dateTime': 'Dátum a čas',
+    'encyclopedia.settings.groupByCategories': 'Zahrnúť kategórie do zoznamu skupín',
+    'encyclopedia.menuOpen': 'Otvorenie ponuky Encyklopédia',
+    'encyclopedia.menuClose': 'Zatvoriť ponuku Encyklopédia',
+    'encyclopedia.menuAria': 'Menu encyklopédie',
+    'enc.newField': 'Nové pole',
+    'enc.entries.limit': `V súčasnosti máte{value, number} z {max, plural,
     =0 {neobmedzený počet položiek}
     one {{max, number} položiek}
     few {{max, number} položiek}
     many {{max, number} položiek}
     other {{max, number} položiek}
   }.`,
-  'enc.entries.limitReached':
-    'Dosiahli ste maximálny povolený počet záznamov v encyklopédii pre aktuálne predplatné.',
-  'enc.menuControls.ariaDesc': 'Menu ovládania encyklopédie',
-  'encyclopedia.pronunciation.title': 'Výslovnosť',
-  'encyclopedia.wip.abbreviation': 'WIP',
-  'encyclopedia.wip.full': 'Práca vo vývoji',
-  'encyclopedia.settings.templateLegend': 'Šablónový vesmír',
-  'encyclopedia.settings.templateDescription':
-    'Povoliť kopírovanie obsahu tejto encyklopédie do iných vesmírov. Toto je najlepšie, ak nemáte žiadne príbehy a namiesto toho len vytvárate šablónu pre ostatných.',
-  'encyclopedia.settings.templateCheckbox': 'Toto je šablónový vesmír',
-  'encyclopedia.templateCopy.modalTitle': 'Kopírovať obsah encyklopédie',
-  'encyclopedia.templateCopy.description':
-    'Skopírujte obsah tejto encyklopédie do vášho vesmíru, aby ste ho obohatili.',
-  'encyclopedia.templateCopy.targetUniverse': 'Vesmír, do ktorého sa má skopírovať',
-  'encyclopedia.templateCopy.finished': 'Kopírovanie dokončené',
-}
+    'enc.entries.limitReached': 'Dosiahli ste maximálny povolený počet záznamov v encyklopédii pre aktuálne predplatné.',
+    'enc.menuControls.ariaDesc': 'Menu ovládania encyklopédie',
+    'encyclopedia.pronunciation.title': 'Výslovnosť',
+    'encyclopedia.wip.abbreviation': 'WIP',
+    'encyclopedia.wip.full': 'Práca vo vývoji',
+    'encyclopedia.settings.templateLegend': 'Šablónový vesmír',
+    'encyclopedia.settings.templateDescription': 'Povoliť kopírovanie obsahu tejto encyklopédie do iných vesmírov. Toto je najlepšie, ak nemáte žiadne príbehy a namiesto toho len vytvárate šablónu pre ostatných.',
+    'encyclopedia.settings.templateCheckbox': 'Toto je šablónový vesmír',
+    'encyclopedia.templateCopy.modalTitle': 'Kopírovať obsah encyklopédie',
+    'encyclopedia.templateCopy.description': 'Skopírujte obsah tejto encyklopédie do vášho vesmíru, aby ste ho obohatili.',
+    'encyclopedia.templateCopy.targetUniverse': 'Vesmír, do ktorého sa má skopírovať',
+    'encyclopedia.templateCopy.finished': 'Kopírovanie dokončené'
+};
