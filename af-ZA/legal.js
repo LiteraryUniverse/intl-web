92f2439092cd0674f9484224fc2d9c3bc833389d
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:13:36 +0200

    New translations legal.js (Afrikaans)

--- af-ZA/legal.js
+++ af-ZA/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Jammer om te onderbreek. Maar voordat ons jou verder laat gaan, is ons wettig verplig om seker te maak dat jy bewus is van en saamstem met ons Diensvoorwaardes, Privaatheidsbeleid en Kopieregsooreenkoms.',
-  'legal.modal.agree': 'Ek Stem Saam',
-  'legal.modal.disagree': 'Ek Stem Nie Saam',
-  'legal.modal.register': 'Jy sal dit nie weer sien as jy registreer nie.',
-  'legal.modal.goToSettings': 'Gaan na jou instellings',
-  'legal.modal.oneLastThing': 'Een laaste ding...',
-  'legal.modal.finish': 'Voltooi',
-  'legal.modal.existingUser': 'Reeds geregistreer?',
-  'legal.modal.pleaseLogin': 'Maak asseblief aanmelding.',
-  'legal.publicationAgreements': 'Publikasie-ooreenkomste',
-  'legal.generalPublishingAgreement': 'Algemene Publikasie-ooreenkoms',
-  'legal.documentsFor': 'Regsdokumente vir {workTitle}',
-}
+    'legal.modal.intro': 'Jammer om te onderbreek. Maar voordat ons jou verder laat gaan, is ons wettig verplig om seker te maak dat jy bewus is van en saamstem met ons Diensvoorwaardes, Privaatheidsbeleid en Kopieregsooreenkoms.',
+    'legal.modal.agree': 'Ek Stem Saam',
+    'legal.modal.disagree': 'Ek Stem Nie Saam',
+    'legal.modal.register': 'Jy sal dit nie weer sien as jy registreer nie.',
+    'legal.modal.goToSettings': 'Gaan na jou instellings',
+    'legal.modal.oneLastThing': 'Een laaste ding...',
+    'legal.modal.finish': 'Voltooi',
+    'legal.modal.existingUser': 'Reeds geregistreer?',
+    'legal.modal.pleaseLogin': 'Maak asseblief aanmelding.',
+    'legal.publicationAgreements': 'Publikasie-ooreenkomste',
+    'legal.generalPublishingAgreement': 'Algemene Publikasie-ooreenkoms',
+    'legal.documentsFor': 'Regsdokumente vir {workTitle}'
+};
