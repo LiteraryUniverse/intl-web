50192526e90a16dcd063f4c6fe17f88f8650f71b
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:23:44 +0200

    New translations audit.js (Norwegian)

--- no-NO/audit.js
+++ no-NO/audit.js
@@ -1,6 +1,6 @@
 export default {
-  'auditLog.header.date': 'Dato',
-  'auditLog.header.action': 'Hva',
-  'auditLog.header.group': 'Gruppe',
-  'auditLog.header.user': 'Hvem',
-}
+    'auditLog.header.date': 'Dato',
+    'auditLog.header.action': 'Hva',
+    'auditLog.header.group': 'Gruppe',
+    'auditLog.header.user': 'Hvem'
+};
