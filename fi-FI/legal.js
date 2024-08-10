de879ff5406b0993bd3ac828c25246c9a6e7045f
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:17:38 +0200

    New translations legal.js (Finnish)

--- fi-FI/legal.js
+++ fi-FI/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Pahoittelut keskeytyksestä. Mutta ennen kuin päästämme sinut etenemään, meidän on laillisesti varmistettava, että olet tietoinen ja hyväksyt palveluehtomme, tietosuojakäytäntömme ja tekijänoikeussopimuksemme.',
-  'legal.modal.agree': 'Hyväksyn',
-  'legal.modal.disagree': 'En hyväksy',
-  'legal.modal.register': 'Et näe tätä enää, jos rekisteröidyt.',
-  'legal.modal.goToSettings': 'Siirry asetuksiisi',
-  'legal.modal.oneLastThing': 'Viimeinen asia...',
-  'legal.modal.finish': 'Valmis',
-  'legal.modal.existingUser': 'Jo rekisteröitynyt?',
-  'legal.modal.pleaseLogin': 'Kirjaudu sisään.',
-  'legal.publicationAgreements': 'Julkaisusopimukset',
-  'legal.generalPublishingAgreement': 'Yleinen julkaisusopimus',
-  'legal.documentsFor': 'Oikeudelliset asiakirjat {workTitle}',
-}
+    'legal.modal.intro': 'Pahoittelut keskeytyksestä. Mutta ennen kuin päästämme sinut etenemään, meidän on laillisesti varmistettava, että olet tietoinen ja hyväksyt palveluehtomme, tietosuojakäytäntömme ja tekijänoikeussopimuksemme.',
+    'legal.modal.agree': 'Hyväksyn',
+    'legal.modal.disagree': 'En hyväksy',
+    'legal.modal.register': 'Et näe tätä enää, jos rekisteröidyt.',
+    'legal.modal.goToSettings': 'Siirry asetuksiisi',
+    'legal.modal.oneLastThing': 'Viimeinen asia...',
+    'legal.modal.finish': 'Valmis',
+    'legal.modal.existingUser': 'Jo rekisteröitynyt?',
+    'legal.modal.pleaseLogin': 'Kirjaudu sisään.',
+    'legal.publicationAgreements': 'Julkaisusopimukset',
+    'legal.generalPublishingAgreement': 'Yleinen julkaisusopimus',
+    'legal.documentsFor': 'Oikeudelliset asiakirjat {workTitle}'
+};
