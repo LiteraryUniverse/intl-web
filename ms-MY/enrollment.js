f66bf1e65d997662ff5cbcae9478c58e4e678d8c
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:34:43 +0200

    New translations enrollment.js (Malay)

--- ms-MY/enrollment.js
+++ ms-MY/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'Tuntut akaun anda!',
-  'enrollment.description':
-    'Selamat datang {username}! Untuk mendapatkan akses kepada akaun istimewa anda, anda hanya perlu mencipta kata laluan.',
-  'enrollment.submitting': 'Menuntut akaun anda...',
-  'enrollment.submit': 'Tuntut akaun anda!',
-}
+    'enrollment.title': 'Tuntut akaun anda!',
+    'enrollment.description': 'Selamat datang {username}! Untuk mendapatkan akses kepada akaun istimewa anda, anda hanya perlu mencipta kata laluan.',
+    'enrollment.submitting': 'Menuntut akaun anda...',
+    'enrollment.submit': 'Tuntut akaun anda!'
+};
