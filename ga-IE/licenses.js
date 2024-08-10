ee843827ff33128b00841afa613ca67616a19693
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:18:00 +0200

    New translations licenses.js (Irish)

--- ga-IE/licenses.js
+++ ga-IE/licenses.js
@@ -3,31 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'Maidir le ceadúnais',
-  'licenses.more': 'Tuilleadh eolais',
-  'licenses.legal': 'Cáipéis dlíthiúil',
-  'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
-  'licenses.cc-by':
-    'Ligeann an ceadúnas seo do dhaoine eile dáileadh, athchumadh, corraigh agus tógáil ar do shaothar, fiú tráchtáil, ach amháin más tú féin aitheantas don chruthaitheacht bunaidh. Is é seo an ceadúnas is feiliúnach de na ceadúnais atá ar fáil. Moladh é don scaipeadh agus úsáid is mó de na hábhair ceadúnaithe.',
-  'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
-  'licenses.cc-by-sa':
-    'Ligeann an ceadúnas seo do dhaoine eile athchumadh, corraigh agus tógáil ar do shaothar fiú le haghaidh críocha tráchtála, ach amháin más tú féin aitheantas dóibh agus a gcuid cruthaithe nua á n-áisghabháil faoi na téarmaí céanna. Déantar an ceadúnas seo a chomhthéacsáil go minic le ceadúnais bogsaí saor agus oscailte. Beidh gach saothar nua bunaithe ar do shaothar ag díriú ar na céanna téarmaí ceadúnais, mar sin gheobhaidh aon athruithe comharthaíochta ceadúnas tráchtála chomh maith. Is é seo an ceadúnas atá in úsáid ag Vicipéid, agus moltar é do ábhair a mbeadh leas acu as ábhar a chur leis ó Vicipéid agus ó thionscnaimh ceadúnaithe den chineál céanna.',
-  'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
-  'licenses.cc-by-nd':
-    'Ligeann an ceadúnas seo athdháileadh, tráchtáil agus neamhthráchtáil, ach amháin más á chur ar aghaidh gan athrú agus go hiomlán, le haitheantas duit.',
-  'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
-  'licenses.cc-by-nc':
-    'Ligeann an ceadúnas seo do dhaoine eile athchumadh, corraigh agus tógáil ar do shaothar gan críoch tráchtála, agus cé gur gá dóibh aitheantas a thabhairt duit agus a gcuid saothar nua gan críoch tráchtála a cheadúnú faoi na téarmaí céanna.',
-  'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
-  'licenses.cc-by-nc-sa':
-    'Ligeann an ceadúnas seo do dhaoine eile athchumadh, corraigh agus tógáil ar do shaothar gan críoch tráchtála, ach amháin más tú féin aitheantas dóibh agus a gcuid saothar nua á n-áisghabháil faoi na téarmaí céanna.',
-  'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
-  'licenses.cc-by-nc-nd':
-    'Is é seo an ceadúnas is teoranta de na sé chéadúnas príomha atá againn, agus a ligeann do dhaoine eile do shaothair a íoslódáil agus a roinnt le daoine eile ach amháin má thugann siad aitheantas duit, ach ní féidir leo iad a athrú ar bith nó a úsáid go tráchtála.',
-  'licenses.name.lu-c': 'Cearta Cóipcheartú na Cruinne Liteartha',
-  'licenses.lu-c':
-    'Standard full copyrights license that allows you to sell publish your work as you like and have your rights to it protected. This license also gives you the option to sell your stories on Literary Universe.',
-  'licenses.name.cc0': 'No Rights Reserved - CC0 1.0',
-  'licenses.cc0':
-    'Put your work into public domain. In contrast to CC’s licenses that allow copyright holders to choose from a range of permissions while retaining their copyright, CC0 empowers yet another choice altogether – the choice to opt out of copyright and database protection, and the exclusive rights automatically granted to creators – the “no rights reserved” alternative to our licenses.',
-}
+    'licenses.about': 'Maidir le ceadúnais',
+    'licenses.more': 'Tuilleadh eolais',
+    'licenses.legal': 'Cáipéis dlíthiúil',
+    'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
+    'licenses.cc-by': 'Ligeann an ceadúnas seo do dhaoine eile dáileadh, athchumadh, corraigh agus tógáil ar do shaothar, fiú tráchtáil, ach amháin más tú féin aitheantas don chruthaitheacht bunaidh. Is é seo an ceadúnas is feiliúnach de na ceadúnais atá ar fáil. Moladh é don scaipeadh agus úsáid is mó de na hábhair ceadúnaithe.',
+    'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
+    'licenses.cc-by-sa': 'Ligeann an ceadúnas seo do dhaoine eile athchumadh, corraigh agus tógáil ar do shaothar fiú le haghaidh críocha tráchtála, ach amháin más tú féin aitheantas dóibh agus a gcuid cruthaithe nua á n-áisghabháil faoi na téarmaí céanna. Déantar an ceadúnas seo a chomhthéacsáil go minic le ceadúnais bogsaí saor agus oscailte. Beidh gach saothar nua bunaithe ar do shaothar ag díriú ar na céanna téarmaí ceadúnais, mar sin gheobhaidh aon athruithe comharthaíochta ceadúnas tráchtála chomh maith. Is é seo an ceadúnas atá in úsáid ag Vicipéid, agus moltar é do ábhair a mbeadh leas acu as ábhar a chur leis ó Vicipéid agus ó thionscnaimh ceadúnaithe den chineál céanna.',
+    'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
+    'licenses.cc-by-nd': 'Ligeann an ceadúnas seo athdháileadh, tráchtáil agus neamhthráchtáil, ach amháin más á chur ar aghaidh gan athrú agus go hiomlán, le haitheantas duit.',
+    'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
+    'licenses.cc-by-nc': 'Ligeann an ceadúnas seo do dhaoine eile athchumadh, corraigh agus tógáil ar do shaothar gan críoch tráchtála, agus cé gur gá dóibh aitheantas a thabhairt duit agus a gcuid saothar nua gan críoch tráchtála a cheadúnú faoi na téarmaí céanna.',
+    'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
+    'licenses.cc-by-nc-sa': 'Ligeann an ceadúnas seo do dhaoine eile athchumadh, corraigh agus tógáil ar do shaothar gan críoch tráchtála, ach amháin más tú féin aitheantas dóibh agus a gcuid saothar nua á n-áisghabháil faoi na téarmaí céanna.',
+    'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
+    'licenses.cc-by-nc-nd': 'Is é seo an ceadúnas is teoranta de na sé chéadúnas príomha atá againn, agus a ligeann do dhaoine eile do shaothair a íoslódáil agus a roinnt le daoine eile ach amháin má thugann siad aitheantas duit, ach ní féidir leo iad a athrú ar bith nó a úsáid go tráchtála.',
+    'licenses.name.lu-c': 'Cearta Cóipcheartú na Cruinne Liteartha',
+    'licenses.lu-c': 'Standard full copyrights license that allows you to sell publish your work as you like and have your rights to it protected. This license also gives you the option to sell your stories on Literary Universe.',
+    'licenses.name.cc0': 'No Rights Reserved - CC0 1.0',
+    'licenses.cc0': 'Put your work into public domain. In contrast to CC’s licenses that allow copyright holders to choose from a range of permissions while retaining their copyright, CC0 empowers yet another choice altogether – the choice to opt out of copyright and database protection, and the exclusive rights automatically granted to creators – the “no rights reserved” alternative to our licenses.'
+};
