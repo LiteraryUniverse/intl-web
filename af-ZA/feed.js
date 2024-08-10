a0c62759b9d461be09d6c8e6759db87ac708563c
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:13:21 +0200

    New translations feed.js (Afrikaans)

--- af-ZA/feed.js
+++ af-ZA/feed.js
@@ -1,9 +1,9 @@
 export default {
-  'feed.name': 'Voer',
-  'feed.post.new': 'Nuwe pos',
-  'feed.post.like': 'Hou van pos',
-  'feed.post.unlike': 'Moenie van pos hou nie',
-  'feed.post.send': 'Plasing',
-  'feed.empty': 'Geen inskrywings in die voer nie.',
-  'feed.placeholder': 'Wat dink jy?',
-}
+    'feed.name': 'Voer',
+    'feed.post.new': 'Nuwe pos',
+    'feed.post.like': 'Hou van pos',
+    'feed.post.unlike': 'Moenie van pos hou nie',
+    'feed.post.send': 'Plasing',
+    'feed.empty': 'Geen inskrywings in die voer nie.',
+    'feed.placeholder': 'Wat dink jy?'
+};
