af5997c62554b54533779f6b7b29b68c60fe0758
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:14:03 +0200

    New translations pm.js (Arabic)

--- ar-SA/pm.js
+++ ar-SA/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'المحادثة',
-  'pm.showolder': 'عرض رسائل أقدم',
-  'pm.errors.saysomething': 'يجب أن تقول شيئًا حقًا...',
-  'pm.errors.addrecipient': 'يجب عليك إضافة مستخدم واحد على الأقل.',
-  'pm.message': 'رسالة',
-  'pm.send.message': 'إرسال رسالة',
-  'pm.messages.own': 'رسائلك',
-  'pm.conversations.none': 'أنت لا تتحدث حاليًا مع أي شخص.',
-  'pm.to': 'إلى:',
-  'pm.with': 'محادثة مع:',
-  'pm.send': 'إرسال',
-  'pm.conversations.unreadCount': `ليس لديك {count, plural,
+    'pm.conversation': 'المحادثة',
+    'pm.showolder': 'عرض رسائل أقدم',
+    'pm.errors.saysomething': 'يجب أن تقول شيئًا حقًا...',
+    'pm.errors.addrecipient': 'يجب عليك إضافة مستخدم واحد على الأقل.',
+    'pm.message': 'رسالة',
+    'pm.send.message': 'إرسال رسالة',
+    'pm.messages.own': 'رسائلك',
+    'pm.conversations.none': 'أنت لا تتحدث حاليًا مع أي شخص.',
+    'pm.to': 'إلى:',
+    'pm.with': 'محادثة مع:',
+    'pm.send': 'إرسال',
+    'pm.conversations.unreadCount': `ليس لديك {count, plural,
     zero {محادثات غير مقروءة}
     one {محادثة غير مقروءة واحدة}
     other {محادثات غير مقروءة #}
   }.`,
-  'pm.form.minimumParticipants': 'يجب عليك اختيار مستلم.',
-  'pm.form.maxParticipants': `يمكنك أن تكون لديك الحد الأقصى من {maxPeople, plural,
+    'pm.form.minimumParticipants': 'يجب عليك اختيار مستلم.',
+    'pm.form.maxParticipants': `يمكنك أن تكون لديك الحد الأقصى من {maxPeople, plural,
     one {شخص واحد}
     other {# أشخاص}
   } في محادثة.`,
-  'pm.new.aria': 'محادثة جديدة',
-  'pm.new.buttonTitle': 'بدء محادثة جديدة',
-}
+    'pm.new.aria': 'محادثة جديدة',
+    'pm.new.buttonTitle': 'بدء محادثة جديدة'
+};
