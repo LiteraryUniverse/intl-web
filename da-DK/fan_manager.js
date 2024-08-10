50c1c7f84fc24996c9b628950722be4da1844345
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:16:01 +0200

    New translations fan_manager.js (Danish)

--- da-DK/fan_manager.js
+++ da-DK/fan_manager.js
@@ -10,15 +10,17 @@ export default {
   'fanManager.state.rejected': 'Afslået',
   'fanManager.state.hidden': 'Skjult',
   'fanManager.publicComment': 'Offentlig kommentar',
-  'fanManager.publicComment.desc':
-    'Denne tekst vil blive vist på den offentlige side som din officielle kommentar til denne kunst.',
+  'fanManager.publicComment.desc': 'Denne tekst vil blive vist på den offentlige side som din officielle kommentar til denne kunst.',
   'fanManager.authorComment': 'Kommentar til forfatteren',
-  'fanManager.authorComment.desc':
-    'Dette er en privat kommentar fra dig til forfatteren af denne kunst.',
+  'fanManager.authorComment.desc': 'Dette er en privat kommentar fra dig til forfatteren af denne kunst.',
   'fanManager.stateChange.approve': 'Godkend fan-kunst',
   'fanManager.stateChange.reject': 'Afvis fan-kunst',
   'fanManager.stateChange.hide': 'Skjul fan-kunst',
   'fanManager.stateChange.unhide': 'Gør fan-kunst synlig',
   'fanManager.fanFictionNotAllowed': 'Fan-fiktion er ikke tilladt på grund af din licens.',
   'fanManager.fanFiction.maxRating': 'Maksimum bedømmelse af vist fan-fiktion.',
-}
+  'fanManager.hidden.nothingFound': 'Ingen skjulte fan-kunstværker fundet.',
+  'fanManager.rejected.nothingFound': 'Ingen afviste fan-kunstværker fundet.',
+  'fanManager.approved.nothingFound': 'Ingen godkendte fan-kunstværker fundet.',
+  'fanManager.submitted.nothingFound': 'Ingen indsendte fan-arts fundet.'
+};
