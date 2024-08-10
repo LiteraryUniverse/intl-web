4c6c16205affd1bcb246bfd03cc4e47f85751a85
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:18:49 +0200

    New translations user.js (Gujarati)

--- gu-IN/user.js
+++ gu-IN/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'મિત્રોમાં ઉમેરો',
-  'user.friend.requests': `તમારે {num, number} નવા મિત્ર {num, plural,
+    'user.friend.request.add': 'મિત્રોમાં ઉમેરો',
+    'user.friend.requests': `તમારે {num, number} નવા મિત્ર {num, plural,
     zero {માટેની વિનંતીઓ}
     one {વિનંતી}
     many {વિનંતીઓ}
     other {વિનંતીઓ}
   }`,
-  'user.friend.unfriend': 'મિત્રતા તોડી નાંખો',
-  'user.block': 'બ્લોક કરો',
-  'user.unblock': 'અનબ્લોક કરો',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'મિત્રતા તોડી નાંખો',
+    'user.block': 'બ્લોક કરો',
+    'user.unblock': 'અનબ્લોક કરો',
+    'user.joined': `{gender, select,
     male {ને}
     female {પર જોડાયેલી છે}
     other {પર જોડાયેલી છે}} પર જોડાયેલી છે {date, date, long}પર જોડાયેલી છે!`,
-  'user.friend.request.accept': 'મિત્રતાની વિનંતી સ્વીકારો',
-  'user.friend.request.cancel': 'મિત્રતાની વિનંતી રદ કરો',
-  'user.friend.request.deny': 'મિત્રતા નકારો',
-  'user.listing': 'વપરાશકર્તાઓની યાદી',
-  'user.profile.visit': 'મુલાકાત લો {user}ની પ્રોફાઇલ પર.',
-  'user.profile.avatar': '{username}નો એવાટાર',
-  'user.friend.list.requests': 'મિત્રતાની વિનંતીઓ',
-  'user.friend.norequests': 'તમારી મિત્રતાની વિનંતીઓ નથી.',
-  'user.dashboardWelcome': `સવાર {dayPart, select,
+    'user.friend.request.accept': 'મિત્રતાની વિનંતી સ્વીકારો',
+    'user.friend.request.cancel': 'મિત્રતાની વિનંતી રદ કરો',
+    'user.friend.request.deny': 'મિત્રતા નકારો',
+    'user.listing': 'વપરાશકર્તાઓની યાદી',
+    'user.profile.visit': 'મુલાકાત લો {user}ની પ્રોફાઇલ પર.',
+    'user.profile.avatar': '{username}નો એવાટાર',
+    'user.friend.list.requests': 'મિત્રતાની વિનંતીઓ',
+    'user.friend.norequests': 'તમારી મિત્રતાની વિનંતીઓ નથી.',
+    'user.dashboardWelcome': `સવાર {dayPart, select,
     morning {સવારે}
     afternoon {બપોર}
     evening {સાંજ}
     night {રાત}
     other {દિવસ}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'આ લેખકે પારંપરિક રીતે પ્રકાશિત કર્યું છે',
-  'user.verification.luAuthor': 'સાહિત્યિક વિશ્વમાં આધારિત લેખક',
-  'user.verification.luEmployee': 'સાહિત્યિક વિશ્વ કર્મચારી',
-  'user.verification.publicFigure': 'ચકાસીત સાર્વજનિક આદર્શ',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'આ લેખકે પારંપરિક રીતે પ્રકાશિત કર્યું છે',
+    'user.verification.luAuthor': 'સાહિત્યિક વિશ્વમાં આધારિત લેખક',
+    'user.verification.luEmployee': 'સાહિત્યિક વિશ્વ કર્મચારી',
+    'user.verification.publicFigure': 'ચકાસીત સાર્વજનિક આદર્શ',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {વાર્તા}
     other {વાર્તાઓ}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {વિશ્વ}
     other {વિશ્વો}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {ફેન ફિક્શન}
     other {ફેન ફિક્શનો} 
   }`,
-  'profile.works': 'કાર્યો',
-  // As in creative/literary works
-  'profile.wishlist': 'ઇચ્છાપત્ર',
-  'profile.storiesBy': '{username}દ્વારા બનાવવામાં આવેલી વાર્તાઓ',
-  'profile.universesBy': '{username}દ્વારા બનાવવામાં આવેલા વિશ્વો',
-  'profile.fanFictionBy': '{username}દ્વારા બનાવવામાં આવેલી ફેન ફિક્શનો',
-}
+    'profile.works': 'કાર્યો',
+    // As in creative/literary works
+    'profile.wishlist': 'ઇચ્છાપત્ર',
+    'profile.storiesBy': '{username}દ્વારા બનાવવામાં આવેલી વાર્તાઓ',
+    'profile.universesBy': '{username}દ્વારા બનાવવામાં આવેલા વિશ્વો',
+    'profile.fanFictionBy': '{username}દ્વારા બનાવવામાં આવેલી ફેન ફિક્શનો'
+};
