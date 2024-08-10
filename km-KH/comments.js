f6b0f8e3bc5781efdcbe66d5f28e80f3838f202c
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:31:03 +0200

    New translations comments.js (Khmer)

--- km-KH/comments.js
+++ km-KH/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': 'សរសេរ​មតិយោបល់​របស់​អ្នក',
-  'comments.none': 'សូមអភ័យទោស, គ្មាន​មតិយោបល់​សម្រាប់​បង្ហាញ',
-  'comments.total': `មិនមាន​មតិយោបល់​ទេ {count, plural,
+    'comment.write': 'សរសេរ​មតិយោបល់​របស់​អ្នក',
+    'comments.none': 'សូមអភ័យទោស, គ្មាន​មតិយោបល់​សម្រាប់​បង្ហាញ',
+    'comments.total': `មិនមាន​មតិយោបល់​ទេ {count, plural,
     =0 {មតិយោបល់}
     one {មតិយោបល់មួយ}
     other {មតិយោបល់}
   }`,
-  'comments.show.older': 'បង្ហាញ​មតិយោបល់​ចាស់បំផុត',
-}
+    'comments.show.older': 'បង្ហាញ​មតិយោបល់​ចាស់បំផុត'
+};
