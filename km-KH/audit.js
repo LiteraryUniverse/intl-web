fba0cf249af9a5aa15f9131d924b534dbe6b7516
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:31:02 +0200

    New translations audit.js (Khmer)

--- km-KH/audit.js
+++ km-KH/audit.js
@@ -1,6 +1,6 @@
 export default {
-  'auditLog.header.date': 'កាលបរិច្ឆេទ',
-  'auditLog.header.action': 'អ្វី',
-  'auditLog.header.group': 'ក្រុម',
-  'auditLog.header.user': 'និយាយខ្លួនឯង',
-}
+    'auditLog.header.date': 'កាលបរិច្ឆេទ',
+    'auditLog.header.action': 'អ្វី',
+    'auditLog.header.group': 'ក្រុម',
+    'auditLog.header.user': 'និយាយខ្លួនឯង'
+};
