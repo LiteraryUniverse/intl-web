abcd999dbb99dfa571bed28c50550ce18bfd1d75
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:31:31 +0200

    New translations pm.js (Tamil)

--- ta-IN/pm.js
+++ ta-IN/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'உரையாடல்',
-  'pm.showolder': 'பழைய செய்திகளைக் காட்டு',
-  'pm.errors.saysomething': 'நீங்கள் ஏதாவது சொல்ல வேண்டும்...',
-  'pm.errors.addrecipient': 'நீங்கள் குறைந்ததும் ஒரு பயனரை சேர்க்க வேண்டும்.',
-  'pm.message': 'செய்தி',
-  'pm.send.message': 'செய்தி அனுப்பு',
-  'pm.messages.own': 'உங்கள் செய்திகள்',
-  'pm.conversations.none': 'நீங்கள் தற்போது யாருடனும் உரையாட்டம் செய்யவில்லை.',
-  'pm.to': 'பெயர்க்கு:',
-  'pm.with': 'உரையாடல் உடனாக:',
-  'pm.send': 'அனுப்பு',
-  'pm.conversations.unreadCount': `உங்களுக்கு {count, plural,
+    'pm.conversation': 'உரையாடல்',
+    'pm.showolder': 'பழைய செய்திகளைக் காட்டு',
+    'pm.errors.saysomething': 'நீங்கள் ஏதாவது சொல்ல வேண்டும்...',
+    'pm.errors.addrecipient': 'நீங்கள் குறைந்ததும் ஒரு பயனரை சேர்க்க வேண்டும்.',
+    'pm.message': 'செய்தி',
+    'pm.send.message': 'செய்தி அனுப்பு',
+    'pm.messages.own': 'உங்கள் செய்திகள்',
+    'pm.conversations.none': 'நீங்கள் தற்போது யாருடனும் உரையாட்டம் செய்யவில்லை.',
+    'pm.to': 'பெயர்க்கு:',
+    'pm.with': 'உரையாடல் உடனாக:',
+    'pm.send': 'அனுப்பு',
+    'pm.conversations.unreadCount': `உங்களுக்கு {count, plural,
     zero {வாசிக்கப்படாத உரையாடல்கள்}
     one {ஒரு வாசிக்கப்படாத உரையாடல்}
     other {# வாசிக்கப்படாத உரையாடல்கள்}
   }.`,
-  'pm.form.minimumParticipants': 'நீங்கள் பொருத்தமாக ஒருவரைத் தேர்ந்தெடுக்க வேண்டும்.',
-  'pm.form.maxParticipants': `உங்களுக்கு உச்சத்தில் {maxPeople, plural,
+    'pm.form.minimumParticipants': 'நீங்கள் பொருத்தமாக ஒருவரைத் தேர்ந்தெடுக்க வேண்டும்.',
+    'pm.form.maxParticipants': `உங்களுக்கு உச்சத்தில் {maxPeople, plural,
     one {ஒரு நபர்}
     other {# மக்கள்}
   } உள்ளனர் உள்ளது.`,
-  'pm.new.aria': 'புதிய உரையாடல்',
-  'pm.new.buttonTitle': 'புதிய உரையாடலைத் தொடங்குங்கள்',
-}
+    'pm.new.aria': 'புதிய உரையாடல்',
+    'pm.new.buttonTitle': 'புதிய உரையாடலைத் தொடங்குங்கள்'
+};
