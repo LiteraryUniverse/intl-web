6c88543dcab33973a114ccc37b068698b2830b30
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:27:08 +0200

    New translations legal.js (Swedish)

--- sv-SE/legal.js
+++ sv-SE/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Ursäkta att jag avbryter. Men innan vi låter dig gå vidare är vi juridiskt förpliktade att se till att du känner till och godkänner våra användarvillkor, vår sekretesspolicy och vårt upphovsrättsavtal.',
-  'legal.modal.agree': 'Jag samtycker',
-  'legal.modal.disagree': 'Jag godkänner inte',
-  'legal.modal.register': 'Du kommer inte att se detta längre om du registrerar dig.',
-  'legal.modal.goToSettings': 'Gå till dina inställningar',
-  'legal.modal.oneLastThing': 'En sista sak...',
-  'legal.modal.finish': 'Slutförande',
-  'legal.modal.existingUser': 'Redan registrerad?',
-  'legal.modal.pleaseLogin': 'Vänligen logga in.',
-  'legal.publicationAgreements': 'Publiceringsavtal',
-  'legal.generalPublishingAgreement': 'Allmänt publiceringsavtal',
-  'legal.documentsFor': 'Juridiska dokument för {workTitle}',
-}
+    'legal.modal.intro': 'Ursäkta att jag avbryter. Men innan vi låter dig gå vidare är vi juridiskt förpliktade att se till att du känner till och godkänner våra användarvillkor, vår sekretesspolicy och vårt upphovsrättsavtal.',
+    'legal.modal.agree': 'Jag samtycker',
+    'legal.modal.disagree': 'Jag godkänner inte',
+    'legal.modal.register': 'Du kommer inte att se detta längre om du registrerar dig.',
+    'legal.modal.goToSettings': 'Gå till dina inställningar',
+    'legal.modal.oneLastThing': 'En sista sak...',
+    'legal.modal.finish': 'Slutförande',
+    'legal.modal.existingUser': 'Redan registrerad?',
+    'legal.modal.pleaseLogin': 'Vänligen logga in.',
+    'legal.publicationAgreements': 'Publiceringsavtal',
+    'legal.generalPublishingAgreement': 'Allmänt publiceringsavtal',
+    'legal.documentsFor': 'Juridiska dokument för {workTitle}'
+};
