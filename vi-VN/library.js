204543fc2c70f550beaa1783acc66466008c63df
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:29:23 +0200

    New translations library.js (Vietnamese)

--- vi-VN/library.js
+++ vi-VN/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'Thêm vào thư viện',
-  'library.remove': 'Xóa khỏi thư viện',
-  'library.bought': 'Các truyện đã mua',
-  'library.stories': 'Các truyện đã theo dõi',
-  'library.universes': 'Các vũ trụ đã theo dõi',
-  'bookmark.continueReading': 'Tiếp tục Đọc',
-  'bookmark.statusSummary': 'Bạn đã đọc {readPercent, number, ::percent} của câu chuyện.',
-}
+    'library.add': 'Thêm vào thư viện',
+    'library.remove': 'Xóa khỏi thư viện',
+    'library.bought': 'Các truyện đã mua',
+    'library.stories': 'Các truyện đã theo dõi',
+    'library.universes': 'Các vũ trụ đã theo dõi',
+    'bookmark.continueReading': 'Tiếp tục Đọc',
+    'bookmark.statusSummary': 'Bạn đã đọc {readPercent, number, ::percent} của câu chuyện.'
+};
