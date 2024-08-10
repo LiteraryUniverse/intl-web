50b1c957dab9aa89202d9758095a002f64501d3a
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:17:55 +0200

    New translations enrollment.js (Irish)

--- ga-IE/enrollment.js
+++ ga-IE/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'Gabh do chuntas!',
-  'enrollment.description':
-    'Fáilte {username}! Chun rochtain a fháil ar do chuntas speisialta, níl ort ach pasfhocal a chruthú.',
-  'enrollment.submitting': 'Ag gabháil do chuntais...',
-  'enrollment.submit': 'Gabh do chuntas!',
-}
+    'enrollment.title': 'Gabh do chuntas!',
+    'enrollment.description': 'Fáilte {username}! Chun rochtain a fháil ar do chuntas speisialta, níl ort ach pasfhocal a chruthú.',
+    'enrollment.submitting': 'Ag gabháil do chuntais...',
+    'enrollment.submit': 'Gabh do chuntas!'
+};
