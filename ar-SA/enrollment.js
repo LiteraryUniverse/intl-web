8620a68ffb5706ab0086a09dc8a5e3bf1f899280
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:13:54 +0200

    New translations enrollment.js (Arabic)

--- ar-SA/enrollment.js
+++ ar-SA/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'ادع Ansible account الخاص بك!',
-  'enrollment.description':
-    'مرحبًا {username}! للوصول إلى حسابك الخاص، تحتاج فقط إلى إنشاء كلمة مرور.',
-  'enrollment.submitting': 'جارٍ استرداد حسابك...',
-  'enrollment.submit': 'ادع Ansible account الخاص بك!',
-}
+    'enrollment.title': 'ادع Ansible account الخاص بك!',
+    'enrollment.description': 'مرحبًا {username}! للوصول إلى حسابك الخاص، تحتاج فقط إلى إنشاء كلمة مرور.',
+    'enrollment.submitting': 'جارٍ استرداد حسابك...',
+    'enrollment.submit': 'ادع Ansible account الخاص بك!'
+};
