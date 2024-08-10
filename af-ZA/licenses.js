8098004f4f21bc9db6c2b120877108bfc4e6126d
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:13:25 +0200

    New translations licenses.js (Afrikaans)

--- af-ZA/licenses.js
+++ af-ZA/licenses.js
@@ -3,31 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'Oor lisensies',
-  'licenses.more': 'Meer inligting',
-  'licenses.legal': 'Regsdokument',
-  'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
-  'licenses.cc-by':
-    'Hierdie lisensie stel ander in staat om jou werk te versprei, te verander, aan te pas en daarop voort te bou, selfs vir kommersiële doeleindes, solank hulle jou erken as die oorspronklike skepper. Dit is die mees inskiklike van die lisensies wat aangebied word. Aanbeveel vir die maksimale verspreiding en gebruik van gelisensieerde materiaal.',
-  'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
-  'licenses.cc-by-sa':
-    'Hierdie lisensie stel ander in staat om jou werk te verander, aan te pas en daarop voort te bou, selfs vir kommersiële doeleindes, solank hulle jou erken en hul nuwe skeppings onder dieselfde terme lisensieer. Hierdie lisensie word dikwels vergelyk met “copyleft” vrye en oopbron sagteware lisensies. Alle nuwe werke gebaseer op jou werk sal dieselfde lisensie dra, sodat enige afgeleides ook kommersiële gebruik sal toelaat. Dit is die lisensie wat deur Wikipedia gebruik word en word aanbeveel vir materiaal wat baat sal vind by die inkorporering van inhoud van Wikipedia en soortgelyk gelisensieerde projekte.',
-  'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
-  'licenses.cc-by-nd':
-    'Hierdie lisensie maak dit moontlik om jou werk te herverdeel, kommersieel en nie-kommersieel, solank dit onveranderd en in sy geheel deurgegee word, met erkenning aan jou.',
-  'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
-  'licenses.cc-by-nc':
-    'Hierdie lisensie stel ander in staat om jou werk nie-kommersieel te verander, aan te pas en daarop voort te bou, en hoewel hul nuwe werke jou ook moet erken en nie-kommersieel moet wees, hoef hulle nie hul afgeleide werke onder dieselfde terme te lisensieer nie.',
-  'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
-  'licenses.cc-by-nc-sa':
-    'Hierdie lisensie stel ander in staat om jou werk nie-kommersieel te verander, aan te pas en daarop voort te bou, solank hulle jou erken en hul nuwe skeppings onder dieselfde terme lisensieer.',
-  'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
-  'licenses.cc-by-nc-nd':
-    'Hierdie lisensie is die mees beperkende van ons ses hooflisensies en stel slegs ander in staat om jou werke af te laai en met ander te deel, solank hulle jou erken, maar hulle mag dit nie op enige manier verander of kommersieel gebruik nie.',
-  'licenses.name.lu-c': 'Literary Universe Auteursregte',
-  'licenses.lu-c':
-    'Standaard volle kopiereg lisensie wat jou toelaat om jou werk te verkoop en publiseer soos jy wil en jou regte daaraan beskerm. Hierdie lisensie gee jou ook die opsie om jou stories op Literary Universe te verkoop.',
-  'licenses.name.cc0': 'Geen Regte Voorbehou - CC0 1.0',
-  'licenses.cc0':
-    "Plaas jou werk in die openbare domein. In teenstelling met CC se lisensies wat kopiereghouers toelaat om van 'n verskeidenheid toestemmings te kies terwyl hulle hul kopiereg behou, gee CC0 nog 'n hele ander keuse - die keuse om uit te tree uit kopiereg en databasisbeskerming, en die eksklusiewe regte wat outomaties aan skeppers verleen word - die “geen regte voorbehou”-alternatief tot ons lisensies.",
-}
+    'licenses.about': 'Oor lisensies',
+    'licenses.more': 'Meer inligting',
+    'licenses.legal': 'Regsdokument',
+    'licenses.name.cc-by': 'Creative Commons Attribution 4.0',
+    'licenses.cc-by': 'Hierdie lisensie stel ander in staat om jou werk te versprei, te verander, aan te pas en daarop voort te bou, selfs vir kommersiële doeleindes, solank hulle jou erken as die oorspronklike skepper. Dit is die mees inskiklike van die lisensies wat aangebied word. Aanbeveel vir die maksimale verspreiding en gebruik van gelisensieerde materiaal.',
+    'licenses.name.cc-by-sa': 'Creative Commons Attribution-ShareAlike 4.0',
+    'licenses.cc-by-sa': 'Hierdie lisensie stel ander in staat om jou werk te verander, aan te pas en daarop voort te bou, selfs vir kommersiële doeleindes, solank hulle jou erken en hul nuwe skeppings onder dieselfde terme lisensieer. Hierdie lisensie word dikwels vergelyk met “copyleft” vrye en oopbron sagteware lisensies. Alle nuwe werke gebaseer op jou werk sal dieselfde lisensie dra, sodat enige afgeleides ook kommersiële gebruik sal toelaat. Dit is die lisensie wat deur Wikipedia gebruik word en word aanbeveel vir materiaal wat baat sal vind by die inkorporering van inhoud van Wikipedia en soortgelyk gelisensieerde projekte.',
+    'licenses.name.cc-by-nd': 'Creative Commons Attribution-NoDerivs 4.0',
+    'licenses.cc-by-nd': 'Hierdie lisensie maak dit moontlik om jou werk te herverdeel, kommersieel en nie-kommersieel, solank dit onveranderd en in sy geheel deurgegee word, met erkenning aan jou.',
+    'licenses.name.cc-by-nc': 'Creative Commons Attribution-NonCommercial 4.0',
+    'licenses.cc-by-nc': 'Hierdie lisensie stel ander in staat om jou werk nie-kommersieel te verander, aan te pas en daarop voort te bou, en hoewel hul nuwe werke jou ook moet erken en nie-kommersieel moet wees, hoef hulle nie hul afgeleide werke onder dieselfde terme te lisensieer nie.',
+    'licenses.name.cc-by-nc-sa': 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
+    'licenses.cc-by-nc-sa': 'Hierdie lisensie stel ander in staat om jou werk nie-kommersieel te verander, aan te pas en daarop voort te bou, solank hulle jou erken en hul nuwe skeppings onder dieselfde terme lisensieer.',
+    'licenses.name.cc-by-nc-nd': 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0',
+    'licenses.cc-by-nc-nd': 'Hierdie lisensie is die mees beperkende van ons ses hooflisensies en stel slegs ander in staat om jou werke af te laai en met ander te deel, solank hulle jou erken, maar hulle mag dit nie op enige manier verander of kommersieel gebruik nie.',
+    'licenses.name.lu-c': 'Literary Universe Auteursregte',
+    'licenses.lu-c': 'Standaard volle kopiereg lisensie wat jou toelaat om jou werk te verkoop en publiseer soos jy wil en jou regte daaraan beskerm. Hierdie lisensie gee jou ook die opsie om jou stories op Literary Universe te verkoop.',
+    'licenses.name.cc0': 'Geen Regte Voorbehou - CC0 1.0',
+    'licenses.cc0': 'Plaas jou werk in die openbare domein. In teenstelling met CC se lisensies wat kopiereghouers toelaat om van \'n verskeidenheid toestemmings te kies terwyl hulle hul kopiereg behou, gee CC0 nog \'n hele ander keuse - die keuse om uit te tree uit kopiereg en databasisbeskerming, en die eksklusiewe regte wat outomaties aan skeppers verleen word - die “geen regte voorbehou”-alternatief tot ons lisensies.'
+};
