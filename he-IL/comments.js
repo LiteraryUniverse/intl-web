0163ea9d8f80f43637b2feda3f34bda1f42af8d9
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:19:18 +0200

    New translations comments.js (Hebrew)

--- he-IL/comments.js
+++ he-IL/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': 'כתוב את התגובה שלך',
-  'comments.none': 'מצטערים, אין תגובות להצגה.',
-  'comments.total': `There {count, plural,
+    'comment.write': 'כתוב את התגובה שלך',
+    'comments.none': 'מצטערים, אין תגובות להצגה.',
+    'comments.total': `There {count, plural,
     =0 {are no comments}
     one {is 1 comment}
     other {are # comments}
   }.`,
-  'comments.show.older': 'הצג תגובות ישנות יותר',
-}
+    'comments.show.older': 'הצג תגובות ישנות יותר'
+};
