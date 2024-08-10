586617a36511549625b9f2c53cd0e748375ed244
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:19:55 +0200

    New translations user.js (Hungarian)

--- hu-HU/user.js
+++ hu-HU/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'Hozzáadás barátokhoz',
-  'user.friend.requests': `Van {num, number} új barát {num, plural,
+    'user.friend.request.add': 'Hozzáadás barátokhoz',
+    'user.friend.requests': `Van {num, number} új barát {num, plural,
     zero {kérésed}
     one {kérés}
     many {kérések}
     other {kérések}
   }`,
-  'user.friend.unfriend': 'Barátság megszüntetése',
-  'user.block': 'Blokkolás',
-  'user.unblock': 'Blokkolás feloldása',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'Barátság megszüntetése',
+    'user.block': 'Blokkolás',
+    'user.unblock': 'Blokkolás feloldása',
+    'user.joined': `{gender, select,
     male {csatlakozott}
     female {-hoz}
     other {-hoz}} -hoz {date, date, long}-kor.`,
-  'user.friend.request.accept': 'Barátságkérés elfogadása',
-  'user.friend.request.cancel': 'Barátságkérés visszavonása',
-  'user.friend.request.deny': 'Barátság visszautasítása',
-  'user.listing': 'Felhasználói lista',
-  'user.profile.visit': 'Látogasd meg {user}profilját.',
-  'user.profile.avatar': '{username}profilképe',
-  'user.friend.list.requests': 'Barátságkérések',
-  'user.friend.norequests': 'Nincsenek barátságkéréseid.',
-  'user.dashboardWelcome': `Jó reggelt {dayPart, select,
+    'user.friend.request.accept': 'Barátságkérés elfogadása',
+    'user.friend.request.cancel': 'Barátságkérés visszavonása',
+    'user.friend.request.deny': 'Barátság visszautasítása',
+    'user.listing': 'Felhasználói lista',
+    'user.profile.visit': 'Látogasd meg {user}profilját.',
+    'user.profile.avatar': '{username}profilképe',
+    'user.friend.list.requests': 'Barátságkérések',
+    'user.friend.norequests': 'Nincsenek barátságkéréseid.',
+    'user.dashboardWelcome': `Jó reggelt {dayPart, select,
     morning {jó napot}
     afternoon {jó délutánt}
     evening {jó estét}
     night {jó éjszakát}
     other {szép napot}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'Ez az író hagyományos módon jelent meg',
-  'user.verification.luAuthor': 'Irodalmi Univerzum alapú jegyzett szerző',
-  'user.verification.luEmployee': 'Irodalmi Univerzum alkalmazott',
-  'user.verification.publicFigure': 'Ellenőrzött közszereplő',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'Ez az író hagyományos módon jelent meg',
+    'user.verification.luAuthor': 'Irodalmi Univerzum alapú jegyzett szerző',
+    'user.verification.luEmployee': 'Irodalmi Univerzum alkalmazott',
+    'user.verification.publicFigure': 'Ellenőrzött közszereplő',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {történet}
     other {történetek}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {univerzum}
     other {univerzumok}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {rajongói írás}
     other {rajongói írások} 
   }`,
-  'profile.works': 'Művek',
-  // As in creative/literary works
-  'profile.wishlist': 'Kívánságlista',
-  'profile.storiesBy': '{username}által létrehozott történetek',
-  'profile.universesBy': '{username}által létrehozott univerzumok',
-  'profile.fanFictionBy': '{username}által létrehozott rajongói írások',
-}
+    'profile.works': 'Művek',
+    // As in creative/literary works
+    'profile.wishlist': 'Kívánságlista',
+    'profile.storiesBy': '{username}által létrehozott történetek',
+    'profile.universesBy': '{username}által létrehozott univerzumok',
+    'profile.fanFictionBy': '{username}által létrehozott rajongói írások'
+};
