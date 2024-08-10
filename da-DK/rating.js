a75b48716d7a8ee9f2bd1ca03ac257ffba0cdeb2
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:15:46 +0200

    New translations rating.js (Danish)

--- da-DK/rating.js
+++ da-DK/rating.js
@@ -2,19 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Bedømmelse',
-  'rating.explained': 'Aldersklassificering og begrænsning.',
-  'rating.all': 'Alle',
-  'rating.desc.all': 'Tilgængelig for alle aldre og grupper.',
-  'rating.teens': 'Teenagere',
-  'rating.desc.teens': 'Egnet til en målgruppe mellem 12 og 18 år.',
-  'rating.mature': 'Voksen',
-  'rating.desc.mature': 'Indeholder avancerede temaer, groft sprog og implicitte scener.',
-  'rating.explicit': 'Eksplicit',
-  'rating.desc.explicit': 'Indeholder eksplicitte scener.',
-  'rating.selection.all': 'Alle - Tilgængelig for alle aldre og grupper.',
-  'rating.selection.teens': 'Unge (12-18) - Egnet til en målgruppe mellem 12 og 18 år.',
-  'rating.selection.mature':
-    'Voksen (18+) - Indeholder avancerede temaer, groft sprog og implicitte scener.',
-  'rating.selection.explicit': 'Eksplicit (21+) - Indeholder eksplicitte scener.',
-}
+    'rating.name': 'Bedømmelse',
+    'rating.explained': 'Aldersklassificering og begrænsning.',
+    'rating.all': 'Alle',
+    'rating.desc.all': 'Tilgængelig for alle aldre og grupper.',
+    'rating.teens': 'Teenagere',
+    'rating.desc.teens': 'Egnet til en målgruppe mellem 12 og 18 år.',
+    'rating.mature': 'Voksen',
+    'rating.desc.mature': 'Indeholder avancerede temaer, groft sprog og implicitte scener.',
+    'rating.explicit': 'Eksplicit',
+    'rating.desc.explicit': 'Indeholder eksplicitte scener.',
+    'rating.selection.all': 'Alle - Tilgængelig for alle aldre og grupper.',
+    'rating.selection.teens': 'Unge (12-18) - Egnet til en målgruppe mellem 12 og 18 år.',
+    'rating.selection.mature': 'Voksen (18+) - Indeholder avancerede temaer, groft sprog og implicitte scener.',
+    'rating.selection.explicit': 'Eksplicit (21+) - Indeholder eksplicitte scener.'
+};
