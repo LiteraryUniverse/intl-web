da63107a273842f99d3c44d7a618763406fa4b32
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:24:20 +0200

    New translations library.js (Punjabi)

--- pa-IN/library.js
+++ pa-IN/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'ਲਾਇਬ੍ਰੇਰੀ ਵਿੱਚ ਸ਼ਾਮਲ ਕਰੋ',
-  'library.remove': 'ਲਾਇਬ੍ਰੇਰੀ ਤੋਂ ਹਟਾਓ',
-  'library.bought': 'ਖਰੀਦੇ ਗਏ ਕਹਾਣੀਆਂ',
-  'library.stories': 'ਪਿਛੇ ਲਈ ਗਈਆਂ ਕਹਾਣੀਆਂ',
-  'library.universes': 'ਪਾਲਿਆ ਗਿਆ ਯੂਨੀਵਰਸ',
-  'bookmark.continueReading': 'ਪੜ੍ਹਨਾ ਜਾਰੀ ਰੱਖੋ',
-  'bookmark.statusSummary': 'ਤੁਸੀਂ ਕਹਾਣੀ ਦੀ ਪੜਾਈ ਕੀਤੀ ਹੈ {readPercent, number, ::percent} ਵਾਰ।',
-}
+    'library.add': 'ਲਾਇਬ੍ਰੇਰੀ ਵਿੱਚ ਸ਼ਾਮਲ ਕਰੋ',
+    'library.remove': 'ਲਾਇਬ੍ਰੇਰੀ ਤੋਂ ਹਟਾਓ',
+    'library.bought': 'ਖਰੀਦੇ ਗਏ ਕਹਾਣੀਆਂ',
+    'library.stories': 'ਪਿਛੇ ਲਈ ਗਈਆਂ ਕਹਾਣੀਆਂ',
+    'library.universes': 'ਪਾਲਿਆ ਗਿਆ ਯੂਨੀਵਰਸ',
+    'bookmark.continueReading': 'ਪੜ੍ਹਨਾ ਜਾਰੀ ਰੱਖੋ',
+    'bookmark.statusSummary': 'ਤੁਸੀਂ ਕਹਾਣੀ ਦੀ ਪੜਾਈ ਕੀਤੀ ਹੈ {readPercent, number, ::percent} ਵਾਰ।'
+};
