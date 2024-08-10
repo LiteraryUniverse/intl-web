73f5bd5ac40b2206dc3577b71f1a21ae36e55146
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:17:32 +0200

    New translations rating.js (Finnish)

--- fi-FI/rating.js
+++ fi-FI/rating.js
@@ -2,19 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Ikäluokitus',
-  'rating.explained': 'Ikäluokitus ja rajoitus.',
-  'rating.all': 'Kaikki',
-  'rating.desc.all': 'Sopii kaikenikäisille ja -ryhmille.',
-  'rating.teens': 'Teini',
-  'rating.desc.teens': 'Sopii 12-18-vuotiaille katsojille.',
-  'rating.mature': 'Kypsä',
-  'rating.desc.mature': 'Sisältää ennakkoteemoja, karkeaa kieltä ja epäsuoria kohtauksia.',
-  'rating.explicit': 'Eksplisiittinen',
-  'rating.desc.explicit': 'Sisältää selkeitä kohtauksia.',
-  'rating.selection.all': 'Kaikki - Sopii kaikenikäisille ja -ryhmille.',
-  'rating.selection.teens': 'Teini (12-18) - Sopii 12-18-vuotiaille.',
-  'rating.selection.mature':
-    'Kypsä (18+) - Sisältää ennakkoteemoja, karkeaa kieltä ja epäsuoria kohtauksia.',
-  'rating.selection.explicit': 'Eksplisiittinen (21+) - Sisältää selkeitä kohtauksia.',
-}
+    'rating.name': 'Ikäluokitus',
+    'rating.explained': 'Ikäluokitus ja rajoitus.',
+    'rating.all': 'Kaikki',
+    'rating.desc.all': 'Sopii kaikenikäisille ja -ryhmille.',
+    'rating.teens': 'Teini',
+    'rating.desc.teens': 'Sopii 12-18-vuotiaille katsojille.',
+    'rating.mature': 'Kypsä',
+    'rating.desc.mature': 'Sisältää ennakkoteemoja, karkeaa kieltä ja epäsuoria kohtauksia.',
+    'rating.explicit': 'Eksplisiittinen',
+    'rating.desc.explicit': 'Sisältää selkeitä kohtauksia.',
+    'rating.selection.all': 'Kaikki - Sopii kaikenikäisille ja -ryhmille.',
+    'rating.selection.teens': 'Teini (12-18) - Sopii 12-18-vuotiaille.',
+    'rating.selection.mature': 'Kypsä (18+) - Sisältää ennakkoteemoja, karkeaa kieltä ja epäsuoria kohtauksia.',
+    'rating.selection.explicit': 'Eksplisiittinen (21+) - Sisältää selkeitä kohtauksia.'
+};
