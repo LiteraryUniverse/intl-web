81b43047076a50aa40be50add1481b6d63c541bc
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:14:27 +0200

    New translations enrollment.js (Catalan)

--- ca-ES/enrollment.js
+++ ca-ES/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'Reclama el teu compte!',
-  'enrollment.description':
-    'Benvingut {username}! Per accedir al teu compte especial només has de crear una contrasenya.',
-  'enrollment.submitting': 'Reclamant el teu compte...',
-  'enrollment.submit': 'Reclama el teu compte!',
-}
+    'enrollment.title': 'Reclama el teu compte!',
+    'enrollment.description': 'Benvingut {username}! Per accedir al teu compte especial només has de crear una contrasenya.',
+    'enrollment.submitting': 'Reclamant el teu compte...',
+    'enrollment.submit': 'Reclama el teu compte!'
+};
