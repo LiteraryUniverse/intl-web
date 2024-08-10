ea2ba42c9777457e50f578ca091231f6e7b621ed
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:17:01 +0200

    New translations audit.js (Greek)

--- el-GR/audit.js
+++ el-GR/audit.js
@@ -1,6 +1,6 @@
 export default {
-  'auditLog.header.date': 'Ημερομηνία',
-  'auditLog.header.action': 'Τι',
-  'auditLog.header.group': 'Ομάδα',
-  'auditLog.header.user': 'Ποιος',
-}
+    'auditLog.header.date': 'Ημερομηνία',
+    'auditLog.header.action': 'Τι',
+    'auditLog.header.group': 'Ομάδα',
+    'auditLog.header.user': 'Ποιος'
+};
