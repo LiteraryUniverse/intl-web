d7764c71e8a94c32caca728718a078ad8f2e3853
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:15:18 +0200

    New translations legal.js (Czech)

--- cs-CZ/legal.js
+++ cs-CZ/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Omlouváme se za přerušení. Než Vás však necháme dále pokračovat, jsme právně vázáni, abyste se ujistili, že jste si vědomi našich smluvních podmínek, zásad ochrany osobních údajů a dohody o autorských právech a souhlasíte s nimi.',
-  'legal.modal.agree': 'Přijmout',
-  'legal.modal.disagree': 'Odmítnout',
-  'legal.modal.register': 'Toto již nebude vidět, pokud se zaregistrujete.',
-  'legal.modal.goToSettings': 'Přejít do nastavení',
-  'legal.modal.oneLastThing': 'Ještě poslední věc...',
-  'legal.modal.finish': 'Dokončit',
-  'legal.modal.existingUser': 'Už jste zaregistrováni?',
-  'legal.modal.pleaseLogin': 'Prosím, přihlašte se.',
-  'legal.publicationAgreements': 'Publikační dohody',
-  'legal.generalPublishingAgreement': 'Obecná publikační dohoda',
-  'legal.documentsFor': 'Právní dokumenty pro {workTitle}',
-}
+    'legal.modal.intro': 'Omlouváme se za přerušení. Než Vás však necháme dále pokračovat, jsme právně vázáni, abyste se ujistili, že jste si vědomi našich smluvních podmínek, zásad ochrany osobních údajů a dohody o autorských právech a souhlasíte s nimi.',
+    'legal.modal.agree': 'Přijmout',
+    'legal.modal.disagree': 'Odmítnout',
+    'legal.modal.register': 'Toto již nebude vidět, pokud se zaregistrujete.',
+    'legal.modal.goToSettings': 'Přejít do nastavení',
+    'legal.modal.oneLastThing': 'Ještě poslední věc...',
+    'legal.modal.finish': 'Dokončit',
+    'legal.modal.existingUser': 'Už jste zaregistrováni?',
+    'legal.modal.pleaseLogin': 'Prosím, přihlašte se.',
+    'legal.publicationAgreements': 'Publikační dohody',
+    'legal.generalPublishingAgreement': 'Obecná publikační dohoda',
+    'legal.documentsFor': 'Právní dokumenty pro {workTitle}'
+};
