0bf7709012b8292adf0e77c0b6ce8c00b3a60e44
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:29:38 +0200

    New translations enrollment.js (Portuguese, Brazilian)

--- pt-BR/enrollment.js
+++ pt-BR/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'Solicite sua conta!',
-  'enrollment.description':
-    'Bem-vindo {username}! Para ter acesso à sua conta especial, você só precisa criar uma senha.',
-  'enrollment.submitting': 'Reclamando sua conta...',
-  'enrollment.submit': 'Solicite sua conta!',
-}
+    'enrollment.title': 'Solicite sua conta!',
+    'enrollment.description': 'Bem-vindo {username}! Para ter acesso à sua conta especial, você só precisa criar uma senha.',
+    'enrollment.submitting': 'Reclamando sua conta...',
+    'enrollment.submit': 'Solicite sua conta!'
+};
