d297acba78edb687bed0561a549c602fd5db3378
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:37:19 +0200

    New translations notifications.js (Lojban)

--- jbo-EN/notifications.js
+++ jbo-EN/notifications.js
@@ -4,27 +4,24 @@ export default {
   'flashnews.startsAt': 'ko se temci lo ka tcini ti bo ta zmadu ti',
   'flashnews.endsAt': 'ko se temci lo ka tcini ti bo ta mleca ti',
   'flashnews.newLanguage': 'ko pilno lo bangu be lo nu ve kalri lo nu zasne ko',
-  'flashnews.onlyDisplayOn':
-    "lo nu co'e lo nu xamsi le'i banzu lo velnu se jdice lo nu cnino co'a nilmivyminde se galfi di'ai lo kagai vei banzu vei gi'a na banzu co'a fau lo girzu ve'o jersi sekai",
-  'notifications.menu.all': "co'e cabna mu'i lo nu prami ko",
+  'flashnews.onlyDisplayOn': 'lo nu co\'e lo nu xamsi le\'i banzu lo velnu se jdice lo nu cnino co\'a nilmivyminde se galfi di\'ai lo kagai vei banzu vei gi\'a na banzu co\'a fau lo girzu ve\'o jersi sekai',
+  'notifications.menu.all': 'co\'e cabna mu\'i lo nu prami ko',
   'notifications.menu.following': 'ko sutra fukpi',
   'notifications.menu.workshop': 'ko pinxe lo ckafi',
-  'notifications.menu.news': "ko jarco lo xatra gi'e jinvi lo ckafi",
-  'notifications.menu.markUnread': "o'o'o lo nu binxo facki",
-  'notifications.menu.markRead': "oi'o'o lo nu binxo facki",
+  'notifications.menu.news': 'ko jarco lo xatra gi\'e jinvi lo ckafi',
+  'notifications.menu.markUnread': 'o\'o\'o lo nu binxo facki',
+  'notifications.menu.markRead': 'oi\'o\'o lo nu binxo facki',
   'notifications.menu.delete': 'ko ranji lo nu vimcu lo jbobau',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'Your story {storyName} was automatically set to hiatus after 6 months of inactivity.',
-  'notification.workshopStoryStatus.toCancelled':
-    'Your story {storyName} was automatically set to cancelled after one year of inactivity.',
+  'notification.workshopStoryStatus.toHiatus': 'Your story {storyName} was automatically set to hiatus after 6 months of inactivity.',
+  'notification.workshopStoryStatus.toCancelled': 'Your story {storyName} was automatically set to cancelled after one year of inactivity.',
   'notification.followingStoryStatus.toHiatus': '{storyName} is on hiatus.',
   'notification.followingStoryStatus.toCancelled': '{storyName} has been cancelled.',
   'notification.followingNewChapter.new': 'New chapter in {storyName} has been published!',
   'notification.workshopStory.newComment': 'has commented on {storyName}',
   // Before this text will be the username of the user
-  'notification.friendRequest': "ko frili ni'o ta zei sanmi",
+  'notification.friendRequest': 'ko frili ni\'o ta zei sanmi',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'klama ro da',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'klama ro da'
+};
