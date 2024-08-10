c8a21bd4590d0da63693cd46ef12910917fdf30e
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:18:15 +0200

    New translations user.js (Irish)

--- ga-IE/user.js
+++ ga-IE/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'Cuir le cairde',
-  'user.friend.requests': `Tá {num, number} iarratas nua cairde {num, plural,
+    'user.friend.request.add': 'Cuir le cairde',
+    'user.friend.requests': `Tá {num, number} iarratas nua cairde {num, plural,
     zero {agat}
     one {iarratas}
     many {iarratas}
     other {iarratas}
   }`,
-  'user.friend.unfriend': 'Cealaigh cairde',
-  'user.block': 'Blocáil',
-  'user.unblock': 'Ná cosc a thuilleadh',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'Cealaigh cairde',
+    'user.block': 'Blocáil',
+    'user.unblock': 'Ná cosc a thuilleadh',
+    'user.joined': `{gender, select,
     male {Gabhann le}
     female {Gabhann le}
     other {Gabhann le}} ar {date, date, long}`,
-  'user.friend.request.accept': 'Glac le iarratas cairdis',
-  'user.friend.request.cancel': 'Cealaigh iarratas cairdis',
-  'user.friend.request.deny': 'Diúltaigh cairde',
-  'user.listing': 'Liosta úsáideoirí',
-  'user.profile.visit': 'Cuairt a thabhairt ar phróifíl {user}.',
-  'user.profile.avatar': 'Aoisbhrat {username}.',
-  'user.friend.list.requests': 'Iarratais cairdis',
-  'user.friend.norequests': 'Níl iarratais cairdis ort.',
-  'user.dashboardWelcome': `Good {dayPart, select,
+    'user.friend.request.accept': 'Glac le iarratas cairdis',
+    'user.friend.request.cancel': 'Cealaigh iarratas cairdis',
+    'user.friend.request.deny': 'Diúltaigh cairde',
+    'user.listing': 'Liosta úsáideoirí',
+    'user.profile.visit': 'Cuairt a thabhairt ar phróifíl {user}.',
+    'user.profile.avatar': 'Aoisbhrat {username}.',
+    'user.friend.list.requests': 'Iarratais cairdis',
+    'user.friend.norequests': 'Níl iarratais cairdis ort.',
+    'user.dashboardWelcome': `Good {dayPart, select,
     morning {morning}
     afternoon {afternoon}
     evening {evening}
     night {night}
     other {day}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'Tá an t-údar seo foilsithe ar an mbealach traidisiúnta',
-  'user.verification.luAuthor': 'Údar bunaithe ar Literary Universe',
-  'user.verification.luEmployee': 'Fostaí Literary Universe',
-  'user.verification.publicFigure': 'Duine poiblí dhearbaithe',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'Tá an t-údar seo foilsithe ar an mbealach traidisiúnta',
+    'user.verification.luAuthor': 'Údar bunaithe ar Literary Universe',
+    'user.verification.luEmployee': 'Fostaí Literary Universe',
+    'user.verification.publicFigure': 'Duine poiblí dhearbaithe',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {scéal}
     other {scéalta}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {unibhersal}
     other {unibhersail}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {fan fiction}
     other {fan fics} 
   }`,
-  'profile.works': 'Oibreacha',
-  // As in creative/literary works
-  'profile.wishlist': 'Liosta mianta',
-  'profile.storiesBy': 'Scéalta cruthaithe ag {username}',
-  'profile.universesBy': 'Unibhersail cruthaithe ag {username}',
-  'profile.fanFictionBy': 'Fan Fiction cruthaithe ag {username}',
-}
+    'profile.works': 'Oibreacha',
+    // As in creative/literary works
+    'profile.wishlist': 'Liosta mianta',
+    'profile.storiesBy': 'Scéalta cruthaithe ag {username}',
+    'profile.universesBy': 'Unibhersail cruthaithe ag {username}',
+    'profile.fanFictionBy': 'Fan Fiction cruthaithe ag {username}'
+};
