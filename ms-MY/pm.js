c8534470f03887584245f52411dfb59a804bc0f0
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:34:52 +0200

    New translations pm.js (Malay)

--- ms-MY/pm.js
+++ ms-MY/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'Perbualan',
-  'pm.showolder': 'Tunjukkan mesej lama',
-  'pm.errors.saysomething': 'Anda sepatutnya mengatakan sesuatu...',
-  'pm.errors.addrecipient': 'Anda perlu menambah sekurang-kurangnya satu pengguna lain.',
-  'pm.message': 'Mesej',
-  'pm.send.message': 'Hantar mesej',
-  'pm.messages.own': 'Mesej anda',
-  'pm.conversations.none': 'Anda tidak sedang berbual dengan sesiapa pun.',
-  'pm.to': 'Kepada:',
-  'pm.with': 'Perbualan dengan:',
-  'pm.send': 'Hantar',
-  'pm.conversations.unreadCount': `Anda mempunyai {count, plural,
+    'pm.conversation': 'Perbualan',
+    'pm.showolder': 'Tunjukkan mesej lama',
+    'pm.errors.saysomething': 'Anda sepatutnya mengatakan sesuatu...',
+    'pm.errors.addrecipient': 'Anda perlu menambah sekurang-kurangnya satu pengguna lain.',
+    'pm.message': 'Mesej',
+    'pm.send.message': 'Hantar mesej',
+    'pm.messages.own': 'Mesej anda',
+    'pm.conversations.none': 'Anda tidak sedang berbual dengan sesiapa pun.',
+    'pm.to': 'Kepada:',
+    'pm.with': 'Perbualan dengan:',
+    'pm.send': 'Hantar',
+    'pm.conversations.unreadCount': `Anda mempunyai {count, plural,
     zero {tiada perbualan yang belum dibaca}
     one {satu perbualan yang belum dibaca}
     other {# perbualan yang belum dibaca}
   }.`,
-  'pm.form.minimumParticipants': 'Anda perlu memilih penerima.',
-  'pm.form.maxParticipants': `You can have maximum of {maxPeople, plural,
+    'pm.form.minimumParticipants': 'Anda perlu memilih penerima.',
+    'pm.form.maxParticipants': `You can have maximum of {maxPeople, plural,
     one {one person}
     other {# people}
   } in a conversation.`,
-  'pm.new.aria': 'Perbualan baru',
-  'pm.new.buttonTitle': 'Mula perbualan baru',
-}
+    'pm.new.aria': 'Perbualan baru',
+    'pm.new.buttonTitle': 'Mula perbualan baru'
+};
