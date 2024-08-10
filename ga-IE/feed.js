db2778238b27bc935ccf4c59f24e862a75a7b2c0
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:17:56 +0200

    New translations feed.js (Irish)

--- ga-IE/feed.js
+++ ga-IE/feed.js
@@ -1,9 +1,9 @@
 export default {
-  'feed.name': 'Féach',
-  'feed.post.new': 'Altachán nua',
-  'feed.post.like': 'Cosaint post',
-  'feed.post.unlike': 'Ná cosaint post',
-  'feed.post.send': 'Poist',
-  'feed.empty': 'Gan iontrálacha sa bhfuinneog.',
-  'feed.placeholder': 'Cad atá ar do intinn?',
-}
+    'feed.name': 'Féach',
+    'feed.post.new': 'Altachán nua',
+    'feed.post.like': 'Cosaint post',
+    'feed.post.unlike': 'Ná cosaint post',
+    'feed.post.send': 'Poist',
+    'feed.empty': 'Gan iontrálacha sa bhfuinneog.',
+    'feed.placeholder': 'Cad atá ar do intinn?'
+};
