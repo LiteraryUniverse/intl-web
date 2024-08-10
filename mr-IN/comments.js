3fd64a315c6b6e98d677e7f6a9ecadb909c9905f
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:32:45 +0200

    New translations comments.js (Marathi)

--- mr-IN/comments.js
+++ mr-IN/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': 'तुमची टिप्पणी लिहा',
-  'comments.none': 'माफ करा, कोणतीही टिप्पणी दर्शविण्यासाठी नाहीत.',
-  'comments.total': `तीन {count, plural,
+    'comment.write': 'तुमची टिप्पणी लिहा',
+    'comments.none': 'माफ करा, कोणतीही टिप्पणी दर्शविण्यासाठी नाहीत.',
+    'comments.total': `तीन {count, plural,
     =0 {टिप्पणी आहेत}
     one {एक टिप्पणी आहे}
     other {# टिप्पण्या आहेत}
   }.`,
-  'comments.show.older': 'मोठ्या प्रमाणात टिप्पणी दाखवा',
-}
+    'comments.show.older': 'मोठ्या प्रमाणात टिप्पणी दाखवा'
+};
