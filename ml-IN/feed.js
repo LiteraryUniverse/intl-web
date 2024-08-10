d3031f8e9892378fa550dde7c09207614e791bfd
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:37:31 +0200

    New translations feed.js (Malayalam)

--- ml-IN/feed.js
+++ ml-IN/feed.js
@@ -1,9 +1,9 @@
 export default {
-  'feed.name': 'ഫീഡ്',
-  'feed.post.new': 'പുതിയ പോസ്റ്റ്',
-  'feed.post.like': 'പോസ്റ്റ് ഇഷ്ടപ്പെട്ടു',
-  'feed.post.unlike': 'പോസ്റ്റ് അരികൊടുക്കുക',
-  'feed.post.send': 'പോസ്റ്റ്',
-  'feed.empty': 'ഫീഡിൽ ഏതെങ്കിലും എൻട്രികൾ ഇല്ല.',
-  'feed.placeholder': 'നിങ്ങളുടെ മനസ്സിലോക്കുന്നത് എന്താണ്?',
-}
+    'feed.name': 'ഫീഡ്',
+    'feed.post.new': 'പുതിയ പോസ്റ്റ്',
+    'feed.post.like': 'പോസ്റ്റ് ഇഷ്ടപ്പെട്ടു',
+    'feed.post.unlike': 'പോസ്റ്റ് അരികൊടുക്കുക',
+    'feed.post.send': 'പോസ്റ്റ്',
+    'feed.empty': 'ഫീഡിൽ ഏതെങ്കിലും എൻട്രികൾ ഇല്ല.',
+    'feed.placeholder': 'നിങ്ങളുടെ മനസ്സിലോക്കുന്നത് എന്താണ്?'
+};
