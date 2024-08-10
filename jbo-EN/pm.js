2f5e3688d2394481b4f640738f80800e918e5eec
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:37:06 +0200

    New translations pm.js (Lojban)

--- jbo-EN/pm.js
+++ jbo-EN/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'cusku',
-  'pm.showolder': "Jmina se lidne cu'o",
-  'pm.errors.saysomething': 'do xamgu lo nu cusku',
-  'pm.errors.addrecipient': 'do djica lo nu jmina lo drata',
-  'pm.message': 'xatra',
-  'pm.send.message': "Tolju'i xatra",
-  'pm.messages.own': 'lo se cusku be do',
-  'pm.conversations.none': "do na'e cusku zo'u",
-  'pm.to': "lo te du'u",
-  'pm.with': "cusku zo'u",
-  'pm.send': 'benji',
-  'pm.conversations.unreadCount': `You have {count, plural,
+    'pm.conversation': 'cusku',
+    'pm.showolder': 'Jmina se lidne cu\'o',
+    'pm.errors.saysomething': 'do xamgu lo nu cusku',
+    'pm.errors.addrecipient': 'do djica lo nu jmina lo drata',
+    'pm.message': 'xatra',
+    'pm.send.message': 'Tolju\'i xatra',
+    'pm.messages.own': 'lo se cusku be do',
+    'pm.conversations.none': 'do na\'e cusku zo\'u',
+    'pm.to': 'lo te du\'u',
+    'pm.with': 'cusku zo\'u',
+    'pm.send': 'benji',
+    'pm.conversations.unreadCount': `You have {count, plural,
     zero {no unread conversations}
     one {one unread conversation}
     other {# unread conversations}
   }.`,
-  'pm.form.minimumParticipants': 'do djica lo nu pilno',
-  'pm.form.maxParticipants': `You can have maximum of {maxPeople, plural,
+    'pm.form.minimumParticipants': 'do djica lo nu pilno',
+    'pm.form.maxParticipants': `You can have maximum of {maxPeople, plural,
     one {one person}
     other {# people}
   } in a conversation.`,
-  'pm.new.aria': 'cmalu cusku',
-  'pm.new.buttonTitle': 'cfari lo nu cusku',
-}
+    'pm.new.aria': 'cmalu cusku',
+    'pm.new.buttonTitle': 'cfari lo nu cusku'
+};
