7f2eb1f3862c683a561793e0f2cb812738b8402b
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:27:45 +0200

    New translations user.js (Turkish)

--- tr-TR/user.js
+++ tr-TR/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'Arkadaşlara ekle',
-  'user.friend.requests': `{num, number} yeni arkadaşlık isteği {num, plural,
+    'user.friend.request.add': 'Arkadaşlara ekle',
+    'user.friend.requests': `{num, number} yeni arkadaşlık isteği {num, plural,
     zero {isteği}
     one {istek}
     many {istek}
     other {istek}
   }var.`,
-  'user.friend.unfriend': 'Arkadaşlıktan çıkar',
-  'user.block': 'Engelle',
-  'user.unblock': 'Engeli kaldır',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'Arkadaşlıktan çıkar',
+    'user.block': 'Engelle',
+    'user.unblock': 'Engeli kaldır',
+    'user.joined': `{gender, select,
     male {}
     female {}
     other {}} {date, date, long}tarihinde katıldı.`,
-  'user.friend.request.accept': 'Arkadaşlık isteğini kabul et',
-  'user.friend.request.cancel': 'Arkadaşlık isteğini iptal et',
-  'user.friend.request.deny': 'Arkadaşlık isteğini reddet',
-  'user.listing': 'Kullanıcı listesi',
-  'user.profile.visit': "{user}'in profilini ziyaret et.",
-  'user.profile.avatar': "{username}'in avatarı",
-  'user.friend.list.requests': 'Arkadaşlık istekleri',
-  'user.friend.norequests': 'Arkadaşlık isteği yok.',
-  'user.dashboardWelcome': `İyi {dayPart, select,
+    'user.friend.request.accept': 'Arkadaşlık isteğini kabul et',
+    'user.friend.request.cancel': 'Arkadaşlık isteğini iptal et',
+    'user.friend.request.deny': 'Arkadaşlık isteğini reddet',
+    'user.listing': 'Kullanıcı listesi',
+    'user.profile.visit': '{user}\'in profilini ziyaret et.',
+    'user.profile.avatar': '{username}\'in avatarı',
+    'user.friend.list.requests': 'Arkadaşlık istekleri',
+    'user.friend.norequests': 'Arkadaşlık isteği yok.',
+    'user.dashboardWelcome': `İyi {dayPart, select,
     morning {sabah}
     afternoon {öğleden sonra}
     evening {akşam}
     night {gece}
     other {gün}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'Bu yazar geleneksel yolla yayımlanmıştır',
-  'user.verification.luAuthor': 'Literary Universe tabanlı tanınmış yazar',
-  'user.verification.luEmployee': 'Literary Universe çalışanı',
-  'user.verification.publicFigure': 'Doğrulanmış kamu figürü',
-  'profile.storiesCount': `{count, number} hikaye {count, plural,
+    'user.verification.publishedAuthor': 'Bu yazar geleneksel yolla yayımlanmıştır',
+    'user.verification.luAuthor': 'Literary Universe tabanlı tanınmış yazar',
+    'user.verification.luEmployee': 'Literary Universe çalışanı',
+    'user.verification.publicFigure': 'Doğrulanmış kamu figürü',
+    'profile.storiesCount': `{count, number} hikaye {count, plural,
     one {hikaye}
     other {hikaye}
   }var.`,
-  'profile.universesCount': `{count, number} evren {count, plural,
+    'profile.universesCount': `{count, number} evren {count, plural,
     one {evren}
     other {evren}
   }var.`,
-  'profile.fanFictionsCount': `{count, number} hayran kurgu {count, plural,
+    'profile.fanFictionsCount': `{count, number} hayran kurgu {count, plural,
     one {hayran kurgu}
     other {hayran kurgu} 
   }var.`,
-  'profile.works': 'Çalışmalar',
-  // As in creative/literary works
-  'profile.wishlist': 'Dilek Listesi',
-  'profile.storiesBy': '{username}tarafından oluşturulan hikayeler',
-  'profile.universesBy': '{username}tarafından oluşturulan evrenler',
-  'profile.fanFictionBy': '{username}tarafından oluşturulan hayran kurguları',
-}
+    'profile.works': 'Çalışmalar',
+    // As in creative/literary works
+    'profile.wishlist': 'Dilek Listesi',
+    'profile.storiesBy': '{username}tarafından oluşturulan hikayeler',
+    'profile.universesBy': '{username}tarafından oluşturulan evrenler',
+    'profile.fanFictionBy': '{username}tarafından oluşturulan hayran kurguları'
+};
