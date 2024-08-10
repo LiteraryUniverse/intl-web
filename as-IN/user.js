7b7fcb547c4c738053ff2624dc1abdcad0b43231
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:39:30 +0200

    New translations user.js (Assamese)

--- as-IN/user.js
+++ as-IN/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'মিত্ৰ সংযোজন কৰক',
-  'user.friend.requests': `আপোনালোকে {num, number} নতুন বন্ধু {num, plural,
+    'user.friend.request.add': 'মিত্ৰ সংযোজন কৰক',
+    'user.friend.requests': `আপোনালোকে {num, number} নতুন বন্ধু {num, plural,
     zero {অনুৰোধ}
     one {অনুৰোধ}
     many {অনুৰোধ}
     other {অনুৰোধ}
   }`,
-  'user.friend.unfriend': 'মিত্ৰতা বিছাৰি পোৱা বন্ধ কৰক',
-  'user.block': 'ব্লক কৰক',
-  'user.unblock': 'আনব্লক কৰক',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'মিত্ৰতা বিছাৰি পোৱা বন্ধ কৰক',
+    'user.block': 'ব্লক কৰক',
+    'user.unblock': 'আনব্লক কৰক',
+    'user.joined': `{gender, select,
     male {যোগদান কৰিছে}
     female {যোগদান কৰিছে}
     other {যোগদান কৰিছে}} তাৰিখত {date, date, long}`,
-  'user.friend.request.accept': 'মিত্ৰতা অনুৰোধ স্বীকাৰ কৰক',
-  'user.friend.request.cancel': 'মিত্ৰতা অনুৰোধ বাতিল কৰক',
-  'user.friend.request.deny': 'মিত্ৰতা অনুৰোধ বাজাওক',
-  'user.listing': 'ব্যৱহাৰকাৰীৰ তালিকা',
-  'user.profile.visit': '{user}ৰ প্ৰোফাইললৈ যাওক।',
-  'user.profile.avatar': '{username}ৰ এভাটাৰ',
-  'user.friend.list.requests': 'মিত্ৰতাৰ অনুৰোধসমূহ',
-  'user.friend.norequests': 'আপুনি কোনো মিত্ৰতাৰ অনুৰোধ নাই।',
-  'user.dashboardWelcome': `ভাল {dayPart, select,
+    'user.friend.request.accept': 'মিত্ৰতা অনুৰোধ স্বীকাৰ কৰক',
+    'user.friend.request.cancel': 'মিত্ৰতা অনুৰোধ বাতিল কৰক',
+    'user.friend.request.deny': 'মিত্ৰতা অনুৰোধ বাজাওক',
+    'user.listing': 'ব্যৱহাৰকাৰীৰ তালিকা',
+    'user.profile.visit': '{user}ৰ প্ৰোফাইললৈ যাওক।',
+    'user.profile.avatar': '{username}ৰ এভাটাৰ',
+    'user.friend.list.requests': 'মিত্ৰতাৰ অনুৰোধসমূহ',
+    'user.friend.norequests': 'আপুনি কোনো মিত্ৰতাৰ অনুৰোধ নাই।',
+    'user.dashboardWelcome': `ভাল {dayPart, select,
     morning {সকাল}
     afternoon {দুপৰ}
     evening {সন্ধ্যা}
     night {রাত্ৰি}
     other {দিন}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'এই লেখকটো ঐতিহাসিক পদ্ধতিতে প্ৰকাশ কৰা হৈছে',
-  'user.verification.luAuthor': 'সাহিত্যিক বিশ্বত প্ৰকাশিত লেখক',
-  'user.verification.luEmployee': 'সাহিত্যিক বিশ্বত কৰ্মচাৰী',
-  'user.verification.publicFigure': 'যাচাইকৃত জনপ্রিয় ব্যক্তি',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'এই লেখকটো ঐতিহাসিক পদ্ধতিতে প্ৰকাশ কৰা হৈছে',
+    'user.verification.luAuthor': 'সাহিত্যিক বিশ্বত প্ৰকাশিত লেখক',
+    'user.verification.luEmployee': 'সাহিত্যিক বিশ্বত কৰ্মচাৰী',
+    'user.verification.publicFigure': 'যাচাইকৃত জনপ্রিয় ব্যক্তি',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {গল্প}
     other {গল্পসমূহ}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {বিশ্ব}
     other {বিশ্বসমূহ}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {ফ্যান ফিকশন}
     other {ফ্যান ফিকশনসমূহ} 
   }`,
-  'profile.works': 'কামকাজ',
-  // As in creative/literary works
-  'profile.wishlist': 'ইচ্ছাপত্র',
-  'profile.storiesBy': '{username}দ্বারা সৃষ্ট গল্পসমূহ',
-  'profile.universesBy': '{username}দ্বারা সৃষ্ট বিশ্বসমূহ',
-  'profile.fanFictionBy': '{username}দ্বারা সৃষ্ট ফ্যান ফিকশনসমূহ',
-}
+    'profile.works': 'কামকাজ',
+    // As in creative/literary works
+    'profile.wishlist': 'ইচ্ছাপত্র',
+    'profile.storiesBy': '{username}দ্বারা সৃষ্ট গল্পসমূহ',
+    'profile.universesBy': '{username}দ্বারা সৃষ্ট বিশ্বসমূহ',
+    'profile.fanFictionBy': '{username}দ্বারা সৃষ্ট ফ্যান ফিকশনসমূহ'
+};
