36ef52fe4e9219715bee73a5958e325080c190d1
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:18:44 +0200

    New translations comments.js (Gujarati)

--- gu-IN/comments.js
+++ gu-IN/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': 'તમારી ટિપ્પણી લખો',
-  'comments.none': 'માફ કરશો, કોઈ ટિપ્પણીઓ બતાવવા માટે નથી.',
-  'comments.total': `અહીં {count, plural,
+    'comment.write': 'તમારી ટિપ્પણી લખો',
+    'comments.none': 'માફ કરશો, કોઈ ટિપ્પણીઓ બતાવવા માટે નથી.',
+    'comments.total': `અહીં {count, plural,
     =0 {કોઈ ટિપ્પણીઓ નથી}
     one {એક ટિપ્પણી છે}
     other {# ટિપ્પણીઓ છે}
   }.`,
-  'comments.show.older': 'જૂની ટિપ્પણીઓ બતાવો',
-}
+    'comments.show.older': 'જૂની ટિપ્પણીઓ બતાવો'
+};
