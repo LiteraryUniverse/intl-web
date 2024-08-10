c8fe8ee52a868efdbd492d6a7025b48d7779609e
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:36:43 +0200

    New translations user.js (Filipino)

--- fil-PH/user.js
+++ fil-PH/user.js
@@ -1,54 +1,53 @@
 export default {
-  'user.friend.request.add': 'Idagdag sa mga kaibigan',
-  'user.friend.requests': `Mayroon kang {num, number} bagong kaibigan {num, plural,
+    'user.friend.request.add': 'Idagdag sa mga kaibigan',
+    'user.friend.requests': `Mayroon kang {num, number} bagong kaibigan {num, plural,
     zero {na mga kahilingan}
     one {kahilingan}
     many {mga kahilingan}
     other {mga kahilingan}
   }`,
-  'user.friend.unfriend': 'Tanggalin sa mga kaibigan',
-  'user.block': 'I-block',
-  'user.unblock': 'I-unblock',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'Tanggalin sa mga kaibigan',
+    'user.block': 'I-block',
+    'user.unblock': 'I-unblock',
+    'user.joined': `{gender, select,
     male {Sumali sa}
     female {Sumali sa}
     other {Sumali sa}} noong {date, date, long}`,
-  'user.friend.request.accept': 'Tanggapin ang kahilingan ng pagkakaibigan',
-  'user.friend.request.cancel': 'Kanselahin ang kahilingan ng pagkakaibigan',
-  'user.friend.request.deny': 'Tanggihan ang pagkakaibigan',
-  'user.listing': 'Listahan ng mga User',
-  'user.profile.visit': 'Pumunta sa profile ni {user}.',
-  'user.profile.avatar': 'Avatar ni {username}',
-  'user.friend.list.requests': 'Mga kahilingan ng pagkakaibigan',
-  'user.friend.norequests': 'Wala kang mga kahilingan para sa pagkakaibigan.',
-  'user.dashboardWelcome': `Magandang umaga {dayPart, select,
+    'user.friend.request.accept': 'Tanggapin ang kahilingan ng pagkakaibigan',
+    'user.friend.request.cancel': 'Kanselahin ang kahilingan ng pagkakaibigan',
+    'user.friend.request.deny': 'Tanggihan ang pagkakaibigan',
+    'user.listing': 'Listahan ng mga User',
+    'user.profile.visit': 'Pumunta sa profile ni {user}.',
+    'user.profile.avatar': 'Avatar ni {username}',
+    'user.friend.list.requests': 'Mga kahilingan ng pagkakaibigan',
+    'user.friend.norequests': 'Wala kang mga kahilingan para sa pagkakaibigan.',
+    'user.dashboardWelcome': `Magandang umaga {dayPart, select,
     morning {tanghali}
     afternoon {hapon}
     evening {gabi}
     night {gabi}
     other {araw}
   } {displayName}!`,
-  'user.verification.publishedAuthor':
-    'Ang may-akda na ito ay nag-publish sa tradisyonal na paraan',
-  'user.verification.luAuthor': 'May-akdang batay sa Literary Universe',
-  'user.verification.luEmployee': 'Kawani ng Literary Universe',
-  'user.verification.publicFigure': 'Berdadong kilalang personalidad',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'Ang may-akda na ito ay nag-publish sa tradisyonal na paraan',
+    'user.verification.luAuthor': 'May-akdang batay sa Literary Universe',
+    'user.verification.luEmployee': 'Kawani ng Literary Universe',
+    'user.verification.publicFigure': 'Berdadong kilalang personalidad',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {story}
     other {stories}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {universe}
     other {universes}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {fan fiction}
     other {fan fictions} 
   }`,
-  'profile.works': 'Mga Gawa',
-  // As in creative/literary works
-  'profile.wishlist': 'Wishlist',
-  'profile.storiesBy': 'Stories created by {username}',
-  'profile.universesBy': 'Universes created by {username}',
-  'profile.fanFictionBy': 'Fan Fiction created by {username}',
-}
+    'profile.works': 'Mga Gawa',
+    // As in creative/literary works
+    'profile.wishlist': 'Wishlist',
+    'profile.storiesBy': 'Stories created by {username}',
+    'profile.universesBy': 'Universes created by {username}',
+    'profile.fanFictionBy': 'Fan Fiction created by {username}'
+};
