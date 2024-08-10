067a1019976b0332641bb34ea2ae29740a3280e6
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:22:34 +0200

    New translations rating.js (Lithuanian)

--- lt-LT/rating.js
+++ lt-LT/rating.js
@@ -2,19 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'Įvertinimas',
-  'rating.explained': 'Amžiaus įvertinimas ir apribojimai.',
-  'rating.all': 'Visi',
-  'rating.desc.all': 'Prieinama visiems amžiams ir grupėms.',
-  'rating.teens': 'Paaugliai',
-  'rating.desc.teens': 'Tinkama auditorijai nuo 12 iki 18 metų.',
-  'rating.mature': 'Suaugusieji',
-  'rating.desc.mature': 'Turinys apie sudėtingas temas, grubų kalbą ir neapibrėžtus vaizdus.',
-  'rating.explicit': 'Aiškus',
-  'rating.desc.explicit': 'Turinys su aiškiais vaizdais.',
-  'rating.selection.all': 'Visi - Prieinama visiems amžiams ir grupėms.',
-  'rating.selection.teens': 'Paaugliai (12-18) - Tinkama auditorijai nuo 12 iki 18 metų.',
-  'rating.selection.mature':
-    'Suaugusieji (18+) - Turinys apie sudėtingas temas, grubų kalbą ir neapibrėžtus vaizdus.',
-  'rating.selection.explicit': 'Aiškus (21+) - Turinys su aiškiais vaizdais.',
-}
+    'rating.name': 'Įvertinimas',
+    'rating.explained': 'Amžiaus įvertinimas ir apribojimai.',
+    'rating.all': 'Visi',
+    'rating.desc.all': 'Prieinama visiems amžiams ir grupėms.',
+    'rating.teens': 'Paaugliai',
+    'rating.desc.teens': 'Tinkama auditorijai nuo 12 iki 18 metų.',
+    'rating.mature': 'Suaugusieji',
+    'rating.desc.mature': 'Turinys apie sudėtingas temas, grubų kalbą ir neapibrėžtus vaizdus.',
+    'rating.explicit': 'Aiškus',
+    'rating.desc.explicit': 'Turinys su aiškiais vaizdais.',
+    'rating.selection.all': 'Visi - Prieinama visiems amžiams ir grupėms.',
+    'rating.selection.teens': 'Paaugliai (12-18) - Tinkama auditorijai nuo 12 iki 18 metų.',
+    'rating.selection.mature': 'Suaugusieji (18+) - Turinys apie sudėtingas temas, grubų kalbą ir neapibrėžtus vaizdus.',
+    'rating.selection.explicit': 'Aiškus (21+) - Turinys su aiškiais vaizdais.'
+};
