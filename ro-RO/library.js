73937a95015b1f3d7a9bc01bbe30bb19c4c81d6c
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:41:36 +0200

    New translations library.js (Romanian)

--- ro-RO/library.js
+++ ro-RO/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'Adaugă în bibliotecă',
-  'library.remove': 'Șterge din bibliotecă',
-  'library.bought': 'Povești cumpărate',
-  'library.stories': 'Povești urmărite',
-  'library.universes': 'Followed universes',
-  'bookmark.continueReading': 'Continue reading',
-  'bookmark.statusSummary': 'You have read {readPercent, number, ::percent} of the story.',
-}
+    'library.add': 'Adaugă în bibliotecă',
+    'library.remove': 'Șterge din bibliotecă',
+    'library.bought': 'Povești cumpărate',
+    'library.stories': 'Povești urmărite',
+    'library.universes': 'Followed universes',
+    'bookmark.continueReading': 'Continue reading',
+    'bookmark.statusSummary': 'You have read {readPercent, number, ::percent} of the story.'
+};
