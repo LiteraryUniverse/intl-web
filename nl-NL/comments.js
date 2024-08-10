1629a930c4d523e7f9a97aa0be47ce23c7a58097
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:23:12 +0200

    New translations comments.js (Dutch)

--- nl-NL/comments.js
+++ nl-NL/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': 'Schrijf uw reactie',
-  'comments.none': 'Sorry, geen reacties om te tonen.',
-  'comments.total': `Er {count, plural,
+    'comment.write': 'Schrijf uw reactie',
+    'comments.none': 'Sorry, geen reacties om te tonen.',
+    'comments.total': `Er {count, plural,
     =0 {zijn geen commentaren}
     one {is 1 commentaar}
     other {zijn # commentaren}
   }.`,
-  'comments.show.older': 'Toon oudere reacties',
-}
+    'comments.show.older': 'Toon oudere reacties'
+};
