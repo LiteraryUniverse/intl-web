0c236b3e33c4b89452e098323e69eee7e1abbe6d
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:35:49 +0200

    New translations enrollment.js (Esperanto)

--- eo-UY/enrollment.js
+++ eo-UY/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'Pretendu vian konton!',
-  'enrollment.description':
-    'Bonvenon {username}! Por akiri aliron al via speciala konto, vi nur bezonas krei pasvorton.',
-  'enrollment.submitting': 'Pretendante vian konton...',
-  'enrollment.submit': 'Pretendu vian konton!',
-}
+    'enrollment.title': 'Pretendu vian konton!',
+    'enrollment.description': 'Bonvenon {username}! Por akiri aliron al via speciala konto, vi nur bezonas krei pasvorton.',
+    'enrollment.submitting': 'Pretendante vian konton...',
+    'enrollment.submit': 'Pretendu vian konton!'
+};
