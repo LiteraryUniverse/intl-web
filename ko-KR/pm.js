1d5684e1928db6c2587d2a73e7d0ca047f353d42
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:21:59 +0200

    New translations pm.js (Korean)

--- ko-KR/pm.js
+++ ko-KR/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': '대화',
-  'pm.showolder': '이전 메시지 표시',
-  'pm.errors.saysomething': '정말 뭐라고 말해야 할 것 같은데...',
-  'pm.errors.addrecipient': '다른 사용자를 한 명 이상 추가해야 합니다.',
-  'pm.message': '메시지',
-  'pm.send.message': '메시지 보내기',
-  'pm.messages.own': '메시지',
-  'pm.conversations.none': '현재 누구와도 대화하고 있지 않습니다.',
-  'pm.to': '받는 사람:',
-  'pm.with': '대화 상대:',
-  'pm.send': '보내기',
-  'pm.conversations.unreadCount': `You have {count, plural,
+    'pm.conversation': '대화',
+    'pm.showolder': '이전 메시지 표시',
+    'pm.errors.saysomething': '정말 뭐라고 말해야 할 것 같은데...',
+    'pm.errors.addrecipient': '다른 사용자를 한 명 이상 추가해야 합니다.',
+    'pm.message': '메시지',
+    'pm.send.message': '메시지 보내기',
+    'pm.messages.own': '메시지',
+    'pm.conversations.none': '현재 누구와도 대화하고 있지 않습니다.',
+    'pm.to': '받는 사람:',
+    'pm.with': '대화 상대:',
+    'pm.send': '보내기',
+    'pm.conversations.unreadCount': `You have {count, plural,
     zero {no unread conversations}
     one {one unread conversation}
     other {# unread conversations}
   }.`,
-  'pm.form.minimumParticipants': '수신자를 선택해야 합니다.',
-  'pm.form.maxParticipants': `대화에서는 최대 {maxPeople, plural,
+    'pm.form.minimumParticipants': '수신자를 선택해야 합니다.',
+    'pm.form.maxParticipants': `대화에서는 최대 {maxPeople, plural,
     one {한 명의 사람}
     other {# 사람들}
   } 가 가능합니다.`,
-  'pm.new.aria': '새 대화',
-  'pm.new.buttonTitle': '새 대화 시작',
-}
+    'pm.new.aria': '새 대화',
+    'pm.new.buttonTitle': '새 대화 시작'
+};
