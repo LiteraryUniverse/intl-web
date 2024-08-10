7f209b91f79cde07022cb3cc60eb2dbd518c3034
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:29:22 +0200

    New translations legal.js (Vietnamese)

--- vi-VN/legal.js
+++ vi-VN/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Xin lỗi vì đã làm phiền. Trước khi bạn tiếp tục điều gì khác, chúng tôi có nghĩa vụ pháp lý để đảm bảo bạn nhận thức và đồng ý với Điều khoản Dịch vụ, Chính sách Bảo mật và Thỏa thuận Bản quyền của chúng tôi.',
-  'legal.modal.agree': 'Tôi Đồng ý',
-  'legal.modal.disagree': 'Tôi Không Đồng ý',
-  'legal.modal.register': 'Bạn sẽ không thấy điều này nữa nếu bạn đăng ký.',
-  'legal.modal.goToSettings': 'Đi đến cài đặt của bạn',
-  'legal.modal.oneLastThing': 'Một điều cuối cùng...',
-  'legal.modal.finish': 'Hoàn thành',
-  'legal.modal.existingUser': 'Đã đăng ký?',
-  'legal.modal.pleaseLogin': 'Vui lòng đăng nhập.',
-  'legal.publicationAgreements': 'Thỏa thuận xuất bản',
-  'legal.generalPublishingAgreement': 'Thỏa thuận xuất bản chung',
-  'legal.documentsFor': 'Tài liệu pháp lý cho {workTitle}',
-}
+    'legal.modal.intro': 'Xin lỗi vì đã làm phiền. Trước khi bạn tiếp tục điều gì khác, chúng tôi có nghĩa vụ pháp lý để đảm bảo bạn nhận thức và đồng ý với Điều khoản Dịch vụ, Chính sách Bảo mật và Thỏa thuận Bản quyền của chúng tôi.',
+    'legal.modal.agree': 'Tôi Đồng ý',
+    'legal.modal.disagree': 'Tôi Không Đồng ý',
+    'legal.modal.register': 'Bạn sẽ không thấy điều này nữa nếu bạn đăng ký.',
+    'legal.modal.goToSettings': 'Đi đến cài đặt của bạn',
+    'legal.modal.oneLastThing': 'Một điều cuối cùng...',
+    'legal.modal.finish': 'Hoàn thành',
+    'legal.modal.existingUser': 'Đã đăng ký?',
+    'legal.modal.pleaseLogin': 'Vui lòng đăng nhập.',
+    'legal.publicationAgreements': 'Thỏa thuận xuất bản',
+    'legal.generalPublishingAgreement': 'Thỏa thuận xuất bản chung',
+    'legal.documentsFor': 'Tài liệu pháp lý cho {workTitle}'
+};
