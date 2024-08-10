bc62ba4d7a117667b920a6e694b3ea36831e76ec
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:20:18 +0200

    New translations pm.js (Italian)

--- it-IT/pm.js
+++ it-IT/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'Chat',
-  'pm.showolder': 'Mostra messaggi più vecchi',
-  'pm.errors.saysomething': 'Dovresti davvero dire qualcosa...',
-  'pm.errors.addrecipient': 'È necessario aggiungere almeno un altro utente.',
-  'pm.message': 'Messaggio',
-  'pm.send.message': 'Invia messaggio',
-  'pm.messages.own': 'I tuoi messaggi',
-  'pm.conversations.none': 'Al momento non stai parlando con nessuno.',
-  'pm.to': 'A:',
-  'pm.with': 'Chat con:',
-  'pm.send': 'Invia',
-  'pm.conversations.unreadCount': `Hai {count, plural,
+    'pm.conversation': 'Chat',
+    'pm.showolder': 'Mostra messaggi più vecchi',
+    'pm.errors.saysomething': 'Dovresti davvero dire qualcosa...',
+    'pm.errors.addrecipient': 'È necessario aggiungere almeno un altro utente.',
+    'pm.message': 'Messaggio',
+    'pm.send.message': 'Invia messaggio',
+    'pm.messages.own': 'I tuoi messaggi',
+    'pm.conversations.none': 'Al momento non stai parlando con nessuno.',
+    'pm.to': 'A:',
+    'pm.with': 'Chat con:',
+    'pm.send': 'Invia',
+    'pm.conversations.unreadCount': `Hai {count, plural,
     zero {nessun messaggio non letto}
     one {un messaggio non letto}
     other {# messaggi non letti}
   }.`,
-  'pm.form.minimumParticipants': 'È necessario scegliere un destinatario.',
-  'pm.form.maxParticipants': `Puoi avere al massimo {maxPeople, plural,
+    'pm.form.minimumParticipants': 'È necessario scegliere un destinatario.',
+    'pm.form.maxParticipants': `Puoi avere al massimo {maxPeople, plural,
     one {una persona}
     other {# persone}
   } in una conversazione.`,
-  'pm.new.aria': 'Nuova chat',
-  'pm.new.buttonTitle': 'Avvia una nuova chat',
-}
+    'pm.new.aria': 'Nuova chat',
+    'pm.new.buttonTitle': 'Avvia una nuova chat'
+};
