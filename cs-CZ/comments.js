62066da714016451ce96f739a17a753ff2e9b446
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:15:17 +0200

    New translations comments.js (Czech)

--- cs-CZ/comments.js
+++ cs-CZ/comments.js
@@ -1,11 +1,11 @@
 export default {
-  'comment.write': 'Napište svůj komentář',
-  'comments.none': 'Omlouváme se, ale nemáme žádné komentáře k zobrazení.',
-  'comments.total': `Celkem {count, plural,
+    'comment.write': 'Napište svůj komentář',
+    'comments.none': 'Omlouváme se, ale nemáme žádné komentáře k zobrazení.',
+    'comments.total': `Celkem {count, plural,
     zero {0 komentářů}
     one {1 komentář}
     few {# komentáře}
     other {# komentářů}
   }.`,
-  'comments.show.older': 'Zobrazit starší komentáře',
-}
+    'comments.show.older': 'Zobrazit starší komentáře'
+};
