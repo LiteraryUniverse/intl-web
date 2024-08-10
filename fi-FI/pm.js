3e885ec60d3fc98fc245d26d023070aa47cad2db
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:17:31 +0200

    New translations pm.js (Finnish)

--- fi-FI/pm.js
+++ fi-FI/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'Keskustelu',
-  'pm.showolder': 'Näytä vanhemmat viestit',
-  'pm.errors.saysomething': 'Sinun pitäisi todella sanoa jotain...',
-  'pm.errors.addrecipient': 'Sinun on lisättävä vähintään yksi muu käyttäjä.',
-  'pm.message': 'Viesti',
-  'pm.send.message': 'Lähetä viesti',
-  'pm.messages.own': 'Sinun viestisi',
-  'pm.conversations.none': 'Et keskustele tällä hetkellä kenenkään kanssa.',
-  'pm.to': 'Vastaanottaja:',
-  'pm.with': 'Keskustelu henkilön kanssa:',
-  'pm.send': 'Lähetä',
-  'pm.conversations.unreadCount': `Sinulla on {count, plural,
+    'pm.conversation': 'Keskustelu',
+    'pm.showolder': 'Näytä vanhemmat viestit',
+    'pm.errors.saysomething': 'Sinun pitäisi todella sanoa jotain...',
+    'pm.errors.addrecipient': 'Sinun on lisättävä vähintään yksi muu käyttäjä.',
+    'pm.message': 'Viesti',
+    'pm.send.message': 'Lähetä viesti',
+    'pm.messages.own': 'Sinun viestisi',
+    'pm.conversations.none': 'Et keskustele tällä hetkellä kenenkään kanssa.',
+    'pm.to': 'Vastaanottaja:',
+    'pm.with': 'Keskustelu henkilön kanssa:',
+    'pm.send': 'Lähetä',
+    'pm.conversations.unreadCount': `Sinulla on {count, plural,
     zero {ei lukematonta keskustelua}
     one {yksi lukematon keskustelu}
     other {# lukematonta keskustelua}
   }.`,
-  'pm.form.minimumParticipants': 'Sinun on valittava vastaanottaja.',
-  'pm.form.maxParticipants': `Voit olla enintään {maxPeople, plural,
+    'pm.form.minimumParticipants': 'Sinun on valittava vastaanottaja.',
+    'pm.form.maxParticipants': `Voit olla enintään {maxPeople, plural,
     one {yksi henkilö}
     other {# henkilöä}
   } keskustelussa.`,
-  'pm.new.aria': 'Uusi keskustelu',
-  'pm.new.buttonTitle': 'Aloita uusi keskustelu',
-}
+    'pm.new.aria': 'Uusi keskustelu',
+    'pm.new.buttonTitle': 'Aloita uusi keskustelu'
+};
