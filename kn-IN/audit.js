30c1a0c68c0972a85f84bc7cb74dd9c036dd77be
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:38:52 +0200

    New translations audit.js (Kannada)

--- kn-IN/audit.js
+++ kn-IN/audit.js
@@ -1,6 +1,6 @@
 export default {
-  'auditLog.header.date': 'ದಿನಾಂಕ',
-  'auditLog.header.action': 'ಏನು',
-  'auditLog.header.group': 'ಗುಂಪು',
-  'auditLog.header.user': 'ಯಾರು',
-}
+    'auditLog.header.date': 'ದಿನಾಂಕ',
+    'auditLog.header.action': 'ಏನು',
+    'auditLog.header.group': 'ಗುಂಪು',
+    'auditLog.header.user': 'ಯಾರು'
+};
