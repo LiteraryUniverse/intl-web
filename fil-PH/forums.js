18baec2eb64437bec116b010d8770ed5daea132d
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:36:45 +0200

    New translations forums.js (Filipino)

--- fil-PH/forums.js
+++ fil-PH/forums.js
@@ -1,8 +1,7 @@
 export default {
   'forums.create': 'Paganahin ang forum',
   'forums.archive': 'I-archive ang forum',
-  'forums.archive.explained':
-    'Ang pagsasara ng forum ay magpapigil sa mga gumagamit na mag-post ng anumang karagdagang nilalaman, ngunit mananatiling nakikita ito.',
+  'forums.archive.explained': 'Ang pagsasara ng forum ay magpapigil sa mga gumagamit na mag-post ng anumang karagdagang nilalaman, ngunit mananatiling nakikita ito.',
   'forums.archive.success': 'Ang forum ay na-archives na',
   'forums.unArchive': 'I-re-activate ang mga forum',
   'forums.unArchive.success': 'Ang forum ay na-re-activate na!',
@@ -12,24 +11,18 @@ export default {
   'forums.category.name': 'Pangalan ng kategorya',
   'forums.category.slug': 'Bahagi ng SEO URL',
   'forums.category.settings': 'Mga setting ng kategorya',
-  'forums.category.language':
-    'Limitahan ang pagpapakita ng kategoryang ito sa sumusunod na wika. Iwanan bilang walang laman kung nais mong ipakita ang kategorya sa lahat ng wika.',
-  'forums.category.intlNameDescription':
-    'May mga lokal na pamagat ng pangkaraniwang mga pangalan ng kategorya para sa iyong pagpili, upang ang kategorya ay magamit sa lahat ng suportadong wika.',
+  'forums.category.language': 'Limitahan ang pagpapakita ng kategoryang ito sa sumusunod na wika. Iwanan bilang walang laman kung nais mong ipakita ang kategorya sa lahat ng wika.',
+  'forums.category.intlNameDescription': 'May mga lokal na pamagat ng pangkaraniwang mga pangalan ng kategorya para sa iyong pagpili, upang ang kategorya ay magamit sa lahat ng suportadong wika.',
   'forums.thread.new': 'Bagong thread',
   'forums.thread.delete': 'Tanggalin ang thread',
   'forums.post.new': 'Bagong post',
-  'forums.archived.forum':
-    'Ang forums na ito ay na-archives sa {archivedAt, date, long}. Maaari ka pa ring tingnan ang mga forums, ngunit hindi ka na makakagawa ng bagong nilalaman.',
+  'forums.archived.forum': 'Ang forums na ito ay na-archives sa {archivedAt, date, long}. Maaari ka pa ring tingnan ang mga forums, ngunit hindi ka na makakagawa ng bagong nilalaman.',
   'forums.category.archive': 'I-archive ang kategorya',
-  'forums.category.archiveExplained':
-    'Ang kategoryang walang mga thread ay mabubura, samantalang ang mga kategoryang may laman ay magpapigil sa paglikha ng bagong mga thread.',
+  'forums.category.archiveExplained': 'Ang kategoryang walang mga thread ay mabubura, samantalang ang mga kategoryang may laman ay magpapigil sa paglikha ng bagong mga thread.',
   'forums.category.reactivate': 'I-re-activate ang kategorya',
   'forums.category.isVotable': 'Payagan ang mga user na bumoto pataas/pababa sa mga thread',
-  'forums.category.titleIsGlobal':
-    'Ang pamagat ng kategoryang ito ay ipapakita sa wika ng user kung suportado natin ito.',
-  'forums.category.languageLimited':
-    'Ang kategoryang ito ay ipapakita lamang sa mga user sa wikang ito.',
+  'forums.category.titleIsGlobal': 'Ang pamagat ng kategoryang ito ay ipapakita sa wika ng user kung suportado natin ito.',
+  'forums.category.languageLimited': 'Ang kategoryang ito ay ipapakita lamang sa mga user sa wikang ito.',
   'forums.threadsCounter': 'Kabuuang bilang ng mga paksa at talakayan',
   // Pre-defined category names
   'forums.category.intlName.general': 'Pangkalahatan',
@@ -49,13 +42,10 @@ export default {
   'forums.category.intlName.questions': 'Mga Tanong',
   'forums.category.back': 'Bumalik sa pangkalahatang-ideya ng forum',
   // Forum restrictions
-  'forums.forumRestriction.usersOnly':
-    'Pahintulutan lamang ang pag-access ng mga naka-rehistrong user',
+  'forums.forumRestriction.usersOnly': 'Pahintulutan lamang ang pag-access ng mga naka-rehistrong user',
   'forums.settings.accessControls': 'Mga kontrol sa pag-access',
-  'forums.forumRestrictionsMessages.usersOnlyForum':
-    'Ang forum na ito ay ma-access lamang ng mga rehistradong gumagamit.',
-  'forums.noThreads':
-    'Walang makikita dito. Nasa iyo na ang desisyon kung mag-uumpisa ka ng pag-uusap.',
+  'forums.forumRestrictionsMessages.usersOnlyForum': 'Ang forum na ito ay ma-access lamang ng mga rehistradong gumagamit.',
+  'forums.noThreads': 'Walang makikita dito. Nasa iyo na ang desisyon kung mag-uumpisa ka ng pag-uusap.',
   'forums.noPosts': 'Walang sumagot pa. Ikaw na ang unang sumagot!',
   // Threads
   'forums.createThread': 'Lumikha ng bagong talakayan',
@@ -74,5 +64,5 @@ export default {
     one {tugon}
     other {mga tugon}
   }`,
-  'forums.thread.createdAt': 'Nilikha noong {createdAt, date, medium}',
-}
+  'forums.thread.createdAt': 'Nilikha noong {createdAt, date, medium}'
+};
