c0a56ff83044335d18fc67db391cd1414f2a503e
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:20:28 +0200

    New translations user.js (Italian)

--- it-IT/user.js
+++ it-IT/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'Aggiungi agli amici',
-  'user.friend.requests': `You have {num, number} new friend {num, plural,
+    'user.friend.request.add': 'Aggiungi agli amici',
+    'user.friend.requests': `You have {num, number} new friend {num, plural,
     zero {requests}
     one {request}
     many {requests}
     other {requests}
   }`,
-  'user.friend.unfriend': 'Rimuovi',
-  'user.block': 'Blocca',
-  'user.unblock': 'Sblocca',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'Rimuovi',
+    'user.block': 'Blocca',
+    'user.unblock': 'Sblocca',
+    'user.joined': `{gender, select,
     male {Aggiunto}
     female {Aggiunta}
 other {Aggiunti}} il {date, date,long}`,
-  'user.friend.request.accept': 'Accetta richiesta di amicizia',
-  'user.friend.request.cancel': 'Cancella richiesta di amicizia',
-  'user.friend.request.deny': 'Rifiuta amicizia',
-  'user.listing': 'Lista utenti',
-  'user.profile.visit': 'Visita il profilo di {user}.',
-  'user.profile.avatar': 'Avatar di {username}',
-  'user.friend.list.requests': 'Richieste di amicizia',
-  'user.friend.norequests': 'Non hai richieste di amicizia.',
-  'user.dashboardWelcome': `Buona {dayPart, select,
+    'user.friend.request.accept': 'Accetta richiesta di amicizia',
+    'user.friend.request.cancel': 'Cancella richiesta di amicizia',
+    'user.friend.request.deny': 'Rifiuta amicizia',
+    'user.listing': 'Lista utenti',
+    'user.profile.visit': 'Visita il profilo di {user}.',
+    'user.profile.avatar': 'Avatar di {username}',
+    'user.friend.list.requests': 'Richieste di amicizia',
+    'user.friend.norequests': 'Non hai richieste di amicizia.',
+    'user.dashboardWelcome': `Buona {dayPart, select,
     morning {mattinata}
     afternoon {giornata}
     evening {sera}
     night {notte}
     other {giornata}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'Questo autore è stato pubblicato in modo tradizionale',
-  'user.verification.luAuthor': 'Noto autore di Literary Universe',
-  'user.verification.luEmployee': 'Dipendente di Literary Universe',
-  'user.verification.publicFigure': 'Figura pubblica verificata',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'Questo autore è stato pubblicato in modo tradizionale',
+    'user.verification.luAuthor': 'Noto autore di Literary Universe',
+    'user.verification.luEmployee': 'Dipendente di Literary Universe',
+    'user.verification.publicFigure': 'Figura pubblica verificata',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {story}
     other {stories}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {universe}
     other {universes}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {fan fiction}
     other {fan fictions} 
   }`,
-  'profile.works': 'Opere',
-  // As in creative/literary works
-  'profile.wishlist': 'Lista dei desideri',
-  'profile.storiesBy': 'Storie create da {username}',
-  'profile.universesBy': 'Universi creati da {username}',
-  'profile.fanFictionBy': 'Fan Fiction create da {username}',
-}
+    'profile.works': 'Opere',
+    // As in creative/literary works
+    'profile.wishlist': 'Lista dei desideri',
+    'profile.storiesBy': 'Storie create da {username}',
+    'profile.universesBy': 'Universi creati da {username}',
+    'profile.fanFictionBy': 'Fan Fiction create da {username}'
+};
