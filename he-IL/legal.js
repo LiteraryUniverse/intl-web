4ec1d1ac53289ee43273a2d0948fb25849bbf5e8
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:19:18 +0200

    New translations legal.js (Hebrew)

--- he-IL/legal.js
+++ he-IL/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'מצטערים על ההפרעה. אך לפני שנתנגש יותר, אנחנו מחוייבים משפטית לוודא שאתה מודע ומסכים לתנאי השימוש שלנו, מדיניות הפרטיות וההסכם על זכויות יוצרים.',
-  'legal.modal.agree': 'אני מסכים',
-  'legal.modal.disagree': 'אני לא מסכים',
-  'legal.modal.register': 'לא תראה זאת יותר אם תרשם.',
-  'legal.modal.goToSettings': 'עבור להגדרות',
-  'legal.modal.oneLastThing': 'דבר אחרון...',
-  'legal.modal.finish': 'סיום',
-  'legal.modal.existingUser': 'כבר רשום?',
-  'legal.modal.pleaseLogin': 'אנא התחבר.',
-  'legal.publicationAgreements': 'הסכמי פרסום',
-  'legal.generalPublishingAgreement': 'הסכם פרסום כללי',
-  'legal.documentsFor': 'מסמכים משפטיים עבור {workTitle}',
-}
+    'legal.modal.intro': 'מצטערים על ההפרעה. אך לפני שנתנגש יותר, אנחנו מחוייבים משפטית לוודא שאתה מודע ומסכים לתנאי השימוש שלנו, מדיניות הפרטיות וההסכם על זכויות יוצרים.',
+    'legal.modal.agree': 'אני מסכים',
+    'legal.modal.disagree': 'אני לא מסכים',
+    'legal.modal.register': 'לא תראה זאת יותר אם תרשם.',
+    'legal.modal.goToSettings': 'עבור להגדרות',
+    'legal.modal.oneLastThing': 'דבר אחרון...',
+    'legal.modal.finish': 'סיום',
+    'legal.modal.existingUser': 'כבר רשום?',
+    'legal.modal.pleaseLogin': 'אנא התחבר.',
+    'legal.publicationAgreements': 'הסכמי פרסום',
+    'legal.generalPublishingAgreement': 'הסכם פרסום כללי',
+    'legal.documentsFor': 'מסמכים משפטיים עבור {workTitle}'
+};
