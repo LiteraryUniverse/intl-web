466c8a7a8e5e922a46d8ae25f7d2bf00b40cc634
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:33:12 +0200

    New translations pm.js (Thai)

--- th-TH/pm.js
+++ th-TH/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'การสนทนา',
-  'pm.showolder': 'แสดงข้อความเก่ากว่า',
-  'pm.errors.saysomething': 'คุณควรพูดอะไรบางอย่าง...',
-  'pm.errors.addrecipient': 'คุณต้องเพิ่มผู้รับอย่างน้อยหนึ่งคน',
-  'pm.message': 'ข้อความ',
-  'pm.send.message': 'ส่งข้อความ',
-  'pm.messages.own': 'ข้อความของคุณ',
-  'pm.conversations.none': 'คุณไม่ได้สนทนากับใครในขณะนี้',
-  'pm.to': 'ถึง:',
-  'pm.with': 'สนทนากับ:',
-  'pm.send': 'ส่ง',
-  'pm.conversations.unreadCount': `คุณมี {count, plural,
+    'pm.conversation': 'การสนทนา',
+    'pm.showolder': 'แสดงข้อความเก่ากว่า',
+    'pm.errors.saysomething': 'คุณควรพูดอะไรบางอย่าง...',
+    'pm.errors.addrecipient': 'คุณต้องเพิ่มผู้รับอย่างน้อยหนึ่งคน',
+    'pm.message': 'ข้อความ',
+    'pm.send.message': 'ส่งข้อความ',
+    'pm.messages.own': 'ข้อความของคุณ',
+    'pm.conversations.none': 'คุณไม่ได้สนทนากับใครในขณะนี้',
+    'pm.to': 'ถึง:',
+    'pm.with': 'สนทนากับ:',
+    'pm.send': 'ส่ง',
+    'pm.conversations.unreadCount': `คุณมี {count, plural,
     zero {การสนทนาที่ยังไม่ได้อ่าน}
     one {การสนทนาที่ยังไม่ได้อ่าน 1 รายการ}
     other {การสนทนาที่ยังไม่ได้อ่าน # รายการ}
   }.`,
-  'pm.form.minimumParticipants': 'คุณต้องเลือกผู้รับ',
-  'pm.form.maxParticipants': `คุณสามารถมีคนได้สูงสุด {maxPeople, plural,
+    'pm.form.minimumParticipants': 'คุณต้องเลือกผู้รับ',
+    'pm.form.maxParticipants': `คุณสามารถมีคนได้สูงสุด {maxPeople, plural,
     one {คน}
     other {# คน}
   } ในการสนทนา`,
-  'pm.new.aria': 'สนทนาใหม่',
-  'pm.new.buttonTitle': 'เริ่มสนทนาใหม่',
-}
+    'pm.new.aria': 'สนทนาใหม่',
+    'pm.new.buttonTitle': 'เริ่มสนทนาใหม่'
+};
