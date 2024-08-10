f1338d55aae578eaeec934d7b9ce8828c764a3d5
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:37:02 +0200

    New translations licenses.js (Lojban)

--- jbo-EN/licenses.js
+++ jbo-EN/licenses.js
@@ -3,37 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': "Cisni tu'a dunda",
-  'licenses.more': "Moi je'u",
-  'licenses.legal': 'Klesi jdini',
-  'licenses.name.cc-by':
-    "lo Cisni tu'a dunda cu se zbasu lo ka se benji lo drata cu'i lo nu kargau lo do te'i jufra. ti cu zmadu lo drata klesi tu'a dunda",
-  'licenses.cc-by':
-    "lo Cisni tu'a dunda cu se zbasu lo ka se benji lo drata cu'i lo nu kargau lo do te'i jufra. ti cu zmadu lo drata klesi tu'a dunda. cmavo lo nu kargau lo drata",
-  'licenses.name.cc-by-sa':
-    "lo Cisni tu'a dunda cu se zbasu lo ka se benji lo drata cu'i lo nu kargau lo do te'i jufra. ti cu zmadu lo drata klesi tu'a dunda. cmavo lo nu kargau je lo nu simxu lo drata",
-  'licenses.cc-by-sa':
-    "lo Cisni tu'a dunda cu se zbasu lo ka se benji lo drata cu'i lo nu kargau lo do te'i jufra. ti cu zmadu lo drata klesi tu'a dunda. cmavo lo nu kargau je lo nu simxu lo drata",
-  'licenses.name.cc-by-nd':
-    "lo Cisni tu'a dunda cu se zbasu lo ka se benji lo drata cu'i lo nu kargau lo do te'i jufra. ti cu zmadu lo drata klesi tu'a dunda. cmavo lo nu kargau je lo nu na benji lo drata",
-  'licenses.cc-by-nd':
-    "lo Cisni tu'a dunda cu se zbasu lo ka se benji lo drata cu'i lo nu kargau lo do te'i jufra. ti cu zmadu lo drata klesi tu'a dunda. cmavo lo nu kargau je lo nu na benji lo drata",
-  'licenses.name.cc-by-nc':
-    "lo Cisni tu'a dunda cu se zbasu lo ka se benji lo drata cu'i lo nu kargau lo do te'i jufra. ti cu zmadu lo drata klesi tu'a dunda. cmavo lo nu kargau je lo nu na simxu lo drata",
-  'licenses.cc-by-nc':
-    "lo Cisni tu'a dunda cu se zbasu lo ka se benji lo drata cu'i lo nu kargau lo do te'i jufra. ti cu zmadu lo drata klesi tu'a dunda. cmavo lo nu kargau je lo nu na simxu lo drata",
-  'licenses.name.cc-by-nc-sa':
-    "lo Cisni tu'a dunda cu se zbasu lo ka se benji lo drata cu'i lo nu kargau lo do te'i jufra. ti cu zmadu lo drata klesi tu'a dunda. cmavo lo nu kargau je lo nu na simxu je lo nu simxu lo drata",
-  'licenses.cc-by-nc-sa':
-    "lo Cisni tu'a dunda cu se zbasu lo ka se benji lo drata cu'i lo nu kargau lo do te'i jufra. ti cu zmadu lo drata klesi tu'a dunda. cmavo lo nu kargau je lo nu na simxu je lo nu simxu lo drata",
-  'licenses.name.cc-by-nc-nd':
-    "lo Cisni tu'a dunda cu se zbasu lo ka se benji lo drata cu'i lo nu kargau lo do te'i jufra. ti cu zmadu lo drata klesi tu'a dunda. cmavo lo nu kargau je lo nu na benji je lo nu na simxu lo drata",
-  'licenses.cc-by-nc-nd':
-    "lo Cisni tu'a dunda cu se zbasu lo ka se benji lo drata cu'i lo nu kargau lo do te'i jufra. ti cu zmadu lo drata klesi tu'a dunda. cmavo lo nu kargau je lo nu na benji je lo nu na simxu lo drata",
-  'licenses.name.lu-c': 'lo Klesi jdini be le Valstefni be lo Klesi Krtis',
-  'licenses.lu-c':
-    'Standard full copyrights license that allows you to sell publish your work as you like and have your rights to it protected. This license also gives you the option to sell your stories on Literary Universe.',
-  'licenses.name.cc0': 'No Rights Reserved - CC0 1.0',
-  'licenses.cc0':
-    'Put your work into public domain. In contrast to CC’s licenses that allow copyright holders to choose from a range of permissions while retaining their copyright, CC0 empowers yet another choice altogether – the choice to opt out of copyright and database protection, and the exclusive rights automatically granted to creators – the “no rights reserved” alternative to our licenses.',
-}
+    'licenses.about': 'Cisni tu\'a dunda',
+    'licenses.more': 'Moi je\'u',
+    'licenses.legal': 'Klesi jdini',
+    'licenses.name.cc-by': 'lo Cisni tu\'a dunda cu se zbasu lo ka se benji lo drata cu\'i lo nu kargau lo do te\'i jufra. ti cu zmadu lo drata klesi tu\'a dunda',
+    'licenses.cc-by': 'lo Cisni tu\'a dunda cu se zbasu lo ka se benji lo drata cu\'i lo nu kargau lo do te\'i jufra. ti cu zmadu lo drata klesi tu\'a dunda. cmavo lo nu kargau lo drata',
+    'licenses.name.cc-by-sa': 'lo Cisni tu\'a dunda cu se zbasu lo ka se benji lo drata cu\'i lo nu kargau lo do te\'i jufra. ti cu zmadu lo drata klesi tu\'a dunda. cmavo lo nu kargau je lo nu simxu lo drata',
+    'licenses.cc-by-sa': 'lo Cisni tu\'a dunda cu se zbasu lo ka se benji lo drata cu\'i lo nu kargau lo do te\'i jufra. ti cu zmadu lo drata klesi tu\'a dunda. cmavo lo nu kargau je lo nu simxu lo drata',
+    'licenses.name.cc-by-nd': 'lo Cisni tu\'a dunda cu se zbasu lo ka se benji lo drata cu\'i lo nu kargau lo do te\'i jufra. ti cu zmadu lo drata klesi tu\'a dunda. cmavo lo nu kargau je lo nu na benji lo drata',
+    'licenses.cc-by-nd': 'lo Cisni tu\'a dunda cu se zbasu lo ka se benji lo drata cu\'i lo nu kargau lo do te\'i jufra. ti cu zmadu lo drata klesi tu\'a dunda. cmavo lo nu kargau je lo nu na benji lo drata',
+    'licenses.name.cc-by-nc': 'lo Cisni tu\'a dunda cu se zbasu lo ka se benji lo drata cu\'i lo nu kargau lo do te\'i jufra. ti cu zmadu lo drata klesi tu\'a dunda. cmavo lo nu kargau je lo nu na simxu lo drata',
+    'licenses.cc-by-nc': 'lo Cisni tu\'a dunda cu se zbasu lo ka se benji lo drata cu\'i lo nu kargau lo do te\'i jufra. ti cu zmadu lo drata klesi tu\'a dunda. cmavo lo nu kargau je lo nu na simxu lo drata',
+    'licenses.name.cc-by-nc-sa': 'lo Cisni tu\'a dunda cu se zbasu lo ka se benji lo drata cu\'i lo nu kargau lo do te\'i jufra. ti cu zmadu lo drata klesi tu\'a dunda. cmavo lo nu kargau je lo nu na simxu je lo nu simxu lo drata',
+    'licenses.cc-by-nc-sa': 'lo Cisni tu\'a dunda cu se zbasu lo ka se benji lo drata cu\'i lo nu kargau lo do te\'i jufra. ti cu zmadu lo drata klesi tu\'a dunda. cmavo lo nu kargau je lo nu na simxu je lo nu simxu lo drata',
+    'licenses.name.cc-by-nc-nd': 'lo Cisni tu\'a dunda cu se zbasu lo ka se benji lo drata cu\'i lo nu kargau lo do te\'i jufra. ti cu zmadu lo drata klesi tu\'a dunda. cmavo lo nu kargau je lo nu na benji je lo nu na simxu lo drata',
+    'licenses.cc-by-nc-nd': 'lo Cisni tu\'a dunda cu se zbasu lo ka se benji lo drata cu\'i lo nu kargau lo do te\'i jufra. ti cu zmadu lo drata klesi tu\'a dunda. cmavo lo nu kargau je lo nu na benji je lo nu na simxu lo drata',
+    'licenses.name.lu-c': 'lo Klesi jdini be le Valstefni be lo Klesi Krtis',
+    'licenses.lu-c': 'Standard full copyrights license that allows you to sell publish your work as you like and have your rights to it protected. This license also gives you the option to sell your stories on Literary Universe.',
+    'licenses.name.cc0': 'No Rights Reserved - CC0 1.0',
+    'licenses.cc0': 'Put your work into public domain. In contrast to CC’s licenses that allow copyright holders to choose from a range of permissions while retaining their copyright, CC0 empowers yet another choice altogether – the choice to opt out of copyright and database protection, and the exclusive rights automatically granted to creators – the “no rights reserved” alternative to our licenses.'
+};
