8f4f7698c0ce77048b451bc083870a3daa02f274
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:23:02 +0200

    New translations licenses.js (Dutch)

--- nl-NL/licenses.js
+++ nl-NL/licenses.js
@@ -3,32 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'Over licenties',
-  'licenses.more': 'Meer informatie',
-  'licenses.legal': 'Juridisch document',
-  'licenses.name.cc-by': 'Creative Commons Naamsvermelding 4.0',
-  'licenses.cc-by':
-    'Deze licentie stelt anderen in staat om jouw werk te verspreiden, te remixen, aan te passen en erop voort te bouwen, zelfs voor commerciële doeleinden, zolang ze jou crediteren voor het oorspronkelijke werk. Dit is de meest flexibele licentie die wordt aangeboden. Aanbevolen voor maximale verspreiding en gebruik van gelicentieerd materiaal.',
-  'licenses.name.cc-by-sa': 'Creative Commons Naamsvermelding-GelijkDelen 4.0',
-  'licenses.cc-by-sa':
-    "Deze licentie stelt anderen in staat om jouw werk te remixen, aan te passen en erop voort te bouwen, zelfs voor commerciële doeleinden, zolang ze jou crediteren en hun nieuwe creaties onder identieke voorwaarden licenseren. Deze licentie wordt vaak vergeleken met 'copyleft' vrije en open source softwarelicenties. Alle nieuwe werken die zijn gebaseerd op het jouwe, zullen dezelfde licentie dragen, waardoor afgeleide werken ook commercieel gebruik toestaan. Dit is de licentie die wordt gebruikt door Wikipedia en wordt aanbevolen voor materialen die baat zouden hebben bij het opnemen van inhoud van Wikipedia en soortgelijke gelicentieerde projecten.",
-  'licenses.name.cc-by-nd': 'Creative Commons Naamsvermelding-GeenAfgeleideWerken 4.0',
-  'licenses.cc-by-nd':
-    'Deze licentie staat herdistributie, commercieel en niet-commercieel, toe, zolang het ongewijzigd en in zijn geheel wordt doorgegeven, met vermelding van jou als de maker.',
-  'licenses.name.cc-by-nc': 'Creative Commons Naamsvermelding-NietCommercieel 4.0',
-  'licenses.cc-by-nc':
-    'Deze licentie stelt anderen in staat om jouw werk niet-commercieel te remixen, aan te passen en erop voort te bouwen, en hoewel hun nieuwe werken jou ook moeten erkennen en niet-commercieel moeten zijn, hoeven ze hun afgeleide werken niet onder dezelfde voorwaarden te licenseren.',
-  'licenses.name.cc-by-nc-sa': 'Creative Commons Naamsvermelding-NietCommercieel-GelijkDelen 4.0',
-  'licenses.cc-by-nc-sa':
-    'Deze licentie stelt anderen in staat om jouw werk niet-commercieel te remixen, aan te passen en erop voort te bouwen, zolang ze jou crediteren en hun nieuwe creaties onder identieke voorwaarden licenseren.',
-  'licenses.name.cc-by-nc-nd':
-    'Creative Commons Naamsvermelding-NietCommercieel-GeenAfgeleideWerken 4.0',
-  'licenses.cc-by-nc-nd':
-    'Deze licentie is de meest beperkende van onze zes belangrijkste licenties en staat anderen alleen toe om jouw werken te downloaden en te delen met anderen, zolang ze jou crediteren, maar ze mogen ze op geen enkele manier wijzigen of commercieel gebruiken.',
-  'licenses.name.lu-c': 'Auteursrechten van het Literair Universum',
-  'licenses.lu-c':
-    'Standaard volledige auteursrechtenlicentie waarmee u uw werk kunt verkopen en publiceren zoals u wilt en uw rechten daartoe beschermd worden. Met deze licentie heeft u ook de mogelijkheid om uw verhalen te verkopen op Literary Universe.',
-  'licenses.name.cc0': 'Geen rechten voorbehouden - CC0 1.0',
-  'licenses.cc0':
-    'Plaats uw werk in het publieke domein. In tegenstelling tot de licenties van CC die auteursrechthouders de keuze geven uit een reeks toestemmingen terwijl ze hun auteursrecht behouden, geeft CC0 nog een andere keuze - de keuze om af te zien van auteursrecht en databankbescherming, en de exclusieve rechten die automatisch aan makers worden verleend - de "geen rechten voorbehouden"-alternatief voor onze licenties.',
-}
+    'licenses.about': 'Over licenties',
+    'licenses.more': 'Meer informatie',
+    'licenses.legal': 'Juridisch document',
+    'licenses.name.cc-by': 'Creative Commons Naamsvermelding 4.0',
+    'licenses.cc-by': 'Deze licentie stelt anderen in staat om jouw werk te verspreiden, te remixen, aan te passen en erop voort te bouwen, zelfs voor commerciële doeleinden, zolang ze jou crediteren voor het oorspronkelijke werk. Dit is de meest flexibele licentie die wordt aangeboden. Aanbevolen voor maximale verspreiding en gebruik van gelicentieerd materiaal.',
+    'licenses.name.cc-by-sa': 'Creative Commons Naamsvermelding-GelijkDelen 4.0',
+    'licenses.cc-by-sa': 'Deze licentie stelt anderen in staat om jouw werk te remixen, aan te passen en erop voort te bouwen, zelfs voor commerciële doeleinden, zolang ze jou crediteren en hun nieuwe creaties onder identieke voorwaarden licenseren. Deze licentie wordt vaak vergeleken met \'copyleft\' vrije en open source softwarelicenties. Alle nieuwe werken die zijn gebaseerd op het jouwe, zullen dezelfde licentie dragen, waardoor afgeleide werken ook commercieel gebruik toestaan. Dit is de licentie die wordt gebruikt door Wikipedia en wordt aanbevolen voor materialen die baat zouden hebben bij het opnemen van inhoud van Wikipedia en soortgelijke gelicentieerde projecten.',
+    'licenses.name.cc-by-nd': 'Creative Commons Naamsvermelding-GeenAfgeleideWerken 4.0',
+    'licenses.cc-by-nd': 'Deze licentie staat herdistributie, commercieel en niet-commercieel, toe, zolang het ongewijzigd en in zijn geheel wordt doorgegeven, met vermelding van jou als de maker.',
+    'licenses.name.cc-by-nc': 'Creative Commons Naamsvermelding-NietCommercieel 4.0',
+    'licenses.cc-by-nc': 'Deze licentie stelt anderen in staat om jouw werk niet-commercieel te remixen, aan te passen en erop voort te bouwen, en hoewel hun nieuwe werken jou ook moeten erkennen en niet-commercieel moeten zijn, hoeven ze hun afgeleide werken niet onder dezelfde voorwaarden te licenseren.',
+    'licenses.name.cc-by-nc-sa': 'Creative Commons Naamsvermelding-NietCommercieel-GelijkDelen 4.0',
+    'licenses.cc-by-nc-sa': 'Deze licentie stelt anderen in staat om jouw werk niet-commercieel te remixen, aan te passen en erop voort te bouwen, zolang ze jou crediteren en hun nieuwe creaties onder identieke voorwaarden licenseren.',
+    'licenses.name.cc-by-nc-nd': 'Creative Commons Naamsvermelding-NietCommercieel-GeenAfgeleideWerken 4.0',
+    'licenses.cc-by-nc-nd': 'Deze licentie is de meest beperkende van onze zes belangrijkste licenties en staat anderen alleen toe om jouw werken te downloaden en te delen met anderen, zolang ze jou crediteren, maar ze mogen ze op geen enkele manier wijzigen of commercieel gebruiken.',
+    'licenses.name.lu-c': 'Auteursrechten van het Literair Universum',
+    'licenses.lu-c': 'Standaard volledige auteursrechtenlicentie waarmee u uw werk kunt verkopen en publiceren zoals u wilt en uw rechten daartoe beschermd worden. Met deze licentie heeft u ook de mogelijkheid om uw verhalen te verkopen op Literary Universe.',
+    'licenses.name.cc0': 'Geen rechten voorbehouden - CC0 1.0',
+    'licenses.cc0': 'Plaats uw werk in het publieke domein. In tegenstelling tot de licenties van CC die auteursrechthouders de keuze geven uit een reeks toestemmingen terwijl ze hun auteursrecht behouden, geeft CC0 nog een andere keuze - de keuze om af te zien van auteursrecht en databankbescherming, en de exclusieve rechten die automatisch aan makers worden verleend - de "geen rechten voorbehouden"-alternatief voor onze licenties.'
+};
