acff0fb832af00d7febc6ae70f70c65256e17c07
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:16:35 +0200

    New translations notifications.js (German)

--- de-DE/notifications.js
+++ de-DE/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'Starte die Anzeige dieser Nachricht um',
   'flashnews.endsAt': 'Diese Nachricht nicht mehr anzeigen bei',
   'flashnews.newLanguage': 'Neue Sprache zum Hinzufügen auswählen',
-  'flashnews.onlyDisplayOn':
-    'Die Nachrichten werden nur in den unten ausgewählten Sprachen angezeigt (falls vorhanden), in anderen Sprachen werden keine Nachrichten angezeigt, auch nicht in der Standardsprache.',
+  'flashnews.onlyDisplayOn': 'Die Nachrichten werden nur in den unten ausgewählten Sprachen angezeigt (falls vorhanden), in anderen Sprachen werden keine Nachrichten angezeigt, auch nicht in der Standardsprache.',
   'notifications.menu.all': 'Alle Benachrichtigungen',
   'notifications.menu.following': 'Folgt',
   'notifications.menu.workshop': 'Werkstatt',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'Als gelesen markieren',
   'notifications.menu.delete': 'Benachrichtigung löschen',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'Ihre Geschichte {storyName} wurde nach 6 Monaten Inaktivität automatisch in den Hiatus versetzt.',
-  'notification.workshopStoryStatus.toCancelled':
-    'Ihre Geschichte {storyName} wurde nach einem Jahr Inaktivität automatisch auf "gelöscht" gesetzt.',
+  'notification.workshopStoryStatus.toHiatus': 'Ihre Geschichte {storyName} wurde nach 6 Monaten Inaktivität automatisch in den Hiatus versetzt.',
+  'notification.workshopStoryStatus.toCancelled': 'Ihre Geschichte {storyName} wurde nach einem Jahr Inaktivität automatisch auf "gelöscht" gesetzt.',
   'notification.followingStoryStatus.toHiatus': '{storyName} ist auf Hiatus.',
   'notification.followingStoryStatus.toCancelled': '{storyName} wurde abgebrochen.',
   'notification.followingNewChapter.new': 'Neues Kapitel in {storyName} wurde veröffentlicht!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'hat dir eine Freundschaftsanfrage gesendet.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Zur Geschichte gehen',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'Zur Geschichte gehen'
+};
