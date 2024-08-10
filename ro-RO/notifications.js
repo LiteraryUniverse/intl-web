27deb4d98edb87eb1dde9596952cd6347596171c
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:41:41 +0200

    New translations notifications.js (Romanian)

--- ro-RO/notifications.js
+++ ro-RO/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'Începeți afișarea acestui mesaj la',
   'flashnews.endsAt': 'Opriți afișarea acestui mesaj la',
   'flashnews.newLanguage': 'Selectați o limbă nouă pentru adăugare',
-  'flashnews.onlyDisplayOn':
-    'Știrile vor fi afișate doar în limbile selectate mai jos (dacă este selectată o limbă), nu vor fi afișate știri nici măcar în limba implicită, în alte limbi.',
+  'flashnews.onlyDisplayOn': 'Știrile vor fi afișate doar în limbile selectate mai jos (dacă este selectată o limbă), nu vor fi afișate știri nici măcar în limba implicită, în alte limbi.',
   'notifications.menu.all': 'Toate notificările',
   'notifications.menu.following': 'Urmăririle',
   'notifications.menu.workshop': 'Ateliere',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'Marchează ca citită',
   'notifications.menu.delete': 'Șterge notificarea',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'Povestea dvs. {storyName} a fost setată automat pe pauză după 6 luni de inactivitate.',
-  'notification.workshopStoryStatus.toCancelled':
-    'Povestea dvs. {storyName} a fost setată automat pe anulată după un an de inactivitate.',
+  'notification.workshopStoryStatus.toHiatus': 'Povestea dvs. {storyName} a fost setată automat pe pauză după 6 luni de inactivitate.',
+  'notification.workshopStoryStatus.toCancelled': 'Povestea dvs. {storyName} a fost setată automat pe anulată după un an de inactivitate.',
   'notification.followingStoryStatus.toHiatus': '{storyName} este în pauză.',
   'notification.followingStoryStatus.toCancelled': '{storyName} a fost anulată.',
   'notification.followingNewChapter.new': 'Capitolul nou în {storyName} a fost publicat!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'ți-a trimis o cerere de prietenie.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Du-te la poveste',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'Du-te la poveste'
+};
