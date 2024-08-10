0b7f22494cbca1733b6c3a50a2e918abd6cd4c7b
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:32:05 +0200

    New translations pm.js (Bengali)

--- bn-BD/pm.js
+++ bn-BD/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'কথোপকথন',
-  'pm.showolder': 'পুরানো মেসেজগুলি দেখান',
-  'pm.errors.saysomething': 'আপনার কিছু বলা উচিত...',
-  'pm.errors.addrecipient': 'আপনাকে কমপক্ষে একজন অন্য ব্যবহারকারী যোগ করতে হবে।',
-  'pm.message': 'বার্তা',
-  'pm.send.message': 'বার্তা পাঠান',
-  'pm.messages.own': 'আপনার বার্তা',
-  'pm.conversations.none': 'আপনি বর্তমানে কারো সঙ্গে কথা বলছেন না।',
-  'pm.to': 'প্রাপক:',
-  'pm.with': 'সঙ্গে কথা বলছেন:',
-  'pm.send': 'পাঠান',
-  'pm.conversations.unreadCount': `You have {count, plural,
+    'pm.conversation': 'কথোপকথন',
+    'pm.showolder': 'পুরানো মেসেজগুলি দেখান',
+    'pm.errors.saysomething': 'আপনার কিছু বলা উচিত...',
+    'pm.errors.addrecipient': 'আপনাকে কমপক্ষে একজন অন্য ব্যবহারকারী যোগ করতে হবে।',
+    'pm.message': 'বার্তা',
+    'pm.send.message': 'বার্তা পাঠান',
+    'pm.messages.own': 'আপনার বার্তা',
+    'pm.conversations.none': 'আপনি বর্তমানে কারো সঙ্গে কথা বলছেন না।',
+    'pm.to': 'প্রাপক:',
+    'pm.with': 'সঙ্গে কথা বলছেন:',
+    'pm.send': 'পাঠান',
+    'pm.conversations.unreadCount': `You have {count, plural,
     zero {no unread conversations}
     one {one unread conversation}
     other {# unread conversations}
   }.`,
-  'pm.form.minimumParticipants': 'আপনাকে প্রাপক নির্বাচন করতে হবে।',
-  'pm.form.maxParticipants': `আপনি একটি কথোপকথনে সর্বাধিক {maxPeople, plural,
+    'pm.form.minimumParticipants': 'আপনাকে প্রাপক নির্বাচন করতে হবে।',
+    'pm.form.maxParticipants': `আপনি একটি কথোপকথনে সর্বাধিক {maxPeople, plural,
     one {একজন ব্যক্তি রাখতে পারেন}
     other {# ব্যক্তি}
   } ।`,
-  'pm.new.aria': 'নতুন কথোপকথন',
-  'pm.new.buttonTitle': 'নতুন কথোপকথন শুরু করুন',
-}
+    'pm.new.aria': 'নতুন কথোপকথন',
+    'pm.new.buttonTitle': 'নতুন কথোপকথন শুরু করুন'
+};
