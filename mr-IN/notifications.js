bcc4d0029419cd129ac7ed493932f56b37051438
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:32:52 +0200

    New translations notifications.js (Marathi)

--- mr-IN/notifications.js
+++ mr-IN/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'सुरुवात करा ही संदेशाची प्रदर्शने',
   'flashnews.endsAt': 'निरोप देण्यास सुरु ठेवा हे संदेश',
   'flashnews.newLanguage': 'नवीन भाषा निवडा घेण्यासाठी',
-  'flashnews.onlyDisplayOn':
-    'स्वतःची काही भाषांवर किंवा निवडलेल्या कोणत्या-त्या भाषांवर फक्त ही समाचार दिसणारच आहेत. इतर तीव्रतेने शिवार नसलेल्या त्या भाषांवरच समाचार दिसणार नाहीत.',
+  'flashnews.onlyDisplayOn': 'स्वतःची काही भाषांवर किंवा निवडलेल्या कोणत्या-त्या भाषांवर फक्त ही समाचार दिसणारच आहेत. इतर तीव्रतेने शिवार नसलेल्या त्या भाषांवरच समाचार दिसणार नाहीत.',
   'notifications.menu.all': 'सर्व सूचना',
   'notifications.menu.following': 'अनुसरण करणारे',
   'notifications.menu.workshop': 'कार्यशाळा',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'वाचायला चिन्हांकित करा',
   'notifications.menu.delete': 'सूचना हटवा',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'आपली कथा {storyName} अक्रिय यानंतर 6 महिन्यांनी स्वतःची गैरसक्रिय केली आहे.',
-  'notification.workshopStoryStatus.toCancelled':
-    'आपली कथा {storyName} अक्रिय यानंतर एक वर्षानंतर स्वतःची रद्द केली गेली आहे.',
+  'notification.workshopStoryStatus.toHiatus': 'आपली कथा {storyName} अक्रिय यानंतर 6 महिन्यांनी स्वतःची गैरसक्रिय केली आहे.',
+  'notification.workshopStoryStatus.toCancelled': 'आपली कथा {storyName} अक्रिय यानंतर एक वर्षानंतर स्वतःची रद्द केली गेली आहे.',
   'notification.followingStoryStatus.toHiatus': '{storyName} अवक्रम आहे.',
   'notification.followingStoryStatus.toCancelled': '{storyName} रद्द केले गेले आहे.',
   'notification.followingNewChapter.new': '{storyName} च्या अंतर्गत नवीन अध्याय प्रकाशित झाला आहे!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'तुला मित्र विनंती पाठविली आहे.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'कथेत जा',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'कथेत जा'
+};
