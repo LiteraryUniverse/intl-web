b256b31e25cd0bc6d739ab0a967f6bb448ddb2d5
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:13:41 +0200

    New translations forums.js (Afrikaans)

--- af-ZA/forums.js
+++ af-ZA/forums.js
@@ -1,8 +1,7 @@
 export default {
   'forums.create': 'Aktiveer forum',
   'forums.archive': 'Argief forum',
-  'forums.archive.explained':
-    'Die argivering van die forum sal voorkom dat gebruikers enige verdere inhoud pos, maar dit sal sigbaar bly.',
+  'forums.archive.explained': 'Die argivering van die forum sal voorkom dat gebruikers enige verdere inhoud pos, maar dit sal sigbaar bly.',
   'forums.archive.success': 'Forum is ge-argiveer',
   'forums.unArchive': 'Heraktiveer forums',
   'forums.unArchive.success': 'Forum is her-geaktiveer!',
@@ -12,24 +11,18 @@ export default {
   'forums.category.name': 'Kategorienaam',
   'forums.category.slug': 'SEO-url-deel',
   'forums.category.settings': 'Kategorie-instellings',
-  'forums.category.language':
-    'Beperk die vertoning van hierdie kategorie tot die volgende taal. Los leeg as jy die kategorie in alle tale wil vertoon.',
-  'forums.category.intlNameDescription':
-    'Ons het gelokaliseerde titels van algemene kategorienommers vir jou keuse, sodat die kategorie beskikbaar kan wees in alle ondersteunde tale.',
+  'forums.category.language': 'Beperk die vertoning van hierdie kategorie tot die volgende taal. Los leeg as jy die kategorie in alle tale wil vertoon.',
+  'forums.category.intlNameDescription': 'Ons het gelokaliseerde titels van algemene kategorienommers vir jou keuse, sodat die kategorie beskikbaar kan wees in alle ondersteunde tale.',
   'forums.thread.new': 'Nuwe draad',
   'forums.thread.delete': 'Verwyder draad',
   'forums.post.new': 'Nuwe pos',
-  'forums.archived.forum':
-    'Hierdie forum is ge-argiveer op {archivedAt, date, long}. Jy kan steeds die forum sien, maar jy kan nie nuwe inhoud skep nie.',
+  'forums.archived.forum': 'Hierdie forum is ge-argiveer op {archivedAt, date, long}. Jy kan steeds die forum sien, maar jy kan nie nuwe inhoud skep nie.',
   'forums.category.archive': 'Argiveer kategorie',
-  'forums.category.archiveExplained':
-    'Kategorie sonder drade sal verwyder word, terwyl kategorieë met inhoud die skepping van nuwe drade sal voorkom.',
+  'forums.category.archiveExplained': 'Kategorie sonder drade sal verwyder word, terwyl kategorieë met inhoud die skepping van nuwe drade sal voorkom.',
   'forums.category.reactivate': 'Heraktiveer kategorie',
   'forums.category.isVotable': 'Laat gebruikers toe om drade op of af te stem',
-  'forums.category.titleIsGlobal':
-    'Hierdie kategorietitel sal in die taal van die gebruiker vertoon word as ons dit ondersteun.',
-  'forums.category.languageLimited':
-    'Hierdie kategorie sal slegs aan gebruikers in hierdie taal vertoon word.',
+  'forums.category.titleIsGlobal': 'Hierdie kategorietitel sal in die taal van die gebruiker vertoon word as ons dit ondersteun.',
+  'forums.category.languageLimited': 'Hierdie kategorie sal slegs aan gebruikers in hierdie taal vertoon word.',
   'forums.threadsCounter': 'Totale aantal onderwerpe besprekings',
   // Pre-defined category names
   'forums.category.intlName.general': 'Algemeen',
@@ -51,26 +44,25 @@ export default {
   // Forum restrictions
   'forums.forumRestriction.usersOnly': 'Laat slegs toegang toe vir geregistreerde gebruikers',
   'forums.settings.accessControls': 'Toegangskontroles',
-  'forums.forumRestrictionsMessages.usersOnlyForum':
-    'Hierdie forum is slegs toeganklik vir geregistreerde gebruikers.',
-  'forums.noThreads': "Niks om hier te sien nie. Dit is aan jou om 'n gesprek te begin.",
+  'forums.forumRestrictionsMessages.usersOnlyForum': 'Hierdie forum is slegs toeganklik vir geregistreerde gebruikers.',
+  'forums.noThreads': 'Niks om hier te sien nie. Dit is aan jou om \'n gesprek te begin.',
   'forums.noPosts': 'Niemand het nog geantwoord nie. Wees die eerste!',
   // Threads
-  'forums.createThread': "Skep 'n nuwe bespreking",
-  'forums.createThreadIn': "Skep 'n nuwe draad in {categoryName}",
+  'forums.createThread': 'Skep \'n nuwe bespreking',
+  'forums.createThreadIn': 'Skep \'n nuwe draad in {categoryName}',
   'forums.openingPost': 'Openingspos',
-  'forums.createQuestionLabel': "Is dit 'n vraag?",
+  'forums.createQuestionLabel': 'Is dit \'n vraag?',
   'forums.thread.noReplies': 'Daar is tans geen antwoorde hier nie.',
   'forums.reply': 'Antwoord',
   'forums.post': 'Plasing',
   'forums.posting': 'Plaas...',
   'forums.reset': 'Herstel',
-  'forum.thread.reply': "Voeg 'n antwoord by hierdie draad",
+  'forum.thread.reply': 'Voeg \'n antwoord by hierdie draad',
   'forums.yourMessage': 'Jou boodskap',
   'forums.thread.replyPosted': 'Jou boodskap is geplaas',
   'forums.thread.totalPosts': `{totalReplies, number} {totalReplies, plural,
     one {antwoord}
     other {antwoorde}
   }`,
-  'forums.thread.createdAt': 'Geskep op {createdAt, date, medium}',
-}
+  'forums.thread.createdAt': 'Geskep op {createdAt, date, medium}'
+};
