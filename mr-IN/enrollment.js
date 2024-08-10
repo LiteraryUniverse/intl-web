e0d2e1a7674a95c4bf1cf283791916c2664f3015
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:32:29 +0200

    New translations enrollment.js (Marathi)

--- mr-IN/enrollment.js
+++ mr-IN/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'तुमचे खाते दावा करा!',
-  'enrollment.description':
-    'स्वागत आहे {username}! तुमच्या विशेष खात्याची प्रवेश मिळवण्यासाठी तुम्हाला केवळ पासवर्ड तयार करायला लागतं.',
-  'enrollment.submitting': 'तुमचे खाते दावा करत आहोत...',
-  'enrollment.submit': 'तुमचे खाते दावा करा!',
-}
+    'enrollment.title': 'तुमचे खाते दावा करा!',
+    'enrollment.description': 'स्वागत आहे {username}! तुमच्या विशेष खात्याची प्रवेश मिळवण्यासाठी तुम्हाला केवळ पासवर्ड तयार करायला लागतं.',
+    'enrollment.submitting': 'तुमचे खाते दावा करत आहोत...',
+    'enrollment.submit': 'तुमचे खाते दावा करा!'
+};
