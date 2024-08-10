5ee1ea093448fd3498c153e6851f755795b7d8fc
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:25:29 +0200

    New translations user.js (Portuguese)

--- pt-PT/user.js
+++ pt-PT/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'Adicionar aos amigos',
-  'user.friend.requests': `Tem {num, number} {num, plural,
+    'user.friend.request.add': 'Adicionar aos amigos',
+    'user.friend.requests': `Tem {num, number} {num, plural,
     zero {pedidos de novo amigos}
     one {pedido de novo amigos}
     many {pedidos de novo amigos}
     other {pedidos de novo amigos}
   }`,
-  'user.friend.unfriend': 'Remover amizade',
-  'user.block': 'Bloquear',
-  'user.unblock': 'Desbloquear',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'Remover amizade',
+    'user.block': 'Bloquear',
+    'user.unblock': 'Desbloquear',
+    'user.joined': `{gender, select,
     male {Aderiu}
     female {Aderiu}
     other {Aderiu}} no {date, date, long}`,
-  'user.friend.request.accept': 'Aceitar pedido de amizade',
-  'user.friend.request.cancel': 'Cancelar pedido de amizade',
-  'user.friend.request.deny': 'Negar pedido de amizade',
-  'user.listing': 'Listagem de utilizadores',
-  'user.profile.visit': 'Visite o perfil do {user}.',
-  'user.profile.avatar': 'Avatar de {username}',
-  'user.friend.list.requests': 'Pedidos de amizade',
-  'user.friend.norequests': 'Não tem pedidos de amizade.',
-  'user.dashboardWelcome': `{dayPart, select,
+    'user.friend.request.accept': 'Aceitar pedido de amizade',
+    'user.friend.request.cancel': 'Cancelar pedido de amizade',
+    'user.friend.request.deny': 'Negar pedido de amizade',
+    'user.listing': 'Listagem de utilizadores',
+    'user.profile.visit': 'Visite o perfil do {user}.',
+    'user.profile.avatar': 'Avatar de {username}',
+    'user.friend.list.requests': 'Pedidos de amizade',
+    'user.friend.norequests': 'Não tem pedidos de amizade.',
+    'user.dashboardWelcome': `{dayPart, select,
     morning {Bom dia}
     afternoon {Boa tarde}
     evening {Boa noite}
     night {Boa noite}
     other {Bom dia}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'Este autor foi publicado da maneira tradicional',
-  'user.verification.luAuthor': 'Autor de nota baseado no Universo Literário',
-  'user.verification.luEmployee': 'Empregado do Universo Literário',
-  'user.verification.publicFigure': 'Figura pública verificada',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'Este autor foi publicado da maneira tradicional',
+    'user.verification.luAuthor': 'Autor de nota baseado no Universo Literário',
+    'user.verification.luEmployee': 'Empregado do Universo Literário',
+    'user.verification.publicFigure': 'Figura pública verificada',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {história}
     other {histórias}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {universo}
     other {universos}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {fan fiction}
     other {fan fictions} 
   }`,
-  'profile.works': 'Obras',
-  // As in creative/literary works
-  'profile.wishlist': 'Lista de desejos',
-  'profile.storiesBy': 'Histórias criadas por {username}',
-  'profile.universesBy': 'Universos criados por {username}',
-  'profile.fanFictionBy': 'Fan Fiction criada por {username}',
-}
+    'profile.works': 'Obras',
+    // As in creative/literary works
+    'profile.wishlist': 'Lista de desejos',
+    'profile.storiesBy': 'Histórias criadas por {username}',
+    'profile.universesBy': 'Universos criados por {username}',
+    'profile.fanFictionBy': 'Fan Fiction criada por {username}'
+};
