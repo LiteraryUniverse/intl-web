46a53da26e269e4739a0153448a3ba3a56806af1
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:41:39 +0200

    New translations user.js (Romanian)

--- ro-RO/user.js
+++ ro-RO/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'Adaugă la prieteni',
-  'user.friend.requests': `Ai {num, number} cerere de prietenie nouă {num, plural,
+    'user.friend.request.add': 'Adaugă la prieteni',
+    'user.friend.requests': `Ai {num, number} cerere de prietenie nouă {num, plural,
     zero {}
     one {cerere}
     many {cereri}
     other {cereri}
   }`,
-  'user.friend.unfriend': 'Șterge din prieteni',
-  'user.block': 'Blochează',
-  'user.unblock': 'Deblochează',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'Șterge din prieteni',
+    'user.block': 'Blochează',
+    'user.unblock': 'Deblochează',
+    'user.joined': `{gender, select,
     male {s-a alăturat}
     female {s-a alăturat}
     other {s-a alăturat}} pe {date, date, long}`,
-  'user.friend.request.accept': 'Acceptă cererea de prietenie',
-  'user.friend.request.cancel': 'Anulează cererea de prietenie',
-  'user.friend.request.deny': 'Respinge prietenia',
-  'user.listing': 'Listă utilizatori',
-  'user.profile.visit': 'Vizitează profilul lui {user}.',
-  'user.profile.avatar': 'Avatarul lui {username}',
-  'user.friend.list.requests': 'Cereri de prietenie',
-  'user.friend.norequests': 'Nu ai cereri de prietenie.',
-  'user.dashboardWelcome': `Good {dayPart, select,
+    'user.friend.request.accept': 'Acceptă cererea de prietenie',
+    'user.friend.request.cancel': 'Anulează cererea de prietenie',
+    'user.friend.request.deny': 'Respinge prietenia',
+    'user.listing': 'Listă utilizatori',
+    'user.profile.visit': 'Vizitează profilul lui {user}.',
+    'user.profile.avatar': 'Avatarul lui {username}',
+    'user.friend.list.requests': 'Cereri de prietenie',
+    'user.friend.norequests': 'Nu ai cereri de prietenie.',
+    'user.dashboardWelcome': `Good {dayPart, select,
     morning {morning}
     afternoon {afternoon}
     evening {evening}
     night {night}
     other {day}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'Acest autor a fost publicat în mod tradițional',
-  'user.verification.luAuthor': 'Autor bazat pe Literary Universe de notă',
-  'user.verification.luEmployee': 'Angajat Literary Universe',
-  'user.verification.publicFigure': 'Persoană publică verificată',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'Acest autor a fost publicat în mod tradițional',
+    'user.verification.luAuthor': 'Autor bazat pe Literary Universe de notă',
+    'user.verification.luEmployee': 'Angajat Literary Universe',
+    'user.verification.publicFigure': 'Persoană publică verificată',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {poveste}
     other {povești}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {univers}
     other {universuri}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {fan fiction}
     other {fan fiction-uri} 
   }`,
-  'profile.works': 'Lucrări',
-  // As in creative/literary works
-  'profile.wishlist': 'Lista de dorințe',
-  'profile.storiesBy': 'Povești create de {username}',
-  'profile.universesBy': 'Universuri create de {username}',
-  'profile.fanFictionBy': 'Fan Fiction creat de {username}',
-}
+    'profile.works': 'Lucrări',
+    // As in creative/literary works
+    'profile.wishlist': 'Lista de dorințe',
+    'profile.storiesBy': 'Povești create de {username}',
+    'profile.universesBy': 'Universuri create de {username}',
+    'profile.fanFictionBy': 'Fan Fiction creat de {username}'
+};
