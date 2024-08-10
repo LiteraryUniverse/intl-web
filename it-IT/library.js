d4ad97224d92562ed41324ffa21cd38ea0fcacd6
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:20:25 +0200

    New translations library.js (Italian)

--- it-IT/library.js
+++ it-IT/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'Aggiungi alla libreria',
-  'library.remove': 'Rimuovi dalla libreria',
-  'library.bought': 'Storie acquistate',
-  'library.stories': 'Storie seguite',
-  'library.universes': 'Universo seguite',
-  'bookmark.continueReading': 'Continua a leggere',
-  'bookmark.statusSummary': 'Hai letto {readPercent, number, ::percent} della storia.',
-}
+    'library.add': 'Aggiungi alla libreria',
+    'library.remove': 'Rimuovi dalla libreria',
+    'library.bought': 'Storie acquistate',
+    'library.stories': 'Storie seguite',
+    'library.universes': 'Universo seguite',
+    'bookmark.continueReading': 'Continua a leggere',
+    'bookmark.statusSummary': 'Hai letto {readPercent, number, ::percent} della storia.'
+};
