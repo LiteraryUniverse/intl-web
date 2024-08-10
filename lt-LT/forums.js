2bad6b9e00b3ad7acdaa5143938904ba58ac5878
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:22:45 +0200

    New translations forums.js (Lithuanian)

--- lt-LT/forums.js
+++ lt-LT/forums.js
@@ -1,8 +1,7 @@
 export default {
   'forums.create': 'Įgalinti forumą',
   'forums.archive': 'Archyvuoti forumą',
-  'forums.archive.explained':
-    'Archyvavus forumą, vartotojai nebegalės rašyti naujų pranešimų, tačiau jis liks matomas.',
+  'forums.archive.explained': 'Archyvavus forumą, vartotojai nebegalės rašyti naujų pranešimų, tačiau jis liks matomas.',
   'forums.archive.success': 'Forumas buvo archyvuotas',
   'forums.unArchive': 'Aktyvuoti forumus iš naujo',
   'forums.unArchive.success': 'Forumas buvo aktyvuotas iš naujo!',
@@ -12,22 +11,17 @@ export default {
   'forums.category.name': 'Kategorijos pavadinimas',
   'forums.category.slug': 'SEO URL dalis',
   'forums.category.settings': 'Kategorijos nustatymai',
-  'forums.category.language':
-    'Apriboti šios kategorijos rodomumą iki šios kalbos. Palikite tuščią, jei norite rodyti kategoriją visomis kalbomis.',
-  'forums.category.intlNameDescription':
-    'Mes turime lokalizuotus bendrų kategorijų pavadinimus, iš kurių galite pasirinkti, kad kategorija būtų prieinama visomis palaikomomis kalbomis.',
+  'forums.category.language': 'Apriboti šios kategorijos rodomumą iki šios kalbos. Palikite tuščią, jei norite rodyti kategoriją visomis kalbomis.',
+  'forums.category.intlNameDescription': 'Mes turime lokalizuotus bendrų kategorijų pavadinimus, iš kurių galite pasirinkti, kad kategorija būtų prieinama visomis palaikomomis kalbomis.',
   'forums.thread.new': 'Nauja tema',
   'forums.thread.delete': 'Ištrinti temą',
   'forums.post.new': 'Naujas įrašas',
-  'forums.archived.forum':
-    'Šis forumas buvo archyvuotas {archivedAt, date, long}. Vis tiek galite peržiūrėti forumus, tačiau negalėsite kurti naujų turinio elementų.',
+  'forums.archived.forum': 'Šis forumas buvo archyvuotas {archivedAt, date, long}. Vis tiek galite peržiūrėti forumus, tačiau negalėsite kurti naujų turinio elementų.',
   'forums.category.archive': 'Archyvuoti kategoriją',
-  'forums.category.archiveExplained':
-    'Kategorija, kuri neturi temų, bus ištrinta, o kategorijos su turiniu neleis sukurti naujų temų.',
+  'forums.category.archiveExplained': 'Kategorija, kuri neturi temų, bus ištrinta, o kategorijos su turiniu neleis sukurti naujų temų.',
   'forums.category.reactivate': 'Aktyvuoti kategoriją iš naujo',
   'forums.category.isVotable': 'Leisti vartotojams balsuoti už/žemyn temes',
-  'forums.category.titleIsGlobal':
-    'Šios kategorijos pavadinimas bus rodomas vartotojo kalba, jei ją palaikome.',
+  'forums.category.titleIsGlobal': 'Šios kategorijos pavadinimas bus rodomas vartotojo kalba, jei ją palaikome.',
   'forums.category.languageLimited': 'Ši kategorija bus rodoma tik vartotojams šia kalba.',
   'forums.threadsCounter': 'Iš viso temas aptarimai',
   // Pre-defined category names
@@ -50,8 +44,7 @@ export default {
   // Forum restrictions
   'forums.forumRestriction.usersOnly': 'Leisti prieigą tik registruotiems vartotojams',
   'forums.settings.accessControls': 'Prieigos valdymas',
-  'forums.forumRestrictionsMessages.usersOnlyForum':
-    'Šis forumas prieinamas tik registruotiems vartotojams.',
+  'forums.forumRestrictionsMessages.usersOnlyForum': 'Šis forumas prieinamas tik registruotiems vartotojams.',
   'forums.noThreads': 'Čia nieko nėra. Tai priklauso nuo jūsų, kad pradėtumėte pokalbį.',
   'forums.noPosts': 'Dar niekas neatsakė. Būkite pirmas!',
   // Threads
@@ -71,5 +64,5 @@ export default {
     one {atsakymas}
     other {atsakymai}
   }`,
-  'forums.thread.createdAt': 'Sukurta {createdAt, date, medium}',
-}
+  'forums.thread.createdAt': 'Sukurta {createdAt, date, medium}'
+};
