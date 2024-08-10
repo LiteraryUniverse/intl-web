f225d504916a4d6c1f81e6a43b680515804e4b2f
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:30:29 +0200

    New translations legal.js (Indonesian)

--- id-ID/legal.js
+++ id-ID/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Maaf mengganggu. Namun sebelum kami membiarkan Anda melanjutkan lebih jauh, kami secara hukum terikat, untuk memastikan bahwa Anda menyadari dan setuju dengan Ketentuan Layanan, Kebijakan Privasi, dan Perjanjian Hak Cipta kami.',
-  'legal.modal.agree': 'Saya Setuju',
-  'legal.modal.disagree': 'Saya Tidak Setuju',
-  'legal.modal.register': 'Anda tidak akan melihat ini lagi jika Anda mendaftar.',
-  'legal.modal.goToSettings': 'Pergi ke pengaturan Anda',
-  'legal.modal.oneLastThing': 'Satu hal terakhir...',
-  'legal.modal.finish': 'Selesai',
-  'legal.modal.existingUser': 'Sudah terdaftar?',
-  'legal.modal.pleaseLogin': 'Silakan masuk.',
-  'legal.publicationAgreements': 'Perjanjian Publikasi',
-  'legal.generalPublishingAgreement': 'Perjanjian Penerbitan Umum',
-  'legal.documentsFor': 'Dokumen hukum untuk {workTitle}',
-}
+    'legal.modal.intro': 'Maaf mengganggu. Namun sebelum kami membiarkan Anda melanjutkan lebih jauh, kami secara hukum terikat, untuk memastikan bahwa Anda menyadari dan setuju dengan Ketentuan Layanan, Kebijakan Privasi, dan Perjanjian Hak Cipta kami.',
+    'legal.modal.agree': 'Saya Setuju',
+    'legal.modal.disagree': 'Saya Tidak Setuju',
+    'legal.modal.register': 'Anda tidak akan melihat ini lagi jika Anda mendaftar.',
+    'legal.modal.goToSettings': 'Pergi ke pengaturan Anda',
+    'legal.modal.oneLastThing': 'Satu hal terakhir...',
+    'legal.modal.finish': 'Selesai',
+    'legal.modal.existingUser': 'Sudah terdaftar?',
+    'legal.modal.pleaseLogin': 'Silakan masuk.',
+    'legal.publicationAgreements': 'Perjanjian Publikasi',
+    'legal.generalPublishingAgreement': 'Perjanjian Penerbitan Umum',
+    'legal.documentsFor': 'Dokumen hukum untuk {workTitle}'
+};
