e37eaafce33f74aa1ff0c9f457cb28b849ef77fd
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:36:10 +0200

    New translations user.js (Esperanto)

--- eo-UY/user.js
+++ eo-UY/user.js
@@ -1,53 +1,53 @@
 export default {
-  'user.friend.request.add': 'Aldoni al amikoj',
-  'user.friend.requests': `Vi havas {num, number} novajn amikajn petojn {num, plural,
+    'user.friend.request.add': 'Aldoni al amikoj',
+    'user.friend.requests': `Vi havas {num, number} novajn amikajn petojn {num, plural,
     zero {peto}
     one {petoj}
     many {peto}
     other {petoj}
   }`,
-  'user.friend.unfriend': 'Forigi amikon',
-  'user.block': 'Blokigi',
-  'user.unblock': 'Malblokigi',
-  'user.joined': `{gender, select,
+    'user.friend.unfriend': 'Forigi amikon',
+    'user.block': 'Blokigi',
+    'user.unblock': 'Malblokigi',
+    'user.joined': `{gender, select,
     male {Aliĝis}
     female {Aliĝis}
     other {Aliĝis}} je {date, date, long}`,
-  'user.friend.request.accept': 'Akcepti peton pri amikeco',
-  'user.friend.request.cancel': 'Nuligi peton pri amikeco',
-  'user.friend.request.deny': 'Rifuzi amikecon',
-  'user.listing': 'Listo de uzantoj',
-  'user.profile.visit': 'Viziti la profilon de {user}.',
-  'user.profile.avatar': 'Avataro de {username}',
-  'user.friend.list.requests': 'Petoj pri amikeco',
-  'user.friend.norequests': 'Vi ne havas petojn pri amikeco.',
-  'user.dashboardWelcome': `Bonan matenon {dayPart, select,
+    'user.friend.request.accept': 'Akcepti peton pri amikeco',
+    'user.friend.request.cancel': 'Nuligi peton pri amikeco',
+    'user.friend.request.deny': 'Rifuzi amikecon',
+    'user.listing': 'Listo de uzantoj',
+    'user.profile.visit': 'Viziti la profilon de {user}.',
+    'user.profile.avatar': 'Avataro de {username}',
+    'user.friend.list.requests': 'Petoj pri amikeco',
+    'user.friend.norequests': 'Vi ne havas petojn pri amikeco.',
+    'user.dashboardWelcome': `Bonan matenon {dayPart, select,
     morning {bonan tagon}
     afternoon {bonan posttagmezon}
     evening {bonan vesperon}
     night {bonan nokton}
     other {bonan tagon}
   } {displayName}!`,
-  'user.verification.publishedAuthor': 'Ĉi tiu aŭtoro estas publikigita la tradicia maniero',
-  'user.verification.luAuthor': 'Aŭtoro bazita en Litera Universo de noto',
-  'user.verification.luEmployee': 'Laboranto de Litera Universo',
-  'user.verification.publicFigure': 'Verifita publika figuro',
-  'profile.storiesCount': `{count, number} {count, plural,
+    'user.verification.publishedAuthor': 'Ĉi tiu aŭtoro estas publikigita la tradicia maniero',
+    'user.verification.luAuthor': 'Aŭtoro bazita en Litera Universo de noto',
+    'user.verification.luEmployee': 'Laboranto de Litera Universo',
+    'user.verification.publicFigure': 'Verifita publika figuro',
+    'profile.storiesCount': `{count, number} {count, plural,
     one {rakonto}
     other {rakontoj}
   }`,
-  'profile.universesCount': `{count, number} {count, plural,
+    'profile.universesCount': `{count, number} {count, plural,
     one {universo}
     other {universoj}
   }`,
-  'profile.fanFictionsCount': `{count, number} {count, plural,
+    'profile.fanFictionsCount': `{count, number} {count, plural,
     one {fanfikcio}
     other {fanfikcioj} 
   }`,
-  'profile.works': 'Verkoj',
-  // As in creative/literary works
-  'profile.wishlist': 'Deziro',
-  'profile.storiesBy': 'Rakontoj kreitaj de {username}',
-  'profile.universesBy': 'Universoj kreitaj de {username}',
-  'profile.fanFictionBy': 'Fanfikcio kreita de {username}',
-}
+    'profile.works': 'Verkoj',
+    // As in creative/literary works
+    'profile.wishlist': 'Deziro',
+    'profile.storiesBy': 'Rakontoj kreitaj de {username}',
+    'profile.universesBy': 'Universoj kreitaj de {username}',
+    'profile.fanFictionBy': 'Fanfikcio kreita de {username}'
+};
