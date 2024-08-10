bd009d0b777b9a4aa4b193ee9864136381b5850c
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:38:04 +0200

    New translations enrollment.js (Tibetan)

--- bo-BT/enrollment.js
+++ bo-BT/enrollment.js
@@ -1,8 +1,6 @@
 export default {
-  'enrollment.title':
-    'ཁྱེད་རང་གི་གྲོགས་ཀྱི་གྲངས་མེད་པའི་དཔེ་མཛོད་འཛོལ་བ་བཞག་པ་ཡོད་པའི་ཡིག་ཚགས་པ་སྤྱོད་པའི་བསྟེན་ཞིབ་དང་བཅས་པ་འདི་ནང་གི་དཔེ་མཛོད་འཛོལ་བ་བཞག་པ་ཡོད།',
-  'enrollment.description':
-    'Welcome {username}! To gain access to your special account you only need to create password.',
-  'enrollment.submitting': 'ཁྱེད་རང་གི་གནས་ཚུལ་འདི་ལུ་བཅར་འདྲི་བ་བཟོ་ཡོད།',
-  'enrollment.submit': 'ཁྱེད་རང་གི་གྲོགས་ཀྱི་གྲངས་མེད་པའི་དཔེ་མཛོད་འཛོལ་བ་བཞག་པ་ཡོད།',
-}
+    'enrollment.title': 'ཁྱེད་རང་གི་གྲོགས་ཀྱི་གྲངས་མེད་པའི་དཔེ་མཛོད་འཛོལ་བ་བཞག་པ་ཡོད་པའི་ཡིག་ཚགས་པ་སྤྱོད་པའི་བསྟེན་ཞིབ་དང་བཅས་པ་འདི་ནང་གི་དཔེ་མཛོད་འཛོལ་བ་བཞག་པ་ཡོད།',
+    'enrollment.description': 'Welcome {username}! To gain access to your special account you only need to create password.',
+    'enrollment.submitting': 'ཁྱེད་རང་གི་གནས་ཚུལ་འདི་ལུ་བཅར་འདྲི་བ་བཟོ་ཡོད།',
+    'enrollment.submit': 'ཁྱེད་རང་གི་གྲོགས་ཀྱི་གྲངས་མེད་པའི་དཔེ་མཛོད་འཛོལ་བ་བཞག་པ་ཡོད།'
+};
