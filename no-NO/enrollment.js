5f19f321fcfba370550b604f20f711d457d332ab
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:23:30 +0200

    New translations enrollment.js (Norwegian)

--- no-NO/enrollment.js
+++ no-NO/enrollment.js
@@ -1,7 +1,6 @@
 export default {
-  'enrollment.title': 'Krev kontoen din!',
-  'enrollment.description':
-    'Velkommen {username}! For å få tilgang til din spesielle konto trenger du bare å opprette et passord.',
-  'enrollment.submitting': 'Krever kontoen din...',
-  'enrollment.submit': 'Krev kontoen din!',
-}
+    'enrollment.title': 'Krev kontoen din!',
+    'enrollment.description': 'Velkommen {username}! For å få tilgang til din spesielle konto trenger du bare å opprette et passord.',
+    'enrollment.submitting': 'Krever kontoen din...',
+    'enrollment.submit': 'Krev kontoen din!'
+};
