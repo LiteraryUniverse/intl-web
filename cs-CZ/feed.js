687f7c9f73da8293da33a58e458d610210adcf10
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:15:02 +0200

    New translations feed.js (Czech)

--- cs-CZ/feed.js
+++ cs-CZ/feed.js
@@ -1,9 +1,9 @@
 export default {
-  'feed.name': 'Kanál',
-  'feed.post.new': 'Nový příspěvek',
-  'feed.post.like': 'Líbí se mi',
-  'feed.post.unlike': 'Nelíbí se mi',
-  'feed.post.send': 'Poslat',
-  'feed.empty': 'Žádné příspěvky k zobrazení.',
-  'feed.placeholder': 'Co vás napadá?',
-}
+    'feed.name': 'Kanál',
+    'feed.post.new': 'Nový příspěvek',
+    'feed.post.like': 'Líbí se mi',
+    'feed.post.unlike': 'Nelíbí se mi',
+    'feed.post.send': 'Poslat',
+    'feed.empty': 'Žádné příspěvky k zobrazení.',
+    'feed.placeholder': 'Co vás napadá?'
+};
