9d26ad210e676c7a5e444ff5fd7cdcf353a6cbac
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:24:41 +0200

    New translations licenses.js (Polish)

--- pl-PL/licenses.js
+++ pl-PL/licenses.js
@@ -3,33 +3,23 @@
  * https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/
  */
 export default {
-  'licenses.about': 'O licencjach',
-  'licenses.more': 'Więcej informacji',
-  'licenses.legal': 'Dokument prawny',
-  'licenses.name.cc-by': 'Creative Commons Uznanie autorstwa 4.0',
-  'licenses.cc-by':
-    'Ta licencja pozwala innym na dystrybucję, remiksowanie, przeróbki i opieranie się na Twojej historii, nawet komercyjnie, tak długo jak dadzą Ci prawa autorskie do oryginalnej pracy. Rekomenowane dla najszerszego rozpowszechniania i użycia materiałów licencjonowanych.',
-  'licenses.name.cc-by-sa': 'CreativeCommons Uznanie autorstwa-Na tych samych warunkach 4.0',
-  'licenses.cc-by-sa':
-    'Licencja ta pozwala innym na remiksowanie, poprawianie i budowanie na podstawie Waszej pracy nawet w celach komercyjnych, pod warunkiem, że podadzą Waszą nazwę i udzielą licencji na swoje nowe dzieła na identycznych warunkach. Licencja ta jest często porównywana do licencji "copyleft" wolnego i otwartego oprogramowania. Wszystkie nowe utwory oparte na Waszych będą opatrzone tą samą licencją, więc wszelkie pochodne będą mogły być wykorzystywane w celach komercyjnych. Jest to licencja używana przez Wikipedię i jest zalecana w przypadku materiałów, które mogłyby skorzystać na włączeniu treści z Wikipedii i projektów o podobnych licencjach.',
-  'licenses.name.cc-by-nd': 'Creative Commons Uznanie autorstwa - Bez utworów zależnych 4.0',
-  'licenses.cc-by-nd':
-    'Ta licencja pozwala na redystrybucję, komercyjną i niekomercyjną, pod warunkiem, że jest ona przekazywana w niezmienionej formie i w całości, z informacją o Tobie.',
-  'licenses.name.cc-by-nc': 'Creative Commons Uznanie autorstwa-Użycie niekomercyjne 4.0',
-  'licenses.cc-by-nc':
-    'Licencja ta pozwala innym na remiksowanie, poprawianie i budowanie na podstawie Twojego utworu w sposób niekomercyjny, i chociaż ich nowe utwory muszą również zawierać informację o Tobie i być niekomercyjne, nie muszą udzielać licencji na swoje utwory pochodne na tych samych warunkach.',
-  'licenses.name.cc-by-nc-sa':
-    'Creative Commons Uznanie autorstwa-Użycie niekomercyjne-Na tych samych warunkach 4.0',
-  'licenses.cc-by-nc-sa':
-    'Licencja ta pozwala innym na remiksowanie, poprawianie i budowanie na podstawie Twojego utworu w sposób niekomercyjny, pod warunkiem, że podadzą informację o Tobie i udzielą licencji na swoje nowe dzieła na identycznych warunkach.',
-  'licenses.name.cc-by-nc-nd':
-    'Creative Commons Uznanie autorstwa - Użycie niekomercyjne - Bez utworów zależnych 4.0',
-  'licenses.cc-by-nc-nd':
-    'Ta licencja jest najbardziej restrykcyjna z naszych sześciu głównych licencji. Pozwala innym na pobieranie Twoich utworów i dzielenie się nimi z innymi, pod warunkiem, że podadzą Twoją nazwę, ale nie mogą ich w żaden sposób zmieniać ani używać do celów komercyjnych.',
-  'licenses.name.lu-c': 'Literary Universe Prawa Autorskie',
-  'licenses.lu-c':
-    'Standardowa licencja z pełnymi prawami autorskimi, która pozwala ci na sprzedaż i publikację twoich prac, jak tylko chcesz, a twoje prawa do nich są chronione. Ta licencja daje Ci również możliwość sprzedaży Twoich opowiadań na Literary Universe.',
-  'licenses.name.cc0': 'Brak praw autorskich - CC0 1.0',
-  'licenses.cc0':
-    'Przekaż swoją pracę do domeny publicznej. W przeciwieństwie do licencji CC, które pozwalają właścicielom praw autorskich na wybór spośród szeregu zezwoleń przy jednoczesnym zachowaniu praw autorskich, CC0 daje jeszcze inny wybór - wybór rezygnacji z ochrony praw autorskich i baz danych oraz wyłącznych praw automatycznie przyznawanych twórcom - alternatywa "bez praw zastrzeżonych" dla naszych licencji.',
-}
+    'licenses.about': 'O licencjach',
+    'licenses.more': 'Więcej informacji',
+    'licenses.legal': 'Dokument prawny',
+    'licenses.name.cc-by': 'Creative Commons Uznanie autorstwa 4.0',
+    'licenses.cc-by': 'Ta licencja pozwala innym na dystrybucję, remiksowanie, przeróbki i opieranie się na Twojej historii, nawet komercyjnie, tak długo jak dadzą Ci prawa autorskie do oryginalnej pracy. Rekomenowane dla najszerszego rozpowszechniania i użycia materiałów licencjonowanych.',
+    'licenses.name.cc-by-sa': 'CreativeCommons Uznanie autorstwa-Na tych samych warunkach 4.0',
+    'licenses.cc-by-sa': 'Licencja ta pozwala innym na remiksowanie, poprawianie i budowanie na podstawie Waszej pracy nawet w celach komercyjnych, pod warunkiem, że podadzą Waszą nazwę i udzielą licencji na swoje nowe dzieła na identycznych warunkach. Licencja ta jest często porównywana do licencji "copyleft" wolnego i otwartego oprogramowania. Wszystkie nowe utwory oparte na Waszych będą opatrzone tą samą licencją, więc wszelkie pochodne będą mogły być wykorzystywane w celach komercyjnych. Jest to licencja używana przez Wikipedię i jest zalecana w przypadku materiałów, które mogłyby skorzystać na włączeniu treści z Wikipedii i projektów o podobnych licencjach.',
+    'licenses.name.cc-by-nd': 'Creative Commons Uznanie autorstwa - Bez utworów zależnych 4.0',
+    'licenses.cc-by-nd': 'Ta licencja pozwala na redystrybucję, komercyjną i niekomercyjną, pod warunkiem, że jest ona przekazywana w niezmienionej formie i w całości, z informacją o Tobie.',
+    'licenses.name.cc-by-nc': 'Creative Commons Uznanie autorstwa-Użycie niekomercyjne 4.0',
+    'licenses.cc-by-nc': 'Licencja ta pozwala innym na remiksowanie, poprawianie i budowanie na podstawie Twojego utworu w sposób niekomercyjny, i chociaż ich nowe utwory muszą również zawierać informację o Tobie i być niekomercyjne, nie muszą udzielać licencji na swoje utwory pochodne na tych samych warunkach.',
+    'licenses.name.cc-by-nc-sa': 'Creative Commons Uznanie autorstwa-Użycie niekomercyjne-Na tych samych warunkach 4.0',
+    'licenses.cc-by-nc-sa': 'Licencja ta pozwala innym na remiksowanie, poprawianie i budowanie na podstawie Twojego utworu w sposób niekomercyjny, pod warunkiem, że podadzą informację o Tobie i udzielą licencji na swoje nowe dzieła na identycznych warunkach.',
+    'licenses.name.cc-by-nc-nd': 'Creative Commons Uznanie autorstwa - Użycie niekomercyjne - Bez utworów zależnych 4.0',
+    'licenses.cc-by-nc-nd': 'Ta licencja jest najbardziej restrykcyjna z naszych sześciu głównych licencji. Pozwala innym na pobieranie Twoich utworów i dzielenie się nimi z innymi, pod warunkiem, że podadzą Twoją nazwę, ale nie mogą ich w żaden sposób zmieniać ani używać do celów komercyjnych.',
+    'licenses.name.lu-c': 'Literary Universe Prawa Autorskie',
+    'licenses.lu-c': 'Standardowa licencja z pełnymi prawami autorskimi, która pozwala ci na sprzedaż i publikację twoich prac, jak tylko chcesz, a twoje prawa do nich są chronione. Ta licencja daje Ci również możliwość sprzedaży Twoich opowiadań na Literary Universe.',
+    'licenses.name.cc0': 'Brak praw autorskich - CC0 1.0',
+    'licenses.cc0': 'Przekaż swoją pracę do domeny publicznej. W przeciwieństwie do licencji CC, które pozwalają właścicielom praw autorskich na wybór spośród szeregu zezwoleń przy jednoczesnym zachowaniu praw autorskich, CC0 daje jeszcze inny wybór - wybór rezygnacji z ochrony praw autorskich i baz danych oraz wyłącznych praw automatycznie przyznawanych twórcom - alternatywa "bez praw zastrzeżonych" dla naszych licencji.'
+};
