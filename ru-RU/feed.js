eeabd2e916b4020f54b3af4990c4c6c622690cc4
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:25:44 +0200

    New translations feed.js (Russian)

--- ru-RU/feed.js
+++ ru-RU/feed.js
@@ -1,9 +1,9 @@
 export default {
-  'feed.name': 'Лента',
-  'feed.post.new': 'Новая публикация',
-  'feed.post.like': 'Нравится',
-  'feed.post.unlike': 'Не нравится',
-  'feed.post.send': 'Опубликовать',
-  'feed.empty': 'Нет записей в ленте.',
-  'feed.placeholder': 'Что у вас нового?',
-}
+    'feed.name': 'Лента',
+    'feed.post.new': 'Новая публикация',
+    'feed.post.like': 'Нравится',
+    'feed.post.unlike': 'Не нравится',
+    'feed.post.send': 'Опубликовать',
+    'feed.empty': 'Нет записей в ленте.',
+    'feed.placeholder': 'Что у вас нового?'
+};
