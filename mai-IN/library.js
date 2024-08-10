9db517c88fcab33f3e8eebc0384e794872cbd8e8
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:40:00 +0200

    New translations library.js (Maithili)

--- mai-IN/library.js
+++ mai-IN/library.js
@@ -1,9 +1,9 @@
 export default {
-  'library.add': 'पुस्तकालयमा जोड़ें',
-  'library.remove': 'पुस्तकालय से हटाएँ',
-  'library.bought': 'खरीदी गई कहानियाँ',
-  'library.stories': 'फ़ॉलो की गई कहानियाँ',
-  'library.universes': 'अनुसरित ब्रह्मांड',
-  'bookmark.continueReading': 'पढ़ना जारी रखें',
-  'bookmark.statusSummary': 'तपाईंले कथा को {readPercent, number, ::percent} पढ़ेको छ।',
-}
+    'library.add': 'पुस्तकालयमा जोड़ें',
+    'library.remove': 'पुस्तकालय से हटाएँ',
+    'library.bought': 'खरीदी गई कहानियाँ',
+    'library.stories': 'फ़ॉलो की गई कहानियाँ',
+    'library.universes': 'अनुसरित ब्रह्मांड',
+    'bookmark.continueReading': 'पढ़ना जारी रखें',
+    'bookmark.statusSummary': 'तपाईंले कथा को {readPercent, number, ::percent} पढ़ेको छ।'
+};
