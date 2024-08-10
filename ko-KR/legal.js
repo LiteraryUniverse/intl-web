672fb7906b758bc1fffbc4182ad09e3f9c4d06b7
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:22:06 +0200

    New translations legal.js (Korean)

--- ko-KR/legal.js
+++ ko-KR/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    '계속 진행하기 전에 우리는 법적으로 귀하가 우리의 서비스 약관, 개인정보 보호정책 및 저작권 동의에 대해 인식하고 동의하는지 확인해야 합니다.',
-  'legal.modal.agree': '동의합니다',
-  'legal.modal.disagree': '동의하지 않습니다',
-  'legal.modal.register': '등록하면 더 이상 이 메시지를 보지 않을 수 있습니다.',
-  'legal.modal.goToSettings': '설정으로 이동',
-  'legal.modal.oneLastThing': '마지막으로...',
-  'legal.modal.finish': '완료',
-  'legal.modal.existingUser': '이미 등록되었습니까?',
-  'legal.modal.pleaseLogin': '로그인해주세요.',
-  'legal.publicationAgreements': '게시 동의서',
-  'legal.generalPublishingAgreement': '일반 게시 동의서',
-  'legal.documentsFor': '법적 문서들을 위한 {workTitle}',
-}
+    'legal.modal.intro': '계속 진행하기 전에 우리는 법적으로 귀하가 우리의 서비스 약관, 개인정보 보호정책 및 저작권 동의에 대해 인식하고 동의하는지 확인해야 합니다.',
+    'legal.modal.agree': '동의합니다',
+    'legal.modal.disagree': '동의하지 않습니다',
+    'legal.modal.register': '등록하면 더 이상 이 메시지를 보지 않을 수 있습니다.',
+    'legal.modal.goToSettings': '설정으로 이동',
+    'legal.modal.oneLastThing': '마지막으로...',
+    'legal.modal.finish': '완료',
+    'legal.modal.existingUser': '이미 등록되었습니까?',
+    'legal.modal.pleaseLogin': '로그인해주세요.',
+    'legal.publicationAgreements': '게시 동의서',
+    'legal.generalPublishingAgreement': '일반 게시 동의서',
+    'legal.documentsFor': '법적 문서들을 위한 {workTitle}'
+};
