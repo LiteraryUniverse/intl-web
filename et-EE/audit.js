5efc5bf85e300f6b93f193522a1d8122966dbffa
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:33:50 +0200

    New translations audit.js (Estonian)

--- et-EE/audit.js
+++ et-EE/audit.js
@@ -1,6 +1,6 @@
 export default {
-  'auditLog.header.date': 'Kuupäev',
-  'auditLog.header.action': 'Mida',
-  'auditLog.header.group': 'Grupp',
-  'auditLog.header.user': 'Kes',
-}
+    'auditLog.header.date': 'Kuupäev',
+    'auditLog.header.action': 'Mida',
+    'auditLog.header.group': 'Grupp',
+    'auditLog.header.user': 'Kes'
+};
