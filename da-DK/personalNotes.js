fbaeec7eb234bac8be4b4fdc15eca5fbb7418d81
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:16:03 +0200

    New translations personalnotes.js (Danish)

--- da-DK/personalNotes.js
+++ da-DK/personalNotes.js
@@ -22,5 +22,5 @@ export default {
   'personalNotes.type.encEntry': 'Encyklopædi indgang',
   'personalNotes.type.revTextBlock': 'Revision tekstblok',
   'personalNotes.chapter': 'Kapitel',
-  'personalNotes.recentSearches': 'Seneste søgninger:',
-}
+  'personalNotes.recentSearches': 'Seneste søgninger:'
+};
