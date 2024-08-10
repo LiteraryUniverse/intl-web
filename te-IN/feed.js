f05b3f04625e3ceb45bbe13b7ffd7eb6419c717c
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:35:17 +0200

    New translations feed.js (Telugu)

--- te-IN/feed.js
+++ te-IN/feed.js
@@ -1,9 +1,9 @@
 export default {
-  'feed.name': 'ఫీడ్',
-  'feed.post.new': 'కొత్త పోస్ట్',
-  'feed.post.like': 'పోస్ట్‌ని ఇష్టమైనది చేస్తుంది',
-  'feed.post.unlike': 'పోస్ట్‌ని ఇష్టమైనది కాదు',
-  'feed.post.send': 'పోస్ట్',
-  'feed.empty': 'ఫీడ్‌లో ఎంట్రీలు లేవు.',
-  'feed.placeholder': 'మీ మనసులో ఏం ఉంది?',
-}
+    'feed.name': 'ఫీడ్',
+    'feed.post.new': 'కొత్త పోస్ట్',
+    'feed.post.like': 'పోస్ట్‌ని ఇష్టమైనది చేస్తుంది',
+    'feed.post.unlike': 'పోస్ట్‌ని ఇష్టమైనది కాదు',
+    'feed.post.send': 'పోస్ట్',
+    'feed.empty': 'ఫీడ్‌లో ఎంట్రీలు లేవు.',
+    'feed.placeholder': 'మీ మనసులో ఏం ఉంది?'
+};
