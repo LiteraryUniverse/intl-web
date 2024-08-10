77d48334099b55949b1f5a9d1fde0efbac99ba69
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:33:03 +0200

    New translations enrollment.js (Thai)

--- th-TH/enrollment.js
+++ th-TH/enrollment.js
@@ -1,6 +1,6 @@
 export default {
-  'enrollment.title': 'เรียกร้องบัญชีของคุณ!',
-  'enrollment.description': 'ยินดีต้อนรับ {username}! เพื่อเข้าถึงบัญชีพิเศษของคุณ คุณต้องสร้างรหัสผ่านเท่านั้น',
-  'enrollment.submitting': 'กำลังเรียกร้องบัญชีของคุณ...',
-  'enrollment.submit': 'เรียกร้องบัญชีของคุณ!',
-}
+    'enrollment.title': 'เรียกร้องบัญชีของคุณ!',
+    'enrollment.description': 'ยินดีต้อนรับ {username}! เพื่อเข้าถึงบัญชีพิเศษของคุณ คุณต้องสร้างรหัสผ่านเท่านั้น',
+    'enrollment.submitting': 'กำลังเรียกร้องบัญชีของคุณ...',
+    'enrollment.submit': 'เรียกร้องบัญชีของคุณ!'
+};
