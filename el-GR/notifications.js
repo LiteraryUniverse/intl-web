dd60f8aafc2d1b5bd91ca634c20ca9526daf8564
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:17:10 +0200

    New translations notifications.js (Greek)

--- el-GR/notifications.js
+++ el-GR/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'Έναρξη εμφάνισης αυτού του μηνύματος στις',
   'flashnews.endsAt': 'Τερματισμός εμφάνισης αυτού του μηνύματος στις',
   'flashnews.newLanguage': 'Επιλέξτε νέα γλώσσα για προσθήκη',
-  'flashnews.onlyDisplayOn':
-    'Οι ειδήσεις θα εμφανίζονται μόνο στις επιλεγμένες παρακάτω γλώσσες (εάν έχουν επιλεγεί), δεν θα εμφανίζονται καθόλου ειδήσεις, ακόμα και στην προεπιλεγμένη γλώσσα, σε άλλες γλώσσες.',
+  'flashnews.onlyDisplayOn': 'Οι ειδήσεις θα εμφανίζονται μόνο στις επιλεγμένες παρακάτω γλώσσες (εάν έχουν επιλεγεί), δεν θα εμφανίζονται καθόλου ειδήσεις, ακόμα και στην προεπιλεγμένη γλώσσα, σε άλλες γλώσσες.',
   'notifications.menu.all': 'Όλες οι ειδοποιήσεις',
   'notifications.menu.following': 'Ακολουθούμενα',
   'notifications.menu.workshop': 'Εργαστήριο',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'Σημείωση ως αναγνωσμένο',
   'notifications.menu.delete': 'Διαγραφή ειδοποίησης',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'Η ιστορία σας {storyName} πήρε αυτόματα μία παύση μετά από 6 μήνες αδράνειας.',
-  'notification.workshopStoryStatus.toCancelled':
-    'Η ιστορία σας {storyName} πήρε αυτόματα ακύρωση μετά από ένα χρόνο αδράνειας.',
+  'notification.workshopStoryStatus.toHiatus': 'Η ιστορία σας {storyName} πήρε αυτόματα μία παύση μετά από 6 μήνες αδράνειας.',
+  'notification.workshopStoryStatus.toCancelled': 'Η ιστορία σας {storyName} πήρε αυτόματα ακύρωση μετά από ένα χρόνο αδράνειας.',
   'notification.followingStoryStatus.toHiatus': '{storyName} είναι σε παύση.',
   'notification.followingStoryStatus.toCancelled': '{storyName} έχει ακυρωθεί.',
   'notification.followingNewChapter.new': 'Νέο κεφάλαιο στο {storyName} έχει δημοσιευτεί!',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'σας στάλθηκε αίτημα φιλίας.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'Μετάβαση στην Ιστορία',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'Μετάβαση στην Ιστορία'
+};
