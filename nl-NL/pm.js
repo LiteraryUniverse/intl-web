9ebde0ef9743dc6495abbf7b030de17e99dddf52
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:23:06 +0200

    New translations pm.js (Dutch)

--- nl-NL/pm.js
+++ nl-NL/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'Gesprek',
-  'pm.showolder': 'Toon oudere berichten',
-  'pm.errors.saysomething': 'Je moet echt iets zeggen...',
-  'pm.errors.addrecipient': 'Je moet minstens één andere gebruiker toevoegen.',
-  'pm.message': 'Bericht',
-  'pm.send.message': 'Verstuur bericht',
-  'pm.messages.own': 'Jouw berichten',
-  'pm.conversations.none': 'Je converseert momenteel niet met iemand.',
-  'pm.to': 'Naar:',
-  'pm.with': 'Gesprek met:',
-  'pm.send': 'Verstuur',
-  'pm.conversations.unreadCount': `Je hebt {count, plural,
+    'pm.conversation': 'Gesprek',
+    'pm.showolder': 'Toon oudere berichten',
+    'pm.errors.saysomething': 'Je moet echt iets zeggen...',
+    'pm.errors.addrecipient': 'Je moet minstens één andere gebruiker toevoegen.',
+    'pm.message': 'Bericht',
+    'pm.send.message': 'Verstuur bericht',
+    'pm.messages.own': 'Jouw berichten',
+    'pm.conversations.none': 'Je converseert momenteel niet met iemand.',
+    'pm.to': 'Naar:',
+    'pm.with': 'Gesprek met:',
+    'pm.send': 'Verstuur',
+    'pm.conversations.unreadCount': `Je hebt {count, plural,
     zero {geen ongelezen gesprekken}
     one {één ongelezen gesprek}
     other {# ongelezen gesprekken}
   }.`,
-  'pm.form.minimumParticipants': 'Je moet een ontvanger kiezen.',
-  'pm.form.maxParticipants': `Je kunt maximaal {maxPeople, plural,
+    'pm.form.minimumParticipants': 'Je moet een ontvanger kiezen.',
+    'pm.form.maxParticipants': `Je kunt maximaal {maxPeople, plural,
     one {één persoon}
     other {# mensen}
   } hebben in een gesprek.`,
-  'pm.new.aria': 'Nieuw gesprek',
-  'pm.new.buttonTitle': 'Start een nieuw gesprek',
-}
+    'pm.new.aria': 'Nieuw gesprek',
+    'pm.new.buttonTitle': 'Start een nieuw gesprek'
+};
