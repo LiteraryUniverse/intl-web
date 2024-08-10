ea63f1886e64eb8f4af18fe6587e89eb46612d5b
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:40:32 +0200

    New translations comments.js (Odia)

--- or-IN/comments.js
+++ or-IN/comments.js
@@ -1,10 +1,10 @@
 export default {
-  'comment.write': 'ଆପଣଙ୍କ ମତ ଲେଖନ୍ତୁ',
-  'comments.none': 'ଦୁଃଖ, କୌଣସି ମତ ଦେଖାଇବା ନାହିଁ।',
-  'comments.total': `ଏଠାରେ {count, plural,
+    'comment.write': 'ଆପଣଙ୍କ ମତ ଲେଖନ୍ତୁ',
+    'comments.none': 'ଦୁଃଖ, କୌଣସି ମତ ଦେଖାଇବା ନାହିଁ।',
+    'comments.total': `ଏଠାରେ {count, plural,
     =0 {ଟିପ୍ପଣୀ ନାହିଁ}
     one {ଏକ ଟିପ୍ପଣୀ ଅଛି}
     other {# ଟିପ୍ପଣୀ ଅଛି}
   }।`,
-  'comments.show.older': 'ପୁରୁଣା ଟିପ୍ପଣୀ ଦେଖାନ୍ତୁ',
-}
+    'comments.show.older': 'ପୁରୁଣା ଟିପ୍ପଣୀ ଦେଖାନ୍ତୁ'
+};
