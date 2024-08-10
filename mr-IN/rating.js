e89c8fc7721281f2498d7788662327eb9841a996
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:32:40 +0200

    New translations rating.js (Marathi)

--- mr-IN/rating.js
+++ mr-IN/rating.js
@@ -2,18 +2,18 @@
  * Age ratings
  */
 export default {
-  'rating.name': 'मुल्यांकन',
-  'rating.explained': 'वय मर्यादा आणि प्रतिबंध',
-  'rating.all': 'सर्व',
-  'rating.desc.all': 'सर्व वय आणि गटांसाठी प्रवेश्यात्प्राप्त.',
-  'rating.teens': 'किशोर',
-  'rating.desc.teens': '12 ते 18 वयोमध्ये अवगणनीय आणि उमवटी दृश्ये.',
-  'rating.mature': 'प्रौढ',
-  'rating.desc.mature': 'अग्रिम विषये, कचऱ्या भाषा आणि अंतर्निहित दृश्ये आहेत.',
-  'rating.explicit': 'स्पष्ट',
-  'rating.desc.explicit': 'स्पष्ट दृश्ये आहेत.',
-  'rating.selection.all': 'सर्व - सर्व वय आणि गटांसाठी प्रवेश्यात्प्राप्त.',
-  'rating.selection.teens': 'किशोर (12-18) - 12 ते 18 वयोमध्ये अवगणनीय आणि उमवटी दृश्ये.',
-  'rating.selection.mature': 'प्रौढ (18+) - अग्रिम विषये, कचऱ्या भाषा आणि अंतर्निहित दृश्ये आहेत.',
-  'rating.selection.explicit': 'स्पष्ट (21+) - स्पष्ट दृश्ये आहेत.',
-}
+    'rating.name': 'मुल्यांकन',
+    'rating.explained': 'वय मर्यादा आणि प्रतिबंध',
+    'rating.all': 'सर्व',
+    'rating.desc.all': 'सर्व वय आणि गटांसाठी प्रवेश्यात्प्राप्त.',
+    'rating.teens': 'किशोर',
+    'rating.desc.teens': '12 ते 18 वयोमध्ये अवगणनीय आणि उमवटी दृश्ये.',
+    'rating.mature': 'प्रौढ',
+    'rating.desc.mature': 'अग्रिम विषये, कचऱ्या भाषा आणि अंतर्निहित दृश्ये आहेत.',
+    'rating.explicit': 'स्पष्ट',
+    'rating.desc.explicit': 'स्पष्ट दृश्ये आहेत.',
+    'rating.selection.all': 'सर्व - सर्व वय आणि गटांसाठी प्रवेश्यात्प्राप्त.',
+    'rating.selection.teens': 'किशोर (12-18) - 12 ते 18 वयोमध्ये अवगणनीय आणि उमवटी दृश्ये.',
+    'rating.selection.mature': 'प्रौढ (18+) - अग्रिम विषये, कचऱ्या भाषा आणि अंतर्निहित दृश्ये आहेत.',
+    'rating.selection.explicit': 'स्पष्ट (21+) - स्पष्ट दृश्ये आहेत.'
+};
