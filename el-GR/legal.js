acc4de8ab2118871fe7738b73b59581a75cd9234
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:17:03 +0200

    New translations legal.js (Greek)

--- el-GR/legal.js
+++ el-GR/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'Λυπούμαστε που διακόπτουμε. Πριν σας αφήσουμε να προχωρήσετε περαιτέρω, είμαστε νομικά υποχρεωμένοι, να σας ενημερώσουμε και να συμφωνήσετε με τους Όρους Χρήσης, την Πολιτική Απορρήτου και την Συμφωνία Πνευματικών Δικαιωμάτων μας.',
-  'legal.modal.agree': 'Συμφωνώ',
-  'legal.modal.disagree': 'Διαφωνώ',
-  'legal.modal.register': 'Δεν θα το ξαναδείτε αυτό αν εγγραφείτε.',
-  'legal.modal.goToSettings': 'Πηγαίνετε στις ρυθμίσεις σας',
-  'legal.modal.oneLastThing': 'Ένα τελευταίο πράγμα...',
-  'legal.modal.finish': 'Ολοκλήρωση',
-  'legal.modal.existingUser': 'Ήδη εγγεγραμμένος;',
-  'legal.modal.pleaseLogin': 'Παρακαλώ συνδεθείτε.',
-  'legal.publicationAgreements': 'Συμβάσεις δημοσίευσης',
-  'legal.generalPublishingAgreement': 'Γενική Συμφωνία Δημοσίευσης',
-  'legal.documentsFor': 'Νομικά έγγραφα για {workTitle}',
-}
+    'legal.modal.intro': 'Λυπούμαστε που διακόπτουμε. Πριν σας αφήσουμε να προχωρήσετε περαιτέρω, είμαστε νομικά υποχρεωμένοι, να σας ενημερώσουμε και να συμφωνήσετε με τους Όρους Χρήσης, την Πολιτική Απορρήτου και την Συμφωνία Πνευματικών Δικαιωμάτων μας.',
+    'legal.modal.agree': 'Συμφωνώ',
+    'legal.modal.disagree': 'Διαφωνώ',
+    'legal.modal.register': 'Δεν θα το ξαναδείτε αυτό αν εγγραφείτε.',
+    'legal.modal.goToSettings': 'Πηγαίνετε στις ρυθμίσεις σας',
+    'legal.modal.oneLastThing': 'Ένα τελευταίο πράγμα...',
+    'legal.modal.finish': 'Ολοκλήρωση',
+    'legal.modal.existingUser': 'Ήδη εγγεγραμμένος;',
+    'legal.modal.pleaseLogin': 'Παρακαλώ συνδεθείτε.',
+    'legal.publicationAgreements': 'Συμβάσεις δημοσίευσης',
+    'legal.generalPublishingAgreement': 'Γενική Συμφωνία Δημοσίευσης',
+    'legal.documentsFor': 'Νομικά έγγραφα για {workTitle}'
+};
