c85211092f4584c37b02500df95ffd432b74cc4c
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:40:59 +0200

    New translations pm.js (Nepali)

--- ne-NP/pm.js
+++ ne-NP/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'कुराकानी',
-  'pm.showolder': 'पुरानो सन्देश देखाउनुहोस्',
-  'pm.errors.saysomething': 'तपाईंले निश्चित रूपमा केही भन्नु पर्छ...',
-  'pm.errors.addrecipient': 'तपाईंले कम्तिमा एक अरु प्रयोगकर्ता थप्नुपर्छ।',
-  'pm.message': 'सन्देश',
-  'pm.send.message': 'सन्देश पठाउनुहोस्',
-  'pm.messages.own': 'तपाईंका सन्देशहरू',
-  'pm.conversations.none': 'तपाईंले हालसालै कसैसँग कुराकानी गर्दैनुहुन्न।',
-  'pm.to': 'सम्म:',
-  'pm.with': 'सँग कुराकानी:',
-  'pm.send': 'पठाउनुहोस्',
-  'pm.conversations.unreadCount': `तपाईंको अपठित कुराकानीहरू छन् {count, plural,
+    'pm.conversation': 'कुराकानी',
+    'pm.showolder': 'पुरानो सन्देश देखाउनुहोस्',
+    'pm.errors.saysomething': 'तपाईंले निश्चित रूपमा केही भन्नु पर्छ...',
+    'pm.errors.addrecipient': 'तपाईंले कम्तिमा एक अरु प्रयोगकर्ता थप्नुपर्छ।',
+    'pm.message': 'सन्देश',
+    'pm.send.message': 'सन्देश पठाउनुहोस्',
+    'pm.messages.own': 'तपाईंका सन्देशहरू',
+    'pm.conversations.none': 'तपाईंले हालसालै कसैसँग कुराकानी गर्दैनुहुन्न।',
+    'pm.to': 'सम्म:',
+    'pm.with': 'सँग कुराकानी:',
+    'pm.send': 'पठाउनुहोस्',
+    'pm.conversations.unreadCount': `तपाईंको अपठित कुराकानीहरू छन् {count, plural,
     zero {नभएको छ}
     one {एक अपठित कुराकानी छ}
     other {# अपठित कुराकानीहरू छन्}
   }.`,
-  'pm.form.minimumParticipants': 'तपाईंले प्राप्तकर्ता छान्नुपर्छ।',
-  'pm.form.maxParticipants': `तपाईं एक संवादमा अधिकतम {maxPeople, plural,
+    'pm.form.minimumParticipants': 'तपाईंले प्राप्तकर्ता छान्नुपर्छ।',
+    'pm.form.maxParticipants': `तपाईं एक संवादमा अधिकतम {maxPeople, plural,
     one {एक व्यक्ति}
     other {# व्यक्ति}
   } हुन सक्नुहुन्छ।`,
-  'pm.new.aria': 'नयाँ कुराकानी',
-  'pm.new.buttonTitle': 'नयाँ कुराकानी सुरु गर्नुहोस्',
-}
+    'pm.new.aria': 'नयाँ कुराकानी',
+    'pm.new.buttonTitle': 'नयाँ कुराकानी सुरु गर्नुहोस्'
+};
