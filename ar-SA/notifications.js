03ec3b37b25c8f20f73c8c3cbb6d91d7a118d714
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:14:16 +0200

    New translations notifications.js (Arabic)

--- ar-SA/notifications.js
+++ ar-SA/notifications.js
@@ -4,8 +4,7 @@ export default {
   'flashnews.startsAt': 'ابدأ عرض هذه الرسالة في',
   'flashnews.endsAt': 'توقف عن عرض هذه الرسالة في',
   'flashnews.newLanguage': 'اختر لغة جديدة للإضافة',
-  'flashnews.onlyDisplayOn':
-    'سيتم عرض الأخبار فقط في اللغات المحددة أدناه (إذا تم تحديد أي لغة)، لن يتم عرض أي أخبار، حتى في اللغة الافتراضية، في اللغات الأخرى.',
+  'flashnews.onlyDisplayOn': 'سيتم عرض الأخبار فقط في اللغات المحددة أدناه (إذا تم تحديد أي لغة)، لن يتم عرض أي أخبار، حتى في اللغة الافتراضية، في اللغات الأخرى.',
   'notifications.menu.all': 'كل الإشعارات',
   'notifications.menu.following': 'متابَعة',
   'notifications.menu.workshop': 'ورشة العمل',
@@ -14,10 +13,8 @@ export default {
   'notifications.menu.markRead': 'وضع علامة كمقروء',
   'notifications.menu.delete': 'حذف الإشعار',
   // Notification messages
-  'notification.workshopStoryStatus.toHiatus':
-    'تم تعليق قصتك {storyName} تلقائيًا بعد 6 أشهر من الخمول.',
-  'notification.workshopStoryStatus.toCancelled':
-    'تم إلغاء قصتك {storyName} تلقائيًا بعد عام من الخمول.',
+  'notification.workshopStoryStatus.toHiatus': 'تم تعليق قصتك {storyName} تلقائيًا بعد 6 أشهر من الخمول.',
+  'notification.workshopStoryStatus.toCancelled': 'تم إلغاء قصتك {storyName} تلقائيًا بعد عام من الخمول.',
   'notification.followingStoryStatus.toHiatus': '{storyName} في وضع الخمول.',
   'notification.followingStoryStatus.toCancelled': '{storyName} تم إلغاؤها.',
   'notification.followingNewChapter.new': 'تم نشر فصل جديد في {storyName} !',
@@ -26,5 +23,5 @@ export default {
   'notification.friendRequest': 'أرسل لك طلب صداقة.',
   // Before this text will be the username of the user
   // Strings for extra section
-  'notificationExtra.workshopStoryStatus.goToStory': 'الانتقال إلى القصة',
-}
+  'notificationExtra.workshopStoryStatus.goToStory': 'الانتقال إلى القصة'
+};
