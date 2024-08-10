ec38b42e2c88648ca3be90af764804f9cea09990
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:24:35 +0200

    New translations encyclopedia.js (Polish)

--- pl-PL/encyclopedia.js
+++ pl-PL/encyclopedia.js
@@ -1,45 +1,43 @@
 export default {
-  'encyclopedia.add': 'Dodaj nowy wpis',
-  'encyclopedia.browse': 'Przeglądaj encyklopedię',
-  'encyclopedia.search': 'Szukaj w encyklopedii',
-  'encyclopedia.back.universe': 'Powrót do strony uniwersum',
-  'encyclopedia.back.workshop': 'Powrót do warsztatu',
-  'encyclopedia.categories': 'Kategorie',
-  'encyclopedia.groups': 'Grupy',
-  'encyclopedia.category.character': 'Postaci',
-  'encyclopedia.category.event': 'Wydarzenia',
-  'encyclopedia.category.group': 'Grupy',
-  'encyclopedia.category.item': 'Przedmioty',
-  'encyclopedia.category.location': 'Lokalizacje',
-  'encyclopedia.category.other': 'Pozostałe',
-  'encyclopedia.category.state': 'Regiony',
-  'encyclopedia.category.governments': 'Rządy',
-  'encyclopedia.category.languages': 'Języki',
-  'encyclopedia.category.creatures': 'Stworzenia',
-  'encyclopedia.category.deity': 'Bóstwa',
-  'encyclopedia.category.religions': 'Religie',
-  'encyclopedia.category.flora': 'Roślinność',
-  'encyclopedia.category.magic': 'Magia',
-  'encyclopedia.category.planets': 'Planety',
-  'encyclopedia.category.species': 'Gatunki',
-  'encyclopedia.category.technologies': 'Technologie',
-  'encyclopedia.search.found': `{results, plural,
+    'encyclopedia.add': 'Dodaj nowy wpis',
+    'encyclopedia.browse': 'Przeglądaj encyklopedię',
+    'encyclopedia.search': 'Szukaj w encyklopedii',
+    'encyclopedia.back.universe': 'Powrót do strony uniwersum',
+    'encyclopedia.back.workshop': 'Powrót do warsztatu',
+    'encyclopedia.categories': 'Kategorie',
+    'encyclopedia.groups': 'Grupy',
+    'encyclopedia.category.character': 'Postaci',
+    'encyclopedia.category.event': 'Wydarzenia',
+    'encyclopedia.category.group': 'Grupy',
+    'encyclopedia.category.item': 'Przedmioty',
+    'encyclopedia.category.location': 'Lokalizacje',
+    'encyclopedia.category.other': 'Pozostałe',
+    'encyclopedia.category.state': 'Regiony',
+    'encyclopedia.category.governments': 'Rządy',
+    'encyclopedia.category.languages': 'Języki',
+    'encyclopedia.category.creatures': 'Stworzenia',
+    'encyclopedia.category.deity': 'Bóstwa',
+    'encyclopedia.category.religions': 'Religie',
+    'encyclopedia.category.flora': 'Roślinność',
+    'encyclopedia.category.magic': 'Magia',
+    'encyclopedia.category.planets': 'Planety',
+    'encyclopedia.category.species': 'Gatunki',
+    'encyclopedia.category.technologies': 'Technologie',
+    'encyclopedia.search.found': `{results, plural,
     =0 {Brak pasujących wpisów}
     one {1 pasujący wpis}
     few {# pasujące wpisy}
     many {# pasujących wpisów}
     other {# pasujących wpisów}
   }.`,
-  'encyclopedia.help.text':
-    'Użyj menu po lewej stronie, aby poruszać się pomiędzy elementami w encyklopedii. Po wybraniu elementu, zostanie on wyświetlony zamiast tego tekstu.',
-  'encyclopedia.help.sidebar': 'Boczny pasek narzędzi',
-  'encyclopedia.help.sidebar.text':
-    'Witamy w Twojej historii! Ten pasek boczny umożliwia ci łatwy dostęp do wszystkich zasobów związanych z twoją historią. Sprawdź ikony powyżej, aby zobaczyć najnowsze komentarze, swoje notatki, przeszukiwać encyklopedię, przeglądać i dodawać nowe wpisy do encyklopedii. Jeśli chcesz dodać połączenie do wpisu z tekstu opowiadania, zaznacz tekst, w którym chcesz je umieścić, a następnie kliknij przycisk połącz. Zostaniesz wtedy poproszony o znalezienie i wybranie elementu.',
-  'encyclopedia.intro': 'Witamy w {universe} encyklopedii.',
-  'encyclopedia.add.title': 'Dodaj nowy wpis do encyklopedii',
-  'encyclopedia.add.heading': 'Co chciałbyś dodać?',
-  'encyclopedia.add.moresoon': 'Więcej kategorii już wkrótce!',
-  'encyclopedia.create': `Dodaj {category, select,
+    'encyclopedia.help.text': 'Użyj menu po lewej stronie, aby poruszać się pomiędzy elementami w encyklopedii. Po wybraniu elementu, zostanie on wyświetlony zamiast tego tekstu.',
+    'encyclopedia.help.sidebar': 'Boczny pasek narzędzi',
+    'encyclopedia.help.sidebar.text': 'Witamy w Twojej historii! Ten pasek boczny umożliwia ci łatwy dostęp do wszystkich zasobów związanych z twoją historią. Sprawdź ikony powyżej, aby zobaczyć najnowsze komentarze, swoje notatki, przeszukiwać encyklopedię, przeglądać i dodawać nowe wpisy do encyklopedii. Jeśli chcesz dodać połączenie do wpisu z tekstu opowiadania, zaznacz tekst, w którym chcesz je umieścić, a następnie kliknij przycisk połącz. Zostaniesz wtedy poproszony o znalezienie i wybranie elementu.',
+    'encyclopedia.intro': 'Witamy w {universe} encyklopedii.',
+    'encyclopedia.add.title': 'Dodaj nowy wpis do encyklopedii',
+    'encyclopedia.add.heading': 'Co chciałbyś dodać?',
+    'encyclopedia.add.moresoon': 'Więcej kategorii już wkrótce!',
+    'encyclopedia.create': `Dodaj {category, select,
     character {postać}
     event {wydarzenie}
     group {grupę}
@@ -58,7 +56,7 @@ export default {
     species {gatunek}
     technologies {technologię}
   }`,
-  'encyclopedia.create.header': `Dodaj {category, select,
+    'encyclopedia.create.header': `Dodaj {category, select,
     character {postać}
     event {wydarzenie}
     group {grupę}
@@ -77,7 +75,7 @@ export default {
     species {gatunek}
     technologies {technologię}
   } do {universe}`,
-  'encyclopedia.entry.category': `Entry category: {category, select,
+    'encyclopedia.entry.category': `Entry category: {category, select,
     character {postać}
     event {wydarzenie}
     group {grupę}
@@ -96,80 +94,71 @@ export default {
     species {gatunek}
     technologies {technologię}
   }`,
-  'encyclopedia.entry.category.legend': 'Kategoria',
-  'encyclopedia.fields.add': 'Dodaj nowe pole',
-  'encyclopedia.fields.text': 'Definicja',
-  'encyclopedia.fields.textarea': 'Tekst',
-  'encyclopedia.fields.connection': 'Połączenie',
-  'encyclopedia.fields.time': 'Czas',
-  'encyclopedia.fields.image': 'Obraz',
-  'encyclopedia.visibility': 'Widoczność',
-  'encyclopedia.public': 'Publiczne',
-  'encyclopedia.private': 'Prywatne',
-  'encyclopedia.container': 'Zbiór',
-  'encyclopedia.container.desc':
-    'Uczyń ten wpis zbiorem grupującym wielu wpisów. Zostanie on ukryty w liście kategorii.',
-  'encyclopedia.entry.name': 'Nazwa wpisu',
-  'encyclopedia.summary': 'Podsumowanie',
-  'encyclopedia.summary.desc':
-    'To podsumowanie będzie jedyną początkową informacją widoczną w czytniku.',
-  'encyclopedia.parent': 'Wpis nadrzędny',
-  'encyclopedia.related': 'Powiązane wpisy',
-  'encyclopedia.errors.tags': 'Jeden wpis może mieć do 10 etykiet.',
-  'encyclopedia.field.private': 'Pole prywatne',
-  'encyclopedia.ft.indev': 'Ta opcja jest jeszcze rozwijana.',
-  'encyclopedia.connections.remove': 'Usuń połączenie',
-  'encyclopedia.connections.extra': 'Dodatkowe informacje',
-  'encyclopedia.connections.existing': 'Istniejący wpis',
-  'encyclopedia.connections.existing.abbr': 'IW',
-  // abbreviation for Existing Entry
-  'encyclopedia.cover': 'Obraz okładki',
-  'encyclopedia.translation.title': 'Witamy w {universe} tłumaczeniu wszechświata!',
-  'encyclopedia.translation.desc':
-    'Ten interfejs encyklopedii jest używany do tłumaczenia na {language}. Wybierz istniejące wpisy z listy po lewej stronie, aby rozpocząć ich tłumaczenie.',
-  'encyclopedia.welcome.totalEntries': `W tym wszechświecie {totalEntries, plural,
+    'encyclopedia.entry.category.legend': 'Kategoria',
+    'encyclopedia.fields.add': 'Dodaj nowe pole',
+    'encyclopedia.fields.text': 'Definicja',
+    'encyclopedia.fields.textarea': 'Tekst',
+    'encyclopedia.fields.connection': 'Połączenie',
+    'encyclopedia.fields.time': 'Czas',
+    'encyclopedia.fields.image': 'Obraz',
+    'encyclopedia.visibility': 'Widoczność',
+    'encyclopedia.public': 'Publiczne',
+    'encyclopedia.private': 'Prywatne',
+    'encyclopedia.container': 'Zbiór',
+    'encyclopedia.container.desc': 'Uczyń ten wpis zbiorem grupującym wielu wpisów. Zostanie on ukryty w liście kategorii.',
+    'encyclopedia.entry.name': 'Nazwa wpisu',
+    'encyclopedia.summary': 'Podsumowanie',
+    'encyclopedia.summary.desc': 'To podsumowanie będzie jedyną początkową informacją widoczną w czytniku.',
+    'encyclopedia.parent': 'Wpis nadrzędny',
+    'encyclopedia.related': 'Powiązane wpisy',
+    'encyclopedia.errors.tags': 'Jeden wpis może mieć do 10 etykiet.',
+    'encyclopedia.field.private': 'Pole prywatne',
+    'encyclopedia.ft.indev': 'Ta opcja jest jeszcze rozwijana.',
+    'encyclopedia.connections.remove': 'Usuń połączenie',
+    'encyclopedia.connections.extra': 'Dodatkowe informacje',
+    'encyclopedia.connections.existing': 'Istniejący wpis',
+    'encyclopedia.connections.existing.abbr': 'IW',
+    // abbreviation for Existing Entry
+    'encyclopedia.cover': 'Obraz okładki',
+    'encyclopedia.translation.title': 'Witamy w {universe} tłumaczeniu wszechświata!',
+    'encyclopedia.translation.desc': 'Ten interfejs encyklopedii jest używany do tłumaczenia na {language}. Wybierz istniejące wpisy z listy po lewej stronie, aby rozpocząć ich tłumaczenie.',
+    'encyclopedia.welcome.totalEntries': `W tym wszechświecie {totalEntries, plural,
     zero {nie ma żadnych wpisów}
     one {jest jedno wejście}
     few {są # wejścia}
     many {jest # wejść}
     other {jest # wejść}
     }.`,
-  'encyclopedia.settings.title': 'Ustawienia encyklopedii',
-  'encyclopedia.settings.categoriesDesc':
-    'Włączaj i wyłączaj kategorie, których chcesz używać w swojej encyklopedii.',
-  'encyclopedia.settings.introText':
-    'Komunikat powitalny, który ma być wyświetlany po opisie wszechświata.',
-  'encyclopedia.fields.placeholder': 'Nazwa pola',
-  'encyclopedia.fields.delete': 'Usuń pole',
-  'encyclopedia.timeOption.fictionalDate': 'Data fikcyjna',
-  'encyclopedia.timeOption.fictionalTime': 'Czas fikcyjny',
-  'encyclopedia.timeOption.fictionalDateTime': 'Fikcyjna data godzina',
-  'encyclopedia.timeOption.date': 'Data',
-  'encyclopedia.timeOption.time': 'Czas',
-  'encyclopedia.timeOption.dateTime': 'Data i czas',
-  'encyclopedia.settings.groupByCategories': 'Uwzględnij kategorie w listingu grupowym',
-  'encyclopedia.menuOpen': 'Otwórz menu Encyklopedia',
-  'encyclopedia.menuClose': 'Zamknij menu Encyklopedia',
-  'encyclopedia.menuAria': 'Menu encyklopedii',
-  'enc.newField': 'Nowe pole',
-  'enc.entries.limit': `Aktualnie masz {value, number} z {max, plural, one {} few {{max, number}} many {{max, number}}
+    'encyclopedia.settings.title': 'Ustawienia encyklopedii',
+    'encyclopedia.settings.categoriesDesc': 'Włączaj i wyłączaj kategorie, których chcesz używać w swojej encyklopedii.',
+    'encyclopedia.settings.introText': 'Komunikat powitalny, który ma być wyświetlany po opisie wszechświata.',
+    'encyclopedia.fields.placeholder': 'Nazwa pola',
+    'encyclopedia.fields.delete': 'Usuń pole',
+    'encyclopedia.timeOption.fictionalDate': 'Data fikcyjna',
+    'encyclopedia.timeOption.fictionalTime': 'Czas fikcyjny',
+    'encyclopedia.timeOption.fictionalDateTime': 'Fikcyjna data godzina',
+    'encyclopedia.timeOption.date': 'Data',
+    'encyclopedia.timeOption.time': 'Czas',
+    'encyclopedia.timeOption.dateTime': 'Data i czas',
+    'encyclopedia.settings.groupByCategories': 'Uwzględnij kategorie w listingu grupowym',
+    'encyclopedia.menuOpen': 'Otwórz menu Encyklopedia',
+    'encyclopedia.menuClose': 'Zamknij menu Encyklopedia',
+    'encyclopedia.menuAria': 'Menu encyklopedii',
+    'enc.newField': 'Nowe pole',
+    'enc.entries.limit': `Aktualnie masz {value, number} z {max, plural, one {} few {{max, number}} many {{max, number}}
     =0 {nieograniczony}
     other {{max, number}}
   } wpisy.`,
-  'enc.entries.limitReached':
-    'Osiągnąłeś maksymalną dozwoloną liczbę wpisów do encyklopedii dla bieżącej subskrypcji.',
-  'enc.menuControls.ariaDesc': 'Kontrolki encyklopedii',
-  'encyclopedia.pronunciation.title': 'Wymowa',
-  'encyclopedia.wip.abbreviation': 'WIP',
-  'encyclopedia.wip.full': 'Praca w toku',
-  'encyclopedia.settings.templateLegend': 'Wszechświat szablonowy',
-  'encyclopedia.settings.templateDescription':
-    'Zezwól na kopiowanie zawartości tej encyklopedii do innych wszechświatów. Jest to najlepsze rozwiązanie, jeśli nie masz żadnych historii i chcesz stworzyć szablon do wykorzystania przez innych.',
-  'encyclopedia.settings.templateCheckbox': 'To jest wszechświat szablonowy',
-  'encyclopedia.templateCopy.modalTitle': 'Skopiuj zawartość encyklopedii',
-  'encyclopedia.templateCopy.description':
-    'Skopiuj zawartość tej encyklopedii do swojego wszechświata, aby go wzbogacić.',
-  'encyclopedia.templateCopy.targetUniverse':
-    'Wszechświat, do którego ma zostać skopiowana zawartość',
-  'encyclopedia.templateCopy.finished': 'Kopiowanie zakończone',
-}
+    'enc.entries.limitReached': 'Osiągnąłeś maksymalną dozwoloną liczbę wpisów do encyklopedii dla bieżącej subskrypcji.',
+    'enc.menuControls.ariaDesc': 'Kontrolki encyklopedii',
+    'encyclopedia.pronunciation.title': 'Wymowa',
+    'encyclopedia.wip.abbreviation': 'WIP',
+    'encyclopedia.wip.full': 'Praca w toku',
+    'encyclopedia.settings.templateLegend': 'Wszechświat szablonowy',
+    'encyclopedia.settings.templateDescription': 'Zezwól na kopiowanie zawartości tej encyklopedii do innych wszechświatów. Jest to najlepsze rozwiązanie, jeśli nie masz żadnych historii i chcesz stworzyć szablon do wykorzystania przez innych.',
+    'encyclopedia.settings.templateCheckbox': 'To jest wszechświat szablonowy',
+    'encyclopedia.templateCopy.modalTitle': 'Skopiuj zawartość encyklopedii',
+    'encyclopedia.templateCopy.description': 'Skopiuj zawartość tej encyklopedii do swojego wszechświata, aby go wzbogacić.',
+    'encyclopedia.templateCopy.targetUniverse': 'Wszechświat, do którego ma zostać skopiowana zawartość',
+    'encyclopedia.templateCopy.finished': 'Kopiowanie zakończone'
+};
