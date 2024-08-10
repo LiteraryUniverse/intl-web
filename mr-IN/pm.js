6581ea8d4ba3974ad40b1c513b69541bd1424a87
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:32:39 +0200

    New translations pm.js (Marathi)

--- mr-IN/pm.js
+++ mr-IN/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'वादांवाद',
-  'pm.showolder': 'जुने संदेश दाखवा',
-  'pm.errors.saysomething': 'तुम्हाला खरंतर काहीतरी सांगायला पाहिजे...',
-  'pm.errors.addrecipient': 'तुम्हाला किमान एक इतर वापरकर्ता जोडायला आवश्यक आहे.',
-  'pm.message': 'संदेश',
-  'pm.send.message': 'संदेश पाठवा',
-  'pm.messages.own': 'तुमचे संदेश',
-  'pm.conversations.none': 'तुम्ही सध्या कोणताही वादांवाद करत नाहीत.',
-  'pm.to': 'कोणाला:',
-  'pm.with': 'वादांवाद सह:',
-  'pm.send': 'पाठवा',
-  'pm.conversations.unreadCount': `तुम्हाला {count, plural,
+    'pm.conversation': 'वादांवाद',
+    'pm.showolder': 'जुने संदेश दाखवा',
+    'pm.errors.saysomething': 'तुम्हाला खरंतर काहीतरी सांगायला पाहिजे...',
+    'pm.errors.addrecipient': 'तुम्हाला किमान एक इतर वापरकर्ता जोडायला आवश्यक आहे.',
+    'pm.message': 'संदेश',
+    'pm.send.message': 'संदेश पाठवा',
+    'pm.messages.own': 'तुमचे संदेश',
+    'pm.conversations.none': 'तुम्ही सध्या कोणताही वादांवाद करत नाहीत.',
+    'pm.to': 'कोणाला:',
+    'pm.with': 'वादांवाद सह:',
+    'pm.send': 'पाठवा',
+    'pm.conversations.unreadCount': `तुम्हाला {count, plural,
     zero {कोणतेही वाचनांचे वादांवाद नाहीत}
     one {एक वाचनांचे वादांवाद नाही}
     other {# वाचनांचे वादांवाद नाहीत}
   }.`,
-  'pm.form.minimumParticipants': 'तुम्हाला एक प्राप्तकर्ता निवडायला आवश्यक आहे.',
-  'pm.form.maxParticipants': `तुम्ही एक चर्चेत {maxPeople, plural,
+    'pm.form.minimumParticipants': 'तुम्हाला एक प्राप्तकर्ता निवडायला आवश्यक आहे.',
+    'pm.form.maxParticipants': `तुम्ही एक चर्चेत {maxPeople, plural,
     one {अधिकतम एक व्यक्ती}
     other {# लोक}
   } असू शकता.`,
-  'pm.new.aria': 'नवीन वादांवाद',
-  'pm.new.buttonTitle': 'नवीन वादांवाद सुरू करा',
-}
+    'pm.new.aria': 'नवीन वादांवाद',
+    'pm.new.buttonTitle': 'नवीन वादांवाद सुरू करा'
+};
