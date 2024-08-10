bb949490bb33b3b1a3c18ce0c04f690ee5489129
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:28:41 +0200

    New translations pm.js (Chinese Traditional)

--- zh-TW/pm.js
+++ zh-TW/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': '對話',
-  'pm.showolder': '顯示較早的訊息',
-  'pm.errors.saysomething': '你應該說些什麼...',
-  'pm.errors.addrecipient': '你需要新增至少一個其他使用者。',
-  'pm.message': '訊息',
-  'pm.send.message': '發送訊息',
-  'pm.messages.own': '你的訊息',
-  'pm.conversations.none': '你目前沒有與任何人對話。',
-  'pm.to': '收件者：',
-  'pm.with': '與以下人對話：',
-  'pm.send': '發送',
-  'pm.conversations.unreadCount': `你有 {count, plural,
+    'pm.conversation': '對話',
+    'pm.showolder': '顯示較早的訊息',
+    'pm.errors.saysomething': '你應該說些什麼...',
+    'pm.errors.addrecipient': '你需要新增至少一個其他使用者。',
+    'pm.message': '訊息',
+    'pm.send.message': '發送訊息',
+    'pm.messages.own': '你的訊息',
+    'pm.conversations.none': '你目前沒有與任何人對話。',
+    'pm.to': '收件者：',
+    'pm.with': '與以下人對話：',
+    'pm.send': '發送',
+    'pm.conversations.unreadCount': `你有 {count, plural,
     zero {沒有未讀對話}
     one {一個未讀對話}
     other {# 個未讀對話}
   }。`,
-  'pm.form.minimumParticipants': '你需要選擇收件者。',
-  'pm.form.maxParticipants': `您在一個對話中最多只能有 {maxPeople, plural,
+    'pm.form.minimumParticipants': '你需要選擇收件者。',
+    'pm.form.maxParticipants': `您在一個對話中最多只能有 {maxPeople, plural,
     one {一個人}
     other {# 人}
   }。`,
-  'pm.new.aria': '新對話',
-  'pm.new.buttonTitle': '開始新對話',
-}
+    'pm.new.aria': '新對話',
+    'pm.new.buttonTitle': '開始新對話'
+};
