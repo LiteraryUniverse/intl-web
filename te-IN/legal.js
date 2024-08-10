8d86981de9e2c31627f9a5843e6e32c00c3fb73e
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:35:33 +0200

    New translations legal.js (Telugu)

--- te-IN/legal.js
+++ te-IN/legal.js
@@ -1,15 +1,14 @@
 export default {
-  'legal.modal.intro':
-    'దయచేసి మధ్యలో ముందుకు వెళ్ళని ముందు, మేము నియమాలను సేకరించడానికి, మా సేవల నిబంధనలను, గోప్యతా నియమాలను మరియు కాపీరైట్ ఒప్పందాన్ని మీకు తెలియజేయాలని కానీ, దయచేసి తరచుగా అంగీకరించండి.',
-  'legal.modal.agree': 'నాకు అంగీకారం',
-  'legal.modal.disagree': 'నాకు అంగీకారం లేదు',
-  'legal.modal.register': 'మీరు నమోదు చేస్తే మీరు ఇక్కడ ఇక్కడ చూడలేరు.',
-  'legal.modal.goToSettings': 'మీ సెట్టింగ్స్‌కు వెళ్ళండి',
-  'legal.modal.oneLastThing': 'ఒక చివరి విషయం...',
-  'legal.modal.finish': 'ముగింపు',
-  'legal.modal.existingUser': 'ఈరోజు నమోదు చేయబడాలా?',
-  'legal.modal.pleaseLogin': 'దయచేసి లాగిన్ చేయండి.',
-  'legal.publicationAgreements': 'ప్రచురణ ఒప్పందాలు',
-  'legal.generalPublishingAgreement': 'సాధారణ ప్రచురణ ఒప్పందం',
-  'legal.documentsFor': '{workTitle}కోసం కానూనీ పత్రాలు',
-}
+    'legal.modal.intro': 'దయచేసి మధ్యలో ముందుకు వెళ్ళని ముందు, మేము నియమాలను సేకరించడానికి, మా సేవల నిబంధనలను, గోప్యతా నియమాలను మరియు కాపీరైట్ ఒప్పందాన్ని మీకు తెలియజేయాలని కానీ, దయచేసి తరచుగా అంగీకరించండి.',
+    'legal.modal.agree': 'నాకు అంగీకారం',
+    'legal.modal.disagree': 'నాకు అంగీకారం లేదు',
+    'legal.modal.register': 'మీరు నమోదు చేస్తే మీరు ఇక్కడ ఇక్కడ చూడలేరు.',
+    'legal.modal.goToSettings': 'మీ సెట్టింగ్స్‌కు వెళ్ళండి',
+    'legal.modal.oneLastThing': 'ఒక చివరి విషయం...',
+    'legal.modal.finish': 'ముగింపు',
+    'legal.modal.existingUser': 'ఈరోజు నమోదు చేయబడాలా?',
+    'legal.modal.pleaseLogin': 'దయచేసి లాగిన్ చేయండి.',
+    'legal.publicationAgreements': 'ప్రచురణ ఒప్పందాలు',
+    'legal.generalPublishingAgreement': 'సాధారణ ప్రచురణ ఒప్పందం',
+    'legal.documentsFor': '{workTitle}కోసం కానూనీ పత్రాలు'
+};
