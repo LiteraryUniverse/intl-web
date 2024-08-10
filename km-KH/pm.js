edccaa3692003820fe66ab5310fcee85418d1faf
Jan Dvorak <hello@jandvorak.me>
2024-08-10 19:30:57 +0200

    New translations pm.js (Khmer)

--- km-KH/pm.js
+++ km-KH/pm.js
@@ -1,25 +1,25 @@
 export default {
-  'pm.conversation': 'ការសន្ទនា',
-  'pm.showolder': 'បង្ហាញសារចាស់បែប',
-  'pm.errors.saysomething': 'អ្នកគួរតែប្រាប់ថាអ្នកអាចនិយាយអ្វីម្តងទៀតហើយ...',
-  'pm.errors.addrecipient': 'អ្នកត្រូវតែបន្ថែមអ្នកប្រើប្រាស់ម្នាក់ទៀតយ៉ាងតិច១នាក់។',
-  'pm.message': 'សារ',
-  'pm.send.message': 'ផ្ញើសារ',
-  'pm.messages.own': 'សាររបស់អ្នក',
-  'pm.conversations.none': 'អ្នកមិនកំពុងលក់ទំនេរជាមួយនរណា។',
-  'pm.to': 'ទៅ៖',
-  'pm.with': 'ការសន្ទនាជាមួយ៖',
-  'pm.send': 'ផ្ញើ',
-  'pm.conversations.unreadCount': `អ្នកមាន {count, plural,
+    'pm.conversation': 'ការសន្ទនា',
+    'pm.showolder': 'បង្ហាញសារចាស់បែប',
+    'pm.errors.saysomething': 'អ្នកគួរតែប្រាប់ថាអ្នកអាចនិយាយអ្វីម្តងទៀតហើយ...',
+    'pm.errors.addrecipient': 'អ្នកត្រូវតែបន្ថែមអ្នកប្រើប្រាស់ម្នាក់ទៀតយ៉ាងតិច១នាក់។',
+    'pm.message': 'សារ',
+    'pm.send.message': 'ផ្ញើសារ',
+    'pm.messages.own': 'សាររបស់អ្នក',
+    'pm.conversations.none': 'អ្នកមិនកំពុងលក់ទំនេរជាមួយនរណា។',
+    'pm.to': 'ទៅ៖',
+    'pm.with': 'ការសន្ទនាជាមួយ៖',
+    'pm.send': 'ផ្ញើ',
+    'pm.conversations.unreadCount': `អ្នកមាន {count, plural,
     zero {ការសន្ទនាដែលមិនទាន់អានទេ}
     one {ការសន្ទនាដែលអានបានមួយការ}
     other {ការសន្ទនាដែលអានបាន # ការសន្ទនាមួយ}
   }។`,
-  'pm.form.minimumParticipants': 'អ្នកត្រូវតែជ្រើសអ្នកទទួលបាន។',
-  'pm.form.maxParticipants': `អ្នកអាចមានអត្តបទអតិបរមា {maxPeople, plural,
+    'pm.form.minimumParticipants': 'អ្នកត្រូវតែជ្រើសអ្នកទទួលបាន។',
+    'pm.form.maxParticipants': `អ្នកអាចមានអត្តបទអតិបរមា {maxPeople, plural,
     one {មួយនាក់}
     other {# មនុស្ស}
   } ក្នុងការសន្ទនា។`,
-  'pm.new.aria': 'ការសន្ទនាថ្មី',
-  'pm.new.buttonTitle': 'ចាប់ផ្ដើមការសន្ទនាថ្មី',
-}
+    'pm.new.aria': 'ការសន្ទនាថ្មី',
+    'pm.new.buttonTitle': 'ចាប់ផ្ដើមការសន្ទនាថ្មី'
+};
