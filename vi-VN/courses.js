fc9452a2d4ed6ad5d4426caff1d82c54b69e7c31
Jan Dvorak <hello@jandvorak.me>
2024-09-17 11:13:01 +0200

    New translations courses.js (Vietnamese)

--- vi-VN/courses.js
+++ vi-VN/courses.js
@@ -29,4 +29,8 @@ export default {
   'course.lesson.title': 'Tiêu đề bài học',
   'course.lesson.add.universe': 'Thêm Câu chuyện/Vũ trụ',
   'course.lesson.add.link': 'Thêm liên kết ngoài',
+  'course.status': 'Status',
+  'course.active': 'Activity',
+  'course.active.message': 'Active',
+  'course.inactive': 'Inactive',
 };
