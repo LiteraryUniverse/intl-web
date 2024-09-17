ca0212fa878cd378d289a6da72129ff3f9cf60cd
Jan Dvorak <hello@jandvorak.me>
2024-09-17 11:12:52 +0200

    New translations courses.js (Norwegian)

--- no-NO/courses.js
+++ no-NO/courses.js
@@ -29,4 +29,8 @@ export default {
   'course.lesson.title': 'Leksjonstittel',
   'course.lesson.add.universe': 'Legg til Historie/Univers',
   'course.lesson.add.link': 'Legg til ekstern lenke',
+  'course.status': 'Status',
+  'course.active': 'Activity',
+  'course.active.message': 'Active',
+  'course.inactive': 'Inactive',
 };
