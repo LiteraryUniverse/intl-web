991fc289f2afa43ea67f549dc8105da4c8d08e88
Jan Dvorak <hello@jandvorak.me>
2024-09-26 12:29:58 +0200

    New translations writer.js (Marathi)

--- mr-IN/writer.js
+++ mr-IN/writer.js
@@ -25,5 +25,6 @@ export default {
     'writer.publish.previousChapterScheduled': 'मागील अध्याय अधिकारात उपलब्ध होण्याची तारीख निश्चित केली होती {publishDate, date, short} रोजी {publishDate, time}वाजता',
     'writer.publish.alreadyPublished': 'हा अध्याय आधीच प्रकाशित झाला होता {publishedAt, date, short}. नवीन आवृत्तींनी तत्परतेने प्रकाशित केली जाईल.',
     'writer.publish.betaReader': 'बीटा',
-    'writer.publish.public': 'प्रकाशित करा'
+    'writer.publish.public': 'प्रकाशित करा',
+    'writer.publish.now': 'Publish now'
 };
