13ac192470a701d9de64c042879f43790e1ed9c6
Jan Dvorak <hello@jandvorak.me>
2024-09-26 12:31:29 +0200

    New translations writer.js (Malay)

--- ms-MY/writer.js
+++ ms-MY/writer.js
@@ -25,5 +25,6 @@ export default {
     'writer.publish.previousChapterScheduled': 'Bab sebelum ini dijadualkan untuk diterbitkan pada {publishDate, date, short} pada {publishDate, time}',
     'writer.publish.alreadyPublished': 'Bab ini telah diterbitkan pada {publishedAt, date, short}. Versi baru akan diterbitkan dengan segera.',
     'writer.publish.betaReader': 'Beta',
-    'writer.publish.public': 'Terbitkan'
+    'writer.publish.public': 'Terbitkan',
+    'writer.publish.now': 'Publish now'
 };
