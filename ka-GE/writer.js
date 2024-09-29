3ef1fb924b6b563894dedbdcc172f02509ae5b30
Jan Dvorak <hello@jandvorak.me>
2024-09-29 12:36:32 +0200

    New translations writer.js (Georgian)

--- ka-GE/writer.js
+++ ka-GE/writer.js
@@ -26,5 +26,6 @@ export default {
     'writer.publish.alreadyPublished': 'ეს თავისუფალი თავად გამოქვეყნდა {publishedAt, date, short}. ახალი ვერსიები იქნება გამოქვეყნებული მყიდველი.',
     'writer.publish.betaReader': 'ბეტა',
     'writer.publish.public': 'Publish',
-    'writer.publish.now': 'Publish now'
+    'writer.publish.now': 'Publish now',
+    'writer.publish.schedulePrevious': 'You need to publish or schedule publication of previous chapter before you can publish this one.'
 };
