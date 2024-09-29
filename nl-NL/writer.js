30d641656b9a302410765f81dca564557eb47add
Jan Dvorak <hello@jandvorak.me>
2024-09-29 12:37:26 +0200

    New translations writer.js (Dutch)

--- nl-NL/writer.js
+++ nl-NL/writer.js
@@ -26,5 +26,6 @@ export default {
     'writer.publish.alreadyPublished': 'Dit hoofdstuk is al gepubliceerd op {publishedAt, date, short}. Nieuwe versies worden direct gepubliceerd.',
     'writer.publish.betaReader': 'Bèta',
     'writer.publish.public': 'Publiceren',
-    'writer.publish.now': 'Publish now'
+    'writer.publish.now': 'Publish now',
+    'writer.publish.schedulePrevious': 'You need to publish or schedule publication of previous chapter before you can publish this one.'
 };
