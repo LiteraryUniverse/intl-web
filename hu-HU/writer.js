f586a0a39f1eb27fb52cf1598c65409fc083175f
Jan Dvorak <hello@jandvorak.me>
2024-09-29 12:35:36 +0200

    New translations writer.js (Hungarian)

--- hu-HU/writer.js
+++ hu-HU/writer.js
@@ -26,5 +26,6 @@ export default {
     'writer.publish.alreadyPublished': 'Ez a fejezet már közzétéve volt ekkor: {publishedAt, date, short}. Az új verziók azonnal közzététele történik.',
     'writer.publish.betaReader': 'Béta',
     'writer.publish.public': 'Közzététel',
-    'writer.publish.now': 'Publish now'
+    'writer.publish.now': 'Publish now',
+    'writer.publish.schedulePrevious': 'You need to publish or schedule publication of previous chapter before you can publish this one.'
 };
