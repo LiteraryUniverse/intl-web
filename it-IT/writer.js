c252c45664263f28de9a70c1c3c298d2ef29b6eb
Jan Dvorak <hello@jandvorak.me>
2024-09-29 12:35:54 +0200

    New translations writer.js (Italian)

--- it-IT/writer.js
+++ it-IT/writer.js
@@ -26,5 +26,6 @@ export default {
     'writer.publish.alreadyPublished': 'Questo capitolo è già stato pubblicato il {publishedAt, date, short}. Le nuove versioni saranno pubblicate immediatamente.',
     'writer.publish.betaReader': 'Beta',
     'writer.publish.public': 'Pubblica',
-    'writer.publish.now': 'Publish now'
+    'writer.publish.now': 'Publish now',
+    'writer.publish.schedulePrevious': 'You need to publish or schedule publication of previous chapter before you can publish this one.'
 };
