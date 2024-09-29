0cea2f500b526f044ef6d331a803a9f323cdac5e
Jan Dvorak <hello@jandvorak.me>
2024-09-29 12:37:44 +0200

    New translations writer.js (Norwegian)

--- no-NO/writer.js
+++ no-NO/writer.js
@@ -26,5 +26,6 @@ export default {
     'writer.publish.alreadyPublished': 'Dette kapittelet ble allerede publisert den {publishedAt, date, short}. Nye versjoner vil bli publisert umiddelbart.',
     'writer.publish.betaReader': 'Beta',
     'writer.publish.public': 'Publiser',
-    'writer.publish.now': 'Publish now'
+    'writer.publish.now': 'Publish now',
+    'writer.publish.schedulePrevious': 'You need to publish or schedule publication of previous chapter before you can publish this one.'
 };
