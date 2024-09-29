35b5880d332a6f5d36a8b88d6a2f23bd045348d7
Jan Dvorak <hello@jandvorak.me>
2024-09-29 12:39:16 +0200

    New translations writer.js (Slovak)

--- sk-SK/writer.js
+++ sk-SK/writer.js
@@ -26,5 +26,6 @@ export default {
     'writer.publish.alreadyPublished': 'Táto kapitola už bola uverejnená {publishedAt, date, short}. Nové verzie budú uverejnené okamžite.',
     'writer.publish.betaReader': 'Beta',
     'writer.publish.public': 'Publikovať',
-    'writer.publish.now': 'Publish now'
+    'writer.publish.now': 'Publish now',
+    'writer.publish.schedulePrevious': 'You need to publish or schedule publication of previous chapter before you can publish this one.'
 };
