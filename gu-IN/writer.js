4308495d93d4531fd204e1579978006f676e8901
Jan Dvorak <hello@jandvorak.me>
2024-10-04 08:56:15 +0200

    New translations writer.js (Gujarati)

--- gu-IN/writer.js
+++ gu-IN/writer.js
@@ -26,10 +26,6 @@ export default {
     'writer.publish.alreadyPublished': 'આ ચાપ પહેલેથી પ્રકાશિત કરવામાં આવ્યી છે {publishedAt, date, short}. નવા આવૃત્તિઓ તત્કાલ પ્રકાશિત થશે.',
     'writer.publish.betaReader': 'બીટા',
     'writer.publish.public': 'Publish',
-<<<<<<< HEAD
-    'writer.publish.now': 'Publish now'
-=======
     'writer.publish.now': 'Publish now',
     'writer.publish.schedulePrevious': 'You need to publish or schedule publication of previous chapter before you can publish this one.'
->>>>>>> origin/l10n_master
 };
