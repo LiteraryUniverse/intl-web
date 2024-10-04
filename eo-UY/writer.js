9e4e352195122930b4d1f214208f6c4bb5a3f75c
Jan Dvorak <hello@jandvorak.me>
2024-10-04 09:07:14 +0200

    New translations writer.js (Esperanto)

--- eo-UY/writer.js
+++ eo-UY/writer.js
@@ -26,6 +26,6 @@ export default {
     'writer.publish.alreadyPublished': 'Ĉi tiu ĉapitro jam estis publikigita je {publishedAt, date, short}. Novaj versioj estos publikigitaj tuj.',
     'writer.publish.betaReader': 'Beta',
     'writer.publish.public': 'Publikigi',
-    'writer.publish.now': 'Publikigu nun',
-    'writer.publish.schedulePrevious': 'Vi devas publikigi aŭ plani la publikigon de la antaŭa ĉapitro antaŭ ol vi povas publikigi ĉi tiun.'
+    'writer.publish.now': 'Publish now',
+    'writer.publish.schedulePrevious': 'You need to publish or schedule publication of previous chapter before you can publish this one.'
 };
