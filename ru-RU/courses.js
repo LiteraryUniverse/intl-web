1cab1e87fc598fbf02de089c2cd1d734728adea2
Jan Dvorak <hello@jandvorak.me>
2024-11-16 18:26:34 +0900

    New translations courses.js (Russian)

--- ru-RU/courses.js
+++ ru-RU/courses.js
@@ -53,5 +53,5 @@ export default {
   'course.remove.story': 'Удалить историю',
   'course.dateOfPublish': 'Дата выпуска',
   'course.lesson.meetInPerson': 'Личная встреча',
-  'course.curriculum.edit': 'Edit curriculum',
+  'course.curriculum.edit': 'Изменить план',
 };
