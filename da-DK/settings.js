47f02ece67e1a8f10cf8886b498c6d5eeeae94a1
Jan Dvorak <hello@jandvorak.me>
2025-01-22 23:07:21 +0100

    New translations settings.js (Danish)

--- da-DK/settings.js
+++ da-DK/settings.js
@@ -114,5 +114,8 @@ export default {
     'settings.accessibility.paragraphHighlighting': 'Fremhæv det aktuelle afsnit, som du holder over',
     'settings.accessibility.ruler': 'Tilknyt en lineal til musen',
     'settings.tab.legal': 'Juridiske dokumenter',
-    'settings.tab.exportDelete': 'Eksporter data og slet konto'
+    'settings.tab.exportDelete': 'Eksporter data og slet konto',
+    'settings.languages.uiLanguage': 'Interface language',
+    'settings.languages.readingLanguage': 'Reading language',
+    'settings.languages.readingLanguageExplained': 'When looking up stories we will offer you stories and their translations in this language unless you specify otherwise.'
 };
