d7e4f17a338c251923f65858bafdb85dff858e39
Jan Dvorak <hello@jandvorak.me>
2025-01-22 14:43:48 +0100

    New translations sharing.js (Indonesian)

--- id-ID/sharing.js
+++ id-ID/sharing.js
@@ -20,5 +20,6 @@ export default {
     'sharing.gettr': 'Gettr',
     'sharing.minds': 'Minds.com',
     'sharing.instagram': 'Instagram',
-    'sharing.reddit': 'Reddit'
+    'sharing.reddit': 'Reddit',
+    'sharing.bsky': 'BlueSky'
 };
