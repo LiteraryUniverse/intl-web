71e38fb68c0f1af79e76a01546e1955e12bbb2c0
Jan Dvorak <hello@jandvorak.me>
2025-01-23 16:14:03 +0100

    New translations library.js (Greek)

--- el-GR/library.js
+++ el-GR/library.js
@@ -3,7 +3,7 @@ export default {
     'library.remove': 'Αφαίρεση από τη βιβλιοθήκη',
     'library.bought': 'Αγόρασε ιστορίες',
     'library.stories': 'Ακολούθησαν ιστορίες',
-    'library.universes': 'Followed universes',
-    'bookmark.continueReading': 'Continue reading',
-    'bookmark.statusSummary': 'You have read {readPercent, number, ::percent} of the story.'
+    'library.universes': 'Ακολουθούμενα σύμπαντα',
+    'bookmark.continueReading': 'Συνεχίστε να διαβάζετε',
+    'bookmark.statusSummary': 'Έχετε διαβάσει {readPercent, number, ::percent} της ιστορίας.'
 };
