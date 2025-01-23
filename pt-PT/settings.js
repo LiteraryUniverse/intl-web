0d703bbc6fad92055afecc44b0aa37c4a27136a0
Jan Dvorak <hello@jandvorak.me>
2025-01-23 16:21:00 +0100

    New translations settings.js (Portuguese)

--- pt-PT/settings.js
+++ pt-PT/settings.js
@@ -115,7 +115,7 @@ export default {
     'settings.accessibility.ruler': 'Anexe uma régua ao rato',
     'settings.tab.legal': 'Documentos legais',
     'settings.tab.exportDelete': 'Exportar dados e excluir conta',
-    'settings.languages.uiLanguage': 'Interface language',
-    'settings.languages.readingLanguage': 'Reading language',
-    'settings.languages.readingLanguageExplained': 'When looking up stories we will offer you stories and their translations in this language unless you specify otherwise.'
+    'settings.languages.uiLanguage': 'Língua de interface',
+    'settings.languages.readingLanguage': 'Língua de leitura',
+    'settings.languages.readingLanguageExplained': 'Ao procurar histórias, oferecemos-lhe histórias e respectivas traduções nesta língua, a menos que especifique o contrário.'
 };
