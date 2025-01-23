4a7c720a93738a9ed4931a8677ae0001f8cbbd87
Jan Dvorak <hello@jandvorak.me>
2025-01-23 16:23:36 +0100

    New translations groups.js (Chinese Traditional)

--- zh-TW/groups.js
+++ zh-TW/groups.js
@@ -11,11 +11,11 @@ export default {
     'groups.type.hiddenDesc': '隱藏 - 只有您邀請的人才能加入。',
     'groups.imageAlt': '{groupName} 張圖片',
     'groups.members': '成員',
-    'groups.members.total': `There {totalMembers, plural,
-    zero {are no members}
-    one {is one member}
-    other {are # members}
-  } in this group`,
+    'groups.members.total': `{totalMembers, plural,
+    zero {沒有成員}
+    one {有一位成員}
+    other {有 # 位成員}
+  } 在這個群組中`,
     'groups.leaveGroup': '離開群組',
     'groups.leaveGroup.confirm': '只是確認一下。您確定要離開 {groupName}嗎？',
     'group.settings': '群組設定',
