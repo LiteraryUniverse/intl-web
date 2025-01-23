e67d08c12d71da800180a343a4f219e0a209f57a
Jan Dvorak <hello@jandvorak.me>
2025-01-23 16:24:28 +0100

    New translations groups.js (Portuguese, Brazilian)

--- pt-BR/groups.js
+++ pt-BR/groups.js
@@ -11,11 +11,11 @@ export default {
     'groups.type.hiddenDesc': 'Oculto - Apenas as pessoas que você convidar podem ingressar.',
     'groups.imageAlt': '{groupName} imagem',
     'groups.members': 'Membros',
-    'groups.members.total': `There {totalMembers, plural,
-    zero {are no members}
-    one {is one member}
-    other {are # members}
-  } in this group`,
+    'groups.members.total': `Não há {totalMembers, plural,
+    zero {nenhum membro}
+    one {há um membro}
+    other {há # membros}
+  } neste grupo`,
     'groups.leaveGroup': 'Sair do grupo',
     'groups.leaveGroup.confirm': 'Apenas para ter certeza. Você tem certeza de que deseja sair de {groupName}?',
     'group.settings': 'Configurações do grupo',
