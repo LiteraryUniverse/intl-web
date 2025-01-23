4cb19531d89446c33395cc32c3baa833e29426c6
Jan Dvorak <hello@jandvorak.me>
2025-01-23 01:06:00 +0100

    New translations organizations.js (Swedish)

--- sv-SE/organizations.js
+++ sv-SE/organizations.js
@@ -9,7 +9,7 @@ export default {
     'org.settings.language': 'Primärt språk',
     'org.meta.desc': 'Organisationer och kreativa grupper på Litterär Universum.',
     'org.luOrgs': 'Litterär Universum Organisationer',
-    'org.otherOrgs': 'Gillen & Organisationer',
+    'org.otherOrgs': 'Organizations',
     'org.aboutTitle': 'Om {orgName}',
     'org.membersTitle': 'Organisationsmedlemmar',
     'org.membersNone': 'Medlemmar är inte offentliga',
