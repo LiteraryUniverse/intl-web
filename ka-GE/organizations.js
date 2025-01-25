8dc50f0b9620a522e031f6e959d7d6cee5e0a963
Jan Dvorak <hello@jandvorak.me>
2025-01-25 23:56:07 +0100

    New translations organizations.js (Georgian)

--- ka-GE/organizations.js
+++ ka-GE/organizations.js
@@ -55,5 +55,10 @@ export default {
     'org.workshop.ownerUser': 'არ არის გილდის სამსახური',
     'org.workshop.caption': 'გილდის სახელმწიფო',
     'org.workshop.title': 'ორგანიზაციის სამუშაობა',
-    'org.title': 'ორგანიზაცია'
+    'org.title': 'ორგანიზაცია',
+    'org.hall.title': '{orgName} members hall',
+    'org.member.leave': 'Leave organization',
+    'org.member.leaveModalTitle': 'Leaving organization confirmation',
+    'org.member.leaveModalHeading': 'Are you sure you want to leave {orgName}',
+    'org.member.leaveModalExplain': 'Once you leave the organization you will loose access to all of the organization\'s associated resources and works. This action can\'t be undone.'
 };
