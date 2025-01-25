306d898f33060c516647af488e87c65bcd6fffae
Jan Dvorak <hello@jandvorak.me>
2025-01-26 00:01:46 +0100

    New translations organizations.js (Tamil)

--- ta-IN/organizations.js
+++ ta-IN/organizations.js
@@ -55,5 +55,10 @@ export default {
     'org.workshop.ownerUser': 'ஒரு குழு வேலை அல்ல',
     'org.workshop.caption': 'குழு உரிமை',
     'org.workshop.title': 'அமைப்பு பயிற்சி',
-    'org.title': 'அமைப்பு'
+    'org.title': 'அமைப்பு',
+    'org.hall.title': '{orgName} members hall',
+    'org.member.leave': 'Leave organization',
+    'org.member.leaveModalTitle': 'Leaving organization confirmation',
+    'org.member.leaveModalHeading': 'Are you sure you want to leave {orgName}',
+    'org.member.leaveModalExplain': 'Once you leave the organization you will loose access to all of the organization\'s associated resources and works. This action can\'t be undone.'
 };
