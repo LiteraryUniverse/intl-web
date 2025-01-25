b0c7824f1e8ec4b8017fd95fd12745fe0fadb52a
Jan Dvorak <hello@jandvorak.me>
2025-01-26 00:05:12 +0100

    New translations organizations.js (Malayalam)

--- ml-IN/organizations.js
+++ ml-IN/organizations.js
@@ -55,5 +55,10 @@ export default {
     'org.workshop.ownerUser': 'ഒരു ഗിൽഡ് പ്രവർത്തനമല്ല',
     'org.workshop.caption': 'ഗിൽഡ് ഉദ്ധാരണം',
     'org.workshop.title': 'സംഘടന പ്രവർത്തനം',
-    'org.title': 'സംഘടന'
+    'org.title': 'സംഘടന',
+    'org.hall.title': '{orgName} members hall',
+    'org.member.leave': 'Leave organization',
+    'org.member.leaveModalTitle': 'Leaving organization confirmation',
+    'org.member.leaveModalHeading': 'Are you sure you want to leave {orgName}',
+    'org.member.leaveModalExplain': 'Once you leave the organization you will loose access to all of the organization\'s associated resources and works. This action can\'t be undone.'
 };
