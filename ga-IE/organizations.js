30cfc33c9d3af3abfe80bee27133e8abc54f7f5f
Jan Dvorak <hello@jandvorak.me>
2025-01-25 23:54:17 +0100

    New translations organizations.js (Irish)

--- ga-IE/organizations.js
+++ ga-IE/organizations.js
@@ -55,5 +55,10 @@ export default {
     'org.workshop.ownerUser': 'Not a guild work',
     'org.workshop.caption': 'Úinéireacht an ghiúiré',
     'org.workshop.title': 'Ceardlann Eagraíochta',
-    'org.title': 'Eagraíocht'
+    'org.title': 'Eagraíocht',
+    'org.hall.title': '{orgName} members hall',
+    'org.member.leave': 'Leave organization',
+    'org.member.leaveModalTitle': 'Leaving organization confirmation',
+    'org.member.leaveModalHeading': 'Are you sure you want to leave {orgName}',
+    'org.member.leaveModalExplain': 'Once you leave the organization you will loose access to all of the organization\'s associated resources and works. This action can\'t be undone.'
 };
