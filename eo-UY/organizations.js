9211dd535ca2b8f9f0c942f758cf92a9ad1e422a
Jan Dvorak <hello@jandvorak.me>
2025-01-26 00:04:15 +0100

    New translations organizations.js (Esperanto)

--- eo-UY/organizations.js
+++ eo-UY/organizations.js
@@ -55,5 +55,10 @@ export default {
     'org.workshop.ownerUser': 'Ne gilda laboro',
     'org.workshop.caption': 'Gilda posedo',
     'org.workshop.title': 'Organiza laborŝtelo',
-    'org.title': 'Organizo'
+    'org.title': 'Organizo',
+    'org.hall.title': '{orgName} members hall',
+    'org.member.leave': 'Leave organization',
+    'org.member.leaveModalTitle': 'Leaving organization confirmation',
+    'org.member.leaveModalHeading': 'Are you sure you want to leave {orgName}',
+    'org.member.leaveModalExplain': 'Once you leave the organization you will loose access to all of the organization\'s associated resources and works. This action can\'t be undone.'
 };
