3466b99456e0881c6e9a76148f587db2b2eae1bc
Jan Dvorak <hello@jandvorak.me>
2025-01-26 00:02:42 +0100

    New translations organizations.js (Thai)

--- th-TH/organizations.js
+++ th-TH/organizations.js
@@ -55,5 +55,10 @@ export default {
     'org.workshop.ownerUser': 'ไม่ใช่งานของกิลด์',
     'org.workshop.caption': 'การเป็นเจ้าของกิลด์',
     'org.workshop.title': 'เวิร์กชอปองค์กร',
-    'org.title': 'องค์กร'
+    'org.title': 'องค์กร',
+    'org.hall.title': '{orgName} members hall',
+    'org.member.leave': 'Leave organization',
+    'org.member.leaveModalTitle': 'Leaving organization confirmation',
+    'org.member.leaveModalHeading': 'Are you sure you want to leave {orgName}',
+    'org.member.leaveModalExplain': 'Once you leave the organization you will loose access to all of the organization\'s associated resources and works. This action can\'t be undone.'
 };
