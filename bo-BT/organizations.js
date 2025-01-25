9c0eef6ef55b743ff500c2f355f50f3c58c5f7f3
Jan Dvorak <hello@jandvorak.me>
2025-01-26 00:05:30 +0100

    New translations organizations.js (Tibetan)

--- bo-BT/organizations.js
+++ bo-BT/organizations.js
@@ -55,5 +55,10 @@ export default {
     'org.workshop.ownerUser': 'Not a guild work',
     'org.workshop.caption': 'Guild ownership',
     'org.workshop.title': 'སྙན་ཞུས་དཔེ་སྐྲུན།',
-    'org.title': 'སྙན་ཞུས།'
+    'org.title': 'སྙན་ཞུས།',
+    'org.hall.title': '{orgName} members hall',
+    'org.member.leave': 'Leave organization',
+    'org.member.leaveModalTitle': 'Leaving organization confirmation',
+    'org.member.leaveModalHeading': 'Are you sure you want to leave {orgName}',
+    'org.member.leaveModalExplain': 'Once you leave the organization you will loose access to all of the organization\'s associated resources and works. This action can\'t be undone.'
 };
