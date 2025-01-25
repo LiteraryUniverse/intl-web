48462cfc9d1848559641368781dee96acbd6e08f
Jan Dvorak <hello@jandvorak.me>
2025-01-25 23:52:27 +0100

    New translations organizations.js (Catalan)

--- ca-ES/organizations.js
+++ ca-ES/organizations.js
@@ -55,5 +55,10 @@ export default {
     'org.workshop.ownerUser': 'Not a guild work',
     'org.workshop.caption': 'Guild ownership',
     'org.workshop.title': 'Taller d\'organització',
-    'org.title': 'Organització'
+    'org.title': 'Organització',
+    'org.hall.title': '{orgName} members hall',
+    'org.member.leave': 'Leave organization',
+    'org.member.leaveModalTitle': 'Leaving organization confirmation',
+    'org.member.leaveModalHeading': 'Are you sure you want to leave {orgName}',
+    'org.member.leaveModalExplain': 'Once you leave the organization you will loose access to all of the organization\'s associated resources and works. This action can\'t be undone.'
 };
