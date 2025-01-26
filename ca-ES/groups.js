9924fec09cb421a441415717b92378f72f956d37
Jan Dvorak <hello@jandvorak.me>
2025-01-26 22:35:16 +0100

    New translations groups.js (Catalan)

--- ca-ES/groups.js
+++ ca-ES/groups.js
@@ -38,5 +38,8 @@ export default {
   }`,
     'groups.invites.title': 'Invitacions de grup',
     'group.invite.accept': 'Acceptar invitació',
-    'group.noticeBoard': 'Tauler d\'anuncis'
+    'group.noticeBoard': 'Tauler d\'anuncis',
+    'group.leaveGroup.modalAria': 'Confirmation that you want to leave the group.',
+    'group.leaveGroup.heading': 'Are you sure that you want to leave {groupName}?',
+    'group.leaveGroup.explain': 'Leaving group means that you will loose access to the group resources and won\'t be able to interact with it.'
 };
