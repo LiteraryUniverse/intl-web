0918101e2ac35e4cb2c86478f5fb5e0eb4c8784c
Jan Dvorak <hello@jandvorak.me>
2025-01-28 01:02:34 +0100

    New translations encyclopedia.js (Kannada)

--- kn-IN/encyclopedia.js
+++ kn-IN/encyclopedia.js
@@ -24,6 +24,16 @@ export default {
     'encyclopedia.category.species': 'ಜಾತಿಗಳು',
     'encyclopedia.category.technologies': 'ತಂತ್ರಗಳು',
     'encyclopedia.category.families': 'Families',
+    'encyclopedia.category.jobs': 'Jobs',
+    'encyclopedia.category.lore': 'Lore',
+    'encyclopedia.category.vehicles': 'Vehicles',
+    'encyclopedia.category.diseases': 'Diseases',
+    'encyclopedia.category.traditions': 'Traditions',
+    'encyclopedia.category.sports': 'Sports',
+    'encyclopedia.category.landmarks': 'Landmarks',
+    'encyclopedia.category.buildings': 'Buildings',
+    'encyclopedia.category.schools': 'Schools',
+    'encyclopedia.category.continents': 'Continents',
     'encyclopedia.search.found': `{results, plural,
     =0 {ಯಾವುದೇ ಸಂಬಂಧಿತ ಪ್ರವೇಶಗಳಿಲ್ಲ}
     one {1 ಸಂಬಂಧಿತ ಪ್ರವೇಶ}
@@ -55,8 +65,18 @@ export default {
     species {specie}
     technologies {technology}
     families {family}
+    jobs {job}
+    lore {lore}
+    vehicles {vehicle}
+    diseases {disease}
+    traditions {tradition}
+    sports {sport}
+    landmarks {landmark}
+    buildings {building}
+    schools {school}
+    continents {continent}
   }`,
-    'encyclopedia.create.header': `Create a new {category, select,
+    'encyclopedia.create.header': `Creating a new {category, select,
     character {character}
     event {event}
     group {group}
@@ -75,6 +95,16 @@ export default {
     species {specie}
     technologies {technology}
     families {family}
+    jobs {job}
+    lore {lore}
+    vehicles {vehicle}
+    diseases {disease}
+    traditions {tradition}
+    sports {sport}
+    landmarks {landmark}
+    buildings {building}
+    schools {school}
+    continents {continent}
   } in {universe}`,
     'encyclopedia.entry.category': `Entry category: {category, select,
     character {Character}
@@ -95,6 +125,16 @@ export default {
     species {Specie}
     technologies {Technology}
     families {Family}
+    jobs {Job}
+    lore {Lore}
+    vehicles {Vehicle}
+    diseases {Disease}
+    traditions {Tradition}
+    sports {Sport}
+    landmarks {Landmark}
+    buildings {Building}
+    schools {School}
+    continents {Continent}
   }`,
     'encyclopedia.entry.category.legend': 'ವರ್ಗ',
     'encyclopedia.fields.add': 'ಹೊಸ ಕ್ಷೇತ್ರ ಸೇರಿಸಿ',
