7b67c785c7b0c147a4fa82ba67fcd7966198d34b
Jan Dvorak <hello@jandvorak.me>
2025-01-29 12:20:23 +0100

    New translations encyclopedia.js (Korean)

--- ko-KR/encyclopedia.js
+++ ko-KR/encyclopedia.js
@@ -34,6 +34,9 @@ export default {
     'encyclopedia.category.buildings': '건물',
     'encyclopedia.category.schools': '학교',
     'encyclopedia.category.continents': '대륙',
+    'encyclopedia.category.villages': '마을',
+    'encyclopedia.category.towns': '마을',
+    'encyclopedia.category.cities': '도시',
     'encyclopedia.search.found': `{results, plural,
     =0 {관련 항목 없음}
     one {1개의 관련 항목}
@@ -59,7 +62,7 @@ export default {
     creatures {생물}
     deity {신}
     religions {종교}
-    flora {동식물}
+    flora {식물}
     magic {마법}
     planets {행성}
     species {종}
@@ -67,7 +70,7 @@ export default {
     families {가족}
     jobs {직업}
     lore {지식}
-    vehicles {차량}
+    vehicles {탈것}
     diseases {질병}
     traditions {전통}
     sports {스포츠}
@@ -75,6 +78,9 @@ export default {
     buildings {건물}
     schools {학교}
     continents {대륙}
+    villages {마을}
+    towns {마을}
+    cities {도시}
   }`,
     'encyclopedia.create.header': `신규 생성 {category, select,
     character {캐릭터}
@@ -89,7 +95,7 @@ export default {
     creatures {생물}
     deity {신}
     religions {종교}
-    flora {동식물}
+    flora {식물}
     magic {마법}
     planets {행성}
     species {종}
@@ -97,7 +103,7 @@ export default {
     families {가족}
     jobs {직업}
     lore {지식}
-    vehicles {차량}
+    vehicles {탈것}
     diseases {질병}
     traditions {전통}
     sports {스포츠}
@@ -105,6 +111,9 @@ export default {
     buildings {건물}
     schools {학교}
     continents {대륙}
+    villages {마을}
+    towns {도시}
+    cities {도시}
   } in {universe}`,
     'encyclopedia.entry.category': `응모 카테고리: {category, select,
     character {캐릭터}
@@ -135,6 +144,9 @@ export default {
     buildings {건물}
     schools {학교}
     continents {대륙}
+    villages {마을}
+    towns {도시}
+    cities {도시}
   }`,
     'encyclopedia.entry.category.legend': '카테고리',
     'encyclopedia.fields.add': '새 필드 추가',
