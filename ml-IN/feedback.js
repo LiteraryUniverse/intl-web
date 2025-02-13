8304781d7f349b872ffd25c02c465145307304a4
Jan Dvorak <hello@jandvorak.me>
2025-02-13 21:02:37 +0100

    New translations feedback.js (Malayalam)

--- ml-IN/feedback.js
+++ ml-IN/feedback.js
@@ -52,5 +52,13 @@ export default {
     universe {യൂണിവേഴ്സ്}
     story {കഥ}
     other {കഥ}
-  } അപകടപ്പെടുത്തപ്പെടുന്നു?`
+  } അപകടപ്പെടുത്തപ്പെടുന്നു?`,
+    'feedback.help.overview': 'Overview',
+    'feedback.help.articles': 'Articles',
+    'feedback.help.courses': 'Courses',
+    'feedback.help.videos': 'Video tutorials',
+    'feedback.help.featuredArticles': 'Featured Articles',
+    'feedback.help.featuredCourses': 'Featured Courses',
+    'feedback.help.featuredVideos': 'Featured Videos',
+    'feedback.help.subCategories': 'Sub-categories'
 };
