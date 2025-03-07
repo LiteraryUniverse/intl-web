d8a918496025e5c9e344a4d825ba5745421e31b7
Jan Dvorak <hello@jandvorak.me>
2025-03-07 19:20:12 +0100

    New translations feedback.js (Punjabi)

--- pa-IN/feedback.js
+++ pa-IN/feedback.js
@@ -47,6 +47,8 @@ export default {
     'feedback.report.copyrightNotice': 'ਕਿਰਪਾ ਕਰਕੇ ਧਿਆਨ ਦਿਓ ਕਿ ਤੁਸੀਂ ਇਸ ਫਾਰਮ ਦੀ ਵਰਤੋਂ ਕਰਨ ਲਈ ਮਾਲਕ ਹੋਣ ਦੀ ਲੋੜ ਹੁੰਦੀ ਹੈ। ਜੇ ਤੁਸੀਂ ਮਾਲਕ ਨਹੀਂ ਹੋ, ਤਾਂ ਕਿਰਪਾ ਕਰਕੇ ਮਾਲਕ ਨੂੰ ਸੂਚਿਤ ਕਰੋ ਜਾਂ ਸਾਨੂੰ ਸੂਚਿਤ ਕਰਨ ਲਈ ਹੋਰ ਸ਼੍ਰੇਣੀ ਦੀ ਵਰਤੋਂ ਕਰੋ। ਅਸੀਂ ਕਾਪੀਰਾਈਟ ਉਲੰਘਣਾਵਾਂ ਨੂੰ ਬਹੁਤ ਗੰਭੀਰਤਾ ਨਾਲ ਲੈ ਰਹੇ ਹਾਂ ਅਤੇ ਇਸ ਫਾਰਮ ਦੀ ਅਸਮਾਨਪੂਰਵ ਅਤੇ ਅਪਮਾਨਜਨਕ ਵਰਤੋਂ ਲਈ ਸਮਾਨ ਲਾਗੂ ਹੁੰਦਾ ਹੈ।',
     'feedback.report.copyrightInternalProperty': 'ਮੈਂ ਇਸ ਕੰਮ ਨੂੰ ਲਿਟਰੇਰੀ ਯੂਨੀਵਰਸ \'ਤੇ ਪ੍ਰਕਾਸ਼ਿਤ ਕੀਤਾ ਹੈ',
     'feedback.reportType.spam': 'ਸਪੈਮ ਜਾਂ ਫਿਸ਼ਿੰਗ',
+    'feedback.reportType.soliciting': 'Soliciting',
+    'feedback.reportType.aiUnmarked': 'Unmarked AI-generated content',
     'feedback.reportType.other': 'ਹੋਰ',
     'feedback.report.copyrightType.propertySelectLabel': `Which {type, select,
     universe {universe}
