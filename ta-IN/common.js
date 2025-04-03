9eac7c49903a823e17d15ba381d9766d131f1deb
Jan Dvorak <hello@jandvorak.me>
2025-04-03 15:42:27 +0200

    New translations common.js (Tamil)

--- ta-IN/common.js
+++ ta-IN/common.js
@@ -273,5 +273,6 @@ export default {
     'common.newest': 'புதியது',
     'common.oldest': 'பழையதாக',
     'common.noSelection': 'No selection',
-    'common.content': 'Content'
+    'common.content': 'Content',
+    'common.theWayOfTheAuthor': 'The Way of the Author'
 };
