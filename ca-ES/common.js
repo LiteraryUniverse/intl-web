864ec3ba57c9b8d466fdaf69ffd888b96377d469
Jan Dvorak <hello@jandvorak.me>
2025-04-03 15:30:37 +0200

    New translations common.js (Catalan)

--- ca-ES/common.js
+++ ca-ES/common.js
@@ -273,5 +273,6 @@ export default {
     'common.newest': 'Més recent',
     'common.oldest': 'Oldest',
     'common.noSelection': 'No selection',
-    'common.content': 'Content'
+    'common.content': 'Content',
+    'common.theWayOfTheAuthor': 'The Way of the Author'
 };
