a501199d0f5e2a9e7e30491fb0bec7eea35b7fc4
Jan Dvorak <hello@jandvorak.me>
2025-04-03 15:35:08 +0200

    New translations workshop.js (Japanese)

--- ja-JP/workshop.js
+++ ja-JP/workshop.js
@@ -281,5 +281,6 @@ export default {
     'workshop.collaborator.leaveExplained': 'プロジェクトを離脱すると、すべてのアクセス、関連付け、権限が失われます。これが何を意味するのかよくわからない場合は、おそらくこれをクリックする必要はないでしょう。',
     'workshop.collaborators.leftInfo': '左に{date, date}',
     'workshop.collaborators.removedInfo': 'Removed on {date, date}',
-    'workshop.aiGeneratedAdjusted': 'このコンテンツのAI生成ステータスは、ユーザーからの報告の結果、管理者によって調整されており、変更することはできません。'
+    'workshop.aiGeneratedAdjusted': 'このコンテンツのAI生成ステータスは、ユーザーからの報告の結果、管理者によって調整されており、変更することはできません。',
+    'workshop.theWay.authorProgress': '著者プログレス'
 };
