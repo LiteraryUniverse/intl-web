80319fd9df610cd047de4dd1eb772830ee2164db
Jan Dvorak <hello@jandvorak.me>
2025-04-03 15:37:52 +0200

    New translations workshop.js (Polish)

--- pl-PL/workshop.js
+++ pl-PL/workshop.js
@@ -289,5 +289,6 @@ export default {
     'workshop.collaborator.leaveExplained': 'Opuszczenie projektu oznacza utratę wszystkich dostępów, powiązań i uprawnień. Jeśli nie są Państwo pewni, co to wszystko oznacza, to prawdopodobnie nie to chcieli Państwo kliknąć.',
     'workshop.collaborators.leftInfo': 'Włączony {date, date}',
     'workshop.collaborators.removedInfo': 'Usunięto na {date, date}',
-    'workshop.aiGeneratedAdjusted': 'Status generowania AI tej zawartości został dostosowany przez administratora w wyniku zgłoszenia użytkownika i nie można go zmienić.'
+    'workshop.aiGeneratedAdjusted': 'Status generowania AI tej zawartości został dostosowany przez administratora w wyniku zgłoszenia użytkownika i nie można go zmienić.',
+    'workshop.theWay.authorProgress': 'Autor Postęp'
 };
