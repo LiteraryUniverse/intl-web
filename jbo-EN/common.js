7da0e1b91121a9e37c490811a7364a59e3e524e0
Jan Dvorak <hello@jandvorak.me>
2025-04-03 15:46:45 +0200

    New translations common.js (Lojban)

--- jbo-EN/common.js
+++ jbo-EN/common.js
@@ -273,5 +273,6 @@ export default {
     'common.newest': 'cioxadni',
     'common.oldest': 'pa laldo',
     'common.noSelection': 'No selection',
-    'common.content': 'Content'
+    'common.content': 'Content',
+    'common.theWayOfTheAuthor': 'The Way of the Author'
 };
