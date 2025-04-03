d00ea83e241984effc2dc318ccd65972fbdd3a9b
Jan Dvorak <hello@jandvorak.me>
2025-04-03 15:45:00 +0200

    New translations common.js (Malay)

--- ms-MY/common.js
+++ ms-MY/common.js
@@ -273,5 +273,6 @@ export default {
     'common.newest': 'Terbaru',
     'common.oldest': 'Tertua',
     'common.noSelection': 'No selection',
-    'common.content': 'Content'
+    'common.content': 'Content',
+    'common.theWayOfTheAuthor': 'The Way of the Author'
 };
