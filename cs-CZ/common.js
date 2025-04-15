4b77808aeecdbb07b72f8d4a462b0d3f61e340c6
Jan Dvorak <hello@jandvorak.me>
2025-04-15 12:26:20 +0200

    New translations common.js (Czech)

--- cs-CZ/common.js
+++ cs-CZ/common.js
@@ -277,5 +277,6 @@ export default {
     'common.theWayOfTheAuthor': 'Cesta autora',
     'common.password.hide': 'Skrýt heslo',
     'common.password.show': 'Zobrazit heslo',
-    'common.forums': 'Fóra'
+    'common.forums': 'Fóra',
+    'common.restriction.goodConduct': 'Z důvodu porušení dobrých mravů jsme omezili používání této funkce.'
 };
