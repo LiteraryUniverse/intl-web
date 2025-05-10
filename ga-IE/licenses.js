5392d908604c7f34419b3987cae494748b11e71f
Jan Dvorak <hello@jandvorak.me>
2025-05-10 19:38:04 +0200

    New translations licenses.js (Irish)

--- ga-IE/licenses.js
+++ ga-IE/licenses.js
@@ -20,6 +20,10 @@ export default {
     'licenses.cc-by-nc-nd': 'Is é seo an ceadúnas is teoranta de na sé chéadúnas príomha atá againn, agus a ligeann do dhaoine eile do shaothair a íoslódáil agus a roinnt le daoine eile ach amháin má thugann siad aitheantas duit, ach ní féidir leo iad a athrú ar bith nó a úsáid go tráchtála.',
     'licenses.name.lu-c': 'Cearta Cóipcheartú na Cruinne Liteartha',
     'licenses.lu-c': 'Standard full copyrights license that allows you to sell publish your work as you like and have your rights to it protected. This license also gives you the option to sell your stories on Literary Universe.',
+    'licenses.name.lu-e': 'Literary Universe Exclusive',
+    'licenses.lu-e': 'This license grants Literary Universe exclusive rights to publish and distribute your work online. You will retain the copyright, but Literary Universe will have the sole right to publish and distribute your work online.',
+    'licenses.name.lu-ne': 'Literary Universe Non-Exclusive',
+    'licenses.lu-ne': 'This license grants Literary Universe non-exclusive rights to publish and distribute your work online. You will retain the copyright and the right to publish and distribute your work through other channels.',
     'licenses.name.cc0': 'No Rights Reserved - CC0 1.0',
     'licenses.cc0': 'Put your work into public domain. In contrast to CC’s licenses that allow copyright holders to choose from a range of permissions while retaining their copyright, CC0 empowers yet another choice altogether – the choice to opt out of copyright and database protection, and the exclusive rights automatically granted to creators – the “no rights reserved” alternative to our licenses.'
 };
