831e0b5e93f24c3c8e5037912c8177a70bd8ce2d
Jan Dvorak <hello@jandvorak.me>
2025-05-14 00:18:24 +0200

    Update source file common.js

--- en/common.js
+++ en/common.js
@@ -309,5 +309,6 @@ export default {
   'common.password.hide': 'Hide Password',
   'common.password.show': 'Show Password',
   'common.forums': 'Forums',
-  'common.restriction.goodConduct': 'Due to good conduct violation we have restricted your use of this feature.'
+  'common.restriction.goodConduct': 'Due to good conduct violation we have restricted your use of this feature.',
+  'common.continue': 'Continue',
 }
