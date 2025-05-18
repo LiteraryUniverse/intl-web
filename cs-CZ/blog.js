8fdde81bff37ced414bda6a5d5eb1d51b05b2523
Jan Dvorak <hello@jandvorak.me>
2025-05-18 22:05:30 +0200

    New translations blog.js (Czech)

--- cs-CZ/blog.js
+++ cs-CZ/blog.js
@@ -21,7 +21,7 @@ export default {
     few {má # články}
     other {má # článků}
   }.`,
-    'blog.post.create': 'Vytvořit nový článek',
+    'blog.post.create': 'Write a new post',
     'blog.post.update': 'Editace {title}',
     'blog.post.text': 'Text článku',
     'common.slug': 'SEO adresa',
