14da332185af1cec12415b61dee7a359ff09fd85
Jan Dvorak <hello@jandvorak.me>
2025-06-01 14:58:59 +0200

    New translations user.js (Norwegian)

--- no-NO/user.js
+++ no-NO/user.js
@@ -49,5 +49,15 @@ export default {
     'profile.wishlist': 'Ønskeliste',
     'profile.storiesBy': 'Historier opprettet av {username}',
     'profile.universesBy': 'Universer opprettet av {username}',
-    'profile.fanFictionBy': 'Fan Fiction opprettet av {username}'
+    'profile.fanFictionBy': 'Fan Fiction opprettet av {username}',
+    'signup.reader.storiesType.title': 'What types of stories do you enjoy?',
+    'signup.reader.storiesType.description': 'Pick the ones that best match your interests.',
+    'storiesType.interactive': 'Interactive',
+    'storiesType.novel': 'Novel',
+    'storiesType.novella': 'Novella',
+    'storiesType.one_shot': 'One shot',
+    'storiesType.play': 'Play',
+    'storiesType.poetry': 'Poetry',
+    'storiesType.serialized': 'Serialized',
+    'storiesType.short_story': 'short_story'
 };
