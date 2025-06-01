ab7dbe48262ea18de50dcdcd19e3dcc42e39b59d
Jan Dvorak <hello@jandvorak.me>
2025-06-01 15:03:41 +0200

    New translations user.js (Khmer)

--- km-KH/user.js
+++ km-KH/user.js
@@ -49,5 +49,15 @@ export default {
     'profile.wishlist': 'បញ្ជីចង់ទិញ',
     'profile.storiesBy': 'រឿងដែលបានបង្កើតដោយ {username}',
     'profile.universesBy': 'សង្គមដែលបានបង្កើតដោយ {username}',
-    'profile.fanFictionBy': 'រឿងចម្រៀងអ្នកចម្រៀងដែលបានបង្កើតដោយ {username}'
+    'profile.fanFictionBy': 'រឿងចម្រៀងអ្នកចម្រៀងដែលបានបង្កើតដោយ {username}',
+    'signup.reader.storiesType.title': 'What types of stories do you enjoy?',
+    'signup.reader.storiesType.description': 'Pick the ones that best match your interests.',
+    'storiesType.interactive': 'Interactive',
+    'storiesType.novel': 'Novel',
+    'storiesType.novella': 'Novella',
+    'storiesType.one_shot': 'One shot',
+    'storiesType.play': 'Play',
+    'storiesType.poetry': 'Poetry',
+    'storiesType.serialized': 'Serialized',
+    'storiesType.short_story': 'short_story'
 };
