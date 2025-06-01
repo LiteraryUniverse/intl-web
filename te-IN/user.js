0d48a468f954b50a55bb4a99bc5efa918ef081e1
Jan Dvorak <hello@jandvorak.me>
2025-06-01 15:06:30 +0200

    New translations user.js (Telugu)

--- te-IN/user.js
+++ te-IN/user.js
@@ -49,5 +49,15 @@ export default {
     'profile.wishlist': 'ఇష్టాలు జాబితా',
     'profile.storiesBy': '{username}ద్వారా సృష్టించిన కథలు',
     'profile.universesBy': '{username}ద్వారా సృష్టించిన యూనివర్సులు',
-    'profile.fanFictionBy': '{username}ద్వారా సృష్టించిన ఫ్యాన్ ఫిక్షన్'
+    'profile.fanFictionBy': '{username}ద్వారా సృష్టించిన ఫ్యాన్ ఫిక్షన్',
+    'signup.reader.storiesType.title': 'What types of stories do you enjoy?',
+    'signup.reader.storiesType.description': 'Pick the ones that best match your interests.',
+    'storiesType.interactive': 'Interactive',
+    'storiesType.novel': 'Novel',
+    'storiesType.novella': 'Novella',
+    'storiesType.one_shot': 'One shot',
+    'storiesType.play': 'Play',
+    'storiesType.poetry': 'Poetry',
+    'storiesType.serialized': 'Serialized',
+    'storiesType.short_story': 'short_story'
 };
