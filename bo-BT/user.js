c197747010420a9dcaf347bb2f3d3fdbcbdfdf2c
Jan Dvorak <hello@jandvorak.me>
2025-06-01 15:08:14 +0200

    New translations user.js (Tibetan)

--- bo-BT/user.js
+++ bo-BT/user.js
@@ -49,5 +49,15 @@ export default {
     'profile.wishlist': 'དགའ་གདམས་',
     'profile.storiesBy': '! {username}! ནང་དོན་ཚུ་གི་སྤྱི་ཚོགས་',
     'profile.universesBy': '! {username}! ནང་དོན་ཚུ་གི་གསར་འགྱུར་',
-    'profile.fanFictionBy': '! {username}! ནང་དོན་ཚུ་གི་རྩོམ་པ་གསར་འགྱུར་'
+    'profile.fanFictionBy': '! {username}! ནང་དོན་ཚུ་གི་རྩོམ་པ་གསར་འགྱུར་',
+    'signup.reader.storiesType.title': 'What types of stories do you enjoy?',
+    'signup.reader.storiesType.description': 'Pick the ones that best match your interests.',
+    'storiesType.interactive': 'Interactive',
+    'storiesType.novel': 'Novel',
+    'storiesType.novella': 'Novella',
+    'storiesType.one_shot': 'One shot',
+    'storiesType.play': 'Play',
+    'storiesType.poetry': 'Poetry',
+    'storiesType.serialized': 'Serialized',
+    'storiesType.short_story': 'short_story'
 };
