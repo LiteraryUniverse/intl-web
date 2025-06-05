5628716c1f81e7a5e2aef6a674f7f44f1d3beb3e
Jan Dvorak <hello@jandvorak.me>
2025-06-05 22:13:08 +0200

    New translations courses.js (Malayalam)

--- ml-IN/courses.js
+++ ml-IN/courses.js
@@ -44,6 +44,7 @@ export default {
   'course.question.no': 'ഇല്ല',
   'course.lesson.title': 'Lesson title',
   'course.lesson.add.universe': 'Add Story/Universe',
+  'course.lesson.externalLinks': 'External links',
   'course.lesson.add.link': 'Add external link',
   'course.status': 'Status',
   'course.active': 'Activity',
