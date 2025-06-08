b4966a45fbe50732711087afd67307548aac8627
Jan Dvorak <hello@jandvorak.me>
2025-06-08 18:16:38 +0200

    New translations challenges.js (Esperanto)

--- eo-UY/challenges.js
+++ eo-UY/challenges.js
@@ -17,7 +17,6 @@ export default {
     'challenges.endDate': 'Fino-dato',
     'challenges.future': 'Venontaj defioj',
     'challenges.future.wordcount': 'Vortnombrado defio komenciĝas je {startDate, date, short} kun celo de {target, number} ĝis {endDate, date, short}.',
-    'challenges.future.nanowrimo': 'NaNoWriMo defio estas preta.',
     'challenges.wordcount': 'Vorto-kvanto',
     'challenges.wordcount.desc': `Starigi al vi mem celon pri kiom da vortoj vi volas skribi ĉi tiun monaton por ĉi tiu {type, select,
     story {rakonto en la}
@@ -29,29 +28,5 @@ export default {
     'challenges.report': 'Raporti',
     'challenges.victory': 'Gratulojn! Vi sukcesis ĉi tiun defion.',
     'challenges.defeat': 'Bedaŭrinde vi ne sukcesis ĉi tiun defion.',
-    'challenges.baseline': 'La aktuala vortokvanto por ĉi tiu rakonto estas {wordcount, number}, tio estos la komencopunkto.',
-    // NaNoWriMo
-    'nanowrimo.full': 'Nacia Monato de Verkado de Romano',
-    'nanowrimo.site': 'Oficiala retejo de NaNoWriMo',
-    'nanowrimo.settings': 'Konto de NaNoWriMo',
-    'nanowrimo.username': 'Uzantonomo de NaNoWriMo',
-    'nanowrimo.key': 'Sekreta ŝlosilo',
-    'nanowrimo.key.get': 'Prenu vian NaNoWriMo ŝlosilon.',
-    'nanowrimo.key.get.notice': 'Vi devas esti ensalutita por vidi ĝin.',
-    'nanowrimo.ongoing': 'estas en plena fluo! {countdown} pliaj tagoj por iri!',
-    'nanowrimo.startsin': `komencos en {countdown, plural,
-      one {1 tago}
-      other {# tagoj}
-    }! Preparu vin!`,
-    'nanowrimo.isover': 'NaNoWriMo finiĝis. Ĝis revido venontjare!',
-    'nanowrimo.select': 'Asignu ĉi tiun rakonton por NaNoWriMo.',
-    'nanowrimo.baseline': 'Kiam NaNoWriMo komenciĝas, ĝi prenos la nunan vortkvanton kaj uzos ĝin kiel bazlinion, el kiu ajn aldonaĵaj vortoj estos konsiderataj kiel parto de NaNoWriMo.',
-    'nanowrimo.victory': 'Gratulon! Vi venkis NaNoWriMo-n!',
-    'nanowrimo.challenge': 'NaNoWriMo defio',
-    'nanowrimo.description': 'La defio estas verki romanon de {words, number} vortoj dum novembro.',
-    'nanowrimo.error.usernotfound': 'La uzantonomo ne estis trovita en la serviloj de NaNoWriMo.',
-    'nanowrimo.error.nobook': 'Neniu libro estis trovita. Bonvolu certiĝi, ke vi kreis vian libron sur NaNoWriMo.',
-    'nanowrimo.error.wordcount': 'Ne eblis agordi vortkvanton sur NaNoWrimo, bonvolu dufoje kontroli viajn akreditojn kaj reprovi.',
-    'nanowrimo.connected': 'Vi asignis ĉi tiun rakonton por NaNoWriMo.',
-    'nanowrimo.connected.other': 'Vi jam asignis alian rakonton.'
+    'challenges.baseline': 'La aktuala vortokvanto por ĉi tiu rakonto estas {wordcount, number}, tio estos la komencopunkto.'
 };
