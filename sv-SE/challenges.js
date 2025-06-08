32595f2dd6a78b89f1566dc445595d13188126e6
Jan Dvorak <hello@jandvorak.me>
2025-06-08 18:16:24 +0200

    New translations challenges.js (Swedish)

--- sv-SE/challenges.js
+++ sv-SE/challenges.js
@@ -17,7 +17,6 @@ export default {
     'challenges.endDate': 'Slutdatum',
     'challenges.future': 'Kommande utmaningar',
     'challenges.future.wordcount': 'Ordantal utmaning som startar den {startDate, date, short} med målet att nå {target, number} före {endDate, date, short}.',
-    'challenges.future.nanowrimo': 'NaNoWriMo-utmaningen är redo.',
     'challenges.wordcount': 'Ordantal',
     'challenges.wordcount.desc': `Sätt dig själv ett mål för hur många ord du vill skriva denna månad för denna {type, select,
     story {berättelse}
@@ -29,29 +28,5 @@ export default {
     'challenges.report': 'Rapportera',
     'challenges.victory': 'Grattis! Du har slutfört denna utmaning.',
     'challenges.defeat': 'Tyvärr klarade du inte denna utmaning.',
-    'challenges.baseline': 'Det aktuella ordantalet för denna berättelse är {wordcount, number}, detta kommer att vara startpunkten.',
-    // NaNoWriMo
-    'nanowrimo.full': 'National Novel Writing Month',
-    'nanowrimo.site': 'NaNoWriMo:s officiella webbplats',
-    'nanowrimo.settings': 'NaNoWriMo-konto',
-    'nanowrimo.username': 'NaNoWriMo-användarnamn',
-    'nanowrimo.key': 'Hemlig nyckel',
-    'nanowrimo.key.get': 'Hämta din NaNoWriMo-nyckel.',
-    'nanowrimo.key.get.notice': 'Du måste vara inloggad för att se den.',
-    'nanowrimo.ongoing': 'är i full gång! {countdown} fler dagar kvar!',
-    'nanowrimo.startsin': `kommer att starta om {countdown, plural,
-      one {1 dag}
-      other {# dagar}
-    }! Gör dig redo!`,
-    'nanowrimo.isover': 'NaNoWriMo är över. Vi ses nästa år!',
-    'nanowrimo.select': 'Ange den här berättelsen för NaNoWriMo.',
-    'nanowrimo.baseline': 'När NaNoWriMo börjar kommer den nuvarande ordräkningen att användas som en grundlinje från vilken eventuella ytterligare ord kommer att räknas som en del av NaNoWriMo.',
-    'nanowrimo.victory': 'Grattis! Du vann NaNoWriMo!',
-    'nanowrimo.challenge': 'NaNoWriMo-utmaning',
-    'nanowrimo.description': 'Utmaningen är att skriva en roman på {words, number} ord under november.',
-    'nanowrimo.error.usernotfound': 'Användarnamnet har inte hittats på NaNoWriMo-servrarna.',
-    'nanowrimo.error.nobook': 'Ingen bok har hittats. Se till att du har skapat din bok på NaNoWriMo.',
-    'nanowrimo.error.wordcount': 'Det gick inte att ange ordräkningen på NaNoWriMo, kontrollera dina referenser och försök igen.',
-    'nanowrimo.connected': 'Du har angett den här berättelsen för NaNoWriMo.',
-    'nanowrimo.connected.other': 'Du har redan anslutit en annan berättelse.'
+    'challenges.baseline': 'Det aktuella ordantalet för denna berättelse är {wordcount, number}, detta kommer att vara startpunkten.'
 };
