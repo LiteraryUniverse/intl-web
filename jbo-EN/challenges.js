544904a97b0d47def10c5774d53bf7329e10b58a
Jan Dvorak <hello@jandvorak.me>
2025-06-08 18:16:40 +0200

    New translations challenges.js (Lojban)

--- jbo-EN/challenges.js
+++ jbo-EN/challenges.js
@@ -17,7 +17,6 @@ export default {
     'challenges.endDate': 'Djuno djedi',
     'challenges.future': 'Xusra dukse',
     'challenges.future.wordcount': 'Wordcount challenge staring on {startDate, date, short} with goal of {target, number} by {endDate, date, short}.',
-    'challenges.future.nanowrimo': 'Lo se valsi ku se finti cu xusra',
     'challenges.wordcount': 'Valsi bajra',
     'challenges.wordcount.desc': `Set yourself a goal of how many words you want to write this month for this {type, select,
     story {story}
@@ -29,29 +28,5 @@ export default {
     'challenges.report': 'Raporti',
     'challenges.victory': 'co\'o djica lo nu do finti',
     'challenges.defeat': 'co\'o na djica lo nu do finti',
-    'challenges.baseline': 'The current wordcount for this story is {wordcount, number}, this will be the starting point.',
-    // NaNoWriMo
-    'nanowrimo.full': 'Djuno djedi be lo valsi karni nanowrimo',
-    'nanowrimo.site': 'Karni nanowrimo be la\'o zoi na\'ono\'irimo zoi',
-    'nanowrimo.settings': 'Karni nanowrimo be la\'o zoi na\'ono\'irimo zoi',
-    'nanowrimo.username': 'Karni nanowrimo be la\'o zoi na\'ono\'irimo zoi',
-    'nanowrimo.key': 'Jitfa\'o cmima',
-    'nanowrimo.key.get': 'Klafe do le NaNoWriMo klani.',
-    'nanowrimo.key.get.notice': 'I do nitcu lo nu logji le klani.',
-    'nanowrimo.ongoing': 'is in full swing! {countdown} more days to go!',
-    'nanowrimo.startsin': `will start in {countdown, plural,
-      one {1 day}
-      other {# days}
-    }! Get ready!`,
-    'nanowrimo.isover': '.i la\'o zoi nanowrimo.isover zoi. cu se jalge lo ka jundi lo nu balvi',
-    'nanowrimo.select': '.i la\'o zoi nanowrimo.select zoi. cu se jalge lo ka jelca lo se cukta',
-    'nanowrimo.baseline': '.i la\'o zoi nanowrimo.baseline zoi. cu se jalge lo ka na\'a ba\'o cu jbini li ji\'i re li ji\'i lo nu jmina lo valsi be\'o cu jdice lo nu se jbini lo nanowrimo',
-    'nanowrimo.victory': '.i la\'o zoi nanowrimo.victory zoi. cu se jalge lo ka zgike lo nanowrimo',
-    'nanowrimo.challenge': '.i la\'o zoi nanowrimo.challenge zoi. cu se jalge lo ka zgike lo nanowrimo',
-    'nanowrimo.description': 'The challenge is to write a novel of {words, number} words during November.',
-    'nanowrimo.error.usernotfound': '.i la\'o zoi nanowrimo.error.usernotfound zoi. cu se jalge lo ka na\'e cilre lo se cusku be lo se cusku be\'o lo nanowrimo',
-    'nanowrimo.error.nobook': '.i la\'o zoi nanowrimo.error.nobook zoi. cu se jalge lo ka na\'e cilre lo se cusku be lo se cukta be\'o lo nanowrimo',
-    'nanowrimo.error.wordcount': '.i la\'o zoi nanowrimo.error.wordcount zoi. cu se jalge lo ka na\'e setca lo se jbini be\'o lo nanowrimo .i .e\'o xalbo',
-    'nanowrimo.connected': '.i la\'o zoi nanowrimo.connected zoi. cu se jalge lo ka jelca lo se cukta',
-    'nanowrimo.connected.other': '.i la\'o zoi nanowrimo.connected.other zoi. cu se jalge lo ka jelca lo se cukta be\'o'
+    'challenges.baseline': 'The current wordcount for this story is {wordcount, number}, this will be the starting point.'
 };
