36151e8c7de41719dcfdb64869a0e608e7a6b11c
Jan Dvorak <hello@jandvorak.me>
2025-06-08 18:16:22 +0200

    New translations challenges.js (Russian)

--- ru-RU/challenges.js
+++ ru-RU/challenges.js
@@ -17,7 +17,6 @@ export default {
     'challenges.endDate': 'Дате окончания',
     'challenges.future': 'Предстоящие испытания',
     'challenges.future.wordcount': 'Испытание на количество слов, поставленное на {startDate, date, short} с целью {target, number} к {endDate, date, short}.',
-    'challenges.future.nanowrimo': 'Испытание NaNoWriMo готово.',
     'challenges.wordcount': 'Количество слов',
     'challenges.wordcount.desc': `Поставьте себе цель - сколько слов вы хотите написать в этом месяце для этой {type, select,
     story {истории}
@@ -29,31 +28,5 @@ export default {
     'challenges.report': 'Сообщить',
     'challenges.victory': 'Поздравляем! Вы выполнили это испытание.',
     'challenges.defeat': 'К сожалению, вы не выполнили это испытание.',
-    'challenges.baseline': 'Текущее количество слов для этой истории равно {wordcount, number}, это и будет отправной точкой.',
-    // NaNoWriMo
-    'nanowrimo.full': 'Национальный месяц написания романов (NaNoWriMo)',
-    'nanowrimo.site': 'Официальный сайт NaNoWriMo',
-    'nanowrimo.settings': 'Учётная запись NaNoWriMe',
-    'nanowrimo.username': 'Имя пользователя NaNoWriMo',
-    'nanowrimo.key': 'Секретный ключ',
-    'nanowrimo.key.get': 'Получить ключ NaNoWriMo.',
-    'nanowrimo.key.get.notice': 'Вы должны войти, чтобы увидеть его.',
-    'nanowrimo.ongoing': 'в самом разгаре! Осталось ещё {countdown} дн.!',
-    'nanowrimo.startsin': `начнётся через {countdown, plural,
-      one {1 день}
-      few {# дня}
-      many {# дней}
-      other {# дней}
-    }! Приготовьтесь!`,
-    'nanowrimo.isover': 'NaNoWriMo закончился. Увидимся в следующем году!',
-    'nanowrimo.select': 'Назначить эту историю для NaNoWriMo.',
-    'nanowrimo.baseline': 'Когда начнётся NaNoWriMo, он примет текущее количество слов и будет использовать её в качестве базового уровня, от которого будут отниматься любые дополнительные слова в рамках NaNoWriMo.',
-    'nanowrimo.victory': 'Поздравляем! Вы выиграли в NaNoWriMo!',
-    'nanowrimo.challenge': 'Испытание NaNoWriMo',
-    'nanowrimo.description': 'Задача - написать роман из {words, number} слов в течение ноября.',
-    'nanowrimo.error.usernotfound': 'Имя пользователя не было найдено на серверах NaNoWriMo.',
-    'nanowrimo.error.nobook': 'Книга не найдена. Пожалуйста, убедитесь, что вы создали свою книгу на NaNoWriMo.',
-    'nanowrimo.error.wordcount': 'Невозможно установить количество слов на NaNoWrimo, пожалуйста, проверьте свои учётные данные и повторите попытку.',
-    'nanowrimo.connected': 'Вы назначили эту историю для NaNoWriMo.',
-    'nanowrimo.connected.other': 'Вы уже подключили другую историю.'
+    'challenges.baseline': 'Текущее количество слов для этой истории равно {wordcount, number}, это и будет отправной точкой.'
 };
