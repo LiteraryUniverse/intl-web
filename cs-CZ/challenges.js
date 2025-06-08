2e7bb99501756075b4b34b1f2fc2e8b1a457cf14
Jan Dvorak <hello@jandvorak.me>
2025-06-08 18:16:04 +0200

    New translations challenges.js (Czech)

--- cs-CZ/challenges.js
+++ cs-CZ/challenges.js
@@ -17,7 +17,6 @@ export default {
     'challenges.endDate': 'Datum ukončení',
     'challenges.future': 'Nadcházející výzvy',
     'challenges.future.wordcount': 'Výzva na počet slov, začínající {startDate, date, short} s cílem dosáhnout {target, number} slov k {endDate, date, short}.',
-    'challenges.future.nanowrimo': 'NaNoWriMo výzva je připravena.',
     'challenges.wordcount': 'Počet slov',
     'challenges.wordcount.desc': `Nastavte si cíl kolik slov chcete napsat tento měsíc pro tento {type, select,
     story {příběh}
@@ -29,30 +28,5 @@ export default {
     'challenges.report': 'Nahlásit',
     'challenges.victory': 'Blahopřejeme! Dokončili jste tuto výzvu.',
     'challenges.defeat': 'Bohužel jste tuto výzvu nedokončili.',
-    'challenges.baseline': 'Současný počet slov pro tento příběh je {wordcount, number}, bude to výchozí bod.',
-    // NaNoWriMo
-    'nanowrimo.full': 'Měsíc národního psaní knih',
-    'nanowrimo.site': 'Oficiální stránka NaNoWriMo',
-    'nanowrimo.settings': 'Účet NaNoWriMo',
-    'nanowrimo.username': 'NaNoWriMo uživatelské jméno',
-    'nanowrimo.key': 'Tajný klíč',
-    'nanowrimo.key.get': 'Získejte svůj NaNoWriMo klíč.',
-    'nanowrimo.key.get.notice': 'Musíte být přihlášení aby jste to viděli.',
-    'nanowrimo.ongoing': 'je v plném proudu! Zbývá {countdown} dní!',
-    'nanowrimo.startsin': `začne za {countdown, plural,
-      one {1 den}
-      few {# dny}
-      other {# dní}
-    }! Připravte se!`,
-    'nanowrimo.isover': 'NaNoWriMo je za námi. Těšíme se na Vás příští rok!',
-    'nanowrimo.select': 'Určete tento příběh pro NaNoWriMo.',
-    'nanowrimo.baseline': 'Až NaNoWriMo začne, bude aktuální počet slov použít jako startovací linie, od které budou všechna další slova považována jako součást NaNoWriMo.',
-    'nanowrimo.victory': 'Blahopřejeme! Vyhráli jste NaNoWriMo!',
-    'nanowrimo.challenge': 'Výzva NaNoWriMo',
-    'nanowrimo.description': 'Výzvou je napsat román o {words, number} slovech během listopadu.',
-    'nanowrimo.error.usernotfound': 'Uživatelské jméno nebylo nalezeno na NaNoWriMo serverech.',
-    'nanowrimo.error.nobook': 'Nebyla nalezena žádná kniha. Ujistěte se, že jste vytvořili knihu pro toto NaNoWriMo.',
-    'nanowrimo.error.wordcount': 'Nelze nastavit počítání slov na NaNoWriMo, zkontrolujte prosím vaše přihlašovací údaje a zkuste to znovu.',
-    'nanowrimo.connected': 'Určete tento příběh pro NaNoWriMo.',
-    'nanowrimo.connected.other': 'Již jsi připojil jiný příběh.'
+    'challenges.baseline': 'Současný počet slov pro tento příběh je {wordcount, number}, bude to výchozí bod.'
 };
