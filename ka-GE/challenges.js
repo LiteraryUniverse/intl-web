cf855046fa88fc55ebff782a56c4761aeca5208d
Jan Dvorak <hello@jandvorak.me>
2025-06-08 18:16:14 +0200

    New translations challenges.js (Georgian)

--- ka-GE/challenges.js
+++ ka-GE/challenges.js
@@ -17,7 +17,6 @@ export default {
     'challenges.endDate': 'დასრულების თარიღი',
     'challenges.future': 'მომავალი გამოცდები',
     'challenges.future.wordcount': 'სიტყვთა რაოდენობის ჩალენჯი დაიწყება {startDate, date, short} თარიღით, მიზეზით, რომელიც არის {target, number} სიტყვა {endDate, date, short}ავტორის მიმართულებაში.',
-    'challenges.future.nanowrimo': 'NaNoWriMo გამოცდა მზად არის.',
     'challenges.wordcount': 'სიტყვების რაოდენობა',
     'challenges.wordcount.desc': `დააყენეთ თქვენი მიზანი, რამდენი სიტყვა გსურთ დაწერა ამ თვისთვის ამ {type, select,
     story {ისტორიაში}
@@ -29,29 +28,5 @@ export default {
     'challenges.report': 'ანგარიში',
     'challenges.victory': 'გილოცავთ! თქვენ ამ გამოცდას დასრულებენი.',
     'challenges.defeat': 'ბედნიერად ამ გამოცდას დასრულებდით.',
-    'challenges.baseline': 'ამ ისტორიის მიმდინარე სიტყვის რაოდენობა არის {wordcount, number}, ეს იქნება საწყისი წერტილი.',
-    // NaNoWriMo
-    'nanowrimo.full': 'ერთადერთი მაგარი რომალისტური რუკაა მთელი კალათბურთის მწიგნობრების სამყაროში დაცული',
-    'nanowrimo.site': 'NaNoWriMo საიტი',
-    'nanowrimo.settings': 'NaNoWriMo ანგარიში',
-    'nanowrimo.username': 'NaNoWriMo მომხმარებელის სახელი',
-    'nanowrimo.key': 'საიდანაც მოგებათ კოდი',
-    'nanowrimo.key.get': 'მიიღეთ თქვენი NaNoWriMo გასაღები.',
-    'nanowrimo.key.get.notice': 'საჭიროა შეხვიდეთ სისტემა, რათა მიიღოთ ის.',
-    'nanowrimo.ongoing': 'მუშაობა მიმდინარეობს! {countdown} დღის გასარჩევად!',
-    'nanowrimo.startsin': `იქნება {countdown, plural,
-      one {1 დღის შემდეგ}
-      other {# დღის}
-    }! მომზადებით!`,
-    'nanowrimo.isover': 'NaNoWriMo დასრულებულია. მივიღეთ კარგად წლის შემდეგ!',
-    'nanowrimo.select': 'მონიშნეთ ეს მოთამაშე ამ NaNoWriMo-თან.',
-    'nanowrimo.baseline': 'როდესაც NaNoWriMo დაიწყება, ის მიიღებს მიმდინარე სიტყვების რაოდენობას და ის იყენებს მათ როგორც ბაზალინის ნაწილს NaNoWriMo-ში.',
-    'nanowrimo.victory': 'გილოცავთ! თქვენ მოიგეთ NaNoWriMo!',
-    'nanowrimo.challenge': 'NaNoWriMo მოგება',
-    'nanowrimo.description': 'ამავებს არის რომ დაწეროთ ნოველას {words, number} სიტყვა ნოემბერში.',
-    'nanowrimo.error.usernotfound': 'მომხმარებელის სახელი არ მოიძებნა NaNoWriMo სერვერზე.',
-    'nanowrimo.error.nobook': 'არ მოიძებნა წიგნი. გთხოვთ დააკმაყოფილდეთ, რომ დაარეგისტრირებული გყავთ თქვენი წიგნი NaNoWriMo-ზე.',
-    'nanowrimo.error.wordcount': 'ვერ მოხერხდა სიტყვის რაოდენობის დაყენება NaNoWrimo-ზე, გთხოვთ გადაამოწმოთ თქვენი აკრედიტების მონაცემები და კიდევ სცადოთ.',
-    'nanowrimo.connected': 'თქვენ მიინიჭეთ ეს მოთამაშე NaNoWriMo-თან.',
-    'nanowrimo.connected.other': 'თქვენ უკვე დაკავებული სხვა მოთამაშეს.'
+    'challenges.baseline': 'ამ ისტორიის მიმდინარე სიტყვის რაოდენობა არის {wordcount, number}, ეს იქნება საწყისი წერტილი.'
 };
