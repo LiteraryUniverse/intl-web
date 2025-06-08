5699a1ee23c7babeff3fd3f2f20ad715a5359bbe
Jan Dvorak <hello@jandvorak.me>
2025-06-08 18:16:27 +0200

    New translations challenges.js (Chinese Traditional)

--- zh-TW/challenges.js
+++ zh-TW/challenges.js
@@ -17,7 +17,6 @@ export default {
     'challenges.endDate': '結束日期',
     'challenges.future': '即將到來的挑戰',
     'challenges.future.wordcount': '挑戰字數挑戰即將開始！目標是在 {startDate, date, short} 前寫下 {target, number} 個字，截止日期為 {endDate, date, short}。',
-    'challenges.future.nanowrimo': 'NaNoWriMo 挑戰已準備好。',
     'challenges.wordcount': '字數',
     'challenges.wordcount.desc': `為這個 {type, select,
     story {故事/宇宙設定設定一個本月想要寫下的字數目標：}
@@ -29,29 +28,5 @@ export default {
     'challenges.report': '報告',
     'challenges.victory': '恭喜！你已完成此挑戰。',
     'challenges.defeat': '很遺憾，你未完成此挑戰。',
-    'challenges.baseline': '本故事的當前字數為 {wordcount, number}，這將是起點。',
-    // NaNoWriMo
-    'nanowrimo.full': '國家小說寫作月',
-    'nanowrimo.site': 'NaNoWriMo 官方網站',
-    'nanowrimo.settings': 'NaNoWriMo 帳戶',
-    'nanowrimo.username': 'NaNoWriMo 使用者名稱',
-    'nanowrimo.key': '密鑰',
-    'nanowrimo.key.get': '獲取你的NaNoWriMo金鑰。',
-    'nanowrimo.key.get.notice': '你需要登錄才能查看它。',
-    'nanowrimo.ongoing': '正在如火如荼進行中！還有 {countdown} 天！',
-    'nanowrimo.startsin': `將在 {countdown, plural,
-      one {1天}
-      other {#天}
-    }準備好了！`,
-    'nanowrimo.isover': 'NaNoWriMo已結束。明年見！',
-    'nanowrimo.select': '將此故事指定為NaNoWriMo。',
-    'nanowrimo.baseline': '當NaNoWriMo開始時，它將以當前的字數作為基準，任何額外的字數都將作為NaNoWriMo的一部分。',
-    'nanowrimo.victory': '恭喜！你贏得了NaNoWriMo！',
-    'nanowrimo.challenge': 'NaNoWriMo挑戰',
-    'nanowrimo.description': '挑戰內容是在十一月期間寫一部 {words, number} 字數的小說。',
-    'nanowrimo.error.usernotfound': '在NaNoWriMo服務器上找不到用戶名。',
-    'nanowrimo.error.nobook': '找不到書籍。請確保您已在NaNoWriMo上創建了您的書籍。',
-    'nanowrimo.error.wordcount': '無法在NaNoWrimo上設置字數，請仔細檢查您的憑據並重試。',
-    'nanowrimo.connected': '您已將此故事指定為NaNoWriMo。',
-    'nanowrimo.connected.other': '您已經連接了另一個故事。'
+    'challenges.baseline': '本故事的當前字數為 {wordcount, number}，這將是起點。'
 };
