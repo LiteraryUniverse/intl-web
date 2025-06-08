a88aaa8e43ed8d818422423efb7bb0190376b03a
Jan Dvorak <hello@jandvorak.me>
2025-06-08 18:16:11 +0200

    New translations challenges.js (Hungarian)

--- hu-HU/challenges.js
+++ hu-HU/challenges.js
@@ -17,7 +17,6 @@ export default {
     'challenges.endDate': 'Befejezés dátuma',
     'challenges.future': 'Következő kihívások',
     'challenges.future.wordcount': 'Szószámláló kihívás kezdődik {startDate, date, short} , a cél pedig {target, number} szó {endDate, date, short}-ig.',
-    'challenges.future.nanowrimo': 'A NaNoWriMo kihívás készen áll.',
     'challenges.wordcount': 'Szószám',
     'challenges.wordcount.desc': `Tűzz ki magadnak egy célt, hogy hány szót szeretnél megírni ebben a hónapban ehhez a {type, select,
     story {történethez}
@@ -29,29 +28,5 @@ export default {
     'challenges.report': 'Jelentés',
     'challenges.victory': 'Gratulálok! Teljesítetted ezt a kihívást.',
     'challenges.defeat': 'Sajnos nem sikerült teljesíteni ezt a kihívást.',
-    'challenges.baseline': 'A jelenlegi szószám ennek a történetnek a {wordcount, number}, ez lesz a kiindulási pont.',
-    // NaNoWriMo
-    'nanowrimo.full': 'National Novel Writing Month',
-    'nanowrimo.site': 'NaNoWriMo hivatalos oldala',
-    'nanowrimo.settings': 'NaNoWriMo fiók',
-    'nanowrimo.username': 'NaNoWriMo felhasználónév',
-    'nanowrimo.key': 'Titkos kulcs',
-    'nanowrimo.key.get': 'Szerezd meg a NaNoWriMo kulcsodat.',
-    'nanowrimo.key.get.notice': 'Be kell jelentkezned, hogy láthasd.',
-    'nanowrimo.ongoing': 'teljes gőzzel zajlik! Még {countdown} nap van hátra!',
-    'nanowrimo.startsin': `!El fog kezdődni {countdown, plural,
-      one {1 nap}
-      other {# nap}
-    }! Készülj fel!`,
-    'nanowrimo.isover': 'A NaNoWriMo véget ért. Viszontlátásra jövőre!',
-    'nanowrimo.select': 'Jelöld meg ezt a történetet a NaNoWriMo számára.',
-    'nanowrimo.baseline': 'Amikor elkezdődik a NaNoWriMo, a jelenlegi szószámlálót alapnak veszi, és minden további szót a NaNoWriMo részeként kezel.',
-    'nanowrimo.victory': 'Gratulálok! Megnyerted a NaNoWriMo-t!',
-    'nanowrimo.challenge': 'NaNoWriMo kihívás',
-    'nanowrimo.description': 'A kihívás az, hogy november folyamán írj egy {words, number} szavas regényt.',
-    'nanowrimo.error.usernotfound': 'A felhasználónév nem található a NaNoWriMo szerverein.',
-    'nanowrimo.error.nobook': 'Nem található könyv. Győződj meg róla, hogy létrehoztad-e a könyvedet a NaNoWriMo-n.',
-    'nanowrimo.error.wordcount': 'Nem sikerült beállítani a szószámlálót a NaNoWrimo-n, kérlek ellenőrizd a hitelesítő adataidat, majd próbáld újra.',
-    'nanowrimo.connected': 'Jelölted ezt a történetet a NaNoWriMo számára.',
-    'nanowrimo.connected.other': 'Már csatlakoztattál egy másik történetet.'
+    'challenges.baseline': 'A jelenlegi szószám ennek a történetnek a {wordcount, number}, ez lesz a kiindulási pont.'
 };
