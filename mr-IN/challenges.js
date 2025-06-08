ef98f3456ecbe850f768b9704c103d3333130b89
Jan Dvorak <hello@jandvorak.me>
2025-06-08 18:16:33 +0200

    New translations challenges.js (Marathi)

--- mr-IN/challenges.js
+++ mr-IN/challenges.js
@@ -17,7 +17,6 @@ export default {
     'challenges.endDate': 'समाप्ती तारीख',
     'challenges.future': 'आगामी आव्हाने',
     'challenges.future.wordcount': 'शब्दांचा मोजमाप चेलेंज आपल्याला {startDate, date, short} पासून सुरु होतंय {target, number} ह्या {endDate, date, short}द्वारे .',
-    'challenges.future.nanowrimo': 'NaNoWriMo आव्हान तयार आहे.',
     'challenges.wordcount': 'शब्दांची संख्या',
     'challenges.wordcount.desc': `आपल्याला त्या {type, select,
     story {कथेसाठी आपण किती शब्द लिहायला लक्ष ठेवायला आहे हे निश्चित करा}
@@ -29,29 +28,5 @@ export default {
     'challenges.report': 'अहवाल',
     'challenges.victory': 'अभिनंदन! तुम्ही हा आव्हान पूर्ण केला आहे.',
     'challenges.defeat': 'दुर्दैवाने तुम्ही हा आव्हान पूर्ण केला नाही.',
-    'challenges.baseline': 'या कथेची सध्या शब्दांची संख्या {wordcount, number}आहे, ही सुरुवातीची बिंदू असेल.',
-    // NaNoWriMo
-    'nanowrimo.full': 'राष्ट्रीय उपन्यास लेखन महिना',
-    'nanowrimo.site': 'NaNoWriMo अधिकृत संकेतस्थळ',
-    'nanowrimo.settings': 'NaNoWriMo खाते',
-    'nanowrimo.username': 'NaNoWriMo वापरकर्तानाव',
-    'nanowrimo.key': 'सीमित की',
-    'nanowrimo.key.get': 'तुमचा NaNoWriMo की मिळवा.',
-    'nanowrimo.key.get.notice': 'तुम्हाला त्याची पाहण्यासाठी लॉग इन करणे आवश्यक आहे.',
-    'nanowrimo.ongoing': 'पूर्ण रफारफा चालू आहे! {countdown} दिवस शिल्लक आहेत!',
-    'nanowrimo.startsin': `will start in {countdown, plural,
-      one {1 day}
-      other {# days}
-    }! Get ready!`,
-    'nanowrimo.isover': 'NaNoWriMo आपल्याला विदा झालेलं आहे. पुढच्या वर्षी भेटूया!',
-    'nanowrimo.select': 'हा लेख NaNoWriMo साठी निश्चित करा.',
-    'nanowrimo.baseline': 'NaNoWriMo सुरु होत असताना, वर्तमान शब्दांची मोजमाप घेऊन त्यातील कोणत्याही अतिरिक्त शब्दांचा वापर NaNoWriMo चा भाग घेतला जाईल.',
-    'nanowrimo.victory': 'अभिनंदन! तुम्ही NaNoWriMo जिंकलं आहे!',
-    'nanowrimo.challenge': 'NaNoWriMo चॅलेंज',
-    'nanowrimo.description': 'चॅलेंज हा आहे की नवंबर महिन्यातील {words, number} शब्दांनी एका कादंबरी लिहायला.',
-    'nanowrimo.error.usernotfound': 'वापरकर्तानाव NaNoWriMo सर्व्हरवर आढळला नाही.',
-    'nanowrimo.error.nobook': 'पुस्तक आढळली नाही. कृपया NaNoWriMo वर आपली पुस्तक तयार केली आहे की नाही हे सुनिश्चित करा.',
-    'nanowrimo.error.wordcount': 'NaNoWrimo वर शब्दांची मोजमाप ठेवणे संपले नाही, कृपया आपले क्रेडेंशियल्स चेक करा आणि पुन्हा प्रयत्न करा.',
-    'nanowrimo.connected': 'तुम्ही हे लेख NaNoWriMo साठी निश्चित केलं आहे.',
-    'nanowrimo.connected.other': 'तुम्ही आधीच किंवा दुसरा लेख जोडलं आहे.'
+    'challenges.baseline': 'या कथेची सध्या शब्दांची संख्या {wordcount, number}आहे, ही सुरुवातीची बिंदू असेल.'
 };
