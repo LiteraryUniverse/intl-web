0f2c6cf25ab918fb104d11e5c1fd26f52a020777
Jan Dvorak <hello@jandvorak.me>
2025-06-08 18:16:03 +0200

    New translations challenges.js (Catalan)

--- ca-ES/challenges.js
+++ ca-ES/challenges.js
@@ -17,7 +17,6 @@ export default {
     'challenges.endDate': 'Data de finalització',
     'challenges.future': 'Desafiaments futurs',
     'challenges.future.wordcount': 'Desafiaments de comptador de paraules començant el {startDate, date, short} amb l\'objectiu de {target, number} per {endDate, date, short}.',
-    'challenges.future.nanowrimo': 'El desafiament de NaNoWriMo està a punt.',
     'challenges.wordcount': 'Compte de paraules',
     'challenges.wordcount.desc': `Estableix-te un objectiu de quantes paraules vols escriure aquest mes per a aquesta història de l'univers {type, select,
     story {}
@@ -29,29 +28,5 @@ export default {
     'challenges.report': 'Informe',
     'challenges.victory': 'Felicitats! Has completat aquest desafiament.',
     'challenges.defeat': 'Lamentablement no has completat aquest desafiament.',
-    'challenges.baseline': 'El nombre actual de paraules d\'aquesta història és {wordcount, number}, aquest serà el punt de partida.',
-    // NaNoWriMo
-    'nanowrimo.full': 'Mes Nacional d\'Escriptura de Novel·la',
-    'nanowrimo.site': 'Lloc web oficial de NaNoWriMo',
-    'nanowrimo.settings': 'Compte de NaNoWriMo',
-    'nanowrimo.username': 'Nom d\'usuari de NaNoWriMo',
-    'nanowrimo.key': 'Clau secreta',
-    'nanowrimo.key.get': 'Obtén la teva clau de NaNoWriMo.',
-    'nanowrimo.key.get.notice': 'Has d\'iniciar sessió per veure-la.',
-    'nanowrimo.ongoing': 'està en ple desenvolupament! {countdown} dies més per anar!',
-    'nanowrimo.startsin': `començarà en {countdown, plural,
-      one {1 dia}
-      other {# dies}
-    }! Prepara't!`,
-    'nanowrimo.isover': 'El NaNoWriMo ha acabat. Ens veiem l\'any que ve!',
-    'nanowrimo.select': 'Designa aquesta història per al NaNoWriMo.',
-    'nanowrimo.baseline': 'Quan comenci el NaNoWriMo, agafarà el recompte de paraules actual i el farà servir com a línia de base, a partir de la qual es comptaran les paraules addicionals com a part del NaNoWriMo.',
-    'nanowrimo.victory': 'Felicitats! Has guanyat el NaNoWriMo!',
-    'nanowrimo.challenge': 'Reto del NaNoWriMo',
-    'nanowrimo.description': 'El repte consisteix a escriure una novel·la de {words, number} paraules durant el novembre.',
-    'nanowrimo.error.usernotfound': 'El nom d\'usuari no s\'ha trobat als servidors de NaNoWriMo.',
-    'nanowrimo.error.nobook': 'No s\'ha trobat cap llibre. Assegura\'t de crear el teu llibre a NaNoWriMo.',
-    'nanowrimo.error.wordcount': 'No s\'ha pogut establir el recompte de paraules a NaNoWrimo, verifica les teves credencials i intenta-ho de nou.',
-    'nanowrimo.connected': 'Has designat aquesta història per al NaNoWriMo.',
-    'nanowrimo.connected.other': 'Ja has connectat una altra història.'
+    'challenges.baseline': 'El nombre actual de paraules d\'aquesta història és {wordcount, number}, aquest serà el punt de partida.'
 };
