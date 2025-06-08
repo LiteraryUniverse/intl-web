3868564e32954785d37db0da6f0488ee21f01e5d
Jan Dvorak <hello@jandvorak.me>
2025-06-08 18:16:28 +0200

    New translations challenges.js (Vietnamese)

--- vi-VN/challenges.js
+++ vi-VN/challenges.js
@@ -17,7 +17,6 @@ export default {
     'challenges.endDate': 'End date',
     'challenges.future': 'Thử thách sắp tới',
     'challenges.future.wordcount': 'Thử thách đếm từ bắt đầu vào {startDate, date, short} với mục tiêu là {target, number} vào {endDate, date, short}.',
-    'challenges.future.nanowrimo': 'Thử thách NaNoWriMo đã sẵn sàng.',
     'challenges.wordcount': 'Số từ',
     'challenges.wordcount.desc': `Đặt mục tiêu cho bản thân về số từ bạn muốn viết trong tháng này cho câu chuyện {type, select,
     story {trong vũ trụ}
@@ -29,29 +28,5 @@ export default {
     'challenges.report': 'Báo cáo',
     'challenges.victory': 'Chúc mừng! Bạn đã hoàn thành thử thách này.',
     'challenges.defeat': 'Rất tiếc, bạn chưa hoàn thành thử thách này.',
-    'challenges.baseline': 'Số từ hiện tại của câu chuyện này là {wordcount, number}, đây sẽ là điểm bắt đầu.',
-    // NaNoWriMo
-    'nanowrimo.full': 'Tháng viết tiểu thuyết quốc gia',
-    'nanowrimo.site': 'Trang web chính thức của NaNoWriMo',
-    'nanowrimo.settings': 'Tài khoản NaNoWriMo',
-    'nanowrimo.username': 'Tên người dùng NaNoWriMo',
-    'nanowrimo.key': 'Khóa bí mật',
-    'nanowrimo.key.get': 'Lấy khóa NaNoWriMo của bạn.',
-    'nanowrimo.key.get.notice': 'Bạn cần phải đăng nhập để xem.',
-    'nanowrimo.ongoing': 'đang diễn ra rầm rộ! Còn {countdown} ngày nữa!',
-    'nanowrimo.startsin': `will start in {countdown, plural,
-      one {1 day}
-      other {# days}
-    }! Get ready!`,
-    'nanowrimo.isover': 'NaNoWriMo đã kết thúc. Hẹn gặp lại năm sau!',
-    'nanowrimo.select': 'Chỉ định câu chuyện này cho NaNoWriMo.',
-    'nanowrimo.baseline': 'Khi NaNoWriMo bắt đầu, nó sẽ lấy số từ hiện tại và sử dụng nó như một cơ sở từ đó bất kỳ từ nào thêm vào sẽ được tính là một phần của NaNoWriMo.',
-    'nanowrimo.victory': 'Xin chúc mừng! Bạn đã chiến thắng NaNoWriMo!',
-    'nanowrimo.challenge': 'Thử thách NaNoWriMo',
-    'nanowrimo.description': 'Thử thách là viết một tiểu thuyết {words, number} từ trong tháng Mười một.',
-    'nanowrimo.error.usernotfound': 'Tên đăng nhập không được tìm thấy trên các máy chủ của NaNoWriMo.',
-    'nanowrimo.error.nobook': 'Không có cuốn sách nào. Vui lòng chắc chắn rằng bạn đã tạo sách của mình trên NaNoWriMo.',
-    'nanowrimo.error.wordcount': 'Không thể đặt số từ cho NaNoWrimo, vui lòng kiểm tra lại thông tin đăng nhập của bạn và thử lại.',
-    'nanowrimo.connected': 'Bạn đã chỉ định câu chuyện này cho NaNoWriMo.',
-    'nanowrimo.connected.other': 'Bạn đã kết nối một câu chuyện khác.'
+    'challenges.baseline': 'Số từ hiện tại của câu chuyện này là {wordcount, number}, đây sẽ là điểm bắt đầu.'
 };
