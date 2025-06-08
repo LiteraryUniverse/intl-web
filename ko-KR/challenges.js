866c6b701b50e27932222fea01865bef920f4577
Jan Dvorak <hello@jandvorak.me>
2025-06-08 18:16:15 +0200

    New translations challenges.js (Korean)

--- ko-KR/challenges.js
+++ ko-KR/challenges.js
@@ -17,7 +17,6 @@ export default {
     'challenges.endDate': '종료 날짜',
     'challenges.future': '다가오는 도전',
     'challenges.future.wordcount': '단어 수 도전은 {startDate, date, short} 에 시작하여 목표로 {target, number} 달성을 위해 {endDate, date, short}에 의해 진행됩니다.',
-    'challenges.future.nanowrimo': 'NaNoWriMo 도전이 준비되었습니다.',
     'challenges.wordcount': '단어 수',
     'challenges.wordcount.desc': `이번 {type, select,
     story {이야기}
@@ -29,29 +28,5 @@ export default {
     'challenges.report': '보고서',
     'challenges.victory': '축하합니다! 이 도전을 완료하셨습니다.',
     'challenges.defeat': '안타깝게도 이 도전을 완료하지 못했습니다.',
-    'challenges.baseline': '현재 이 이야기의 단어 수는 {wordcount, number}입니다, 이것이 시작점이 될 것입니다.',
-    // NaNoWriMo
-    'nanowrimo.full': 'National Novel Writing Month',
-    'nanowrimo.site': 'NaNoWriMo 공식 사이트',
-    'nanowrimo.settings': 'NaNoWriMo 계정',
-    'nanowrimo.username': 'NaNoWriMo 사용자 이름',
-    'nanowrimo.key': '비밀 키',
-    'nanowrimo.key.get': 'NaNoWriMo 키를 받으세요.',
-    'nanowrimo.key.get.notice': '로그인해야 볼 수 있습니다.',
-    'nanowrimo.ongoing': '풀 스윙입니다! {countdown} 일 더 남았습니다!',
-    'nanowrimo.startsin': `{countdown, plural,
-      one {일 후에 시작합니다! 1일}
-      other {#일}
-    }! 준비하세요!`,
-    'nanowrimo.isover': '나노라이모가 끝났습니다. 내년에 봐요!',
-    'nanowrimo.select': '이 이야기를 나노라이모로 지정하세요.',
-    'nanowrimo.baseline': '나노라이모가 시작되면 현재 단어 수를 기준으로 하여 추가 단어가 나노라이모의 일부로 인식됩니다.',
-    'nanowrimo.victory': '축하합니다! 나노라이모에 성공하셨습니다!',
-    'nanowrimo.challenge': '나노라이모 도전',
-    'nanowrimo.description': '도전은 {words, number} 단어의 소설을 11월 동안 쓰는 것입니다.',
-    'nanowrimo.error.usernotfound': '사용자 이름이 나노라이모 서버에서 찾을 수 없습니다.',
-    'nanowrimo.error.nobook': '책을 찾을 수 없습니다. 나노라이모에서 책을 생성했는지 확인하세요.',
-    'nanowrimo.error.wordcount': '나노라이모에 단어 수를 설정할 수 없습니다. 자격 증명을 다시 확인하고 다시 시도하세요.',
-    'nanowrimo.connected': '이 이야기를 나노라이모로 지정했습니다.',
-    'nanowrimo.connected.other': '이미 다른 이야기에 연결되어 있습니다.'
+    'challenges.baseline': '현재 이 이야기의 단어 수는 {wordcount, number}입니다, 이것이 시작점이 될 것입니다.'
 };
