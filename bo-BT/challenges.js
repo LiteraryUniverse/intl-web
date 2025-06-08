332eef5aa4afbd16263400e87a2d4564c3b7198f
Jan Dvorak <hello@jandvorak.me>
2025-06-08 18:16:42 +0200

    New translations challenges.js (Tibetan)

--- bo-BT/challenges.js
+++ bo-BT/challenges.js
@@ -17,7 +17,6 @@ export default {
     'challenges.endDate': 'End date',
     'challenges.future': 'Upcoming challenges',
     'challenges.future.wordcount': 'Wordcount challenge staring on {startDate, date, short} with goal of {target, number} by {endDate, date, short}.',
-    'challenges.future.nanowrimo': 'NaNoWriMo challenge is ready.',
     'challenges.wordcount': 'Word count',
     'challenges.wordcount.desc': `Set yourself a goal of how many words you want to write this month for this {type, select,
     story {story}
@@ -29,29 +28,5 @@ export default {
     'challenges.report': 'Report',
     'challenges.victory': 'Congratulations! You have completed this challenge.',
     'challenges.defeat': 'Sadly you didn\'t complete this challenge.',
-    'challenges.baseline': 'The current wordcount for this story is {wordcount, number}, this will be the starting point.',
-    // NaNoWriMo
-    'nanowrimo.full': 'National Novel Writing Month',
-    'nanowrimo.site': 'NaNoWriMo official site',
-    'nanowrimo.settings': 'NaNoWriMo account',
-    'nanowrimo.username': 'NaNoWriMo username',
-    'nanowrimo.key': 'Secret key',
-    'nanowrimo.key.get': 'ཁྱེད་ཀྱི་NaNoWriMo གསརཔ་རིས་དང་གཅིག་ཁྲམ་སྐྱོང་བའི་ལྟ་བའི་གྲལ་རིམ་གཏོགས་པ་བྱེད།',
-    'nanowrimo.key.get.notice': 'ཁྱེད་ཀྱི་དང་པོ་འགོ་བཙུགས་པའི་གདམ་ཁ་སྐོར་བཀོད་པའི་དོན་ལུ་བལྟ་བའི་འདི་ལྟ་བའི་སྒྲོམ་བརྡའི་གནས་སྡུད་བྱེད།',
-    'nanowrimo.ongoing': 'is in full swing! {countdown} more days to go!',
-    'nanowrimo.startsin': `will start in {countdown, plural,
-      one {1 day}
-      other {# days}
-    }! Get ready!`,
-    'nanowrimo.isover': 'NaNoWriMo is over. See you next year!',
-    'nanowrimo.select': 'Designate this story for NaNoWriMo.',
-    'nanowrimo.baseline': 'When NaNoWriMo starts it will take the current wordcount and use it as a baseline from which any additional words will be taken as part of NaNoWriMo.',
-    'nanowrimo.victory': 'Congratulation! You won NaNoWriMo!',
-    'nanowrimo.challenge': 'NaNoWriMo challenge',
-    'nanowrimo.description': 'The challenge is to write a novel of {words, number} words during November.',
-    'nanowrimo.error.usernotfound': 'Username has not been found on the NaNoWriMo servers.',
-    'nanowrimo.error.nobook': 'No book has been found. Please make sure you have created your book on NaNoWriMo.',
-    'nanowrimo.error.wordcount': 'Unable to set wordcount on NaNoWrimo, please double check your credentials and try again.',
-    'nanowrimo.connected': 'You have designated this story for NaNoWriMo.',
-    'nanowrimo.connected.other': 'You have already connected another story.'
+    'challenges.baseline': 'The current wordcount for this story is {wordcount, number}, this will be the starting point.'
 };
