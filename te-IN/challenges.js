29e4fde73e7663a3058e61dfb1f59a6fba6b0351
Jan Dvorak <hello@jandvorak.me>
2025-06-08 18:16:38 +0200

    New translations challenges.js (Telugu)

--- te-IN/challenges.js
+++ te-IN/challenges.js
@@ -17,7 +17,6 @@ export default {
     'challenges.endDate': 'ముగింపు తేదీ',
     'challenges.future': 'రాజివీకృత ఛాలెంజ్‌లు',
     'challenges.future.wordcount': 'Wordcount challenge staring on {startDate, date, short} with goal of {target, number} by {endDate, date, short}.',
-    'challenges.future.nanowrimo': 'NaNoWriMo ఛాలెంజ్ సిద్ధం అయ్యింది.',
     'challenges.wordcount': 'పదాల సంఖ్య',
     'challenges.wordcount.desc': `Set yourself a goal of how many words you want to write this month for this {type, select,
     story {story}
@@ -29,29 +28,5 @@ export default {
     'challenges.report': 'నివేదింపు',
     'challenges.victory': 'అభినందనలు! మీరు ఈ ఛాలెంజ్‌ను పూర్తిగా చేసారు.',
     'challenges.defeat': 'క్షమించండి, మీరు ఈ ఛాలెంజ్‌ను పూర్తిగా చేయలేకపోయారు.',
-    'challenges.baseline': 'ఈ కథ కు ప్రస్తుత పదసంఖ్య ఉంది {wordcount, number}, ఇది ప్రారంభ పాయింట్ అవుతుంది.',
-    // NaNoWriMo
-    'nanowrimo.full': 'జాతీయ నవల రచన నెల',
-    'nanowrimo.site': 'NaNoWriMo అధికారిక సైట్',
-    'nanowrimo.settings': 'NaNoWriMo ఖాతా',
-    'nanowrimo.username': 'NaNoWriMo వాడుకరి పేరు',
-    'nanowrimo.key': 'రహస్య కీ',
-    'nanowrimo.key.get': 'మీ NaNoWriMo కీని పొందండి.',
-    'nanowrimo.key.get.notice': 'దయచేసి లాగిన్ అయ్యి చూడటానికి మీరు కావాలి.',
-    'nanowrimo.ongoing': 'పూర్తి ప్రాంగణంలో ఉంది! {countdown} ఇనుకుంటే ఇనుకుంటే రోజులు ఉన్నాయి!',
-    'nanowrimo.startsin': `అందులో ప్రారంభం అవుతుంది {countdown, plural,
-      one {1 రోజు}
-      other {# రోజులు}
-    }! సిద్ధంగా ఉండండి!`,
-    'nanowrimo.isover': 'NaNoWriMo ముగిసింది. మీకు వచ్చే సంవత్సరం కలవాలని ఆకాంక్షలు!',
-    'nanowrimo.select': 'ఈ కథని NaNoWriMo కోసం నిర్దిష్టం చేయండి.',
-    'nanowrimo.baseline': 'NaNoWriMo ప్రారంభించడం విధానంలో ప్రస్తావిత పదాలను తీసుకోవడం మరియు అవినీతి పదాలను భాగంగా తీసుకోవడం కోసం ప్రస్తుత పదాంకాలను తీసుకోవడం వల్ల ఈ కథను నిర్ధిష్టం చేస్తుంది.',
-    'nanowrimo.victory': 'అభినందనలు! మీరు NaNoWriMo గెలిచారు!',
-    'nanowrimo.challenge': 'NaNoWriMo ఛాలెంజ్',
-    'nanowrimo.description': 'ఛాలెంజ్ అయినది నవలను {words, number} పదాలు నవంబర్ లో రాయడం.',
-    'nanowrimo.error.usernotfound': 'యూజర్ కనుగొనబడలేదు NaNoWriMo సర్వర్లులో.',
-    'nanowrimo.error.nobook': 'పుస్తకం కనుగొనబడలేదు. దయచేసి మీరు NaNoWriMo లో మీ పుస్తకంను సృష్టించారని మీరు ఖచ్చితంగా ఉంచండి.',
-    'nanowrimo.error.wordcount': 'NaNoWrimo లో పదసంఖ్య సెట్ చేయలేకపోయింది, దయచేసి మీ ప్రమాణాలను రాబడం మరియు మళ్లీ ప్రయత్నించండి.',
-    'nanowrimo.connected': 'ఈ కథను NaNoWriMo కోసం నిర్దిష్టం చేసినట్టుగా ఉంది.',
-    'nanowrimo.connected.other': 'మీరు ఇప్పటివరకు మరొక కథను కనెక్ట్ చేసారు.'
+    'challenges.baseline': 'ఈ కథ కు ప్రస్తుత పదసంఖ్య ఉంది {wordcount, number}, ఇది ప్రారంభ పాయింట్ అవుతుంది.'
 };
