ae7b61dd4deb7ea1f5942603130b13e83dec71c8
Jan Dvorak <hello@jandvorak.me>
2025-06-18 00:32:17 +0200

    New translations common.js (Catalan)

--- ca-ES/common.js
+++ ca-ES/common.js
@@ -119,7 +119,7 @@ export default {
     copyrights: 'Drets d\'autor',
     'common.challenges': 'Desafiaments',
     // for beta only
-    'beta.notice': 'Welcome to the Literary Universe BETA! This is an early preview of our platform, and we’re still actively working on improvements. As a result, you may encounter some bugs or incomplete features. If something doesn’t seem right, we’d greatly appreciate your feedback and understanding as we continue refining the experience.',
+    'beta.notice': 'Welcome to the Literary Universe BETA! We are excited to let you know that we are officially launching this Friday! We hope that you will join us and support us as we start a new chapter in our journey.',
     'beta.thanks': 'Gràcies pel teu suport!',
     'scratchpad.about': 'Les teves notes i idees accessibles des de qualsevol lloc del lloc web.',
     'common.feedback': 'Retroalimentació',
@@ -260,7 +260,7 @@ export default {
     'select.removeOption': 'Elimina {option}',
     create: 'Crear',
     'banner.support': 'Literary Universe is made possible thanks to the support of users like you. To help us remain independent and improve the platform, please consider subscribing to one of our plans.',
-    'banner.seePlans': 'Visit our roadmap for more information.',
+    'banner.seePlans': 'Visit our plans page for more information.',
     'common.noAccountQuestion': 'No tens un compte?',
     'common.haveAccountQuestion': 'Ja tens un compte?',
     'common.socialLogin': 'Inicia sessió amb altres serveis',
