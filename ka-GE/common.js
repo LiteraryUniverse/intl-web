88bb0145a04c6747ab9d0fedb2d005c31508b312
Jan Dvorak <hello@jandvorak.me>
2025-06-18 00:35:18 +0200

    New translations common.js (Georgian)

--- ka-GE/common.js
+++ ka-GE/common.js
@@ -119,7 +119,7 @@ export default {
     copyrights: 'ავტორული უფლებები',
     'common.challenges': 'თამაშები',
     // for beta only
-    'beta.notice': 'Welcome to the Literary Universe BETA! This is an early preview of our platform, and we’re still actively working on improvements. As a result, you may encounter some bugs or incomplete features. If something doesn’t seem right, we’d greatly appreciate your feedback and understanding as we continue refining the experience.',
+    'beta.notice': 'Welcome to the Literary Universe BETA! We are excited to let you know that we are officially launching this Friday! We hope that you will join us and support us as we start a new chapter in our journey.',
     'beta.thanks': 'გმადლობთ თქვენი მხარდაჭერისთვის!',
     'scratchpad.about': 'თქვენი შენიშვნები და იდეები ხელმისაწვდომია ნებისმიერ საიტზე.',
     'common.feedback': 'კავშირი',
@@ -260,7 +260,7 @@ export default {
     'select.removeOption': 'ამოიღე {option}',
     create: 'შექმენი',
     'banner.support': 'Literary Universe is made possible thanks to the support of users like you. To help us remain independent and improve the platform, please consider subscribing to one of our plans.',
-    'banner.seePlans': 'Visit our roadmap for more information.',
+    'banner.seePlans': 'Visit our plans page for more information.',
     'common.noAccountQuestion': 'არ გაქვთ ანგარიში?',
     'common.haveAccountQuestion': 'უკვე გაქვთ ანგარიში?',
     'common.socialLogin': 'შესვლა სხვა სერვისებით',
