f61a49b68520faab485e73003d60dd786e299d4e
Jan Dvorak <hello@jandvorak.me>
2025-06-18 00:42:20 +0200

    New translations common.js (Lojban)

--- jbo-EN/common.js
+++ jbo-EN/common.js
@@ -119,7 +119,7 @@ export default {
     copyrights: 'Copyrights',
     'common.challenges': 'Challenges',
     // for beta only
-    'beta.notice': 'Welcome to the Literary Universe BETA! This is an early preview of our platform, and we’re still actively working on improvements. As a result, you may encounter some bugs or incomplete features. If something doesn’t seem right, we’d greatly appreciate your feedback and understanding as we continue refining the experience.',
+    'beta.notice': 'Welcome to the Literary Universe BETA! We are excited to let you know that we are officially launching this Friday! We hope that you will join us and support us as we start a new chapter in our journey.',
     'beta.thanks': 'Thank you for your support!',
     'scratchpad.about': 'Your notes and ideas accessible anywhere on the site.',
     'common.feedback': 'Feedback',
@@ -260,7 +260,7 @@ export default {
     'select.removeOption': '.i kalri lo cmene {option}',
     create: '.i finti',
     'banner.support': 'Literary Universe is made possible thanks to the support of users like you. To help us remain independent and improve the platform, please consider subscribing to one of our plans.',
-    'banner.seePlans': 'Visit our roadmap for more information.',
+    'banner.seePlans': 'Visit our plans page for more information.',
     'common.noAccountQuestion': '.i na ka\'e pilno lo valsi',
     'common.haveAccountQuestion': '.i ka\'e pilno lo valsi',
     'common.socialLogin': '.i pilno lo drata se jacfa',
