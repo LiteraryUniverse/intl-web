6c46b7f11ede5dbe7dc58ffeb0d6d346947e9528
Jan Dvorak <hello@jandvorak.me>
2025-06-18 00:34:18 +0200

    New translations common.js (Hebrew)

--- he-IL/common.js
+++ he-IL/common.js
@@ -119,7 +119,7 @@ export default {
     copyrights: 'זכויות יוצרים',
     'common.challenges': 'אתגרים',
     // for beta only
-    'beta.notice': 'Welcome to the Literary Universe BETA! This is an early preview of our platform, and we’re still actively working on improvements. As a result, you may encounter some bugs or incomplete features. If something doesn’t seem right, we’d greatly appreciate your feedback and understanding as we continue refining the experience.',
+    'beta.notice': 'Welcome to the Literary Universe BETA! We are excited to let you know that we are officially launching this Friday! We hope that you will join us and support us as we start a new chapter in our journey.',
     'beta.thanks': 'תודה על התמיכה שלך!',
     'scratchpad.about': 'ההערות והרעיונות שלך נגישים בכל מקום באתר.',
     'common.feedback': 'משוב',
@@ -260,7 +260,7 @@ export default {
     'select.removeOption': 'Remove {option}',
     create: 'Create',
     'banner.support': 'Literary Universe is made possible thanks to the support of users like you. To help us remain independent and improve the platform, please consider subscribing to one of our plans.',
-    'banner.seePlans': 'Visit our roadmap for more information.',
+    'banner.seePlans': 'Visit our plans page for more information.',
     'common.noAccountQuestion': 'Don\'t have an account?',
     'common.haveAccountQuestion': 'Already have an account?',
     'common.socialLogin': 'Login with other services',
