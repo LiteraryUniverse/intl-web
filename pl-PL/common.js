b5f65a1527b2e318556fe2bf59b93b79a1f18e12
Jan Dvorak <hello@jandvorak.me>
2025-06-20 13:21:48 +0200

    New translations common.js (Polish)

--- pl-PL/common.js
+++ pl-PL/common.js
@@ -118,7 +118,9 @@ export default {
     'story.note.fanfiction': 'UWAGA: Ta historia jest fan fiction na {universe} wszechświata.',
     copyrights: 'Prawa Autorskie',
     'common.challenges': 'Wyzwania',
-    'notice.thankYou': 'Dziękujemy za Państwa wsparcie!',
+    // for beta only
+    'beta.notice': 'Witamy w Literary Universe BETA! Z radością informujemy Państwa, że oficjalnie startujemy w ten piątek! Mamy nadzieję, że dołączą Państwo do nas i wesprą nas w rozpoczęciu nowego rozdziału w naszej podróży.',
+    'beta.thanks': 'Dziękujemy za wsparcie!',
     'scratchpad.about': 'Twoje notatki i pomysły dostępne w każdym miejscu na stronie.',
     'common.feedback': 'Sugestie',
     'common.createdOn': 'Utworzony w dniu {date, date, long}',
