66bfc6f3f9e8865eb1e7cce335e007fa0e8949fd
Jan Dvorak <hello@jandvorak.me>
2025-06-20 13:19:52 +0200

    New translations common.js (Japanese)

--- ja-JP/common.js
+++ ja-JP/common.js
@@ -118,7 +118,9 @@ export default {
     'story.note.fanfiction': '注：この物語は{universe}宇宙のファンフィクションです。',
     copyrights: '著作権',
     'common.challenges': '挑戦',
-    'notice.thankYou': 'ご声援ありがとうございました！',
+    // for beta only
+    'beta.notice': 'リテラリーユニバースBETAへようこそ！今週金曜日に正式にスタートすることをお知らせできることを嬉しく思います！私たちの新たな旅立ちを応援してください。',
+    'beta.thanks': 'ご協力ありがとうございました。',
     'scratchpad.about': 'サイトのどこからでもご自身専用のメモ欄やアイデアにアクセスが可能です。',
     'common.feedback': 'ご意見',
     'common.createdOn': '{date, date, long}に作成',
