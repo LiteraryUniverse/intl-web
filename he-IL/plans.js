7f193b9deebe03c62f01b2f6894b027f0ec77da0
Jan Dvorak <hello@jandvorak.me>
2025-06-20 13:19:08 +0200

    New translations plans.js (Hebrew)

--- he-IL/plans.js
+++ he-IL/plans.js
@@ -321,6 +321,5 @@ export default {
     'plans.lifetime.subscription.benefit2': 'Custom avatar design',
     'plans.lifetime.subscription.benefit3': 'Monthly team calls',
     'plans.lifetime.subscription.benefit4': 'Feature request priority',
-    'plans.subscription.lifetimeMember': 'You have a lifetime membership',
-    'plans.lifetime.announcement': 'You have now a limited opportunity to gain lifetime membership! Check it out!'
+    'plans.subscription.lifetimeMember': 'You have a lifetime membership'
 };
