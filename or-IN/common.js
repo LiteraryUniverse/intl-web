b712995149ee5cfaf4131aae24244e4cc44e34bc
Jan Dvorak <hello@jandvorak.me>
2025-06-20 12:15:30 +0200

    New translations common.js (Odia)

--- or-IN/common.js
+++ or-IN/common.js
@@ -118,9 +118,7 @@ export default {
     'story.note.fanfiction': 'NOTE: This story is a fan fiction on {universe} universe.',
     copyrights: 'Copyrights',
     'common.challenges': 'Challenges',
-    // for beta only
-    'beta.notice': 'Welcome to the Literary Universe BETA! We are excited to let you know that we are officially launching this Friday! We hope that you will join us and support us as we start a new chapter in our journey.',
-    'beta.thanks': 'Thank you for your support!',
+    'notice.thankYou': 'Thank you for your support!',
     'scratchpad.about': 'Your notes and ideas accessible anywhere on the site.',
     'common.feedback': 'Feedback',
     'common.createdOn': 'ସୃଷ୍ଟି ହୋଇଥିବା {date, date, long}',
