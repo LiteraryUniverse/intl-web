465f2789d6306301b39c0fb901816e36bb302d3f
Jan Dvorak <hello@jandvorak.me>
2025-06-20 13:19:57 +0200

    New translations plans.js (Japanese)

--- ja-JP/plans.js
+++ ja-JP/plans.js
@@ -321,6 +321,5 @@ export default {
     'plans.lifetime.subscription.benefit2': 'カスタムアバターデザイン',
     'plans.lifetime.subscription.benefit3': '月1回のチーム・コール',
     'plans.lifetime.subscription.benefit4': '機能リクエストの優先順位',
-    'plans.subscription.lifetimeMember': '終身会員',
-    'plans.lifetime.announcement': '終身会員になるチャンスは限られています！ぜひご覧ください！'
+    'plans.subscription.lifetimeMember': '終身会員'
 };
