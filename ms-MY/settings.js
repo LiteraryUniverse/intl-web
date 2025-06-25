25d9f413331925e110c838a99995dc1b724b3a34
Jan Dvorak <hello@jandvorak.me>
2025-06-25 22:09:58 +0200

    New translations settings.js (Malay)

--- ms-MY/settings.js
+++ ms-MY/settings.js
@@ -50,7 +50,7 @@ export default {
     'settings.profile.identity': 'Identity',
     'settings.profile.about': 'About You',
     'settings.profile.basic.description': 'Manage your basic profile information that will be visible to other users.',
-    'settings.interest.title': 'Your Interests',
+    'settings.interests.title': 'Your Interests',
     'settings.account': 'Tetapan akaun',
     'settings.legal.export.download': 'Muat turun Data Anda',
     'settings.legal.export.gathering': 'Mengumpulkan Data Anda... Ini mungkin mengambil sedikit masa.',
@@ -131,5 +131,7 @@ export default {
     'settings.tab.exportDelete': 'Eksport data & padam akaun',
     'settings.languages.uiLanguage': 'Interface language',
     'settings.languages.readingLanguage': 'Reading language',
-    'settings.languages.readingLanguageExplained': 'When looking up stories we will offer you stories and their translations in this language unless you specify otherwise.'
+    'settings.languages.readingLanguageExplained': 'When looking up stories we will offer you stories and their translations in this language unless you specify otherwise.',
+    'settings.biography.help': 'Tell others about yourself',
+    'settings.username.help': 'Your unique username on the platform'
 };
