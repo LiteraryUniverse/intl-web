5d7d74cf72b974cff9bbad33aa2a40f895f6eb16
Jan Dvorak <hello@jandvorak.me>
2025-06-25 20:49:40 +0200

    New translations user.js (Dutch)

--- nl-NL/user.js
+++ nl-NL/user.js
@@ -59,7 +59,7 @@ export default {
     'storiesType.play': 'Speel',
     'storiesType.poetry': 'Poëzie',
     'storiesType.serialized': 'Geserialiseerd',
-    'storiesType.short_story': 'kort_verhaal',
+    'storiesType.short_story': 'Kort verhaal',
     'user.settings.introTitle': 'Welkom bij uw instellingen!',
     'user.settings.introText': 'Profielinstellingen stellen u in staat om informatie over uzelf aan te passen, zoals uw naam, avatar, enz. Met Accountinstellingen kunt u de meer technische aspecten instellen die invloed hebben op uw ervaring, bijvoorbeeld uw taal, e-mails, sitethema, AI-instellingen, enz. Abonnement & Betalingen geeft u de mogelijkheid om uw abonnementen en betalingsopties te beheren. Tot slot kunt u met Monetization uw uitbetalingen, fanclubs en andere monetariseringsopties beheren.'
 };
