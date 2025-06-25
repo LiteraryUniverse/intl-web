55eaea8ae80c39126500c2aa6eb1cb51c2e1fb16
Jan Dvorak <hello@jandvorak.me>
2025-06-25 22:11:35 +0200

    New translations settings.js (Lojban)

--- jbo-EN/settings.js
+++ jbo-EN/settings.js
@@ -50,7 +50,7 @@ export default {
     'settings.profile.identity': 'Identity',
     'settings.profile.about': 'About You',
     'settings.profile.basic.description': 'Manage your basic profile information that will be visible to other users.',
-    'settings.interest.title': 'Your Interests',
+    'settings.interests.title': 'Your Interests',
     'settings.account': '.i mi\'o se setca lo du\'u do zukte ma kau',
     'settings.legal.export.download': '.i mi\'o ba\'o zukte lo nu do vasru lo do datni',
     'settings.legal.export.gathering': '.i mi\'o zukte lo nu vasru sepi\'o datni... ti se kanpe lo nu se waitau',
@@ -131,5 +131,7 @@ export default {
     'settings.tab.exportDelete': 'Izvezi podatke i izbriši račun',
     'settings.languages.uiLanguage': 'Interface language',
     'settings.languages.readingLanguage': 'Reading language',
-    'settings.languages.readingLanguageExplained': 'When looking up stories we will offer you stories and their translations in this language unless you specify otherwise.'
+    'settings.languages.readingLanguageExplained': 'When looking up stories we will offer you stories and their translations in this language unless you specify otherwise.',
+    'settings.biography.help': 'Tell others about yourself',
+    'settings.username.help': 'Your unique username on the platform'
 };
