aaaa7d6a4c008424188428231e75e4037011eff6
Jan Dvorak <hello@jandvorak.me>
2025-06-29 20:27:48 +0200

    New translations library.js (Spanish)

--- es-ES/library.js
+++ es-ES/library.js
@@ -5,5 +5,8 @@ export default {
     'library.stories': 'Historias que sigues',
     'library.universes': 'Universo que sigues',
     'bookmark.continueReading': 'Continuar leyendo',
-    'bookmark.statusSummary': 'Has leído un {readPercent, number, ::percent} de la historia.'
+    'bookmark.statusSummary': 'Has leído un {readPercent, number, ::percent} de la historia.',
+    'library.search.prompt': 'Search your library to find content',
+    'library.yourLibrary': 'Your Library',
+    'library.search': 'Search Your Library'
 };
