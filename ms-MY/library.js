68b7d440d1b710ec02d2a8509c06dad8611a4364
Jan Dvorak <hello@jandvorak.me>
2025-06-29 21:52:20 +0200

    New translations library.js (Malay)

--- ms-MY/library.js
+++ ms-MY/library.js
@@ -8,5 +8,6 @@ export default {
     'bookmark.statusSummary': 'Anda telah membaca {readPercent, number, ::percent} daripada cerita.',
     'library.search.prompt': 'Search your library to find content',
     'library.yourLibrary': 'Your Library',
-    'library.search': 'Search Your Library'
+    'library.search': 'Search Your Library',
+    'library.search.noResults': 'No results found for your search'
 };
