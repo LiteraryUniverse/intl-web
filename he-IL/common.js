9ea69c20148ebfb795a1e35d779caa432cd9e629
Jan Dvorak <hello@jandvorak.me>
2025-06-29 17:36:31 +0200

    New translations common.js (Hebrew)

--- he-IL/common.js
+++ he-IL/common.js
@@ -85,14 +85,17 @@ export default {
     'common.legal.community': 'הנחיות הקהילה',
     'common.legal.privacy': 'מדיניות פרטיות',
     'common.legal.copyright': 'הסכם זכויות יוצרים',
+    'common.legal.cookies': 'About cookies',
+    'cookies.desc': 'This website uses cookies to ensure you get the best experience on our website.',
     'common.legal.versionDisplay': `{type, select,
-    tos {תנאי השימוש}
-    community {הנחיות הקהילה}
-    privacy {מדיניות פרטיות}
-    copyright {הסכם זכויות יוצרים}
-    publishingAgreement {הסכם לפרסום}
-    other {הסכם}
-  } גרסה {version} - {date, date}`,
+    tos {Terms of Service}
+    community {Community Guidelines}
+    privacy {Privacy Policy}
+    copyright {Copyright Agreement}
+    publishingAgreement {Publishing Agreement}
+    cookies {About Cookies}
+    other {Agreement}
+  } Version {version} - {date, date}`,
     'common.legal.newVersionAvailable': 'גרסה חדשה ({newVersion}) של מסמך זה תיכנס לתוקף ב {effectiveAt, date}.',
     'common.legal.disclaimer': '© {year} כל הזכויות שמורות, כל הטקסטים שייכים למחבריהם.',
     'premium.only.title': 'דרגת חברות לא מספיקה',
