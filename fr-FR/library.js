c3d87423b71db52ca48c64fd4c520f46b2e0f688
Jan Dvorak <hello@jandvorak.me>
2025-06-29 20:27:28 +0200

    New translations library.js (French)

--- fr-FR/library.js
+++ fr-FR/library.js
@@ -5,5 +5,8 @@ export default {
     'library.stories': 'Histoires suivies',
     'library.universes': 'Univers suivis',
     'bookmark.continueReading': 'Continuer la lecture',
-    'bookmark.statusSummary': 'Vous avez lu {readPercent, number, ::percent} de l\'histoire.'
+    'bookmark.statusSummary': 'Vous avez lu {readPercent, number, ::percent} de l\'histoire.',
+    'library.search.prompt': 'Search your library to find content',
+    'library.yourLibrary': 'Your Library',
+    'library.search': 'Search Your Library'
 };
