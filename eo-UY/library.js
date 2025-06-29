eb9b4132e50abf5e30bc9e2c0abcb8a916caf081
Jan Dvorak <hello@jandvorak.me>
2025-06-29 20:42:28 +0200

    New translations library.js (Esperanto)

--- eo-UY/library.js
+++ eo-UY/library.js
@@ -5,5 +5,8 @@ export default {
     'library.stories': 'Sekvitaj rakontoj',
     'library.universes': 'Sekvitaj universoj',
     'bookmark.continueReading': 'Legi plu',
-    'bookmark.statusSummary': 'Vi legis {readPercent, number, ::percent} de la rakonto.'
+    'bookmark.statusSummary': 'Vi legis {readPercent, number, ::percent} de la rakonto.',
+    'library.search.prompt': 'Search your library to find content',
+    'library.yourLibrary': 'Your Library',
+    'library.search': 'Search Your Library'
 };
