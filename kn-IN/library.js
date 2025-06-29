a864347efe794bcf3eefb77234e893dfbfc81fab
Jan Dvorak <hello@jandvorak.me>
2025-06-29 21:54:09 +0200

    New translations library.js (Kannada)

--- kn-IN/library.js
+++ kn-IN/library.js
@@ -8,5 +8,6 @@ export default {
     'bookmark.statusSummary': 'ನೀವು ಕಥೆಯ ಆರಂಭದಿಂದ {readPercent, number, ::percent} ಓದಿದ್ದೀರಿ.',
     'library.search.prompt': 'Search your library to find content',
     'library.yourLibrary': 'Your Library',
-    'library.search': 'Search Your Library'
+    'library.search': 'Search Your Library',
+    'library.search.noResults': 'No results found for your search'
 };
