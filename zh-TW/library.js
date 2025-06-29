648b4007dc5733d9f8ff2274070699fa2a2b2612
Jan Dvorak <hello@jandvorak.me>
2025-06-29 21:49:27 +0200

    New translations library.js (Chinese Traditional)

--- zh-TW/library.js
+++ zh-TW/library.js
@@ -8,5 +8,6 @@ export default {
     'bookmark.statusSummary': '你已閱讀 {readPercent, number, ::percent} 篇故事。',
     'library.search.prompt': 'Search your library to find content',
     'library.yourLibrary': 'Your Library',
-    'library.search': 'Search Your Library'
+    'library.search': 'Search Your Library',
+    'library.search.noResults': 'No results found for your search'
 };
