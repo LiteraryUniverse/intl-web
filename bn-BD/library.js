0b6db2a52268cc549030ce8129ace551ddd8fe7e
Jan Dvorak <hello@jandvorak.me>
2025-06-29 21:51:01 +0200

    New translations library.js (Bengali)

--- bn-BD/library.js
+++ bn-BD/library.js
@@ -8,5 +8,6 @@ export default {
     'bookmark.statusSummary': 'আপনি গল্পের {readPercent, number, ::percent} পড়েছেন।',
     'library.search.prompt': 'Search your library to find content',
     'library.yourLibrary': 'Your Library',
-    'library.search': 'Search Your Library'
+    'library.search': 'Search Your Library',
+    'library.search.noResults': 'No results found for your search'
 };
