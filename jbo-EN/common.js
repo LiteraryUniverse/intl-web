8c2b5250c8bfc38d5478abae8f368db316a68cf2
Jan Dvorak <hello@jandvorak.me>
2025-06-29 17:46:54 +0200

    New translations common.js (Lojban)

--- jbo-EN/common.js
+++ jbo-EN/common.js
@@ -85,12 +85,15 @@ export default {
     'common.legal.community': 'lo nu do zukte fi lo nu zukte',
     'common.legal.privacy': 'lo nu do zukte fi lo nu zukte',
     'common.legal.copyright': 'lo nu do zukte fi lo nu zukte',
+    'common.legal.cookies': 'About cookies',
+    'cookies.desc': 'This website uses cookies to ensure you get the best experience on our website.',
     'common.legal.versionDisplay': `{type, select,
     tos {Terms of Service}
     community {Community Guidelines}
     privacy {Privacy Policy}
     copyright {Copyright Agreement}
     publishingAgreement {Publishing Agreement}
+    cookies {About Cookies}
     other {Agreement}
   } Version {version} - {date, date}`,
     'common.legal.newVersionAvailable': 'Lo se nu jdima cu jbogri ({newVersion}) ca zvati le nu jdima lebna li {effectiveAt, date}.',
