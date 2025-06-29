23fec9b30d297ca7fd82155c40a9b2ee7b693260
Jan Dvorak <hello@jandvorak.me>
2025-06-29 20:36:20 +0200

    New translations library.js (Slovak)

--- sk-SK/library.js
+++ sk-SK/library.js
@@ -5,5 +5,8 @@ export default {
     'library.stories': 'Sledované príbehy',
     'library.universes': 'Sledované vesmíry',
     'bookmark.continueReading': 'Pokračovať v čítaní',
-    'bookmark.statusSummary': 'Přečetli jste si {readPercent, number, ::percent} příběhu.'
+    'bookmark.statusSummary': 'Přečetli jste si {readPercent, number, ::percent} příběhu.',
+    'library.search.prompt': 'Search your library to find content',
+    'library.yourLibrary': 'Your Library',
+    'library.search': 'Search Your Library'
 };
