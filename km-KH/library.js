6db67901cc3d46df8b88dd4cdbf166ebbd43e0f8
Jan Dvorak <hello@jandvorak.me>
2025-06-29 21:50:30 +0200

    New translations library.js (Khmer)

--- km-KH/library.js
+++ km-KH/library.js
@@ -8,5 +8,6 @@ export default {
     'bookmark.statusSummary': 'អ្នកបានអានរួចហើយ {readPercent, number, ::percent} នៃរឿងនេះ។',
     'library.search.prompt': 'Search your library to find content',
     'library.yourLibrary': 'Your Library',
-    'library.search': 'Search Your Library'
+    'library.search': 'Search Your Library',
+    'library.search.noResults': 'No results found for your search'
 };
