46cbe862be0683d4b685ef518f349d8119b4f807
Jan Dvorak <hello@jandvorak.me>
2025-06-29 20:40:42 +0200

    New translations library.js (Thai)

--- th-TH/library.js
+++ th-TH/library.js
@@ -5,5 +5,8 @@ export default {
     'library.stories': 'เรื่องที่ติดตาม',
     'library.universes': 'จำนวนยูนิเวิร์สที่ติดตาม',
     'bookmark.continueReading': 'ดำเนินการอ่านต่อ',
-    'bookmark.statusSummary': 'คุณได้อ่านเรื่องนี้แล้ว {readPercent, number, ::percent} ของเรื่อง'
+    'bookmark.statusSummary': 'คุณได้อ่านเรื่องนี้แล้ว {readPercent, number, ::percent} ของเรื่อง',
+    'library.search.prompt': 'Search your library to find content',
+    'library.yourLibrary': 'Your Library',
+    'library.search': 'Search Your Library'
 };
