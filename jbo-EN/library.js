14096bb921022dee661650f7ce16e106e22ad60e
Jan Dvorak <hello@jandvorak.me>
2025-06-29 20:43:09 +0200

    New translations library.js (Lojban)

--- jbo-EN/library.js
+++ jbo-EN/library.js
@@ -5,5 +5,8 @@ export default {
     'library.stories': 'Cuxna lo pelji',
     'library.universes': 'Jorne universiteto',
     'bookmark.continueReading': 'Jorne lo nu tcidu',
-    'bookmark.statusSummary': 'I do\'u cu tcidu {readPercent, number, ::percent} be lo lisri.'
+    'bookmark.statusSummary': 'I do\'u cu tcidu {readPercent, number, ::percent} be lo lisri.',
+    'library.search.prompt': 'Search your library to find content',
+    'library.yourLibrary': 'Your Library',
+    'library.search': 'Search Your Library'
 };
