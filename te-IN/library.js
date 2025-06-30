1e383478121db8f69ca0e4ec7c229c504a9b0306
Jan Dvorak <hello@jandvorak.me>
2025-06-30 10:34:10 +0200

    New translations library.js (Telugu)

--- te-IN/library.js
+++ te-IN/library.js
@@ -8,6 +8,5 @@ export default {
     'bookmark.statusSummary': 'మీరు కథని చదువుతున్నారు {readPercent, number, ::percent} నుండి.',
     'library.search.prompt': 'Search your library to find content',
     'library.yourLibrary': 'Your Library',
-    'library.search': 'Search Your Library',
-    'library.search.noResults': 'No results found for your search'
+    'library.search': 'Search Your Library'
 };
