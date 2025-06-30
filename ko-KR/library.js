076d582012ab9bfabcf50754a71e03fdad2fd7c3
Jan Dvorak <hello@jandvorak.me>
2025-06-30 10:23:31 +0200

    New translations library.js (Korean)

--- ko-KR/library.js
+++ ko-KR/library.js
@@ -8,6 +8,5 @@ export default {
     'bookmark.statusSummary': '이야기의 {readPercent, number, ::percent} 를 읽으셨습니다.',
     'library.search.prompt': 'Search your library to find content',
     'library.yourLibrary': 'Your Library',
-    'library.search': 'Search Your Library',
-    'library.search.noResults': 'No results found for your search'
+    'library.search': 'Search Your Library'
 };
