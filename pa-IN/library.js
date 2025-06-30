26dc994bfab1cad21ce744df4dd578fb274f3aea
Jan Dvorak <hello@jandvorak.me>
2025-06-30 10:25:18 +0200

    New translations library.js (Punjabi)

--- pa-IN/library.js
+++ pa-IN/library.js
@@ -8,6 +8,5 @@ export default {
     'bookmark.statusSummary': 'ਤੁਸੀਂ ਕਹਾਣੀ ਦੀ ਪੜਾਈ ਕੀਤੀ ਹੈ {readPercent, number, ::percent} ਵਾਰ।',
     'library.search.prompt': 'Search your library to find content',
     'library.yourLibrary': 'Your Library',
-    'library.search': 'Search Your Library',
-    'library.search.noResults': 'No results found for your search'
+    'library.search': 'Search Your Library'
 };
