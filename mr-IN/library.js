f8897e5bbee36520f767ce9d82b9bf265d3aa352
Jan Dvorak <hello@jandvorak.me>
2025-06-30 10:32:01 +0200

    New translations library.js (Marathi)

--- mr-IN/library.js
+++ mr-IN/library.js
@@ -8,6 +8,5 @@ export default {
     'bookmark.statusSummary': 'तुम्ही कथेचे {readPercent, number, ::percent} वाचले आहे.',
     'library.search.prompt': 'Search your library to find content',
     'library.yourLibrary': 'Your Library',
-    'library.search': 'Search Your Library',
-    'library.search.noResults': 'No results found for your search'
+    'library.search': 'Search Your Library'
 };
