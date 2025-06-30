4be2be31e7997e0c5498340fb189182798ffd16d
Jan Dvorak <hello@jandvorak.me>
2025-06-30 10:17:34 +0200

    New translations library.js (Arabic)

--- ar-SA/library.js
+++ ar-SA/library.js
@@ -8,6 +8,5 @@ export default {
     'bookmark.statusSummary': 'لقد قرأت {readPercent, number, ::percent} من القصة.',
     'library.search.prompt': 'Search your library to find content',
     'library.yourLibrary': 'Your Library',
-    'library.search': 'Search Your Library',
-    'library.search.noResults': 'No results found for your search'
+    'library.search': 'Search Your Library'
 };
