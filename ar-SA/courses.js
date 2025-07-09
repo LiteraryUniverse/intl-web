cd7566db019f8121d83dc3bf743a9549ab5cdb96
Jan Dvorak <hello@jandvorak.me>
2025-07-09 13:59:44 +0200

    New translations courses.js (Arabic)

--- ar-SA/courses.js
+++ ar-SA/courses.js
@@ -117,4 +117,6 @@ export default {
   'contentSelector.myLibrary': 'My library',
   'contentSelector.tabsLabel': 'Search options',
   'contentSelector.title': 'Select Content',
+  'course.startDate': 'تاريخ البدء',
+  'course.endDate': 'تاريخ الانتهاء',
 };
