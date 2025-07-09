76ead106ab64abae8281c02cf332a2105f4a0a6c
Jan Dvorak <hello@jandvorak.me>
2025-07-09 13:59:17 +0200

    New translations courses.js (Romanian)

--- ro-RO/courses.js
+++ ro-RO/courses.js
@@ -117,4 +117,6 @@ export default {
   'contentSelector.myLibrary': 'My library',
   'contentSelector.tabsLabel': 'Search options',
   'contentSelector.title': 'Select Content',
+  'course.startDate': 'Data de începere',
+  'course.endDate': 'Data de încheiere',
 };
