71658326afd833c40ede2ad27b9b17c93cf28000
Jan Dvorak <hello@jandvorak.me>
2025-08-21 22:04:08 +0200

    New translations writer.js (Swedish)

--- sv-SE/writer.js
+++ sv-SE/writer.js
@@ -32,5 +32,7 @@ export default {
     'writer.publish.betaReader': 'Beta',
     'writer.publish.public': 'Publicera',
     'writer.publish.now': 'Publicera nu',
-    'writer.publish.schedulePrevious': 'Du måste publicera eller schemalägga publiceringen av föregående kapitel innan du kan publicera det här.'
+    'writer.publish.schedulePrevious': 'Du måste publicera eller schemalägga publiceringen av föregående kapitel innan du kan publicera det här.',
+    'writer.publish.earlyAccess.title': 'Tidig tillgång för fans',
+    'writer.publish.earlyAccess.groupLine': '{days} dagar i förväg: {tiers} - fans får tillgång på {date, date, short} på {date, time}'
 };
