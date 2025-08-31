bf03a46013258a1c0d292164f4c52e38a5a47a65
Jan Dvorak <hello@jandvorak.me>
2025-08-31 13:14:26 +0200

    New translations common.js (Hungarian)

--- hu-HU/common.js
+++ hu-HU/common.js
@@ -7,6 +7,7 @@ export default {
     'site.description': 'Az Irodalmi Univerzum egy mindenre kiterjedő kreatív és kiadói platform a nagyszerű történetek univerzumaihoz.',
     'site.keywords': 'kreatív írás, sci-fi, fantasy, írás, kiadás, történet, történetek, univerzum, webregény, webnovella',
     'site.logoAlt': 'Irodalmi Univerzum logó',
+    'site.titleTemplate': '{title} | {siteName}',
     mobileMenu: 'Mobil menü',
     'common.signin': 'Bejelentkezés',
     'common.signup': 'Regisztráció',
