cdc0525f8a864b656bd1e66167530347f211c9f6
Jan Dvorak <hello@jandvorak.me>
2025-08-31 13:27:53 +0200

    New translations common.js (Odia)

--- or-IN/common.js
+++ or-IN/common.js
@@ -7,6 +7,7 @@ export default {
     'site.description': 'Literary Universe is an all-in-one creative and publishing platform for grand story universes.',
     'site.keywords': 'creative writing, scifi, sci-fi, fantasy, writing, publishing, story, stories, universe, web novel, webnovel',
     'site.logoAlt': 'Literary Universe logo',
+    'site.titleTemplate': '{title} | {siteName}',
     mobileMenu: 'Mobile menu',
     'common.signin': 'Login',
     'common.signup': 'Register',
