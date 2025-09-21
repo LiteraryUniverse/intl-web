34d313e79702ff3d285f20f13f348efc795a4ff8
Jan Dvorak <hello@jandvorak.me>
2025-09-21 14:00:36 +0200

    New translations emails.js (Norwegian)

--- no-NO/emails.js
+++ no-NO/emails.js
@@ -15,6 +15,7 @@ export default {
     'email.salutation': 'Hei {username},',
     'email.welcome': 'Velkommen til Literary Universe!',
     'email.signature': 'Med vennlig hilsen,<br />Literary Universe-teamet',
+    'email.footer.settingsHtml': 'You can change your email settings in <a href="{link}">your account settings</a>.',
     // Enrollment
     'email.enrollAccount.welcome': 'Velkommen om bord',
     'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe. Please join us in creating the next generation of creative writing and reading!',
