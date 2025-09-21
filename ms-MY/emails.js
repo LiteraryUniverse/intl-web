85a2028d7e5a4d61d9f32398fe0f464a2d39cb73
Jan Dvorak <hello@jandvorak.me>
2025-09-21 14:07:49 +0200

    New translations emails.js (Malay)

--- ms-MY/emails.js
+++ ms-MY/emails.js
@@ -15,6 +15,7 @@ export default {
     'email.salutation': 'Hello {username},',
     'email.welcome': 'Selamat datang ke Alam Sastera!',
     'email.signature': 'Salam hormat,<br />pasukan Alam Sastera',
+    'email.footer.settingsHtml': 'You can change your email settings in <a href="{link}">your account settings</a>.',
     // Enrollment
     'email.enrollAccount.welcome': 'Selamat datang',
     'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe. Please join us in creating the next generation of creative writing and reading!',
