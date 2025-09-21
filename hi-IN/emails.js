06dc5b6f6f55125c5703850486bd660f5c6f6f57
Jan Dvorak <hello@jandvorak.me>
2025-09-21 14:07:28 +0200

    New translations emails.js (Hindi)

--- hi-IN/emails.js
+++ hi-IN/emails.js
@@ -15,6 +15,7 @@ export default {
     'email.salutation': 'नमस्ते {username},',
     'email.welcome': 'कविता यूनिवर्स में आपका स्वागत है!',
     'email.signature': 'आभारी,<br />कविता यूनिवर्स टीम',
+    'email.footer.settingsHtml': 'You can change your email settings in <a href="{link}">your account settings</a>.',
     // Enrollment
     'email.enrollAccount.welcome': 'स्वागत करते हैं',
     'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe. Please join us in creating the next generation of creative writing and reading!',
