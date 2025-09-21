b5f134da82c46a2cd49d2909a10e394b9a99ba07
Jan Dvorak <hello@jandvorak.me>
2025-09-21 14:08:56 +0200

    New translations emails.js (Filipino)

--- fil-PH/emails.js
+++ fil-PH/emails.js
@@ -15,6 +15,7 @@ export default {
     'email.salutation': 'Kamusta {username},',
     'email.welcome': 'Maligayang pagdating sa Literary Universe!',
     'email.signature': 'Buong katapatan,<br />Literary Universe team',
+    'email.footer.settingsHtml': 'You can change your email settings in <a href="{link}">your account settings</a>.',
     // Enrollment
     'email.enrollAccount.welcome': 'Maligayang pagdating sa Literary Universe!',
     'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe. Please join us in creating the next generation of creative writing and reading!',
