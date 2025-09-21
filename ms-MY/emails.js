27031f5d0da1b1233e96838230f26165c1a7719f
Jan Dvorak <hello@jandvorak.me>
2025-09-21 11:07:37 +0200

    New translations emails.js (Malay)

--- ms-MY/emails.js
+++ ms-MY/emails.js
@@ -34,20 +34,20 @@ export default {
     storyteller {Storyteller}
     other {}
   } tier for the next {days} days.\n\nEnjoy,\nLiterary Universe team`,
-    'email.adminUpgrade.messageHtml': `Kami telah meningkat taraf akaun anda kepada {plan, select,
-    explorer {Penjelajah}
-    adventurer {Pengembara}
-    storyteller {Pencerita}
+    'email.adminUpgrade.messageHtml': `We have upgraded your account to {plan, select,
+    explorer {Explorer}
+    adventurer {Adventurer}
+    storyteller {Storyteller}
     other {}
-  } untuk {days} hari seterusnya. Nikmati!`,
+  } tier for the next {days, number} days. Enjoy!`,
     'email.verification': 'Pengesahan E-mel',
     'email.verifying': 'Sedang mengesahkan e-mel anda. Ini hanya akan mengambil seketika...',
     'email.verified': 'Berita baik! E-mel anda telah disahkan!',
     // The Way (Author Level Up)
-    'theWay.levelUp.emailSubject': 'Congratulations! You\'ve reached Author Level {level} on Literary Universe!',
-    'theWay.levelUp.emailTitle': 'Congratulations! You\'ve reached Author Level {level}!',
+    'theWay.levelUp.emailSubject': 'Congratulations! You\'ve reached Author Level {level, number} on Literary Universe!',
+    'theWay.levelUp.emailTitle': 'Congratulations! You\'ve reached Author Level {level, number}!',
     'theWay.levelUp.emailIntro': 'Your dedication to the craft of writing has been rewarded!',
-    'theWay.levelUp.emailGenre': 'You have achieved level {level} in the {genre} genre.',
+    'theWay.levelUp.emailGenre': 'You have achieved level {level, number} in the {genre} genre.',
     'theWay.levelUp.emailContinue': 'Continue your journey and unlock more achievements on The Way of the Author.',
     'theWay.levelUp.viewDashboard': 'View Your Dashboard',
     // Notifications - General content comment
