778f821be4195954317cde7c48607a3f578a9e5d
Jan Dvorak <hello@jandvorak.me>
2025-09-21 11:08:20 +0200

    New translations emails.js (Esperanto)

--- eo-UY/emails.js
+++ eo-UY/emails.js
@@ -34,20 +34,20 @@ export default {
     storyteller {Rakontisto}
     other {}
   } rango por la venontaj {days} tagoj.\n\nĜuu,\nteamo de Litera Universo`,
-    'email.adminUpgrade.messageHtml': `Ni plibonigis vian konton al {plan, select,
-    explorer {Esploristo}
-    adventurer {Aventuristo}
-    storyteller {Rakontisto}
+    'email.adminUpgrade.messageHtml': `We have upgraded your account to {plan, select,
+    explorer {Explorer}
+    adventurer {Adventurer}
+    storyteller {Storyteller}
     other {}
-  } rango por la venontaj {days} tagoj. Ĝuu!`,
+  } tier for the next {days, number} days. Enjoy!`,
     'email.verification': 'Verifo de retpoŝto',
     'email.verifying': 'Ni verifas vian retpoŝton. Tio nur iom daŭros...',
     'email.verified': 'Granda novaĵo! Via retpoŝto estis verifita!',
     // The Way (Author Level Up)
-    'theWay.levelUp.emailSubject': 'Congratulations! You\'ve reached Author Level {level} on Literary Universe!',
-    'theWay.levelUp.emailTitle': 'Congratulations! You\'ve reached Author Level {level}!',
+    'theWay.levelUp.emailSubject': 'Congratulations! You\'ve reached Author Level {level, number} on Literary Universe!',
+    'theWay.levelUp.emailTitle': 'Congratulations! You\'ve reached Author Level {level, number}!',
     'theWay.levelUp.emailIntro': 'Your dedication to the craft of writing has been rewarded!',
-    'theWay.levelUp.emailGenre': 'You have achieved level {level} in the {genre} genre.',
+    'theWay.levelUp.emailGenre': 'You have achieved level {level, number} in the {genre} genre.',
     'theWay.levelUp.emailContinue': 'Continue your journey and unlock more achievements on The Way of the Author.',
     'theWay.levelUp.viewDashboard': 'View Your Dashboard',
     // Notifications - General content comment
