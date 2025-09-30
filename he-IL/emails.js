558cee0a5ab23912cdbd8b73e25b2380c275e9e6
Jan Dvorak <hello@jandvorak.me>
2025-09-30 21:56:59 +0200

    New translations emails.js (Hebrew)

--- he-IL/emails.js
+++ he-IL/emails.js
@@ -14,13 +14,13 @@ export default {
     // New html email keys
     'email.salutation': 'שלום {username},',
     'email.welcome': 'ברוך הבא ליקום הספרותי!',
-    'email.signature': 'בברכה,<br />צוות יקום הספרותי',
-    'email.footer.settingsHtml': 'ניתן לשנות את הגדרות הדוא"ל <a href="{link}">בהגדרות החשבון</a>.',
+    'email.signature': 'בכבוד רב,<br>צוות Literary Universe</br>',
+    'email.footer.settingsHtml': 'ניתן לשנות את הגדרות הדוא"ל <link>בהגדרות החשבון</link>.',
     // Enrollment
     'email.enrollAccount.welcome': 'ברוך הבא לסירוב!',
     'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe. Please join us in creating the next generation of creative writing and reading!',
     'email.enrollAccount.button': 'גש לחשבון שלך',
-    'email.enrollAccount.signature': 'We are looking forward to seeing you,<br />Literary Universe team',
+    'email.enrollAccount.signature': 'אנו מצפים לראותכם,<br>צוות Literary Universe</br>',
     // Reset password
     'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account. Please click on the button below to continue the process. If you haven\'t made this request, you can ignore this e-mail, but it might be a good time to review your security settings.',
     'email.resetPassword.button': 'איפוס סיסמה',
