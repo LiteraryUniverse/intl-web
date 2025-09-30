805a1fcf2dc1a74e5640445e9772088634da981f
Jan Dvorak <hello@jandvorak.me>
2025-09-30 22:07:37 +0200

    New translations emails.js (Hindi)

--- hi-IN/emails.js
+++ hi-IN/emails.js
@@ -14,13 +14,13 @@ export default {
     // New html email keys
     'email.salutation': 'नमस्ते {username},',
     'email.welcome': 'कविता यूनिवर्स में आपका स्वागत है!',
-    'email.signature': 'आभारी,<br />कविता यूनिवर्स टीम',
-    'email.footer.settingsHtml': 'You can change your email settings in <a href="{link}">your account settings</a>.',
+    'email.signature': 'Sincerely,<br>Literary Universe team</br>',
+    'email.footer.settingsHtml': 'You can change your email settings in <link>your account settings</link>.',
     // Enrollment
     'email.enrollAccount.welcome': 'स्वागत करते हैं',
     'email.enrollAccount.messageHtml': 'We would like to invite you to Literary Universe! We went ahead and prepared a personal account for you where you can access all features of Literary Universe. Please join us in creating the next generation of creative writing and reading!',
     'email.enrollAccount.button': 'अपने खाते में पहुंचें',
-    'email.enrollAccount.signature': 'We are looking forward to seeing you,<br />Literary Universe team',
+    'email.enrollAccount.signature': 'We are looking forward to seeing you,<br>Literary Universe team</br>',
     // Reset password
     'email.resetPassword.messageHtml': 'We have received a request to reset your password for your account. Please click on the button below to continue the process. If you haven\'t made this request, you can ignore this e-mail, but it might be a good time to review your security settings.',
     'email.resetPassword.button': 'पासवर्ड रीसेट करें',
