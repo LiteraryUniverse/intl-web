ae0211a00d80e0d8cb01949347dcd4af7f20d463
Jan Dvorak <hello@jandvorak.me>
2025-10-11 00:02:54 +0200

    New translations stats.js (Czech)

--- cs-CZ/stats.js
+++ cs-CZ/stats.js
@@ -59,5 +59,6 @@ export default {
     'workshop.theWay.overallStats.demographics': 'Demografické údaje čtenářů',
     'workshop.theWay.overallStats.topCountry': 'Nejlepší země',
     'workshop.theWay.overallStats.topAgeGroup': 'Nejlepší věková skupina',
-    'workshop.theWay.overallStats.genres': 'Vaše žánry'
+    'workshop.theWay.overallStats.genres': 'Vaše žánry',
+    'stats.title': '{name} - Statistiky'
 };
