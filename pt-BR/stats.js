cda38d8eefe0951768bc8a6a95e378517687cf97
Jan Dvorak <hello@jandvorak.me>
2025-10-11 00:12:23 +0200

    New translations stats.js (Portuguese, Brazilian)

--- pt-BR/stats.js
+++ pt-BR/stats.js
@@ -59,5 +59,6 @@ export default {
     'workshop.theWay.overallStats.demographics': 'Dados demográficos dos leitores',
     'workshop.theWay.overallStats.topCountry': 'País principal',
     'workshop.theWay.overallStats.topAgeGroup': 'Melhor faixa etária',
-    'workshop.theWay.overallStats.genres': 'Seus gêneros'
+    'workshop.theWay.overallStats.genres': 'Seus gêneros',
+    'stats.title': '{name} - Estatísticas'
 };
