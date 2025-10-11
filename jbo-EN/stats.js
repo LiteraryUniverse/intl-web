61b684a3f529e5e16f291228d7f19993e8c058dd
Jan Dvorak <hello@jandvorak.me>
2025-10-11 15:54:32 +0200

    New translations stats.js (Lojban)

--- jbo-EN/stats.js
+++ jbo-EN/stats.js
@@ -31,7 +31,7 @@ export default {
     'stats.chapter.chartTooltipWordCount': 'Xrisrbirka {chapter, number}: {wordCount, number} valsi',
     'stats.totalHits': 'viska cu\'o',
     'stats.totalVisitors': 'viska se jalge cu\'o',
-    'stats.totalWords': 'valsi cu\'o',
+    'stats.totalWords': 'Total Words',
     'stats.totalEntries': 'xanri jvike',
     'stats.blogPage': 'bloti',
     'stats.blog.typeSelectionAria': 'Klesi xanri',
@@ -60,5 +60,6 @@ export default {
     'workshop.theWay.overallStats.topCountry': 'Top Country',
     'workshop.theWay.overallStats.topAgeGroup': 'Top Age Group',
     'workshop.theWay.overallStats.genres': 'Your Genres',
-    'stats.title': '{name} - Statistics'
+    'stats.title': '{name} - Statistics',
+    'stats.totalCharacters': 'Total Characters'
 };
