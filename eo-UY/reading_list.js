a31411678c70aeea214ffdc1b4d69f0b3459512f
Jan Dvorak <hello@jandvorak.me>
2025-10-11 20:47:32 +0200

    New translations reading_list.js (Esperanto)

--- eo-UY/reading_list.js
+++ eo-UY/reading_list.js
@@ -22,6 +22,13 @@ export default {
     other {rakontoj kaj universoj}
   } al la legolisto.`,
   'readingList.addItem.fromLibrary': 'El biblioteko',
+  'readingList.addItem.yourOwn': `Your own {type, select,
+    universes {universes}
+    stories {stories}
+    other {properties}
+  }`,
+  'readingList.addItem.noOwnStories': 'You have no stories yet',
+  'readingList.addItem.noOwnUniverses': 'You have no universes yet',
   'readingList.button.added': 'Aldita',
   'readingList.delete': 'Delete list',
   'readingList.delete.confirm': 'Delete Reading List',
