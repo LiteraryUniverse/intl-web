76fd6df506004c9690323efe037915a8dfff3461
Jan Dvorak <hello@jandvorak.me>
2025-10-11 18:20:11 +0200

    New translations reading_list.js (Italian)

--- it-IT/reading_list.js
+++ it-IT/reading_list.js
@@ -22,5 +22,19 @@ export default {
     other {storie e universi}
   } alla lista di lettura.`,
   'readingList.addItem.fromLibrary': 'Dalla libreria',
-  'readingList.button.added': 'Aggiunto'
+  'readingList.button.added': 'Aggiunto',
+  'readingList.delete': 'Cancellare l\'elenco',
+  'readingList.delete.confirm': 'Cancellare l\'elenco di lettura',
+  'readingList.delete.modalAria': 'Modale di conferma per l\'eliminazione dell\'elenco di lettura',
+  'readingList.delete.warning': 'È sicuro di voler eliminare l\'elenco "{name}"? Questa azione non può essere annullata.',
+  'readingList.delete.itemsInfo': 'Questo rimuoverà l\'elenco ma non le storie o gli universi stessi.',
+  'readingList.delete.confirm.button': 'Cancella elenco',
+  'readingList.delete.deleting': 'Cancellare...',
+  'readingList.public.title': 'Liste di lettura pubbliche',
+  'readingList.public.noLists': 'Non ci sono ancora liste di lettura pubbliche',
+  'readingList.public.seo.title': '{username}Liste di lettura di \'s',
+  'readingList.public.seo.description': 'Sfoglia le liste di lettura di storie e universi curate da {username}su Universo Letterario.',
+  'readingList.public.list.seo.title': '{listName} - Elenco di letture di {username}',
+  'readingList.public.list.seo.description': 'Esplori la lista di lettura curata di {username}\'{listName}\' con storie e universi su Universo Letterario.',
+  'readingList.public.list.description': 'Descrizione: {description}'
 };
