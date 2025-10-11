28859362f92917e2b2cc46b924132c2b308487dd
Jan Dvorak <jdvorak@literaryuniverse.com>
2025-10-11 19:43:52 +0200

    Reading list own

--- en/reading_list.js
+++ en/reading_list.js
@@ -22,6 +22,13 @@ export default {
     other {stories and universes}
   } to the reading list.`,
   'readingList.addItem.fromLibrary': 'From library',
+  'readingList.addItem.yourOwn': `Your own {type, select,
+    universes {universes}
+    stories {stories}
+    other {properties}
+  }`,
+  'readingList.addItem.noOwnStories': 'You have no stories yet',
+  'readingList.addItem.noOwnUniverses': 'You have no universes yet',
   'readingList.button.added': 'Added',
   'readingList.delete': 'Delete list',
   'readingList.delete.confirm': 'Delete Reading List',
