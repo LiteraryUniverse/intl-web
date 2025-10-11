3d00a8535eea45ee2cdebe9728e2744687ad805b
Jan Dvorak <hello@jandvorak.me>
2025-10-11 15:55:36 +0200

    New translations stats.js (Kannada)

--- kn-IN/stats.js
+++ kn-IN/stats.js
@@ -31,7 +31,7 @@ export default {
     'stats.chapter.chartTooltipWordCount': 'ಅಧ್ಯಾಯ {chapter, number}: {wordCount, number} ಪದಗಳು',
     'stats.totalHits': 'ಒಟ್ಟು ನೋಟಗಳು',
     'stats.totalVisitors': 'ಒಟ್ಟು ವಿಶೇಷ ಆಗಮನಿಕರು',
-    'stats.totalWords': 'ಒಟ್ಟು ಪದಗಳು',
+    'stats.totalWords': 'Total Words',
     'stats.totalEntries': 'ವಿಜ್ಞಾನಕೋಶ ಲೇಖನಗಳು',
     'stats.blogPage': 'ಪುಟ',
     'stats.blog.typeSelectionAria': 'ಟೈಪ್ ಆಯ್ಕೆ',
@@ -60,5 +60,6 @@ export default {
     'workshop.theWay.overallStats.topCountry': 'Top Country',
     'workshop.theWay.overallStats.topAgeGroup': 'Top Age Group',
     'workshop.theWay.overallStats.genres': 'Your Genres',
-    'stats.title': '{name} - Statistics'
+    'stats.title': '{name} - Statistics',
+    'stats.totalCharacters': 'Total Characters'
 };
