8990019a123ca639f04464c017ac0947d36e5b29
Jan Dvorak <hello@jandvorak.me>
2025-10-11 20:51:52 +0200

    New translations reading_list.js (Afrikaans)

--- af-ZA/reading_list.js
+++ af-ZA/reading_list.js
@@ -22,6 +22,13 @@ export default {
     other {stories en heelale}
   } by die leeslys te voeg.`,
   'readingList.addItem.fromLibrary': 'Vanaf biblioteek',
+  'readingList.addItem.yourOwn': `Your own {type, select,
+    universes {universes}
+    stories {stories}
+    other {properties}
+  }`,
+  'readingList.addItem.noOwnStories': 'You have no stories yet',
+  'readingList.addItem.noOwnUniverses': 'You have no universes yet',
   'readingList.button.added': 'Bygevoeg',
   'readingList.delete': 'Delete list',
   'readingList.delete.confirm': 'Delete Reading List',
