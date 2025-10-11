2032994c86eff59934dc6458ad890862e405e777
Jan Dvorak <hello@jandvorak.me>
2025-10-11 18:23:08 +0200

    New translations reading_list.js (Polish)

--- pl-PL/reading_list.js
+++ pl-PL/reading_list.js
@@ -22,5 +22,19 @@ export default {
     other {opowieści i wszechświatów}
   } do listy czytelniczej.`,
   'readingList.addItem.fromLibrary': 'Z biblioteki',
-  'readingList.button.added': 'Dodano'
+  'readingList.button.added': 'Dodano',
+  'readingList.delete': 'Proszę usunąć listę',
+  'readingList.delete.confirm': 'Usuń listę czytelniczą',
+  'readingList.delete.modalAria': 'Modalne potwierdzenie usunięcia listy czytelniczej',
+  'readingList.delete.warning': 'Czy na pewno chcą Państwo usunąć listę "{name}"? Tej czynności nie można cofnąć.',
+  'readingList.delete.itemsInfo': 'Spowoduje to usunięcie listy, ale nie samych historii lub światów.',
+  'readingList.delete.confirm.button': 'Usuń listę',
+  'readingList.delete.deleting': 'Usuwanie...',
+  'readingList.public.title': 'Publiczne listy czytelnicze',
+  'readingList.public.noLists': 'Nie ma jeszcze publicznych list czytelniczych',
+  'readingList.public.seo.title': '{username}Listy czytelnicze',
+  'readingList.public.seo.description': 'Proszę przejrzeć wyselekcjonowane przez {username}listy opowiadań i uniwersów na Literary Universe.',
+  'readingList.public.list.seo.title': '{listName} - Lista czytelnicza według {username}',
+  'readingList.public.list.seo.description': 'Proszę zapoznać się z wyselekcjonowaną listą lektur {username}\'{listName}\' zawierającą historie i uniwersa w serwisie Literary Universe.',
+  'readingList.public.list.description': 'Opis: {description}'
 };
