4e4721591dfc0a01adfef63e38e5f2924b0f91d2
Jan Dvorak <hello@jandvorak.me>
2025-10-11 18:18:19 +0200

    New translations reading_list.js (Finnish)

--- fi-FI/reading_list.js
+++ fi-FI/reading_list.js
@@ -22,5 +22,19 @@ export default {
     other {tarinoille ja universumeille}
   } lisäämistä varten lukulistaan.`,
   'readingList.addItem.fromLibrary': 'Kirjastosta',
-  'readingList.button.added': 'Lisätty'
+  'readingList.button.added': 'Lisätty',
+  'readingList.delete': 'Poista luettelo',
+  'readingList.delete.confirm': 'Poista lukulista',
+  'readingList.delete.modalAria': 'Vahvistusmodaliteetti lukulistan poistamista varten',
+  'readingList.delete.warning': 'Oletko varma, että haluat poistaa "{name}" -luettelon? Tätä toimenpidettä ei voi peruuttaa.',
+  'readingList.delete.itemsInfo': 'Tämä poistaa luettelon mutta ei itse tarinoita tai universumeja.',
+  'readingList.delete.confirm.button': 'Poista lista',
+  'readingList.delete.deleting': 'Poistaminen...',
+  'readingList.public.title': 'Julkiset lukulistat',
+  'readingList.public.noLists': 'Ei vielä julkisia lukulistoja',
+  'readingList.public.seo.title': '{username}\'s Reading Listat',
+  'readingList.public.seo.description': 'Selaa {username}\'n kuratoituja lukulistoja tarinoista ja universumeista Literary Universessa.',
+  'readingList.public.list.seo.title': '{listName} - Lukulista {username}',
+  'readingList.public.list.seo.description': 'Tutustu {username}\'s curated reading list \'{listName}\' featuring stories and universes on Literary Universe.',
+  'readingList.public.list.description': 'Kuvaus: {description}'
 };
