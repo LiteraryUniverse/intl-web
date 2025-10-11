3c0f9c9ad6f60fd1a85d19516f275cd64d290855
Jan Dvorak <hello@jandvorak.me>
2025-10-11 18:20:33 +0200

    New translations reading_list.js (Japanese)

--- ja-JP/reading_list.js
+++ ja-JP/reading_list.js
@@ -22,5 +22,19 @@ export default {
     other {物語と宇宙}
   }を追加するためのモーダル。`,
   'readingList.addItem.fromLibrary': 'ライブラリから',
-  'readingList.button.added': '追加されました'
+  'readingList.button.added': '追加されました',
+  'readingList.delete': 'リスト削除',
+  'readingList.delete.confirm': '読書リストの削除',
+  'readingList.delete.modalAria': 'リーディングリスト削除の確認モーダル',
+  'readingList.delete.warning': '本当に "{name}" リストを削除しますか？この操作は元に戻せません。',
+  'readingList.delete.itemsInfo': 'これでリストは削除されるが、ストーリーやユニバース自体は削除されない。',
+  'readingList.delete.confirm.button': 'リスト削除',
+  'readingList.delete.deleting': '削除...',
+  'readingList.public.title': '公開リーディング・リスト',
+  'readingList.public.noLists': '読書リストはまだ公開されていない',
+  'readingList.public.seo.title': '{username}読書リスト',
+  'readingList.public.seo.description': '{username}「Literary Universe」で、ストーリーやユニバースのキュレーションによるリーディングリストを閲覧。',
+  'readingList.public.list.seo.title': '{listName} - {username}の読書リスト',
+  'readingList.public.list.seo.description': '{username}\'の監修による読書リスト\'{listName}\' 物語と宇宙をフィーチャーした Literary Universe を探検しよう。',
+  'readingList.public.list.description': '説明： {description}'
 };
