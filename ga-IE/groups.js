db62bf718e0267ed273a8b269e069bd895d2ab5e
Jan Dvorak <hello@jandvorak.me>
2025-10-11 19:37:44 +0200

    New translations groups.js (Irish)

--- ga-IE/groups.js
+++ ga-IE/groups.js
@@ -10,6 +10,11 @@ export default {
     'groups.type.hidden': 'Folaithiúil',
     'groups.type.hiddenDesc': 'Folaithiúil - Is féidir le daoine a gcuireann tú cuireadh orthu a bheith páirteach.',
     'groups.imageAlt': '{groupName} grianghraf',
+    'groups.bannerAlt': '{groupName} banner',
+    'groups.logo.label': 'Group Logo',
+    'groups.logo.help': 'Square image, recommended 200x200px or larger',
+    'groups.banner.label': 'Group Banner',
+    'groups.banner.help': 'Wide banner image, recommended 1200x675px (16:9 ratio)',
     'groups.members': 'Baill',
     'groups.members.total': `There {totalMembers, plural,
     zero {are no members}
