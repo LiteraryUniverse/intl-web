6dad1ac70405ff79b530a07c4ef5e3b69dc74ab9
Jan Dvorak <hello@jandvorak.me>
2025-10-21 14:12:16 +0200

    New translations forums.js (Irish)

--- ga-IE/forums.js
+++ ga-IE/forums.js
@@ -65,5 +65,20 @@ export default {
   'forums.unArchive': 'Ath-ghníomhaigh na fóraim',
   'forums.unArchive.success': 'Tá an fóram athghníomhaithe!',
   'forums.universeTitle': 'Universe forums for {universeName}',
-  'forums.yourMessage': 'Do theachtaireacht'
+  'forums.yourMessage': 'Do theachtaireacht',
+  // Q&A threads
+  'forums.thread.answered': 'Answered',
+  'forums.post.selectedAnswer': 'Selected Answer',
+  'forums.post.selectAsAnswer': 'Select as Answer',
+  // Category posting permissions
+  'forums.category.postingPermission': 'Who can create threads',
+  'forums.category.postingPermission.everyone': 'Everyone',
+  'forums.category.postingPermission.owner': 'Forum owner only',
+  // System Posts
+  'forums.settings.systemPosts': 'System Posts',
+  'forums.settings.systemPosts.description': 'Configure automatic posting of system-generated content to your forum.',
+  'forums.settings.systemPosts.blogs': 'Blog Posts',
+  'forums.settings.systemPosts.blogs.enable': 'Automatically create forum threads for new blog posts',
+  'forums.settings.systemPosts.blogs.category': 'Post to category:',
+  'forums.settings.systemPosts.blogs.selectCategory': 'Select a category...'
 };
