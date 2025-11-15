cc401df1afd7f97ae7a9b5bd0be66bb48239d563
Jan Dvorak <hello@jandvorak.me>
2025-11-16 00:19:39 +0100

    New translations reader.js (Indonesian)

--- id-ID/reader.js
+++ id-ID/reader.js
@@ -15,7 +15,7 @@ export default {
     'reader.fullscreen.exit': 'Keluar dari layar penuh',
     'reader.details.universe': 'Detail alam semesta',
     'reader.details.story': 'Detail cerita',
-    'reader.darkmode': 'Ubah mode gelap',
+    'reader.darkmode': 'Toggle light',
     'reader.flowtext': 'Ubah ukuran teks',
     'reader.menu.optionsAria': 'Menu opsi pembaca',
     'reader.accessibility': 'Ubah aksesibilitas',
