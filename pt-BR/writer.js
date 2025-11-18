20a5ab7e2ebde2b57deeb8fd13406df94d098433
Jan Dvorak <hello@jandvorak.me>
2025-11-18 02:58:53 +0100

    New translations writer.js (Portuguese, Brazilian)

--- pt-BR/writer.js
+++ pt-BR/writer.js
@@ -54,8 +54,12 @@ export default {
     'writer.publish.previousChapterScheduled': 'O capítulo anterior está agendado para ser publicado em {publishDate, date, short} às {publishDate, time}',
     'writer.publish.alreadyPublished': 'Este capítulo já foi publicado em {publishedAt, date, short}. Novas versões serão publicadas imediatamente.',
     'writer.publish.betaReader': 'Beta',
+    'writer.publish.betaReader.original': 'Beta (Original)',
     'writer.publish.public': 'Publicar',
+    'writer.publish.publish': 'Publish',
+    'writer.publish.publish.original': 'Publish (Original)',
     'writer.publish.now': 'Publicar agora',
+    'writer.publish.moreOptions': 'More save options',
     'writer.publish.schedulePrevious': 'O senhor precisa publicar ou programar a publicação do capítulo anterior antes de publicar este.',
     'writer.publish.earlyAccess.title': 'Acesso antecipado para os fãs',
     'writer.publish.earlyAccess.groupLine': '{days} dias antes: {tiers} - os fãs têm acesso em {date, date, short} em {date, time}'
