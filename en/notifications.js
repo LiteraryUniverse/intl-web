101c0f7c925a787f4d64c37ae7be303476b5dd24
Jan Dvorak <jdvorak@literaryuniverse.com>
2025-12-08 17:59:23 +0100

    3 new strings for events and flashnews

--- en/notifications.js
+++ en/notifications.js
@@ -1,11 +1,13 @@
 // Notifications & flashnews
 export default {
+  'flashnews.admin': 'Flashnews Admin',
   'flashnews.create': 'Create a new news flash',
   'flashnews.startsAt': 'Start displaying this message at',
   'flashnews.endsAt': 'Stop displaying this message at',
   'flashnews.newLanguage': 'Select new language to add',
   'flashnews.onlyDisplayOn':
     'The news is going to be displayed only on the languages selected bellow (if any selected), there will be no news shown, even in the default language, in other languages.',
+  'flashnews.current': 'Active flashnews',
 
   'notifications.menu.all': 'All notification',
   'notifications.menu.following': 'Following',
