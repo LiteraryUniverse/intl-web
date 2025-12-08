46ea973458cf162cde77fdae636c891c32ec38b2
Jan Dvorak <hello@jandvorak.me>
2025-12-08 18:43:38 +0100

    New translations notifications.js (German)

--- de-DE/notifications.js
+++ de-DE/notifications.js
@@ -1,10 +1,12 @@
 // Notifications & flashnews
 export default {
+  'flashnews.admin': 'Flashnews Verwaltung',
   'flashnews.create': 'Neuen News-Flash erstellen',
   'flashnews.startsAt': 'Starte die Anzeige dieser Nachricht um',
   'flashnews.endsAt': 'Diese Nachricht nicht mehr anzeigen bei',
   'flashnews.newLanguage': 'Neue Sprache zum Hinzufügen auswählen',
   'flashnews.onlyDisplayOn': 'Die Nachrichten werden nur in den unten ausgewählten Sprachen angezeigt (falls vorhanden), in anderen Sprachen werden keine Nachrichten angezeigt, auch nicht in der Standardsprache.',
+  'flashnews.current': 'Aktive Blitznachrichten',
   'notifications.menu.all': 'Alle Benachrichtigungen',
   'notifications.menu.following': 'Folgt',
   'notifications.menu.workshop': 'Werkstatt',
