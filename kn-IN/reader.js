60d6189a04593fd1c7da2f251c3f4d30bdfb1b99
Jan Dvorak <hello@jandvorak.me>
2026-01-23 00:44:37 +0100

    New translations reader.js (Kannada)

--- kn-IN/reader.js
+++ kn-IN/reader.js
@@ -21,7 +21,7 @@ export default {
     'reader.accessibility': 'ಪ್ರವೇಶಿಸಲು ಅನುಕೂಲತೆಗಳನ್ನು ಟಾಗಲ್ ಮಾಡಿ',
     'chapter.translation.notYet': 'ಈ ಅಧ್ಯಾಯವನ್ನು ಇನ್ನೂ ಭಾಷಾಂತರಿಸಲಾಗಿಲ್ಲ.',
     'externalShopping.buyLink.amazon': 'ಆಮೆಜಾನ್‌ನಲ್ಲಿ ಖರೀದಿಸಿ',
-    'externalShopping.buyLink.other': '{storeName}ನಲ್ಲಿ ಖರಿದುಕೊಳ್ಳಿ',
+    'externalShopping.buyLink.other': 'Get on {storeName}',
     'externalShopping.buyLink.bookshop': 'ಬುಕ್‌ಶಾಪ್.ಆರ್ಜಿ ನಲ್ಲಿ ಖರೀದಿಸಿ',
     'reader.accessibility.aria': 'ಪ್ರವೇಶ್ಯತೆ ಆಯ್ಕೆಗಳು',
     'reader.accessibility.toggleDyslexic': 'ಡಿಸ್ಲೆಕ್ಸಿಕ್ ಫಾಂಟ್ ಟಾಗಲ್ ಮಾಡು',
