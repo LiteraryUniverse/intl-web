5d95258069e7b957d9cf743a48c1d87a2b2ec52c
Jan Dvorak <hello@jandvorak.me>
2026-02-12 00:06:09 +0100

    New translations discovery.js (Portuguese)

--- pt-PT/discovery.js
+++ pt-PT/discovery.js
@@ -65,6 +65,7 @@ export default {
     'discovery.isAIGenerated': 'Este trabalho foi gerado pela IA.',
     'discovery.isPreview': 'Pré-visualização',
     'discovery.isPreview.explained': 'Esta história é fornecida apenas como uma pré-visualização da obra.',
+    'discovery.includesAds': 'Inclui anúncios',
     // AI content related strings
     'discovery.ai.content': 'Conteúdo gerado por IA',
     'discovery.ai.universes': 'Universos gerados por IA',
