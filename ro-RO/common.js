03c82111014890a49ee206fb04e9517a8d7694cd
Jan Dvorak <hello@jandvorak.me>
2026-02-14 14:51:48 +0100

    New translations common.js (Romanian)

--- ro-RO/common.js
+++ ro-RO/common.js
@@ -312,5 +312,6 @@ export default {
     'common.aiContent.model': 'Model AI: {model}',
     'common.aiContent.custom': 'Model AI personalizat',
     'common.characters': 'characters',
-    'common.words': 'words'
+    'common.words': 'words',
+    'common.quickSummary': 'Rezumat rapid'
 };
