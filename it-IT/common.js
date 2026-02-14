d0148697903007f8b1b662817379977c0eda2061
Jan Dvorak <hello@jandvorak.me>
2026-02-14 14:37:45 +0100

    New translations common.js (Italian)

--- it-IT/common.js
+++ it-IT/common.js
@@ -312,5 +312,6 @@ export default {
     'common.aiContent.model': 'Modello AI: {model}',
     'common.aiContent.custom': 'Modello AI personalizzato',
     'common.characters': 'characters',
-    'common.words': 'words'
+    'common.words': 'words',
+    'common.quickSummary': 'Riepilogo rapido'
 };
