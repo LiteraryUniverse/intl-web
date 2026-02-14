9b2cfbf092fe0cb4a5863ea764d7e229c4e20b39
Jan Dvorak <hello@jandvorak.me>
2026-02-14 14:44:23 +0100

    New translations common.js (Indonesian)

--- id-ID/common.js
+++ id-ID/common.js
@@ -312,5 +312,6 @@ export default {
     'common.aiContent.model': 'Model AI: {model}',
     'common.aiContent.custom': 'Model AI khusus',
     'common.characters': 'characters',
-    'common.words': 'words'
+    'common.words': 'words',
+    'common.quickSummary': 'Ringkasan Singkat'
 };
