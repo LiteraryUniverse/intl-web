6c8515dc28ae3a9c9f26480e4d03756d6bb03030
Jan Dvorak <hello@jandvorak.me>
2026-02-17 08:48:38 +0100

    New translations discovery.js (Romanian)

--- ro-RO/discovery.js
+++ ro-RO/discovery.js
@@ -81,5 +81,6 @@ export default {
     'discovery.aiContent.indicator.model': 'Model AI: {model}',
     'discovery.recommendations.storiesTitle': 'Povestiri recomandate',
     'discovery.recommendations.recommendationsExplained': 'Aceste povestiri sunt recomandate pe baza motorului nostru de recomandări. Care utilizează învățarea automată pentru a vă recomanda povești.',
-    'discovery.recommendations.recommendationsSettings': 'Puteți dezactiva acest lucru în setările dvs.'
+    'discovery.recommendations.recommendationsSettings': 'Puteți dezactiva acest lucru în setările dvs.',
+    'discover.search.templateUniverses': 'Afișați numai universuri șablon (enciclopedii copiabile)'
 };
