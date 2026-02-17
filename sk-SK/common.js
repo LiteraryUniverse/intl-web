d4c360f8a698b865c83b464a20a244ba496f373f
Jan Dvorak <hello@jandvorak.me>
2026-02-17 08:40:17 +0100

    New translations common.js (Slovak)

--- sk-SK/common.js
+++ sk-SK/common.js
@@ -313,5 +313,6 @@ export default {
     'common.aiContent.custom': 'Vlastný model AI',
     'common.characters': 'characters',
     'common.words': 'words',
-    'common.quickSummary': 'Rýchle zhrnutie'
+    'common.quickSummary': 'Rýchle zhrnutie',
+    'common.creating': 'Vytváranie...'
 };
