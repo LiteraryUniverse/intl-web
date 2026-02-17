f459f80f4be35b6d4538b46ff37a2f44d362c9fd
Jan Dvorak <hello@jandvorak.me>
2026-02-17 08:35:04 +0100

    New translations discovery.js (Greek)

--- el-GR/discovery.js
+++ el-GR/discovery.js
@@ -81,5 +81,6 @@ export default {
     'discovery.aiContent.indicator.model': 'Μοντέλο AI: {model}',
     'discovery.recommendations.storiesTitle': 'Συνιστώμενες ιστορίες',
     'discovery.recommendations.recommendationsExplained': 'Αυτές οι ιστορίες συνιστώνται με βάση τη μηχανή συστάσεών μας. Η οποία χρησιμοποιεί μηχανική μάθηση για να σας προτείνει ιστορίες.',
-    'discovery.recommendations.recommendationsSettings': 'Μπορείτε να το απενεργοποιήσετε στις ρυθμίσεις σας.'
+    'discovery.recommendations.recommendationsSettings': 'Μπορείτε να το απενεργοποιήσετε στις ρυθμίσεις σας.',
+    'discover.search.templateUniverses': 'Εμφάνιση μόνο πρότυπων συμπάντων (αντιγράψιμες εγκυκλοπαίδειες)'
 };
