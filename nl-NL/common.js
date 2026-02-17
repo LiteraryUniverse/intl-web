bf759ea5cf8c3a155b60e70a2b9498e0bc03f251
Jan Dvorak <hello@jandvorak.me>
2026-02-17 08:38:29 +0100

    New translations common.js (Dutch)

--- nl-NL/common.js
+++ nl-NL/common.js
@@ -313,5 +313,6 @@ export default {
     'common.aiContent.custom': 'Aangepast AI-model',
     'common.characters': 'characters',
     'common.words': 'words',
-    'common.quickSummary': 'Korte samenvatting'
+    'common.quickSummary': 'Korte samenvatting',
+    'common.creating': 'Creëren...'
 };
