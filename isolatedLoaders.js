279524aa01cf57bce08af40d64b003adeb2532e6
Jan Dvorak <jdvorak@literaryuniverse.com>
2026-02-23 15:33:53 +0100

    Remove js version form isolatedLoaders

--- isolatedLoaders.js
+++
@@ -1,9 +0,0 @@
-export {
-  loadComparisonsMessages,
-  loadCookieConsentMessages,
-  loadFaqMessages,
-  loadIsolatedIntlMessages,
-  loadSignupMessages,
-  loadTheWayMessages,
-  loadToursMessages,
-} from './isolatedLoaders.ts'
