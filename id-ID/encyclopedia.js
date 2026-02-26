d931b6708293d309b4b975152814c8a8b909b4c8
Jan Dvorak <hello@jandvorak.me>
2026-02-26 17:55:44 +0100

    New translations encyclopedia.js (Indonesian)

--- id-ID/encyclopedia.js
+++ id-ID/encyclopedia.js
@@ -225,7 +225,7 @@ export default {
     'encyclopedia.templateCopy.targetUniverse': 'Univers tujuan untuk disalin',
     'encyclopedia.templateCopy.finished': 'Penyalinan selesai',
     'encyclopedia.templateCreateUniverse.modalTitle': 'Buat alam semesta dari templat',
-    'encyclopedia.templateCreateUniverse.description': 'Buat alam semesta baru dengan konten ensiklopedia yang sudah disiapkan sebelumnya dari templat ini. Alam semesta Anda akan tetap tidak dipublikasikan hingga Anda memutuskan untuk mempublikasikannya.',
+    'encyclopedia.templateCreateUniverse.description': 'Buat alam semesta baru dengan konten ensiklopedia yang sudah disiapkan sebelumnya dari templat ini. Alam semesta Anda akan tetap bersifat pribadi hingga Anda memutuskan untuk menjadikannya publik.',
     'encyclopedia.templateCreateUniverse.success': 'Alam semesta berhasil diciptakan! Mengarahkan...',
     'encyclopedia.pronunciation.audio.play': 'Mainkan pengucapan',
     'encyclopedia.pronunciation.guide': 'Gunakan <ipaLink>notasi IPA</ipaLink> jika memungkinkan. Pengucapan API Web Speech adalah upaya terbaik; lihat <webSpeechLink>dokumen MDN</webSpeechLink>.'
