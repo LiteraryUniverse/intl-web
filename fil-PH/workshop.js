2a0134b43e398d4529074bed4b04955f92a6eb73
Jan Dvorak <hello@jandvorak.me>
2026-02-27 07:18:55 +0100

    New translations workshop.js (Filipino)

--- fil-PH/workshop.js
+++ fil-PH/workshop.js
@@ -81,12 +81,12 @@ export default {
     universe {Universe}
     story {Story}
     other {Work}
-  }`,
+  } Public`,
     'workshop.work.makePublic.modalAria': `Confirmation modal for making a {type, select,
     universe {universe}
     story {story}
     other {work}
-  }`,
+  } Public`,
     'workshop.work.makePublic.warning': `Making this {type, select,
     universe {universe}
     story {story}
@@ -128,6 +128,11 @@ export default {
     'workshop.thema.quickRef': '📖 Thema Quick Reference',
     'workshop.story.new': 'Lumikha ng bagong kwento',
     'workshop.story.publishing.notice': 'Ang kwento ay awtomatikong mai-publish kapag inilathala mo ang unang kabanata.',
+    'workshop.story.published.congratulations.title': 'Congratulations! Your story is published',
+    'workshop.story.published.congratulations.heading': 'Congratulations! Your story has been published',
+    'workshop.story.published.congratulations.description': 'Your writing journey continues. Your story is now ready to be discovered and enjoyed by readers.',
+    'workshop.story.published.congratulations.publicStoryBtn': 'Go to story page',
+    'workshop.story.published.congratulations.imageAlt': 'Celebration illustration for published story',
     'workshop.settings.edit': 'I-edit ang mga setting',
     'workshop.story.publishedOn': 'Ang kuwentong ito ay na-publish noong {date, date, long} {date, time, short}',
     'workshop.story.settings': 'Mga setting ng kwento',
