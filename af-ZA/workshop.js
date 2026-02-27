ba7ca8f370537a3450f8eeb7380e00a0ae295e73
Jan Dvorak <hello@jandvorak.me>
2026-02-27 07:17:41 +0100

    New translations workshop.js (Afrikaans)

--- af-ZA/workshop.js
+++ af-ZA/workshop.js
@@ -81,12 +81,12 @@ export default {
     universe {Universe}
     story {Story}
     other {Work}
-  }`,
+  } Public`,
     'workshop.work.makePublic.modalAria': `Confirmation modal for making a {type, select,
     universe {universe}
     story {story}
     other {work}
-  }`,
+  } Public`,
     'workshop.work.makePublic.warning': `Making this {type, select,
     universe {universe}
     story {story}
@@ -128,6 +128,11 @@ export default {
     'workshop.thema.quickRef': '📖 Thema Quick Reference',
     'workshop.story.new': 'Skep \'n nuwe storie',
     'workshop.story.publishing.notice': 'Die storie sal outomaties gepubliseer word sodra jy die eerste hoofstuk publiseer.',
+    'workshop.story.published.congratulations.title': 'Congratulations! Your story is published',
+    'workshop.story.published.congratulations.heading': 'Congratulations! Your story has been published',
+    'workshop.story.published.congratulations.description': 'Your writing journey continues. Your story is now ready to be discovered and enjoyed by readers.',
+    'workshop.story.published.congratulations.publicStoryBtn': 'Go to story page',
+    'workshop.story.published.congratulations.imageAlt': 'Celebration illustration for published story',
     'workshop.settings.edit': 'Wysig instellings',
     'workshop.story.publishedOn': 'Hierdie verhaal is gepubliseer op {date, date, long} {date, time, short}',
     'workshop.story.settings': 'Storie-instellings',
