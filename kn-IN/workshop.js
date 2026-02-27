9eb288bd9ae7635dbb2a390e5f8d19bae1391ae4
Jan Dvorak <hello@jandvorak.me>
2026-02-27 07:19:01 +0100

    New translations workshop.js (Kannada)

--- kn-IN/workshop.js
+++ kn-IN/workshop.js
@@ -81,12 +81,12 @@ export default {
     universe {Universe}
     story {Story}
     other {Work}
-  }`,
+  } Public`,
     'workshop.work.makePublic.modalAria': `Confirmation modal for making a {type, select,
     universe {universe}
     story {story}
     other {work}
-  }`,
+  } Public`,
     'workshop.work.makePublic.warning': `Making this {type, select,
     universe {universe}
     story {story}
@@ -128,6 +128,11 @@ export default {
     'workshop.thema.quickRef': '📖 Thema Quick Reference',
     'workshop.story.new': 'ಹೊಸ ಕಥೆಯನ್ನು ರಚಿಸಿ',
     'workshop.story.publishing.notice': 'ಮೊದಲ ಅಧ್ಯಾಯವನ್ನು ಪ್ರಕಟಿಸುವ ಮೊದಲನೇ ಸಲ ಕಥೆ ಸ್ವಯಂಚಾಲಿತವಾಗುತ್ತದೆ.',
+    'workshop.story.published.congratulations.title': 'Congratulations! Your story is published',
+    'workshop.story.published.congratulations.heading': 'Congratulations! Your story has been published',
+    'workshop.story.published.congratulations.description': 'Your writing journey continues. Your story is now ready to be discovered and enjoyed by readers.',
+    'workshop.story.published.congratulations.publicStoryBtn': 'Go to story page',
+    'workshop.story.published.congratulations.imageAlt': 'Celebration illustration for published story',
     'workshop.settings.edit': 'ಸೆಟ್ಟಿಂಗ್‌ಗಳನ್ನು ಸಂಪಾದಿಸಿ',
     'workshop.story.publishedOn': 'ಈ ಕಥೆ ಪ್ರಕಟವಾಗಿದೆ ನೋಡಿ {date, date, long} {date, time, short}',
     'workshop.story.settings': 'ಕಥೆ ಸೆಟ್ಟಿಂಗ್‌ಗಳು',
