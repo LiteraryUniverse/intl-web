3f6529dd96aa01b84a790dc15445b156ee3948bf
Jan Dvorak <hello@jandvorak.me>
2026-02-27 21:52:14 +0100

    New translations workshop.js (Turkish)

--- tr-TR/workshop.js
+++ tr-TR/workshop.js
@@ -423,6 +423,8 @@ export default {
     'workshop.story.preventPublishing': 'Yayınlamayı önleyin',
     'workshop.story.preventPublishingTooltip': 'Etkinleştirildiğinde, bu hikaye yayınlanamaz. Bu, hala geliştirme aşamasında olan veya yalnızca özel kullanım amaçlı hikayeler için kullanışlıdır.',
     'workshop.story.preventPublishingTooltipUniverse': 'Evrende yayınlamayı engelle ayarı etkinleştirilmiş olduğundan yayınlama engellenir.',
+    'workshop.story.showChapterNumbersInListing': 'Bölüm listesinde bölüm numaralarını göster',
+    'workshop.story.showChapterNumbersInListingTooltip': 'Etkinleştirildiğinde, bölüm numaraları hikaye bölüm listelerinde okuyucular tarafından görülebilir.',
     'workshop.revisions.new.public.disabled': 'Öykü ayarlarında yayınlamayı engelle etkinleştirildiğinde yayınlanamıyor.',
     'workshop.publishing.restricted': 'İyi hal ihlali nedeniyle yayınlama yeteneğinizi devre dışı bıraktık.',
     'workshop.story.buyingSettings': 'Satış ayarları',
