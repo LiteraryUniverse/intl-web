4caa23292f65445aa5de182539650a9fb30b3c6c
Jan Dvorak <hello@jandvorak.me>
2026-03-07 10:17:12 +0100

    New translations pages.js (Marathi)

--- mr-IN/pages.js
+++ mr-IN/pages.js
@@ -40,8 +40,6 @@ export default {
     'about.team': 'The team',
     'about.history': 'History',
     'about.company': 'कंपनी',
-    'about.companyCountry.czechia': 'चेकिया',
-    'about.companyCountry.japan': 'जपान',
     'about.companyCity.prague': 'प्राग',
     'about.companyCity.sendai': 'सेंडाई',
     'about.CEO': 'Chief Executive Officer',
