b3dc47a5297c09a2d01aadb06b8ad76d5f59e627
Jan Dvorak <hello@jandvorak.me>
2026-03-14 14:45:39 +0100

    New translations courses.js (Esperanto)

--- eo-UY/courses.js
+++ eo-UY/courses.js
@@ -166,4 +166,7 @@ export default {
   'course.lesson.virtualClassroom': 'Virtual Classroom',
   'course.lesson.joinClassroom': 'Join the virtual classroom session',
   'course.lesson.enterClassroom': 'Enter Classroom',
+  'courses.role.owner': 'Posedanto',
+  'courses.role.teacher': 'Instruisto',
+  'courses.role.student': 'Studento',
 };
