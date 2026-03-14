cfc34daa4269f6580a80abb263833d10e6a21a06
Jan Dvorak <hello@jandvorak.me>
2026-03-14 14:45:17 +0100

    New translations courses.js (Telugu)

--- te-IN/courses.js
+++ te-IN/courses.js
@@ -166,4 +166,7 @@ export default {
   'course.lesson.virtualClassroom': 'Virtual Classroom',
   'course.lesson.joinClassroom': 'Join the virtual classroom session',
   'course.lesson.enterClassroom': 'Enter Classroom',
+  'courses.role.owner': 'యజమాని',
+  'courses.role.teacher': 'ఉపాధ్యాయుడు',
+  'courses.role.student': 'విద్యార్థి',
 };
