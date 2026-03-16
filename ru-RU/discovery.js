c9bf8e746caa9c4ff7edea89c4f59a70c6d0637c
Jan Dvorak <hello@jandvorak.me>
2026-03-16 17:22:08 +0100

    New translations discovery.js (Russian)

--- ru-RU/discovery.js
+++ ru-RU/discovery.js
@@ -90,5 +90,6 @@ export default {
     'discovery.recommendations.storiesTitle': 'Рекомендуемые истории',
     'discovery.recommendations.recommendationsExplained': 'Эти истории рекомендуются на основе нашей системы рекомендаций. Она использует машинное обучение, чтобы рекомендовать Вам истории.',
     'discovery.recommendations.recommendationsSettings': 'Вы можете отключить это в Ваших настройках.',
-    'discover.search.templateUniverses': 'Показывайте только шаблонные вселенные (копируемые энциклопедии)'
+    'discover.search.templateUniverses': 'Показывайте только шаблонные вселенные (копируемые энциклопедии)',
+    'discover.ratingExplained': 'Средняя оценка {avgRating, number} из {reviewsNum, number} обзора'
 };
