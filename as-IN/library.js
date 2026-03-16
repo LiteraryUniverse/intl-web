172ccf32ee672ae5e3725bc0f4c3064d8ec3d8ab
Jan Dvorak <hello@jandvorak.me>
2026-03-16 12:39:04 +0100

    New translations library.js (Assamese)

--- as-IN/library.js
+++ as-IN/library.js
@@ -7,6 +7,7 @@ export default {
     'bookmark.continueReading': 'পঢ়ি থাকক',
     'bookmark.statusSummary': 'আপুনি গল্পৰ পৰা {readPercent, number, ::percent} পঢ়িছে।',
     'library.search.prompt': 'Search your library to find content',
+    'library.search.noFilters': 'অনুগ্ৰহ কৰি অনুসন্ধান শব্দসমূহ দিয়ক বা ফিল্টাৰসমূহ প্ৰয়োগ কৰক',
     'library.yourLibrary': 'Your Library',
     'library.search': 'Search Your Library'
 };
