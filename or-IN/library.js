443efae2bcf09ae557154b8d6942d189bd67e324
Jan Dvorak <hello@jandvorak.me>
2026-03-16 12:40:12 +0100

    New translations library.js (Odia)

--- or-IN/library.js
+++ or-IN/library.js
@@ -7,6 +7,7 @@ export default {
     'bookmark.continueReading': 'ପଠନ ଜାରି ରଖନ୍ତୁ',
     'bookmark.statusSummary': 'ଆପଣ କହିବାକୁ ପଢ଼ିଛନ୍ତି {readPercent, number, ::percent} ଗଳ୍ପଗୁଡ଼ିକ |',
     'library.search.prompt': 'Search your library to find content',
+    'library.search.noFilters': 'Please enter search terms or apply filters',
     'library.yourLibrary': 'Your Library',
     'library.search': 'Search Your Library'
 };
