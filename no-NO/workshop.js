bf00f6ce94fa8057a212d471e6e2028b50efb6c8
Jan Dvorak <hello@jandvorak.me>
2026-03-18 00:56:58 +0100

    New translations workshop.js (Norwegian)

--- no-NO/workshop.js
+++ no-NO/workshop.js
@@ -133,6 +133,8 @@ export default {
     'workshop.story.published.congratulations.description': 'Your writing journey continues. Your story is now ready to be discovered and enjoyed by readers.',
     'workshop.story.published.congratulations.publicStoryBtn': 'Go to story page',
     'workshop.story.published.congratulations.imageAlt': 'Celebration illustration for published story',
+    'workshop.writingHistory.title': 'Writing history',
+    'workshop.writingHistory.sessions': 'Writing sessions',
     'workshop.settings.edit': 'Rediger innstillinger',
     'workshop.story.publishedOn': 'Denne historien ble publisert på {date, date, long} {date, time, short}',
     'workshop.story.settings': 'Historieinnstillinger',
