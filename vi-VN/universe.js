78a33ae6b19a10d34798d760586ec0db63e7e8aa
Jan Dvorak <hello@jandvorak.me>
2026-03-18 01:08:44 +0100

    New translations universe.js (Vietnamese)

--- vi-VN/universe.js
+++ vi-VN/universe.js
@@ -1,31 +1,31 @@
 export default {
     'universe.authors.title.meta': '{title} tác giả',
-    'universe.fanfiction.see': 'All Fan Fiction for {universe}',
+    'universe.fanfiction.see': 'Tất cả các tác phẩm fan fiction về {universe}',
     'universe.fanfiction.desc': 'Fan Fiction cho {universe}',
     'universe.fanfiction.for': '{universe} Fan Fiction',
     'universe.fanarts.for': '{universe} Fan Arts',
     'universe.fansettings.for': '{universe} Fan Settings',
     'universe.fanfiction.stories': 'Câu chuyện',
     'universe.fanfiction.fanarts': 'Fan Arts',
-    'universe.stories.count': `There {num, plural,
-    =0 {are no stories}
-    one {is one story}
-    other {are # stories}
-    } in this universe.`,
+    'universe.stories.count': `Không có câu chuyện nào {num, plural,
+    =0 {chỉ có một câu chuyện}
+    one {có # câu chuyện}
+    other {trong vũ trụ này.}
+    }`,
     'universe.authors.title': 'Tác giả',
     'universe.authors.desc': 'Người tạo và đóng góp cho {universe}',
     'universe.contributors': 'Người đóng góp',
     'universe.translators': 'Các dịch giả',
     'universe.authors.notice': 'Mỗi câu chuyện có thể có thêm nhân sự đã làm việc trên đó. Để biết thêm chi tiết, xem thông tin chi tiết của câu chuyện.',
     'universe.createdBy': `{gender, select,
-    male {Created}
-    female {Created}
-    other {Created}} by `,
-    'universe.stories.for': 'Stories for {universe}',
-    'universe.stories.own': '{universe} stories',
-    'universe.stories.desc': 'Cannon stories for {universe}',
-    'universe.encyclopedia.title': '{universe} encyclopedia',
-    'universe.encyclopedia.desc': 'All the details for {universe}',
+    male {Được tạo bởi}
+    female {Được tạo bởi}
+    other {Được tạo bởi}} bởi `,
+    'universe.stories.for': 'Các bài viết về {universe}',
+    'universe.stories.own': '{universe} câu chuyện',
+    'universe.stories.desc': 'Những câu chuyện về súng đại bác dành cho {universe}',
+    'universe.encyclopedia.title': '{universe} bách khoa toàn thư',
+    'universe.encyclopedia.desc': 'Tất cả thông tin chi tiết về {universe}',
     'universe.fanArt.submit': 'Submit your art',
     'universe.fanArtHeaderDesc.item': '{universe} nghệ thuật của fan - {name}',
     'universe.fanArtHeaderDesc.submit': '{universe} nộp nghệ thuật của fan',
