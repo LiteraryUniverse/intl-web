6efc50763ddf7c54f4eb4b772cbad1893917c474
Jan Dvorak <hello@jandvorak.me>
2026-03-18 01:10:40 +0100

    New translations comments.js (Bengali)

--- bn-BD/comments.js
+++ bn-BD/comments.js
@@ -1,10 +1,10 @@
 export default {
     'comment.write': 'আপনার মন্তব্য লিখুন',
     'comments.none': 'দুঃখিত, কোন মন্তব্য দেখানো যায় না।',
-    'comments.total': `There {count, plural,
-    =0 {are no comments}
-    one {is 1 comment}
-    other {are # comments}
-  }.`,
+    'comments.total': `সেখানে {count, plural,
+    =0 {কোনো মন্তব্য নেই}
+    one {১টি মন্তব্য আছে}
+    other {#টি মন্তব্য আছে}
+  }।`,
     'comments.show.older': 'পুরানো মন্তব্য দেখান'
 };
