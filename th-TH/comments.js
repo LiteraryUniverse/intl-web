575d25341ef6f02a73c60ea2cb2de9c1fe911a7d
Jan Dvorak <hello@jandvorak.me>
2026-03-18 01:11:15 +0100

    New translations comments.js (Thai)

--- th-TH/comments.js
+++ th-TH/comments.js
@@ -1,10 +1,10 @@
 export default {
     'comment.write': 'เขียนความคิดเห็นของคุณ',
     'comments.none': 'ขออภัย ไม่มีความคิดเห็นที่จะแสดง',
-    'comments.total': `There {count, plural,
-    =0 {are no comments}
-    one {is 1 comment}
-    other {are # comments}
+    'comments.total': `มี {count, plural,
+    =0 {ไม่มีความคิดเห็น}
+    one {มี 1 ความคิดเห็น}
+    other {มี # ความคิดเห็น}
   }.`,
     'comments.show.older': 'แสดงความคิดเห็นเก่ากว่า'
 };
