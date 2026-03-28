96779fe8e0fb5fdba55643ffb7dc7475a09da66b
Jan Dvorak <hello@jandvorak.me>
2026-03-29 00:19:57 +0100

    New translations sharing.js (Portuguese, Brazilian)

--- pt-BR/sharing.js
+++ pt-BR/sharing.js
@@ -65,5 +65,6 @@ export default {
     // Canvas labels
     'sharing.canvas.by': 'por',
     'sharing.canvas.chapter': 'Capítulo {chapterNumber}',
-    'sharing.canvas.publishesOn': 'Publica em {date}'
+    'sharing.canvas.publishesOn': 'Publica em {date}',
+    'sharing.advanced.shareQR': 'Share QR Link'
 };
