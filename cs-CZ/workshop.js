e49fe41c67ec73be0b6c34efeece318f93c11af2
Jan Dvorak <hello@jandvorak.me>
2026-03-28 19:10:27 +0100

    New translations workshop.js (Czech)

--- cs-CZ/workshop.js
+++ cs-CZ/workshop.js
@@ -214,6 +214,7 @@ export default {
     'workshop.monetization.saved': 'Uložení nastavení cen.',
     'workshop.monetization.synced': 'Synchronizace produktů a cen služby Stripe.',
     'workshop.monetization.syncStripe': 'Synchronizace se službou Stripe',
+    'workshop.monetization.fullStoryUnfinishedWarning': 'You can set a full story price before the story is complete, but the full-story purchase option will not activate until the story is marked as finished.',
     'workshop.collaborators.premium': `Chcete-li přidat více {type, select,
     collaborator {spolupracovníků}
     beta {beta čtenářů}
