4b53f7cd07bc67681a358bca18ec93fa3167be77
Jan Dvorak <hello@jandvorak.me>
2026-03-28 19:10:46 +0100

    New translations workshop.js (Portuguese, Brazilian)

--- pt-BR/workshop.js
+++ pt-BR/workshop.js
@@ -214,6 +214,7 @@ export default {
     'workshop.monetization.saved': 'Configurações de preços salvas.',
     'workshop.monetization.synced': 'Produtos e preços do Stripe sincronizados.',
     'workshop.monetization.syncStripe': 'Sincronização com o Stripe',
+    'workshop.monetization.fullStoryUnfinishedWarning': 'You can set a full story price before the story is complete, but the full-story purchase option will not activate until the story is marked as finished.',
     'workshop.collaborators.premium': `Para adicionar mais {type, select,
     collaborator {colaboradores}
     beta {leitores beta}
