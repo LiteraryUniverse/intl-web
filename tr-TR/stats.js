3cda6036c6f72158845d440003cbcc03895025c2
Jan Dvorak <hello@jandvorak.me>
2026-04-10 10:45:25 +0700

    New translations stats.js (Turkish)

--- tr-TR/stats.js
+++ tr-TR/stats.js
@@ -108,6 +108,8 @@ export default {
     'stats.storyReadingAnalytics.weekHourTooltip': '{day} at {hour}: {readers, number} readers, {sessions, number} sessions, {duration}',
     'stats.storyReadingAnalytics.topWindows': 'Top reading windows',
     'stats.storyReadingAnalytics.loadError': 'Failed to load reading analytics',
+    // Writing history summary cards
+    'stats.writingHistory.diaryWords': 'Günlük kelimeleri',
     // Writing history day tooltip - per-category breakdown
     'stats.writingHistory.dayTooltip.stories': 'Hikayeler: {words}',
     'stats.writingHistory.dayTooltip.encyclopedia': 'Ansiklopedi: {words}',
