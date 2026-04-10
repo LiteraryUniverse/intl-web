119283c74807d2e48d0954eba613d8761363f36b
Jan Dvorak <hello@jandvorak.me>
2026-04-10 10:39:50 +0700

    New translations common.js (Hungarian)

--- hu-HU/common.js
+++ hu-HU/common.js
@@ -202,6 +202,7 @@ export default {
     'a11y.user.menu': 'Felhasználói menü',
     'a11y.navigation': 'Navigáció',
     'dashboard.profile': 'Profilod',
+    'dashboard.writerResume': 'Írói önéletrajz',
     'profile.website': 'Személyes weboldalam',
     'story.notes.concept': 'Koncepció',
     'story.notes.settings': 'Történet beállítása',
