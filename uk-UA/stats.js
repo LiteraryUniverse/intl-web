00008310a4827d8fabe1892446c4c204837ab6f5
Jan Dvorak <hello@jandvorak.me>
2026-04-10 10:45:48 +0700

    New translations stats.js (Ukrainian)

--- uk-UA/stats.js
+++ uk-UA/stats.js
@@ -108,6 +108,8 @@ export default {
     'stats.storyReadingAnalytics.weekHourTooltip': '{day} at {hour}: {readers, number} readers, {sessions, number} sessions, {duration}',
     'stats.storyReadingAnalytics.topWindows': 'Top reading windows',
     'stats.storyReadingAnalytics.loadError': 'Failed to load reading analytics',
+    // Writing history summary cards
+    'stats.writingHistory.diaryWords': 'Слова зі щоденника',
     // Writing history day tooltip - per-category breakdown
     'stats.writingHistory.dayTooltip.stories': 'Історії: {words}',
     'stats.writingHistory.dayTooltip.encyclopedia': 'Енциклопедія: {words}',
