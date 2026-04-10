422e7e595ce3f6105100f28ae00a803ae59cbc22
Jan Dvorak <hello@jandvorak.me>
2026-04-10 10:48:51 +0700

    New translations stats.js (Estonian)

--- et-EE/stats.js
+++ et-EE/stats.js
@@ -108,6 +108,8 @@ export default {
     'stats.storyReadingAnalytics.weekHourTooltip': '{day} at {hour}: {readers, number} readers, {sessions, number} sessions, {duration}',
     'stats.storyReadingAnalytics.topWindows': 'Top reading windows',
     'stats.storyReadingAnalytics.loadError': 'Failed to load reading analytics',
+    // Writing history summary cards
+    'stats.writingHistory.diaryWords': 'Päeviku sõnad',
     // Writing history day tooltip - per-category breakdown
     'stats.writingHistory.dayTooltip.stories': 'Lood: {words}',
     'stats.writingHistory.dayTooltip.encyclopedia': 'Entsüklopeedia: {words}',
