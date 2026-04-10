4f3e982bd390e559a141dc7dcfc582c3455b5e6c
Jan Dvorak <hello@jandvorak.me>
2026-04-10 13:33:36 +0700

    Update source file common.js

--- en/common.js
+++ en/common.js
@@ -218,7 +218,7 @@ export default {
   'a11y.user.menu': 'User menu',
   'a11y.navigation': 'Navigation',
   'dashboard.profile': 'Your profile',
-  'dashboard.writerResume': "Writer's Resume",
+  'dashboard.writerResume': "Author's Resume",
   'profile.website': 'My personal website',
   'story.notes.concept': 'Concept',
   'story.notes.settings': 'Story setting',
