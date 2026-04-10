c40f10b58965d89e5405d6cfb938850084851991
Jan Dvorak <hello@jandvorak.me>
2026-04-10 10:48:05 +0700

    New translations stats.js (Bengali)

--- bn-BD/stats.js
+++ bn-BD/stats.js
@@ -108,6 +108,8 @@ export default {
     'stats.storyReadingAnalytics.weekHourTooltip': '{day} at {hour}: {readers, number} readers, {sessions, number} sessions, {duration}',
     'stats.storyReadingAnalytics.topWindows': 'Top reading windows',
     'stats.storyReadingAnalytics.loadError': 'Failed to load reading analytics',
+    // Writing history summary cards
+    'stats.writingHistory.diaryWords': 'ডায়েরির শব্দ',
     // Writing history day tooltip - per-category breakdown
     'stats.writingHistory.dayTooltip.stories': 'গল্প: {words}',
     'stats.writingHistory.dayTooltip.encyclopedia': 'এনসাইক্লোপিডিয়া: {words}',
