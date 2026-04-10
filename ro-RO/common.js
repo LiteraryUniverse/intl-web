54a86c3e97d542671ac9bcdf7e81f6f934a04e75
Jan Dvorak <hello@jandvorak.me>
2026-04-10 13:48:21 +0700

    New translations common.js (Romanian)

--- ro-RO/common.js
+++ ro-RO/common.js
@@ -202,7 +202,7 @@ export default {
     'a11y.user.menu': 'Meniu utilizator',
     'a11y.navigation': 'Navigare',
     'dashboard.profile': 'Profilul dumneavoastră',
-    'dashboard.writerResume': 'CV-ul scriitorului',
+    'dashboard.writerResume': 'CV-ul autorului',
     'profile.website': 'Website-ul meu personal',
     'story.notes.concept': 'Concept',
     'story.notes.settings': 'Cadru al poveștii',
