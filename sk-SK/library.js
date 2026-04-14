ebca59b0c877d3f5bd3db9af7c3b15e9061f0eb6
Jan Dvorak <hello@jandvorak.me>
2026-04-14 15:37:19 +0700

    New translations library.js (Slovak)

--- sk-SK/library.js
+++ sk-SK/library.js
@@ -9,5 +9,6 @@ export default {
     'library.search.prompt': 'Search your library to find content',
     'library.search.noFilters': 'Zadajte prosím vyhľadávacie výrazy alebo použite filtre',
     'library.yourLibrary': 'Your Library',
-    'library.search': 'Search Your Library'
+    'library.search': 'Search Your Library',
+    'library.ownedStories': 'Zakúpené príbehy'
 };
