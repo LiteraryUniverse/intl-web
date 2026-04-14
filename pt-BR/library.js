5b46733555a27dc4c6620391c98417f1088597c4
Jan Dvorak <hello@jandvorak.me>
2026-04-14 15:39:46 +0700

    New translations library.js (Portuguese, Brazilian)

--- pt-BR/library.js
+++ pt-BR/library.js
@@ -9,5 +9,6 @@ export default {
     'library.search.prompt': 'Search your library to find content',
     'library.search.noFilters': 'Digite os termos de pesquisa ou aplique filtros',
     'library.yourLibrary': 'Your Library',
-    'library.search': 'Search Your Library'
+    'library.search': 'Search Your Library',
+    'library.ownedStories': 'Histórias compradas'
 };
