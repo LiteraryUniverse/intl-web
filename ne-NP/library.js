b821c827d868e20b5ea2bfa3eabcccbc2e2c28f7
Jan Dvorak <hello@jandvorak.me>
2026-04-14 15:43:42 +0700

    New translations library.js (Nepali)

--- ne-NP/library.js
+++ ne-NP/library.js
@@ -9,5 +9,6 @@ export default {
     'library.search.prompt': 'Search your library to find content',
     'library.search.noFilters': 'कृपया खोज शब्दहरू प्रविष्ट गर्नुहोस् वा फिल्टरहरू लागू गर्नुहोस्।',
     'library.yourLibrary': 'Your Library',
-    'library.search': 'Search Your Library'
+    'library.search': 'Search Your Library',
+    'library.ownedStories': 'खरिद गरिएका कथाहरू'
 };
