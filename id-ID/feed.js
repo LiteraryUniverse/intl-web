fc02175126159595c157a5f9a6c653694cfec2e3
Jan Dvorak <hello@jandvorak.me>
2026-04-16 01:29:31 +0700

    New translations feed.js (Indonesian)

--- id-ID/feed.js
+++ id-ID/feed.js
@@ -3,7 +3,6 @@ export default {
     'feed.post.new': 'Pos baru',
     'feed.post.like': 'Suka pos',
     'feed.post.unlike': 'Batal suka pos',
-    'feed.post.send': 'Kiriman',
     'feed.empty': 'Tidak ada entri dalam feed.',
     'feed.placeholder': 'Ada yang ada di pikiranmu?'
 };
