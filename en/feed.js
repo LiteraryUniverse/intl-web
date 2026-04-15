b421f5b281b2e16ba670fc5543d2e6a91626f7dd
Jan Dvorak <hello@jandvorak.me>
2026-04-16 01:10:23 +0700

    Update source file feed.js

--- en/feed.js
+++ en/feed.js
@@ -3,6 +3,7 @@ export default {
   'feed.post.new': 'New post',
   'feed.post.like': 'Like post',
   'feed.post.unlike': 'Unlike post',
+  'feed.post.send': 'Post',
   'feed.empty': 'No entries in the feed.',
   'feed.placeholder': "What's on your mind?"
 }
