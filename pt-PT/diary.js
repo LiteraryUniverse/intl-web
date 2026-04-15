cb550eb45297183371640337396b17273d736cee
Jan Dvorak <hello@jandvorak.me>
2026-04-16 01:36:08 +0700

    New translations diary.js (Portuguese)

--- pt-PT/diary.js
+++ pt-PT/diary.js
@@ -39,7 +39,6 @@ export default {
  'diary.settings.includeDiaryInStats': 'Inclua a contagem de palavras do diário no total das estatísticas de escrita',
  'diary.settings.prompts': 'Writing Prompts',
  'diary.settings.writingPromptsEnabled': 'Show writing prompts when starting a new diary entry',
- 'diary.settings.dashboard': 'Dashboard',
  'diary.settings.showOnDashboard': 'Show diary reminder on the dashboard',
  // Blog post creation
  'diary.createBlogPost.title': 'Create Blog Post from Diary Entry',
