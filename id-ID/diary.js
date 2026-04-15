e15d681d7633a209e84815cd378fc80ef5671f11
Jan Dvorak <hello@jandvorak.me>
2026-04-16 01:36:19 +0700

    New translations diary.js (Indonesian)

--- id-ID/diary.js
+++ id-ID/diary.js
@@ -39,7 +39,6 @@ export default {
  'diary.settings.includeDiaryInStats': 'Sertakan jumlah kata buku harian dalam statistik penulisan total',
  'diary.settings.prompts': 'Writing Prompts',
  'diary.settings.writingPromptsEnabled': 'Show writing prompts when starting a new diary entry',
- 'diary.settings.dashboard': 'Dashboard',
  'diary.settings.showOnDashboard': 'Show diary reminder on the dashboard',
  // Blog post creation
  'diary.createBlogPost.title': 'Create Blog Post from Diary Entry',
