c60a215e6cae2f6cbaa0013cdee336e5fec553d1
Jan Dvorak <hello@jandvorak.me>
2026-04-16 01:24:00 +0700

    New translations feed.js (Polish)

--- pl-PL/feed.js
+++ pl-PL/feed.js
@@ -3,7 +3,6 @@ export default {
     'feed.post.new': 'Nowy post',
     'feed.post.like': 'Nowy post',
     'feed.post.unlike': 'W odróżnieniu od poczty',
-    'feed.post.send': 'Post',
     'feed.empty': 'Brak wpisów w streame.',
     'feed.placeholder': 'Co Ci chodzi po głowie?'
 };
