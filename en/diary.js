3b3c328a3021ea742fd8d62731f4959c18b07409
Jan Dvorak <hello@jandvorak.me>
2026-04-16 01:10:19 +0700

    Update source file diary.js

--- en/diary.js
+++ en/diary.js
@@ -49,6 +49,7 @@ export default {
 	'diary.settings.prompts': 'Writing Prompts',
 	'diary.settings.writingPromptsEnabled':
 		'Show writing prompts when starting a new diary entry',
+	'diary.settings.dashboard': 'Dashboard',
 	'diary.settings.showOnDashboard':
 		'Show diary reminder on the dashboard',
 
