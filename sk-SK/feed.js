1ca4496eb162535229a779c2e2d9ce8f1952d752
Jan Dvorak <hello@jandvorak.me>
2026-04-16 01:25:38 +0700

    New translations feed.js (Slovak)

--- sk-SK/feed.js
+++ sk-SK/feed.js
@@ -3,7 +3,6 @@ export default {
     'feed.post.new': 'Nový príspevok',
     'feed.post.like': 'Páči sa mi príspevok',
     'feed.post.unlike': 'Na rozdiel od príspevku',
-    'feed.post.send': 'Príspevok',
     'feed.empty': 'Vo feede nie sú žiadne záznamy.',
     'feed.placeholder': 'Čo máte na mysli?'
 };
