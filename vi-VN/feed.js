ded5a45f6e070efe4a0f1fabf02a71b4c958f876
Jan Dvorak <hello@jandvorak.me>
2026-04-16 01:28:24 +0700

    New translations feed.js (Vietnamese)

--- vi-VN/feed.js
+++ vi-VN/feed.js
@@ -3,7 +3,6 @@ export default {
     'feed.post.new': 'Bài viết mới',
     'feed.post.like': 'Thích bài viết',
     'feed.post.unlike': 'Bỏ thích bài viết',
-    'feed.post.send': 'Bài viết',
     'feed.empty': 'Không có bài viết trong dòng thời gian.',
     'feed.placeholder': 'Bạn đang nghĩ gì?'
 };
