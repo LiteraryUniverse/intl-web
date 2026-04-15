ab40e9e2ad2764ab9e11d701e128cbef5ae9a340
Jan Dvorak <hello@jandvorak.me>
2026-04-16 01:26:12 +0700

    New translations feed.js (Swedish)

--- sv-SE/feed.js
+++ sv-SE/feed.js
@@ -3,7 +3,6 @@ export default {
     'feed.post.new': 'Nytt inlägg',
     'feed.post.like': 'Gilla inlägg',
     'feed.post.unlike': 'Till skillnad från inlägget',
-    'feed.post.send': 'Inlägg',
     'feed.empty': 'Inga poster i matningen.',
     'feed.placeholder': 'Vad tänker du på?'
 };
