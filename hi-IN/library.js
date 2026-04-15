5569071967b343ae25852319fb0cdd7de52eb0a2
Jan Dvorak <hello@jandvorak.me>
2026-04-16 01:32:41 +0700

    New translations library.js (Hindi)

--- hi-IN/library.js
+++ hi-IN/library.js
@@ -8,7 +8,6 @@ export default {
     'bookmark.statusSummary': 'आपने कहानी का {readPercent, number, ::percent} पढ़ा है।',
     'library.search.prompt': 'Search your library to find content',
     'library.search.noFilters': 'कृपया खोज शब्द दर्ज करें या फ़िल्टर लागू करें',
-    'library.yourLibrary': 'Your Library',
     'library.search': 'Search Your Library',
     'library.ownedStories': 'खरीदी गई कहानियाँ'
 };
