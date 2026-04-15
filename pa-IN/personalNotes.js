9a3e5649d4e3244bbddd7b1e1e83442e724f5be2
Jan Dvorak <hello@jandvorak.me>
2026-04-16 01:23:48 +0700

    New translations personalnotes.js (Punjabi)

--- pa-IN/personalNotes.js
+++ pa-IN/personalNotes.js
@@ -16,12 +16,8 @@ export default {
   'personalNotes.chapterName': 'ਅਧਿਆਯ {sequence} - {title}',
   'personalNotes.objectPersonalNotes': '{object} ਨਿੱਜੀ ਨੋਟ',
   'personalNotes.searchResults': 'ਖੋਜ ਨਤੀਜੇ',
-  'personalNotes.type.story': 'ਕਹਾਣੀ',
-  'personalNotes.type.universe': 'ਯੂਨੀਵਰਸ',
-  'personalNotes.type.chapter': 'ਅਧਿਆਯ',
   'personalNotes.type.encEntry': 'ਇੰਸਾਇਕਲੋਪੀਡੀਆ ਐਂਟਰੀ',
   'personalNotes.type.revTextBlock': 'ਸੰਪਾਦਨ ਟੈਕਸਟ ਬਲਾਕ',
-  'personalNotes.chapter': 'ਅਧਿਆਯ',
   'personalNotes.recentSearches': 'ਤਾਜ਼ਾ ਖੋਜਾਂ:',
   'personalNotes.myNotes': 'My Personal Notes',
   'personalNotes.hasNotes': 'Has Personal Notes'
