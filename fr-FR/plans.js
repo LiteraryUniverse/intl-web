06d5d5b51dcda6aaed9e0c6f671e94d21652cc40
Jan Dvorak <hello@jandvorak.me>
2026-04-16 17:37:24 +0700

    New translations plans.js (French)

--- fr-FR/plans.js
+++ fr-FR/plans.js
@@ -327,6 +327,7 @@ export default {
     'plans.fanClub.access.permanentChip': 'In your library',
     'plans.fanClub.access.earnedProgress': '{current} of {total} months',
     'plans.fanClub.access.emptyStoryAccess': 'No story access benefits are attached to your membership yet.',
+    'plans.fanClub.access.unlocksLabel': 'Déverrouillé par',
     'plans.benefits.giftAfter': 'Don après {months} mois',
     'plans.benefits.giftGranted': 'Cadeau débloqué',
     'plans.reader.earlyAccess.until': 'Disponible pour les abonnés jusqu\'à {date}',
