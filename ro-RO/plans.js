11c7d0d3477af0828e76ef1844009ba8159fa822
Jan Dvorak <hello@jandvorak.me>
2026-04-16 17:37:23 +0700

    New translations plans.js (Romanian)

--- ro-RO/plans.js
+++ ro-RO/plans.js
@@ -327,6 +327,7 @@ export default {
     'plans.fanClub.access.permanentChip': 'In your library',
     'plans.fanClub.access.earnedProgress': '{current} of {total} months',
     'plans.fanClub.access.emptyStoryAccess': 'No story access benefits are attached to your membership yet.',
+    'plans.fanClub.access.unlocksLabel': 'Deblocat prin',
     'plans.benefits.giftAfter': 'Cadou după {months} luni',
     'plans.benefits.giftGranted': 'Cadou deblocat',
     'plans.reader.earlyAccess.until': 'Disponibil pentru abonați până la {date}',
