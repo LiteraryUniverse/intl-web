0df050325f18234afd28d4157451dee44c27c841
Jan Dvorak <hello@jandvorak.me>
2026-04-16 17:37:46 +0700

    New translations plans.js (Ukrainian)

--- uk-UA/plans.js
+++ uk-UA/plans.js
@@ -327,6 +327,7 @@ export default {
     'plans.fanClub.access.permanentChip': 'In your library',
     'plans.fanClub.access.earnedProgress': '{current} of {total} months',
     'plans.fanClub.access.emptyStoryAccess': 'No story access benefits are attached to your membership yet.',
+    'plans.fanClub.access.unlocksLabel': 'Розблоковано через',
     'plans.benefits.giftAfter': 'Подарунок через {months} місяців',
     'plans.benefits.giftGranted': 'Подарунок розблоковано',
     'plans.reader.earlyAccess.until': 'Доступно для передплатників до {date}',
