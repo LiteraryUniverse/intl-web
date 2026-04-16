ecea10e140348101e9b02e9472b1a71e30b104e0
Jan Dvorak <hello@jandvorak.me>
2026-04-16 17:38:10 +0700

    New translations plans.js (Dutch)

--- nl-NL/plans.js
+++ nl-NL/plans.js
@@ -327,6 +327,7 @@ export default {
     'plans.fanClub.access.permanentChip': 'In your library',
     'plans.fanClub.access.earnedProgress': '{current} of {total} months',
     'plans.fanClub.access.emptyStoryAccess': 'No story access benefits are attached to your membership yet.',
+    'plans.fanClub.access.unlocksLabel': 'Ontgrendeld via',
     'plans.benefits.giftAfter': 'Gift na {months} maanden',
     'plans.benefits.giftGranted': 'Cadeau ontgrendeld',
     'plans.reader.earlyAccess.until': 'Beschikbaar voor abonnees tot {date}',
