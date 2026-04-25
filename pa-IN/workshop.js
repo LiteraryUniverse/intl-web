f08eb50b131a4cda1e56671dcafcc93a34a23d5a
Jan Dvorak <hello@jandvorak.me>
2026-04-25 23:34:01 +0700

    New translations workshop.js (Punjabi)

--- pa-IN/workshop.js
+++ pa-IN/workshop.js
@@ -348,6 +348,9 @@ export default {
     'workshop.shoppingExternal.storeName': 'ਸਟੋਰ ਦਾ ਨਾਮ',
     'workshop.shoppingExternal.itemUrl': 'ਆਈਟਮ URL',
     'workshop.shoppingExternal.storesOther': 'ਹੋਰ ਦੁਕਾਨਾਂ',
+    'workshop.shoppingExternal.copyFromOriginal': 'Copy from original',
+    'workshop.shoppingExternal.copyFromOriginalHelp': 'Pre-fill this translation\'s links with the values from the original. Review and save to apply.',
+    'workshop.shoppingExternal.originalReadOnly': 'Only collaborators can edit the original story\'s external sales links. Switch to a translation tab to edit links for your language.',
     'fictionalTime.units.milliseconds': '{units, number} ਮਿਲੀਸਕਿੰਟ',
     'fictionalTime.countdownToStart': 'ਸ਼ੁਰੂ ਹੋਣ ਦੇ ਲਈ ਖੁੰਜਾਂ ਵਾਲਾ ਟਾਈਮ',
     'fictionalTime.unitsToETList': `{units, number} {unitName} = 
