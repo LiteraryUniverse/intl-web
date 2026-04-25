ac2c9628db2fcba13811ef9238906d9f8e2153d5
Jan Dvorak <hello@jandvorak.me>
2026-04-25 23:34:32 +0700

    New translations workshop.js (Nepali)

--- ne-NP/workshop.js
+++ ne-NP/workshop.js
@@ -348,6 +348,9 @@ export default {
     'workshop.shoppingExternal.storeName': 'स्टोर नाम',
     'workshop.shoppingExternal.itemUrl': 'वस्तु URL',
     'workshop.shoppingExternal.storesOther': 'अन्य स्टोरहरू',
+    'workshop.shoppingExternal.copyFromOriginal': 'Copy from original',
+    'workshop.shoppingExternal.copyFromOriginalHelp': 'Pre-fill this translation\'s links with the values from the original. Review and save to apply.',
+    'workshop.shoppingExternal.originalReadOnly': 'Only collaborators can edit the original story\'s external sales links. Switch to a translation tab to edit links for your language.',
     'fictionalTime.units.milliseconds': '{units, number} मिलिसेकेण्ड',
     'fictionalTime.countdownToStart': 'समय सुरु हुने गणना',
     'fictionalTime.unitsToETList': `{units, number} {unitName} = 
