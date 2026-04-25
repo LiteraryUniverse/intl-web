ae11ed05e88637636b69801bbeef2f2e3b3f3e46
Jan Dvorak <hello@jandvorak.me>
2026-04-25 23:33:39 +0700

    New translations workshop.js (French)

--- fr-FR/workshop.js
+++ fr-FR/workshop.js
@@ -348,6 +348,9 @@ export default {
     'workshop.shoppingExternal.storeName': 'Nom de la boutique',
     'workshop.shoppingExternal.itemUrl': 'URL de l\'article',
     'workshop.shoppingExternal.storesOther': 'Autres magasins',
+    'workshop.shoppingExternal.copyFromOriginal': 'Copy from original',
+    'workshop.shoppingExternal.copyFromOriginalHelp': 'Pre-fill this translation\'s links with the values from the original. Review and save to apply.',
+    'workshop.shoppingExternal.originalReadOnly': 'Only collaborators can edit the original story\'s external sales links. Switch to a translation tab to edit links for your language.',
     'fictionalTime.units.milliseconds': '{units, number} millisecondes',
     'fictionalTime.countdownToStart': 'Compte à rebours pour le début du temps',
     'fictionalTime.unitsToETList': `{units, number} {unitName} = 
